75763d0c026f13bb477d9058d1d3bdde0f6b52f8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:19 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/data_feeds.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Data Feeds
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DataFeeds.!error.feed.valid'] = 'Veuillez saisir le nom du flux de données.';
+$lang['DataFeeds.!error.dir.empty'] = 'Veuillez entrer le répertoire du plugin.';
+$lang['DataFeeds.!error.class.valid'] = 'La classe fournie n\'est pas une classe de flux de données valide.';
+$lang['DataFeeds.!error.company_id.exists'] = 'ID de l\'entreprise non valide.';
+$lang['DataFeeds.!error.endpoint.valid'] = 'Veuillez saisir le point final.';
+$lang['DataFeeds.!error.enabled.valid'] = 'L\'option activée doit être réglée sur 1 ou 0.';
+$lang['DataFeeds.execute.endpoint_not_found'] = 'Le point d\'accès demandé n\'existe pas.';
+
