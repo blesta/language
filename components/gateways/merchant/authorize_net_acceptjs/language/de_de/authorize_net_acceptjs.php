815672ce7c4aee5a7c9413386f8aaafe1c3c7c2d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:56 -0800

    Updated de_de language files

---
+++ components/gateways/merchant/authorize_net_acceptjs/language/de_de/authorize_net_acceptjs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Authorize Net Acceptjs
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AuthorizeNetAcceptjs.charge_description'] = 'Gebühr für %1$s';
+$lang['AuthorizeNetAcceptjs.charge_description_default'] = 'Entgelt für einen bestimmten Betrag';
+$lang['AuthorizeNetAcceptjs.sandbox_note'] = 'Wenn Sie diese Option aktivieren, werden Transaktionen in der Authorize.Net Sandbox-Umgebung gebucht. Aktivieren Sie diese Option nur, wenn Sie mit einem Authorize.Net Sandbox-Konto testen.';
+$lang['AuthorizeNetAcceptjs.sandbox'] = 'Sandkasten';
+$lang['AuthorizeNetAcceptjs.transaction_key'] = 'Transaktionsschlüssel';
+$lang['AuthorizeNetAcceptjs.login_id'] = 'Anmelde-ID';
+$lang['AuthorizeNetAcceptjs.field_expiration'] = 'Verfallsdatum';
+$lang['AuthorizeNetAcceptjs.field_security'] = 'Sicherheitscode';
+$lang['AuthorizeNetAcceptjs.field_number'] = 'Nummer';
+$lang['AuthorizeNetAcceptjs.description'] = 'Senden Sie sichere Zahlungsdaten direkt an Authorize.net. Accept.js erfasst die Zahlungsdaten und sendet sie direkt an Authorize.net.';
+$lang['AuthorizeNetAcceptjs.name'] = 'Authorize.Net Accept.js';
+$lang['AuthorizeNetAcceptjs.!error.sandbox.valid'] = 'Sandbox muss auf "true" gesetzt werden, falls angegeben.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.valid'] = 'Verbindung zur Authorize.net-API unter Verwendung der angegebenen Anmelde-ID nicht möglich.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.empty'] = 'Bitte geben Sie eine Login-ID ein.';
+$lang['AuthorizeNetAcceptjs.!error.transaction_key.empty'] = 'Bitte geben Sie einen Transaktionsschlüssel ein.';
+$lang['AuthorizeNetAcceptjs.!error.auth'] = 'Das Gateway konnte sich nicht authentifizieren.';
+
