39fcaf4aba29478c4b1bc55181072793d9876186
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:03 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/admin_excluded_packages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Admin Excluded Packages
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminExcludedPackages.index.text_no_packages'] = 'Es sind keine aktiven Pakete verfügbar.';
+$lang['AdminExcludedPackages.index.field_updatesubmit'] = 'Einstellungen aktualisieren';
+$lang['AdminExcludedPackages.index.heading_hidden_packages'] = 'Versteckte Pakete';
+$lang['AdminExcludedPackages.index.heading_available_packages'] = 'Verfügbare Pakete';
+$lang['AdminExcludedPackages.index.tab_hidden_packages'] = 'Versteckte Pakete';
+$lang['AdminExcludedPackages.index.tab_available_packages'] = 'Verfügbare Pakete';
+$lang['AdminExcludedPackages.index.boxtitle_excluded_packages'] = 'Ausgeschlossene Pakete';
+$lang['AdminExcludedPackages.!success.packages_updated'] = 'Die ausgeschlossenen Pakete wurden erfolgreich aktualisiert.';
+
