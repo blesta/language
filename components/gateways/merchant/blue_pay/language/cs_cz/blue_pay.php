0c9bdf955c650700a140682936eb1b592e468246
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:53 -0800

    Updated cs_cz language files

---
+++ components/gateways/merchant/blue_pay/language/cs_cz/blue_pay.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Blue Pay
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Blue_pay.test_mode'] = 'Testovací režim';
+$lang['Blue_pay.secret_key'] = 'Tajný klíč';
+$lang['Blue_pay.account_id'] = 'ID účtu';
+$lang['Blue_pay.description'] = 'BluePay nabízí rychlá, snadná a bezpečná řešení zpracování plateb, která vyhovují potřebám každého obchodníka. Působí v Severní Americe';
+$lang['Blue_pay.name'] = 'BluePay';
+$lang['Blue_pay.!error.test_mode.valid'] = 'Testovací režim musí být nastaven na "true", pokud je zadán.';
+$lang['Blue_pay.!error.secret_key.empty'] = 'Zadejte svůj tajný klíč.';
+$lang['Blue_pay.!error.account_id.empty'] = 'Zadejte prosím své ID účtu.';
+
