edb2168da358459a2ed919c0c07aaaa7690d9f8e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:31 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/offline/language/el_gr/offline.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Offline
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Offline.!error.instructions.valid'] = 'Πρέπει να εισαγάγετε ορισμένες οδηγίες.';
+$lang['Offline.instructions'] = 'Οδηγίες';
+$lang['Offline.description'] = 'Σας επιτρέπει να εμφανίζετε ειδικές οδηγίες για τη λήψη πληρωμών εκτός σύνδεσης';
+$lang['Offline.name'] = 'Πληρωμή εκτός σύνδεσης';
+
