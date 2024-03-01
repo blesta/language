bb7105253338300c544a630f1230e255da0714fe
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:52 -0800

    Updated it_it language files

---
+++ plugins/system_overview/language/it_it/system_overview_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * System Overview Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SystemOverviewSettings.!error.settings[][value].length'] = 'La lunghezza di ogni singolo valore di impostazione non può superare i 255 caratteri.';
+$lang['SystemOverviewSettings.!error.settings[][key].empty'] = 'Nessun tasto di impostazione può essere vuoto.';
+$lang['SystemOverviewSettings.!error.company_id.exists'] = 'ID azienda non valido.';
+$lang['SystemOverviewSettings.!error.staff_id.exists'] = 'ID personale non valido.';
+
