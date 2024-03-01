75169bbb53d9ff78c7528011b0ce95d8b945ba7c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:12 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/forms.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Forms
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Forms.index.order_now'] = 'Ordina ora';
+$lang['Forms.index.register'] = 'Registro';
+
