0445b06eb0bcec9e96e9bcfb4e9b0ff25e64c53b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:21 -0800

    Updated it_it language files

---
+++ language/it_it/staff.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Staff.!error.action[].exists'] = 'Almeno una delle azioni del gruppo di e-mail non esiste.';
+$lang['Staff.!error.action.exists'] = 'L\'azione del gruppo e-mail %1$s non esiste.';
+$lang['Staff.!error.staff_id.exists'] = 'ID personale non valido.';
+$lang['Staff.!error.staff_group_id.exists'] = 'ID gruppo personale non valido.';
+$lang['Staff.!error.order.length'] = 'La lunghezza dell\'ordine non può superare i 5 caratteri.';
+$lang['Staff.!error.order.format'] = 'L\'ordine deve essere un numero.';
+$lang['Staff.!error.title.empty'] = 'Inserire un titolo.';
+$lang['Staff.!error.uri.empty'] = 'Inserire un URI.';
+$lang['Staff.!error.company_id.format'] = 'L\'ID dell\'azienda deve essere un numero.';
+$lang['Staff.!error.staff_id.format'] = 'L\'ID del personale deve essere un numero.';
+$lang['Staff.!error.groups.unique_company'] = 'A un membro del personale può essere assegnato un solo gruppo per azienda.';
+$lang['Staff.!error.groups[].format'] = 'Sono presenti uno o più ID di gruppo non validi.';
+$lang['Staff.!error.status.format'] = 'Stato non valido.';
+$lang['Staff.!error.email.format'] = 'Inserire un indirizzo e-mail valido.';
+$lang['Staff.!error.last_name.empty'] = 'Inserire un cognome.';
+$lang['Staff.!error.first_name.empty'] = 'Inserire un nome.';
+$lang['Staff.!error.user_id.unique'] = 'L\'ID utente del personale %1$s è già occupato.';
+$lang['Staff.!error.user_id.exists'] = 'ID utente non valido.';
+
