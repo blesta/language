15a31496c057360b4ce8f5ad46e3edee56f98671
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:26 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_company_plugins.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Admin Company Plugins
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyPlugins.settings.actions_heading_secondary'] = 'Sekundární možnosti';
+$lang['AdminCompanyPlugins.settings.actions_heading_sub'] = 'Dílčí možnosti';
+$lang['AdminCompanyPlugins.settings.actions_uri'] = 'URI: %1$s';
+$lang['AdminCompanyPlugins.settings.actions_description'] = 'Akce definují, co se může jménem zásuvného modulu zobrazovat v rozhraní správce a klienta, například navigační odkazy nebo widgety.';
+$lang['AdminCompanyPlugins.settings.events_no_results'] = 'Pro tento zásuvný modul nejsou nastaveny žádné události.';
+$lang['AdminCompanyPlugins.settings.automation_no_results'] = 'Pro tento zásuvný modul nejsou nastaveny žádné automatizační úlohy.';
+$lang['AdminCompanyPlugins.settings.actions_no_results'] = 'Pro tento zásuvný modul nejsou nastaveny žádné akce.';
+$lang['AdminCompanyPlugins.settings.events_description'] = 'Zásuvný modul naslouchá níže uvedeným událostem a provede nějakou akci, když nastanou.';
+$lang['AdminCompanyPlugins.settings.automation_description'] = 'Zásuvný modul zaregistroval níže uvedené automatizační úlohy, které provádějí určité akce při spuštění pomocí cronu.';
+$lang['AdminCompanyPlugins.settings.tab_events'] = 'Události';
+$lang['AdminCompanyPlugins.settings.tab_actions'] = 'Akce';
+$lang['AdminCompanyPlugins.settings.tab_automation'] = 'Automatizace';
+$lang['AdminCompanyPlugins.settings.page_title'] = '%1$s Nastavení';
+$lang['AdminCompanyPlugins.settings.field_submit'] = 'Aktualizace nastavení';
+$lang['AdminCompanyPlugins.settings.field_enabled'] = 'Povoleno';
+$lang['AdminCompanyPlugins.installed.text_none'] = 'Nejsou nainstalovány žádné zásuvné moduly.';
+$lang['AdminCompanyPlugins.installed.btn_upgrade'] = 'Upgrade';
+$lang['AdminCompanyPlugins.installed.btn_settings'] = 'Nastavení';
+$lang['AdminCompanyPlugins.installed.btn_manage'] = 'Správa';
+$lang['AdminCompanyPlugins.installed.btn_enable'] = 'Povolit';
+$lang['AdminCompanyPlugins.installed.btn_disable'] = 'Zakázat';
+$lang['AdminCompanyPlugins.installed.btn_uninstall'] = 'Odinstalujte stránku';
+$lang['AdminCompanyPlugins.installed.confirm_enable'] = 'Opravdu povolit tento plugin?';
+$lang['AdminCompanyPlugins.installed.confirm_disable'] = 'Opravdu zakázat tento plugin?';
+$lang['AdminCompanyPlugins.installed.confirm_uninstall'] = 'Opravdu odinstalovat tento plugin?';
+$lang['AdminCompanyPlugins.installed.text_author'] = 'Autor: Mgr:';
+$lang['AdminCompanyPlugins.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyPlugins.installed.boxtitle_plugin'] = 'Zásuvné moduly';
+$lang['AdminCompanyPlugins.installed.page_title'] = 'Nastavení > Společnost > Zásuvné moduly > Nainstalováno';
+$lang['AdminCompanyPlugins.available.text_none'] = 'Nejsou k dispozici žádné zásuvné moduly.';
+$lang['AdminCompanyPlugins.available.btn_install'] = 'Instalace';
+$lang['AdminCompanyPlugins.available.text_author'] = 'Autor: Mgr:';
+$lang['AdminCompanyPlugins.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyPlugins.available.boxtitle_plugins'] = 'Zásuvné moduly';
+$lang['AdminCompanyPlugins.available.page_title'] = 'Nastavení > Společnost > Zásuvné moduly > Dostupné';
+$lang['AdminCompanyPlugins.settings.event.users.logout'] = 'Tato událost se spustí, když se uživatel odhlásí.';
+$lang['AdminCompanyPlugins.settings.event.users.login'] = 'Tato událost se spustí, když se uživatel přihlásí.';
+$lang['AdminCompanyPlugins.settings.event.users.delete'] = 'Tato událost se spustí, když je uživatel odstraněn.';
+$lang['AdminCompanyPlugins.settings.event.transactions.edit'] = 'Tato událost je vyvolána při aktualizaci transakce.';
+$lang['AdminCompanyPlugins.settings.event.transactions.add'] = 'Tato událost je vyvolána při vytvoření transakce.';
+$lang['AdminCompanyPlugins.settings.event.staff.edit'] = 'Tato událost je spuštěna při aktualizaci zaměstnance.';
+$lang['AdminCompanyPlugins.settings.event.staff.add'] = 'Tato událost se spustí při vytvoření zaměstnance.';
+$lang['AdminCompanyPlugins.settings.event.services.unsuspend'] = 'Tato událost je spuštěna, když je služba zrušena.';
+$lang['AdminCompanyPlugins.settings.event.services.suspend'] = 'Tato událost je spuštěna, když je služba pozastavena.';
+$lang['AdminCompanyPlugins.settings.event.services.cancel'] = 'Tato událost je spuštěna, když je služba zrušena nebo je její zrušení naplánováno.';
+$lang['AdminCompanyPlugins.settings.event.services.edit'] = 'Tato událost je spuštěna při aktualizaci služby.';
+$lang['AdminCompanyPlugins.settings.event.services.add'] = 'Tato událost je spuštěna při vytvoření služby.';
+$lang['AdminCompanyPlugins.settings.event.report.clientdata'] = 'Tato událost se spustí, když je vygenerována zpráva o přenositelnosti údajů klienta.';
+$lang['AdminCompanyPlugins.settings.event.packages.delete'] = 'Tato událost je vyvolána při odstranění balíčku.';
+$lang['AdminCompanyPlugins.settings.event.packages.edit'] = 'Tato událost je vyvolána při aktualizaci balíčku.';
+$lang['AdminCompanyPlugins.settings.event.packages.add'] = 'Tato událost je vyvolána při vytvoření balíčku.';
+$lang['AdminCompanyPlugins.settings.event.navigation.getsearchoptions'] = 'Tato událost se spustí, když se zobrazí možnosti vyhledávání pro zaměstnance.';
+$lang['AdminCompanyPlugins.settings.event.modulemanager.delete'] = 'Tato událost je vyvolána při odinstalování modulu.';
+$lang['AdminCompanyPlugins.settings.event.modulemanager.add'] = 'Tato událost je spuštěna při instalaci modulu.';
+$lang['AdminCompanyPlugins.settings.event.invoices.setclosed'] = 'Tato událost se spustí, když je faktura uzavřena.';
+$lang['AdminCompanyPlugins.settings.event.invoices.edit'] = 'Tato událost je vyvolána při aktualizaci faktury.';
+$lang['AdminCompanyPlugins.settings.event.invoices.add'] = 'Tato událost je vyvolána při vytvoření faktury.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.delete'] = 'Tato událost se spustí, když je brána odinstalována.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.edit'] = 'Tato událost je spuštěna při aktualizaci brány.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.add'] = 'Tato událost je spuštěna při instalaci brány.';
+$lang['AdminCompanyPlugins.settings.event.emails.sendcustom'] = 'Tato událost je spuštěna při odeslání vlastního e-mailu.';
+$lang['AdminCompanyPlugins.settings.event.emails.send'] = 'Tato událost se spustí při odeslání šablony e-mailu.';
+$lang['AdminCompanyPlugins.settings.event.contacts.delete'] = 'Tato událost se spustí, když je kontakt klienta odstraněn.';
+$lang['AdminCompanyPlugins.settings.event.contacts.edit'] = 'Tato událost je spuštěna při aktualizaci kontaktu klienta.';
+$lang['AdminCompanyPlugins.settings.event.contacts.add'] = 'Tato událost se spustí při vytvoření kontaktu klienta.';
+$lang['AdminCompanyPlugins.settings.event.companies.delete'] = 'Tato událost se spustí, když je společnost odstraněna.';
+$lang['AdminCompanyPlugins.settings.event.companies.edit'] = 'Tato událost se spustí při aktualizaci společnosti.';
+$lang['AdminCompanyPlugins.settings.event.companies.add'] = 'Tato událost se spustí při vytvoření společnosti.';
+$lang['AdminCompanyPlugins.settings.event.clients.deletenote'] = 'Tato událost je vyvolána při odstranění poznámky klienta.';
+$lang['AdminCompanyPlugins.settings.event.clients.delete'] = 'Tato událost se spustí, když je klient odstraněn.';
+$lang['AdminCompanyPlugins.settings.event.clients.editnote'] = 'Tato událost je spuštěna při aktualizaci poznámky klienta.';
+$lang['AdminCompanyPlugins.settings.event.clients.edit'] = 'Tato událost je spuštěna při aktualizaci klienta.';
+$lang['AdminCompanyPlugins.settings.event.clients.create'] = 'Tato událost je spuštěna při vytvoření klienta.';
+$lang['AdminCompanyPlugins.settings.event.clients.addnote'] = 'Tato událost je vyvolána při vytvoření poznámky klienta.';
+$lang['AdminCompanyPlugins.settings.event.clients.add'] = 'Tato událost je spuštěna při přidání klienta.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.delete'] = 'Tato událost je vyvolána při odstranění skupiny klientů.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.edit'] = 'Tato událost je spuštěna při aktualizaci skupiny klientů.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.add'] = 'Tato událost je spuštěna při vytvoření skupiny klientů.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.delete'] = 'Tato událost se spustí, když je odstraněna událost kalendáře.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.edit'] = 'Tato událost je spuštěna při aktualizaci události kalendáře.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.add'] = 'Tato událost je spuštěna při vytvoření události kalendáře.';
+$lang['AdminCompanyPlugins.settings.event.appcontroller.structure'] = 'Tato událost se spustí, když dojde k načtení stránky, které není provedeno pomocí AJAXu.';
+$lang['AdminCompanyPlugins.settings.event.appcontroller.preaction'] = 'Tato událost se spustí, když dojde k načtení stránky.';
+$lang['AdminCompanyPlugins.!tab.available'] = 'Dostupné na';
+$lang['AdminCompanyPlugins.!tab.installed'] = 'Nainstalováno';
+$lang['AdminCompanyPlugins.!error.setting_controller_invalid'] = 'Zadaný řadič nastavení pro daný zásuvný modul neexistuje.';
+$lang['AdminCompanyPlugins.!success.events_updated'] = 'Nastavení události bylo úspěšně aktualizováno.';
+$lang['AdminCompanyPlugins.!success.actions_updated'] = 'Nastavení akce bylo úspěšně aktualizováno.';
+$lang['AdminCompanyPlugins.!success.automation_updated'] = 'Nastavení automatizace bylo úspěšně aktualizováno.';
+$lang['AdminCompanyPlugins.!success.disabled'] = 'Zásuvný modul byl úspěšně zakázán.';
+$lang['AdminCompanyPlugins.!success.enabled'] = 'Zásuvný modul byl úspěšně povolen.';
+$lang['AdminCompanyPlugins.!success.upgraded'] = 'Zásuvný modul byl úspěšně aktualizován.';
+$lang['AdminCompanyPlugins.!success.uninstalled'] = 'Zásuvný modul byl úspěšně odinstalován.';
+$lang['AdminCompanyPlugins.!success.installed'] = 'Zásuvný modul byl úspěšně nainstalován. Je možné, že zaregistroval oprávnění ACL pro různé prostředky. Pro přístup k těmto zdrojům bude možná nutné udělit skupině zaměstnanců přístup k těmto oprávněním.';
+
