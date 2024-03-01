562ba8e53d17e609ba7b95b4722a4dcada50216e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:44 -0800

    Updated de_de language files

---
+++ plugins/extension_generator/language/de_de/extension_generator_extensions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Extension Generator Extensions
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorExtensions.getformtypes.advanced'] = 'Fortgeschrittene';
+$lang['ExtensionGeneratorExtensions.getformtypes.basic'] = 'Grundlegend';
+$lang['ExtensionGeneratorExtensions.gettypes.nonmerchant'] = 'Nicht-Händler-Gateway';
+$lang['ExtensionGeneratorExtensions.gettypes.merchant'] = 'Händler-Gateway';
+$lang['ExtensionGeneratorExtensions.gettypes.plugin'] = 'Plugin';
+$lang['ExtensionGeneratorExtensions.gettypes.module'] = 'Modul';
+$lang['ExtensionGeneratorExtensions.!error.id.exists'] = 'Die angegebene Nebenstellen-ID ist ungültig.';
+$lang['ExtensionGeneratorExtensions.!error.code_examples.format'] = 'Ungültiges Code-Beispiel-Optionsformat.';
+$lang['ExtensionGeneratorExtensions.!error.form_type.valid'] = 'Ungültiger Formulartyp.';
+$lang['ExtensionGeneratorExtensions.!error.type.valid'] = 'Ungültiger Erweiterungstyp.';
+$lang['ExtensionGeneratorExtensions.!error.date_updated.format'] = 'Ungültiges Format für das aktualisierte Datum.';
+$lang['ExtensionGeneratorExtensions.!error.company_id.exists'] = 'Ungültige Unternehmens-ID.';
+$lang['ExtensionGeneratorExtensions.!error.name.empty'] = 'Bitte geben Sie einen Durchwahlnamen ein.';
+
