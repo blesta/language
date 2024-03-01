5b5b7b079d97f54314efaafb2a08d9af7e4634cc
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:38 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/ajax/language/el_gr/summary.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Summary
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Summary.index.have_coupon'] = 'Χρησιμοποιήστε έναν κωδικό κουπονιού';
+$lang['Summary.index.coupon'] = 'Κουπόνι';
+$lang['Summary.index.coupon_btn'] = 'Εξαργύρωση';
+$lang['Summary.index.heading_summary'] = 'Περίληψη παραγγελίας';
+$lang['Summary.index.summary_note'] = 'Οποιοσδήποτε ισχύων φόρος θα υπολογιστεί αφού εγγραφείτε ή συνδεθείτε.';
+$lang['Summary.index.table_description'] = 'Περιγραφή';
+$lang['Summary.index.table_price'] = 'Τιμή';
+$lang['Summary.index.empty_cart'] = 'Άδειο καλάθι';
+$lang['Summary.index.heading_paymethods'] = 'Μέθοδος πληρωμής';
+$lang['Summary.index.paymethods_note'] = 'Παρακαλούμε επιλέξτε την επιθυμητή μέθοδο πληρωμής.';
+
