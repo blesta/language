4e88dc8393d3fd9d4382aba7b161f547070038a7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:04 -0800

    Updated it_it language files

---
+++ language/it_it/gateway_manager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Gateway Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GatewayManager.!error.currencies[].in_use'] = 'Almeno una valuta è attualmente utilizzata da un altro gateway.';
+$lang['GatewayManager.!error.currencies[].exists'] = 'Almeno una valuta non è accettata da questo gateway.';
+$lang['GatewayManager.!error.version.valid'] = 'La versione non è valida.';
+$lang['GatewayManager.!error.name.valid'] = 'Il nome non è valido.';
+$lang['GatewayManager.!error.class.valid'] = 'La classe non è valida.';
+$lang['GatewayManager.!error.company_id.valid'] = 'L\'ID della società non è valido.';
+
