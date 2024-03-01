46ad2a5590e93714001c1af0e0d090ee3cb8016f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:41 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_company_automation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Company Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyAutomation.getintervals.text_hours'] = 'ώρες';
+$lang['AdminCompanyAutomation.getintervals.text_hour'] = 'ώρα';
+$lang['AdminCompanyAutomation.getintervals.text_minutes'] = 'λεπτά';
+$lang['AdminCompanyAutomation.getintervals.text_minute'] = 'λεπτό';
+$lang['AdminCompanyAutomation.index.confirm_clear_task'] = 'Είστε σίγουροι ότι θέλετε να καταργήσετε το κλείδωμα αυτής της εργασίας cron; Εάν αυτή η εργασία καθυστερεί συνεχώς, μπορεί να υπάρχει ένα πιο σοβαρό πρόβλημα. Δοκιμάστε να εκτελέσετε το cron χειροκίνητα ή ελέγξτε το αρχείο καταγραφής cron για σφάλματα για να προσδιορίσετε την αιτία.';
+$lang['AdminCompanyAutomation.index.option_clear_task'] = 'Εκκαθάριση κλειδώματος εργασιών';
+$lang['AdminCompanyAutomation.index.text_cron_never_ran'] = 'Το cron δεν έχει εκτελεστεί ποτέ.';
+$lang['AdminCompanyAutomation.index.text_cron_last_ran'] = 'Το cron εκτελέστηκε τελευταία φορά στις %1$s.';
+$lang['AdminCompanyAutomation.index.no_cron_lastran'] = 'Ποτέ';
+$lang['AdminCompanyAutomation.index.no_results'] = 'Δεν υπάρχουν εργασίες cron.';
+$lang['AdminCompanyAutomation.index.text_task_lastran'] = 'Εργασία Τελευταία εκτέλεση:';
+$lang['AdminCompanyAutomation.index.text_starttime'] = 'Ώρα έναρξης';
+$lang['AdminCompanyAutomation.index.text_interval'] = 'Διάστημα';
+$lang['AdminCompanyAutomation.index.field_enabled'] = 'Ενεργοποιημένο';
+$lang['AdminCompanyAutomation.index.field_automationsubmit'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminCompanyAutomation.index.boxtitle_automation'] = 'Αυτοματισμός';
+$lang['AdminCompanyAutomation.index.page_title'] = 'Ρυθμίσεις > Εταιρεία > Αυτοματισμός';
+$lang['AdminCompanyAutomation.!success.task_cleared'] = 'Η εργασία cron έχει εκκαθαριστεί.';
+$lang['AdminCompanyAutomation.!success.automation_updated'] = 'Οι ρυθμίσεις αυτοματισμού ενημερώθηκαν με επιτυχία!';
+$lang['AdminCompanyAutomation.!notice.cron_task_interval'] = 'Υπάρχουν εργασίες αυτοματοποίησης που έχουν οριστεί να εκτελούνται κάθε λεπτό, αλλά το cron δεν φαίνεται να εκτελείται κάθε λεπτό. Ενημερώστε το cron σας ώστε να εκτελείται κάθε λεπτό για να επωφεληθείτε από αυτά τα μικρότερα διαστήματα.';
+
