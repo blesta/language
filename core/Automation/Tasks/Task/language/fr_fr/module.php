10dba0f99e12110f66c0f114108212eb1dcdfda8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:22 -0800

    Updated fr_fr language files

---
+++ core/Automation/Tasks/Task/language/fr_fr/module.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.module.attempt'] = 'Tentative de module cron pour %1$s %2$s.';
+$lang['Automation.task.module.completed'] = 'Fin du module cron pour %1$s %2$s.';
+
