65417ef05dd7ee42faa1ab7c47e37d8b700e9b34
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:26 -0800

    Updated es_es language files

---
+++ language/es_es/gateway_payments.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Gateway Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GatewayPayments.!error.gateway.exists'] = 'La pasarela no existe o no está habilitada.';
+$lang['GatewayPayments.!error.transaction_id.exists'] = 'No se ha podido encontrar la transacción para ese cliente.';
+$lang['GatewayPayments.!error.account_id.exists'] = 'No se ha podido encontrar la cuenta de pago para ese cliente.';
+$lang['GatewayPayments.!error.contact_id.exists'] = 'No se ha podido encontrar el contacto para ese cliente.';
+$lang['GatewayPayments.!error.type.valid'] = 'El tipo de pago no es compatible con esta pasarela.';
+$lang['GatewayPayments.!error.type.instance'] = 'La pasarela no admite pagos de esta forma.';
+$lang['GatewayPayments.!error.amount.valid'] = 'El importe debe ser superior a cero.';
+$lang['GatewayPayments.!error.reference_id.store'] = 'La pasarela de pago no ha podido almacenar la cuenta de pago.';
+$lang['GatewayPayments.!error.reference_id.update'] = 'La pasarela de pago no ha podido actualizar la cuenta de pago.';
+$lang['GatewayPayments.!error.reference_id.verify'] = 'La pasarela de pago no ha podido verificar la cuenta de pago.';
+$lang['GatewayPayments.!error.reference_id.remove'] = 'La pasarela de pago no ha podido eliminar la cuenta de pago.';
+$lang['GatewayPayments.!error.response_status'] = 'La pasarela de pago ha devuelto una respuesta inesperada.';
+$lang['GatewayPayments.!error.gateway.declined'] = 'El pago fue rechazado.';
+$lang['GatewayPayments.!error.gateway.error'] = 'La pasarela de pago ha devuelto un error al procesar la solicitud.';
+$lang['GatewayPayments.!error.type.invalid'] = 'El tipo de cuenta debe ser "ach" o "cc".';
+
