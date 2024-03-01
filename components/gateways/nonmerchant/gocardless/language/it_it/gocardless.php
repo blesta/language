64fbaf94fb6eda759e0ebb0fe0865e579e11fe7f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:17 -0800

    Updated it_it language files

---
+++ components/gateways/nonmerchant/gocardless/language/it_it/gocardless.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Gocardless
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Gocardless.!tooltip.webhook_secret'] = 'Il segreto del webhook deve essere lungo almeno 40 caratteri.';
+$lang['Gocardless.!error.dev_mode.valid'] = 'La modalità sviluppatore deve essere impostata su "true", se fornita.';
+$lang['Gocardless.!error.webhook_secret.valid'] = 'È necessario inserire un segreto Webhook valido di almeno 40 caratteri.';
+$lang['Gocardless.!error.access_token.valid'] = 'È necessario inserire un token di accesso valido.';
+$lang['Gocardless.webhook_note'] = 'Prima di iniziare a utilizzare questo gateway, è necessario configurare il seguente url come Webhook nel proprio account GoCardless. Il segreto del Webhook deve essere lungo almeno 40 caratteri.';
+$lang['Gocardless.webhook'] = 'Webhook di GoCardless';
+$lang['Gocardless.buildprocess.subscription'] = 'Abbonarsi con GoCardless';
+$lang['Gocardless.buildprocess.submit'] = 'Pagare con GoCardless';
+$lang['Gocardless.dev_mode_note'] = 'Se si attiva questa opzione, le transazioni verranno pubblicate nell\'ambiente GoCardless Sandbox. Attivare questa opzione solo se si sta effettuando un test con un account GoCardless Sandbox.';
+$lang['Gocardless.dev_mode'] = 'Modalità sviluppatore';
+$lang['Gocardless.pay_type_both'] = 'Pagamenti una tantum e in abbonamento, quando possibile';
+$lang['Gocardless.pay_type_subscribe'] = 'Solo pagamenti in abbonamento';
+$lang['Gocardless.pay_type_onetime'] = 'Solo pagamenti una tantum';
+$lang['Gocardless.pay_type'] = 'Opzioni di pagamento';
+$lang['Gocardless.webhook_secret'] = 'Segreto Webhook';
+$lang['Gocardless.access_token'] = 'Gettone di accesso';
+$lang['Gocardless.description'] = 'Raccogliere pagamenti ricorrenti da più di 30 paesi e farli versare sul vostro conto corrente bancario esistente';
+$lang['Gocardless.name'] = 'GoCardless';
+
