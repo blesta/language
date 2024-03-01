d69e14cac29438e08ce63380193bceabcf16c611
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:53 -0800

    Updated it_it language files

---
+++ plugins/import_manager/components/migrators/csv/1.0/language/it_it/csv1_0.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Csv1
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Csv1_0.configuration.title'] = 'Titolo';
+$lang['Csv1_0.configuration.company'] = 'Azienda';
+$lang['Csv1_0.configuration.phone'] = 'Telefono';
+$lang['Csv1_0.configuration.country'] = 'Paese';
+$lang['Csv1_0.configuration.city'] = 'Città';
+$lang['Csv1_0.configuration.zip'] = 'Codice postale';
+$lang['Csv1_0.configuration.state'] = 'Stato';
+$lang['Csv1_0.configuration.address2'] = 'Indirizzo 2';
+$lang['Csv1_0.configuration.address1'] = 'Indirizzo 1';
+$lang['Csv1_0.configuration.email'] = 'Email';
+$lang['Csv1_0.configuration.last_name'] = 'Cognome';
+$lang['Csv1_0.configuration.first_name'] = 'Nome';
+$lang['Csv1_0.configuration.username'] = 'Nome utente';
+$lang['Csv1_0.configuration.no_import'] = 'Nessuna importazione';
+$lang['Csv1_0.configuration.remote_field'] = 'Campo remoto';
+$lang['Csv1_0.configuration.local_field'] = 'Campo locale';
+$lang['Csv1_0.settings.enable_debug'] = 'Abilita il debug';
+$lang['Csv1_0.settings.csv_data'] = 'Dati CSV';
+$lang['Csv1_0.!error.required_fields'] = 'I seguenti campi devono essere importati: Nome utente, Nome, Cognome, Email, Telefono';
+$lang['Csv1_0.!error.import'] = 'L\'importazione è stata completata ma si sono verificati i seguenti errori:';
+$lang['Csv1_0.!error.csv_data.invalid'] = 'I dati CSV forniti non sono validi.';
+
