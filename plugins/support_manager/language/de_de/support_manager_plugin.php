f15e540c5964fd14a4852367caa43224dd46374f
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:46 -0800

    Updated de_de language files

---
+++ plugins/support_manager/language/de_de/support_manager_plugin.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Support Manager Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerPlugin.permission.admin_reminders'] = 'Mahnungen';
+$lang['SupportManagerPlugin.permission.admin_knowledgebase'] = 'Wissensdatenbank';
+$lang['SupportManagerPlugin.permission.admin_staff'] = 'Personal';
+$lang['SupportManagerPlugin.permission.admin_responses'] = 'Antworten';
+$lang['SupportManagerPlugin.permission.admin_departments'] = 'Abteilungen';
+$lang['SupportManagerPlugin.permission.admin_tickets_client'] = 'Kundenprofil-Widget';
+$lang['SupportManagerPlugin.permission.admin_tickets_delete'] = 'Dauerhaftes Löschen von Tickets';
+$lang['SupportManagerPlugin.permission.admin_tickets'] = 'Eintrittskarten';
+$lang['SupportManagerPlugin.permission.admin_main'] = 'Unterstützung';
+$lang['SupportManagerPlugin.event_getsearchoptions.tickets'] = 'Ticket-Suche';
+$lang['SupportManagerPlugin.card_client.tickets'] = 'Eintrittskarten';
+$lang['SupportManagerPlugin.action_staff_client.add'] = 'Ticket öffnen';
+$lang['SupportManagerPlugin.widget_staff_client.tickets'] = 'Eintrittskarten';
+$lang['SupportManagerPlugin.nav_primary_staff.reminders'] = 'Mahnungen';
+$lang['SupportManagerPlugin.nav_primary_staff.knowledgebase'] = 'Wissensdatenbank';
+$lang['SupportManagerPlugin.nav_primary_staff.staff'] = 'Personal';
+$lang['SupportManagerPlugin.nav_primary_staff.responses'] = 'Antworten';
+$lang['SupportManagerPlugin.nav_primary_staff.departments'] = 'Abteilungen';
+$lang['SupportManagerPlugin.nav_primary_staff.tickets'] = 'Eintrittskarten';
+$lang['SupportManagerPlugin.nav_primary_staff.main'] = 'Unterstützung';
+$lang['SupportManagerPlugin.widget_client_home.main'] = 'Eintrittskarten';
+$lang['SupportManagerPlugin.nav_primary_client.knowledgebase'] = 'Wissensdatenbank';
+$lang['SupportManagerPlugin.nav_primary_client.tickets'] = 'Eintrittskarten';
+$lang['SupportManagerPlugin.nav_primary_client.main'] = 'Unterstützung';
+$lang['SupportManagerPlugin.cron.send_reminders_desc'] = 'Versenden Sie automatisch Erinnerungen für Tickets mit einem bestimmten Status nach einer bestimmten Zeitspanne.';
+$lang['SupportManagerPlugin.cron.send_reminders_name'] = 'Mahnungen senden';
+$lang['SupportManagerPlugin.cron.delete_tickets_desc'] = 'Automatische Löschung von Papierkorbanfragen, wie für Abteilungen im Support Manager konfiguriert.';
+$lang['SupportManagerPlugin.cron.delete_tickets_name'] = 'Papierkorb-Tickets löschen';
+$lang['SupportManagerPlugin.cron.close_tickets_desc'] = 'Schließt automatisch offene Anfragen, wie für Abteilungen im Support Manager konfiguriert.';
+$lang['SupportManagerPlugin.cron.close_tickets_name'] = 'Tickets schließen';
+$lang['SupportManagerPlugin.cron.poll_tickets_desc'] = 'Stellt eine Verbindung zum POP3/IMAP-Server her, um E-Mails herunterzuladen und sie in Tickets umzuwandeln.';
+$lang['SupportManagerPlugin.cron.poll_tickets_name'] = 'Fahrkarten herunterladen';
+$lang['SupportManagerPlugin.description'] = 'Ermöglicht es Benutzern und Mitarbeitern, Support-Tickets zu öffnen und zu verwalten.';
+$lang['SupportManagerPlugin.name'] = 'Support Manager';
+
