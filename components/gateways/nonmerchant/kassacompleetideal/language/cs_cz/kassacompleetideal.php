0ebc4141537ce92d610d67f2fd9770fc1b7ed91e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:05 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/kassacompleetideal/language/cs_cz/kassacompleetideal.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Kassacompleetideal
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Kassacompleetideal.buildprocess.submit'] = 'Plaťte pomocí iDeal přes Kassa Compleet';
+$lang['Kassacompleetideal.buildprocess.issuer_id'] = 'iDeal Emitent';
+$lang['Kassacompleetideal.tooltip.api_key'] = 'Chcete-li použít testovací režim, jednoduše zadejte klíč API z testovacího účtu.';
+$lang['Kassacompleetideal.api_key'] = 'Klíč API';
+$lang['Kassacompleetideal.description'] = 'Integrace Kassa Compleet pomocí typu platby iDeal';
+$lang['Kassacompleetideal.name'] = 'iDeal (Kassa Compleet)';
+$lang['Kassacompleetideal.!error.api_key.valid'] = 'Klíč API je neplatný';
+$lang['Kassacompleetideal.!error.api_key.empty'] = 'Zadejte prosím klíč API';
+
