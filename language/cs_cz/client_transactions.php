0f9ec2ea9562b6a7cc326fee3fe3be1ef8f88604
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:50 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/client_transactions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Client Transactions
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientTransactions.applied.no_results'] = 'Tato transakce nebyla použita na žádné faktury.';
+$lang['ClientTransactions.applied.heading_appliedon'] = 'Applied On';
+$lang['ClientTransactions.applied.heading_amount'] = 'Částka';
+$lang['ClientTransactions.applied.heading_invoice'] = 'Faktura';
+$lang['ClientTransactions.index.no_results'] = 'Nemáte žádné %1$s Transakce.';
+$lang['ClientTransactions.index.heading_date'] = 'Datum';
+$lang['ClientTransactions.index.heading_number'] = 'Číslo';
+$lang['ClientTransactions.index.heading_applied'] = 'Aplikované';
+$lang['ClientTransactions.index.heading_credited'] = 'Připsáno na';
+$lang['ClientTransactions.index.heading_amount'] = 'Částka';
+$lang['ClientTransactions.index.heading_type'] = 'Typ';
+$lang['ClientTransactions.index.category_declined'] = 'Odmítnuto';
+$lang['ClientTransactions.index.category_pending'] = 'Čeká se na';
+$lang['ClientTransactions.index.category_approved'] = 'Schváleno';
+$lang['ClientTransactions.index.boxtitle_transactions'] = 'Transakce';
+$lang['ClientTransactions.index.page_title'] = 'Transakce klienta #%1$s';
+
