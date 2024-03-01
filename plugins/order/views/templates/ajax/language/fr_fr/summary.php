409ff58d1fd8d6da91ce7f0673d31de00d73cf90
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:49 -0800

    Updated fr_fr language files

---
+++ plugins/order/views/templates/ajax/language/fr_fr/summary.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Summary
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Summary.index.have_coupon'] = 'Utiliser un code de réduction';
+$lang['Summary.index.coupon'] = 'Coupon';
+$lang['Summary.index.coupon_btn'] = 'Racheter';
+$lang['Summary.index.heading_summary'] = 'Résumé de la commande';
+$lang['Summary.index.summary_note'] = 'Les taxes applicables seront calculées après l\'inscription ou la connexion.';
+$lang['Summary.index.table_description'] = 'Description';
+$lang['Summary.index.table_price'] = 'Prix';
+$lang['Summary.index.empty_cart'] = 'Panier vide';
+$lang['Summary.index.heading_paymethods'] = 'Mode de paiement';
+$lang['Summary.index.paymethods_note'] = 'Veuillez sélectionner le mode de paiement souhaité.';
+
