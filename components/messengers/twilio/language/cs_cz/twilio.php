b4ac21cb5836e597ddf6b4a058b7bcd78c8cc8a1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:15 -0800

    Updated cs_cz language files

---
+++ components/messengers/twilio/language/cs_cz/twilio.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Twilio
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Twilio.configuration_fields.token'] = 'Token';
+$lang['Twilio.configuration_fields.sid'] = 'SID účtu';
+$lang['Twilio.configuration_fields.phone_number'] = 'Telefonní číslo';
+$lang['Twilio.description'] = 'Odesílání oznámení SMS pomocí služby Twilio.';
+$lang['Twilio.name'] = 'Twilio';
+
