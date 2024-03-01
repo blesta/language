dbc7d0775e4482d8f21e0202850e1ee43f1b7e27
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:12 -0800

    Updated de_de language files

---
+++ core/Util/Filters/language/de_de/package_option_filters.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Package Option Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.package_option_filters.field_options'] = 'Optionen';
+$lang['Util.filters.package_option_filters.options.hidden'] = 'Versteckte Paketoptionen anzeigen';
+
