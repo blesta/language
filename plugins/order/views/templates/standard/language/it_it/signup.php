2860dc89a613c8965ca6d3c857aca9e5689a72b0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:31 -0800

    Updated it_it language files

---
+++ plugins/order/views/templates/standard/language/it_it/signup.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Signup
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Signup.index.heading_settings'] = 'Impostazioni aggiuntive';
+$lang['Signup.index.heading_authentication'] = 'Autenticazione';
+$lang['Signup.index.heading_billing'] = 'Informazioni sulla fatturazione';
+$lang['Signup.index.heading_contact'] = 'Informazioni di contatto';
+$lang['Signup.index.header_login'] = 'Accedi';
+$lang['Signup.index.link_resetpassword'] = 'Reimpostare la password';
+$lang['Signup.index.header_login_or_signup'] = 'Registrati o accedi';
+
