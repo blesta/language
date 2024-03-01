1723b6d0ffaf15690ce9a922cde4fefed3e584f3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:02 -0800

    Updated fr_fr language files

---
+++ plugins/softaculous/language/fr_fr/softaculous_plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Softaculous Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SoftaculousPlugin.name'] = 'Soft Auto Installer';
+$lang['SoftaculousPlugin.library_error'] = 'Impossible de charger la bibliothèque.';
+$lang['SoftaculousPlugin.remote_error'] = 'Impossible de se connecter au serveur distant.';
+$lang['SoftaculousPlugin.remote_error_message'] = 'Impossible de se connecter au serveur distant. Erreur : %1$s';
+$lang['SoftaculousPlugin.remote_curl_error'] = 'Impossible de se connecter au serveur distant. cURL Error : %1$s';
+$lang['SoftaculousPlugin.remote_firewall_error'] = 'Impossible de déterminer l\'emplacement de Softaculous sur le serveur distant. Il se peut qu\'un pare-feu empêche l\'accès.';
+$lang['SoftaculousPlugin.script_selected_error'] = 'Impossible de déterminer le script à installer. Veuillez vous assurer que le nom du script est correct. Nom du script : %1$s';
+$lang['SoftaculousPlugin.script_installed'] = 'Script installé avec succès';
+$lang['SoftaculousPlugin.script_no_installed'] = 'Impossible d\'installer le script : %1$s';
+$lang['SoftaculousPlugin.admin_fname'] = 'Système';
+$lang['SoftaculousPlugin.admin_lname'] = 'Administrateur';
+$lang['SoftaculousPlugin.installation_success'] = 'Script installé avec succès pour le service avec ID %1$s';
+$lang['SoftaculousPlugin.cron.script_installation_name'] = 'Scripts de service de mise à disposition';
+$lang['SoftaculousPlugin.cron.script_installation_desc'] = 'Installe les scripts en attente pour les services.';
+
