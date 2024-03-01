c3232bfa67e5a611f524336bf94e2738b41cbf6a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:41 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/orders.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Orders
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Orders.!success.order_canceled'] = 'La commande a été annulée avec succès.';
+$lang['Orders.index.boxtitle_orders'] = 'Historique des commandes';
+$lang['Orders.index.heading_order_number'] = 'N° de commande';
+$lang['Orders.index.heading_status'] = 'Statut';
+$lang['Orders.index.heading_invoice_number'] = 'Facture #';
+$lang['Orders.index.heading_total'] = 'Total';
+$lang['Orders.index.heading_paid'] = 'Payé';
+$lang['Orders.index.heading_date_ordered'] = 'Date de la commande';
+$lang['Orders.index.heading_options'] = 'Options';
+$lang['Orders.index.option_view'] = 'Voir';
+$lang['Orders.index.option_cancel'] = 'Annuler';
+$lang['Orders.index.no_results'] = 'Vous n\'avez pas de commande.';
+$lang['Orders.view.boxtitle_view_order'] = 'Voir la commande';
+$lang['Orders.view.heading_invoices'] = 'Factures';
+$lang['Orders.view.heading_services'] = 'Services';
+$lang['Orders.view.heading_transactions'] = 'Transactions';
+$lang['Orders.view.heading_invoice_number'] = 'Facture #';
+$lang['Orders.view.heading_amount'] = 'Montant';
+$lang['Orders.view.heading_paid'] = 'Payé';
+$lang['Orders.view.heading_date_billed'] = 'Date de facturation';
+$lang['Orders.view.heading_package'] = 'Paquet';
+$lang['Orders.view.heading_label'] = 'Étiquette';
+$lang['Orders.view.heading_term'] = 'Durée';
+$lang['Orders.view.heading_date_created'] = 'Date de création';
+$lang['Orders.view.heading_date_renews'] = 'Date Renouvellement';
+$lang['Orders.view.heading_options'] = 'Options';
+$lang['Orders.view.heading_type'] = 'Type';
+$lang['Orders.view.heading_applied'] = 'Appliqué';
+$lang['Orders.view.heading_status'] = 'Statut';
+$lang['Orders.view.heading_number'] = 'Nombre';
+$lang['Orders.view.heading_date'] = 'Date';
+$lang['Orders.view.option_view'] = 'Voir';
+$lang['Orders.view.option_manage'] = 'Gérer';
+$lang['Orders.view.field_back_order_history'] = 'Retour à l\'historique des commandes';
+$lang['Orders.view.no_invoices'] = 'Aucune facture n\'est associée à cette commande.';
+$lang['Orders.view.no_services'] = 'Aucun service n\'est associé à cette commande.';
+$lang['Orders.view.no_transactions'] = 'Aucune transaction n\'est associée à cette commande.';
+$lang['Orders.view.recurring_term'] = '%1$s %2$s @ %3$s';
+
