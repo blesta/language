d9dadd57bd789d7177505890fc2b620dfe863afa
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:04 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/merchant_gateway.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Merchant Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MerchantGateway.!error.general'] = 'Při zpracování požadavku bránou došlo k chybě.';
+$lang['MerchantGateway.!error.unsupported'] = 'Brána tuto akci nepodporuje.';
+$lang['MerchantGateway.!error.transaction_not_found'] = 'Transakce je neplatná nebo již není k dispozici pro zpracování dané akce.';
+$lang['MerchantGateway.!error.address_verification_failed'] = 'Adresa se zdá být neplatná.';
+$lang['MerchantGateway.!error.invalid_security_code'] = 'Bezpečnostní kód se zdá být neplatný.';
+$lang['MerchantGateway.!error.card_not_accepted'] = 'Karty tohoto typu nejsou přijímány.';
+$lang['MerchantGateway.!error.duplicate_transaction'] = 'Byla zjištěna duplicitní transakce.';
+$lang['MerchantGateway.!error.account_number_invalid'] = 'Číslo účtu je neplatné.';
+$lang['MerchantGateway.!error.routing_number_invalid'] = 'Směrovací číslo je neplatné.';
+$lang['MerchantGateway.!error.card_expired'] = 'Platnost karty vypršela.';
+$lang['MerchantGateway.!error.card_number_invalid'] = 'Číslo karty je neplatné.';
+
