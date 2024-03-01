fca47b444d4dd34a2d62ca72bd39a3ed877fe2c4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:45 -0800

    Updated it_it language files

---
+++ plugins/support_manager/language/it_it/client_main.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.index.confirm_close'] = 'È sicuro di voler chiudere questo ticket?';
+$lang['ClientMain.index.no_results'] = 'Al momento non ci sono biglietti con questo stato.';
+$lang['ClientMain.index.field_last_reply'] = 'Ultima risposta';
+$lang['ClientMain.index.field_summary'] = 'Sintesi';
+$lang['ClientMain.index.field_priority'] = 'Priorità';
+$lang['ClientMain.index.field_ticket_number'] = 'Numero di biglietto';
+$lang['ClientMain.index.hours'] = '%1$s ore';
+$lang['ClientMain.index.hour'] = '1 ora';
+$lang['ClientMain.index.minutes'] = '%1$s minuti';
+$lang['ClientMain.index.any'] = 'Qualsiasi';
+$lang['ClientMain.index.option_reply'] = 'Risposta';
+$lang['ClientMain.index.option_close'] = 'Chiudere';
+$lang['ClientMain.index.heading_options'] = 'Opzioni';
+$lang['ClientMain.index.heading_status'] = 'Stato';
+$lang['ClientMain.index.heading_last_reply_date'] = 'Ultima risposta';
+$lang['ClientMain.index.heading_summary'] = 'Sintesi';
+$lang['ClientMain.index.heading_department_name'] = 'Dipartimento';
+$lang['ClientMain.index.heading_priority'] = 'Priorità';
+$lang['ClientMain.index.heading_code'] = 'Numero di biglietto';
+$lang['ClientMain.index.boxtitle_tickets'] = 'Biglietti';
+$lang['ClientMain.index.categorylink_createticket'] = 'Biglietto aperto';
+$lang['ClientMain.index.category_closed'] = 'Chiuso';
+$lang['ClientMain.index.category_open'] = 'Aperto';
+$lang['ClientMain.index.page_title'] = 'Assistenza > Biglietti';
+
