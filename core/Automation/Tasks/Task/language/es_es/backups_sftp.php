610181687cae709bf49f12cd5595c900a4b91825
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:17 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/backups_sftp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Backups Sftp
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.backups_sftp.attempt'] = 'Intento de copia de seguridad de la base de datos a través de SFTP.';
+$lang['Automation.task.backups_sftp.completed'] = 'La tarea de copia de seguridad de la base de datos SFTP ha finalizado.';
+$lang['Automation.task.backups_sftp.success'] = 'La copia de seguridad se ha realizado correctamente.';
+
