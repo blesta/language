dd68f2a9a74d0e699f2ce48ed58823d037eaf0ea
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:12 -0800

    Updated el_gr language files

---
+++ language/el_gr/countries.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Countries
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Countries.!error.name.format'] = 'Παρακαλώ εισάγετε ένα όνομα.';
+$lang['Countries.!error.alpha3.in_use'] = 'Ο κωδικός χώρας Alpha3, %1$s, είναι ήδη κατειλημμένος.';
+$lang['Countries.!error.alpha3.format'] = 'Ο κωδικός χώρας Alpha3 πρέπει να αποτελείται από 3 χαρακτήρες άλφα.';
+$lang['Countries.!error.alpha2.exists'] = 'Μη έγκυρος κωδικός alpha2.';
+$lang['Countries.!error.alpha2.in_use'] = 'Ο κωδικός χώρας Alpha2, %1$s, είναι ήδη κατειλημμένος.';
+$lang['Countries.!error.alpha2.format'] = 'Ο κωδικός χώρας Alpha2 πρέπει να είναι 2 χαρακτήρες άλφα.';
+
