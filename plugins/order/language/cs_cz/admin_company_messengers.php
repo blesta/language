d2ba1a216a0f8f7cfb9fcc8b298a311ee5ac3ae3
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:00 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/admin_company_messengers.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Admin Company Messengers
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyMessengers.templates.Order.received_staff_desc'] = 'Oznámení odeslané po přijetí objednávky.';
+$lang['AdminCompanyMessengers.templates.Order.received_staff_name'] = 'Přijatá objednávka';
+
