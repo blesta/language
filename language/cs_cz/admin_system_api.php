9136c7113802288da98245ef49a43be78b7e1c46
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:35 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_system_api.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Admin System Api
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemApi.edit.field_apisubmit'] = 'Aktualizovat uživatele';
+$lang['AdminSystemApi.edit.field_notes'] = 'Poznámky';
+$lang['AdminSystemApi.edit.field_user'] = 'Uživatel';
+$lang['AdminSystemApi.edit.field_company'] = 'Společnost';
+$lang['AdminSystemApi.edit.boxtitle_edit'] = 'Upravit uživatele API';
+$lang['AdminSystemApi.add.field_apisubmit'] = 'Přidat uživatele';
+$lang['AdminSystemApi.add.field_notes'] = 'Poznámky';
+$lang['AdminSystemApi.add.field_user'] = 'Uživatel';
+$lang['AdminSystemApi.add.field_company'] = 'Společnost';
+$lang['AdminSystemApi.add.boxtitle_add'] = 'Přidání uživatele API';
+$lang['AdminSystemApi.index.confirm_delete'] = 'Opravdu odstranit tohoto uživatele API?';
+$lang['AdminSystemApi.index.no_results'] = 'Neexistují žádní uživatelé API.';
+$lang['AdminSystemApi.index.option_delete'] = 'Odstranit';
+$lang['AdminSystemApi.index.option_edit'] = 'Upravit';
+$lang['AdminSystemApi.index.heading_options'] = 'Možnosti';
+$lang['AdminSystemApi.index.heading_date'] = 'Datum vytvoření';
+$lang['AdminSystemApi.index.heading_company'] = 'Společnost';
+$lang['AdminSystemApi.index.heading_key'] = 'Klíč';
+$lang['AdminSystemApi.index.heading_user'] = 'Uživatel';
+$lang['AdminSystemApi.index.categorylink_keyadd'] = 'Přidání uživatele API';
+$lang['AdminSystemApi.index.boxtitle_api'] = 'Přístup k rozhraní API';
+$lang['AdminSystemApi.index.page_title'] = 'Nastavení > Systém > Přístup k rozhraní API';
+$lang['AdminSystemApi.!success.key_updated'] = 'Uživatel API byl úspěšně aktualizován.';
+$lang['AdminSystemApi.!success.key_deleted'] = 'Uživatel API byl úspěšně odstraněn.';
+$lang['AdminSystemApi.!success.key_added'] = 'Uživatel API byl úspěšně přidán.';
+$lang['AdminSystemApi.!notice.api_not_restrictive'] = 'POZOR! Společnost je společnost, pod kterou se uživatel při připojení ověřuje. Přístup k datům jiné společnosti není omezen požadavky API, které takový přístup podporují.';
+
