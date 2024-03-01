c8becd18ee182f5f56e53bb07b9f29381ee5eb14
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:24 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/bitpay/language/el_gr/bitpay.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Bitpay
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Bitpay.!error.payment.expired'] = 'Η συναλλαγή έχει λήξει και δεν μπόρεσε να διεκπεραιωθεί.';
+$lang['Bitpay.!error.payment.invalid'] = 'Η συναλλαγή είναι άκυρη και δεν μπόρεσε να διεκπεραιωθεί.';
+$lang['Bitpay.!error.failed.response'] = 'Η συναλλαγή δεν μπόρεσε να διεκπεραιωθεί.';
+$lang['Bitpay.!error.token.valid'] = 'Το κουπόνι είναι άκυρο ή ο κωδικός αντιστοίχισης δεν έχει εξουσιοδοτηθεί στο BitPay.';
+$lang['Bitpay.!error.transaction_speed.valid'] = 'Παρακαλούμε επιλέξτε μια έγκυρη ταχύτητα συναλλαγής.';
+$lang['Bitpay.buildprocess.submit'] = 'Πληρώστε με Bitcoin';
+$lang['Bitpay.disconnect.button'] = 'Αποσύνδεση από το BitPay';
+$lang['Bitpay.connect.button'] = 'Συνδεθείτε με το BitPay';
+$lang['Bitpay.test_mode_note'] = 'Εάν είναι επιλεγμένο, η πύλη θα στέλνει αιτήματα πληρωμής στη δοκιμαστική διεύθυνση URL. Αν το αλλάξετε αυτό, θα αποσυνδεθεί η BitPay και θα δημιουργηθεί ένα νέο token.';
+$lang['Bitpay.test_mode'] = 'Λειτουργία δοκιμής';
+$lang['Bitpay.transaction_speed_note'] = 'Η ταχύτητα συναλλαγής καθορίζει πόσο γρήγορα ένα τιμολόγιο (στο Bitpay) μπορεί να θεωρηθεί "επιβεβαιωμένο".';
+$lang['Bitpay.pairing_code_note'] = 'Μεταβείτε στο ταμπλό της BitPay στην ενότητα "Payment Tools > API Tokens" και εγκρίνετε το παραγόμενο token χρησιμοποιώντας αυτόν τον κωδικό αντιστοίχισης "%1$s", και στη συνέχεια αποθηκεύστε τις ρυθμίσεις της πύλης κάνοντας κλικ στο "Update Settings".';
+$lang['Bitpay.transaction.speed.low'] = 'Χαμηλή';
+$lang['Bitpay.transaction.speed.medium'] = 'Μεσαίο';
+$lang['Bitpay.transaction.speed.high'] = 'Υψηλή';
+$lang['Bitpay.transaction_speed'] = 'Ταχύτητα συναλλαγής';
+$lang['Bitpay.pairing_code'] = 'Κωδικός αντιστοίχισης';
+$lang['Bitpay.description'] = 'Πάροχος υπηρεσιών πληρωμών Bitcoin και κρυπτονομισμάτων';
+$lang['Bitpay.name'] = 'Bitpay';
+
