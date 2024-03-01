bb81b9f648018d28ab6a697084d86f288f2561b8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:15 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/apply_invoice_late_fees.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Apply Invoice Late Fees
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.apply_invoice_late_fees.attempt'] = 'Intentar aplicar recargos por demora a las facturas vencidas.';
+$lang['Automation.task.apply_invoice_late_fees.completed'] = 'La tarea de aplicar recargos ha finalizado.';
+$lang['Automation.task.apply_invoice_late_fees.invoice_line'] = 'Tasa de demora';
+$lang['Automation.task.apply_invoice_late_fees.late_fee_failed'] = 'No se ha podido aplicar el recargo por demora a la factura #%1$s del cliente #%2$s.';
+$lang['Automation.task.apply_invoice_late_fees.late_fee_success'] = 'Se ha aplicado correctamente el recargo por demora a la factura #%1$s del cliente #%2$s.';
+
