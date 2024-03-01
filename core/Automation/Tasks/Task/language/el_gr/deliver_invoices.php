29fa65566dba75b2623cc778e65f55e0c8be5241
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:18 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/deliver_invoices.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Deliver Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.deliver_invoices.attempt'] = 'Προσπάθεια παράδοσης τιμολογίων που έχουν προγραμματιστεί για παράδοση.';
+$lang['Automation.task.deliver_invoices.completed'] = 'Η εργασία παράδοσης τιμολογίων έχει ολοκληρωθεί.';
+$lang['Automation.task.deliver_invoices.delivery_error'] = 'Αδυναμία παράδοσης τιμολογίων %3$s στον πελάτη #%1$s μέσω %2$s λόγω σφάλματος: %4$s';
+$lang['Automation.task.deliver_invoices.delivery_error_one'] = 'Αδυναμία παράδοσης 1 τιμολογίου στον πελάτη #%1$s μέσω %2$s λόγω σφάλματος: %3$s';
+$lang['Automation.task.deliver_invoices.delivery_success'] = 'Παραδόθηκαν επιτυχώς %3$s τιμολόγια στον πελάτη #%1$s μέσω %2$s.';
+$lang['Automation.task.deliver_invoices.delivery_success_one'] = 'Παραδόθηκε επιτυχώς 1 τιμολόγιο στον πελάτη #%1$s μέσω %2$s.';
+$lang['Automation.task.deliver_invoices.none'] = 'Δεν έχει προγραμματιστεί η παράδοση τιμολογίων.';
+$lang['Automation.task.deliver_invoices.method_email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['Automation.task.deliver_invoices.method_interfax'] = 'InterFax';
+$lang['Automation.task.deliver_invoices.method_postalmethods'] = 'PostalMethods';
+
