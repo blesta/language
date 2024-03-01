358c7e39b39764581d9e336aed98da68ffcac1c9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:36 -0800

    Updated es_es language files

---
+++ language/es_es/package_option_conditions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Package Option Conditions
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptionConditions.!error.id.exists'] = 'El ID de la condición de la opción dada no existe';
+$lang['PackageOptionConditions.!error.condition_set_id.exists'] = 'El ID del conjunto de condiciones de la opción dada no existe';
+$lang['PackageOptionConditions.!error.trigger_option_id.exists'] = 'El ID de la opción de activación no existe';
+$lang['PackageOptionConditions.!error.operator.valid'] = 'El operador dado no es válido';
+$lang['PackageOptionConditions.!error.value.valid'] = 'El valor dado no es válido';
+$lang['PackageOptionConditions.!error.value_id.exists'] = 'El valor ID dado no existe';
+$lang['PackageOptionConditions.getoperators.in'] = 'En';
+$lang['PackageOptionConditions.getoperators.notin'] = 'No en';
+$lang['PackageOptionConditions.getactions.hide'] = 'Ocultar';
+$lang['PackageOptionConditions.getactions.show'] = 'Mostrar';
+
