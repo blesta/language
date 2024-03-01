9f24ec21f0f72d39ba68f81a5faa04eb94d2c682
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:43 -0800

    Updated cs_cz language files

---
+++ plugins/extension_generator/language/cs_cz/extension_generator_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Extension Generator Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorPlugin.nav_secondary_staff.admin_main'] = 'Generátor rozšíření';
+$lang['ExtensionGeneratorPlugin.description'] = 'Jedná se o zásuvný modul pro automatické generování souborů pro nová rozšíření systému Blesta.';
+$lang['ExtensionGeneratorPlugin.name'] = 'Generátor rozšíření';
+
