defecdfda4d8622f9c823f68455892f136dc8d2a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:15 -0800

    Updated de_de language files

---
+++ core/Util/Filters/language/de_de/transaction_filters.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Transaction Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.transaction_filters.field_maximum_amount'] = 'Höchstbetrag';
+$lang['Util.filters.transaction_filters.field_minimum_amount'] = 'Mindestbetrag';
+$lang['Util.filters.transaction_filters.field_amount'] = 'Betrag';
+$lang['Util.filters.transaction_filters.field_end_date'] = 'Enddatum';
+$lang['Util.filters.transaction_filters.field_start_date'] = 'Datum des Beginns';
+$lang['Util.filters.transaction_filters.field_date'] = 'Datum';
+$lang['Util.filters.transaction_filters.field_applied_status'] = 'Angewandter Status';
+$lang['Util.filters.transaction_filters.field_reference_id'] = 'Referenz';
+$lang['Util.filters.transaction_filters.field_transaction_id'] = 'Nummer';
+$lang['Util.filters.transaction_filters.field_payment_type'] = 'Zahlungsart';
+$lang['Util.filters.transaction_filters.not_applied'] = 'Nicht angewandt';
+$lang['Util.filters.transaction_filters.partially_applied'] = 'Teilweise angewandt';
+$lang['Util.filters.transaction_filters.fully_applied'] = 'Vollständig angewandt';
+$lang['Util.filters.transaction_filters.any'] = 'Jede';
+
