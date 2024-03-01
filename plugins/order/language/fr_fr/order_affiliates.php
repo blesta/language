aa2ac3f0e2f31800aee55d6385342df4e3ef16d0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:37 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/order_affiliates.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Order Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliates.!error.affiliate_id.exists'] = 'ID d\'affilié non valide.';
+$lang['OrderAffiliates.!error.client_id.exists'] = 'ID de client non valide.';
+$lang['OrderAffiliates.!error.client_id.unique'] = 'L\'identifiant du client est déjà associé à un autre affilié.';
+$lang['OrderAffiliates.!error.code.empty'] = 'Veuillez saisir un code d\'affiliation.';
+$lang['OrderAffiliates.!error.code.unique'] = 'Ce code est déjà pris par un autre affilié.';
+$lang['OrderAffiliates.!error.status.valid'] = 'Veuillez sélectionner un statut d\'affilié valide.';
+$lang['OrderAffiliates.!error.date_added.format'] = 'La date ajoutée est dans un format de date non valide.';
+$lang['OrderAffiliates.!error.date_updated.format'] = 'La date mise à jour est dans un format de date non valide.';
+$lang['OrderAffiliates.getStatuses.active'] = 'Actif';
+$lang['OrderAffiliates.getStatuses.inactive'] = 'Inactif';
+
