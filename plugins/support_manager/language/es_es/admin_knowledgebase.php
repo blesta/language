7c8e01b514f3b01ae873cb441feed998461d8344
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:08 -0800

    Updated es_es language files

---
+++ plugins/support_manager/language/es_es/admin_knowledgebase.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Admin Knowledgebase
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminKnowledgebase.!success.category_added'] = 'La categoría se ha creado correctamente.';
+$lang['AdminKnowledgebase.!success.category_updated'] = 'La categoría se ha actualizado correctamente.';
+$lang['AdminKnowledgebase.!success.category_deleted'] = 'La categoría se ha eliminado correctamente.';
+$lang['AdminKnowledgebase.!success.article_added'] = 'El artículo se ha creado correctamente.';
+$lang['AdminKnowledgebase.!success.article_updated'] = 'El artículo se ha actualizado correctamente.';
+$lang['AdminKnowledgebase.!success.article_deleted'] = 'El artículo se ha eliminado correctamente.';
+$lang['AdminKnowledgebase.index.page_title'] = 'Gestor de asistencia > Base de conocimientos';
+$lang['AdminKnowledgebase.add.page_title'] = 'Gestor de Soporte > Base de Conocimientos > Añadir Artículo';
+$lang['AdminKnowledgebase.edit.page_title'] = 'Gestor de Soporte > Base de Conocimientos > Editar Artículo';
+$lang['AdminKnowledgebase.addcategory.page_title'] = 'Gestor de Soporte > Base de Conocimientos > Añadir Categoría';
+$lang['AdminKnowledgebase.editcategory.page_title'] = 'Gestor de Soporte > Base de Conocimientos > Editar Categoría';
+$lang['AdminKnowledgebase.index.add_article'] = 'Añadir artículo';
+$lang['AdminKnowledgebase.index.add_category'] = 'Añadir categoría';
+$lang['AdminKnowledgebase.index.boxtitle_knowledgebase'] = 'Base de conocimientos';
+$lang['AdminKnowledgebase.index.go_back'] = 'Subir un nivel';
+$lang['AdminKnowledgebase.index.edit'] = 'Editar';
+$lang['AdminKnowledgebase.index.delete'] = 'Borrar';
+$lang['AdminKnowledgebase.modal.delete_article'] = '¿Seguro que quieres borrar este artículo?';
+$lang['AdminKnowledgebase.modal.delete_category'] = '¿Está seguro de que desea eliminar esta categoría? Todas las subcategorías y artículos de esta categoría se trasladarán a la categoría principal.';
+$lang['AdminKnowledgebase.index.no_results'] = 'No hay categorías ni artículos en esta sección.';
+$lang['AdminKnowledgebase.addcategory.boxtitle_addcategory'] = 'Añadir categoría';
+$lang['AdminKnowledgebase.addcategory.field_name'] = 'Nombre';
+$lang['AdminKnowledgebase.addcategory.field_parent_id'] = 'Categoría principal';
+$lang['AdminKnowledgebase.addcategory.field_description'] = 'Descripción';
+$lang['AdminKnowledgebase.addcategory.field_access'] = 'Accesibilidad';
+$lang['AdminKnowledgebase.addcategory.submit_add'] = 'Crear categoría';
+$lang['AdminKnowledgebase.addcategory.submit_cancel'] = 'Cancelar';
+$lang['AdminKnowledgebase.editcategory.boxtitle_editcategory'] = 'Categoría de actualización';
+$lang['AdminKnowledgebase.editcategory.field_name'] = 'Nombre';
+$lang['AdminKnowledgebase.editcategory.field_parent_id'] = 'Categoría principal';
+$lang['AdminKnowledgebase.editcategory.field_description'] = 'Descripción';
+$lang['AdminKnowledgebase.editcategory.field_access'] = 'Accesibilidad';
+$lang['AdminKnowledgebase.editcategory.submit_edit'] = 'Categoría de actualización';
+$lang['AdminKnowledgebase.editcategory.submit_cancel'] = 'Cancelar';
+$lang['AdminKnowledgebase.add.boxtitle_add'] = 'Añadir artículo';
+$lang['AdminKnowledgebase.add.field_access'] = 'Accesibilidad';
+$lang['AdminKnowledgebase.add.field_categories'] = 'Categoría';
+$lang['AdminKnowledgebase.add.submit_add'] = 'Crear artículo';
+$lang['AdminKnowledgebase.add.submit_cancel'] = 'Cancelar';
+$lang['AdminKnowledgebase.edit.boxtitle_edit'] = 'Actualizar el artículo';
+$lang['AdminKnowledgebase.edit.field_access'] = 'Accesibilidad';
+$lang['AdminKnowledgebase.edit.field_categories'] = 'Categoría';
+$lang['AdminKnowledgebase.edit.submit_edit'] = 'Actualizar el artículo';
+$lang['AdminKnowledgebase.edit.submit_cancel'] = 'Cancelar';
+$lang['AdminKnowledgebase.article_content.title'] = 'Título';
+$lang['AdminKnowledgebase.article_content.content_type'] = 'Tipo de contenido';
+$lang['AdminKnowledgebase.article_content.body'] = 'Cuerpo';
+$lang['AdminKnowledgebase.select.none'] = '-- Ninguno --';
+$lang['AdminKnowledgebase.!text.root_directory'] = 'Directorio principal';
+$lang['AdminKnowledgebase.!text.open_enclosure'] = '(';
+$lang['AdminKnowledgebase.!text.closed_enclosure'] = ')';
+$lang['AdminKnowledgebase.!text.enclosure_separator'] = '/';
+$lang['AdminKnowledgebase.!text.add_category'] = 'Añadir categoría';
+$lang['AdminKnowledgebase.!text.directory_separator'] = '/';
+$lang['AdminKnowledgebase.!tooltip.access_article'] = 'Los artículos públicos son accesibles a todo el mundo. Los artículos privados sólo son accesibles para los usuarios que han iniciado sesión. Los artículos ocultos son inaccesibles. Los artículos del personal sólo son visibles para los miembros del personal.';
+$lang['AdminKnowledgebase.!tooltip.access_category'] = 'Las categorías públicas son accesibles a todo el mundo. Las categorías privadas sólo son accesibles para los usuarios que han iniciado sesión. Las categorías ocultas son inaccesibles. Las categorías del personal sólo son visibles para los miembros del personal.';
+
