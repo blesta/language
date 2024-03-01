8867a1e1d805246d7fba89a57cfd7390b6c5d004
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:08 -0800

    Updated cs_cz language files

---
+++ core/Util/Filters/language/cs_cz/client_filters.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Client Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.client_filters.field_last_seen_end_date'] = 'Datum ukončení';
+$lang['Util.filters.client_filters.field_last_seen_start_date'] = 'Datum zahájení';
+$lang['Util.filters.client_filters.field_last_seen'] = 'Naposledy viděno';
+$lang['Util.filters.client_filters.field_invoice_method'] = 'Způsob fakturace';
+$lang['Util.filters.client_filters.field_client_group_id'] = 'Skupina klientů';
+$lang['Util.filters.client_filters.field_contact_country'] = 'Země';
+$lang['Util.filters.client_filters.field_contact_company'] = 'Společnost';
+$lang['Util.filters.client_filters.field_contact_email'] = 'E-mail';
+$lang['Util.filters.client_filters.field_contact_name'] = 'Název';
+$lang['Util.filters.client_filters.any'] = 'Jakýkoli';
+
