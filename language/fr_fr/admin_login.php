7de5e450baa6fd23e6713a9dc12b944a62248d2b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:53 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_login.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Admin Login
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminLogin.index.page_title'] = 'Se connecter';
+$lang['AdminLogin.index.title_adminarea'] = '%1$s | Domaine du personnel';
+$lang['AdminLogin.index.field_username'] = 'Nom d\'utilisateur';
+$lang['AdminLogin.index.field_password'] = 'Mot de passe';
+$lang['AdminLogin.index.field_rememberme'] = 'Souvenez-vous de moi sur cet ordinateur.';
+$lang['AdminLogin.index.field_loginsubmit'] = 'Se connecter';
+$lang['AdminLogin.index.link_resetpassword'] = 'Réinitialiser mon mot de passe';
+$lang['AdminLogin.otp.page_title'] = 'Connexion OTP';
+$lang['AdminLogin.otp.title_adminarea'] = '%1$s | Domaine du personnel';
+$lang['AdminLogin.otp.field_password'] = 'Mot de passe unique';
+$lang['AdminLogin.otp.field_loginsubmit'] = 'Se connecter';
+$lang['AdminLogin.otp.link_login'] = 'Annuler, Se connecter';
+$lang['AdminLogin.reset.page_title'] = 'Réinitialiser le mot de passe';
+$lang['AdminLogin.reset.title_adminarea'] = '%1$s | Réinitialiser le mot de passe';
+$lang['AdminLogin.reset.field_username'] = 'Nom d\'utilisateur';
+$lang['AdminLogin.reset.field_resetsubmit'] = 'Réinitialiser le mot de passe';
+$lang['AdminLogin.reset.link_login'] = 'Annuler, Se connecter';
+$lang['AdminLogin.confirmreset.page_title'] = 'Confirmer la réinitialisation du mot de passe';
+$lang['AdminLogin.confirmreset.title_adminarea'] = '%1$s | Confirmer la réinitialisation du mot de passe';
+$lang['AdminLogin.confirmreset.field_new_password'] = 'Nouveau mot de passe';
+$lang['AdminLogin.confirmreset.field_confirm_password'] = 'Confirmer le nouveau mot de passe';
+$lang['AdminLogin.confirmreset.field_resetsubmit'] = 'Définir le mot de passe';
+$lang['AdminLogin.confirmreset.link_login'] = 'Annuler, Se connecter';
+$lang['AdminLogin.setup.page_title'] = 'Configuration initiale';
+$lang['AdminLogin.setup.title_adminarea'] = 'Configuration initiale';
+$lang['AdminLogin.setup.field_license_key'] = 'Clé de licence';
+$lang['AdminLogin.setup.trial_newsletter'] = 'En vous inscrivant à un essai, vous acceptez de recevoir des courriels de notre part pendant la durée de l\'essai. Vous pouvez vous désinscrire à tout moment.';
+$lang['AdminLogin.setup.field_newsletter'] = 'S\'inscrire à notre bulletin d\'information. Vous pouvez vous désinscrire à tout moment.';
+$lang['AdminLogin.setup.heading_create_account'] = 'Créez votre compte personnel';
+$lang['AdminLogin.setup.field_first_name'] = 'Prénom';
+$lang['AdminLogin.setup.field_last_name'] = 'Nom de famille';
+$lang['AdminLogin.setup.field_username'] = 'Nom d\'utilisateur';
+$lang['AdminLogin.setup.field_email'] = 'Adresse électronique';
+$lang['AdminLogin.setup.field_password'] = 'Mot de passe';
+$lang['AdminLogin.setup.field_confirm_password'] = 'Confirmer le mot de passe';
+$lang['AdminLogin.setup.field_enter_key_true'] = 'J\'ai une clé de licence à saisir';
+$lang['AdminLogin.setup.field_enter_key_false'] = 'Je souhaite commencer un essai gratuit de 30 jours';
+$lang['AdminLogin.setup.field_submit'] = 'Finition';
+$lang['AdminLogin.!error.unknown_user'] = 'Ce nom d\'utilisateur n\'est pas reconnu ou le mot de passe ne peut pas être réinitialisé.';
+$lang['AdminLogin.!error.captcha.invalid'] = 'Le captcha saisi n\'est pas valide. Veuillez réessayer.';
+$lang['AdminLogin.!success.reset_sent'] = 'Un courriel de confirmation a été envoyé à l\'adresse indiquée.';
+$lang['AdminLogin.!info.reset_password'] = 'Saisissez votre nom d\'utilisateur ci-dessous et un lien vous sera envoyé par courrier électronique afin que vous puissiez créer un nouveau mot de passe.';
+$lang['AdminLogin.!info.otp'] = 'L\'authentification à deux facteurs est requise pour cet utilisateur. Veuillez saisir votre OTP (mot de passe à usage unique) ci-dessous.';
+
