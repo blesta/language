273fde8d7f582fc028a7106bd443e784238cd2f4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:19 -0800

    Updated fr_fr language files

---
+++ core/Automation/Tasks/Task/language/fr_fr/card_expiration_reminders.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Card Expiration Reminders
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.card_expiration_reminders.attempt'] = 'Tentative d\'envoi de rappels d\'expiration de cartes.';
+$lang['Automation.task.card_expiration_reminders.completed'] = 'La tâche de rappel de l\'expiration de la carte est terminée.';
+$lang['Automation.task.card_expiration_reminders.failed'] = 'Le rappel d\'expiration pour le contact %1$s %2$s du client #%3$s n\'a pas pu être envoyé.';
+$lang['Automation.task.card_expiration_reminders.success'] = 'Le rappel d\'expiration pour le contact %1$s %2$s du client #%3$s a été envoyé avec succès.';
+
