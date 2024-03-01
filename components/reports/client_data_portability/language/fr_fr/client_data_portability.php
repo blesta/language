767bfa4f1b79bca2e8c61a9f9f3fa5ed4ca39afc
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:10 -0800

    Updated fr_fr language files

---
+++ components/reports/client_data_portability/language/fr_fr/client_data_portability.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Client Data Portability
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDataPortability.name'] = 'Portabilité des données des clients';
+$lang['ClientDataPortability.options.field_client_id'] = 'Identifiant du client';
+$lang['ClientDataPortability.!error.client_id'] = 'L\'identifiant du client n\'est pas valide.';
+$lang['ClientDataPortability.!tooltip.client_id'] = 'Il s\'agit de l\'identifiant interne du client qui apparaît dans l\'URL lorsque vous consultez le profil d\'un client.';
+
