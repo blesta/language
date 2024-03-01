09da61e46fe30c31092375df4a8f373aa22a645a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:57 -0800

    Updated de_de language files

---
+++ plugins/mass_mailer/language/de_de/mass_mailer_plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Mass Mailer Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerPlugin.cron.mass_mail_desc'] = 'Versendet Massen-E-Mails für alle anstehenden Mailing-Aufträge.';
+$lang['MassMailerPlugin.cron.mass_mail_name'] = 'Massen-E-Mail senden';
+$lang['MassMailerPlugin.cron.export_desc'] = 'Erstellt eine CSV-Datei für jeden zum Export anstehenden Massenversandauftrag.';
+$lang['MassMailerPlugin.cron.export_name'] = 'Mailing-Export erstellen';
+$lang['MassMailerPlugin.permission.admin_compose'] = 'Massenmailer E-Mail verfassen';
+$lang['MassMailerPlugin.permission.admin_filter'] = 'Massenmailer-Filter';
+$lang['MassMailerPlugin.permission.admin_main'] = 'Massenmailer-Listen';
+$lang['MassMailerPlugin.nav_secondary_staff.admin_main'] = 'Massenversender';
+$lang['MassMailerPlugin.description'] = 'Sendet E-Mails an mehrere Clients im System auf einmal.';
+$lang['MassMailerPlugin.name'] = 'Massenversender';
+
