22fae98e35cd655e2c0fc06884fbd8c21e21fa97
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:50 -0800

    Updated el_gr language files

---
+++ components/modules/ispmanager/language/el_gr/ispmanager.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Ispmanager
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Ispmanager.!error.api'] = 'Προέκυψε ένα εσωτερικό σφάλμα ή ο διακομιστής δεν απάντησε στην αίτηση.';
+$lang['Ispmanager.!error.ispmanager_password.matches'] = 'Ο κωδικός πρόσβασης και ο κωδικός επιβεβαίωσης δεν ταιριάζουν.';
+$lang['Ispmanager.!error.ispmanager_password.valid'] = 'Ο κωδικός πρόσβασης πρέπει να έχει μήκος τουλάχιστον 8 χαρακτήρων.';
+$lang['Ispmanager.!error.ispmanager_username.length'] = 'Το όνομα χρήστη πρέπει να έχει μήκος μεταξύ 1 και 16 χαρακτήρων.';
+$lang['Ispmanager.!error.ispmanager_username.test'] = 'Το όνομα χρήστη δεν πρέπει να αρχίζει με "test".';
+$lang['Ispmanager.!error.ispmanager_username.format'] = 'Το όνομα χρήστη μπορεί να περιέχει μόνο γράμματα και αριθμούς.';
+$lang['Ispmanager.!error.ispmanager_domain.test'] = 'Το όνομα τομέα δεν μπορεί να αρχίζει με \'test\'.';
+$lang['Ispmanager.!error.ispmanager_domain.format'] = 'Παρακαλούμε εισάγετε ένα έγκυρο όνομα τομέα, π.χ. domain.com.';
+$lang['Ispmanager.!error.module_row.missing'] = 'Εμφανίστηκε ένα εσωτερικό σφάλμα. Η σειρά της μονάδας δεν είναι διαθέσιμη.';
+$lang['Ispmanager.!error.api.internal'] = 'Προέκυψε ένα εσωτερικό σφάλμα ή ο διακομιστής δεν απάντησε στην αίτηση.';
+$lang['Ispmanager.!error.meta[template].empty'] = 'Απαιτείται ένα πρότυπο.';
+$lang['Ispmanager.!error.name_servers_count'] = 'Πρέπει να ορίσετε τουλάχιστον 2 διακομιστές ονομάτων.';
+$lang['Ispmanager.!error.name_servers_valid'] = 'Ένας ή περισσότεροι από τους καταχωρημένους διακομιστές ονομάτων είναι άκυροι.';
+$lang['Ispmanager.!error.account_limit_valid'] = 'Το Όριο λογαριασμού πρέπει να παραμείνει κενό (για απεριόριστους λογαριασμούς) ή να οριστεί σε κάποια ακέραια τιμή.';
+$lang['Ispmanager.!error.password_valid_connection'] = 'Δεν κατέστη δυνατή η σύνδεση με τον διακομιστή. Ελέγξτε αν το Όνομα χρήστη και ο Κωδικός πρόσβασης είναι σωστά.';
+$lang['Ispmanager.!error.password_valid'] = 'Ο κωδικός πρόσβασης φαίνεται να είναι άκυρος.';
+$lang['Ispmanager.!error.user_name_valid'] = 'Το όνομα χρήστη φαίνεται να είναι άκυρο.';
+$lang['Ispmanager.!error.host_name_valid'] = 'Το όνομα κεντρικού υπολογιστή φαίνεται να είναι άκυρο.';
+$lang['Ispmanager.!error.server_name_valid'] = 'Πρέπει να εισαγάγετε μια ετικέτα διακομιστή.';
+$lang['Ispmanager.service_field.tooltip.password'] = 'Μπορείτε να αφήσετε τον κωδικό πρόσβασης κενό για να δημιουργηθεί αυτόματα.';
+$lang['Ispmanager.service_field.tooltip.username'] = 'Μπορείτε να αφήσετε το όνομα χρήστη κενό για να δημιουργηθεί αυτόματα.';
+$lang['Ispmanager.service_field.tooltip.username_edit'] = 'Αυτή η αλλαγή δεν θα επηρεάσει το ISPmanager, αλλά θα αλλάξει μόνο την τοπική εγγραφή στο Blesta.';
+$lang['Ispmanager.service_field.tooltip.domain_edit'] = 'Αυτή η αλλαγή δεν θα επηρεάσει το ISPmanager, αλλά θα αλλάξει μόνο την τοπική εγγραφή στο Blesta.';
+$lang['Ispmanager.service_info.option_login'] = 'Συνδεθείτε';
+$lang['Ispmanager.service_info.options'] = 'Επιλογές';
+$lang['Ispmanager.service_info.server'] = 'Διακομιστής';
+$lang['Ispmanager.service_info.password'] = 'Κωδικός πρόσβασης';
+$lang['Ispmanager.service_info.username'] = 'Όνομα χρήστη';
+$lang['Ispmanager.service_field.password'] = 'Κωδικός πρόσβασης';
+$lang['Ispmanager.service_field.username'] = 'Όνομα χρήστη';
+$lang['Ispmanager.service_field.domain'] = 'Τομέας';
+$lang['Ispmanager.package_fields.template'] = 'Πρότυπο';
+$lang['Ispmanager.tab_client_actions.field_password_submit'] = 'Ενημέρωση κωδικού πρόσβασης';
+$lang['Ispmanager.tab_client_actions.field_ispmanager_confirm_password'] = 'Επιβεβαίωση κωδικού πρόσβασης';
+$lang['Ispmanager.tab_client_actions.field_ispmanager_password'] = 'Κωδικός πρόσβασης';
+$lang['Ispmanager.tab_client_actions.change_password'] = 'Αλλαγή κωδικού πρόσβασης';
+$lang['Ispmanager.row_meta.account_limit'] = 'Όριο λογαριασμού';
+$lang['Ispmanager.row_meta.use_ssl'] = 'Χρησιμοποιήστε SSL κατά τη σύνδεση στο API (συνιστάται)';
+$lang['Ispmanager.row_meta.password'] = 'Κωδικός πρόσβασης';
+$lang['Ispmanager.row_meta.user_name'] = 'Όνομα χρήστη';
+$lang['Ispmanager.row_meta.host_name'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Ispmanager.row_meta.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Ispmanager.edit_row.add_btn'] = 'Επεξεργασία διακομιστή';
+$lang['Ispmanager.edit_row.remove_name_server'] = 'Αφαιρέστε το';
+$lang['Ispmanager.edit_row.name_server'] = 'Διακομιστής ονομάτων %1$s';
+$lang['Ispmanager.edit_row.name_server_host_col'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Ispmanager.edit_row.name_server_col'] = 'Διακομιστής ονομάτων';
+$lang['Ispmanager.edit_row.name_server_btn'] = 'Προσθήκη πρόσθετου διακομιστή ονομάτων';
+$lang['Ispmanager.edit_row.notes_title'] = 'Σημειώσεις';
+$lang['Ispmanager.edit_row.name_servers_title'] = 'Εξυπηρετητές ονομάτων';
+$lang['Ispmanager.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Ispmanager.edit_row.box_title'] = 'Επεξεργασία διακομιστή ISPmanager';
+$lang['Ispmanager.add_row.add_btn'] = 'Προσθήκη διακομιστή';
+$lang['Ispmanager.add_row.remove_name_server'] = 'Αφαιρέστε το';
+$lang['Ispmanager.add_row.name_server'] = 'Διακομιστής ονομάτων %1$s';
+$lang['Ispmanager.add_row.name_server_host_col'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Ispmanager.add_row.name_server_col'] = 'Διακομιστής ονομάτων';
+$lang['Ispmanager.add_row.name_server_btn'] = 'Προσθήκη πρόσθετου διακομιστή ονομάτων';
+$lang['Ispmanager.add_row.notes_title'] = 'Σημειώσεις';
+$lang['Ispmanager.add_row.name_servers_title'] = 'Εξυπηρετητές ονομάτων';
+$lang['Ispmanager.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Ispmanager.add_row.box_title'] = 'Προσθήκη διακομιστή ISPmanager';
+$lang['Ispmanager.order_options.roundrobin'] = 'Ομοιόμορφη κατανομή μεταξύ των διακομιστών';
+$lang['Ispmanager.order_options.first'] = 'Πρώτος μη πλήρης διακομιστής';
+$lang['Ispmanager.manage.module_groups_no_results'] = 'Δεν υπάρχουν ομάδες διακομιστών.';
+$lang['Ispmanager.manage.module_rows_no_results'] = 'Δεν υπάρχουν διακομιστές.';
+$lang['Ispmanager.manage.module_groups.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την ομάδα διακομιστών;';
+$lang['Ispmanager.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον διακομιστή;';
+$lang['Ispmanager.manage.module_groups.delete'] = 'Διαγραφή';
+$lang['Ispmanager.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['Ispmanager.manage.module_groups.edit'] = 'Επεξεργασία';
+$lang['Ispmanager.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['Ispmanager.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Ispmanager.manage.module_groups_heading.options'] = 'Επιλογές';
+$lang['Ispmanager.manage.module_groups_heading.servers'] = 'Αριθμός διακομιστών';
+$lang['Ispmanager.manage.module_groups_heading.name'] = 'Όνομα ομάδας';
+$lang['Ispmanager.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['Ispmanager.manage.module_rows_heading.accounts'] = 'Λογαριασμοί';
+$lang['Ispmanager.manage.module_rows_heading.hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Ispmanager.manage.module_rows_heading.name'] = 'Ετικέτα διακομιστή';
+$lang['Ispmanager.manage.module_groups_title'] = 'Ομάδες διακομιστών';
+$lang['Ispmanager.manage.module_rows_title'] = 'Διακομιστές';
+$lang['Ispmanager.add_module_group'] = 'Προσθήκη ομάδας διακομιστών';
+$lang['Ispmanager.add_module_row'] = 'Προσθήκη διακομιστή';
+$lang['Ispmanager.tab_client_actions'] = 'Δράσεις';
+$lang['Ispmanager.module_group'] = 'Ομάδα διακομιστών';
+$lang['Ispmanager.module_row_plural'] = 'Διακομιστές';
+$lang['Ispmanager.module_row'] = 'Διακομιστής';
+$lang['Ispmanager.description'] = 'Το ISPmanager παρέχει πλούσιο σύνολο δυνατοτήτων για τη διαχείριση ιστότοπων, τη δημιουργία χρηστών, το χειρισμό domains, emails, βάσεων δεδομένων κ.λπ.';
+$lang['Ispmanager.name'] = 'ISPmanager';
+
