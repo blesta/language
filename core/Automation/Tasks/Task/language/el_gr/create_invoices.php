63b0414049bf0bd924ffad70e03cd8152bdcb6b8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:17 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/create_invoices.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Create Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.create_invoices.attempt'] = 'Προσπάθεια ανανέωσης υπηρεσιών και δημιουργίας τιμολογίων.';
+$lang['Automation.task.create_invoices.completed'] = 'Η εργασία δημιουργίας τιμολογίων έχει ολοκληρωθεί.';
+$lang['Automation.task.create_invoices.recurring_invoice_failed'] = 'Αδυναμία δημιουργίας νέου τιμολογίου από επαναλαμβανόμενο τιμολόγιο #%1$s για τον πελάτη #%2$s.';
+$lang['Automation.task.create_invoices.recurring_invoice_success'] = 'Δημιουργήθηκε επιτυχώς ένα νέο τιμολόγιο από το επαναλαμβανόμενο τιμολόγιο #%1$s για τον πελάτη #%2$s.';
+$lang['Automation.task.create_invoices.service_invoice_success'] = 'Δημιουργήθηκε επιτυχώς το τιμολόγιο #%1$s για τον πελάτη #%2$s που περιέχει υπηρεσίες %3$s.';
+$lang['Automation.task.create_invoices.service_invoice_error'] = 'Η δημιουργία τιμολογίου για τον πελάτη #%2$s που περιέχει υπηρεσίες %3$s απέτυχε διότι: %1$s';
+
