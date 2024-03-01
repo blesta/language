434b8169771a14b2f2cd116de4dafeb06a1c8ed0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:10 -0800

    Updated fr_fr language files

---
+++ plugins/support_manager/language/fr_fr/support_manager_kb_categories.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Support Manager Kb Categories
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerKbCategories.!error.parent_id.exists'] = 'ID parent invalide.';
+$lang['SupportManagerKbCategories.!error.parent_id.valid_company'] = 'Cette catégorie ne peut pas être attribuée à un parent d\'une société distincte.';
+$lang['SupportManagerKbCategories.!error.parent_id.valid_parent'] = 'Cette catégorie ne peut pas être un parent pour elle-même.';
+$lang['SupportManagerKbCategories.!error.company_id.exists'] = 'ID de l\'entreprise non valide.';
+$lang['SupportManagerKbCategories.!error.name.empty'] = 'Veuillez saisir un nom de catégorie.';
+$lang['SupportManagerKbCategories.!error.name.length'] = 'Le nom de la catégorie ne doit pas dépasser 255 caractères.';
+$lang['SupportManagerKbCategories.!error.access.type'] = 'Type d\'accès non valide.';
+$lang['SupportManagerKbCategories.!error.category_id.exists'] = 'ID de catégorie non valide.';
+$lang['SupportManagerKbCategories.!error.articles.parent'] = 'La catégorie ne peut être supprimée que si tous les articles qu\'elle contient appartiennent à une autre catégorie.';
+$lang['SupportManagerKbCategories.access_types.public'] = 'Public';
+$lang['SupportManagerKbCategories.access_types.private'] = 'Privé';
+$lang['SupportManagerKbCategories.access_types.hidden'] = 'Caché';
+$lang['SupportManagerKbCategories.access_types.staff'] = 'Personnel';
+
