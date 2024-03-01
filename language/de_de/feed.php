af2bf3f67d0b207736cee479a245351751cb060d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:01 -0800

    Updated de_de language files

---
+++ language/de_de/feed.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Feed.!error.disabled'] = 'Dieser Datenfeed ist deaktiviert. Um ihn zu aktivieren, besuchen Sie Einstellungen > Unternehmen > Feeds im Personalbereich.';
+$lang['Feed.!error.invalid'] = 'Die angeforderte Ressource existiert nicht oder ist ungültig.';
+
