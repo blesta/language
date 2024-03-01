19629471a5723fe5cb5ee5a218fe119da52bb5a7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:32 -0800

    Updated es_es language files

---
+++ language/es_es/module_manager.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Module Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ModuleManager.!error.company_id.valid'] = 'El ID de la empresa no es válido.';
+$lang['ModuleManager.!error.class.valid'] = 'La clase no es válida.';
+$lang['ModuleManager.!error.name.valid'] = 'El nombre no es válido.';
+$lang['ModuleManager.!error.version.valid'] = 'La versión no es válida.';
+$lang['ModuleManager.!error.type_id.valid'] = 'El ID de tipo no es válido.';
+$lang['ModuleManager.!error.name.empty'] = 'Debe especificar un nombre para el grupo.';
+$lang['ModuleManager.!error.module_rows[].valid'] = 'Una o más de las filas especificadas no pertenecen a este módulo.';
+$lang['ModuleManager.!error.force_limits.valid'] = 'El valor de si se deben aplicar límites estrictos debe ser 0 ó 1.';
+$lang['ModuleManager.!error.module_row_id.assigned_service'] = 'La fila no puede borrarse porque pertenece a uno o varios servicios.';
+$lang['ModuleManager.!error.module_row_id.assigned_package'] = 'La fila no se puede eliminar porque pertenece a uno o varios paquetes.';
+$lang['ModuleManager.!error.module_group_id.assigned_package'] = 'El grupo no se puede eliminar porque pertenece a uno o varios paquetes.';
+$lang['ModuleManager.!error.module_id.assigned_package'] = 'El módulo no puede ser desinstalado porque es utilizado por uno o más paquetes activos.';
+$lang['ModuleManager.!error.module_id.assigned_service'] = 'El módulo no puede ser desinstalado porque es utilizado por uno o más servicios no cancelados.';
+
