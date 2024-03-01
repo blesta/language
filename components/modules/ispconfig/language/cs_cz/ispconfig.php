fb27d50ed81aab9e884a043cefb9e91a7cdc2045
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:26 -0800

    Updated cs_cz language files

---
+++ components/modules/ispconfig/language/cs_cz/ispconfig.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Ispconfig
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Ispconfig.!error.ispconfig_password.matches'] = 'Heslo a potvrzovací heslo se neshodují.';
+$lang['Ispconfig.!error.ispconfig_password.valid'] = 'Heslo musí mít alespoň 8 znaků.';
+$lang['Ispconfig.!error.ispconfig_username.length'] = 'Uživatelské jméno musí mít délku 1 až 16 znaků.';
+$lang['Ispconfig.!error.ispconfig_username.test'] = 'Uživatelské jméno nesmí začínat slovem "test".';
+$lang['Ispconfig.!error.ispconfig_username.format'] = 'Uživatelské jméno může obsahovat pouze písmena a číslice a nesmí začínat číslicí.';
+$lang['Ispconfig.!error.ispconfig_domain.test'] = 'Název domény nemůže začínat na \'test\'.';
+$lang['Ispconfig.!error.ispconfig_domain.format'] = 'Zadejte platný název domény, např. domain.com.';
+$lang['Ispconfig.!error.module_row.missing'] = 'Došlo k interní chybě. Řádek modulu není k dispozici.';
+$lang['Ispconfig.!error.api.internal'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+$lang['Ispconfig.!error.meta[package].empty'] = 'Je vyžadován balíček ISPConfig.';
+$lang['Ispconfig.!error.name_servers_count'] = 'Je třeba definovat alespoň 2 jmenné servery.';
+$lang['Ispconfig.!error.name_servers_valid'] = 'Jeden nebo více zadaných názvových serverů jsou neplatné.';
+$lang['Ispconfig.!error.account_limit_valid'] = 'Limit účtu musí zůstat prázdný (pro neomezený počet účtů) nebo musí být nastaven na nějakou celočíselnou hodnotu.';
+$lang['Ispconfig.!error.remote_password_valid_connection'] = 'Připojení k serveru se nepodařilo navázat. Zkontrolujte, zda jsou název hostitele, uživatelské jméno a heslo správné.';
+$lang['Ispconfig.!error.remote_password_valid'] = 'Heslo se zdá být neplatné.';
+$lang['Ispconfig.!error.user_name_valid'] = 'Uživatelské jméno se zdá být neplatné.';
+$lang['Ispconfig.!error.port_format'] = 'Port musí být číslo.';
+$lang['Ispconfig.!error.host_name_valid'] = 'Název hostitele se zdá být neplatný.';
+$lang['Ispconfig.!error.server_name_valid'] = 'Je třeba zadat označení serveru.';
+$lang['Ispconfig.service_field.tooltip.password'] = 'Heslo můžete nechat prázdné, aby se vygenerovalo automaticky.';
+$lang['Ispconfig.service_field.tooltip.username'] = 'Uživatelské jméno můžete nechat prázdné, aby se vygenerovalo automaticky.';
+$lang['Ispconfig.service_info.option_login'] = 'Přihlášení';
+$lang['Ispconfig.service_info.options'] = 'Možnosti';
+$lang['Ispconfig.service_info.server'] = 'Server';
+$lang['Ispconfig.service_info.password'] = 'Heslo';
+$lang['Ispconfig.service_info.username'] = 'Uživatelské jméno';
+$lang['Ispconfig.tab_client_stats.usage_unlimited'] = '(%1$s MB/∞)';
+$lang['Ispconfig.tab_client_stats.usage'] = '(%1$s MB/%2$s MB)';
+$lang['Ispconfig.tab_client_stats.disk_title'] = 'Využití disku';
+$lang['Ispconfig.tab_client_stats.bandwidth_title'] = 'Využití šířky pásma (měsíc k datu)';
+$lang['Ispconfig.tab_client_stats.info.limit_database'] = 'Databáze Limit';
+$lang['Ispconfig.tab_client_stats.info.limit_webdav_user'] = 'Limit uživatelů WebDAV';
+$lang['Ispconfig.tab_client_stats.info.limit_web_aliasdomain'] = 'Omezení domény aliasu';
+$lang['Ispconfig.tab_client_stats.info.limit_web_subdomain'] = 'Omezení subdomén';
+$lang['Ispconfig.tab_client_stats.info.limit_web_domain'] = 'Limit webových domén';
+$lang['Ispconfig.tab_client_stats.info.limit_ftp_user'] = 'Limit uživatelů FTP';
+$lang['Ispconfig.tab_client_stats.info.limit_traffic_quota'] = 'Kvóta šířky pásma';
+$lang['Ispconfig.tab_client_stats.info.limit_web_quota'] = 'Webová kvóta';
+$lang['Ispconfig.tab_client_stats.info_heading.value'] = 'Hodnota';
+$lang['Ispconfig.tab_client_stats.info_heading.field'] = 'Pole';
+$lang['Ispconfig.tab_client_stats.info_title'] = 'Informace';
+$lang['Ispconfig.tab_client_actions.field_password_submit'] = 'Aktualizace hesla';
+$lang['Ispconfig.tab_client_actions.field_ispconfig_password'] = 'Heslo';
+$lang['Ispconfig.tab_client_actions.change_password'] = 'Změna hesla';
+$lang['Ispconfig.tab_stats.info.limit_database'] = 'Databáze Limit';
+$lang['Ispconfig.tab_stats.info.limit_webdav_user'] = 'Limit uživatelů WebDAV';
+$lang['Ispconfig.tab_stats.info.limit_web_aliasdomain'] = 'Omezení domény aliasu';
+$lang['Ispconfig.tab_stats.info.limit_web_subdomain'] = 'Omezení subdomén';
+$lang['Ispconfig.tab_stats.info.limit_web_domain'] = 'Limit webových domén';
+$lang['Ispconfig.tab_stats.info.limit_ftp_user'] = 'Limit uživatelů FTP';
+$lang['Ispconfig.tab_stats.info.limit_traffic_quota'] = 'Kvóta šířky pásma';
+$lang['Ispconfig.tab_stats.info.limit_web_quota'] = 'Webová kvóta';
+$lang['Ispconfig.tab_stats.info_heading.value'] = 'Hodnota';
+$lang['Ispconfig.tab_stats.info_heading.field'] = 'Pole';
+$lang['Ispconfig.tab_stats.info_title'] = 'Informace';
+$lang['Ispconfig.service_field.password'] = 'Heslo';
+$lang['Ispconfig.service_field.username'] = 'Uživatelské jméno';
+$lang['Ispconfig.service_field.domain'] = 'Doména';
+$lang['Ispconfig.package_fields.ssh_options'] = 'Možnosti SSH';
+$lang['Ispconfig.package_fields.php_options'] = 'Možnosti PHP';
+$lang['Ispconfig.package_fields.package'] = 'Balíček ISPConfig';
+$lang['Ispconfig.row_meta.account_limit'] = 'Limit účtu';
+$lang['Ispconfig.row_meta.use_ssl'] = 'Při připojování k rozhraní API používejte protokol SSL (doporučeno).';
+$lang['Ispconfig.row_meta.password'] = 'Heslo';
+$lang['Ispconfig.row_meta.user_name'] = 'Uživatelské jméno';
+$lang['Ispconfig.row_meta.default_port'] = '8080';
+$lang['Ispconfig.row_meta.port'] = 'Přístav';
+$lang['Ispconfig.row_meta.host_name'] = 'Hostitelské jméno';
+$lang['Ispconfig.row_meta.server_name'] = 'Označení serveru';
+$lang['Ispconfig.edit_row.add_btn'] = 'Upravit server';
+$lang['Ispconfig.edit_row.remove_name_server'] = 'Odstranění adresy';
+$lang['Ispconfig.edit_row.name_server'] = 'Name server %1$s';
+$lang['Ispconfig.edit_row.name_server_host_col'] = 'Hostitelské jméno';
+$lang['Ispconfig.edit_row.name_server_col'] = 'Jmenný server';
+$lang['Ispconfig.edit_row.name_server_btn'] = 'Přidání dalšího jmenného serveru';
+$lang['Ispconfig.edit_row.notes_title'] = 'Poznámky';
+$lang['Ispconfig.edit_row.name_servers_title'] = 'Jmenné servery';
+$lang['Ispconfig.edit_row.basic_title'] = 'Základní nastavení';
+$lang['Ispconfig.edit_row.box_title'] = 'Úprava serveru ISPConfig';
+$lang['Ispconfig.add_row.add_btn'] = 'Přidat server';
+$lang['Ispconfig.add_row.remove_name_server'] = 'Odstranění adresy';
+$lang['Ispconfig.add_row.name_server'] = 'Name server %1$s';
+$lang['Ispconfig.add_row.name_server_host_col'] = 'Hostitelské jméno';
+$lang['Ispconfig.add_row.name_server_col'] = 'Jmenný server';
+$lang['Ispconfig.add_row.name_server_btn'] = 'Přidání dalšího jmenného serveru';
+$lang['Ispconfig.add_row.notes_title'] = 'Poznámky';
+$lang['Ispconfig.add_row.name_servers_title'] = 'Jmenné servery';
+$lang['Ispconfig.add_row.basic_title'] = 'Základní nastavení';
+$lang['Ispconfig.add_row.box_title'] = 'Přidání serveru ISPConfig';
+$lang['Ispconfig.order_options.roundrobin'] = 'Rovnoměrné rozdělení mezi servery';
+$lang['Ispconfig.order_options.first'] = 'První nenaplněný server';
+$lang['Ispconfig.manage.module_groups_no_results'] = 'Neexistují žádné skupiny serverů.';
+$lang['Ispconfig.manage.module_rows_no_results'] = 'Neexistují žádné servery.';
+$lang['Ispconfig.manage.module_groups.confirm_delete'] = 'Opravdu chcete tuto skupinu serverů odstranit?';
+$lang['Ispconfig.manage.module_rows.confirm_delete'] = 'Opravdu chcete tento server odstranit?';
+$lang['Ispconfig.manage.module_groups.delete'] = 'Odstranit';
+$lang['Ispconfig.manage.module_rows.delete'] = 'Odstranit';
+$lang['Ispconfig.manage.module_groups.edit'] = 'Upravit';
+$lang['Ispconfig.manage.module_rows.edit'] = 'Upravit';
+$lang['Ispconfig.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Ispconfig.manage.module_groups_heading.options'] = 'Možnosti';
+$lang['Ispconfig.manage.module_groups_heading.servers'] = 'Počet serverů';
+$lang['Ispconfig.manage.module_groups_heading.name'] = 'Název skupiny';
+$lang['Ispconfig.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['Ispconfig.manage.module_rows_heading.accounts'] = 'Účty';
+$lang['Ispconfig.manage.module_rows_heading.hostname'] = 'Hostitelské jméno';
+$lang['Ispconfig.manage.module_rows_heading.name'] = 'Označení serveru';
+$lang['Ispconfig.manage.module_groups_title'] = 'Skupiny serverů';
+$lang['Ispconfig.manage.module_rows_title'] = 'Servery';
+$lang['Ispconfig.add_module_group'] = 'Přidání skupiny serverů';
+$lang['Ispconfig.add_module_row'] = 'Přidat server';
+$lang['Ispconfig.tab_client_actions'] = 'Akce';
+$lang['Ispconfig.tab_client_stats'] = 'Statistiky';
+$lang['Ispconfig.tab_stats'] = 'Statistiky';
+$lang['Ispconfig.module_group'] = 'Skupina serverů';
+$lang['Ispconfig.module_row_plural'] = 'Servery';
+$lang['Ispconfig.module_row'] = 'Server';
+$lang['Ispconfig.description'] = 'ISPConfig 3 je open source panel pro Linux, který dokáže spravovat více serverů z jednoho ovládacího panelu.';
+$lang['Ispconfig.name'] = 'ISPConfig';
+
