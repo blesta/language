46815095b9e34ea66aceefe4bbe3f2c7762df1c8
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:41 -0800

    Updated cs_cz language files

---
+++ plugins/support_manager/language/cs_cz/client_tickets.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Client Tickets
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientTickets.!error.email.blacklist'] = 'Je nám líto, ale v tuto chvíli nemůžeme vaši letenku přijmout.';
+$lang['ClientTickets.formatDepartmentCustomFields.encrypted_field'] = 'Tato data jsou zašifrovaná a nelze je zobrazit.';
+$lang['ClientTickets.reply.dropzone_remove_file'] = 'Odebrat soubor';
+$lang['ClientTickets.reply.dropzone_drop_files_here'] = 'Zde můžete nahrát soubory nebo kliknutím vybrat soubory';
+$lang['ClientTickets.reply.staff_title'] = 'Podpůrný personál';
+$lang['ClientTickets.reply.system'] = 'Systém';
+$lang['ClientTickets.reply.log_date'] = '%1$s od %2$s %3$s';
+$lang['ClientTickets.reply.reply_date'] = 'Na otázku %1$s %2$s %3$s odpovědělo';
+$lang['ClientTickets.reply.text_add_attachment'] = 'Přidat přílohu';
+$lang['ClientTickets.reply.confirm_close'] = 'Opravdu chcete tento tip uzavřít?';
+$lang['ClientTickets.reply.field_closesubmit'] = 'Zavřít';
+$lang['ClientTickets.reply.field_replysubmit'] = 'Odpovědět';
+$lang['ClientTickets.reply.field_attachments'] = 'Přílohy';
+$lang['ClientTickets.reply.field_details'] = 'Podrobnosti na';
+$lang['ClientTickets.reply.placeholder_reply'] = 'Zadejte podrobnosti';
+$lang['ClientTickets.reply.heading_reply'] = 'Přidat odpověď';
+$lang['ClientTickets.reply.heading_date_added'] = 'Datum otevření';
+$lang['ClientTickets.reply.heading_status'] = 'Stav';
+$lang['ClientTickets.reply.heading_priority'] = 'Priorita';
+$lang['ClientTickets.reply.heading_department'] = 'Oddělení';
+$lang['ClientTickets.reply.heading_summary'] = 'Souhrn';
+$lang['ClientTickets.reply.boxtitle_reply'] = 'Vstupenka #%1$s';
+$lang['ClientTickets.add.dropzone_remove_file'] = 'Odebrat soubor';
+$lang['ClientTickets.add.dropzone_drop_files_here'] = 'Zde můžete nahrát soubory nebo kliknutím vybrat soubory';
+$lang['ClientTickets.add.placeholder_details'] = 'Zadejte podrobnosti';
+$lang['ClientTickets.add.placeholder_summary'] = 'Zadejte shrnutí';
+$lang['ClientTickets.add.field_addsubmit'] = 'Otevřený letenkový lístek';
+$lang['ClientTickets.add.field_attachments'] = 'Přílohy';
+$lang['ClientTickets.add.field_details'] = 'Podrobnosti na';
+$lang['ClientTickets.add.field_priority'] = 'Priorita';
+$lang['ClientTickets.add.field_summary'] = 'Souhrn';
+$lang['ClientTickets.add.field_email'] = 'E-mail';
+$lang['ClientTickets.add.boxtitle_add'] = 'Otevřený letenkový lístek';
+$lang['ClientTickets.departments.boxtitle_departments'] = 'Podpůrná oddělení';
+$lang['ClientTickets.index.confirm_close'] = 'Opravdu chcete tento tip uzavřít?';
+$lang['ClientTickets.index.no_results'] = 'V současné době nejsou k dispozici žádné tipy s tímto stavem.';
+$lang['ClientTickets.add.text_add_attachment'] = 'Přidat přílohu';
+$lang['ClientTickets.index.field_last_reply'] = 'Poslední odpověď';
+$lang['ClientTickets.index.field_summary'] = 'Souhrn';
+$lang['ClientTickets.index.field_priority'] = 'Priorita';
+$lang['ClientTickets.index.field_ticket_number'] = 'Číslo letenky';
+$lang['ClientTickets.index.hours'] = '%1$s hodin';
+$lang['ClientTickets.index.hour'] = '1 hodina';
+$lang['ClientTickets.index.minutes'] = '%1$s minut';
+$lang['ClientTickets.index.any'] = 'Jakýkoli';
+$lang['ClientTickets.index.option_reply'] = 'Odpovědět';
+$lang['ClientTickets.index.option_close'] = 'Zavřít';
+$lang['ClientTickets.index.heading_options'] = 'Možnosti';
+$lang['ClientTickets.index.heading_status'] = 'Stav';
+$lang['ClientTickets.index.heading_last_reply_date'] = 'Poslední odpověď';
+$lang['ClientTickets.index.heading_summary'] = 'Souhrn';
+$lang['ClientTickets.index.heading_department_name'] = 'Oddělení';
+$lang['ClientTickets.index.heading_priority'] = 'Priorita';
+$lang['ClientTickets.index.heading_code'] = 'Číslo letenky';
+$lang['ClientTickets.index.boxtitle_tickets'] = 'Vstupenky';
+$lang['ClientTickets.index.categorylink_createticket'] = 'Otevřený lístek';
+$lang['ClientTickets.index.category_closed'] = 'Uzavřeno';
+$lang['ClientTickets.index.category_open'] = 'Otevřít';
+$lang['ClientTickets.reply.page_title'] = 'Podpora > Aktualizace ticketu';
+$lang['ClientTickets.add.page_title'] = 'Podpora > Otevřít tiket';
+$lang['ClientTickets.index.page_title'] = 'Podpora > Vstupenky';
+$lang['ClientTickets.!error.captcha.invalid'] = 'Zadaná captcha byla neplatná. Zkuste to prosím znovu.';
+$lang['ClientTickets.!notice.no_departments'] = 'Nejsou k dispozici žádná podpůrná oddělení.';
+$lang['ClientTickets.!success.ticket_closed'] = 'Tiket #%1$s byl úspěšně uzavřen.';
+$lang['ClientTickets.!success.ticket_updated'] = 'Tiket #%1$s byl úspěšně aktualizován.';
+$lang['ClientTickets.!success.ticket_created'] = 'Tiket #%1$s byl úspěšně vytvořen.';
+
