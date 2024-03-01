fd5fe7d50c4c4e9e32407e6d0fe4d38c4e003323
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:04 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/affiliates.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Affiliates.signup.login'] = 'Přihlášení';
+$lang['Affiliates.signup.signup'] = 'Aktivace účtu';
+$lang['Affiliates.signup.boxtitle_affiliates'] = 'Registrace partnerů';
+$lang['Affiliates.index.text_code_order_form'] = 'Na konkrétní stránku objednávkového formuláře můžete také odkázat jako partner přidáním odkazu ?a=%1$s na konec odkazu bez parametrů dotazu nebo &a=%1$s na odkaz s parametry dotazu.';
+$lang['Affiliates.index.activate'] = 'Kliknutím zde se znovu aktivujete.';
+$lang['Affiliates.index.inactive'] = 'Tato pobočka byla označena jako neaktivní.';
+$lang['Affiliates.index.request_payout'] = 'Žádost o výplatu';
+$lang['Affiliates.index.field.commission_percentage'] = '%1$s každého dospělého doporučení';
+$lang['Affiliates.index.field.commission_fixed'] = '%1$s jednorázově za každé zralé doporučení';
+$lang['Affiliates.index.available_payout'] = 'K dispozici pro výplatu';
+$lang['Affiliates.index.maturity_days'] = 'Dny do zralosti';
+$lang['Affiliates.index.min_withdrawal_amount'] = 'Minimální výše výplaty';
+$lang['Affiliates.index.commission_type'] = 'Úroveň výplaty';
+$lang['Affiliates.index.conversion_fraction'] = '%1$s/%2$s v průměru';
+$lang['Affiliates.index.conversion_rate'] = 'Konverzní poměr';
+$lang['Affiliates.index.monthly_sales'] = '%1$s/měsíc v průměru';
+$lang['Affiliates.index.sales'] = 'Celkové tržby';
+$lang['Affiliates.index.monthly_visits'] = '%1$s/měsíc v průměru';
+$lang['Affiliates.index.visits'] = 'Kliknutí na';
+$lang['Affiliates.index.title_referral'] = 'Odkaz na odkaz';
+$lang['Affiliates.index.title_details'] = 'Podrobnosti na';
+$lang['Affiliates.index.boxtitle_affiliates'] = 'Přehled partnerů';
+$lang['Affiliates.tabs.tab_dashboard'] = 'Zpět na přístrojovou desku';
+$lang['Affiliates.tabs.tab_referrals'] = 'Doporučení';
+$lang['Affiliates.tabs.tab_payouts'] = 'Výplaty';
+$lang['Affiliates.tabs.tab_overview'] = 'Přehled partnerů';
+$lang['Affiliates.!success.affiliate_added'] = 'Váš partnerský účet byl vytvořen.';
+
