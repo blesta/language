13171447a8674113953356f8e1db1f92d8833716
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:31 -0800

    Updated cs_cz language files

---
+++ plugins/phpids/language/cs_cz/admin_company_emails.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Phpids.email_alert_desc'] = 'Email s upozorněním na narušení PHPIDS, odeslaný na adresy nakonfigurované v zásuvném modulu PHPIDS.';
+$lang['AdminCompanyEmails.templates.Phpids.email_alert_name'] = 'Upozornění na narušení PHPIDS';
+
