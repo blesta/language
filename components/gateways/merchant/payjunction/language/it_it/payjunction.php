bf6273fe6a1723743aa0f50569d1cf131aa0db6e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:10 -0800

    Updated it_it language files

---
+++ components/gateways/merchant/payjunction/language/it_it/payjunction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payjunction
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PayJunction.!error.test_mode.valid'] = 'La modalità sviluppatore deve essere impostata su "true", se fornita.';
+$lang['PayJunction.!error.password.empty'] = 'Inserire la password del proprio account.';
+$lang['PayJunction.!error.user_name.empty'] = 'Inserire il nome utente del proprio account.';
+$lang['PayJunction.!error.api_key.empty'] = 'Inserire una chiave API valida.';
+$lang['PayJunction.test_mode_note'] = 'Abilitando la Modalità sviluppatore, le transazioni verranno inviate all\'ambiente demo di PayJunction Labs. Per utilizzare la modalità sviluppatore è necessario utilizzare il nome utente e la password della demo, oltre alla chiave API Labs.';
+$lang['PayJunction.password_note'] = 'Inserite la password del vostro conto PayJunction.';
+$lang['PayJunction.user_name_note'] = 'Inserite il nome utente del vostro conto PayJunction.';
+$lang['PayJunction.api_key_note'] = 'Inserite la chiave API del vostro account PayJunction.';
+$lang['PayJunction.test_mode'] = 'Modalità sviluppatore';
+$lang['PayJunction.password'] = 'Password';
+$lang['PayJunction.user_name'] = 'Nome utente';
+$lang['PayJunction.api_key'] = 'Chiave API';
+$lang['PayJunction.description'] = 'PayJunction elabora 4 miliardi di dollari all\'anno e sta crescendo rapidamente. La sua tecnologia è ideale per le aziende con più sedi e più reparti che elaborano un volume mensile di carte di credito pari o superiore a 10.000 dollari.';
+$lang['PayJunction.name'] = 'PayJunction';
+
