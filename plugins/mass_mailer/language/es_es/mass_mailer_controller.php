d5f62a6fcaf7142f2d7fc731892de3d26fcf747a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:22 -0800

    Updated es_es language files

---
+++ plugins/mass_mailer/language/es_es/mass_mailer_controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Mass Mailer Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerController.!success.export_job_added'] = 'La exportación se ha puesto en cola correctamente. Estará disponible en la página del listado una vez se haya procesado a través de cron.';
+$lang['MassMailerController.!success.mail_job_added'] = 'La tarea de envío se ha puesto en cola correctamente. Los correos electrónicos se enviarán a través de cron.';
+
