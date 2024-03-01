9e5bb49c49750ca82b5526f305eacaf51ef0eae2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:36 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/pagseguro/language/fr_fr/pagseguro.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Pagseguro
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pagseguro.name'] = 'PagSeguro';
+$lang['Pagseguro.description'] = 'PagSeguro est un service brésilien de commerce électronique basé sur le paiement en ligne ou mobile pour les opérations commerciales. En 2015, il a été reconnu comme la "meilleure méthode de paiement" par le Congresso Afiliados Brasil.';
+$lang['Pagseguro.email_id'] = 'Courriel';
+$lang['Pagseguro.email_note'] = 'Saisissez l\'adresse électronique de votre compte PagSeguro.';
+$lang['Pagseguro.token'] = 'Jeton';
+$lang['Pagseguro.token_note'] = 'Saisissez le jeton de sécurité créé dans votre compte PagSeguro.';
+$lang['Pagseguro.buildprocess.submit'] = 'Payer avec PagSeguro';
+$lang['Pagseguro.!error.libxml_required'] = 'Les extensions libxml et simplexml sont nécessaires pour cette passerelle.';
+$lang['Pagseguro.!error.dom_required'] = 'L\'extension dom est nécessaire pour cette passerelle.';
+$lang['Pagseguro.!error.email.format'] = 'Veuillez saisir une adresse électronique valide.';
+$lang['Pagseguro.!error.token.empty'] = 'Veuillez saisir votre jeton de sécurité.';
+$lang['Pagseguro.!error.notification.type'] = 'Type de notification inconnu :';
+$lang['Pagseguro.!error.notification.invalid'] = 'Paramètres de notification non valides.';
+
