5fec379e929e2feaf0285821d24fcc6edc1c5f30
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:27 -0800

    Updated cs_cz language files

---
+++ components/modules/multicraft/language/cs_cz/multicraft.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Multicraft
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Multicraft.row_meta.log_all'] = 'Protokolování všech požadavků API';
+$lang['Multicraft.row_meta.key'] = 'Klíč API';
+$lang['Multicraft.row_meta.username'] = 'Uživatelské jméno';
+$lang['Multicraft.row_meta.panel_api_url'] = 'Adresa URL rozhraní API panelu';
+$lang['Multicraft.row_meta.panel_url'] = 'Adresa URL panelu';
+$lang['Multicraft.row_meta.server_name'] = 'Označení serveru';
+$lang['Multicraft.!tooltip.log_all'] = 'Zaškrtnutím zaškrtněte všechny požadavky API nebo zaškrtnutí zrušte, pokud chcete zaznamenávat pouze základní požadavky.';
+$lang['Multicraft.!tooltip.key'] = 'Zadejte klíč API vygenerovaný pro vašeho uživatele v aplikaci Multicraft.';
+$lang['Multicraft.!tooltip.panel_api_url'] = 'Zadejte adresu URL rozhraní Multicraft API, např. http://multicraft.domain.com/api.php.';
+$lang['Multicraft.!tooltip.panel_url'] = 'Zadejte adresu URL svého panelu Multicraft. např. http://multicraft.domain.com/.';
+$lang['Multicraft.edit_row.in_use_col'] = 'V provozu';
+$lang['Multicraft.edit_row.port_col'] = 'Přístav';
+$lang['Multicraft.edit_row.ip_col'] = 'IP adresa';
+$lang['Multicraft.edit_row.daemon_col'] = 'ID démona';
+$lang['Multicraft.edit_row.remove_ip'] = 'Odstranění adresy';
+$lang['Multicraft.edit_row.ip_btn'] = 'Přidání IP adresy';
+$lang['Multicraft.edit_row.daemons_title'] = 'Vyhrazené IP adresy';
+$lang['Multicraft.edit_row.add_btn'] = 'Upravit server';
+$lang['Multicraft.edit_row.basic_title'] = 'Základní nastavení';
+$lang['Multicraft.edit_row.box_title'] = 'Upravit server Multicraft';
+$lang['Multicraft.add_row.in_use_col'] = 'V provozu';
+$lang['Multicraft.add_row.port_col'] = 'Přístav';
+$lang['Multicraft.add_row.ip_col'] = 'IP adresa';
+$lang['Multicraft.add_row.daemon_col'] = 'ID démona';
+$lang['Multicraft.add_row.remove_ip'] = 'Odstranění adresy';
+$lang['Multicraft.add_row.ip_btn'] = 'Přidání IP adresy';
+$lang['Multicraft.add_row.daemons_title'] = 'Vyhrazené IP adresy';
+$lang['Multicraft.add_row.add_btn'] = 'Přidat server';
+$lang['Multicraft.add_row.basic_title'] = 'Základní nastavení';
+$lang['Multicraft.add_row.box_title'] = 'Přidání serveru Multicraft';
+$lang['Multicraft.tab_client_players.heading_chat_tab'] = 'Protokoly chatu';
+$lang['Multicraft.tab_client_players.heading_console_tab'] = 'Protokoly konzoly';
+$lang['Multicraft.tab_client_console.submit_clear_log'] = 'Vymazat protokol';
+$lang['Multicraft.tab_client_console.chat_entry'] = '[%1$s] %2$s: %3$s';
+$lang['Multicraft.tab_client_console.submit_send'] = 'Odeslat';
+$lang['Multicraft.tab_client_console.field_command'] = 'Příkaz';
+$lang['Multicraft.tab_client_players.heading_console'] = 'Konzola serveru';
+$lang['Multicraft.tab_client_players.no_results'] = 'Nejsou připojeni žádní hráči.';
+$lang['Multicraft.tab_client_players.action_kick'] = 'Kopající hráč';
+$lang['Multicraft.tab_client_players.heading_action'] = 'Akce';
+$lang['Multicraft.tab_client_players.heading_player_name'] = 'Jméno hráče';
+$lang['Multicraft.tab_client_players.heading_players'] = 'Připojení hráči';
+$lang['Multicraft.tab_client_actions.status_unknown'] = 'Neznámý';
+$lang['Multicraft.tab_client_actions.status_suspended'] = 'Pozastaveno';
+$lang['Multicraft.tab_client_actions.status_offline'] = 'Offline';
+$lang['Multicraft.tab_client_actions.status_online'] = 'Online, %1$s/%2$s hráčů';
+$lang['Multicraft.tab_client_actions.submit_save'] = 'Uložit';
+$lang['Multicraft.tab_client_console.field_server_name'] = 'Název serveru';
+$lang['Multicraft.tab_client_actions.heading_server_name'] = 'Nastavení názvu serveru';
+$lang['Multicraft.tab_client_actions.unknown'] = 'Neznámý';
+$lang['Multicraft.tab_client_actions.action_server_name'] = 'Nastavení názvu serveru';
+$lang['Multicraft.tab_client_actions.action_set_nighttime'] = 'Nastavení na noční čas';
+$lang['Multicraft.tab_client_actions.action_set_daytime'] = 'Nastavení na denní čas';
+$lang['Multicraft.tab_client_actions.action_start'] = 'Start';
+$lang['Multicraft.tab_client_actions.action_stop'] = 'Zastavte';
+$lang['Multicraft.tab_client_actions.action_restart'] = 'Restartování';
+$lang['Multicraft.tab_client_actions.heading_server_actions'] = 'Akce serveru';
+$lang['Multicraft.tab_client_actions.heading_server_status'] = 'Stav serveru';
+$lang['Multicraft.tab_client_actions.address_port'] = 'Přístav';
+$lang['Multicraft.tab_client_actions.address_ip'] = 'IP adresa';
+$lang['Multicraft.tab_client_actions.heading_server_address'] = 'Adresa serveru';
+$lang['Multicraft.tab_client_console'] = 'Konzole';
+$lang['Multicraft.tab_client_players'] = 'Hráči';
+$lang['Multicraft.tab_client_actions'] = 'Akce';
+$lang['Multicraft.service_info.option_login'] = 'Přihlášení';
+$lang['Multicraft.service_info.options'] = 'Možnosti';
+$lang['Multicraft.manage.module_rows_in_use_1'] = 'Ano';
+$lang['Multicraft.manage.module_rows_in_use_0'] = 'Ne';
+$lang['Multicraft.manage.module_rows_no_results'] = 'Neexistují žádné servery.';
+$lang['Multicraft.manage.module_rows.confirm_delete'] = 'Opravdu chcete tento server odstranit?';
+$lang['Multicraft.manage.module_rows.delete'] = 'Odstranit';
+$lang['Multicraft.manage.module_rows.edit'] = 'Upravit';
+$lang['Multicraft.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['Multicraft.manage.module_rows_heading.panel_api_url'] = 'Adresa URL rozhraní API panelu';
+$lang['Multicraft.manage.module_rows_heading.server_name'] = 'Označení serveru';
+$lang['Multicraft.manage.module_rows_heading.username'] = 'Uživatelské jméno';
+$lang['Multicraft.manage.module_rows_title'] = 'Servery';
+$lang['Multicraft.add_module_row'] = 'Přidat server';
+$lang['Multicraft.module_rows.name'] = 'Servery';
+$lang['Multicraft.module_row.name'] = 'Server';
+$lang['Multicraft.name'] = 'Multicraft';
+$lang['Multicraft.!error.module_row.missing'] = 'Došlo k interní chybě. Řádek modulu není k dispozici.';
+
