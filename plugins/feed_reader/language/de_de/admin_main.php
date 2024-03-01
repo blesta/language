cc53c0d55dfc703526e08b7a34985b2afa45b14b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:46 -0800

    Updated de_de language files

---
+++ plugins/feed_reader/language/de_de/admin_main.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.settings.option_remove'] = 'entfernen';
+$lang['AdminMain.settings.option_refresh'] = 'Aktualisieren';
+$lang['AdminMain.settings.heading_options'] = 'Optionen';
+$lang['AdminMain.settings.heading_lastupdated'] = 'Zuletzt aktualisiert';
+$lang['AdminMain.settings.heading_url'] = 'URL';
+$lang['AdminMain.settings.submit_cancel'] = 'Zurück zu den Feeds';
+$lang['AdminMain.settings.add_feed'] = 'Einspeisung hinzufügen';
+$lang['AdminMain.settings.no_feeds'] = 'Es gibt keine Einspeisungen.';
+$lang['AdminMain.add.submit_cancel'] = 'Abbrechen';
+$lang['AdminMain.add.submit_add'] = 'Feed hinzufügen';
+$lang['AdminMain.add.field_url'] = 'URL';
+$lang['AdminMain.add.boxtitle_feed'] = 'Einspeisung hinzufügen';
+$lang['AdminMain.index.last_updated'] = 'Zuletzt aktualisiert: %1$s';
+$lang['AdminMain.index.no_results'] = 'Es gibt keine Futtermittelergebnisse.';
+$lang['AdminMain.index.boxtitle_feed'] = 'Feed-Reader';
+$lang['AdminMain.!success.feed_removed'] = 'Das Futter wurde erfolgreich entfernt.';
+$lang['AdminMain.!success.feed_refreshed'] = 'Der Feed wurde erfolgreich aufgefrischt.';
+$lang['AdminMain.!success.feed_added'] = 'Der Feed wurde erfolgreich hinzugefügt.';
+
