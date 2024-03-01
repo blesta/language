3ef1bdff984049f57aca58fe3c57ee8e4870a79e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:11 -0800

    Updated de_de language files

---
+++ core/Util/Filters/language/de_de/package_filters.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Package Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.package_filters.field_options'] = 'Optionen';
+$lang['Util.filters.package_filters.field_assigned_services'] = 'Zugewiesene Dienste';
+$lang['Util.filters.package_filters.field_name'] = 'Name des Pakets';
+$lang['Util.filters.package_filters.field_package_group_id'] = 'Paket Gruppe';
+$lang['Util.filters.package_filters.field_module_id'] = 'Modul';
+$lang['Util.filters.package_filters.options.hidden'] = 'Versteckte Pakete anzeigen';
+$lang['Util.filters.package_filters.services.none'] = 'Keine Dienstleistungen';
+$lang['Util.filters.package_filters.services.canceled'] = 'Nur stornierte Dienste';
+$lang['Util.filters.package_filters.services.any'] = 'Alle Dienstleistungen';
+$lang['Util.filters.package_filters.services.na'] = 'K.A.';
+$lang['Util.filters.package_filters.any'] = 'Jede';
+
