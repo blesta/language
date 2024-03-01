9b36c989280b843967c4c9f7dd4e3a0b43ec6e3a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:06 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/offline/language/cs_cz/offline.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Offline
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Offline.!error.instructions.valid'] = 'Musíte zadat několik pokynů.';
+$lang['Offline.instructions'] = 'Pokyny';
+$lang['Offline.description'] = 'Umožňuje zobrazit speciální pokyny pro přijímání plateb offline.';
+$lang['Offline.name'] = 'Offline platba';
+
