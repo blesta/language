f15849b7d53035cf002d94179b7bad57a532488e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:09 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/package_option_conditions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Package Option Conditions
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptionConditions.getactions.show'] = 'Zobrazit';
+$lang['PackageOptionConditions.getactions.hide'] = 'Skrýt';
+$lang['PackageOptionConditions.getoperators.notin'] = 'Není v';
+$lang['PackageOptionConditions.getoperators.in'] = 'Na adrese';
+$lang['PackageOptionConditions.!error.value_id.exists'] = 'ID dané hodnoty neexistuje';
+$lang['PackageOptionConditions.!error.value.valid'] = 'Zadaná hodnota je neplatná';
+$lang['PackageOptionConditions.!error.operator.valid'] = 'Zadaný operátor je neplatný';
+$lang['PackageOptionConditions.!error.trigger_option_id.exists'] = 'Zadané ID spouštěcí možnosti neexistuje';
+$lang['PackageOptionConditions.!error.condition_set_id.exists'] = 'ID dané sady podmínek volby neexistuje';
+$lang['PackageOptionConditions.!error.id.exists'] = 'ID dané podmínky volby neexistuje';
+
