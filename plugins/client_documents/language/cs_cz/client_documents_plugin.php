50f6dba594cfacd726e2fce9338fcb48cce095c4
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:28 -0800

    Updated cs_cz language files

---
+++ plugins/client_documents/language/cs_cz/client_documents_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Client Documents Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDocumentsPlugin.action_staff_client.index'] = 'Dokumenty';
+$lang['ClientDocumentsPlugin.description'] = 'Nahrávání souborů ke stažení konkrétními klienty.';
+$lang['ClientDocumentsPlugin.name'] = 'Dokumenty klienta';
+$lang['ClientDocumentsPlugin.nav_primary_client.main'] = 'Dokumenty';
+
