675ce82ff6b831a33a6fa21f2cb3aa9675baeaef
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:40 -0800

    Updated es_es language files

---
+++ language/es_es/report_manager.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Report Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReportManager.!error.type.valid'] = 'Seleccione un tipo de informe válido.';
+$lang['ReportManager.!error.format.valid'] = 'Seleccione un formato de informe válido.';
+$lang['ReportManager.!error.temp_dir.writable'] = 'No se puede escribir en el directorio temporal o no se ha podido escribir el informe en él.';
+$lang['ReportManager.!error.name.valid'] = 'El informe debe tener un nombre.';
+$lang['ReportManager.!error.query.empty'] = 'El informe debe tener una consulta.';
+$lang['ReportManager.!error.query.valid'] = 'Sólo se permite una consulta, y debe ser una consulta SELECT.';
+$lang['ReportManager.!error.date_created.format'] = 'Fecha de creación es un formato no válido.';
+$lang['ReportManager.getformats.csv'] = 'CSV';
+$lang['ReportManager.getformats.json'] = 'JSON';
+$lang['ReportManager.reportfieldtypes.text'] = 'Texto';
+$lang['ReportManager.reportfieldtypes.select'] = 'Seleccione';
+$lang['ReportManager.reportfieldtypes.date'] = 'Fecha';
+$lang['ReportManager.reportrequiredtypes.no'] = 'No';
+$lang['ReportManager.reportrequiredtypes.yes'] = 'Sí';
+$lang['ReportManager.reportrequiredtypes.custom'] = 'Regex personalizada';
+
