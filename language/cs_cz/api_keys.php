4e4f3f4283143ae1f64048e62f795130f3c93696
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:40 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/api_keys.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Api Keys
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ApiKeys.!error.user.unique'] = 'Uživatel API pro danou společnost již existuje.';
+$lang['ApiKeys.!error.user.valid'] = 'Uživatel nebo klíč API se zdá být neplatný.';
+$lang['ApiKeys.!error.user.format'] = 'Zadejte prosím uživatele API o délce 3 až 64 znaků.';
+$lang['ApiKeys.!error.company_id.exists'] = 'Uvedená společnost neexistuje.';
+$lang['ApiKeys.!error.key.generate'] = 'Klíč se nepodařilo vygenerovat.';
+
