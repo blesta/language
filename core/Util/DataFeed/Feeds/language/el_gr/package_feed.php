1da81476e18baef8e1c23837d4843e7a9c73a153
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:30 -0800

    Updated el_gr language files

---
+++ core/Util/DataFeed/Feeds/language/el_gr/package_feed.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Package Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageFeed.name'] = 'Πακέτα';
+$lang['PackageFeed.description'] = 'Επιστρέφει το όνομα, την περιγραφή ή την τιμή ενός πακέτου.';
+$lang['PackageFeed.getOptionFields.title_row_example_code'] = 'Παράδειγμα κώδικα';
+$lang['PackageFeed.getOptionFields.example_code_name'] = 'Εμφάνιση του ονόματος ενός πακέτου:';
+$lang['PackageFeed.getOptionFields.example_code_description'] = 'Εμφάνιση της περιγραφής του πακέτου:';
+$lang['PackageFeed.getOptionFields.example_code_pricing'] = 'Εμφάνιση της τιμής, της ανανέωσης, του τέλους εγκατάστασης, του τέλους ακύρωσης ή του νομίσματος της τιμολόγησης ενός πακέτου:';
+$lang['PackageFeed.!error.invalid_endpoint'] = 'Το ζητούμενο τελικό σημείο δεν είναι έγκυρο ή δεν υπάρχει.';
+
