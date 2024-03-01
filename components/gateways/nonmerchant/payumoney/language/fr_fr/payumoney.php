ca323e3d9416dd1d9159db3e6b6738d4954773af
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:39 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/payumoney/language/fr_fr/payumoney.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Payumoney
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payumoney.name'] = 'Payumoney';
+$lang['Payumoney.description'] = 'En Inde, PayU sert plus de 4 50 000 commerçants avec plus de 100 méthodes de paiement et est le partenaire de paiement privilégié de près de 60 % des commerçants en ligne.';
+$lang['Payumoney.merchant_key'] = 'Clé du commerçant';
+$lang['Payumoney.merchant_salt'] = 'Sel marchand';
+$lang['Payumoney.auth_header'] = 'En-tête d\'autorisation';
+$lang['Payumoney.auth_header_note'] = 'L\'en-tête d\'autorisation se trouve sur le tableau de bord de Payumoney sous Gérer le compte -> Mon compte -> Clé marchande.';
+$lang['Payumoney.merchant_key_note'] = 'La clé de commerçant peut être trouvée sur le tableau de bord de Payumoney sous Gérer le compte -> Mon compte -> Clé de commerçant - sel.';
+$lang['Payumoney.merchant_salt_note'] = 'Le sel marchand peut être trouvé sur le tableau de bord de Payumoney sous Gérer le compte -> Mon compte -> Sel de clé marchand.';
+$lang['Payumoney.test_mode'] = 'Mode bac à sable';
+$lang['Payumoney.test_mode_note'] = 'En activant cette option, vous utiliserez le bac à sable de Payumoney. N\'activez cette option que si vous testez avec un compte Payumoney sandbox.';
+$lang['Payumoney.buildprocess.submit'] = 'Payer via Payumoney';
+$lang['Payumoney.!error.merchant_key.valid'] = 'Veuillez saisir une clé de commerçant valide.';
+$lang['Payumoney.!error.merchant_salt.valid'] = 'Veuillez saisir un sel valide.';
+$lang['Payumoney.!error.auth_header.valid'] = 'Veuillez saisir un en-tête d\'autorisation valide.';
+$lang['Payumoney.!error.key.valid'] = 'Clé du commerçant non valide.';
+$lang['Payumoney.!error.invalid_token.response'] = 'Payumoney a répondu avec un jeton invalide.';
+$lang['Payumoney.!error.hash.valid'] = 'Hachage non valide.';
+
