cfe8dbac877afc252069b338b73583bf8979a5c1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:26 -0800

    Updated de_de language files

---
+++ plugins/order/views/templates/standard/language/de_de/domain.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Domain
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Domain.lookup.term_onetime'] = '%1$s @ %2$s';
+$lang['Domain.lookup.term_recurring'] = '%1$s %2$s @ %3$s (verlängert sich @ %4$s)';
+$lang['Domain.lookup.term'] = '%1$s %2$s @ %3$s';
+$lang['Domain.lookup.order_btn'] = 'Ausgewählte Bestellung';
+$lang['Domain.lookup.term_years'] = '%1$s Jahre';
+$lang['Domain.lookup.term_year'] = '%1$s Jahr';
+$lang['Domain.lookup.term_months'] = '%1$s Monate';
+$lang['Domain.lookup.term_month'] = '%1$s Monat';
+$lang['Domain.lookup.term_weeks'] = '%1$s-Wochen';
+$lang['Domain.lookup.term_week'] = '%1$s Woche';
+$lang['Domain.lookup.term_days'] = '%1$s Tage';
+$lang['Domain.lookup.term_day'] = '%1$s Tag';
+$lang['Domain.lookup.heading_renew_price'] = 'Preis erneuern';
+$lang['Domain.lookup.heading_transfer_price'] = 'Verrechnungspreis';
+$lang['Domain.lookup.heading_price'] = 'Preis';
+$lang['Domain.lookup.heading_tld'] = 'TLD';
+$lang['Domain.lookup.domain_eligible_free'] = 'Berechtigt für %1$s Free';
+$lang['Domain.lookup.domain_unavailable'] = 'Nicht verfügbar';
+$lang['Domain.lookup.domain_available'] = 'Verfügbar';
+$lang['Domain.lookup.domains_term'] = 'Begriff';
+$lang['Domain.lookup.domains_status'] = 'Status';
+$lang['Domain.lookup.domains_domain_name'] = 'Domänenname';
+$lang['Domain.lookup.domains_header_free'] = 'Die Domain kann kostenlos sein, wenn sie mit einem Paket aus einer der folgenden Paketgruppen für einen Mindestbetrag von %1$s: %2$s gebündelt wird.';
+$lang['Domain.lookup.domains_header_unavailable'] = 'Diese Domain ist leider nicht verfügbar.';
+$lang['Domain.lookup.domains_header_available'] = 'Herzlichen Glückwunsch, die Domain ist verfügbar.';
+$lang['Domain.lookup.other_btn'] = 'Andere Artikel bestellen';
+$lang['Domain.lookup.lookup_btn'] = 'Verfügbarkeit prüfen';
+$lang['Domain.lookup.transfer_btn'] = 'Übertragung';
+$lang['Domain.lookup.loading'] = 'Laden...';
+$lang['Domain.lookup.hide_all'] = 'Alle ausblenden';
+$lang['Domain.lookup.show_all'] = 'Alle anzeigen';
+$lang['Domain.lookup.domain_placeholder'] = 'IhreDomain.de';
+$lang['Domain.lookup.title'] = 'Domainverfügbarkeit prüfen';
+
