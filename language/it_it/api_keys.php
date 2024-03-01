b07a9acb7c8d6a884cc9d17069547174c0a38331
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:45 -0800

    Updated it_it language files

---
+++ language/it_it/api_keys.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Api Keys
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ApiKeys.!error.user.unique'] = 'L\'utente API esiste già per l\'azienda indicata.';
+$lang['ApiKeys.!error.user.valid'] = 'L\'utente o la chiave API sembrano non essere validi.';
+$lang['ApiKeys.!error.user.format'] = 'Inserire un utente API di lunghezza compresa tra 3 e 64 caratteri.';
+$lang['ApiKeys.!error.company_id.exists'] = 'L\'azienda indicata non esiste.';
+$lang['ApiKeys.!error.key.generate'] = 'La chiave non è stata generata.';
+
