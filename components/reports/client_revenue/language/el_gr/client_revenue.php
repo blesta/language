78eb702a024b5ab862b353e5d3da829419af2b99
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:08 -0800

    Updated el_gr language files

---
+++ components/reports/client_revenue/language/el_gr/client_revenue.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Client Revenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientRevenue.name'] = 'Έσοδα πελατών';
+$lang['ClientRevenue.description'] = 'Υπολογίζει τα έσοδα από ένα νόμισμα για κάθε πελάτη αθροίζοντας το ποσό που καταβλήθηκε σε όλα τα τιμολόγια που σχετίζονται με τον πελάτη.';
+$lang['ClientRevenue.option.any'] = 'Οποιοδήποτε';
+$lang['ClientRevenue.options.currency'] = 'Νόμισμα';
+$lang['ClientRevenue.options.field_start_date'] = 'Ημερομηνία έναρξης';
+$lang['ClientRevenue.options.field_end_date'] = 'Ημερομηνία λήξης';
+$lang['ClientRevenue.heading.client_id_code'] = 'Αναγνωριστικό πελάτη';
+$lang['ClientRevenue.heading.client_name'] = 'Όνομα πελάτη';
+$lang['ClientRevenue.heading.company'] = 'Εταιρεία';
+$lang['ClientRevenue.heading.total'] = 'Σύνολο';
+
