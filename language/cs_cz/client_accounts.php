c588109a78937f241a8d574368cfa9356a5158a0
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:43 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/client_accounts.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Client Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientAccounts.navigation.nav_return'] = 'Zpět na přístrojovou desku';
+$lang['ClientAccounts.navigation.nav_paymentaccounts_add'] = 'Přidat platební účet';
+$lang['ClientAccounts.navigation.nav_paymentaccounts'] = 'Platební účty';
+$lang['ClientAccounts.navigation.nav_dashboard'] = 'Přístrojová deska';
+$lang['ClientAccounts.ach_info.field_savedetails'] = 'Uložit účet';
+$lang['ClientAccounts.ach_info.field_routingnum'] = 'Směrovací číslo';
+$lang['ClientAccounts.ach_info.field_accountnum'] = 'Číslo účtu';
+$lang['ClientAccounts.ach_info.field_type'] = 'Typ';
+$lang['ClientAccounts.ach_info.heading_ach'] = 'Informace o bankovním účtu';
+$lang['ClientAccounts.cc_info.tooltip_code'] = 'Třímístný nebo čtyřmístný bezpečnostní kód, který se obvykle nachází na zadní straně karty.';
+$lang['ClientAccounts.cc_info.field_savedetails'] = 'Uložit účet';
+$lang['ClientAccounts.cc_info.field_expiration'] = 'Datum vypršení platnosti';
+$lang['ClientAccounts.cc_info.field_security'] = 'Bezpečnostní kód';
+$lang['ClientAccounts.cc_info.field_number'] = 'Číslo';
+$lang['ClientAccounts.cc_info.heading_cc'] = 'Informace o kreditní kartě';
+$lang['ClientAccounts.contact_info.field_email'] = 'E-mail';
+$lang['ClientAccounts.contact_info.field_zip'] = 'Poštovní směrovací číslo';
+$lang['ClientAccounts.contact_info.field_state'] = 'Stát';
+$lang['ClientAccounts.contact_info.field_country'] = 'Země';
+$lang['ClientAccounts.contact_info.field_city'] = 'Město';
+$lang['ClientAccounts.contact_info.field_address2'] = 'Adresa 2';
+$lang['ClientAccounts.contact_info.field_address1'] = 'Adresa 1';
+$lang['ClientAccounts.contact_info.field_title'] = 'Název';
+$lang['ClientAccounts.contact_info.field_company'] = 'Společnost';
+$lang['ClientAccounts.contact_info.field_last_name'] = 'Příjmení';
+$lang['ClientAccounts.contact_info.field_first_name'] = 'Jméno';
+$lang['ClientAccounts.contact_info.field_contact_id'] = 'Zkopírovat kontaktní informace z';
+$lang['ClientAccounts.contact_info.text_select_contact'] = 'Pro předvyplnění tohoto formuláře můžete vybrat existující kontakt.';
+$lang['ClientAccounts.contact_info.heading_contact'] = 'Kontaktní informace';
+$lang['ClientAccounts.setcontactview.text_none'] = 'Žádné';
+$lang['ClientAccounts.editcc.field_accountsubmit'] = 'Upravit účet';
+$lang['ClientAccounts.editcc.boxtitle_editcc'] = 'Úprava účtu kreditní karty';
+$lang['ClientAccounts.editcc.page_title'] = 'Klient #%1$s Upravit účet kreditní karty';
+$lang['ClientAccounts.verifyach.field_accountsubmit'] = 'Ověření účtu';
+$lang['ClientAccounts.verifyach.field_seconddeposit'] = 'Druhý vklad';
+$lang['ClientAccounts.verifyach.field_firstdeposit'] = 'První vklad';
+$lang['ClientAccounts.verifyach.boxtitle_verifyach'] = 'Ověření účtu ACH';
+$lang['ClientAccounts.verifyach.page_title'] = 'Klient #%1$s Ověření účtu ACH';
+$lang['ClientAccounts.editach.field_accountsubmit'] = 'Upravit účet';
+$lang['ClientAccounts.editach.boxtitle_editach'] = 'Upravit účet ACH';
+$lang['ClientAccounts.editach.page_title'] = 'Klient #%1$s Upravit účet ACH';
+$lang['ClientAccounts.add.field_accountsubmit'] = 'Vytvořit účet';
+$lang['ClientAccounts.add.field_nextsubmit'] = 'Pokračovat';
+$lang['ClientAccounts.add.boxtitle_create'] = 'Přidat platební účet';
+$lang['ClientAccounts.add.field_ach_type'] = 'ACH';
+$lang['ClientAccounts.add.field_cc_type'] = 'Kreditní karta';
+$lang['ClientAccounts.add.heading_account_type'] = 'Typ účtu';
+$lang['ClientAccounts.add.page_title'] = 'Klient #%1$s Přidat platební účet';
+$lang['ClientAccounts.index.no_results'] = 'Nemáte žádné platební účty.';
+$lang['ClientAccounts.index.field_removeautodebit'] = 'Odstranění automatického inkasa';
+$lang['ClientAccounts.index.field_accountsubmit'] = 'Nastavit jako výchozí platební účet';
+$lang['ClientAccounts.index.confirm_delete'] = 'Opravdu chcete tento platební účet odstranit?';
+$lang['ClientAccounts.index.type_ach'] = '%1$s - %2$s';
+$lang['ClientAccounts.index.type_cc'] = '%1$s - %2$s';
+$lang['ClientAccounts.index.option_delete'] = 'Odstranit';
+$lang['ClientAccounts.index.option_verify'] = 'Ověřit';
+$lang['ClientAccounts.index.option_edit'] = 'Upravit';
+$lang['ClientAccounts.index.heading_options'] = 'Možnosti';
+$lang['ClientAccounts.index.heading_last4'] = 'Poslední 4';
+$lang['ClientAccounts.index.heading_type'] = 'Typ';
+$lang['ClientAccounts.index.heading_name'] = 'Název';
+$lang['ClientAccounts.index.boxtitle_accounts'] = 'Platební účty';
+$lang['ClientAccounts.index.create_account'] = 'Přidat platební účet';
+$lang['ClientAccounts.index.page_title'] = 'Účty klienta #%1$s';
+$lang['ClientAccounts.!info.ach_verification_edit'] = 'Po úpravě tohoto účtu jej budete muset ověřit, než jej budete moci použít k provedení platby.';
+$lang['ClientAccounts.!info.ach_verification'] = 'Tento účet je třeba ověřit, abyste jej mohli použít k provedení platby.';
+$lang['ClientAccounts.!info.no_accounts'] = 'V současné době nemáte žádné platební účty. Přidejte svůj první platební účet níže.';
+$lang['ClientAccounts.!info.account_info'] = 'Platební účty umožňují uchovávat informace o platbách a mohou být použity k úhradě faktur. Platební účet, který je zvolen jako výchozí účet, se bude používat pro automatické opakované platby.';
+$lang['ClientAccounts.!notice.reenable_autodebit'] = 'Automatické platby byly pro tento účet výslovně zakázány, pro jejich opětovnou aktivaci se prosím obraťte na zaměstnance. Platební účet můžete zvolit jako výchozí, ale nebude automaticky účtován, dokud jej zaměstnanci znovu neaktivují.';
+$lang['ClientAccounts.!notice.cc_disabled'] = 'Platební účty kreditních karet jsou v současné době vypnuté a nelze je aktualizovat.';
+$lang['ClientAccounts.!notice.ach_disabled'] = 'Platební účty ACH jsou v současné době zakázány a nelze je aktualizovat.';
+$lang['ClientAccounts.!notice.disabled'] = 'Možnost přidávat platební účty je v současné době zakázána.';
+$lang['ClientAccounts.!error.payment_account_type_invalid'] = 'Neplatný typ platebního účtu.';
+$lang['ClientAccounts.!error.account_invalid'] = 'Zadaný typ platebního účtu je zakázaný nebo neplatný.';
+$lang['ClientAccounts.!success.achaccount_verified'] = 'Platební účet byl úspěšně ověřen.';
+$lang['ClientAccounts.!success.achaccount_updated'] = 'Platební účet byl úspěšně aktualizován.';
+$lang['ClientAccounts.!success.achaccount_deleted'] = 'Platební účet byl úspěšně odstraněn.';
+$lang['ClientAccounts.!success.ccaccount_updated'] = 'Platební účet byl úspěšně aktualizován.';
+$lang['ClientAccounts.!success.ccaccount_deleted'] = 'Platební účet byl úspěšně odstraněn.';
+$lang['ClientAccounts.!success.account_created'] = 'Platební účet byl úspěšně vytvořen.';
+$lang['ClientAccounts.!success.defaultaccount_deleted'] = 'Výchozí platební účet byl odstraněn.';
+$lang['ClientAccounts.!success.defaultaccount_updated'] = 'Výchozí platební účet byl aktualizován.';
+
