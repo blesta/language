b5cdd1113acb1c585e43f89346fb2073cc79e12d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:51 -0800

    Updated el_gr language files

---
+++ plugins/domains/language/el_gr/domains_feed.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Domains Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsFeed.name'] = 'Τιμολόγηση τομέα';
+$lang['DomainsFeed.description'] = 'Επιστρέφει έναν πίνακα HTML που περιέχει τις τιμές για όλους τους TLD.';
+$lang['DomainsFeed.getOptionFields.title_row_example_code'] = 'Παράδειγμα κώδικα';
+$lang['DomainsFeed.getOptionFields.example_code_table'] = 'Εμφάνιση ενός πίνακα που περιέχει την τιμολόγηση για όλους τους TLDs σε ένα συγκεκριμένο νόμισμα:';
+$lang['DomainsFeed.getOptionFields.header_name'] = 'Όνομα';
+$lang['DomainsFeed.getOptionFields.header_description'] = 'Περιγραφή';
+$lang['DomainsFeed.getOptionFields.params'] = 'Παράμετροι';
+$lang['DomainsFeed.getOptionFields.param_currency'] = 'νόμισμα';
+$lang['DomainsFeed.getOptionFields.param_currency_description'] = 'Ο κωδικός νομίσματος 3 χαρακτήρων για τον οποίο θα αντληθεί η τιμολόγηση.';
+$lang['DomainsFeed.getOptionFields.param_style'] = 'στυλ';
+$lang['DomainsFeed.getOptionFields.param_style_description'] = 'Το στυλ του πίνακα που θα αντληθεί: html ή bootstrap.';
+$lang['DomainsFeed.getOptionFields.param_term'] = 'όρος';
+$lang['DomainsFeed.getOptionFields.param_term_description'] = 'Ένας κατάλογος με διαχωρισμό με κόμμα των όρων του έτους που πρέπει να συμπεριληφθούν στον πίνακα τιμών';
+$lang['DomainsFeed.table.heading_tlds'] = 'TLDs';
+$lang['DomainsFeed.table.heading_year'] = '%1$s Έτος';
+$lang['DomainsFeed.table.heading_years'] = '%1$s Έτη';
+$lang['DomainsFeed.table.register'] = 'Εγγραφή';
+$lang['DomainsFeed.table.transfer'] = 'Μεταφορά';
+$lang['DomainsFeed.table.renew'] = 'Ανανέωση';
+$lang['DomainsFeed.table.not_available'] = 'Δεν είναι διαθέσιμο';
+$lang['DomainsFeed.!error.invalid_endpoint'] = 'Το ζητούμενο τελικό σημείο δεν είναι έγκυρο ή δεν υπάρχει.';
+$lang['DomainsFeed.!error.invalid_style'] = 'Το ζητούμενο στυλ δεν είναι έγκυρο.';
+
