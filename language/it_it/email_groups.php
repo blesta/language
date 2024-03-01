a23a28edbecd5665dcf522649d1a47981ffea756
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:01 -0800

    Updated it_it language files

---
+++ language/it_it/email_groups.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Email Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['EmailGroups.!error.group_id.exists'] = 'ID gruppo e-mail non valido.';
+$lang['EmailGroups.!error.notice_type.valid'] = 'Il tipo di avviso deve essere impostato su bcc o to.';
+$lang['EmailGroups.!error.type.format'] = 'Tipo non valido.';
+$lang['EmailGroups.!error.action.unique'] = 'Esiste già un\'azione con questo nome.';
+$lang['EmailGroups.!error.action.length'] = 'La lunghezza dell\'azione non può superare i 64 caratteri.';
+$lang['EmailGroups.!error.action.empty'] = 'Inserire un\'azione.';
+
