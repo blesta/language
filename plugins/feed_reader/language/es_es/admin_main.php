80326179531a503c87cf995bfba1d408268287bf
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:12 -0800

    Updated es_es language files

---
+++ plugins/feed_reader/language/es_es/admin_main.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.feed_added'] = 'La alimentación se ha añadido correctamente.';
+$lang['AdminMain.!success.feed_refreshed'] = 'El feed se ha actualizado correctamente.';
+$lang['AdminMain.!success.feed_removed'] = 'La alimentación se retiró con éxito.';
+$lang['AdminMain.index.boxtitle_feed'] = 'Lector de feeds';
+$lang['AdminMain.index.no_results'] = 'No hay resultados de alimentación.';
+$lang['AdminMain.index.last_updated'] = 'Última actualización: %1$s';
+$lang['AdminMain.add.boxtitle_feed'] = 'Añadir un feed';
+$lang['AdminMain.add.field_url'] = 'URL';
+$lang['AdminMain.add.submit_add'] = 'Añadir Feed';
+$lang['AdminMain.add.submit_cancel'] = 'Cancelar';
+$lang['AdminMain.settings.no_feeds'] = 'No hay alimentación.';
+$lang['AdminMain.settings.add_feed'] = 'Añadir un feed';
+$lang['AdminMain.settings.submit_cancel'] = 'Volver a la alimentación';
+$lang['AdminMain.settings.heading_url'] = 'URL';
+$lang['AdminMain.settings.heading_lastupdated'] = 'Última actualización';
+$lang['AdminMain.settings.heading_options'] = 'Opciones';
+$lang['AdminMain.settings.option_refresh'] = 'Actualizar';
+$lang['AdminMain.settings.option_remove'] = 'Eliminar';
+
