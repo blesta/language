7878e7c968314ef678a513334e6939aa9696d15a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:07 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_maintenance.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Client Maintenance
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMaintenance.index.maintenance_heading'] = 'Λειτουργία συντήρησης';
+$lang['ClientMaintenance.index.page_title'] = 'Συντήρηση';
+
