b06dc59d6ece54bd51ada92648bbf6fcc089c830
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:46 -0800

    Updated cs_cz language files

---
+++ plugins/feed_reader/language/cs_cz/feed_reader_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Feed Reader Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FeedReaderPlugin.!error.dom_required'] = 'Pro tento zásuvný modul je vyžadováno rozšíření dom.';
+$lang['FeedReaderPlugin.!error.libxml_required'] = 'Tento zásuvný modul vyžaduje rozšíření libxml.';
+$lang['FeedReaderPlugin.description'] = 'Zobrazení libovolného kanálu RSS/Atom přímo na ovládacím panelu.';
+$lang['FeedReaderPlugin.name'] = 'Čtečka krmiv';
+
