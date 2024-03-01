fb3c8280bf428780b8c9f18677889a90964eabea
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:42 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_company_clientoptions.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Admin Company Clientoptions
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyClientOptions.requiredfields.text_submit'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminCompanyClientOptions.requiredfields.field_fax'] = 'Φαξ';
+$lang['AdminCompanyClientOptions.requiredfields.field_phone'] = 'Τηλέφωνο';
+$lang['AdminCompanyClientOptions.requiredfields.field_email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['AdminCompanyClientOptions.requiredfields.field_zip'] = 'Ταχυδρομικός/Ταχυδρομικός κώδικας';
+$lang['AdminCompanyClientOptions.requiredfields.field_state'] = 'Πολιτεία/Περιφέρεια';
+$lang['AdminCompanyClientOptions.requiredfields.field_country'] = 'Χώρα';
+$lang['AdminCompanyClientOptions.requiredfields.field_city'] = 'Πόλη';
+$lang['AdminCompanyClientOptions.requiredfields.field_address2'] = 'Διεύθυνση 2';
+$lang['AdminCompanyClientOptions.requiredfields.field_address1'] = 'Διεύθυνση 1';
+$lang['AdminCompanyClientOptions.requiredfields.field_title'] = 'Τίτλος';
+$lang['AdminCompanyClientOptions.requiredfields.field_company'] = 'Εταιρεία/Οργανισμός.';
+$lang['AdminCompanyClientOptions.requiredfields.field_last_name'] = 'Επώνυμο';
+$lang['AdminCompanyClientOptions.requiredfields.field_first_name'] = 'Όνομα';
+$lang['AdminCompanyClientOptions.requiredfields.heading_read_only'] = 'Μόνο για ανάγνωση';
+$lang['AdminCompanyClientOptions.requiredfields.heading_show'] = 'Εμφάνιση';
+$lang['AdminCompanyClientOptions.requiredfields.heading_required'] = 'Απαιτούμενο';
+$lang['AdminCompanyClientOptions.requiredfields.heading_field'] = 'Πεδίο';
+$lang['AdminCompanyClientOptions.requiredfields.description'] = 'Ελέγξτε τα πεδία που πρέπει να απαιτούνται κατά τη δημιουργία ή την ενημέρωση ενός πελάτη ή μιας επαφής.';
+$lang['AdminCompanyClientOptions.requiredfields.boxtitle'] = 'Απαιτούμενα πεδία πελάτη';
+$lang['AdminCompanyClientoptions.requiredfields.page_title'] = 'Ρυθμίσεις > Εταιρεία > Επιλογές πελάτη > Προσαρμοσμένα πεδία πελάτη > Υποχρεωτικά πεδία πελάτη';
+$lang['AdminCompanyClientOptions.getRequired.regex'] = 'Προσαρμοσμένη Regex';
+$lang['AdminCompanyClientOptions.getRequired.yes'] = 'Ναι';
+$lang['AdminCompanyClientOptions.getRequired.no'] = 'Όχι';
+$lang['AdminCompanyClientOptions.editcustomfield.text_remove'] = 'Αφαιρέστε το';
+$lang['AdminCompanyClientOptions.editcustomfield.heading_select_default'] = 'Προεπιλογή';
+$lang['AdminCompanyClientOptions.editcustomfield.heading_select_option'] = 'Όνομα επιλογής';
+$lang['AdminCompanyClientOptions.editcustomfield.heading_select_value'] = 'Αξία';
+$lang['AdminCompanyClientOptions.editcustomfield.categorylink_select'] = 'Προσθήκη πρόσθετης επιλογής';
+$lang['AdminCompanyClientOptions.editcustomfield.field.default_text'] = 'Προεπιλεγμένη τιμή κειμένου';
+$lang['AdminCompanyClientOptions.editcustomfield.field.default_checkbox'] = 'Προεπιλεγμένη τιμή Ελέγχεται';
+$lang['AdminCompanyClientOptions.editcustomfield.field.checkbox_value'] = 'Αξία';
+$lang['AdminCompanyClientOptions.editcustomfield.field.editsubmit'] = 'Επεξεργασία προσαρμοσμένου πεδίου';
+$lang['AdminCompanyClientOptions.editcustomfield.field.encrypted'] = 'Κρυπτογράφηση τιμών';
+$lang['AdminCompanyClientOptions.editcustomfield.field.regex'] = 'Προσαρμοσμένη Regex';
+$lang['AdminCompanyClientOptions.editcustomfield.field.required'] = 'Απαιτούμενο';
+$lang['AdminCompanyClientOptions.editcustomfield.field.read_only'] = 'Διαβάστε μόνο για πελάτες';
+$lang['AdminCompanyClientOptions.editcustomfield.field.show_client'] = 'Ορατό στους πελάτες';
+$lang['AdminCompanyClientOptions.editcustomfield.field.type'] = 'Τύπος';
+$lang['AdminCompanyClientOptions.editcustomfield.field.link'] = 'Σύνδεσμος';
+$lang['AdminCompanyClientOptions.editcustomfield.field.is_lang'] = 'Το όνομα είναι ένας γλωσσικός ορισμός';
+$lang['AdminCompanyClientOptions.editcustomfield.field.name'] = 'Όνομα';
+$lang['AdminCompanyClientOptions.editcustomfield.boxtitle_edit'] = 'Επεξεργασία προσαρμοσμένου πεδίου';
+$lang['AdminCompanyClientoptions.editcustomfield.page_title'] = 'Ρυθμίσεις > Εταιρεία > Επιλογές πελάτη > Προσαρμοσμένα πεδία πελάτη > Επεξεργασία προσαρμοσμένου πεδίου';
+$lang['AdminCompanyClientOptions.addcustomfield.text_remove'] = 'Αφαιρέστε το';
+$lang['AdminCompanyClientOptions.addcustomfield.heading_select_default'] = 'Προεπιλογή';
+$lang['AdminCompanyClientOptions.addcustomfield.heading_select_option'] = 'Όνομα επιλογής';
+$lang['AdminCompanyClientOptions.addcustomfield.heading_select_value'] = 'Αξία';
+$lang['AdminCompanyClientOptions.addcustomfield.categorylink_select'] = 'Προσθήκη πρόσθετης επιλογής';
+$lang['AdminCompanyClientOptions.addcustomfield.configuration_warning'] = 'Η απαίτηση αυτού του πεδίου χωρίς να είναι ορατό στους πελάτες θα έχει ως αποτέλεσμα οι πελάτες να μην μπορούν να εγγραφούν ή να ενημερώσουν τις πληροφορίες του λογαριασμού τους.';
+$lang['AdminCompanyClientOptions.addcustomfield.field.default_text'] = 'Προεπιλεγμένη τιμή κειμένου';
+$lang['AdminCompanyClientOptions.addcustomfield.field.default_checkbox'] = 'Προεπιλεγμένη τιμή Ελέγχεται';
+$lang['AdminCompanyClientOptions.addcustomfield.field.checkbox_value'] = 'Αξία';
+$lang['AdminCompanyClientOptions.addcustomfield.field.addsubmit'] = 'Προσθήκη προσαρμοσμένου πεδίου';
+$lang['AdminCompanyClientOptions.addcustomfield.field.encrypted'] = 'Κρυπτογράφηση τιμών';
+$lang['AdminCompanyClientOptions.addcustomfield.field.regex'] = 'Προσαρμοσμένη Regex';
+$lang['AdminCompanyClientOptions.addcustomfield.field.required'] = 'Απαιτούμενο';
+$lang['AdminCompanyClientOptions.addcustomfield.field.read_only'] = 'Διαβάστε μόνο για πελάτες';
+$lang['AdminCompanyClientOptions.addcustomfield.field.show_client'] = 'Ορατό στους πελάτες';
+$lang['AdminCompanyClientOptions.addcustomfield.field.type'] = 'Τύπος';
+$lang['AdminCompanyClientOptions.addcustomfield.field.link'] = 'Σύνδεσμος';
+$lang['AdminCompanyClientOptions.addcustomfield.field.is_lang'] = 'Το όνομα είναι ένας γλωσσικός ορισμός';
+$lang['AdminCompanyClientOptions.addcustomfield.field.name'] = 'Όνομα';
+$lang['AdminCompanyClientOptions.addcustomfield.field.client_group_id'] = 'Ομάδα πελατών';
+$lang['AdminCompanyClientOptions.addcustomfield.boxtitle_add'] = 'Προσθήκη προσαρμοσμένου πεδίου';
+$lang['AdminCompanyClientoptions.addcustomfield.page_title'] = 'Ρυθμίσεις > Εταιρεία > Επιλογές πελάτη > Προσαρμοσμένα πεδία πελάτη > Προσθήκη προσαρμοσμένου πεδίου';
+$lang['AdminCompanyClientOptions.customfields.no_results'] = 'Δεν υπάρχουν προσαρμοσμένα πεδία.';
+$lang['AdminCompanyClientOptions.customfields.confirm_delete'] = 'Η διαγραφή αυτού του προσαρμοσμένου πεδίου θα διαγράψει όλα τα δεδομένα που είναι αποθηκευμένα για αυτό για κάθε πελάτη εντός αυτής της ομάδας. Είστε σίγουροι ότι θέλετε να διαγράψετε αυτό το προσαρμοσμένο πεδίο;';
+$lang['AdminCompanyClientOptions.customfields.option_delete'] = 'Διαγραφή';
+$lang['AdminCompanyClientOptions.customfields.option_edit'] = 'Επεξεργασία';
+$lang['AdminCompanyClientOptions.customfields.text_options'] = 'Επιλογές';
+$lang['AdminCompanyClientOptions.customfields.text_read_only'] = 'Διαβάστε μόνο για πελάτες';
+$lang['AdminCompanyClientOptions.customfields.text_visible'] = 'Ορατό στους πελάτες';
+$lang['AdminCompanyClientOptions.customfields.text_required'] = 'Απαιτούμενο';
+$lang['AdminCompanyClientOptions.customfields.text_type'] = 'Τύπος';
+$lang['AdminCompanyClientOptions.customfields.text_name'] = 'Όνομα';
+$lang['AdminCompanyClientOptions.customfields.categorylink_addfield'] = 'Δημιουργία πεδίου';
+$lang['AdminCompanyClientOptions.customfields.boxtitle_browse'] = 'Αναζήτηση προσαρμοσμένων πεδίων πελάτη';
+$lang['AdminCompanyClientoptions.customfields.page_title'] = 'Ρυθμίσεις > Εταιρεία > Επιλογές πελάτη > Προσαρμοσμένα πεδία πελάτη > Αναζήτηση';
+$lang['AdminCompanyClientOptions.general.text_submit'] = 'Υποβολή';
+$lang['AdminCompanyClientOptions.general.prevent_unverified_payments'] = 'Αποτροπή πληρωμών από μη επαληθευμένους πελάτες';
+$lang['AdminCompanyClientOptions.general.field_clients_increment'] = 'Αύξηση τιμής ID πελάτη';
+$lang['AdminCompanyClientOptions.general.field_clients_start'] = 'Αναγνωριστικό πελάτη Αξία έναρξης';
+$lang['AdminCompanyClientOptions.general.field_clients_format'] = 'Μορφή αναγνωριστικού πελάτη';
+$lang['AdminCompanyClientOptions.general.field_email_verification'] = 'Ενεργοποίηση επαλήθευσης email';
+$lang['AdminCompanyClientOptions.general.field_force_email_usernames'] = 'Εξαναγκασμός ονομάτων χρήστη email';
+$lang['AdminCompanyClientOptions.general.field_unique_contact_emails_all'] = 'Όλες οι επαφές';
+$lang['AdminCompanyClientOptions.general.field_unique_contact_emails_primary'] = 'Κύριες επαφές';
+$lang['AdminCompanyClientOptions.general.field_unique_contact_emails_none'] = '-- Καμία --';
+$lang['AdminCompanyClientOptions.general.field_unique_contact_emails'] = 'Επιβολή μοναδικών διευθύνσεων email επικοινωνίας';
+$lang['AdminCompanyClientOptions.general.boxtitle'] = 'Γενικές ρυθμίσεις πελάτη';
+$lang['AdminCompanyClientoptions.general.page_title'] = 'Ρυθμίσεις > Εταιρεία > Επιλογές πελάτη > Γενικά';
+$lang['AdminCompanyClientOptions.!tooltip.select_default'] = 'Η επιλεγμένη τιμή της επιλογής θα είναι η προεπιλεγμένη τιμή που επιλέγεται όταν προστίθεται αυτή η επιλογή για έναν πελάτη.';
+$lang['AdminCompanyClientOptions.!tooltip.default_checkbox'] = 'Εάν είναι επιλεγμένο, αυτό το πλαίσιο ελέγχου θα είναι προεπιλεγμένο από προεπιλογή όταν προστίθεται αυτό το προσαρμοσμένο πεδίο για έναν πελάτη.';
+$lang['AdminCompanyClientOptions.!tooltip.default_text'] = 'Το κείμενο που εισάγεται εδώ θα είναι η προεπιλεγμένη τιμή που ορίζεται για αυτή την επιλογή όταν προστίθεται αυτό το προσαρμοσμένο πεδίο για έναν πελάτη.';
+$lang['AdminCompanyClientOptions.!tooltip.checkbox_value'] = 'Η τιμή που υποβάλλεται όταν είναι επιλεγμένο το πλαίσιο ελέγχου.';
+$lang['AdminCompanyClientOptions.!tooltip.encrypted'] = 'Επιλέξτε αυτό το πλαίσιο για να αποθηκεύσετε την τιμή κρυπτογραφημένη. Αυτό συνιστάται ιδιαίτερα αν αποθηκεύετε ευαίσθητες ή προσωπικές πληροφορίες.';
+$lang['AdminCompanyClientOptions.!tooltip.regex'] = 'Αυτή η επιλογή θα εμφανιστεί εάν η επιλογή "Required" έχει οριστεί σε "Custom Regex". Εισάγετε εδώ την προσαρμοσμένη κανονική έκφραση για την επικύρωση αυτού του πεδίου.';
+$lang['AdminCompanyClientOptions.!tooltip.required'] = 'Επιλέξτε "Ναι" για να διασφαλίσετε ότι δίνεται μια τιμή για αυτό το πεδίο, για τους τύπους Drop Down η επιλογή πρέπει να εμφανίζεται στη λίστα επιλογών. Επιλέξτε "Όχι" για να δεχτείτε οποιαδήποτε τιμή για αυτό το πεδίο. Επιλέξτε "Custom Regex" για να χρησιμοποιήσετε μια προσαρμοσμένη κανονική έκφραση για την επικύρωση αυτού του πεδίου.';
+$lang['AdminCompanyClientOptions.!tooltip.read_only_field'] = 'Η επιλογή αυτού του πλαισίου θα καταστήσει αυτό το προσαρμοσμένο πεδίο αμετάβλητο από τον πελάτη. Τα πεδία που είναι μόνο για ανάγνωση θα οριστούν αυτόματα στην προεπιλεγμένη τιμή τους.';
+$lang['AdminCompanyClientOptions.!tooltip.show_client'] = 'Ελέγξτε για να επιτρέψετε στους πελάτες να βλέπουν και να ενημερώνουν αυτό το πεδίο.';
+$lang['AdminCompanyClientOptions.!tooltip.type'] = 'Το προσαρμοσμένο πεδίο θα εμφανιστεί ως ο επιλεγμένος τύπος φόρμας.';
+$lang['AdminCompanyClientOptions.!tooltip.link'] = 'Ένας προσαρμοσμένος σύνδεσμος που μπορεί να εισαχθεί στο όνομα του πεδίου. Κλείστε το κείμενο που θέλετε να εφαρμόσετε το σύνδεσμο μέσα σε αγκύλες (π.χ. [terms]).';
+$lang['AdminCompanyClientOptions.!tooltip.is_lang'] = 'Επιλέξτε αυτό το πλαίσιο μόνο εάν έχετε προσθέσει έναν ορισμό γλώσσας για αυτό το προσαρμοσμένο πεδίο στο αρχείο προσαρμοσμένης γλώσσας.';
+$lang['AdminCompanyClientOptions.!tooltip.name'] = 'Αυτό είναι το όνομα εμφάνισης για αυτό το πεδίο. Μπορεί να είναι ορισμός γλώσσας.';
+$lang['AdminCompanyClientOptions.!tooltip.client_group_id'] = 'Το προσαρμοσμένο πεδίο θα ισχύει μόνο για τα μέλη της επιλεγμένης ομάδας πελατών.';
+$lang['AdminCompanyClientOptions.!tooltip.clients_increment'] = 'Οι επόμενοι αριθμοί αναγνωριστικών πελάτη θα αυξάνονται κατά αυτή την τιμή.';
+$lang['AdminCompanyClientOptions.!tooltip.clients_start'] = 'Το Client ID Start είναι η αρχική τιμή για τα Client ID. Οι νέοι πελάτες θα έχουν αυτή την τιμή, εκτός εάν είναι μικρότερη από την τιμή του πιο πρόσφατα δημιουργημένου πελάτη.';
+$lang['AdminCompanyClientOptions.!tooltip.clients_format'] = 'Client ID Format είναι η μορφή του Client ID. Μια τιμή ABC-{num} θα οδηγήσει σε ένα αναγνωριστικό πελάτη ABC-1500, όπου 1500 είναι η τιμή του αναγνωριστικού του πελάτη.';
+$lang['AdminCompanyClientOptions.!tooltip.email_verification'] = 'Ελέγξτε για να στέλνετε ένα email επαλήθευσης όταν δημιουργείται μια νέα σύνδεση ή όταν ένας πελάτης αλλάζει τη διεύθυνση email του. Μια ειδοποίηση θα εμφανίζεται στο προφίλ του πελάτη μέχρι να επαληθευτεί.';
+$lang['AdminCompanyClientOptions.!tooltip.force_email_usernames'] = 'Οι πελάτες πρέπει να χρησιμοποιούν τη διεύθυνση ηλεκτρονικού ταχυδρομείου τους ως όνομα χρήστη και όχι να ορίζουν το δικό τους. Οι υπάρχοντες πελάτες μπορούν να εξακολουθούν να συνδέονται με το τρέχον όνομα χρήστη τους.';
+$lang['AdminCompanyClientOptions.!tooltip.unique_contact_emails'] = 'Περιορίζει τις διευθύνσεις ηλεκτρονικού ταχυδρομείου για τις επαφές. Πρωτογενείς επαφές σημαίνει ότι δύο πρωτογενείς επαφές (π.χ. πελάτες) δεν μπορούν να έχουν την ίδια διεύθυνση ηλεκτρονικού ταχυδρομείου. Όλες οι επαφές σημαίνει ότι καμία επαφή οποιουδήποτε τύπου δεν μπορεί να έχει την ίδια διεύθυνση ηλεκτρονικού ταχυδρομείου με άλλη επαφή.';
+$lang['AdminCompanyClientOptions.!tooltip.state'] = 'Να είστε προσεκτικοί όταν απαιτείται η επιλογή μιας Πολιτείας/Περιφέρειας. Ορισμένες χώρες δεν έχουν καθόλου πολιτείες. Οι πελάτες σε αυτές τις χώρες δεν θα μπορούν να αποθηκεύσουν τα στοιχεία επικοινωνίας τους. Συνιστούμε να μην απαιτείται αυτό το πεδίο.';
+$lang['AdminCompanyClientOptions.!tooltip.read_only'] = 'Εάν είναι επιλεγμένο, το πεδίο δεν μπορεί να τροποποιηθεί από τον πελάτη εάν περιέχει δεδομένα.';
+$lang['AdminCompanyClientOptions.!error.clients_format'] = 'Η μορφή ID πελάτη πρέπει να περιέχει {num}.';
+$lang['AdminCompanyClientOptions.!notice.group_settings'] = 'ΣΗΜΕΙΩΣΗ: Αυτές οι ρυθμίσεις ισχύουν μόνο για τις ομάδες πελατών που κληρονομούν τις ρυθμίσεις τους από την εταιρεία.';
+$lang['AdminCompanyClientOptions.!success.general_updated'] = 'Οι γενικές ρυθμίσεις ενημερώθηκαν με επιτυχία.';
+$lang['AdminCompanyClientOptions.!success.requiredfields_updated'] = 'Τα απαιτούμενα πεδία ενημερώθηκαν επιτυχώς.';
+$lang['AdminCompanyClientOptions.!success.field_deleted'] = 'Το προσαρμοσμένο πεδίο πελάτη διαγράφηκε επιτυχώς.';
+$lang['AdminCompanyClientOptions.!success.field_created'] = 'Το προσαρμοσμένο πεδίο πελάτη δημιουργήθηκε με επιτυχία.';
+$lang['AdminCompanyClientOptions.!success.field_updated'] = 'Το προσαρμοσμένο πεδίο πελάτη ενημερώθηκε επιτυχώς.';
+
