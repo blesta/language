6c396d53dffef2bc2392a8f8f9fbe03752d771d0
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:05 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/messenger_manager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Messenger Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MessengerManager.!error.version.valid'] = 'Verze je neplatná.';
+$lang['MessengerManager.!error.name.valid'] = 'Název je neplatný.';
+$lang['MessengerManager.!error.dir.valid'] = 'Adresář je neplatný.';
+$lang['MessengerManager.!error.company_id.valid'] = 'ID společnosti je neplatné.';
+$lang['MessengerManager.!error.messenger.missing'] = 'Pro daný typ není nakonfigurován žádný messenger.';
+
