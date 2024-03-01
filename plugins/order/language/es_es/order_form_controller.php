2bdce78e9e554b34a96a20b379fdf70534933df4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:39 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/order_form_controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Order Form Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderFormController.!notice.items_removed'] = 'Algunos artículos han sido eliminados de su cesta debido a restricciones de embalaje.';
+
