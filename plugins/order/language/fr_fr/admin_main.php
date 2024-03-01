cb8e362e46fbdd74b819f5eaef594088d6615488
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:28 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/admin_main.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.status_updated'] = 'Les commandes sélectionnées ont été mises à jour avec succès.';
+$lang['AdminMain.!success.settings_updated'] = 'Vos paramètres de commande ont été mis à jour avec succès.';
+$lang['AdminMain.!success.affiliate_settings_updated'] = 'Les paramètres d\'affiliation ont été mis à jour.';
+$lang['AdminMain.index.boxtitle_order'] = 'Commandes';
+$lang['AdminMain.index.no_results'] = 'Il n\'y a pas de commande de ce type.';
+$lang['AdminMain.index.category_pending'] = 'En revue';
+$lang['AdminMain.index.category_accepted'] = 'Accepté';
+$lang['AdminMain.index.category_fraud'] = 'Fraude';
+$lang['AdminMain.index.category_canceled'] = 'Annulé';
+$lang['AdminMain.index.heading_order_number'] = 'N° de commande';
+$lang['AdminMain.index.heading_client_id_code'] = 'Identifiant du client';
+$lang['AdminMain.index.heading_invoice_id_code'] = 'Facture #';
+$lang['AdminMain.index.heading_total'] = 'Total';
+$lang['AdminMain.index.heading_paid'] = 'Payé';
+$lang['AdminMain.index.heading_date_added'] = 'Date de la commande';
+$lang['AdminMain.index.field_ordersubmit'] = 'Mise à jour des commandes';
+$lang['AdminMain.index.field_markas'] = 'Marquer comme :';
+$lang['AdminMain.index.text_location'] = 'Localisation';
+$lang['AdminMain.search.page_title'] = 'Résultats de la recherche pour "%1$s"';
+$lang['AdminMain.search.boxtitle_order'] = 'Rechercher les commandes pour "%1$s".';
+$lang['AdminMain.getfilters.field_order_number'] = 'N° de commande';
+$lang['AdminMain.orderinfo.fraudreport_heading'] = 'Rapport sur la fraude';
+$lang['AdminMain.orderinfo.fraudreport_heading_key'] = 'Clé';
+$lang['AdminMain.orderinfo.fraudreport_heading_value'] = 'Valeur';
+$lang['AdminMain.orderinfo.applied_heading'] = 'Transactions';
+$lang['AdminMain.orderinfo.applied_heading_paymenttype'] = 'Type de paiement';
+$lang['AdminMain.orderinfo.applied_heading_amount'] = 'Montant';
+$lang['AdminMain.orderinfo.applied_heading_applied'] = 'Appliqué';
+$lang['AdminMain.orderinfo.applied_heading_appliedon'] = 'Appliqué le';
+$lang['AdminMain.orderinfo.applied_heading_options'] = 'Options';
+$lang['AdminMain.orderinfo.applied_option_edit'] = 'Editer';
+$lang['AdminMain.orderinfo.applied_no_results'] = 'Aucune transaction n\'est appliquée à cette commande.';
+$lang['AdminMain.orderinfo.services_heading'] = 'Services';
+$lang['AdminMain.orderinfo.services_heading_package'] = 'Paquet';
+$lang['AdminMain.orderinfo.services_heading_label'] = 'Étiquette';
+$lang['AdminMain.orderinfo.services_heading_term'] = 'Durée';
+$lang['AdminMain.orderinfo.services_heading_dateadded'] = 'Date d\'ajout';
+$lang['AdminMain.orderinfo.services_heading_daterenews'] = 'Date Renouvellement';
+$lang['AdminMain.orderinfo.services_heading_options'] = 'Options';
+$lang['AdminMain.orderinfo.services_option_manage'] = 'Gérer';
+$lang['AdminMain.orderinfo.services_text_never'] = 'Jamais';
+$lang['AdminMain.orderinfo.services_no_results'] = 'Il n\'y a pas de services dans cet ordre.';
+$lang['AdminMain.settings.heading_notifications'] = 'Notifications de commande';
+$lang['AdminMain.settings.field_email_notice'] = 'Courriel';
+$lang['AdminMain.settings.field_email_notice_never'] = 'Ne pas envoyer';
+$lang['AdminMain.settings.field_email_notice_manual'] = 'N\'envoyer que si une approbation manuelle est requise';
+$lang['AdminMain.settings.field_email_notice_always'] = 'Toujours envoyer';
+$lang['AdminMain.settings.field_mobile_notice'] = 'Mobile';
+$lang['AdminMain.settings.field_mobile_notice_never'] = 'Ne pas envoyer';
+$lang['AdminMain.settings.field_mobile_notice_manual'] = 'N\'envoyer que si une approbation manuelle est requise';
+$lang['AdminMain.settings.field_mobile_notice_always'] = 'Toujours envoyer';
+$lang['AdminMain.settings.field_messenger_notice'] = 'Messagers';
+$lang['AdminMain.settings.field_messenger_notice_never'] = 'Ne pas envoyer';
+$lang['AdminMain.settings.field_messenger_notice_manual'] = 'N\'envoyer que si une approbation manuelle est requise';
+$lang['AdminMain.settings.field_messenger_notice_always'] = 'Toujours envoyer';
+$lang['AdminMain.settings.heading_affiliate_notifications'] = 'Notifications aux affiliés';
+$lang['AdminMain.settings.field_payout_notice'] = 'Paiement';
+$lang['AdminMain.settings.field_payout_notice_never'] = 'Jamais';
+$lang['AdminMain.settings.field_payout_notice_always'] = 'Toujours';
+$lang['AdminMain.settings.submit_settings'] = 'Mise à jour des paramètres';
+$lang['AdminMain.settings.submit_cancel'] = 'Retour à la liste des commandes';
+$lang['AdminMain.affiliates.page_title'] = 'Aperçu de l\'affiliation';
+$lang['AdminMain.affiliates.boxtitle_affiliates'] = 'Aperçu de l\'affiliation';
+$lang['AdminMain.affiliates.inactive'] = 'Cet affilié a été marqué comme inactif.';
+$lang['AdminMain.affiliates.activate'] = 'Cliquez ici pour réactiver';
+$lang['AdminMain.affiliates.no_results'] = 'Aucun affilié n\'est associé à ce client.  Cliquez ici pour vous inscrire.';
+$lang['AdminMain.affiliates.title_details'] = 'Détails';
+$lang['AdminMain.affiliates.title_referral'] = 'Lien de référence';
+$lang['AdminMain.affiliates.visits'] = 'Clics';
+$lang['AdminMain.affiliates.monthly_visits'] = '%1$s/mo en moyenne';
+$lang['AdminMain.affiliates.sales'] = 'Ventes totales';
+$lang['AdminMain.affiliates.monthly_sales'] = '%1$s/mo en moyenne';
+$lang['AdminMain.affiliates.conversion_rate'] = 'Taux de conversion';
+$lang['AdminMain.affiliates.conversion_fraction'] = '%1$s/%2$s en moyenne';
+$lang['AdminMain.affiliates.commission_type'] = 'Niveau de paiement';
+$lang['AdminMain.affiliates.min_withdrawal_amount'] = 'Montant minimum du paiement';
+$lang['AdminMain.affiliates.maturity_days'] = 'Jours de maturation';
+$lang['AdminMain.affiliates.available_payout'] = 'Disponible pour le paiement';
+$lang['AdminMain.affiliates.field.commission_fixed'] = '%1$s en une seule fois pour chaque parrainage effectué';
+$lang['AdminMain.affiliates.field.commission_percentage'] = '%1$s de chaque référence échue';
+$lang['AdminMain.affiliates.request_payout'] = 'Demande de paiement';
+$lang['AdminMain.affiliates.boxtitle_client'] = 'Client #%1$s - %2$s %3$s';
+$lang['AdminMain.affiliates.heading_affiliates'] = 'Paramètres d\'affiliation';
+$lang['AdminMain.affiliates.heading_referral_link'] = 'Lien de référence';
+$lang['AdminMain.affiliates.field_commission_type'] = 'Type de Commission';
+$lang['AdminMain.affiliates.tooltip_commission_type'] = 'La méthode de calcul utilisée pour déterminer la commission de recommandation.  Soit une valeur fixe par commande, soit un pourcentage du total de la commande.';
+$lang['AdminMain.affiliates.field_commission_amount'] = 'Montant de la Commission';
+$lang['AdminMain.affiliates.field_order_frequency'] = 'Ordre Fréquence';
+$lang['AdminMain.affiliates.tooltip_order_frequency'] = 'Premier pour rémunérer les référents uniquement pour la première commande qu\'un client effectue à l\'aide d\'un lien de référence, ou Tout pour rémunérer les référents pour chaque commande qu\'un client effectue à l\'aide d\'un lien de référence.';
+$lang['AdminMain.affiliates.field_order_recurring'] = 'Commande récurrente';
+$lang['AdminMain.affiliates.tooltip_order_recurring'] = 'Si les références seront payées pour les services commandés lors du renouvellement.';
+$lang['AdminMain.affiliates.field_maturity_days'] = 'Jours d\'échéance';
+$lang['AdminMain.affiliates.tooltip_maturity_days'] = 'Le nombre de jours après le paiement d\'une recommandation qui arrive à maturité et devient disponible pour le paiement.';
+$lang['AdminMain.affiliates.field_min_withdrawal_amount'] = 'Montant minimum de retrait';
+$lang['AdminMain.affiliates.field_max_withdrawal_amount'] = 'Montant maximum de retrait';
+$lang['AdminMain.affiliates.field_withdrawal_currency'] = 'Devise de retrait';
+$lang['AdminMain.affiliates.text_code_order_form'] = 'Vous pouvez également créer un lien vers une page particulière du formulaire de commande en tant qu\'affilié en ajoutant à la fin ?a=%1$s à un lien sans paramètres de requête ou &a=%1$s à un lien avec paramètres de requête.';
+$lang['AdminMain.affiliates.heading_statistics'] = 'Statistiques';
+$lang['AdminMain.affiliates.graph_referrals'] = 'Renvois';
+$lang['AdminMain.affiliates.graph_visits'] = 'Visites';
+$lang['AdminMain.affiliates.graph_sales'] = 'Vente';
+$lang['AdminMain.affiliates.submit_settings'] = 'Sauvegarder les paramètres';
+$lang['AdminMain.affiliates.submit_cancel'] = 'Annuler';
+
