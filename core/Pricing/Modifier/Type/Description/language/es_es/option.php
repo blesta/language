637d7cd71a31cf32805dc600ab7a7f43c267b962
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:29 -0800

    Updated es_es language files

---
+++ core/Pricing/Modifier/Type/Description/language/es_es/option.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Option
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Option.description.prefix.nest'] = '↳';
+$lang['Option.description.option.item'] = '%1$s - %2$s';
+$lang['Option.description.option.item.text'] = '%1$s';
+$lang['Option.description.option.item.date'] = '%1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.date.text'] = '%1$s (%2$s - %3$s)';
+$lang['Option.description.option.item_qty'] = '%1$s - %3$sx %2$s';
+$lang['Option.description.option.item_qty.date'] = '%1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.setup'] = '%1$s - %2$s Gastos de instalación';
+$lang['Option.description.option.cancel'] = '%1$s - %2$s Tasa de cancelación';
+$lang['Option.description.option.item.added'] = 'Suma de %1$s - %2$s';
+$lang['Option.description.option.item.added.text'] = 'Adición de %1$s';
+$lang['Option.description.option.item.added.date'] = 'Suma de %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.added.date.text'] = 'Suma de %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item_qty.added'] = 'Suma de %1$s - %3$sx %2$s';
+$lang['Option.description.option.item_qty.added.date'] = 'Suma de %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.removed'] = 'Eliminación de %1$s - %2$s';
+$lang['Option.description.option.item.removed.text'] = 'Eliminación de %1$s';
+$lang['Option.description.option.item.removed.date'] = 'Eliminación de %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.removed.date.text'] = 'Eliminación de %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item_qty.removed'] = 'Eliminación de %1$s - %3$sx %2$s';
+$lang['Option.description.option.item_qty.removed.date'] = 'Eliminación de %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.updated'] = 'Cambio de %1$s de %3$s a %2$s';
+$lang['Option.description.option.item.updated.text'] = 'Cambio de %1$s';
+$lang['Option.description.option.item.updated.date'] = 'Cambio de %1$s de %3$s a %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.updated.date.text'] = 'Cambio de %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item_qty.updated'] = 'Cambio de %1$s de %4$s a %3$sx %2$s';
+$lang['Option.description.option.item_qty.updated.date'] = 'Cambio de %1$s de %4$s a %3$sx %2$s (%5$s - %6$s)';
+$lang['Option.description.option.item.prorated'] = 'Adición prorrateada de %1$s - %2$s';
+$lang['Option.description.option.item.prorated.text'] = 'Adición prorrateada de %1$s';
+$lang['Option.description.option.item.prorated.date'] = 'Suma prorrateada de %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.prorated.date.text'] = 'Adición prorrateada de %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item_qty.prorated'] = 'Suma prorrateada de %1$s - %3$sx %2$s';
+$lang['Option.description.option.item_qty.prorated.date'] = 'Suma prorrateada de %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.added.prorated'] = 'Adición prorrateada de %1$s - %2$s';
+$lang['Option.description.option.item.added.prorated.text'] = 'Adición prorrateada de %1$s';
+$lang['Option.description.option.item.added.prorated.date'] = 'Suma prorrateada de %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.added.prorated.date.text'] = 'Adición prorrateada de %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item_qty.added.prorated'] = 'Suma prorrateada de %1$s - %3$sx %2$s';
+$lang['Option.description.option.item_qty.added.prorated.date'] = 'Suma prorrateada de %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.removed.prorated'] = 'Retirada prorrateada de %1$s - %2$s';
+$lang['Option.description.option.item.removed.prorated.text'] = 'Retirada prorrateada de %1$s';
+$lang['Option.description.option.item.removed.prorated.date'] = 'Retirada prorrateada de %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.removed.prorated.date.text'] = 'Retirada prorrateada de %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item_qty.removed.prorated'] = 'Retirada prorrateada de %1$s - %3$sx %2$s';
+$lang['Option.description.option.item_qty.removed.prorated.date'] = 'Retirada prorrateada de %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.updated.prorated'] = 'Cambio prorrateado de %1$s de %3$s a %2$s';
+$lang['Option.description.option.item.updated.prorated.text'] = 'Cambio prorrateado de %1$s';
+$lang['Option.description.option.item.updated.prorated.date'] = 'Cambio prorrateado de %1$s de %3$s a %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.updated.prorated.date.text'] = 'Cambio prorrateado de %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item_qty.updated.prorated'] = 'Cambio prorrateado de %1$s de %4$s a %3$sx %2$s';
+$lang['Option.description.option.item_qty.updated.prorated.date'] = 'Cambio prorrateado de %1$s de %4$s a %3$sx %2$s (%5$s - %6$s)';
+
