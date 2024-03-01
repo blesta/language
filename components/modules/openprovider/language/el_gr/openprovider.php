f6fd0b94b9f5dede2abd84dc853869570e318910
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:56 -0800

    Updated el_gr language files

---
+++ components/modules/openprovider/language/el_gr/openprovider.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Openprovider
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OpenProvider.config.name'] = 'Openprovider';
+$lang['OpenProvider.config.description'] = 'Μονάδα Openprovider για την Blesta.';
+$lang['OpenProvider.config.module.row'] = 'Λογαριασμός καταχωρητή';
+$lang['OpenProvider.config.module.rows'] = 'Λογαριασμοί καταχωρητή';
+$lang['OpenProvider.config.module.group'] = 'Λογαριασμοί καταχωρητή';
+$lang['OpenProvider.config.module.row_keys'] = '';
+$lang['OpenProvider.manage.add_row_btn'] = 'Προσθήκη λογαριασμού';
+$lang['OpenProvider.manage.module_rows_title'] = 'Κατάλογος λογαριασμών';
+$lang['OpenProvider.manage.module_rows_no_results'] = 'Δεν υπάρχουν λογαριασμοί.';
+$lang['OpenProvider.manage.module_rows_header.username'] = 'Όνομα χρήστη';
+$lang['OpenProvider.manage.module_rows_header.test_mode'] = 'Λειτουργία δοκιμής';
+$lang['OpenProvider.manage.module_rows_header.options'] = 'Επιλογές';
+$lang['OpenProvider.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['OpenProvider.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['OpenProvider.manage.module_rows.confirm_delete'] = 'Είστε σίγουρος ότι θέλετε να διαγράψετε αυτόν τον λογαριασμό;';
+$lang['OpenProvider.manage.manage_packages'] = 'Διαχείριση πακέτων';
+$lang['OpenProvider.add_row.box_title'] = 'Προσθήκη λογαριασμού OpenProvider';
+$lang['OpenProvider.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['OpenProvider.add_row.add_btn'] = 'Προσθήκη λογαριασμού';
+$lang['OpenProvider.edit_row.box_title'] = 'Επεξεργασία λογαριασμού OpenProvider';
+$lang['OpenProvider.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['OpenProvider.edit_row.add_btn'] = 'Ενημέρωση λογαριασμού';
+$lang['OpenProvider.row_meta.username'] = 'Όνομα χρήστη';
+$lang['OpenProvider.row_meta.password'] = 'Κωδικός πρόσβασης';
+$lang['OpenProvider.row_meta.test_mode'] = 'Λειτουργία δοκιμής';
+$lang['OpenProvider.row_meta.test_mode_true'] = 'Ναι';
+$lang['OpenProvider.row_meta.test_mode_false'] = 'Όχι';
+$lang['OpenProvider.package_fields.type'] = 'Τύπος';
+$lang['OpenProvider.package_fields.type_domain'] = 'Εγγραφή τομέα';
+$lang['OpenProvider.package_fields.type_ssl'] = 'Πιστοποιητικό SSL';
+$lang['OpenProvider.package_fields.tld_options'] = 'TLDs';
+$lang['OpenProvider.nameserver.ns1'] = 'Διακομιστής ονομάτων 1';
+$lang['OpenProvider.nameserver.ns2'] = 'Διακομιστής ονομάτων 2';
+$lang['OpenProvider.nameserver.ns3'] = 'Διακομιστής ονομάτων 3';
+$lang['OpenProvider.nameserver.ns4'] = 'Διακομιστής ονομάτων 4';
+$lang['OpenProvider.nameserver.ns5'] = 'Διακομιστής ονομάτων 5';
+$lang['OpenProvider.domain.domain'] = 'Όνομα τομέα';
+$lang['OpenProvider.domain.identification_type'] = 'Τύπος αναγνώρισης';
+$lang['OpenProvider.domain.identification_type.passport_number'] = 'Αριθμός διαβατηρίου';
+$lang['OpenProvider.domain.identification_type.passport_series'] = 'Σειρά Passport';
+$lang['OpenProvider.domain.identification_type.company_registration_number'] = 'Αριθμός μητρώου εταιρείας';
+$lang['OpenProvider.transfer.domain'] = 'Όνομα τομέα';
+$lang['OpenProvider.transfer.EPPCode'] = 'Κωδικός EPP';
+$lang['OpenProvider.tab_nameservers.title'] = 'Διακομιστές ονομάτων';
+$lang['OpenProvider.tab_nameserver.field_ns'] = 'Διακομιστής ονομάτων %1$s';
+$lang['OpenProvider.tab_nameservers.field_submit'] = 'Ενημέρωση διακομιστών ονομάτων';
+$lang['OpenProvider.tab_domain_contacts.title'] = 'Επαφές τομέα';
+$lang['OpenProvider.tab_domain_contacts.field_submit'] = 'Ενημέρωση επαφών τομέα';
+$lang['OpenProvider.tab_domain_contacts.contact_type.admin'] = 'Διαχειριστής';
+$lang['OpenProvider.tab_domain_contacts.contact_type.billing'] = 'Τιμολόγηση';
+$lang['OpenProvider.tab_domain_contacts.contact_type.tech'] = 'Τεχνολογία';
+$lang['OpenProvider.tab_domain_contacts.contact_type.owner'] = 'Ιδιοκτήτης';
+$lang['OpenProvider.tab_domain_contacts.field.first_name'] = 'Όνομα';
+$lang['OpenProvider.tab_domain_contacts.field.last_name'] = 'Επώνυμο';
+$lang['OpenProvider.tab_domain_contacts.field.middle_name'] = 'Μεσαίο όνομα';
+$lang['OpenProvider.tab_domain_contacts.field.email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['OpenProvider.tab_domain_contacts.field.phone_number'] = 'Τηλέφωνο';
+$lang['OpenProvider.tab_domain_contacts.field.company_name'] = 'Εταιρεία';
+$lang['OpenProvider.tab_domain_contacts.field.address'] = 'Διεύθυνση';
+$lang['OpenProvider.tab_domain_contacts.field.city'] = 'Πόλη';
+$lang['OpenProvider.tab_domain_contacts.field.state'] = 'Κράτος';
+$lang['OpenProvider.tab_domain_contacts.field.country'] = 'Χώρα';
+$lang['OpenProvider.tab_domain_contacts.field.zipcode'] = 'Ταχυδρομικός κώδικας';
+$lang['OpenProvider.tab_domain_contacts.field.vat'] = 'Vat';
+$lang['OpenProvider.tab_domain_contacts.field.company_or_individual_id'] = 'Αναγνωριστικό εταιρείας ή ατόμου';
+$lang['OpenProvider.tab_settings.title'] = 'Ρυθμίσεις';
+$lang['OpenProvider.tab_settings.field.epp'] = 'Κωδικός EPP/κλειδί μεταφοράς';
+$lang['OpenProvider.tab_settings.field.submit'] = 'Ενημέρωση ρυθμίσεων';
+$lang['OpenProvider.tab_settings.message.epp_code'] = 'Κωδικός Epp: %s';
+$lang['OpenProvider.tab_settings.field.is_locked_true'] = 'Το κλείδωμα μεταφοράς τομέα είναι ενεργοποιημένο. Αλλάξτε για να το απενεργοποιήσετε.';
+$lang['OpenProvider.tab_settings.field.is_locked_false'] = 'Το κλείδωμα μεταφοράς τομέα είναι απενεργοποιημένο. Αλλάξτε για να το ενεργοποιήσετε.';
+$lang['OpenProvider.!error.username.empty'] = 'Το πεδίο Όνομα χρήστη δεν πρέπει να είναι κενό!';
+$lang['OpenProvider.!error.password.empty'] = 'Το πεδίο κωδικού πρόσβασης δεν πρέπει να είναι κενό!';
+$lang['OpenProvider.!error.password.valid_connection'] = 'Ουπς, κάτι δεν πάει καλά: Ο συνδυασμός ονόματος χρήστη, κωδικού πρόσβασης και λειτουργίας δοκιμής φαίνεται να είναι άκυρος ή ο λογαριασμός σας OpenProvider μπορεί να μην έχει ρυθμιστεί ώστε να επιτρέπει την πρόσβαση στο API.';
+$lang['OpenProvider.!error.client.not_exist'] = 'Πελάτης δεν υπάρχει!';
+$lang['OpenProvider.!error.client.phone_not_exist'] = 'Ο αριθμός τηλεφώνου του πελάτη είναι άδειος!';
+$lang['OpenProvider.!error.epp.empty'] = 'Οι μεταφορές τομέων απαιτούν την εισαγωγή ενός κωδικού EPP.';
+$lang['OpenProvider.!error.passport_number.empty'] = 'Αυτή η διεύθυνση απαιτεί την εισαγωγή ενός αριθμού διαβατηρίου.';
+$lang['OpenProvider.!error.passport_series.empty'] = 'Αυτή η διεύθυνση απαιτεί την εισαγωγή μιας σειράς διαβατηρίων.';
+$lang['OpenProvider.!error.domain.not_exist'] = 'Αυτός ο τομέας δεν υπάρχει στο Openprovider.';
+$lang['OpenProvider.!error.domain.contact_support'] = 'Επικοινωνήστε με την υποστήριξη για λεπτομέρειες σχετικά με τον τομέα.';
+$lang['OpenProvider.!error.domain.name_undefined'] = 'Όνομα τομέα απροσδιόριστο.';
+$lang['OpenProvider.!error.service.domain.status_not_active_in_blesta'] = 'Το τμήμα αυτό δεν είναι προσβάσιμο όσο η υπηρεσία δεν βρίσκεται σε κατάσταση Ενεργή.';
+
