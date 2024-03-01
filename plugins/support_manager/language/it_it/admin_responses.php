314266d81d99a65acd61baac2c4ae8c2a631eaed
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:44 -0800

    Updated it_it language files

---
+++ plugins/support_manager/language/it_it/admin_responses.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Admin Responses
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminResponses.editcategory.field_submit'] = 'Categoria di aggiornamento';
+$lang['AdminResponses.editcategory.field_name'] = 'Nome';
+$lang['AdminResponses.editcategory.boxtitle_editcategory'] = 'Categoria di aggiornamento';
+$lang['AdminResponses.editcategory.boxtitle_root'] = 'Modifica categoria';
+$lang['AdminResponses.addcategory.field_submit'] = 'Crea categoria';
+$lang['AdminResponses.addcategory.field_name'] = 'Nome';
+$lang['AdminResponses.addcategory.boxtitle_addcategory'] = 'Aggiungi la categoria alla categoria [%1$s].';
+$lang['AdminResponses.addcategory.boxtitle_root'] = 'Aggiungi categoria';
+$lang['AdminResponses.edit.field_submit'] = 'Risposta all\'aggiornamento';
+$lang['AdminResponses.edit.field_details'] = 'Dettagli della risposta';
+$lang['AdminResponses.edit.field_name'] = 'Nome';
+$lang['AdminResponses.edit.boxtitle_edit'] = 'Modifica della risposta predefinita';
+$lang['AdminResponses.add.field_submit'] = 'Creare una risposta';
+$lang['AdminResponses.add.field_details'] = 'Dettagli della risposta';
+$lang['AdminResponses.add.field_name'] = 'Nome';
+$lang['AdminResponses.add.boxtitle_add'] = 'Aggiungi una risposta predefinita alla categoria [%1$s].';
+$lang['AdminResponses.modal.delete_response'] = 'Si è sicuri di voler eliminare questa risposta predefinita?';
+$lang['AdminResponses.modal.delete_category'] = 'Siete sicuri di voler eliminare questa categoria? Tutte le sottocategorie e le risposte predefinite di questa categoria saranno spostate nella categoria madre.';
+$lang['AdminResponses.index.no_responses'] = 'Non ci sono risposte in questa sezione.';
+$lang['AdminResponses.index.delete'] = 'Cancellare';
+$lang['AdminResponses.index.edit'] = 'Modifica';
+$lang['AdminResponses.index.text_up_level'] = 'Salire di livello';
+$lang['AdminResponses.index.add_category'] = 'Aggiungi categoria qui';
+$lang['AdminResponses.index.add_response'] = 'Aggiungi risposta qui';
+$lang['AdminResponses.index.boxtitle_responses'] = 'Risposte predefinite';
+$lang['AdminResponses.!text.closed_parenthesis'] = ')';
+$lang['AdminResponses.!text.forward_slash'] = '/';
+$lang['AdminResponses.!text.open_parenthesis'] = '(';
+$lang['AdminResponses.editcategory.page_title'] = 'Gestione supporto > Modifica categoria';
+$lang['AdminResponses.addcategory.page_title'] = 'Gestione supporto > Aggiungi categoria';
+$lang['AdminResponses.edit.page_title'] = 'Gestione supporto > Modifica risposta predefinita';
+$lang['AdminResponses.add.page_title'] = 'Gestione supporto > Aggiungi risposta predefinita';
+$lang['AdminResponses.index.page_title'] = 'Support Manager > Risposte predefinite';
+$lang['AdminResponses.!success.response_deleted'] = 'La risposta %1$s è stata cancellata con successo.';
+$lang['AdminResponses.!success.response_updated'] = 'La risposta %1$s è stata aggiornata con successo.';
+$lang['AdminResponses.!success.response_added'] = 'La risposta %1$s è stata aggiunta con successo.';
+$lang['AdminResponses.!success.category_deleted'] = 'La categoria %1$s è stata cancellata con successo.';
+$lang['AdminResponses.!success.category_updated'] = 'La categoria %1$s è stata aggiornata con successo.';
+$lang['AdminResponses.!success.category_added'] = 'La categoria %1$s è stata aggiunta con successo.';
+
