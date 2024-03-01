40c286e899810925a9883a525dd09742a0b23c76
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:15 -0800

    Updated es_es language files

---
+++ plugins/support_manager/language/es_es/support_manager_responses.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Support Manager Responses
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerResponses.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['SupportManagerResponses.!error.parent_id.exists'] = 'ID de categoría superior no válido.';
+$lang['SupportManagerResponses.!error.parent_id.company'] = 'Esta categoría debe pertenecer a la misma empresa que su matriz.';
+$lang['SupportManagerResponses.!error.name.empty'] = 'Introduzca un nombre de categoría.';
+$lang['SupportManagerResponses.!error.name.length'] = 'El nombre de la categoría no puede tener más de 64 caracteres.';
+$lang['SupportManagerResponses.!error.category_id.exists'] = 'ID de categoría no válido.';
+$lang['SupportManagerResponses.!error.name.response_empty'] = 'Introduzca un nombre para esta respuesta.';
+$lang['SupportManagerResponses.!error.name.response_length'] = 'El nombre de la categoría no puede tener más de 64 caracteres.';
+$lang['SupportManagerResponses.!error.details.empty'] = 'Por favor, introduzca los detalles de la respuesta.';
+$lang['SupportManagerResponses.!error.category_id.root_responses'] = 'La categoría no puede eliminarse porque contiene respuestas predefinidas.';
+
