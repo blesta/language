76c015faad01bf8b3fe3b50608c25913ccc49e55
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:24 -0800

    Updated es_es language files

---
+++ language/es_es/emails.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Emails.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['Emails.!error.html.parse'] = 'Error de análisis de plantilla: %1$s';
+$lang['Emails.!error.text.parse'] = 'Error de análisis de plantilla: %1$s';
+$lang['Emails.!error.email_signature_id.in_use'] = 'Esa firma de correo electrónico está actualmente en uso y no puede eliminarse.';
+$lang['Emails.!error.name.empty'] = 'Introduzca un nombre para la firma.';
+$lang['Emails.!error.text.empty'] = 'Por favor, introduzca una firma en texto plano.';
+$lang['Emails.!error.email_signature_id.exists'] = 'ID de firma de correo electrónico no válida.';
+$lang['Emails.!error.email_group_id.exists'] = 'ID de grupo de correo electrónico no válido.';
+$lang['Emails.!error.lang.empty'] = 'Por favor, introduzca un idioma.';
+$lang['Emails.!error.lang.length'] = 'La longitud de la lengua no puede superar los 5 caracteres.';
+$lang['Emails.!error.from.format'] = 'Introduzca una dirección de remitente válida.';
+$lang['Emails.!error.from_name.empty'] = 'Por favor, introduzca un nombre.';
+$lang['Emails.!error.reply_to.format'] = 'Introduzca una dirección de respuesta válida.';
+$lang['Emails.!error.subject.empty'] = 'Por favor, introduzca un asunto.';
+$lang['Emails.!error.include_attachments'] = 'El valor 0 o 1 debe indicar si se deben incluir los archivos adjuntos.';
+$lang['Emails.!error.status.format'] = 'Estado no válido.';
+$lang['Emails.!error.email_id.exists'] = 'ID de correo electrónico no válida.';
+$lang['Emails.!error.company_id.unique'] = 'El ID del grupo de correo electrónico y el ID de la empresa para el idioma indicado ya están ocupados.';
+$lang['Emails.!error.action.exists'] = 'La acción indicada es una acción de grupo de correo electrónico no válida.';
+$lang['Emails.!error.to_addresses.empty'] = 'Debe facilitarse al menos una dirección To.';
+$lang['Emails.!error.to_addresses.format'] = 'Al menos una de las direcciones de correo electrónico proporcionadas no es válida.';
+$lang['Emails.!error.cc_addresses.format'] = 'Al menos una de las direcciones CC de correo electrónico proporcionadas no es una dirección de correo electrónico válida.';
+$lang['Emails.!error.bcc_addresses.format'] = 'Al menos una de las direcciones CCO de correo electrónico proporcionadas no es una dirección de correo electrónico válida.';
+$lang['Emails.!error.attachments.exist'] = 'Al menos uno de los archivos adjuntos proporcionados no existe en el sistema de archivos.';
+$lang['Emails.!error.email.failed_to_send'] = 'El correo electrónico no se ha podido enviar debido a un problema de configuración.';
+$lang['Emails.getStatusTypes.active'] = 'No';
+$lang['Emails.getStatusTypes.inactive'] = 'Sí';
+
