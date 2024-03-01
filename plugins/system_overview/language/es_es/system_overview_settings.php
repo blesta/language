250c5d8289eeae93121d42ceae6b98b825c7618f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:18 -0800

    Updated es_es language files

---
+++ plugins/system_overview/language/es_es/system_overview_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * System Overview Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SystemOverviewSettings.!error.staff_id.exists'] = 'ID de personal no válida.';
+$lang['SystemOverviewSettings.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['SystemOverviewSettings.!error.settings[][key].empty'] = 'Ninguna tecla de ajuste puede estar vacía.';
+$lang['SystemOverviewSettings.!error.settings[][value].length'] = 'La longitud de cada valor de ajuste individual no puede superar los 255 caracteres.';
+
