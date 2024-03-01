11b62da0513bb1d011524dfc2c6d8e5b6ce766e8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:52 -0800

    Updated el_gr language files

---
+++ components/modules/multicraft/language/el_gr/multicraft.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Multicraft
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Multicraft.row_meta.log_all'] = 'Καταγραφή όλων των αιτημάτων API';
+$lang['Multicraft.row_meta.key'] = 'Κλειδί API';
+$lang['Multicraft.row_meta.username'] = 'Όνομα χρήστη';
+$lang['Multicraft.row_meta.panel_api_url'] = 'URL API πίνακα';
+$lang['Multicraft.row_meta.panel_url'] = 'URL πάνελ';
+$lang['Multicraft.row_meta.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Multicraft.!tooltip.log_all'] = 'Επιλέξτε το για να καταγράψετε όλες τις αιτήσεις API ή καταργήστε το για να καταγράψετε μόνο τις βασικές αιτήσεις.';
+$lang['Multicraft.!tooltip.key'] = 'Εισάγετε το κλειδί API που δημιουργήθηκε για τον χρήστη σας στο Multicraft.';
+$lang['Multicraft.!tooltip.panel_api_url'] = 'Εισάγετε τη διεύθυνση URL του Multicraft API. π.χ. http://multicraft.domain.com/api.php';
+$lang['Multicraft.!tooltip.panel_url'] = 'Εισάγετε τη διεύθυνση URL του πίνακα Multicraft. π.χ. http://multicraft.domain.com/';
+$lang['Multicraft.edit_row.in_use_col'] = 'Σε χρήση';
+$lang['Multicraft.edit_row.port_col'] = 'Λιμάνι';
+$lang['Multicraft.edit_row.ip_col'] = 'Διεύθυνση IP';
+$lang['Multicraft.edit_row.daemon_col'] = 'Αναγνωριστικό δαίμονα';
+$lang['Multicraft.edit_row.remove_ip'] = 'Αφαιρέστε το';
+$lang['Multicraft.edit_row.ip_btn'] = 'Προσθήκη διεύθυνσης IP';
+$lang['Multicraft.edit_row.daemons_title'] = 'Αφιερωμένες διευθύνσεις IP';
+$lang['Multicraft.edit_row.add_btn'] = 'Επεξεργασία διακομιστή';
+$lang['Multicraft.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Multicraft.edit_row.box_title'] = 'Επεξεργασία διακομιστή Multicraft';
+$lang['Multicraft.add_row.in_use_col'] = 'Σε χρήση';
+$lang['Multicraft.add_row.port_col'] = 'Λιμάνι';
+$lang['Multicraft.add_row.ip_col'] = 'Διεύθυνση IP';
+$lang['Multicraft.add_row.daemon_col'] = 'Αναγνωριστικό δαίμονα';
+$lang['Multicraft.add_row.remove_ip'] = 'Αφαιρέστε το';
+$lang['Multicraft.add_row.ip_btn'] = 'Προσθήκη διεύθυνσης IP';
+$lang['Multicraft.add_row.daemons_title'] = 'Αφιερωμένες διευθύνσεις IP';
+$lang['Multicraft.add_row.add_btn'] = 'Προσθήκη διακομιστή';
+$lang['Multicraft.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Multicraft.add_row.box_title'] = 'Προσθήκη διακομιστή Multicraft';
+$lang['Multicraft.tab_client_players.heading_chat_tab'] = 'Ημερολόγια συνομιλίας';
+$lang['Multicraft.tab_client_players.heading_console_tab'] = 'Καταγραφές κονσόλας';
+$lang['Multicraft.tab_client_console.submit_clear_log'] = 'Εκκαθάριση αρχείου καταγραφής';
+$lang['Multicraft.tab_client_console.chat_entry'] = '[%1$s] %2$s: %3$s';
+$lang['Multicraft.tab_client_console.submit_send'] = 'Στείλτε';
+$lang['Multicraft.tab_client_console.field_command'] = 'Εντολή';
+$lang['Multicraft.tab_client_players.heading_console'] = 'Κονσόλα διακομιστή';
+$lang['Multicraft.tab_client_players.no_results'] = 'Δεν υπάρχουν συνδεδεμένοι παίκτες.';
+$lang['Multicraft.tab_client_players.action_kick'] = 'Kick Player';
+$lang['Multicraft.tab_client_players.heading_action'] = 'Δράση';
+$lang['Multicraft.tab_client_players.heading_player_name'] = 'Όνομα παίκτη';
+$lang['Multicraft.tab_client_players.heading_players'] = 'Συνδεδεμένοι παίκτες';
+$lang['Multicraft.tab_client_actions.status_unknown'] = 'Άγνωστος';
+$lang['Multicraft.tab_client_actions.status_suspended'] = 'Αναστολή';
+$lang['Multicraft.tab_client_actions.status_offline'] = 'Offline';
+$lang['Multicraft.tab_client_actions.status_online'] = 'Online, %1$s/%2$s παίκτες';
+$lang['Multicraft.tab_client_actions.submit_save'] = 'Αποθήκευση';
+$lang['Multicraft.tab_client_console.field_server_name'] = 'Όνομα διακομιστή';
+$lang['Multicraft.tab_client_actions.heading_server_name'] = 'Ορισμός ονόματος διακομιστή';
+$lang['Multicraft.tab_client_actions.unknown'] = 'Άγνωστος';
+$lang['Multicraft.tab_client_actions.action_server_name'] = 'Ορισμός ονόματος διακομιστή';
+$lang['Multicraft.tab_client_actions.action_set_nighttime'] = 'Ρύθμιση σε Νυχτερινή ώρα';
+$lang['Multicraft.tab_client_actions.action_set_daytime'] = 'Ρύθμιση σε ώρα ημέρας';
+$lang['Multicraft.tab_client_actions.action_start'] = 'Έναρξη';
+$lang['Multicraft.tab_client_actions.action_stop'] = 'Σταματήστε το';
+$lang['Multicraft.tab_client_actions.action_restart'] = 'Επανεκκίνηση';
+$lang['Multicraft.tab_client_actions.heading_server_actions'] = 'Ενέργειες διακομιστή';
+$lang['Multicraft.tab_client_actions.heading_server_status'] = 'Κατάσταση διακομιστή';
+$lang['Multicraft.tab_client_actions.address_port'] = 'Λιμάνι';
+$lang['Multicraft.tab_client_actions.address_ip'] = 'Διεύθυνση IP';
+$lang['Multicraft.tab_client_actions.heading_server_address'] = 'Διεύθυνση διακομιστή';
+$lang['Multicraft.tab_client_console'] = 'Κονσόλα';
+$lang['Multicraft.tab_client_players'] = 'Παίκτες';
+$lang['Multicraft.tab_client_actions'] = 'Δράσεις';
+$lang['Multicraft.service_info.option_login'] = 'Συνδεθείτε';
+$lang['Multicraft.service_info.options'] = 'Επιλογές';
+$lang['Multicraft.manage.module_rows_in_use_1'] = 'Ναι';
+$lang['Multicraft.manage.module_rows_in_use_0'] = 'Όχι';
+$lang['Multicraft.manage.module_rows_no_results'] = 'Δεν υπάρχουν διακομιστές.';
+$lang['Multicraft.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον διακομιστή;';
+$lang['Multicraft.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['Multicraft.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['Multicraft.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['Multicraft.manage.module_rows_heading.panel_api_url'] = 'URL API πίνακα';
+$lang['Multicraft.manage.module_rows_heading.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Multicraft.manage.module_rows_heading.username'] = 'Όνομα χρήστη';
+$lang['Multicraft.manage.module_rows_title'] = 'Servers';
+$lang['Multicraft.add_module_row'] = 'Προσθήκη διακομιστή';
+$lang['Multicraft.module_rows.name'] = 'Servers';
+$lang['Multicraft.module_row.name'] = 'Διακομιστής';
+$lang['Multicraft.name'] = 'Multicraft';
+$lang['Multicraft.!error.module_row.missing'] = 'Εμφανίστηκε ένα εσωτερικό σφάλμα. Η σειρά της μονάδας δεν είναι διαθέσιμη.';
+
