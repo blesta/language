c74ab27194e57c7e009a99e9745861ce26b7a45c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:19 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/orders.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Orders
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Orders.view.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['Orders.view.no_transactions'] = 'Non ci sono transazioni associate a questo ordine.';
+$lang['Orders.view.no_services'] = 'Non ci sono servizi associati a questo ordine.';
+$lang['Orders.view.no_invoices'] = 'Non ci sono fatture associate a questo ordine.';
+$lang['Orders.view.field_back_order_history'] = 'Torna alla cronologia degli ordini';
+$lang['Orders.view.option_manage'] = 'Gestire';
+$lang['Orders.view.option_view'] = 'Vista';
+$lang['Orders.view.heading_date'] = 'Data';
+$lang['Orders.view.heading_number'] = 'Numero';
+$lang['Orders.view.heading_status'] = 'Stato';
+$lang['Orders.view.heading_applied'] = 'Applicato';
+$lang['Orders.view.heading_type'] = 'Tipo';
+$lang['Orders.view.heading_options'] = 'Opzioni';
+$lang['Orders.view.heading_date_renews'] = 'Data Rinnovo';
+$lang['Orders.view.heading_date_created'] = 'Data di creazione';
+$lang['Orders.view.heading_term'] = 'Termine';
+$lang['Orders.view.heading_label'] = 'Etichetta';
+$lang['Orders.view.heading_package'] = 'Pacchetto';
+$lang['Orders.view.heading_date_billed'] = 'Data di fatturazione';
+$lang['Orders.view.heading_paid'] = 'Pagato';
+$lang['Orders.view.heading_amount'] = 'Importo';
+$lang['Orders.view.heading_invoice_number'] = 'Fattura #';
+$lang['Orders.view.heading_transactions'] = 'Transazioni';
+$lang['Orders.view.heading_services'] = 'Servizi';
+$lang['Orders.view.heading_invoices'] = 'Fatture';
+$lang['Orders.view.boxtitle_view_order'] = 'Visualizza l\'ordine';
+$lang['Orders.index.no_results'] = 'Non avete ordini.';
+$lang['Orders.index.option_cancel'] = 'Annullamento';
+$lang['Orders.index.option_view'] = 'Vista';
+$lang['Orders.index.heading_options'] = 'Opzioni';
+$lang['Orders.index.heading_date_ordered'] = 'Data dell\'ordine';
+$lang['Orders.index.heading_paid'] = 'Pagato';
+$lang['Orders.index.heading_total'] = 'Totale';
+$lang['Orders.index.heading_invoice_number'] = 'Fattura #';
+$lang['Orders.index.heading_status'] = 'Stato';
+$lang['Orders.index.heading_order_number'] = 'Ordine #';
+$lang['Orders.index.boxtitle_orders'] = 'Storia dell\'ordine';
+$lang['Orders.!success.order_canceled'] = 'L\'ordine è stato annullato con successo.';
+
