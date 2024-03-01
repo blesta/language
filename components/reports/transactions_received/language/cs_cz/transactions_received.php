a53ff0d9d600a0a5bdc019773cc4aaff58b456df
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:47 -0800

    Updated cs_cz language files

---
+++ components/reports/transactions_received/language/cs_cz/transactions_received.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Transactions Received
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['TransactionsReceived.heading.date_added'] = 'Datum přidání';
+$lang['TransactionsReceived.heading.status'] = 'Stav';
+$lang['TransactionsReceived.heading.currency'] = 'Měna';
+$lang['TransactionsReceived.heading.amount'] = 'Výše platby';
+$lang['TransactionsReceived.heading.income_type'] = 'Typ transakce';
+$lang['TransactionsReceived.heading.type_name'] = 'Typ platby';
+$lang['TransactionsReceived.heading.gateway_name'] = 'Platební brána';
+$lang['TransactionsReceived.heading.client_id_code'] = 'Klient #';
+$lang['TransactionsReceived.heading.reference_id'] = 'Referenční ID';
+$lang['TransactionsReceived.heading.transaction_id'] = 'ID transakce';
+$lang['TransactionsReceived.heading.id'] = 'Transakce #';
+$lang['TransactionsReceived.options.field_payment_type'] = 'Typ platby';
+$lang['TransactionsReceived.options.field_status'] = 'Stav';
+$lang['TransactionsReceived.options.field_gateway'] = 'Brána';
+$lang['TransactionsReceived.options.field_end_date'] = 'Datum ukončení';
+$lang['TransactionsReceived.options.field_start_date'] = 'Datum zahájení';
+$lang['TransactionsReceived.option.any'] = 'Jakýkoli';
+$lang['TransactionsReceived.name'] = 'Přijaté transakce';
+
