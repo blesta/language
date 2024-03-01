bd256b4f961e1e0364916ab1bdc0cfa1af12065e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:01 -0800

    Updated de_de language files

---
+++ components/gateways/merchant/stripe_gateway/language/de_de/stripe_gateway.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Stripe Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Stripe_gateway.charge_description'] = 'Gebühr für %1$s';
+$lang['Stripe_gateway.tooltip_stored'] = 'Aktivieren Sie dieses Kontrollkästchen, um die Kartendaten des Zahlungskontos bei Stripe und nicht in Blesta zu speichern.';
+$lang['Stripe_gateway.stored'] = 'Speichern von Karteninformationen außerhalb des Unternehmens';
+$lang['Stripe_gateway.tooltip_api'] = 'Ihr API-Geheimschlüssel ist entweder für den Live- oder den Testmodus bestimmt. Stellen Sie sicher, dass Sie den richtigen Schlüssel verwenden.';
+$lang['Stripe_gateway.api_key'] = 'API-Geheimschlüssel';
+$lang['Stripe_gateway.description'] = 'Dies ist das alte Stripe-Gateway, das Stripe Checkout verwendet. Es erfordert, dass Ihr Stripe-Konto auf "Zahlungen unsicher verarbeiten" aktualisiert wird. Wir empfehlen stattdessen die Verwendung des Stripe Payments-Gateways in Blesta';
+$lang['Stripe_gateway.name'] = 'Streifen';
+$lang['Stripe_gateway.!error.json_required'] = 'Die JSON-Erweiterung ist für dieses Gateway erforderlich.';
+$lang['Stripe_gateway.!error.api_key.empty'] = 'Bitte geben Sie einen API-Schlüssel ein.';
+$lang['Stripe_gateway.!error.auth'] = 'Das Gateway konnte sich nicht authentifizieren.';
+
