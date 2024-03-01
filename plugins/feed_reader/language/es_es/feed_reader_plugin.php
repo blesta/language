1b68f1d9705211c022e381fd0476243793439764
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:14 -0800

    Updated es_es language files

---
+++ plugins/feed_reader/language/es_es/feed_reader_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Feed Reader Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FeedReaderPlugin.!error.dom_required'] = 'La extensión dom es necesaria para este plugin.';
+$lang['FeedReaderPlugin.!error.libxml_required'] = 'La extensión libxml es necesaria para este plugin.';
+$lang['FeedReaderPlugin.description'] = 'Visualiza cualquier fuente RSS/Atom directamente en tu panel de control.';
+$lang['FeedReaderPlugin.name'] = 'Lector de feeds';
+
