6d9a06be53fd0c16c6cf248ebd7d851663bc6ca2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:51 -0800

    Updated it_it language files

---
+++ plugins/import_manager/components/migrators/blesta/2.5/language/it_it/blesta2_5.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Blesta2 5
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Blesta2_5.configuration.no_local_packages'] = 'È necessario creare i pacchetti prima di poterli mappare.';
+$lang['Blesta2_5.configuration.remote_package'] = 'Pacchetto remoto';
+$lang['Blesta2_5.configuration.local_package'] = 'Pacchetto locale';
+$lang['Blesta2_5.configuration.create_packages_false'] = 'Mappare manualmente i pacchetti';
+$lang['Blesta2_5.configuration.create_packages_true'] = 'Creare automaticamente i pacchetti necessari durante l\'importazione';
+$lang['Blesta2_5.settings.key.info'] = 'Inserire il valore della definizione AESKEY in /inc/config.php. Il valore deve essere di 32 caratteri.';
+$lang['Blesta2_5.settings.key'] = 'AESKEY';
+$lang['Blesta2_5.settings.pass'] = 'Password del database';
+$lang['Blesta2_5.settings.user'] = 'Utente del database';
+$lang['Blesta2_5.settings.database'] = 'Nome del database';
+$lang['Blesta2_5.settings.host'] = 'Host del database';
+$lang['Blesta2_5.!error.key.invalid'] = 'È necessario disporre di una AESKEY valida.';
+$lang['Blesta2_5.!error.pass.invalid'] = 'È richiesta la password del database.';
+$lang['Blesta2_5.!error.user.invalid'] = 'È necessario l\'utente del database.';
+$lang['Blesta2_5.!error.database.invalid'] = 'Il nome del database è obbligatorio.';
+$lang['Blesta2_5.!error.host.invalid'] = 'È necessario un host di database.';
+
