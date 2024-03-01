2d1f76bb91b97b9107bb750963bc069a430642bb
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:00 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/bitpay/language/cs_cz/bitpay.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Bitpay
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Bitpay.!error.payment.expired'] = 'Platnost transakce vypršela a nemohla být zpracována.';
+$lang['Bitpay.!error.payment.invalid'] = 'Transakce je neplatná a nemohla být zpracována.';
+$lang['Bitpay.!error.failed.response'] = 'Transakci nebylo možné zpracovat.';
+$lang['Bitpay.!error.token.valid'] = 'Token je neplatný nebo párovací kód nebyl autorizován v systému BitPay.';
+$lang['Bitpay.!error.transaction_speed.valid'] = 'Vyberte prosím platnou rychlost transakce.';
+$lang['Bitpay.buildprocess.submit'] = 'Platba pomocí Bitcoinu';
+$lang['Bitpay.disconnect.button'] = 'Odpojení od BitPay';
+$lang['Bitpay.connect.button'] = 'Spojte se s BitPay';
+$lang['Bitpay.test_mode_note'] = 'Pokud je zaškrtnuto, brána bude odesílat požadavky na platbu na testovací adresu URL. Změnou této hodnoty dojde k odpojení služby BitPay a vygenerování nového tokenu.';
+$lang['Bitpay.test_mode'] = 'Testovací režim';
+$lang['Bitpay.transaction_speed_note'] = 'Rychlost transakce určuje, jak rychle lze fakturu (v Bitpay) považovat za "potvrzenou".';
+$lang['Bitpay.pairing_code_note'] = 'Přejděte na ovládací panel BitPay do sekce "Platební nástroje > Tokeny API" a schválte vygenerovaný token pomocí tohoto párovacího kódu "%1$s", poté uložte nastavení brány kliknutím na "Aktualizovat nastavení".';
+$lang['Bitpay.transaction.speed.low'] = 'Nízká';
+$lang['Bitpay.transaction.speed.medium'] = 'Střední';
+$lang['Bitpay.transaction.speed.high'] = 'Vysoká';
+$lang['Bitpay.transaction_speed'] = 'Rychlost transakce';
+$lang['Bitpay.pairing_code'] = 'Párovací kód';
+$lang['Bitpay.description'] = 'Poskytovatel platebních služeb v bitcoinech a kryptoměnách';
+$lang['Bitpay.name'] = 'Bitpay';
+
