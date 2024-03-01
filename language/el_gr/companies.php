44eadba4c299ea379a7bbbc4d205a89cde8a525b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:11 -0800

    Updated el_gr language files

---
+++ language/el_gr/companies.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Companies
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Companies.!error.inv_proforma_start.number'] = 'Pro Forma Invoice Start Η τιμή έναρξης πρέπει να είναι αριθμός.';
+$lang['Companies.!error.inv_proforma_format.contains'] = 'Το Pro Forma Invoice Format πρέπει να περιέχει {num}.';
+$lang['Companies.!error.inv_proforma_format.format'] = 'Η μορφή Pro Forma Invoice Format δεν μπορεί να συγκρούεται με τη μορφή Draft Invoice Format.';
+$lang['Companies.!error.inv_pad_str.length'] = 'Invoice Padding Ο χαρακτήρας πρέπει να είναι ένας μόνο χαρακτήρας.';
+$lang['Companies.!error.inv_pad_size.number'] = 'Το Μέγεθος συμπλήρωσης τιμολογίου πρέπει να είναι αριθμός.';
+$lang['Companies.!error.inv_increment.number'] = 'Η τιμή αύξησης τιμολογίου πρέπει να είναι αριθμός.';
+$lang['Companies.!error.inv_start.number'] = 'Η τιμή έναρξης τιμολογίου πρέπει να είναι αριθμός.';
+$lang['Companies.!error.inv_draft_format.contains'] = 'Η μορφή του σχεδίου τιμολογίου πρέπει να περιέχει {num}.';
+$lang['Companies.!error.inv_draft_format.format'] = 'Η μορφή του σχεδίου τιμολογίου δεν μπορεί να συγκρούεται με τη μορφή του τιμολογίου.';
+$lang['Companies.!error.inv_format.contains'] = 'Το Invoice Format πρέπει να περιέχει {num}.';
+$lang['Companies.!error.inv_format.format'] = 'Η μορφή τιμολογίου δεν μπορεί να συγκρούεται με τη μορφή σχεδίου τιμολογίου.';
+$lang['Companies.!error.quota.allowed'] = 'Δεν υπάρχουν διαθέσιμες πρόσθετες άδειες εταιρειών. Παρακαλούμε συνδεθείτε στο λογαριασμό σας στο account.blesta.com ή επικοινωνήστε με τις πωλήσεις για να αγοράσετε πρόσθετες άδειες εταιρίας.';
+$lang['Companies.!error.private_key_passphrase.valid'] = 'Η κωδική φράση που καταχωρήσατε είναι άκυρη.';
+$lang['Companies.!error.fax.length'] = 'Το μήκος του φαξ δεν μπορεί να υπερβαίνει τους 64 χαρακτήρες.';
+$lang['Companies.!error.phone.length'] = 'Το μήκος του τηλεφώνου δεν μπορεί να υπερβαίνει τους 64 χαρακτήρες.';
+$lang['Companies.!error.hostname.unique'] = 'Αυτό το όνομα κεντρικού υπολογιστή έχει ήδη ληφθεί από άλλη εταιρεία.';
+$lang['Companies.!error.hostname.length'] = 'Το μήκος του ονόματος κεντρικού υπολογιστή δεν μπορεί να υπερβαίνει τους 255 χαρακτήρες.';
+$lang['Companies.!error.hostname.valid'] = 'Παρακαλώ εισάγετε ένα έγκυρο όνομα κεντρικού υπολογιστή.';
+$lang['Companies.!error.name.length'] = 'Το μήκος του ονόματος δεν μπορεί να υπερβαίνει τους 64 χαρακτήρες.';
+$lang['Companies.!error.name.empty'] = 'Παρακαλώ εισάγετε ένα όνομα.';
+$lang['Companies.!error.company_id.exists'] = 'Μη έγκυρο αναγνωριστικό εταιρείας.';
+$lang['Companies.!error.company_id.in_use'] = 'Αυτή η εταιρεία χρησιμοποιείται επί του παρόντος και δεν μπορεί να διαγραφεί.';
+
