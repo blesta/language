3fbcca5aa34bbffae0a52beba119f62bf228b09c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:01 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/transition_quotations.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Transition Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.transition_quotations.dead_success'] = 'Prošlá nabídka #%1$s od klienta #%2$s je nyní mrtvá.';
+$lang['Automation.task.transition_quotations.expiration_success'] = 'Platnost čekající nabídky #%1$s od klienta #%2$s vypršela.';
+$lang['Automation.task.transition_quotations.none'] = 'Pro přechod nejsou k dispozici žádné nabídky, jejichž platnost vypršela.';
+$lang['Automation.task.transition_quotations.completed'] = 'Úkol přechodu na citace byl dokončen.';
+$lang['Automation.task.transition_quotations.attempt'] = 'Pokus o přechod citací, kterým vypršela platnost.';
+
