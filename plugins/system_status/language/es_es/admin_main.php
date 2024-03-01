f1b51dfeff989bf6b6da46cb0045c536dd08f607
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:19 -0800

    Updated es_es language files

---
+++ plugins/system_status/language/es_es/admin_main.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.index.health_excellent'] = 'El sistema goza de buena salud y parece funcionar con normalidad.';
+$lang['AdminMain.index.health_good'] = 'En general, el sistema goza de buena salud, pero hay algunos elementos que pueden requerir su atención.';
+$lang['AdminMain.index.health_fair'] = 'Hay algunos problemas que afectan a la capacidad del sistema para funcionar con normalidad.';
+$lang['AdminMain.index.health_poor'] = 'Hay problemas graves que afectan a la capacidad del sistema para funcionar con normalidad. Deben resolverse lo antes posible.';
+$lang['AdminMain.index.boxtitle_feed'] = 'Estado del sistema';
+$lang['AdminMain.index.cron_serious'] = 'Cron nunca ha funcionado.';
+$lang['AdminMain.index.cron_minor'] = 'Cron no se ha ejecutado en las últimas 24 horas.';
+$lang['AdminMain.index.cron_configure'] = '¿Configurar?';
+$lang['AdminMain.index.cron_task_stalled_minor'] = 'Hay una o varias tareas cron que llevan ejecutándose más de %1$s minutos.';
+$lang['AdminMain.index.cron_task_stalled_automation'] = 'Ver tareas automatizadas';
+$lang['AdminMain.index.trial_minor'] = 'Su licencia de prueba caduca el %1$s.';
+$lang['AdminMain.index.trial_buy'] = '¿Comprar ahora?';
+$lang['AdminMain.index.invoices_minor'] = 'Las facturas no se han creado automáticamente a través de cron en las últimas 24 horas.';
+$lang['AdminMain.index.invoices_configure'] = '¿Configurar?';
+$lang['AdminMain.index.backup_minor'] = 'No se han realizado copias de seguridad en los últimos 7 días.';
+$lang['AdminMain.index.docroot_minor'] = 'La ruta raíz del documento detectada no coincide con la configuración del Directorio Web Raíz.';
+$lang['AdminMain.index.docroot_setting'] = '¿Actualización?';
+$lang['AdminMain.index.system_dir_writable_minor'] = 'No se puede escribir en un directorio del sistema.';
+$lang['AdminMain.index.system_dir_writable_setting'] = '¿Actualización?';
+$lang['AdminMain.index.log_files_owner_minor'] = 'Hay algunos archivos de registro que no pertenecen al mismo usuario que el servidor web.  Esto suele deberse a que el cron se ejecuta con un usuario distinto al del servidor web.';
+$lang['AdminMain.index.error_reporting'] = 'errorReporting o System.debug están activados en /config/blesta.php. A menos que esté solucionando activamente un problema, deben estar desactivados. errorReporting debe ser 0, System.debug debe ser false.';
+$lang['AdminMain.index.updates_forever'] = 'Su apoyo y actualizaciones son buenos para siempre.';
+$lang['AdminMain.index.updates_minor'] = 'Su soporte y actualizaciones son buenos hasta %1$s.';
+$lang['AdminMain.index.updates_serious'] = 'Su soporte y actualizaciones expiraron el %1$s.';
+$lang['AdminMain.index.updates_buy'] = '¿Añadir asistencia y actualizaciones?';
+$lang['AdminMain.index.db_version_serious'] = 'La versión de la base de datos no coincide con la versión del sistema.';
+$lang['AdminMain.index.db_version_upgrade'] = '¿Actualización?';
+$lang['AdminMain.index.php_version_serious'] = 'La versión de PHP no cumple los requisitos mínimos del sistema.';
+$lang['AdminMain.index.php_version_requirements'] = 'Ver requisitos mínimos';
+$lang['AdminMain.index.sql_mysql_version_serious'] = 'La versión de MySQL no cumple los requisitos mínimos del sistema.';
+$lang['AdminMain.index.sql_mysql_version_requirements'] = 'Ver requisitos mínimos';
+$lang['AdminMain.index.sql_mariadb_version_serious'] = 'La versión de MariaDB no cumple los requisitos mínimos del sistema.';
+$lang['AdminMain.index.sql_mariadb_version_requirements'] = 'Ver requisitos mínimos';
+
