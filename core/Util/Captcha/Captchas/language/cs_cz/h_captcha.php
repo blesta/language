a0d7c64325032500b466a270fdb2df507f76f7a9
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:05 -0800

    Updated cs_cz language files

---
+++ core/Util/Captcha/Captchas/language/cs_cz/h_captcha.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * H Captcha
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['HCaptcha.name'] = 'hCaptcha';
+$lang['HCaptcha.options.field_hcaptcha_secret_key'] = 'Tajný klíč hCaptcha';
+$lang['HCaptcha.options.field_hcaptcha_site_key'] = 'Klíč k webu hCaptcha';
+
