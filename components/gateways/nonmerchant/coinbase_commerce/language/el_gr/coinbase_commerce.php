9f6114373aecbf499b441ed0d625487e3c07c321
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:27 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/coinbase_commerce/language/el_gr/coinbase_commerce.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Coinbase Commerce
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CoinbaseCommerce.buildprocess.submit'] = 'Πληρώστε με την Coinbase';
+$lang['CoinbaseCommerce.webhook_note'] = 'Πριν ξεκινήσετε να χρησιμοποιείτε αυτή την πύλη, πρέπει να ρυθμίσετε το ακόλουθο url ως Webhook στο λογαριασμό σας Coinbase Commerce και να ορίσετε το Webhook Secret.';
+$lang['CoinbaseCommerce.webhook'] = 'Coinbase Commerce Webhook';
+$lang['CoinbaseCommerce.meta.webhook_secret'] = 'Μυστικό Webhook';
+$lang['CoinbaseCommerce.meta.api_key'] = 'Κλειδί API';
+$lang['CoinbaseCommerce.!error.webhook_secret.valid'] = 'Webhook Secret άκυρο.';
+$lang['CoinbaseCommerce.!error.api_key.valid'] = 'Κλειδί API άκυρο.';
+$lang['CoinbaseCommerce.description'] = 'Το Coinbase Commerce σας επιτρέπει να ενσωματώσετε πληρωμές σε κρυπτονόμισμα στην επιχείρησή σας και να αφήνετε τους πελάτες να σας πληρώνουν απευθείας από τον υπολογιστή ή την κινητή συσκευή τους χρησιμοποιώντας την αλυσίδα μπλοκ.';
+$lang['CoinbaseCommerce.name'] = 'Coinbase Commerce';
+
