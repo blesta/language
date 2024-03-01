f1fcda82c71b40345cdb8ceb054c6d7b48eef084
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:34 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/permissions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Permissions
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Permissions.!error.group_id.exists'] = 'Le groupe de permission indiqué n\'existe pas.';
+$lang['Permissions.!error.name.empty'] = 'L\'autorisation doit avoir un nom.';
+$lang['Permissions.!error.alias.empty'] = 'L\'autorisation doit avoir un alias.';
+$lang['Permissions.!error.action.empty'] = 'L\'autorisation doit être assortie d\'une action.';
+$lang['Permissions.!error.plugin_id.exists'] = 'Le plugin spécifié pour cette autorisation n\'existe pas.';
+$lang['Permissions.!error.name.group_empty'] = 'Le groupe de permissions doit avoir un nom.';
+$lang['Permissions.!error.level.group_empty'] = 'Le niveau doit être soit "personnel", soit "client".';
+$lang['Permissions.!error.alias.group_empty'] = 'Le groupe de permissions doit avoir un alias.';
+$lang['Permissions.!error.plugin_id.group_exists'] = 'Le plugin spécifié pour ce groupe de permission n\'existe pas.';
+
