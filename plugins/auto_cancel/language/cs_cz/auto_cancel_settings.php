3f8102dda58555552fb6ed3e4f71705b65efb9e2
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:24 -0800

    Updated cs_cz language files

---
+++ plugins/auto_cancel/language/cs_cz/auto_cancel_settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Auto Cancel Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AutoCancelSettings.!error.cancel_days.greater'] = 'Dny zrušení služeb po pozastavení musí být větší nebo rovny dnům zrušení plánu po pozastavení.';
+$lang['AutoCancelSettings.!error.cancel_days.valid'] = 'Zrušení služeb několik dní po pozastavení není platné.';
+$lang['AutoCancelSettings.!error.schedule_days.valid'] = 'Plán zrušení dnů po pozastavení není platný.';
+
