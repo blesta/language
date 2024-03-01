9cb2d638867ac025b2a9510255c59e1afe17be98
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:06 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/coingate/language/de_de/coingate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Coingate
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Coingate.!error.failed.response'] = 'Die Transaktion konnte nicht verarbeitet werden.';
+$lang['Coingate.!error.payment.expired'] = 'Die Transaktion ist abgelaufen und konnte nicht bearbeitet werden.';
+$lang['Coingate.!error.payment.canceled'] = 'Die Transaktion wurde storniert und konnte nicht bearbeitet werden.';
+$lang['Coingate.!error.payment.invalid'] = 'Die Transaktion ist ungültig und konnte nicht bearbeitet werden.';
+$lang['Coingate.!error.api_secret.empty'] = 'API Secret darf nicht leer sein';
+$lang['Coingate.!error.api_key.empty'] = 'API-Schlüssel darf nicht leer sein';
+$lang['Coingate.!error.app_id.empty'] = 'Die APP-ID darf nicht leer sein.';
+$lang['Coingate.receive_currency.eur'] = 'Euro €';
+$lang['Coingate.receive_currency.btc'] = 'Bitcoin ฿';
+$lang['Coingate.receive_currency.usd'] = 'US-Dollars $';
+$lang['Coingate.buildprocess.submit'] = 'CoinGate.de';
+$lang['Coingate.environment_note'] = 'Aktivieren Sie "Sandbox", um auf sandbox.coingate.com zu testen. Bitte beachten Sie, dass Sie für den Sandbox-Modus separate API-Anmeldedaten auf sandbox.coingate.com generieren müssen. API-Zugangsdaten, die auf coingate.com generiert wurden, funktionieren im Sandbox-Modus nicht.';
+$lang['Coingate.receive_currency_note'] = 'Währung, die Sie bei einer Auszahlung bei CoinGate erhalten möchten.
+										   Bitte beachten Sie, dass Sie bei der Wahl von EUR oder USD aufgefordert werden, Ihr Geschäft zu verifizieren
+										   bevor Sie eine Auszahlung bei CoinGate vornehmen.';
+$lang['Coingate.receive_currency'] = 'Auszahlungswährung';
+$lang['Coingate.environment.live'] = 'live';
+$lang['Coingate.environment.sandbox'] = 'Sandkasten';
+$lang['Coingate.coingate_environment'] = 'CoinGate-Umgebung';
+$lang['Coingate.api_secret'] = 'API-Geheimnis';
+$lang['Coingate.api_key'] = 'API-Schlüssel';
+$lang['Coingate.app_id'] = 'APP-ID';
+$lang['Coingate.description'] = 'Bezahlen mit Bitcoin oder Altcoins über CoinGate.com';
+$lang['Coingate.name'] = 'CoinGate.de';
+
