cfe2ddb3cb94a1c66a9f7fa3dc038a9df4eba502
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:19 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_company_automation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Company Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyAutomation.getintervals.text_hours'] = 'hodiny';
+$lang['AdminCompanyAutomation.getintervals.text_hour'] = 'hodina';
+$lang['AdminCompanyAutomation.getintervals.text_minutes'] = 'minuty';
+$lang['AdminCompanyAutomation.getintervals.text_minute'] = 'minuta';
+$lang['AdminCompanyAutomation.index.confirm_clear_task'] = 'Jste si jisti, že chcete vymazat tento zámek úlohy cron? Pokud se tato úloha neustále zasekává, může jít o závažnější problém. Zkuste spustit cron ručně nebo zkontrolujte, zda protokol cronu neobsahuje chyby, a zjistěte příčinu.';
+$lang['AdminCompanyAutomation.index.option_clear_task'] = 'Vymazání zámku úlohy';
+$lang['AdminCompanyAutomation.index.text_cron_never_ran'] = 'Cron se nikdy nespustil.';
+$lang['AdminCompanyAutomation.index.text_cron_last_ran'] = 'Cron byl naposledy spuštěn dne %1$s.';
+$lang['AdminCompanyAutomation.index.no_cron_lastran'] = 'Nikdy';
+$lang['AdminCompanyAutomation.index.no_results'] = 'Neexistují žádné úlohy cron.';
+$lang['AdminCompanyAutomation.index.text_task_lastran'] = 'Naposledy spuštěná úloha:';
+$lang['AdminCompanyAutomation.index.text_starttime'] = 'Čas zahájení';
+$lang['AdminCompanyAutomation.index.text_interval'] = 'Interval';
+$lang['AdminCompanyAutomation.index.field_enabled'] = 'Povoleno';
+$lang['AdminCompanyAutomation.index.field_automationsubmit'] = 'Aktualizace nastavení';
+$lang['AdminCompanyAutomation.index.boxtitle_automation'] = 'Automatizace';
+$lang['AdminCompanyAutomation.index.page_title'] = 'Nastavení > Společnost > Automatizace';
+$lang['AdminCompanyAutomation.!success.task_cleared'] = 'Úloha cron byla vymazána.';
+$lang['AdminCompanyAutomation.!success.automation_updated'] = 'Nastavení automatizace bylo úspěšně aktualizováno!';
+$lang['AdminCompanyAutomation.!notice.cron_task_interval'] = 'Automatizační úlohy jsou nastaveny tak, aby se spouštěly každou minutu, ale nezdá se, že by se váš cron spouštěl každou minutu. Aktualizujte cron tak, aby se spouštěl každou minutu, a využijte tak kratší intervaly.';
+
