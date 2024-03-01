533a9bced8b71b9f468095f59d711d1a2c8af975
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:42 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/order_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Order Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderSettings.!error.embed_code.parse'] = 'Embed code parse error: %1$s';
+$lang['OrderSettings.getantifraud.fraudlabspro'] = 'FraudLabs Pro';
+$lang['OrderSettings.getantifraud.maxmind'] = 'Legado MaxMind';
+$lang['OrderSettings.getantifraud.maxmind_v2'] = 'MaxMind v2';
+
