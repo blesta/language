14033a0c44a19bbb2b388fc8650a24529a2d3e69
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:54 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/card_expiration_reminders.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Card Expiration Reminders
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.card_expiration_reminders.success'] = 'Erfolgreiche Zustellung der Ablauferinnerung für den Kontakt %1$s %2$s vom Kunden #%3$s.';
+$lang['Automation.task.card_expiration_reminders.failed'] = 'Die Ablauferinnerung für den Kontakt %1$s %2$s vom Kunden #%3$s konnte nicht gesendet werden.';
+$lang['Automation.task.card_expiration_reminders.completed'] = 'Die Aufgabe der Kartenablauferinnerung ist abgeschlossen.';
+$lang['Automation.task.card_expiration_reminders.attempt'] = 'Es wird versucht, Erinnerungen an den Ablauf der Gültigkeit der Karte zu senden.';
+
