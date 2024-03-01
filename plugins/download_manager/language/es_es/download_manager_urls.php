edeeaf9d2f68148162fd1bbf8290a0967a2cf98e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:04 -0800

    Updated es_es language files

---
+++ plugins/download_manager/language/es_es/download_manager_urls.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Download Manager Urls
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerUrls.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['DownloadManagerUrls.!error.url.exists'] = 'Esta URL ya existe.';
+$lang['DownloadManagerUrls.!error.url.match'] = 'Esta URL no coincide con la extensión del archivo.';
+$lang['DownloadManagerUrls.!error.url.format'] = 'El formato URL no es válido, sólo puede tener letras, números y guiones.';
+$lang['DownloadManagerUrls.!error.category_id.exists'] = 'La categoría seleccionada no existe.';
+$lang['DownloadManagerUrls.!error.file_id.exists'] = 'El fichero seleccionado no existe.';
+
