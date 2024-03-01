9d4fedd9c795e8e944db11fb46baefb52d9c06bd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:29 -0800

    Updated el_gr language files

---
+++ language/el_gr/package_option_groups.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Package Option Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptionGroups.!error.hidden.valid'] = 'Η απόκρυψη αυτής της ομάδας επιλογών πρέπει να οριστεί σε 0 ή 1.';
+$lang['PackageOptionGroups.!error.hide_options.valid'] = 'Το αν πρέπει να αποκρύπτονται οι επιλογές πρέπει να οριστεί σε 0 ή 1.';
+$lang['PackageOptionGroups.!error.group_id.exists'] = 'Μη έγκυρο αναγνωριστικό ομάδας επιλογών πακέτου.';
+$lang['PackageOptionGroups.!error.name.length'] = 'Το όνομα της ομάδας δεν μπορεί να υπερβαίνει τους 128 χαρακτήρες σε μήκος.';
+$lang['PackageOptionGroups.!error.name.empty'] = 'Παρακαλούμε καθορίστε ένα όνομα ομάδας.';
+$lang['PackageOptionGroups.!error.company_id.exists'] = 'Παρακαλούμε καθορίστε ένα έγκυρο εταιρικό αναγνωριστικό.';
+
