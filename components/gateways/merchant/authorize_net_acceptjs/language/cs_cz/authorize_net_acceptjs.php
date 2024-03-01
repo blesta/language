f3f761a146038064d299d602d296853bcb4dc89e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:53 -0800

    Updated cs_cz language files

---
+++ components/gateways/merchant/authorize_net_acceptjs/language/cs_cz/authorize_net_acceptjs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Authorize Net Acceptjs
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AuthorizeNetAcceptjs.charge_description'] = 'Poplatek za %1$s';
+$lang['AuthorizeNetAcceptjs.charge_description_default'] = 'Poplatek za určitou částku';
+$lang['AuthorizeNetAcceptjs.sandbox_note'] = 'Povolením této možnosti se transakce odešlou do prostředí Authorize.Net Sandbox. Tuto možnost povolte pouze v případě, že testujete pomocí účtu Authorize.Net Sandbox.';
+$lang['AuthorizeNetAcceptjs.sandbox'] = 'Sandbox';
+$lang['AuthorizeNetAcceptjs.transaction_key'] = 'Transakční klíč';
+$lang['AuthorizeNetAcceptjs.login_id'] = 'ID přihlášení';
+$lang['AuthorizeNetAcceptjs.field_expiration'] = 'Datum vypršení platnosti';
+$lang['AuthorizeNetAcceptjs.field_security'] = 'Bezpečnostní kód';
+$lang['AuthorizeNetAcceptjs.field_number'] = 'Číslo';
+$lang['AuthorizeNetAcceptjs.description'] = 'Odesílejte zabezpečené platební údaje přímo do služby Authorize.net. Accept.js zachytí platební údaje a odešle je přímo společnosti Authorize.net.';
+$lang['AuthorizeNetAcceptjs.name'] = 'Authorize.Net Accept.js';
+$lang['AuthorizeNetAcceptjs.!error.sandbox.valid'] = 'Sandbox musí být nastaven na "true", pokud je zadán.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.valid'] = 'Nelze se připojit k rozhraní API Authorize.net pomocí zadaného přihlašovacího ID.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.empty'] = 'Zadejte prosím přihlašovací ID.';
+$lang['AuthorizeNetAcceptjs.!error.transaction_key.empty'] = 'Zadejte prosím transakční klíč.';
+$lang['AuthorizeNetAcceptjs.!error.auth'] = 'Bránu se nepodařilo ověřit.';
+
