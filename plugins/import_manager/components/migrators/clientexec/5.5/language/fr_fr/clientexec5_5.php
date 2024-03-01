686d90bfe85863a333b02144285f67f9a65c7c89
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:13 -0800

    Updated fr_fr language files

---
+++ plugins/import_manager/components/migrators/clientexec/5.5/language/fr_fr/clientexec5_5.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Clientexec5 5
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Clientexec5_5.!error.host.invalid'] = 'L\'hôte de la base de données est requis.';
+$lang['Clientexec5_5.!error.database.invalid'] = 'Le nom de la base de données est obligatoire.';
+$lang['Clientexec5_5.!error.user.invalid'] = 'L\'utilisateur de la base de données est requis.';
+$lang['Clientexec5_5.!error.pass.invalid'] = 'Le mot de passe de la base de données est requis.';
+$lang['Clientexec5_5.!error.passphrase.invalid'] = 'La phrase d\'authentification CC est requise.';
+$lang['Clientexec5_5.!error.import'] = 'L\'importation s\'est terminée mais les erreurs suivantes se sont produites :';
+$lang['Clientexec5_5.settings.host'] = 'Hôte de la base de données';
+$lang['Clientexec5_5.settings.database'] = 'Nom de la base de données';
+$lang['Clientexec5_5.settings.user'] = 'Utilisateur de la base de données';
+$lang['Clientexec5_5.settings.pass'] = 'Mot de passe de la base de données';
+$lang['Clientexec5_5.settings.passphrase'] = 'Phrase de passe CC';
+$lang['Clientexec5_5.settings.enable_debug'] = 'Activer le débogage';
+$lang['Clientexec5_5.configuration.create_packages_true'] = 'Création automatique de tous les paquets nécessaires lors de l\'importation';
+$lang['Clientexec5_5.configuration.create_packages_false'] = 'Cartographier manuellement les paquets';
+$lang['Clientexec5_5.configuration.local_package'] = 'Paquet local';
+$lang['Clientexec5_5.configuration.remote_package'] = 'Paquet de télécommande';
+$lang['Clientexec5_5.configuration.no_local_packages'] = 'Vous devez créer des paquets avant de pouvoir les cartographier.';
+
