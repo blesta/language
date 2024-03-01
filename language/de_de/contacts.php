904a3099fceaff3e274a2a0ea1e33a3c249de579
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:53 -0800

    Updated de_de language files

---
+++ language/de_de/contacts.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Contacts
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Contacts.getPermissionOptions.client_quotations'] = 'Kostenvoranschläge';
+$lang['Contacts.getPermissionOptions._credits'] = 'Kredite';
+$lang['Contacts.getPermissionOptions._invoice_delivery'] = 'Lieferung von Rechnungen';
+$lang['Contacts.getPermissionOptions._managed'] = 'Verwaltete Konten';
+$lang['Contacts.getPermissionOptions.client_emails'] = 'E-Mail-Verlauf';
+$lang['Contacts.getPermissionOptions.client_accounts'] = 'Zahlungskonten';
+$lang['Contacts.getPermissionOptions.client_contacts'] = 'Kontakte';
+$lang['Contacts.getPermissionOptions.client_transactions'] = 'Transaktionen';
+$lang['Contacts.getPermissionOptions.client_services'] = 'Dienstleistungen';
+$lang['Contacts.getPermissionOptions.client_invoices'] = 'Rechnungen';
+$lang['Contacts.getnumberlocations.mobile'] = 'Mobil';
+$lang['Contacts.getnumberlocations.work'] = 'Arbeit';
+$lang['Contacts.getnumberlocations.home'] = 'Startseite';
+$lang['Contacts.getnumbertypes.fax'] = 'Fax';
+$lang['Contacts.getnumbertypes.phone'] = 'Telefon';
+$lang['Contacts.getcontacttypes.other'] = 'Andere';
+$lang['Contacts.getcontacttypes.billing'] = 'Rechnungsstellung';
+$lang['Contacts.getcontacttypes.primary'] = 'Primäre Seite';
+$lang['Contacts.!error.contact_type_id.exists'] = 'Ungültige Kontaktart-ID.';
+$lang['Contacts.!error.is_lang.length'] = 'Die Länge von is_lang darf 1 Zeichen nicht überschreiten.';
+$lang['Contacts.!error.is_lang.format'] = 'is_lang muss eine Zahl sein.';
+$lang['Contacts.!error.name.empty'] = 'Bitte geben Sie einen Namen ein.';
+$lang['Contacts.!error.company_id.format'] = 'Ungültige Unternehmens-ID.';
+$lang['Contacts.!error.id.exists'] = 'Ungültige Kontaktnummer-ID.';
+$lang['Contacts.!error.location.format'] = 'Ungültiger Standort.';
+$lang['Contacts.!error.type.format'] = 'Ungültiger Typ.';
+$lang['Contacts.!error.number.empty'] = 'Bitte geben Sie eine Kontaktnummer ein.';
+$lang['Contacts.!error.zip.read_only'] = 'Die Postleitzahl ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Setzen Sie sich mit uns in Verbindung, um sie zu ändern.';
+$lang['Contacts.!error.address2.read_only'] = 'Die zweite Adresszeile ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Setzen Sie sich mit uns in Verbindung, um sie zu ändern.';
+$lang['Contacts.!error.address1.read_only'] = 'Die erste Adresszeile ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Setzen Sie sich mit uns in Verbindung, um sie zu ändern.';
+$lang['Contacts.!error.country.read_only'] = 'Das Land ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Kontaktieren Sie uns, um es zu ändern.';
+$lang['Contacts.!error.state.read_only'] = 'Staat/Provinz ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Bitte kontaktieren Sie uns, wenn Sie Änderungen vornehmen möchten.';
+$lang['Contacts.!error.city.read_only'] = 'Die Stadt ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Setzen Sie sich mit uns in Verbindung, um sie zu ändern.';
+$lang['Contacts.!error.email.read_only'] = 'Die Adresse ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Kontaktieren Sie uns, um sie zu ändern.';
+$lang['Contacts.!error.company.read_only'] = 'Das Unternehmen ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Kontaktieren Sie uns, um es zu ändern.';
+$lang['Contacts.!error.title.read_only'] = 'Der Titel ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Kontaktieren Sie uns, um ihn zu ändern.';
+$lang['Contacts.!error.last_name.read_only'] = 'Der Nachname ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Kontaktieren Sie uns, um ihn zu ändern.';
+$lang['Contacts.!error.first_name.read_only'] = 'Der Vorname ist ein schreibgeschütztes Feld und kann nicht aktualisiert werden. Kontaktieren Sie uns, um ihn zu ändern.';
+$lang['Contacts.!error.fax.required'] = 'Bitte geben Sie mindestens eine Faxnummer an.';
+$lang['Contacts.!error.phone.required'] = 'Bitte geben Sie mindestens eine Telefonnummer ein.';
+$lang['Contacts.!error.zip.empty'] = 'Bitte geben Sie eine Postleitzahl ein.';
+$lang['Contacts.!error.address2.empty'] = 'Bitte geben Sie die zweite Zeile Ihrer Adresse ein.';
+$lang['Contacts.!error.address1.empty'] = 'Bitte geben Sie die erste Zeile Ihrer Adresse ein.';
+$lang['Contacts.!error.contact_id.primary'] = 'Der primäre Kundenkontakt darf nicht gelöscht werden.';
+$lang['Contacts.!error.contact_id.exists'] = 'Ungültige Kontakt-ID.';
+$lang['Contacts.!error.country.empty'] = 'Bitte wählen Sie ein Land aus.';
+$lang['Contacts.!error.country.length'] = 'Die Länge des Landes darf 3 Zeichen nicht überschreiten.';
+$lang['Contacts.!error.state.empty'] = 'Bitte wählen Sie einen Staat/Provinz.';
+$lang['Contacts.!error.state.country_exists'] = 'Bitte wählen Sie das Land aus, das dem ausgewählten Staat entspricht.';
+$lang['Contacts.!error.state.length'] = 'Die Länge der Angabe darf 3 Zeichen nicht überschreiten.';
+$lang['Contacts.!error.city.empty'] = 'Bitte geben Sie eine Stadt ein.';
+$lang['Contacts.!error.email.unique'] = 'Die E-Mail-Adresse ist bereits vergeben.';
+$lang['Contacts.!error.email.format'] = 'Ungültige E-Mail Adresse.';
+$lang['Contacts.!error.company.empty'] = 'Bitte geben Sie ein Unternehmen ein.';
+$lang['Contacts.!error.company.length'] = 'Die Länge des Unternehmens darf 128 Zeichen nicht überschreiten.';
+$lang['Contacts.!error.title.empty'] = 'Bitte geben Sie einen Titel ein.';
+$lang['Contacts.!error.title.length'] = 'Die Länge des Titels darf 64 Zeichen nicht überschreiten.';
+$lang['Contacts.!error.last_name.empty'] = 'Bitte geben Sie einen Nachnamen ein.';
+$lang['Contacts.!error.first_name.empty'] = 'Bitte geben Sie einen Vornamen ein.';
+$lang['Contacts.!error.contact_type_id.format'] = 'Ungültige Kontaktart-ID.';
+$lang['Contacts.!error.contact_type.inv_address_to'] = 'Rechnungen sind so eingestellt, dass sie an diesen Kontakt adressiert werden und müssen vor der Aktualisierung dieses Kontakts geändert werden.';
+$lang['Contacts.!error.contact_type.format'] = 'Ungültige Kontaktart.';
+$lang['Contacts.!error.user_id.exists'] = 'Ungültige Benutzer-ID.';
+$lang['Contacts.!error.client_id.exists'] = 'Ungültige Client-ID.';
+
