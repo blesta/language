1f3c1be8c61d8fac6e9e0b563cfb8bd530b1f467
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:56 -0800

    Updated fr_fr language files

---
+++ plugins/domains/language/fr_fr/domains_plugin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Domains Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsPlugin.name'] = 'Gestionnaire de domaine';
+$lang['DomainsPlugin.description'] = 'Utilisé pour gérer les différents aspects de la gestion et de la vente de domaines. Ce plugin est actuellement en phase bêta.';
+$lang['DomainsPlugin.!error.module_id.exists'] = 'ID de module non valide.';
+$lang['DomainsPlugin.getCronTasks.domain_synchronization'] = 'Synchronisation des domaines';
+$lang['DomainsPlugin.getCronTasks.domain_synchronization_description'] = 'Synchroniser les services de domaine avec la date d\'expiration de leur module d\'enregistrement';
+$lang['DomainsPlugin.getCronTasks.domain_tld_synchronization'] = 'Synchronisation des domaines TLD';
+$lang['DomainsPlugin.getCronTasks.domain_tld_synchronization_description'] = 'Synchroniser les paquets avec la tarification des TLD de leur module d\'enregistrement';
+$lang['DomainsPlugin.getCronTasks.domain_term_change'] = 'Modifier le terme du domaine';
+$lang['DomainsPlugin.getCronTasks.domain_term_change_description'] = 'Remplacer les services d\'une durée supérieure à un an par des services d\'une durée annuelle';
+$lang['DomainsPlugin.getCronTasks.domain_renewal_reminders'] = 'Envoyer des rappels d\'expiration';
+$lang['DomainsPlugin.getCronTasks.domain_renewal_reminders_description'] = 'Envoyer des rappels par courrier électronique pour les domaines dont la date d\'expiration est proche';
+$lang['DomainsPlugin.nav_secondary_staff.domain_options'] = 'Options de domaine';
+$lang['DomainsPlugin.nav_secondary_staff.domains'] = 'Domaines';
+$lang['DomainsPlugin.nav_client.services'] = 'Services';
+$lang['DomainsPlugin.nav_client.domains'] = 'Domaines';
+$lang['DomainsPlugin.card_client.getDomainCount'] = 'Domaines';
+$lang['DomainsPlugin.permission.admin_domains'] = 'Domaines';
+$lang['DomainsPlugin.permission.admin_domains.browse'] = 'Domaines';
+$lang['DomainsPlugin.permission.admin_domains.tlds'] = 'Prix des TLD';
+$lang['DomainsPlugin.permission.admin_domains.registrars'] = 'Greffiers';
+$lang['DomainsPlugin.permission.admin_domains.whois'] = 'Whois';
+$lang['DomainsPlugin.permission.admin_domains.configuration'] = 'Configuration';
+$lang['DomainsPlugin.tld_package_group.name'] = 'Tarifs des TLDs';
+$lang['DomainsPlugin.tld_package_group.description'] = 'Un groupe de paquets pour cacher et gérer tous les paquets de prix des TLD';
+$lang['DomainsPlugin.email_forwarding.name'] = 'Transfert d\'e-mail';
+$lang['DomainsPlugin.email_forwarding.description'] = 'Transfert d\'e-mail';
+$lang['DomainsPlugin.dns_management.name'] = 'Gestion du DNS';
+$lang['DomainsPlugin.dns_management.description'] = 'Gestion du DNS';
+$lang['DomainsPlugin.id_protection.name'] = 'Protection de l\'identité';
+$lang['DomainsPlugin.id_protection.description'] = 'Protection de l\'identité';
+$lang['DomainsPlugin.enabled'] = 'Activé';
+$lang['DomainsPlugin.widget_staff_home.main'] = 'Domaines';
+$lang['DomainsPlugin.widget_client_home.main'] = 'Domaines';
+$lang['DomainsPlugin.upgrade.domains_dns_management_option_group'] = 'Gérer la zone DNS pour votre nom de domaine par le biais des serveurs de noms que nous fournissons. Créez et modifiez des enregistrements tels que A, CNAME, MX, TXT, etc. pour votre domaine.';
+$lang['DomainsPlugin.upgrade.domains_email_forwarding_option_group'] = 'Créez des adresses e-mail dans votre domaine qui sont redirigées vers une adresse e-mail externe.';
+$lang['DomainsPlugin.upgrade.domains_id_protection_option_group'] = 'Tous les noms de domaine doivent comporter des informations de contact valides, conformément aux règles de l\'ICANN. Cette option permet de masquer ces informations dans le whois public, afin de préserver la confidentialité de vos données.';
+
