edf050e6cfcf1c267ebb9a69750ee3ed4d3c1fad
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:43 -0800

    Updated es_es language files

---
+++ components/modules/apnscp/language/es_es/apnscp.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Apnscp
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Apnscp.name'] = 'ApisCP';
+$lang['Apnscp.description'] = 'Antes denominada APNSCP.';
+$lang['Apnscp.module_row.name'] = 'Servidor';
+$lang['Apnscp.module_row_plural.name'] = 'Servidores';
+$lang['Apnscp.module_group.name'] = 'Grupo de servidores';
+$lang['Apnscp.tab_stats'] = 'Estadísticas';
+$lang['Apnscp.tab_client_stats'] = 'Estadísticas';
+$lang['Apnscp.tab_client_actions'] = 'Acciones';
+$lang['Apnscp.add_module_row'] = 'Añadir servidor';
+$lang['Apnscp.add_module_group'] = 'Añadir grupo de servidores';
+$lang['Apnscp.manage.module_rows_title'] = 'Servidores';
+$lang['Apnscp.manage.module_groups_title'] = 'Grupos de servidores';
+$lang['Apnscp.manage.module_rows_heading.name'] = 'Etiqueta del servidor';
+$lang['Apnscp.manage.module_rows_heading.hostname'] = 'Nombre de host';
+$lang['Apnscp.manage.module_rows_heading.accounts'] = 'Cuentas';
+$lang['Apnscp.manage.module_rows_heading.options'] = 'Opciones';
+$lang['Apnscp.manage.module_groups_heading.name'] = 'Nombre del grupo';
+$lang['Apnscp.manage.module_groups_heading.servers'] = 'Recuento de servidores';
+$lang['Apnscp.manage.module_groups_heading.options'] = 'Opciones';
+$lang['Apnscp.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Apnscp.manage.module_rows.edit'] = 'Editar';
+$lang['Apnscp.manage.module_groups.edit'] = 'Editar';
+$lang['Apnscp.manage.module_rows.delete'] = 'Borrar';
+$lang['Apnscp.manage.module_groups.delete'] = 'Borrar';
+$lang['Apnscp.manage.module_rows.confirm_delete'] = '¿Está seguro de que desea eliminar este servidor?';
+$lang['Apnscp.manage.module_groups.confirm_delete'] = '¿Está seguro de que desea eliminar este grupo de servidores?';
+$lang['Apnscp.manage.module_rows_no_results'] = 'No hay servidores.';
+$lang['Apnscp.manage.module_groups_no_results'] = 'No hay grupos de servidores.';
+$lang['Apnscp.order_options.first'] = 'Primer servidor no lleno';
+$lang['Apnscp.order_options.roundrobin'] = 'Distribución uniforme entre servidores';
+$lang['Apnscp.add_row.box_title'] = 'Añadir servidor ApisCP';
+$lang['Apnscp.add_row.basic_title'] = 'Configuración básica';
+$lang['Apnscp.add_row.name_servers_title'] = 'Servidores de nombres';
+$lang['Apnscp.add_row.notes_title'] = 'Notas';
+$lang['Apnscp.add_row.name_server_btn'] = 'Añadir servidor de nombres adicional';
+$lang['Apnscp.add_row.name_server_col'] = 'Servidor de nombres';
+$lang['Apnscp.add_row.name_server_host_col'] = 'Nombre de host';
+$lang['Apnscp.add_row.name_server'] = 'Servidor de nombres %1$s';
+$lang['Apnscp.add_row.remove_name_server'] = 'Eliminar';
+$lang['Apnscp.add_row.add_btn'] = 'Añadir servidor';
+$lang['Apnscp.edit_row.box_title'] = 'Editar servidor ApisCP';
+$lang['Apnscp.edit_row.basic_title'] = 'Configuración básica';
+$lang['Apnscp.edit_row.name_servers_title'] = 'Servidores de nombres';
+$lang['Apnscp.edit_row.notes_title'] = 'Notas';
+$lang['Apnscp.edit_row.name_server_btn'] = 'Añadir servidor de nombres adicional';
+$lang['Apnscp.edit_row.name_server_col'] = 'Servidor de nombres';
+$lang['Apnscp.edit_row.name_server_host_col'] = 'Nombre de host';
+$lang['Apnscp.edit_row.name_server'] = 'Servidor de nombres %1$s';
+$lang['Apnscp.edit_row.remove_name_server'] = 'Eliminar';
+$lang['Apnscp.edit_row.add_btn'] = 'Editar servidor';
+$lang['Apnscp.row_meta.server_name'] = 'Etiqueta del servidor';
+$lang['Apnscp.row_meta.host_name'] = 'Nombre de host';
+$lang['Apnscp.row_meta.port'] = 'Puerto';
+$lang['Apnscp.row_meta.default_port'] = '2083';
+$lang['Apnscp.row_meta.api_key'] = 'Clave API';
+$lang['Apnscp.row_meta.use_ssl'] = 'Utilice SSL al conectarse a la API (recomendado)';
+$lang['Apnscp.row_meta.account_limit'] = 'Límite de la cuenta';
+$lang['Apnscp.package_fields.package'] = 'Paquete ApisCP';
+$lang['Apnscp.service_field.domain'] = 'Dominio';
+$lang['Apnscp.service_field.username'] = 'Nombre de usuario';
+$lang['Apnscp.service_field.password'] = 'Contraseña';
+$lang['Apnscp.tab_stats.info_title'] = 'Información';
+$lang['Apnscp.tab_stats.info_heading.field'] = 'Campo';
+$lang['Apnscp.tab_stats.info_heading.value'] = 'Valor';
+$lang['Apnscp.tab_stats.info.disk'] = 'Cuota de disco';
+$lang['Apnscp.tab_stats.info.bandwidth'] = 'Cuota de ancho de banda';
+$lang['Apnscp.tab_client_actions.change_password'] = 'Cambiar contraseña';
+$lang['Apnscp.tab_client_actions.field_apnscp_password'] = 'Contraseña';
+$lang['Apnscp.tab_client_actions.field_password_submit'] = 'Actualizar contraseña';
+$lang['Apnscp.tab_client_stats.info_title'] = 'Información';
+$lang['Apnscp.tab_client_stats.bandwidth_title'] = 'Uso de ancho de banda (mes hasta la fecha)';
+$lang['Apnscp.tab_client_stats.disk_title'] = 'Uso del disco';
+$lang['Apnscp.tab_client_stats.usage'] = '(%1$s MB/%2$s MB)';
+$lang['Apnscp.tab_client_stats.usage_unlimited'] = '(%1$s MB/∞)';
+$lang['Apnscp.service_info.username'] = 'Nombre de usuario';
+$lang['Apnscp.service_info.password'] = 'Contraseña';
+$lang['Apnscp.service_info.server'] = 'Servidor';
+$lang['Apnscp.service_info.options'] = 'Opciones';
+$lang['Apnscp.service_info.option_login'] = 'Conectarse';
+$lang['Apnscp.service_field.tooltip.username'] = 'Puede dejar el nombre de usuario en blanco para generarlo automáticamente.';
+$lang['Apnscp.service_field.tooltip.password'] = 'Puede dejar la contraseña en blanco para que se genere automáticamente.';
+$lang['Apnscp.!error.server_name_valid'] = 'Debe introducir una Etiqueta de servidor.';
+$lang['Apnscp.!error.host_name_valid'] = 'El nombre de host no es válido.';
+$lang['Apnscp.!error.port_format'] = 'El puerto debe ser un número.';
+$lang['Apnscp.!error.api_key_valid'] = 'La clave API parece no ser válida.';
+$lang['Apnscp.!error.api_key_valid_connection'] = 'No se ha podido establecer la conexión con el servidor. Compruebe que el nombre de host, la clave API y el puerto son correctos.';
+$lang['Apnscp.!error.account_limit_valid'] = 'El Límite de cuenta debe dejarse en blanco (para cuentas ilimitadas) o establecerse en algún valor entero.';
+$lang['Apnscp.!error.name_servers_valid'] = 'Uno o varios de los servidores de nombres introducidos no son válidos.';
+$lang['Apnscp.!error.name_servers_count'] = 'Debe definir al menos 2 servidores de nombres.';
+$lang['Apnscp.!error.meta[package].empty'] = 'Se requiere un paquete ApisCP.';
+$lang['Apnscp.!error.api.internal'] = 'Se ha producido un error interno o el servidor no ha respondido a la solicitud.';
+$lang['Apnscp.!error.module_row.missing'] = 'Se ha producido un error interno. La fila del módulo no está disponible.';
+$lang['Apnscp.!error.apnscp_domain.format'] = 'Introduzca un nombre de dominio válido, por ejemplo, dominio.com.';
+$lang['Apnscp.!error.apnscp_domain.test'] = 'El nombre de dominio no puede empezar por \'test\'.';
+$lang['Apnscp.!error.apnscp_username.format'] = 'El nombre de usuario sólo puede contener letras y números y no puede empezar por un número.';
+$lang['Apnscp.!error.apnscp_username.test'] = 'El nombre de usuario no puede empezar por "test".';
+$lang['Apnscp.!error.apnscp_username.length'] = 'El nombre de usuario debe tener entre 1 y 16 caracteres.';
+$lang['Apnscp.!error.apnscp_password.valid'] = 'La contraseña debe tener al menos 8 caracteres.';
+$lang['Apnscp.!error.apnscp_password.matches'] = 'La contraseña y la contraseña de confirmación no coinciden.';
+
