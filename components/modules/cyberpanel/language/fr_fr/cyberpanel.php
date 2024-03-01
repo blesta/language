2a7f0d5590499bdb179362ac3ff180d490084f16
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:49 -0800

    Updated fr_fr language files

---
+++ components/modules/cyberpanel/language/fr_fr/cyberpanel.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Cyberpanel
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cyberpanel.name'] = 'CyberPanel';
+$lang['Cyberpanel.description'] = 'CyberPanel est un panneau de contrôle d\'hébergement de nouvelle génération alimenté par OpenLiteSpeed. Construit pour la vitesse, la sécurité et la fiabilité.';
+$lang['Cyberpanel.module_row'] = 'Serveur';
+$lang['Cyberpanel.module_row_plural'] = 'Serveurs';
+$lang['Cyberpanel.module_group'] = 'Groupe de serveurs';
+$lang['Cyberpanel.tab_client_actions'] = 'Actions';
+$lang['Cyberpanel.add_module_row'] = 'Ajouter un serveur';
+$lang['Cyberpanel.add_module_group'] = 'Ajouter un groupe de serveurs';
+$lang['Cyberpanel.manage.module_rows_title'] = 'Serveurs';
+$lang['Cyberpanel.manage.module_groups_title'] = 'Groupes de serveurs';
+$lang['Cyberpanel.manage.module_rows_heading.name'] = 'Label du serveur';
+$lang['Cyberpanel.manage.module_rows_heading.hostname'] = 'Nom d\'hôte';
+$lang['Cyberpanel.manage.module_rows_heading.options'] = 'Options';
+$lang['Cyberpanel.manage.module_groups_heading.name'] = 'Nom du groupe';
+$lang['Cyberpanel.manage.module_groups_heading.servers'] = 'Nombre de serveurs';
+$lang['Cyberpanel.manage.module_groups_heading.options'] = 'Options';
+$lang['Cyberpanel.manage.module_rows.edit'] = 'Editer';
+$lang['Cyberpanel.manage.module_groups.edit'] = 'Editer';
+$lang['Cyberpanel.manage.module_rows.delete'] = 'Supprimer';
+$lang['Cyberpanel.manage.module_groups.delete'] = 'Supprimer';
+$lang['Cyberpanel.manage.module_rows.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce serveur ?';
+$lang['Cyberpanel.manage.module_groups.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce groupe de serveurs ?';
+$lang['Cyberpanel.manage.module_rows_no_results'] = 'Il n\'y a pas de serveurs.';
+$lang['Cyberpanel.manage.module_groups_no_results'] = 'Il n\'y a pas de groupes de serveurs.';
+$lang['Cyberpanel.order_options.first'] = 'Premier serveur non complet';
+$lang['Cyberpanel.order_options.roundrobin'] = 'Répartir uniformément entre les serveurs';
+$lang['Cyberpanel.add_row.box_title'] = 'Ajouter un serveur CyberPanel';
+$lang['Cyberpanel.add_row.basic_title'] = 'Paramètres de base';
+$lang['Cyberpanel.add_row.name_servers_title'] = 'Serveurs de noms';
+$lang['Cyberpanel.add_row.notes_title'] = 'Notes';
+$lang['Cyberpanel.add_row.name_server_btn'] = 'Ajouter un serveur de noms supplémentaire';
+$lang['Cyberpanel.add_row.name_server_col'] = 'Serveur de noms';
+$lang['Cyberpanel.add_row.name_server_host_col'] = 'Nom d\'hôte';
+$lang['Cyberpanel.add_row.name_server'] = 'Serveur de noms %1$s';
+$lang['Cyberpanel.add_row.remove_name_server'] = 'Retirer';
+$lang['Cyberpanel.add_row.add_btn'] = 'Ajouter un serveur';
+$lang['Cyberpanel.edit_row.box_title'] = 'Modifier le serveur CyberPanel';
+$lang['Cyberpanel.edit_row.basic_title'] = 'Paramètres de base';
+$lang['Cyberpanel.edit_row.name_servers_title'] = 'Serveurs de noms';
+$lang['Cyberpanel.edit_row.notes_title'] = 'Notes';
+$lang['Cyberpanel.edit_row.name_server_btn'] = 'Ajouter un serveur de noms supplémentaire';
+$lang['Cyberpanel.edit_row.name_server_col'] = 'Serveur de noms';
+$lang['Cyberpanel.edit_row.name_server_host_col'] = 'Nom d\'hôte';
+$lang['Cyberpanel.edit_row.name_server'] = 'Serveur de noms %1$s';
+$lang['Cyberpanel.edit_row.remove_name_server'] = 'Retirer';
+$lang['Cyberpanel.edit_row.add_btn'] = 'Editer le serveur';
+$lang['Cyberpanel.row_meta.server_name'] = 'Label du serveur';
+$lang['Cyberpanel.row_meta.host_name'] = 'Nom d\'hôte';
+$lang['Cyberpanel.row_meta.admin_username'] = 'Nom d\'utilisateur de l\'administrateur';
+$lang['Cyberpanel.row_meta.admin_password'] = 'Mot de passe administrateur';
+$lang['Cyberpanel.row_meta.use_ssl'] = 'Utiliser SSL lors de la connexion à l\'API (recommandé)';
+$lang['Cyberpanel.package_fields.package'] = 'Nom du paquet';
+$lang['Cyberpanel.service_field.domain'] = 'Domaine';
+$lang['Cyberpanel.service_field.username'] = 'Nom d\'utilisateur';
+$lang['Cyberpanel.service_field.password'] = 'Mot de passe';
+$lang['Cyberpanel.tab_client_actions.change_password'] = 'Modifier le mot de passe';
+$lang['Cyberpanel.tab_client_actions.field_cyberpanel_password'] = 'Mot de passe';
+$lang['Cyberpanel.tab_client_actions.field_cyberpanel_confirm_password'] = 'Confirmer le mot de passe';
+$lang['Cyberpanel.tab_client_actions.field_password_submit'] = 'Mise à jour du mot de passe';
+$lang['Cyberpanel.service_info.username'] = 'Nom d\'utilisateur';
+$lang['Cyberpanel.service_info.password'] = 'Mot de passe';
+$lang['Cyberpanel.service_info.server'] = 'Serveur';
+$lang['Cyberpanel.service_info.options'] = 'Options';
+$lang['Cyberpanel.service_info.option_login'] = 'Se connecter';
+$lang['Cyberpanel.service_field.tooltip.username'] = 'Vous pouvez laisser le nom d\'utilisateur vide pour qu\'il soit généré automatiquement.';
+$lang['Cyberpanel.service_field.tooltip.password'] = 'Vous pouvez laisser le mot de passe en blanc pour qu\'il soit généré automatiquement.';
+$lang['Cyberpanel.!error.server_name_valid'] = 'Vous devez saisir une étiquette de serveur.';
+$lang['Cyberpanel.!error.host_name_valid'] = 'Le nom d\'hôte semble être invalide.';
+$lang['Cyberpanel.!error.remote_admin_username_valid'] = 'Le nom d\'utilisateur de l\'administrateur semble être invalide.';
+$lang['Cyberpanel.!error.remote_admin_password_valid'] = 'Le mot de passe administrateur semble être invalide.';
+$lang['Cyberpanel.!error.remote_admin_password_valid_connection'] = 'La connexion au serveur n\'a pas pu être établie. Veuillez vérifier que le nom d\'hôte et le mot de passe administrateur sont corrects.';
+$lang['Cyberpanel.!error.name_servers_valid'] = 'Un ou plusieurs des serveurs de noms saisis ne sont pas valides.';
+$lang['Cyberpanel.!error.name_servers_count'] = 'Vous devez définir au moins 2 serveurs de noms.';
+$lang['Cyberpanel.!error.meta[package].empty'] = 'Un pack CyberPanel est nécessaire.';
+$lang['Cyberpanel.!error.api.internal'] = 'Une erreur interne s\'est produite ou le serveur n\'a pas répondu à la demande.';
+$lang['Cyberpanel.!error.module_row.missing'] = 'Une erreur interne s\'est produite. La ligne du module n\'est pas disponible.';
+$lang['Cyberpanel.!error.cyberpanel_domain.format'] = 'Veuillez saisir un nom de domaine valide, par exemple domain.com.';
+$lang['Cyberpanel.!error.cyberpanel_domain.test'] = 'Le nom de domaine ne peut pas commencer par "test".';
+$lang['Cyberpanel.!error.cyberpanel_username.format'] = 'Le nom d\'utilisateur ne peut contenir que des lettres et des chiffres et ne peut pas commencer par un chiffre.';
+$lang['Cyberpanel.!error.cyberpanel_username.test'] = 'Le nom d\'utilisateur ne peut pas commencer par "test".';
+$lang['Cyberpanel.!error.cyberpanel_username.length'] = 'Le nom d\'utilisateur doit comporter entre 1 et 16 caractères.';
+$lang['Cyberpanel.!error.cyberpanel_password.valid'] = 'Le mot de passe doit comporter au moins 8 caractères.';
+$lang['Cyberpanel.!error.cyberpanel_password.matches'] = 'Le mot de passe et le mot de passe de confirmation ne correspondent pas.';
+
