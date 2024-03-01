11d6605f6191e66b17b28b8883b82f71fe5ba616
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:42 -0800

    Updated cs_cz language files

---
+++ plugins/extension_generator/language/cs_cz/extension_generator_controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Extension Generator Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorController.gettasktypes.interval'] = 'Interval';
+$lang['ExtensionGeneratorController.gettasktypes.time'] = 'Čas';
+$lang['ExtensionGeneratorController.gettablevels.client'] = 'Klient';
+$lang['ExtensionGeneratorController.gettablevels.staff'] = 'Zaměstnanci';
+$lang['ExtensionGeneratorController.getfieldtypes.checkbox'] = 'Zaškrtávací políčko';
+$lang['ExtensionGeneratorController.getfieldtypes.textarea'] = 'Textový prostor';
+$lang['ExtensionGeneratorController.getfieldtypes.text'] = 'Text';
+$lang['ExtensionGeneratorController.getnodes.confirm'] = 'Potvrzení';
+$lang['ExtensionGeneratorController.getnodes.additional_features'] = 'Další funkce';
+$lang['ExtensionGeneratorController.getnodes.nonmerchant_fields'] = 'Konfigurační pole';
+$lang['ExtensionGeneratorController.getnodes.merchant_features'] = 'Podporované funkce';
+$lang['ExtensionGeneratorController.getnodes.merchant_fields'] = 'Konfigurační pole';
+$lang['ExtensionGeneratorController.getnodes.module_fields'] = 'Pole modulu';
+$lang['ExtensionGeneratorController.getnodes.plugin_integrations'] = 'Základní integrace';
+$lang['ExtensionGeneratorController.getnodes.plugin_database'] = 'Informace o databázi';
+$lang['ExtensionGeneratorController.getnodes.basic_info'] = 'Základní informace';
+$lang['ExtensionGeneratorController.getnodes.general_settings'] = 'Obecná nastavení';
+
