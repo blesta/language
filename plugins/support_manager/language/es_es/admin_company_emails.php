49d9038bd7df9fb20cef558b2938e9e31fab551d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:06 -0800

    Updated es_es language files

---
+++ plugins/support_manager/language/es_es/admin_company_emails.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_name'] = 'Ticket de personal actualizado';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_desc'] = 'Aviso enviado a un miembro del personal cuando se actualiza un ticket.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_mobile_name'] = 'Ticket del personal actualizado (móvil)';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_mobile_desc'] = 'Aviso móvil enviado a un miembro del personal cuando se actualiza un ticket.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_assigned_name'] = 'Billete de personal asignado';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_assigned_desc'] = 'Aviso enviado a un miembro del personal cuando se le ha asignado un ticket.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_received_name'] = 'Billete recibido';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_received_desc'] = 'Aviso enviado a un usuario cuando se recibe un ticket.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_updated_name'] = 'Billete actualizado';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_updated_desc'] = 'Aviso enviado cuando se actualiza un ticket.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_bounce_name'] = 'Boleto rebotado';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_bounce_desc'] = 'Aviso enviado cuando no se ha podido abrir un ticket por correo electrónico.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_reminder_name'] = 'Recordatorio de entradas';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_reminder_desc'] = 'Aviso enviado cuando un ticket no ha recibido respuesta durante un periodo de tiempo.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_reminder_name'] = 'Recordatorio de entradas para el personal';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_reminder_desc'] = 'Aviso enviado cuando un ticket no ha recibido respuesta durante un periodo de tiempo.';
+
