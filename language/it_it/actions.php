4431569a24739e8c2f7811b2b1eabab7dc8bba75
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:24 -0800

    Updated it_it language files

---
+++ language/it_it/actions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Actions
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Actions.getLocationDescriptions.action_staff_client'] = 'Crea un link nella sezione "Azioni del conto" della pagina del profilo del cliente del personale.';
+$lang['Actions.getLocationDescriptions.widget_staff_billing'] = 'Visualizza un widget nella pagina di fatturazione del personale.';
+$lang['Actions.getLocationDescriptions.widget_staff_client'] = 'Visualizza un widget nella pagina del profilo del cliente del personale.';
+$lang['Actions.getLocationDescriptions.widget_staff_home'] = 'Visualizza un widget sul cruscotto del personale.';
+$lang['Actions.getLocationDescriptions.widget_client_home'] = '	Visualizza un widget sulla dashboard del client.';
+$lang['Actions.getLocationDescriptions.nav_public'] = 'Rende disponibile un collegamento alla navigazione dell\'interfaccia pubblica.';
+$lang['Actions.getLocationDescriptions.nav_client'] = 'Rende disponibile un collegamento alla navigazione dell\'interfaccia del client.';
+$lang['Actions.getLocationDescriptions.nav_staff'] = 'Rende disponibile un collegamento alla navigazione dell\'interfaccia di amministrazione.';
+$lang['Actions.getLocations.action_staff_client'] = 'Collegamento "Azioni sul conto" del cliente';
+$lang['Actions.getLocations.widget_staff_billing'] = 'Widget per la fatturazione del personale';
+$lang['Actions.getLocations.widget_staff_client'] = 'Widget per il profilo dei clienti dello staff';
+$lang['Actions.getLocations.widget_staff_home'] = 'Widget del cruscotto del personale';
+$lang['Actions.getLocations.widget_client_home'] = 'Widget del cruscotto del cliente';
+$lang['Actions.getLocations.nav_public'] = 'Menu di navigazione pubblico';
+$lang['Actions.getLocations.nav_client'] = 'Menu di navigazione del cliente';
+$lang['Actions.getLocations.nav_staff'] = 'Menu di navigazione del personale';
+$lang['Actions.!error.enabled.valid'] = 'L\'opzione abilitata deve essere impostata su 1 o 0.';
+$lang['Actions.!error.editable.valid'] = 'L\'opzione modificabile deve essere impostata a 1 o a 0.';
+$lang['Actions.!error.company_id.valid'] = 'ID azienda non valido';
+$lang['Actions.!error.plugin_id.valid'] = 'Fornire un ID plugin valido.';
+$lang['Actions.!error.name.action_empty'] = 'Inserire il nome di un\'azione.';
+$lang['Actions.!error.url.empty'] = 'Inserire un URL.';
+$lang['Actions.!error.location.unique'] = 'La sede e l\'URL devono essere unici per questa azienda.';
+$lang['Actions.!error.location.valid'] = 'Posizione valida non valida.';
+
