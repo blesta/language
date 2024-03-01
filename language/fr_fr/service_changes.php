46aeb846c0ae5e38140609f42565c5fa02065e7f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:37 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/service_changes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Service Changes
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ServiceChanges.!error.id.exists'] = 'ID de changement de service non valide.';
+$lang['ServiceChanges.!error.service_id.exists'] = 'ID de service non valide.';
+$lang['ServiceChanges.!error.invoice_id.exists'] = 'ID de la facture non valide.';
+$lang['ServiceChanges.!error.invoice_id.unique'] = 'L\'identifiant de la facture est déjà défini pour une modification de service et ne peut pas l\'être à nouveau.';
+$lang['ServiceChanges.!error.status.valid'] = 'Veuillez sélectionner un statut valide.';
+$lang['ServiceChanges.status.pending'] = 'En attente';
+$lang['ServiceChanges.status.completed'] = 'Terminé';
+$lang['ServiceChanges.status.error'] = 'Erreur';
+$lang['ServiceChanges.status.canceled'] = 'Annulé';
+
