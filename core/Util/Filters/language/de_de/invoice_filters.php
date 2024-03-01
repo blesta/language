6d326123732753eb2505558192cd89e91611c566
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:10 -0800

    Updated de_de language files

---
+++ core/Util/Filters/language/de_de/invoice_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Invoice Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.invoice_filters.field_invoice_line'] = 'Posten Inhalt';
+$lang['Util.filters.invoice_filters.field_currency'] = 'Währung';
+$lang['Util.filters.invoice_filters.field_invoice_number'] = 'Invoice #';
+$lang['Util.filters.invoice_filters.any'] = 'Jede';
+
