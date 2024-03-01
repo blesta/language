425acaab0f3d7b65eb04a736067713f4a6394e81
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:47 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_company_feeds.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Admin Company Feeds
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyFeeds.!success.datafeeds_updated'] = 'Les paramètres des flux de données ont été mis à jour avec succès.';
+$lang['AdminCompanyFeeds.!success.settings_updated'] = 'Les paramètres du flux ont été mis à jour avec succès.';
+$lang['AdminCompanyFeeds.index.page_title'] = 'Réglages > Entreprise > Flux de données';
+$lang['AdminCompanyFeeds.index.boxtitle_data_feeds'] = 'Flux de données';
+$lang['AdminCompanyFeeds.index.data_feeds_description'] = 'Les flux de données permettent d\'intégrer le contenu de Blesta sur des sites Web externes, comme les noms de colis, les prix, etc.';
+$lang['AdminCompanyFeeds.index.field_enable_data_feeds'] = 'Activer les flux de données';
+$lang['AdminCompanyFeeds.index.field_submit'] = 'Mise à jour des paramètres';
+$lang['AdminCompanyFeeds.settings.page_title'] = 'Réglages > Entreprise > Flux de données > Réglages des flux';
+$lang['AdminCompanyFeeds.settings.boxtitle_feed_settings'] = 'Réglages de l\'alimentation';
+$lang['AdminCompanyFeeds.settings.header_endpoints'] = 'Points finaux';
+$lang['AdminCompanyFeeds.settings.field_submit'] = 'Mise à jour des paramètres';
+
