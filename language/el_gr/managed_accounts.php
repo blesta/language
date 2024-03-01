e15280187c864a6e583929d5a965e2aa72f2eb1b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:22 -0800

    Updated el_gr language files

---
+++ language/el_gr/managed_accounts.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Managed Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ManagedAccounts.!error.permissions.empty'] = 'Πρέπει να εκχωρήσετε τουλάχιστον ένα δικαίωμα στον διαχειριστή.';
+$lang['ManagedAccounts.!error.permissions.manage'] = 'Δεν είστε εξουσιοδοτημένοι να διαχειρίζεστε αυτόν τον λογαριασμό.';
+$lang['ManagedAccounts.!error.contact_id.exists'] = 'Μη έγκυρο αναγνωριστικό επαφής.';
+$lang['ManagedAccounts.!error.invitation_id.exists'] = 'Μη έγκυρο αναγνωριστικό πρόσκλησης.';
+$lang['ManagedAccounts.!error.email.invitation'] = 'Μια πρόσκληση έχει ήδη σταλεί σε αυτόν τον λογαριασμό.';
+$lang['ManagedAccounts.!error.client_id.exists'] = 'Μη έγκυρο αναγνωριστικό πελάτη.';
+$lang['ManagedAccounts.!error.client.exists'] = 'Δεν υπάρχει λογαριασμός που να έχει αντιστοιχιστεί στη διεύθυνση ηλεκτρονικού ταχυδρομείου που δηλώσατε.';
+
