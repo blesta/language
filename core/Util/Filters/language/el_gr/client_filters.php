c245bc9f868053a362c4039f50db3bf686b4c0c6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:30 -0800

    Updated el_gr language files

---
+++ core/Util/Filters/language/el_gr/client_filters.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Client Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.client_filters.any'] = 'Οποιοδήποτε';
+$lang['Util.filters.client_filters.field_contact_name'] = 'Όνομα';
+$lang['Util.filters.client_filters.field_contact_email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['Util.filters.client_filters.field_contact_company'] = 'Εταιρεία';
+$lang['Util.filters.client_filters.field_contact_country'] = 'Χώρα';
+$lang['Util.filters.client_filters.field_client_group_id'] = 'Ομάδα πελατών';
+$lang['Util.filters.client_filters.field_invoice_method'] = 'Μέθοδος τιμολόγησης';
+$lang['Util.filters.client_filters.field_last_seen'] = 'Τελευταία εμφάνιση';
+$lang['Util.filters.client_filters.field_last_seen_start_date'] = 'Ημερομηνία έναρξης';
+$lang['Util.filters.client_filters.field_last_seen_end_date'] = 'Ημερομηνία λήξης';
+
