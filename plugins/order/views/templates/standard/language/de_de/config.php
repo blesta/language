f56da14ce2550490fc68f0f7a43805ec47739506
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:26 -0800

    Updated de_de language files

---
+++ plugins/order/views/templates/standard/language/de_de/config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Config
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Config.index.term_years'] = '%1$s Jahre';
+$lang['Config.index.term_year'] = '%1$s Jahr';
+$lang['Config.index.term_months'] = '%1$s Monate';
+$lang['Config.index.term_month'] = '%1$s Monat';
+$lang['Config.index.term_weeks'] = '%1$s Wochen';
+$lang['Config.index.term_week'] = '%1$s Woche';
+$lang['Config.index.term_days'] = '%1$s Tage';
+$lang['Config.index.term_day'] = '%1$s Tag';
+$lang['Config.index.continue_btn'] = 'Weiter';
+$lang['Config.index.header_addons'] = 'Verfügbare Add-Ons';
+$lang['Config.index.header_module'] = 'Konfigurierbare Optionen';
+$lang['Config.index.header_term'] = 'Laufzeit und Preis';
+$lang['Config.index.text_free_domain'] = 'Eine Preisoption von %1$s oder mehr qualifiziert Sie für eine kostenlose Domain im nächsten Schritt.';
+$lang['Config.index.text_free_domain_next_step'] = 'Wählen Sie im nächsten Schritt einen qualifizierten Artikel mit einer Mindestlaufzeit von %1$s, um diese Domain kostenlos zu erhalten.';
+$lang['Config.index.text_free_domain_next_step_limited'] = 'Wählen Sie im nächsten Schritt einen qualifizierten Artikel mit einer Mindestlaufzeit von %1$s aus, um diese Domain für eine Laufzeit von %2$s kostenlos zu erhalten.';
+$lang['Config.index.header_configure_service'] = 'Konfigurieren Sie %1$s';
+$lang['Config.index.header_configuration'] = 'Konfiguration';
+
