656fc81038282f94685cce97c7e38abf4c2120e0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:25 -0800

    Updated it_it language files

---
+++ language/it_it/users.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Users
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Users.!error.recovery_email.format'] = 'Indirizzo e-mail di recupero non valido.';
+$lang['Users.!error.clients.exist'] = 'L\'utente non può essere eliminato perché esiste almeno un client assegnato all\'utente.';
+$lang['Users.!error.username.company'] = 'Non si è autorizzati ad accedere a questa posizione.';
+$lang['Users.!error.username.attempts'] = 'Sono stati rilevati troppi tentativi di accesso falliti.';
+$lang['Users.!error.user_id.exists'] = 'ID utente non valido.';
+$lang['Users.!error.otp.auth'] = 'La password unica inserita non è valida. La lunghezza massima è di 16 caratteri';
+$lang['Users.!error.username.auth'] = 'Non è stata trovata alcuna corrispondenza per quella combinazione di utente/password.';
+$lang['Users.!error.two_factor_key.format'] = 'Chiave a due fattori non valida.';
+$lang['Users.!error.two_factor_mode.format'] = 'Modalità a due fattori non valida.';
+$lang['Users.!error.new_password.matches'] = 'Le password non corrispondono.';
+$lang['Users.!error.new_password.format'] = 'Inserire una password di almeno 6 caratteri.';
+$lang['Users.!error.current_password.matches'] = 'Password non valida.';
+$lang['Users.!error.username.unique'] = 'Quel nome utente è già stato preso.';
+$lang['Users.!error.username.empty'] = 'Inserire un nome utente.';
+
