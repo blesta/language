6118f757d1dbe16a08d4b8f0da173b59733a5c55
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:21 -0800

    Updated fr_fr language files

---
+++ core/Automation/Tasks/Task/language/fr_fr/deliver_reports.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Deliver Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.deliver_reports.attempt'] = 'Tentative de remise de rapports.';
+$lang['Automation.task.deliver_reports.completed'] = 'La tâche de livraison des rapports est terminée.';
+$lang['Automation.task.deliver_reports.aging_invoices.attempt'] = 'Tentative d\'envoi du rapport Factures vieillissantes.';
+$lang['Automation.task.deliver_reports.aging_invoices.attachment_fail'] = 'Impossible de générer la pièce jointe du rapport Factures vieillissantes.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_error'] = 'L\'envoi de l\'e-mail relatif au vieillissement des factures a échoué.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_success'] = 'L\'email de rapport sur les factures vieillissantes a été envoyé.';
+$lang['Automation.task.deliver_reports.invoice_creation.attempt'] = 'Tentative d\'envoi du rapport sur la création de factures.';
+$lang['Automation.task.deliver_reports.invoice_creation.attachment_fail'] = 'Impossible de générer la pièce jointe du rapport sur la création de la facture.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_error'] = 'L\'email du rapport de création de facture n\'a pas pu être envoyé.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_success'] = 'L\'email de rapport de création de facture a été envoyé.';
+$lang['Automation.task.deliver_reports.tax_liability.attempt'] = 'Tentative d\'envoi de la déclaration d\'assujettissement à l\'impôt.';
+$lang['Automation.task.deliver_reports.tax_liability.attachment_fail'] = 'Impossible de générer la pièce jointe du rapport sur l\'assujettissement à l\'impôt.';
+$lang['Automation.task.deliver_reports.tax_liability.email_error'] = 'Le courriel relatif à l\'assujettissement à l\'impôt n\'a pas été envoyé.';
+$lang['Automation.task.deliver_reports.tax_liability.email_success'] = 'L\'e-mail relatif au rapport sur l\'assujettissement à l\'impôt a été envoyé.';
+
