71976274864a442c73abb865a612ea99265209db
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:47 -0800

    Updated de_de language files

---
+++ components/reports/invoice_creation/language/de_de/invoice_creation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Invoice Creation
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['InvoiceCreation.heading.date_closed'] = 'Datum abgeschlossen';
+$lang['InvoiceCreation.heading.date_due'] = 'Datum Fällig';
+$lang['InvoiceCreation.heading.date_billed'] = 'Datum der Abrechnung';
+$lang['InvoiceCreation.heading.status'] = 'Status';
+$lang['InvoiceCreation.heading.currency'] = 'Währung';
+$lang['InvoiceCreation.heading.paid'] = 'Bezahlt';
+$lang['InvoiceCreation.heading.total'] = 'Insgesamt';
+$lang['InvoiceCreation.heading.subtotal'] = 'Zwischensumme';
+$lang['InvoiceCreation.heading.client_id_code'] = 'Kunde #';
+$lang['InvoiceCreation.heading.id_code'] = 'Invoice #';
+$lang['InvoiceCreation.option.any'] = 'Jede';
+$lang['InvoiceCreation.options.field_status'] = 'Status';
+$lang['InvoiceCreation.options.field_end_date'] = 'Enddatum';
+$lang['InvoiceCreation.options.field_start_date'] = 'Datum des Beginns';
+$lang['InvoiceCreation.name'] = 'Erstellung von Rechnungen';
+
