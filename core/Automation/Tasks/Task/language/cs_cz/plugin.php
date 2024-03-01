8ec6786f1a80f5a2320f16f529585a655fe4aa1e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:58 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.plugin.attempt'] = 'Pokus o zásuvný modul cron pro %1$s %2$s.';
+$lang['Automation.task.plugin.completed'] = 'Dokončen plugin cron pro %1$s %2$s.';
+
