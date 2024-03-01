5ba18b02e1bc73a83e090426d68c0cbff0a85a54
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:26 -0800

    Updated de_de language files

---
+++ plugins/billing_overview/language/de_de/admin_main.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.graph_line_name.credits'] = 'Kredite';
+$lang['AdminMain.graph_line_name.other'] = 'Andere';
+$lang['AdminMain.graph_line_name.ach'] = 'ACH';
+$lang['AdminMain.graph_line_name.credit'] = 'Kreditkarte';
+$lang['AdminMain.graph_line_name.total'] = 'Insgesamt';
+$lang['AdminMain.graph_name.graph_invoiced'] = 'In Rechnung gestellter Betrag';
+$lang['AdminMain.graph_name.graph_revenue_year'] = 'Einnahmen (bisheriges Jahr)';
+$lang['AdminMain.graph_name.graph_revenue'] = 'Einnahmen (Spanne)';
+$lang['AdminMain.date_range.days'] = 'Tage';
+$lang['AdminMain.settings.submit_cancel'] = 'Zurück zur Übersicht';
+$lang['AdminMain.settings.submit_settings'] = 'Einstellungen aktualisieren';
+$lang['AdminMain.settings.date_range'] = 'Diagramm Datumsbereich';
+$lang['AdminMain.settings.show_legend'] = 'Legende in Diagrammen anzeigen';
+$lang['AdminMain.settings.text_graph_settings'] = 'Verwalten Sie die Diagrammeinstellungen für die Diagramme, die in der Abrechnungsübersicht erscheinen sollen.';
+$lang['AdminMain.settings.graph_invoiced'] = 'In Rechnung gestellter Betrag Grafik';
+$lang['AdminMain.settings.graph_revenue_year'] = 'Einnahmegrafik (bisheriges Jahr)';
+$lang['AdminMain.settings.graph_revenue'] = 'Einnahmegrafik (Bereich)';
+$lang['AdminMain.settings.text_graphs'] = 'Prüfen Sie, welche Diagramme in der Rechnungsübersicht erscheinen sollen.';
+$lang['AdminMain.settings.services_canceled_today'] = 'Heute gestrichene Dienste';
+$lang['AdminMain.settings.services_added_today'] = 'Heute hinzugefügte Dienste';
+$lang['AdminMain.settings.services_active'] = 'Aktive Dienste';
+$lang['AdminMain.settings.scheduled_cancelation'] = 'Geplante Stornierungen';
+$lang['AdminMain.settings.balance_overdue'] = 'Überfälliger Saldo';
+$lang['AdminMain.settings.balance_outstanding'] = 'Ausstehender Saldo';
+$lang['AdminMain.settings.invoiced_month_proforma'] = 'In diesem Monat in Rechnung gestellt (Proforma)';
+$lang['AdminMain.settings.invoiced_today_proforma'] = 'Heute in Rechnung gestellt (Proforma)';
+$lang['AdminMain.settings.invoiced_month'] = 'In diesem Monat in Rechnung gestellt';
+$lang['AdminMain.settings.invoiced_today'] = 'Heute in Rechnung gestellt';
+$lang['AdminMain.settings.credits_year'] = 'Kredite in diesem Jahr';
+$lang['AdminMain.settings.credits_month'] = 'Kredite diesen Monat';
+$lang['AdminMain.settings.credits_today'] = 'Kredite heute';
+$lang['AdminMain.settings.revenue_year'] = 'Einnahmen in diesem Jahr';
+$lang['AdminMain.settings.revenue_month'] = 'Einnahmen in diesem Monat';
+$lang['AdminMain.settings.revenue_today'] = 'Einnahmen heute';
+$lang['AdminMain.settings.heading_graph_settings'] = 'Graph-Einstellungen';
+$lang['AdminMain.settings.heading_graphs'] = 'Schaubilder';
+$lang['AdminMain.settings.heading_statistics'] = 'Statistik';
+$lang['AdminMain.settings.text_options'] = 'Kreuzen Sie die Optionen an, die in der Rechnungsübersicht erscheinen sollen.';
+$lang['AdminMain.index.statistic.services_canceled_today'] = 'Heute gestrichene Dienste';
+$lang['AdminMain.index.statistic.services_added_today'] = 'Heute hinzugefügte Dienste';
+$lang['AdminMain.index.statistic.services_active'] = 'Aktive Dienste';
+$lang['AdminMain.index.statistic.scheduled_cancelation'] = 'Geplante Stornierungen';
+$lang['AdminMain.index.statistic.balance_overdue'] = 'Überfälliger Saldo';
+$lang['AdminMain.index.statistic.balance_outstanding'] = 'Ausstehender Saldo';
+$lang['AdminMain.index.statistic.invoiced_month_proforma'] = 'In diesem Monat in Rechnung gestellt (Proforma)';
+$lang['AdminMain.index.statistic.invoiced_today_proforma'] = 'Heute in Rechnung gestellt (Proforma)';
+$lang['AdminMain.index.statistic.invoiced_month'] = 'In diesem Monat in Rechnung gestellt';
+$lang['AdminMain.index.statistic.invoiced_today'] = 'Heute in Rechnung gestellt';
+$lang['AdminMain.index.statistic.credits_year'] = 'Kredite in diesem Jahr';
+$lang['AdminMain.index.statistic.credits_month'] = 'Kredite diesen Monat';
+$lang['AdminMain.index.statistic.credits_today'] = 'Kredite heute';
+$lang['AdminMain.index.statistic.revenue_year'] = 'Einnahmen in diesem Jahr';
+$lang['AdminMain.index.statistic.revenue_month'] = 'Einnahmen in diesem Monat';
+$lang['AdminMain.index.statistic.revenue_today'] = 'Einnahmen heute';
+$lang['AdminMain.index.boxtitle_overview'] = 'Rechnungsstellung im Überblick';
+$lang['AdminMain.!success.options_updated'] = 'Die Optionen von "Rechnung auf einen Blick" wurden erfolgreich aktualisiert!';
+
