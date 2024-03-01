cdd43d4c3561c631b66343ab4857c924c48fec5a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:26 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/admin_company_emails.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Order.received_name'] = 'Ordre reçu';
+$lang['AdminCompanyEmails.templates.Order.received_desc'] = 'Avis envoyé lorsqu\'une commande est reçue.';
+$lang['AdminCompanyEmails.templates.Order.received_mobile_name'] = 'Ordre reçu (Mobile)';
+$lang['AdminCompanyEmails.templates.Order.received_mobile_desc'] = 'Avis envoyé lors de la réception d\'une commande, destiné aux appareils mobiles.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_name'] = 'Demande de remboursement d\'un affilié';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_desc'] = 'Avis envoyé au personnel lorsqu\'une demande de paiement est soumise.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_received_name'] = 'Paiement de l\'affilié reçu';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_received_desc'] = 'Avis envoyé à un client lorsque sa demande de paiement est soumise.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_monthly_report_name'] = 'Rapport d\'affiliation (mensuel)';
+$lang['AdminCompanyEmails.templates.Order.affiliate_monthly_report_desc'] = 'Rapport listant les inscriptions et les données associées pour le mois dernier.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_first_name'] = 'Jours de commande abandonnée (1er avis)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_first_desc'] = 'Premier avis envoyé à un client, après l\'abandon d\'une commande impayée.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_second_name'] = 'Jours de commande abandonnée (2e avis)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_second_desc'] = 'Deuxième avis envoyé à un client, après l\'abandon d\'une commande impayée.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_third_name'] = 'Jours de commande abandonnée (3ème avis)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_third_desc'] = 'Troisième et dernier avis envoyé à un client, après l\'abandon d\'une commande impayée.';
+
