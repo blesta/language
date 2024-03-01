fe00fd51098447ca11ef059f316b15f38ec859b0
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:17 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/payouts.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Payouts
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payouts.add.request_payout'] = 'Auszahlung beantragen';
+$lang['Payouts.add.available_payout'] = 'Verfügbar für Auszahlung';
+$lang['Payouts.add.max_withdrawal_amount'] = 'Maximaler Auszahlungsbetrag';
+$lang['Payouts.add.min_withdrawal_amount'] = 'Mindestauszahlungsbetrag';
+$lang['Payouts.add.field_payment_method'] = 'Zahlungsmethode';
+$lang['Payouts.add.field_requested_currency'] = 'Gewünschte Währung';
+$lang['Payouts.add.field_requested_amount'] = 'Abhebungsbetrag';
+$lang['Payouts.add.title_payout_request'] = 'Auszahlungsantrag';
+$lang['Payouts.add.boxtitle_request_payout'] = 'Auszahlung beantragen';
+$lang['Payouts.index.heading_date_requested'] = 'Gewünschtes Datum';
+$lang['Payouts.index.heading_payment_method'] = 'Zahlungsmethode';
+$lang['Payouts.index.heading_requested_currency'] = 'Gewünschte Währung';
+$lang['Payouts.index.heading_requested_amount'] = 'Angeforderter Betrag';
+$lang['Payouts.index.heading_id'] = 'Auszahlungs-ID';
+$lang['Payouts.index.no_results'] = 'Derzeit gibt es keine Auszahlungen mit diesem Status.';
+$lang['Payouts.index.categorylink_request_payout'] = 'Auszahlung beantragen';
+$lang['Payouts.index.category_declined'] = 'Abgelehnt';
+$lang['Payouts.index.category_pending'] = 'Anhängig';
+$lang['Payouts.index.category_approved'] = 'Genehmigt';
+$lang['Payouts.index.boxtitle_payouts'] = 'Auszahlungen';
+$lang['Payouts.!error.minimum_payout_not_available'] = 'Die verfügbaren Mittel reichen nicht aus, um den Mindestbetrag für Auszahlungen zu erreichen.';
+$lang['Payouts.!error.insufficient_funds_payout_request'] = 'Es sind nicht genügend Mittel vorhanden, um den Auszahlungsantrag zu bearbeiten.';
+$lang['Payouts.!error.exceeded_payout_requests'] = 'Sie haben das maximale Limit für ausstehende Auszahlungsanträge überschritten.';
+$lang['Payouts.!success.payout_requested'] = 'Ihre Auszahlung wurde angefordert.';
+
