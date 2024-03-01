0d6ce541f2b383e1d12b29b2ffce8f1293464f03
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:05 -0800

    Updated de_de language files

---
+++ language/de_de/managed_accounts.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Managed Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ManagedAccounts.!error.permissions.empty'] = 'Sie müssen dem Manager mindestens eine Berechtigung zuweisen.';
+$lang['ManagedAccounts.!error.permissions.manage'] = 'Sie sind nicht berechtigt, dieses Konto zu verwalten.';
+$lang['ManagedAccounts.!error.contact_id.exists'] = 'Ungültige Kontakt-ID.';
+$lang['ManagedAccounts.!error.invitation_id.exists'] = 'Ungültige Einladungs-ID.';
+$lang['ManagedAccounts.!error.email.invitation'] = 'Eine Einladung wurde bereits an dieses Konto gesendet.';
+$lang['ManagedAccounts.!error.client_id.exists'] = 'Ungültige Client-ID.';
+$lang['ManagedAccounts.!error.client.exists'] = 'Der angegebenen E-Mail-Adresse ist kein Konto zugeordnet.';
+
