9c3b2c2d391792d9a12c6bf3d9ef0a1673bc920d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:45 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/wizard/language/el_gr/signup.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Signup
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Signup.index.header_login_or_signup'] = 'Εγγραφείτε ή συνδεθείτε';
+$lang['Signup.index.link_resetpassword'] = 'Επαναφορά του κωδικού μου';
+$lang['Signup.index.login_or_signup_note'] = 'Εάν έχετε ήδη λογαριασμό μαζί μας, παρακαλούμε συνδεθείτε για να προσθέσετε αυτή την παραγγελία στον υπάρχοντα λογαριασμό σας.';
+$lang['Signup.index.login_note'] = 'Θα έχετε την ευκαιρία να επιβεβαιώσετε την παραγγελία σας αφού συνδεθείτε.';
+$lang['Signup.index.signup_note'] = 'Θα έχετε την ευκαιρία να επιβεβαιώσετε την παραγγελία σας αφού δημιουργήσετε λογαριασμό.';
+$lang['Signup.index.client_note'] = 'Αυτή η παραγγελία θα προστεθεί στον ακόλουθο λογαριασμό.';
+$lang['Signup.index.logout_btn'] = 'Αποσύνδεση';
+$lang['Signup.index.continue_btn'] = 'Συνεχίστε τις αγορές';
+$lang['Signup.index.checkout_btn'] = 'Ταμείο & Πληρωμή';
+$lang['Signup.index.configure_btn'] = 'Συνεχίστε';
+$lang['Signup.index.field_agree_tos'] = 'Έχω διαβάσει και συμφωνώ με τους <a href="%1$s" target="_blank">Όρους και Προϋποθέσεις</a>';
+$lang['Signup.index.heading_client'] = 'Πληροφορίες πελατών';
+$lang['Signup.index.heading_contact'] = 'Στοιχεία επικοινωνίας';
+$lang['Signup.index.heading_billing'] = 'Πληροφορίες χρέωσης';
+$lang['Signup.index.heading_authentication'] = 'Αυθεντικοποίηση';
+$lang['Signup.index.heading_settings'] = 'Πρόσθετες ρυθμίσεις';
+
