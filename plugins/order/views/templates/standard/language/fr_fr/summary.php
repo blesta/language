b8e1962ff17c3ba265623756df0f844c7c88a2b0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:53 -0800

    Updated fr_fr language files

---
+++ plugins/order/views/templates/standard/language/fr_fr/summary.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Summary
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Summary.index.box_heading'] = 'Votre panier';
+$lang['Summary.index.login_customer'] = 'Client existant ?';
+$lang['Summary.index.login_link'] = 'Connexion';
+$lang['Summary.index.link_resetpassword'] = 'Réinitialiser mon mot de passe';
+$lang['Summary.index.field_username'] = 'Nom d\'utilisateur';
+$lang['Summary.index.field_password'] = 'Mot de passe';
+$lang['Summary.index.login_btn'] = 'Connexion';
+$lang['Summary.index.checkout_btn'] = 'Passer à la caisse';
+$lang['Summary.index.have_coupon'] = 'Vous avez un coupon ?';
+$lang['Summary.index.coupon'] = 'Coupon';
+$lang['Summary.index.coupon_btn'] = 'Racheter';
+$lang['Summary.index.cart_empty'] = 'Il n\'y a pas d\'articles dans votre panier.';
+
