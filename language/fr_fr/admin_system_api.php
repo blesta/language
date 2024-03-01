2152730ed855783ea4e826bccfa4413e2740f4d3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:58 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_system_api.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Admin System Api
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemApi.!notice.api_not_restrictive'] = 'ATTENTION ! L\'entreprise est l\'entreprise auprès de laquelle l\'utilisateur s\'authentifie lorsqu\'il se connecte. L\'accès aux données d\'autres entreprises n\'est pas limité par les demandes d\'API qui prennent en charge cet accès.';
+$lang['AdminSystemApi.!success.key_added'] = 'L\'utilisateur de l\'API a été ajouté avec succès.';
+$lang['AdminSystemApi.!success.key_deleted'] = 'L\'utilisateur API a été supprimé avec succès.';
+$lang['AdminSystemApi.!success.key_updated'] = 'L\'utilisateur de l\'API a été mis à jour avec succès.';
+$lang['AdminSystemApi.index.page_title'] = 'Paramètres > Système > Accès API';
+$lang['AdminSystemApi.index.boxtitle_api'] = 'Accès à l\'API';
+$lang['AdminSystemApi.index.categorylink_keyadd'] = 'Ajouter un utilisateur API';
+$lang['AdminSystemApi.index.heading_user'] = 'Utilisateur';
+$lang['AdminSystemApi.index.heading_key'] = 'Clé';
+$lang['AdminSystemApi.index.heading_company'] = 'Entreprise';
+$lang['AdminSystemApi.index.heading_date'] = 'Date de création';
+$lang['AdminSystemApi.index.heading_options'] = 'Options';
+$lang['AdminSystemApi.index.option_edit'] = 'Editer';
+$lang['AdminSystemApi.index.option_delete'] = 'Supprimer';
+$lang['AdminSystemApi.index.no_results'] = 'Il n\'y a pas d\'utilisateurs de l\'API.';
+$lang['AdminSystemApi.index.confirm_delete'] = 'Supprimer vraiment cet utilisateur de l\'API ?';
+$lang['AdminSystemApi.add.boxtitle_add'] = 'Ajouter un utilisateur API';
+$lang['AdminSystemApi.add.field_company'] = 'Entreprise';
+$lang['AdminSystemApi.add.field_user'] = 'Utilisateur';
+$lang['AdminSystemApi.add.field_notes'] = 'Notes';
+$lang['AdminSystemApi.add.field_apisubmit'] = 'Ajouter un utilisateur';
+$lang['AdminSystemApi.edit.boxtitle_edit'] = 'Modifier l\'utilisateur API';
+$lang['AdminSystemApi.edit.field_company'] = 'Entreprise';
+$lang['AdminSystemApi.edit.field_user'] = 'Utilisateur';
+$lang['AdminSystemApi.edit.field_notes'] = 'Notes';
+$lang['AdminSystemApi.edit.field_apisubmit'] = 'Mise à jour de l\'utilisateur';
+
