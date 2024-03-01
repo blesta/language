0bd88a19198ef6356a16d148fc75f0083290784a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:09 -0800

    Updated el_gr language files

---
+++ components/reports/custom_report/language/el_gr/custom_report.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom Report
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CustomReport.!error.field'] = 'Το %1$s δεν είναι έγκυρο.';
+$lang['CustomReports.options.field_report_select'] = '-- Παρακαλώ επιλέξτε --';
+$lang['CustomReports.options.field_report'] = 'Αναφορά';
+$lang['CustomReport.name'] = 'Προσαρμοσμένη αναφορά';
+
