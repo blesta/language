ef2a3dcb56bad17a9b002146240c1ec3cb8f4bea
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:59 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_system_upgrade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Admin System Upgrade
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemUpgrade.index.boxtitle_upgrade'] = 'Επιλογές αναβάθμισης';
+$lang['AdminSystemUpgrade.index.field_upgradesubmit'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminSystemUpgrade.index.no_results'] = 'Οι επιλογές αναβάθμισης δεν είναι διαθέσιμες αυτή τη στιγμή.';
+$lang['AdminSystemUpgrade.index.page_title'] = 'Ρυθμίσεις > Σύστημα > Επιλογές αναβάθμισης';
+
