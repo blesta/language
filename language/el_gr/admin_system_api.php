ea3a3eddaa102684c951d6cfe4e877f1de02a195
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:55 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_system_api.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Admin System Api
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemApi.edit.field_apisubmit'] = 'Ενημέρωση χρήστη';
+$lang['AdminSystemApi.edit.field_notes'] = 'Σημειώσεις';
+$lang['AdminSystemApi.edit.field_user'] = 'Χρήστης';
+$lang['AdminSystemApi.edit.field_company'] = 'Εταιρεία';
+$lang['AdminSystemApi.edit.boxtitle_edit'] = 'Επεξεργασία χρήστη API';
+$lang['AdminSystemApi.add.field_apisubmit'] = 'Προσθήκη χρήστη';
+$lang['AdminSystemApi.add.field_notes'] = 'Σημειώσεις';
+$lang['AdminSystemApi.add.field_user'] = 'Χρήστης';
+$lang['AdminSystemApi.add.field_company'] = 'Εταιρεία';
+$lang['AdminSystemApi.add.boxtitle_add'] = 'Προσθήκη χρήστη API';
+$lang['AdminSystemApi.index.confirm_delete'] = 'Πραγματικά διαγράφετε αυτόν τον χρήστη API;';
+$lang['AdminSystemApi.index.no_results'] = 'Δεν υπάρχουν χρήστες API.';
+$lang['AdminSystemApi.index.option_delete'] = 'Διαγραφή';
+$lang['AdminSystemApi.index.option_edit'] = 'Επεξεργασία';
+$lang['AdminSystemApi.index.heading_options'] = 'Επιλογές';
+$lang['AdminSystemApi.index.heading_date'] = 'Ημερομηνία δημιουργίας';
+$lang['AdminSystemApi.index.heading_company'] = 'Εταιρεία';
+$lang['AdminSystemApi.index.heading_key'] = 'Βασικό';
+$lang['AdminSystemApi.index.heading_user'] = 'Χρήστης';
+$lang['AdminSystemApi.index.categorylink_keyadd'] = 'Προσθήκη χρήστη API';
+$lang['AdminSystemApi.index.boxtitle_api'] = 'Πρόσβαση API';
+$lang['AdminSystemApi.index.page_title'] = 'Ρυθμίσεις > Σύστημα > Πρόσβαση API';
+$lang['AdminSystemApi.!success.key_updated'] = 'Ο χρήστης του API ενημερώθηκε επιτυχώς.';
+$lang['AdminSystemApi.!success.key_deleted'] = 'Ο χρήστης του API διαγράφηκε επιτυχώς.';
+$lang['AdminSystemApi.!success.key_added'] = 'Ο χρήστης του API προστέθηκε επιτυχώς.';
+$lang['AdminSystemApi.!notice.api_not_restrictive'] = 'ΠΡΟΕΙΔΟΠΟΙΗΣΗ! Εταιρεία είναι η εταιρεία με την οποία πιστοποιείται ο χρήστης κατά τη σύνδεση. Η πρόσβαση σε δεδομένα άλλων εταιρειών δεν περιορίζεται από αιτήματα API που υποστηρίζουν τέτοια πρόσβαση.';
+
