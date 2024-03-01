244925ac82bc6cd29ec29f435aa0e3312be0ff8f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:10 -0800

    Updated el_gr language files

---
+++ components/reports/package_revenue/language/el_gr/package_revenue.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Package Revenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageRevenue.heading.tax'] = 'Φόρος';
+$lang['PackageRevenue.heading.package_revenue'] = 'Έσοδα';
+$lang['PackageRevenue.heading.service_count'] = 'Μονάδες';
+$lang['PackageRevenue.heading.module_name'] = 'Ενότητα';
+$lang['PackageRevenue.heading.status'] = 'Κατάσταση';
+$lang['PackageRevenue.heading.package_name'] = 'Πακέτο';
+$lang['PackageRevenue.options.field_end_date'] = 'Ημερομηνία λήξης κλειστού τιμολογίου';
+$lang['PackageRevenue.options.field_start_date'] = 'Ημερομηνία έναρξης κλειστού τιμολογίου';
+$lang['PackageRevenue.options.currency'] = 'Νόμισμα';
+$lang['PackageRevenue.option.any'] = 'Οποιοδήποτε';
+$lang['PackageRevenue.description'] = 'Υπολογίζει τα έσοδα από ένα νόμισμα για κάθε πακέτο με το άθροισμα των πληρωμένων στοιχείων του τιμολογίου που σχετίζονται με το πακέτο.';
+$lang['PackageRevenue.name'] = 'Έσοδα πακέτου';
+
