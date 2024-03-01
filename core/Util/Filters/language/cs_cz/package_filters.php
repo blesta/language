d2f1ce896caa02e35e9fc98fe2dd141fe95c4aec
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:10 -0800

    Updated cs_cz language files

---
+++ core/Util/Filters/language/cs_cz/package_filters.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Package Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.package_filters.field_options'] = 'Možnosti';
+$lang['Util.filters.package_filters.field_assigned_services'] = 'Přiřazené služby';
+$lang['Util.filters.package_filters.field_name'] = 'Název balíčku';
+$lang['Util.filters.package_filters.field_package_group_id'] = 'Skupina balíčků';
+$lang['Util.filters.package_filters.field_module_id'] = 'Modul';
+$lang['Util.filters.package_filters.options.hidden'] = 'Zobrazit skryté balíčky';
+$lang['Util.filters.package_filters.services.none'] = 'Žádné služby';
+$lang['Util.filters.package_filters.services.canceled'] = 'Pouze zrušené služby';
+$lang['Util.filters.package_filters.services.any'] = 'Jakékoli služby';
+$lang['Util.filters.package_filters.services.na'] = 'NEUPLATŇUJE SE';
+$lang['Util.filters.package_filters.any'] = 'Jakýkoli';
+
