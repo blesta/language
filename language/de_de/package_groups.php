9198d84de959d72a02eaef640e6c4bcb30301a63
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:10 -0800

    Updated de_de language files

---
+++ language/de_de/package_groups.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Package Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageGroups.gettypes.addon'] = 'Add-on';
+$lang['PackageGroups.gettypes.standard'] = 'Standard';
+$lang['PackageGroups.!error.allow_upgrades.format'] = 'Ob Pakete innerhalb dieser Gruppe upgegradet/downgegradet werden können, muss auf 1 oder 0 gesetzt werden.';
+$lang['PackageGroups.!error.hidden.format'] = 'Ob diese Gruppe in der Schnittstelle ausgeblendet werden soll, muss auf 1 oder 0 gesetzt werden.';
+$lang['PackageGroups.!error.parents.format'] = 'Mindestens eine der angegebenen übergeordneten Gruppen-IDs ist eine Nicht-Standardgruppe, die nicht als übergeordnete Gruppe verwendet werden kann.';
+$lang['PackageGroups.!error.company_id.exists'] = 'Ungültige Unternehmens-ID.';
+$lang['PackageGroups.!error.type.format'] = 'Ungültiger Gruppentyp.';
+$lang['PackageGroups.!error.descriptions.empty_lang'] = 'Bitte geben Sie für jede Beschreibung einen Sprachencode der Paketgruppe an.';
+$lang['PackageGroups.!error.descriptions.format'] = 'Die Paketgruppenbeschreibungen haben ein ungültiges Format.';
+$lang['PackageGroups.!error.names.empty_lang'] = 'Bitte geben Sie für jeden Namen einen Sprachencode der Paketgruppe an.';
+$lang['PackageGroups.!error.names.empty_name'] = 'Bitte geben Sie für jede Sprache einen Paketgruppennamen an.';
+$lang['PackageGroups.!error.names.format'] = 'Die Paketgruppennamen haben ein ungültiges Format.';
+
