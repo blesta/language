181f26d78138e6d15ef8fbbfd1efb2fea8e5322c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:03 -0800

    Updated el_gr language files

---
+++ plugins/system_status/language/el_gr/admin_main.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.index.health_excellent'] = 'Το σύστημα είναι σε καλή κατάσταση και φαίνεται να λειτουργεί κανονικά.';
+$lang['AdminMain.index.health_good'] = 'Συνολικά, το σύστημα είναι σε καλή κατάσταση, αλλά υπάρχουν ορισμένα στοιχεία που μπορεί να απαιτούν την προσοχή σας.';
+$lang['AdminMain.index.health_fair'] = 'Υπάρχουν ορισμένα ζητήματα που επηρεάζουν την ικανότητα του συστήματος να λειτουργεί κανονικά.';
+$lang['AdminMain.index.health_poor'] = 'Υπάρχουν σοβαρά ζητήματα που επηρεάζουν την ικανότητα του συστήματος να λειτουργεί κανονικά. Αυτά θα πρέπει να επιλυθούν το συντομότερο δυνατό.';
+$lang['AdminMain.index.boxtitle_feed'] = 'Κατάσταση συστήματος';
+$lang['AdminMain.index.cron_serious'] = 'Το Cron δεν έχει εκτελεστεί ποτέ.';
+$lang['AdminMain.index.cron_minor'] = 'Το Cron δεν έχει εκτελεστεί τις τελευταίες 24 ώρες.';
+$lang['AdminMain.index.cron_configure'] = 'Διαμόρφωση;';
+$lang['AdminMain.index.cron_task_stalled_minor'] = 'Υπάρχουν μία ή περισσότερες εργασίες cron που εκτελούνται για περισσότερα από %1$s λεπτά.';
+$lang['AdminMain.index.cron_task_stalled_automation'] = 'Προβολή αυτοματοποιημένων εργασιών';
+$lang['AdminMain.index.trial_minor'] = 'Η δοκιμαστική σας άδεια χρήσης λήγει στις %1$s.';
+$lang['AdminMain.index.trial_buy'] = 'Αγοράζετε τώρα;';
+$lang['AdminMain.index.invoices_minor'] = 'Τα τιμολόγια δεν έχουν δημιουργηθεί αυτόματα μέσω cron τις τελευταίες 24 ώρες.';
+$lang['AdminMain.index.invoices_configure'] = 'Διαμόρφωση;';
+$lang['AdminMain.index.backup_minor'] = 'Δεν έχουν εκτελεστεί αντίγραφα ασφαλείας τις τελευταίες 7 ημέρες.';
+$lang['AdminMain.index.docroot_minor'] = 'Η διαδρομή ρίζας του εγγράφου που εντοπίστηκε δεν ταιριάζει με τη ρύθμιση Root Web Directory.';
+$lang['AdminMain.index.docroot_setting'] = 'Ενημέρωση;';
+$lang['AdminMain.index.system_dir_writable_minor'] = 'Ένας κατάλογος συστήματος δεν είναι εγγράψιμος.';
+$lang['AdminMain.index.system_dir_writable_setting'] = 'Ενημέρωση;';
+$lang['AdminMain.index.log_files_owner_minor'] = 'Υπάρχουν ορισμένα αρχεία καταγραφής που δεν ανήκουν στον ίδιο χρήστη με τον διακομιστή ιστού.  Αυτό συνήθως προκαλείται από το cron που εκτελείται ως διαφορετικός χρήστης από το διακομιστή ιστού.';
+$lang['AdminMain.index.error_reporting'] = 'errorReporting ή System.debug είναι ενεργοποιημένα στο αρχείο /config/blesta.php. Εκτός αν αντιμετωπίζετε ενεργά ένα πρόβλημα, αυτά θα πρέπει να είναι απενεργοποιημένα. errorReporting θα πρέπει να είναι 0, System.debug θα πρέπει να είναι false.';
+$lang['AdminMain.index.updates_forever'] = 'Η υποστήριξή σας και οι ενημερώσεις είναι καλές για πάντα.';
+$lang['AdminMain.index.updates_minor'] = 'Η υποστήριξη και οι ενημερώσεις σας είναι καλές μέχρι το %1$s.';
+$lang['AdminMain.index.updates_serious'] = 'Η υποστήριξη και οι ενημερώσεις σας έληξαν στις %1$s.';
+$lang['AdminMain.index.updates_buy'] = 'Προσθέστε υποστήριξη και ενημερώσεις;';
+$lang['AdminMain.index.db_version_serious'] = 'Η έκδοση της βάσης δεδομένων δεν συμφωνεί με την έκδοση του συστήματος.';
+$lang['AdminMain.index.db_version_upgrade'] = 'Αναβάθμιση;';
+$lang['AdminMain.index.php_version_serious'] = 'Η έκδοση PHP δεν πληροί τις ελάχιστες απαιτήσεις συστήματος.';
+$lang['AdminMain.index.php_version_requirements'] = 'Δείτε τις ελάχιστες απαιτήσεις';
+$lang['AdminMain.index.sql_mysql_version_serious'] = 'Η έκδοση της MySQL δεν πληροί τις ελάχιστες απαιτήσεις συστήματος.';
+$lang['AdminMain.index.sql_mysql_version_requirements'] = 'Δείτε τις ελάχιστες απαιτήσεις';
+$lang['AdminMain.index.sql_mariadb_version_serious'] = 'Η έκδοση της MariaDB δεν πληροί τις ελάχιστες απαιτήσεις συστήματος.';
+$lang['AdminMain.index.sql_mariadb_version_requirements'] = 'Δείτε τις ελάχιστες απαιτήσεις';
+
