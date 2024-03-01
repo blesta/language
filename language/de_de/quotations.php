add757e5a7755fc656905c645c8aa3f4efa3f3e2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:15 -0800

    Updated de_de language files

---
+++ language/de_de/quotations.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Quotations.getstatuses.lost'] = 'Verloren';
+$lang['Quotations.getstatuses.dead'] = 'Tot';
+$lang['Quotations.getstatuses.invoiced'] = 'In Rechnung gestellt';
+$lang['Quotations.getstatuses.expired'] = 'Abgelaufen';
+$lang['Quotations.getstatuses.pending'] = 'Anhängig';
+$lang['Quotations.getstatuses.approved'] = 'Genehmigt';
+$lang['Quotations.getstatuses.draft'] = 'Entwurf';
+$lang['Quotations.!error.lines[][tax].format'] = 'Die Steuerposition muss "wahr" oder "falsch" sein.';
+$lang['Quotations.!error.lines[][amount].format'] = 'Die Stückkosten müssen eine Zahl sein.';
+$lang['Quotations.!error.lines[][qty].minimum'] = 'Bitte geben Sie eine Menge von 1 oder mehr ein.';
+$lang['Quotations.!error.lines[][description].empty'] = 'Bitte geben Sie eine Positionsbeschreibung ein.';
+$lang['Quotations.!error.lines[][id].exists'] = 'Ungültige Einzelposten-ID.';
+$lang['Quotations.!error.percentage_due.valid'] = 'Der fällige Prozentsatz muss eine Zahl größer als Null und kleiner oder gleich 100 sein.';
+$lang['Quotations.!error.percentage_due.format'] = 'Der fällige Prozentsatz muss eine Zahl sein.';
+$lang['Quotations.!error.second_due_date.format'] = 'Das zweite Fälligkeitsdatum hat ein ungültiges Datumsformat.';
+$lang['Quotations.!error.first_due_date.format'] = 'Das erste Fälligkeitsdatum hat ein ungültiges Datumsformat.';
+$lang['Quotations.!error.status.valid'] = 'Der Kostenvoranschlag muss entweder genehmigt oder in Bearbeitung sein, um in Rechnung gestellt zu werden.';
+$lang['Quotations.!error.currency.length'] = 'Der Währungscode muss 3 Zeichen lang sein.';
+$lang['Quotations.!error.status.format'] = 'Ungültiger Status.';
+$lang['Quotations.!error.date_expires.after_created'] = 'Das Verfallsdatum muss auf oder nach der in Rechnung gestellten Erstellung liegen.';
+$lang['Quotations.!error.date_expires.format'] = 'Das Verfallsdatum hat ein ungültiges Datumsformat.';
+$lang['Quotations.!error.date_created.format'] = 'Das Erstellungsdatum hat ein ungültiges Datumsformat.';
+$lang['Quotations.!error.title.length'] = 'Der Titel für Zitate darf nicht länger als 255 Zeichen sein.';
+$lang['Quotations.!error.title.empty'] = 'Bitte geben Sie einen Titel ein.';
+$lang['Quotations.!error.staff_id.exists'] = 'Ungültige Personal-ID.';
+$lang['Quotations.!error.client_id.exists'] = 'Ungültige Client-ID.';
+$lang['Quotations.!error.id_value.valid'] = 'Der Wert der Angebots-ID kann nicht ermittelt werden.';
+$lang['Quotations.!error.id_format.length'] = 'Das ID-Format für Zitate darf nicht länger als 64 Zeichen sein.';
+$lang['Quotations.!error.id_format.empty'] = 'Kein ID-Format für Zitate festgelegt.';
+$lang['Quotations.!error.id.quotation_invoiced'] = 'Dieses Angebot wurde in Rechnung gestellt und kann nicht bearbeitet werden.';
+$lang['Quotations.!error.quotation_add.failed'] = 'Dieses Angebot konnte nicht erstellt werden. Bitte versuchen Sie es erneut.';
+
