81fa3e6e25d23309cb73f511cf80e234b87ec706
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:18 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/admin_main.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.status_updated'] = 'Οι επιλεγμένες παραγγελίες ενημερώθηκαν επιτυχώς.';
+$lang['AdminMain.!success.settings_updated'] = 'Οι ρυθμίσεις της παραγγελίας σας ενημερώθηκαν επιτυχώς.';
+$lang['AdminMain.!success.affiliate_settings_updated'] = 'Οι ρυθμίσεις θυγατρικών έχουν ενημερωθεί.';
+$lang['AdminMain.index.boxtitle_order'] = 'Παραγγελίες';
+$lang['AdminMain.index.no_results'] = 'Δεν υπάρχουν παραγγελίες αυτού του τύπου.';
+$lang['AdminMain.index.category_pending'] = 'Ανασκόπηση';
+$lang['AdminMain.index.category_accepted'] = 'Αποδεκτή';
+$lang['AdminMain.index.category_fraud'] = 'Απάτη';
+$lang['AdminMain.index.category_canceled'] = 'Ακυρώθηκε';
+$lang['AdminMain.index.heading_order_number'] = 'Παραγγελία #';
+$lang['AdminMain.index.heading_client_id_code'] = 'Αναγνωριστικό πελάτη';
+$lang['AdminMain.index.heading_invoice_id_code'] = 'Τιμολόγιο #';
+$lang['AdminMain.index.heading_total'] = 'Σύνολο';
+$lang['AdminMain.index.heading_paid'] = 'Πληρωμένο';
+$lang['AdminMain.index.heading_date_added'] = 'Ημερομηνία παραγγελίας';
+$lang['AdminMain.index.field_ordersubmit'] = 'Ενημέρωση παραγγελιών';
+$lang['AdminMain.index.field_markas'] = 'Σημειώστε ως:';
+$lang['AdminMain.index.text_location'] = 'Τοποθεσία';
+$lang['AdminMain.search.page_title'] = 'Αποτελέσματα αναζήτησης για "%1$s"';
+$lang['AdminMain.search.boxtitle_order'] = 'Αναζήτηση παραγγελιών για "%1$s"';
+$lang['AdminMain.getfilters.field_order_number'] = 'Παραγγελία #';
+$lang['AdminMain.orderinfo.fraudreport_heading'] = 'Έκθεση απάτης';
+$lang['AdminMain.orderinfo.fraudreport_heading_key'] = 'Βασικό';
+$lang['AdminMain.orderinfo.fraudreport_heading_value'] = 'Αξία';
+$lang['AdminMain.orderinfo.applied_heading'] = 'Συναλλαγές';
+$lang['AdminMain.orderinfo.applied_heading_paymenttype'] = 'Τύπος πληρωμής';
+$lang['AdminMain.orderinfo.applied_heading_amount'] = 'Ποσό';
+$lang['AdminMain.orderinfo.applied_heading_applied'] = 'Εφαρμοσμένο';
+$lang['AdminMain.orderinfo.applied_heading_appliedon'] = 'Εφαρμόζεται στο';
+$lang['AdminMain.orderinfo.applied_heading_options'] = 'Επιλογές';
+$lang['AdminMain.orderinfo.applied_option_edit'] = 'Επεξεργασία';
+$lang['AdminMain.orderinfo.applied_no_results'] = 'Δεν υπάρχουν συναλλαγές σε αυτή την παραγγελία.';
+$lang['AdminMain.orderinfo.services_heading'] = 'Υπηρεσίες';
+$lang['AdminMain.orderinfo.services_heading_package'] = 'Πακέτο';
+$lang['AdminMain.orderinfo.services_heading_label'] = 'Ετικέτα';
+$lang['AdminMain.orderinfo.services_heading_term'] = 'Όρος';
+$lang['AdminMain.orderinfo.services_heading_dateadded'] = 'Ημερομηνία προσθήκης';
+$lang['AdminMain.orderinfo.services_heading_daterenews'] = 'Ημερομηνία Ανανεώνει';
+$lang['AdminMain.orderinfo.services_heading_options'] = 'Επιλογές';
+$lang['AdminMain.orderinfo.services_option_manage'] = 'Διαχείριση';
+$lang['AdminMain.orderinfo.services_text_never'] = 'Ποτέ';
+$lang['AdminMain.orderinfo.services_no_results'] = 'Δεν υπάρχουν υπηρεσίες σε αυτή τη σειρά.';
+$lang['AdminMain.settings.heading_notifications'] = 'Ειδοποιήσεις παραγγελιών';
+$lang['AdminMain.settings.field_email_notice'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['AdminMain.settings.field_email_notice_never'] = 'Μην στέλνετε';
+$lang['AdminMain.settings.field_email_notice_manual'] = 'Αποστολή μόνο εάν απαιτείται χειροκίνητη έγκριση';
+$lang['AdminMain.settings.field_email_notice_always'] = 'Πάντα να στέλνετε';
+$lang['AdminMain.settings.field_mobile_notice'] = 'Κινητό';
+$lang['AdminMain.settings.field_mobile_notice_never'] = 'Μην στέλνετε';
+$lang['AdminMain.settings.field_mobile_notice_manual'] = 'Αποστολή μόνο εάν απαιτείται χειροκίνητη έγκριση';
+$lang['AdminMain.settings.field_mobile_notice_always'] = 'Πάντα να στέλνετε';
+$lang['AdminMain.settings.field_messenger_notice'] = 'Αγγελιοφόροι';
+$lang['AdminMain.settings.field_messenger_notice_never'] = 'Μην στέλνετε';
+$lang['AdminMain.settings.field_messenger_notice_manual'] = 'Αποστολή μόνο εάν απαιτείται χειροκίνητη έγκριση';
+$lang['AdminMain.settings.field_messenger_notice_always'] = 'Πάντα να στέλνετε';
+$lang['AdminMain.settings.heading_affiliate_notifications'] = 'Ειδοποιήσεις θυγατρικών';
+$lang['AdminMain.settings.field_payout_notice'] = 'Πληρωμή';
+$lang['AdminMain.settings.field_payout_notice_never'] = 'Ποτέ';
+$lang['AdminMain.settings.field_payout_notice_always'] = 'Πάντα';
+$lang['AdminMain.settings.submit_settings'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminMain.settings.submit_cancel'] = 'Επιστροφή στη λίστα παραγγελιών';
+$lang['AdminMain.affiliates.page_title'] = 'Επισκόπηση θυγατρικών';
+$lang['AdminMain.affiliates.boxtitle_affiliates'] = 'Επισκόπηση θυγατρικών';
+$lang['AdminMain.affiliates.inactive'] = 'Αυτή η θυγατρική έχει χαρακτηριστεί ανενεργή.';
+$lang['AdminMain.affiliates.activate'] = 'Κάντε κλικ εδώ για να επανενεργοποιήσετε';
+$lang['AdminMain.affiliates.no_results'] = 'Κανένας συνεργάτης δεν συνδέεται με αυτόν τον πελάτη.  Κάντε κλικ εδώ για να εγγραφείτε.';
+$lang['AdminMain.affiliates.title_details'] = 'Λεπτομέρειες';
+$lang['AdminMain.affiliates.title_referral'] = 'Σύνδεσμος παραπομπής';
+$lang['AdminMain.affiliates.visits'] = 'Clicks';
+$lang['AdminMain.affiliates.monthly_visits'] = '%1$s/mo κατά μέσο όρο';
+$lang['AdminMain.affiliates.sales'] = 'Συνολικές πωλήσεις';
+$lang['AdminMain.affiliates.monthly_sales'] = '%1$s/mo κατά μέσο όρο';
+$lang['AdminMain.affiliates.conversion_rate'] = 'Ποσοστό μετατροπής';
+$lang['AdminMain.affiliates.conversion_fraction'] = '%1$s/%2$s κατά μέσο όρο';
+$lang['AdminMain.affiliates.commission_type'] = 'Επίπεδο πληρωμών';
+$lang['AdminMain.affiliates.min_withdrawal_amount'] = 'Ελάχιστο ποσό πληρωμής';
+$lang['AdminMain.affiliates.maturity_days'] = 'Ημέρες μέχρι την ωρίμανση';
+$lang['AdminMain.affiliates.available_payout'] = 'Διαθέσιμο για πληρωμή';
+$lang['AdminMain.affiliates.field.commission_fixed'] = '%1$s εφάπαξ για κάθε ώριμη παραπομπή';
+$lang['AdminMain.affiliates.field.commission_percentage'] = '%1$s κάθε ώριμης παραπομπής';
+$lang['AdminMain.affiliates.request_payout'] = 'Αίτημα πληρωμής';
+$lang['AdminMain.affiliates.boxtitle_client'] = 'Πελάτης #%1$s - %2$s %3$s';
+$lang['AdminMain.affiliates.heading_affiliates'] = 'Ρυθμίσεις θυγατρικών';
+$lang['AdminMain.affiliates.heading_referral_link'] = 'Σύνδεσμος παραπομπής';
+$lang['AdminMain.affiliates.field_commission_type'] = 'Τύπος Επιτροπής';
+$lang['AdminMain.affiliates.tooltip_commission_type'] = 'Η μέθοδος υπολογισμού που χρησιμοποιείται για τον προσδιορισμό της προμήθειας παραπομπής.  Είτε μια σταθερή τιμή ανά παραγγελία είτε ένα ποσοστό επί του συνόλου της παραγγελίας.';
+$lang['AdminMain.affiliates.field_commission_amount'] = 'Ποσό προμήθειας';
+$lang['AdminMain.affiliates.field_order_frequency'] = 'Συχνότητα παραγγελίας';
+$lang['AdminMain.affiliates.tooltip_order_frequency'] = 'Πρώτα για να πληρώνετε τις παραπομπές μόνο για την πρώτη παραγγελία που κάνει ένας πελάτης χρησιμοποιώντας έναν σύνδεσμο παραπομπής, ή Οποιαδήποτε για να πληρώνετε τις παραπομπές για κάθε παραγγελία που κάνει ένας πελάτης χρησιμοποιώντας έναν σύνδεσμο παραπομπής.';
+$lang['AdminMain.affiliates.field_order_recurring'] = 'Παραγγελία Επαναλαμβανόμενη';
+$lang['AdminMain.affiliates.tooltip_order_recurring'] = 'Εάν οι παραπομπές θα πληρώνονται για υπηρεσίες που παραγγέλλονται ότι ανανεώνονται.';
+$lang['AdminMain.affiliates.field_maturity_days'] = 'Ημέρες ωρίμανσης';
+$lang['AdminMain.affiliates.tooltip_maturity_days'] = 'Ο αριθμός των ημερών μετά την πληρωμή μιας παραπομπής που ωριμάζει και γίνεται διαθέσιμη για πληρωμή.';
+$lang['AdminMain.affiliates.field_min_withdrawal_amount'] = 'Ελάχιστο ποσό ανάληψης';
+$lang['AdminMain.affiliates.field_max_withdrawal_amount'] = 'Μέγιστο ποσό ανάληψης';
+$lang['AdminMain.affiliates.field_withdrawal_currency'] = 'Νόμισμα ανάληψης';
+$lang['AdminMain.affiliates.text_code_order_form'] = 'Μπορείτε επίσης να συνδεθείτε σε μια συγκεκριμένη σελίδα φόρμας παραγγελίας ως συνεργάτης προσθέτοντας στο τέλος ?a=%1$s σε ένα σύνδεσμο χωρίς παραμέτρους ερωτήματος ή &a=%1$s σε ένα σύνδεσμο με παραμέτρους ερωτήματος.';
+$lang['AdminMain.affiliates.heading_statistics'] = 'Στατιστικά στοιχεία';
+$lang['AdminMain.affiliates.graph_referrals'] = 'Παραπομπές';
+$lang['AdminMain.affiliates.graph_visits'] = 'Επισκέψεις';
+$lang['AdminMain.affiliates.graph_sales'] = 'Πωλήσεις';
+$lang['AdminMain.affiliates.submit_settings'] = 'Αποθήκευση ρυθμίσεων';
+$lang['AdminMain.affiliates.submit_cancel'] = 'Ακύρωση';
+
