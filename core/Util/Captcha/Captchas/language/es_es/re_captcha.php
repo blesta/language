9e8ee22c737db9fbf2898ef3d590d9d60f3da09b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:32 -0800

    Updated es_es language files

---
+++ core/Util/Captcha/Captchas/language/es_es/re_captcha.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Re Captcha
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReCaptcha.name'] = 'reCaptcha';
+$lang['ReCaptcha.options.field_recaptcha_pub_key'] = 'Clave del sitio reCaptcha';
+$lang['ReCaptcha.options.field_recaptcha_shared_key'] = 'Clave compartida reCaptcha';
+
