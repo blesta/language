b1dda9e4bda064656056172e8db2435720450134
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:29 -0800

    Updated cs_cz language files

---
+++ plugins/cms/language/cs_cz/cms_manage_plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Cms Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CmsManagePlugin.index.field_submit'] = 'Aktualizace stránky';
+$lang['CmsManagePlugin.index.field_content'] = 'Obsah úvodní stránky';
+$lang['CmsManagePlugin.index.field_tags'] = 'Dostupné štítky';
+$lang['CmsManagePlugin.index.field_title'] = 'Rejstřík Název stránky';
+$lang['CmsManagePlugin.index.boxtitle_portal'] = 'Portál';
+$lang['CmsManagePlugin.index.page_title'] = 'Portál > Správa';
+$lang['CmsManagePlugin.!success.plugin_updated'] = 'Portál byl úspěšně aktualizován.';
+
