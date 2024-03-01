8067c590df84e399bca48001eeffe220c91c1007
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:56 -0800

    Updated it_it language files

---
+++ language/it_it/companies.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Companies
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Companies.!error.inv_proforma_start.number'] = 'Valore iniziale della fattura proforma deve essere un numero.';
+$lang['Companies.!error.inv_proforma_format.contains'] = 'Il formato della fattura proforma deve contenere {num}.';
+$lang['Companies.!error.inv_proforma_format.format'] = 'Il formato della fattura proforma non può essere in conflitto con il formato della bozza di fattura.';
+$lang['Companies.!error.inv_pad_str.length'] = 'Il carattere di imbottitura della fattura deve essere un carattere singolo.';
+$lang['Companies.!error.inv_pad_size.number'] = 'La dimensione dell\'imbottitura della fattura deve essere un numero.';
+$lang['Companies.!error.inv_increment.number'] = 'Il valore di incremento della fattura deve essere un numero.';
+$lang['Companies.!error.inv_start.number'] = 'Il valore di inizio fattura deve essere un numero.';
+$lang['Companies.!error.inv_draft_format.contains'] = 'Il formato della bozza di fattura deve contenere {num}.';
+$lang['Companies.!error.inv_draft_format.format'] = 'Il formato della bozza di fattura non può essere in conflitto con il formato della fattura.';
+$lang['Companies.!error.inv_format.contains'] = 'Il formato della fattura deve contenere {num}.';
+$lang['Companies.!error.inv_format.format'] = 'Il formato della fattura non può essere in conflitto con il formato della bozza di fattura.';
+$lang['Companies.!error.quota.allowed'] = 'Non sono disponibili licenze aziendali aggiuntive. Accedere al proprio account all\'indirizzo account.blesta.com o contattare le vendite per acquistare licenze aziendali aggiuntive.';
+$lang['Companies.!error.private_key_passphrase.valid'] = 'La passphrase inserita non è valida.';
+$lang['Companies.!error.fax.length'] = 'La lunghezza del fax non può superare i 64 caratteri.';
+$lang['Companies.!error.phone.length'] = 'La lunghezza del telefono non può superare i 64 caratteri.';
+$lang['Companies.!error.hostname.unique'] = 'Il nome dell\'host è già stato preso da un\'altra azienda.';
+$lang['Companies.!error.hostname.length'] = 'La lunghezza del nome host non può superare i 255 caratteri.';
+$lang['Companies.!error.hostname.valid'] = 'Inserire un nome host valido.';
+$lang['Companies.!error.name.length'] = 'La lunghezza del nome non può superare i 64 caratteri.';
+$lang['Companies.!error.name.empty'] = 'Inserire un nome.';
+$lang['Companies.!error.company_id.exists'] = 'ID azienda non valido.';
+$lang['Companies.!error.company_id.in_use'] = 'Questa società è attualmente in uso e non può essere cancellata.';
+
