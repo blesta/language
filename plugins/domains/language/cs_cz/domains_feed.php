b4734e643dba12144009f16bdb68758cd55bece3
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:34 -0800

    Updated cs_cz language files

---
+++ plugins/domains/language/cs_cz/domains_feed.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Domains Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsFeed.!error.invalid_style'] = 'Požadovaný styl není platný.';
+$lang['DomainsFeed.!error.invalid_endpoint'] = 'Požadovaný koncový bod není platný nebo neexistuje.';
+$lang['DomainsFeed.table.not_available'] = 'Není k dispozici';
+$lang['DomainsFeed.table.renew'] = 'Obnovit';
+$lang['DomainsFeed.table.transfer'] = 'Přenos';
+$lang['DomainsFeed.table.register'] = 'Registrace';
+$lang['DomainsFeed.table.heading_years'] = '%1$s let';
+$lang['DomainsFeed.table.heading_year'] = '%1$s Rok';
+$lang['DomainsFeed.table.heading_tlds'] = 'TLD';
+$lang['DomainsFeed.getOptionFields.param_term_description'] = 'Seznam termínů roku oddělených čárkou, které mají být zahrnuty do tabulky cen.';
+$lang['DomainsFeed.getOptionFields.param_term'] = 'termín';
+$lang['DomainsFeed.getOptionFields.param_style_description'] = 'Styl tabulky pro načtení: html nebo bootstrap.';
+$lang['DomainsFeed.getOptionFields.param_style'] = 'styl';
+$lang['DomainsFeed.getOptionFields.param_currency_description'] = 'Třímístný kód měny, pro který se má načíst cena.';
+$lang['DomainsFeed.getOptionFields.param_currency'] = 'měna';
+$lang['DomainsFeed.getOptionFields.params'] = 'Parametry';
+$lang['DomainsFeed.getOptionFields.header_description'] = 'Popis';
+$lang['DomainsFeed.getOptionFields.header_name'] = 'Název';
+$lang['DomainsFeed.getOptionFields.example_code_table'] = 'Zobrazí tabulku s cenami všech TLD v dané měně:';
+$lang['DomainsFeed.getOptionFields.title_row_example_code'] = 'Příklad kódu';
+$lang['DomainsFeed.description'] = 'Vrátí tabulku HTML obsahující ceny všech TLD.';
+$lang['DomainsFeed.name'] = 'Ceny domén';
+
