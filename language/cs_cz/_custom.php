bec86d3e77d97cf7021eaa7b1811fa0ae10de811
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:16 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/_custom.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['_PaymentTypes.in_house_credit'] = 'In House Credit';
+$lang['_PaymentTypes.money_order'] = 'Peněžní poukázka';
+$lang['_PaymentTypes.check'] = 'Podívejte se na stránky .';
+$lang['_PaymentTypes.cash'] = 'Hotovost';
+
