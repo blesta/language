086442b58acf17078566556d1922687ad7ef4184
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:59 -0800

    Updated el_gr language files

---
+++ plugins/extension_generator/language/el_gr/extension_generator_controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Extension Generator Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorController.getnodes.general_settings'] = 'Γενικές ρυθμίσεις';
+$lang['ExtensionGeneratorController.getnodes.basic_info'] = 'Βασικές πληροφορίες';
+$lang['ExtensionGeneratorController.getnodes.plugin_database'] = 'Πληροφορίες βάσης δεδομένων';
+$lang['ExtensionGeneratorController.getnodes.plugin_integrations'] = 'Βασικές ενσωματώσεις';
+$lang['ExtensionGeneratorController.getnodes.module_fields'] = 'Πεδία ενότητας';
+$lang['ExtensionGeneratorController.getnodes.merchant_fields'] = 'Πεδία διαμόρφωσης';
+$lang['ExtensionGeneratorController.getnodes.merchant_features'] = 'Υποστηριζόμενα χαρακτηριστικά';
+$lang['ExtensionGeneratorController.getnodes.nonmerchant_fields'] = 'Πεδία διαμόρφωσης';
+$lang['ExtensionGeneratorController.getnodes.additional_features'] = 'Πρόσθετα χαρακτηριστικά';
+$lang['ExtensionGeneratorController.getnodes.confirm'] = 'Επιβεβαίωση';
+$lang['ExtensionGeneratorController.getfieldtypes.text'] = 'Κείμενο';
+$lang['ExtensionGeneratorController.getfieldtypes.textarea'] = 'Περιοχή κειμένου';
+$lang['ExtensionGeneratorController.getfieldtypes.checkbox'] = 'Κουτάκι ελέγχου';
+$lang['ExtensionGeneratorController.gettablevels.staff'] = 'Προσωπικό';
+$lang['ExtensionGeneratorController.gettablevels.client'] = 'Πελάτης';
+$lang['ExtensionGeneratorController.gettasktypes.time'] = 'Χρόνος';
+$lang['ExtensionGeneratorController.gettasktypes.interval'] = 'Διάστημα';
+
