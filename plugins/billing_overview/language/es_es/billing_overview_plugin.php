21a081c26cf02c652867c7cf1f58b55016544b76
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:50 -0800

    Updated es_es language files

---
+++ plugins/billing_overview/language/es_es/billing_overview_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Billing Overview Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BillingOverviewPlugin.description'] = 'Muestra las estadísticas de facturación en un widget en la página Resumen de facturación.';
+$lang['BillingOverviewPlugin.name'] = 'La facturación de un vistazo';
+
