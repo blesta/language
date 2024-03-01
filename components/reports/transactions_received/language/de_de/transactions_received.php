ef60fddb0ef13d0e1b7698004d5c056812eceb74
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:50 -0800

    Updated de_de language files

---
+++ components/reports/transactions_received/language/de_de/transactions_received.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Transactions Received
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['TransactionsReceived.heading.date_added'] = 'Datum hinzugefügt';
+$lang['TransactionsReceived.heading.status'] = 'Status';
+$lang['TransactionsReceived.heading.currency'] = 'Währung';
+$lang['TransactionsReceived.heading.amount'] = 'Zahlungsbetrag';
+$lang['TransactionsReceived.heading.income_type'] = 'Transaktionsart';
+$lang['TransactionsReceived.heading.type_name'] = 'Zahlungsart';
+$lang['TransactionsReceived.heading.gateway_name'] = 'Zahlungs-Gateway';
+$lang['TransactionsReceived.heading.client_id_code'] = 'Kunde #';
+$lang['TransactionsReceived.heading.reference_id'] = 'Referenz-ID';
+$lang['TransactionsReceived.heading.transaction_id'] = 'Transaktions-ID';
+$lang['TransactionsReceived.heading.id'] = 'Transaktion #';
+$lang['TransactionsReceived.options.field_payment_type'] = 'Zahlungsart';
+$lang['TransactionsReceived.options.field_status'] = 'Status';
+$lang['TransactionsReceived.options.field_gateway'] = 'Gateway';
+$lang['TransactionsReceived.options.field_end_date'] = 'Enddatum';
+$lang['TransactionsReceived.options.field_start_date'] = 'Datum des Beginns';
+$lang['TransactionsReceived.option.any'] = 'Jede';
+$lang['TransactionsReceived.name'] = 'Empfangene Transaktionen';
+
