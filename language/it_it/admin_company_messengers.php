cfcb49d5625bf293a8143399ea9ab2746b0dc049
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:32 -0800

    Updated it_it language files

---
+++ language/it_it/admin_company_messengers.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Admin Company Messengers
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyMessengers.edittemplate.field.edittemplatesubmit'] = 'Modello di aggiornamento';
+$lang['AdminCompanyMessengers.edittemplate.field.cancel'] = 'Annullamento';
+$lang['AdminCompanyMessengers.edittemplate.field.status'] = 'Abilitato';
+$lang['AdminCompanyMessengers.edittemplate.field.tags'] = 'Tag disponibili';
+$lang['AdminCompanyMessengers.edittemplate.text_none'] = 'Nessuno';
+$lang['AdminCompanyMessengers.edittemplate.boxtitle_edittemplate'] = 'Modifica modello di messaggio %1$s';
+$lang['AdminCompanyMessengers.edittemplate.page_title'] = 'Impostazioni > Azienda > Email > Modifica modello di messaggio';
+$lang['AdminCompanyMessengers.templates.no_results'] = 'Non esistono modelli di questo tipo.';
+$lang['AdminCompanyMessengers.templates.option_edit'] = 'Modifica';
+$lang['AdminCompanyMessengers.templates.staff_quotation_approved_desc'] = 'Avviso inviato dopo l\'approvazione di un preventivo da parte del cliente.';
+$lang['AdminCompanyMessengers.templates.staff_quotation_approved_name'] = 'Approvazione del preventivo';
+$lang['AdminCompanyMessengers.templates.quotation_delivery_desc'] = 'Avviso contenente un numero di preventivo e un link.';
+$lang['AdminCompanyMessengers.templates.quotation_delivery_name'] = 'Consegna del preventivo';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_paid_desc'] = 'Avviso contenente l\'importo totale di una fattura pagata.';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_paid_name'] = 'Consegna della fattura (pagata)';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_unpaid_desc'] = 'Avviso contenente l\'importo totale di una fattura non pagata.';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_unpaid_name'] = 'Consegna della fattura (non pagata)';
+$lang['AdminCompanyMessengers.templates.service_unsuspension_desc'] = 'Avviso di servizio non sospeso, inviato quando un servizio viene automaticamente sospeso.';
+$lang['AdminCompanyMessengers.templates.service_unsuspension_name'] = 'Servizio non sospeso';
+$lang['AdminCompanyMessengers.templates.service_suspension_desc'] = 'Avviso di sospensione del servizio, inviato quando un servizio viene sospeso automaticamente.';
+$lang['AdminCompanyMessengers.templates.service_suspension_name'] = 'Sospensione del servizio';
+$lang['AdminCompanyMessengers.templates.service_scheduled_cancellation_desc'] = 'Avviso di cancellazione del servizio programmato, inviato quando è prevista la cancellazione di un servizio.';
+$lang['AdminCompanyMessengers.templates.service_scheduled_cancellation_name'] = 'Cancellazione del servizio programmato';
+$lang['AdminCompanyMessengers.templates.service_creation_desc'] = 'Avviso di creazione del servizio, inviato quando è stato creato un servizio.';
+$lang['AdminCompanyMessengers.templates.service_creation_name'] = 'Creazione di servizi';
+$lang['AdminCompanyMessengers.templates.invoice_notice_third_desc'] = 'Terzo avviso di fattura, sia esso un sollecito di pagamento o un avviso di mora.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_third_name'] = 'Avviso di fatturazione (3°)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_second_desc'] = 'Secondo avviso di fattura, sia esso un sollecito di pagamento o un avviso di mora.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_second_name'] = 'Avviso di fatturazione (2°)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_first_desc'] = 'Primo avviso di fattura, sia esso un sollecito di pagamento o un avviso di mora.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_first_name'] = 'Avviso di fatturazione (1°)';
+$lang['AdminCompanyMessengers.templates.auto_debit_pending_desc'] = 'Avviso inviato che indica che si tenterà presto di effettuare un pagamento automatico.';
+$lang['AdminCompanyMessengers.templates.auto_debit_pending_name'] = 'Addebito automatico in sospeso';
+$lang['AdminCompanyMessengers.templates.account_welcome_desc'] = 'Invio di un avviso di benvenuto per le registrazioni di nuovi account.';
+$lang['AdminCompanyMessengers.templates.account_welcome_name'] = 'Registrazione del conto';
+$lang['AdminCompanyMessengers.templates.text_options'] = 'Opzioni';
+$lang['AdminCompanyMessengers.templates.text_description'] = 'Descrizione';
+$lang['AdminCompanyMessengers.templates.text_plugin'] = 'Plugin';
+$lang['AdminCompanyMessengers.templates.text_name'] = 'Nome';
+$lang['AdminCompanyMessengers.templates.heading_plugins'] = 'Messaggi del plugin';
+$lang['AdminCompanyMessengers.templates.heading_staff'] = 'Messaggi del personale';
+$lang['AdminCompanyMessengers.templates.heading_client'] = 'Messaggi del cliente';
+$lang['AdminCompanyMessengers.templates.boxtitle_message_templates'] = 'Modelli di messaggio';
+$lang['AdminCompanyMessengers.templates.page_title'] = 'Impostazioni > Azienda > Messaggeri > Modelli di messaggio';
+$lang['AdminCompanyMessengers.configuration.text_none'] = 'Non ci sono messaggeri installati.';
+$lang['AdminCompanyMessengers.configuration.field_update'] = 'Aggiornamento delle impostazioni';
+$lang['AdminCompanyMessengers.configuration.heading_messenger'] = 'Messaggero';
+$lang['AdminCompanyMessengers.configuration.heading_message_type'] = 'Tipo di messaggio';
+$lang['AdminCompanyMessengers.configuration.boxtitle_messenger_configuration'] = 'Configurazione del messaggero';
+$lang['AdminCompanyMessengers.configuration.page_title'] = 'Impostazioni > Azienda > Messaggeri > Configurazione messaggeri';
+$lang['AdminCompanyMessengers.manage.field_update'] = 'Aggiornamento delle impostazioni';
+$lang['AdminCompanyMessengers.manage.none_text'] = 'Non c\'è nulla da gestire per questo messaggero.';
+$lang['AdminCompanyMessengers.manage.boxtitle_manage'] = 'Gestire %1$s';
+$lang['AdminCompanyMessengers.manage.page_title'] = 'Impostazioni > Azienda > Messaggeri > Gestisci %1$s';
+$lang['AdminCompanyMessengers.installed.confirm_uninstall'] = 'Sei sicuro di voler disinstallare questo messenger?';
+$lang['AdminCompanyMessengers.installed.btn_upgrade'] = 'Aggiornamento';
+$lang['AdminCompanyMessengers.installed.btn_manage'] = 'Gestire';
+$lang['AdminCompanyMessengers.installed.btn_uninstall'] = 'Disinstallare';
+$lang['AdminCompanyMessengers.installed.text_none'] = 'Non ci sono messaggeri installati.';
+$lang['AdminCompanyMessengers.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyMessengers.installed.text_author'] = 'Autore:';
+$lang['AdminCompanyMessengers.installed.boxtitle_installed'] = 'Messaggeri';
+$lang['AdminCompanyMessengers.installed.page_title'] = 'Impostazioni > Azienda > Messaggeri > Installati';
+$lang['AdminCompanyMessengers.available.btn_install'] = 'Installare';
+$lang['AdminCompanyMessengers.available.text_none'] = 'Non ci sono messaggeri disponibili.';
+$lang['AdminCompanyMessengers.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyMessengers.available.text_author'] = 'Autore:';
+$lang['AdminCompanyMessengers.available.boxtitle_messenger'] = 'Messaggeri';
+$lang['AdminCompanyMessengers.available.page_title'] = 'Impostazioni > Azienda > Messaggeri > Disponibile';
+$lang['AdminCompanyMessengers.!tab.available'] = 'Disponibile';
+$lang['AdminCompanyMessengers.!tab.installed'] = 'Installato';
+$lang['AdminCompanyMessengers.!tooltip.status'] = 'Non verranno inviati messaggi utilizzando questo modello, a meno che questa opzione non sia abilitata.';
+$lang['AdminCompanyMessengers.!success.messenger_updated'] = 'Il messenger è stato aggiornato con successo.';
+$lang['AdminCompanyMessengers.!success.edit_template_updated'] = 'Il modello di messaggio è stato aggiornato con successo.';
+$lang['AdminCompanyMessengers.!success.messenger_options_updated'] = 'La configurazione del messenger è stata aggiornata con successo!';
+$lang['AdminCompanyMessengers.!success.uninstalled'] = 'Il messenger è stato disinstallato con successo.';
+$lang['AdminCompanyMessengers.!success.installed'] = 'Il messenger è stato installato con successo.';
+$lang['AdminCompanyMessengers.!success.upgraded'] = 'Il messaggero è stato aggiornato con successo.';
+
