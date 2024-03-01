8ce0bb854065e6cf6216a12665955e79846270d7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:03 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_system_upgrade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Admin System Upgrade
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemUpgrade.index.boxtitle_upgrade'] = 'Options de mise à niveau';
+$lang['AdminSystemUpgrade.index.field_upgradesubmit'] = 'Mise à jour des paramètres';
+$lang['AdminSystemUpgrade.index.no_results'] = 'Les options de mise à niveau ne sont pas disponibles pour le moment.';
+$lang['AdminSystemUpgrade.index.page_title'] = 'Réglages > Système > Options de mise à niveau';
+
