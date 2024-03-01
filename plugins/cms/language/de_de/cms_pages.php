3aeb2c2b61041c6391cea74b1627d62d85ee66f6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:31 -0800

    Updated de_de language files

---
+++ plugins/cms/language/de_de/cms_pages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cms Pages
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CmsPages.!error.content.valid'] = 'Fehler beim Parsen des Inhalts: %1$s';
+$lang['CmsPages.!error.title.empty'] = 'Bitte geben Sie einen Seitentitel ein.';
+$lang['CmsPages.!error.company_id.exists'] = 'Ungültige Unternehmens-ID.';
+$lang['CmsPages.!error.uri.empty'] = 'Bitte geben Sie den URI-Pfad zu der Seite ein.';
+
