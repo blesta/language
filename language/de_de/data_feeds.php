cf9a74a439d820e7c70e699e807e3ef0d313a2bf
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:58 -0800

    Updated de_de language files

---
+++ language/de_de/data_feeds.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Data Feeds
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DataFeeds.execute.endpoint_not_found'] = 'Der angeforderte Endpunkt existiert nicht.';
+$lang['DataFeeds.!error.enabled.valid'] = 'Die Option aktiviert muss auf 1 oder 0 gesetzt werden.';
+$lang['DataFeeds.!error.endpoint.valid'] = 'Bitte geben Sie den Endpunkt ein.';
+$lang['DataFeeds.!error.company_id.exists'] = 'Ungültige Unternehmens-ID.';
+$lang['DataFeeds.!error.class.valid'] = 'Die angegebene Klasse ist keine gültige Datenfeed-Klasse.';
+$lang['DataFeeds.!error.dir.empty'] = 'Bitte geben Sie das Plugin-Verzeichnis an.';
+$lang['DataFeeds.!error.feed.valid'] = 'Bitte geben Sie den Namen des Datenfeeds ein.';
+
