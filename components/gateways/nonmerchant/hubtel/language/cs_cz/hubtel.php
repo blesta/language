35afd4cdcaa2e71dca3d2b6250bda61ac7dc4dd2
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:04 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/hubtel/language/cs_cz/hubtel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Hubtel
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Hubtel.!error.client_secret.valid'] = 'Musíte zadat platné klientské tajemství.';
+$lang['Hubtel.!error.client_id.valid'] = 'Musíte zadat platné ID klienta.';
+$lang['Hubtel.buildprocess.submit'] = 'Plaťte s Hubtelem';
+$lang['Hubtel.client_secret'] = 'Tajemství klienta';
+$lang['Hubtel.client_id'] = 'ID klienta';
+$lang['Hubtel.description'] = 'Hubtel je přední ghanská platforma pro agregaci a platby mobilních služeb s přidanou hodnotou.';
+$lang['Hubtel.name'] = 'Hubtel';
+
