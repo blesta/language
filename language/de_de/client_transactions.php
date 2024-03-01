e3b2ee05446d7d27bc8072941349f1561ac9024f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:50 -0800

    Updated de_de language files

---
+++ language/de_de/client_transactions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Client Transactions
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientTransactions.applied.no_results'] = 'Diese Transaktion wurde nicht auf Rechnungen angewandt.';
+$lang['ClientTransactions.applied.heading_appliedon'] = 'Angewandt auf';
+$lang['ClientTransactions.applied.heading_amount'] = 'Betrag';
+$lang['ClientTransactions.applied.heading_invoice'] = 'Rechnung';
+$lang['ClientTransactions.index.no_results'] = 'Sie haben keine %1$s-Transaktionen.';
+$lang['ClientTransactions.index.heading_date'] = 'Datum';
+$lang['ClientTransactions.index.heading_number'] = 'Nummer';
+$lang['ClientTransactions.index.heading_applied'] = 'Angewandt';
+$lang['ClientTransactions.index.heading_credited'] = 'Anerkannt';
+$lang['ClientTransactions.index.heading_amount'] = 'Betrag';
+$lang['ClientTransactions.index.heading_type'] = 'Typ';
+$lang['ClientTransactions.index.category_declined'] = 'Abgelehnt';
+$lang['ClientTransactions.index.category_pending'] = 'Anhängig';
+$lang['ClientTransactions.index.category_approved'] = 'Genehmigt';
+$lang['ClientTransactions.index.boxtitle_transactions'] = 'Transaktionen';
+$lang['ClientTransactions.index.page_title'] = 'Kunde #%1$s Transaktionen';
+
