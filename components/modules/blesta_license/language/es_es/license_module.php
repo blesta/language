f88e3a14b03cfaeb3bc3d7ba6ac1a36a398ce6ef
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:43 -0800

    Updated es_es language files

---
+++ components/modules/blesta_license/language/es_es/license_module.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * License Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Package.!error.meta[pricing_id].format'] = 'Seleccione una licencia válida.';
+$lang['BlestaLicense.module_row.name'] = 'Revendedor';
+$lang['BlestaLicense.module_rows.name'] = 'Revendedores';
+$lang['BlestaLicense.add_module_row'] = 'Añadir revendedor';
+$lang['BlestaLicense.manage.module_rows_title'] = 'Revendedores';
+$lang['BlestaLicense.manage.module_rows_heading.username'] = 'Nombre de usuario';
+$lang['BlestaLicense.manage.module_rows_heading.options'] = 'Opciones';
+$lang['BlestaLicense.manage.module_rows.edit'] = 'Editar';
+$lang['BlestaLicense.manage.module_rows.delete'] = 'Borrar';
+$lang['BlestaLicense.manage.module_rows.confirm_delete'] = '¿Está seguro de que desea eliminar este revendedor?';
+$lang['BlestaLicense.manage.module_rows_no_results'] = 'No hay revendedores.';
+$lang['BlestaLicense.!tooltip.username'] = 'Introduzca el nombre de usuario de su cuenta Blesta.';
+$lang['BlestaLicense.!tooltip.password'] = 'Introduce la contraseña de tu cuenta Blesta.';
+$lang['BlestaLicense.row_meta.username'] = 'Nombre de usuario';
+$lang['BlestaLicense.row_meta.password'] = 'Contraseña';
+$lang['BlestaLicense.row_meta.test_mode'] = 'Modo de prueba';
+$lang['BlestaLicense.add_row.box_title'] = 'Añadir revendedor';
+$lang['BlestaLicense.add_row.basic_title'] = 'Ajustes básicos';
+$lang['BlestaLicense.add_row.add_btn'] = 'Añadir revendedor';
+$lang['BlestaLicense.edit_row.box_title'] = 'Editar revendedor';
+$lang['BlestaLicense.edit_row.basic_title'] = 'Ajustes básicos';
+$lang['BlestaLicense.edit_row.edit_btn'] = 'Actualizar revendedor';
+$lang['Package.getpricings.pricing_id'] = '%1$s (ID %2$s)';
+$lang['Package.package_fields.pricing_id'] = 'Licencia';
+$lang['BlestaLicense.tab_reissue'] = 'Reemitir licencia';
+$lang['BlestaLicense.tab_reissue.heading_reissue'] = 'Reedición';
+$lang['BlestaLicense.tab_reissue.description'] = 'La reemisión de la licencia permitirá instalarla o trasladarla a otra ubicación, donde quedará bloqueada.';
+$lang['BlestaLicense.tab_reissue.field_submit'] = 'Reedición';
+
