c63a98d5ad46badc7bcf7e8ed3ac1487bbaa187c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:12 -0800

    Updated cs_cz language files

---
+++ core/Util/Filters/language/cs_cz/quotation_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Quotation Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.quotation_filters.any'] = 'Jakýkoli';
+$lang['Util.filters.quotation_filters.field_currency'] = 'Měna';
+$lang['Util.filters.quotation_filters.field_quotation_line'] = 'Obsah položky';
+$lang['Util.filters.quotation_filters.field_quotation_number'] = 'Citace #';
+
