11c0f3fe00304702db99d982d41572676eb1bc7c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:03 -0800

    Updated de_de language files

---
+++ core/Pricing/Modifier/Type/Description/language/de_de/discount.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Discount
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Discount.description.coupon.amount'] = 'Coupon %1$s';
+$lang['Discount.description.coupon.percent'] = 'Coupon %1$s (%2$s%%)';
+
