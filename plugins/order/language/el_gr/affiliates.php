d6db0a951b0cb189e571d98601a490f5bea808ec
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:20 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/affiliates.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Affiliates.!success.affiliate_added'] = 'Ο λογαριασμός σας ως συνεργάτης έχει δημιουργηθεί.';
+$lang['Affiliates.tabs.tab_overview'] = 'Επισκόπηση θυγατρικών';
+$lang['Affiliates.tabs.tab_payouts'] = 'Πληρωμές';
+$lang['Affiliates.tabs.tab_referrals'] = 'Παραπομπές';
+$lang['Affiliates.tabs.tab_dashboard'] = 'Επιστροφή στο ταμπλό';
+$lang['Affiliates.index.boxtitle_affiliates'] = 'Επισκόπηση θυγατρικών';
+$lang['Affiliates.index.title_details'] = 'Λεπτομέρειες';
+$lang['Affiliates.index.title_referral'] = 'Σύνδεσμος παραπομπής';
+$lang['Affiliates.index.visits'] = 'Clicks';
+$lang['Affiliates.index.monthly_visits'] = '%1$s/mo κατά μέσο όρο';
+$lang['Affiliates.index.sales'] = 'Συνολικές πωλήσεις';
+$lang['Affiliates.index.monthly_sales'] = '%1$s/mo κατά μέσο όρο';
+$lang['Affiliates.index.conversion_rate'] = 'Ποσοστό μετατροπής';
+$lang['Affiliates.index.conversion_fraction'] = '%1$s/%2$s κατά μέσο όρο';
+$lang['Affiliates.index.commission_type'] = 'Επίπεδο πληρωμών';
+$lang['Affiliates.index.min_withdrawal_amount'] = 'Ελάχιστο ποσό πληρωμής';
+$lang['Affiliates.index.maturity_days'] = 'Ημέρες μέχρι την ωρίμανση';
+$lang['Affiliates.index.available_payout'] = 'Διαθέσιμο για πληρωμή';
+$lang['Affiliates.index.field.commission_fixed'] = '%1$s εφάπαξ για κάθε ώριμη παραπομπή';
+$lang['Affiliates.index.field.commission_percentage'] = '%1$s κάθε ώριμης παραπομπής';
+$lang['Affiliates.index.request_payout'] = 'Αίτημα πληρωμής';
+$lang['Affiliates.index.inactive'] = 'Αυτή η θυγατρική έχει χαρακτηριστεί ανενεργή.';
+$lang['Affiliates.index.activate'] = 'Κάντε κλικ εδώ για να επανενεργοποιηθείτε.';
+$lang['Affiliates.index.text_code_order_form'] = 'Μπορείτε επίσης να συνδεθείτε σε μια συγκεκριμένη σελίδα φόρμας παραγγελίας ως συνεργάτης προσθέτοντας στο τέλος ?a=%1$s σε ένα σύνδεσμο χωρίς παραμέτρους ερωτήματος ή &a=%1$s σε ένα σύνδεσμο με παραμέτρους ερωτήματος.';
+$lang['Affiliates.signup.boxtitle_affiliates'] = 'Εγγραφή θυγατρικών';
+$lang['Affiliates.signup.signup'] = 'Ενεργοποίηση λογαριασμού';
+$lang['Affiliates.signup.login'] = 'Σύνδεση';
+
