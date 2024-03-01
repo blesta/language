a07712a656e353429c3a0eaa61e348b83cc055bb
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:32 -0800

    Updated de_de language files

---
+++ plugins/order/views/templates/wizard/language/de_de/summary.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Summary
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Summary.index.term_years'] = '%1$s Jahre';
+$lang['Summary.index.term_year'] = '%1$s Jahr';
+$lang['Summary.index.term_months'] = '%1$s Monate';
+$lang['Summary.index.term_month'] = '%1$s Monat';
+$lang['Summary.index.term_weeks'] = '%1$s-Wochen';
+$lang['Summary.index.term_week'] = '%1$s Woche';
+$lang['Summary.index.term_days'] = '%1$s Tage';
+$lang['Summary.index.term_day'] = '%1$s Tag';
+$lang['Summary.index.paymethods_note'] = 'Bitte wählen Sie die gewünschte Zahlungsmethode.';
+$lang['Summary.index.heading_paymethods'] = 'Zahlungsmethode';
+$lang['Summary.index.empty_cart'] = 'Warenkorb leeren';
+$lang['Summary.index.table_price'] = 'Preis';
+$lang['Summary.index.table_description'] = 'Beschreibung';
+$lang['Summary.index.summary_note'] = 'Alle anfallenden Steuern werden nach Ihrer Registrierung oder Anmeldung berechnet.';
+$lang['Summary.index.heading_summary'] = 'Zusammenfassung der Bestellung';
+$lang['Summary.index.coupon_btn'] = 'Einlösen';
+$lang['Summary.index.coupon'] = 'Coupon';
+$lang['Summary.index.text_free_domain'] = 'Konfigurieren Sie ein %1$s-Paket für mindestens %2$s, um sich für eine kostenlose Domain zu qualifizieren.';
+$lang['Summary.index.have_coupon'] = 'Verwenden Sie einen Gutscheincode';
+
