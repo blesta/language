c165f4f51a587c262f751a9f6c654000dd7200a6
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:09 -0800

    Updated cs_cz language files

---
+++ core/Util/Filters/language/cs_cz/invoice_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Invoice Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.invoice_filters.any'] = 'Jakýkoli';
+$lang['Util.filters.invoice_filters.field_currency'] = 'Měna';
+$lang['Util.filters.invoice_filters.field_invoice_line'] = 'Obsah položky';
+$lang['Util.filters.invoice_filters.field_invoice_number'] = 'Faktura #';
+
