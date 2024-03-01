e070bde8157aadcc5841c0265bd43f6df8fc6551
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:58 -0800

    Updated it_it language files

---
+++ language/it_it/coupon_terms.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Coupon Terms
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CouponTerms.getperiods.onetime'] = 'Una sola volta';
+$lang['CouponTerms.getperiods.year'] = 'Anno';
+$lang['CouponTerms.getperiods.month'] = 'Mese';
+$lang['CouponTerms.getperiods.week'] = 'Settimana';
+$lang['CouponTerms.getperiods.day'] = 'Giorno';
+$lang['CouponTerms.!error.term_id.exists'] = 'Il termine coupon indicato non esiste.';
+$lang['CouponTerms.!error.period.valid'] = 'Inserire un periodo valido.';
+$lang['CouponTerms.!error.term.format'] = 'Inserire un termine valido per ogni periodo.';
+$lang['CouponTerms.!error.coupon_id.exists'] = 'Il coupon indicato non esiste.';
+
