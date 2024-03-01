b6daa2ec6457c3079bdd78b421ed7b695cbb5fc2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:46 -0800

    Updated el_gr language files

---
+++ components/modules/generic_domains/language/el_gr/generic_domains.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Generic Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GenericDomains.manage.text'] = 'Το προσωπικό μπορεί να λαμβάνει αντίγραφα BCC των μηνυμάτων ηλεκτρονικού ταχυδρομείου δημιουργίας, αναστολής και ακύρωσης υπηρεσιών, εάν η ομάδα προσωπικού έχει ορίσει αυτές τις ειδοποιήσεις BCC και το μέλος του προσωπικού έχει εγγραφεί σε αυτές στην ενότητα [Τα στοιχεία μου] -> [Ειδοποιήσεις].';
+$lang['GenericDomains.package_fields.tld_options'] = 'TLDs';
+$lang['GenericDomains.transfer.transfer_key'] = 'Κωδικός EPP';
+$lang['GenericDomains.transfer.domain'] = 'Τομέας';
+$lang['GenericDomains.domain.domain'] = 'Τομέας';
+$lang['GenericDomains.module_row_plural'] = 'Σειρές ενότητας';
+$lang['GenericDomains.module_row'] = 'Σειρά ενότητας';
+$lang['GenericDomains.description'] = 'Μια γενική ενότητα για την καταχώριση domain.';
+$lang['GenericDomains.name'] = 'Γενικοί τομείς';
+
