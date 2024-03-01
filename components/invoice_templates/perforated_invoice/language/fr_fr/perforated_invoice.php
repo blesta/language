6516200c384f2d15298529618d1f43540f94980c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:43 -0800

    Updated fr_fr language files

---
+++ components/invoice_templates/perforated_invoice/language/fr_fr/perforated_invoice.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Perforated Invoice
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PerforatedInvoice.name'] = 'Perforé';
+$lang['PerforatedInvoice.page_of'] = 'Page %1$s de %2$s';
+$lang['PerforatedInvoice.terms_heading'] = 'Conditions';
+$lang['PerforatedInvoice.type_active'] = 'FACTURE';
+$lang['PerforatedInvoice.type_draft'] = 'FACTURE - PROJET';
+$lang['PerforatedInvoice.type_void'] = 'INVOICE - VOID';
+$lang['PerforatedInvoice.type_proforma'] = 'FACTURE PRO FORMA';
+$lang['PerforatedInvoice.type_quotation'] = 'CITATION';
+$lang['PerforatedInvoice.type_quotation_draft'] = 'PROJET DE DEVIS';
+$lang['PerforatedInvoice.invoice_id_code'] = 'Numéro de facture :';
+$lang['PerforatedInvoice.proforma_id_code'] = 'Numéro pro forma :';
+$lang['PerforatedInvoice.draft_id_code'] = 'Numéro de projet :';
+$lang['PerforatedInvoice.quotation_id_code'] = 'Numéro de devis :';
+$lang['PerforatedInvoice.client_id_code'] = 'Numéro de client :';
+$lang['PerforatedInvoice.client_name'] = 'Nom du client :';
+$lang['PerforatedInvoice.tax_id'] = 'Numéro d\'identification fiscale/VATIN :';
+$lang['PerforatedInvoice.client_tax_id'] = 'Numéro d\'identification fiscale du client/VATIN :';
+$lang['PerforatedInvoice.date_created'] = 'Date de création :';
+$lang['PerforatedInvoice.date_billed'] = 'Date de la facture :';
+$lang['PerforatedInvoice.date_expires'] = 'Date d\'expiration :';
+$lang['PerforatedInvoice.date_due'] = 'Date d\'échéance :';
+$lang['PerforatedInvoice.notes_heading'] = 'Notes';
+$lang['PerforatedInvoice.subtotal_heading'] = 'Sous-total';
+$lang['PerforatedInvoice.total_heading'] = 'Total';
+$lang['PerforatedInvoice.lines_description'] = 'Description';
+$lang['PerforatedInvoice.lines_quantity'] = 'Quantité';
+$lang['PerforatedInvoice.lines_unit_price'] = 'Prix unitaire';
+$lang['PerforatedInvoice.lines_cost'] = 'Coût';
+$lang['PerforatedInvoice.watermark_paid'] = 'PAYÉ';
+$lang['PerforatedInvoice.payments_heading'] = 'Paiements';
+$lang['PerforatedInvoice.payments_applied_date'] = 'Date d\'application';
+$lang['PerforatedInvoice.payments_type_name'] = 'Type';
+$lang['PerforatedInvoice.payments_transaction_id'] = 'Numéro de transaction';
+$lang['PerforatedInvoice.payments_applied_amount'] = 'Montant appliqué';
+$lang['PerforatedInvoice.balance_heading'] = 'Équilibre';
+$lang['PerforatedInvoice.detach_heading'] = 'DÉTACHER ET RENVOYER AVEC LE PAIEMENT';
+$lang['PerforatedInvoice.detach_remit_to'] = 'Remettre à :';
+$lang['PerforatedInvoice.detach_total_due'] = 'Montant total dû :';
+$lang['PerforatedInvoice.detach_total_payment'] = 'Montant total du paiement :';
+
