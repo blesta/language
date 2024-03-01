fd4cf7fc4d5ae6b360767c6e2aa70c8eea1d783b
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:35 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_system_automation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin System Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemAutomation.index.note_cron_command'] = 'Toto je příklad příkazu cron, který lze použít k vytvoření úlohy cron na serveru. Při nastavování úlohy cronu nezapomeňte aktualizovat příkaz cronu tak, aby ukazoval na místo, kde je PHP nainstalováno, pokud se liší od toho, co je uvedeno v tomto příkladu.';
+$lang['AdminSystemAutomation.index.text_generate_code'] = 'Generování kódu';
+$lang['AdminSystemAutomation.index.text_update_key'] = 'Aktualizace klíče Cron';
+$lang['AdminSystemAutomation.index.text_cron_currently_running'] = 'Cron je právě spuštěn.';
+$lang['AdminSystemAutomation.index.text_cron_never_ran'] = 'Cron se nikdy nespustil.';
+$lang['AdminSystemAutomation.index.text_cron_last_ran'] = 'Cron byl naposledy spuštěn dne %1$s.';
+$lang['AdminSystemAutomation.index.field_cronkey_submit'] = 'Aktualizace klíče Cron';
+$lang['AdminSystemAutomation.index.field_cron_key'] = 'Klíč Cron';
+$lang['AdminSystemAutomation.index.field_runcron'] = 'Ruční spuštění Cronu';
+$lang['AdminSystemAutomation.index.field_croncommand'] = 'Příklad příkazu Cron';
+$lang['AdminSystemAutomation.index.heading_status'] = 'Stav Cron';
+$lang['AdminSystemAutomation.index.boxtitle_automation'] = 'Automatizace';
+$lang['AdminSystemAutomation.index.page_title'] = 'Nastavení > Systém > Automatizace';
+$lang['AdminSystemAutomation.!success.cron_key'] = 'Klíč cron byl úspěšně aktualizován!';
+$lang['AdminSystemAutomation.!success.automation_updated'] = 'Nastavení automatizace bylo úspěšně aktualizováno!';
+$lang['AdminSystemAutomation.!error.empty_cron_key'] = 'Nastavte prosím klíč cron.';
+
