5583668d93a9206d0cb9f4f3786154de3ea278e8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:28 -0800

    Updated de_de language files

---
+++ language/de_de/admin_company_taxes.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Admin Company Taxes
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyTaxes.edit.field.editsubmit'] = 'Regel bearbeiten';
+$lang['AdminCompanyTaxes.edit.field.state'] = 'Staat/Provinz';
+$lang['AdminCompanyTaxes.edit.field.country'] = 'Land';
+$lang['AdminCompanyTaxes.edit.field.amount'] = 'Betrag';
+$lang['AdminCompanyTaxes.edit.field.name'] = 'Name der Steuer';
+$lang['AdminCompanyTaxes.edit.field.level2'] = 'Stufe 2';
+$lang['AdminCompanyTaxes.edit.field.level1'] = 'Stufe 1';
+$lang['AdminCompanyTaxes.edit.field.level'] = 'Steuerliche Ebene';
+$lang['AdminCompanyTaxes.edit.field.type'] = 'Steuerart';
+$lang['AdminCompanyTaxes.edit.boxtitle_edit'] = 'Steuervorschrift bearbeiten';
+$lang['AdminCompanyTaxes.edit.page_title'] = 'Einstellungen > Unternehmen > Steuern > Steuerregel bearbeiten';
+$lang['AdminCompanyTaxes.add.field.addsubmit'] = 'Regel erstellen';
+$lang['AdminCompanyTaxes.add.field.state'] = 'Staat/Provinz';
+$lang['AdminCompanyTaxes.add.field.country'] = 'Land';
+$lang['AdminCompanyTaxes.add.field.amount'] = 'Betrag';
+$lang['AdminCompanyTaxes.add.field.name'] = 'Name der Steuer';
+$lang['AdminCompanyTaxes.add.field.level2'] = 'Stufe 2';
+$lang['AdminCompanyTaxes.add.field.level1'] = 'Stufe 1';
+$lang['AdminCompanyTaxes.add.field.level'] = 'Steuerliche Ebene';
+$lang['AdminCompanyTaxes.add.field.type'] = 'Steuerart';
+$lang['AdminCompanyTaxes.add.boxtitle_add'] = 'Steuervorschrift hinzufügen';
+$lang['AdminCompanyTaxes.add.page_title'] = 'Einstellungen > Unternehmen > Steuern > Steuerregel hinzufügen';
+$lang['AdminCompanyTaxes.rules.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Steuerregel löschen wollen?';
+$lang['AdminCompanyTaxes.rules.option_delete'] = 'Löschen';
+$lang['AdminCompanyTaxes.rules.option_edit'] = 'bearbeiten';
+$lang['AdminCompanyTaxes.rules.text_all'] = 'Alle';
+$lang['AdminCompanyTaxes.rules.text_options'] = 'Optionen';
+$lang['AdminCompanyTaxes.rules.text_state'] = 'Staat/Provinz';
+$lang['AdminCompanyTaxes.rules.text_country'] = 'Land';
+$lang['AdminCompanyTaxes.rules.text_amount'] = 'Betrag';
+$lang['AdminCompanyTaxes.rules.text_type'] = 'Typ';
+$lang['AdminCompanyTaxes.rules.text_name'] = 'Name';
+$lang['AdminCompanyTaxes.rules.heading_level2'] = 'Level 2 Regeln';
+$lang['AdminCompanyTaxes.rules.heading_level1'] = 'Level 1 Regeln';
+$lang['AdminCompanyTaxes.rules.boxtitle_rules'] = 'Steuerliche Vorschriften';
+$lang['AdminCompanyTaxes.rules.categorylink_addrule'] = 'Steuervorschrift hinzufügen';
+$lang['AdminCompanyTaxes.rules.no_results'] = 'Es gibt keine Steuerregeln der Stufe %1$s.';
+$lang['AdminCompanyTaxes.rules.page_title'] = 'Einstellungen > Unternehmen > Steuern > Steuerregeln';
+$lang['AdminCompanyTaxes.basic.field_addsubmit'] = 'Einstellungen aktualisieren';
+$lang['AdminCompanyTaxes.basic.heading_tax_provider'] = '%1$s Einstellungen';
+$lang['AdminCompanyTaxes.basic.field_taxid'] = 'Steuer-ID/VATIN';
+$lang['AdminCompanyTaxes.basic.note_cancelation_fee_tax'] = 'Wenn diese Option aktiviert ist, werden die Stornogebühren besteuert.';
+$lang['AdminCompanyTaxes.basic.field_cancelation_fee_tax'] = 'Gebühren für die Stornierung von Steuern';
+$lang['AdminCompanyTaxes.basic.note_setup_fee_tax'] = 'Falls aktiviert, werden alle Einrichtungsgebühren besteuert.';
+$lang['AdminCompanyTaxes.basic.field_setup_fee_tax'] = 'Gebühren für die Einrichtung der Steuer';
+$lang['AdminCompanyTaxes.basic.note_cascade_tax'] = 'Wenn diese Option aktiviert ist, wird zunächst die Steuerstufe 1 auf den Gesamtbetrag der Rechnung erhoben, und die Steuerstufe 2 würde auf diesen neuen Gesamtbetrag einschließlich der Steuerstufe 1 erhoben werden. Dies führt zu einer Steuer auf die Steuer. Andernfalls werden Steuerstufe 1 und Steuerstufe 2 nur auf die Rechnungssumme vor Steuern erhoben.';
+$lang['AdminCompanyTaxes.basic.field_cascade_tax'] = 'Kaskadensteuer';
+$lang['AdminCompanyTaxes.basic.note_enable_tax'] = 'Aktivieren Sie diese Option, um die Steuer für dieses Unternehmen zu aktivieren.';
+$lang['AdminCompanyTaxes.basic.field_enable_tax'] = 'Steuer aktivieren';
+$lang['AdminCompanyTaxes.basic.heading_general'] = 'Allgemeine Einstellungen';
+$lang['AdminCompanyTaxes.basic.boxtitle_basic'] = 'Grundlegende Steuereinstellungen';
+$lang['AdminCompanyTaxes.basic.page_title'] = 'Einstellungen > Unternehmen > Steuern > Grundlegende Steuereinstellungen';
+$lang['AdminCompanyTaxes.!tooltip.state'] = 'Wählen Sie den Staat/die Provinz aus, für den/die diese Steuerregel gilt.';
+$lang['AdminCompanyTaxes.!tooltip.country'] = 'Wählen Sie das Land aus, für das diese Steuerregel gilt.';
+$lang['AdminCompanyTaxes.!tooltip.amount'] = 'Der Steuerbetrag in Prozent.';
+$lang['AdminCompanyTaxes.!tooltip.name'] = 'Der angezeigte Name der Steuer (z. B. Sales Tax).';
+$lang['AdminCompanyTaxes.!tooltip.level'] = 'Die Steuerebene ermöglicht es Ihnen, die Reihenfolge der Steuern festzulegen, wenn mehrere Steuervorschriften gelten.';
+$lang['AdminCompanyTaxes.!tooltip.type'] = 'Inklusive (Additiv): Die Steuer wird als Teil des von Ihnen festgelegten Artikelpreises berechnet und für steuerbefreite Benutzer vom Artikelpreis abgezogen.<br/>
+Inklusive (Additiv) berechnet die Steuer zusätzlich zu den von Ihnen festgelegten Artikelpreisen.<br/>
+Exklusive (Additiv) berechnet die Steuer zusätzlich zu den von Ihnen eingestellten Artikelpreisen, berücksichtigt sie aber nicht in der Anzeige der Bestellsumme.';
+$lang['AdminCompanyTaxes.states.all'] = '-- Alle --';
+$lang['AdminCompanyTaxes.countries.all'] = '-- Alle --';
+$lang['AdminCompanyTaxes.!success.rule_deleted'] = 'Die Steuerregel wurde erfolgreich gelöscht.';
+$lang['AdminCompanyTaxes.!success.taxrule_updated'] = 'Die Steuerregel wurde erfolgreich aktualisiert!';
+$lang['AdminCompanyTaxes.!success.taxrule_created'] = 'Die Steuerregel ist erfolgreich erstellt worden!';
+$lang['AdminCompanyTaxes.!success.basic_updated'] = 'Die Grundsteuereinstellungen wurden erfolgreich aktualisiert!';
+
