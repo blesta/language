78650f5831907c975c08893d964f2410bd5b5102
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:27 -0800

    Updated es_es language files

---
+++ plugins/order/components/antifraud/maxmind_v2/language/es_es/maxmind_v2.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Maxmind V2
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MaxmindV2.!error.reject.private_ip'] = 'No se aceptan pedidos desde IP privadas.';
+$lang['MaxmindV2.!error.review.private_ip'] = 'Se están revisando los pedidos de IP privadas.';
+$lang['MaxmindV2.!error.reject.reject_score'] = 'Su pedido no puede ser procesado en este momento.';
+$lang['MaxmindV2.!error.review.review_score'] = 'Su pedido debe ser revisado manualmente.';
+$lang['MaxmindV2.!error.reject.free_email'] = 'No se aceptan pedidos con cuentas de correo electrónico gratuitas.';
+$lang['MaxmindV2.!error.review.free_email'] = 'Se están revisando los pedidos con cuentas de correo electrónico gratuitas.';
+$lang['MaxmindV2.!error.reject.country_mismatch'] = 'No se aceptan pedidos que no coincidan con el país.';
+$lang['MaxmindV2.!error.review.country_mismatch'] = 'Se están revisando los pedidos con discordancia de países.';
+$lang['MaxmindV2.!error.reject.risky_country'] = 'No se aceptan pedidos de países de riesgo.';
+$lang['MaxmindV2.!error.review.risky_country'] = 'Se están revisando los pedidos procedentes de países de riesgo.';
+$lang['MaxmindV2.!error.reject.anon_proxy'] = 'No se aceptan pedidos de apoderados anónimos.';
+$lang['MaxmindV2.!error.review.anon_proxy'] = 'Se están revisando los pedidos de apoderados anónimos.';
+$lang['MaxmindV2.settings.tooltip.enable_js'] = 'Esta opción inserta un código JavaScript en todos los formularios de pedido, que permite que MaxMind recopile y rastree información adicional del usuario. Activar esta opción mejora la precisión de los resultados.';
+$lang['MaxmindV2.settings.field_server'] = 'Servidor';
+$lang['MaxmindV2.settings.field_account_id'] = 'ID de cuenta';
+$lang['MaxmindV2.settings.field_key'] = 'Clave de licencia';
+$lang['MaxmindV2.settings.field_minfraud_api'] = 'API minFraude';
+$lang['MaxmindV2.settings.field_reject_score'] = 'Puntuación mínima para rechazar';
+$lang['MaxmindV2.settings.field_review_score'] = 'Puntuación mínima para revisar';
+$lang['MaxmindV2.settings.field_free_email'] = 'Correo electrónico gratuito';
+$lang['MaxmindV2.settings.field_country_mismatch'] = 'Desajuste de países';
+$lang['MaxmindV2.settings.field_risky_country'] = 'País de riesgo';
+$lang['MaxmindV2.settings.field_anon_proxy'] = 'Proxy anónimo';
+$lang['MaxmindV2.settings.field_private_ip'] = 'IP privada';
+$lang['MaxmindV2.settings.field_enable_js'] = 'Activar el complemento de seguimiento de JavaScript';
+$lang['MaxmindV2.settings.option_score'] = 'Puntuación mínFraude';
+$lang['MaxmindV2.settings.option_insights'] = 'Perspectivas minFraude';
+$lang['MaxmindV2.settings.option_factors'] = 'Factores minFraude';
+$lang['MaxmindV2.settings.option_allow'] = 'Permitir';
+$lang['MaxmindV2.settings.option_review'] = 'Consulte';
+$lang['MaxmindV2.settings.option_reject'] = 'Rechazar';
+$lang['MaxmindV2.settings.option_enable'] = 'Activar';
+$lang['MaxmindV2.settings.option_disable'] = 'Desactivar';
+
