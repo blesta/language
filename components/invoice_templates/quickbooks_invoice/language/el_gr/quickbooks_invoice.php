31cb61f36f22cac1a408195989bd2815c97658ee
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:40 -0800

    Updated el_gr language files

---
+++ components/invoice_templates/quickbooks_invoice/language/el_gr/quickbooks_invoice.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Quickbooks Invoice
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['QuickbooksInvoice.balance_heading'] = 'Υπόλοιπο';
+$lang['QuickbooksInvoice.payments_applied_amount'] = 'Εφαρμοσμένο ποσό';
+$lang['QuickbooksInvoice.payments_transaction_id'] = 'Αριθμός συναλλαγής';
+$lang['QuickbooksInvoice.payments_type_name'] = 'Τύπος';
+$lang['QuickbooksInvoice.payments_applied_date'] = 'Ημερομηνία αίτησης';
+$lang['QuickbooksInvoice.payments_heading'] = 'Πληρωμές';
+$lang['QuickbooksInvoice.watermark_paid'] = 'ΠΛΗΡΩΜΗ';
+$lang['QuickbooksInvoice.client_tax_id'] = 'ΑΦΜ/ΦΠΑ πελάτη: %1$s';
+$lang['QuickbooksInvoice.tax_id'] = 'ΑΦΜ/ΦΠΑ: %1$s';
+$lang['QuickbooksInvoice.address_city_state'] = '%1$s, %2$s %3$s %4$s';
+$lang['QuickbooksInvoice.address_heading'] = 'Bill To';
+$lang['QuickbooksInvoice.lines_cost'] = 'Κόστος';
+$lang['QuickbooksInvoice.lines_unit_price'] = 'Μονάδα Λιανική τιμή';
+$lang['QuickbooksInvoice.lines_quantity'] = 'Ποσότητα';
+$lang['QuickbooksInvoice.lines_description'] = 'Περιγραφή';
+$lang['QuickbooksInvoice.total_heading'] = 'Σύνολο';
+$lang['QuickbooksInvoice.subtotal_heading'] = 'Υποσύνολο';
+$lang['QuickbooksInvoice.notes_heading'] = 'Σημειώσεις';
+$lang['QuickbooksInvoice.date_due'] = 'Ημερομηνία λήξης';
+$lang['QuickbooksInvoice.date_expires'] = 'Ημερομηνία λήξης';
+$lang['QuickbooksInvoice.date_billed'] = 'Ημερομηνία';
+$lang['QuickbooksInvoice.date_created'] = 'Ημερομηνία δημιουργίας';
+$lang['QuickbooksInvoice.client_id_code'] = 'Αριθμός πελάτη.';
+$lang['QuickbooksInvoice.quotation_id_code'] = 'Αριθμός προσφοράς.';
+$lang['QuickbooksInvoice.draft_id_code'] = 'Σχέδιο αριθ.';
+$lang['QuickbooksInvoice.proforma_id_code'] = 'Pro Forma αριθ.';
+$lang['QuickbooksInvoice.invoice_id_code'] = 'Αριθμός τιμολογίου.';
+$lang['QuickbooksInvoice.type_quotation_draft'] = 'ΣΧΈΔΙΟ ΕΙΣΑΓΩΓΙΚΏΝ';
+$lang['QuickbooksInvoice.type_quotation'] = 'QUOTATION';
+$lang['QuickbooksInvoice.type_proforma'] = 'PRO FORMA ΤΙΜΟΛΌΓΙΟ';
+$lang['QuickbooksInvoice.type_void'] = 'ΤΙΜΟΛΟΓΙΟ - ΑΚΥΡΟ';
+$lang['QuickbooksInvoice.type_draft'] = 'ΤΙΜΟΛΌΓΙΟ - ΣΧΈΔΙΟ';
+$lang['QuickbooksInvoice.type_active'] = 'ΤΙΜΟΛΟΓΙΟ';
+$lang['QuickbooksInvoice.terms_heading'] = 'Όροι';
+$lang['QuickbooksInvoice.page_of'] = 'Σελίδα %1$s της %2$s';
+$lang['QuickbooksInvoice.name'] = 'Quickbooks';
+
