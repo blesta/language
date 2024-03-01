da12168fc5b2ac031d21e652128b8eeda5894b5b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:40 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/order_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Order Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderSettings.!error.embed_code.parse'] = 'Erreur d\'analyse du code intégré : %1$s';
+$lang['OrderSettings.getantifraud.fraudlabspro'] = 'FraudLabs Pro';
+$lang['OrderSettings.getantifraud.maxmind'] = 'L\'héritage MaxMind';
+$lang['OrderSettings.getantifraud.maxmind_v2'] = 'MaxMind v2';
+
