731a3c570549e009318ba160ee3566715bd2ff84
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:32 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/checkout.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Checkout
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Checkout.!error.invalid_agree_tos'] = 'Vous devez reconnaître que vous acceptez les conditions générales.';
+$lang['Checkout.!error.no_payment_info'] = 'Vous devez sélectionner un mode de paiement pour continuer.';
+$lang['Checkout.!error.not_client_owner'] = 'Vous devez être connecté en tant que propriétaire du client pour continuer.';
+$lang['Checkout.!error.payment_authorize'] = 'Le paiement n\'a pas pu être autorisé pour le compte de paiement sélectionné.  Veuillez sélectionner un autre compte ou réessayer.';
+$lang['Checkout.!info.unverified_email'] = 'Un lien a été envoyé à l\'adresse électronique que vous avez fournie. Veuillez cliquer sur le lien contenu dans l\'e-mail pour vérifier votre adresse e-mail avant de procéder au paiement.';
+$lang['Checkout.!info.unverified_email_button'] = 'Renvoyer l\'e-mail de vérification';
+$lang['Checkout.!info.ach_verification'] = 'Vous devez vérifier ce compte avant de pouvoir l\'utiliser pour effectuer un paiement. Ce processus vous redirigera hors de la page actuelle.';
+$lang['Checkout.index.description_invoice'] = 'Facture #%1$s';
+$lang['Checkout.setcontactview.text_none'] = 'Aucun';
+$lang['Checkout.getpaymentaccounts.paymentaccount_cc'] = 'Comptes de cartes de crédit';
+$lang['Checkout.getpaymentaccounts.paymentaccount_ach'] = 'Comptes ACH';
+$lang['Checkout.getpaymentaccounts.account_name'] = '%1$s %2$s - %3$s x%4$s';
+$lang['Checkout.index.field_agree_tos'] = 'J\'ai lu et j\'accepte les <a href="%1$s" target="_blank">Termes et conditions</a>';
+$lang['Checkout.index.totals.setup_fee'] = 'Frais de mise en place :';
+$lang['Checkout.index.totals.discount'] = 'Remise :';
+$lang['Checkout.index.totals.subtotal'] = 'Sous-total :';
+$lang['Checkout.index.totals.tax'] = '%1$s (%2$s%%) :';
+$lang['Checkout.index.totals.credit'] = 'Crédit :';
+$lang['Checkout.index.totals.paid'] = 'Payé :';
+$lang['Checkout.index.totals.total'] = 'Total dû :';
+$lang['Checkout.contact_info.field_first_name'] = 'Prénom';
+$lang['Checkout.contact_info.field_last_name'] = 'Nom de famille';
+$lang['Checkout.contact_info.field_address1'] = 'Adresse 1';
+$lang['Checkout.contact_info.field_address2'] = 'Adresse 2';
+$lang['Checkout.contact_info.field_city'] = 'Ville';
+$lang['Checkout.contact_info.field_country'] = 'Pays';
+$lang['Checkout.contact_info.field_state'] = 'État';
+$lang['Checkout.contact_info.field_zip'] = 'Code postal';
+$lang['Checkout.contact_info.field_email'] = 'Courriel';
+$lang['Checkout.cc_info.field_number'] = 'Nombre';
+$lang['Checkout.cc_info.field_security'] = 'Code de sécurité';
+$lang['Checkout.cc_info.field_expiration'] = 'Date d\'expiration';
+$lang['Checkout.cc_info.field_savedetails'] = 'Sauvegarder les détails du paiement';
+$lang['Checkout.cc_info.tooltip_savedetails'] = 'Les données de paiement enregistrées peuvent être utilisées pour effectuer un paiement à l\'avenir sans avoir à les saisir à chaque fois.';
+$lang['Checkout.cc_info.tooltip_code'] = 'Le code de sécurité à 3 ou 4 chiffres, qui se trouve généralement au dos de la carte.';
+$lang['Checkout.ach_info.field_type'] = 'Type';
+$lang['Checkout.ach_info.field_accountnum'] = 'Numéro de compte';
+$lang['Checkout.ach_info.field_routingnum'] = 'Numéro de routage';
+$lang['Checkout.ach_info.field_savedetails'] = 'Sauvegarder les détails du paiement';
+$lang['Checkout.ach_info.tooltip_savedetails'] = 'Les données de paiement enregistrées peuvent être utilisées pour effectuer un paiement à l\'avenir sans avoir à les saisir à chaque fois.';
+$lang['Checkout.complete.order_number'] = 'Commande #%1$s';
+$lang['Checkout.complete.complete_description'] = 'Merci pour votre commande ! Votre commande est maintenant terminée.';
+$lang['Checkout.complete.almost_complete_description'] = 'Votre commande a été reçue, en attendant votre paiement ci-dessous.';
+$lang['Checkout.complete.table_description'] = 'Description';
+$lang['Checkout.complete.table_qty'] = 'Quantité';
+$lang['Checkout.complete.table_price'] = 'Prix unitaire';
+$lang['Checkout.complete.quantity'] = '%1$s @';
+$lang['Checkout.complete.totals.subtotal'] = 'Sous-total :';
+$lang['Checkout.complete.totals.tax'] = '%1$s (%2$s%%) :';
+$lang['Checkout.complete.totals.total'] = 'Total dû :';
+$lang['Checkout.complete.totals.total_paid'] = 'Total payé :';
+
