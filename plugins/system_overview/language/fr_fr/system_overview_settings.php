f32680b398050abe5d4120ee7e04d555ab97f063
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:14 -0800

    Updated fr_fr language files

---
+++ plugins/system_overview/language/fr_fr/system_overview_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * System Overview Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SystemOverviewSettings.!error.staff_id.exists'] = 'L\'ID du personnel n\'est pas valide.';
+$lang['SystemOverviewSettings.!error.company_id.exists'] = 'ID de l\'entreprise non valide.';
+$lang['SystemOverviewSettings.!error.settings[][key].empty'] = 'Il se peut qu\'aucune touche de réglage ne soit vide.';
+$lang['SystemOverviewSettings.!error.settings[][value].length'] = 'La longueur de chaque valeur de réglage individuelle ne doit pas dépasser 255 caractères.';
+
