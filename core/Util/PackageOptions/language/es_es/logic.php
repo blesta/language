b9723c20a08148460461212069bfcd5b6f43b7c1
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:40 -0800

    Updated es_es language files

---
+++ core/Util/PackageOptions/language/es_es/logic.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Logic
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.packageoptions.logic.invalid_option'] = 'No se cumplieron los conjuntos de condiciones para permitir la opción configurable %1$s. No debe enviarse.';
+
