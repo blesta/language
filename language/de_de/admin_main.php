d8cd88555b098f141e7eeb4e93bba85429cc8039
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:30 -0800

    Updated de_de language files

---
+++ language/de_de/admin_main.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.editevent.confirm_delete'] = 'Diese Veranstaltung wirklich löschen?';
+$lang['AdminMain.editevent.field_deleteeventsubmit'] = 'Ereignis löschen';
+$lang['AdminMain.editevent.field_editeventsubmit'] = 'Ereignis aktualisieren';
+$lang['AdminMain.editevent.field_all_day'] = 'Ganztägig';
+$lang['AdminMain.editevent.field_shared'] = 'Dieses Ereignis für andere Mitarbeiter sichtbar machen';
+$lang['AdminMain.editevent.field_end_date'] = 'Enddatum';
+$lang['AdminMain.editevent.field_start_date'] = 'Datum des Beginns';
+$lang['AdminMain.editevent.field_title'] = 'Titel';
+$lang['AdminMain.editevent.boxtitle_editevent'] = 'Ereignis bearbeiten';
+$lang['AdminMain.addevent.field_addeventsubmit'] = 'Ereignis erstellen';
+$lang['AdminMain.addevent.field_all_day'] = 'Ganztägig';
+$lang['AdminMain.addevent.field_shared'] = 'Dieses Ereignis für andere Mitarbeiter sichtbar machen';
+$lang['AdminMain.addevent.field_end_date'] = 'Enddatum';
+$lang['AdminMain.addevent.field_start_date'] = 'Datum des Beginns';
+$lang['AdminMain.addevent.field_title'] = 'Titel';
+$lang['AdminMain.addevent.boxtitle_addevent'] = 'Ereignis erstellen';
+$lang['AdminMain.getEvents.shared_event_title'] = '%2$s %3$s: %1$s';
+$lang['AdminMain.calendar.category_day'] = 'Tag';
+$lang['AdminMain.calendar.category_week'] = 'Woche';
+$lang['AdminMain.calendar.category_month'] = 'Monat';
+$lang['AdminMain.calendar.categorylink_addevent'] = 'Ereignis erstellen';
+$lang['AdminMain.calendar.boxtitle_calendar'] = 'Kalender';
+$lang['AdminMain.calendar.page_title'] = 'Kalender';
+$lang['AdminMain.updatedashboard.text_layout'] = 'Wählen Sie unten ein Layout aus, um Ihr Dashboard individuell zu gestalten.';
+$lang['AdminMain.managewidgets.text_author'] = 'Autor:';
+$lang['AdminMain.managewidgets.text_version'] = 'ver %1$s';
+$lang['AdminMain.managewidgets.text_widgets'] = 'Ziehen Sie Widgets von rechts nach links, um sie zu Ihrem Dashboard hinzuzufügen, oder von links nach rechts, um sie zu entfernen.';
+$lang['AdminMain.index.customize_dashboard'] = 'Dashboard anpassen';
+$lang['AdminMain.index.manage_widgets'] = 'Widgets verwalten';
+$lang['AdminMain.index.no_quicklinks'] = 'Es wurden keine Quick Links gesetzt.';
+$lang['AdminMain.index.heading_quicklinks'] = 'Schnelle Links';
+$lang['AdminMain.index.page_title'] = 'Dashboard';
+$lang['AdminMain.!success.event_deleted'] = 'Das Ereignis wurde erfolgreich gelöscht!';
+$lang['AdminMain.!success.event_edited'] = 'Die Veranstaltung wurde erfolgreich aktualisiert!';
+$lang['AdminMain.!success.event_added'] = 'Das Ereignis wurde erfolgreich hinzugefügt!';
+$lang['AdminMain.!error.event_editable'] = 'Nur der Ersteller des Ereignisses kann dieses Ereignis löschen.';
+$lang['AdminMain.!error.delete_event.staff_id'] = 'Nur der Ersteller des Ereignisses kann dieses Ereignis löschen.';
+
