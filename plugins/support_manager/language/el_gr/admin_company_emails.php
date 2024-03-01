3df32fe446a74fdb813f6cab89ed7f74cc1db98a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:52 -0800

    Updated el_gr language files

---
+++ plugins/support_manager/language/el_gr/admin_company_emails.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_name'] = 'Ενημερωμένο εισιτήριο προσωπικού';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_desc'] = 'Ειδοποίηση που αποστέλλεται σε ένα μέλος του προσωπικού όταν ενημερώνεται ένα εισιτήριο.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_mobile_name'] = 'Ενημέρωση εισιτηρίων προσωπικού (Mobile)';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_mobile_desc'] = 'Κινητή ειδοποίηση που αποστέλλεται σε ένα μέλος του προσωπικού όταν ενημερώνεται ένα εισιτήριο.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_assigned_name'] = 'Εκχωρημένο εισιτήριο προσωπικού';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_assigned_desc'] = 'Ειδοποίηση που αποστέλλεται σε ένα μέλος του προσωπικού όταν του έχει ανατεθεί ένα εισιτήριο.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_received_name'] = 'Εισιτήριο που ελήφθη';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_received_desc'] = 'Ειδοποίηση που αποστέλλεται σε έναν χρήστη όταν λαμβάνεται ένα εισιτήριο.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_updated_name'] = 'Ενημερωμένο εισιτήριο';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_updated_desc'] = 'Ειδοποίηση που αποστέλλεται όταν ενημερώνεται ένα εισιτήριο.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_bounce_name'] = 'Αναπήδηση εισιτηρίων';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_bounce_desc'] = 'Ειδοποίηση που αποστέλλεται όταν ένα εισιτήριο απέτυχε να ανοίξει μέσω email.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_reminder_name'] = 'Υπενθύμιση εισιτηρίων';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_reminder_desc'] = 'Ειδοποίηση που αποστέλλεται όταν ένα εισιτήριο δεν έχει λάβει απάντηση για κάποιο χρονικό διάστημα.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_reminder_name'] = 'Υπενθύμιση εισιτηρίων προσωπικού';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_reminder_desc'] = 'Ειδοποίηση που αποστέλλεται όταν ένα εισιτήριο δεν έχει λάβει απάντηση για κάποιο χρονικό διάστημα.';
+
