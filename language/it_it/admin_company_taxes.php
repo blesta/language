b483ee1d90993db7ae0c500930cb95d07bd8489f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:33 -0800

    Updated it_it language files

---
+++ language/it_it/admin_company_taxes.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Admin Company Taxes
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyTaxes.edit.field.editsubmit'] = 'Regola di modifica';
+$lang['AdminCompanyTaxes.edit.field.state'] = 'Stato/Provincia';
+$lang['AdminCompanyTaxes.edit.field.country'] = 'Paese';
+$lang['AdminCompanyTaxes.edit.field.amount'] = 'Importo';
+$lang['AdminCompanyTaxes.edit.field.name'] = 'Nome dell\'imposta';
+$lang['AdminCompanyTaxes.edit.field.level2'] = 'Livello 2';
+$lang['AdminCompanyTaxes.edit.field.level1'] = 'Livello 1';
+$lang['AdminCompanyTaxes.edit.field.level'] = 'Livello di tassazione';
+$lang['AdminCompanyTaxes.edit.field.type'] = 'Tipo di imposta';
+$lang['AdminCompanyTaxes.edit.boxtitle_edit'] = 'Modifica della regola fiscale';
+$lang['AdminCompanyTaxes.edit.page_title'] = 'Impostazioni > Azienda > Imposte > Modifica regola fiscale';
+$lang['AdminCompanyTaxes.add.field.addsubmit'] = 'Crea regola';
+$lang['AdminCompanyTaxes.add.field.state'] = 'Stato/Provincia';
+$lang['AdminCompanyTaxes.add.field.country'] = 'Paese';
+$lang['AdminCompanyTaxes.add.field.amount'] = 'Importo';
+$lang['AdminCompanyTaxes.add.field.name'] = 'Nome dell\'imposta';
+$lang['AdminCompanyTaxes.add.field.level2'] = 'Livello 2';
+$lang['AdminCompanyTaxes.add.field.level1'] = 'Livello 1';
+$lang['AdminCompanyTaxes.add.field.level'] = 'Livello di tassazione';
+$lang['AdminCompanyTaxes.add.field.type'] = 'Tipo di imposta';
+$lang['AdminCompanyTaxes.add.boxtitle_add'] = 'Aggiungi regola fiscale';
+$lang['AdminCompanyTaxes.add.page_title'] = 'Impostazioni > Azienda > Imposte > Aggiungi regola fiscale';
+$lang['AdminCompanyTaxes.rules.confirm_delete'] = 'È sicuro di voler eliminare questa regola fiscale?';
+$lang['AdminCompanyTaxes.rules.option_delete'] = 'Cancellare';
+$lang['AdminCompanyTaxes.rules.option_edit'] = 'Modifica';
+$lang['AdminCompanyTaxes.rules.text_all'] = 'Tutti';
+$lang['AdminCompanyTaxes.rules.text_options'] = 'Opzioni';
+$lang['AdminCompanyTaxes.rules.text_state'] = 'Stato/Provincia';
+$lang['AdminCompanyTaxes.rules.text_country'] = 'Paese';
+$lang['AdminCompanyTaxes.rules.text_amount'] = 'Importo';
+$lang['AdminCompanyTaxes.rules.text_type'] = 'Tipo';
+$lang['AdminCompanyTaxes.rules.text_name'] = 'Nome';
+$lang['AdminCompanyTaxes.rules.heading_level2'] = 'Regole di livello 2';
+$lang['AdminCompanyTaxes.rules.heading_level1'] = 'Regole del livello 1';
+$lang['AdminCompanyTaxes.rules.boxtitle_rules'] = 'Regole fiscali';
+$lang['AdminCompanyTaxes.rules.categorylink_addrule'] = 'Aggiungi regola fiscale';
+$lang['AdminCompanyTaxes.rules.no_results'] = 'Non esistono regole fiscali di livello %1$s.';
+$lang['AdminCompanyTaxes.rules.page_title'] = 'Impostazioni > Azienda > Imposte > Regole fiscali';
+$lang['AdminCompanyTaxes.basic.field_addsubmit'] = 'Aggiornamento delle impostazioni';
+$lang['AdminCompanyTaxes.basic.heading_tax_provider'] = '%1$s Impostazioni';
+$lang['AdminCompanyTaxes.basic.field_taxid'] = 'Codice fiscale/VATIN';
+$lang['AdminCompanyTaxes.basic.note_cancelation_fee_tax'] = 'Se abilitato, le spese di annullamento saranno tassate.';
+$lang['AdminCompanyTaxes.basic.field_cancelation_fee_tax'] = 'Tasse di annullamento delle imposte';
+$lang['AdminCompanyTaxes.basic.note_setup_fee_tax'] = 'Se abilitato, le spese di installazione saranno tassate.';
+$lang['AdminCompanyTaxes.basic.field_setup_fee_tax'] = 'Tasse per l\'installazione dell\'imposta';
+$lang['AdminCompanyTaxes.basic.note_cascade_tax'] = 'Se abilitato, il livello d\'imposta 1 sarà prima valutato sul totale della fattura e il livello d\'imposta 2 sarà valutato su questo nuovo totale, compreso il livello d\'imposta 1. In questo modo si ottiene un\'imposta su imposta. In questo modo si ottiene un\'imposta su imposta. Al contrario, il livello d\'imposta 1 e il livello d\'imposta 2 sono valutati solo sul totale della fattura prima delle imposte.';
+$lang['AdminCompanyTaxes.basic.field_cascade_tax'] = 'Imposta di Cascade';
+$lang['AdminCompanyTaxes.basic.note_enable_tax'] = 'Selezionare questa opzione per abilitare l\'imposta per questa società.';
+$lang['AdminCompanyTaxes.basic.field_enable_tax'] = 'Abilitazione dell\'imposta';
+$lang['AdminCompanyTaxes.basic.heading_general'] = 'Impostazioni generali';
+$lang['AdminCompanyTaxes.basic.boxtitle_basic'] = 'Impostazioni fiscali di base';
+$lang['AdminCompanyTaxes.basic.page_title'] = 'Impostazioni > Azienda > Imposte > Impostazioni fiscali di base';
+$lang['AdminCompanyTaxes.!tooltip.state'] = 'Selezionare lo stato/provincia a cui si applica questa regola fiscale.';
+$lang['AdminCompanyTaxes.!tooltip.country'] = 'Selezionare il Paese a cui si applica questa regola fiscale.';
+$lang['AdminCompanyTaxes.!tooltip.amount'] = 'L\'importo dell\'imposta in percentuale.';
+$lang['AdminCompanyTaxes.!tooltip.name'] = 'Il nome visualizzato dell\'imposta (ad es. Imposta sulle vendite).';
+$lang['AdminCompanyTaxes.!tooltip.level'] = 'Il livello di tassazione consente di impostare l\'ordine di tassazione se si applicano più regole fiscali.';
+$lang['AdminCompanyTaxes.!tooltip.type'] = 'Inclusive calcolerà l\'imposta come parte dei prezzi degli articoli impostati e sarà sottratta dal prezzo dell\'articolo per gli utenti esenti da imposte.<br/>
+Inclusive (Additive) calcola l\'imposta in aggiunta ai prezzi degli articoli impostati.
+Esclusivo calcola le imposte in aggiunta ai prezzi degli articoli impostati, ma non le include nella visualizzazione del totale dell\'ordine.';
+$lang['AdminCompanyTaxes.states.all'] = '-- Tutti...';
+$lang['AdminCompanyTaxes.countries.all'] = '-- Tutti...';
+$lang['AdminCompanyTaxes.!success.rule_deleted'] = 'La regola fiscale è stata cancellata con successo.';
+$lang['AdminCompanyTaxes.!success.taxrule_updated'] = 'La norma fiscale è stata aggiornata con successo!';
+$lang['AdminCompanyTaxes.!success.taxrule_created'] = 'La regola fiscale è stata creata con successo!';
+$lang['AdminCompanyTaxes.!success.basic_updated'] = 'Le impostazioni della tassa di base sono state aggiornate con successo!';
+
