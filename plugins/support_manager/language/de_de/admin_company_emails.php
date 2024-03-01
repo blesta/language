309cbdd9010b9b990f6882827a70b8a1b8c8984f
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:39 -0800

    Updated de_de language files

---
+++ plugins/support_manager/language/de_de/admin_company_emails.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_reminder_desc'] = 'Benachrichtigung, wenn ein Ticket über einen bestimmten Zeitraum hinweg nicht beantwortet wurde.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_reminder_name'] = 'Erinnerung an den Personalausweis';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_reminder_desc'] = 'Mitteilung, die gesendet wird, wenn ein Ticket eine bestimmte Zeit lang nicht beantwortet wurde.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_reminder_name'] = 'Ticket-Erinnerung';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_bounce_desc'] = 'Benachrichtigung, wenn ein Ticket per E-Mail nicht geöffnet werden konnte.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_bounce_name'] = 'Ticket Bounce';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_updated_desc'] = 'Benachrichtigung, wenn ein Ticket aktualisiert wird.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_updated_name'] = 'Ticket aktualisiert';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_received_desc'] = 'Mitteilung, die an einen Benutzer gesendet wird, wenn ein Ticket eingegangen ist.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_received_name'] = 'Ticket erhalten';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_assigned_desc'] = 'Mitteilung an einen Mitarbeiter, wenn ihm ein Ticket zugewiesen wurde.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_assigned_name'] = 'Mitarbeiter Ticket zugewiesen';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_mobile_desc'] = 'Mobile Benachrichtigung, die an einen Mitarbeiter gesendet wird, wenn ein Ticket aktualisiert wird.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_mobile_name'] = 'Mitarbeiter-Ticket aktualisiert (Mobile)';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_desc'] = 'Mitteilung an einen Mitarbeiter, wenn ein Ticket aktualisiert wird.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_name'] = 'Personal-Ticket aktualisiert';
+
