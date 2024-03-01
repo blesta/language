c1e11a0f347857fe2cac326233c76b1447265cec
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:14 -0800

    Updated cs_cz language files

---
+++ components/invoice_templates/perforated_invoice/language/cs_cz/perforated_invoice.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Perforated Invoice
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PerforatedInvoice.detach_total_payment'] = 'Celková částka platby:';
+$lang['PerforatedInvoice.detach_total_due'] = 'Celková dlužná částka:';
+$lang['PerforatedInvoice.detach_remit_to'] = 'Odeslat na adresu:';
+$lang['PerforatedInvoice.detach_heading'] = 'ODDĚLIT A VRÁTIT S PLATBOU';
+$lang['PerforatedInvoice.balance_heading'] = 'Bilance';
+$lang['PerforatedInvoice.payments_applied_amount'] = 'Použitá částka';
+$lang['PerforatedInvoice.payments_transaction_id'] = 'Číslo transakce';
+$lang['PerforatedInvoice.payments_type_name'] = 'Typ';
+$lang['PerforatedInvoice.payments_applied_date'] = 'Datum podání žádosti';
+$lang['PerforatedInvoice.payments_heading'] = 'Platby';
+$lang['PerforatedInvoice.watermark_paid'] = 'PAID';
+$lang['PerforatedInvoice.lines_cost'] = 'Náklady';
+$lang['PerforatedInvoice.lines_unit_price'] = 'Jednotková cena';
+$lang['PerforatedInvoice.lines_quantity'] = 'Množství';
+$lang['PerforatedInvoice.lines_description'] = 'Popis';
+$lang['PerforatedInvoice.total_heading'] = 'Celkem';
+$lang['PerforatedInvoice.subtotal_heading'] = 'Mezisoučet';
+$lang['PerforatedInvoice.notes_heading'] = 'Poznámky';
+$lang['PerforatedInvoice.date_due'] = 'Datum splatnosti:';
+$lang['PerforatedInvoice.date_expires'] = 'Datum vypršení platnosti:';
+$lang['PerforatedInvoice.date_billed'] = 'Datum faktury:';
+$lang['PerforatedInvoice.date_created'] = 'Datum vytvoření:';
+$lang['PerforatedInvoice.client_tax_id'] = 'Daňové identifikační číslo zákazníka / DIČ:';
+$lang['PerforatedInvoice.tax_id'] = 'Daňové identifikační číslo / DIČ:';
+$lang['PerforatedInvoice.client_name'] = 'Jméno zákazníka:';
+$lang['PerforatedInvoice.client_id_code'] = 'Číslo zákazníka:';
+$lang['PerforatedInvoice.quotation_id_code'] = 'Číslo nabídky:';
+$lang['PerforatedInvoice.draft_id_code'] = 'Číslo návrhu:';
+$lang['PerforatedInvoice.proforma_id_code'] = 'Číslo pro forma:';
+$lang['PerforatedInvoice.invoice_id_code'] = 'Číslo faktury:';
+$lang['PerforatedInvoice.type_quotation_draft'] = 'NÁVRH CENOVÉ NABÍDKY';
+$lang['PerforatedInvoice.type_quotation'] = 'CITACE';
+$lang['PerforatedInvoice.type_proforma'] = 'PROFORMA FAKTURA';
+$lang['PerforatedInvoice.type_void'] = 'FAKTURA - NEPLATNÁ';
+$lang['PerforatedInvoice.type_draft'] = 'FAKTURA - NÁVRH';
+$lang['PerforatedInvoice.type_active'] = 'INVOICE';
+$lang['PerforatedInvoice.terms_heading'] = 'Podmínky';
+$lang['PerforatedInvoice.page_of'] = 'Stránka %1$s z %2$s';
+$lang['PerforatedInvoice.name'] = 'Perforované stránky';
+
