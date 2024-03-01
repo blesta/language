81fcbead98ef8fd2613fe3ec4de6d43a35f80d84
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:51 -0800

    Updated de_de language files

---
+++ language/de_de/client_verify.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Client Verify
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientVerify.!error.invalid_token'] = 'Das angegebene Token ist ungültig oder abgelaufen.';
+$lang['ClientVerify.!success.email_sent'] = 'Es wurde eine Bestätigungs-E-Mail an %1$s gesendet.';
+$lang['ClientVerify.!success.email_verified'] = 'Ihre E-Mail Adresse wurde erfolgreich verifiziert.';
+
