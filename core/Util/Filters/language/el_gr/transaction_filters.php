565384f0d9e0f4c0039a14128fc6c62fa9d7956c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:35 -0800

    Updated el_gr language files

---
+++ core/Util/Filters/language/el_gr/transaction_filters.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Transaction Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.transaction_filters.any'] = 'Οποιοδήποτε';
+$lang['Util.filters.transaction_filters.fully_applied'] = 'Πλήρως εφαρμοσμένο';
+$lang['Util.filters.transaction_filters.partially_applied'] = 'Μερικώς εφαρμοσμένο';
+$lang['Util.filters.transaction_filters.not_applied'] = 'Δεν εφαρμόζεται';
+$lang['Util.filters.transaction_filters.field_payment_type'] = 'Τύπος πληρωμής';
+$lang['Util.filters.transaction_filters.field_transaction_id'] = 'Αριθμός';
+$lang['Util.filters.transaction_filters.field_reference_id'] = 'Αναφορά';
+$lang['Util.filters.transaction_filters.field_applied_status'] = 'Εφαρμοσμένη κατάσταση';
+$lang['Util.filters.transaction_filters.field_date'] = 'Ημερομηνία';
+$lang['Util.filters.transaction_filters.field_start_date'] = 'Ημερομηνία έναρξης';
+$lang['Util.filters.transaction_filters.field_end_date'] = 'Ημερομηνία λήξης';
+$lang['Util.filters.transaction_filters.field_amount'] = 'Ποσό';
+$lang['Util.filters.transaction_filters.field_minimum_amount'] = 'Ελάχιστο ποσό';
+$lang['Util.filters.transaction_filters.field_maximum_amount'] = 'Μέγιστο ποσό';
+
