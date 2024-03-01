20b92275d56dc480a0ee43cb0a7a8232ccd7c874
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:30 -0800

    Updated it_it language files

---
+++ language/it_it/admin_company_gateways.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Admin Company Gateways
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyGateways.manage.update_settings'] = 'Aggiornamento delle impostazioni';
+$lang['AdminCompanyGateways.manage.text_in_use_by'] = 'in uso da';
+$lang['AdminCompanyGateways.manage.text_addcurrency'] = 'aggiungere la valuta';
+$lang['AdminCompanyGateways.manage.heading_currencies'] = 'Valute accettate';
+$lang['AdminCompanyGateways.manage.heading_gateway'] = 'Opzioni del gateway';
+$lang['AdminCompanyGateways.manage.boxtitle_manage'] = 'Gestire %1$s';
+$lang['AdminCompanyGateways.manage.page_title'] = 'Impostazioni > Azienda > Gateway > Gestisci %1$s';
+$lang['AdminCompanyGateways.installed.confirm_uninstall'] = 'Siete sicuri di voler disinstallare questo gateway?';
+$lang['AdminCompanyGateways.installed.btn_upgrade'] = 'Aggiornamento';
+$lang['AdminCompanyGateways.installed.btn_manage'] = 'Gestire';
+$lang['AdminCompanyGateways.installed.btn_uninstall'] = 'Disinstallare';
+$lang['AdminCompanyGateways.installed.text_nonmerchant_none'] = 'Non sono installati gateway non commerciali.';
+$lang['AdminCompanyGateways.installed.text_merchant_none'] = 'Non ci sono gateway commerciali installati.';
+$lang['AdminCompanyGateways.installed.text_merchantccform'] = 'Modulo personalizzato per carta di credito';
+$lang['AdminCompanyGateways.installed.text_merchantccoffsite'] = 'Carta di credito fuori sede';
+$lang['AdminCompanyGateways.installed.text_merchantcc'] = 'Carta di credito';
+$lang['AdminCompanyGateways.installed.text_merchantachform'] = 'Modulo personalizzato ACH';
+$lang['AdminCompanyGateways.installed.text_merchantachoffsite'] = 'ACH fuori sede';
+$lang['AdminCompanyGateways.installed.text_merchantach'] = 'ACH';
+$lang['AdminCompanyGateways.installed.text_supports'] = 'Supporti:';
+$lang['AdminCompanyGateways.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyGateways.installed.signup_url'] = 'Iscriviti';
+$lang['AdminCompanyGateways.installed.text_author_url'] = 'URL autore';
+$lang['AdminCompanyGateways.installed.text_author'] = 'Autore:';
+$lang['AdminCompanyGateways.installed.heading_nonmerchant'] = 'Non commerciante';
+$lang['AdminCompanyGateways.installed.heading_merchant'] = 'Commerciante';
+$lang['AdminCompanyGateways.installed.boxtitle_gateway'] = 'Portali';
+$lang['AdminCompanyGateways.installed.page_title'] = 'Impostazioni > Azienda > Gateway > Installato';
+$lang['AdminCompanyGateways.available.btn_install'] = 'Installare';
+$lang['AdminCompanyGateways.available.text_nonmerchant_none'] = 'Non sono disponibili gateway non commerciali.';
+$lang['AdminCompanyGateways.available.text_merchant_none'] = 'Non ci sono gateway commerciali disponibili.';
+$lang['AdminCompanyGateways.available.text_merchantccform'] = 'Modulo personalizzato per carta di credito';
+$lang['AdminCompanyGateways.available.text_merchantccoffsite'] = 'Carta di credito fuori sede';
+$lang['AdminCompanyGateways.available.text_merchantcc'] = 'Carta di credito';
+$lang['AdminCompanyGateways.available.text_merchantachform'] = 'Modulo personalizzato ACH';
+$lang['AdminCompanyGateways.available.text_merchantachoffsite'] = 'ACH fuori sede';
+$lang['AdminCompanyGateways.available.text_merchantach'] = 'ACH';
+$lang['AdminCompanyGateways.available.text_supports'] = 'Supporti:';
+$lang['AdminCompanyGateways.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyGateways.available.signup_url'] = 'Iscriviti';
+$lang['AdminCompanyGateways.available.text_author_url'] = 'URL autore';
+$lang['AdminCompanyGateways.available.text_author'] = 'Autore:';
+$lang['AdminCompanyGateways.available.heading_nonmerchant'] = 'Non commerciante';
+$lang['AdminCompanyGateways.available.heading_merchant'] = 'Commerciante';
+$lang['AdminCompanyGateways.available.boxtitle_gateway'] = 'Portali';
+$lang['AdminCompanyGateways.available.page_title'] = 'Impostazioni > Azienda > Gateway > Disponibile';
+$lang['AdminCompanyGateways.!tab.available'] = 'Disponibile';
+$lang['AdminCompanyGateways.!tab.installed'] = 'Installato';
+$lang['AdminCompanyGateways.!success.installed'] = 'Il gateway è stato installato con successo.';
+$lang['AdminCompanyGateways.!success.uninstalled'] = 'Il gateway è stato disinstallato con successo.';
+$lang['AdminCompanyGateways.!success.upgraded'] = 'Il gateway è stato aggiornato con successo.';
+$lang['AdminCompanyGateways.!success.manage_updated'] = 'Il gateway è stato aggiornato con successo.';
+
