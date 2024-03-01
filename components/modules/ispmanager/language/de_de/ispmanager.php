c41a5b549e94860356c048108d7eb7bfa9d93beb
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:28 -0800

    Updated de_de language files

---
+++ components/modules/ispmanager/language/de_de/ispmanager.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Ispmanager
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Ispmanager.!error.api'] = 'Es ist ein interner Fehler aufgetreten, oder der Server hat nicht auf die Anfrage geantwortet.';
+$lang['Ispmanager.!error.ispmanager_password.matches'] = 'Kennwort und Bestätigungskennwort stimmen nicht überein.';
+$lang['Ispmanager.!error.ispmanager_password.valid'] = 'Das Passwort muss mindestens 8 Zeichen lang sein.';
+$lang['Ispmanager.!error.ispmanager_username.length'] = 'Der Benutzername muss zwischen 1 und 16 Zeichen lang sein.';
+$lang['Ispmanager.!error.ispmanager_username.test'] = 'Der Benutzername darf nicht mit \'test\' beginnen.';
+$lang['Ispmanager.!error.ispmanager_username.format'] = 'Der Benutzername darf nur Buchstaben und Zahlen enthalten.';
+$lang['Ispmanager.!error.ispmanager_domain.test'] = 'Der Domänenname kann nicht mit \'test\' beginnen.';
+$lang['Ispmanager.!error.ispmanager_domain.format'] = 'Bitte geben Sie einen gültigen Domainnamen ein, z. B. domain.com.';
+$lang['Ispmanager.!error.module_row.missing'] = 'Es ist ein interner Fehler aufgetreten. Die Modulzeile ist nicht verfügbar.';
+$lang['Ispmanager.!error.api.internal'] = 'Es ist ein interner Fehler aufgetreten, oder der Server hat nicht auf die Anfrage geantwortet.';
+$lang['Ispmanager.!error.meta[template].empty'] = 'Eine Vorlage ist erforderlich.';
+$lang['Ispmanager.!error.name_servers_count'] = 'Sie müssen mindestens 2 Nameserver definieren.';
+$lang['Ispmanager.!error.name_servers_valid'] = 'Einer oder mehrere der eingegebenen Nameserver sind ungültig.';
+$lang['Ispmanager.!error.account_limit_valid'] = 'Kontolimit muss leer gelassen werden (für unbegrenzte Konten) oder auf einen ganzzahligen Wert gesetzt werden.';
+$lang['Ispmanager.!error.password_valid_connection'] = 'Eine Verbindung zum Server konnte nicht hergestellt werden. Bitte überprüfen Sie, ob der Benutzername und das Passwort korrekt sind.';
+$lang['Ispmanager.!error.password_valid'] = 'Das Passwort scheint ungültig zu sein.';
+$lang['Ispmanager.!error.user_name_valid'] = 'Der Benutzername scheint ungültig zu sein.';
+$lang['Ispmanager.!error.host_name_valid'] = 'Der Hostname scheint ungültig zu sein.';
+$lang['Ispmanager.!error.server_name_valid'] = 'Sie müssen ein Server-Label eingeben.';
+$lang['Ispmanager.service_field.tooltip.password'] = 'Sie können das Passwort leer lassen, damit es automatisch generiert wird.';
+$lang['Ispmanager.service_field.tooltip.username'] = 'Sie können den Benutzernamen leer lassen, um ihn automatisch zu generieren.';
+$lang['Ispmanager.service_field.tooltip.username_edit'] = 'Diese Änderung hat keine Auswirkungen auf ISPmanager, sondern ändert nur den lokalen Eintrag in Blesta.';
+$lang['Ispmanager.service_field.tooltip.domain_edit'] = 'Diese Änderung hat keine Auswirkungen auf ISPmanager, sondern ändert nur den lokalen Eintrag in Blesta.';
+$lang['Ispmanager.service_info.option_login'] = 'Einloggen';
+$lang['Ispmanager.service_info.options'] = 'Optionen';
+$lang['Ispmanager.service_info.server'] = 'Server';
+$lang['Ispmanager.service_info.password'] = 'Passwort';
+$lang['Ispmanager.service_info.username'] = 'Benutzername';
+$lang['Ispmanager.service_field.password'] = 'Passwort';
+$lang['Ispmanager.service_field.username'] = 'Benutzername';
+$lang['Ispmanager.service_field.domain'] = 'Bereich';
+$lang['Ispmanager.package_fields.template'] = 'Vorlage';
+$lang['Ispmanager.tab_client_actions.field_password_submit'] = 'Passwort aktualisieren';
+$lang['Ispmanager.tab_client_actions.field_ispmanager_confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['Ispmanager.tab_client_actions.field_ispmanager_password'] = 'Passwort';
+$lang['Ispmanager.tab_client_actions.change_password'] = 'Passwort ändern';
+$lang['Ispmanager.row_meta.account_limit'] = 'Konto-Limit';
+$lang['Ispmanager.row_meta.use_ssl'] = 'Verwenden Sie SSL für die Verbindung zur API (empfohlen)';
+$lang['Ispmanager.row_meta.password'] = 'Passwort';
+$lang['Ispmanager.row_meta.user_name'] = 'Name des Benutzers';
+$lang['Ispmanager.row_meta.host_name'] = 'Hostname';
+$lang['Ispmanager.row_meta.server_name'] = 'Server-Etikett';
+$lang['Ispmanager.edit_row.add_btn'] = 'Server bearbeiten';
+$lang['Ispmanager.edit_row.remove_name_server'] = 'entfernen';
+$lang['Ispmanager.edit_row.name_server'] = 'Namensserver %1$s';
+$lang['Ispmanager.edit_row.name_server_host_col'] = 'Hostname';
+$lang['Ispmanager.edit_row.name_server_col'] = 'Name Server';
+$lang['Ispmanager.edit_row.name_server_btn'] = 'Zusätzlichen Namensserver hinzufügen';
+$lang['Ispmanager.edit_row.notes_title'] = 'Anmerkungen';
+$lang['Ispmanager.edit_row.name_servers_title'] = 'Namensserver';
+$lang['Ispmanager.edit_row.basic_title'] = 'Grundeinstellungen';
+$lang['Ispmanager.edit_row.box_title'] = 'ISPmanager-Server bearbeiten';
+$lang['Ispmanager.add_row.add_btn'] = 'Server hinzufügen';
+$lang['Ispmanager.add_row.remove_name_server'] = 'entfernen';
+$lang['Ispmanager.add_row.name_server'] = 'Namensserver %1$s';
+$lang['Ispmanager.add_row.name_server_host_col'] = 'Hostname';
+$lang['Ispmanager.add_row.name_server_col'] = 'Name Server';
+$lang['Ispmanager.add_row.name_server_btn'] = 'Zusätzlichen Namensserver hinzufügen';
+$lang['Ispmanager.add_row.notes_title'] = 'Anmerkungen';
+$lang['Ispmanager.add_row.name_servers_title'] = 'Namensserver';
+$lang['Ispmanager.add_row.basic_title'] = 'Grundeinstellungen';
+$lang['Ispmanager.add_row.box_title'] = 'ISPmanager-Server hinzufügen';
+$lang['Ispmanager.order_options.roundrobin'] = 'Gleichmäßig auf die Server verteilen';
+$lang['Ispmanager.order_options.first'] = 'Erster nicht-ausgelasteter Server';
+$lang['Ispmanager.manage.module_groups_no_results'] = 'Es gibt keine Servergruppen.';
+$lang['Ispmanager.manage.module_rows_no_results'] = 'Es gibt keine Server.';
+$lang['Ispmanager.manage.module_groups.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Servergruppe löschen wollen?';
+$lang['Ispmanager.manage.module_rows.confirm_delete'] = 'Sind Sie sicher, dass Sie diesen Server löschen wollen?';
+$lang['Ispmanager.manage.module_groups.delete'] = 'Löschen';
+$lang['Ispmanager.manage.module_rows.delete'] = 'Löschen';
+$lang['Ispmanager.manage.module_groups.edit'] = 'bearbeiten';
+$lang['Ispmanager.manage.module_rows.edit'] = 'bearbeiten';
+$lang['Ispmanager.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Ispmanager.manage.module_groups_heading.options'] = 'Optionen';
+$lang['Ispmanager.manage.module_groups_heading.servers'] = 'Anzahl der Server';
+$lang['Ispmanager.manage.module_groups_heading.name'] = 'Name der Gruppe';
+$lang['Ispmanager.manage.module_rows_heading.options'] = 'Optionen';
+$lang['Ispmanager.manage.module_rows_heading.accounts'] = 'Konten';
+$lang['Ispmanager.manage.module_rows_heading.hostname'] = 'Hostname';
+$lang['Ispmanager.manage.module_rows_heading.name'] = 'Server-Etikett';
+$lang['Ispmanager.manage.module_groups_title'] = 'Server-Gruppen';
+$lang['Ispmanager.manage.module_rows_title'] = 'Server';
+$lang['Ispmanager.add_module_group'] = 'Server-Gruppe hinzufügen';
+$lang['Ispmanager.add_module_row'] = 'Server hinzufügen';
+$lang['Ispmanager.tab_client_actions'] = 'Aktionen';
+$lang['Ispmanager.module_group'] = 'Server-Gruppe';
+$lang['Ispmanager.module_row_plural'] = 'Server';
+$lang['Ispmanager.module_row'] = 'Server';
+$lang['Ispmanager.description'] = 'ISPmanager bietet umfangreiche Funktionen für die Verwaltung von Websites, die Erstellung von Benutzern, die Verwaltung von Domains, E-Mails, Datenbanken usw.';
+$lang['Ispmanager.name'] = 'ISPmanager';
+
