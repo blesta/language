fc30f63a9867374ca1fb90057a3acb8a99135b78
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:54 -0800

    Updated it_it language files

---
+++ plugins/import_manager/components/migrators/whmcs/8.0/language/it_it/whmcs8_0.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Whmcs8
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Whmcs8_0.configuration.no_local_packages'] = 'È necessario creare i pacchetti prima di poterli mappare.';
+$lang['Whmcs8_0.configuration.remote_package'] = 'Pacchetto remoto';
+$lang['Whmcs8_0.configuration.local_package'] = 'Pacchetto locale';
+$lang['Whmcs8_0.configuration.create_packages_false'] = 'Mappare manualmente i pacchetti';
+$lang['Whmcs8_0.configuration.create_packages_true'] = 'Creare automaticamente i pacchetti necessari durante l\'importazione';
+$lang['Whmcs8_0.settings.enable_debug'] = 'Abilita il debug';
+$lang['Whmcs8_0.settings.balance_credit.info'] = 'Se selezionata, genera una transazione o una fattura per garantire che il credito del cliente in Blesta corrisponda a quello impostato per il cliente in WHMCS. Questo è necessario perché WHMCS non contabilizza correttamente i crediti del cliente.';
+$lang['Whmcs8_0.settings.balance_credit'] = 'Bilanciamento automatico delle transazioni per farle corrispondere ai crediti dei clienti in WHMCS';
+$lang['Whmcs8_0.settings.key.info'] = 'È la chiave di cifratura utilizzata da WHMCS per criptare vari bit di dati. Si trova nel file configuration.php di WHMCS.';
+$lang['Whmcs8_0.settings.key'] = 'Cifratura CC Hash';
+$lang['Whmcs8_0.settings.pass'] = 'Password del database';
+$lang['Whmcs8_0.settings.user'] = 'Utente del database';
+$lang['Whmcs8_0.settings.database'] = 'Nome del database';
+$lang['Whmcs8_0.settings.host'] = 'Host del database';
+$lang['Whmcs8_0.!notice.gmp'] = 'L\'estensione \'gmp\' non è stata trovata. Questa estensione è consigliata per migliorare le prestazioni e il processo di importazione. È possibile continuare senza questa estensione, ma il processo di importazione potrebbe essere molto più lento.';
+$lang['Whmcs8_0.!error.import'] = 'L\'importazione è stata completata ma si sono verificati i seguenti errori:';
+$lang['Whmcs8_0.!error.key.invalid'] = 'È necessario disporre di una AESKEY valida.';
+$lang['Whmcs8_0.!error.pass.invalid'] = 'È richiesta la password del database.';
+$lang['Whmcs8_0.!error.user.invalid'] = 'È necessario l\'utente del database.';
+$lang['Whmcs8_0.!error.database.invalid'] = 'Il nome del database è obbligatorio.';
+$lang['Whmcs8_0.!error.host.invalid'] = 'È necessario un host di database.';
+
