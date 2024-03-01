f42b7129503f3e83311a5c6c933b291cd3ccde13
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:42 -0800

    Updated cs_cz language files

---
+++ components/reports/aging_invoices/language/cs_cz/aging_invoices.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Aging Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AgingInvoices.heading.past90'] = '90+';
+$lang['AgingInvoices.heading.past60'] = '60';
+$lang['AgingInvoices.heading.past30'] = '30';
+$lang['AgingInvoices.heading.date_due'] = 'Datum splatnosti';
+$lang['AgingInvoices.heading.date_billed'] = 'Datum vyúčtování';
+$lang['AgingInvoices.heading.status'] = 'Stav';
+$lang['AgingInvoices.heading.currency'] = 'Měna';
+$lang['AgingInvoices.heading.paid'] = 'Placené';
+$lang['AgingInvoices.heading.total'] = 'Celkem';
+$lang['AgingInvoices.heading.subtotal'] = 'Mezisoučet';
+$lang['AgingInvoices.heading.client_numbers'] = 'Telefonní čísla klientů';
+$lang['AgingInvoices.heading.client_email'] = 'E-mail klienta';
+$lang['AgingInvoices.heading.client_last_name'] = 'Příjmení klienta';
+$lang['AgingInvoices.heading.client_first_name'] = 'Křestní jméno klienta';
+$lang['AgingInvoices.heading.client_id_code'] = 'Klient #';
+$lang['AgingInvoices.heading.id_code'] = 'Faktura #';
+$lang['AgingInvoices.option.any'] = 'Jakýkoli';
+$lang['AgingInvoices.options.field_status'] = 'Stav';
+$lang['AgingInvoices.name'] = 'Stárnutí faktur';
+
