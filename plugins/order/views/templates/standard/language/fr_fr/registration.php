3835fb325730a091ed1308c82739e4e1205c6ce7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:52 -0800

    Updated fr_fr language files

---
+++ plugins/order/views/templates/standard/language/fr_fr/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Registration
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Registration.signup.header_signup'] = 'S\'inscrire';
+
