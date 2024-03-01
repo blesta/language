5c67e8a5754e8185df966407f015b51135d8d53b
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:17 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/referrals.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Referrals
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Referrals.index.heading_date_added'] = 'Datum hinzugefügt';
+$lang['Referrals.index.heading_commission'] = 'Kommission';
+$lang['Referrals.index.heading_currency'] = 'Währung';
+$lang['Referrals.index.heading_amount'] = 'Betrag';
+$lang['Referrals.index.heading_name'] = 'Name';
+$lang['Referrals.index.heading_order'] = 'Bestellung #';
+$lang['Referrals.index.no_results'] = 'Derzeit gibt es keine Verweise mit diesem Status.';
+$lang['Referrals.index.category_canceled'] = 'Abgesagt';
+$lang['Referrals.index.category_mature'] = 'Ausgereift';
+$lang['Referrals.index.category_pending'] = 'Anhängig';
+$lang['Referrals.index.boxtitle_referrals'] = 'Verweise';
+
