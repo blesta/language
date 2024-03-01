8f9f1d21a94eeb2cf40fe0ff3e580b158dc29bd5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:28 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/messages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Messages
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Messages.!error.message_group_id.valid'] = 'ID de groupe de messages non valide.';
+$lang['Messages.!error.company_id.valid'] = 'ID d\'entreprise non valide.';
+$lang['Messages.!error.type.valid'] = 'Le type de message doit être défini sur "sms".';
+$lang['Messages.!error.status.valid'] = 'Le statut du message doit être "actif" ou "inactif".';
+$lang['Messages.!error.content.format'] = 'Le contenu du message doit être un tableau.';
+$lang['Messages.getTypes.sms'] = 'SMS';
+$lang['Messages.getStatusTypes.active'] = 'Actif';
+$lang['Messages.getStatusTypes.inactive'] = 'Inactif';
+
