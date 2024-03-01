f6034485ff9d709905d0d2f304bc9d85950aa73a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:56 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/deliver_reports.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Deliver Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.deliver_reports.tax_liability.email_success'] = 'Die E-Mail mit dem Bericht über die Steuerpflicht wurde versandt.';
+$lang['Automation.task.deliver_reports.tax_liability.email_error'] = 'Die E-Mail zur Steuerpflicht wurde nicht gesendet.';
+$lang['Automation.task.deliver_reports.tax_liability.attachment_fail'] = 'Der Anhang des Steuerverbindlichkeitsberichts kann nicht erstellt werden.';
+$lang['Automation.task.deliver_reports.tax_liability.attempt'] = 'Ich versuche, den Bericht über die Steuerpflicht zu senden.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_success'] = 'Die E-Mail mit dem Bericht zur Rechnungserstellung wurde gesendet.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_error'] = 'Die E-Mail mit dem Bericht zur Rechnungserstellung konnte nicht gesendet werden.';
+$lang['Automation.task.deliver_reports.invoice_creation.attachment_fail'] = 'Der Anhang des Berichts zur Rechnungserstellung kann nicht erstellt werden.';
+$lang['Automation.task.deliver_reports.invoice_creation.attempt'] = 'Es wird versucht, den Bericht "Rechnungserstellung" zu senden.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_success'] = 'Die E-Mail mit dem Bericht über fällige Rechnungen wurde versandt.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_error'] = 'Die E-Mail mit den fälligen Rechnungen konnte nicht gesendet werden.';
+$lang['Automation.task.deliver_reports.aging_invoices.attachment_fail'] = 'Der Anhang des Berichts "Fällige Rechnungen" kann nicht erstellt werden.';
+$lang['Automation.task.deliver_reports.aging_invoices.attempt'] = 'Es wird versucht, den Bericht "Fällige Rechnungen" zu senden.';
+$lang['Automation.task.deliver_reports.completed'] = 'Die Aufgabe Berichte liefern ist abgeschlossen.';
+$lang['Automation.task.deliver_reports.attempt'] = 'Versuch, Berichte zu liefern.';
+
