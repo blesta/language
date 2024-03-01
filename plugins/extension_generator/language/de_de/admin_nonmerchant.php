e4db7bee4657bd6a557743a321d104251d591156
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:43 -0800

    Updated de_de language files

---
+++ plugins/extension_generator/language/de_de/admin_nonmerchant.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Admin Nonmerchant
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminNonmerchant.features.confirm'] = 'Nächste - Bestätigung';
+$lang['AdminNonmerchant.features.heading_optional_functions'] = 'Optionale Funktionen';
+$lang['AdminNonmerchant.fields.features'] = 'Weiter - Zusätzliche Funktionen';
+$lang['AdminNonmerchant.fields.text_remove'] = 'entfernen';
+$lang['AdminNonmerchant.fields.text_options'] = 'Optionen';
+$lang['AdminNonmerchant.fields.field_row_add'] = 'Konfigurationsfeld hinzufügen';
+$lang['AdminNonmerchant.fields.placeholder_label'] = 'z.B. Konfigurationsfeld';
+$lang['AdminNonmerchant.fields.placeholder_name'] = 'z.B. Konfiguration_Feld';
+$lang['AdminNonmerchant.fields.tooltip_encryptable'] = 'ob dieses Feld in der Datenbank verschlüsselt werden soll';
+$lang['AdminNonmerchant.fields.tooltip_tooltip'] = 'Der Text des Tooltips, der neben dem Feld angezeigt werden soll (leer lassen, um keinen Tooltip zu haben)';
+$lang['AdminNonmerchant.fields.tooltip_type'] = 'Die Art des zu erstellenden Feldes (Kontrollkästchen, Text, usw.)';
+$lang['AdminNonmerchant.fields.tooltip_label'] = 'Der Anzeigename des Feldes';
+$lang['AdminNonmerchant.fields.tooltip_name'] = 'Der Name des Feldes in der Codebasis';
+$lang['AdminNonmerchant.fields.encryptable'] = 'Verschlüsselbar';
+$lang['AdminNonmerchant.fields.tooltip'] = 'Tooltip-Text';
+$lang['AdminNonmerchant.fields.type'] = 'Typ';
+$lang['AdminNonmerchant.fields.label'] = 'Etikett';
+$lang['AdminNonmerchant.fields.name'] = 'Name';
+$lang['AdminNonmerchant.fields.heading_fields'] = 'Konfiguration Felder';
+$lang['AdminNonmerchant.basic.confirm'] = 'Nächste - Bestätigung';
+$lang['AdminNonmerchant.basic.fields'] = 'Weiter - Konfigurationsfelder';
+$lang['AdminNonmerchant.basic.placeholder_author_url'] = 'z.B. https://blesta.com/';
+$lang['AdminNonmerchant.basic.placeholder_author_name'] = 'z.B. Blesta';
+$lang['AdminNonmerchant.basic.placeholder_currencies'] = 'z.B. USD,EUR,JPY';
+$lang['AdminNonmerchant.basic.placeholder_signup_url'] = 'z.B. https://dashboard.stripe.com/register';
+$lang['AdminNonmerchant.basic.tooltip_currencies'] = 'Eine durch Kommata getrennte Liste der unterstützten Währungen (z. B. USD, JPY, EUR)';
+$lang['AdminNonmerchant.basic.tooltip_signup_url'] = 'Die URL zur Anmeldeseite des Gateways';
+$lang['AdminNonmerchant.basic.tooltip_logo'] = 'Das Logo wird in der Plugin-Liste angezeigt. Die Bilder werden nicht in der Größe verändert, daher sind die idealen Abmessungen 150x70';
+$lang['AdminNonmerchant.basic.tooltip_description'] = 'Die in der Plugin-Liste angezeigte Beschreibung';
+$lang['AdminNonmerchant.basic.text_remove'] = 'entfernen';
+$lang['AdminNonmerchant.basic.author_row_add'] = 'Autor hinzufügen';
+$lang['AdminNonmerchant.basic.text_options'] = 'Optionen';
+$lang['AdminNonmerchant.basic.author_url'] = 'Autor URL';
+$lang['AdminNonmerchant.basic.author_name'] = 'Name des Autors';
+$lang['AdminNonmerchant.basic.currencies'] = 'Währungen';
+$lang['AdminNonmerchant.basic.signup_url'] = 'Anmelde-URL';
+$lang['AdminNonmerchant.basic.logo'] = 'Logo';
+$lang['AdminNonmerchant.basic.description'] = 'Beschreibung';
+$lang['AdminNonmerchant.basic.heading_authors'] = 'Autoren';
+$lang['AdminNonmerchant.basic.heading_basic'] = 'Grundlegende Informationen';
+$lang['AdminNonmerchant.index.boxtitle_extension_generator'] = 'Erweiterungsgenerator - Nicht-Merchant Gateway';
+$lang['AdminNonmerchant.index.page_title'] = 'Erweiterung Generator - %1$s';
+
