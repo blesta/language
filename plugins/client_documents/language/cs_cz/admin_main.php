4fbf7168816ca77d3087bf04c247031b91707174
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:27 -0800

    Updated cs_cz language files

---
+++ plugins/client_documents/language/cs_cz/admin_main.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.add.field_submit'] = 'Nahrát dokument';
+$lang['AdminMain.add.field_description'] = 'Popis';
+$lang['AdminMain.add.field_document'] = 'Dokument';
+$lang['AdminMain.add.field_name'] = 'Název';
+$lang['AdminMain.add.boxtitle_adddocument'] = 'Nahrát dokument';
+$lang['AdminMain.modal.delete_document'] = 'Opravdu tento dokument vymazat?';
+$lang['AdminMain.index.option_download'] = 'Stáhnout';
+$lang['AdminMain.index.option_delete'] = 'Odstranit';
+$lang['AdminMain.index.heading_date_options'] = 'Možnosti';
+$lang['AdminMain.index.heading_date_added'] = 'Datum přidání';
+$lang['AdminMain.index.heading_description'] = 'Popis';
+$lang['AdminMain.index.heading_name'] = 'Název';
+$lang['AdminMain.index.no_results'] = 'Pro tohoto klienta nejsou nastaveny žádné dokumenty.';
+$lang['AdminMain.index.boxtitle_documents'] = 'Dokumenty';
+$lang['AdminMain.index.boxtitle_client'] = 'Klient #%1$s - %2$s %3$s';
+$lang['AdminMain.index.categorylink_adddocument'] = 'Přidat dokument';
+$lang['AdminMain.add.page_title'] = 'Dokumenty > Přidat';
+$lang['AdminMain.index.page_title'] = 'Dokumenty';
+$lang['AdminMain.!success.document_deleted'] = 'Dokument byl smazán.';
+$lang['AdminMain.!success.document_uploaded'] = 'Dokument byl nahrán.';
+
