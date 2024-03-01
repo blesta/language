30f6ff582b382c77a5bd3e50e927c724928fa2b5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:27 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/admin_excluded_packages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Admin Excluded Packages
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminExcludedPackages.!success.packages_updated'] = 'Les paquets exclus ont été mis à jour avec succès.';
+$lang['AdminExcludedPackages.index.boxtitle_excluded_packages'] = 'Forfaits exclus';
+$lang['AdminExcludedPackages.index.tab_available_packages'] = 'Packages disponibles';
+$lang['AdminExcludedPackages.index.tab_hidden_packages'] = 'Paquets cachés';
+$lang['AdminExcludedPackages.index.heading_available_packages'] = 'Packages disponibles';
+$lang['AdminExcludedPackages.index.heading_hidden_packages'] = 'Paquets cachés';
+$lang['AdminExcludedPackages.index.field_updatesubmit'] = 'Mise à jour des paramètres';
+$lang['AdminExcludedPackages.index.text_no_packages'] = 'Il n\'y a pas de forfaits actifs disponibles.';
+
