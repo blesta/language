9ddbc1bac5a702da86d6b172754b7dd3a5469b5c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:32 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/package_option_condition_sets.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Package Option Condition Sets
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptionConditionSets.!error.id.exists'] = 'L\'ID de la condition de l\'option donnée n\'existe pas';
+$lang['PackageOptionConditionSets.!error.option_group_id.exists'] = 'L\'ID du groupe d\'options donné n\'existe pas';
+$lang['PackageOptionConditionSets.!error.option_id.exists'] = 'L\'ID de l\'option donnée n\'existe pas';
+$lang['PackageOptionConditionSets.!error.option_value_ids.exists'] = 'L\'un des identifiants de valeur d\'option donnés n\'existe pas.';
+
