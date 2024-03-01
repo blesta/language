752a0c79a75d731cb57d8f084d5e77363b145184
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:44 -0800

    Updated fr_fr language files

---
+++ components/invoice_templates/quickbooks_invoice/language/fr_fr/quickbooks_invoice.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Quickbooks Invoice
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['QuickbooksInvoice.name'] = 'Quickbooks';
+$lang['QuickbooksInvoice.page_of'] = 'Page %1$s de %2$s';
+$lang['QuickbooksInvoice.terms_heading'] = 'Conditions';
+$lang['QuickbooksInvoice.type_active'] = 'FACTURE';
+$lang['QuickbooksInvoice.type_draft'] = 'FACTURE - PROJET';
+$lang['QuickbooksInvoice.type_void'] = 'INVOICE - VOID';
+$lang['QuickbooksInvoice.type_proforma'] = 'FACTURE PRO FORMA';
+$lang['QuickbooksInvoice.type_quotation'] = 'CITATION';
+$lang['QuickbooksInvoice.type_quotation_draft'] = 'PROJET DE DEVIS';
+$lang['QuickbooksInvoice.invoice_id_code'] = 'Numéro de facture';
+$lang['QuickbooksInvoice.proforma_id_code'] = 'Pro Forma No.';
+$lang['QuickbooksInvoice.draft_id_code'] = 'Projet n°.';
+$lang['QuickbooksInvoice.quotation_id_code'] = 'N° de devis';
+$lang['QuickbooksInvoice.client_id_code'] = 'Numéro de client';
+$lang['QuickbooksInvoice.date_created'] = 'Date de création';
+$lang['QuickbooksInvoice.date_billed'] = 'Date';
+$lang['QuickbooksInvoice.date_expires'] = 'Date d\'expiration';
+$lang['QuickbooksInvoice.date_due'] = 'Date d\'échéance';
+$lang['QuickbooksInvoice.notes_heading'] = 'Notes';
+$lang['QuickbooksInvoice.subtotal_heading'] = 'Sous-total';
+$lang['QuickbooksInvoice.total_heading'] = 'Total';
+$lang['QuickbooksInvoice.lines_description'] = 'Description';
+$lang['QuickbooksInvoice.lines_quantity'] = 'Quantité';
+$lang['QuickbooksInvoice.lines_unit_price'] = 'Prix unitaire';
+$lang['QuickbooksInvoice.lines_cost'] = 'Coût';
+$lang['QuickbooksInvoice.address_heading'] = 'Projet de loi';
+$lang['QuickbooksInvoice.address_city_state'] = '%1$s, %2$s %3$s %4$s';
+$lang['QuickbooksInvoice.tax_id'] = 'Numéro d\'identification fiscale/VATIN : %1$s';
+$lang['QuickbooksInvoice.client_tax_id'] = 'Numéro d\'identification fiscale/VATIN du client : %1$s';
+$lang['QuickbooksInvoice.watermark_paid'] = 'PAYÉ';
+$lang['QuickbooksInvoice.payments_heading'] = 'Paiements';
+$lang['QuickbooksInvoice.payments_applied_date'] = 'Date d\'application';
+$lang['QuickbooksInvoice.payments_type_name'] = 'Type';
+$lang['QuickbooksInvoice.payments_transaction_id'] = 'Numéro de transaction';
+$lang['QuickbooksInvoice.payments_applied_amount'] = 'Montant appliqué';
+$lang['QuickbooksInvoice.balance_heading'] = 'Équilibre';
+
