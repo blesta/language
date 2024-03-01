1326e407d072cb16049ec8ecf7b4bbfeebfffc72
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:00 -0800

    Updated de_de language files

---
+++ language/de_de/encryption.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Encryption
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Encryption.!error.agree.empty'] = 'Sie müssen bestätigen, dass Sie diese Passphrase an einem sicheren Ort gespeichert haben.';
+$lang['Encryption.!error.confirm_new_passphrase.matches'] = 'Die neue Passphrase stimmt nicht überein.';
+$lang['Encryption.!error.private_key_passphrase.empty'] = 'Bitte geben Sie eine neue Passphrase für den privaten Schlüssel ein.';
+$lang['Encryption.!error.current_passphrase.matches'] = 'Die aktuelle Passphrase des privaten Schlüssels ist ungültig.';
+$lang['Encryption.!error.current_passphrase.empty'] = 'Bitte geben Sie die aktuelle Passphrase für den privaten Schlüssel ein.';
+
