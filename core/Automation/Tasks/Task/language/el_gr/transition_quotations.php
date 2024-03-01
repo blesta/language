806419997b6ca5997d4e547d0eefbfe5835e176a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:23 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/transition_quotations.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Transition Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.transition_quotations.attempt'] = 'Προσπάθεια μετάβασης ληγμένων εισαγωγικών.';
+$lang['Automation.task.transition_quotations.completed'] = 'Η εργασία μετάβασης των εισαγωγικών έχει ολοκληρωθεί.';
+$lang['Automation.task.transition_quotations.none'] = 'Δεν είναι διαθέσιμες ληγμένες τιμές για μετάβαση.';
+$lang['Automation.task.transition_quotations.expiration_success'] = 'Η εκκρεμής προσφορά #%1$s από τον πελάτη #%2$s έχει πλέον λήξει.';
+$lang['Automation.task.transition_quotations.dead_success'] = 'Η ληγμένη προσφορά #%1$s από τον πελάτη #%2$s είναι πλέον νεκρή.';
+
