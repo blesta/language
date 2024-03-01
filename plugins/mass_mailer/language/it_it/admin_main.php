b906a9fe7540708e089dd0cde468fce5a5c12987
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:58 -0800

    Updated it_it language files

---
+++ plugins/mass_mailer/language/it_it/admin_main.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.index.no_results'] = 'Al momento non ci sono lavori di mailing.';
+$lang['AdminMain.index.no_export_details'] = 'Non sono disponibili dettagli sull\'esportazione.';
+$lang['AdminMain.index.email_to_recipients'] = '%1$s Destinatario/i';
+$lang['AdminMain.index.email_subject'] = 'Oggetto';
+$lang['AdminMain.index.email_from'] = 'Da';
+$lang['AdminMain.index.email_to'] = 'A';
+$lang['AdminMain.index.type.export'] = 'Esportazione';
+$lang['AdminMain.index.type.email'] = 'Email';
+$lang['AdminMain.index.option_export'] = 'Esportazione';
+$lang['AdminMain.index.heading_options'] = 'Opzioni';
+$lang['AdminMain.index.heading_complete'] = 'Completato';
+$lang['AdminMain.index.heading_status'] = 'Stato';
+$lang['AdminMain.index.heading_type'] = 'Tipo';
+$lang['AdminMain.index.heading_date'] = 'Data aggiunta';
+$lang['AdminMain.index.job_task_total'] = '%1$s / %2$s';
+$lang['AdminMain.index.categorylink_compose'] = 'Creare un nuovo mailing';
+$lang['AdminMain.index.boxtitle'] = 'Mass Mailer Offerte di lavoro';
+$lang['AdminMain.index.page_title'] = 'Mailer di massa';
+$lang['AdminMain.!error.export_found'] = 'L\'esportazione non è stata trovata nel file system.';
+
