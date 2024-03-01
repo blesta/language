423747bba9b610a5345838d0812201b108dd34ff
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:56 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/license_validation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * License Validation
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.license_validation.attempt'] = 'Pokus o ověření licence.';
+$lang['Automation.task.license_validation.completed'] = 'Úloha ověření licence byla dokončena.';
+
