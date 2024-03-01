8b2e70418b2b22e76f5d771ec17e524ecb366778
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:25 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/ccavenue/language/el_gr/ccavenue.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Ccavenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Ccavenue.!error.authentication.log'] = 'Η συναλλαγή απορρίφθηκε από την τράπεζα.';
+$lang['Ccavenue.!error.authentication.response'] = 'Σας ευχαριστούμε που κάνατε τις αγορές σας μαζί μας. Ωστόσο, η συναλλαγή απορρίφθηκε. Παρακαλώ επαναλάβετε τη συναλλαγή.';
+$lang['Ccavenue.!error.security.response'] = 'Σφάλμα ασφαλείας. Εντοπίστηκε παράνομη πρόσβαση.';
+$lang['Ccavenue.!error.delay.response'] = 'Σας ευχαριστούμε που κάνατε τις αγορές σας μαζί μας. Θα σας κρατάμε ενήμερους σχετικά με την κατάσταση της παραγγελίας σας μέσω e-mail.';
+$lang['Ccavenue.!error.pending.log'] = 'Η κατάσταση πληρωμής έλαβε εκκρεμότητα από την πύλη.';
+$lang['Ccavenue.!error.encryption_key.valid'] = 'Εισάγετε ένα κλειδί κρυπτογράφησης.';
+$lang['Ccavenue.!error.access_code.empty'] = 'Παρακαλώ εισάγετε έναν κωδικό πρόσβασης.';
+$lang['Ccavenue.!error.merchant_id.valid'] = 'Παρακαλώ εισάγετε έγκυρο αναγνωριστικό εμπόρου.';
+$lang['Ccavenue.buildprocess.submit'] = 'Πληρωμή μέσω CCAvenue';
+$lang['Ccavenue.test_mode_note'] = 'Εάν είναι επιλεγμένο, η πύλη θα στέλνει αιτήματα πληρωμής στη δοκιμαστική διεύθυνση URL.';
+$lang['Ccavenue.test_mode'] = 'Λειτουργία δοκιμής';
+$lang['Ccavenue.access_code_note'] = 'Αυτό μπορείτε να το βρείτε στο λογαριασμό σας στο CCAvenue στην ενότητα API Keys.';
+$lang['Ccavenue.access_code'] = 'Κωδικός πρόσβασης';
+$lang['Ccavenue.encryption_key_note'] = 'Αυτό δημιουργείται στο λογαριασμό σας στο CCAvenue στην ενότητα API Keys.';
+$lang['Ccavenue.encryption_key'] = 'Κλειδί κρυπτογράφησης';
+$lang['Ccavenue.merchant_id_note'] = 'Το αναγνωριστικό του εμπόρου σας είναι διαθέσιμο στο λογαριασμό σας στο CCAvenue στην ενότητα Κλειδιά API.';
+$lang['Ccavenue.merchant_id'] = 'Αναγνωριστικό εμπόρου';
+$lang['Ccavenue.description'] = 'Με περισσότερες από 240 επιλογές πληρωμών που συνδέουν περισσότερες από 55 ινδικές τράπεζες σε πραγματικό χρόνο, η CCAvenue είναι μία από τις μεγαλύτερες μηχανές άμεσης χρέωσης στην Ινδία.';
+$lang['Ccavenue.name'] = 'Πληρωμή CCAvenue';
+
