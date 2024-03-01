8d34bf55f18532e3015e80cb450b3b8eaa37a2ab
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:53 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/backups_amazons3.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Backups Amazons3
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.backups_amazons3.attempt'] = 'Ich versuche, die Datenbank auf AmazonS3 zu sichern.';
+$lang['Automation.task.backups_amazons3.completed'] = 'Der Sicherungsauftrag für die AmazonS3-Datenbank wurde abgeschlossen.';
+$lang['Automation.task.backups_amazons3.success'] = 'Die Sicherung wurde erfolgreich abgeschlossen.';
+
