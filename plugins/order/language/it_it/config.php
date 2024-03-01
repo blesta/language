b8092f9b09d9590241369b5f8bd45016ec183605
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:11 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Config
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Config.index.field_addon_label_recurring'] = '%1$s (%2$s @ %3$s, rinnovo @ %4$s)';
+$lang['Config.index.field_addon_label'] = '%1$s (%2$s @ %3$s)';
+$lang['Config.index.field_default_addon'] = 'Nessuno';
+$lang['Config.index.field_module_group_id'] = 'Gruppo';
+$lang['Config.index.package_price_recurring'] = '%1$s @ %2$s (rinnova a %3$s)';
+$lang['Config.index.package_price'] = '%1$s @ %2$s';
+$lang['Config.!error.invalid_domain_term'] = 'Il termine selezionato per il dominio "%1$s" non è valido.';
+$lang['Config.!error.domain_duplicated'] = 'Il dominio "%1$s" è già presente nel carrello.';
+$lang['Config.!error.invalid_module'] = 'Il pacchetto non è attualmente disponibile.';
+$lang['Config.!error.invalid_pricing_id'] = 'Il pacchetto indicato non è stato trovato.';
+
