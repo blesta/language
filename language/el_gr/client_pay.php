cf93f1619906d4631d00febb29a26f490a9b87ae
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:08 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_pay.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Client Pay
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientPay.setcontactview.text_none'] = 'Κανένα';
+$lang['ClientPay.received.text_try_again'] = 'Δοκιμάστε ξανά';
+$lang['ClientPay.received.invoices'] = 'Τιμολόγιο #';
+$lang['ClientPay.received.amount'] = 'Ποσό';
+$lang['ClientPay.received.transaction_id'] = 'Αναγνωριστικό συναλλαγής';
+$lang['ClientPay.received.statement'] = 'Η πληρωμή σας επεξεργάζεται.';
+$lang['ClientPay.received.boxtitle_received'] = 'Σας ευχαριστώ!';
+$lang['ClientPay.received.page_title'] = 'Πελάτης %1$s Πληρωμή που ελήφθη';
+$lang['ClientPay.confirm.description_credit'] = 'Πίστωση πληρωμής';
+$lang['ClientPay.confirm.description_invoice_number'] = '#%1$s';
+$lang['ClientPay.confirm.description_invoice_separator'] = ',';
+$lang['ClientPay.confirm.description_invoice'] = 'Τιμολόγιο #%1$s';
+$lang['ClientPay.confirm.field_edit'] = 'Επεξεργασία πληρωμής';
+$lang['ClientPay.confirm.field_submit'] = 'Υποβολή πληρωμής';
+$lang['ClientPay.confirm.total'] = 'Σύνολο:';
+$lang['ClientPay.confirm.account_exp'] = 'λήγει %1$s';
+$lang['ClientPay.confirm.account_info_type'] = '%1$s';
+$lang['ClientPay.confirm.account_info'] = '%1$s (%2$s) που τελειώνει σε %3$s';
+$lang['ClientPay.confirm.payment_details'] = 'Λεπτομέρειες πληρωμής';
+$lang['ClientPay.confirm.boxtitle_confirm'] = 'Επιβεβαίωση πληρωμής';
+$lang['ClientPay.confirm.page_title'] = 'Πελάτης %1$s Επιβεβαίωση πληρωμής';
+$lang['ClientPay.multipleinvoices.no_results'] = 'Δεν υπάρχουν τιμολόγια σε αυτό το νόμισμα.';
+$lang['ClientPay.multipleinvoices.text_datedue'] = 'Ημερομηνία λήξης';
+$lang['ClientPay.multipleinvoices.text_invoice'] = 'Τιμολόγιο #';
+$lang['ClientPay.multipleinvoices.text_due'] = 'Οφειλόμενο ποσό';
+$lang['ClientPay.multipleinvoices.text_amount'] = 'Ποσό προς πληρωμή';
+$lang['ClientPay.multipleinvoices.text_edit_amounts'] = 'Αντ\' αυτού, κάντε μερική πληρωμή';
+$lang['ClientPay.method.invoice_amount'] = 'Ποσό';
+$lang['ClientPay.method.invoice_quantity'] = 'Ποσότητα';
+$lang['ClientPay.method.invoice_description'] = 'Περιγραφή';
+$lang['ClientPay.method.heading_invoice'] = 'Τιμολόγιο #%1$s';
+$lang['ClientPay.method.no_options'] = 'Δεν υπάρχουν διαθέσιμες επιλογές πληρωμής.';
+$lang['ClientPay.method.field_credit_submit'] = 'Εφαρμογή πίστωσης και συνέχιση';
+$lang['ClientPay.method.field_submit'] = 'Επανεξέταση και επιβεβαίωση';
+$lang['ClientPay.method.field_apply_credit'] = 'Εφαρμογή πίστωσης (%1$s Διαθέσιμο)';
+$lang['ClientPay.method.field_paymentaccount_ach'] = 'Λογαριασμοί ACH';
+$lang['ClientPay.method.field_paymentaccount_autodebit'] = '(Αυτόματη χρέωση) %1$s %2$s - %3$s x%4$s';
+$lang['ClientPay.method.field_paymentaccount_cc'] = 'Λογαριασμοί πιστωτικών καρτών';
+$lang['ClientPay.method.field_paymentaccount'] = '%1$s %2$s - %3$s x%4$s';
+$lang['ClientPay.method.heading_payment_options'] = 'Επιλογές πληρωμής';
+$lang['ClientPay.method.heading_other_payment_options'] = 'Άλλες επιλογές πληρωμής';
+$lang['ClientPay.method.field_newdetails'] = 'Νέα στοιχεία πληρωμής';
+$lang['ClientPay.method.field_useaccount'] = 'Χρήση λογαριασμού πληρωμών';
+$lang['ClientPay.method.boxtitle_method'] = 'Πληρωμή';
+$lang['ClientPay.method.page_title'] = 'Πελάτης %1$s Μέθοδος πληρωμής';
+$lang['ClientPay.index.field_submit'] = 'Συνεχίστε';
+$lang['ClientPay.index.field_credit'] = 'Άλλο ποσό πληρωμής';
+$lang['ClientPay.index.boxtitle_pay'] = 'Πληρωμή';
+$lang['ClientPay.index.page_title'] = 'Πελάτης %1$s Πληρωμή';
+$lang['ClientPay.!error.payment_authorize'] = 'Η πληρωμή δεν μπόρεσε να εγκριθεί για τον επιλεγμένο λογαριασμό πληρωμών.  Παρακαλούμε επιλέξτε έναν άλλο λογαριασμό ή δοκιμάστε ξανά.';
+$lang['ClientPay.!error.invoice_voided'] = 'Αυτό το τιμολόγιο έχει ακυρωθεί και δεν μπορεί να πληρωθεί.';
+$lang['ClientPay.!error.payment_amounts'] = 'Παρακαλούμε επιλέξτε τιμολόγια προς πληρωμή ή πληκτρολογήστε άλλο ποσό πληρωμής.';
+$lang['ClientPay.!error.invalid_amount'] = 'Άλλα ποσά πληρωμών δεν μπορούν να είναι αρνητικά.';
+$lang['ClientPay.!error.invalid_details'] = 'Παρακαλούμε επιλέξτε μια επιλογή πληρωμής.';
+$lang['ClientPay.!info.ach_verification_redirect'] = 'Πρέπει να επαληθεύσετε αυτόν τον λογαριασμό πριν τον χρησιμοποιήσετε για να πραγματοποιήσετε μια πληρωμή. Αυτή η διαδικασία θα σας ανακατευθύνει μακριά από την τρέχουσα σελίδα.';
+$lang['ClientPay.!info.ach_verification'] = 'Πρέπει να επαληθεύσετε αυτόν τον λογαριασμό πριν τον χρησιμοποιήσετε για να πραγματοποιήσετε μια πληρωμή.';
+$lang['ClientPay.!info.unverified_email_button'] = 'Επαναποστολή email επαλήθευσης';
+$lang['ClientPay.!info.unverified_email'] = 'Ένας σύνδεσμος εστάλη στη διεύθυνση ηλεκτρονικού ταχυδρομείου που δώσατε. Παρακαλούμε κάντε κλικ στο σύνδεσμο του email για να επαληθεύσετε τη διεύθυνση email σας πριν προχωρήσετε στην πληρωμή.';
+$lang['ClientPay.!success.invoice_paid'] = 'Αυτό το τιμολόγιο έχει ήδη πληρωθεί.';
+$lang['ClientPay.!success.credit_partial_processed'] = 'Οι διαθέσιμες πιστώσεις έχουν εφαρμοστεί, αλλά υπάρχει ακόμη υπόλοιπο. Παρακαλούμε να ολοκληρώσετε αυτή τη συναλλαγή καταβάλλοντας το υπόλοιπο ποσό που αναγράφεται παρακάτω.';
+$lang['ClientPay.!success.credit_full_processed'] = 'Οι διαθέσιμες πιστώσεις έχουν εφαρμοστεί.';
+$lang['ClientPay.!success.payment_processed'] = 'Η πληρωμή διεκπεραιώθηκε επιτυχώς για το %1$s. Αριθμός συναλλαγής: %2$s';
+
