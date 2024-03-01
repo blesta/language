b9564085edc065c25cf5088e253b12c469d0e07c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:00 -0800

    Updated it_it language files

---
+++ language/it_it/currencies.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Currencies
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Currencies.!error.processor.empty'] = 'Non è stato impostato un processore del tasso di cambio.';
+$lang['Currencies.!error.processor.invalid'] = 'Processore del tasso di cambio non valido.';
+$lang['Currencies.!error.currency_code.is_default'] = 'La valuta %1$s è quella predefinita e non può essere eliminata.';
+$lang['Currencies.!error.currency_code.in_use'] = 'La valuta %1$s è attualmente in uso e non può essere cancellata.';
+$lang['Currencies.!error.exchange_updated.format'] = 'La data di aggiornamento dello scambio è in un formato non valido.';
+$lang['Currencies.!error.exchange_rate.format'] = 'Il tasso di cambio deve essere un numero.';
+$lang['Currencies.!error.suffix.length'] = 'Il suffisso di valuta non può superare i 10 caratteri.';
+$lang['Currencies.!error.prefix.length'] = 'Il prefisso della valuta non può superare i 10 caratteri.';
+$lang['Currencies.!error.precision.format'] = 'La precisione della valuta deve essere un numero compreso tra 0 e 4, incluso.';
+$lang['Currencies.!error.format.format'] = 'Formato di valuta non valido.';
+$lang['Currencies.!error.company_id.exists'] = 'ID azienda non valido.';
+$lang['Currencies.!error.code.exists'] = 'Una valuta con codice %1$s e ID azienda %2$s è già stata presa.';
+$lang['Currencies.!error.code.length'] = 'Il codice della valuta deve essere composto da 3 caratteri alfa, come definito nella norma ISO 4217.';
+
