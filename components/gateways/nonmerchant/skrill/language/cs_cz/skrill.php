bb58ecf4845a01ceeaf96ce22c2bbf512debc2ca
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:11 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/skrill/language/cs_cz/skrill.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Skrill
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Skrill.!error.merchant_id.empty'] = 'Zadejte prosím své ID obchodníka.';
+$lang['Skrill.!error.mqi.empty'] = 'Zadejte prosím své heslo MQI.';
+$lang['Skrill.!error.secret_word.empty'] = 'Zadejte prosím tajné slovo účtu.';
+$lang['Skrill.!error.user_email.valid'] = 'Zadejte prosím platnou e-mailovou adresu.';
+$lang['Skrill.!error.libxml_required'] = 'Pro tuto bránu jsou vyžadována rozšíření libxml a simplexml.';
+$lang['Skrill.invoice.description'] = 'Faktury';
+$lang['Skrill.buildprocess.submit'] = 'Plaťte pomocí Skrillu';
+$lang['Skrill.mqi_note'] = 'Zadejte heslo MQI API svého účtu Skrill.';
+$lang['Skrill.merchant_id_note'] = 'Zadejte ID obchodníka účtu Skrill.';
+$lang['Skrill.user_email_note'] = 'Zadejte e-mailovou adresu svého účtu Skrill.';
+$lang['Skrill.secret_word_note'] = 'Zadejte svůj účet Skrill Secret Word.';
+$lang['Skrill.mqi'] = 'Heslo MQI';
+$lang['Skrill.merchant_id'] = 'ID obchodníka';
+$lang['Skrill.secret_word'] = 'Tajné slovo';
+$lang['Skrill.user_email'] = 'E-mail';
+$lang['Skrill.description'] = 'Platforma elektronického obchodování se sídlem v Londýně, která se zaměřuje na nízkonákladové mezinárodní převody peněz.';
+$lang['Skrill.name'] = 'Skrill';
+
