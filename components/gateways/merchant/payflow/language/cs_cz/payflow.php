781ed9244198f6825bfd800038e56887055d3f5a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:56 -0800

    Updated cs_cz language files

---
+++ components/gateways/merchant/payflow/language/cs_cz/payflow.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payflow
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payflow.tooltip_partner'] = 'Jedná se o obchodního partnera, kterého vám přidělil autorizovaný prodejce PayPal, který vás zaregistroval pro Payflow SDK. Pokud byl váš účet zakoupen přímo od společnosti PayPal, použijte místo toho "PayPal".';
+$lang['Payflow.tooltip_user'] = 'Pokud máte pro svůj účet nastaveno více uživatelů, zadejte uživatele oprávněného zpracovávat transakce. V opačném případě zadejte stejnou hodnotu jako u přihlášení prodejce/obchodníka.';
+$lang['Payflow.test_mode'] = 'Testovací režim';
+$lang['Payflow.partner'] = 'Partner';
+$lang['Payflow.password'] = 'Heslo';
+$lang['Payflow.user'] = 'Uživatel';
+$lang['Payflow.vendor'] = 'Přihlášení prodejce/obchodníka';
+$lang['Payflow.description'] = 'Služba Payflow Pro zpracovává všechny hlavní kreditní a debetní karty.';
+$lang['Payflow.name'] = 'PayPal Payflow Pro';
+$lang['Payflow.!error.test_mode.valid'] = 'Testovací režim musí být nastaven na "true", pokud je zadán.';
+$lang['Payflow.!error.password.format'] = 'Zadejte heslo o délce 6 až 32 znaků.';
+$lang['Payflow.!error.partner.empty'] = 'Zadejte prosím svého obchodního partnera.';
+$lang['Payflow.!error.vendor.empty'] = 'Zadejte prosím přihlašovací údaje k obchodnímu účtu.';
+$lang['Payflow.!error.user.empty'] = 'Zadejte prosím uživatele oprávněného zpracovávat transakce.';
+
