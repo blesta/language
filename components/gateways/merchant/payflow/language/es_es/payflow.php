d06597eac1bd198a9fcf4c46ebbc8978031ee63e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:23 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/payflow/language/es_es/payflow.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payflow
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payflow.!error.user.empty'] = 'Por favor, introduzca un usuario autorizado para procesar transacciones.';
+$lang['Payflow.!error.vendor.empty'] = 'Por favor, introduzca el nombre de usuario de su cuenta.';
+$lang['Payflow.!error.partner.empty'] = 'Por favor, introduzca su socio comercial.';
+$lang['Payflow.!error.password.format'] = 'Introduzca una contraseña de entre 6 y 32 caracteres.';
+$lang['Payflow.!error.test_mode.valid'] = 'El modo de prueba debe establecerse en "true" si se indica.';
+$lang['Payflow.name'] = 'PayPal Payflow Pro';
+$lang['Payflow.description'] = 'Payflow Pro gestiona las principales tarjetas de crédito y débito.';
+$lang['Payflow.vendor'] = 'Inicio de sesión de vendedor/comerciante';
+$lang['Payflow.user'] = 'Usuario';
+$lang['Payflow.password'] = 'Contraseña';
+$lang['Payflow.partner'] = 'Socio';
+$lang['Payflow.test_mode'] = 'Modo de prueba';
+$lang['Payflow.tooltip_user'] = 'Si tiene más de un usuario configurado para su cuenta, introduzca el usuario autorizado para procesar transacciones. De lo contrario, introduzca el mismo valor que en Inicio de sesión de vendedor/comerciante.';
+$lang['Payflow.tooltip_partner'] = 'Este es el socio comercial que le ha proporcionado un distribuidor autorizado de PayPal que le ha registrado para el SDK de Payflow. Si compró su cuenta directamente a PayPal, utilice "PayPal" en su lugar.';
+
