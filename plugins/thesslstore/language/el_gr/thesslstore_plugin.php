400d9c81ff59c275cdacd4f43edeed1f35b700b8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:05 -0800

    Updated el_gr language files

---
+++ plugins/thesslstore/language/el_gr/thesslstore_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Thesslstore Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['TheSSLStorePlugin.getCronTasks.tss_expiration_reminder_desc'] = 'Μια υπενθύμιση θα αποσταλεί 30 ημέρες πριν από την ημερομηνία λήξης του πιστοποιητικού.';
+$lang['TheSSLStorePlugin.getCronTasks.tss_expiration_reminder_name'] = 'Υπενθύμιση λήξης πιστοποιητικού SSL';
+$lang['TheSSLStorePlugin.name'] = 'TheSSLStore Plugin';
+
