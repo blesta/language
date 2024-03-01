4dfcf35b126d8b6ff768c07eb823b12c2cc63880
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:12 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/client_quotations.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Client Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientQuotations.!error.password_mismatch'] = 'Le mot de passe ne correspond pas à votre mot de passe de connexion.';
+$lang['ClientQuotations.!success.approved_quotation'] = 'Le devis a été approuvé avec succès.';
+$lang['ClientQuotations.index.page_title'] = 'Devis du client #%1$s';
+$lang['ClientQuotations.index.category_pending'] = 'En attente';
+$lang['ClientQuotations.index.category_approved'] = 'Approuvé';
+$lang['ClientQuotations.index.category_expired'] = 'Expiré';
+$lang['ClientQuotations.index.boxtitle_quotations'] = 'Citations';
+$lang['ClientQuotations.index.heading_quotation'] = 'Citation #';
+$lang['ClientQuotations.index.heading_title'] = 'Titre';
+$lang['ClientQuotations.index.heading_subtotal'] = 'Sous-total';
+$lang['ClientQuotations.index.heading_total'] = 'Montant';
+$lang['ClientQuotations.index.heading_date_created'] = 'Date de création';
+$lang['ClientQuotations.index.heading_date_expires'] = 'Date d\'expiration';
+$lang['ClientQuotations.index.heading_options'] = 'Options';
+$lang['ClientQuotations.index.option_view'] = 'Voir';
+$lang['ClientQuotations.index.option_approve'] = 'Approuver';
+$lang['ClientQuotations.index.no_results'] = 'Vous n\'avez pas de %1$s Citations.';
+$lang['ClientQuotations.approve.heading_approve'] = 'Approuver';
+$lang['ClientQuotations.approve.field_password'] = 'Se connecter Mot de passe pour approuver';
+$lang['ClientQuotations.approve.field_cancel'] = 'Annuler, ne pas approuver';
+$lang['ClientQuotations.approve.field_submit'] = 'Approuver';
+$lang['ClientQuotations.invoices.heading_invoice'] = 'Facture #';
+$lang['ClientQuotations.invoices.heading_amount'] = 'Montant';
+$lang['ClientQuotations.invoices.heading_paid'] = 'Payé';
+$lang['ClientQuotations.invoices.heading_date_billed'] = 'Date de facturation';
+$lang['ClientQuotations.invoices.no_results'] = 'Aucune facture n\'est associée à ce devis.';
+
