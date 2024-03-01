525741022f824aada08617b006fa9f0e6b599d7a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:39 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/standard/language/el_gr/config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Config
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Config.index.header_configuration'] = 'Διαμόρφωση';
+$lang['Config.index.header_configure_service'] = 'Διαμόρφωση %1$s';
+$lang['Config.index.text_free_domain_next_step_limited'] = 'Στο επόμενο βήμα, επιλέξτε ένα στοιχείο που πληροί τις προϋποθέσεις για ελάχιστη διάρκεια %1$s για να αποκτήσετε αυτό το domain δωρεάν για διάρκεια %2$s.';
+$lang['Config.index.text_free_domain_next_step'] = 'Στο επόμενο βήμα, επιλέξτε ένα στοιχείο που πληροί τις προϋποθέσεις για ελάχιστη διάρκεια %1$s για να αποκτήσετε αυτόν τον τομέα δωρεάν.';
+$lang['Config.index.text_free_domain'] = 'Μια επιλογή τιμολόγησης της τάξης του %1$s ή μεγαλύτερη σας δίνει τη δυνατότητα να αποκτήσετε δωρεάν domain στο επόμενο βήμα.';
+$lang['Config.index.header_term'] = 'Όρος και τιμή';
+$lang['Config.index.header_module'] = 'Διαμορφώσιμες επιλογές';
+$lang['Config.index.header_addons'] = 'Διαθέσιμα πρόσθετα';
+$lang['Config.index.continue_btn'] = 'Συνεχίστε';
+$lang['Config.index.term_day'] = '%1$s ημέρα';
+$lang['Config.index.term_days'] = '%1$s ημέρες';
+$lang['Config.index.term_week'] = '%1$s εβδομάδα';
+$lang['Config.index.term_weeks'] = '%1$s εβδομάδες';
+$lang['Config.index.term_month'] = '%1$s μήνας';
+$lang['Config.index.term_months'] = '%1$s μήνες';
+$lang['Config.index.term_year'] = '%1$s έτος';
+$lang['Config.index.term_years'] = '%1$s χρόνια';
+
