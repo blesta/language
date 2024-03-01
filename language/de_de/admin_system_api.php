64bf552ad8c0336d5de6ca94372542c480a9b15e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:35 -0800

    Updated de_de language files

---
+++ language/de_de/admin_system_api.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Admin System Api
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemApi.edit.field_apisubmit'] = 'Benutzer aktualisieren';
+$lang['AdminSystemApi.edit.field_notes'] = 'Anmerkungen';
+$lang['AdminSystemApi.edit.field_user'] = 'Benutzer';
+$lang['AdminSystemApi.edit.field_company'] = 'Unternehmen';
+$lang['AdminSystemApi.edit.boxtitle_edit'] = 'API-Benutzer bearbeiten';
+$lang['AdminSystemApi.add.field_apisubmit'] = 'Benutzer hinzufügen';
+$lang['AdminSystemApi.add.field_notes'] = 'Anmerkungen';
+$lang['AdminSystemApi.add.field_user'] = 'Benutzer';
+$lang['AdminSystemApi.add.field_company'] = 'Unternehmen';
+$lang['AdminSystemApi.add.boxtitle_add'] = 'API-Benutzer hinzufügen';
+$lang['AdminSystemApi.index.confirm_delete'] = 'Diesen API-Benutzer wirklich löschen?';
+$lang['AdminSystemApi.index.no_results'] = 'Es gibt keine API-Benutzer.';
+$lang['AdminSystemApi.index.option_delete'] = 'Löschen';
+$lang['AdminSystemApi.index.option_edit'] = 'bearbeiten';
+$lang['AdminSystemApi.index.heading_options'] = 'Optionen';
+$lang['AdminSystemApi.index.heading_date'] = 'Datum der Erstellung';
+$lang['AdminSystemApi.index.heading_company'] = 'Unternehmen';
+$lang['AdminSystemApi.index.heading_key'] = 'Schlüssel';
+$lang['AdminSystemApi.index.heading_user'] = 'Benutzer';
+$lang['AdminSystemApi.index.categorylink_keyadd'] = 'API-Benutzer hinzufügen';
+$lang['AdminSystemApi.index.boxtitle_api'] = 'API-Zugang';
+$lang['AdminSystemApi.index.page_title'] = 'Einstellungen > System > API-Zugang';
+$lang['AdminSystemApi.!success.key_updated'] = 'Der API-Benutzer wurde erfolgreich aktualisiert.';
+$lang['AdminSystemApi.!success.key_deleted'] = 'Der API-Benutzer wurde erfolgreich gelöscht.';
+$lang['AdminSystemApi.!success.key_added'] = 'Der API-Benutzer wurde erfolgreich hinzugefügt.';
+$lang['AdminSystemApi.!notice.api_not_restrictive'] = 'WARNUNG! Unternehmen ist das Unternehmen, mit dem sich der Benutzer bei der Verbindung authentifiziert. Der Zugriff auf die Daten anderer Unternehmen wird durch API-Anfragen, die einen solchen Zugriff unterstützen, nicht eingeschränkt.';
+
