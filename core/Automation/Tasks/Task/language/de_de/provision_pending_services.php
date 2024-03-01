4dfe5298ba4929e62090989a7d029825a00da9b4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:01 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/provision_pending_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Provision Pending Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.provision_pending_services.success'] = 'Der ausstehende Dienst #%1$s vom Client #%2$s ist jetzt aktiv.';
+$lang['Automation.task.provision_pending_services.error'] = 'Der ausstehende Dienst #%1$s vom Client #%2$s konnte nicht aktiviert werden.';
+$lang['Automation.task.provision_pending_services.completed'] = 'Die Aufgabe "Bezahlte ausstehende Dienstleistungen" ist abgeschlossen.';
+$lang['Automation.task.provision_pending_services.attempt'] = 'Versuch der Bereitstellung bezahlter ausstehender Dienstleistungen.';
+
