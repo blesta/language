3caa6fa46694f2b5eb6e44cca0ad9fa76f7fc0a6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:59 -0800

    Updated de_de language files

---
+++ plugins/order/components/antifraud/fraudlabspro/language/de_de/fraudlabspro.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fraudlabspro
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FraudLabsPro.settings.option_no'] = 'Nein';
+$lang['FraudLabsPro.settings.option_yes'] = 'Ja';
+$lang['FraudLabsPro.settings.field_follow_flp_result'] = 'Automatisches Ablehnen/Überprüfen auf der Grundlage von Empfehlungen von FraudLabs Pro';
+$lang['FraudLabsPro.settings.field_review_score'] = 'Mindestpunktzahl für die Überprüfung';
+$lang['FraudLabsPro.settings.field_reject_score'] = 'Mindestpunktzahl für Ablehnung';
+$lang['FraudLabsPro.settings.label_description'] = 'Holen Sie sich den kostenlosen FraudLabs Pro Micro Plan (500 Abfragen/Monat) von http://www.fraudlabspro.com/plan';
+$lang['FraudLabsPro.settings.field_api_key'] = 'API-Schlüssel';
+$lang['FraudLabsPro.!error.review.fraudlabspro'] = 'Ihre Bestellung wurde von FraudLabs Pro als potenziell betrügerisch eingestuft. Sie wird manuell überprüft werden.';
+$lang['FraudLabsPro.!error.reject.fraudlabspro'] = 'Ihre Bestellung wurde von FraudLabs Pro als Betrug eingestuft.';
+$lang['FraudLabsPro.!error.review.review_score'] = 'Ihre Bestellung muss manuell überprüft werden.';
+$lang['FraudLabsPro.!error.reject.reject_score'] = 'Ihre Bestellung kann zur Zeit nicht bearbeitet werden.';
+
