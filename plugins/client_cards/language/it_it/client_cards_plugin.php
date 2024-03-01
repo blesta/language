f30fd2772437a7bf54978c768530f5c6740cdc78
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:30 -0800

    Updated it_it language files

---
+++ plugins/client_cards/language/it_it/client_cards_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Client Cards Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientCardsPlugin.card_client.invoices'] = 'Fatture';
+$lang['ClientCardsPlugin.card_client.services'] = 'Servizi';
+$lang['ClientCardsPlugin.description'] = 'Visualizza le informazioni di riepilogo del client in schede sull\'interfaccia del client.';
+$lang['ClientCardsPlugin.name'] = 'Schede clienti';
+
