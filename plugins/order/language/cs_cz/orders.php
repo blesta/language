269252cbb0ef190a0cc1432f45da8b6dfea12899
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:14 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/orders.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Orders
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Orders.view.heading_options'] = 'Možnosti';
+$lang['Orders.view.heading_date_renews'] = 'Datum obnovení';
+$lang['Orders.view.heading_date_created'] = 'Datum vytvoření';
+$lang['Orders.view.heading_term'] = 'Termín';
+$lang['Orders.view.heading_label'] = 'Štítek';
+$lang['Orders.view.heading_package'] = 'Balíček';
+$lang['Orders.view.heading_date_billed'] = 'Datum vyúčtování';
+$lang['Orders.view.heading_paid'] = 'Placené';
+$lang['Orders.view.heading_amount'] = 'Částka';
+$lang['Orders.view.heading_invoice_number'] = 'Faktura #';
+$lang['Orders.view.heading_transactions'] = 'Transakce';
+$lang['Orders.view.heading_services'] = 'Služby';
+$lang['Orders.view.heading_invoices'] = 'Faktury';
+$lang['Orders.view.boxtitle_view_order'] = 'Zobrazit objednávku';
+$lang['Orders.index.no_results'] = 'Nemáte žádné objednávky.';
+$lang['Orders.index.option_cancel'] = 'Zrušit';
+$lang['Orders.index.option_view'] = 'Zobrazit';
+$lang['Orders.index.heading_options'] = 'Možnosti';
+$lang['Orders.index.heading_date_ordered'] = 'Datum objednání';
+$lang['Orders.index.heading_paid'] = 'Placené';
+$lang['Orders.index.heading_total'] = 'Celkem';
+$lang['Orders.index.heading_invoice_number'] = 'Faktura #';
+$lang['Orders.index.heading_status'] = 'Stav';
+$lang['Orders.index.heading_order_number'] = 'Objednávka #';
+$lang['Orders.index.boxtitle_orders'] = 'Historie objednávek';
+$lang['Orders.!success.order_canceled'] = 'Objednávka byla úspěšně zrušena.';
+$lang['Orders.view.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['Orders.view.no_transactions'] = 'S touto objednávkou nejsou spojeny žádné transakce.';
+$lang['Orders.view.no_services'] = 'S touto zakázkou nejsou spojeny žádné služby.';
+$lang['Orders.view.no_invoices'] = 'K této objednávce nejsou přiřazeny žádné faktury.';
+$lang['Orders.view.field_back_order_history'] = 'Zpět na historii objednávek';
+$lang['Orders.view.option_manage'] = 'Správa';
+$lang['Orders.view.option_view'] = 'Zobrazit';
+$lang['Orders.view.heading_date'] = 'Datum';
+$lang['Orders.view.heading_number'] = 'Číslo';
+$lang['Orders.view.heading_status'] = 'Stav';
+$lang['Orders.view.heading_applied'] = 'Aplikované';
+$lang['Orders.view.heading_type'] = 'Typ';
+
