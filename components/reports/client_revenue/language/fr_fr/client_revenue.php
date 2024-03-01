4879aa50c26a99afef0880f1e181b35a1b6df4a2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:11 -0800

    Updated fr_fr language files

---
+++ components/reports/client_revenue/language/fr_fr/client_revenue.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Client Revenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientRevenue.name'] = 'Recettes des clients';
+$lang['ClientRevenue.description'] = 'Calcule les recettes provenant d\'une devise pour chaque client en totalisant le montant payé pour toutes les factures associées au client.';
+$lang['ClientRevenue.option.any'] = 'Tous';
+$lang['ClientRevenue.options.currency'] = 'Monnaie';
+$lang['ClientRevenue.options.field_start_date'] = 'Date de début';
+$lang['ClientRevenue.options.field_end_date'] = 'Date de fin';
+$lang['ClientRevenue.heading.client_id_code'] = 'Identifiant du client';
+$lang['ClientRevenue.heading.client_name'] = 'Nom du client';
+$lang['ClientRevenue.heading.company'] = 'Entreprise';
+$lang['ClientRevenue.heading.total'] = 'Total';
+
