6d8bd2038b50565636726a682b5e1e50c98075d1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:34 -0800

    Updated cs_cz language files

---
+++ plugins/domains/language/cs_cz/domains_domains.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Domains Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsDomains.!error.invalid_term'] = 'Tuto doménu nelze po zvolený počet let prodloužit.';
+$lang['DomainsDomains.!error.invoices_renew_service'] = 'Doménu nelze prodloužit, dokud nejsou uhrazeny všechny faktury obsahující tuto doménu.';
+
