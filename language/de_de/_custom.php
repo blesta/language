ed7a374e2bec84428733175c0ca52423cf2f902c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:18 -0800

    Updated de_de language files

---
+++ language/de_de/_custom.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['_PaymentTypes.in_house_credit'] = 'Hausinterner Kredit';
+$lang['_PaymentTypes.money_order'] = 'Zahlungsanweisung';
+$lang['_PaymentTypes.check'] = 'Siehe';
+$lang['_PaymentTypes.cash'] = 'Bargeld';
+
