42dd0b639127cb654ee442b2c05e640351c69d4f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:12 -0800

    Updated el_gr language files

---
+++ language/el_gr/coupon_terms.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Coupon Terms
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CouponTerms.getperiods.onetime'] = 'Μία φορά';
+$lang['CouponTerms.getperiods.year'] = 'Έτος';
+$lang['CouponTerms.getperiods.month'] = 'Μήνας';
+$lang['CouponTerms.getperiods.week'] = 'Εβδομάδα';
+$lang['CouponTerms.getperiods.day'] = 'Ημέρα';
+$lang['CouponTerms.!error.term_id.exists'] = 'Ο συγκεκριμένος όρος κουπονιού δεν υπάρχει.';
+$lang['CouponTerms.!error.period.valid'] = 'Παρακαλώ εισάγετε μια έγκυρη περίοδο.';
+$lang['CouponTerms.!error.term.format'] = 'Παρακαλούμε εισάγετε έναν έγκυρο όρο για κάθε περίοδο.';
+$lang['CouponTerms.!error.coupon_id.exists'] = 'Το συγκεκριμένο κουπόνι δεν υπάρχει.';
+
