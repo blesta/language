086551a60374c859d171e7e56f44eac626b8c2fc
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:34 -0800

    Updated es_es language files

---
+++ language/es_es/package_groups.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Package Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageGroups.!error.names.format'] = 'Los nombres de los grupos de paquetes tienen un formato no válido.';
+$lang['PackageGroups.!error.names.empty_name'] = 'Especifique un nombre de grupo de paquetes para cada idioma.';
+$lang['PackageGroups.!error.names.empty_lang'] = 'Especifique un código de idioma de grupo de paquetes para cada nombre.';
+$lang['PackageGroups.!error.descriptions.format'] = 'Las descripciones de los grupos de paquetes tienen un formato no válido.';
+$lang['PackageGroups.!error.descriptions.empty_lang'] = 'Especifique un código de idioma de grupo de paquetes para cada descripción.';
+$lang['PackageGroups.!error.type.format'] = 'Tipo de grupo no válido.';
+$lang['PackageGroups.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['PackageGroups.!error.parents.format'] = 'Al menos un ID de grupo de padres dado es un grupo no estándar no disponible para su uso como padre.';
+$lang['PackageGroups.!error.hidden.format'] = 'Si este grupo debe estar oculto en la interfaz debe establecerse en 1 o 0.';
+$lang['PackageGroups.!error.allow_upgrades.format'] = 'Si los paquetes dentro de este grupo pueden ser actualizados/desactualizados debe establecerse a 1 o 0.';
+$lang['PackageGroups.gettypes.standard'] = 'Estándar';
+$lang['PackageGroups.gettypes.addon'] = 'Complemento';
+
