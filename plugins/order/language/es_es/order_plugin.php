c18898d109e322dcc416492481551dcaea594daf
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:41 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/order_plugin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Order Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderPlugin.name'] = 'Sistema de pedidos';
+$lang['OrderPlugin.admin_main.name'] = 'Pedidos';
+$lang['OrderPlugin.admin_forms.name'] = 'Formularios de pedido';
+$lang['OrderPlugin.admin_affiliates.name'] = 'Afiliados';
+$lang['OrderPlugin.admin_affiliates.payouts'] = 'Pagos a afiliados';
+$lang['OrderPlugin.admin_affiliates.payment_methods'] = 'Métodos de pago para afiliados';
+$lang['OrderPlugin.client.name'] = 'Pida';
+$lang['OrderPlugin.client_affiliates.name'] = 'Afiliados';
+$lang['OrderPlugin.action_staff_client.affiliates'] = 'Gestión de afiliados';
+$lang['OrderPlugin.client_orders.name'] = 'Historial de pedidos';
+$lang['OrderPlugin.cron.accept_paid_orders_name'] = 'Aceptar pedidos pendientes pagados';
+$lang['OrderPlugin.cron.accept_paid_orders_desc'] = 'Acepta automáticamente los pedidos pendientes de pago si el formulario de pedido lo permite.';
+$lang['OrderPlugin.cron.affiliate_monthly_report_name'] = 'Informe mensual de afiliación';
+$lang['OrderPlugin.cron.affiliate_monthly_report_desc'] = 'El día 1 de cada mes se enviará por correo electrónico un informe mensual de afiliación correspondiente al mes anterior.';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_name'] = 'Pedidos de afiliados';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_desc'] = 'Evalúa si las referencias de afiliados pendientes han llegado a su vencimiento y, en caso afirmativo, las actualiza y los importes de pago.';
+$lang['OrderPlugin.cron.process_abandoned_orders_name'] = 'Procesar pedidos abandonados';
+$lang['OrderPlugin.cron.process_abandoned_orders_desc'] = 'Enviar correos electrónicos recordatorios, realizar cancelaciones y desactivar clientes por pedidos abandonados.';
+$lang['OrderPlugin.event_getsearchoptions.orders'] = 'Búsqueda de pedidos';
+$lang['OrderPlugin.card_client.orders'] = 'Pedidos';
+$lang['AdminCompanyPlugins.settings.event.domains.delete'] = 'Este evento se activa cuando se elimina un TLD del Administrador de Dominios.';
+$lang['AdminCompanyPlugins.settings.event.domains.enable'] = 'Este evento se activa cuando se habilita un TLD desde el Administrador de Dominios.';
+$lang['AdminCompanyPlugins.settings.event.domains.disable'] = 'Este evento se activa cuando se desactiva un TLD desde el Administrador de Dominios.';
+$lang['AdminCompanyPlugins.settings.event.domains.updatedomainscompanysettingsafter'] = 'Este evento se activa después de que se actualice la configuración de la empresa del Administrador de Dominios.';
+$lang['AdminCompanyPlugins.settings.event.domains.updatepricingafter'] = 'Este evento se activa cuando se actualiza el precio de un TLD.';
+$lang['OrderPlugin.permission.admin_affiliates'] = 'Afiliados';
+$lang['OrderPlugin.permission.admin_affiliates_add'] = 'Añadir afiliado';
+$lang['OrderPlugin.permission.admin_affiliates_update'] = 'Actualizar afiliado';
+$lang['OrderPlugin.permission.admin_affiliates_activate'] = 'Activar afiliado';
+$lang['OrderPlugin.permission.admin_affiliates_deactivate'] = 'Desactivar afiliado';
+$lang['OrderPlugin.permission.admin_affiliates_settings'] = 'Ajustes de afiliación';
+$lang['OrderPlugin.permission.admin_payouts'] = 'Pagos';
+$lang['OrderPlugin.permission.admin_payouts_edit'] = 'Editar pago';
+$lang['OrderPlugin.permission.admin_payouts_approve'] = 'Aprobar pago';
+$lang['OrderPlugin.permission.admin_payouts_decline'] = 'Rechazar el pago';
+$lang['OrderPlugin.permission.admin_payment_methods'] = 'Formas de pago';
+$lang['OrderPlugin.permission.admin_payment_methods_add'] = 'Añadir método de pago';
+$lang['OrderPlugin.permission.admin_payment_methods_edit'] = 'Editar método de pago';
+$lang['OrderPlugin.permission.admin_payment_methods_delete'] = 'Borrar método de pago';
+$lang['OrderPlugin.permission.admin_main'] = 'Pedidos';
+$lang['OrderPlugin.permission.admin_main_widget'] = 'Widget de perfil de cliente';
+
