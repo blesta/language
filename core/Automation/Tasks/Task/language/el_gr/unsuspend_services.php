00ffa985f427b095d13521dcf9c7604a51baa1d9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:24 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/unsuspend_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Unsuspend Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.unsuspendservices.attempt'] = 'Προσπάθεια άρσης της αναστολής πληρωμένων υπηρεσιών που έχουν ανασταλεί.';
+$lang['Automation.task.unsuspendservices.completed'] = 'Η εργασία unsuspend services έχει ολοκληρωθεί.';
+$lang['Automation.task.unsuspendservices.error'] = 'Η υπηρεσία #%1$s από τον πελάτη %2$s δεν μπόρεσε να ανασταλεί.';
+$lang['Automation.task.unsuspendservices.success'] = 'Η υπηρεσία #%1$s από τον πελάτη %2$s δεν έχει ανασταλεί.';
+
