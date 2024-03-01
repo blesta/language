d4ba1cccce38bbd2b6bea338b4eaa1ad615de2e4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:56 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_system_companies.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Admin System Companies
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemCompanies.!tooltip.hostname'] = 'Το όνομα κεντρικού υπολογιστή θα πρέπει να περιέχει μόνο το όνομα τομέα, το TLD και τον προαιρετικό αριθμό θύρας. Για παράδειγμα, \'domain.com\' ή \'subdomain.domain.com:8080\'.';
+$lang['AddSystemCompanies.edit.field_companysubmit'] = 'Επεξεργασία εταιρείας';
+$lang['AdminSystemCompanies.edit.field_fax'] = 'Φαξ';
+$lang['AdminSystemCompanies.edit.field_phone'] = 'Τηλέφωνο';
+$lang['AdminSystemCompanies.edit.field_address'] = 'Διεύθυνση';
+$lang['AdminSystemCompanies.edit.field_hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['AdminSystemCompanies.edit.field_name'] = 'Όνομα';
+$lang['AdminSystemCompanies.edit.boxtitle_editcompany'] = 'Επεξεργασία εταιρείας';
+$lang['AdminSystemCompanies.edit.page_title'] = 'Ρυθμίσεις > Σύστημα > Εταιρείες > Επεξεργασία εταιρείας';
+$lang['AddSystemCompanies.add.field_companysubmit'] = 'Δημιουργία εταιρείας';
+$lang['AdminSystemCompanies.add.field_fax'] = 'Φαξ';
+$lang['AdminSystemCompanies.add.field_phone'] = 'Τηλέφωνο';
+$lang['AdminSystemCompanies.add.field_address'] = 'Διεύθυνση';
+$lang['AdminSystemCompanies.add.field_hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['AdminSystemCompanies.add.field_name'] = 'Όνομα';
+$lang['AdminSystemCompanies.add.field_company'] = 'Εισαγωγή ρυθμίσεων εταιρείας από';
+$lang['AdminSystemCompanies.add.boxtitle_addcompany'] = 'Προσθήκη εταιρείας';
+$lang['AdminSystemCompanies.add.page_title'] = 'Ρυθμίσεις > Σύστημα > Εταιρείες > Προσθήκη εταιρείας';
+$lang['AdminSystemCompanies.index.confirm_delete'] = 'Πραγματικά διαγράψετε αυτή την εταιρεία; Όλα όσα σχετίζονται με αυτή την εταιρεία θα εξαφανιστούν.';
+$lang['AdminSystemCompanies.index.option_delete'] = 'Διαγραφή';
+$lang['AdminSystemCompanies.index.option_edit'] = 'Επεξεργασία';
+$lang['AdminSystemCompanies.index.text_options'] = 'Επιλογές';
+$lang['AdminSystemCompanies.index.text_phone'] = 'Τηλέφωνο';
+$lang['AdminSystemCompanies.index.text_address'] = 'Διεύθυνση';
+$lang['AdminSystemCompanies.index.text_hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['AdminSystemCompanies.index.text_name'] = 'Όνομα';
+$lang['AdminSystemCompanies.index.no_results'] = 'Δεν υπάρχουν εταιρείες.';
+$lang['AdminSystemCompanies.index.categorylink_addcompany'] = 'Προσθήκη εταιρείας';
+$lang['AdminSystemCompanies.index.boxtitle_companies'] = 'Εταιρείες';
+$lang['AdminSystemCompanies.index.page_title'] = 'Ρυθμίσεις > Σύστημα > Εταιρείες';
+$lang['AdminSystemCompanies.!success.company_deleted'] = 'Η εταιρεία %1$s διαγράφηκε επιτυχώς!';
+$lang['AdminSystemCompanies.!success.company_edited'] = 'Η εταιρεία %1$s ενημερώθηκε επιτυχώς!';
+$lang['AdminSystemCompanies.!success.company_added'] = 'Η εταιρεία %1$s δημιουργήθηκε με επιτυχία!';
+
