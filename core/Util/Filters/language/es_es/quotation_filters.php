8e4549288d1056933e7c60386fd8c2132d9f134e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:38 -0800

    Updated es_es language files

---
+++ core/Util/Filters/language/es_es/quotation_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Quotation Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.quotation_filters.any'] = 'Cualquier';
+$lang['Util.filters.quotation_filters.field_quotation_number'] = 'Presupuesto';
+$lang['Util.filters.quotation_filters.field_currency'] = 'Moneda';
+$lang['Util.filters.quotation_filters.field_quotation_line'] = 'Contenido de la partida';
+
