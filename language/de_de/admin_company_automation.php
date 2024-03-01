7bc99d11543d615b118e1ec641173a64c79d2c26
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:21 -0800

    Updated de_de language files

---
+++ language/de_de/admin_company_automation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Company Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyAutomation.getintervals.text_hours'] = 'Stunden';
+$lang['AdminCompanyAutomation.getintervals.text_hour'] = 'Stunde';
+$lang['AdminCompanyAutomation.getintervals.text_minutes'] = 'Minuten';
+$lang['AdminCompanyAutomation.getintervals.text_minute'] = 'Minute';
+$lang['AdminCompanyAutomation.index.confirm_clear_task'] = 'Sind Sie sicher, dass Sie die Sperre dieser Cron-Aufgabe aufheben wollen? Wenn diese Aufgabe ständig blockiert wird, liegt möglicherweise ein ernsteres Problem vor. Versuchen Sie, die Cron-Aufgabe manuell auszuführen, oder überprüfen Sie das Cron-Protokoll auf Fehler, um die Ursache zu ermitteln.';
+$lang['AdminCompanyAutomation.index.option_clear_task'] = 'Aufgabensperre aufheben';
+$lang['AdminCompanyAutomation.index.text_cron_never_ran'] = 'Der Cron wurde noch nie ausgeführt.';
+$lang['AdminCompanyAutomation.index.text_cron_last_ran'] = 'Der Cron lief zuletzt am %1$s.';
+$lang['AdminCompanyAutomation.index.no_cron_lastran'] = 'Niemals';
+$lang['AdminCompanyAutomation.index.no_results'] = 'Es gibt keine Cron-Aufgaben.';
+$lang['AdminCompanyAutomation.index.text_task_lastran'] = 'Aufgabe Zuletzt ausgeführt:';
+$lang['AdminCompanyAutomation.index.text_starttime'] = 'Startzeit';
+$lang['AdminCompanyAutomation.index.text_interval'] = 'Intervall';
+$lang['AdminCompanyAutomation.index.field_enabled'] = 'Aktiviert';
+$lang['AdminCompanyAutomation.index.field_automationsubmit'] = 'Einstellungen aktualisieren';
+$lang['AdminCompanyAutomation.index.boxtitle_automation'] = 'Automatisierung';
+$lang['AdminCompanyAutomation.index.page_title'] = 'Einstellungen > Unternehmen > Automatisierung';
+$lang['AdminCompanyAutomation.!success.task_cleared'] = 'Die Cron-Aufgabe wurde gelöscht.';
+$lang['AdminCompanyAutomation.!success.automation_updated'] = 'Die Automatisierungseinstellungen wurden erfolgreich aktualisiert!';
+$lang['AdminCompanyAutomation.!notice.cron_task_interval'] = 'Es gibt Automatisierungsaufgaben, die jede Minute laufen sollen, aber Ihr Cron scheint nicht jede Minute zu laufen. Aktualisieren Sie Ihren Cron so, dass er jede Minute läuft, um die Vorteile dieser kürzeren Intervalle zu nutzen.';
+
