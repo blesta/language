df838f5c5b35811778b1e2fa6028a69f6037a1b0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:04 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_dialog.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Client Dialog
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDialog.password.btn_use_password'] = 'Χρήση κωδικού πρόσβασης';
+$lang['ClientDialog.password.btn_cancel'] = 'Ακύρωση';
+$lang['ClientDialog.password.btn_generate'] = 'Δημιουργία';
+$lang['ClientDialog.password.field_modal_password_copied'] = 'Έχω αντιγράψει αυτόν τον κωδικό πρόσβασης σε ασφαλές μέρος';
+$lang['ClientDialog.password.field_modal_password'] = 'Κωδικός πρόσβασης';
+$lang['ClientDialog.password.title'] = 'Δημιουργία κωδικού πρόσβασης';
+$lang['ClientDialog.confirm.text_no'] = 'Όχι';
+$lang['ClientDialog.confirm.text_yes'] = 'Ναι';
+
