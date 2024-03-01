49712c297a5d3e26d1b0acae0f2d0737de928e87
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:29 -0800

    Updated el_gr language files

---
+++ language/el_gr/package_options.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Package Options
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptions.getfields.label_textarea_setup_recurring'] = '%1$s + %2$s setup (ανανεώνεται @ %3$s)';
+$lang['PackageOptions.getfields.label_textarea_setup'] = '%1$s + %2$s ρύθμιση';
+$lang['PackageOptions.getfields.label_textarea_recurring'] = '%1$s (ανανεώνεται @ %2$s)';
+$lang['PackageOptions.getfields.label_textarea'] = '%1$s';
+$lang['PackageOptions.getfields.label_password_setup_recurring'] = '%1$s + %2$s setup (ανανεώνεται @ %3$s)';
+$lang['PackageOptions.getfields.label_password_setup'] = '%1$s + %2$s ρύθμιση';
+$lang['PackageOptions.getfields.label_password_recurring'] = '%1$s (ανανεώνεται @ %2$s)';
+$lang['PackageOptions.getfields.label_password'] = '%1$s';
+$lang['PackageOptions.getfields.label_text_setup_recurring'] = '%1$s + %2$s setup (ανανεώνεται @ %3$s)';
+$lang['PackageOptions.getfields.label_text_setup'] = '%1$s + %2$s ρύθμιση';
+$lang['PackageOptions.getfields.label_text_recurring'] = '%1$s (ανανεώνεται @ %2$s)';
+$lang['PackageOptions.getfields.label_text'] = '%1$s';
+$lang['PackageOptions.getfields.label_checkbox_setup_recurring'] = '%1$s (%2$s + %3$s εγκατάσταση, ανανεώνεται @ %4$s)';
+$lang['PackageOptions.getfields.label_checkbox_setup'] = '%1$s (%2$s + %3$s ρύθμιση)';
+$lang['PackageOptions.getfields.label_checkbox_recurring'] = '%1$s (%2$s, ανανεώνεται @ %3$s)';
+$lang['PackageOptions.getfields.label_checkbox'] = '%1$s (%2$s)';
+$lang['PackageOptions.getfields.label_select_setup_recurring'] = '%1$s (%2$s + %3$s εγκατάσταση, ανανεώνεται @ %4$s)';
+$lang['PackageOptions.getfields.label_select_setup'] = '%1$s (%2$s + %3$s ρύθμιση)';
+$lang['PackageOptions.getfields.label_select_recurring'] = '%1$s (%2$s, ανανεώνεται @ %3$s)';
+$lang['PackageOptions.getfields.label_select'] = '%1$s (%2$s)';
+$lang['PackageOptions.getfields.label_radio_setup_recurring'] = '%1$s (%2$s + %3$s εγκατάσταση, ανανεώνεται @ %4$s)';
+$lang['PackageOptions.getfields.label_radio_setup'] = '%1$s (%2$s + %3$s ρύθμιση)';
+$lang['PackageOptions.getfields.label_radio_recurring'] = '%1$s (%2$s, ανανεώνεται @ %3$s)';
+$lang['PackageOptions.getfields.label_radio'] = '%1$s (%2$s)';
+$lang['PackageOptions.getfields.label_quantity_setup_recurring'] = 'x %1$s %2$s + %3$s setup (ανανεώνεται @ %4$s)';
+$lang['PackageOptions.getfields.label_quantity_setup'] = 'x %1$s %2$s + %3$s ρύθμιση';
+$lang['PackageOptions.getfields.label_quantity_recurring'] = 'x %1$s %2$s (ανανεώνεται @ %3$s)';
+$lang['PackageOptions.getfields.label_quantity'] = 'x %1$s %2$s';
+$lang['PackageOptions.getvaluestatuses.inactive'] = 'Ανενεργό';
+$lang['PackageOptions.getvaluestatuses.active'] = 'Ενεργό';
+$lang['PackageOptions.gettypes.password'] = 'Κωδικός πρόσβασης';
+$lang['PackageOptions.gettypes.textarea'] = 'Περιοχή κειμένου';
+$lang['PackageOptions.gettypes.text'] = 'Κείμενο';
+$lang['PackageOptions.gettypes.radio'] = 'Ραδιόφωνο';
+$lang['PackageOptions.gettypes.quantity'] = 'Ποσότητα';
+$lang['PackageOptions.gettypes.select'] = 'Drop-down';
+$lang['PackageOptions.gettypes.checkbox'] = 'Κουτάκι ελέγχου';
+$lang['PackageOptions.!error.values[][pricing][][currency].format'] = 'Ο κωδικός νομίσματος πρέπει να είναι 3 χαρακτήρες.';
+$lang['PackageOptions.!error.values[][pricing][][setup_fee].format'] = 'Η αμοιβή εγκατάστασης πρέπει να είναι ένας αριθμός.';
+$lang['PackageOptions.!error.values[][pricing][][price_renews].valid'] = 'Η τιμή ανανέωσης δεν μπορεί να οριστεί για μία χρονική περίοδο.';
+$lang['PackageOptions.!error.values[][pricing][][price_renews].format'] = 'Η τιμή ανανέωσης πρέπει να είναι ένας αριθμός.';
+$lang['PackageOptions.!error.values[][pricing][][price].format'] = 'Η τιμή πρέπει να είναι ένας αριθμός.';
+$lang['PackageOptions.!error.values[][pricing][][period].format'] = 'Μη έγκυρος τύπος περιόδου.';
+$lang['PackageOptions.!error.values[][pricing][][term].valid'] = 'Ο όρος πρέπει να είναι μεγαλύτερος από 0.';
+$lang['PackageOptions.!error.values[][pricing][][term].length'] = 'Το μήκος του όρου δεν μπορεί να υπερβαίνει τους 5 χαρακτήρες.';
+$lang['PackageOptions.!error.values[][pricing][][term].format'] = 'Ο όρος πρέπει να είναι αριθμός.';
+$lang['PackageOptions.!error.values[][pricing][][id].delete_in_use'] = 'Η τιμολόγηση της αξίας επιλογής δεν μπορεί να διαγραφεί επειδή χρησιμοποιείται επί του παρόντος από μια υπηρεσία.';
+$lang['PackageOptions.!error.values[][pricing][][id].exists'] = 'Μη έγκυρο αναγνωριστικό τιμολόγησης επιλογής πακέτου.';
+$lang['PackageOptions.!error.hidden.valid'] = 'Η επιλογή αυτή πρέπει να είναι 0 ή 1 για την απόκρυψη.';
+$lang['PackageOptions.!error.option_id.exists'] = 'Μη έγκυρο αναγνωριστικό επιλογής πακέτου.';
+$lang['PackageOptions.!error.groups.exists'] = 'Τουλάχιστον ένα από τα αναγνωριστικά ομάδας επιλογών πακέτου που δόθηκαν δεν υπάρχει ή δεν ανήκει στην ίδια εταιρεία.';
+$lang['PackageOptions.!error.values[][id].exists'] = 'Μη έγκυρο αναγνωριστικό τιμής επιλογής πακέτου.';
+$lang['PackageOptions.!error.values[][default].format'] = 'Η προεπιλογή πρέπει να είναι ένας αριθμός.';
+$lang['PackageOptions.!error.values[][default].qty_valid'] = 'Η επιλεγμένη ποσότητα προεπιλεγμένης τιμής είναι άκυρη για τα ρυθμισμένα min, max και step.';
+$lang['PackageOptions.!error.values[][default].inactive'] = 'Μια ανενεργή τιμή επιλογής δεν μπορεί να οριστεί ως προεπιλεγμένη τιμή.';
+$lang['PackageOptions.!error.values[][status].valid'] = 'Παρακαλούμε επιλέξτε μια έγκυρη κατάσταση τιμής επιλογής.';
+$lang['PackageOptions.!error.values[][value].delete_in_use'] = 'Η τιμή επιλογής δεν μπορεί να διαγραφεί επειδή χρησιμοποιείται επί του παρόντος από μια υπηρεσία.';
+$lang['PackageOptions.!error.values[][value].edit_in_use'] = 'Η τιμή της επιλογής δεν μπορεί να αλλάξει επειδή χρησιμοποιείται επί του παρόντος από μια υπηρεσία.';
+$lang['PackageOptions.!error.values[][value].length'] = 'Η τιμή της επιλογής δεν μπορεί να υπερβαίνει τους 255 χαρακτήρες σε μήκος.';
+$lang['PackageOptions.!error.values[][name].length'] = 'Το όνομα της τιμής επιλογής δεν μπορεί να υπερβαίνει τους 128 χαρακτήρες σε μήκος.';
+$lang['PackageOptions.!error.values[][name].empty'] = 'Εισάγετε ένα όνομα για την τιμή της επιλογής.';
+$lang['PackageOptions.!error.values[][max].valid'] = 'Η μέγιστη οριακή τιμή μπορεί να οριστεί μόνο για τον τύπο της ποσότητας και πρέπει να έχει τιμή 1 ή μεγαλύτερη.';
+$lang['PackageOptions.!error.values[][min].valid'] = 'Μια ελάχιστη οριακή τιμή μπορεί να οριστεί μόνο για τον τύπο της ποσότητας και πρέπει να έχει τιμή 0 ή μεγαλύτερη.';
+$lang['PackageOptions.!error.values[][step].valid'] = 'Μια τιμή βήματος μπορεί να οριστεί μόνο για τον τύπο ποσότητας και πρέπει να έχει τιμή 1 ή μεγαλύτερη.';
+$lang['PackageOptions.!error.values.single_default_value'] = 'Μόνο μία τιμή μπορεί να επιλεγεί ως προεπιλεγμένη τιμή.';
+$lang['PackageOptions.!error.values.active_status'] = 'Τουλάχιστον μία τιμή επιλογής πρέπει να είναι ενεργή.';
+$lang['PackageOptions.!error.values.select_value'] = 'Τουλάχιστον μία τιμή επιλογής περιέχει μη έγκυρους ειδικούς χαρακτήρες.';
+$lang['PackageOptions.!error.values.unique'] = 'Οι τιμές των επιλογών πρέπει να είναι μοναδικές.';
+$lang['PackageOptions.!error.values.count'] = 'Για τους τύπους checkbox, quantity, text, textarea ή password μπορεί να υπάρχει μόνο μία καταχώρηση τιμής επιλογής.';
+$lang['PackageOptions.!error.type.valid'] = 'Παρακαλούμε επιλέξτε έναν τύπο επιλογής.';
+$lang['PackageOptions.!error.name.length'] = 'Το μήκος του ονόματος δεν μπορεί να υπερβαίνει τους 128 χαρακτήρες.';
+$lang['PackageOptions.!error.name.empty'] = 'Παρακαλούμε εισάγετε ένα όνομα για αυτή την επιλογή.';
+$lang['PackageOptions.!error.label.length'] = 'Το μήκος της ετικέτας δεν μπορεί να υπερβαίνει τους 128 χαρακτήρες.';
+$lang['PackageOptions.!error.label.empty'] = 'Παρακαλώ εισάγετε μια ετικέτα.';
+$lang['PackageOptions.!error.company_id.exists'] = 'Μη έγκυρο αναγνωριστικό εταιρείας.';
+
