e92af0a8b18097ea92a1ff9e3e2a10699caffb04
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:51 -0800

    Updated fr_fr language files

---
+++ plugins/order/views/templates/standard/language/fr_fr/main.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Main.steps.step_1_name'] = 'Étape 1';
+$lang['Main.steps.step_1_description'] = 'Choisissez votre plan';
+$lang['Main.steps.step_2_name'] = 'Étape 2';
+$lang['Main.steps.step_2_description'] = 'Configuration';
+$lang['Main.steps.step_3_name'] = 'Étape 3';
+$lang['Main.steps.step_3_description'] = 'Ordonnance de réexamen';
+$lang['Main.steps.step_4_name'] = 'Étape 4';
+$lang['Main.steps.step_4_description'] = 'Sortie de caisse';
+$lang['Main.index.domain_bundle_eligble'] = 'Qualifie pour un domaine gratuit';
+$lang['Main.index.field_currency'] = 'Monnaie :';
+$lang['Main.packages.text_free_domain'] = 'Sélectionnez un forfait dans ce groupe avec une option de prix de %1$s ou plus pour un domaine gratuit.';
+$lang['Main.index.select_btn'] = 'Ajouter au panier';
+$lang['Main.index.sold_out'] = 'En rupture de stock';
+$lang['Main.index.client_limit'] = 'Limite atteinte';
+$lang['Main.packages.term_day'] = 'jour %1$s';
+$lang['Main.packages.term_days'] = '%1$s jours';
+$lang['Main.packages.term_week'] = 'Semaine %1$s';
+$lang['Main.packages.term_weeks'] = 'semaines %1$s';
+$lang['Main.packages.term_month'] = 'mois %1$s';
+$lang['Main.packages.term_months'] = '%1$s mois';
+$lang['Main.packages.term_year'] = '%1$s année';
+$lang['Main.packages.term_years'] = '%1$s années';
+
