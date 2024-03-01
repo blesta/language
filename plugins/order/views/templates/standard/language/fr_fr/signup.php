e0a68018489eb388dbdf657e2c5b2224f3f8b400
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:52 -0800

    Updated fr_fr language files

---
+++ plugins/order/views/templates/standard/language/fr_fr/signup.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Signup
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Signup.index.header_login_or_signup'] = 'S\'inscrire ou se connecter';
+$lang['Signup.index.link_resetpassword'] = 'Réinitialiser mon mot de passe';
+$lang['Signup.index.header_login'] = 'Se connecter';
+$lang['Signup.index.heading_contact'] = 'Informations sur le contact';
+$lang['Signup.index.heading_billing'] = 'Informations sur la facturation';
+$lang['Signup.index.heading_authentication'] = 'Authentification';
+$lang['Signup.index.heading_settings'] = 'Paramètres supplémentaires';
+
