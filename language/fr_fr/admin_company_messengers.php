ec0caba9d6965d98cea1ae3a97f059ccaea0f091
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:50 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_company_messengers.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Admin Company Messengers
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyMessengers.!success.upgraded'] = 'Le messager a été mis à niveau avec succès.';
+$lang['AdminCompanyMessengers.!success.installed'] = 'Le messager a été installé avec succès.';
+$lang['AdminCompanyMessengers.!success.uninstalled'] = 'Le messager a été désinstallé avec succès.';
+$lang['AdminCompanyMessengers.!success.messenger_options_updated'] = 'La configuration de la messagerie a été mise à jour avec succès !';
+$lang['AdminCompanyMessengers.!success.edit_template_updated'] = 'Le modèle de message a été mis à jour avec succès.';
+$lang['AdminCompanyMessengers.!success.messenger_updated'] = 'La mise à jour du messager a été effectuée avec succès.';
+$lang['AdminCompanyMessengers.!tooltip.status'] = 'Aucun message ne sera envoyé à l\'aide de ce modèle si cette option n\'est pas activée.';
+$lang['AdminCompanyMessengers.!tab.installed'] = 'Installé';
+$lang['AdminCompanyMessengers.!tab.available'] = 'Disponible';
+$lang['AdminCompanyMessengers.available.page_title'] = 'Réglages > Entreprise > Messagers > Disponible';
+$lang['AdminCompanyMessengers.available.boxtitle_messenger'] = 'Messagers';
+$lang['AdminCompanyMessengers.available.text_author'] = 'Auteur :';
+$lang['AdminCompanyMessengers.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyMessengers.available.text_none'] = 'Il n\'y a pas de messagers disponibles.';
+$lang['AdminCompanyMessengers.available.btn_install'] = 'Installer';
+$lang['AdminCompanyMessengers.installed.page_title'] = 'Réglages > Entreprise > Messagers > Installé';
+$lang['AdminCompanyMessengers.installed.boxtitle_installed'] = 'Messagers';
+$lang['AdminCompanyMessengers.installed.text_author'] = 'Auteur :';
+$lang['AdminCompanyMessengers.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyMessengers.installed.text_none'] = 'Il n\'y a pas de messagers installés.';
+$lang['AdminCompanyMessengers.installed.btn_uninstall'] = 'Désinstaller';
+$lang['AdminCompanyMessengers.installed.btn_manage'] = 'Gérer';
+$lang['AdminCompanyMessengers.installed.btn_upgrade'] = 'Mise à niveau';
+$lang['AdminCompanyMessengers.installed.confirm_uninstall'] = 'Êtes-vous sûr de vouloir désinstaller ce messager ?';
+$lang['AdminCompanyMessengers.manage.page_title'] = 'Réglages > Entreprise > Messagers > Gérer %1$s';
+$lang['AdminCompanyMessengers.manage.boxtitle_manage'] = 'Gérer %1$s';
+$lang['AdminCompanyMessengers.manage.none_text'] = 'Il n\'y a rien à gérer pour ce messager.';
+$lang['AdminCompanyMessengers.manage.field_update'] = 'Mise à jour des paramètres';
+$lang['AdminCompanyMessengers.configuration.page_title'] = 'Réglages > Entreprise > Messageries > Configuration de la messagerie';
+$lang['AdminCompanyMessengers.configuration.boxtitle_messenger_configuration'] = 'Configuration du messager';
+$lang['AdminCompanyMessengers.configuration.heading_message_type'] = 'Type de message';
+$lang['AdminCompanyMessengers.configuration.heading_messenger'] = 'Messager';
+$lang['AdminCompanyMessengers.configuration.field_update'] = 'Mise à jour des paramètres';
+$lang['AdminCompanyMessengers.configuration.text_none'] = 'Il n\'y a pas de messagers installés.';
+$lang['AdminCompanyMessengers.templates.page_title'] = 'Réglages > Entreprise > Messagers > Modèles de messages';
+$lang['AdminCompanyMessengers.templates.boxtitle_message_templates'] = 'Modèles de messages';
+$lang['AdminCompanyMessengers.templates.heading_client'] = 'Messages des clients';
+$lang['AdminCompanyMessengers.templates.heading_staff'] = 'Messages du personnel';
+$lang['AdminCompanyMessengers.templates.heading_plugins'] = 'Messages du plugin';
+$lang['AdminCompanyMessengers.templates.text_name'] = 'Nom';
+$lang['AdminCompanyMessengers.templates.text_plugin'] = 'Plugin';
+$lang['AdminCompanyMessengers.templates.text_description'] = 'Description';
+$lang['AdminCompanyMessengers.templates.text_options'] = 'Options';
+$lang['AdminCompanyMessengers.templates.account_welcome_name'] = 'Enregistrement du compte';
+$lang['AdminCompanyMessengers.templates.account_welcome_desc'] = 'Envoi de l\'avis de bienvenue pour l\'enregistrement de nouveaux comptes.';
+$lang['AdminCompanyMessengers.templates.auto_debit_pending_name'] = 'Auto-Débit en attente';
+$lang['AdminCompanyMessengers.templates.auto_debit_pending_desc'] = 'Envoi d\'un avis indiquant qu\'une tentative de paiement automatique sera bientôt effectuée.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_first_name'] = 'Avis de facturation (1er)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_first_desc'] = 'Avis de première facture, qu\'il s\'agisse d\'un rappel de paiement ou d\'un avis de retard.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_second_name'] = 'Avis de facturation (2ème)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_second_desc'] = 'Deuxième avis de facture, qu\'il s\'agisse d\'un rappel de paiement ou d\'un avis de retard.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_third_name'] = 'Avis de facturation (3ème)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_third_desc'] = 'Troisième avis de facture, qu\'il s\'agisse d\'un rappel de paiement ou d\'un avis de retard.';
+$lang['AdminCompanyMessengers.templates.service_creation_name'] = 'Création de services';
+$lang['AdminCompanyMessengers.templates.service_creation_desc'] = 'Avis de création de service, envoyé lorsqu\'un service a été créé.';
+$lang['AdminCompanyMessengers.templates.service_scheduled_cancellation_name'] = 'Annulation du service prévu';
+$lang['AdminCompanyMessengers.templates.service_scheduled_cancellation_desc'] = 'Avis d\'annulation d\'un service programmé, envoyé lorsqu\'un service est programmé pour être annulé.';
+$lang['AdminCompanyMessengers.templates.service_suspension_name'] = 'Suspension du service';
+$lang['AdminCompanyMessengers.templates.service_suspension_desc'] = 'Avis de suspension de service, envoyé lorsqu\'un service est automatiquement suspendu.';
+$lang['AdminCompanyMessengers.templates.service_unsuspension_name'] = 'Non-suspension de service';
+$lang['AdminCompanyMessengers.templates.service_unsuspension_desc'] = 'Avis de suspension de service, envoyé lorsqu\'un service est automatiquement suspendu.';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_unpaid_name'] = 'Livraison de la facture (impayée)';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_unpaid_desc'] = 'Avis contenant le montant total d\'une facture impayée.';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_paid_name'] = 'Livraison de la facture (payée)';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_paid_desc'] = 'Avis contenant le montant total d\'une facture payée.';
+$lang['AdminCompanyMessengers.templates.quotation_delivery_name'] = 'Devis Livraison';
+$lang['AdminCompanyMessengers.templates.quotation_delivery_desc'] = 'Avis contenant un numéro de devis et un lien.';
+$lang['AdminCompanyMessengers.templates.staff_quotation_approved_name'] = 'Approbation des devis';
+$lang['AdminCompanyMessengers.templates.staff_quotation_approved_desc'] = 'Avis envoyé après l\'approbation d\'un devis par le client.';
+$lang['AdminCompanyMessengers.templates.option_edit'] = 'Editer';
+$lang['AdminCompanyMessengers.templates.no_results'] = 'Il n\'y a pas de modèle de ce type.';
+$lang['AdminCompanyMessengers.edittemplate.page_title'] = 'Réglages > Entreprise > Emails > Modifier le modèle de message';
+$lang['AdminCompanyMessengers.edittemplate.boxtitle_edittemplate'] = 'Modifier le modèle de message %1$s';
+$lang['AdminCompanyMessengers.edittemplate.text_none'] = 'Aucun';
+$lang['AdminCompanyMessengers.edittemplate.field.tags'] = 'Tags disponibles';
+$lang['AdminCompanyMessengers.edittemplate.field.status'] = 'Activé';
+$lang['AdminCompanyMessengers.edittemplate.field.cancel'] = 'Annuler';
+$lang['AdminCompanyMessengers.edittemplate.field.edittemplatesubmit'] = 'Mise à jour du modèle';
+
