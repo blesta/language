e9710cdab7bdddf52e72b5f547475e26f3e4b995
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:15 -0800

    Updated fr_fr language files

---
+++ components/upload/language/fr_fr/upload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Upload
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Upload.!error.max_file_size'] = 'Le fichier dépasse la taille maximale autorisée.';
+$lang['Upload.!error.mime_type'] = 'Le type de fichier %1$s n\'est pas autorisé.';
+$lang['Upload.!error.file_exists'] = 'Le fichier existe déjà.';
+$lang['Upload.!error.write_failed'] = 'Le fichier n\'a pas pu être écrit.';
+$lang['Upload.!error.path_created'] = 'Le chemin de téléchargement n\'a pas pu être créé.';
+
