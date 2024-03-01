34364b9a48c88f21bb87bb69283f4909260b07c9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:26 -0800

    Updated de_de language files

---
+++ language/de_de/admin_company_groups.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Admin Company Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyGroups.edit.text_editsubmit'] = 'Gruppe bearbeiten';
+$lang['AdminCompanyGroups.edit.field_force_email_usernames'] = 'E-Mail-Adressen als Nutzernamen erzwingen';
+$lang['AdminCompanyGroups.edit.field_company_settings'] = 'Firmeneinstellungen verwenden (deaktivieren, um unten anzugeben)';
+$lang['AdminCompanyGroups.edit.field_delivery_methods'] = 'Zustellungsmethoden für Rechnungen';
+$lang['AdminCompanyGroups.edit.field_description'] = 'Beschreibung';
+$lang['AdminCompanyGroups.edit.field_color'] = 'Farbe';
+$lang['AdminCompanyGroups.edit.field_name'] = 'Name';
+$lang['AdminCompanyGroups.edit.heading_client_fields'] = 'Erforderliche Kundenfelder';
+$lang['AdminCompanyGroups.edit.heading_client_settings'] = 'Allgemeine Client-Einstellungen';
+$lang['AdminCompanyGroups.edit.heading_payment'] = 'Notizen';
+$lang['AdminCompanyGroups.edit.heading_delivery'] = 'Lieferung von Rechnungen';
+$lang['AdminCompanyGroups.edit.heading_late_fees'] = 'Säumniszuschläge';
+$lang['AdminCompanyGroups.edit.heading_invoice'] = 'Optionen für Rechnungen und Entgelte';
+$lang['AdminCompanyGroups.edit.heading_basic'] = 'Grundlegende Optionen';
+$lang['AdminCompanyGroups.edit.boxtitle_editgroup'] = 'Gruppe bearbeiten';
+$lang['AdminCompanyGroups.edit.page_title'] = 'Einstellungen > Unternehmen > Kundengruppen > Gruppe bearbeiten';
+$lang['AdminCompanyGroups.add.text_addsubmit'] = 'Gruppe erstellen';
+$lang['AdminCompanyGroups.add.field_email_verification'] = 'Aktivieren der E-Mail-Überprüfung';
+$lang['AdminCompanyGroups.add.field_force_email_usernames'] = 'E-Mail-Adressen als Nutzernamen erzwingen';
+$lang['AdminCompanyGroups.add.field_company_settings'] = 'Firmeneinstellungen verwenden (deaktivieren, um unten anzugeben)';
+$lang['AdminCompanyGroups.add.field_delivery_methods'] = 'Zustellungsmethoden für Rechnungen';
+$lang['AdminCompanyGroups.add.field_description'] = 'Beschreibung';
+$lang['AdminCompanyGroups.add.field_color'] = 'Farbe';
+$lang['AdminCompanyGroups.add.field_name'] = 'Name';
+$lang['AdminCompanyGroups.add.heading_client_fields'] = 'Erforderliche Kundenfelder';
+$lang['AdminCompanyGroups.add.heading_client_settings'] = 'Allgemeine Client-Einstellungen';
+$lang['AdminCompanyGroups.add.heading_payment'] = 'Notizen';
+$lang['AdminCompanyGroups.add.heading_delivery'] = 'Lieferung von Rechnungen';
+$lang['AdminCompanyGroups.add.heading_late_fees'] = 'Säumniszuschläge';
+$lang['AdminCompanyGroups.add.heading_invoice'] = 'Optionen für Rechnungen und Entgelte';
+$lang['AdminCompanyGroups.add.heading_basic'] = 'Grundlegende Optionen';
+$lang['AdminCompanyGroups.add.boxtitle_addgroup'] = 'Gruppe erstellen';
+$lang['AdminCompanyGroups.add.page_title'] = 'Einstellungen > Unternehmen > Kundengruppen > Gruppe erstellen';
+$lang['AdminCompanyGroups.index.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Kundengruppe löschen wollen? Alle Kunden in dieser Gruppe werden in die Standardgruppe verschoben.';
+$lang['AdminCompanyGroups.index.no_results'] = 'Es gibt keine Kundengruppen.';
+$lang['AdminCompanyGroups.index.option_delete'] = 'Löschen';
+$lang['AdminCompanyGroups.index.option_edit'] = 'bearbeiten';
+$lang['AdminCompanyGroups.index.text_options'] = 'Optionen';
+$lang['AdminCompanyGroups.index.text_clients'] = 'Anzahl der Kunden';
+$lang['AdminCompanyGroups.index.text_description'] = 'Beschreibung';
+$lang['AdminCompanyGroups.index.text_name'] = 'Name';
+$lang['AdminCompanyGroups.index.categorylink_addgroup'] = 'Gruppe erstellen';
+$lang['AdminCompanyGroups.index.boxtitle_groups'] = 'Kundengruppen';
+$lang['AdminCompanyGroups.index.page_title'] = 'Einstellungen > Unternehmen > Kundengruppen';
+$lang['AdminCompanyGroups.!tooltip.email_verification'] = 'Aktivieren Sie dieses Kontrollkästchen, um eine E-Mail zur Verifizierung zu senden, wenn eine neue Anmeldung erstellt wird oder ein Kunde seine E-Mail-Adresse ändert. Im Profil des Kunden erscheint ein Hinweis, bis er verifiziert ist.';
+$lang['AdminCompanyGroups.!tooltip.force_email_usernames'] = 'Entfernt die Möglichkeit für Kunden, ihren eigenen Benutzernamen zu definieren.';
+$lang['AdminCompanyGroups.!error.delete_failed'] = '%1$s ist die Standardgruppe und kann nicht gelöscht werden.';
+$lang['AdminCompanyGroups.!success.delete_deleted'] = '%1$s wurde erfolgreich gelöscht!';
+$lang['AdminCompanyGroups.!success.edit_updated'] = '%1$s wurde erfolgreich bearbeitet!';
+$lang['AdminCompanyGroups.!success.add_created'] = '%1$s wurde erfolgreich erstellt!';
+
