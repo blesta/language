df7df87555eb746e349681f058cc664fdb1dc424
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:20 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/email_verifications.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Email Verifications
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['EmailVerifications.!error.contact_id.exists'] = 'L\'identifiant du contact n\'est pas valide.';
+$lang['EmailVerifications.!error.email.valid'] = 'Veuillez saisir une adresse électronique valide.';
+$lang['EmailVerifications.!error.redirect_url.valid'] = 'Veuillez saisir une URL valide.';
+$lang['EmailVerifications.!error.token.format'] = 'Veuillez saisir un jeton valide.';
+$lang['EmailVerifications.!error.verified.valid'] = 'Le champ vérifié doit être "1" ou "0".';
+
