bd97ac1efc07f56268fe28d9225b0ae327ed8693
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:50 -0800

    Updated es_es language files

---
+++ plugins/order/views/templates/ajax/language/es_es/signup.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Signup
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Signup.index.header_login_or_signup'] = 'Registrarse o iniciar sesión';
+$lang['Signup.index.link_resetpassword'] = 'Restablecer mi contraseña';
+$lang['Signup.index.login_or_signup_note'] = 'Si ya tiene una cuenta con nosotros, inicie sesión para añadir este pedido a su cuenta existente.';
+$lang['Signup.index.login_note'] = 'Tendrá la oportunidad de confirmar su pedido después de iniciar sesión.';
+$lang['Signup.index.signup_note'] = 'Tendrá la oportunidad de confirmar su pedido después de crear una cuenta.';
+$lang['Signup.index.client_note'] = 'Este pedido se añadirá a la siguiente cuenta.';
+$lang['Signup.index.logout_btn'] = 'Cerrar sesión';
+$lang['Signup.index.continue_btn'] = 'Seguir comprando';
+$lang['Signup.index.checkout_btn'] = 'Comprobar y pagar';
+$lang['Signup.index.configure_btn'] = 'Continuar';
+$lang['Signup.index.field_agree_tos'] = 'He leído y acepto los <a href="%1$s" target="_blank">Condiciones generales</a>.';
+$lang['Signup.index.heading_client'] = 'Información para el cliente';
+$lang['Signup.index.heading_contact'] = 'Información de contacto';
+$lang['Signup.index.heading_billing'] = 'Información de facturación';
+$lang['Signup.index.heading_authentication'] = 'Autenticación';
+$lang['Signup.index.heading_settings'] = 'Ajustes adicionales';
+
