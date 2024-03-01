79a6ad9fc282a06fb7f15c9857bac3c9e3f509da
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:55 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_system_automation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin System Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemAutomation.index.note_cron_command'] = 'Αυτό είναι ένα παράδειγμα εντολής cron που μπορεί να χρησιμοποιηθεί για τη δημιουργία μιας εργασίας cron στο διακομιστή σας. Κατά τη δημιουργία της εργασίας cron, φροντίστε να ενημερώσετε την εντολή cron ώστε να δείχνει το σημείο όπου είναι εγκατεστημένη η PHP, αν αυτό διαφέρει από αυτό που φαίνεται σε αυτό το παράδειγμα.';
+$lang['AdminSystemAutomation.index.text_generate_code'] = 'Δημιουργία κώδικα';
+$lang['AdminSystemAutomation.index.text_update_key'] = 'Ενημέρωση κλειδιού Cron';
+$lang['AdminSystemAutomation.index.text_cron_currently_running'] = 'Το cron τρέχει αυτή τη στιγμή.';
+$lang['AdminSystemAutomation.index.text_cron_never_ran'] = 'Το cron δεν έχει εκτελεστεί ποτέ.';
+$lang['AdminSystemAutomation.index.text_cron_last_ran'] = 'Το cron εκτελέστηκε τελευταία φορά στις %1$s.';
+$lang['AdminSystemAutomation.index.field_cronkey_submit'] = 'Ενημέρωση κλειδιού Cron';
+$lang['AdminSystemAutomation.index.field_cron_key'] = 'Κλειδί Cron';
+$lang['AdminSystemAutomation.index.field_runcron'] = 'Εκτέλεση Cron με μη αυτόματο τρόπο';
+$lang['AdminSystemAutomation.index.field_croncommand'] = 'Παράδειγμα εντολής Cron';
+$lang['AdminSystemAutomation.index.heading_status'] = 'Κατάσταση Cron';
+$lang['AdminSystemAutomation.index.boxtitle_automation'] = 'Αυτοματισμός';
+$lang['AdminSystemAutomation.index.page_title'] = 'Ρυθμίσεις > Σύστημα > Αυτοματισμός';
+$lang['AdminSystemAutomation.!success.cron_key'] = 'Το κλειδί cron ενημερώθηκε επιτυχώς!';
+$lang['AdminSystemAutomation.!success.automation_updated'] = 'Οι ρυθμίσεις αυτοματισμού ενημερώθηκαν με επιτυχία!';
+$lang['AdminSystemAutomation.!error.empty_cron_key'] = 'Παρακαλούμε ορίστε ένα κλειδί cron.';
+
