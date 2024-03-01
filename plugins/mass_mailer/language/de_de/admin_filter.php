2e0f35c92417a6d128f362d296d817a72b53d396
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:54 -0800

    Updated de_de language files

---
+++ plugins/mass_mailer/language/de_de/admin_filter.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Admin Filter
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminFilter.index.form.export'] = 'Exportieren';
+$lang['AdminFilter.index.form.continue'] = 'Weiter';
+$lang['AdminFilter.index.form.include_all_services'] = 'Für alle Dienstleistungen einbeziehen';
+$lang['AdminFilter.index.form.module_id'] = 'Modul';
+$lang['AdminFilter.index.form.selectable_packages'] = 'Verfügbare Pakete';
+$lang['AdminFilter.index.form.selected_packages'] = 'Ausgewählte Pakete';
+$lang['AdminFilter.index.form.package_group'] = 'Paketgruppenfilter';
+$lang['AdminFilter.index.form.service_parent_type_module'] = 'Modul';
+$lang['AdminFilter.index.form.service_parent_type_package'] = 'Paket';
+$lang['AdminFilter.index.form.service_parent_type'] = 'Paket/Modul';
+$lang['AdminFilter.index.form.service_statuses'] = 'Service-Status';
+$lang['AdminFilter.index.form.service_renew_date'] = 'Erneuert von';
+$lang['AdminFilter.index.heading.service_filters'] = 'Optionen zur Dienstfilterung';
+$lang['AdminFilter.index.form.filter_services'] = 'Nach Dienstleistungen filtern';
+$lang['AdminFilter.index.form.receive_email_marketing'] = 'Beschränkung auf marktfähige Kunden';
+$lang['AdminFilter.index.form.client_end_date'] = 'Enddatum der Kundenerstellung';
+$lang['AdminFilter.index.form.client_start_date'] = 'Client-Erstellung Startdatum';
+$lang['AdminFilter.index.form.contact_types'] = 'Kontaktarten';
+$lang['AdminFilter.index.form.languages'] = 'Sprachen';
+$lang['AdminFilter.index.form.client_statuses'] = 'Client-Status';
+$lang['AdminFilter.index.form.client_group_ids'] = 'Kundengruppen';
+$lang['AdminFilter.index.heading.client_filters'] = 'Optionen für die Kundenfilterung';
+$lang['AdminFilter.index.boxtitle'] = 'Massenversender';
+$lang['AdminFilter.!tooltip.receive_email_marketing'] = 'Wenn diese Option aktiviert ist, erhalten nur Kunden, die sich für Marketing/Angebote entschieden haben, diese E-Mail.';
+$lang['AdminFilter.!tooltip.include_all_services'] = 'Wenn diese Option aktiviert ist, werden alle Dienste einbezogen. Die Kontakte erhalten dann möglicherweise mehrere E-Mails, eine für jeden passenden Dienst. Wenn diese Option nicht aktiviert ist, erhalten die Kontakte nur eine E-Mail, die den ersten passenden Dienst enthält.';
+$lang['AdminFilter.!tooltip.selected_packages'] = 'Die Dienste müssen zu einem der ausgewählten Pakete gehören.';
+$lang['AdminFilter.!tooltip.service_parent_type'] = 'Die Dienste werden weiter gefiltert, entweder nach Paket oder Modul.';
+$lang['AdminFilter.!tooltip.module_id'] = 'Die Dienste müssen zu dem ausgewählten Modul gehören.';
+$lang['AdminFilter.!tooltip.service_statuses'] = 'Die Dienste müssen sich in einem der ausgewählten Zustände befinden.';
+$lang['AdminFilter.!tooltip.service_renew_date'] = 'Die Dienste müssen an oder vor diesem Datum erneuert werden. Lassen Sie dieses Feld leer, um es zu ignorieren.';
+$lang['AdminFilter.!tooltip.filter_services'] = 'Wenn diese Option aktiviert ist, müssen die Clients auch den Filterkriterien für den Dienst entsprechen.';
+$lang['AdminFilter.!tooltip.client_end_date'] = 'Die Kunden müssen an oder vor diesem Datum erstellt worden sein. Lassen Sie dieses Feld leer, um es zu ignorieren.';
+$lang['AdminFilter.!tooltip.client_start_date'] = 'Die Kunden müssen an oder nach diesem Datum erstellt worden sein. Lassen Sie dieses Feld leer, um es zu ignorieren.';
+$lang['AdminFilter.!tooltip.contact_types'] = 'Die Kontakte müssen einem der ausgewählten Typen angehören.';
+$lang['AdminFilter.!tooltip.languages'] = 'Die Sprache der Clients muss auf eine der ausgewählten Sprachen eingestellt sein.';
+$lang['AdminFilter.!tooltip.client_statuses'] = 'Die Kunden müssen einem der ausgewählten Status angehören.';
+$lang['AdminFilter.!tooltip.client_group_ids'] = 'Die Clients müssen zu einer der ausgewählten Gruppen gehören.';
+$lang['AdminFilter.text.all'] = 'Alle';
+$lang['AdminFilter.index.page_title'] = 'Massenversand > Filter';
+
