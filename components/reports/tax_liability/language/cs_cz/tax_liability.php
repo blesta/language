4dbf926936eb9e53312077bab604180fa4fc3f14
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:46 -0800

    Updated cs_cz language files

---
+++ components/reports/tax_liability/language/cs_cz/tax_liability.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Tax Liability
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['TaxLiability.heading.date_closed'] = 'Datum uzavření';
+$lang['TaxLiability.heading.currency'] = 'Měna';
+$lang['TaxLiability.heading.cascade'] = 'Cascade Tax';
+$lang['TaxLiability.heading.level2_tax_state'] = 'Úroveň 2 Stát/provincie';
+$lang['TaxLiability.heading.level2_tax_country'] = 'Úroveň 2 Země';
+$lang['TaxLiability.heading.level2_tax_amount'] = 'Úroveň 2 Výše daně';
+$lang['TaxLiability.heading.level2_tax_rate'] = 'Úroveň 2 Sazba daně (%)';
+$lang['TaxLiability.heading.level1_tax_state'] = 'Úroveň 1 Stát/provincie';
+$lang['TaxLiability.heading.level1_tax_country'] = 'Úroveň 1 Země';
+$lang['TaxLiability.heading.level1_tax_amount'] = 'Úroveň 1 Výše daně';
+$lang['TaxLiability.heading.level1_tax_rate'] = 'Úroveň 1 Sazba daně (%)';
+$lang['TaxLiability.heading.taxable_amount'] = 'Zdanitelná částka';
+$lang['TaxLiability.heading.subtotal'] = 'Mezisoučet';
+$lang['TaxLiability.heading.client_id_code'] = 'Klient #';
+$lang['TaxLiability.heading.id_code'] = 'Faktura #';
+$lang['TaxLiability.options.field_cascade_false'] = 'Ne';
+$lang['TaxLiability.options.field_cascade_true'] = 'Ano';
+$lang['TaxLiability.options.field_end_date'] = 'Datum ukončení';
+$lang['TaxLiability.options.field_start_date'] = 'Datum zahájení';
+$lang['TaxLiability.name'] = 'Daňová povinnost';
+
