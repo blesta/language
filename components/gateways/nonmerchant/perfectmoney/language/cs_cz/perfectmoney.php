e98fb0d89181372e062c4e2beead5f0041d54a6c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:10 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/perfectmoney/language/cs_cz/perfectmoney.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Perfectmoney
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Perfectmoney.!error.passphrase.valid'] = 'Musíte zadat platnou přístupovou frázi.';
+$lang['Perfectmoney.!error.payee_account.valid'] = 'Musíte zadat platný účet.';
+$lang['Perfectmoney.buildprocess.submit'] = 'Platba pomocí služby Perfect Money';
+$lang['Perfectmoney.passphrase_note'] = 'Alternativní přístupová fráze se používá pro výpočet hashe MD5 k ověření platnosti plateb.';
+$lang['Perfectmoney.passphrase'] = 'Alternativní přístupová fráze';
+$lang['Perfectmoney.payee_account'] = 'Účet příjemce';
+$lang['Perfectmoney.name'] = 'Perfect Money';
+
