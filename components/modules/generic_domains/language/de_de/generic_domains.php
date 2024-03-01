55e39ee7964e6ac140899112524786a7fd9da961
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:25 -0800

    Updated de_de language files

---
+++ components/modules/generic_domains/language/de_de/generic_domains.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Generic Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GenericDomains.manage.text'] = 'Mitarbeiter können BCC-Kopien von E-Mails zur Einrichtung, Aussetzung und Kündigung von Diensten erhalten, wenn ihre Personalgruppe diese BCC-Benachrichtigungen eingestellt hat und der Mitarbeiter sie unter [Meine Info] -> [Benachrichtigungen] abonniert hat.';
+$lang['GenericDomains.package_fields.tld_options'] = 'TLDs';
+$lang['GenericDomains.transfer.transfer_key'] = 'EPP-Code';
+$lang['GenericDomains.transfer.domain'] = 'Bereich';
+$lang['GenericDomains.domain.domain'] = 'Bereich';
+$lang['GenericDomains.module_row_plural'] = 'Modul-Zeilen';
+$lang['GenericDomains.module_row'] = 'Modulreihe';
+$lang['GenericDomains.description'] = 'Ein allgemeines Modul für die Domainregistrierung.';
+$lang['GenericDomains.name'] = 'Generische Domains';
+
