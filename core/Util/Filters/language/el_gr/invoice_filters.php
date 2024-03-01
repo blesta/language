b62507cb3e40efb251624328f13e694102909083
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:31 -0800

    Updated el_gr language files

---
+++ core/Util/Filters/language/el_gr/invoice_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Invoice Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.invoice_filters.any'] = 'Οποιοδήποτε';
+$lang['Util.filters.invoice_filters.field_invoice_number'] = 'Τιμολόγιο #';
+$lang['Util.filters.invoice_filters.field_currency'] = 'Νόμισμα';
+$lang['Util.filters.invoice_filters.field_invoice_line'] = 'Περιεχόμενο γραμμής';
+
