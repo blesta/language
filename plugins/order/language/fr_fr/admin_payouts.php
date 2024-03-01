fb6cac435c9abd057af74b870666cd309d70e3cd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:30 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/admin_payouts.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Admin Payouts
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminPayouts.!success.payout_updated'] = 'Le paiement a été mis à jour avec succès.';
+$lang['AdminPayouts.!success.payout_approved'] = 'Le paiement a été approuvé avec succès.';
+$lang['AdminPayouts.!success.payout_declined'] = 'Le paiement a été refusé avec succès.';
+$lang['AdminPayouts.!error.insufficient_funds_payout_request'] = 'Il n\'y a pas assez de fonds pour traiter la demande de paiement.';
+$lang['AdminPayouts.index.boxtitle_payouts'] = 'Paiements des affiliés';
+$lang['AdminPayouts.index.category_approved'] = 'Approuvé';
+$lang['AdminPayouts.index.category_pending'] = 'En attente';
+$lang['AdminPayouts.index.category_declined'] = 'Refusé';
+$lang['AdminPayouts.index.heading_payout_id'] = 'ID de paiement';
+$lang['AdminPayouts.index.heading_name'] = 'Client';
+$lang['AdminPayouts.index.heading_code'] = 'Code';
+$lang['AdminPayouts.index.heading_payment_method_id'] = 'Mode de paiement';
+$lang['AdminPayouts.index.heading_requested_amount'] = 'Montant demandé';
+$lang['AdminPayouts.index.heading_requested_currency'] = 'Monnaie demandée';
+$lang['AdminPayouts.index.heading_paid_amount'] = 'Montant payé';
+$lang['AdminPayouts.index.heading_paid_currency'] = 'Monnaie payée';
+$lang['AdminPayouts.index.heading_date_requested'] = 'Date de la demande';
+$lang['AdminPayouts.index.heading_options'] = 'Options';
+$lang['AdminPayouts.index.option_edit'] = 'Editer';
+$lang['AdminPayouts.index.option_approve'] = 'Approuver';
+$lang['AdminPayouts.index.option_decline'] = 'Déclin';
+$lang['AdminPayouts.index.confirm_approve'] = 'Êtes-vous sûr de vouloir approuver ce paiement ?';
+$lang['AdminPayouts.index.confirm_decline'] = 'Êtes-vous sûr de vouloir refuser ce paiement ?';
+$lang['AdminPayouts.index.no_results'] = 'Il n\'y a pas de demande de paiement pour le moment.';
+$lang['AdminPayouts.index.payment_method_na'] = 'N/A';
+$lang['AdminPayouts.edit.boxtitle_edit_payout'] = 'Modifier le paiement';
+$lang['AdminPayouts.edit.heading_payout_request'] = 'Demande de paiement';
+$lang['AdminPayouts.edit.field_payment_method_id'] = 'Mode de paiement';
+$lang['AdminPayouts.edit.field_requested_amount'] = 'Montant demandé';
+$lang['AdminPayouts.edit.field_requested_currency'] = 'Monnaie demandée';
+$lang['AdminPayouts.edit.field_status'] = 'Statut';
+$lang['AdminPayouts.edit.modify_payout'] = 'Modifier le paiement';
+
