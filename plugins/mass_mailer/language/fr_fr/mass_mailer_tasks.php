30dcdb0a159cee3d93531f761ef5821a15957e2f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:23 -0800

    Updated fr_fr language files

---
+++ plugins/mass_mailer/language/fr_fr/mass_mailer_tasks.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Mass Mailer Tasks
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerTasks.!error.job_id.valid'] = 'ID de travail non valide.';
+
