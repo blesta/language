f50b6c051c7ae6a31360d550cd82862f4b5ba4c4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:27 -0800

    Updated de_de language files

---
+++ plugins/billing_overview/language/de_de/billing_overview_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Billing Overview Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BillingOverviewSettings.!error.settings[][value].length'] = 'Die Länge jedes einzelnen Einstellungswertes darf 255 Zeichen nicht überschreiten.';
+$lang['BillingOverviewSettings.!error.settings[][key].empty'] = 'Kein Einstellschlüssel darf leer sein.';
+$lang['BillingOverviewSettings.!error.company_id.exists'] = 'Ungültige Unternehmens-ID.';
+$lang['BillingOverviewSettings.!error.staff_id.exists'] = 'Ungültige Personal-ID.';
+
