66329cb8c8db03b0447a9616cbe5e811068b83e4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:02 -0800

    Updated it_it language files

---
+++ language/it_it/emails.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Emails.getStatusTypes.inactive'] = 'Sì';
+$lang['Emails.getStatusTypes.active'] = 'No';
+$lang['Emails.!error.email.failed_to_send'] = 'L\'invio dell\'e-mail non è riuscito a causa di un problema di configurazione.';
+$lang['Emails.!error.attachments.exist'] = 'Almeno uno degli allegati forniti non esiste nel file system.';
+$lang['Emails.!error.bcc_addresses.format'] = 'Almeno uno degli indirizzi e-mail BCC forniti non è un indirizzo e-mail valido.';
+$lang['Emails.!error.cc_addresses.format'] = 'Almeno uno degli indirizzi e-mail CC forniti non è un indirizzo e-mail valido.';
+$lang['Emails.!error.to_addresses.format'] = 'Almeno uno degli indirizzi e-mail A forniti non è un indirizzo e-mail valido.';
+$lang['Emails.!error.to_addresses.empty'] = 'Deve essere fornito almeno un indirizzo A.';
+$lang['Emails.!error.action.exists'] = 'L\'azione fornita è un\'azione di gruppo non valida.';
+$lang['Emails.!error.company_id.unique'] = 'L\'ID del gruppo e-mail e l\'ID dell\'azienda per la lingua indicata sono già stati presi.';
+$lang['Emails.!error.email_id.exists'] = 'ID e-mail non valido.';
+$lang['Emails.!error.status.format'] = 'Stato non valido.';
+$lang['Emails.!error.include_attachments'] = 'La scelta di includere gli allegati deve essere impostata su 0 o 1.';
+$lang['Emails.!error.subject.empty'] = 'Inserire un oggetto.';
+$lang['Emails.!error.reply_to.format'] = 'Inserire un indirizzo di risposta valido.';
+$lang['Emails.!error.from_name.empty'] = 'Inserire un nome di provenienza.';
+$lang['Emails.!error.from.format'] = 'Inserire un indirizzo di posta elettronica valido.';
+$lang['Emails.!error.lang.length'] = 'La lunghezza della lingua non può superare i 5 caratteri.';
+$lang['Emails.!error.lang.empty'] = 'Inserire una lingua.';
+$lang['Emails.!error.email_group_id.exists'] = 'ID gruppo e-mail non valido.';
+$lang['Emails.!error.email_signature_id.exists'] = 'ID firma e-mail non valido.';
+$lang['Emails.!error.text.empty'] = 'Inserire una firma in chiaro.';
+$lang['Emails.!error.name.empty'] = 'Inserire il nome della firma.';
+$lang['Emails.!error.email_signature_id.in_use'] = 'Questa firma e-mail è attualmente in uso e non può essere cancellata.';
+$lang['Emails.!error.text.parse'] = 'Errore di analisi del modello: %1$s';
+$lang['Emails.!error.html.parse'] = 'Errore di analisi del modello: %1$s';
+$lang['Emails.!error.company_id.exists'] = 'ID azienda non valido.';
+
