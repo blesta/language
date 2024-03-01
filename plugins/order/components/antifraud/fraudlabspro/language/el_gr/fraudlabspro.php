2e4d1b035906d581482c03607ae48d5761c649e2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:14 -0800

    Updated el_gr language files

---
+++ plugins/order/components/antifraud/fraudlabspro/language/el_gr/fraudlabspro.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fraudlabspro
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FraudLabsPro.!error.reject.reject_score'] = 'Η παραγγελία σας δεν μπορεί να διεκπεραιωθεί αυτή τη στιγμή.';
+$lang['FraudLabsPro.!error.review.review_score'] = 'Η παραγγελία σας πρέπει να επανεξεταστεί χειροκίνητα.';
+$lang['FraudLabsPro.!error.reject.fraudlabspro'] = 'Η παραγγελία σας έχει χαρακτηριστεί ως απάτη από την FraudLabs Pro.';
+$lang['FraudLabsPro.!error.review.fraudlabspro'] = 'Η παραγγελία σας έχει χαρακτηριστεί ως πιθανή απάτη από την FraudLabs Pro. Θα επανεξεταστεί χειροκίνητα.';
+$lang['FraudLabsPro.settings.field_api_key'] = 'Κλειδί API';
+$lang['FraudLabsPro.settings.label_description'] = 'Αποκτήστε δωρεάν το FraudLabs Pro Micro Plan (500 ερωτήματα/μήνα) από το http://www.fraudlabspro.com/plan';
+$lang['FraudLabsPro.settings.field_reject_score'] = 'Ελάχιστη βαθμολογία για απόρριψη';
+$lang['FraudLabsPro.settings.field_review_score'] = 'Ελάχιστη βαθμολογία για αναθεώρηση';
+$lang['FraudLabsPro.settings.field_follow_flp_result'] = 'Αυτόματη απόρριψη/αναθεώρηση με βάση τη σύσταση του FraudLabs Pro';
+$lang['FraudLabsPro.settings.option_yes'] = 'Ναι';
+$lang['FraudLabsPro.settings.option_no'] = 'Όχι';
+
