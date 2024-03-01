32429ef4eea501c56a21e414ad87b371e24ee794
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:02 -0800

    Updated it_it language files

---
+++ core/Automation/Tasks/Task/language/it_it/deliver_reports.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Deliver Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.deliver_reports.tax_liability.email_success'] = 'L\'e-mail di segnalazione della responsabilità fiscale è stata inviata.';
+$lang['Automation.task.deliver_reports.tax_liability.email_error'] = 'L\'e-mail di responsabilità fiscale non è stata inviata.';
+$lang['Automation.task.deliver_reports.tax_liability.attachment_fail'] = 'Impossibile generare l\'allegato del rapporto sulla responsabilità fiscale.';
+$lang['Automation.task.deliver_reports.tax_liability.attempt'] = 'Tentativo di invio del rapporto di responsabilità fiscale.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_success'] = 'L\'e-mail di segnalazione della creazione della fattura è stata inviata.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_error'] = 'L\'invio dell\'e-mail di segnalazione della creazione della fattura non è riuscito.';
+$lang['Automation.task.deliver_reports.invoice_creation.attachment_fail'] = 'Impossibile generare l\'allegato del report Creazione fattura.';
+$lang['Automation.task.deliver_reports.invoice_creation.attempt'] = 'Tentativo di inviare il report Creazione fattura.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_success'] = 'L\'e-mail di segnalazione dell\'invecchiamento delle fatture è stata inviata.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_error'] = 'L\'invio dell\'e-mail di invecchiamento delle fatture non è riuscito.';
+$lang['Automation.task.deliver_reports.aging_invoices.attachment_fail'] = 'Impossibile generare l\'allegato del report Fatture in scadenza.';
+$lang['Automation.task.deliver_reports.aging_invoices.attempt'] = 'Tentativo di inviare il report Fatture invecchiate.';
+$lang['Automation.task.deliver_reports.completed'] = 'L\'attività di consegna dei rapporti è stata completata.';
+$lang['Automation.task.deliver_reports.attempt'] = 'Tentativo di consegna dei rapporti.';
+
