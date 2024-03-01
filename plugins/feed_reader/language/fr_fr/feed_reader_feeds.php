34359fe7096d10bfd98b83f93b7b6d352605c089
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:11 -0800

    Updated fr_fr language files

---
+++ plugins/feed_reader/language/fr_fr/feed_reader_feeds.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Feed Reader Feeds
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FeedReaderFeeds.!error.url.valid'] = 'L\'URL indiquée n\'est pas valide.';
+$lang['FeedReaderFeeds.!error.company_id.exists'] = 'L\'entreprise spécifiée n\'existe pas.';
+$lang['FeedReaderFeeds.!error.updated.valid'] = 'Vous devez spécifier une date de mise à jour valide.';
+$lang['FeedReaderFeeds.!error.feed_id.exists'] = 'Le flux donné n\'existe pas.';
+$lang['FeedReaderFeeds.!error.staff_id.exists'] = 'Le membre du personnel spécifié n\'existe pas.';
+$lang['FeedReaderFeeds.!error.date.valid'] = 'Les données spécifiées ne sont pas valides.';
+
