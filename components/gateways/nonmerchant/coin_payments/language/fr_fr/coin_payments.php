4b4f83a8bb8e5b35d2903258fd343360c8a5849a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:31 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/coin_payments/language/fr_fr/coin_payments.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Coin Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CoinPayments.name'] = 'CoinPayments.net';
+$lang['CoinPayments.description'] = 'Un système de paiement pour les crypto-monnaies telles que le bitcoin et le litecoin avec des frais réduits';
+$lang['CoinPayments.merchant_id'] = 'ID du commerçant CoinPayments';
+$lang['CoinPayments.ipn_secret'] = 'Secret IPN';
+$lang['CoinPayments.buildprocess.submit'] = 'Payer avec CoinPayments';
+
