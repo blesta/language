ca81b2934d72df656368313db861e43cf0f966a2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:48 -0800

    Updated fr_fr language files

---
+++ components/modules/cwatch/language/fr_fr/cwatch.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Cwatch
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CWatch.name'] = 'CWatch';
+$lang['CWatch.description'] = 'Un outil de protection pour votre site web, vos serveurs web et vos applications web contre les menaces de plus en plus sophistiquées des pirates informatiques.';
+$lang['CWatch.module_row.name'] = 'Compte API';
+$lang['CWatch.module_row_plural.name'] = 'Comptes API';
+$lang['CWatch.!success.sftp_test'] = 'La connexion SFTP a été établie avec succès !';
+$lang['CWatch.!success.ftp_test'] = 'La connexion FTP a été établie avec succès !';
+$lang['CWatch.!tooltip.initiateDns'] = 'Indique s\'il faut lancer une analyse des enregistrements DNS.';
+$lang['CWatch.!tooltip.autoSsl'] = 'Installer ou non un certificat SSL gratuit de Comodo.';
+$lang['CWatch.!error.username.valid'] = 'Veuillez saisir un nom d\'utilisateur.';
+$lang['CWatch.!error.password.valid'] = 'Veuillez saisir un mot de passe.';
+$lang['CWatch.!error.password.valid_connection'] = 'Impossible d\'établir une connexion en utilisant les informations d\'identification données.';
+$lang['CWatch.!error.cwatch_email.format'] = 'Format d\'email non valide.';
+$lang['CWatch.!error.cwatch_email.unique'] = 'Un client avec l\'adresse e-mail donnée existe déjà.';
+$lang['CWatch.!error.cwatch_firstname.empty'] = 'Saisissez un prénom.';
+$lang['CWatch.!error.cwatch_lastname.empty'] = 'Saisissez un nom de famille.';
+$lang['CWatch.!error.cwatch_country.length'] = 'Comté non valide.';
+$lang['CWatch.!error.cwatch_domain.format'] = 'Veuillez saisir un nom de domaine valide, par exemple domain.com.';
+$lang['CWatch.!error.cwatch_domain.unique'] = 'Domaine déjà enregistré. Veuillez sélectionner un autre domaine.';
+$lang['CWatch.!error.limit_exceeded'] = 'Le nombre de licences actuellement attribuées à des domaines dépasse la limite indiquée.  Veuillez libérer des licences en supprimant des domaines avant de réduire votre limite.';
+$lang['CWatch.!error.api.internal'] = 'Une erreur interne s\'est produite ou le serveur n\'a pas répondu à la demande.';
+$lang['CWatch.!error.sftp_test'] = 'La connexion SFTP a échoué ! Veuillez vérifier vos paramètres et réessayer.';
+$lang['CWatch.!error.ftp_test'] = 'La connexion FTP a échoué ! Veuillez vérifier vos paramètres et réessayer.';
+$lang['CWatch.manage.boxtitle'] = 'Gérer CWatch';
+$lang['CWatch.manage.title'] = 'Comptes API';
+$lang['CWatch.manage.add_module_row'] = 'Ajouter un compte API';
+$lang['CWatch.manage.option_edit'] = 'Editer';
+$lang['CWatch.manage.delete'] = 'Supprimer';
+$lang['CWatch.manage.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer cet utilisateur ?';
+$lang['CWatch.manage.no_results'] = 'Aucun compte trouvé';
+$lang['CWatch.manage.heading_username'] = 'Nom d\'utilisateur';
+$lang['CWatch.manage.heading_options'] = 'Options';
+$lang['CWatch.package_fields.package_type'] = 'Type d\'emballage';
+$lang['CWatch.package_fields.available_terms'] = 'Conditions disponibles';
+$lang['CWatch.package_fields.tooltip.package_type'] = 'Les paquets à licence unique ne fournissent qu\'une seule licence dans cWatch du type sélectionné ici par le paquet.  Les paquets de licences multiples utilisent des options configurables pour permettre aux clients de commander un nombre variable de licences de différents types.';
+$lang['CWatch.package_fields.license_type'] = 'Type de licence';
+$lang['CWatch.package_fields.license_basic_detection'] = 'De base';
+$lang['CWatch.package_fields.license_starter'] = 'Démarrage';
+$lang['CWatch.package_fields.license_starter_paid_with_trial'] = 'Démarrage Payé avec Essai';
+$lang['CWatch.package_fields.license_waf_starter'] = 'Waf Starter';
+$lang['CWatch.package_fields.license_pro'] = 'Professionnel';
+$lang['CWatch.package_fields.license_pro_paid_with_trial'] = 'Professionnel payé à l\'essai';
+$lang['CWatch.package_fields.license_pro_free'] = 'Essai professionnel';
+$lang['CWatch.package_fields.license_pro_free_60d'] = 'Essai professionnel prolongé';
+$lang['CWatch.package_fields.license_premium'] = 'Prime';
+$lang['CWatch.package_fields.license_premium_paid_with_trial'] = 'Prime payée à l\'essai';
+$lang['CWatch.package_fields.license_premium_free'] = 'Essai Premium';
+$lang['CWatch.package_fields.license_premium_free_60d'] = 'Essai Premium étendu';
+$lang['CWatch.packagetypes.single_license'] = 'Licence unique';
+$lang['CWatch.packagetypes.multi_license'] = 'Licence multiple';
+$lang['Cwatch.service_field.domain'] = 'Domaine';
+$lang['Cwatch.service_field.email'] = 'Courriel';
+$lang['Cwatch.service_field.firstname'] = 'Prénom';
+$lang['Cwatch.service_field.lastname'] = 'Nom de famille';
+$lang['Cwatch.service_field.country'] = 'Pays';
+$lang['CWatch.add_row.box_title'] = 'Ajouter un compte API';
+$lang['CWatch.add_row.basic_title'] = 'Paramètres de l\'utilisateur';
+$lang['CWatch.add_row.field_username'] = 'Nom d\'utilisateur';
+$lang['CWatch.add_row.field_password'] = 'Mot de passe';
+$lang['CWatch.add_row.field_sandbox'] = 'Activer SandBox';
+$lang['CWatch.add_row.add_btn'] = 'Ajouter un compte';
+$lang['CWatch.edit_row.box_title'] = 'Modifier le compte API';
+$lang['CWatch.edit_row.basic_title'] = 'Paramètres de l\'utilisateur';
+$lang['CWatch.edit_row.field_username'] = 'Nom d\'utilisateur';
+$lang['CWatch.edit_row.field_password'] = 'Mot de passe';
+$lang['CWatch.edit_row.field_sandbox'] = 'Activer SandBox';
+$lang['CWatch.edit_row.add_btn'] = 'Modifier les informations d\'identification';
+$lang['CWatch.service_info.option_login'] = 'Se connecter à cWatch';
+$lang['CWatch.tab_licenses.licenses'] = 'Gérer les licences';
+$lang['CWatch.tab_licenses.inactive_licenses'] = 'Licences inactives';
+$lang['CWatch.tab_licenses.add'] = 'Ajouter un nouveau site';
+$lang['CWatch.tab_licenses.licenseKey'] = 'Clé de licence';
+$lang['CWatch.tab_licenses.type'] = 'Type';
+$lang['CWatch.tab_licenses.domain'] = 'Domaine';
+$lang['CWatch.tab_licenses.status'] = 'Statut du domaine';
+$lang['CWatch.tab_licenses.malware_scanner'] = 'Scanner';
+$lang['CWatch.tab_licenses.options'] = 'Options';
+$lang['CWatch.tab_licenses.no_upgrade_options'] = 'Il n\'y a pas de licences disponibles pour la mise à niveau.';
+$lang['CWatch.tab_licenses.no_results'] = 'Aucune licence n\'a été fournie pour ce service.';
+$lang['CWatch.tab_licenses.upgrade_delay'] = 'Les mises à jour peuvent prendre un certain temps à se propager, de sorte que l\'ancien type de licence peut encore être affiché.';
+$lang['CWatch.tab_licenses.not_applicable'] = 'N/A';
+$lang['CWatch.tab_licenses.deactivate_license'] = 'Désactiver la licence';
+$lang['CWatch.tab_licenses.remove_site'] = 'Supprimer le site';
+$lang['CWatch.tab_licenses.add_site'] = 'Ajouter un site';
+$lang['CWatch.tab_licenses.upgrade_site'] = 'Site de mise à niveau';
+$lang['CWatch.tab_licenses.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce domaine ?';
+$lang['CWatch.tab_licenses.confirm_deactivate'] = 'Êtes-vous sûr de vouloir désactiver cette licence ?';
+$lang['CWatch.tab_licenses.cancel'] = 'Annuler';
+$lang['CWatch.tab_licenses.current_license'] = 'Licence actuelle';
+$lang['CWatch.tab_licenses.license_name'] = '%1$s (%2$s)';
+$lang['CWatch.tab_licenses.initiateDns'] = 'DNS initial';
+$lang['CWatch.tab_licenses.autoSsl'] = 'AutoSSL';
+$lang['CWatch.tab_licenses.submit'] = 'Soumettre';
+$lang['CWatch.tab_malware.malware'] = 'Contrôle des logiciels malveillants';
+$lang['CWatch.tab_malware.add_scanner'] = 'Ajouter un scanner';
+$lang['CWatch.tab_malware.test_credentials'] = 'Testez ces références';
+$lang['CWatch.tab_malware.domainname'] = 'Nom de domaine';
+$lang['CWatch.tab_malware.ftppassword'] = 'Mot de passe FTP';
+$lang['CWatch.tab_malware.ftpusername'] = 'Nom d\'utilisateur FTP';
+$lang['CWatch.tab_malware.host'] = 'Nom d\'hôte FTP';
+$lang['CWatch.tab_malware.port'] = 'Port FTP';
+$lang['CWatch.tab_malware.sftp'] = 'Utiliser SFTP';
+$lang['CWatch.tab_malware.path'] = 'Répertoire des sites';
+$lang['CWatch.tab_malware.submit'] = 'Soumettre';
+$lang['CWatch.getsitestatuses.waiting'] = 'Attente';
+$lang['CWatch.getsitestatuses.site_inprogress'] = 'Mise à disposition du site en cours';
+$lang['CWatch.getsitestatuses.site_retry'] = 'Réessayer la mise à disposition du site';
+$lang['CWatch.getsitestatuses.site_completed'] = 'Site ajouté';
+$lang['CWatch.getsitestatuses.site_failed'] = 'Échec de la mise à disposition du site';
+$lang['CWatch.getsitestatuses.dns_inprogress'] = 'Analyse DNS en cours';
+$lang['CWatch.getsitestatuses.dns_retry'] = 'Nouvelle tentative d\'analyse DNS';
+$lang['CWatch.getsitestatuses.dns_completed'] = 'Analyse DNS terminée';
+$lang['CWatch.getsitestatuses.dns_failed'] = 'Site ajouté, échec de l\'analyse DNS';
+$lang['CWatch.getsitestatuses.ssl_inprogress'] = 'Installation de SSL en cours';
+$lang['CWatch.getsitestatuses.ssl_retry'] = 'Réessayer l\'installation de SSL';
+$lang['CWatch.getsitestatuses.ssl_completed'] = 'Ajout d\'un certificat SSL';
+$lang['CWatch.getsitestatuses.ssl_fail'] = 'Site ajouté, échec de l\'installation de SSL';
+
