82f72aa1c836d0652ff6ce08d03d50b666abb8d2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:13 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/apply_payments.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Apply Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.apply_payments.attempt'] = 'Προσπάθεια εφαρμογής πιστώσεων σε ανοικτά τιμολόγια.';
+$lang['Automation.task.apply_payments.completed'] = 'Η εργασία "Εφαρμογή πιστώσεων" έχει ολοκληρωθεί.';
+$lang['Automation.task.apply_payments.attempt_group'] = 'Προσπάθεια εφαρμογής πιστώσεων για την ομάδα πελατών %1$s.';
+$lang['Automation.task.apply_payments.completed_group'] = 'Ολοκληρώθηκε η εφαρμογή πιστώσεων για την ομάδα πελατών %1$s.';
+$lang['Automation.task.apply_payments.apply_failed'] = 'Δεν είναι δυνατή η εφαρμογή εκκρεμών πιστώσεων για τον πελάτη #%1$s.';
+$lang['Automation.task.apply_payments.apply_success'] = 'Εφαρμόστηκαν επιτυχώς εκκρεμείς πιστώσεις από τη συναλλαγή %1$s για τον πελάτη #%2$s στο τιμολόγιο #%3$s ύψους %4$s.';
+$lang['Automation.task.apply_payments.apply_none'] = 'Δεν υπάρχουν τιμολόγια στα οποία μπορούν να εφαρμοστούν πιστώσεις.';
+
