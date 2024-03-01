7e0781284b36f03a91096c8a14fd2e54226e5d94
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:56 -0800

    Updated de_de language files

---
+++ language/de_de/coupons.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Coupons
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Coupons.!error.amounts[][type].format'] = 'Ungültiger Betragstyp.';
+$lang['Coupons.!error.amounts[][amount].positive'] = 'Jeder Abschlagswert muss positiv sein.';
+$lang['Coupons.!error.amounts[][amount].format'] = 'Jeder Rabattwert muss eine Zahl sein.';
+$lang['Coupons.!error.amounts[][currency].length'] = 'Der Währungscode muss aus 3 Zeichen bestehen.';
+$lang['Coupons.!error.amounts.exists'] = 'Für diesen Coupon kann nur eine Währung jeder Art als Rabatt gelten.';
+$lang['Coupons.!error.packages[].exists'] = 'Mindestens eines der Pakete, denen Sie diesen Coupon zuweisen, ist ungültig.';
+$lang['Coupons.!error.coupon_id.exists'] = 'Ungültige Coupon-ID.';
+$lang['Coupons.!error.internal_use_only.format'] = 'Die Angabe, ob der Coupon nur für den internen Gebrauch bestimmt ist, muss auf 1 oder 0 gesetzt werden.';
+$lang['Coupons.!error.apply_package_options.format'] = 'Ob der Coupon für konfigurierbare Optionen gilt, muss auf 1 oder 0 gesetzt werden.';
+$lang['Coupons.!error.limit_recurring.length'] = 'Limit Wiederkehrende Länge darf 1 Zeichen nicht überschreiten.';
+$lang['Coupons.!error.limit_recurring.format'] = 'Limit Wiederkehrend muss eine Zahl sein.';
+$lang['Coupons.!error.recurring.length'] = 'Die wiederkehrende Länge darf 1 Zeichen nicht überschreiten.';
+$lang['Coupons.!error.recurring.format'] = 'Wiederkehrend muss eine Zahl sein.';
+$lang['Coupons.!error.status.format'] = 'Ungültiger Status.';
+$lang['Coupons.!error.end_date.format'] = 'Ungültiges Enddatum-Format.';
+$lang['Coupons.!error.start_date.format'] = 'Ungültiges Format für das Startdatum.';
+$lang['Coupons.!error.max_qty.exceeded'] = 'Die verwendete Menge darf die maximale Menge nicht überschreiten.';
+$lang['Coupons.!error.max_qty.length'] = 'Die maximale Länge der Menge darf 10 Zeichen nicht überschreiten.';
+$lang['Coupons.!error.max_qty.format'] = 'Die maximale Menge muss eine Zahl sein.';
+$lang['Coupons.!error.used_qty.length'] = 'Die Länge der verwendeten Menge darf 10 Zeichen nicht überschreiten.';
+$lang['Coupons.!error.used_qty.format'] = 'Die verwendete Menge muss eine Zahl sein.';
+$lang['Coupons.!error.package_group_id.length'] = 'Die Paketgruppen-ID darf nicht länger als 10 Zeichen sein.';
+$lang['Coupons.!error.company_id.exists'] = 'Ungültige Unternehmens-ID angegeben.';
+$lang['Coupons.!error.code.unique'] = 'Der Gutscheincode ist derzeit auf einen anderen Gutschein eingestellt und kann nicht wiederverwendet werden.';
+$lang['Coupons.!error.code.length'] = 'Die Länge des Coupon-Codes darf 64 Zeichen nicht überschreiten.';
+$lang['Coupons.!error.code.empty'] = 'Bitte geben Sie einen Gutscheincode ein.';
+$lang['Coupons.getAmountTypes.percent'] = 'Prozentsatz';
+$lang['Coupons.getAmountTypes.amount'] = 'Betrag';
+
