4558a78616d8835d611108d3cec60486aa56d497
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:08 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/client_dialog.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Client Dialog
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDialog.confirm.text_yes'] = 'Oui';
+$lang['ClientDialog.confirm.text_no'] = 'Non';
+$lang['ClientDialog.password.title'] = 'Générer un mot de passe';
+$lang['ClientDialog.password.field_modal_password'] = 'Mot de passe';
+$lang['ClientDialog.password.field_modal_password_copied'] = 'J\'ai copié ce mot de passe dans un endroit sûr';
+$lang['ClientDialog.password.btn_generate'] = 'Générer';
+$lang['ClientDialog.password.btn_cancel'] = 'Annuler';
+$lang['ClientDialog.password.btn_use_password'] = 'Utiliser le mot de passe';
+
