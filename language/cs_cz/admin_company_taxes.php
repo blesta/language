5df284d2ad8af5f3e2453ebe03e0c13d54124e85
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:26 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_company_taxes.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Admin Company Taxes
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyTaxes.edit.field.editsubmit'] = 'Upravit pravidlo';
+$lang['AdminCompanyTaxes.edit.field.state'] = 'Stát/provincie';
+$lang['AdminCompanyTaxes.edit.field.country'] = 'Země';
+$lang['AdminCompanyTaxes.edit.field.amount'] = 'Částka';
+$lang['AdminCompanyTaxes.edit.field.name'] = 'Název daně';
+$lang['AdminCompanyTaxes.edit.field.level2'] = 'Úroveň 2';
+$lang['AdminCompanyTaxes.edit.field.level1'] = 'Úroveň 1';
+$lang['AdminCompanyTaxes.edit.field.level'] = 'Úroveň zdanění';
+$lang['AdminCompanyTaxes.edit.field.type'] = 'Typ daně';
+$lang['AdminCompanyTaxes.edit.boxtitle_edit'] = 'Upravit daňové pravidlo';
+$lang['AdminCompanyTaxes.edit.page_title'] = 'Nastavení > Společnost > Daně > Upravit daňové pravidlo';
+$lang['AdminCompanyTaxes.add.field.addsubmit'] = 'Vytvořit pravidlo';
+$lang['AdminCompanyTaxes.add.field.state'] = 'Stát/provincie';
+$lang['AdminCompanyTaxes.add.field.country'] = 'Země';
+$lang['AdminCompanyTaxes.add.field.amount'] = 'Částka';
+$lang['AdminCompanyTaxes.add.field.name'] = 'Název daně';
+$lang['AdminCompanyTaxes.add.field.level2'] = 'Úroveň 2';
+$lang['AdminCompanyTaxes.add.field.level1'] = 'Úroveň 1';
+$lang['AdminCompanyTaxes.add.field.level'] = 'Úroveň zdanění';
+$lang['AdminCompanyTaxes.add.field.type'] = 'Typ daně';
+$lang['AdminCompanyTaxes.add.boxtitle_add'] = 'Přidat daňové pravidlo';
+$lang['AdminCompanyTaxes.add.page_title'] = 'Nastavení > Společnost > Daně > Přidat daňové pravidlo';
+$lang['AdminCompanyTaxes.rules.confirm_delete'] = 'Opravdu chcete toto daňové pravidlo odstranit?';
+$lang['AdminCompanyTaxes.rules.option_delete'] = 'Odstranit';
+$lang['AdminCompanyTaxes.rules.option_edit'] = 'Upravit';
+$lang['AdminCompanyTaxes.rules.text_all'] = 'Všechny';
+$lang['AdminCompanyTaxes.rules.text_options'] = 'Možnosti';
+$lang['AdminCompanyTaxes.rules.text_state'] = 'Stát/provincie';
+$lang['AdminCompanyTaxes.rules.text_country'] = 'Země';
+$lang['AdminCompanyTaxes.rules.text_amount'] = 'Částka';
+$lang['AdminCompanyTaxes.rules.text_type'] = 'Typ';
+$lang['AdminCompanyTaxes.rules.text_name'] = 'Název';
+$lang['AdminCompanyTaxes.rules.heading_level2'] = 'Pravidla úrovně 2';
+$lang['AdminCompanyTaxes.rules.heading_level1'] = 'Pravidla úrovně 1';
+$lang['AdminCompanyTaxes.rules.boxtitle_rules'] = 'Daňová pravidla';
+$lang['AdminCompanyTaxes.rules.categorylink_addrule'] = 'Přidat daňové pravidlo';
+$lang['AdminCompanyTaxes.rules.no_results'] = 'Neexistují žádná daňová pravidla na úrovni %1$s.';
+$lang['AdminCompanyTaxes.rules.page_title'] = 'Nastavení > Společnost > Daně > Daňová pravidla';
+$lang['AdminCompanyTaxes.basic.field_addsubmit'] = 'Aktualizace nastavení';
+$lang['AdminCompanyTaxes.basic.heading_tax_provider'] = '%1$s Nastavení';
+$lang['AdminCompanyTaxes.basic.field_taxid'] = 'Daňové identifikační číslo/VATIN';
+$lang['AdminCompanyTaxes.basic.note_cancelation_fee_tax'] = 'Pokud je tato možnost povolena, budou případné stornopoplatky zdaněny.';
+$lang['AdminCompanyTaxes.basic.field_cancelation_fee_tax'] = 'Poplatky za zrušení daně';
+$lang['AdminCompanyTaxes.basic.note_setup_fee_tax'] = 'Pokud je tato možnost povolena, budou veškeré poplatky za zřízení zdaněny.';
+$lang['AdminCompanyTaxes.basic.field_setup_fee_tax'] = 'Poplatky za zřízení daně';
+$lang['AdminCompanyTaxes.basic.note_cascade_tax'] = 'Pokud je tato možnost povolena, bude nejprve vyměřena daňová úroveň 1 z celkové částky faktury a daňová úroveň 2 bude vyměřena z této nové celkové částky včetně daňové úrovně 1. Výsledkem je daň na dani. Jinak se úroveň daně 1 a úroveň daně 2 vyměří pouze z celkové částky faktury před zdaněním.';
+$lang['AdminCompanyTaxes.basic.field_cascade_tax'] = 'Cascade Tax';
+$lang['AdminCompanyTaxes.basic.note_enable_tax'] = 'Zaškrtnutím této možnosti povolíte daň pro tuto společnost.';
+$lang['AdminCompanyTaxes.basic.field_enable_tax'] = 'Povolit daň';
+$lang['AdminCompanyTaxes.basic.heading_general'] = 'Obecná nastavení';
+$lang['AdminCompanyTaxes.basic.boxtitle_basic'] = 'Základní nastavení daní';
+$lang['AdminCompanyTaxes.basic.page_title'] = 'Nastavení > Společnost > Daně > Základní nastavení daní';
+$lang['AdminCompanyTaxes.!tooltip.state'] = 'Vyberte stát/provincii, na kterou se toto daňové pravidlo vztahuje.';
+$lang['AdminCompanyTaxes.!tooltip.country'] = 'Vyberte zemi, na kterou se toto daňové pravidlo vztahuje.';
+$lang['AdminCompanyTaxes.!tooltip.amount'] = 'Výše daně v procentech.';
+$lang['AdminCompanyTaxes.!tooltip.name'] = 'Zobrazený název daně (např. Daň z prodeje).';
+$lang['AdminCompanyTaxes.!tooltip.level'] = 'Úroveň daně umožňuje nastavit pořadí daně, pokud se použije více daňových pravidel.';
+$lang['AdminCompanyTaxes.!tooltip.type'] = 'Inclusive vypočítá daň jako součást ceny položky, kterou nastavíte, a u uživatelů osvobozených od daně se od ceny položky odečte.<br/>
+Inclusive (Additive) bude daň počítat navíc k nastaveným cenám položek.<br/>
+Exclusive (Včetně) vypočítá daň navíc k vámi nastaveným cenám položek, ale nezahrne ji do celkového zobrazení objednávky.';
+$lang['AdminCompanyTaxes.states.all'] = '-- Vše --';
+$lang['AdminCompanyTaxes.countries.all'] = '-- Vše --';
+$lang['AdminCompanyTaxes.!success.rule_deleted'] = 'Daňové pravidlo bylo úspěšně odstraněno.';
+$lang['AdminCompanyTaxes.!success.taxrule_updated'] = 'Daňové pravidlo bylo úspěšně aktualizováno!';
+$lang['AdminCompanyTaxes.!success.taxrule_created'] = 'Daňové pravidlo bylo úspěšně vytvořeno!';
+$lang['AdminCompanyTaxes.!success.basic_updated'] = 'Základní nastavení daně bylo úspěšně aktualizováno!';
+
