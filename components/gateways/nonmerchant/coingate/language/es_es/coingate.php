25849e0f74b78b9986a336f85189751c4ff9e4db
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:29 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/coingate/language/es_es/coingate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Coingate
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Coingate.name'] = 'CoinGate.com';
+$lang['Coingate.description'] = 'Pague con Bitcoin o Altcoins a través de CoinGate.com';
+$lang['Coingate.app_id'] = 'ID APP';
+$lang['Coingate.api_key'] = 'Clave API';
+$lang['Coingate.api_secret'] = 'Secreto API';
+$lang['Coingate.coingate_environment'] = 'Entorno CoinGate';
+$lang['Coingate.environment.sandbox'] = 'arenero';
+$lang['Coingate.environment.live'] = 'en directo';
+$lang['Coingate.receive_currency'] = 'Moneda de pago';
+$lang['Coingate.receive_currency_note'] = 'Divisa que desea recibir al retirar dinero en CoinGate.
+										   Tenga en cuenta que si elige EUR o USD se le pedirá que verifique
+										   su negocio antes de realizar una retirada en CoinGate.';
+$lang['Coingate.environment_note'] = 'Activa "sandbox" para probar en sandbox.coingate.com. Tenga en cuenta que para el modo sandbox debe generar credenciales de API independientes en sandbox.coingate.com. Las credenciales de API generadas en coingate.com no funcionarán en el modo sandbox.';
+$lang['Coingate.buildprocess.submit'] = 'CoinGate.com';
+$lang['Coingate.receive_currency.usd'] = 'Dólares estadounidenses $';
+$lang['Coingate.receive_currency.btc'] = 'Bitcoin ฿';
+$lang['Coingate.receive_currency.eur'] = 'Euros';
+$lang['Coingate.!error.app_id.empty'] = 'El ID de APP no puede estar vacío.';
+$lang['Coingate.!error.api_key.empty'] = 'La clave API no puede estar vacía';
+$lang['Coingate.!error.api_secret.empty'] = 'API Secret no puede estar vacío';
+$lang['Coingate.!error.payment.invalid'] = 'La transacción no es válida y no ha podido ser procesada.';
+$lang['Coingate.!error.payment.canceled'] = 'La transacción se ha cancelado y no se ha podido procesar.';
+$lang['Coingate.!error.payment.expired'] = 'La transacción ha caducado y no ha podido ser procesada.';
+$lang['Coingate.!error.failed.response'] = 'No se ha podido procesar la transacción.';
+
