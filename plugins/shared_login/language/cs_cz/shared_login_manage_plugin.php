7cac62461d320054b1651674cf2166a370c60c4b
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:34 -0800

    Updated cs_cz language files

---
+++ plugins/shared_login/language/cs_cz/shared_login_manage_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Shared Login Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SharedLoginManagePlugin.index.submit'] = 'Uložit';
+$lang['SharedLoginManagePlugin.index.field_key'] = 'Sdílený klíč';
+$lang['SharedLoginManagePlugin.index.boxtitle_manage'] = 'Sdílené přihlášení';
+$lang['SharedLoginManagePlugin.index.page_title'] = 'Sdílené přihlášení';
+
