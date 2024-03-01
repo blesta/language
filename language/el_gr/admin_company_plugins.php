ebc02a629617fef8eb41b8cde555fd3fe4f79c58
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:48 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_company_plugins.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Admin Company Plugins
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyPlugins.settings.actions_heading_secondary'] = 'Δευτερεύουσες επιλογές';
+$lang['AdminCompanyPlugins.settings.actions_heading_sub'] = 'Υποεπιλογές';
+$lang['AdminCompanyPlugins.settings.actions_uri'] = 'URI: %1$s';
+$lang['AdminCompanyPlugins.settings.actions_description'] = 'Οι ενέργειες καθορίζουν τι μπορεί να εμφανίζεται στις διεπαφές διαχειριστή και πελάτη εκ μέρους του πρόσθετου, όπως σύνδεσμοι πλοήγησης ή widgets.';
+$lang['AdminCompanyPlugins.settings.events_no_results'] = 'Δεν έχουν οριστεί συμβάντα για αυτό το πρόσθετο.';
+$lang['AdminCompanyPlugins.settings.automation_no_results'] = 'Δεν έχουν οριστεί εργασίες αυτοματοποίησης για αυτό το πρόσθετο.';
+$lang['AdminCompanyPlugins.settings.actions_no_results'] = 'Δεν έχουν οριστεί ενέργειες για αυτό το πρόσθετο.';
+$lang['AdminCompanyPlugins.settings.events_description'] = 'Το πρόσθετο ακούει τα παρακάτω συμβάντα και εκτελεί κάποια ενέργεια όταν αυτά συμβαίνουν.';
+$lang['AdminCompanyPlugins.settings.automation_description'] = 'Το πρόσθετο έχει καταχωρήσει τις παρακάτω εργασίες αυτοματοποίησης για την εκτέλεση ορισμένων ενεργειών όταν εκτελούνται από το cron.';
+$lang['AdminCompanyPlugins.settings.tab_events'] = 'Εκδηλώσεις';
+$lang['AdminCompanyPlugins.settings.tab_actions'] = 'Δράσεις';
+$lang['AdminCompanyPlugins.settings.tab_automation'] = 'Αυτοματισμός';
+$lang['AdminCompanyPlugins.settings.page_title'] = '%1$s Ρυθμίσεις';
+$lang['AdminCompanyPlugins.settings.field_submit'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminCompanyPlugins.settings.field_enabled'] = 'Ενεργοποιημένο';
+$lang['AdminCompanyPlugins.installed.text_none'] = 'Δεν υπάρχουν εγκατεστημένα πρόσθετα.';
+$lang['AdminCompanyPlugins.installed.btn_upgrade'] = 'Αναβάθμιση';
+$lang['AdminCompanyPlugins.installed.btn_settings'] = 'Ρυθμίσεις';
+$lang['AdminCompanyPlugins.installed.btn_manage'] = 'Διαχείριση';
+$lang['AdminCompanyPlugins.installed.btn_enable'] = 'Ενεργοποίηση';
+$lang['AdminCompanyPlugins.installed.btn_disable'] = 'Απενεργοποίηση';
+$lang['AdminCompanyPlugins.installed.btn_uninstall'] = 'Απεγκατάσταση του';
+$lang['AdminCompanyPlugins.installed.confirm_enable'] = 'Ενεργοποιήστε πραγματικά αυτό το πρόσθετο;';
+$lang['AdminCompanyPlugins.installed.confirm_disable'] = 'Πραγματικά απενεργοποιήστε αυτό το πρόσθετο;';
+$lang['AdminCompanyPlugins.installed.confirm_uninstall'] = 'Πραγματικά απεγκαταστήστε αυτό το πρόσθετο;';
+$lang['AdminCompanyPlugins.installed.text_author'] = 'Συγγραφέας:';
+$lang['AdminCompanyPlugins.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyPlugins.installed.boxtitle_plugin'] = 'Plugins';
+$lang['AdminCompanyPlugins.installed.page_title'] = 'Ρυθμίσεις > Εταιρεία > Πρόσθετα > Εγκατεστημένα';
+$lang['AdminCompanyPlugins.available.text_none'] = 'Δεν υπάρχουν διαθέσιμα πρόσθετα.';
+$lang['AdminCompanyPlugins.available.btn_install'] = 'Εγκαταστήστε το';
+$lang['AdminCompanyPlugins.available.text_author'] = 'Συγγραφέας:';
+$lang['AdminCompanyPlugins.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyPlugins.available.boxtitle_plugins'] = 'Plugins';
+$lang['AdminCompanyPlugins.available.page_title'] = 'Ρυθμίσεις > Εταιρεία > Plugins > Διαθέσιμα';
+$lang['AdminCompanyPlugins.settings.event.users.logout'] = 'Αυτό το συμβάν ενεργοποιείται όταν ένας χρήστης αποσυνδεθεί.';
+$lang['AdminCompanyPlugins.settings.event.users.login'] = 'Αυτό το συμβάν ενεργοποιείται όταν ένας χρήστης συνδεθεί.';
+$lang['AdminCompanyPlugins.settings.event.users.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν ένας χρήστης διαγράφεται.';
+$lang['AdminCompanyPlugins.settings.event.transactions.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται μια συναλλαγή.';
+$lang['AdminCompanyPlugins.settings.event.transactions.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται μια συναλλαγή.';
+$lang['AdminCompanyPlugins.settings.event.staff.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται ένα μέλος του προσωπικού.';
+$lang['AdminCompanyPlugins.settings.event.staff.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται ένα μέλος του προσωπικού.';
+$lang['AdminCompanyPlugins.settings.event.services.unsuspend'] = 'Αυτό το συμβάν ενεργοποιείται όταν μια υπηρεσία δεν αναστέλλεται.';
+$lang['AdminCompanyPlugins.settings.event.services.suspend'] = 'Αυτό το συμβάν ενεργοποιείται όταν ανασταλεί μια υπηρεσία.';
+$lang['AdminCompanyPlugins.settings.event.services.cancel'] = 'Αυτό το συμβάν ενεργοποιείται όταν μια υπηρεσία ακυρώνεται ή έχει προγραμματιστεί να ακυρωθεί.';
+$lang['AdminCompanyPlugins.settings.event.services.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται μια υπηρεσία.';
+$lang['AdminCompanyPlugins.settings.event.services.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται μια υπηρεσία.';
+$lang['AdminCompanyPlugins.settings.event.report.clientdata'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται η αναφορά Φορητότητας δεδομένων πελάτη.';
+$lang['AdminCompanyPlugins.settings.event.packages.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν διαγράφεται ένα πακέτο.';
+$lang['AdminCompanyPlugins.settings.event.packages.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται ένα πακέτο.';
+$lang['AdminCompanyPlugins.settings.event.packages.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται ένα πακέτο.';
+$lang['AdminCompanyPlugins.settings.event.navigation.getsearchoptions'] = 'Αυτό το συμβάν ενεργοποιείται όταν εμφανίζονται οι επιλογές αναζήτησης για το προσωπικό.';
+$lang['AdminCompanyPlugins.settings.event.modulemanager.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν απεγκαθίσταται μια ενότητα.';
+$lang['AdminCompanyPlugins.settings.event.modulemanager.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν εγκαθίσταται μια ενότητα.';
+$lang['AdminCompanyPlugins.settings.event.invoices.setclosed'] = 'Αυτό το συμβάν ενεργοποιείται όταν κλείνει ένα τιμολόγιο.';
+$lang['AdminCompanyPlugins.settings.event.invoices.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται ένα τιμολόγιο.';
+$lang['AdminCompanyPlugins.settings.event.invoices.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται ένα τιμολόγιο.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν απεγκαθίσταται μια πύλη.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται μια πύλη.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν εγκαθίσταται μια πύλη.';
+$lang['AdminCompanyPlugins.settings.event.emails.sendcustom'] = 'Αυτό το συμβάν ενεργοποιείται όταν αποστέλλεται ένα προσαρμοσμένο email.';
+$lang['AdminCompanyPlugins.settings.event.emails.send'] = 'Αυτό το συμβάν ενεργοποιείται όταν αποστέλλεται ένα πρότυπο email.';
+$lang['AdminCompanyPlugins.settings.event.contacts.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν διαγράφεται μια επαφή πελάτη.';
+$lang['AdminCompanyPlugins.settings.event.contacts.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται μια επαφή πελάτη.';
+$lang['AdminCompanyPlugins.settings.event.contacts.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται μια επαφή πελάτη.';
+$lang['AdminCompanyPlugins.settings.event.companies.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν μια εταιρεία διαγράφεται.';
+$lang['AdminCompanyPlugins.settings.event.companies.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται μια εταιρεία.';
+$lang['AdminCompanyPlugins.settings.event.companies.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται μια εταιρεία.';
+$lang['AdminCompanyPlugins.settings.event.clients.deletenote'] = 'Αυτό το συμβάν ενεργοποιείται όταν διαγράφεται μια σημείωση πελάτη.';
+$lang['AdminCompanyPlugins.settings.event.clients.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν ένας πελάτης διαγράφεται.';
+$lang['AdminCompanyPlugins.settings.event.clients.editnote'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται μια σημείωση πελάτη.';
+$lang['AdminCompanyPlugins.settings.event.clients.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται ένας πελάτης.';
+$lang['AdminCompanyPlugins.settings.event.clients.create'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται ένας πελάτης.';
+$lang['AdminCompanyPlugins.settings.event.clients.addnote'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται μια σημείωση πελάτη.';
+$lang['AdminCompanyPlugins.settings.event.clients.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν προστίθεται ένας πελάτης.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν διαγράφεται μια ομάδα πελατών.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται μια ομάδα πελατών.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται μια ομάδα πελατών.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν διαγράφεται ένα συμβάν ημερολογίου.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.edit'] = 'Αυτό το συμβάν ενεργοποιείται όταν ενημερώνεται ένα συμβάν ημερολογίου.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.add'] = 'Αυτό το συμβάν ενεργοποιείται όταν δημιουργείται ένα συμβάν ημερολογίου.';
+$lang['AdminCompanyPlugins.settings.event.appcontroller.structure'] = 'Αυτό το συμβάν ενεργοποιείται όταν πραγματοποιείται φόρτωση σελίδας που δεν γίνεται μέσω AJAX.';
+$lang['AdminCompanyPlugins.settings.event.appcontroller.preaction'] = 'Αυτό το συμβάν ενεργοποιείται όταν πραγματοποιείται φόρτωση σελίδας.';
+$lang['AdminCompanyPlugins.!tab.available'] = 'Διαθέσιμο';
+$lang['AdminCompanyPlugins.!tab.installed'] = 'Εγκατεστημένο';
+$lang['AdminCompanyPlugins.!error.setting_controller_invalid'] = 'Ο καθορισμένος ελεγκτής ρυθμίσεων δεν υπάρχει για το συγκεκριμένο πρόσθετο.';
+$lang['AdminCompanyPlugins.!success.events_updated'] = 'Οι ρυθμίσεις συμβάντος ενημερώθηκαν επιτυχώς.';
+$lang['AdminCompanyPlugins.!success.actions_updated'] = 'Οι ρυθμίσεις δράσης ενημερώθηκαν με επιτυχία.';
+$lang['AdminCompanyPlugins.!success.automation_updated'] = 'Οι ρυθμίσεις αυτοματισμού ενημερώθηκαν με επιτυχία.';
+$lang['AdminCompanyPlugins.!success.disabled'] = 'Το πρόσθετο απενεργοποιήθηκε επιτυχώς.';
+$lang['AdminCompanyPlugins.!success.enabled'] = 'Το πρόσθετο ενεργοποιήθηκε με επιτυχία.';
+$lang['AdminCompanyPlugins.!success.upgraded'] = 'Το πρόσθετο αναβαθμίστηκε με επιτυχία.';
+$lang['AdminCompanyPlugins.!success.uninstalled'] = 'Το πρόσθετο απεγκαταστάθηκε επιτυχώς.';
+$lang['AdminCompanyPlugins.!success.installed'] = 'Το πρόσθετο εγκαταστάθηκε με επιτυχία. Μπορεί να έχει καταχωρήσει δικαιώματα ACL για διάφορους πόρους. Ίσως χρειαστεί να χορηγήσετε στην ομάδα προσωπικού σας πρόσβαση σε αυτά τα δικαιώματα για να αποκτήσετε πρόσβαση σε αυτούς τους πόρους.';
+
