b57a03c2110fe530a533b9deafaeb2484b3ee6a9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:37 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/perfectmoney/language/es_es/perfectmoney.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Perfectmoney
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Perfectmoney.name'] = 'Dinero perfecto';
+$lang['Perfectmoney.payee_account'] = 'Cuenta del beneficiario';
+$lang['Perfectmoney.passphrase'] = 'Frase de acceso alternativa';
+$lang['Perfectmoney.passphrase_note'] = 'La frase de contraseña alternativa se utiliza para el cálculo del hash MD5 para validar los pagos.';
+$lang['Perfectmoney.buildprocess.submit'] = 'Pagar con Perfect Money';
+$lang['Perfectmoney.!error.payee_account.valid'] = 'Debe introducir una cuenta válida.';
+$lang['Perfectmoney.!error.passphrase.valid'] = 'Debe introducir una frase de contraseña válida.';
+
