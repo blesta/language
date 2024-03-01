83e560bb9f9e36f5e7982afc06abf834ee232a98
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:36 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/razorpay/language/el_gr/razorpay.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Razorpay
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Razorpay.!error.key_secret.valid'] = 'Πρέπει να εισαγάγετε ένα έγκυρο Μυστικό κλειδί.';
+$lang['Razorpay.!error.key_id.valid'] = 'Πρέπει να εισαγάγετε ένα έγκυρο αναγνωριστικό κλειδιού.';
+$lang['Razorpay.buildprocess.submit'] = 'Πληρώστε με το Razorpay';
+$lang['Razorpay.webhook_note'] = 'Πριν ξεκινήσετε να χρησιμοποιείτε αυτή την πύλη, πρέπει να ρυθμίσετε το ακόλουθο url ως Webhook στο λογαριασμό σας Razorpay.';
+$lang['Razorpay.webhook'] = 'Razorpay Webhook';
+$lang['Razorpay.webhook_secret'] = 'Μυστικό Webhook';
+$lang['Razorpay.key_secret'] = 'Μυστικό κλειδί';
+$lang['Razorpay.key_id'] = 'Αναγνωριστικό κλειδί';
+$lang['Razorpay.description'] = 'Η Razorpay είναι μια εταιρεία πληρωμών που παρέχει λύσεις πληρωμών σε διαδικτυακούς εμπόρους στην Ινδία.';
+$lang['Razorpay.name'] = 'Razorpay';
+
