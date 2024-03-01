a439c1d7c01238a78b402eecf1ae0eda90dda5e1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:17 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/actions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Actions
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Actions.getLocationDescriptions.action_staff_client'] = 'Zobrazí odkaz v části "Akce účtu" na stránce profilu klienta.';
+$lang['Actions.getLocationDescriptions.widget_staff_billing'] = 'Vykreslí widget na stránce vyúčtování zaměstnanců.';
+$lang['Actions.getLocationDescriptions.widget_staff_client'] = 'Vykreslí widget na stránce s profilem zaměstnance.';
+$lang['Actions.getLocationDescriptions.widget_staff_home'] = 'Vykreslí widget na palubní desce zaměstnanců.';
+$lang['Actions.getLocationDescriptions.widget_client_home'] = '	Vykreslí widget na panelu klienta.';
+$lang['Actions.getLocationDescriptions.nav_public'] = 'Zpřístupní odkaz v navigaci veřejného rozhraní.';
+$lang['Actions.getLocationDescriptions.nav_client'] = 'Zpřístupní odkaz v navigaci klientského rozhraní.';
+$lang['Actions.getLocationDescriptions.nav_staff'] = 'Zpřístupní odkaz v navigaci rozhraní správce.';
+$lang['Actions.getLocations.action_staff_client'] = 'Odkaz "Akce na účtu" klienta';
+$lang['Actions.getLocations.widget_staff_billing'] = 'Widget pro fakturaci zaměstnanců';
+$lang['Actions.getLocations.widget_staff_client'] = 'Widget profilu klienta Staff';
+$lang['Actions.getLocations.widget_staff_home'] = 'Widget pro ovládací panel zaměstnanců';
+$lang['Actions.getLocations.widget_client_home'] = 'Widget klientského panelu';
+$lang['Actions.getLocations.nav_public'] = 'Veřejné navigační menu';
+$lang['Actions.getLocations.nav_client'] = 'Navigační menu klienta';
+$lang['Actions.getLocations.nav_staff'] = 'Navigační menu pro zaměstnance';
+$lang['Actions.!error.enabled.valid'] = 'Možnost povoleno musí být nastavena na hodnotu 1 nebo 0.';
+$lang['Actions.!error.editable.valid'] = 'Možnost upravitelné musí být nastavena na 1 nebo 0.';
+$lang['Actions.!error.company_id.valid'] = 'Neplatné ID společnosti';
+$lang['Actions.!error.plugin_id.valid'] = 'Uveďte platné ID zásuvného modulu.';
+$lang['Actions.!error.name.action_empty'] = 'Zadejte název akce.';
+$lang['Actions.!error.url.empty'] = 'Zadejte prosím adresu URL.';
+$lang['Actions.!error.location.unique'] = 'Umístění a adresa URL musí být pro tuto společnost jedinečné.';
+$lang['Actions.!error.location.valid'] = 'Neplatné platné umístění.';
+
