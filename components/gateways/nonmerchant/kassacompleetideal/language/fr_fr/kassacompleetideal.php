306b693475065046f7711c7b71f9fe491cd9bb07
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:35 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/kassacompleetideal/language/fr_fr/kassacompleetideal.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Kassacompleetideal
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Kassacompleetideal.!error.api_key.empty'] = 'Veuillez saisir une clé API';
+$lang['Kassacompleetideal.!error.api_key.valid'] = 'La clé API n\'est pas valide';
+$lang['Kassacompleetideal.name'] = 'iDeal (Kassa Compleet)';
+$lang['Kassacompleetideal.description'] = 'Intégration de Kassa Compleet avec le type de paiement iDeal';
+$lang['Kassacompleetideal.api_key'] = 'Clé API';
+$lang['Kassacompleetideal.tooltip.api_key'] = 'Pour utiliser le mode test, il suffit d\'entrer une clé API à partir d\'un compte test';
+$lang['Kassacompleetideal.buildprocess.issuer_id'] = 'iDeal Émetteur';
+$lang['Kassacompleetideal.buildprocess.submit'] = 'Payer avec iDeal via Kassa Compleet';
+
