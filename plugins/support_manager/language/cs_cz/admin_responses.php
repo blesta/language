16d007aca9eda63379ab29c67c9cf45fee1fa760
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:39 -0800

    Updated cs_cz language files

---
+++ plugins/support_manager/language/cs_cz/admin_responses.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Admin Responses
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminResponses.editcategory.field_submit'] = 'Kategorie aktualizace';
+$lang['AdminResponses.editcategory.field_name'] = 'Název';
+$lang['AdminResponses.editcategory.boxtitle_editcategory'] = 'Kategorie aktualizace';
+$lang['AdminResponses.editcategory.boxtitle_root'] = 'Upravit kategorii';
+$lang['AdminResponses.addcategory.field_submit'] = 'Vytvořit kategorii';
+$lang['AdminResponses.addcategory.field_name'] = 'Název';
+$lang['AdminResponses.addcategory.boxtitle_addcategory'] = 'Přidat kategorii do kategorie [%1$s]';
+$lang['AdminResponses.addcategory.boxtitle_root'] = 'Přidat kategorii';
+$lang['AdminResponses.edit.field_submit'] = 'Reakce na aktualizaci';
+$lang['AdminResponses.edit.field_details'] = 'Podrobnosti o reakci';
+$lang['AdminResponses.edit.field_name'] = 'Název';
+$lang['AdminResponses.edit.boxtitle_edit'] = 'Upravit předdefinovanou odpověď';
+$lang['AdminResponses.add.field_submit'] = 'Vytvořit reakci';
+$lang['AdminResponses.add.field_details'] = 'Podrobnosti o reakci';
+$lang['AdminResponses.add.field_name'] = 'Název';
+$lang['AdminResponses.add.boxtitle_add'] = 'Přidat předdefinovanou odpověď do kategorie [%1$s]';
+$lang['AdminResponses.modal.delete_response'] = 'Opravdu chcete tuto předdefinovanou odpověď odstranit?';
+$lang['AdminResponses.modal.delete_category'] = 'Opravdu chcete tuto kategorii odstranit? Všechny podkategorie a předdefinované odpovědi v rámci této kategorie budou přesunuty do nadřazené kategorie.';
+$lang['AdminResponses.index.no_responses'] = 'V této části nejsou žádné odpovědi.';
+$lang['AdminResponses.index.delete'] = 'Odstranit';
+$lang['AdminResponses.index.edit'] = 'Upravit';
+$lang['AdminResponses.index.text_up_level'] = 'Přejděte o úroveň výš';
+$lang['AdminResponses.index.add_category'] = 'Přidat kategorii zde';
+$lang['AdminResponses.index.add_response'] = 'Přidat odpověď zde';
+$lang['AdminResponses.index.boxtitle_responses'] = 'Předdefinované odpovědi';
+$lang['AdminResponses.!text.closed_parenthesis'] = ')';
+$lang['AdminResponses.!text.forward_slash'] = '/';
+$lang['AdminResponses.!text.open_parenthesis'] = '(';
+$lang['AdminResponses.editcategory.page_title'] = 'Správce podpory > Upravit kategorii';
+$lang['AdminResponses.addcategory.page_title'] = 'Správce podpory > Přidat kategorii';
+$lang['AdminResponses.edit.page_title'] = 'Správce podpory > Upravit předdefinovanou odpověď';
+$lang['AdminResponses.add.page_title'] = 'Správce podpory > Přidat předdefinovanou odpověď';
+$lang['AdminResponses.index.page_title'] = 'Správce podpory > Předdefinované odpovědi';
+$lang['AdminResponses.!success.response_deleted'] = 'Odpověď %1$s byla úspěšně odstraněna.';
+$lang['AdminResponses.!success.response_updated'] = 'Odpověď %1$s byla úspěšně aktualizována.';
+$lang['AdminResponses.!success.response_added'] = 'Odpověď %1$s byla úspěšně přidána.';
+$lang['AdminResponses.!success.category_deleted'] = 'Kategorie %1$s byla úspěšně odstraněna.';
+$lang['AdminResponses.!success.category_updated'] = 'Kategorie %1$s byla úspěšně aktualizována.';
+$lang['AdminResponses.!success.category_added'] = 'Kategorie %1$s byla úspěšně přidána.';
+
