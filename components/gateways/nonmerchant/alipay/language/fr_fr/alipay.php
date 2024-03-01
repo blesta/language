218fd13c0ff47b81eb862f4053c0c0d8e7624cd3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:28 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/alipay/language/fr_fr/alipay.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Alipay
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Alipay.name'] = 'Alipay';
+$lang['Alipay.description'] = 'L\'une des plus grandes plateformes de paiement mobile et en ligne au monde. Le marché principal est la Chine continentale.';
+$lang['Alipay.merchant_email'] = 'Email du commerçant';
+$lang['Alipay.merchant_uid'] = 'UID/PID du commerçant';
+$lang['Alipay.signature_key'] = 'Clé de signature';
+$lang['Alipay.dev_mode'] = 'Mode développeur';
+$lang['Alipay.dev_mode_note'] = 'L\'activation de cette option permet d\'enregistrer les transactions dans l\'environnement Alipay Sandbox. N\'activez cette option que si vous testez avec un compte Alipay Sandbox.';
+$lang['Alipay.buildprocess.submit'] = 'Payer avec Alipay';
+$lang['Alipay.!error.merchant_email.valid'] = 'Vous devez saisir une adresse électronique valide.';
+$lang['Alipay.!error.merchant_uid.valid'] = 'Vous devez saisir un UID/PID marchand valide.';
+$lang['Alipay.!error.signature_key.valid'] = 'Vous devez saisir une clé de signature valide.';
+
