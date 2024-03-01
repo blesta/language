103cd66767eb454e7b9d68c404b6ddc6d669ef18
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:20 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/payment_reminders.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Payment Reminders
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.payment_reminders.attempt'] = 'Προσπάθεια αποστολής υπενθυμίσεων πληρωμής.';
+$lang['Automation.task.payment_reminders.completed'] = 'Η εργασία υπενθυμίσεων πληρωμής έχει ολοκληρωθεί.';
+$lang['Automation.task.payment_reminders.failed'] = 'Αδυναμία αποστολής της ειδοποίησης υπενθύμισης για το τιμολόγιο #%4$s στην επαφή %1$s %2$s από τον πελάτη #%3$s.';
+$lang['Automation.task.payment_reminders.success'] = 'Παραδόθηκε επιτυχώς η ειδοποίηση υπενθύμισης για το τιμολόγιο #%4$s στην επαφή %1$s %2$s από τον πελάτη #%3$s.';
+$lang['Automation.task.payment_reminders.autodebit_failed'] = 'Αδυναμία αποστολής της ειδοποίησης αυτόματης χρέωσης για το τιμολόγιο #%4$s στην επαφή %1$s %2$s από τον πελάτη #%3$s.';
+$lang['Automation.task.payment_reminders.autodebit_success'] = 'Παραδόθηκε επιτυχώς η ειδοποίηση υπενθύμισης αυτόματης χρέωσης για το τιμολόγιο #%4$s στην επαφή %1$s %2$s από τον πελάτη #%3$s.';
+
