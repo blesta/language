9b38425a280f993785ea519366213ce40d2a758c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:18 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/order_plugin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Order Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderPlugin.permission.admin_main_widget'] = 'Widget del profilo del cliente';
+$lang['OrderPlugin.permission.admin_main'] = 'Ordini';
+$lang['OrderPlugin.permission.admin_payment_methods_delete'] = 'Cancellare il metodo di pagamento';
+$lang['OrderPlugin.permission.admin_payment_methods_edit'] = 'Modifica del metodo di pagamento';
+$lang['OrderPlugin.permission.admin_payment_methods_add'] = 'Aggiungi metodo di pagamento';
+$lang['OrderPlugin.permission.admin_payment_methods'] = 'Metodi di pagamento';
+$lang['OrderPlugin.permission.admin_payouts_decline'] = 'Declinare il pagamento';
+$lang['OrderPlugin.permission.admin_payouts_approve'] = 'Approvare il pagamento';
+$lang['OrderPlugin.permission.admin_payouts_edit'] = 'Modifica del pagamento';
+$lang['OrderPlugin.permission.admin_payouts'] = 'Pagamenti';
+$lang['OrderPlugin.permission.admin_affiliates_settings'] = 'Impostazioni dell\'affiliato';
+$lang['OrderPlugin.permission.admin_affiliates_deactivate'] = 'Disattivare l\'affiliazione';
+$lang['OrderPlugin.permission.admin_affiliates_activate'] = 'Attivare l\'affiliazione';
+$lang['OrderPlugin.permission.admin_affiliates_update'] = 'Aggiornamento Affiliato';
+$lang['OrderPlugin.permission.admin_affiliates_add'] = 'Aggiungi Affiliato';
+$lang['OrderPlugin.permission.admin_affiliates'] = 'Affiliati';
+$lang['AdminCompanyPlugins.settings.event.domains.updatepricingafter'] = 'Questo evento viene attivato dopo l\'aggiornamento dei prezzi di un TLD.';
+$lang['AdminCompanyPlugins.settings.event.domains.updatedomainscompanysettingsafter'] = 'Questo evento viene attivato dopo l\'aggiornamento delle impostazioni aziendali di Domain Manager.';
+$lang['AdminCompanyPlugins.settings.event.domains.disable'] = 'Questo evento viene attivato quando un TLD viene disattivato da Domain Manager.';
+$lang['AdminCompanyPlugins.settings.event.domains.enable'] = 'Questo evento viene attivato quando un TLD viene abilitato da Domain Manager.';
+$lang['AdminCompanyPlugins.settings.event.domains.delete'] = 'Questo evento viene attivato quando un TLD viene cancellato da Domain Manager.';
+$lang['OrderPlugin.card_client.orders'] = 'Ordini';
+$lang['OrderPlugin.event_getsearchoptions.orders'] = 'Ricerca ordini';
+$lang['OrderPlugin.cron.process_abandoned_orders_desc'] = 'Inviare e-mail di promemoria, eseguire cancellazioni e disattivare i clienti per gli ordini abbandonati.';
+$lang['OrderPlugin.cron.process_abandoned_orders_name'] = 'Elaborazione degli ordini abbandonati';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_desc'] = 'Valuta se i referral degli affiliati in sospeso hanno raggiunto la scadenza e, in caso affermativo, aggiorna i referral e gli importi dei pagamenti.';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_name'] = 'Ordini degli affiliati';
+$lang['OrderPlugin.cron.affiliate_monthly_report_desc'] = 'Il 1° del mese verrà inviato un rapporto mensile di affiliazione via e-mail per il mese precedente.';
+$lang['OrderPlugin.cron.affiliate_monthly_report_name'] = 'Rapporto mensile degli affiliati';
+$lang['OrderPlugin.cron.accept_paid_orders_desc'] = 'Accetta automaticamente gli ordini pendenti pagati se il modulo d\'ordine lo consente.';
+$lang['OrderPlugin.cron.accept_paid_orders_name'] = 'Accettare gli ordini in sospeso pagati';
+$lang['OrderPlugin.client_orders.name'] = 'Storia dell\'ordine';
+$lang['OrderPlugin.action_staff_client.affiliates'] = 'Gestire l\'affiliazione';
+$lang['OrderPlugin.client_affiliates.name'] = 'Affiliati';
+$lang['OrderPlugin.client.name'] = 'Ordine';
+$lang['OrderPlugin.admin_affiliates.payment_methods'] = 'Metodi di pagamento degli affiliati';
+$lang['OrderPlugin.admin_affiliates.payouts'] = 'Pagamenti degli affiliati';
+$lang['OrderPlugin.admin_affiliates.name'] = 'Affiliati';
+$lang['OrderPlugin.admin_forms.name'] = 'Moduli d\'ordine';
+$lang['OrderPlugin.admin_main.name'] = 'Ordini';
+$lang['OrderPlugin.name'] = 'Sistema di ordinazione';
+
