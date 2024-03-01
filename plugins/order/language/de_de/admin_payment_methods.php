ca16909d5d02dec94600c3e0fc41426004a526ce
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:04 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/admin_payment_methods.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Admin Payment Methods
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminPaymentMethods.edit.field_payment_methodsubmit'] = 'Zahlungsmethode aktualisieren';
+$lang['AdminPaymentMethods.edit.field_name'] = 'Name';
+$lang['AdminPaymentMethods.edit.boxtitle_edit_payment_method'] = 'Partner-Zahlungsmethode bearbeiten';
+$lang['AdminPaymentMethods.add.field_payment_methodsubmit'] = 'Zahlungsmethode hinzufügen';
+$lang['AdminPaymentMethods.add.field_name'] = 'Name';
+$lang['AdminPaymentMethods.add.boxtitle_add_payment_method'] = 'Partner-Zahlungsmethode hinzufügen';
+$lang['AdminPaymentMethods.index.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Zahlungsmethode löschen möchten?';
+$lang['AdminPaymentMethods.index.option_delete'] = 'Löschen';
+$lang['AdminPaymentMethods.index.option_edit'] = 'bearbeiten';
+$lang['AdminPaymentMethods.index.heading_options'] = 'Optionen';
+$lang['AdminPaymentMethods.index.heading_name'] = 'Name';
+$lang['AdminPaymentMethods.index.no_results'] = 'Zur Zeit gibt es keine Zahlungsmöglichkeiten.';
+$lang['AdminPaymentMethods.index.affiliate_payment_method_add'] = 'Partner-Zahlungsmethode hinzufügen';
+$lang['AdminPaymentMethods.index.boxtitle_payment_methods'] = 'Partner-Zahlungsmethoden';
+$lang['AdminPaymentMethods.index.page_title'] = 'Partner-Zahlungsmethoden';
+$lang['AdminPaymentMethods.!success.payment_method_updated'] = 'Die Zahlungsmethode wurde erfolgreich aktualisiert.';
+$lang['AdminPaymentMethods.!success.payment_method_deleted'] = 'Die Zahlungsmethode wurde erfolgreich gelöscht.';
+$lang['AdminPaymentMethods.!success.payment_method_added'] = 'Die Zahlungsmethode wurde erfolgreich hinzugefügt.';
+
