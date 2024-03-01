44c82be5276b7ebe75f4a69f0de82fe35f2f14e3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:29 -0800

    Updated fr_fr language files

---
+++ core/Pricing/Modifier/Type/Description/language/fr_fr/service.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Service
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Service.description.service.item'] = '%1$s - %2$s';
+$lang['Service.description.service.item.date'] = '%1$s - %2$s (%3$s - %4$s)';
+$lang['Service.description.service.item.updated'] = 'Passage de %1$s à %2$s - %3$s';
+$lang['Service.description.service.item.updated.date'] = 'Passage de %1$s à %2$s - %3$s (%4$s - %5$s)';
+$lang['Service.description.service.setup'] = '%1$s - %2$s Frais de mise en place';
+$lang['Service.description.service.cancel'] = '%1$s - %2$s Frais d\'annulation';
+$lang['Service.description.service.item.prorate'] = 'Au prorata %1$s - %2$s (%3$s - %4$s)';
+$lang['Service.description.service.item.updated.prorate'] = 'Modification au prorata de %1$s à %2$s - %3$s (%4$s - %5$s)';
+$lang['Service.description.package.item'] = '%1$s';
+$lang['Service.description.package.item.date'] = '%1$s (%2$s - %3$s)';
+$lang['Service.description.package.item.updated'] = 'Passer de %1$s à %2$s';
+$lang['Service.description.package.item.updated.date'] = 'Changement de %1$s à %2$s (%3$s - %4$s)';
+$lang['Service.description.package.setup'] = 'Frais d\'installation %1$s';
+$lang['Service.description.package.cancel'] = 'Frais d\'annulation %1$s';
+$lang['Service.description.package.item.prorate'] = 'Au prorata %1$s (%2$s - %3$s)';
+$lang['Service.description.package.item.updated.prorate'] = 'Changement au prorata de %1$s à %2$s (%3$s - %4$s)';
+
