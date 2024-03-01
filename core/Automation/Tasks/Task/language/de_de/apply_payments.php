972c778e36e55e19c6fbf9fc2b015a40c163b1e3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:51 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/apply_payments.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Apply Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.apply_payments.apply_none'] = 'Es gibt keine Rechnungen, auf die Gutschriften angewandt werden können.';
+$lang['Automation.task.apply_payments.apply_success'] = 'Ausstehende Gutschriften aus Transaktion %1$s für Kunde #%2$s wurden erfolgreich auf Rechnung #%3$s in Höhe von %4$s angewendet.';
+$lang['Automation.task.apply_payments.apply_failed'] = 'Ausstehende Gutschriften für Kunde #%1$s können nicht angewendet werden.';
+$lang['Automation.task.apply_payments.completed_group'] = 'Die Anwendung von Krediten für die Kundengruppe %1$s wurde abgeschlossen.';
+$lang['Automation.task.apply_payments.attempt_group'] = 'Es wird versucht, Kredite für die Kundengruppe %1$s zu beantragen.';
+$lang['Automation.task.apply_payments.completed'] = 'Die Aufgabe Kredite anwenden ist abgeschlossen.';
+$lang['Automation.task.apply_payments.attempt'] = 'Der Versuch, Gutschriften auf offene Rechnungen anzuwenden.';
+
