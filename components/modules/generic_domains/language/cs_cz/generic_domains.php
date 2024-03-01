ca2fbe64df8a7d6f25c4ec295f24b877b01885fb
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:23 -0800

    Updated cs_cz language files

---
+++ components/modules/generic_domains/language/cs_cz/generic_domains.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Generic Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GenericDomains.manage.text'] = 'Zaměstnanci mohou dostávat BCC kopie e-mailů o vytvoření, pozastavení a zrušení služby, pokud má jejich skupina zaměstnanců nastavena tato BCC oznámení a zaměstnanec je odebírá ve své sekci [Moje informace] -> [Oznámení].';
+$lang['GenericDomains.package_fields.tld_options'] = 'TLD';
+$lang['GenericDomains.transfer.transfer_key'] = 'Kód EPP';
+$lang['GenericDomains.transfer.domain'] = 'Doména';
+$lang['GenericDomains.domain.domain'] = 'Doména';
+$lang['GenericDomains.module_row_plural'] = 'Řádky modulu';
+$lang['GenericDomains.module_row'] = 'Řádek modulu';
+$lang['GenericDomains.description'] = 'Obecný modul pro registraci domén.';
+$lang['GenericDomains.name'] = 'Obecné domény';
+
