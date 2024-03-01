066adff7f6c70d03ae947bba663b77005efb6b53
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:15 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/order_affiliate_referrals.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Order Affiliate Referrals
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliateReferrals.getStatuses.canceled'] = 'Annullato';
+$lang['OrderAffiliateReferrals.getStatuses.mature'] = 'Maturo';
+$lang['OrderAffiliateReferrals.getStatuses.pending'] = 'In attesa';
+$lang['OrderAffiliateReferrals.!error.date_updated.format'] = 'La data aggiornata è in un formato non valido.';
+$lang['OrderAffiliateReferrals.!error.date_added.format'] = 'La data aggiunta è in un formato non valido.';
+$lang['OrderAffiliateReferrals.!error.commission.format'] = 'La commissione di referral deve essere un numero.';
+$lang['OrderAffiliateReferrals.!error.currency.length'] = 'Il codice della valuta deve essere di 3 caratteri.';
+$lang['OrderAffiliateReferrals.!error.amount.format'] = 'L\'importo del rinvio deve essere un numero.';
+$lang['OrderAffiliateReferrals.!error.status.valid'] = 'Selezionare uno stato di referral valido.';
+$lang['OrderAffiliateReferrals.!error.name.length'] = 'Il nome del referral deve essere inferiore a 255 caratteri.';
+$lang['OrderAffiliateReferrals.!error.order_id.exists'] = 'ID ordine non valido.';
+$lang['OrderAffiliateReferrals.!error.affiliate_id.exists'] = 'ID affiliato non valido.';
+$lang['OrderAffiliateReferrals.!error.referral_id.exists'] = 'ID di riferimento non valido.';
+
