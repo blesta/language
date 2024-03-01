a8847b7fc14c1f2f2e9576df8a6f0f4fe8e7583a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:07 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/duitku/language/de_de/duitku.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Duitku
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Duitku.buildprocess.submit'] = 'Bezahlen mit Duitku';
+$lang['Duitku.sandbox'] = 'Sandkasten';
+$lang['Duitku.merchant_code'] = 'Händler-Code';
+$lang['Duitku.merchant_key'] = 'Händlerschlüssel';
+$lang['Duitku.!error.sandbox.valid'] = 'Das Feld Sandbox muss entweder true oder false sein.';
+$lang['Duitku.!error.merchant_code.empty'] = 'Der Merchant Code darf nicht leer sein.';
+$lang['Duitku.!error.merchant_key.valid'] = 'Der angegebene Merchant Key ist nicht gültig.';
+$lang['Duitku.description'] = 'Akzeptieren Sie Kredit- und Debitkarten, E-Wallets, Banküberweisungen und versenden Sie Massenzahlungen über eine einzige Integration in Indonesien.';
+$lang['Duitku.name'] = 'Duitku';
+
