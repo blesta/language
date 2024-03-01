cfde6013158d3b1b363e933fc638a168de3c04c0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:24 -0800

    Updated fr_fr language files

---
+++ components/gateways/merchant/cornerstone/language/fr_fr/cornerstone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cornerstone
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cornerstone.!error.security_key.empty'] = 'Veuillez saisir votre clé de sécurité.';
+$lang['Cornerstone.name'] = 'Pierre angulaire';
+$lang['Cornerstone.description'] = 'Cornerstone est l\'une des principales organisations commerciales indépendantes détenues et gérées par des chrétiens dans le secteur du traitement des transactions commerciales aux États-Unis.';
+$lang['Cornerstone.security_key'] = 'Clé de sécurité';
+
