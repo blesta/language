76978efcd43c4fc910c071ac61cc7e504697ec24
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:57 -0800

    Updated it_it language files

---
+++ core/Automation/Tasks/Task/language/it_it/apply_invoice_late_fees.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Apply Invoice Late Fees
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.apply_invoice_late_fees.late_fee_success'] = 'Applicata con successo la tassa di mora alla fattura #%1$s per il cliente #%2$s.';
+$lang['Automation.task.apply_invoice_late_fees.late_fee_failed'] = 'Impossibile applicare la tassa di mora alla fattura #%1$s per il cliente #%2$s.';
+$lang['Automation.task.apply_invoice_late_fees.invoice_line'] = 'Tassa di mora';
+$lang['Automation.task.apply_invoice_late_fees.completed'] = 'L\'attività Applicare le tasse di mora è stata completata.';
+$lang['Automation.task.apply_invoice_late_fees.attempt'] = 'Tentativi di applicare tasse di mora alle fatture scadute.';
+
