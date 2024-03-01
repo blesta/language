eead47b0c0d333ab99c164dd6275641b9512dd99
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:05 -0800

    Updated el_gr language files

---
+++ components/modules/virtualmin/language/el_gr/virtualmin.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Virtualmin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Virtualmin.!error.virtualmin_password.valid'] = 'Ο κωδικός πρόσβασης πρέπει να έχει μήκος τουλάχιστον 8 χαρακτήρων.';
+$lang['Virtualmin.!error.virtualmin_username.length'] = 'Το όνομα χρήστη πρέπει να έχει μήκος μεταξύ 1 και 16 χαρακτήρων.';
+$lang['Virtualmin.!error.virtualmin_username.test'] = 'Το όνομα χρήστη δεν πρέπει να αρχίζει με "test".';
+$lang['Virtualmin.!error.virtualmin_username.format'] = 'Το όνομα χρήστη μπορεί να περιέχει μόνο γράμματα και αριθμούς και δεν πρέπει να αρχίζει με αριθμό.';
+$lang['Virtualmin.!error.virtualmin_domain.test'] = 'Το όνομα τομέα δεν μπορεί να αρχίζει με \'test\'.';
+$lang['Virtualmin.!error.virtualmin_domain.format'] = 'Παρακαλούμε εισάγετε ένα έγκυρο όνομα τομέα, π.χ. domain.com.';
+$lang['Virtualmin.!error.module_row.missing'] = 'Εμφανίστηκε ένα εσωτερικό σφάλμα. Η σειρά της μονάδας δεν είναι διαθέσιμη.';
+$lang['Virtualmin.!error.api.internal'] = 'Προέκυψε ένα εσωτερικό σφάλμα ή ο διακομιστής δεν απάντησε στην αίτηση.';
+$lang['Virtualmin.!error.meta[template].empty'] = 'Απαιτείται ένα πρότυπο VirtualMin.';
+$lang['Virtualmin.!error.meta[plan].empty'] = 'Απαιτείται ένα σχέδιο VirtualMin.';
+$lang['Virtualmin.!error.account_limit_valid'] = 'Παρακαλώ εισάγετε ένα έγκυρο όριο λογαριασμού.';
+$lang['Virtualmin.!error.password_valid_connection'] = 'Δεν κατέστη δυνατή η σύνδεση με τον διακομιστή. Ελέγξτε αν το όνομα κεντρικού υπολογιστή, το όνομα χρήστη, ο κωδικός πρόσβασης και η θύρα είναι σωστά.';
+$lang['Virtualmin.!error.password_valid'] = 'Ο κωδικός πρόσβασης φαίνεται να είναι άκυρος.';
+$lang['Virtualmin.!error.port_valid'] = 'Η θύρα φαίνεται να είναι άκυρη.';
+$lang['Virtualmin.!error.user_name_valid'] = 'Το όνομα χρήστη φαίνεται να είναι άκυρο.';
+$lang['Virtualmin.!error.host_name_valid'] = 'Το όνομα κεντρικού υπολογιστή φαίνεται να είναι άκυρο.';
+$lang['Virtualmin.!error.server_name_valid'] = 'Πρέπει να εισαγάγετε μια ετικέτα διακομιστή.';
+$lang['Virtualmin.service_field.tooltip.password'] = 'Μπορείτε να αφήσετε τον κωδικό πρόσβασης κενό για να δημιουργηθεί αυτόματα.';
+$lang['Virtualmin.service_field.tooltip.username'] = 'Μπορείτε να αφήσετε το όνομα χρήστη κενό για να δημιουργηθεί αυτόματα.';
+$lang['Virtualmin.service_info.option_login'] = 'Συνδεθείτε';
+$lang['Virtualmin.service_info.options'] = 'Επιλογές';
+$lang['Virtualmin.service_info.server'] = 'Διακομιστής';
+$lang['Virtualmin.service_info.password'] = 'Κωδικός πρόσβασης';
+$lang['Virtualmin.service_info.username'] = 'Όνομα χρήστη';
+$lang['Virtualmin.tab_stats.info.maximum_mailboxes'] = 'Μέγιστο αριθμό γραμματοκιβωτίων';
+$lang['Virtualmin.tab_stats.info.maximum_databases'] = 'Μέγιστες βάσεις δεδομένων';
+$lang['Virtualmin.tab_stats.info.databases_count'] = 'Αριθμός βάσεων δεδομένων';
+$lang['Virtualmin.tab_stats.info.databases_size'] = 'Μέγεθος βάσης δεδομένων';
+$lang['Virtualmin.tab_stats.info.disk_used'] = 'Χρησιμοποιημένος δίσκος';
+$lang['Virtualmin.tab_stats.info_heading.value'] = 'Αξία';
+$lang['Virtualmin.tab_stats.info_heading.field'] = 'Πεδίο';
+$lang['Virtualmin.tab_stats.info_title'] = 'Πληροφορίες';
+$lang['Virtualmin.tab_client_actions.field_password_submit'] = 'Ενημέρωση κωδικού πρόσβασης';
+$lang['Virtualmin.tab_client_actions.field_virtualmin_confirm_password'] = 'Επιβεβαίωση κωδικού πρόσβασης';
+$lang['Virtualmin.tab_client_actions.field_virtualmin_password'] = 'Κωδικός πρόσβασης';
+$lang['Virtualmin.tab_client_actions.change_password'] = 'Αλλαγή κωδικού πρόσβασης';
+$lang['Virtualmin.service_field.confirm_password'] = 'Επιβεβαίωση κωδικού πρόσβασης';
+$lang['Virtualmin.service_field.password'] = 'Κωδικός πρόσβασης';
+$lang['Virtualmin.service_field.username'] = 'Όνομα χρήστη';
+$lang['Virtualmin.service_field.domain'] = 'Τομέας';
+$lang['Virtualmin.package_fields.template'] = 'Πρότυπο VirtualMin';
+$lang['Virtualmin.package_fields.plan'] = 'Σχέδιο VirtualMin';
+$lang['Virtualmin.row_meta.account_limit'] = 'Όριο λογαριασμού';
+$lang['Virtualmin.row_meta.use_ssl'] = 'Χρησιμοποιήστε SSL κατά τη σύνδεση στο API (συνιστάται)';
+$lang['Virtualmin.row_meta.port'] = 'Λιμάνι';
+$lang['Virtualmin.row_meta.password'] = 'Κωδικός πρόσβασης';
+$lang['Virtualmin.row_meta.user_name'] = 'Όνομα χρήστη';
+$lang['Virtualmin.row_meta.host_name'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Virtualmin.row_meta.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Virtualmin.edit_row.add_btn'] = 'Επεξεργασία διακομιστή';
+$lang['Virtualmin.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Virtualmin.edit_row.box_title'] = 'Επεξεργασία διακομιστή VirtualMin';
+$lang['Virtualmin.add_row.add_btn'] = 'Προσθήκη διακομιστή';
+$lang['Virtualmin.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Virtualmin.add_row.box_title'] = 'Προσθήκη διακομιστή VirtualMin';
+$lang['Virtualmin.order_options.first'] = 'Πρώτος μη πλήρης διακομιστής';
+$lang['Virtualmin.manage.module_groups_no_results'] = 'Δεν υπάρχουν ομάδες διακομιστών.';
+$lang['Virtualmin.manage.module_rows_no_results'] = 'Δεν υπάρχουν διακομιστές.';
+$lang['Virtualmin.manage.module_groups.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την ομάδα διακομιστών;';
+$lang['Virtualmin.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον διακομιστή;';
+$lang['Virtualmin.manage.module_groups.delete'] = 'Διαγραφή';
+$lang['Virtualmin.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['Virtualmin.manage.module_groups.edit'] = 'Επεξεργασία';
+$lang['Virtualmin.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['Virtualmin.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Virtualmin.manage.module_groups_heading.options'] = 'Επιλογές';
+$lang['Virtualmin.manage.module_groups_heading.servers'] = 'Αριθμός διακομιστών';
+$lang['Virtualmin.manage.module_groups_heading.name'] = 'Όνομα ομάδας';
+$lang['Virtualmin.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['Virtualmin.manage.module_rows_heading.accounts'] = 'Λογαριασμοί';
+$lang['Virtualmin.manage.module_rows_heading.port'] = 'Λιμάνι';
+$lang['Virtualmin.manage.module_rows_heading.hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Virtualmin.manage.module_rows_heading.name'] = 'Ετικέτα διακομιστή';
+$lang['Virtualmin.manage.module_groups_title'] = 'Ομάδες διακομιστών';
+$lang['Virtualmin.manage.module_rows_title'] = 'Servers';
+$lang['Virtualmin.add_module_group'] = 'Προσθήκη ομάδας διακομιστών';
+$lang['Virtualmin.add_module_row'] = 'Προσθήκη διακομιστή';
+$lang['Virtualmin.tab_client_actions'] = 'Δράσεις';
+$lang['Virtualmin.tab_stats'] = 'Στατιστικά στοιχεία';
+$lang['Virtualmin.module_group'] = 'Ομάδα διακομιστών';
+$lang['Virtualmin.module_row_plural'] = 'Servers';
+$lang['Virtualmin.module_row'] = 'Διακομιστής';
+$lang['Virtualmin.description'] = 'Το Virtualmin είναι ένας πίνακας ελέγχου για τη φιλοξενία domains και ιστοσελίδων, ο οποίος δίνει τη δυνατότητα δημιουργίας και διαχείρισης πολλών domains και διατίθεται τόσο σε εκδόσεις ανοιχτού κώδικα όσο και σε εμπορικές εκδόσεις. Βασίζεται στο Webmin.';
+$lang['Virtualmin.name'] = 'VirtualMin';
+
