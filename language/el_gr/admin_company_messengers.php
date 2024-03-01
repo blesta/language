83c92e27b98f5baa537125ad206995b9867c44b3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:47 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_company_messengers.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Admin Company Messengers
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyMessengers.edittemplate.field.edittemplatesubmit'] = 'Πρότυπο ενημέρωσης';
+$lang['AdminCompanyMessengers.edittemplate.field.cancel'] = 'Ακύρωση';
+$lang['AdminCompanyMessengers.edittemplate.field.status'] = 'Ενεργοποιημένο';
+$lang['AdminCompanyMessengers.edittemplate.field.tags'] = 'Διαθέσιμες ετικέτες';
+$lang['AdminCompanyMessengers.edittemplate.text_none'] = 'Κανένα';
+$lang['AdminCompanyMessengers.edittemplate.boxtitle_edittemplate'] = 'Επεξεργασία προτύπου μηνύματος %1$s';
+$lang['AdminCompanyMessengers.edittemplate.page_title'] = 'Ρυθμίσεις > Εταιρεία > Emails > Επεξεργασία προτύπου μηνύματος';
+$lang['AdminCompanyMessengers.templates.no_results'] = 'Δεν υπάρχουν πρότυπα αυτού του τύπου.';
+$lang['AdminCompanyMessengers.templates.option_edit'] = 'Επεξεργασία';
+$lang['AdminCompanyMessengers.templates.staff_quotation_approved_desc'] = 'Ειδοποίηση που αποστέλλεται μετά την έγκριση μιας προσφοράς από τον πελάτη.';
+$lang['AdminCompanyMessengers.templates.staff_quotation_approved_name'] = 'Έγκριση απόσπασμα';
+$lang['AdminCompanyMessengers.templates.quotation_delivery_desc'] = 'Ανακοίνωση που περιέχει αριθμό και σύνδεσμο.';
+$lang['AdminCompanyMessengers.templates.quotation_delivery_name'] = 'Παράδοση προσφοράς';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_paid_desc'] = 'Ειδοποίηση που περιέχει το συνολικό ποσό ενός εξοφλημένου τιμολογίου.';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_paid_name'] = 'Παράδοση τιμολογίου (πληρωμένο)';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_unpaid_desc'] = 'Ειδοποίηση που περιέχει το συνολικό ποσό ενός απλήρωτου τιμολογίου.';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_unpaid_name'] = 'Παράδοση τιμολογίου (απλήρωτο)';
+$lang['AdminCompanyMessengers.templates.service_unsuspension_desc'] = 'Ειδοποίηση αναστολής υπηρεσίας, αποστέλλεται όταν μια υπηρεσία ανασταλεί αυτόματα.';
+$lang['AdminCompanyMessengers.templates.service_unsuspension_name'] = 'Μη αναστολή υπηρεσίας';
+$lang['AdminCompanyMessengers.templates.service_suspension_desc'] = 'Ειδοποίηση αναστολής υπηρεσίας, αποστέλλεται όταν μια υπηρεσία ανασταλεί αυτόματα.';
+$lang['AdminCompanyMessengers.templates.service_suspension_name'] = 'Αναστολή υπηρεσιών';
+$lang['AdminCompanyMessengers.templates.service_scheduled_cancellation_desc'] = 'Προγραμματισμένη ειδοποίηση ακύρωσης υπηρεσίας, αποστέλλεται όταν μια υπηρεσία είναι προγραμματισμένη για ακύρωση.';
+$lang['AdminCompanyMessengers.templates.service_scheduled_cancellation_name'] = 'Προγραμματισμένη ακύρωση υπηρεσίας';
+$lang['AdminCompanyMessengers.templates.service_creation_desc'] = 'Ειδοποίηση δημιουργίας υπηρεσίας, αποστέλλεται όταν δημιουργείται μια υπηρεσία.';
+$lang['AdminCompanyMessengers.templates.service_creation_name'] = 'Δημιουργία υπηρεσιών';
+$lang['AdminCompanyMessengers.templates.invoice_notice_third_desc'] = 'Τρίτη ειδοποίηση τιμολογίου, είτε υπενθύμιση πληρωμής είτε ειδοποίηση καθυστέρησης.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_third_name'] = 'Ειδοποίηση τιμολογίου (3ο)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_second_desc'] = 'Δεύτερη ειδοποίηση τιμολογίου, είτε υπενθύμιση πληρωμής είτε ειδοποίηση καθυστέρησης.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_second_name'] = 'Ειδοποίηση τιμολογίου (2η)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_first_desc'] = 'Πρώτη ειδοποίηση τιμολογίου, είτε υπενθύμιση πληρωμής είτε ειδοποίηση καθυστέρησης.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_first_name'] = 'Ειδοποίηση τιμολογίου (1ο)';
+$lang['AdminCompanyMessengers.templates.auto_debit_pending_desc'] = 'Αποστολή ειδοποίησης που υποδεικνύει ότι σύντομα θα επιχειρηθεί αυτόματη πληρωμή.';
+$lang['AdminCompanyMessengers.templates.auto_debit_pending_name'] = 'Αυτόματη χρέωση σε εκκρεμότητα';
+$lang['AdminCompanyMessengers.templates.account_welcome_desc'] = 'Αποστολή ειδοποίησης καλωσορίσματος για εγγραφές νέων λογαριασμών.';
+$lang['AdminCompanyMessengers.templates.account_welcome_name'] = 'Εγγραφή λογαριασμού';
+$lang['AdminCompanyMessengers.templates.text_options'] = 'Επιλογές';
+$lang['AdminCompanyMessengers.templates.text_description'] = 'Περιγραφή';
+$lang['AdminCompanyMessengers.templates.text_plugin'] = 'Plugin';
+$lang['AdminCompanyMessengers.templates.text_name'] = 'Όνομα';
+$lang['AdminCompanyMessengers.templates.heading_plugins'] = 'Μηνύματα Plugin';
+$lang['AdminCompanyMessengers.templates.heading_staff'] = 'Μηνύματα προσωπικού';
+$lang['AdminCompanyMessengers.templates.heading_client'] = 'Μηνύματα πελατών';
+$lang['AdminCompanyMessengers.templates.boxtitle_message_templates'] = 'Πρότυπα μηνυμάτων';
+$lang['AdminCompanyMessengers.templates.page_title'] = 'Ρυθμίσεις > Εταιρεία > Messengers > Πρότυπα μηνυμάτων';
+$lang['AdminCompanyMessengers.configuration.text_none'] = 'Δεν υπάρχουν εγκατεστημένοι αγγελιοφόροι.';
+$lang['AdminCompanyMessengers.configuration.field_update'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminCompanyMessengers.configuration.heading_messenger'] = 'Αγγελιοφόρος';
+$lang['AdminCompanyMessengers.configuration.heading_message_type'] = 'Τύπος μηνύματος';
+$lang['AdminCompanyMessengers.configuration.boxtitle_messenger_configuration'] = 'Διαμόρφωση Messenger';
+$lang['AdminCompanyMessengers.configuration.page_title'] = 'Ρυθμίσεις > Εταιρεία > Messengers > Διαμόρφωση Messenger';
+$lang['AdminCompanyMessengers.manage.field_update'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminCompanyMessengers.manage.none_text'] = 'Δεν υπάρχει τίποτα να διαχειριστεί αυτός ο αγγελιοφόρος.';
+$lang['AdminCompanyMessengers.manage.boxtitle_manage'] = 'Διαχείριση %1$s';
+$lang['AdminCompanyMessengers.manage.page_title'] = 'Ρυθμίσεις > Εταιρεία > Messengers > Διαχείριση %1$s';
+$lang['AdminCompanyMessengers.installed.confirm_uninstall'] = 'Είστε σίγουροι ότι θέλετε να απεγκαταστήσετε αυτόν τον αγγελιοφόρο;';
+$lang['AdminCompanyMessengers.installed.btn_upgrade'] = 'Αναβάθμιση';
+$lang['AdminCompanyMessengers.installed.btn_manage'] = 'Διαχείριση';
+$lang['AdminCompanyMessengers.installed.btn_uninstall'] = 'Απεγκατάσταση του';
+$lang['AdminCompanyMessengers.installed.text_none'] = 'Δεν υπάρχουν εγκατεστημένοι αγγελιοφόροι.';
+$lang['AdminCompanyMessengers.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyMessengers.installed.text_author'] = 'Συγγραφέας:';
+$lang['AdminCompanyMessengers.installed.boxtitle_installed'] = 'Αγγελιοφόροι';
+$lang['AdminCompanyMessengers.installed.page_title'] = 'Ρυθμίσεις > Εταιρεία > Messengers > Εγκατεστημένα';
+$lang['AdminCompanyMessengers.available.btn_install'] = 'Εγκαταστήστε το';
+$lang['AdminCompanyMessengers.available.text_none'] = 'Δεν υπάρχουν διαθέσιμοι αγγελιοφόροι.';
+$lang['AdminCompanyMessengers.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyMessengers.available.text_author'] = 'Συγγραφέας:';
+$lang['AdminCompanyMessengers.available.boxtitle_messenger'] = 'Αγγελιοφόροι';
+$lang['AdminCompanyMessengers.available.page_title'] = 'Ρυθμίσεις > Εταιρεία > Messengers > Διαθέσιμο';
+$lang['AdminCompanyMessengers.!tab.available'] = 'Διαθέσιμο';
+$lang['AdminCompanyMessengers.!tab.installed'] = 'Εγκατεστημένο';
+$lang['AdminCompanyMessengers.!tooltip.status'] = 'Δεν θα αποστέλλονται μηνύματα χρησιμοποιώντας αυτό το πρότυπο, εκτός εάν αυτή η επιλογή είναι ενεργοποιημένη.';
+$lang['AdminCompanyMessengers.!success.messenger_updated'] = 'Ο αγγελιοφόρος ενημερώθηκε επιτυχώς.';
+$lang['AdminCompanyMessengers.!success.edit_template_updated'] = 'Το πρότυπο μηνύματος ενημερώθηκε επιτυχώς.';
+$lang['AdminCompanyMessengers.!success.messenger_options_updated'] = 'Η διαμόρφωση του messenger ενημερώθηκε με επιτυχία!';
+$lang['AdminCompanyMessengers.!success.uninstalled'] = 'Ο αγγελιοφόρος απεγκαταστάθηκε επιτυχώς.';
+$lang['AdminCompanyMessengers.!success.installed'] = 'Ο αγγελιοφόρος εγκαταστάθηκε με επιτυχία.';
+$lang['AdminCompanyMessengers.!success.upgraded'] = 'Ο αγγελιοφόρος αναβαθμίστηκε επιτυχώς.';
+
