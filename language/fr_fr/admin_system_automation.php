7883f3e10ff1ef1243cd53675a368c6b924b6088
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:59 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_system_automation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin System Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemAutomation.!error.empty_cron_key'] = 'Veuillez définir une clé cron.';
+$lang['AdminSystemAutomation.!success.automation_updated'] = 'Les paramètres d\'automatisation ont été mis à jour avec succès !';
+$lang['AdminSystemAutomation.!success.cron_key'] = 'La clé cron a été mise à jour avec succès !';
+$lang['AdminSystemAutomation.index.page_title'] = 'Réglages > Système > Automatisation';
+$lang['AdminSystemAutomation.index.boxtitle_automation'] = 'Automatisation';
+$lang['AdminSystemAutomation.index.heading_status'] = 'Statut de Cron';
+$lang['AdminSystemAutomation.index.field_croncommand'] = 'Exemple de commande Cron';
+$lang['AdminSystemAutomation.index.field_runcron'] = 'Exécution manuelle de Cron';
+$lang['AdminSystemAutomation.index.field_cron_key'] = 'Clé Cron';
+$lang['AdminSystemAutomation.index.field_cronkey_submit'] = 'Mise à jour de la clé Cron';
+$lang['AdminSystemAutomation.index.text_cron_last_ran'] = 'Le cron a été exécuté pour la dernière fois le %1$s.';
+$lang['AdminSystemAutomation.index.text_cron_never_ran'] = 'Le cron n\'a jamais été exécuté.';
+$lang['AdminSystemAutomation.index.text_cron_currently_running'] = 'Le cron est en cours d\'exécution.';
+$lang['AdminSystemAutomation.index.text_update_key'] = 'Mise à jour de la clé Cron';
+$lang['AdminSystemAutomation.index.text_generate_code'] = 'Générer le code';
+$lang['AdminSystemAutomation.index.note_cron_command'] = 'Ceci est un exemple de commande cron qui peut être utilisé pour créer une tâche cron sur votre serveur. Lors de la mise en place de la tâche cron, assurez-vous de mettre à jour la commande cron pour qu\'elle pointe vers l\'endroit où PHP est installé si cela diffère de ce qui est montré dans cet exemple.';
+
