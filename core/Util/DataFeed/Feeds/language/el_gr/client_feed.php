e8d156ae7450d9400a8094617bb2a417a0dff463
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:29 -0800

    Updated el_gr language files

---
+++ core/Util/DataFeed/Feeds/language/el_gr/client_feed.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Client Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientFeed.name'] = 'Πελάτες';
+$lang['ClientFeed.description'] = 'Επιστρέφει τον αριθμό των πελατών μιας συγκεκριμένης κατάστασης.';
+$lang['ClientFeed.getOptionFields.title_row_example_code'] = 'Παράδειγμα κώδικα';
+$lang['ClientFeed.getOptionFields.example_code_active'] = 'Εμφάνιση του αριθμού των ενεργών πελατών:';
+$lang['ClientFeed.getOptionFields.example_code_inactive'] = 'Εμφάνιση του αριθμού των ανενεργών πελατών:';
+$lang['ClientFeed.getOptionFields.example_code_fraud'] = 'Εμφάνιση του αριθμού των απατηλών πελατών:';
+$lang['ClientFeed.!error.invalid_endpoint'] = 'Το ζητούμενο τελικό σημείο δεν είναι έγκυρο ή δεν υπάρχει.';
+
