94e107592d1ff65b3328c18432e1699ba497f281
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:17 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/accounts.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Accounts.getCcTypes.other'] = 'Další';
+$lang['Accounts.getCcTypes.visa'] = 'Visa';
+$lang['Accounts.getCcTypes.switch'] = 'Přepínač';
+$lang['Accounts.getCcTypes.solo'] = 'Sólo';
+$lang['Accounts.getCcTypes.mc'] = 'Master Card';
+$lang['Accounts.getCcTypes.maes'] = 'Maestro';
+$lang['Accounts.getCcTypes.lasr'] = 'Laser';
+$lang['Accounts.getCcTypes.jcb'] = 'Japonská úvěrová kancelář';
+$lang['Accounts.getCcTypes.ipi'] = 'InstaPayment';
+$lang['Accounts.getCcTypes.disc'] = 'Objevte';
+$lang['Accounts.getCcTypes.dc-uc'] = 'Diners Club USA a Kanada';
+$lang['Accounts.getCcTypes.dc-int'] = 'Diners Club International';
+$lang['Accounts.getCcTypes.dc-er'] = 'Diners Club EnRoute';
+$lang['Accounts.getCcTypes.dc-cb'] = 'Diners Club Carte Blanche';
+$lang['Accounts.getCcTypes.cup'] = 'Čína Union Pay';
+$lang['Accounts.getCcTypes.bc'] = 'Bankovní karta';
+$lang['Accounts.getCcTypes.amex'] = 'American Express';
+$lang['Accounts.getAchTypes.savings'] = 'Úspory';
+$lang['Accounts.getAchTypes.checking'] = 'Kontrola';
+$lang['Accounts.getTypes.other'] = 'Další';
+$lang['Accounts.getTypes.ach'] = 'Automatizované zúčtovací středisko';
+$lang['Accounts.getTypes.cc'] = 'Kreditní karta';
+$lang['Accounts.!error.cc_account_id.exists'] = 'Neplatné ID účtu.';
+$lang['Accounts.!error.type.cc_format'] = 'Typ karty je neplatný nebo jej nebylo možné určit.';
+$lang['Accounts.!error.last4.empty_cc'] = 'Zadejte prosím poslední čtyři číslice čísla kreditní karty.';
+$lang['Accounts.!error.expiration.valid'] = 'Datum vypršení platnosti již vypršelo.';
+$lang['Accounts.!error.expiration.empty'] = 'Zadejte prosím datum vypršení platnosti kreditní karty.';
+$lang['Accounts.!error.number.valid'] = 'Uvedené číslo karty je neplatné.';
+$lang['Accounts.!error.ach_account_id.exists'] = 'Neplatné ID účtu.';
+$lang['Accounts.!error.type.ach_format'] = 'Nesprávný typ.';
+$lang['Accounts.!error.last4.empty_ach'] = 'Zadejte poslední čtyři číslice čísla účtu.';
+$lang['Accounts.!error.routing.empty'] = 'Uveďte prosím směrovací číslo.';
+$lang['Accounts.!error.account.length'] = 'Uveďte prosím číslo účtu.';
+$lang['Accounts.!error.currency.format'] = 'Měna je neplatná.';
+$lang['Accounts.!error.state.country_exists'] = 'Vyberte zemi, která odpovídá vybranému státu.';
+$lang['Accounts.!error.country.length'] = 'Délka země nesmí překročit 3 znaky.';
+$lang['Accounts.!error.state.length'] = 'Délka státu nesmí překročit 3 znaky.';
+$lang['Accounts.!error.last_name.empty'] = 'Uveďte prosím příjmení.';
+$lang['Accounts.!error.first_name.empty'] = 'Uveďte prosím křestní jméno.';
+$lang['Accounts.!error.contact_id.exists'] = 'Neplatné ID kontaktu.';
+
