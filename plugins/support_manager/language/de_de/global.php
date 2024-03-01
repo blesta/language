25530354a316b27410ff8d82f5b643b0a16193f4
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:43 -0800

    Updated de_de language files

---
+++ plugins/support_manager/language/de_de/global.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Global
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Global.action.update_status'] = 'Status aktualisieren';
+$lang['Global.action.reassign'] = 'Neu zuordnen';
+$lang['Global.action.merge'] = 'Zusammenführen';
+$lang['Global.action.delete'] = 'Löschen';
+$lang['Global.action.split'] = 'Teilen';
+$lang['Global.action.quote'] = 'Zitat';
+$lang['Global.time_since.minute'] = '%1$s min';
+$lang['Global.time_since.hour'] = '%1$s Stunde';
+$lang['Global.time_since.day'] = '%1$s d';
+$lang['Global.select.never'] = '-- Niemals --';
+
