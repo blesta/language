da3e0d5152fa3affb8863c8958bcbc24ca9275ae
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:12 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/square/language/cs_cz/square.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Square
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Square.!error.location_id.valid'] = 'Musíte zadat platné ID umístění.';
+$lang['Square.!error.access_token.valid'] = 'Musíte zadat platný přístupový token.';
+$lang['Square.!error.application_id.valid'] = 'Musíte zadat platné ID aplikace.';
+$lang['Square.buildprocess.submit'] = 'Plaťte pomocí služby Square';
+$lang['Square.location_id'] = 'ID lokality';
+$lang['Square.access_token'] = 'Přístupový token';
+$lang['Square.application_id'] = 'ID aplikace';
+$lang['Square.description'] = 'Square, Inc. je společnost poskytující finanční služby, agregátor obchodních služeb a mobilní platby se sídlem v San Franciscu v Kalifornii. Má silnou spotřebitelskou základnu v malých podnicích';
+$lang['Square.name'] = 'Čtverec';
+
