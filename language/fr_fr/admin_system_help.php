2d9bd7d5e0092584a3a06e21262ac2100423929a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:01 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_system_help.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Admin System Help
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemHelp.index.page_title'] = 'Réglages > Système > Aide > Ressources';
+$lang['AdminSystemHelp.boxtitle_help'] = 'Ressources';
+$lang['AdminSystemHelp.index.heading_user_manual'] = 'Manuel de l\'utilisateur';
+$lang['AdminSystemHelp.index.text_user_manual'] = 'Vous vous demandez comment configurer Blesta ou comment profiter des fonctions les plus avancées ? Consultez le manuel d\'utilisation.';
+$lang['AdminSystemHelp.index.heading_dev_manual'] = 'Manuel du développeur';
+$lang['AdminSystemHelp.index.text_dev_manual'] = 'Adapté aux besoins des développeurs, le manuel du développeur contient tout ce qu\'il faut savoir sur le développement pour la plateforme Blesta.';
+$lang['AdminSystemHelp.index.heading_forums'] = 'Forums communautaires';
+$lang['AdminSystemHelp.index.text_forums'] = 'Les forums de Blesta sont un centre d\'assistance communautaire, où les utilisateurs soumettent des questions et des réponses allant du plus simple au plus complexe.';
+$lang['AdminSystemHelp.index.heading_discord'] = 'Discord Chat';
+$lang['AdminSystemHelp.index.text_discord'] = 'Rejoignez-nous sur Discord pour discuter et passer du temps avec d\'autres membres de la communauté.';
+
