db7c6a5452ab2d3f4d500e2d1f574c2476845183
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:24 -0800

    Updated de_de language files

---
+++ plugins/auto_cancel/language/de_de/admin_manage_plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Admin Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminManagePlugin.getDays.text_days'] = '%1$s Tage';
+$lang['AdminManagePlugin.getDays.text_day'] = '%1$s Tag';
+$lang['AdminManagePlugin.getDays.never'] = 'Niemals';
+$lang['AdminManagePlugin.index.field.submit'] = 'Speichern Sie';
+$lang['AdminManagePlugin.index.field.cancel_days'] = 'Stornierung von Diensten Tage nach der Suspendierung';
+$lang['AdminManagePlugin.index.field.schedule_days'] = 'Zeitplan Stornierung Tage nach der Suspendierung';
+$lang['AdminManagePlugin.index.heading_settings'] = 'Einstellungen';
+$lang['AdminManagePlugin.index.boxtitle_auto_cancel'] = 'Automatisch abbrechen';
+$lang['AdminManagePlugin.index.page_title'] = 'Automatisch abbrechen';
+$lang['AdminManagePlugin.!success.settings_saved'] = 'Die Einstellungen sind gespeichert worden.';
+
