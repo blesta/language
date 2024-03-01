b8837de5e8c0dad8e826499d9e39203b0f8e2cce
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:21 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/signup.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Signup
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Signup.index.field_recovery_email'] = 'Email di recupero (opzionale)';
+$lang['Signup.!error.email.blacklist'] = 'Il vostro ordine non può essere elaborato in questo momento.';
+$lang['Signup.index.signup_btn'] = 'Creare un account';
+$lang['Signup.index.continue_btn'] = 'Proseguire alla cassa';
+$lang['Signup.index.login_btn'] = 'Accedi';
+$lang['Signup.index.field_receive_email_marketing'] = 'Desidero ricevere e-mail su nuovi prodotti, servizi e offerte.';
+$lang['Signup.index.field_confirmpassword'] = 'Conferma la password';
+$lang['Signup.index.field_newpassword'] = 'Password';
+$lang['Signup.index.field_password'] = 'Password';
+$lang['Signup.index.field_username'] = 'Nome utente';
+$lang['Signup.index.field_action_signup'] = 'Creare un account';
+$lang['Signup.index.field_action_login'] = 'Accedi';
+$lang['Signup.index.field_action_continue'] = 'Continua come %1$s %2$s';
+$lang['Signup.index.field_username_type_username'] = 'Specificare un nome utente';
+$lang['Signup.index.field_username_type_email'] = 'Utilizzare l\'e-mail come nome utente';
+$lang['Signup.index.field_email'] = 'Email';
+$lang['Signup.index.field_fax'] = 'Fax';
+$lang['Signup.index.field_phone'] = 'Telefono';
+$lang['Signup.index.field_title'] = 'Titolo';
+$lang['Signup.index.field_country'] = 'Paese';
+$lang['Signup.index.field_zip'] = 'Codice postale';
+$lang['Signup.index.field_state'] = 'Stato/Provincia';
+$lang['Signup.index.field_city'] = 'Città';
+$lang['Signup.index.field_address2'] = 'Indirizzo 2';
+$lang['Signup.index.field_address1'] = 'Indirizzo 1';
+$lang['Signup.index.field_company'] = 'Azienda/Organismo.';
+$lang['Signup.index.field_lastname'] = 'Cognome';
+$lang['Signup.index.field_firstname'] = 'Nome';
+$lang['Signup.index.field_taxid'] = 'Codice fiscale/VATIN';
+$lang['Signup.!notice.email_verification'] = 'È stato inviato un messaggio per verificare il vostro indirizzo e-mail. La preghiamo di completare la verifica al più presto.';
+$lang['Signup.!notice.force_email_usernames'] = 'Il nome utente sarà lo stesso del vostro indirizzo e-mail.';
+$lang['Signup.!error.not_client_owner'] = 'Per continuare, è necessario aver effettuato l\'accesso come proprietario del cliente.';
+$lang['Signup.!error.captcha.invalid'] = 'Il captcha inserito non è valido. Si prega di riprovare.';
+
