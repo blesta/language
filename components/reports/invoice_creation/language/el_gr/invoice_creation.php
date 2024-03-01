ccba8e9bb31dfda8c2eefd62071c97b243480cbe
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:09 -0800

    Updated el_gr language files

---
+++ components/reports/invoice_creation/language/el_gr/invoice_creation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Invoice Creation
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['InvoiceCreation.heading.date_closed'] = 'Ημερομηνία κλεισίματος';
+$lang['InvoiceCreation.heading.date_due'] = 'Ημερομηνία λήξης';
+$lang['InvoiceCreation.heading.date_billed'] = 'Ημερομηνία χρέωσης';
+$lang['InvoiceCreation.heading.status'] = 'Κατάσταση';
+$lang['InvoiceCreation.heading.currency'] = 'Νόμισμα';
+$lang['InvoiceCreation.heading.paid'] = 'Πληρωμένο';
+$lang['InvoiceCreation.heading.total'] = 'Σύνολο';
+$lang['InvoiceCreation.heading.subtotal'] = 'Υποσύνολο';
+$lang['InvoiceCreation.heading.client_id_code'] = 'Πελάτης #';
+$lang['InvoiceCreation.heading.id_code'] = 'Τιμολόγιο #';
+$lang['InvoiceCreation.option.any'] = 'Οποιοδήποτε';
+$lang['InvoiceCreation.options.field_status'] = 'Κατάσταση';
+$lang['InvoiceCreation.options.field_end_date'] = 'Ημερομηνία λήξης';
+$lang['InvoiceCreation.options.field_start_date'] = 'Ημερομηνία έναρξης';
+$lang['InvoiceCreation.name'] = 'Δημιουργία τιμολογίου';
+
