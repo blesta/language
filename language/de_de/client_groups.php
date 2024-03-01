4e053eb66eab2855dd3697c265fd53c95fa02865
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:45 -0800

    Updated de_de language files

---
+++ language/de_de/client_groups.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Client Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientGroups.!error.clients_format.format'] = 'Das Format der Client-ID muss {num} enthalten.';
+$lang['ClientGroups.!error.group_id.exists'] = 'Ungültige Kundengruppen-ID.';
+$lang['ClientGroups.!error.color.length'] = 'Die Länge der Farbe darf 16 Zeichen nicht überschreiten.';
+$lang['ClientGroups.!error.company_id.exists'] = 'Ungültige Unternehmens-ID.';
+$lang['ClientGroups.!error.name.empty'] = 'Bitte geben Sie einen Gruppennamen an.';
+
