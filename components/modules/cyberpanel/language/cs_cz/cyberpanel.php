4208813d0fe6b49c7d3158982f5df43cb4455eec
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:20 -0800

    Updated cs_cz language files

---
+++ components/modules/cyberpanel/language/cs_cz/cyberpanel.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Cyberpanel
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cyberpanel.!error.cyberpanel_password.matches'] = 'Heslo a potvrzovací heslo se neshodují.';
+$lang['Cyberpanel.!error.cyberpanel_password.valid'] = 'Heslo musí mít alespoň 8 znaků.';
+$lang['Cyberpanel.!error.cyberpanel_username.length'] = 'Uživatelské jméno musí mít délku 1 až 16 znaků.';
+$lang['Cyberpanel.!error.cyberpanel_username.test'] = 'Uživatelské jméno nesmí začínat slovem "test".';
+$lang['Cyberpanel.!error.cyberpanel_username.format'] = 'Uživatelské jméno může obsahovat pouze písmena a číslice a nesmí začínat číslicí.';
+$lang['Cyberpanel.!error.cyberpanel_domain.test'] = 'Název domény nemůže začínat na \'test\'.';
+$lang['Cyberpanel.!error.cyberpanel_domain.format'] = 'Zadejte platný název domény, např. domain.com.';
+$lang['Cyberpanel.!error.module_row.missing'] = 'Došlo k interní chybě. Řádek modulu není k dispozici.';
+$lang['Cyberpanel.!error.api.internal'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+$lang['Cyberpanel.!error.meta[package].empty'] = 'Je vyžadován balíček CyberPanel.';
+$lang['Cyberpanel.!error.name_servers_count'] = 'Je třeba definovat alespoň 2 jmenné servery.';
+$lang['Cyberpanel.!error.name_servers_valid'] = 'Jeden nebo více zadaných názvových serverů jsou neplatné.';
+$lang['Cyberpanel.!error.remote_admin_password_valid_connection'] = 'Připojení k serveru se nepodařilo navázat. Zkontrolujte, zda jsou název hostitele a heslo správce správné.';
+$lang['Cyberpanel.!error.remote_admin_password_valid'] = 'Heslo správce se zdá být neplatné.';
+$lang['Cyberpanel.!error.remote_admin_username_valid'] = 'Uživatelské jméno správce se zdá být neplatné.';
+$lang['Cyberpanel.!error.host_name_valid'] = 'Název hostitele se zdá být neplatný.';
+$lang['Cyberpanel.!error.server_name_valid'] = 'Je třeba zadat označení serveru.';
+$lang['Cyberpanel.service_field.tooltip.password'] = 'Heslo můžete nechat prázdné, aby se vygenerovalo automaticky.';
+$lang['Cyberpanel.service_field.tooltip.username'] = 'Uživatelské jméno můžete nechat prázdné, aby se vygenerovalo automaticky.';
+$lang['Cyberpanel.service_info.option_login'] = 'Přihlášení';
+$lang['Cyberpanel.service_info.options'] = 'Možnosti';
+$lang['Cyberpanel.service_info.server'] = 'Server';
+$lang['Cyberpanel.service_info.password'] = 'Heslo';
+$lang['Cyberpanel.service_info.username'] = 'Uživatelské jméno';
+$lang['Cyberpanel.tab_client_actions.field_password_submit'] = 'Aktualizace hesla';
+$lang['Cyberpanel.tab_client_actions.field_cyberpanel_confirm_password'] = 'Potvrzení hesla';
+$lang['Cyberpanel.tab_client_actions.field_cyberpanel_password'] = 'Heslo';
+$lang['Cyberpanel.tab_client_actions.change_password'] = 'Změna hesla';
+$lang['Cyberpanel.service_field.password'] = 'Heslo';
+$lang['Cyberpanel.service_field.username'] = 'Uživatelské jméno';
+$lang['Cyberpanel.service_field.domain'] = 'Doména';
+$lang['Cyberpanel.package_fields.package'] = 'Název balíčku';
+$lang['Cyberpanel.row_meta.use_ssl'] = 'Při připojování k rozhraní API používejte protokol SSL (doporučeno).';
+$lang['Cyberpanel.row_meta.admin_password'] = 'Heslo správce';
+$lang['Cyberpanel.row_meta.admin_username'] = 'Uživatelské jméno správce';
+$lang['Cyberpanel.row_meta.host_name'] = 'Hostitelské jméno';
+$lang['Cyberpanel.row_meta.server_name'] = 'Označení serveru';
+$lang['Cyberpanel.edit_row.add_btn'] = 'Upravit server';
+$lang['Cyberpanel.edit_row.remove_name_server'] = 'Odstranění stránky';
+$lang['Cyberpanel.edit_row.name_server'] = 'Name server %1$s';
+$lang['Cyberpanel.edit_row.name_server_host_col'] = 'Hostitelské jméno';
+$lang['Cyberpanel.edit_row.name_server_col'] = 'Jmenný server';
+$lang['Cyberpanel.edit_row.name_server_btn'] = 'Přidání dalšího jmenného serveru';
+$lang['Cyberpanel.edit_row.notes_title'] = 'Poznámky';
+$lang['Cyberpanel.edit_row.name_servers_title'] = 'Jmenné servery';
+$lang['Cyberpanel.edit_row.basic_title'] = 'Základní nastavení';
+$lang['Cyberpanel.edit_row.box_title'] = 'Upravit server CyberPanel';
+$lang['Cyberpanel.add_row.add_btn'] = 'Přidat server';
+$lang['Cyberpanel.add_row.remove_name_server'] = 'Odstranění stránky';
+$lang['Cyberpanel.add_row.name_server'] = 'Name server %1$s';
+$lang['Cyberpanel.add_row.name_server_host_col'] = 'Hostitelské jméno';
+$lang['Cyberpanel.add_row.name_server_col'] = 'Jmenný server';
+$lang['Cyberpanel.add_row.name_server_btn'] = 'Přidání dalšího jmenného serveru';
+$lang['Cyberpanel.add_row.notes_title'] = 'Poznámky';
+$lang['Cyberpanel.add_row.name_servers_title'] = 'Jmenné servery';
+$lang['Cyberpanel.add_row.basic_title'] = 'Základní nastavení';
+$lang['Cyberpanel.add_row.box_title'] = 'Přidání serveru CyberPanel';
+$lang['Cyberpanel.order_options.roundrobin'] = 'Rovnoměrné rozdělení mezi servery';
+$lang['Cyberpanel.order_options.first'] = 'První nenaplněný server';
+$lang['Cyberpanel.manage.module_groups_no_results'] = 'Neexistují žádné skupiny serverů.';
+$lang['Cyberpanel.manage.module_rows_no_results'] = 'Neexistují žádné servery.';
+$lang['Cyberpanel.manage.module_groups.confirm_delete'] = 'Opravdu chcete tuto skupinu serverů odstranit?';
+$lang['Cyberpanel.manage.module_rows.confirm_delete'] = 'Opravdu chcete tento server odstranit?';
+$lang['Cyberpanel.manage.module_groups.delete'] = 'Odstranit';
+$lang['Cyberpanel.manage.module_rows.delete'] = 'Odstranit';
+$lang['Cyberpanel.manage.module_groups.edit'] = 'Upravit';
+$lang['Cyberpanel.manage.module_rows.edit'] = 'Upravit';
+$lang['Cyberpanel.manage.module_groups_heading.options'] = 'Možnosti';
+$lang['Cyberpanel.manage.module_groups_heading.servers'] = 'Počet serverů';
+$lang['Cyberpanel.manage.module_groups_heading.name'] = 'Název skupiny';
+$lang['Cyberpanel.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['Cyberpanel.manage.module_rows_heading.hostname'] = 'Hostitelské jméno';
+$lang['Cyberpanel.manage.module_rows_heading.name'] = 'Označení serveru';
+$lang['Cyberpanel.manage.module_groups_title'] = 'Skupiny serverů';
+$lang['Cyberpanel.manage.module_rows_title'] = 'Servery';
+$lang['Cyberpanel.add_module_group'] = 'Přidání skupiny serverů';
+$lang['Cyberpanel.add_module_row'] = 'Přidat server';
+$lang['Cyberpanel.tab_client_actions'] = 'Akce';
+$lang['Cyberpanel.module_group'] = 'Skupina serverů';
+$lang['Cyberpanel.module_row_plural'] = 'Servery';
+$lang['Cyberpanel.module_row'] = 'Server';
+$lang['Cyberpanel.description'] = 'CyberPanel je ovládací panel hostingu nové generace založený na technologii OpenLiteSpeed. Vytvořen pro rychlost, bezpečnost a spolehlivost.';
+$lang['Cyberpanel.name'] = 'CyberPanel';
+
