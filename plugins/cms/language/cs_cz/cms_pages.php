f286cc3f0b81686bd32d5a6c9d8bda8d3cb21b07
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:30 -0800

    Updated cs_cz language files

---
+++ plugins/cms/language/cs_cz/cms_pages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cms Pages
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CmsPages.!error.content.valid'] = 'Chyba parsování obsahu: %1$s';
+$lang['CmsPages.!error.title.empty'] = 'Zadejte prosím název stránky.';
+$lang['CmsPages.!error.company_id.exists'] = 'Neplatné ID společnosti.';
+$lang['CmsPages.!error.uri.empty'] = 'Zadejte cestu URI ke stránce.';
+
