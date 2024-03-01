08691ff31253f2183f91c719b148fe58ab1fa98e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:58 -0800

    Updated cs_cz language files

---
+++ plugins/order/components/antifraud/maxmind/language/cs_cz/maxmind.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Maxmind
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Maxmind.settings.option_reject'] = 'Odmítnout';
+$lang['Maxmind.settings.option_review'] = 'Recenze';
+$lang['Maxmind.settings.option_allow'] = 'Povolit';
+$lang['Maxmind.settings.field_anon_proxy'] = 'Anonymní proxy server';
+$lang['Maxmind.settings.field_risky_country'] = 'Riziková země';
+$lang['Maxmind.settings.field_country_mismatch'] = 'Nesoulad zemí';
+$lang['Maxmind.settings.field_free_email'] = 'Bezplatný e-mail';
+$lang['Maxmind.settings.field_review_score'] = 'Minimální počet bodů k přezkoumání';
+$lang['Maxmind.settings.field_reject_score'] = 'Minimální počet bodů pro zamítnutí';
+$lang['Maxmind.settings.field_key'] = 'Licenční klíč';
+$lang['Maxmind.settings.field_server'] = 'Server';
+$lang['Maxmind.!error.review.anon_proxy'] = 'Objednávky od anonymních zmocněnců se přezkoumávají.';
+$lang['Maxmind.!error.reject.anon_proxy'] = 'Objednávky od anonymních zmocněnců se nepřijímají.';
+$lang['Maxmind.!error.review.risky_country'] = 'Objednávky z rizikových zemí se přezkoumávají.';
+$lang['Maxmind.!error.reject.risky_country'] = 'Objednávky z rizikových zemí nejsou přijímány.';
+$lang['Maxmind.!error.review.country_mismatch'] = 'Objednávky s nesouladem zemí se přezkoumávají.';
+$lang['Maxmind.!error.reject.country_mismatch'] = 'Objednávky s neshodnou zemí nejsou přijímány.';
+$lang['Maxmind.!error.review.free_email'] = 'Objednávky s bezplatnými e-mailovými účty se přezkoumávají.';
+$lang['Maxmind.!error.reject.free_email'] = 'Objednávky s bezplatnými e-mailovými účty nejsou přijímány.';
+$lang['Maxmind.!error.review.review_score'] = 'Vaše objednávka musí být zkontrolována ručně.';
+$lang['Maxmind.!error.reject.reject_score'] = 'Vaši objednávku nelze v tuto chvíli zpracovat.';
+
