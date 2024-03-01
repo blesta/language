b7297dae8392d97f835d164c09c27e25d0db8cc3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:02 -0800

    Updated el_gr language files

---
+++ plugins/system_overview/language/el_gr/system_overview_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * System Overview Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SystemOverviewPlugin.description'] = 'Εμφανίζει στατιστικά στοιχεία του συστήματος σε ένα widget στο ταμπλό του διαχειριστή.';
+$lang['SystemOverviewPlugin.name'] = 'Επισκόπηση συστήματος';
+
