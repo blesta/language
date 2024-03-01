d7b37b0fc2f3285dd0ff26965589c7bd0902484e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:25 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/admin_affiliates.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Admin Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminAffiliates.!success.affiliate_added'] = 'L\'affilié a été ajouté avec succès.';
+$lang['AdminAffiliates.!success.settings_updated'] = 'Les paramètres d\'affiliation ont été mis à jour.';
+$lang['AdminAffiliates.!success.affiliate_activated'] = 'L\'affilié a été activé avec succès.';
+$lang['AdminAffiliates.!success.affiliate_deactivated'] = 'L\'affilié a été désactivé avec succès.';
+$lang['AdminAffiliates.!success.affilates_enabled'] = 'Le système d\'affiliation a été activé avec succès.';
+$lang['AdminAffiliates.leftnav.nav_affiliates'] = 'Affiliés';
+$lang['AdminAffiliates.leftnav.nav_payouts'] = 'Paiements';
+$lang['AdminAffiliates.leftnav.nav_settings'] = 'Paramètres';
+$lang['AdminAffiliates.leftnav.nav_settings_general'] = 'Paramètres généraux';
+$lang['AdminAffiliates.leftnav.nav_settings_payment_methods'] = 'Modes de paiement';
+$lang['AdminAffiliates.leftnav.nav_settings_excluded_packages'] = 'Forfaits exclus';
+$lang['AdminAffiliates.index.page_title'] = 'Aperçu de l\'affiliation';
+$lang['AdminAffiliates.index.boxtitle_affiliates'] = 'Affiliés';
+$lang['AdminAffiliates.index.category_active'] = 'Actif';
+$lang['AdminAffiliates.index.category_inactive'] = 'Inactif';
+$lang['AdminAffiliates.index.order_affiliate_add'] = 'Ajouter un affilié à la commande';
+$lang['AdminAffiliates.index.no_results'] = 'Il n\'y a pas d\'affiliés pour l\'instant.';
+$lang['AdminAffiliates.index.disable_affiliates'] = 'Pour désactiver les pages d\'affiliation et supprimer les liens de navigation associés au système d\'affiliation, utilisez l\'onglet Action de la page Paramètres du plugin de commande.';
+$lang['AdminAffiliates.index.heading_name'] = 'Client';
+$lang['AdminAffiliates.index.heading_code'] = 'Code';
+$lang['AdminAffiliates.index.heading_visits'] = 'Visites';
+$lang['AdminAffiliates.index.heading_sales'] = 'Vente';
+$lang['AdminAffiliates.index.heading_date_added'] = 'Date d\'ajout';
+$lang['AdminAffiliates.index.heading_date_updated'] = 'Date de mise à jour';
+$lang['AdminAffiliates.index.heading_options'] = 'Options';
+$lang['AdminAffiliates.index.option_manage'] = 'Gérer';
+$lang['AdminAffiliates.index.option_deactivate'] = 'Désactiver';
+$lang['AdminAffiliates.index.option_activate'] = 'Activer';
+$lang['AdminAffiliates.index.confirm_deactivate'] = 'Êtes-vous sûr de vouloir désactiver cet affilié ?';
+$lang['AdminAffiliates.index.confirm_activate'] = 'Êtes-vous sûr de vouloir activer cet affilié ?';
+$lang['AdminAffiliates.index.text_enable'] = 'Le système d\'affiliation est actuellement désactivé. Cliquez sur le bouton "Activer" pour activer le système d\'affiliation et permettre aux affiliés de commencer à s\'inscrire et à effectuer des parrainages.';
+$lang['AdminAffiliates.index.enable'] = 'Activer le système d\'affiliation';
+$lang['AdminAffiliates.add.boxtitle_order'] = 'Système de commande - Ajouter un affilié';
+$lang['AdminAffiliates.add.field_client'] = 'Client';
+$lang['AdminAffiliates.add.client_placeholder'] = 'ID ou nom du client';
+$lang['AdminAffiliates.add.field_status'] = 'Statut';
+$lang['AdminAffiliates.add.field_addsubmit'] = 'Ajouter un affilié';
+$lang['AdminAffiliates.add.text_no_clients'] = 'Aucun client n\'a été trouvé. Essayez de chercher à nouveau.';
+
