15c2d7e8b19914435b307d64239b57a364bfd108
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:06 -0800

    Updated es_es language files

---
+++ language/es_es/app_controller.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * App Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AppController.lang.dir'] = 'ltr';
+$lang['AppController.!error.unauthorized_access'] = 'No está autorizado a acceder a ese recurso';
+$lang['AppController.!error.invalid_csrf'] = 'El token del formulario no es válido.';
+$lang['AppController.!success.license_updated'] = 'Su licencia ha sido revalidada con éxito. Por favor, inicie sesión.';
+$lang['AppController.!success.license_key_updated'] = 'Su clave de licencia se ha actualizado correctamente. Por favor, inicie sesión.';
+$lang['AppController.select.please'] = '-- Seleccione --';
+$lang['AppController.modal.text_close'] = 'Cerrar';
+$lang['AppController.modal.text_confirm'] = 'Confirmar';
+$lang['AppController.tooltip.text'] = '?';
+$lang['AppController.banners.client_manager'] = 'Estás gestionando una cuenta que te ha invitado a gestionarla.';
+$lang['AppController.banners.text_switch_back'] = 'Volver a mi cuenta';
+$lang['AppController.message.close'] = '×';
+$lang['AppController.structure.text_myinfo'] = 'Mi información';
+$lang['AppController.structure.text_settings'] = 'Ajustes';
+$lang['AppController.structure.text_logout'] = 'Cerrar sesión';
+$lang['AppController.structure.text_maintenance'] = 'El modo de mantenimiento está actualmente activado y los clientes no pueden iniciar sesión. Haga clic aquí para cambiarlo.';
+$lang['AppController.structure.text_search_placeholder'] = '%1$s...';
+$lang['AppController.client_structure.staff_as_client_note'] = 'Volver al Portal del Personal';
+$lang['AppController.client_structure.default_title'] = 'Mi cuenta';
+$lang['AppController.client_structure.text_return_to_portal'] = 'Volver al portal';
+$lang['AppController.client_structure.text_logout'] = 'Cerrar sesión';
+$lang['AppController.client_structure.text_login'] = 'Iniciar sesión';
+$lang['AppController.client_structure.text_update_account'] = 'Gestionar cuenta';
+$lang['AppController.client_structure.text_emails'] = 'Historial del correo electrónico';
+$lang['AppController.client_structure.text_accounts'] = 'Cuentas de pago';
+$lang['AppController.client_structure.text_contacts'] = 'Contactos';
+$lang['AppController.client_structure.text_managers'] = 'Directivos';
+$lang['AppController.dates.day_sun'] = 'Domingo';
+$lang['AppController.dates.day_mon'] = 'Lunes';
+$lang['AppController.dates.day_tue'] = 'Martes';
+$lang['AppController.dates.day_wed'] = 'Miércoles';
+$lang['AppController.dates.day_thu'] = 'Jueves';
+$lang['AppController.dates.day_fri'] = 'Viernes';
+$lang['AppController.dates.day_sat'] = 'Sábado';
+$lang['AppController.dates.dayabbr_sun'] = 'Sol';
+$lang['AppController.dates.dayabbr_mon'] = 'Lun';
+$lang['AppController.dates.dayabbr_tue'] = 'Mar';
+$lang['AppController.dates.dayabbr_wed'] = 'Mie';
+$lang['AppController.dates.dayabbr_thur'] = 'Jue';
+$lang['AppController.dates.dayabbr_fri'] = 'Vie';
+$lang['AppController.dates.dayabbr_sat'] = 'Sáb';
+$lang['AppController.dates.month_jan'] = 'Enero';
+$lang['AppController.dates.month_feb'] = 'Febrero';
+$lang['AppController.dates.month_mar'] = 'Marzo';
+$lang['AppController.dates.month_apr'] = 'Abril';
+$lang['AppController.dates.month_may'] = 'Mayo';
+$lang['AppController.dates.month_jun'] = 'Junio';
+$lang['AppController.dates.month_jul'] = 'Julio';
+$lang['AppController.dates.month_aug'] = 'Agosto';
+$lang['AppController.dates.month_sep'] = 'Septiembre';
+$lang['AppController.dates.month_oct'] = 'Octubre';
+$lang['AppController.dates.month_nov'] = 'Noviembre';
+$lang['AppController.dates.month_dec'] = 'Diciembre';
+$lang['AppController.dates.monthabbr_jan'] = 'Jan';
+$lang['AppController.dates.monthabbr_feb'] = 'Febrero';
+$lang['AppController.dates.monthabbr_mar'] = 'Mar';
+$lang['AppController.dates.monthabbr_apr'] = 'Abr';
+$lang['AppController.dates.monthabbr_may'] = 'Mayo';
+$lang['AppController.dates.monthabbr_jun'] = 'Jun';
+$lang['AppController.dates.monthabbr_jul'] = 'Julio';
+$lang['AppController.dates.monthabbr_aug'] = 'Agosto';
+$lang['AppController.dates.monthabbr_sep'] = 'Sep';
+$lang['AppController.dates.monthabbr_oct'] = 'Octubre';
+$lang['AppController.dates.monthabbr_nov'] = 'Nov';
+$lang['AppController.dates.monthabbr_dec'] = 'Diciembre';
+$lang['AppController.graphs.control_label.stacked'] = 'Apilado';
+$lang['AppController.graphs.control_label.stream'] = 'Corriente';
+$lang['AppController.graphs.control_label.expanded'] = 'Ampliado';
+$lang['AppController.graphs.control_label.grouped'] = 'Agrupado';
+$lang['AppController.graphs.label.total'] = 'Total';
+$lang['AppController.sreader.dropdown'] = 'Desplegable';
+$lang['AppController.sreader.navigation'] = 'Alternar navegación';
+
