c2f3a5b14219a79d06c9539c8c2694244358ad1c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:22 -0800

    Updated fr_fr language files

---
+++ components/gateways/merchant/authorize_net_acceptjs/language/fr_fr/authorize_net_acceptjs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Authorize Net Acceptjs
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AuthorizeNetAcceptjs.!error.auth'] = 'La passerelle n\'a pas pu s\'authentifier.';
+$lang['AuthorizeNetAcceptjs.!error.transaction_key.empty'] = 'Veuillez saisir une clé de transaction.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.empty'] = 'Veuillez saisir un identifiant de connexion.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.valid'] = 'Impossible de se connecter à l\'API Authorize.net en utilisant l\'identifiant de connexion donné.';
+$lang['AuthorizeNetAcceptjs.!error.sandbox.valid'] = 'Le bac à sable doit être défini à "true" s\'il est donné.';
+$lang['AuthorizeNetAcceptjs.name'] = 'Authorize.Net Accept.js';
+$lang['AuthorizeNetAcceptjs.description'] = 'Envoyez des données de paiement sécurisées directement à Authorize.net. Accept.js capture les données de paiement et les transmet directement à Authorize.net.';
+$lang['AuthorizeNetAcceptjs.field_number'] = 'Nombre';
+$lang['AuthorizeNetAcceptjs.field_security'] = 'Code de sécurité';
+$lang['AuthorizeNetAcceptjs.field_expiration'] = 'Date d\'expiration';
+$lang['AuthorizeNetAcceptjs.login_id'] = 'Identifiant de connexion';
+$lang['AuthorizeNetAcceptjs.transaction_key'] = 'Clé de transaction';
+$lang['AuthorizeNetAcceptjs.sandbox'] = 'Bac à sable';
+$lang['AuthorizeNetAcceptjs.sandbox_note'] = 'L\'activation de cette option permet d\'enregistrer les transactions dans l\'environnement Authorize.Net Sandbox. N\'activez cette option que si vous effectuez des tests avec un compte Authorize.Net Sandbox.';
+$lang['AuthorizeNetAcceptjs.charge_description_default'] = 'Facturation d\'un montant déterminé';
+$lang['AuthorizeNetAcceptjs.charge_description'] = 'Frais pour %1$s';
+
