cb6c7add20f37c0ec5c423e8877f3912aa040b03
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:33 -0800

    Updated it_it language files

---
+++ language/it_it/admin_company_plugins.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Admin Company Plugins
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyPlugins.settings.actions_heading_secondary'] = 'Opzioni secondarie';
+$lang['AdminCompanyPlugins.settings.actions_heading_sub'] = 'Opzioni secondarie';
+$lang['AdminCompanyPlugins.settings.actions_uri'] = 'URI: %1$s';
+$lang['AdminCompanyPlugins.settings.actions_description'] = 'Le azioni definiscono ciò che può essere visualizzato nelle interfacce admin e client per conto del plugin, come ad esempio i link di navigazione o i widget.';
+$lang['AdminCompanyPlugins.settings.events_no_results'] = 'Non ci sono eventi impostati per questo plugin.';
+$lang['AdminCompanyPlugins.settings.automation_no_results'] = 'Non sono state impostate attività di automazione per questo plugin.';
+$lang['AdminCompanyPlugins.settings.actions_no_results'] = 'Non ci sono azioni impostate per questo plugin.';
+$lang['AdminCompanyPlugins.settings.events_description'] = 'Il plugin ascolta gli eventi seguenti ed esegue alcune azioni quando si verificano.';
+$lang['AdminCompanyPlugins.settings.automation_description'] = 'Il plugin ha registrato i seguenti task di automazione per eseguire determinate azioni quando vengono eseguiti da cron.';
+$lang['AdminCompanyPlugins.settings.tab_events'] = 'Eventi';
+$lang['AdminCompanyPlugins.settings.tab_actions'] = 'Azioni';
+$lang['AdminCompanyPlugins.settings.tab_automation'] = 'Automazione';
+$lang['AdminCompanyPlugins.settings.page_title'] = '%1$s Impostazioni';
+$lang['AdminCompanyPlugins.settings.field_submit'] = 'Aggiornamento delle impostazioni';
+$lang['AdminCompanyPlugins.settings.field_enabled'] = 'Abilitato';
+$lang['AdminCompanyPlugins.installed.text_none'] = 'Non ci sono plugin installati.';
+$lang['AdminCompanyPlugins.installed.btn_upgrade'] = 'Aggiornamento';
+$lang['AdminCompanyPlugins.installed.btn_settings'] = 'Impostazioni';
+$lang['AdminCompanyPlugins.installed.btn_manage'] = 'Gestire';
+$lang['AdminCompanyPlugins.installed.btn_enable'] = 'Abilitazione';
+$lang['AdminCompanyPlugins.installed.btn_disable'] = 'Disattivare';
+$lang['AdminCompanyPlugins.installed.btn_uninstall'] = 'Disinstallare';
+$lang['AdminCompanyPlugins.installed.confirm_enable'] = 'Attivare davvero questo plugin?';
+$lang['AdminCompanyPlugins.installed.confirm_disable'] = 'Disattivare davvero questo plugin?';
+$lang['AdminCompanyPlugins.installed.confirm_uninstall'] = 'Disinstallare davvero questo plugin?';
+$lang['AdminCompanyPlugins.installed.text_author'] = 'Autore:';
+$lang['AdminCompanyPlugins.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyPlugins.installed.boxtitle_plugin'] = 'Plugin';
+$lang['AdminCompanyPlugins.installed.page_title'] = 'Impostazioni > Azienda > Plugin > Installati';
+$lang['AdminCompanyPlugins.available.text_none'] = 'Non ci sono plugin disponibili.';
+$lang['AdminCompanyPlugins.available.btn_install'] = 'Installare';
+$lang['AdminCompanyPlugins.available.text_author'] = 'Autore:';
+$lang['AdminCompanyPlugins.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyPlugins.available.boxtitle_plugins'] = 'Plugin';
+$lang['AdminCompanyPlugins.available.page_title'] = 'Impostazioni > Azienda > Plugin > Disponibili';
+$lang['AdminCompanyPlugins.settings.event.users.logout'] = 'Questo evento viene attivato quando un utente si disconnette.';
+$lang['AdminCompanyPlugins.settings.event.users.login'] = 'Questo evento viene attivato quando un utente effettua il login.';
+$lang['AdminCompanyPlugins.settings.event.users.delete'] = 'Questo evento viene attivato quando un utente viene cancellato.';
+$lang['AdminCompanyPlugins.settings.event.transactions.edit'] = 'Questo evento viene attivato quando una transazione viene aggiornata.';
+$lang['AdminCompanyPlugins.settings.event.transactions.add'] = 'Questo evento viene attivato quando viene creata una transazione.';
+$lang['AdminCompanyPlugins.settings.event.staff.edit'] = 'Questo evento viene attivato quando un membro del personale viene aggiornato.';
+$lang['AdminCompanyPlugins.settings.event.staff.add'] = 'Questo evento viene attivato quando viene creato un membro del personale.';
+$lang['AdminCompanyPlugins.settings.event.services.unsuspend'] = 'Questo evento viene attivato quando un servizio viene sospeso.';
+$lang['AdminCompanyPlugins.settings.event.services.suspend'] = 'Questo evento viene attivato quando un servizio viene sospeso.';
+$lang['AdminCompanyPlugins.settings.event.services.cancel'] = 'Questo evento viene attivato quando un servizio viene annullato o programmato per essere annullato.';
+$lang['AdminCompanyPlugins.settings.event.services.edit'] = 'Questo evento viene attivato quando un servizio viene aggiornato.';
+$lang['AdminCompanyPlugins.settings.event.services.add'] = 'Questo evento viene attivato quando viene creato un servizio.';
+$lang['AdminCompanyPlugins.settings.event.report.clientdata'] = 'Questo evento viene attivato quando viene generato il rapporto sulla Portabilità dei dati del cliente.';
+$lang['AdminCompanyPlugins.settings.event.packages.delete'] = 'Questo evento viene attivato quando un pacchetto viene eliminato.';
+$lang['AdminCompanyPlugins.settings.event.packages.edit'] = 'Questo evento viene attivato quando un pacchetto viene aggiornato.';
+$lang['AdminCompanyPlugins.settings.event.packages.add'] = 'Questo evento viene attivato quando viene creato un pacchetto.';
+$lang['AdminCompanyPlugins.settings.event.navigation.getsearchoptions'] = 'Questo evento viene attivato quando vengono visualizzate le opzioni di ricerca per il personale.';
+$lang['AdminCompanyPlugins.settings.event.modulemanager.delete'] = 'Questo evento viene attivato quando un modulo viene disinstallato.';
+$lang['AdminCompanyPlugins.settings.event.modulemanager.add'] = 'Questo evento viene attivato quando viene installato un modulo.';
+$lang['AdminCompanyPlugins.settings.event.invoices.setclosed'] = 'Questo evento viene attivato quando una fattura viene chiusa.';
+$lang['AdminCompanyPlugins.settings.event.invoices.edit'] = 'Questo evento viene attivato quando viene aggiornata una fattura.';
+$lang['AdminCompanyPlugins.settings.event.invoices.add'] = 'Questo evento viene attivato quando viene creata una fattura.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.delete'] = 'Questo evento viene attivato quando un gateway viene disinstallato.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.edit'] = 'Questo evento viene attivato quando un gateway viene aggiornato.';
+$lang['AdminCompanyPlugins.settings.event.gatewaymanager.add'] = 'Questo evento viene attivato quando viene installato un gateway.';
+$lang['AdminCompanyPlugins.settings.event.emails.sendcustom'] = 'Questo evento viene attivato quando viene inviata un\'e-mail personalizzata.';
+$lang['AdminCompanyPlugins.settings.event.emails.send'] = 'Questo evento viene attivato quando viene inviato un modello di e-mail.';
+$lang['AdminCompanyPlugins.settings.event.contacts.delete'] = 'Questo evento viene attivato quando un contatto cliente viene eliminato.';
+$lang['AdminCompanyPlugins.settings.event.contacts.edit'] = 'Questo evento viene attivato quando viene aggiornato un contatto cliente.';
+$lang['AdminCompanyPlugins.settings.event.contacts.add'] = 'Questo evento viene attivato quando viene creato un contatto cliente.';
+$lang['AdminCompanyPlugins.settings.event.companies.delete'] = 'Questo evento viene attivato quando un\'azienda viene cancellata.';
+$lang['AdminCompanyPlugins.settings.event.companies.edit'] = 'Questo evento viene attivato quando una società viene aggiornata.';
+$lang['AdminCompanyPlugins.settings.event.companies.add'] = 'Questo evento viene attivato quando viene creata un\'azienda.';
+$lang['AdminCompanyPlugins.settings.event.clients.deletenote'] = 'Questo evento viene attivato quando una nota del cliente viene eliminata.';
+$lang['AdminCompanyPlugins.settings.event.clients.delete'] = 'Questo evento viene attivato quando un cliente viene eliminato.';
+$lang['AdminCompanyPlugins.settings.event.clients.editnote'] = 'Questo evento viene attivato quando viene aggiornata una nota del cliente.';
+$lang['AdminCompanyPlugins.settings.event.clients.edit'] = 'Questo evento viene attivato quando un client viene aggiornato.';
+$lang['AdminCompanyPlugins.settings.event.clients.create'] = 'Questo evento viene attivato quando viene creato un client.';
+$lang['AdminCompanyPlugins.settings.event.clients.addnote'] = 'Questo evento viene attivato quando viene creata una nota del cliente.';
+$lang['AdminCompanyPlugins.settings.event.clients.add'] = 'Questo evento viene attivato quando viene aggiunto un cliente.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.delete'] = 'Questo evento viene attivato quando un gruppo di clienti viene eliminato.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.edit'] = 'Questo evento viene attivato quando viene aggiornato un gruppo di clienti.';
+$lang['AdminCompanyPlugins.settings.event.clientgroups.add'] = 'Questo evento viene attivato quando viene creato un gruppo di clienti.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.delete'] = 'Questo evento viene attivato quando un evento del calendario viene cancellato.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.edit'] = 'Questo evento viene attivato quando un evento del calendario viene aggiornato.';
+$lang['AdminCompanyPlugins.settings.event.calendarevents.add'] = 'Questo evento viene attivato quando viene creato un evento di calendario.';
+$lang['AdminCompanyPlugins.settings.event.appcontroller.structure'] = 'Questo evento viene attivato quando si verifica un caricamento di pagina non tramite AJAX.';
+$lang['AdminCompanyPlugins.settings.event.appcontroller.preaction'] = 'Questo evento viene attivato quando si verifica il caricamento di una pagina.';
+$lang['AdminCompanyPlugins.!tab.available'] = 'Disponibile';
+$lang['AdminCompanyPlugins.!tab.installed'] = 'Installato';
+$lang['AdminCompanyPlugins.!error.setting_controller_invalid'] = 'Il controllore delle impostazioni specificato non esiste per quel plugin.';
+$lang['AdminCompanyPlugins.!success.events_updated'] = 'Le impostazioni dell\'evento sono state aggiornate con successo.';
+$lang['AdminCompanyPlugins.!success.actions_updated'] = 'Le impostazioni dell\'azione sono state aggiornate con successo.';
+$lang['AdminCompanyPlugins.!success.automation_updated'] = 'Le impostazioni dell\'automazione sono state aggiornate con successo.';
+$lang['AdminCompanyPlugins.!success.disabled'] = 'Il plugin è stato disattivato con successo.';
+$lang['AdminCompanyPlugins.!success.enabled'] = 'Il plugin è stato attivato con successo.';
+$lang['AdminCompanyPlugins.!success.upgraded'] = 'Il plugin è stato aggiornato con successo.';
+$lang['AdminCompanyPlugins.!success.uninstalled'] = 'Il plugin è stato disinstallato con successo.';
+$lang['AdminCompanyPlugins.!success.installed'] = 'Il plugin è stato installato correttamente. Potrebbe aver registrato permessi ACL per varie risorse. Potrebbe essere necessario concedere al gruppo del personale l\'accesso a tali autorizzazioni per poter accedere a queste risorse.';
+
