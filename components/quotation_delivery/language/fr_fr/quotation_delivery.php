bb41fc236fa1613e3795f0af560911977f68ce34
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:09 -0800

    Updated fr_fr language files

---
+++ components/quotation_delivery/language/fr_fr/quotation_delivery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Quotation Delivery
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['QuotationDelivery.!error.libxml_required'] = 'Les extensions libxml et simplexml sont nécessaires pour l\'envoi des factures.';
+$lang['QuotationDelivery.deliverinvoices.interfax_subject'] = 'Citation #%1$s';
+$lang['QuotationDelivery.deliverinvoices.postalmethods_description'] = 'Citation #%1$s';
+
