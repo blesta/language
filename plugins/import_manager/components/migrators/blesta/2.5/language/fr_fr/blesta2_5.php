7ca46baf89d738db7ca3102d515f9842c92026bc
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:13 -0800

    Updated fr_fr language files

---
+++ plugins/import_manager/components/migrators/blesta/2.5/language/fr_fr/blesta2_5.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Blesta2 5
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Blesta2_5.!error.host.invalid'] = 'L\'hôte de la base de données est requis.';
+$lang['Blesta2_5.!error.database.invalid'] = 'Le nom de la base de données est obligatoire.';
+$lang['Blesta2_5.!error.user.invalid'] = 'L\'utilisateur de la base de données est requis.';
+$lang['Blesta2_5.!error.pass.invalid'] = 'Le mot de passe de la base de données est requis.';
+$lang['Blesta2_5.!error.key.invalid'] = 'Une AESKEY valide est nécessaire.';
+$lang['Blesta2_5.settings.host'] = 'Hôte de la base de données';
+$lang['Blesta2_5.settings.database'] = 'Nom de la base de données';
+$lang['Blesta2_5.settings.user'] = 'Utilisateur de la base de données';
+$lang['Blesta2_5.settings.pass'] = 'Mot de passe de la base de données';
+$lang['Blesta2_5.settings.key'] = 'AESKEY';
+$lang['Blesta2_5.settings.key.info'] = 'Saisissez la valeur de la définition AESKEY dans le fichier /inc/config.php. Cette valeur doit comporter 32 caractères.';
+$lang['Blesta2_5.configuration.create_packages_true'] = 'Création automatique de tous les paquets nécessaires lors de l\'importation';
+$lang['Blesta2_5.configuration.create_packages_false'] = 'Cartographier manuellement les paquets';
+$lang['Blesta2_5.configuration.local_package'] = 'Paquet local';
+$lang['Blesta2_5.configuration.remote_package'] = 'Paquet de télécommande';
+$lang['Blesta2_5.configuration.no_local_packages'] = 'Vous devez créer des paquets avant de pouvoir les cartographier.';
+
