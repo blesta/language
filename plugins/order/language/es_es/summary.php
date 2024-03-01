3f253095eb6596bc959b101e160468e17765edf9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:44 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/summary.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Summary
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Summary.index.price'] = '@ %1$s';
+$lang['Summary.index.totals.discount'] = 'Descuento:';
+$lang['Summary.index.totals.subtotal'] = 'Subtotal:';
+$lang['Summary.index.totals.tax'] = '%1$s:';
+$lang['Summary.index.totals.total'] = 'Total a pagar hoy:';
+$lang['Summary.index.totals.total_recurring'] = 'Total al renovar:';
+$lang['Summary.index.totals.total_recurring_amount'] = '%1$s %2$s @ %3$s';
+
