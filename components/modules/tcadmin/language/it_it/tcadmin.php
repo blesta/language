b0bea1adba3fc3dbffb41624898482333d1da4c1
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:47 -0800

    Updated it_it language files

---
+++ components/modules/tcadmin/language/it_it/tcadmin.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Tcadmin
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Tcadmin.!error.private_password.valid'] = 'La password privata deve essere lunga almeno 8 caratteri.';
+$lang['Tcadmin.!error.rcon_password.valid'] = 'RCON La password deve essere lunga almeno 8 caratteri.';
+$lang['Tcadmin.!error.user_password.valid'] = 'La password deve essere lunga almeno 8 caratteri.';
+$lang['Tcadmin.!error.user_name.empty'] = 'Il nome utente non può essere vuoto.';
+$lang['Tcadmin.!error.hostname.test'] = 'Il nome di dominio non può iniziare con "test".';
+$lang['Tcadmin.!error.hostname.format'] = 'Inserire un nome di dominio valido, ad esempio domain.com.';
+$lang['Tcadmin.!error.module_row.missing'] = 'Si è verificato un errore interno. La riga del modulo non è disponibile.';
+$lang['Tcadmin.!error.api.internal'] = 'Si è verificato un errore interno o il server non ha risposto alla richiesta.';
+$lang['Tcadmin.!error.meta[startup].valid'] = 'Selezionare un\'opzione di avvio valida';
+$lang['Tcadmin.!error.meta[priority].valid'] = 'Selezionare una priorità valida';
+$lang['Tcadmin.!error.meta[start].valid'] = 'Selezionare un\'opzione di avvio valida';
+$lang['Tcadmin.!error.meta[supported_servers].empty'] = 'Selezionare uno dei server Tcadmin supportati.';
+$lang['Tcadmin.!error.meta[server_type].valid'] = 'Selezionare un tipo di server valido.';
+$lang['Tcadmin.!error.account_limit_valid'] = 'Il limite del conto sembra non essere valido.';
+$lang['Tcadmin.!error.password_valid'] = 'La password sembra non essere valida.';
+$lang['Tcadmin.!error.port_valid'] = 'La porta sembra non essere valida.';
+$lang['Tcadmin.!error.user_name_valid'] = 'Il nome utente sembra non essere valido.';
+$lang['Tcadmin.!error.host_name_valid'] = 'Il nome host sembra non essere valido.';
+$lang['Tcadmin.!error.server_name_valid'] = 'È necessario inserire un\'etichetta del server.';
+$lang['Tcadmin.service_info.option_login'] = 'Accedi';
+$lang['Tcadmin.service_info.options'] = 'Opzioni';
+$lang['Tcadmin.service_info.server'] = 'Server';
+$lang['Tcadmin.service_info.password'] = 'Password';
+$lang['Tcadmin.service_info.username'] = 'Nome utente';
+$lang['Tcadmin.service_info.private_password'] = 'Password privata';
+$lang['Tcadmin.service_info.rcon_password'] = 'Password RCON';
+$lang['Tcadmin.service_info.hostname'] = 'Nome host';
+$lang['Tcadmin.stored_locally_only'] = 'Questo campo verrà aggiornato solo localmente';
+$lang['Tcadmin.service_field.private_password'] = 'Password privata';
+$lang['Tcadmin.service_field.rcon_password'] = 'Password RCON';
+$lang['Tcadmin.service_field.user_password'] = 'Password Tcadmin';
+$lang['Tcadmin.service_field.user_name'] = 'Nome utente Tcadmin';
+$lang['Tcadmin.service_field.hostname'] = 'Nome host';
+$lang['Tcadmin.package_fields.startup.disabled'] = 'Disabili';
+$lang['Tcadmin.package_fields.startup.manual'] = 'Manuale';
+$lang['Tcadmin.package_fields.startup.automatic'] = 'Automatico';
+$lang['Tcadmin.package_fields.priority.realtime'] = 'Tempo reale';
+$lang['Tcadmin.package_fields.priority.idle'] = 'Inattivo';
+$lang['Tcadmin.package_fields.priority.high'] = 'Alto';
+$lang['Tcadmin.package_fields.priority.normal'] = 'Normale';
+$lang['Tcadmin.package_fields.priority.belownormal'] = 'Al di sotto della norma';
+$lang['Tcadmin.package_fields.priority.abovenormal'] = 'Superiore alla norma';
+$lang['Tcadmin.package_fields.startup'] = 'Avviamento';
+$lang['Tcadmin.package_fields.priority'] = 'Priorità';
+$lang['Tcadmin.package_fields.start'] = 'Avvio dopo la creazione';
+$lang['Tcadmin.package_fields.no'] = 'No';
+$lang['Tcadmin.package_fields.yes'] = 'Sì';
+$lang['Tcadmin.package_fields.supported_servers'] = 'Server Tcadmin supportati';
+$lang['Tcadmin.package_fields.voice_server'] = 'Server vocale';
+$lang['Tcadmin.package_fields.game_server'] = 'Server di gioco';
+$lang['Tcadmin.package_fields.server_type'] = 'Tipo di server';
+$lang['Tcadmin.row_meta.account_limit'] = 'Limite del conto';
+$lang['Tcadmin.row_meta.use_ssl'] = 'Usare SSL quando ci si connette all\'API (consigliato)';
+$lang['Tcadmin.row_meta.port'] = 'Porto';
+$lang['Tcadmin.row_meta.password'] = 'Password';
+$lang['Tcadmin.row_meta.user_name'] = 'Nome utente';
+$lang['Tcadmin.row_meta.host_name'] = 'Nome host';
+$lang['Tcadmin.row_meta.server_name'] = 'Etichetta del server';
+$lang['Tcadmin.edit_row.add_btn'] = 'Modifica del server';
+$lang['Tcadmin.edit_row.basic_title'] = 'Impostazioni di base';
+$lang['Tcadmin.edit_row.box_title'] = 'Modifica del server Tcadmin';
+$lang['Tcadmin.add_row.add_btn'] = 'Aggiungi server';
+$lang['Tcadmin.add_row.basic_title'] = 'Impostazioni di base';
+$lang['Tcadmin.add_row.box_title'] = 'Aggiungere il server Tcadmin';
+$lang['Tcadmin.order_options.first'] = 'Primo server non completo';
+$lang['Tcadmin.manage.module_groups_no_results'] = 'Non esistono gruppi di server.';
+$lang['Tcadmin.manage.module_rows_no_results'] = 'Non ci sono server.';
+$lang['Tcadmin.manage.module_groups.confirm_delete'] = 'Siete sicuri di voler eliminare questo gruppo di server?';
+$lang['Tcadmin.manage.module_rows.confirm_delete'] = 'Siete sicuri di voler cancellare questo server?';
+$lang['Tcadmin.manage.module_groups.delete'] = 'Cancellare';
+$lang['Tcadmin.manage.module_rows.delete'] = 'Cancellare';
+$lang['Tcadmin.manage.module_groups.edit'] = 'Modifica';
+$lang['Tcadmin.manage.module_rows.edit'] = 'Modifica';
+$lang['Tcadmin.manage.module_groups_heading.options'] = 'Opzioni';
+$lang['Tcadmin.manage.module_groups_heading.servers'] = 'Conteggio del server';
+$lang['Tcadmin.manage.module_groups_heading.name'] = 'Nome del gruppo';
+$lang['Tcadmin.manage.module_rows_heading.options'] = 'Opzioni';
+$lang['Tcadmin.manage.module_rows_heading.port'] = 'Porto';
+$lang['Tcadmin.manage.module_rows_heading.hostname'] = 'Nome host';
+$lang['Tcadmin.manage.module_rows_heading.name'] = 'Etichetta del server';
+$lang['Tcadmin.manage.module_groups_title'] = 'Gruppi di server';
+$lang['Tcadmin.manage.module_rows_title'] = 'Server';
+$lang['Tcadmin.add_module_group'] = 'Aggiungi gruppo di server';
+$lang['Tcadmin.add_module_row'] = 'Aggiungi server';
+$lang['Tcadmin.module_group'] = 'Gruppo di server';
+$lang['Tcadmin.module_row_plural'] = 'Server';
+$lang['Tcadmin.module_row'] = 'Server';
+$lang['Tcadmin.description'] = 'TCAdmin, il pannello di controllo per l\'hosting dei giochi, è stato progettato da zero per aiutare i fornitori di servizi di gioco a risparmiare tempo e denaro, consentendo agli utenti finali il controllo completo dei loro server.';
+$lang['Tcadmin.name'] = 'Tcadmin';
+
