af542643178cd32eb0343c6a9ca863a1e6cb615b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:36 -0800

    Updated es_es language files

---
+++ language/es_es/package_option_groups.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Package Option Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptionGroups.!error.company_id.exists'] = 'Especifique una identificación de empresa válida.';
+$lang['PackageOptionGroups.!error.name.empty'] = 'Especifique un nombre de grupo.';
+$lang['PackageOptionGroups.!error.name.length'] = 'El nombre del grupo no puede tener más de 128 caracteres.';
+$lang['PackageOptionGroups.!error.group_id.exists'] = 'ID de grupo de opciones de paquete no válido.';
+$lang['PackageOptionGroups.!error.hide_options.valid'] = 'La opción de ocultar opciones debe ser 0 o 1.';
+$lang['PackageOptionGroups.!error.hidden.valid'] = 'Si se desea ocultar este grupo de opciones debe establecerse en 0 o 1.';
+
