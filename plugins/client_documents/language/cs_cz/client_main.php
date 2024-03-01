e8a8d177eadf586143511d84a5fc2eb535a9cc2c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:28 -0800

    Updated cs_cz language files

---
+++ plugins/client_documents/language/cs_cz/client_main.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.index.option_download'] = 'Stáhnout';
+$lang['ClientMain.index.heading_options'] = 'Možnosti';
+$lang['ClientMain.index.heading_date_added'] = 'Datum přidání';
+$lang['ClientMain.index.heading_description'] = 'Popis';
+$lang['ClientMain.index.heading_name'] = 'Název';
+$lang['ClientMain.index.no_results'] = 'Nejsou k dispozici žádné dokumenty ke stažení.';
+$lang['ClientMain.index.boxtitle_documents'] = 'Dokumenty';
+$lang['ClientMain.index.page_title'] = 'Dokumenty';
+
