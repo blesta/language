43d6c58bc1f2cea093b4fbe4c31e9aac69087325
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:18 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/cancel_scheduled_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cancel Scheduled Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.cancel_scheduled_services.attempt'] = 'Intentar cancelar servicios programados.';
+$lang['Automation.task.cancel_scheduled_services.completed'] = 'La tarea de cancelación de servicios programados ha finalizado.';
+$lang['Automation.task.cancel_scheduled_services.cancel_error'] = 'No se ha podido cancelar el servicio #%1$s del cliente #%2$s.';
+$lang['Automation.task.cancel_scheduled_services.cancel_success'] = 'El servicio #%1$s del cliente #%2$s ha sido cancelado.';
+
