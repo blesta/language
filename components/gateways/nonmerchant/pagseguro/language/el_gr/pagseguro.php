f50ae13c9fefee8e74502e0f65c665940c66becd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:32 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/pagseguro/language/el_gr/pagseguro.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Pagseguro
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pagseguro.!error.notification.invalid'] = 'Μη έγκυρες παράμετροι ειδοποίησης.';
+$lang['Pagseguro.!error.notification.type'] = 'Άγνωστος τύπος ειδοποίησης:';
+$lang['Pagseguro.!error.token.empty'] = 'Εισάγετε το διακριτικό ασφαλείας σας.';
+$lang['Pagseguro.!error.email.format'] = 'Παρακαλούμε εισάγετε μια έγκυρη διεύθυνση ηλεκτρονικού ταχυδρομείου.';
+$lang['Pagseguro.!error.dom_required'] = 'Η επέκταση dom απαιτείται για αυτή την πύλη.';
+$lang['Pagseguro.!error.libxml_required'] = 'Οι επεκτάσεις libxml και simplexml απαιτούνται για αυτή την πύλη.';
+$lang['Pagseguro.buildprocess.submit'] = 'Πληρώστε με το PagSeguro';
+$lang['Pagseguro.token_note'] = 'Εισάγετε το διακριτικό ασφαλείας που δημιουργήθηκε στο λογαριασμό σας στο PagSeguro.';
+$lang['Pagseguro.token'] = 'Token';
+$lang['Pagseguro.email_note'] = 'Εισάγετε τη διεύθυνση ηλεκτρονικού ταχυδρομείου του λογαριασμού σας PagSeguro.';
+$lang['Pagseguro.email_id'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['Pagseguro.description'] = 'Το PagSeguro είναι μια βραζιλιάνικη υπηρεσία ηλεκτρονικού εμπορίου για εμπορικές επιχειρήσεις που βασίζεται σε ηλεκτρονικές ή κινητές πληρωμές. Το 2015, αναγνωρίστηκε ως η "Καλύτερη Μέθοδος Πληρωμής" από το Congresso Afiliados Brasil';
+$lang['Pagseguro.name'] = 'PagSeguro';
+
