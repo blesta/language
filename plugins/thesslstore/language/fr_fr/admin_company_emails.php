6378544a52e045b878efefb1c8b7895e554b53aa
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:15 -0800

    Updated fr_fr language files

---
+++ plugins/thesslstore/language/fr_fr/admin_company_emails.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Thesslstore.expiration_reminder_desc'] = 'Avis envoyé avant 30 jours de la date d\'expiration du certificat.';
+$lang['AdminCompanyEmails.templates.Thesslstore.expiration_reminder_name'] = 'Rappel de l\'expiration du certificat SSL';
+
