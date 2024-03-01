d61eae738cffc4fb913b1855d68f424bd22d6274
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:52 -0800

    Updated es_es language files

---
+++ plugins/client_documents/language/es_es/admin_main.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.document_uploaded'] = 'El documento ha sido cargado.';
+$lang['AdminMain.!success.document_deleted'] = 'El documento ha sido eliminado.';
+$lang['AdminMain.index.page_title'] = 'Documentos';
+$lang['AdminMain.add.page_title'] = 'Documentos > Añadir';
+$lang['AdminMain.index.categorylink_adddocument'] = 'Añadir documento';
+$lang['AdminMain.index.boxtitle_client'] = 'Cliente #%1$s - %2$s %3$s';
+$lang['AdminMain.index.boxtitle_documents'] = 'Documentos';
+$lang['AdminMain.index.no_results'] = 'No hay documentos establecidos para este cliente.';
+$lang['AdminMain.index.heading_name'] = 'Nombre';
+$lang['AdminMain.index.heading_description'] = 'Descripción';
+$lang['AdminMain.index.heading_date_added'] = 'Fecha de adición';
+$lang['AdminMain.index.heading_date_options'] = 'Opciones';
+$lang['AdminMain.index.option_delete'] = 'Borrar';
+$lang['AdminMain.index.option_download'] = 'Descargar';
+$lang['AdminMain.modal.delete_document'] = '¿Borrar realmente este documento?';
+$lang['AdminMain.add.boxtitle_adddocument'] = 'Cargar documento';
+$lang['AdminMain.add.field_name'] = 'Nombre';
+$lang['AdminMain.add.field_document'] = 'Documento';
+$lang['AdminMain.add.field_description'] = 'Descripción';
+$lang['AdminMain.add.field_submit'] = 'Cargar documento';
+
