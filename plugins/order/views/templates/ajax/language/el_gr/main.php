da1677d891b1a11d2c81b4c67996382d2a68631f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:37 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/ajax/language/el_gr/main.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Main.index.heading'] = 'Επιλογή πακέτου';
+$lang['Main.index.subheading'] = 'Παρακαλούμε επιλέξτε μια ομάδα πακέτων παρακάτω για να συνεχίσετε.';
+$lang['Main.index.domain_bundle_eligble'] = 'Δικαιούται δωρεάν domain';
+$lang['Main.index.group_heading'] = 'Επιλέξτε ένα πακέτο παρακάτω.';
+$lang['Main.index.field_pricing_id'] = 'Επιλέξτε τον κύκλο χρέωσής σας.';
+$lang['Main.index.button_order'] = 'Παραγγείλετε τώρα';
+$lang['Main.index.client_limit'] = 'Επίτευξη ορίου';
+$lang['Main.index.sold_out'] = 'Εκτός αποθέματος';
+$lang['Main.index.field_currency'] = 'Νόμισμα:';
+$lang['Main.packages.subheading'] = 'Επιλέξτε ένα πακέτο και, στη συνέχεια, ρυθμίστε το παρακάτω.';
+$lang['Main.packages.text_free_domain'] = 'Επιλέξτε ένα Πακέτο σε αυτή την Ομάδα με επιλογή τιμολόγησης %1$s ή μεγαλύτερη για δωρεάν domain.';
+$lang['Main.packages.select_note'] = 'Σύρετε για να επιλέξετε ένα διαφορετικό πακέτο.';
+$lang['Main.packages.price_start'] = 'Ξεκινώντας από';
+$lang['Main.packages.show_cart_btn'] = 'Εμφάνιση καλαθιού';
+$lang['Main.packages.box_select'] = 'Επιλέξτε';
+$lang['Main.packages.box_selected'] = 'Επιλεγμένα';
+$lang['Main.packages.box_client_limit'] = 'Επίτευξη ορίου';
+$lang['Main.packages.box_sold_out'] = 'Εκτός αποθέματος';
+$lang['Main.packages.term_day'] = '%1$s ημέρα';
+$lang['Main.packages.term_days'] = '%1$s ημέρες';
+$lang['Main.packages.term_week'] = '%1$s εβδομάδα';
+$lang['Main.packages.term_weeks'] = '%1$s εβδομάδες';
+$lang['Main.packages.term_month'] = '%1$s μήνας';
+$lang['Main.packages.term_months'] = '%1$s μήνες';
+$lang['Main.packages.term_year'] = '%1$s έτος';
+$lang['Main.packages.term_years'] = '%1$s χρόνια';
+
