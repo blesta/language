60befc8b5ca2f039fbb0187854de0d1b0861f210
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:00 -0800

    Updated de_de language files

---
+++ components/gateways/merchant/payjunction/language/de_de/payjunction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payjunction
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PayJunction.!error.test_mode.valid'] = 'Der Entwicklermodus muss auf "true" gesetzt werden, wenn er angegeben ist.';
+$lang['PayJunction.!error.password.empty'] = 'Bitte geben Sie Ihr Kontopasswort ein.';
+$lang['PayJunction.!error.user_name.empty'] = 'Bitte geben Sie Ihren Benutzernamen ein.';
+$lang['PayJunction.!error.api_key.empty'] = 'Bitte geben Sie einen gültigen API-Schlüssel ein.';
+$lang['PayJunction.test_mode_note'] = 'Wenn Sie den Entwicklermodus aktivieren, werden Transaktionen an die PayJunction Labs Demo-Umgebung gesendet. Sie müssen den Demo-Benutzernamen und das Passwort sowie Ihren Labs API Key verwenden, um den Entwicklermodus zu nutzen.';
+$lang['PayJunction.password_note'] = 'Geben Sie das Passwort für Ihr PayJunction-Konto ein.';
+$lang['PayJunction.user_name_note'] = 'Geben Sie den Benutzernamen Ihres PayJunction-Kontos ein.';
+$lang['PayJunction.api_key_note'] = 'Geben Sie den API-Schlüssel aus Ihrem PayJunction-Konto ein.';
+$lang['PayJunction.test_mode'] = 'Entwickler-Modus';
+$lang['PayJunction.password'] = 'Passwort';
+$lang['PayJunction.user_name'] = 'Benutzername';
+$lang['PayJunction.api_key'] = 'API-Schlüssel';
+$lang['PayJunction.description'] = 'PayJunction verarbeitet jährlich 4 Milliarden Dollar und wächst schnell. Die Technologie von PayJunction eignet sich ideal für Unternehmen mit mehreren Standorten und Abteilungen, die ein monatliches Kreditkartenvolumen von 10.000 US-Dollar oder mehr verarbeiten.';
+$lang['PayJunction.name'] = 'PayJunction';
+
