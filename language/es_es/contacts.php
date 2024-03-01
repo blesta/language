48d76833f4324fc27e6c6954823ef9cf78a8de96
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:18 -0800

    Updated es_es language files

---
+++ language/es_es/contacts.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Contacts
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Contacts.!error.client_id.exists'] = 'ID de cliente no válido.';
+$lang['Contacts.!error.user_id.exists'] = 'ID de usuario no válida.';
+$lang['Contacts.!error.contact_type.format'] = 'Tipo de contacto no válido.';
+$lang['Contacts.!error.contact_type.inv_address_to'] = 'Las facturas están configuradas para ser enviadas a este contacto y deben cambiarse antes de actualizar este contacto.';
+$lang['Contacts.!error.contact_type_id.format'] = 'ID de tipo de contacto no válido.';
+$lang['Contacts.!error.first_name.empty'] = 'Por favor, introduzca un nombre.';
+$lang['Contacts.!error.last_name.empty'] = 'Por favor, introduzca un apellido.';
+$lang['Contacts.!error.title.length'] = 'La longitud del título no puede superar los 64 caracteres.';
+$lang['Contacts.!error.title.empty'] = 'Por favor, introduzca un título.';
+$lang['Contacts.!error.company.length'] = 'La longitud de la empresa no puede superar los 128 caracteres.';
+$lang['Contacts.!error.company.empty'] = 'Por favor, introduzca una empresa.';
+$lang['Contacts.!error.email.format'] = 'Dirección de correo electrónico no válida.';
+$lang['Contacts.!error.email.unique'] = 'La dirección de correo electrónico ya está ocupada.';
+$lang['Contacts.!error.city.empty'] = 'Por favor, introduzca una ciudad.';
+$lang['Contacts.!error.state.length'] = 'La longitud del estado no puede superar los 3 caracteres.';
+$lang['Contacts.!error.state.country_exists'] = 'Seleccione el país que corresponda al estado seleccionado.';
+$lang['Contacts.!error.state.empty'] = 'Seleccione un estado/provincia.';
+$lang['Contacts.!error.country.length'] = 'La longitud del país no puede superar los 3 caracteres.';
+$lang['Contacts.!error.country.empty'] = 'Seleccione un país.';
+$lang['Contacts.!error.contact_id.exists'] = 'ID de contacto no válida.';
+$lang['Contacts.!error.contact_id.primary'] = 'El contacto principal del cliente no puede ser eliminado.';
+$lang['Contacts.!error.address1.empty'] = 'Introduzca la primera línea de su dirección.';
+$lang['Contacts.!error.address2.empty'] = 'Introduzca la segunda línea de su dirección.';
+$lang['Contacts.!error.zip.empty'] = 'Introduzca un código postal.';
+$lang['Contacts.!error.phone.required'] = 'Introduzca al menos un número de teléfono.';
+$lang['Contacts.!error.fax.required'] = 'Introduzca al menos un número de fax.';
+$lang['Contacts.!error.first_name.read_only'] = 'El nombre es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.last_name.read_only'] = 'El apellido es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.title.read_only'] = 'El título es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.company.read_only'] = 'Empresa es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.email.read_only'] = 'La dirección es un campo de sólo lectura y no puede ser actualizado, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.city.read_only'] = 'Ciudad es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.state.read_only'] = 'Estado/provincia es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.country.read_only'] = 'El país es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.address1.read_only'] = 'La primera línea de dirección es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarla.';
+$lang['Contacts.!error.address2.read_only'] = 'La segunda línea de dirección es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarla.';
+$lang['Contacts.!error.zip.read_only'] = 'El código postal es un campo de sólo lectura y no se puede actualizar, póngase en contacto con nosotros para cambiarlo.';
+$lang['Contacts.!error.number.empty'] = 'Por favor, introduzca un número de contacto.';
+$lang['Contacts.!error.type.format'] = 'Tipo no válido.';
+$lang['Contacts.!error.location.format'] = 'Ubicación no válida.';
+$lang['Contacts.!error.id.exists'] = 'ID de número de contacto no válido.';
+$lang['Contacts.!error.company_id.format'] = 'ID de empresa no válida.';
+$lang['Contacts.!error.name.empty'] = 'Por favor, introduzca un nombre.';
+$lang['Contacts.!error.is_lang.format'] = 'is_lang debe ser un número.';
+$lang['Contacts.!error.is_lang.length'] = 'La longitud de is_lang no puede superar 1 carácter.';
+$lang['Contacts.!error.contact_type_id.exists'] = 'ID de tipo de contacto no válido.';
+$lang['Contacts.getcontacttypes.primary'] = 'Principal';
+$lang['Contacts.getcontacttypes.billing'] = 'Facturación';
+$lang['Contacts.getcontacttypes.other'] = 'Otros';
+$lang['Contacts.getnumbertypes.phone'] = 'Teléfono';
+$lang['Contacts.getnumbertypes.fax'] = 'Fax';
+$lang['Contacts.getnumberlocations.home'] = 'Inicio';
+$lang['Contacts.getnumberlocations.work'] = 'Trabajo';
+$lang['Contacts.getnumberlocations.mobile'] = 'Móvil';
+$lang['Contacts.getPermissionOptions.client_invoices'] = 'Facturas';
+$lang['Contacts.getPermissionOptions.client_services'] = 'Servicios';
+$lang['Contacts.getPermissionOptions.client_transactions'] = 'Transacciones';
+$lang['Contacts.getPermissionOptions.client_contacts'] = 'Contactos';
+$lang['Contacts.getPermissionOptions.client_accounts'] = 'Cuentas de pago';
+$lang['Contacts.getPermissionOptions.client_emails'] = 'Historial del correo electrónico';
+$lang['Contacts.getPermissionOptions._managed'] = 'Cuentas gestionadas';
+$lang['Contacts.getPermissionOptions._invoice_delivery'] = 'Entrega de facturas';
+$lang['Contacts.getPermissionOptions._credits'] = 'Créditos';
+$lang['Contacts.getPermissionOptions.client_quotations'] = 'Citas';
+
