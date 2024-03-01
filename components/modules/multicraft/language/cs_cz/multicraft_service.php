4e85d8d5186c4154c1af74efdc03cb492a0112ff
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:29 -0800

    Updated cs_cz language files

---
+++ components/modules/multicraft/language/cs_cz/multicraft_service.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Multicraft Service
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MulticraftService.!error.configoptions[dedicated_ip].format'] = 'Zda se má použít vyhrazená IP adresa, musí být nastaveno na 1 nebo 0.';
+$lang['MulticraftService.!error.configoptions[daemon_id].format'] = 'Nastavte prosím platného démona.';
+$lang['MulticraftService.!error.configoptions[memory].format'] = 'Nastavte platnou velikost paměti.';
+$lang['MulticraftService.!error.configoptions[players].format'] = 'Nastavte prosím počet hráčů.';
+$lang['MulticraftService.!error.configoptions[user_players].format'] = 'Vlastník serveru může nastavit počet hráčů na 1 nebo 0.';
+$lang['MulticraftService.service_fields.no'] = 'Ne';
+$lang['MulticraftService.service_fields.yes'] = 'Ano';
+$lang['MulticraftService.service_fields.tooltip.port'] = 'Nastavte port serveru Multicraft, kterému má být tato služba přiřazena. V opačném případě jej ponechte prázdný.';
+$lang['MulticraftService.service_fields.port'] = 'Přístav';
+$lang['MulticraftService.service_fields.tooltip.ip'] = 'Nastavte IP adresu serveru Multicraft, kterému má být tato služba přiřazena. V opačném případě ji ponechte prázdnou.';
+$lang['MulticraftService.service_fields.ip'] = 'IP adresa';
+$lang['MulticraftService.service_fields.tooltip.daemon_id'] = 'Nastavte ID démona Multicraft, kterému má být tato služba přiřazena. V opačném případě jej ponechte prázdný.';
+$lang['MulticraftService.service_fields.daemon_id'] = 'ID démona';
+$lang['MulticraftService.service_fields.tooltip.user_id'] = 'Nastavte jedinečné ID uživatele Multicraft, kterému má být tato služba přiřazena. V opačném případě jej ponechte prázdný.';
+$lang['MulticraftService.service_fields.user_id'] = 'ID uživatele';
+$lang['MulticraftService.service_fields.tooltip.server_id'] = 'Nastavte jedinečné ID serveru Multicraft Server, kterému má být tato služba přiřazena. V opačném případě jej ponechte prázdný.';
+$lang['MulticraftService.service_fields.server_id'] = 'ID serveru';
+$lang['MulticraftService.!error.port.format'] = 'Zadejte platné číslo portu nebo jej nechte prázdné.';
+$lang['MulticraftService.!error.ip.format'] = 'Zadejte platnou IP adresu nebo ji nechte prázdnou.';
+$lang['MulticraftService.!error.daemon_id.format'] = 'ID démona musí být celé číslo nebo musí zůstat prázdné.';
+$lang['MulticraftService.!error.user_id.format'] = 'ID uživatele musí být celé číslo nebo musí zůstat prázdné.';
+$lang['MulticraftService.!error.server_id.format'] = 'ID serveru musí být celé číslo nebo musí zůstat prázdné.';
+$lang['MulticraftService.!error.client'] = 'Nebyl nalezen žádný klient, ze kterého by bylo možné vytvořit uživatele Multicraft.';
+$lang['MulticraftService.!error.internal'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+
