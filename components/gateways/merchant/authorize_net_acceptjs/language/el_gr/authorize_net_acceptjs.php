e4c02ac9f7fdd25eb678c61f9381d6fef7f9321c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:17 -0800

    Updated el_gr language files

---
+++ components/gateways/merchant/authorize_net_acceptjs/language/el_gr/authorize_net_acceptjs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Authorize Net Acceptjs
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AuthorizeNetAcceptjs.charge_description'] = 'Χρέωση για %1$s';
+$lang['AuthorizeNetAcceptjs.charge_description_default'] = 'Χρέωση για καθορισμένο ποσό';
+$lang['AuthorizeNetAcceptjs.sandbox_note'] = 'Η ενεργοποίηση αυτής της επιλογής θα δημοσιεύσει τις συναλλαγές στο περιβάλλον Authorize.Net Sandbox. Ενεργοποιήστε αυτήν την επιλογή μόνο αν κάνετε δοκιμές με λογαριασμό Authorize.Net Sandbox.';
+$lang['AuthorizeNetAcceptjs.sandbox'] = 'Sandbox';
+$lang['AuthorizeNetAcceptjs.transaction_key'] = 'Κλειδί συναλλαγής';
+$lang['AuthorizeNetAcceptjs.login_id'] = 'Αναγνωριστικό σύνδεσης';
+$lang['AuthorizeNetAcceptjs.field_expiration'] = 'Ημερομηνία λήξης';
+$lang['AuthorizeNetAcceptjs.field_security'] = 'Κωδικός ασφαλείας';
+$lang['AuthorizeNetAcceptjs.field_number'] = 'Αριθμός';
+$lang['AuthorizeNetAcceptjs.description'] = 'Αποστολή ασφαλών δεδομένων πληρωμής απευθείας στο Authorize.net. Το Accept.js συλλαμβάνει τα δεδομένα πληρωμής και τα υποβάλλει απευθείας στο Authorize.net.';
+$lang['AuthorizeNetAcceptjs.name'] = 'Authorize.Net Accept.js';
+$lang['AuthorizeNetAcceptjs.!error.sandbox.valid'] = 'Το Sandbox πρέπει να οριστεί σε "true", αν δοθεί.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.valid'] = 'Δεν είναι δυνατή η σύνδεση στο API της Authorize.net με το συγκεκριμένο αναγνωριστικό σύνδεσης.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.empty'] = 'Παρακαλώ εισάγετε ένα αναγνωριστικό σύνδεσης.';
+$lang['AuthorizeNetAcceptjs.!error.transaction_key.empty'] = 'Παρακαλώ εισάγετε ένα κλειδί συναλλαγής.';
+$lang['AuthorizeNetAcceptjs.!error.auth'] = 'Η πύλη δεν μπόρεσε να πραγματοποιήσει έλεγχο ταυτότητας.';
+
