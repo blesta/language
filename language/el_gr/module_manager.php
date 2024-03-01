ec4bd0d33d9ea57eb35745ee096108aee6419f7b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:25 -0800

    Updated el_gr language files

---
+++ language/el_gr/module_manager.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Module Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ModuleManager.!error.module_id.assigned_service'] = 'Η ενότητα δεν μπορεί να απεγκατασταθεί επειδή χρησιμοποιείται από μία ή περισσότερες υπηρεσίες που δεν έχουν ακυρωθεί.';
+$lang['ModuleManager.!error.module_id.assigned_package'] = 'Η ενότητα δεν μπορεί να απεγκατασταθεί επειδή χρησιμοποιείται από ένα ή περισσότερα ενεργά πακέτα.';
+$lang['ModuleManager.!error.module_group_id.assigned_package'] = 'Η ομάδα δεν μπορεί να διαγραφεί επειδή ανήκει σε ένα ή περισσότερα πακέτα.';
+$lang['ModuleManager.!error.module_row_id.assigned_package'] = 'Η γραμμή δεν μπορεί να διαγραφεί επειδή ανήκει σε ένα ή περισσότερα πακέτα.';
+$lang['ModuleManager.!error.module_row_id.assigned_service'] = 'Η γραμμή δεν μπορεί να διαγραφεί επειδή ανήκει σε μία ή περισσότερες υπηρεσίες.';
+$lang['ModuleManager.!error.force_limits.valid'] = 'Η επιλογή για την επιβολή αυστηρών ορίων πρέπει να οριστεί σε 0 ή 1.';
+$lang['ModuleManager.!error.module_rows[].valid'] = 'Μία ή περισσότερες από τις σειρές που καθορίζονται δεν ανήκουν σε αυτήν την ενότητα.';
+$lang['ModuleManager.!error.name.empty'] = 'Πρέπει να καθορίσετε ένα όνομα για την ομάδα.';
+$lang['ModuleManager.!error.type_id.valid'] = 'Το αναγνωριστικό τύπου είναι άκυρο.';
+$lang['ModuleManager.!error.version.valid'] = 'Η έκδοση είναι άκυρη.';
+$lang['ModuleManager.!error.name.valid'] = 'Το όνομα είναι άκυρο.';
+$lang['ModuleManager.!error.class.valid'] = 'Η κλάση είναι άκυρη.';
+$lang['ModuleManager.!error.company_id.valid'] = 'Το αναγνωριστικό της εταιρείας είναι άκυρο.';
+
