cd45d62acdfe0606e75323d3c88b56d21ab4445f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:11 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/payfast/language/de_de/payfast.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Payfast
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payfast.buildprocess.submit'] = 'Bezahlen mit PayFast';
+$lang['Payfast.meta.sandbox'] = 'Sandkasten';
+$lang['Payfast.meta.passphrase'] = 'Passphrase';
+$lang['Payfast.meta.merchant_key'] = 'Händlerschlüssel';
+$lang['Payfast.meta.merchant_id'] = 'Händler-ID';
+$lang['Payfast.!error.sandbox.valid'] = 'Sandbox-Wert ungültig.';
+$lang['Payfast.!error.merchant_key.valid'] = 'Merchant Key ungültig.';
+$lang['Payfast.!error.merchant_id.valid'] = 'Händler-ID ungültig.';
+$lang['Payfast.description'] = 'Erleben Sie schnelle und sichere Online-Zahlungen in Südafrika von fast überall auf der Welt.';
+$lang['Payfast.name'] = 'PayFast';
+
