ec0dcd8813381bf9715494abcc84844d3b501b4f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:07 -0800

    Updated es_es language files

---
+++ language/es_es/backup.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Backup
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Backup.!error.driver.support'] = 'El controlador de la base de datos no es compatible.';
+$lang['Backup.!error.temp_dir.writable'] = 'No se puede escribir en el directorio temporal.';
+$lang['Backup.!error.ftp_failed'] = 'La copia de seguridad SFTP ha fallado.';
+$lang['Backup.!error.amazons3_failed'] = 'La copia de seguridad de AmazonS3 ha fallado.';
+$lang['Backup.frequencies.never'] = 'Nunca';
+$lang['Backup.frequencies.hour'] = 'Hora';
+$lang['Backup.frequencies.hours'] = '%1$s Horas';
+$lang['Backup.frequencies.day'] = 'Día';
+$lang['Backup.frequencies.days'] = '%1$s Días';
+
