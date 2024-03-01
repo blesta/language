998bc4d63b5199e98fa54e32e42567de374514c4
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:44 -0800

    Updated cs_cz language files

---
+++ components/reports/client_revenue/language/cs_cz/client_revenue.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Client Revenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientRevenue.heading.total'] = 'Celkem';
+$lang['ClientRevenue.heading.company'] = 'Společnost';
+$lang['ClientRevenue.heading.client_name'] = 'Jméno klienta';
+$lang['ClientRevenue.heading.client_id_code'] = 'ID klienta';
+$lang['ClientRevenue.options.field_end_date'] = 'Datum ukončení';
+$lang['ClientRevenue.options.field_start_date'] = 'Datum zahájení';
+$lang['ClientRevenue.options.currency'] = 'Měna';
+$lang['ClientRevenue.option.any'] = 'Jakýkoli';
+$lang['ClientRevenue.description'] = 'Vypočítá příjmy z měny pro každého klienta součtem částek zaplacených na všechny faktury spojené s klientem.';
+$lang['ClientRevenue.name'] = 'Příjmy klientů';
+
