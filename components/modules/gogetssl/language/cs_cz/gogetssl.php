fdeda7763683b26d75da9fa1853ed4c908cb2359
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:24 -0800

    Updated cs_cz language files

---
+++ components/modules/gogetssl/language/cs_cz/gogetssl.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Gogetssl
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GoGetSSL.tab_reissue.heading_reissue'] = 'Reedice';
+$lang['GoGetSSL.tab_reissue'] = 'Reedice';
+$lang['GoGetSSL.service_field.gogetssl_fax'] = 'Číslo faxu (mezinárodní formát)';
+$lang['GoGetSSL.service_field.gogetssl_organization_unit'] = 'Organizační jednotka (pokud se nepoužije, zadejte NA)';
+$lang['GoGetSSL.service_field.gogetssl_organization'] = 'Organizace (pokud se nepoužije, zadejte NA)';
+$lang['GoGetSSL.service_field.gogetssl_number'] = 'Telefonní číslo (mezinárodní formát)';
+$lang['GoGetSSL.service_field.gogetssl_email'] = 'E-mail';
+$lang['GoGetSSL.service_field.gogetssl_country'] = 'Země';
+$lang['GoGetSSL.service_field.gogetssl_state'] = 'Stát';
+$lang['GoGetSSL.service_field.gogetssl_zip'] = 'ZIP';
+$lang['GoGetSSL.service_field.gogetssl_city'] = 'Město';
+$lang['GoGetSSL.service_field.gogetssl_address2'] = 'Adresní řádek 2';
+$lang['GoGetSSL.service_field.gogetssl_address1'] = 'Adresní řádek 1';
+$lang['GoGetSSL.service_field.gogetssl_lastname'] = 'Příjmení';
+$lang['GoGetSSL.service_field.gogetssl_firstname'] = 'Křestní jméno';
+$lang['GoGetSSL.service_field.gogetssl_title'] = 'Název';
+$lang['GoGetSSL.service_field.gogetssl_approver_email'] = 'E-mail schvalovatele';
+$lang['GoGetSSL.service_field.gogetssl_webserver_type'] = 'Typ webového serveru';
+$lang['GoGetSSL.service_field.gogetssl_csr'] = 'Obsah CSR';
+$lang['GoGetSSL.service_field.gogetssl_fqdn'] = 'Hostitel / doména';
+$lang['GoGetSSL.package_fields.product'] = 'Produkt';
+$lang['GoGetSSL.edit_row.add_btn'] = 'Aktualizace prodejce';
+$lang['GoGetSSL.edit_row.basic_title'] = 'Základní nastavení';
+$lang['GoGetSSL.edit_row.box_title'] = 'Upravit GoGetSSL Reseller';
+$lang['GoGetSSL.add_row.add_btn'] = 'Přidat prodejce';
+$lang['GoGetSSL.add_row.basic_title'] = 'Základní nastavení';
+$lang['GoGetSSL.add_row.box_title'] = 'Přidání prodejce GoGetSSL';
+$lang['GoGetSSL.row_meta.sandbox'] = 'Sandbox';
+$lang['GoGetSSL.row_meta.api_password'] = 'Heslo API';
+$lang['GoGetSSL.row_meta.api_username'] = 'Uživatelské jméno API';
+$lang['GoGetSSL.row_meta.gogetssl_name'] = 'Název prodejce';
+$lang['GoGetSSL.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['GoGetSSL.manage.module_rows_heading.gogetssl_name'] = 'Název prodejce';
+$lang['GoGetSSL.manage.module_rows_no_results'] = 'Neexistují žádní prodejci.';
+$lang['GoGetSSL.manage.module_rows.confirm_delete'] = 'Opravdu chcete tohoto prodejce odstranit?';
+$lang['GoGetSSL.manage.module_rows.delete'] = 'Odstranit';
+$lang['GoGetSSL.manage.module_rows.edit'] = 'Upravit';
+$lang['GoGetSSL.manage.module_rows_title'] = 'Prodejci GoGetSSL';
+$lang['GoGetSSL.add_module_row'] = 'Přidat prodejce';
+$lang['GoGetSSL.module_row_plural'] = 'Prodejci';
+$lang['GoGetSSL.module_row'] = 'Prodejce GoGetSSL';
+$lang['GoGetSSL.description'] = 'Společnost GoGetSSL poskytuje SSL certifikáty od roku 2009.';
+$lang['GoGetSSL.name'] = 'GoGetSSL';
+$lang['GoGetSSL.please_select'] = '-- Vyberte prosím --';
+$lang['GoGetSSL.!error.api.internal'] = 'Došlo k interní chybě nebo server neodpověděl na požadavek.';
+$lang['GoGetSSL.!error.meta[gogetssl_name].empty'] = 'Vyberte alespoň jednoho prodejce.';
+$lang['GoGetSSL.!error.meta[product].valid'] = 'Vyberte si prosím produkt.';
+$lang['GoGetSSL.!error.gogetssl_webserver_type.format'] = 'Zadejte platný typ webového serveru.';
+$lang['GoGetSSL.!error.gogetssl_fqdn.format'] = 'Zadejte prosím platnou doménu.';
+$lang['GoGetSSL.!error.gogetssl_csr.format'] = 'Zadejte prosím hodnotu CSR.';
+$lang['GoGetSSL.!error.gogetssl_approver_email.format'] = 'Zadejte prosím platný e-mail schvalovatele.';
+$lang['GoGetSSL.!error.gogetssl_validity.empty'] = 'Zadejte prosím dobu platnosti.';
+$lang['GoGetSSL.!error.gogetssl_name.empty'] = 'Zadejte název prodejce.';
+$lang['GoGetSSL.!error.api_password.empty'] = 'Zadejte prosím heslo API.';
+$lang['GoGetSSL.!error.api_username.valid'] = 'Připojení k serveru se nepodařilo navázat. Zkontrolujte, zda jsou pověření API správná.';
+$lang['GoGetSSL.!error.api_username.empty'] = 'Zadejte prosím uživatelské jméno API.';
+
