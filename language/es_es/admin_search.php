885c7873e43383ddbdcfa15836a71be87814b3be
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:59 -0800

    Updated es_es language files

---
+++ language/es_es/admin_search.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Admin Search
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSearch.!notice.results_truncated'] = 'El número de resultados se ha truncado. Puede que desee afinar su búsqueda.';
+$lang['AdminSearch.index.page_title'] = 'Resultados de la búsqueda de "%1$s"';
+$lang['AdminSearch.index.boxtitle_search'] = 'Resultados de la búsqueda de "%1$s"';
+$lang['AdminSearch.clients.heading'] = 'Clientes';
+$lang['AdminSearch.clients.heading_client_id'] = 'ID de cliente';
+$lang['AdminSearch.clients.heading_group'] = 'Grupo';
+$lang['AdminSearch.clients.heading_name'] = 'Nombre';
+$lang['AdminSearch.clients.heading_company'] = 'Empresa';
+$lang['AdminSearch.clients.heading_address'] = 'Dirección';
+$lang['AdminSearch.clients.heading_email'] = 'Correo electrónico';
+$lang['AdminSearch.clients.no_results'] = 'No hay clientes que coincidan con los criterios de búsqueda.';
+$lang['AdminSearch.invoices.heading'] = 'Facturas';
+$lang['AdminSearch.invoice.heading_invoice'] = 'Factura #';
+$lang['AdminSearch.invoice.heading_client_id'] = 'ID de cliente';
+$lang['AdminSearch.invoice.heading_amount'] = 'Opciones';
+$lang['AdminSearch.invoice.heading_paid'] = 'Pagado';
+$lang['AdminSearch.invoice.heading_due'] = 'Debido';
+$lang['AdminSearch.invoice.heading_date_closed'] = 'Fecha de cierre';
+$lang['AdminSearch.invoice.heading_date_billed'] = 'Fecha de facturación';
+$lang['AdminSearch.invoice.heading_date_due'] = 'Fecha de vencimiento';
+$lang['AdminSearch.invoice.heading_sent'] = 'Enviado';
+$lang['AdminSearch.invoice.heading_status'] = 'Estado';
+$lang['AdminSearch.invoice.option_view'] = 'Ver';
+$lang['AdminSearch.invoices.status_open'] = 'Abrir';
+$lang['AdminSearch.invoices.status_closed'] = 'Cerrado';
+$lang['AdminSearch.invoices.status_void'] = 'Anulado';
+$lang['AdminSearch.invoices.status_draft'] = 'Borrador';
+$lang['AdminSearch.invoices.status_sent'] = 'Enviado';
+$lang['AdminSearch.invoices.status_unsent'] = 'No enviado';
+$lang['AdminSearch.invoice.no_results'] = 'No hay facturas que coincidan con los criterios de búsqueda.';
+$lang['AdminSearch.transactions.heading'] = 'Transacciones';
+$lang['AdminSearch.transactions.heading_client_id'] = 'ID de cliente';
+$lang['AdminSearch.transactions.heading_type'] = 'Tipo';
+$lang['AdminSearch.transactions.heading_amount'] = 'Importe';
+$lang['AdminSearch.transactions.heading_credited'] = 'Acreditado';
+$lang['AdminSearch.transactions.heading_applied'] = 'Aplicada';
+$lang['AdminSearch.transactions.heading_number'] = 'Número';
+$lang['AdminSearch.transactions.heading_status'] = 'Estado';
+$lang['AdminSearch.transactions.heading_date'] = 'Fecha';
+$lang['AdminSearch.transactions.heading_options'] = 'Opciones';
+$lang['AdminSearch.transactions.option_edit'] = 'Editar';
+$lang['AdminSearch.transactions.no_results'] = 'No hay transacciones que coincidan con los criterios de búsqueda.';
+$lang['AdminSearch.packages.heading'] = 'Paquetes';
+$lang['AdminSearch.packages.heading_package_id'] = 'ID del paquete';
+$lang['AdminSearch.packages.heading_name'] = 'Nombre';
+$lang['AdminSearch.packages.heading_module'] = 'Módulo';
+$lang['AdminSearch.packages.heading_quantity'] = 'Cantidad';
+$lang['AdminSearch.packages.heading_status'] = 'Estado';
+$lang['AdminSearch.packages.no_results'] = 'No hay paquetes que coincidan con los criterios de búsqueda.';
+$lang['AdminSearch.services.heading'] = 'Servicios';
+$lang['AdminSearch.services.heading_client_id'] = 'ID de cliente';
+$lang['AdminSearch.services.heading_client'] = 'Cliente';
+$lang['AdminSearch.services.heading_package'] = 'Paquete';
+$lang['AdminSearch.services.heading_term'] = 'Plazo';
+$lang['AdminSearch.services.heading_date_added'] = 'Fecha de creación';
+$lang['AdminSearch.services.heading_date_renews'] = 'Fecha Renueva';
+$lang['AdminSearch.services.heading_status'] = 'Estado';
+$lang['AdminSearch.services.text_never'] = 'Nunca';
+$lang['AdminSearch.services.no_results'] = 'No hay servicios que coincidan con los criterios de búsqueda.';
+
