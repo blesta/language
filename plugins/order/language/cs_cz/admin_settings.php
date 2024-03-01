2d23d5d6a9b73e834fb606cea7024e71c00b859f
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:03 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/admin_settings.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Admin Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSettings.settings.submit_cancel'] = 'Zrušit';
+$lang['AdminSettings.settings.submit_settings'] = 'Uložit nastavení';
+$lang['AdminSettings.settings.text_inheritance'] = 'Nastavení pobočky společnosti jsou děděna nově vytvořenými pobočkami, ale poté jsou přepsána nastavením jednotlivých poboček.';
+$lang['AdminSettings.settings.zero_amount'] = 'Výše výběru je v současné době nulová, což znamená, že partneři neobdrží žádné prostředky z doporučení.';
+$lang['AdminSettings.settings.tooltip_signup_content'] = 'Obsah HTML, který se zobrazí klientům na partnerské stránce, pokud ještě nejsou zaregistrováni.';
+$lang['AdminSettings.settings.field_signup_content'] = 'Obsah registrace';
+$lang['AdminSettings.settings.field_withdrawal_currency'] = 'Měna výběru';
+$lang['AdminSettings.settings.field_max_withdrawal_amount'] = 'Maximální částka výběru';
+$lang['AdminSettings.settings.field_min_withdrawal_amount'] = 'Minimální částka výběru';
+$lang['AdminSettings.settings.tooltip_maturity_days'] = 'Počet dní, po kterých je doporučení splatné a je k dispozici pro výplatu.';
+$lang['AdminSettings.settings.field_maturity_days'] = 'Dny zralosti';
+$lang['AdminSettings.settings.tooltip_order_recurring'] = 'Zda budou za objednané služby, které se obnovují, zaplaceny referraly.';
+$lang['AdminSettings.settings.field_order_recurring'] = 'Objednávka Opakující se';
+$lang['AdminSettings.settings.tooltip_order_frequency'] = 'Zaprvé platit doporučeným osobám pouze za první objednávku, kterou klient provede pomocí odkazu, nebo zadruhé platit doporučeným osobám za každou objednávku, kterou klient provede pomocí odkazu.';
+$lang['AdminSettings.settings.field_order_frequency'] = 'Frekvence objednávek';
+$lang['AdminSettings.settings.field_commission_amount'] = 'Výše provize';
+$lang['AdminSettings.settings.tooltip_commission_type'] = 'Metoda výpočtu použitá pro stanovení provize za postoupení.  Buď pevná hodnota za objednávku, nebo procento z celkové objednávky.';
+$lang['AdminSettings.settings.field_commission_type'] = 'Typ provize';
+$lang['AdminSettings.settings.tooltip_cookie_tld'] = 'Dny, po které zůstává affiliate cookie platný po následování odkazu.';
+$lang['AdminSettings.settings.field_cookie_tld'] = 'Doba trvání souboru cookie (ve dnech)';
+$lang['AdminSettings.settings.boxtitle_settings'] = 'Obecná nastavení';
+$lang['AdminSettings.!success.settings_updated'] = 'Nastavení poboček bylo aktualizováno.';
+
