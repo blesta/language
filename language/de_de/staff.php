816e0e0bc9f912c7535c582501dc77fadc66c463
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:18 -0800

    Updated de_de language files

---
+++ language/de_de/staff.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Staff.!error.action[].exists'] = 'Mindestens eine der E-Mail-Gruppenaktionen ist nicht vorhanden.';
+$lang['Staff.!error.action.exists'] = 'Die E-Mail-Gruppenaktion %1$s existiert nicht.';
+$lang['Staff.!error.staff_id.exists'] = 'Ungültige Personal-ID.';
+$lang['Staff.!error.staff_group_id.exists'] = 'Ungültige Personalgruppen-ID.';
+$lang['Staff.!error.order.length'] = 'Die Länge der Sortierreihenfolge darf 5 Zeichen nicht überschreiten.';
+$lang['Staff.!error.order.format'] = 'Die Sortierreihenfolge muss eine Zahl sein.';
+$lang['Staff.!error.title.empty'] = 'Bitte geben Sie einen Titel ein.';
+$lang['Staff.!error.uri.empty'] = 'Bitte geben Sie eine URI ein.';
+$lang['Staff.!error.company_id.format'] = 'Die Unternehmens-ID muss eine Nummer sein.';
+$lang['Staff.!error.staff_id.format'] = 'Die Personal-ID muss eine Nummer sein.';
+$lang['Staff.!error.groups.unique_company'] = 'Einem Mitarbeiter kann nur eine Personalgruppe pro Unternehmen zugewiesen werden.';
+$lang['Staff.!error.groups[].format'] = 'Es gibt eine oder mehrere ungültige Gruppen-IDs.';
+$lang['Staff.!error.status.format'] = 'Ungültiger Status.';
+$lang['Staff.!error.email.format'] = 'Bitte geben Sie eine gültige E-Mail Adresse ein.';
+$lang['Staff.!error.last_name.empty'] = 'Bitte geben Sie einen Nachnamen ein.';
+$lang['Staff.!error.first_name.empty'] = 'Bitte geben Sie einen Vornamen ein.';
+$lang['Staff.!error.user_id.unique'] = 'Die Mitarbeiter-Benutzer-ID %1$s ist bereits vergeben.';
+$lang['Staff.!error.user_id.exists'] = 'Ungültige Benutzer-ID.';
+
