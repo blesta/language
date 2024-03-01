05dc643dbb3f24243a184606fae08f188c3bb08b
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:30 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_login.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Admin Login
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminLogin.!info.otp'] = 'Pro tohoto uživatele je vyžadováno dvoufaktorové ověřování. Níže zadejte své jednorázové heslo (OTP).';
+$lang['AdminLogin.!info.reset_password'] = 'Níže zadejte své uživatelské jméno a e-mailem vám bude zaslán časově omezený odkaz, abyste si mohli nastavit nové heslo.';
+$lang['AdminLogin.!success.reset_sent'] = 'Na zadanou adresu byl odeslán potvrzovací e-mail.';
+$lang['AdminLogin.!error.captcha.invalid'] = 'Zadaná captcha byla neplatná. Zkuste to prosím znovu.';
+$lang['AdminLogin.!error.unknown_user'] = 'Toto uživatelské jméno není rozpoznáno nebo heslo nelze obnovit.';
+$lang['AdminLogin.setup.field_submit'] = 'Dokončení';
+$lang['AdminLogin.setup.field_enter_key_false'] = 'Chci zahájit 30denní bezplatnou zkušební verzi';
+$lang['AdminLogin.setup.field_enter_key_true'] = 'Mám licenční klíč k zadání';
+$lang['AdminLogin.setup.field_confirm_password'] = 'Potvrzení hesla';
+$lang['AdminLogin.setup.field_password'] = 'Heslo';
+$lang['AdminLogin.setup.field_email'] = 'E-mailová adresa';
+$lang['AdminLogin.setup.field_username'] = 'Uživatelské jméno';
+$lang['AdminLogin.setup.field_last_name'] = 'Příjmení';
+$lang['AdminLogin.setup.field_first_name'] = 'Jméno';
+$lang['AdminLogin.setup.heading_create_account'] = 'Vytvoření účtu zaměstnance';
+$lang['AdminLogin.setup.field_newsletter'] = 'Přihlaste se k odběru našeho newsletteru. Z odběru se můžete kdykoli odhlásit.';
+$lang['AdminLogin.setup.trial_newsletter'] = 'Přihlášením ke zkušební verzi souhlasíte s tím, že od nás budete během zkušební verze dostávat e-maily. Kdykoli se můžete odhlásit.';
+$lang['AdminLogin.setup.field_license_key'] = 'Licenční klíč';
+$lang['AdminLogin.setup.title_adminarea'] = 'Počáteční nastavení';
+$lang['AdminLogin.setup.page_title'] = 'Počáteční nastavení';
+$lang['AdminLogin.confirmreset.link_login'] = 'Zrušit, Přihlásit se';
+$lang['AdminLogin.confirmreset.field_resetsubmit'] = 'Nastavení hesla';
+$lang['AdminLogin.confirmreset.field_confirm_password'] = 'Potvrzení nového hesla';
+$lang['AdminLogin.confirmreset.field_new_password'] = 'Nové heslo';
+$lang['AdminLogin.confirmreset.title_adminarea'] = '%1$s | Potvrzení obnovení hesla';
+$lang['AdminLogin.confirmreset.page_title'] = 'Potvrzení obnovení hesla';
+$lang['AdminLogin.reset.link_login'] = 'Zrušit, Přihlásit se';
+$lang['AdminLogin.reset.field_resetsubmit'] = 'Obnovení hesla';
+$lang['AdminLogin.reset.field_username'] = 'Uživatelské jméno';
+$lang['AdminLogin.reset.title_adminarea'] = '%1$s | Obnovení hesla';
+$lang['AdminLogin.reset.page_title'] = 'Obnovení hesla';
+$lang['AdminLogin.otp.link_login'] = 'Zrušit, Přihlásit se';
+$lang['AdminLogin.otp.field_loginsubmit'] = 'Přihlášení';
+$lang['AdminLogin.otp.field_password'] = 'Jednorázové heslo';
+$lang['AdminLogin.otp.title_adminarea'] = '%1$s | Oblast pro zaměstnance';
+$lang['AdminLogin.otp.page_title'] = 'Přihlášení OTP';
+$lang['AdminLogin.index.link_resetpassword'] = 'Obnovení hesla';
+$lang['AdminLogin.index.field_loginsubmit'] = 'Přihlášení';
+$lang['AdminLogin.index.field_rememberme'] = 'Zapamatujte si mě na tomto počítači.';
+$lang['AdminLogin.index.field_password'] = 'Heslo';
+$lang['AdminLogin.index.field_username'] = 'Uživatelské jméno';
+$lang['AdminLogin.index.title_adminarea'] = '%1$s | Oblast pro zaměstnance';
+$lang['AdminLogin.index.page_title'] = 'Přihlášení';
+
