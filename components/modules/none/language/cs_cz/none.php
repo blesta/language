934fc0d3d242f88728b1e1313b4518f610a1ca87
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:31 -0800

    Updated cs_cz language files

---
+++ components/modules/none/language/cs_cz/none.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * None
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['None.manage.text'] = 'U tohoto modulu není co spravovat.';
+$lang['None.name'] = 'Žádné';
+
