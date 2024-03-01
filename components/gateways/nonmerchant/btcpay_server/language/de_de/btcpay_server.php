9db5f85e4b3c8aa7794179bf01d74d518a28bb67
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:03 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/btcpay_server/language/de_de/btcpay_server.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Btcpay Server
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BtcpayServer.buildprocess.submit'] = 'Zahlung einreichen';
+$lang['BtcpayServer.webhook_note'] = 'Bevor Sie mit der Nutzung dieses Gateways beginnen, müssen Sie die folgende URL als Webhook in Ihrem BTCPay-Server konfigurieren.';
+$lang['BtcpayServer.webhook'] = 'BTCPay Server Webhook';
+$lang['BtcpayServer.transaction.speed.low'] = 'Niedrig';
+$lang['BtcpayServer.transaction.speed.medium'] = 'Mittel';
+$lang['BtcpayServer.transaction.speed.high'] = 'Hoch';
+$lang['BtcpayServer.meta.webhook_secret'] = 'Webhook Geheimnis';
+$lang['BtcpayServer.meta.transaction_speed'] = 'Transaktionsgeschwindigkeit';
+$lang['BtcpayServer.meta.api_key'] = 'API-Schlüssel';
+$lang['BtcpayServer.meta.store_id'] = 'Speicher-ID';
+$lang['BtcpayServer.meta.server_url'] = 'Server-URL';
+$lang['BtcpayServer.!error.webhook_secret.valid'] = 'Sie müssen ein gültiges Webhook-Geheimnis eingeben.';
+$lang['BtcpayServer.!error.payment.expired'] = 'Die Transaktion ist abgelaufen und konnte nicht bearbeitet werden.';
+$lang['BtcpayServer.!error.payment.invalid'] = 'Die Transaktion ist ungültig und konnte nicht verarbeitet werden.';
+$lang['BtcpayServer.!error.failed.response'] = 'Die Transaktion konnte nicht verarbeitet werden.';
+$lang['BtcpayServer.!error.api_key.valid'] = 'API-Schlüssel ungültig.';
+$lang['BtcpayServer.!error.store_id.valid'] = 'Store ID ungültig.';
+$lang['BtcpayServer.!error.transaction_speed.valid'] = 'Bitte wählen Sie eine gültige Transaktionsgeschwindigkeit.';
+$lang['BtcpayServer.!error.server_url.valid'] = 'Server-URL ungültig.';
+$lang['BtcpayServer.description'] = 'BTCPayServer ist ein selbstgehostetes, gebührenfreies Bitcoin-Zahlungsgateway.';
+$lang['BtcpayServer.name'] = 'BTCPay (Bitcoin)';
+
