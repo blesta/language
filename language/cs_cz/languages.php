1162181e1921380a8a0c7428efa2e650e3f86f9e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:02 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/languages.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Languages
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Languages.!error.name.format'] = 'Název jazyka nesmí být prázdný.';
+$lang['Languages.!error.company_id.format'] = 'ID společnosti se zdá být neplatné.';
+$lang['Languages.!error.code.unique'] = 'Tento jazykový kód již pro tuto společnost existuje.';
+$lang['Languages.!error.code.format'] = 'Kód jazyka není správně naformátován.';
+$lang['Languages.!error.code.valid'] = 'Výchozí jazyk "cs_us" nelze odstranit.';
+
