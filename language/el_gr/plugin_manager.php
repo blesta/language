b0e8917e3f74726a1494d06b531ea9394b6bda9b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:31 -0800

    Updated el_gr language files

---
+++ language/el_gr/plugin_manager.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PluginManager.!error.background_type.valid'] = 'Ο τύπος φόντου πρέπει να έχει οριστεί σε "χρώμα" ή "εικόνα".';
+$lang['PluginManager.!error.background.valid'] = 'Το φόντο πρέπει να είναι μια έγκυρη διεύθυνση URL για μια εικόνα.';
+$lang['PluginManager.!error.background.empty'] = 'Εισάγετε ένα χρώμα φόντου ή μια διεύθυνση URL εικόνας.';
+$lang['PluginManager.!error.text_color.empty'] = 'Παρακαλώ εισάγετε ένα χρώμα κειμένου.';
+$lang['PluginManager.!error.link.empty'] = 'Παρακαλώ εισάγετε έναν σύνδεσμο.';
+$lang['PluginManager.!error.label.empty'] = 'Παρακαλώ εισάγετε μια ετικέτα.';
+$lang['PluginManager.!error.callback_type.valid'] = 'Ο τύπος της επανάκλησης πρέπει να έχει οριστεί σε "value" ή "html".';
+$lang['PluginManager.!error.callback.unique'] = 'Το callback πρέπει να είναι μοναδικό για αυτή την κάρτα πρόσθετου.';
+$lang['PluginManager.!error.callback.exists'] = 'Δεν βρέθηκε το callback της κάρτας του πρόσθετου.';
+$lang['PluginManager.!error.level.valid'] = 'Το επίπεδο πρέπει να είναι "πελάτης" ή "προσωπικό".';
+$lang['PluginManager.!error.callback.empty'] = 'Παρακαλούμε εισάγετε μια επανάκληση.';
+$lang['PluginManager.!error.event.exists'] = 'Το συμβάν του πρόσθετου δεν βρέθηκε.';
+$lang['PluginManager.!error.event.length'] = 'Το μήκος του συμβάντος δεν μπορεί να υπερβαίνει τους 128 χαρακτήρες.';
+$lang['PluginManager.!error.event.empty'] = 'Παρακαλώ εισάγετε ένα γεγονός.';
+$lang['PluginManager.!error.version.length'] = 'Το μήκος της έκδοσης του πρόσθετου δεν μπορεί να υπερβαίνει τους 16 χαρακτήρες.';
+$lang['PluginManager.!error.version.empty'] = 'Παρακαλώ εισάγετε την έκδοση του πρόσθετου.';
+$lang['PluginManager.!error.author.empty'] = 'Παρακαλώ εισάγετε τον συγγραφέα του πρόσθετου.';
+$lang['PluginManager.!error.name.empty'] = 'Παρακαλούμε εισάγετε ένα όνομα πρόσθετου.';
+$lang['PluginManager.!error.company_id.exists'] = 'Μη έγκυρο αναγνωριστικό εταιρείας.';
+$lang['PluginManager.!error.dir.length'] = 'Το μήκος του καταλόγου του πρόσθετου δεν μπορεί να υπερβαίνει τους 64 χαρακτήρες.';
+$lang['PluginManager.!error.dir.empty'] = 'Παρακαλούμε εισάγετε τον κατάλογο του πρόσθετου.';
+
