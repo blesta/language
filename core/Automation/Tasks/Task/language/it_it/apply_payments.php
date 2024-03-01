a150c5ca83b6bf39c67b4cd34cd65e786155bc06
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:58 -0800

    Updated it_it language files

---
+++ core/Automation/Tasks/Task/language/it_it/apply_payments.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Apply Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.apply_payments.apply_none'] = 'Non ci sono fatture a cui possono essere applicati i crediti.';
+$lang['Automation.task.apply_payments.apply_success'] = 'Applicati con successo i crediti in sospeso dalla transazione %1$s per il cliente #%2$s alla fattura #%3$s per un importo di %4$s.';
+$lang['Automation.task.apply_payments.apply_failed'] = 'Impossibile applicare i crediti in sospeso per il cliente #%1$s.';
+$lang['Automation.task.apply_payments.completed_group'] = 'Completata l\'applicazione dei crediti per il gruppo di clienti %1$s.';
+$lang['Automation.task.apply_payments.attempt_group'] = 'Tentativo di applicare i crediti per il gruppo di clienti %1$s.';
+$lang['Automation.task.apply_payments.completed'] = 'L\'attività di applicazione dei crediti è stata completata.';
+$lang['Automation.task.apply_payments.attempt'] = 'Tentativo di applicare i crediti alle fatture aperte.';
+
