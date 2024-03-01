3f37fc70d969e144eab1a96d8cbb656d30d51733
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:15 -0800

    Updated de_de language files

---
+++ language/de_de/pricings.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Pricings
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pricings.getPeriods.onetime_plural'] = 'Einmalig';
+$lang['Pricings.getPeriods.year_plural'] = 'Jahre';
+$lang['Pricings.getPeriods.month_plural'] = 'Monate';
+$lang['Pricings.getPeriods.week_plural'] = 'Wochen';
+$lang['Pricings.getPeriods.day_plural'] = 'Tage';
+$lang['Pricings.getPeriods.onetime'] = 'Einmalig';
+$lang['Pricings.getPeriods.year'] = 'Jahr';
+$lang['Pricings.getPeriods.month'] = 'Monat';
+$lang['Pricings.getPeriods.week'] = 'Woche';
+$lang['Pricings.getPeriods.day'] = 'Tag';
+$lang['Pricings.!error.currency.in_use'] = 'Die Währung kann nicht in einer Preisfindung aktualisiert werden, die in Gebrauch ist.';
+$lang['Pricings.!error.period.in_use'] = 'Die Periode kann nicht in einer Preisfindung aktualisiert werden, die in Gebrauch ist.';
+$lang['Pricings.!error.term.in_use'] = 'Der Begriff kann nicht in einer Preisfindung aktualisiert werden, die in Gebrauch ist.';
+$lang['Pricings.!error.currency.format'] = 'Der Währungscode muss aus 3 Zeichen bestehen.';
+$lang['Pricings.!error.cancel_fee.format'] = 'Die Stornogebühr muss eine Zahl sein.';
+$lang['Pricings.!error.setup_fee.format'] = 'Die Einrichtungsgebühr muss eine Zahl sein.';
+$lang['Pricings.!error.price_transfer.format'] = 'Der Erneuerungspreis muss eine Zahl sein.';
+$lang['Pricings.!error.price_renews.valid'] = 'Der Verlängerungspreis kann nicht für einen bestimmten Zeitraum festgelegt werden.';
+$lang['Pricings.!error.price_renews.format'] = 'Der Erneuerungspreis muss eine Zahl sein.';
+$lang['Pricings.!error.price.format'] = 'Der Preis muss eine Zahl sein.';
+$lang['Pricings.!error.period.format'] = 'Ungültiger Periodentyp.';
+$lang['Pricings.!error.term.valid'] = 'Der Term muss größer als 0 sein.';
+$lang['Pricings.!error.term.length'] = 'Die Länge des Begriffs darf 5 Zeichen nicht überschreiten.';
+$lang['Pricings.!error.term.format'] = 'Der Begriff muss eine Zahl sein.';
+
