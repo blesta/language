b24bffbd88a1a25100f7c52f30893d80c3859b32
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:45 -0800

    Updated el_gr language files

---
+++ plugins/client_documents/language/el_gr/client_documents_files.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Client Documents Files
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDocumentsFiles.!error.date_added.valid'] = 'Απαιτείται η προσθήκη έγκυρης ημερομηνίας.';
+$lang['ClientDocumentsFiles.!error.name.valid'] = 'Απαιτείται ένα όνομα εγγράφου.';
+$lang['ClientDocumentsFiles.!error.client_id.exists'] = 'Ο πελάτης δεν υπάρχει.';
+
