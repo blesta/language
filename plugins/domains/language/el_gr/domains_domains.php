566ba9260449f5e3520f0c0bd976778bab857b98
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:51 -0800

    Updated el_gr language files

---
+++ plugins/domains/language/el_gr/domains_domains.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Domains Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsDomains.!error.invalid_term'] = 'Αυτός ο τομέας δεν μπορεί να ανανεωθεί για τα επιλεγμένα έτη.';
+$lang['DomainsDomains.!error.invoices_renew_service'] = 'Ο τομέας δεν μπορεί να ανανεωθεί έως ότου εξοφληθούν όλα τα τιμολόγια που περιέχουν αυτόν τον τομέα.';
+
