e700f2abb45e96adf8c197189697194be4ab10c5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:33 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/forms.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Forms
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Forms.index.order_now'] = 'Commander';
+$lang['Forms.index.register'] = 'Registre';
+
