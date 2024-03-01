c7a22b0551bf3374bb5d0cbdb5ced25ac38c6be1
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:14 -0800

    Updated it_it language files

---
+++ components/gateways/nonmerchant/checkout2/language/it_it/checkout2.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Checkout2
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Checkout2.!error.sid.valid'] = 'Il numero di conto del fornitore non corrisponde al numero di conto fornito dalla transazione.';
+$lang['Checkout2.!error.credit_card_processed.completed'] = 'La transazione non è stata elaborata con successo.';
+$lang['Checkout2.!error.hash.valid'] = 'L\'hash utilizzato per verificare la provenienza della vendita da 2Checkout non è valido.';
+$lang['Checkout2.!error.key.valid'] = 'La chiave utilizzata per verificare che questa vendita provenga da 2Checkout non è valida.';
+$lang['Checkout2.!error.sandbox.valid'] = 'Sandbox deve essere impostato su "true" o "false".';
+$lang['Checkout2.!error.test_mode.valid'] = 'La modalità di test deve essere impostata su "true" o "false".';
+$lang['Checkout2.!error.secret_key.empty'] = 'Inserire la chiave segreta.';
+$lang['Checkout2.!error.buy_link_secret_word.empty'] = 'Inserire la parola segreta Buy Link.';
+$lang['Checkout2.!error.secret_word.empty'] = 'Inserisci la tua parola segreta.';
+$lang['Checkout2.!error.merchant_code.empty'] = 'Inserire il codice commerciante.';
+$lang['Checkout2.!error.vendor_id.empty'] = 'Inserire il numero di conto del fornitore.';
+$lang['Checkout2.!error.api_version.valid'] = 'Inserire una versione API valida.';
+$lang['Checkout2.getapiversions.v5'] = 'Versione 5';
+$lang['Checkout2.getapiversions.v1'] = 'Versione 1 (Legacy)';
+$lang['Checkout2.buildprocess.submit'] = 'Pagare con 2Checkout';
+$lang['Checkout2.refund.comment'] = 'Avvio di un rimborso per %1$s.';
+$lang['Checkout2.test_mode'] = 'Modalità test';
+$lang['Checkout2.sandbox'] = 'Sandbox';
+$lang['Checkout2.api_password'] = 'Password API';
+$lang['Checkout2.api_username_note'] = 'Questa, così come la password API, sono necessarie per elaborare i rimborsi attraverso 2Checkout.';
+$lang['Checkout2.api_username'] = 'Nome utente API';
+$lang['Checkout2.buy_link_secret_word'] = 'Acquista Link Parola segreta';
+$lang['Checkout2.secret_word'] = 'Parola segreta';
+$lang['Checkout2.secret_key'] = 'Chiave segreta';
+$lang['Checkout2.merchant_code'] = 'Codice commerciante';
+$lang['Checkout2.vendor_id'] = 'Numero di conto del fornitore';
+$lang['Checkout2.api_version'] = 'Versione API';
+$lang['Checkout2.description'] = '2Checkout.com è un servizio di elaborazione dei pagamenti online che consente di accettare carte di credito, PayPal e carte di debito. Oltre 17.000 aziende in più di 180 paesi utilizzano 2Checkout';
+$lang['Checkout2.name'] = '2Checkout';
+
