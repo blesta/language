f2078f3a303ad19b11dafb149b42ebbf29fb3bbd
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:07 -0800

    Updated cs_cz language files

---
+++ core/Util/DataFeed/Feeds/language/cs_cz/client_feed.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Client Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientFeed.!error.invalid_endpoint'] = 'Požadovaný koncový bod není platný nebo neexistuje.';
+$lang['ClientFeed.getOptionFields.example_code_fraud'] = 'Zobrazte počet podvodných klientů:';
+$lang['ClientFeed.getOptionFields.example_code_inactive'] = 'Zobrazení počtu neaktivních klientů:';
+$lang['ClientFeed.getOptionFields.example_code_active'] = 'Zobrazení počtu aktivních klientů:';
+$lang['ClientFeed.getOptionFields.title_row_example_code'] = 'Příklad kódu';
+$lang['ClientFeed.description'] = 'Vrací počet klientů určitého stavu.';
+$lang['ClientFeed.name'] = 'Klienti';
+
