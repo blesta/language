d178716fb93ea24ab7ded981332aafedd0e3ddeb
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:38 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/service_invoices.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Service Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ServiceInvoices.!error.service_id.exists'] = 'ID de service non valide.';
+$lang['ServiceInvoices.!error.invoice_id.exists'] = 'ID de la facture non valide.';
+$lang['ServiceInvoices.!error.failed_attempts.format'] = 'Les tentatives échouées doivent être un nombre.';
+$lang['ServiceInvoices.!error.maximum_attempts.format'] = 'Le nombre maximum de tentatives doit être un nombre.';
+$lang['ServiceInvoices.!error.date_next_attempt.format'] = 'La date de la prochaine tentative doit être une date.';
+
