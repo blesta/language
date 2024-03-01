4bb8f74d10b57d061871fbfd41eeaf8b10e59577
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:39 -0800

    Updated cs_cz language files

---
+++ plugins/download_manager/language/cs_cz/download_manager_urls.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Download Manager Urls
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerUrls.!error.file_id.exists'] = 'Vybraný soubor neexistuje.';
+$lang['DownloadManagerUrls.!error.category_id.exists'] = 'Vybraná kategorie neexistuje.';
+$lang['DownloadManagerUrls.!error.url.format'] = 'Formát adresy URL není platný, může obsahovat pouze písmena, číslice a pomlčky.';
+$lang['DownloadManagerUrls.!error.url.match'] = 'Tato adresa URL neodpovídá příponě souboru.';
+$lang['DownloadManagerUrls.!error.url.exists'] = 'Tato adresa URL již existuje.';
+$lang['DownloadManagerUrls.!error.company_id.exists'] = 'Neplatné ID společnosti.';
+
