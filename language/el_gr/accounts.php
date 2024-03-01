a9fdc8bbb72f688cbe12c49fdf2760d2a7418b14
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:38 -0800

    Updated el_gr language files

---
+++ language/el_gr/accounts.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Accounts.getCcTypes.other'] = 'Άλλα';
+$lang['Accounts.getCcTypes.visa'] = 'Visa';
+$lang['Accounts.getCcTypes.switch'] = 'Διακόπτης';
+$lang['Accounts.getCcTypes.solo'] = 'Σόλο';
+$lang['Accounts.getCcTypes.mc'] = 'Master Card';
+$lang['Accounts.getCcTypes.maes'] = 'Μαέστρος';
+$lang['Accounts.getCcTypes.lasr'] = 'Laser';
+$lang['Accounts.getCcTypes.jcb'] = 'Ιαπωνικό πιστωτικό γραφείο';
+$lang['Accounts.getCcTypes.ipi'] = 'InstaPayment';
+$lang['Accounts.getCcTypes.disc'] = 'Ανακαλύψτε το';
+$lang['Accounts.getCcTypes.dc-uc'] = 'Diners Club ΗΠΑ και Καναδάς';
+$lang['Accounts.getCcTypes.dc-int'] = 'Diners Club International';
+$lang['Accounts.getCcTypes.dc-er'] = 'Diners Club EnRoute';
+$lang['Accounts.getCcTypes.dc-cb'] = 'Diners Club Carte Blanche';
+$lang['Accounts.getCcTypes.cup'] = 'Πληρωμή της Ένωσης της Κίνας';
+$lang['Accounts.getCcTypes.bc'] = 'Τραπεζική κάρτα';
+$lang['Accounts.getCcTypes.amex'] = 'American Express';
+$lang['Accounts.getAchTypes.savings'] = 'Αποταμίευση';
+$lang['Accounts.getAchTypes.checking'] = 'Έλεγχος';
+$lang['Accounts.getTypes.other'] = 'Άλλα';
+$lang['Accounts.getTypes.ach'] = 'Αυτοματοποιημένος Οίκος Εκκαθάρισης';
+$lang['Accounts.getTypes.cc'] = 'Πιστωτική κάρτα';
+$lang['Accounts.!error.cc_account_id.exists'] = 'Μη έγκυρο αναγνωριστικό λογαριασμού.';
+$lang['Accounts.!error.type.cc_format'] = 'Ο τύπος της κάρτας είναι άκυρος ή δεν μπόρεσε να προσδιοριστεί.';
+$lang['Accounts.!error.last4.empty_cc'] = 'Παρακαλούμε εισάγετε τα τέσσερα τελευταία ψηφία του αριθμού της πιστωτικής κάρτας.';
+$lang['Accounts.!error.expiration.valid'] = 'Η ημερομηνία λήξης έχει ήδη παρέλθει.';
+$lang['Accounts.!error.expiration.empty'] = 'Παρακαλώ εισάγετε την ημερομηνία λήξης της πιστωτικής κάρτας.';
+$lang['Accounts.!error.number.valid'] = 'Ο αριθμός της κάρτας που δόθηκε είναι άκυρος.';
+$lang['Accounts.!error.ach_account_id.exists'] = 'Μη έγκυρο αναγνωριστικό λογαριασμού.';
+$lang['Accounts.!error.type.ach_format'] = 'Μη έγκυρος τύπος.';
+$lang['Accounts.!error.last4.empty_ach'] = 'Εισάγετε τα τέσσερα τελευταία ψηφία του αριθμού λογαριασμού.';
+$lang['Accounts.!error.routing.empty'] = 'Παρακαλούμε καθορίστε έναν αριθμό δρομολόγησης.';
+$lang['Accounts.!error.account.length'] = 'Παρακαλούμε καθορίστε έναν αριθμό λογαριασμού.';
+$lang['Accounts.!error.currency.format'] = 'Το νόμισμα είναι άκυρο.';
+$lang['Accounts.!error.state.country_exists'] = 'Επιλέξτε τη χώρα που αντιστοιχεί στην επιλεγμένη πολιτεία.';
+$lang['Accounts.!error.country.length'] = 'Το μήκος της χώρας δεν μπορεί να υπερβαίνει τους 3 χαρακτήρες.';
+$lang['Accounts.!error.state.length'] = 'Το μήκος της κατάστασης δεν μπορεί να υπερβαίνει τους 3 χαρακτήρες.';
+$lang['Accounts.!error.last_name.empty'] = 'Παρακαλείστε να καθορίσετε ένα επώνυμο.';
+$lang['Accounts.!error.first_name.empty'] = 'Παρακαλούμε καθορίστε ένα μικρό όνομα.';
+$lang['Accounts.!error.contact_id.exists'] = 'Μη έγκυρο αναγνωριστικό επαφής.';
+
