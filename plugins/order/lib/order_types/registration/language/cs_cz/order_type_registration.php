e71166ac0079641a972f68ef74c9917387bfc13a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:18 -0800

    Updated cs_cz language files

---
+++ plugins/order/lib/order_types/registration/language/cs_cz/order_type_registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Order Type Registration
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderTypeRegistration.!error.template.valid'] = 'Registrace klienta podporuje pouze šablonu Standard.';
+$lang['OrderTypeRegistration.!success.signup'] = 'Děkujeme, že jste si u nás zaregistrovali účet.';
+$lang['OrderTypeRegistration.name'] = 'Registrace klienta';
+
