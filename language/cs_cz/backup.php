77cd40bc9957974cd4fe5318132dfc8598ef3851
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:41 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/backup.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Backup
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Backup.frequencies.days'] = '%1$s dní';
+$lang['Backup.frequencies.day'] = 'Den';
+$lang['Backup.frequencies.hours'] = '%1$s hodin';
+$lang['Backup.frequencies.hour'] = 'Hodina';
+$lang['Backup.frequencies.never'] = 'Nikdy';
+$lang['Backup.!error.amazons3_failed'] = 'Zálohování AmazonS3 selhalo.';
+$lang['Backup.!error.ftp_failed'] = 'Zálohování SFTP se nezdařilo.';
+$lang['Backup.!error.temp_dir.writable'] = 'Do adresáře temp nelze zapisovat.';
+$lang['Backup.!error.driver.support'] = 'Ovladač databáze není podporován.';
+
