02d94031b456aef3356ff7918b7b3963d56b6996
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:17 -0800

    Updated it_it language files

---
+++ components/gateways/nonmerchant/hubtel/language/it_it/hubtel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Hubtel
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Hubtel.!error.client_secret.valid'] = 'È necessario inserire un Client Secret valido.';
+$lang['Hubtel.!error.client_id.valid'] = 'È necessario inserire un ID cliente valido.';
+$lang['Hubtel.buildprocess.submit'] = 'Pagare con Hubtel';
+$lang['Hubtel.client_secret'] = 'Segreto del cliente';
+$lang['Hubtel.client_id'] = 'ID cliente';
+$lang['Hubtel.description'] = 'Hubtel è la principale piattaforma di aggregazione e pagamento mobile a valore aggiunto del Ghana.';
+$lang['Hubtel.name'] = 'Hubtel';
+
