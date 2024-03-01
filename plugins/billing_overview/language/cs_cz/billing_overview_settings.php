84e3ff0c0337f00af3b76b08cfb598f320940176
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:26 -0800

    Updated cs_cz language files

---
+++ plugins/billing_overview/language/cs_cz/billing_overview_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Billing Overview Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BillingOverviewSettings.!error.settings[][value].length'] = 'Délka každé jednotlivé hodnoty nastavení nesmí překročit 255 znaků.';
+$lang['BillingOverviewSettings.!error.settings[][key].empty'] = 'Žádný klíč nastavení nesmí být prázdný.';
+$lang['BillingOverviewSettings.!error.company_id.exists'] = 'Neplatné ID společnosti.';
+$lang['BillingOverviewSettings.!error.staff_id.exists'] = 'Neplatné ID zaměstnance.';
+
