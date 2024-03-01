6e8a4e2a3a8b8508e229d04e82d6b4dab45cea8b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:36 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/order_affiliate_payouts.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Order Affiliate Payouts
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliatePayouts.!error.payout_id.exists'] = 'ID de paiement invalide.';
+$lang['OrderAffiliatePayouts.!error.affiliate_id.exists'] = 'ID d\'affilié non valide.';
+$lang['OrderAffiliatePayouts.!error.payment_method_id.exists'] = 'ID de la méthode de paiement non valide.';
+$lang['OrderAffiliatePayouts.!error.status.valid'] = 'Veuillez sélectionner un statut de paiement valide.';
+$lang['OrderAffiliatePayouts.!error.requested_amount.format'] = 'Le montant demandé doit être un nombre.';
+$lang['OrderAffiliatePayouts.!error.requested_amount.valid'] = 'Le montant demandé est en dehors des limites de paiement.';
+$lang['OrderAffiliatePayouts.!error.requested_currency.length'] = 'Le code devise demandé doit comporter 3 caractères.';
+$lang['OrderAffiliatePayouts.!error.paid_amount.format'] = 'Le montant payé doit être un nombre.';
+$lang['OrderAffiliatePayouts.!error.paid_amount.valid'] = 'Le montant payé est en dehors des limites de paiement.';
+$lang['OrderAffiliatePayouts.!error.paid_currency.length'] = 'Le code de la devise payée doit comporter 3 caractères.';
+$lang['OrderAffiliatePayouts.getStatuses.pending'] = 'En attente';
+$lang['OrderAffiliatePayouts.getStatuses.approved'] = 'Approuvé';
+$lang['OrderAffiliatePayouts.getStatuses.declined'] = 'Refusé';
+
