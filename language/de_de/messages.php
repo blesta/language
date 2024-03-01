055822b01f1bcbb69d434b746908b7449151400d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:06 -0800

    Updated de_de language files

---
+++ language/de_de/messages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Messages
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Messages.getStatusTypes.inactive'] = 'Inaktiv';
+$lang['Messages.getStatusTypes.active'] = 'Aktiv';
+$lang['Messages.getTypes.sms'] = 'SMS';
+$lang['Messages.!error.content.format'] = 'Der Inhalt der Nachricht muss ein Array sein.';
+$lang['Messages.!error.status.valid'] = 'Der Meldungsstatus muss auf "aktiv" oder "inaktiv" gesetzt werden.';
+$lang['Messages.!error.type.valid'] = 'Der Nachrichtentyp muss auf "sms" eingestellt sein.';
+$lang['Messages.!error.company_id.valid'] = 'Ungültige Unternehmens-ID.';
+$lang['Messages.!error.message_group_id.valid'] = 'Ungültige Nachrichtengruppen-ID.';
+
