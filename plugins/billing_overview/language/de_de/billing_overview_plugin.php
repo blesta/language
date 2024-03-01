4020351bf72c162bf67fc262ee4330560bc4a700
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:26 -0800

    Updated de_de language files

---
+++ plugins/billing_overview/language/de_de/billing_overview_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Billing Overview Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BillingOverviewPlugin.description'] = 'Zeigt Abrechnungsstatistiken in einem Widget auf der Seite Abrechnungsübersicht an.';
+$lang['BillingOverviewPlugin.name'] = 'Rechnungsstellung im Überblick';
+
