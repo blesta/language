7e706d87e6a9d1a5851a6f0e0fa37763a69626ac
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:20 -0800

    Updated fr_fr language files

---
+++ core/Automation/Tasks/Task/language/fr_fr/create_invoices.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Create Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.create_invoices.attempt'] = 'Tentative de renouvellement des services et de création de factures.';
+$lang['Automation.task.create_invoices.completed'] = 'La tâche de création de factures est terminée.';
+$lang['Automation.task.create_invoices.recurring_invoice_failed'] = 'Impossible de créer une nouvelle facture à partir de la facture récurrente #%1$s pour le client #%2$s.';
+$lang['Automation.task.create_invoices.recurring_invoice_success'] = 'Création réussie d\'une nouvelle facture à partir de la facture récurrente #%1$s pour le client #%2$s.';
+$lang['Automation.task.create_invoices.service_invoice_success'] = 'Création réussie de la facture #%1$s pour le client #%2$s contenant les services %3$s.';
+$lang['Automation.task.create_invoices.service_invoice_error'] = 'La facture n\'a pas pu être générée pour le client #%2$s contenant les services %3$s car : %1$s';
+
