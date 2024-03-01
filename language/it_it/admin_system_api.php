ac81c890c061968e18bbefa38a6c34350f464403
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:40 -0800

    Updated it_it language files

---
+++ language/it_it/admin_system_api.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Admin System Api
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemApi.edit.field_apisubmit'] = 'Aggiornamento Utente';
+$lang['AdminSystemApi.edit.field_notes'] = 'Note';
+$lang['AdminSystemApi.edit.field_user'] = 'Utente';
+$lang['AdminSystemApi.edit.field_company'] = 'Azienda';
+$lang['AdminSystemApi.edit.boxtitle_edit'] = 'Modifica utente API';
+$lang['AdminSystemApi.add.field_apisubmit'] = 'Aggiungi utente';
+$lang['AdminSystemApi.add.field_notes'] = 'Note';
+$lang['AdminSystemApi.add.field_user'] = 'Utente';
+$lang['AdminSystemApi.add.field_company'] = 'Azienda';
+$lang['AdminSystemApi.add.boxtitle_add'] = 'Aggiungi utente API';
+$lang['AdminSystemApi.index.confirm_delete'] = 'Cancellare davvero questo utente API?';
+$lang['AdminSystemApi.index.no_results'] = 'Non ci sono utenti API.';
+$lang['AdminSystemApi.index.option_delete'] = 'Cancellare';
+$lang['AdminSystemApi.index.option_edit'] = 'Modifica';
+$lang['AdminSystemApi.index.heading_options'] = 'Opzioni';
+$lang['AdminSystemApi.index.heading_date'] = 'Data di creazione';
+$lang['AdminSystemApi.index.heading_company'] = 'Azienda';
+$lang['AdminSystemApi.index.heading_key'] = 'Chiave';
+$lang['AdminSystemApi.index.heading_user'] = 'Utente';
+$lang['AdminSystemApi.index.categorylink_keyadd'] = 'Aggiungi utente API';
+$lang['AdminSystemApi.index.boxtitle_api'] = 'Accesso API';
+$lang['AdminSystemApi.index.page_title'] = 'Impostazioni > Sistema > Accesso API';
+$lang['AdminSystemApi.!success.key_updated'] = 'L\'utente API è stato aggiornato con successo.';
+$lang['AdminSystemApi.!success.key_deleted'] = 'L\'utente API è stato eliminato con successo.';
+$lang['AdminSystemApi.!success.key_added'] = 'L\'utente API è stato aggiunto con successo.';
+$lang['AdminSystemApi.!notice.api_not_restrictive'] = 'ATTENZIONE! Azienda è l\'azienda con cui l\'utente si autentica al momento della connessione. L\'accesso ai dati di altre aziende non è limitato dalle richieste API che supportano tale accesso.';
+
