bddbabfc035e7df30bfc65ab1222f66a91f19375
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:23 -0800

    Updated de_de language files

---
+++ components/modules/cyberpanel/language/de_de/cyberpanel.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Cyberpanel
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cyberpanel.!error.cyberpanel_password.matches'] = 'Kennwort und Bestätigungskennwort stimmen nicht überein.';
+$lang['Cyberpanel.!error.cyberpanel_password.valid'] = 'Das Passwort muss mindestens 8 Zeichen lang sein.';
+$lang['Cyberpanel.!error.cyberpanel_username.length'] = 'Der Benutzername muss zwischen 1 und 16 Zeichen lang sein.';
+$lang['Cyberpanel.!error.cyberpanel_username.test'] = 'Der Benutzername darf nicht mit \'test\' beginnen.';
+$lang['Cyberpanel.!error.cyberpanel_username.format'] = 'Der Benutzername darf nur Buchstaben und Zahlen enthalten und darf nicht mit einer Zahl beginnen.';
+$lang['Cyberpanel.!error.cyberpanel_domain.test'] = 'Der Domänenname kann nicht mit \'test\' beginnen.';
+$lang['Cyberpanel.!error.cyberpanel_domain.format'] = 'Bitte geben Sie einen gültigen Domainnamen ein, z. B. domain.com.';
+$lang['Cyberpanel.!error.module_row.missing'] = 'Es ist ein interner Fehler aufgetreten. Die Modulzeile ist nicht verfügbar.';
+$lang['Cyberpanel.!error.api.internal'] = 'Es ist ein interner Fehler aufgetreten, oder der Server hat nicht auf die Anfrage geantwortet.';
+$lang['Cyberpanel.!error.meta[package].empty'] = 'Ein CyberPanel-Paket ist erforderlich.';
+$lang['Cyberpanel.!error.name_servers_count'] = 'Sie müssen mindestens 2 Nameserver definieren.';
+$lang['Cyberpanel.!error.name_servers_valid'] = 'Einer oder mehrere der eingegebenen Nameserver sind ungültig.';
+$lang['Cyberpanel.!error.remote_admin_password_valid_connection'] = 'Eine Verbindung zum Server konnte nicht hergestellt werden. Bitte überprüfen Sie, ob der Hostname und das Admin-Passwort korrekt sind.';
+$lang['Cyberpanel.!error.remote_admin_password_valid'] = 'Das Admin-Passwort scheint ungültig zu sein.';
+$lang['Cyberpanel.!error.remote_admin_username_valid'] = 'Der Admin-Benutzername scheint ungültig zu sein.';
+$lang['Cyberpanel.!error.host_name_valid'] = 'Der Hostname scheint ungültig zu sein.';
+$lang['Cyberpanel.!error.server_name_valid'] = 'Sie müssen ein Server-Label eingeben.';
+$lang['Cyberpanel.service_field.tooltip.password'] = 'Sie können das Passwort leer lassen, damit es automatisch generiert wird.';
+$lang['Cyberpanel.service_field.tooltip.username'] = 'Sie können den Benutzernamen leer lassen, um ihn automatisch zu generieren.';
+$lang['Cyberpanel.service_info.option_login'] = 'Einloggen';
+$lang['Cyberpanel.service_info.options'] = 'Optionen';
+$lang['Cyberpanel.service_info.server'] = 'Server';
+$lang['Cyberpanel.service_info.password'] = 'Passwort';
+$lang['Cyberpanel.service_info.username'] = 'Benutzername';
+$lang['Cyberpanel.tab_client_actions.field_password_submit'] = 'Passwort aktualisieren';
+$lang['Cyberpanel.tab_client_actions.field_cyberpanel_confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['Cyberpanel.tab_client_actions.field_cyberpanel_password'] = 'Passwort';
+$lang['Cyberpanel.tab_client_actions.change_password'] = 'Passwort ändern';
+$lang['Cyberpanel.service_field.password'] = 'Passwort';
+$lang['Cyberpanel.service_field.username'] = 'Benutzername';
+$lang['Cyberpanel.service_field.domain'] = 'Bereich';
+$lang['Cyberpanel.package_fields.package'] = 'Name des Pakets';
+$lang['Cyberpanel.row_meta.use_ssl'] = 'Verwenden Sie SSL für die Verbindung zur API (empfohlen)';
+$lang['Cyberpanel.row_meta.admin_password'] = 'Administrator-Passwort';
+$lang['Cyberpanel.row_meta.admin_username'] = 'Administrator-Benutzername';
+$lang['Cyberpanel.row_meta.host_name'] = 'Hostname';
+$lang['Cyberpanel.row_meta.server_name'] = 'Server-Etikett';
+$lang['Cyberpanel.edit_row.add_btn'] = 'Server bearbeiten';
+$lang['Cyberpanel.edit_row.remove_name_server'] = 'entfernen';
+$lang['Cyberpanel.edit_row.name_server'] = 'Namensserver %1$s';
+$lang['Cyberpanel.edit_row.name_server_host_col'] = 'Hostname';
+$lang['Cyberpanel.edit_row.name_server_col'] = 'Name Server';
+$lang['Cyberpanel.edit_row.name_server_btn'] = 'Zusätzlichen Namensserver hinzufügen';
+$lang['Cyberpanel.edit_row.notes_title'] = 'Anmerkungen';
+$lang['Cyberpanel.edit_row.name_servers_title'] = 'Namensserver';
+$lang['Cyberpanel.edit_row.basic_title'] = 'Grundeinstellungen';
+$lang['Cyberpanel.edit_row.box_title'] = 'CyberPanel Server bearbeiten';
+$lang['Cyberpanel.add_row.add_btn'] = 'Server hinzufügen';
+$lang['Cyberpanel.add_row.remove_name_server'] = 'entfernen';
+$lang['Cyberpanel.add_row.name_server'] = 'Namensserver %1$s';
+$lang['Cyberpanel.add_row.name_server_host_col'] = 'Hostname';
+$lang['Cyberpanel.add_row.name_server_col'] = 'Name Server';
+$lang['Cyberpanel.add_row.name_server_btn'] = 'Zusätzlichen Namensserver hinzufügen';
+$lang['Cyberpanel.add_row.notes_title'] = 'Anmerkungen';
+$lang['Cyberpanel.add_row.name_servers_title'] = 'Namensserver';
+$lang['Cyberpanel.add_row.basic_title'] = 'Grundeinstellungen';
+$lang['Cyberpanel.add_row.box_title'] = 'CyberPanel Server hinzufügen';
+$lang['Cyberpanel.order_options.roundrobin'] = 'Gleichmäßig auf die Server verteilen';
+$lang['Cyberpanel.order_options.first'] = 'Erster nicht-ausgelasteter Server';
+$lang['Cyberpanel.manage.module_groups_no_results'] = 'Es gibt keine Servergruppen.';
+$lang['Cyberpanel.manage.module_rows_no_results'] = 'Es gibt keine Server.';
+$lang['Cyberpanel.manage.module_groups.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Servergruppe löschen wollen?';
+$lang['Cyberpanel.manage.module_rows.confirm_delete'] = 'Sind Sie sicher, dass Sie diesen Server löschen wollen?';
+$lang['Cyberpanel.manage.module_groups.delete'] = 'Löschen';
+$lang['Cyberpanel.manage.module_rows.delete'] = 'Löschen';
+$lang['Cyberpanel.manage.module_groups.edit'] = 'bearbeiten';
+$lang['Cyberpanel.manage.module_rows.edit'] = 'bearbeiten';
+$lang['Cyberpanel.manage.module_groups_heading.options'] = 'Optionen';
+$lang['Cyberpanel.manage.module_groups_heading.servers'] = 'Anzahl der Server';
+$lang['Cyberpanel.manage.module_groups_heading.name'] = 'Name der Gruppe';
+$lang['Cyberpanel.manage.module_rows_heading.options'] = 'Optionen';
+$lang['Cyberpanel.manage.module_rows_heading.hostname'] = 'Hostname';
+$lang['Cyberpanel.manage.module_rows_heading.name'] = 'Server-Etikett';
+$lang['Cyberpanel.manage.module_groups_title'] = 'Server-Gruppen';
+$lang['Cyberpanel.manage.module_rows_title'] = 'Server';
+$lang['Cyberpanel.add_module_group'] = 'Server-Gruppe hinzufügen';
+$lang['Cyberpanel.add_module_row'] = 'Server hinzufügen';
+$lang['Cyberpanel.tab_client_actions'] = 'Aktionen';
+$lang['Cyberpanel.module_group'] = 'Server-Gruppe';
+$lang['Cyberpanel.module_row_plural'] = 'Server';
+$lang['Cyberpanel.module_row'] = 'Server';
+$lang['Cyberpanel.description'] = 'CyberPanel ist ein Hosting-Control-Panel der nächsten Generation, das von OpenLiteSpeed unterstützt wird. Gebaut für Geschwindigkeit, Sicherheit und Zuverlässigkeit.';
+$lang['Cyberpanel.name'] = 'CyberPanel';
+
