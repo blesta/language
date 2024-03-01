c380bd6bd4ebf93839ee8b7880a01169619e494d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:43 -0800

    Updated de_de language files

---
+++ language/de_de/client_accounts.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Client Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientAccounts.navigation.nav_return'] = 'Zurück zum Dashboard';
+$lang['ClientAccounts.navigation.nav_paymentaccounts_add'] = 'Zahlungskonto hinzufügen';
+$lang['ClientAccounts.navigation.nav_paymentaccounts'] = 'Zahlungskonten';
+$lang['ClientAccounts.navigation.nav_dashboard'] = 'Dashboard';
+$lang['ClientAccounts.ach_info.field_savedetails'] = 'Konto speichern';
+$lang['ClientAccounts.ach_info.field_routingnum'] = 'Routing-Nummer';
+$lang['ClientAccounts.ach_info.field_accountnum'] = 'Kontonummer';
+$lang['ClientAccounts.ach_info.field_type'] = 'Typ';
+$lang['ClientAccounts.ach_info.heading_ach'] = 'Informationen zum Bankkonto';
+$lang['ClientAccounts.cc_info.tooltip_code'] = 'Der 3- oder 4-stellige Sicherheitscode, der sich normalerweise auf der Rückseite der Karte befindet.';
+$lang['ClientAccounts.cc_info.field_savedetails'] = 'Konto speichern';
+$lang['ClientAccounts.cc_info.field_expiration'] = 'Verfallsdatum';
+$lang['ClientAccounts.cc_info.field_security'] = 'Sicherheitscode';
+$lang['ClientAccounts.cc_info.field_number'] = 'Nummer';
+$lang['ClientAccounts.cc_info.heading_cc'] = 'Informationen zur Kreditkarte';
+$lang['ClientAccounts.contact_info.field_email'] = 'E-Mail';
+$lang['ClientAccounts.contact_info.field_zip'] = 'Postleitzahl/Postleitzahl';
+$lang['ClientAccounts.contact_info.field_state'] = 'Staat';
+$lang['ClientAccounts.contact_info.field_country'] = 'Land';
+$lang['ClientAccounts.contact_info.field_city'] = 'Stadt';
+$lang['ClientAccounts.contact_info.field_address2'] = 'Adresse 2';
+$lang['ClientAccounts.contact_info.field_address1'] = 'Adresse 1';
+$lang['ClientAccounts.contact_info.field_title'] = 'Titel';
+$lang['ClientAccounts.contact_info.field_company'] = 'Unternehmen';
+$lang['ClientAccounts.contact_info.field_last_name'] = 'Nachname';
+$lang['ClientAccounts.contact_info.field_first_name'] = 'Vorname';
+$lang['ClientAccounts.contact_info.field_contact_id'] = 'Kontaktinformationen kopieren von';
+$lang['ClientAccounts.contact_info.text_select_contact'] = 'Sie können einen bestehenden Kontakt auswählen, um dieses Formular vorauszufüllen.';
+$lang['ClientAccounts.contact_info.heading_contact'] = 'Kontaktinformationen';
+$lang['ClientAccounts.setcontactview.text_none'] = 'Keine';
+$lang['ClientAccounts.editcc.field_accountsubmit'] = 'Konto bearbeiten';
+$lang['ClientAccounts.editcc.boxtitle_editcc'] = 'Kreditkartenkonto bearbeiten';
+$lang['ClientAccounts.editcc.page_title'] = 'Kunde #%1$s Kreditkartenkonto bearbeiten';
+$lang['ClientAccounts.verifyach.field_accountsubmit'] = 'Konto verifizieren';
+$lang['ClientAccounts.verifyach.field_seconddeposit'] = 'Zweite Einzahlung';
+$lang['ClientAccounts.verifyach.field_firstdeposit'] = 'Erste Einzahlung';
+$lang['ClientAccounts.verifyach.boxtitle_verifyach'] = 'ACH-Konto verifizieren';
+$lang['ClientAccounts.verifyach.page_title'] = 'Kunde #%1$s ACH-Konto verifizieren';
+$lang['ClientAccounts.editach.field_accountsubmit'] = 'Konto bearbeiten';
+$lang['ClientAccounts.editach.boxtitle_editach'] = 'ACH-Konto bearbeiten';
+$lang['ClientAccounts.editach.page_title'] = 'Kunde #%1$s ACH-Konto bearbeiten';
+$lang['ClientAccounts.add.field_accountsubmit'] = 'Konto erstellen';
+$lang['ClientAccounts.add.field_nextsubmit'] = 'Weiter';
+$lang['ClientAccounts.add.boxtitle_create'] = 'Zahlungskonto hinzufügen';
+$lang['ClientAccounts.add.field_ach_type'] = 'ACH';
+$lang['ClientAccounts.add.field_cc_type'] = 'Kreditkarte';
+$lang['ClientAccounts.add.heading_account_type'] = 'Konto-Typ';
+$lang['ClientAccounts.add.page_title'] = 'Kunde #%1$s Zahlungskonto hinzufügen';
+$lang['ClientAccounts.index.no_results'] = 'Sie haben keine Zahlungskonten.';
+$lang['ClientAccounts.index.field_removeautodebit'] = 'Automatische Abbuchung entfernen';
+$lang['ClientAccounts.index.field_accountsubmit'] = 'Als Standard-Zahlungskonto festlegen';
+$lang['ClientAccounts.index.confirm_delete'] = 'Sind Sie sicher, dass Sie dieses Zahlungskonto löschen möchten?';
+$lang['ClientAccounts.index.type_ach'] = '%1$s - %2$s';
+$lang['ClientAccounts.index.type_cc'] = '%1$s - %2$s';
+$lang['ClientAccounts.index.option_delete'] = 'Löschen';
+$lang['ClientAccounts.index.option_verify'] = 'Überprüfen Sie';
+$lang['ClientAccounts.index.option_edit'] = 'bearbeiten';
+$lang['ClientAccounts.index.heading_options'] = 'Optionen';
+$lang['ClientAccounts.index.heading_last4'] = 'Letzte 4';
+$lang['ClientAccounts.index.heading_type'] = 'Typ';
+$lang['ClientAccounts.index.heading_name'] = 'Name';
+$lang['ClientAccounts.index.boxtitle_accounts'] = 'Zahlungskonten';
+$lang['ClientAccounts.index.create_account'] = 'Zahlungskonto hinzufügen';
+$lang['ClientAccounts.index.page_title'] = 'Kunde #%1$s Konten';
+$lang['ClientAccounts.!info.ach_verification_edit'] = 'Nachdem Sie dieses Konto bearbeitet haben, müssen Sie es verifizieren, bevor Sie es für eine Zahlung verwenden können.';
+$lang['ClientAccounts.!info.ach_verification'] = 'Sie müssen dieses Konto verifizieren, bevor Sie es für eine Zahlung verwenden können.';
+$lang['ClientAccounts.!info.no_accounts'] = 'Sie haben derzeit noch keine Zahlungskonten. Fügen Sie unten Ihr erstes Zahlungskonto hinzu.';
+$lang['ClientAccounts.!info.account_info'] = 'Zahlungskonten ermöglichen es Ihnen, Zahlungsinformationen zu speichern und können zur Bezahlung von Rechnungen verwendet werden. Ein Zahlungskonto, das als Standardkonto ausgewählt ist, wird für automatische wiederkehrende Zahlungen verwendet.';
+$lang['ClientAccounts.!notice.reenable_autodebit'] = 'Automatische Zahlungen wurden für dieses Konto ausdrücklich deaktiviert. Bitte wenden Sie sich an einen Mitarbeiter, um es wieder zu aktivieren. Sie können ein Zahlungskonto als Standardkonto auswählen, aber es wird nicht automatisch belastet, bis es vom Personal reaktiviert wurde.';
+$lang['ClientAccounts.!notice.cc_disabled'] = 'Konten für Kreditkartenzahlungen sind derzeit deaktiviert und können nicht aktualisiert werden.';
+$lang['ClientAccounts.!notice.ach_disabled'] = 'ACH-Zahlungskonten sind derzeit deaktiviert und können nicht aktualisiert werden.';
+$lang['ClientAccounts.!notice.disabled'] = 'Die Möglichkeit, Zahlungskonten hinzuzufügen, ist derzeit deaktiviert.';
+$lang['ClientAccounts.!error.payment_account_type_invalid'] = 'Ungültiger Zahlungskontotyp.';
+$lang['ClientAccounts.!error.account_invalid'] = 'Der angegebene Zahlungskontotyp ist deaktiviert oder ungültig.';
+$lang['ClientAccounts.!success.achaccount_verified'] = 'Das Zahlungskonto wurde erfolgreich verifiziert.';
+$lang['ClientAccounts.!success.achaccount_updated'] = 'Das Zahlungskonto wurde erfolgreich aktualisiert.';
+$lang['ClientAccounts.!success.achaccount_deleted'] = 'Das Zahlungskonto wurde erfolgreich gelöscht.';
+$lang['ClientAccounts.!success.ccaccount_updated'] = 'Das Zahlungskonto wurde erfolgreich aktualisiert.';
+$lang['ClientAccounts.!success.ccaccount_deleted'] = 'Das Zahlungskonto wurde erfolgreich gelöscht.';
+$lang['ClientAccounts.!success.account_created'] = 'Das Zahlungskonto wurde erfolgreich erstellt.';
+$lang['ClientAccounts.!success.defaultaccount_deleted'] = 'Das Standardzahlungskonto wurde entfernt.';
+$lang['ClientAccounts.!success.defaultaccount_updated'] = 'Das Standardzahlungskonto wurde aktualisiert.';
+
