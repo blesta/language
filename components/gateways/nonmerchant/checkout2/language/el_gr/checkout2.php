ffcc56f6a47e598abe1db23c4e5f86b4d03eacce
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:26 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/checkout2/language/el_gr/checkout2.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Checkout2
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Checkout2.!error.sid.valid'] = 'Ο αριθμός λογαριασμού προμηθευτή δεν ταιριάζει με τον αριθμό λογαριασμού που παρέχεται από τη συναλλαγή.';
+$lang['Checkout2.!error.credit_card_processed.completed'] = 'Η συναλλαγή δεν διεκπεραιώθηκε με επιτυχία.';
+$lang['Checkout2.!error.hash.valid'] = 'Ο κατακερματισμός που χρησιμοποιήθηκε για την επαλήθευση αυτής της πώλησης που προήλθε από το 2Checkout είναι άκυρος.';
+$lang['Checkout2.!error.key.valid'] = 'Το κλειδί που χρησιμοποιήθηκε για την επαλήθευση αυτής της πώλησης που προήλθε από το 2Checkout είναι άκυρο.';
+$lang['Checkout2.!error.sandbox.valid'] = 'Το Sandbox πρέπει να οριστεί είτε σε \'true\' είτε σε \'false\'.';
+$lang['Checkout2.!error.test_mode.valid'] = 'Η λειτουργία δοκιμής πρέπει να οριστεί είτε σε \'true\' είτε σε \'false\'.';
+$lang['Checkout2.!error.secret_key.empty'] = 'Εισάγετε το μυστικό σας κλειδί.';
+$lang['Checkout2.!error.buy_link_secret_word.empty'] = 'Παρακαλούμε εισάγετε τη μυστική σας λέξη Σύνδεσμος αγοράς.';
+$lang['Checkout2.!error.secret_word.empty'] = 'Παρακαλώ εισάγετε τη μυστική σας λέξη.';
+$lang['Checkout2.!error.merchant_code.empty'] = 'Παρακαλώ εισάγετε τον κωδικό σας.';
+$lang['Checkout2.!error.vendor_id.empty'] = 'Παρακαλούμε εισάγετε τον αριθμό λογαριασμού προμηθευτή σας.';
+$lang['Checkout2.!error.api_version.valid'] = 'Παρακαλούμε εισάγετε μια έγκυρη έκδοση API.';
+$lang['Checkout2.getapiversions.v5'] = 'Έκδοση 5';
+$lang['Checkout2.getapiversions.v1'] = 'Έκδοση 1 (Legacy)';
+$lang['Checkout2.buildprocess.submit'] = 'Πληρώστε με το 2Checkout';
+$lang['Checkout2.refund.comment'] = 'Έναρξη επιστροφής χρημάτων για %1$s.';
+$lang['Checkout2.test_mode'] = 'Λειτουργία δοκιμής';
+$lang['Checkout2.sandbox'] = 'Sandbox';
+$lang['Checkout2.api_password'] = 'Κωδικός πρόσβασης API';
+$lang['Checkout2.api_username_note'] = 'Αυτό, καθώς και ο κωδικός πρόσβασης API, απαιτούνται για τη διεκπεραίωση επιστροφών μέσω του 2Checkout.';
+$lang['Checkout2.api_username'] = 'Όνομα χρήστη API';
+$lang['Checkout2.buy_link_secret_word'] = 'Αγοράστε Σύνδεσμος Μυστική λέξη';
+$lang['Checkout2.secret_word'] = 'Μυστική λέξη';
+$lang['Checkout2.secret_key'] = 'Μυστικό κλειδί';
+$lang['Checkout2.merchant_code'] = 'Κωδικός εμπόρου';
+$lang['Checkout2.vendor_id'] = 'Αριθμός λογαριασμού προμηθευτή';
+$lang['Checkout2.api_version'] = 'Έκδοση API';
+$lang['Checkout2.description'] = 'Το 2Checkout.com είναι μια υπηρεσία επεξεργασίας ηλεκτρονικών πληρωμών που σας βοηθά να δέχεστε πιστωτικές κάρτες, PayPal και χρεωστικές κάρτες. Πάνω από 17.000 εταιρείες σε περισσότερες από 180 χώρες χρησιμοποιούν το 2Checkout';
+$lang['Checkout2.name'] = '2Checkout';
+
