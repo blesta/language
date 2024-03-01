de10eeb4e1eac12c6a06ce5702c2e346b8f35c98
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:44 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/client_dialog.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Client Dialog
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDialog.password.btn_use_password'] = 'Použití hesla';
+$lang['ClientDialog.password.btn_cancel'] = 'Zrušit';
+$lang['ClientDialog.password.btn_generate'] = 'Generovat';
+$lang['ClientDialog.password.field_modal_password_copied'] = 'Toto heslo jsem si zkopíroval na bezpečné místo.';
+$lang['ClientDialog.password.field_modal_password'] = 'Heslo';
+$lang['ClientDialog.password.title'] = 'Generování hesla';
+$lang['ClientDialog.confirm.text_no'] = 'Ne';
+$lang['ClientDialog.confirm.text_yes'] = 'Ano';
+
