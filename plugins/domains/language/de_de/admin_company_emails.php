ecdd599ce421f0b2fc5c3345bbf07fff0c274a3e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:32 -0800

    Updated de_de language files

---
+++ plugins/domains/language/de_de/admin_company_emails.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Domains.domain_expiration_desc'] = 'Mitteilung an einen Kunden, kurz nachdem seine Domain abgelaufen ist.';
+$lang['AdminCompanyEmails.templates.Domains.domain_expiration_name'] = 'Hinweis zum Ablauf der Domain';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_2_desc'] = 'Zweite Mitteilung an einen Kunden, wenn seine Domain bald abläuft.';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_2_name'] = 'Erinnerung an den Ablauf der Domain 2';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_1_desc'] = 'Erste Mitteilung an einen Kunden, wenn seine Domäne demnächst ausläuft.';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_1_name'] = 'Erinnerung an den Ablauf der Domain 1';
+
