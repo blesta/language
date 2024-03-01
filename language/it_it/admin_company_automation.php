defb77ad00bbd0e901e8c490c471755f1300d176
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:26 -0800

    Updated it_it language files

---
+++ language/it_it/admin_company_automation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Company Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyAutomation.getintervals.text_hours'] = 'ore';
+$lang['AdminCompanyAutomation.getintervals.text_hour'] = 'ora';
+$lang['AdminCompanyAutomation.getintervals.text_minutes'] = 'minuti';
+$lang['AdminCompanyAutomation.getintervals.text_minute'] = 'minuto';
+$lang['AdminCompanyAutomation.index.confirm_clear_task'] = 'Siete sicuri di voler cancellare il blocco dell\'attività cron? Se l\'attività si blocca continuamente, il problema potrebbe essere più serio. Provare a eseguire il cron manualmente o a controllare il registro del cron per verificare la presenza di errori per determinare la causa.';
+$lang['AdminCompanyAutomation.index.option_clear_task'] = 'Cancella blocco attività';
+$lang['AdminCompanyAutomation.index.text_cron_never_ran'] = 'Il cron non è mai stato eseguito.';
+$lang['AdminCompanyAutomation.index.text_cron_last_ran'] = 'Il cron è stato eseguito l\'ultima volta il %1$s.';
+$lang['AdminCompanyAutomation.index.no_cron_lastran'] = 'Mai';
+$lang['AdminCompanyAutomation.index.no_results'] = 'Non ci sono attività cron.';
+$lang['AdminCompanyAutomation.index.text_task_lastran'] = 'Attività eseguita per ultima:';
+$lang['AdminCompanyAutomation.index.text_starttime'] = 'Ora di inizio';
+$lang['AdminCompanyAutomation.index.text_interval'] = 'Intervallo';
+$lang['AdminCompanyAutomation.index.field_enabled'] = 'Abilitato';
+$lang['AdminCompanyAutomation.index.field_automationsubmit'] = 'Aggiornamento delle impostazioni';
+$lang['AdminCompanyAutomation.index.boxtitle_automation'] = 'Automazione';
+$lang['AdminCompanyAutomation.index.page_title'] = 'Impostazioni > Azienda > Automazione';
+$lang['AdminCompanyAutomation.!success.task_cleared'] = 'L\'attività cron è stata cancellata.';
+$lang['AdminCompanyAutomation.!success.automation_updated'] = 'Le impostazioni di automazione sono state aggiornate con successo!';
+$lang['AdminCompanyAutomation.!notice.cron_task_interval'] = 'Ci sono attività di automazione impostate per essere eseguite ogni minuto, ma il cron non sembra essere in esecuzione ogni minuto. Aggiornare il cron in modo che venga eseguito ogni minuto per sfruttare questi intervalli più brevi.';
+
