7f4f6bc4b54eda0f99a8758ec12901ebbf5b1010
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:15 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/companies.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Companies
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Companies.!error.company_id.in_use'] = 'Cette société est actuellement utilisée et ne peut être supprimée.';
+$lang['Companies.!error.company_id.exists'] = 'ID de l\'entreprise non valide.';
+$lang['Companies.!error.name.empty'] = 'Veuillez saisir un nom.';
+$lang['Companies.!error.name.length'] = 'La longueur du nom ne doit pas dépasser 64 caractères.';
+$lang['Companies.!error.hostname.valid'] = 'Veuillez saisir un nom d\'hôte valide.';
+$lang['Companies.!error.hostname.length'] = 'Le nom d\'hôte ne doit pas dépasser 255 caractères.';
+$lang['Companies.!error.hostname.unique'] = 'Ce nom d\'hôte a déjà été pris par une autre entreprise.';
+$lang['Companies.!error.phone.length'] = 'La longueur du téléphone ne doit pas dépasser 64 caractères.';
+$lang['Companies.!error.fax.length'] = 'La longueur du fax ne doit pas dépasser 64 caractères.';
+$lang['Companies.!error.private_key_passphrase.valid'] = 'La phrase de passe que vous avez saisie n\'est pas valide.';
+$lang['Companies.!error.quota.allowed'] = 'Il n\'y a pas de licences d\'entreprise supplémentaires disponibles. Veuillez vous connecter à votre compte sur account.blesta.com ou contacter le service des ventes pour acheter des licences d\'entreprise supplémentaires.';
+$lang['Companies.!error.inv_format.format'] = 'Le format de la facture ne peut pas entrer en conflit avec le format du projet de facture.';
+$lang['Companies.!error.inv_format.contains'] = 'Le format de la facture doit contenir {num}.';
+$lang['Companies.!error.inv_draft_format.format'] = 'Le format du projet de facture ne peut pas entrer en conflit avec le format de la facture.';
+$lang['Companies.!error.inv_draft_format.contains'] = 'Le format du projet de facture doit contenir {num}.';
+$lang['Companies.!error.inv_start.number'] = 'La valeur de début de facture doit être un nombre.';
+$lang['Companies.!error.inv_increment.number'] = 'La valeur d\'incrémentation de la facture doit être un nombre.';
+$lang['Companies.!error.inv_pad_size.number'] = 'La taille de l\'intercalaire de la facture doit être un nombre.';
+$lang['Companies.!error.inv_pad_str.length'] = 'Le caractère de remplissage de la facture doit être un caractère unique.';
+$lang['Companies.!error.inv_proforma_format.format'] = 'Le format de facture pro forma ne peut pas être en contradiction avec le format de projet de facture.';
+$lang['Companies.!error.inv_proforma_format.contains'] = 'Le format de la facture pro forma doit contenir {num}.';
+$lang['Companies.!error.inv_proforma_start.number'] = 'La valeur de départ de la facture pro forma doit être un nombre.';
+
