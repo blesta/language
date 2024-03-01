86ad50ae1298a2363410fdb8b59966641c5871e7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:00 -0800

    Updated el_gr language files

---
+++ components/modules/pterodactyl/language/el_gr/pterodactyl_rule.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Pterodactyl Rule
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PterodactylRule.!error.digitsBetween'] = 'Το πεδίο %1$s πρέπει να περιέχει μια τιμή μεταξύ %2$s και %3$s ψηφίων.';
+$lang['PterodactylRule.!error.between'] = 'Το πεδίο %1$s πρέπει να περιέχει μια τιμή μεταξύ %2$s και %3$s.';
+$lang['PterodactylRule.!error.max'] = 'Το πεδίο %1$s έχει μέγιστο όριο %2$s.';
+$lang['PterodactylRule.!error.min'] = 'Το πεδίο %1$s πρέπει να περιέχει τουλάχιστον %2$s.';
+$lang['PterodactylRule.!error.url'] = 'Το πεδίο %1$s πρέπει να περιέχει ένα έγκυρο url (π.χ. www.domain.com).';
+$lang['PterodactylRule.!error.alphaDash'] = 'Το πεδίο %1$s πρέπει να περιέχει μόνο τους ακόλουθους χαρακτήρες a-z, A-Z, 0-9, - ή _.';
+$lang['PterodactylRule.!error.alphaNum'] = 'Το πεδίο %1$s πρέπει να περιέχει μόνο τους ακόλουθους χαρακτήρες a-z, A-Z ή 0-9.';
+$lang['PterodactylRule.!error.string'] = 'Το πεδίο %1$s πρέπει να περιέχει μια συμβολοσειρά.';
+$lang['PterodactylRule.!error.integer'] = 'Το πεδίο %1$s πρέπει να περιέχει έναν ακέραιο αριθμό.';
+$lang['PterodactylRule.!error.numeric'] = 'Το πεδίο %1$s πρέπει να περιέχει έναν αριθμό.';
+$lang['PterodactylRule.!error.regex'] = 'Το πεδίο %1$s πρέπει να ταιριάζει με το regex %2$s.';
+$lang['PterodactylRule.!error.required'] = 'Το πεδίο %1$s είναι υποχρεωτικό.';
+
