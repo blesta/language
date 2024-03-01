856f52d3a614835ba2c1c0abb4ced5ac3892dedd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:04 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/ccavenue/language/de_de/ccavenue.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Ccavenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Ccavenue.!error.authentication.log'] = 'Die Transaktion wurde von der Bank abgelehnt.';
+$lang['Ccavenue.!error.authentication.response'] = 'Danke, dass Sie bei uns eingekauft haben. Die Transaktion wurde jedoch abgelehnt. Bitte wiederholen Sie die Transaktion.';
+$lang['Ccavenue.!error.security.response'] = 'Sicherheitsfehler. Unerlaubter Zugriff entdeckt.';
+$lang['Ccavenue.!error.delay.response'] = 'Vielen Dank, dass Sie bei uns eingekauft haben. Wir werden Sie über den Status Ihrer Bestellung per E-Mail auf dem Laufenden halten';
+$lang['Ccavenue.!error.pending.log'] = 'Zahlungsstatus vom Gateway erhalten und ausstehend.';
+$lang['Ccavenue.!error.encryption_key.valid'] = 'Bitte geben Sie einen Verschlüsselungscode ein.';
+$lang['Ccavenue.!error.access_code.empty'] = 'Bitte geben Sie einen Zugangscode ein.';
+$lang['Ccavenue.!error.merchant_id.valid'] = 'Bitte geben Sie eine gültige Händler-ID ein.';
+$lang['Ccavenue.buildprocess.submit'] = 'Bezahlen über CCAvenue';
+$lang['Ccavenue.test_mode_note'] = 'Wenn diese Option aktiviert ist, sendet das Gateway Zahlungsanforderungen an die Test-URL.';
+$lang['Ccavenue.test_mode'] = 'Test-Modus';
+$lang['Ccavenue.access_code_note'] = 'Diese finden Sie in Ihrem CCAvenue-Konto unter API-Schlüssel.';
+$lang['Ccavenue.access_code'] = 'Zugangscode';
+$lang['Ccavenue.encryption_key_note'] = 'Dieser wird in Ihrem CCAvenue-Konto unter API-Schlüssel generiert.';
+$lang['Ccavenue.encryption_key'] = 'Verschlüsselungsschlüssel';
+$lang['Ccavenue.merchant_id_note'] = 'Ihre Händler-ID ist in Ihrem CCAvenue-Konto unter API-Schlüssel verfügbar';
+$lang['Ccavenue.merchant_id'] = 'Händler-ID';
+$lang['Ccavenue.description'] = 'Mit über 240 Zahlungsoptionen, die mehr als 55 indische Banken in Echtzeit verbinden, ist CCAvenue eine der größten Lastschriftmaschinen Indiens.';
+$lang['Ccavenue.name'] = 'CCAvenue Zahlung';
+
