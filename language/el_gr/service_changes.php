2eef4272dcc3fcd6e48dcfb4c4e6ee3e32d91c74
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:33 -0800

    Updated el_gr language files

---
+++ language/el_gr/service_changes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Service Changes
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ServiceChanges.status.canceled'] = 'Ακυρώθηκε';
+$lang['ServiceChanges.status.error'] = 'Σφάλμα';
+$lang['ServiceChanges.status.completed'] = 'Ολοκληρωμένο';
+$lang['ServiceChanges.status.pending'] = 'Εκκρεμεί';
+$lang['ServiceChanges.!error.status.valid'] = 'Παρακαλούμε επιλέξτε μια έγκυρη κατάσταση.';
+$lang['ServiceChanges.!error.invoice_id.unique'] = 'Το αναγνωριστικό τιμολογίου έχει ήδη οριστεί για μια αλλαγή υπηρεσίας και δεν μπορεί να οριστεί ξανά.';
+$lang['ServiceChanges.!error.invoice_id.exists'] = 'Μη έγκυρο αναγνωριστικό τιμολογίου.';
+$lang['ServiceChanges.!error.service_id.exists'] = 'Μη έγκυρο αναγνωριστικό υπηρεσίας.';
+$lang['ServiceChanges.!error.id.exists'] = 'Μη έγκυρο αναγνωριστικό αλλαγής υπηρεσίας.';
+
