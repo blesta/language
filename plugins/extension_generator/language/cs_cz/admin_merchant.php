a6c7b01f7dea4369130378a2bb0b0ad4e50d1c47
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:40 -0800

    Updated cs_cz language files

---
+++ plugins/extension_generator/language/cs_cz/admin_merchant.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Admin Merchant
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMerchant.features.confirm'] = 'Další - Potvrzení';
+$lang['AdminMerchant.features.heading_supported_features'] = 'Podporované funkce';
+$lang['AdminMerchant.fields.features'] = 'Další - Podporované funkce';
+$lang['AdminMerchant.fields.text_remove'] = 'Odstranění stránky';
+$lang['AdminMerchant.fields.text_options'] = 'Možnosti';
+$lang['AdminMerchant.fields.field_row_add'] = 'Přidání konfiguračního pole';
+$lang['AdminMerchant.fields.placeholder_label'] = 'např. konfigurační pole';
+$lang['AdminMerchant.fields.placeholder_name'] = 'např. configuration_field';
+$lang['AdminMerchant.fields.tooltip_encryptable'] = 'Zda se má toto pole v databázi šifrovat';
+$lang['AdminMerchant.fields.tooltip_tooltip'] = 'Text nápovědy, který se zobrazí vedle pole (pokud nechcete zobrazit žádnou nápovědu, nechte prázdný).';
+$lang['AdminMerchant.fields.tooltip_type'] = 'Typ pole, které chcete vytvořit (zaškrtávací políčko, text atd.)';
+$lang['AdminMerchant.fields.tooltip_label'] = 'Zobrazovaný název pole';
+$lang['AdminMerchant.fields.tooltip_name'] = 'Název pole v základně kódu';
+$lang['AdminMerchant.fields.encryptable'] = 'Šifrovatelné';
+$lang['AdminMerchant.fields.tooltip'] = 'Text nápovědy k nástroji';
+$lang['AdminMerchant.fields.type'] = 'Typ';
+$lang['AdminMerchant.fields.label'] = 'Štítek';
+$lang['AdminMerchant.fields.name'] = 'Název';
+$lang['AdminMerchant.fields.heading_fields'] = 'Konfigurační pole';
+$lang['AdminMerchant.basic.features'] = 'Další - Podporované funkce';
+$lang['AdminMerchant.basic.fields'] = 'Další - Konfigurační pole';
+$lang['AdminMerchant.basic.placeholder_author_url'] = 'např. https://blesta.com/';
+$lang['AdminMerchant.basic.placeholder_author_name'] = 'např. Blesta';
+$lang['AdminMerchant.basic.placeholder_currencies'] = 'např. USD,EUR,JPY';
+$lang['AdminMerchant.basic.placeholder_signup_url'] = 'např. https://dashboard.stripe.com/register';
+$lang['AdminMerchant.basic.tooltip_currencies'] = 'Seznam podporovaných měn oddělený čárkou (např. USD,JPY,EUR).';
+$lang['AdminMerchant.basic.tooltip_signup_url'] = 'Adresa URL registrační stránky brány';
+$lang['AdminMerchant.basic.tooltip_logo'] = 'Logo zobrazené ve výpisu zásuvných modulů. Velikost obrázků se nemění, takže ideální rozměry jsou 150x70.';
+$lang['AdminMerchant.basic.tooltip_description'] = 'Popis uvedený ve výpisu zásuvných modulů';
+$lang['AdminMerchant.basic.text_remove'] = 'Odstranění stránky';
+$lang['AdminMerchant.basic.author_row_add'] = 'Přidat autora';
+$lang['AdminMerchant.basic.text_options'] = 'Možnosti';
+$lang['AdminMerchant.basic.author_url'] = 'Adresa URL autora';
+$lang['AdminMerchant.basic.author_name'] = 'Jméno autora';
+$lang['AdminMerchant.basic.currencies'] = 'Měny';
+$lang['AdminMerchant.basic.signup_url'] = 'Adresa URL registrace';
+$lang['AdminMerchant.basic.logo'] = 'Logo';
+$lang['AdminMerchant.basic.description'] = 'Popis';
+$lang['AdminMerchant.basic.heading_authors'] = 'Autoři';
+$lang['AdminMerchant.basic.heading_basic'] = 'Základní informace';
+$lang['AdminMerchant.index.boxtitle_extension_generator'] = 'Generátor rozšíření - Obchodní brána';
+$lang['AdminMerchant.index.page_title'] = 'Generátor rozšíření - %1$s';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_ach_offsite'] = 'Automatizované zpracování v clearingovém centru s využitím pověření uložených mimo pracoviště zpracovatele.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_ach'] = 'Automatizované zpracování clearingového centra pomocí neuložených pověření nebo pověření uložených v systému Blesta.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc_form'] = 'To umožňuje bráně poskytovat vlastní html pro formuláře kreditních karet.  To je určeno pro podporu kreditních karet mimo web, které mohou používat iframe nebo vyžadovat další pole.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc_offsite'] = 'Zpracování kreditních karet pomocí pověření uložených mimo pracoviště zpracovatele.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc'] = 'zpracování kreditních karet pomocí neuložených pověření nebo pověření uložených v systému Blesta.';
+$lang['AdminMerchant.getsupportedfeatures.ach_offsite'] = 'Offsite ACH';
+$lang['AdminMerchant.getsupportedfeatures.ach'] = 'ACH';
+$lang['AdminMerchant.getsupportedfeatures.cc_form'] = 'Vlastní formulář kreditní karty';
+$lang['AdminMerchant.getsupportedfeatures.cc_offsite'] = 'Kreditní karta Offsite';
+$lang['AdminMerchant.getsupportedfeatures.cc'] = 'Kreditní karta';
+
