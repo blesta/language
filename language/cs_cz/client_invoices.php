4e10e7179719415b09b6f3b25e8fbec9c4e670ab
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:46 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/client_invoices.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Client Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientInvoices.applied.no_results'] = 'Na tuto fakturu nebyly uplatněny žádné transakce.';
+$lang['ClientInvoices.applied.heading_appliedon'] = 'Applied On';
+$lang['ClientInvoices.applied.heading_applied'] = 'Aplikované';
+$lang['ClientInvoices.applied.heading_amount'] = 'Částka';
+$lang['ClientInvoices.applied.heading_paymenttype'] = 'Typ platby';
+$lang['ClientInvoices.index.no_results'] = 'Nemáte žádné faktury %1$s.';
+$lang['ClientInvoices.index.option_pay'] = 'Platit';
+$lang['ClientInvoices.index.option_view'] = 'Zobrazit';
+$lang['ClientInvoices.index.heading_options'] = 'Možnosti';
+$lang['ClientInvoices.index.heading_datedue'] = 'Datum splatnosti';
+$lang['ClientInvoices.index.heading_datebilled'] = 'Datum vyúčtování';
+$lang['ClientInvoices.index.heading_dateclosed'] = 'Datum uzavření';
+$lang['ClientInvoices.index.heading_due'] = 'Due';
+$lang['ClientInvoices.index.heading_paid'] = 'Placené';
+$lang['ClientInvoices.index.heading_amount'] = 'Částka';
+$lang['ClientInvoices.index.heading_invoice'] = 'Faktura #';
+$lang['ClientInvoices.index.boxtitle_invoices'] = 'Faktury';
+$lang['ClientInvoices.index.categorylink_make_payment'] = 'Provést platbu';
+$lang['ClientInvoices.index.category_closed'] = 'Uzavřeno';
+$lang['ClientInvoices.index.category_open'] = 'Otevřít';
+$lang['ClientInvoices.index.page_title'] = 'Klientské faktury #%1$s';
+
