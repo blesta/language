ba567d56074483109c9e5c03a106bc941abc239e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:10 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/package_option_groups.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Package Option Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptionGroups.!error.hidden.valid'] = 'Zda se má tato skupina možností skrýt, musí být nastaveno na 0 nebo 1.';
+$lang['PackageOptionGroups.!error.hide_options.valid'] = 'Zda skrýt možnosti, musí být nastaveno na 0 nebo 1.';
+$lang['PackageOptionGroups.!error.group_id.exists'] = 'Nesprávné ID skupiny možností balíčku.';
+$lang['PackageOptionGroups.!error.name.length'] = 'Délka názvu skupiny nesmí přesáhnout 128 znaků.';
+$lang['PackageOptionGroups.!error.name.empty'] = 'Zadejte název skupiny.';
+$lang['PackageOptionGroups.!error.company_id.exists'] = 'Uveďte prosím platné ID společnosti.';
+
