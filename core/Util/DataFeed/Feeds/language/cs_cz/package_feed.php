19562ba8e7f98092525f1f99c4c045f8bc3b9f62
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:08 -0800

    Updated cs_cz language files

---
+++ core/Util/DataFeed/Feeds/language/cs_cz/package_feed.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Package Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageFeed.!error.invalid_endpoint'] = 'Požadovaný koncový bod není platný nebo neexistuje.';
+$lang['PackageFeed.getOptionFields.example_code_pricing'] = 'Zobrazení ceny, obnovení, poplatku za zřízení, poplatku za zrušení nebo měny ceny balíčku:';
+$lang['PackageFeed.getOptionFields.example_code_description'] = 'Zobrazit popis balíčku:';
+$lang['PackageFeed.getOptionFields.example_code_name'] = 'Zobrazení názvu balíčku:';
+$lang['PackageFeed.getOptionFields.title_row_example_code'] = 'Příklad kódu';
+$lang['PackageFeed.description'] = 'Vrátí název, popis nebo cenu balíčku.';
+$lang['PackageFeed.name'] = 'Balíčky';
+
