e42bf2c6a7e0b472af2e6c9eacf3a3596468cc13
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:09 -0800

    Updated it_it language files

---
+++ core/Automation/Tasks/Task/language/it_it/unsuspend_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Unsuspend Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.unsuspendservices.success'] = 'Il servizio #%1$s del cliente %2$s è stato sospeso.';
+$lang['Automation.task.unsuspendservices.error'] = 'Il servizio #%1$s del client %2$s non può essere sospeso.';
+$lang['Automation.task.unsuspendservices.completed'] = 'L\'attività di disattivazione dei servizi è stata completata.';
+$lang['Automation.task.unsuspendservices.attempt'] = 'Tentativo di disinstallare i servizi sospesi a pagamento.';
+
