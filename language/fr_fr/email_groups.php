949b7296703911d44b9a2a05b3eb39cc3b203e37
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:20 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/email_groups.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Email Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['EmailGroups.!error.action.empty'] = 'Veuillez saisir une action.';
+$lang['EmailGroups.!error.action.length'] = 'La longueur de l\'action ne doit pas dépasser 64 caractères.';
+$lang['EmailGroups.!error.action.unique'] = 'Une action de ce nom existe déjà.';
+$lang['EmailGroups.!error.type.format'] = 'Type non valide.';
+$lang['EmailGroups.!error.notice_type.valid'] = 'Le type d\'avis doit être défini sur bcc ou to.';
+$lang['EmailGroups.!error.group_id.exists'] = 'ID du groupe de messagerie invalide.';
+
