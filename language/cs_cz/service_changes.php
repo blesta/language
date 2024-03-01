5587ac0314210ba5eba1e79f8e647311569ffae1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:14 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/service_changes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Service Changes
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ServiceChanges.status.canceled'] = 'Zrušeno';
+$lang['ServiceChanges.status.error'] = 'Chyba';
+$lang['ServiceChanges.status.completed'] = 'Dokončeno';
+$lang['ServiceChanges.status.pending'] = 'Čeká se na';
+$lang['ServiceChanges.!error.status.valid'] = 'Vyberte prosím platný stav.';
+$lang['ServiceChanges.!error.invoice_id.unique'] = 'ID faktury je již nastaveno pro změnu služby a nesmí být nastaveno znovu.';
+$lang['ServiceChanges.!error.invoice_id.exists'] = 'Neplatné ID faktury.';
+$lang['ServiceChanges.!error.service_id.exists'] = 'Neplatné ID služby.';
+$lang['ServiceChanges.!error.id.exists'] = 'Neplatné ID změny služby.';
+
