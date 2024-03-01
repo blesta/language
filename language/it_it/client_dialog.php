6fd6db0b4fb1a5825eefa49728f378b2a78d02d3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:49 -0800

    Updated it_it language files

---
+++ language/it_it/client_dialog.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Client Dialog
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDialog.password.btn_use_password'] = 'Utilizzare la password';
+$lang['ClientDialog.password.btn_cancel'] = 'Annullamento';
+$lang['ClientDialog.password.btn_generate'] = 'Generare';
+$lang['ClientDialog.password.field_modal_password_copied'] = 'Ho copiato questa password in un luogo sicuro';
+$lang['ClientDialog.password.field_modal_password'] = 'Password';
+$lang['ClientDialog.password.title'] = 'Generare la password';
+$lang['ClientDialog.confirm.text_no'] = 'No';
+$lang['ClientDialog.confirm.text_yes'] = 'Sì';
+
