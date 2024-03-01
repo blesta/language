85d4614c772a0cc0698d224a01502337f6059d6a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:22 -0800

    Updated it_it language files

---
+++ plugins/order/lib/order_types/domain/language/it_it/order_type_domain.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Order Type Domain
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderTypeDomain.settings.field_search'] = 'Ricerca TLD';
+$lang['Main.steps.step_1_description'] = 'Scegliere un dominio';
+$lang['OrderTypeDomain.settings.heading_options'] = 'Opzioni';
+$lang['OrderTypeDomain.settings.heading_period'] = 'Periodo';
+$lang['OrderTypeDomain.settings.heading_term'] = 'Termine';
+$lang['OrderTypeDomain.settings.text_term'] = 'Durata e periodo minimo';
+$lang['OrderTypeDomain.settings.text_available_package_groups'] = 'Gruppi di pacchetti disponibili';
+$lang['OrderTypeDomain.settings.text_assigned_package_groups'] = 'Gruppi di pacchetti idonei';
+$lang['OrderTypeDomain.settings.text_available_tlds'] = 'TLD disponibili';
+$lang['OrderTypeDomain.settings.text_assigned_tlds'] = 'TLD approvati';
+$lang['OrderTypeDomain.settings.tooltip_free_domain'] = 'Include un dominio gratuito da un elenco di TLD approvati con l\'acquisto di un altro pacchetto.';
+$lang['OrderTypeDomain.settings.field_first_term'] = 'Gratuito solo per il primo trimestre';
+$lang['OrderTypeDomain.settings.field_limit_term'] = 'Limitare il dominio al minor numero di anni';
+$lang['OrderTypeDomain.settings.field_free_domain'] = 'Includere un dominio gratuito quando si acquista qualcos\'altro';
+$lang['OrderTypeDomain.settings.free_domain_title'] = 'Dominio libero';
+$lang['OrderTypeDomain.settings.domain_group_title'] = 'Gruppo di pacchetti di dominio';
+$lang['OrderTypeDomain.!error.meta[domain_group].groups'] = 'Il gruppo di pacchetti di dominio non può essere anche un gruppo di pacchetti assegnato.';
+$lang['OrderTypeDomain.!error.meta[domain_group].exists'] = 'Selezionare un gruppo di pacchetti di dominio valido.';
+$lang['OrderTypeDomain.!error.template.valid'] = 'Il modello selezionato richiede almeno un gruppo di pacchetti.';
+$lang['OrderTypeDomain.!error.domain.empty'] = 'Inserire un nome di dominio.';
+$lang['OrderTypeDomain.!error.domain.invalid'] = 'Il nome di dominio non è supportato.';
+$lang['OrderTypeDomain.settings.tooltip_limit_term'] = 'Limitare il dominio al numero minimo di anni consentito per il TLD, di solito 1 o 2 anni.';
+$lang['OrderTypeDomain.name'] = 'Dominio e altro';
+
