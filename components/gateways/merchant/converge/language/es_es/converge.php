9b13af864af2db8928b916be52217dc817d969ae
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:21 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/converge/language/es_es/converge.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Converge
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Converge.name'] = 'Converge';
+$lang['Converge.description'] = 'Antes VirtualMerchant';
+$lang['Converge.login_id'] = 'Identificador';
+$lang['Converge.transaction_key'] = 'Clave de transacción';
+$lang['Converge.api'] = 'API de pago';
+$lang['Converge.merchant_id'] = 'ID de comerciante';
+$lang['Converge.user_id'] = 'ID de usuario';
+$lang['Converge.pin'] = 'PIN';
+$lang['Converge.live_mode'] = 'Modo Live';
+$lang['Converge.multiCurrency'] = 'La Cuenta Converge está configurada para Multidivisa';
+$lang['Converge.yes'] = 'Sí';
+$lang['Converge.no'] = 'No';
+$lang['Converge.multiCurrency.error'] = 'Debes especificar si tu cuenta Converge está configurada para multidivisa o no.';
+$lang['Converge.!error.login_id.format'] = 'El ID de inicio de sesión no debe tener más de 20 caracteres y no puede estar vacío.';
+$lang['Converge.!error.transaction_key.format'] = 'La clave de la transacción debe tener 16 caracteres de longitud.';
+$lang['Converge.!error.live_mode.valid'] = 'El modo en directo debe establecerse en "true" si se da.';
+
