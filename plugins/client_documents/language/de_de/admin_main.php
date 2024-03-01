fb195fbcc480bb0f30e14ca7ce0bc7f64df8d6d5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:28 -0800

    Updated de_de language files

---
+++ plugins/client_documents/language/de_de/admin_main.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.add.field_submit'] = 'Dokument hochladen';
+$lang['AdminMain.add.field_description'] = 'Beschreibung';
+$lang['AdminMain.add.field_document'] = 'Dokument';
+$lang['AdminMain.add.field_name'] = 'Name';
+$lang['AdminMain.add.boxtitle_adddocument'] = 'Dokument hochladen';
+$lang['AdminMain.modal.delete_document'] = 'Dieses Dokument wirklich löschen?';
+$lang['AdminMain.index.option_download'] = 'Herunterladen';
+$lang['AdminMain.index.option_delete'] = 'Löschen';
+$lang['AdminMain.index.heading_date_options'] = 'Optionen';
+$lang['AdminMain.index.heading_date_added'] = 'Datum hinzugefügt';
+$lang['AdminMain.index.heading_description'] = 'Beschreibung';
+$lang['AdminMain.index.heading_name'] = 'Name';
+$lang['AdminMain.index.no_results'] = 'Es sind keine Dokumente für diesen Mandanten eingestellt.';
+$lang['AdminMain.index.boxtitle_documents'] = 'Dokumente';
+$lang['AdminMain.index.boxtitle_client'] = 'Kunde #%1$s - %2$s %3$s';
+$lang['AdminMain.index.categorylink_adddocument'] = 'Dokument hinzufügen';
+$lang['AdminMain.add.page_title'] = 'Dokumente > Hinzufügen';
+$lang['AdminMain.index.page_title'] = 'Dokumente';
+$lang['AdminMain.!success.document_deleted'] = 'Das Dokument wurde gelöscht.';
+$lang['AdminMain.!success.document_uploaded'] = 'Das Dokument wurde hochgeladen.';
+
