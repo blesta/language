87e017c54d53f0681aac543981f92aba5fb6af25
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:12 -0800

    Updated es_es language files

---
+++ components/reports/tax_liability/language/es_es/tax_liability.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Tax Liability
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['TaxLiability.name'] = 'Responsabilidad fiscal';
+$lang['TaxLiability.options.field_start_date'] = 'Fecha de inicio';
+$lang['TaxLiability.options.field_end_date'] = 'Fecha final';
+$lang['TaxLiability.options.field_cascade_true'] = 'Sí';
+$lang['TaxLiability.options.field_cascade_false'] = 'No';
+$lang['TaxLiability.heading.id_code'] = 'Factura #';
+$lang['TaxLiability.heading.client_id_code'] = 'Cliente';
+$lang['TaxLiability.heading.subtotal'] = 'Subtotal';
+$lang['TaxLiability.heading.taxable_amount'] = 'Base imponible';
+$lang['TaxLiability.heading.level1_tax_rate'] = 'Nivel 1 Tipo impositivo (%)';
+$lang['TaxLiability.heading.level1_tax_amount'] = 'Nivel 1 Importe del impuesto';
+$lang['TaxLiability.heading.level1_tax_country'] = 'Nivel 1 País';
+$lang['TaxLiability.heading.level1_tax_state'] = 'Nivel 1 Estado/Provincia';
+$lang['TaxLiability.heading.level2_tax_rate'] = 'Nivel 2 Tipo impositivo (%)';
+$lang['TaxLiability.heading.level2_tax_amount'] = 'Nivel 2 Importe del impuesto';
+$lang['TaxLiability.heading.level2_tax_country'] = 'Nivel 2 País';
+$lang['TaxLiability.heading.level2_tax_state'] = 'Nivel 2 Estado/Provincia';
+$lang['TaxLiability.heading.cascade'] = 'Impuesto de Cascada';
+$lang['TaxLiability.heading.currency'] = 'Moneda';
+$lang['TaxLiability.heading.date_closed'] = 'Fecha de cierre';
+
