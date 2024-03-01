f862291fbb2f638681199055f166684468b5fd2c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:33 -0800

    Updated es_es language files

---
+++ language/es_es/navigation.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Navigation
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Navigation.!error.action_id.valid'] = 'ID de acción no válido';
+$lang['Navigation.!error.order.valid'] = 'El índice de orden debe ser un valor numérico';
+$lang['Navigation.!error.parent_id.valid'] = 'ID de elemento de navegación principal no válido';
+$lang['Navigation.getprimary.nav_home'] = 'Inicio';
+$lang['Navigation.getprimary.nav_home_dashboard'] = 'Cuadro de mandos';
+$lang['Navigation.getprimary.nav_clients'] = 'Clientes';
+$lang['Navigation.getprimary.nav_clients_browse'] = 'Buscar clientes';
+$lang['Navigation.getprimary.nav_billing'] = 'Facturación';
+$lang['Navigation.getprimary.nav_billing_overview'] = 'Visión general';
+$lang['Navigation.getprimary.nav_billing_invoices'] = 'Facturas';
+$lang['Navigation.getprimary.nav_billing_quotations'] = 'Citas';
+$lang['Navigation.getprimary.nav_billing_transactions'] = 'Transacciones';
+$lang['Navigation.getprimary.nav_billing_services'] = 'Servicios';
+$lang['Navigation.getprimary.nav_billing_reports'] = 'Informes';
+$lang['Navigation.getprimary.nav_billing_printqueue'] = 'Cola de impresión';
+$lang['Navigation.getprimary.nav_billing_batch'] = 'Lote';
+$lang['Navigation.getprimary.nav_packages'] = 'Paquetes';
+$lang['Navigation.getprimary.nav_packages_browse'] = 'Ver paquetes';
+$lang['Navigation.getprimary.nav_packages_groups'] = 'Grupos de paquetes';
+$lang['Navigation.getprimary.nav_package_options'] = 'Opciones configurables';
+$lang['Navigation.getprimary.nav_tools'] = 'Herramientas';
+$lang['Navigation.getprimary.nav_tools_logs'] = 'Registros';
+$lang['Navigation.getprimary.nav_tools_currency'] = 'Convertir moneda';
+$lang['Navigation.getprimary.nav_tools_utilities'] = 'Servicios';
+$lang['Navigation.getprimary.nav_tools_renewals'] = 'Cola de renovación';
+$lang['Navigation.getprimary.nav_settings_company'] = 'Empresa';
+$lang['Navigation.getprimary.nav_settings_system'] = 'Sistema';
+$lang['Navigation.getprimaryclient.nav_dashboard'] = 'Cuadro de mandos';
+$lang['Navigation.getprimaryclient.nav_services'] = 'Servicios';
+$lang['Navigation.getprimaryclient.nav_paymentaccounts'] = 'Cuentas de pago';
+$lang['Navigation.getprimaryclient.nav_paymentaccounts_add'] = 'Añadir cuenta de pago';
+$lang['Navigation.getprimaryclient.nav_contacts'] = 'Contactos';
+$lang['Navigation.getprimaryclient.nav_contacts_add'] = 'Añadir contacto';
+$lang['Navigation.getprimaryclient.nav_return'] = 'Volver al panel de control';
+$lang['Navigation.getprimarypublic.nav_dashboard'] = 'Inicio';
+$lang['Navigation.getcompany.nav_general'] = 'General';
+$lang['Navigation.getcompany.nav_general_localization'] = 'Localización';
+$lang['Navigation.getcompany.nav_general_international'] = 'Internacionalización';
+$lang['Navigation.getcompany.nav_general_encryption'] = 'Cifrado';
+$lang['Navigation.getcompany.nav_general_contacttypes'] = 'Tipos de contacto';
+$lang['Navigation.getcompany.nav_general_marketing'] = 'Marketing';
+$lang['Navigation.getcompany.nav_general_smart_search'] = 'Búsqueda inteligente';
+$lang['Navigation.getcompany.nav_general_humanverification'] = 'Verificación humana';
+$lang['Navigation.getcompany.nav_lookandfeel'] = 'Aspecto y tacto';
+$lang['Navigation.getcompany.nav_lookandfeel_themes'] = 'Temas';
+$lang['Navigation.getcompany.nav_lookandfeel_template'] = 'Plantilla';
+$lang['Navigation.getcompany.nav_lookandfeel_layout'] = 'Diseño';
+$lang['Navigation.getcompany.nav_lookandfeel_navigation'] = 'Navegación';
+$lang['Navigation.getcompany.nav_lookandfeel_actions'] = 'Acciones';
+$lang['Navigation.getcompany.nav_lookandfeel_customize'] = 'Personalice';
+$lang['Navigation.getcompany.nav_automation'] = 'Automatización';
+$lang['Navigation.getcompany.nav_billing'] = 'Facturación/Pago';
+$lang['Navigation.getcompany.nav_billing_invoices'] = 'Opciones de facturación y cobro';
+$lang['Navigation.getcompany.nav_billing_custominvoice'] = 'Personalización de facturas';
+$lang['Navigation.getcompany.nav_billing_deliverymethods'] = 'Entrega de facturas';
+$lang['Navigation.getcompany.nav_billing_latefees'] = 'Tasas de demora';
+$lang['Navigation.getcompany.nav_billing_notices'] = 'Avisos';
+$lang['Navigation.getcompany.nav_billing_coupons'] = 'Cupones';
+$lang['Navigation.getcompany.nav_billing_acceptedtypes'] = 'Formas de pago aceptadas';
+$lang['Navigation.getcompany.nav_modules'] = 'Módulos';
+$lang['Navigation.getcompany.nav_messengers'] = 'Mensajeros';
+$lang['Navigation.getcompany.nav_messengers_messengers'] = 'Mensajeros';
+$lang['Navigation.getcompany.nav_messengers_configuration'] = 'Configuración de Messenger';
+$lang['Navigation.getcompany.nav_messengers_templates'] = 'Plantillas de mensajes';
+$lang['Navigation.getcompany.nav_gateways'] = 'Pasarelas de pago';
+$lang['Navigation.getcompany.nav_taxes'] = 'Impuestos';
+$lang['Navigation.getcompany.nav_taxes_basictax'] = 'Configuración fiscal básica';
+$lang['Navigation.getcompany.nav_taxes_taxrules'] = 'Normas fiscales';
+$lang['Navigation.getcompany.nav_emails'] = 'Correos electrónicos';
+$lang['Navigation.getcompany.nav_emails_mail'] = 'Configuración del correo';
+$lang['Navigation.getcompany.nav_emails_templates'] = 'Plantillas de correo electrónico';
+$lang['Navigation.getcompany.nav_emails_signatures'] = 'Firmas';
+$lang['Navigation.getcompany.nav_clientoptions'] = 'Opciones para clientes';
+$lang['Navigation.getcompany.nav_clientoptions_general'] = 'Configuración general del cliente';
+$lang['Navigation.getcompany.nav_clientoptions_customfields'] = 'Campos personalizados de cliente';
+$lang['Navigation.getcompany.nav_clientoptions_requiredfields'] = 'Campos obligatorios del cliente';
+$lang['Navigation.getcompany.nav_currencies'] = 'Divisas';
+$lang['Navigation.getcompany.nav_currency_currencysetup'] = 'Configuración de divisas';
+$lang['Navigation.getcompany.nav_currency_active'] = 'Divisas activas';
+$lang['Navigation.getcompany.nav_plugins'] = 'Plugins';
+$lang['Navigation.getcompany.nav_groups'] = 'Grupos de clientes';
+$lang['Navigation.getcompany.nav_feeds'] = 'Fuentes de datos';
+$lang['Navigation.getcompany.nav_feeds_general'] = 'General';
+$lang['Navigation.getcompany.nav_feeds_settings'] = 'Ajustes de alimentación';
+$lang['Navigation.getsystem.nav_general'] = 'General';
+$lang['Navigation.getsystem.nav_general_basic'] = 'Configuración básica';
+$lang['Navigation.getsystem.nav_general_geoip'] = 'Configuración GeoIP';
+$lang['Navigation.getsystem.nav_general_maintenance'] = 'Mantenimiento';
+$lang['Navigation.getsystem.nav_general_license'] = 'Clave de licencia';
+$lang['Navigation.getsystem.nav_general_paymenttypes'] = 'Tipos de pago';
+$lang['Navigation.getsystem.nav_automation'] = 'Automatización';
+$lang['Navigation.getsystem.nav_companies'] = 'Empresas';
+$lang['Navigation.getsystem.nav_backup'] = 'Copia de seguridad';
+$lang['Navigation.getsystem.nav_backup_ftp'] = 'FTP seguro';
+$lang['Navigation.getsystem.nav_backup_index'] = 'A la carta';
+$lang['Navigation.getsystem.nav_backup_amazon'] = 'Amazon S3';
+$lang['Navigation.getsystem.nav_staff'] = 'Personal';
+$lang['Navigation.getsystem.nav_staff_manage'] = 'Gestionar el personal';
+$lang['Navigation.getsystem.nav_staff_groups'] = 'Grupos de personal';
+$lang['Navigation.getsystem.nav_api'] = 'Acceso API';
+$lang['Navigation.getsystem.nav_upgrade'] = 'Opciones de actualización';
+$lang['Navigation.getsystem.nav_help'] = 'Ayuda';
+$lang['Navigation.getsystem.nav_help_index'] = 'Recursos';
+$lang['Navigation.getsystem.nav_help_notes'] = 'Notas de publicación';
+$lang['Navigation.getsystem.nav_help_about'] = 'Acerca de Blesta';
+$lang['Navigation.getsystem.nav_marketplace'] = 'Mercado';
+$lang['Navigation.getsearchoptions.smart'] = 'Búsqueda inteligente';
+$lang['Navigation.getsearchoptions.clients'] = 'Búsqueda de clientes';
+$lang['Navigation.getsearchoptions.invoices'] = 'Búsqueda de facturas';
+$lang['Navigation.getsearchoptions.transactions'] = 'Búsqueda de transacciones';
+$lang['Navigation.getsearchoptions.services'] = 'Búsqueda de servicios';
+$lang['Navigation.getsearchoptions.packages'] = 'Búsqueda de paquetes';
+$lang['Navigation.getprimary.nav_tools_blacklist'] = 'Lista negra';
+
