5fec10c2307a5e8eb770ae837eb9fc930a1de511
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:27 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/unsuspend_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Unsuspend Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.unsuspendservices.attempt'] = 'Intentar anular la suspensión de servicios de pago.';
+$lang['Automation.task.unsuspendservices.completed'] = 'La tarea de cancelación de servicios ha finalizado.';
+$lang['Automation.task.unsuspendservices.error'] = 'El servicio #%1$s del cliente %2$s no ha podido ser desbloqueado.';
+$lang['Automation.task.unsuspendservices.success'] = 'El servicio #%1$s del cliente %2$s ha sido cancelado.';
+
