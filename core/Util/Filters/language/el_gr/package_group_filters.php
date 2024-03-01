4a90c5cb0c43302470d838979d48a3823edb78c5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:32 -0800

    Updated el_gr language files

---
+++ core/Util/Filters/language/el_gr/package_group_filters.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Package Group Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.package_group_filters.field_options'] = 'Επιλογές';
+$lang['Util.filters.package_group_filters.options.hidden'] = 'Εμφάνιση κρυφών ομάδων πακέτων';
+
