df53425c1bed47df53a3439fcffff308569ce85b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:52 -0800

    Updated it_it language files

---
+++ components/reports/aging_invoices/language/it_it/aging_invoices.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Aging Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AgingInvoices.heading.past90'] = '90+';
+$lang['AgingInvoices.heading.past60'] = '60';
+$lang['AgingInvoices.heading.past30'] = '30';
+$lang['AgingInvoices.heading.date_due'] = 'Data di scadenza';
+$lang['AgingInvoices.heading.date_billed'] = 'Data di fatturazione';
+$lang['AgingInvoices.heading.status'] = 'Stato';
+$lang['AgingInvoices.heading.currency'] = 'Valuta';
+$lang['AgingInvoices.heading.paid'] = 'Pagato';
+$lang['AgingInvoices.heading.total'] = 'Totale';
+$lang['AgingInvoices.heading.subtotal'] = 'Subtotale';
+$lang['AgingInvoices.heading.client_numbers'] = 'Numeri di telefono dei clienti';
+$lang['AgingInvoices.heading.client_email'] = 'Email del cliente';
+$lang['AgingInvoices.heading.client_last_name'] = 'Cognome del cliente';
+$lang['AgingInvoices.heading.client_first_name'] = 'Nome del cliente';
+$lang['AgingInvoices.heading.client_id_code'] = 'Cliente #';
+$lang['AgingInvoices.heading.id_code'] = 'Fattura #';
+$lang['AgingInvoices.option.any'] = 'Qualsiasi';
+$lang['AgingInvoices.options.field_status'] = 'Stato';
+$lang['AgingInvoices.name'] = 'Invecchiamento delle fatture';
+
