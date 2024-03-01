858ec97c7a52bd6c370636f4a436cfb6b44aee69
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:29 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/btcpay_server/language/fr_fr/btcpay_server.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Btcpay Server
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BtcpayServer.name'] = 'BTCPay (Bitcoin)';
+$lang['BtcpayServer.description'] = 'BTCPayServer est une passerelle de paiement Bitcoin auto-hébergée et sans frais.';
+$lang['BtcpayServer.!error.server_url.valid'] = 'L\'URL du serveur n\'est pas valide.';
+$lang['BtcpayServer.!error.transaction_speed.valid'] = 'Veuillez sélectionner une vitesse de transaction valide.';
+$lang['BtcpayServer.!error.store_id.valid'] = 'L\'ID du magasin n\'est pas valide.';
+$lang['BtcpayServer.!error.api_key.valid'] = 'Clé API non valide.';
+$lang['BtcpayServer.!error.failed.response'] = 'La transaction n\'a pas pu être traitée.';
+$lang['BtcpayServer.!error.payment.invalid'] = 'La transaction n\'est pas valide et n\'a pas pu être traitée.';
+$lang['BtcpayServer.!error.payment.expired'] = 'La transaction a expiré et n\'a pas pu être traitée.';
+$lang['BtcpayServer.!error.webhook_secret.valid'] = 'Vous devez saisir un secret Webhook valide.';
+$lang['BtcpayServer.meta.server_url'] = 'URL du serveur';
+$lang['BtcpayServer.meta.store_id'] = 'ID du magasin';
+$lang['BtcpayServer.meta.api_key'] = 'Clé API';
+$lang['BtcpayServer.meta.transaction_speed'] = 'Vitesse de transaction';
+$lang['BtcpayServer.meta.webhook_secret'] = 'Secret du webhook';
+$lang['BtcpayServer.transaction.speed.high'] = 'Haut';
+$lang['BtcpayServer.transaction.speed.medium'] = 'Moyen';
+$lang['BtcpayServer.transaction.speed.low'] = 'Faible';
+$lang['BtcpayServer.webhook'] = 'Webhook du serveur BTCPay';
+$lang['BtcpayServer.webhook_note'] = 'Avant de commencer à utiliser cette passerelle, vous devez configurer l\'url suivante en tant que Webhook dans votre serveur BTCPay.';
+$lang['BtcpayServer.buildprocess.submit'] = 'Soumettre le paiement';
+
