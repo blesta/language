22b3490e4cfba08d3f166d9afe9e01aef7b0a673
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:34 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/ajax/language/el_gr/cart.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Cart
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cart.index.header_cart'] = 'Περίληψη παραγγελίας';
+$lang['Cart.index.header_note'] = 'Οποιοσδήποτε ισχύων φόρος θα υπολογιστεί αφού εγγραφείτε ή συνδεθείτε.';
+$lang['Cart.index.table_description'] = 'Περιγραφή';
+$lang['Cart.index.table_term'] = 'Όρος';
+$lang['Cart.index.table_qty'] = 'Ποσότητα';
+$lang['Cart.index.table_price'] = 'Τιμή';
+$lang['Cart.index.edit_item'] = 'Επεξεργασία';
+$lang['Cart.index.remove_item'] = 'Αφαιρέστε το';
+$lang['Cart.index.have_coupon'] = 'Χρησιμοποιήστε έναν κωδικό κουπονιού';
+$lang['Cart.index.coupon'] = 'Κουπόνι';
+$lang['Cart.index.order_more_btn'] = 'Κάντε κλικ για να παραγγείλετε περισσότερα αντικείμενα';
+$lang['Cart.index.order_more_note'] = 'Ή εγγραφείτε/συνδεθείτε και συνεχίστε την πληρωμή.';
+
