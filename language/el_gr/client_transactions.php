ec13b94b4ac84fd7dbecee31b8cb846414d6fb15
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:09 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_transactions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Client Transactions
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientTransactions.applied.no_results'] = 'Αυτή η συναλλαγή δεν έχει εφαρμοστεί σε κανένα τιμολόγιο.';
+$lang['ClientTransactions.applied.heading_appliedon'] = 'Εφαρμόζεται στο';
+$lang['ClientTransactions.applied.heading_amount'] = 'Ποσό';
+$lang['ClientTransactions.applied.heading_invoice'] = 'Τιμολόγιο';
+$lang['ClientTransactions.index.no_results'] = 'Δεν έχετε συναλλαγές %1$s.';
+$lang['ClientTransactions.index.heading_date'] = 'Ημερομηνία';
+$lang['ClientTransactions.index.heading_number'] = 'Αριθμός';
+$lang['ClientTransactions.index.heading_applied'] = 'Εφαρμοσμένο';
+$lang['ClientTransactions.index.heading_credited'] = 'Συντελεστές';
+$lang['ClientTransactions.index.heading_amount'] = 'Ποσό';
+$lang['ClientTransactions.index.heading_type'] = 'Τύπος';
+$lang['ClientTransactions.index.category_declined'] = 'Απορρίφθηκε';
+$lang['ClientTransactions.index.category_pending'] = 'Εκκρεμεί';
+$lang['ClientTransactions.index.category_approved'] = 'Εγκεκριμένο';
+$lang['ClientTransactions.index.boxtitle_transactions'] = 'Συναλλαγές';
+$lang['ClientTransactions.index.page_title'] = 'Πελάτης #%1$s Συναλλαγές';
+
