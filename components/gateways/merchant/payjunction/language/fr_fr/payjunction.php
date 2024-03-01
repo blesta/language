46379ff61d60ff4c076bb308384f76575c133a4a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:26 -0800

    Updated fr_fr language files

---
+++ components/gateways/merchant/payjunction/language/fr_fr/payjunction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payjunction
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PayJunction.name'] = 'PayJunction';
+$lang['PayJunction.description'] = 'PayJunction traite 4 milliards de dollars par an et connaît une croissance rapide. Sa technologie est idéale pour les entreprises multi-sites et multi-départements qui traitent un volume mensuel de cartes de crédit de 10 000 $ ou plus.';
+$lang['PayJunction.api_key'] = 'Clé API';
+$lang['PayJunction.user_name'] = 'Nom d\'utilisateur';
+$lang['PayJunction.password'] = 'Mot de passe';
+$lang['PayJunction.test_mode'] = 'Mode développeur';
+$lang['PayJunction.api_key_note'] = 'Saisissez la clé API de votre compte PayJunction.';
+$lang['PayJunction.user_name_note'] = 'Saisissez le nom d\'utilisateur de votre compte PayJunction.';
+$lang['PayJunction.password_note'] = 'Saisissez le mot de passe de votre compte PayJunction.';
+$lang['PayJunction.test_mode_note'] = 'L\'activation du mode développeur permet de poster des transactions dans l\'environnement de démonstration de PayJunction Labs. Vous devez utiliser le nom d\'utilisateur et le mot de passe de la démo, ainsi que votre clé API Labs, pour utiliser le mode développeur.';
+$lang['PayJunction.!error.api_key.empty'] = 'Veuillez saisir une clé API valide.';
+$lang['PayJunction.!error.user_name.empty'] = 'Veuillez saisir le nom d\'utilisateur de votre compte.';
+$lang['PayJunction.!error.password.empty'] = 'Veuillez saisir le mot de passe de votre compte.';
+$lang['PayJunction.!error.test_mode.valid'] = 'Le mode développeur doit être défini sur "true" s\'il est indiqué.';
+
