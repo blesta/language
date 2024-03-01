5ac0b6b1251e5220fa0af4cd809d53c4d5b0db9d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:48 -0800

    Updated it_it language files

---
+++ plugins/support_manager/language/it_it/support_manager_departments.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Support Manager Departments
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerDepartments.reminder_intervals.minutes'] = '%1$s Minuti';
+$lang['SupportManagerDepartments.reminder_intervals.hours'] = '%1$s Ore';
+$lang['SupportManagerDepartments.reminder_intervals.hour'] = '1 ora';
+$lang['SupportManagerDepartments.ticket_intervals.days'] = '%1$s Giorni';
+$lang['SupportManagerDepartments.ticket_intervals.day'] = '1 giorno';
+$lang['SupportManagerDepartments.visibility_options.staff'] = 'Solo per il personale';
+$lang['SupportManagerDepartments.visibility_options.client'] = 'Cliente e personale';
+$lang['SupportManagerDepartments.field_types.password'] = 'Password';
+$lang['SupportManagerDepartments.field_types.textarea'] = 'Area di testo';
+$lang['SupportManagerDepartments.field_types.text'] = 'Testo';
+$lang['SupportManagerDepartments.field_types.quantity'] = 'Quantità';
+$lang['SupportManagerDepartments.field_types.select'] = 'A discesa';
+$lang['SupportManagerDepartments.field_types.radio'] = 'Radio';
+$lang['SupportManagerDepartments.field_types.checkbox'] = 'Casella di controllo';
+$lang['SupportManagerDepartments.message_types.deleted'] = 'Soppresso';
+$lang['SupportManagerDepartments.message_types.read'] = 'Leggi';
+$lang['SupportManagerDepartments.security_types.tls'] = 'TLS';
+$lang['SupportManagerDepartments.security_types.ssl'] = 'SSL';
+$lang['SupportManagerDepartments.security_types.none'] = 'Nessuno';
+$lang['SupportManagerDepartments.priorities.low'] = 'Basso';
+$lang['SupportManagerDepartments.priorities.medium'] = 'Medio';
+$lang['SupportManagerDepartments.priorities.high'] = 'Alto';
+$lang['SupportManagerDepartments.priorities.critical'] = 'Critico';
+$lang['SupportManagerDepartments.priorities.emergency'] = 'Emergenza';
+$lang['SupportManagerDepartments.statuses.visible'] = 'Visibile';
+$lang['SupportManagerDepartments.statuses.hidden'] = 'Nascosto';
+$lang['SupportManagerDepartments.methods.imap'] = 'IMAP';
+$lang['SupportManagerDepartments.methods.pop3'] = 'POP3';
+$lang['SupportManagerDepartments.methods.pipe'] = 'Tubazioni';
+$lang['SupportManagerDepartments.methods.none'] = 'Nessuno';
+$lang['SupportManagerDepartments.!error.department_id.has_tickets'] = 'Non è stato possibile eliminare il reparto perché attualmente gli sono stati assegnati dei ticket.';
+$lang['SupportManagerDepartments.!error.auto_delete.format'] = 'La scelta di cancellare automaticamente i dati del campo dopo la chiusura di un ticket deve essere impostata su 0 o 1.';
+$lang['SupportManagerDepartments.!error.encrypted.format'] = 'L\'opzione di crittografia dei dati del campo deve essere impostata su 0 o 1.';
+$lang['SupportManagerDepartments.!error.client_add.format'] = 'Se consentire al client di aggiungere deve essere impostato a 0 o 1.';
+$lang['SupportManagerDepartments.!error.type.format'] = 'Selezionare un tipo di campo valido.';
+$lang['SupportManagerDepartments.!error.visibility.format'] = 'Selezionare uno stato di visibilità valido.';
+$lang['SupportManagerDepartments.!error.label.empty'] = 'Inserire un\'etichetta.';
+$lang['SupportManagerDepartments.!error.department_id.exists'] = 'ID reparto non valido.';
+$lang['SupportManagerDepartments.!error.status.format'] = 'Tipo di stato non valido.';
+$lang['SupportManagerDepartments.!error.response_id.format'] = 'Selezionare una risposta di chiusura automatica valida.';
+$lang['SupportManagerDepartments.!error.max_attachment_size.format'] = 'La dimensione massima dell\'allegato deve essere un valore numerico.';
+$lang['SupportManagerDepartments.!error.attachment_types.length'] = 'L\'elenco dei tipi di allegati non può superare i 255 caratteri.';
+$lang['SupportManagerDepartments.!error.include_attachments.format'] = 'La scelta di includere o meno gli allegati dei biglietti deve essere impostata su 0 o 1.';
+$lang['SupportManagerDepartments.!error.reminder_ticket_priority.format'] = 'Si prega di selezionare una priorità valida per il promemoria dei biglietti.';
+$lang['SupportManagerDepartments.!error.reminder_ticket_status.format'] = 'Selezionare uno stato di sollecito del biglietto valido.';
+$lang['SupportManagerDepartments.!error.reminder_ticket_interval.format'] = 'Selezionare un intervallo valido per i biglietti di sollecito.';
+$lang['SupportManagerDepartments.!error.delete_ticket_interval.format'] = 'Selezionare un intervallo di cancellazione valido.';
+$lang['SupportManagerDepartments.!error.close_ticket_interval.format'] = 'Selezionare un intervallo di chiusura valido.';
+$lang['SupportManagerDepartments.!error.send_ticket_received.format'] = 'L\'invio di e-mail di conferma del ticket per questo reparto deve essere impostato su 0 o 1.';
+$lang['SupportManagerDepartments.!error.override_from_email.format'] = 'L\'opzione che consente di utilizzare l\'indirizzo e-mail di questo reparto come indirizzo di provenienza nei modelli di e-mail deve essere impostata su 0 o 1.';
+$lang['SupportManagerDepartments.!error.require_captcha.format'] = 'La scelta di richiedere la verifica umana deve essere impostata su 0 o 1.';
+$lang['SupportManagerDepartments.!error.clients_only.format'] = 'L\'opzione che consente ai clienti di aprire o rispondere ai ticket deve essere impostata su 0 o 1.';
+$lang['SupportManagerDepartments.!error.mark_messages.valid'] = 'I messaggi che utilizzano POP3 possono essere contrassegnati come eliminati.';
+$lang['SupportManagerDepartments.!error.mark_messages.format'] = 'Selezionare un tipo di messaggio valido per contrassegnare i messaggi.';
+$lang['SupportManagerDepartments.!error.security.format'] = 'Selezionare un tipo di sicurezza valido.';
+$lang['SupportManagerDepartments.!error.service.format'] = 'Selezionare un tipo di servizio valido.';
+$lang['SupportManagerDepartments.!error.port.length'] = 'La lunghezza della porta non può superare le 6 cifre.';
+$lang['SupportManagerDepartments.!error.port.format'] = 'Inserire un numero di porta.';
+$lang['SupportManagerDepartments.!error.password.format'] = 'Inserire una password.';
+$lang['SupportManagerDepartments.!error.user.length'] = 'Il nome utente non può superare i 64 caratteri.';
+$lang['SupportManagerDepartments.!error.user.format'] = 'Inserire un nome utente.';
+$lang['SupportManagerDepartments.!error.host.length'] = 'Il nome dell\'host non può superare i 128 caratteri.';
+$lang['SupportManagerDepartments.!error.host.format'] = 'Inserire un nome host valido.';
+$lang['SupportManagerDepartments.!error.default_priority.format'] = 'Tipo di priorità predefinita non valida.';
+$lang['SupportManagerDepartments.!error.method.mailparse'] = 'L\'estensione PHP Mailparse PECL è necessaria per analizzare i biglietti di posta elettronica.';
+$lang['SupportManagerDepartments.!error.method.imap'] = 'L\'estensione PHP IMAP è necessaria per scaricare i messaggi tramite POP3 o IMAP.';
+$lang['SupportManagerDepartments.!error.method.format'] = 'Tipo di metodo non valido.';
+$lang['SupportManagerDepartments.!error.email.format'] = 'Inserire un indirizzo e-mail valido.';
+$lang['SupportManagerDepartments.!error.description.empty'] = 'Inserire una descrizione.';
+$lang['SupportManagerDepartments.!error.name.empty'] = 'Inserire un nome per questo reparto.';
+$lang['SupportManagerDepartments.!error.company_id.exists'] = 'ID azienda non valido.';
+
