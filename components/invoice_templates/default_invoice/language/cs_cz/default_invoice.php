6a3f7309483e1010c4c9c7b2b9f0a9f5cfb8d7cb
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:13 -0800

    Updated cs_cz language files

---
+++ components/invoice_templates/default_invoice/language/cs_cz/default_invoice.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Default Invoice
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DefaultInvoice.balance_heading'] = 'Bilance';
+$lang['DefaultInvoice.payments_applied_amount'] = 'Použitá částka';
+$lang['DefaultInvoice.payments_transaction_id'] = 'Číslo transakce';
+$lang['DefaultInvoice.payments_type_name'] = 'Typ';
+$lang['DefaultInvoice.payments_applied_date'] = 'Datum podání žádosti';
+$lang['DefaultInvoice.payments_heading'] = 'Platby';
+$lang['DefaultInvoice.watermark_paid'] = 'PAID';
+$lang['DefaultInvoice.lines_cost'] = 'Náklady';
+$lang['DefaultInvoice.lines_unit_price'] = 'Jednotková cena';
+$lang['DefaultInvoice.lines_quantity'] = 'Množství';
+$lang['DefaultInvoice.lines_description'] = 'Popis';
+$lang['DefaultInvoice.total_heading'] = 'Celkem';
+$lang['DefaultInvoice.subtotal_heading'] = 'Mezisoučet';
+$lang['DefaultInvoice.notes_heading'] = 'Poznámky';
+$lang['DefaultInvoice.date_due'] = 'Datum splatnosti:';
+$lang['DefaultInvoice.date_expires'] = 'Datum vypršení platnosti:';
+$lang['DefaultInvoice.date_billed'] = 'Datum faktury:';
+$lang['DefaultInvoice.date_created'] = 'Datum vytvoření:';
+$lang['DefaultInvoice.client_tax_id'] = 'Daňové identifikační číslo zákazníka / DIČ:';
+$lang['DefaultInvoice.tax_id'] = 'Daňové identifikační číslo / DIČ:';
+$lang['DefaultInvoice.client_id_code'] = 'Číslo zákazníka:';
+$lang['DefaultInvoice.quotation_id_code'] = 'Číslo nabídky:';
+$lang['DefaultInvoice.draft_id_code'] = 'Číslo návrhu:';
+$lang['DefaultInvoice.proforma_id_code'] = 'Číslo pro forma:';
+$lang['DefaultInvoice.invoice_id_code'] = 'Číslo faktury:';
+$lang['DefaultInvoice.type_quotation_draft'] = 'NÁVRH CENOVÉ NABÍDKY';
+$lang['DefaultInvoice.type_quotation'] = 'CITACE';
+$lang['DefaultInvoice.type_proforma'] = 'PROFORMA FAKTURA';
+$lang['DefaultInvoice.type_void'] = 'FAKTURA - NEPLATNÁ';
+$lang['DefaultInvoice.type_draft'] = 'FAKTURA - NÁVRH';
+$lang['DefaultInvoice.type_active'] = 'INVOICE';
+$lang['DefaultInvoice.terms_heading'] = 'Podmínky';
+$lang['DefaultInvoice.page_of'] = 'Stránka %1$s z %2$s';
+$lang['DefaultInvoice.name'] = 'Výchozí';
+
