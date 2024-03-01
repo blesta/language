ce700bfad33adff0a553ea6bdb908e3fcf36c1b8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:42 -0800

    Updated es_es language files

---
+++ language/es_es/_custom.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['_PaymentTypes.cash'] = 'Efectivo';
+$lang['_PaymentTypes.check'] = 'Consulte';
+$lang['_PaymentTypes.money_order'] = 'Giro postal';
+$lang['_PaymentTypes.in_house_credit'] = 'Crédito interno';
+
