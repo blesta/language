982e2fbfc6506c831ba2afcdc779d1ecdd323a4d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:19 -0800

    Updated el_gr language files

---
+++ language/el_gr/gateway_payments.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Gateway Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GatewayPayments.!error.type.invalid'] = 'Ο τύπος λογαριασμού πρέπει να είναι "ach" ή "cc".';
+$lang['GatewayPayments.!error.gateway.error'] = 'Η πύλη πληρωμών επέστρεψε σφάλμα κατά την επεξεργασία του αιτήματος.';
+$lang['GatewayPayments.!error.gateway.declined'] = 'Η πληρωμή απορρίφθηκε.';
+$lang['GatewayPayments.!error.response_status'] = 'Η πύλη πληρωμών επέστρεψε μια μη αναμενόμενη απάντηση.';
+$lang['GatewayPayments.!error.reference_id.remove'] = 'Η πύλη πληρωμών δεν μπόρεσε να αφαιρέσει τον λογαριασμό πληρωμής.';
+$lang['GatewayPayments.!error.reference_id.verify'] = 'Η πύλη πληρωμών δεν μπόρεσε να επαληθεύσει τον λογαριασμό πληρωμής.';
+$lang['GatewayPayments.!error.reference_id.update'] = 'Η πύλη πληρωμών δεν μπόρεσε να ενημερώσει τον λογαριασμό πληρωμής.';
+$lang['GatewayPayments.!error.reference_id.store'] = 'Η πύλη πληρωμών δεν μπόρεσε να αποθηκεύσει το λογαριασμό πληρωμής.';
+$lang['GatewayPayments.!error.amount.valid'] = 'Το ποσό πρέπει να είναι μεγαλύτερο του μηδενός.';
+$lang['GatewayPayments.!error.type.instance'] = 'Η πύλη δεν υποστηρίζει πληρωμές με αυτόν τον τρόπο.';
+$lang['GatewayPayments.!error.type.valid'] = 'Ο τύπος πληρωμής δεν υποστηρίζεται από αυτή την πύλη.';
+$lang['GatewayPayments.!error.contact_id.exists'] = 'Η επαφή δεν βρέθηκε για τον συγκεκριμένο πελάτη.';
+$lang['GatewayPayments.!error.account_id.exists'] = 'Ο λογαριασμός πληρωμής δεν βρέθηκε για τον συγκεκριμένο πελάτη.';
+$lang['GatewayPayments.!error.transaction_id.exists'] = 'Η συναλλαγή δεν βρέθηκε για τον συγκεκριμένο πελάτη.';
+$lang['GatewayPayments.!error.gateway.exists'] = 'Η πύλη δεν υπάρχει ή δεν είναι ενεργοποιημένη.';
+
