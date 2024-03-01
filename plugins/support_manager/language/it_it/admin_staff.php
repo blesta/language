867ee333fd2e504c814d1690cd60ae924fc88e7d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:44 -0800

    Updated it_it language files

---
+++ plugins/support_manager/language/it_it/admin_staff.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Admin Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminStaff.text.no_time'] = '--';
+$lang['AdminStaff.schedule_list.no_department_results'] = 'Questo membro del personale non è assegnato ad alcun reparto.';
+$lang['AdminStaff.schedule_list.heading_description'] = 'Descrizione';
+$lang['AdminStaff.schedule_list.heading_department'] = 'Dipartimento';
+$lang['AdminStaff.schedule_list.heading_departments'] = 'Dipartimenti';
+$lang['AdminStaff.schedule_list.no_results'] = 'Questo membro del personale non è previsto per nessun giorno.';
+$lang['AdminStaff.schedule_list.heading_end_time'] = 'Tempo della fine';
+$lang['AdminStaff.schedule_list.heading_start_time'] = 'Ora di inizio';
+$lang['AdminStaff.schedule_list.heading_day'] = 'Giorno';
+$lang['AdminStaff.schedule_list.heading_schedule'] = 'Programma';
+$lang['AdminStaff.staff.name'] = '%1$s %2$s';
+$lang['AdminStaff.edit.field_editsubmit'] = 'Modifica dello staff';
+$lang['AdminStaff.edit.text_availabledepartments'] = 'Dipartimenti disponibili';
+$lang['AdminStaff.edit.text_assigneddepartments'] = 'Dipartimenti assegnati';
+$lang['AdminStaff.edit.signature'] = 'Firma';
+$lang['AdminStaff.edit.title'] = 'Titolo';
+$lang['AdminStaff.edit.ticket_messenger_notifications'] = 'Ricevere le notifiche di Ticket Messenger';
+$lang['AdminStaff.edit.mobile_ticket_emails'] = 'Ricevere e-mail di biglietti mobili';
+$lang['AdminStaff.edit.ticket_emails'] = 'Ricevere le e-mail dei biglietti';
+$lang['AdminStaff.edit.heading_end_time'] = 'Tempo di fine';
+$lang['AdminStaff.edit.heading_start_time'] = 'Ora di inizio';
+$lang['AdminStaff.edit.heading_all_day'] = 'Tutto il giorno';
+$lang['AdminStaff.edit.heading_day'] = 'Giorno';
+$lang['AdminStaff.edit.heading_departments'] = 'Dipartimenti';
+$lang['AdminStaff.edit.heading_settings'] = 'Impostazioni';
+$lang['AdminStaff.edit.heading_schedule'] = 'Programma';
+$lang['AdminStaff.edit.boxtitle_editstaff'] = 'Modifica del personale %1$s %2$s';
+$lang['AdminStaff.add.field_addsubmit'] = 'Aggiungi personale';
+$lang['AdminStaff.add.text_availabledepartments'] = 'Dipartimenti disponibili';
+$lang['AdminStaff.add.text_assigneddepartments'] = 'Dipartimenti assegnati';
+$lang['AdminStaff.add.signature'] = 'Firma';
+$lang['AdminStaff.add.title'] = 'Titolo';
+$lang['AdminStaff.add.ticket_messenger_notifications'] = 'Ricevere le notifiche di Ticket Messenger';
+$lang['AdminStaff.add.mobile_ticket_emails'] = 'Ricevere e-mail di biglietti mobili';
+$lang['AdminStaff.add.ticket_emails'] = 'Ricevere le e-mail dei biglietti';
+$lang['AdminStaff.add.heading_end_time'] = 'Tempo della fine';
+$lang['AdminStaff.add.heading_start_time'] = 'Ora di inizio';
+$lang['AdminStaff.add.heading_all_day'] = 'Tutto il giorno';
+$lang['AdminStaff.add.heading_day'] = 'Giorno';
+$lang['AdminStaff.add.field_staff_id'] = 'Membro del personale';
+$lang['AdminStaff.add.field_staff_group_id'] = 'Gruppo di lavoro';
+$lang['AdminStaff.add.heading_departments'] = 'Dipartimenti';
+$lang['AdminStaff.add.heading_settings'] = 'Impostazioni';
+$lang['AdminStaff.add.heading_schedule'] = 'Programma';
+$lang['AdminStaff.add.heading_staff'] = 'Personale';
+$lang['AdminStaff.add.boxtitle_addstaff'] = 'Aggiungi personale';
+$lang['AdminStaff.index.no_results'] = 'Attualmente non c\'è personale assegnato al sistema di supporto.';
+$lang['AdminStaff.index.confirm_delete'] = 'L\'eliminazione di questo membro del personale comporta l\'annullamento di tutti i biglietti attualmente assegnati a questo membro del personale. Siete sicuri di voler eliminare questo membro del personale?';
+$lang['AdminStaff.index.option_delete'] = 'Cancellare';
+$lang['AdminStaff.index.option_edit'] = 'Modifica';
+$lang['AdminStaff.index.heading_options'] = 'Opzioni';
+$lang['AdminStaff.index.heading_ticket_messenger_notifications'] = 'Ricevere le notifiche di Ticket Messenger';
+$lang['AdminStaff.index.heading_mobile_ticket_emails'] = 'Ricevere e-mail di biglietti mobili';
+$lang['AdminStaff.index.heading_ticket_emails'] = 'Ricevere le e-mail dei biglietti';
+$lang['AdminStaff.index.heading_email'] = 'Email';
+$lang['AdminStaff.index.heading_first_name'] = 'Nome';
+$lang['AdminStaff.index.boxtitle_staff'] = 'Personale';
+$lang['AdminStaff.index.categorylink_addstaff'] = 'Aggiungi personale';
+$lang['AdminStaff.edit.page_title'] = 'Manager dell\'assistenza > Modifica del personale';
+$lang['AdminStaff.add.page_title'] = 'Manager dell\'assistenza > Aggiungi personale';
+$lang['AdminStaff.index.page_title'] = 'Manager di supporto > Personale';
+$lang['AdminStaff.!tooltip.signature'] = 'Inserire una firma per renderla automaticamente disponibile quando si risponde ai ticket.';
+$lang['AdminStaff.!tooltip.title'] = 'L\'inserimento di un titolo per questo membro del personale sovrascrive il titolo predefinito: %1$s';
+$lang['AdminStaff.!tooltip.ticket_messenger_notifications'] = 'Selezionare la casella di ciascuna priorità di ticket per la quale il personale deve ricevere le notifiche di ticket messenger.';
+$lang['AdminStaff.!tooltip.mobile_ticket_emails'] = 'Selezionare la casella di ciascuna priorità di ticket per la quale il personale deve ricevere e-mail di ticket mobile.';
+$lang['AdminStaff.!tooltip.ticket_emails'] = 'Selezionare la casella di ciascuna priorità di ticket per la quale il personale deve ricevere le e-mail di ticket.';
+$lang['AdminStaff.!tooltip.all_day'] = 'Selezionare questa casella per rendere il membro del personale disponibile tutto il giorno.';
+$lang['AdminStaff.!success.staff_deleted'] = 'Il membro del personale è stato cancellato con successo.';
+$lang['AdminStaff.!success.staff_updated'] = 'Il membro del personale è stato aggiornato con successo.';
+$lang['AdminStaff.!success.staff_added'] = 'Il membro del personale è stato aggiunto con successo.';
+
