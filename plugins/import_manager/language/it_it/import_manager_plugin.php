333fc3a8ba5a8d2dc590bae52de34b63dd4f4e6d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:55 -0800

    Updated it_it language files

---
+++ plugins/import_manager/language/it_it/import_manager_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Import Manager Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ImportManagerPlugin.description'] = 'Importa i dati da un altro sistema in Blesta.';
+$lang['ImportManagerPlugin.name'] = 'Gestione delle importazioni';
+
