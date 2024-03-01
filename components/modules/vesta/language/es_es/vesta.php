e2eda12e4102ddaa39aa62788277b36313bfa574
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:05 -0800

    Updated es_es language files

---
+++ components/modules/vesta/language/es_es/vesta.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Vesta
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Vesta.name'] = 'Vesta';
+$lang['Vesta.description'] = 'Vesta, o VestaCP, es un panel de control web de código abierto que se considera una gran alternativa a cPanel. No solo su seguridad y sus frecuentes actualizaciones igualan en calidad a las de cPanel, sino que además es completamente gratuito.';
+$lang['Vesta.module_row'] = 'Servidor';
+$lang['Vesta.module_row_plural'] = 'Servidores';
+$lang['Vesta.module_group'] = 'Grupo de servidores';
+$lang['Vesta.tab_stats'] = 'Estadísticas';
+$lang['Vesta.tab_client_actions'] = 'Acciones';
+$lang['Vesta.submit'] = 'Enviar';
+$lang['Vesta.tab_stats.info.WEB_DOMAINS'] = 'Dominios';
+$lang['Vesta.tab_stats.info.WEB_ALIASES'] = 'Alias';
+$lang['Vesta.tab_stats.info.DNS_DOMAINS'] = 'Dominios DNS';
+$lang['Vesta.tab_stats.info.DNS_RECORDS'] = 'Registros DNS';
+$lang['Vesta.tab_stats.info.MAIL_DOMAINS'] = 'Dominios de correo';
+$lang['Vesta.tab_stats.info.MAIL_ACCOUNTS'] = 'Cuentas de correo';
+$lang['Vesta.tab_stats.info.DATABASES'] = 'Bases de datos';
+$lang['Vesta.tab_stats.info.CRON_JOBS'] = 'Cron Jobs';
+$lang['Vesta.tab_stats.info.DISK_QUOTA'] = 'Uso del disco';
+$lang['Vesta.tab_stats.info.BANDWIDTH'] = 'Uso del ancho de banda';
+$lang['Vesta.tab_stats.info.BACKUPS'] = 'Copias de seguridad';
+$lang['Vesta.add_module_row'] = 'Añadir servidor';
+$lang['Vesta.add_module_group'] = 'Añadir grupo de servidores';
+$lang['Vesta.manage.module_rows_title'] = 'Servidores';
+$lang['Vesta.manage.module_groups_title'] = 'Grupos de servidores';
+$lang['Vesta.manage.module_rows_heading.name'] = 'Etiqueta del servidor';
+$lang['Vesta.manage.module_rows_heading.hostname'] = 'Nombre de host';
+$lang['Vesta.manage.module_rows_heading.port'] = 'Puerto';
+$lang['Vesta.manage.module_rows_heading.accounts'] = 'Cuentas';
+$lang['Vesta.manage.module_rows_heading.options'] = 'Opciones';
+$lang['Vesta.manage.module_groups_heading.name'] = 'Nombre del grupo';
+$lang['Vesta.manage.module_groups_heading.servers'] = 'Recuento de servidores';
+$lang['Vesta.manage.module_groups_heading.options'] = 'Opciones';
+$lang['Vesta.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Vesta.manage.module_rows.edit'] = 'Editar';
+$lang['Vesta.manage.module_groups.edit'] = 'Editar';
+$lang['Vesta.manage.module_rows.delete'] = 'Borrar';
+$lang['Vesta.manage.module_groups.delete'] = 'Borrar';
+$lang['Vesta.manage.module_rows.confirm_delete'] = '¿Está seguro de que desea eliminar este servidor?';
+$lang['Vesta.manage.module_groups.confirm_delete'] = '¿Está seguro de que desea eliminar este grupo de servidores?';
+$lang['Vesta.manage.module_rows_no_results'] = 'No hay servidores.';
+$lang['Vesta.manage.module_groups_no_results'] = 'No hay grupos de servidores.';
+$lang['Vesta.order_options.first'] = 'Primer servidor no completo';
+$lang['Vesta.add_row.box_title'] = 'Añadir servidor Vesta';
+$lang['Vesta.add_row.basic_title'] = 'Configuración básica';
+$lang['Vesta.add_row.remove_name_server'] = 'Eliminar';
+$lang['Vesta.add_row.add_btn'] = 'Añadir servidor';
+$lang['Vesta.edit_row.box_title'] = 'Editar el servidor Vesta';
+$lang['Vesta.edit_row.basic_title'] = 'Configuración básica';
+$lang['Vesta.edit_row.remove_name_server'] = 'Eliminar';
+$lang['Vesta.edit_row.add_btn'] = 'Editar servidor';
+$lang['Vesta.row_meta.server_name'] = 'Etiqueta del servidor';
+$lang['Vesta.row_meta.host_name'] = 'Nombre de host';
+$lang['Vesta.row_meta.user_name'] = 'Nombre de usuario';
+$lang['Vesta.row_meta.password'] = 'Contraseña';
+$lang['Vesta.row_meta.port'] = 'Puerto';
+$lang['Vesta.row_meta.use_ssl'] = 'Utilice SSL al conectarse a la API (recomendado)';
+$lang['Vesta.package_fields.package_name'] = 'Nombre del paquete Vesta';
+$lang['Vesta.package_fields.package_name_how_to'] = 'Puede obtener el nombre del paquete accediendo a su Panel de Control de Vesta > Paquetes';
+$lang['Vesta.service_field.domain'] = 'Nombre de dominio';
+$lang['Vesta.service_field.username'] = 'Nombre de usuario';
+$lang['Vesta.service_field.password'] = 'Contraseña';
+$lang['Vesta.stored_locally_only'] = 'Este campo sólo se actualizará localmente';
+$lang['Vesta.service_info.hostname'] = 'Nombre de host';
+$lang['Vesta.service_info.username'] = 'Nombre de usuario';
+$lang['Vesta.service_info.password'] = 'Contraseña';
+$lang['Vesta.service_info.server'] = 'Servidor';
+$lang['Vesta.service_info.options'] = 'Opciones';
+$lang['Vesta.service_info.option_login'] = 'Conectarse';
+$lang['Vesta.tab_client_actions.change_password'] = 'Cambiar contraseña';
+$lang['Vesta.tab_client_actions.password'] = 'Contraseña';
+$lang['Vesta.tab_client_actions.confirm_password'] = 'Confirmar contraseña';
+$lang['Vesta.tab_stats.info_heading.field'] = 'Campo';
+$lang['Vesta.tab_stats.info_heading.value'] = 'Valor';
+$lang['Vesta.!error.server_name_valid'] = 'Debe introducir una Etiqueta de servidor.';
+$lang['Vesta.!error.host_name_valid'] = 'El nombre de host no es válido.';
+$lang['Vesta.!error.user_name_valid'] = 'El nombre de usuario no es válido.';
+$lang['Vesta.!error.port_valid'] = 'El puerto parece no ser válido.';
+$lang['Vesta.!error.password_valid'] = 'La contraseña no es válida.';
+$lang['Vesta.!error.meta[package_name].empty'] = 'Introduzca el nombre del paquete Vesta.';
+$lang['Vesta.!error.api.internal'] = 'Se ha producido un error interno o el servidor no ha respondido a la solicitud.';
+$lang['Vesta.!error.module_row.missing'] = 'Se ha producido un error interno. La fila del módulo no está disponible.';
+$lang['Vesta.!error.domain.format'] = 'Introduzca un nombre de dominio válido, por ejemplo, dominio.com.';
+$lang['Vesta.!error.domain.test'] = 'El nombre de dominio no puede empezar por \'test\'.';
+$lang['Vesta.!error.user_name.empty'] = 'El nombre de usuario no puede estar vacío.';
+$lang['Vesta.!error.password.valid'] = 'La contraseña debe tener al menos 8 caracteres.';
+$lang['Vesta.!error.actions_password.valid'] = 'Los campos Contraseña y Confirmar contraseña no pueden estar vacíos.';
+
