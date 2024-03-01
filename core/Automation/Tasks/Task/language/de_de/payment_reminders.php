326b2968da5f22a061adda3e5ae9dbd2602ab169
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:59 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/payment_reminders.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Payment Reminders
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.payment_reminders.autodebit_success'] = 'Die Lastschriftmahnung für die Rechnung #%4$s wurde erfolgreich an den Kontakt %1$s %2$s vom Kunden #%3$s übermittelt.';
+$lang['Automation.task.payment_reminders.autodebit_failed'] = 'Die Lastschrifterinnerung für die Rechnung #%4$s konnte nicht an den Kontakt %1$s %2$s vom Kunden #%3$s gesendet werden.';
+$lang['Automation.task.payment_reminders.success'] = 'Erfolgreich die Mahnung für Rechnung #%4$s an Kontakt %1$s %2$s von Kunde #%3$s zugestellt.';
+$lang['Automation.task.payment_reminders.failed'] = 'Die Mahnung für die Rechnung #%4$s kann nicht an den Kontakt %1$s %2$s vom Kunden #%3$s gesendet werden.';
+$lang['Automation.task.payment_reminders.completed'] = 'Die Aufgabe der Zahlungserinnerungen ist abgeschlossen.';
+$lang['Automation.task.payment_reminders.attempt'] = 'Versucht, Zahlungserinnerungen zu versenden.';
+
