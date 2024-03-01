6c8ef74914558cd3cbc366708dfba5b969cfe29c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:25 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/order_affiliate_payment_methods.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Order Affiliate Payment Methods
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliatePaymentMethods.!error.company_id.exists'] = 'Μη έγκυρο αναγνωριστικό εταιρείας.';
+$lang['OrderAffiliatePaymentMethods.!error.names[][lang].format'] = 'Ο κωδικός γλώσσας δεν είναι σωστά διαμορφωμένος.';
+$lang['OrderAffiliatePaymentMethods.!error.names[][name].empty'] = 'Παρακαλώ εισάγετε ένα όνομα μεθόδου πληρωμής.';
+$lang['OrderAffiliatePaymentMethods.!error.payment_method_id.exists'] = 'Μη έγκυρο αναγνωριστικό μεθόδου πληρωμής.';
+
