7b1d1c0e66005ed6af139759ef198a2bda2fe03e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:15 -0800

    Updated it_it language files

---
+++ core/Util/Filters/language/it_it/client_filters.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Client Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.client_filters.field_last_seen_end_date'] = 'Data di fine';
+$lang['Util.filters.client_filters.field_last_seen_start_date'] = 'Data di inizio';
+$lang['Util.filters.client_filters.field_last_seen'] = 'Ultimo visto';
+$lang['Util.filters.client_filters.field_invoice_method'] = 'Metodo di fatturazione';
+$lang['Util.filters.client_filters.field_client_group_id'] = 'Gruppo di clienti';
+$lang['Util.filters.client_filters.field_contact_country'] = 'Paese';
+$lang['Util.filters.client_filters.field_contact_company'] = 'Azienda';
+$lang['Util.filters.client_filters.field_contact_email'] = 'Email';
+$lang['Util.filters.client_filters.field_contact_name'] = 'Nome';
+$lang['Util.filters.client_filters.any'] = 'Qualsiasi';
+
