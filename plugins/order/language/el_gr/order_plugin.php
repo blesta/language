72bcb2e73bd6fc4de9cc29c652f5b755e1365386
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:29 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/order_plugin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Order Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderPlugin.name'] = 'Σύστημα παραγγελιών';
+$lang['OrderPlugin.admin_main.name'] = 'Παραγγελίες';
+$lang['OrderPlugin.admin_forms.name'] = 'Έντυπα παραγγελιών';
+$lang['OrderPlugin.admin_affiliates.name'] = 'Συνεργάτες';
+$lang['OrderPlugin.admin_affiliates.payouts'] = 'Πληρωμές θυγατρικών';
+$lang['OrderPlugin.admin_affiliates.payment_methods'] = 'Μέθοδοι πληρωμής θυγατρικών';
+$lang['OrderPlugin.client.name'] = 'Παραγγελία';
+$lang['OrderPlugin.client_affiliates.name'] = 'Συνεργάτες';
+$lang['OrderPlugin.action_staff_client.affiliates'] = 'Διαχείριση θυγατρικών';
+$lang['OrderPlugin.client_orders.name'] = 'Ιστορικό παραγγελιών';
+$lang['OrderPlugin.cron.accept_paid_orders_name'] = 'Αποδοχή πληρωμένων εκκρεμών παραγγελιών';
+$lang['OrderPlugin.cron.accept_paid_orders_desc'] = 'Αποδέχεται αυτόματα πληρωμένες εκκρεμείς παραγγελίες, εάν το επιτρέπει η φόρμα παραγγελίας.';
+$lang['OrderPlugin.cron.affiliate_monthly_report_name'] = 'Μηνιαία έκθεση θυγατρικών';
+$lang['OrderPlugin.cron.affiliate_monthly_report_desc'] = 'Μια μηνιαία αναφορά ηλεκτρονικού ταχυδρομείου για τους συνεργάτες θα αποστέλλεται την 1η του μήνα για τον προηγούμενο μήνα.';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_name'] = 'Παραγγελίες θυγατρικών';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_desc'] = 'Αξιολογεί εάν οι παραπομπές θυγατρικών που εκκρεμούν έχουν φτάσει στην ωριμότητα και, εάν ναι, τις ενημερώνει και τα ποσά πληρωμής.';
+$lang['OrderPlugin.cron.process_abandoned_orders_name'] = 'Διαδικασία Εγκαταλελειμμένων Παραγγελιών';
+$lang['OrderPlugin.cron.process_abandoned_orders_desc'] = 'Αποστολή μηνυμάτων υπενθύμισης, εκτέλεση ακυρώσεων και απενεργοποίηση πελατών για εγκαταλελειμμένες παραγγελίες';
+$lang['OrderPlugin.event_getsearchoptions.orders'] = 'Αναζήτηση παραγγελιών';
+$lang['OrderPlugin.card_client.orders'] = 'Παραγγελίες';
+$lang['AdminCompanyPlugins.settings.event.domains.delete'] = 'Αυτό το συμβάν ενεργοποιείται όταν ένα TLD διαγράφεται από τη Διαχείριση τομέων.';
+$lang['AdminCompanyPlugins.settings.event.domains.enable'] = 'Αυτό το συμβάν ενεργοποιείται όταν ένας TLD ενεργοποιείται από τη Διαχείριση τομέων.';
+$lang['AdminCompanyPlugins.settings.event.domains.disable'] = 'Αυτό το συμβάν ενεργοποιείται όταν ένας TLD απενεργοποιείται από τη Διαχείριση τομέων.';
+$lang['AdminCompanyPlugins.settings.event.domains.updatedomainscompanysettingsafter'] = 'Αυτό το συμβάν ενεργοποιείται μετά την ενημέρωση των ρυθμίσεων της εταιρείας Domain Manager.';
+$lang['AdminCompanyPlugins.settings.event.domains.updatepricingafter'] = 'Αυτό το συμβάν ενεργοποιείται μετά την ενημέρωση της τιμολόγησης ενός TLD.';
+$lang['OrderPlugin.permission.admin_affiliates'] = 'Συνεργάτες';
+$lang['OrderPlugin.permission.admin_affiliates_add'] = 'Προσθήκη θυγατρικών';
+$lang['OrderPlugin.permission.admin_affiliates_update'] = 'Ενημέρωση θυγατρικών';
+$lang['OrderPlugin.permission.admin_affiliates_activate'] = 'Ενεργοποίηση θυγατρικών';
+$lang['OrderPlugin.permission.admin_affiliates_deactivate'] = 'Απενεργοποίηση θυγατρικών';
+$lang['OrderPlugin.permission.admin_affiliates_settings'] = 'Ρυθμίσεις θυγατρικών';
+$lang['OrderPlugin.permission.admin_payouts'] = 'Πληρωμές';
+$lang['OrderPlugin.permission.admin_payouts_edit'] = 'Επεξεργασία πληρωμής';
+$lang['OrderPlugin.permission.admin_payouts_approve'] = 'Έγκριση πληρωμής';
+$lang['OrderPlugin.permission.admin_payouts_decline'] = 'Μείωση Πληρωμή';
+$lang['OrderPlugin.permission.admin_payment_methods'] = 'Μέθοδοι πληρωμής';
+$lang['OrderPlugin.permission.admin_payment_methods_add'] = 'Προσθήκη μεθόδου πληρωμής';
+$lang['OrderPlugin.permission.admin_payment_methods_edit'] = 'Επεξεργασία μεθόδου πληρωμής';
+$lang['OrderPlugin.permission.admin_payment_methods_delete'] = 'Διαγραφή μεθόδου πληρωμής';
+$lang['OrderPlugin.permission.admin_main'] = 'Παραγγελίες';
+$lang['OrderPlugin.permission.admin_main_widget'] = 'Προφίλ πελάτη Widget';
+
