eb36e91f6d6d17a7305fa2e12dd83c844fb381d9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:54 -0800

    Updated it_it language files

---
+++ language/it_it/client_transactions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Client Transactions
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientTransactions.applied.no_results'] = 'Questa transazione non è stata applicata a nessuna fattura.';
+$lang['ClientTransactions.applied.heading_appliedon'] = 'Applicato su';
+$lang['ClientTransactions.applied.heading_amount'] = 'Importo';
+$lang['ClientTransactions.applied.heading_invoice'] = 'Fattura';
+$lang['ClientTransactions.index.no_results'] = 'Non avete %1$s Transazioni.';
+$lang['ClientTransactions.index.heading_date'] = 'Data';
+$lang['ClientTransactions.index.heading_number'] = 'Numero';
+$lang['ClientTransactions.index.heading_applied'] = 'Applicato';
+$lang['ClientTransactions.index.heading_credited'] = 'Accreditato';
+$lang['ClientTransactions.index.heading_amount'] = 'Importo';
+$lang['ClientTransactions.index.heading_type'] = 'Tipo';
+$lang['ClientTransactions.index.category_declined'] = 'Declinato';
+$lang['ClientTransactions.index.category_pending'] = 'In attesa';
+$lang['ClientTransactions.index.category_approved'] = 'Approvato';
+$lang['ClientTransactions.index.boxtitle_transactions'] = 'Transazioni';
+$lang['ClientTransactions.index.page_title'] = 'Cliente #%1$s Transazioni';
+
