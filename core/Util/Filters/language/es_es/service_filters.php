bbc2d2f8d448a648ac8462e89506c86a0ae805e6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:39 -0800

    Updated es_es language files

---
+++ core/Util/Filters/language/es_es/service_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Service Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.service_filters.any'] = 'Cualquier';
+$lang['Util.filters.service_filters.field_module_id'] = 'Módulo';
+$lang['Util.filters.service_filters.field_package_name'] = 'Nombre del paquete';
+$lang['Util.filters.service_filters.field_service_meta'] = 'Servicio Meta';
+
