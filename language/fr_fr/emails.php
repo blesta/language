43f6da377ed73c81127f5abb3a42e669df1e09ac
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:21 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/emails.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Emails.!error.company_id.exists'] = 'ID de l\'entreprise non valide.';
+$lang['Emails.!error.html.parse'] = 'Erreur d\'analyse du modèle : %1$s';
+$lang['Emails.!error.text.parse'] = 'Erreur d\'analyse du modèle : %1$s';
+$lang['Emails.!error.email_signature_id.in_use'] = 'Cette signature est actuellement utilisée et ne peut pas être supprimée.';
+$lang['Emails.!error.name.empty'] = 'Veuillez saisir un nom de signature.';
+$lang['Emails.!error.text.empty'] = 'Veuillez saisir une signature en texte clair.';
+$lang['Emails.!error.email_signature_id.exists'] = 'L\'identifiant de la signature du courriel n\'est pas valide.';
+$lang['Emails.!error.email_group_id.exists'] = 'ID du groupe de messagerie invalide.';
+$lang['Emails.!error.lang.empty'] = 'Veuillez saisir une langue.';
+$lang['Emails.!error.lang.length'] = 'La longueur de la langue ne doit pas dépasser 5 caractères.';
+$lang['Emails.!error.from.format'] = 'Veuillez saisir une adresse de courrier électronique valide.';
+$lang['Emails.!error.from_name.empty'] = 'Veuillez saisir un nom de domaine.';
+$lang['Emails.!error.reply_to.format'] = 'Veuillez saisir une adresse de réponse valide.';
+$lang['Emails.!error.subject.empty'] = 'Veuillez saisir un sujet.';
+$lang['Emails.!error.include_attachments'] = 'Le choix d\'inclure ou non les pièces jointes doit être fixé à 0 ou 1.';
+$lang['Emails.!error.status.format'] = 'Statut non valide.';
+$lang['Emails.!error.email_id.exists'] = 'ID email invalide.';
+$lang['Emails.!error.company_id.unique'] = 'L\'ID du groupe de messagerie et l\'ID de l\'entreprise pour la langue donnée sont déjà pris.';
+$lang['Emails.!error.action.exists'] = 'L\'action donnée est une action de groupe de messagerie non valide.';
+$lang['Emails.!error.to_addresses.empty'] = 'Au moins une adresse To doit être fournie.';
+$lang['Emails.!error.to_addresses.format'] = 'Au moins une des adresses électroniques fournies n\'est pas valide.';
+$lang['Emails.!error.cc_addresses.format'] = 'Au moins une des adresses électroniques CC fournies n\'est pas une adresse électronique valide.';
+$lang['Emails.!error.bcc_addresses.format'] = 'Au moins une des adresses électroniques BCC fournies n\'est pas une adresse électronique valide.';
+$lang['Emails.!error.attachments.exist'] = 'Au moins une des pièces jointes fournies n\'existe pas sur le système de fichiers.';
+$lang['Emails.!error.email.failed_to_send'] = 'L\'envoi du courriel a échoué en raison d\'un problème de configuration.';
+$lang['Emails.getStatusTypes.active'] = 'Non';
+$lang['Emails.getStatusTypes.inactive'] = 'Oui';
+
