46994e4b75b4c1cad4d57ac1cd350b013abdb278
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:14 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/report_manager.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Report Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReportManager.reportrequiredtypes.custom'] = 'Vlastní regex';
+$lang['ReportManager.reportrequiredtypes.yes'] = 'Ano';
+$lang['ReportManager.reportrequiredtypes.no'] = 'Ne';
+$lang['ReportManager.reportfieldtypes.date'] = 'Datum';
+$lang['ReportManager.reportfieldtypes.select'] = 'Vyberte';
+$lang['ReportManager.reportfieldtypes.text'] = 'Text';
+$lang['ReportManager.getformats.json'] = 'JSON';
+$lang['ReportManager.getformats.csv'] = 'CSV';
+$lang['ReportManager.!error.date_created.format'] = 'Datum vytvoření je neplatný formát.';
+$lang['ReportManager.!error.query.valid'] = 'Je povolen pouze jeden dotaz a musí to být dotaz SELECT.';
+$lang['ReportManager.!error.query.empty'] = 'Zpráva musí obsahovat dotaz.';
+$lang['ReportManager.!error.name.valid'] = 'Zpráva musí mít název.';
+$lang['ReportManager.!error.temp_dir.writable'] = 'Do dočasného adresáře nelze zapisovat nebo do něj nelze zapsat zprávu.';
+$lang['ReportManager.!error.format.valid'] = 'Vyberte platný formát zprávy.';
+$lang['ReportManager.!error.type.valid'] = 'Vyberte platný typ zprávy.';
+
