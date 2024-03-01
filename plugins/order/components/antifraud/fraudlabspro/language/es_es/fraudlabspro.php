4c7d5baf591b39d519dfa297a58fe2d3622abd16
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:25 -0800

    Updated es_es language files

---
+++ plugins/order/components/antifraud/fraudlabspro/language/es_es/fraudlabspro.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fraudlabspro
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FraudLabsPro.!error.reject.reject_score'] = 'Su pedido no puede ser procesado en este momento.';
+$lang['FraudLabsPro.!error.review.review_score'] = 'Su pedido debe ser revisado manualmente.';
+$lang['FraudLabsPro.!error.reject.fraudlabspro'] = 'Su pedido ha sido marcado como fraude por FraudLabs Pro.';
+$lang['FraudLabsPro.!error.review.fraudlabspro'] = 'Su pedido ha sido marcado como potencialmente fraudulento por FraudLabs Pro. Será revisado manualmente.';
+$lang['FraudLabsPro.settings.field_api_key'] = 'Clave API';
+$lang['FraudLabsPro.settings.label_description'] = 'Consiga gratis FraudLabs Pro Micro Plan (500 consultas/mes) en http://www.fraudlabspro.com/plan';
+$lang['FraudLabsPro.settings.field_reject_score'] = 'Puntuación mínima para rechazar';
+$lang['FraudLabsPro.settings.field_review_score'] = 'Puntuación mínima para revisar';
+$lang['FraudLabsPro.settings.field_follow_flp_result'] = 'Rechazo/Revisión automática basada en la recomendación de FraudLabs Pro';
+$lang['FraudLabsPro.settings.option_yes'] = 'Sí';
+$lang['FraudLabsPro.settings.option_no'] = 'No';
+
