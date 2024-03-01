e96cb142d3cce01e1f3e52c46cf750c9bdb87ccc
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:57 -0800

    Updated it_it language files

---
+++ language/it_it/countries.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Countries
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Countries.!error.name.format'] = 'Inserire un nome.';
+$lang['Countries.!error.alpha3.in_use'] = 'Il codice paese Alpha3, %1$s, è già occupato.';
+$lang['Countries.!error.alpha3.format'] = 'Il codice paese Alpha3 deve essere composto da 3 caratteri alfa.';
+$lang['Countries.!error.alpha2.exists'] = 'Codice alpha2 non valido.';
+$lang['Countries.!error.alpha2.in_use'] = 'Il codice paese Alpha2, %1$s, è già occupato.';
+$lang['Countries.!error.alpha2.format'] = 'Il codice paese Alpha2 deve essere composto da 2 caratteri alfa.';
+
