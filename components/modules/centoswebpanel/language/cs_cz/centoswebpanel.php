d949c59280ec7ce475d0610281ba7be05cc3bc72
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:17 -0800

    Updated cs_cz language files

---
+++ components/modules/centoswebpanel/language/cs_cz/centoswebpanel.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Centoswebpanel
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Centoswebpanel.!error.api'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+$lang['Centoswebpanel.!error.centoswebpanel_password.matches'] = 'Heslo a potvrzovací heslo se neshodují.';
+$lang['Centoswebpanel.!error.centoswebpanel_password.valid'] = 'Heslo musí mít alespoň 8 znaků.';
+$lang['Centoswebpanel.!error.centoswebpanel_username.length'] = 'Uživatelské jméno musí mít délku 1 až 16 znaků.';
+$lang['Centoswebpanel.!error.centoswebpanel_username.test'] = 'Uživatelské jméno nesmí začínat slovem "test".';
+$lang['Centoswebpanel.!error.centoswebpanel_username.format'] = 'Uživatelské jméno může obsahovat pouze písmena a číslice a nesmí začínat číslicí.';
+$lang['Centoswebpanel.!error.centoswebpanel_domain.test'] = 'Název domény nemůže začínat na \'test\'.';
+$lang['Centoswebpanel.!error.centoswebpanel_domain.format'] = 'Zadejte platný název domény, např. domain.com.';
+$lang['Centoswebpanel.!error.module_row.missing'] = 'Došlo k interní chybě. Řádek modulu není k dispozici.';
+$lang['Centoswebpanel.!error.api.internal'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+$lang['Centoswebpanel.!error.meta[nproc].valid'] = 'Maximum Process musí být číselná hodnota.';
+$lang['Centoswebpanel.!error.meta[nofile].valid'] = 'Maximální soubory musí být číselná hodnota.';
+$lang['Centoswebpanel.!error.meta[inode].valid'] = 'Maximální počet inodů musí být číselná hodnota.';
+$lang['Centoswebpanel.!error.meta[package].empty'] = 'Je vyžadován balíček CentOS WebPanel.';
+$lang['Centoswebpanel.!error.name_servers_count'] = 'Je třeba definovat alespoň 2 jmenné servery.';
+$lang['Centoswebpanel.!error.name_servers_valid'] = 'Jeden nebo více zadaných názvových serverů jsou neplatné.';
+$lang['Centoswebpanel.!error.account_limit_valid'] = 'Limit účtu musí zůstat prázdný (pro neomezený počet účtů) nebo musí být nastaven na nějakou celočíselnou hodnotu.';
+$lang['Centoswebpanel.!error.remote_api_key_valid_connection'] = 'Připojení k serveru se nepodařilo navázat. Zkontrolujte, zda jsou název hostitele a klíč API správné.';
+$lang['Centoswebpanel.!error.remote_api_key_valid'] = 'Klíč API se zdá být neplatný.';
+$lang['Centoswebpanel.!error.port_valid'] = 'Musíte zadat port API.';
+$lang['Centoswebpanel.!error.login_port_valid'] = 'Musíte zadat přihlašovací port.';
+$lang['Centoswebpanel.!error.user_name_valid'] = 'Uživatelské jméno se zdá být neplatné.';
+$lang['Centoswebpanel.!error.host_name_valid'] = 'Název hostitele se zdá být neplatný.';
+$lang['Centoswebpanel.!error.server_name_valid'] = 'Je třeba zadat označení serveru.';
+$lang['Centoswebpanel.service_field.tooltip.password'] = 'Heslo můžete nechat prázdné, aby se vygenerovalo automaticky.';
+$lang['Centoswebpanel.service_field.tooltip.username'] = 'Uživatelské jméno můžete nechat prázdné, aby se vygenerovalo automaticky.';
+$lang['Centoswebpanel.service_field.tooltip.username_edit'] = 'Tato změna neovlivní webový panel CentOS, ale pouze změní místní záznam v systému Blesta.';
+$lang['Centoswebpanel.service_field.tooltip.domain_edit'] = 'Tato změna neovlivní webový panel CentOS, ale pouze změní místní záznam v systému Blesta.';
+$lang['Centoswebpanel.service_info.option_login'] = 'Přihlášení';
+$lang['Centoswebpanel.service_info.options'] = 'Možnosti';
+$lang['Centoswebpanel.service_info.server'] = 'Server';
+$lang['Centoswebpanel.service_info.password'] = 'Heslo';
+$lang['Centoswebpanel.service_info.username'] = 'Uživatelské jméno';
+$lang['Centoswebpanel.service_field.password'] = 'Heslo';
+$lang['Centoswebpanel.service_field.username'] = 'Uživatelské jméno';
+$lang['Centoswebpanel.service_field.domain'] = 'Doména';
+$lang['Centoswebpanel.package_fields.nproc'] = 'Maximální proces';
+$lang['Centoswebpanel.package_fields.nofile'] = 'Maximální počet souborů';
+$lang['Centoswebpanel.package_fields.inode'] = 'Maximální počet inodů';
+$lang['Centoswebpanel.package_fields.package'] = 'ID balíčku';
+$lang['Centoswebpanel.row_meta.account_limit'] = 'Limit účtu';
+$lang['Centoswebpanel.row_meta.use_ssl'] = 'Při připojování k rozhraní API používejte protokol SSL (doporučeno).';
+$lang['Centoswebpanel.row_meta.api_key'] = 'Klíč API';
+$lang['Centoswebpanel.row_meta.default_port'] = '2304';
+$lang['Centoswebpanel.row_meta.port'] = 'Port API';
+$lang['Centoswebpanel.row_meta.default_login_port'] = '2031';
+$lang['Centoswebpanel.row_meta.login_port'] = 'Přihlašovací port';
+$lang['Centoswebpanel.row_meta.host_name'] = 'Hostitelské jméno';
+$lang['Centoswebpanel.row_meta.server_name'] = 'Označení serveru';
+$lang['Centoswebpanel.edit_row.add_btn'] = 'Upravit server';
+$lang['Centoswebpanel.edit_row.remove_name_server'] = 'Odstranění adresy';
+$lang['Centoswebpanel.edit_row.name_server'] = 'Name server %1$s';
+$lang['Centoswebpanel.edit_row.name_server_host_col'] = 'Hostitelské jméno';
+$lang['Centoswebpanel.edit_row.name_server_col'] = 'Jmenný server';
+$lang['Centoswebpanel.edit_row.name_server_btn'] = 'Přidání dalšího jmenného serveru';
+$lang['Centoswebpanel.edit_row.notes_title'] = 'Poznámky';
+$lang['Centoswebpanel.edit_row.name_servers_title'] = 'Jmenné servery';
+$lang['Centoswebpanel.edit_row.basic_title'] = 'Základní nastavení';
+$lang['Centoswebpanel.edit_row.box_title'] = 'Úprava serveru CentOS WebPanel';
+$lang['Centoswebpanel.add_row.add_btn'] = 'Přidat server';
+$lang['Centoswebpanel.add_row.remove_name_server'] = 'Odstranění adresy';
+$lang['Centoswebpanel.add_row.name_server'] = 'Name server %1$s';
+$lang['Centoswebpanel.add_row.name_server_host_col'] = 'Hostitelské jméno';
+$lang['Centoswebpanel.add_row.name_server_col'] = 'Jmenný server';
+$lang['Centoswebpanel.add_row.name_server_btn'] = 'Přidání dalšího jmenného serveru';
+$lang['Centoswebpanel.add_row.notes_title'] = 'Poznámky';
+$lang['Centoswebpanel.add_row.name_servers_title'] = 'Jmenné servery';
+$lang['Centoswebpanel.add_row.basic_title'] = 'Základní nastavení';
+$lang['Centoswebpanel.add_row.box_title'] = 'Přidání serveru CentOS WebPanel';
+$lang['Centoswebpanel.order_options.roundrobin'] = 'Rovnoměrné rozdělení mezi servery';
+$lang['Centoswebpanel.order_options.first'] = 'První nenaplněný server';
+$lang['Centoswebpanel.manage.module_groups_no_results'] = 'Neexistují žádné skupiny serverů.';
+$lang['Centoswebpanel.manage.module_rows_no_results'] = 'Neexistují žádné servery.';
+$lang['Centoswebpanel.manage.module_groups.confirm_delete'] = 'Opravdu chcete tuto skupinu serverů odstranit?';
+$lang['Centoswebpanel.manage.module_rows.confirm_delete'] = 'Opravdu chcete tento server odstranit?';
+$lang['Centoswebpanel.manage.module_groups.delete'] = 'Odstranit';
+$lang['Centoswebpanel.manage.module_rows.delete'] = 'Odstranit';
+$lang['Centoswebpanel.manage.module_groups.edit'] = 'Upravit';
+$lang['Centoswebpanel.manage.module_rows.edit'] = 'Upravit';
+$lang['Centoswebpanel.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Centoswebpanel.manage.module_groups_heading.options'] = 'Možnosti';
+$lang['Centoswebpanel.manage.module_groups_heading.servers'] = 'Počet serverů';
+$lang['Centoswebpanel.manage.module_groups_heading.name'] = 'Název skupiny';
+$lang['Centoswebpanel.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['Centoswebpanel.manage.module_rows_heading.accounts'] = 'Účty';
+$lang['Centoswebpanel.manage.module_rows_heading.hostname'] = 'Hostitelské jméno';
+$lang['Centoswebpanel.manage.module_rows_heading.name'] = 'Označení serveru';
+$lang['Centoswebpanel.manage.module_groups_title'] = 'Skupiny serverů';
+$lang['Centoswebpanel.manage.module_rows_title'] = 'Servery';
+$lang['Centoswebpanel.add_module_group'] = 'Přidání skupiny serverů';
+$lang['Centoswebpanel.add_module_row'] = 'Přidat server';
+$lang['Centoswebpanel.module_group'] = 'Skupina serverů';
+$lang['Centoswebpanel.module_row_plural'] = 'Servery';
+$lang['Centoswebpanel.module_row'] = 'Server';
+$lang['Centoswebpanel.description'] = 'Bezplatný ovládací panel webhostingu určený pro rychlou a snadnou správu (dedikovaných a VPS) serverů';
+$lang['Centoswebpanel.name'] = 'Webový panel CentOS';
+
