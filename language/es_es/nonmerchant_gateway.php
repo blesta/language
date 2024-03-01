0b342ee9f4a9524c6a2c2a6ee5e06a68835cb2d9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:34 -0800

    Updated es_es language files

---
+++ language/es_es/nonmerchant_gateway.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Nonmerchant Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['NonmerchantGateway.!error.invalid'] = 'Se ha recibido una respuesta de pago no válida o no reconocida.';
+$lang['NonmerchantGateway.!error.transaction_not_found'] = 'La transacción no es válida o ya no está disponible para procesar esa acción.';
+$lang['NonmerchantGateway.!error.unsupported'] = 'La pasarela no admite esa acción.';
+$lang['NonmerchantGateway.!error.general'] = 'Se ha producido un error al procesar la solicitud con la pasarela.';
+
