84ae4f9903ca3c57e7b1d0e98398f9766ea2ff31
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:54 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/deliver_reports.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Deliver Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.deliver_reports.tax_liability.email_success'] = 'E-mail s hlášením o daňové povinnosti byl odeslán.';
+$lang['Automation.task.deliver_reports.tax_liability.email_error'] = 'E-mail o daňové povinnosti se nepodařilo odeslat.';
+$lang['Automation.task.deliver_reports.tax_liability.attachment_fail'] = 'Nelze vygenerovat přílohu zprávy o daňové povinnosti.';
+$lang['Automation.task.deliver_reports.tax_liability.attempt'] = 'Pokus o odeslání zprávy o daňové povinnosti.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_success'] = 'E-mail s hlášením o vytvoření faktury byl odeslán.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_error'] = 'E-mail se zprávou o vytvoření faktury se nepodařilo odeslat.';
+$lang['Automation.task.deliver_reports.invoice_creation.attachment_fail'] = 'Nelze vygenerovat přílohu sestavy Vytvoření faktury.';
+$lang['Automation.task.deliver_reports.invoice_creation.attempt'] = 'Pokus o odeslání sestavy Vytvoření faktury.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_success'] = 'Byl odeslán e-mail s hlášením o stárnoucích fakturách.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_error'] = 'E-mail o stárnoucích fakturách se nepodařilo odeslat.';
+$lang['Automation.task.deliver_reports.aging_invoices.attachment_fail'] = 'Nelze vygenerovat přílohu sestavy Stárnoucí faktury.';
+$lang['Automation.task.deliver_reports.aging_invoices.attempt'] = 'Pokus o odeslání sestavy Stárnoucí faktury.';
+$lang['Automation.task.deliver_reports.completed'] = 'Úloha doručování zpráv byla dokončena.';
+$lang['Automation.task.deliver_reports.attempt'] = 'Pokus o doručení zpráv.';
+
