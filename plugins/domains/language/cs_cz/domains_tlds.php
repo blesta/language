7f5124b76e428a76fc6502614599c527cf40d4fe
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:35 -0800

    Updated cs_cz language files

---
+++ plugins/domains/language/cs_cz/domains_tlds.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Domains Tlds
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsTlds.getTldFields.group'] = 'Skupina';
+$lang['DomainsTlds.!error.feature.unsupported'] = 'Funkce %1$s není vybraným modulem registrátora podporována.';
+$lang['DomainsTlds.!error.tld.supported'] = 'Zadaný TLD není podporován vybraným modulem registrátora.';
+$lang['DomainsTlds.!error.package_pricing.service'] = 'Cenu nebylo možné zakázat, protože ji právě používá nějaká služba.';
+$lang['DomainsTlds.!error.package_pricing.count'] = 'Ceny nelze vypnout, TLD musí mít alespoň jednu cenu.';
+$lang['DomainsTlds.!error.epp_code.valid'] = 'Neplatná hodnota pro kód EPP, musí být 0 nebo 1.';
+$lang['DomainsTlds.!error.id_protection.valid'] = 'Neplatná hodnota pro ID Protection, musí být 0 nebo 1.';
+$lang['DomainsTlds.!error.email_forwarding.valid'] = 'Neplatná hodnota pro Email Forwarding, musí být 0 nebo 1.';
+$lang['DomainsTlds.!error.dns_management.valid'] = 'Neplatná hodnota pro správu DNS, musí být 0 nebo 1.';
+$lang['DomainsTlds.!error.commpany_id.exists'] = 'Neplatné ID společnosti.';
+$lang['DomainsTlds.!error.module_id.exists'] = 'Neplatné ID modulu.';
+$lang['DomainsTlds.!error.package_group_id.exists'] = 'Neplatné ID skupiny balíčků.';
+$lang['DomainsTlds.!error.package_id.exists'] = 'Neplatné ID balíčku.';
+$lang['DomainsTlds.!error.tld_id.exists'] = 'Neplatné ID TLD.';
+$lang['DomainsTlds.!error.tld.length'] = 'TLD musí mít alespoň 3 znaky.';
+$lang['DomainsTlds.!error.tld.not_exists'] = 'TLD ve společnosti neexistuje.';
+$lang['DomainsTlds.!error.tld.exists'] = 'TLD již ve společnosti existuje.';
+$lang['DomainsTlds.!error.tld.empty'] = 'Zadejte TLD.';
+
