171b118863b61b0d6e303ef214dd10e3c6499202
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:48 -0800

    Updated it_it language files

---
+++ plugins/extension_generator/language/it_it/extension_generator_extensions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Extension Generator Extensions
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorExtensions.getformtypes.advanced'] = 'Avanzato';
+$lang['ExtensionGeneratorExtensions.getformtypes.basic'] = 'Base';
+$lang['ExtensionGeneratorExtensions.gettypes.nonmerchant'] = 'Gateway non commerciale';
+$lang['ExtensionGeneratorExtensions.gettypes.merchant'] = 'Gateway commerciale';
+$lang['ExtensionGeneratorExtensions.gettypes.plugin'] = 'Plugin';
+$lang['ExtensionGeneratorExtensions.gettypes.module'] = 'Modulo';
+$lang['ExtensionGeneratorExtensions.!error.id.exists'] = 'L\'ID dell\'estensione indicato non è valido.';
+$lang['ExtensionGeneratorExtensions.!error.code_examples.format'] = 'Formato di opzione degli esempi di codice non valido.';
+$lang['ExtensionGeneratorExtensions.!error.form_type.valid'] = 'Tipo di modulo non valido.';
+$lang['ExtensionGeneratorExtensions.!error.type.valid'] = 'Tipo di estensione non valido.';
+$lang['ExtensionGeneratorExtensions.!error.date_updated.format'] = 'Formato di aggiornamento della data non valido.';
+$lang['ExtensionGeneratorExtensions.!error.company_id.exists'] = 'ID azienda non valido.';
+$lang['ExtensionGeneratorExtensions.!error.name.empty'] = 'Inserire un nome di estensione.';
+
