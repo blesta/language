d9fe2bbcaffe5e7e5a6355d5aced943063208c5f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:19 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/admin_payouts.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Admin Payouts
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminPayouts.!success.payout_updated'] = 'Η πληρωμή ενημερώθηκε επιτυχώς.';
+$lang['AdminPayouts.!success.payout_approved'] = 'Η πληρωμή εγκρίθηκε επιτυχώς.';
+$lang['AdminPayouts.!success.payout_declined'] = 'Η πληρωμή απορρίφθηκε επιτυχώς.';
+$lang['AdminPayouts.!error.insufficient_funds_payout_request'] = 'Δεν υπάρχουν επαρκή κεφάλαια για την επεξεργασία του αιτήματος πληρωμής.';
+$lang['AdminPayouts.index.boxtitle_payouts'] = 'Πληρωμές θυγατρικών';
+$lang['AdminPayouts.index.category_approved'] = 'Εγκεκριμένο';
+$lang['AdminPayouts.index.category_pending'] = 'Εκκρεμεί';
+$lang['AdminPayouts.index.category_declined'] = 'Απορρίφθηκε';
+$lang['AdminPayouts.index.heading_payout_id'] = 'ID πληρωμής';
+$lang['AdminPayouts.index.heading_name'] = 'Πελάτης';
+$lang['AdminPayouts.index.heading_code'] = 'Κωδικός';
+$lang['AdminPayouts.index.heading_payment_method_id'] = 'Μέθοδος πληρωμής';
+$lang['AdminPayouts.index.heading_requested_amount'] = 'Αιτούμενο ποσό';
+$lang['AdminPayouts.index.heading_requested_currency'] = 'Ζητούμενο νόμισμα';
+$lang['AdminPayouts.index.heading_paid_amount'] = 'Καταβληθέν ποσό';
+$lang['AdminPayouts.index.heading_paid_currency'] = 'Πληρωμένο νόμισμα';
+$lang['AdminPayouts.index.heading_date_requested'] = 'Ημερομηνία αίτησης';
+$lang['AdminPayouts.index.heading_options'] = 'Επιλογές';
+$lang['AdminPayouts.index.option_edit'] = 'Επεξεργασία';
+$lang['AdminPayouts.index.option_approve'] = 'Έγκριση';
+$lang['AdminPayouts.index.option_decline'] = 'Πτώση';
+$lang['AdminPayouts.index.confirm_approve'] = 'Είστε σίγουρος ότι θέλετε να εγκρίνετε αυτή την πληρωμή;';
+$lang['AdminPayouts.index.confirm_decline'] = 'Είστε σίγουρος ότι θέλετε να αρνηθείτε αυτή την πληρωμή;';
+$lang['AdminPayouts.index.no_results'] = 'Αυτή τη στιγμή δεν υπάρχουν αιτήματα πληρωμής.';
+$lang['AdminPayouts.index.payment_method_na'] = 'N/A';
+$lang['AdminPayouts.edit.boxtitle_edit_payout'] = 'Επεξεργασία πληρωμής';
+$lang['AdminPayouts.edit.heading_payout_request'] = 'Αίτημα πληρωμής';
+$lang['AdminPayouts.edit.field_payment_method_id'] = 'Μέθοδος πληρωμής';
+$lang['AdminPayouts.edit.field_requested_amount'] = 'Αιτούμενο ποσό';
+$lang['AdminPayouts.edit.field_requested_currency'] = 'Ζητούμενο νόμισμα';
+$lang['AdminPayouts.edit.field_status'] = 'Κατάσταση';
+$lang['AdminPayouts.edit.modify_payout'] = 'Τροποποίηση πληρωμής';
+
