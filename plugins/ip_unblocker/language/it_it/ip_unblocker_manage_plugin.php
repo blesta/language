23c7f627739397eef753ea4707061317e949d9e8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:55 -0800

    Updated it_it language files

---
+++ plugins/ip_unblocker/language/it_it/ip_unblocker_manage_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Ip Unblocker Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['IpUnblockerManagePlugin.index.submit_settings'] = 'Risparmiare';
+$lang['IpUnblockerManagePlugin.index.client_set_ip'] = 'Consentire che l\'indirizzo IP sia fornito dal client';
+$lang['IpUnblockerManagePlugin.index.boxtitle_manage'] = 'Sbloccatore IP';
+$lang['IpUnblockerManagePlugin.!success.settings_updated'] = 'Le impostazioni sono state aggiornate con successo!';
+
