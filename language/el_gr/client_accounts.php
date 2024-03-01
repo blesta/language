3f127cb8a3a121a2d601a3da51ab5036e9fadaa4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:02 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_accounts.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Client Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientAccounts.navigation.nav_return'] = 'Επιστροφή στο ταμπλό';
+$lang['ClientAccounts.navigation.nav_paymentaccounts_add'] = 'Προσθήκη λογαριασμού πληρωμών';
+$lang['ClientAccounts.navigation.nav_paymentaccounts'] = 'Λογαριασμοί πληρωμών';
+$lang['ClientAccounts.navigation.nav_dashboard'] = 'Ταμπλό';
+$lang['ClientAccounts.ach_info.field_savedetails'] = 'Αποθήκευση λογαριασμού';
+$lang['ClientAccounts.ach_info.field_routingnum'] = 'Αριθμός δρομολόγησης';
+$lang['ClientAccounts.ach_info.field_accountnum'] = 'Αριθμός λογαριασμού';
+$lang['ClientAccounts.ach_info.field_type'] = 'Τύπος';
+$lang['ClientAccounts.ach_info.heading_ach'] = 'Πληροφορίες τραπεζικού λογαριασμού';
+$lang['ClientAccounts.cc_info.tooltip_code'] = 'Ο 3- ή 4-ψήφιος κωδικός ασφαλείας, που βρίσκεται συνήθως στο πίσω μέρος της κάρτας.';
+$lang['ClientAccounts.cc_info.field_savedetails'] = 'Αποθήκευση λογαριασμού';
+$lang['ClientAccounts.cc_info.field_expiration'] = 'Ημερομηνία λήξης';
+$lang['ClientAccounts.cc_info.field_security'] = 'Κωδικός ασφαλείας';
+$lang['ClientAccounts.cc_info.field_number'] = 'Αριθμός';
+$lang['ClientAccounts.cc_info.heading_cc'] = 'Πληροφορίες πιστωτικής κάρτας';
+$lang['ClientAccounts.contact_info.field_email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['ClientAccounts.contact_info.field_zip'] = 'Ταχυδρομικός/Ταχυδρομικός κώδικας';
+$lang['ClientAccounts.contact_info.field_state'] = 'Κράτος';
+$lang['ClientAccounts.contact_info.field_country'] = 'Χώρα';
+$lang['ClientAccounts.contact_info.field_city'] = 'Πόλη';
+$lang['ClientAccounts.contact_info.field_address2'] = 'Διεύθυνση 2';
+$lang['ClientAccounts.contact_info.field_address1'] = 'Διεύθυνση 1';
+$lang['ClientAccounts.contact_info.field_title'] = 'Τίτλος';
+$lang['ClientAccounts.contact_info.field_company'] = 'Εταιρεία';
+$lang['ClientAccounts.contact_info.field_last_name'] = 'Επώνυμο';
+$lang['ClientAccounts.contact_info.field_first_name'] = 'Όνομα';
+$lang['ClientAccounts.contact_info.field_contact_id'] = 'Αντιγραφή στοιχείων επικοινωνίας από';
+$lang['ClientAccounts.contact_info.text_select_contact'] = 'Μπορείτε να επιλέξετε μια υπάρχουσα επαφή για να προσυμπληρώσετε αυτή τη φόρμα.';
+$lang['ClientAccounts.contact_info.heading_contact'] = 'Στοιχεία επικοινωνίας';
+$lang['ClientAccounts.setcontactview.text_none'] = 'Κανένα';
+$lang['ClientAccounts.editcc.field_accountsubmit'] = 'Επεξεργασία λογαριασμού';
+$lang['ClientAccounts.editcc.boxtitle_editcc'] = 'Επεξεργασία λογαριασμού πιστωτικής κάρτας';
+$lang['ClientAccounts.editcc.page_title'] = 'Πελάτης #%1$s Επεξεργασία λογαριασμού πιστωτικής κάρτας';
+$lang['ClientAccounts.verifyach.field_accountsubmit'] = 'Επαλήθευση λογαριασμού';
+$lang['ClientAccounts.verifyach.field_seconddeposit'] = 'Δεύτερη κατάθεση';
+$lang['ClientAccounts.verifyach.field_firstdeposit'] = 'Πρώτη κατάθεση';
+$lang['ClientAccounts.verifyach.boxtitle_verifyach'] = 'Επαλήθευση λογαριασμού ACH';
+$lang['ClientAccounts.verifyach.page_title'] = 'Πελάτης #%1$s Επαλήθευση λογαριασμού ACH';
+$lang['ClientAccounts.editach.field_accountsubmit'] = 'Επεξεργασία λογαριασμού';
+$lang['ClientAccounts.editach.boxtitle_editach'] = 'Επεξεργασία λογαριασμού ACH';
+$lang['ClientAccounts.editach.page_title'] = 'Πελάτης #%1$s Επεξεργασία λογαριασμού ACH';
+$lang['ClientAccounts.add.field_accountsubmit'] = 'Δημιουργία λογαριασμού';
+$lang['ClientAccounts.add.field_nextsubmit'] = 'Συνεχίστε';
+$lang['ClientAccounts.add.boxtitle_create'] = 'Προσθήκη λογαριασμού πληρωμών';
+$lang['ClientAccounts.add.field_ach_type'] = 'ACH';
+$lang['ClientAccounts.add.field_cc_type'] = 'Πιστωτική κάρτα';
+$lang['ClientAccounts.add.heading_account_type'] = 'Τύπος λογαριασμού';
+$lang['ClientAccounts.add.page_title'] = 'Πελάτης #%1$s Προσθήκη λογαριασμού πληρωμών';
+$lang['ClientAccounts.index.no_results'] = 'Δεν έχετε λογαριασμούς πληρωμών.';
+$lang['ClientAccounts.index.field_removeautodebit'] = 'Κατάργηση αυτόματης χρέωσης';
+$lang['ClientAccounts.index.field_accountsubmit'] = 'Ορισμός ως Προεπιλεγμένος Λογαριασμός Πληρωμής';
+$lang['ClientAccounts.index.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον λογαριασμό πληρωμών;';
+$lang['ClientAccounts.index.type_ach'] = '%1$s - %2$s';
+$lang['ClientAccounts.index.type_cc'] = '%1$s - %2$s';
+$lang['ClientAccounts.index.option_delete'] = 'Διαγραφή';
+$lang['ClientAccounts.index.option_verify'] = 'Επαλήθευση';
+$lang['ClientAccounts.index.option_edit'] = 'Επεξεργασία';
+$lang['ClientAccounts.index.heading_options'] = 'Επιλογές';
+$lang['ClientAccounts.index.heading_last4'] = 'Τελευταία 4';
+$lang['ClientAccounts.index.heading_type'] = 'Τύπος';
+$lang['ClientAccounts.index.heading_name'] = 'Όνομα';
+$lang['ClientAccounts.index.boxtitle_accounts'] = 'Λογαριασμοί πληρωμών';
+$lang['ClientAccounts.index.create_account'] = 'Προσθήκη λογαριασμού πληρωμών';
+$lang['ClientAccounts.index.page_title'] = 'Πελάτης #%1$s Λογαριασμοί';
+$lang['ClientAccounts.!info.ach_verification_edit'] = 'Μετά την επεξεργασία αυτού του λογαριασμού, θα πρέπει να τον επαληθεύσετε πριν μπορέσετε να τον χρησιμοποιήσετε για να πραγματοποιήσετε μια πληρωμή.';
+$lang['ClientAccounts.!info.ach_verification'] = 'Πρέπει να επαληθεύσετε αυτόν τον λογαριασμό πριν τον χρησιμοποιήσετε για να πραγματοποιήσετε μια πληρωμή.';
+$lang['ClientAccounts.!info.no_accounts'] = 'Προς το παρόν δεν έχετε λογαριασμούς πληρωμών. Προσθέστε τον πρώτο σας λογαριασμό πληρωμών παρακάτω.';
+$lang['ClientAccounts.!info.account_info'] = 'Οι λογαριασμοί πληρωμών σας επιτρέπουν να διατηρείτε πληροφορίες πληρωμής σε αρχείο και μπορούν να χρησιμοποιηθούν για την πληρωμή τιμολογίων. Ένας λογαριασμός πληρωμών που έχει επιλεγεί ως προεπιλεγμένος λογαριασμός θα χρησιμοποιείται για αυτόματες επαναλαμβανόμενες πληρωμές.';
+$lang['ClientAccounts.!notice.reenable_autodebit'] = 'Οι αυτόματες πληρωμές έχουν ειδικά απενεργοποιηθεί για αυτόν τον λογαριασμό, παρακαλούμε επικοινωνήστε με ένα μέλος του προσωπικού για να τις ενεργοποιήσετε ξανά. Μπορείτε να επιλέξετε έναν λογαριασμό πληρωμών ως προεπιλεγμένο, αλλά δεν θα χρεώνεται αυτόματα μέχρι να ενεργοποιηθεί εκ νέου από το προσωπικό.';
+$lang['ClientAccounts.!notice.cc_disabled'] = 'Οι λογαριασμοί πληρωμών με πιστωτική κάρτα είναι προς το παρόν απενεργοποιημένοι και δεν μπορούν να ενημερωθούν.';
+$lang['ClientAccounts.!notice.ach_disabled'] = 'Οι λογαριασμοί πληρωμών ACH είναι προς το παρόν απενεργοποιημένοι και δεν μπορούν να ενημερωθούν.';
+$lang['ClientAccounts.!notice.disabled'] = 'Η δυνατότητα προσθήκης λογαριασμών πληρωμών είναι προς το παρόν απενεργοποιημένη.';
+$lang['ClientAccounts.!error.payment_account_type_invalid'] = 'Μη έγκυρος τύπος λογαριασμού πληρωμής.';
+$lang['ClientAccounts.!error.account_invalid'] = 'Ο τύπος λογαριασμού πληρωμών που δηλώθηκε είναι απενεργοποιημένος ή άκυρος.';
+$lang['ClientAccounts.!success.achaccount_verified'] = 'Ο λογαριασμός πληρωμής έχει επαληθευτεί επιτυχώς.';
+$lang['ClientAccounts.!success.achaccount_updated'] = 'Ο λογαριασμός πληρωμής ενημερώθηκε επιτυχώς.';
+$lang['ClientAccounts.!success.achaccount_deleted'] = 'Ο λογαριασμός πληρωμών διαγράφηκε επιτυχώς.';
+$lang['ClientAccounts.!success.ccaccount_updated'] = 'Ο λογαριασμός πληρωμής ενημερώθηκε επιτυχώς.';
+$lang['ClientAccounts.!success.ccaccount_deleted'] = 'Ο λογαριασμός πληρωμών διαγράφηκε επιτυχώς.';
+$lang['ClientAccounts.!success.account_created'] = 'Ο λογαριασμός πληρωμών δημιουργήθηκε με επιτυχία.';
+$lang['ClientAccounts.!success.defaultaccount_deleted'] = 'Ο προεπιλεγμένος λογαριασμός πληρωμής έχει καταργηθεί.';
+$lang['ClientAccounts.!success.defaultaccount_updated'] = 'Ο προεπιλεγμένος λογαριασμός πληρωμής έχει ενημερωθεί.';
+
