ff1e65494ea07409c663bd94049934fd7dde1f63
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:26 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/admin_company_messengers.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Admin Company Messengers
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyMessengers.templates.Order.received_staff_name'] = 'Ordre reçu';
+$lang['AdminCompanyMessengers.templates.Order.received_staff_desc'] = 'Avis envoyé lorsqu\'une commande est reçue.';
+
