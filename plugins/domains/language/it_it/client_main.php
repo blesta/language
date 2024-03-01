f2dcd5392c4596ba4f113c8531b348a0b342feb7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:37 -0800

    Updated it_it language files

---
+++ plugins/domains/language/it_it/client_main.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.getfilters.field_service_meta'] = 'Nome di dominio';
+$lang['ClientMain.getfilters.field_package_name'] = 'TLD';
+$lang['ClientMain.getfilters.field_module_id'] = 'Moduli Registrar';
+$lang['ClientMain.getfilters.any'] = 'Qualsiasi';
+$lang['ClientMain.serviceinfo.cancellation_reason'] = 'Motivo della cancellazione: %1$s';
+$lang['ClientMain.serviceinfo.parent_service'] = 'Questo dominio appartiene a <a href="%1$s">%2$s (%3$s)</a>. Se il servizio viene cancellato, anche questo dominio verrà cancellato.';
+$lang['ClientMain.serviceinfo.no_results'] = 'Questo dominio non ha dettagli.';
+$lang['ClientMain.index.no_results'] = 'Non hai domini %1$s.';
+$lang['ClientMain.index.text_never'] = 'Mai';
+$lang['ClientMain.index.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['ClientMain.index.option_manage'] = 'Gestire';
+$lang['ClientMain.index.heading_options'] = 'Opzioni';
+$lang['ClientMain.index.heading_deletion_date'] = 'Data di cancellazione';
+$lang['ClientMain.index.heading_suspension_date'] = 'Data di sospensione';
+$lang['ClientMain.index.heading_expiration_date'] = 'Data di scadenza';
+$lang['ClientMain.index.heading_renewal_date'] = 'Data di rinnovo';
+$lang['ClientMain.index.heading_registration_date'] = 'Data di registrazione';
+$lang['ClientMain.index.heading_term'] = 'Termine';
+$lang['ClientMain.index.heading_domain'] = 'Dominio';
+$lang['ClientMain.index.heading_status'] = 'Stato';
+$lang['ClientMain.index.heading_addons'] = 'Componenti aggiuntivi';
+$lang['ClientMain.index.category_deleted'] = 'Soppresso';
+$lang['ClientMain.index.category_suspended'] = 'Sospeso';
+$lang['ClientMain.index.category_pending'] = 'In attesa';
+$lang['ClientMain.index.category_active'] = 'Attivo';
+$lang['ClientMain.index.boxtitle_domains'] = 'Domini';
+$lang['ClientMain.index.page_title'] = 'Dominio del cliente #%1$s';
+
