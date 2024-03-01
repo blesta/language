963239f5cc9dc4f27e7f484386594d1540f02c6b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:54 -0800

    Updated el_gr language files

---
+++ plugins/support_manager/language/el_gr/admin_staff.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Admin Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminStaff.!success.staff_added'] = 'Το μέλος του προσωπικού προστέθηκε επιτυχώς.';
+$lang['AdminStaff.!success.staff_updated'] = 'Το μέλος του προσωπικού ενημερώθηκε επιτυχώς.';
+$lang['AdminStaff.!success.staff_deleted'] = 'Το μέλος του προσωπικού διαγράφηκε επιτυχώς.';
+$lang['AdminStaff.!tooltip.all_day'] = 'Επιλέξτε αυτό το πλαίσιο για να είναι ο υπάλληλος διαθέσιμος όλη την ημέρα.';
+$lang['AdminStaff.!tooltip.ticket_emails'] = 'Τσεκάρετε το πλαίσιο κάθε προτεραιότητας εισιτηρίου για την οποία το προσωπικό πρέπει να λαμβάνει μηνύματα ηλεκτρονικού ταχυδρομείου εισιτηρίων.';
+$lang['AdminStaff.!tooltip.mobile_ticket_emails'] = 'Τσεκάρετε το πλαίσιο κάθε προτεραιότητας εισιτηρίου για την οποία το προσωπικό πρέπει να λαμβάνει μηνύματα ηλεκτρονικού ταχυδρομείου για κινητά εισιτήρια.';
+$lang['AdminStaff.!tooltip.ticket_messenger_notifications'] = 'Επιλέξτε το πλαίσιο κάθε προτεραιότητας εισιτηρίου για την οποία το προσωπικό πρέπει να λαμβάνει ειδοποιήσεις μέσω ticket messenger.';
+$lang['AdminStaff.!tooltip.title'] = 'Η εισαγωγή ενός τίτλου για αυτό το μέλος του προσωπικού αντικαθιστά τον προεπιλεγμένο τίτλο: %1$s';
+$lang['AdminStaff.!tooltip.signature'] = 'Εισάγετε μια υπογραφή για να είναι αυτόματα διαθέσιμη όταν απαντάτε σε εισιτήρια.';
+$lang['AdminStaff.index.page_title'] = 'Διευθυντής υποστήριξης > Προσωπικό';
+$lang['AdminStaff.add.page_title'] = 'Διαχειριστής υποστήριξης > Προσθήκη προσωπικού';
+$lang['AdminStaff.edit.page_title'] = 'Διαχειριστής υποστήριξης > Επεξεργασία προσωπικού';
+$lang['AdminStaff.index.categorylink_addstaff'] = 'Προσθήκη προσωπικού';
+$lang['AdminStaff.index.boxtitle_staff'] = 'Προσωπικό';
+$lang['AdminStaff.index.heading_first_name'] = 'Όνομα';
+$lang['AdminStaff.index.heading_email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['AdminStaff.index.heading_ticket_emails'] = 'Λάβετε email εισιτηρίων';
+$lang['AdminStaff.index.heading_mobile_ticket_emails'] = 'Λήψη email εισιτηρίων για κινητά';
+$lang['AdminStaff.index.heading_ticket_messenger_notifications'] = 'Λάβετε ειδοποιήσεις Ticket Messenger';
+$lang['AdminStaff.index.heading_options'] = 'Επιλογές';
+$lang['AdminStaff.index.option_edit'] = 'Επεξεργασία';
+$lang['AdminStaff.index.option_delete'] = 'Διαγραφή';
+$lang['AdminStaff.index.confirm_delete'] = 'Η διαγραφή αυτού του μέλους του προσωπικού θα καταργήσει την ανάθεση όλων των εισιτηρίων που έχουν ανατεθεί σε αυτό το μέλος του προσωπικού. Είστε σίγουροι ότι θέλετε να διαγράψετε αυτό το μέλος του προσωπικού;';
+$lang['AdminStaff.index.no_results'] = 'Προς το παρόν δεν υπάρχει προσωπικό που να έχει οριστεί στο σύστημα υποστήριξης.';
+$lang['AdminStaff.add.boxtitle_addstaff'] = 'Προσθήκη προσωπικού';
+$lang['AdminStaff.add.heading_staff'] = 'Προσωπικό';
+$lang['AdminStaff.add.heading_schedule'] = 'Πρόγραμμα';
+$lang['AdminStaff.add.heading_settings'] = 'Ρυθμίσεις';
+$lang['AdminStaff.add.heading_departments'] = 'Τμήματα';
+$lang['AdminStaff.add.field_staff_group_id'] = 'Ομάδα προσωπικού';
+$lang['AdminStaff.add.field_staff_id'] = 'Μέλος του προσωπικού';
+$lang['AdminStaff.add.heading_day'] = 'Ημέρα';
+$lang['AdminStaff.add.heading_all_day'] = 'Όλη την ημέρα';
+$lang['AdminStaff.add.heading_start_time'] = 'Ώρα έναρξης';
+$lang['AdminStaff.add.heading_end_time'] = 'Χρόνος τέλους';
+$lang['AdminStaff.add.ticket_emails'] = 'Λάβετε email εισιτηρίων';
+$lang['AdminStaff.add.mobile_ticket_emails'] = 'Λήψη email εισιτηρίων για κινητά';
+$lang['AdminStaff.add.ticket_messenger_notifications'] = 'Λάβετε ειδοποιήσεις Ticket Messenger';
+$lang['AdminStaff.add.title'] = 'Τίτλος';
+$lang['AdminStaff.add.signature'] = 'Υπογραφή';
+$lang['AdminStaff.add.text_assigneddepartments'] = 'Ανατεθειμένα Τμήματα';
+$lang['AdminStaff.add.text_availabledepartments'] = 'Διαθέσιμα Τμήματα';
+$lang['AdminStaff.add.field_addsubmit'] = 'Προσθήκη προσωπικού';
+$lang['AdminStaff.edit.boxtitle_editstaff'] = 'Επεξεργασία προσωπικού %1$s %2$s';
+$lang['AdminStaff.edit.heading_schedule'] = 'Πρόγραμμα';
+$lang['AdminStaff.edit.heading_settings'] = 'Ρυθμίσεις';
+$lang['AdminStaff.edit.heading_departments'] = 'Τμήματα';
+$lang['AdminStaff.edit.heading_day'] = 'Ημέρα';
+$lang['AdminStaff.edit.heading_all_day'] = 'Όλη την ημέρα';
+$lang['AdminStaff.edit.heading_start_time'] = 'Ώρα έναρξης';
+$lang['AdminStaff.edit.heading_end_time'] = 'Χρόνος τέλους';
+$lang['AdminStaff.edit.ticket_emails'] = 'Λάβετε email εισιτηρίων';
+$lang['AdminStaff.edit.mobile_ticket_emails'] = 'Λήψη email εισιτηρίων για κινητά';
+$lang['AdminStaff.edit.ticket_messenger_notifications'] = 'Λάβετε ειδοποιήσεις Ticket Messenger';
+$lang['AdminStaff.edit.title'] = 'Τίτλος';
+$lang['AdminStaff.edit.signature'] = 'Υπογραφή';
+$lang['AdminStaff.edit.text_assigneddepartments'] = 'Ανατεθειμένα Τμήματα';
+$lang['AdminStaff.edit.text_availabledepartments'] = 'Διαθέσιμα Τμήματα';
+$lang['AdminStaff.edit.field_editsubmit'] = 'Επεξεργασία προσωπικού';
+$lang['AdminStaff.staff.name'] = '%1$s %2$s';
+$lang['AdminStaff.schedule_list.heading_schedule'] = 'Πρόγραμμα';
+$lang['AdminStaff.schedule_list.heading_day'] = 'Ημέρα';
+$lang['AdminStaff.schedule_list.heading_start_time'] = 'Ώρα έναρξης';
+$lang['AdminStaff.schedule_list.heading_end_time'] = 'Χρόνος τέλους';
+$lang['AdminStaff.schedule_list.no_results'] = 'Το εν λόγω μέλος του προσωπικού δεν έχει προγραμματιστεί για καμία ημέρα.';
+$lang['AdminStaff.schedule_list.heading_departments'] = 'Τμήματα';
+$lang['AdminStaff.schedule_list.heading_department'] = 'Τμήμα';
+$lang['AdminStaff.schedule_list.heading_description'] = 'Περιγραφή';
+$lang['AdminStaff.schedule_list.no_department_results'] = 'Ο εν λόγω υπάλληλος δεν είναι τοποθετημένος σε καμία υπηρεσία.';
+$lang['AdminStaff.text.no_time'] = '--';
+
