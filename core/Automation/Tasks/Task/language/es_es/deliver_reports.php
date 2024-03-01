debbb673d457eaf790d85cd5137734f815f562ce
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:21 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/deliver_reports.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Deliver Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.deliver_reports.attempt'] = 'Intento de entrega de informes.';
+$lang['Automation.task.deliver_reports.completed'] = 'La tarea de entrega de informes ha finalizado.';
+$lang['Automation.task.deliver_reports.aging_invoices.attempt'] = 'Intentando enviar el informe de facturas vencidas.';
+$lang['Automation.task.deliver_reports.aging_invoices.attachment_fail'] = 'No se puede generar el anexo del informe de facturas vencidas.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_error'] = 'No se ha podido enviar el correo electrónico de facturas vencidas.';
+$lang['Automation.task.deliver_reports.aging_invoices.email_success'] = 'Se ha enviado el correo electrónico de informe de facturas vencidas.';
+$lang['Automation.task.deliver_reports.invoice_creation.attempt'] = 'Intento de envío del informe de creación de factura.';
+$lang['Automation.task.deliver_reports.invoice_creation.attachment_fail'] = 'No se puede generar el anexo del informe de creación de facturas.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_error'] = 'No se ha podido enviar el correo electrónico del informe de creación de factura.';
+$lang['Automation.task.deliver_reports.invoice_creation.email_success'] = 'Se ha enviado el correo electrónico del informe de creación de factura.';
+$lang['Automation.task.deliver_reports.tax_liability.attempt'] = 'Intento de envío del informe de Obligaciones Tributarias.';
+$lang['Automation.task.deliver_reports.tax_liability.attachment_fail'] = 'No se puede generar el anexo del informe de deuda tributaria.';
+$lang['Automation.task.deliver_reports.tax_liability.email_error'] = 'No se ha podido enviar el correo electrónico de responsabilidad fiscal.';
+$lang['Automation.task.deliver_reports.tax_liability.email_success'] = 'Se ha enviado el correo electrónico del informe de deuda tributaria.';
+
