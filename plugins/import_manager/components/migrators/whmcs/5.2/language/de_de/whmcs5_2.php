900bcbc7b95f5fdc98f100a4f033e5dfbe9708dd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:50 -0800

    Updated de_de language files

---
+++ plugins/import_manager/components/migrators/whmcs/5.2/language/de_de/whmcs5_2.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Whmcs5 2
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Whmcs5_2.configuration.no_local_packages'] = 'Sie müssen Pakete erstellen, bevor Sie sie zuordnen können.';
+$lang['Whmcs5_2.configuration.remote_package'] = 'Ferngesteuertes Paket';
+$lang['Whmcs5_2.configuration.local_package'] = 'Lokales Paket';
+$lang['Whmcs5_2.configuration.create_packages_false'] = 'Pakete manuell zuordnen';
+$lang['Whmcs5_2.configuration.create_packages_true'] = 'Automatisches Erstellen aller erforderlichen Pakete während des Imports';
+$lang['Whmcs5_2.settings.enable_debug'] = 'Debugging einschalten';
+$lang['Whmcs5_2.settings.balance_credit.info'] = 'Wenn diese Option aktiviert ist, wird eine Transaktion oder eine Rechnung erstellt, um sicherzustellen, dass das Kundenguthaben in Blesta mit dem für den Kunden in WHMCS eingestellten Wert übereinstimmt. Dies ist notwendig, weil WHMCS die Kundenguthaben nicht richtig berücksichtigt.';
+$lang['Whmcs5_2.settings.balance_credit'] = 'Automatischer Abgleich von Transaktionen mit Kundenguthaben in WHMCS';
+$lang['Whmcs5_2.settings.key.info'] = 'Dies ist der Verschlüsselungsschlüssel, der von WHMCS verwendet wird, um verschiedene Datenbits zu verschlüsseln. Sie finden ihn in Ihrer WHMCS configuration.php Datei.';
+$lang['Whmcs5_2.settings.key'] = 'CC-Verschlüsselung Hash';
+$lang['Whmcs5_2.settings.pass'] = 'Datenbank-Passwort';
+$lang['Whmcs5_2.settings.user'] = 'Datenbank-Benutzer';
+$lang['Whmcs5_2.settings.database'] = 'Name der Datenbank';
+$lang['Whmcs5_2.settings.host'] = 'Datenbank-Host';
+$lang['Whmcs5_2.!notice.gmp'] = 'Die Erweiterung \'gmp\' wurde nicht gefunden. Diese Erweiterung wird empfohlen, um die Leistung zu erhöhen und den Importprozess zu verbessern. Sie können auch ohne diese Erweiterung fortfahren, aber der Importvorgang kann sehr viel langsamer sein.';
+$lang['Whmcs5_2.!error.import'] = 'Der Import wurde abgeschlossen, aber es traten die folgenden Fehler auf:';
+$lang['Whmcs5_2.!error.key.invalid'] = 'Ein gültiger AESKEY ist erforderlich.';
+$lang['Whmcs5_2.!error.pass.invalid'] = 'Das Datenbank-Passwort ist erforderlich.';
+$lang['Whmcs5_2.!error.user.invalid'] = 'Datenbankbenutzer ist erforderlich.';
+$lang['Whmcs5_2.!error.database.invalid'] = 'Datenbankname ist erforderlich.';
+$lang['Whmcs5_2.!error.host.invalid'] = 'Datenbank-Host ist erforderlich.';
+
