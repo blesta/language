60238ecac5e5f0e0585e92087f06956df2aff907
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:41 -0800

    Updated cs_cz language files

---
+++ components/modules/whmsonic/language/cs_cz/whmsonic.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Whmsonic
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Whmsonic.!error.user_password.valid'] = 'Heslo musí mít alespoň 8 znaků.';
+$lang['Whmsonic.!error.user_name.empty'] = 'Uživatelské jméno nemůže být prázdné.';
+$lang['Whmsonic.!error.meta[autodj].valid'] = 'Vyberte platnou možnost přístupu AutoDJ.';
+$lang['Whmsonic.!error.meta[listeners].empty'] = 'Zadejte limit posluchačů.';
+$lang['Whmsonic.!error.meta[bandwidth].empty'] = 'Zadejte limit šířky pásma (v MB), např.: 1000.';
+$lang['Whmsonic.!error.meta[hspace].empty'] = 'Zadejte prostor pro hostování (v MB), např.: 1000.';
+$lang['Whmsonic.!error.meta[bitrate].valid'] = 'Vyberte platný limit přenosové rychlosti.';
+$lang['Whmsonic.!error.meta[client_type].valid'] = 'Vyberte platný typ klienta.';
+$lang['Whmsonic.!error.module_row.missing'] = 'Došlo k interní chybě. Řádek modulu není k dispozici.';
+$lang['Whmsonic.!error.api.internal'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+$lang['Whmsonic.!error.password_valid'] = 'Pověření API se zdají být neplatná.';
+$lang['Whmsonic.!error.ip_address_valid'] = 'Adresa IP se zdá být neplatná.';
+$lang['Whmsonic.!error.server_name_valid'] = 'Je třeba zadat označení serveru.';
+$lang['Whmsonic.service_info.option_login'] = 'Přihlášení';
+$lang['Whmsonic.service_info.options'] = 'Možnosti';
+$lang['Whmsonic.service_info.password'] = 'Heslo';
+$lang['Whmsonic.service_info.username'] = 'Uživatelské jméno';
+$lang['Whmsonic.service_info.ip_address'] = 'IP adresa';
+$lang['Whmsonic.service_field.password.tooltip'] = 'Heslo bude aktualizováno pouze lokálně v rámci systému Blesta.';
+$lang['Whmsonic.service_field.username.tooltip'] = 'Uživatelské jméno bude aktualizováno pouze lokálně v rámci systému Blesta.';
+$lang['Whmsonic.service_field.password'] = 'Heslo cPanel';
+$lang['Whmsonic.service_field.radio_password'] = 'Heslo k rádiu';
+$lang['Whmsonic.service_field.username'] = 'Uživatelské jméno rádia';
+$lang['Whmsonic.service_field.cpanel_username'] = 'Uživatelské jméno cPanel';
+$lang['Whmsonic.package_fields.autodj.no'] = 'Ne';
+$lang['Whmsonic.package_fields.autodj.yes'] = 'Ano';
+$lang['Whmsonic.package_fields.autodj'] = 'AutoDJ Access';
+$lang['Whmsonic.package_fields.listeners'] = 'Limit posluchačů';
+$lang['Whmsonic.package_fields.bandwidth'] = 'Limit šířky pásma (v MB)';
+$lang['Whmsonic.package_fields.hspace'] = 'Prostor pro hostování (v MB)';
+$lang['Whmsonic.package_fields.bitrate'] = 'Omezení přenosové rychlosti';
+$lang['Whmsonic.package_fields.client_type.internal'] = 'Interní';
+$lang['Whmsonic.package_fields.client_type.external'] = 'Externí';
+$lang['Whmsonic.package_fields.client_type'] = 'Typ klienta';
+$lang['Whmsonic.row_meta.use_ssl'] = 'Při připojování k rozhraní API používejte protokol SSL (doporučeno).';
+$lang['Whmsonic.row_meta.password'] = 'Heslo';
+$lang['Whmsonic.row_meta.ip_address'] = 'IP adresa';
+$lang['Whmsonic.row_meta.server_name'] = 'Označení serveru';
+$lang['Whmsonic.edit_row.add_btn'] = 'Upravit server';
+$lang['Whmsonic.edit_row.basic_title'] = 'Základní nastavení';
+$lang['Whmsonic.edit_row.box_title'] = 'Upravit server WHMSonic';
+$lang['Whmsonic.add_row.add_btn'] = 'Přidat server';
+$lang['Whmsonic.add_row.basic_title'] = 'Základní nastavení';
+$lang['Whmsonic.add_row.box_title'] = 'Přidání serveru WHMSonic';
+$lang['Whmsonic.order_options.first'] = 'První neplnohodnotný server';
+$lang['Whmsonic.manage.module_groups_no_results'] = 'Neexistují žádné skupiny serverů.';
+$lang['Whmsonic.manage.module_rows_no_results'] = 'Neexistují žádné servery.';
+$lang['Whmsonic.manage.module_groups.confirm_delete'] = 'Opravdu chcete tuto skupinu serverů odstranit?';
+$lang['Whmsonic.manage.module_rows.confirm_delete'] = 'Opravdu chcete tento server odstranit?';
+$lang['Whmsonic.manage.module_groups.delete'] = 'Odstranit';
+$lang['Whmsonic.manage.module_rows.delete'] = 'Odstranit';
+$lang['Whmsonic.manage.module_groups.edit'] = 'Upravit';
+$lang['Whmsonic.manage.module_rows.edit'] = 'Upravit';
+$lang['Whmsonic.manage.module_groups_heading.options'] = 'Možnosti';
+$lang['Whmsonic.manage.module_groups_heading.servers'] = 'Počet serverů';
+$lang['Whmsonic.manage.module_groups_heading.name'] = 'Název skupiny';
+$lang['Whmsonic.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['Whmsonic.manage.module_rows_heading.ip_address'] = 'IP adresa';
+$lang['Whmsonic.manage.module_rows_heading.name'] = 'Označení serveru';
+$lang['Whmsonic.manage.module_groups_title'] = 'Skupiny serverů';
+$lang['Whmsonic.manage.module_rows_title'] = 'Servery';
+$lang['Whmsonic.add_module_group'] = 'Přidání skupiny serverů';
+$lang['Whmsonic.add_module_row'] = 'Přidat server';
+$lang['Whmsonic.tab_client_actions.radio_password'] = 'Heslo k rádiu';
+$lang['Whmsonic.tab_client_actions.password'] = 'Heslo cPanel';
+$lang['Whmsonic.tab_client_actions.change_password'] = 'Změna hesel';
+$lang['Whmsonic.tab_stats.info.whmsonic_radio_password'] = 'Heslo k rádiu';
+$lang['Whmsonic.tab_stats.info.whmsonic_radio_ip'] = 'Rádio IP';
+$lang['Whmsonic.tab_stats.info.whmsonic_ftp'] = 'Přístup k FTP';
+$lang['Whmsonic.tab_stats.info.password'] = 'Heslo';
+$lang['Whmsonic.tab_stats.info.username'] = 'Uživatelské jméno';
+$lang['Whmsonic.tab_stats.info.autodj'] = 'AutoDJ Access';
+$lang['Whmsonic.tab_stats.info.listeners'] = 'Limit posluchačů';
+$lang['Whmsonic.tab_stats.info.hspace'] = 'Prostor pro hostování (v MB)';
+$lang['Whmsonic.tab_stats.info.bandwidth'] = 'Limit šířky pásma (v MB)';
+$lang['Whmsonic.tab_stats.info.bitrate'] = 'Omezení přenosové rychlosti';
+$lang['Whmsonic.tab_stats.info.client_type'] = 'Typ klienta';
+$lang['Whmsonic.tab_stats.info_heading.value'] = 'Hodnota';
+$lang['Whmsonic.tab_stats.info_heading.field'] = 'Pole';
+$lang['Whmsonic.submit'] = 'Odeslat';
+$lang['Whmsonic.tab_client_actions'] = 'Akce';
+$lang['Whmsonic.tab_stats'] = 'Informace o službě';
+$lang['Whmsonic.module_group'] = 'Skupina serverů';
+$lang['Whmsonic.module_row_plural'] = 'Servery';
+$lang['Whmsonic.module_row'] = 'Server';
+$lang['Whmsonic.description'] = 'Umožňuje vám nabízet shoutcast, icecast, streamování médií, AutoDJ, prodej rádia z vašeho dedikovaného nebo VPS serveru.';
+$lang['Whmsonic.name'] = 'WHMSonic';
+
