ba5b1e41709d738203ceb5a4c24fc17cd6d2626c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:53 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_reports.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admin Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminReports.index.customize'] = 'Προσαρμογή';
+$lang['AdminReports.index.reports'] = 'Δημιουργία';
+$lang['AdminReports.index.field_submit'] = 'Εξαγωγή';
+$lang['AdminReports.index.field_report_type'] = 'Τύπος έκθεσης';
+$lang['AdminReports.index.field_format'] = 'Μορφή';
+$lang['AdminReports.index.box_title'] = 'Αναφορές';
+$lang['AdminReports.index.page_title'] = 'Αναφορές χρέωσης';
+
