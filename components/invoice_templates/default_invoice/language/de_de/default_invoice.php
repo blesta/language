a986485bf8821176be9ee5ad8b6eeef42e003fba
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:17 -0800

    Updated de_de language files

---
+++ components/invoice_templates/default_invoice/language/de_de/default_invoice.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Default Invoice
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DefaultInvoice.balance_heading'] = 'Bilanz';
+$lang['DefaultInvoice.payments_applied_amount'] = 'Angewandter Betrag';
+$lang['DefaultInvoice.payments_transaction_id'] = 'Vorgangsnummer';
+$lang['DefaultInvoice.payments_type_name'] = 'Typ';
+$lang['DefaultInvoice.payments_applied_date'] = 'Datum des Antrags';
+$lang['DefaultInvoice.payments_heading'] = 'Zahlungen';
+$lang['DefaultInvoice.watermark_paid'] = 'PAID';
+$lang['DefaultInvoice.lines_cost'] = 'Kosten';
+$lang['DefaultInvoice.lines_unit_price'] = 'Preis pro Einheit';
+$lang['DefaultInvoice.lines_quantity'] = 'Menge';
+$lang['DefaultInvoice.lines_description'] = 'Beschreibung';
+$lang['DefaultInvoice.total_heading'] = 'Insgesamt';
+$lang['DefaultInvoice.subtotal_heading'] = 'Zwischensumme';
+$lang['DefaultInvoice.notes_heading'] = 'Anmerkungen';
+$lang['DefaultInvoice.date_due'] = 'Fälligkeitsdatum:';
+$lang['DefaultInvoice.date_expires'] = 'Verfallsdatum:';
+$lang['DefaultInvoice.date_billed'] = 'Datum der Rechnung:';
+$lang['DefaultInvoice.date_created'] = 'Erstellungsdatum:';
+$lang['DefaultInvoice.client_tax_id'] = 'Steuer-ID/VATIN des Kunden:';
+$lang['DefaultInvoice.tax_id'] = 'Steuer-ID/VATIN:';
+$lang['DefaultInvoice.client_id_code'] = 'Kundennummer:';
+$lang['DefaultInvoice.quotation_id_code'] = 'Angebotsnummer:';
+$lang['DefaultInvoice.draft_id_code'] = 'Entwurfsnummer:';
+$lang['DefaultInvoice.proforma_id_code'] = 'Pro-forma-Nummer:';
+$lang['DefaultInvoice.invoice_id_code'] = 'Rechnungsnummer:';
+$lang['DefaultInvoice.type_quotation_draft'] = 'ANGEBOTSENTWURF';
+$lang['DefaultInvoice.type_quotation'] = 'ANGEBOT';
+$lang['DefaultInvoice.type_proforma'] = 'PRO-FORMA-RECHNUNG';
+$lang['DefaultInvoice.type_void'] = 'RECHNUNG - GÜLTIG';
+$lang['DefaultInvoice.type_draft'] = 'RECHNUNG - ENTWURF';
+$lang['DefaultInvoice.type_active'] = 'RECHNUNG';
+$lang['DefaultInvoice.terms_heading'] = 'Bedingungen';
+$lang['DefaultInvoice.page_of'] = 'Seite %1$s von %2$s';
+$lang['DefaultInvoice.name'] = 'Standard';
+
