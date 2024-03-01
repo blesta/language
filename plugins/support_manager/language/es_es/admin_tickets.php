f9eab0f135ee7f1e5139ac9096e95bc97111a92d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:10 -0800

    Updated es_es language files

---
+++ plugins/support_manager/language/es_es/admin_tickets.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Admin Tickets
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminTickets.!success.ticket_created'] = 'El ticket #%1$s ha sido abierto con éxito.';
+$lang['AdminTickets.!success.ticket_updated'] = 'El ticket #%1$s ha sido actualizado correctamente.';
+$lang['AdminTickets.!success.ticket_split'] = 'El ticket #%1$s ha sido dividido con éxito en el ticket #%2$s.';
+$lang['AdminTickets.!success.ticket_merge'] = 'Los tickets seleccionados se han fusionado correctamente en el ticket #%1$s.';
+$lang['AdminTickets.!success.ticket_reassign'] = 'Los tickets seleccionados se han reasignado correctamente.';
+$lang['AdminTickets.!success.ticket_update_status'] = 'Los tickets seleccionados se han actualizado correctamente.';
+$lang['AdminTickets.!success.ticket_delete'] = 'Los tickets seleccionados se han eliminado correctamente.';
+$lang['AdminTickets.!notice.no_departments_staff'] = 'Aún no se ha creado ningún empleado y/o departamento. Haga clic en %1$s arriba para crear un departamento, o en %2$s para asignar un miembro del personal.';
+$lang['AdminTickets.index.page_title'] = 'Gestor de Soporte > Tickets';
+$lang['AdminTickets.add.page_title'] = 'Gestor de Soporte > Abrir Ticket';
+$lang['AdminTickets.reply.page_title'] = 'Gestor de Soporte > Ticket #%1$s';
+$lang['AdminTickets.search.page_title'] = 'Resultados de la búsqueda de "%1$s"';
+$lang['AdminTickets.text.unassigned'] = 'No asignado';
+$lang['AdminTickets.index.category_open'] = 'A la espera de la respuesta del personal';
+$lang['AdminTickets.index.category_awaiting_reply'] = 'A la espera de la respuesta del cliente';
+$lang['AdminTickets.index.category_in_progress'] = 'En curso';
+$lang['AdminTickets.index.category_on_hold'] = 'En espera';
+$lang['AdminTickets.index.category_closed'] = 'Cerrado';
+$lang['AdminTickets.index.category_trash'] = 'Basura';
+$lang['AdminTickets.index.categorylink_createticket'] = 'Billete abierto';
+$lang['AdminTickets.index.boxtitle_tickets'] = 'Entradas';
+$lang['AdminTickets.index.heading_code'] = 'Número de billete';
+$lang['AdminTickets.index.heading_client'] = 'Cliente';
+$lang['AdminTickets.index.heading_priority'] = 'Prioridad';
+$lang['AdminTickets.index.heading_department_name'] = 'Departamento';
+$lang['AdminTickets.index.heading_summary'] = 'Resumen';
+$lang['AdminTickets.index.heading_assigned_staff'] = 'Asignado a';
+$lang['AdminTickets.index.heading_last_reply_date'] = 'Última respuesta';
+$lang['AdminTickets.index.any'] = 'Cualquier';
+$lang['AdminTickets.index.minutes'] = '%1$s minutos';
+$lang['AdminTickets.index.hour'] = '1 hora';
+$lang['AdminTickets.index.hours'] = '%1$s horas';
+$lang['AdminTickets.index.field_ticket_number'] = 'Número de billete';
+$lang['AdminTickets.index.field_priority'] = 'Prioridad';
+$lang['AdminTickets.index.field_department_id'] = 'Departamento';
+$lang['AdminTickets.index.field_summary'] = 'Resumen';
+$lang['AdminTickets.index.field_assigned_staff'] = 'Asignado a';
+$lang['AdminTickets.index.field_last_reply'] = 'Última respuesta';
+$lang['AdminTickets.index.unassigned'] = 'Sin asignar';
+$lang['AdminTickets.index.last_reply_by'] = 'por';
+$lang['AdminTickets.index.no_results'] = 'Actualmente no hay entradas con este estado.';
+$lang['AdminTickets.index.text_with_selected'] = 'Con entradas seleccionadas, actúe:';
+$lang['AdminTickets.index.text_into_ticket'] = 'En billete:';
+$lang['AdminTickets.index.text_to_client'] = 'Al cliente:';
+$lang['AdminTickets.index.text_to_status'] = 'Cambia a:';
+$lang['AdminTickets.index.ticket_number_placeholder'] = 'Número de billete';
+$lang['AdminTickets.index.text_no_tickets'] = 'No se han encontrado entradas abiertas. Intenta buscar de nuevo.';
+$lang['AdminTickets.index.field_actionsubmit'] = 'Enviar';
+$lang['AdminTickets.index.ticket_name'] = '#%1$s %2$s %3$s';
+$lang['AdminTickets.index.ticket_email'] = '#%1$s %2$s';
+$lang['AdminTickets.add.boxtitle_add'] = 'Billete abierto';
+$lang['AdminTickets.add.heading_search_client'] = 'Buscar al cliente';
+$lang['AdminTickets.add.text_no_clients'] = 'No se han encontrado clientes. Intenta buscar de nuevo.';
+$lang['AdminTickets.add.heading_summary'] = 'Resumen';
+$lang['AdminTickets.add.heading_client'] = 'Cliente';
+$lang['AdminTickets.add.heading_department'] = 'Departamento';
+$lang['AdminTickets.add.heading_staff_id'] = 'Asignado a';
+$lang['AdminTickets.add.heading_priority'] = 'Prioridad';
+$lang['AdminTickets.add.heading_status'] = 'Estado';
+$lang['AdminTickets.add.field_attachments'] = 'Archivos adjuntos';
+$lang['AdminTickets.add.field_details'] = 'Detalles';
+$lang['AdminTickets.add.text_add_attachment'] = 'Añadir anexo';
+$lang['AdminTickets.add.field_addsubmit'] = 'Billete abierto';
+$lang['Admintickets.add.client_placeholder'] = 'ID o nombre del cliente';
+$lang['AdminTickets.add.text_add_response'] = 'Insertar una respuesta predefinida';
+$lang['AdminTickets.add.dropzone_drop_files_here'] = 'Suelte los archivos aquí para cargarlos o haga clic para seleccionarlos';
+$lang['AdminTickets.add.dropzone_remove_file'] = 'Eliminar archivo';
+$lang['AdminTickets.reply.boxtitle_reply'] = 'Billete #%1$s';
+$lang['AdminTickets.reply.heading_summary'] = 'Resumen';
+$lang['AdminTickets.reply.heading_client'] = 'Cliente';
+$lang['AdminTickets.reply.heading_department'] = 'Departamento';
+$lang['AdminTickets.reply.heading_staff_id'] = 'Asignado a';
+$lang['AdminTickets.reply.heading_priority'] = 'Prioridad';
+$lang['AdminTickets.reply.heading_status'] = 'Estado';
+$lang['AdminTickets.reply.heading_date_added'] = 'Fecha de apertura';
+$lang['AdminTickets.reply.text_add_response'] = 'Insertar una respuesta predefinida';
+$lang['AdminTickets.reply.text_with_selected'] = 'Con las respuestas seleccionadas, realizar:';
+$lang['AdminTickets.reply.heading_reply'] = 'Añadir respuesta';
+$lang['AdminTickets.reply.field_reply'] = 'Respuesta';
+$lang['AdminTickets.reply.field_note'] = 'Nota';
+$lang['AdminTickets.reply.field_attachments'] = 'Archivos adjuntos';
+$lang['AdminTickets.reply.text_add_attachment'] = 'Añadir anexo';
+$lang['AdminTickets.reply.field_replysubmit'] = 'Ticket de actualización';
+$lang['AdminTickets.reply.field_actionsubmit'] = 'Vaya a';
+$lang['AdminTickets.reply.refresh'] = 'Hay nuevas respuestas o cambios de estado.';
+$lang['AdminTickets.reply.refresh_link'] = 'Haga clic para mostrar.';
+$lang['AdminTickets.reply.reply_date'] = 'El %1$s %2$s %3$s respondió';
+$lang['AdminTickets.reply.log_date'] = '%1$s por %2$s %3$s';
+$lang['AdminTickets.reply.system'] = 'Sistema';
+$lang['AdminTickets.reply.staff_title'] = 'Personal de apoyo';
+$lang['AdminTickets.reply.dropzone_drop_files_here'] = 'Suelte los archivos aquí para cargarlos o haga clic para seleccionarlos';
+$lang['AdminTickets.reply.dropzone_remove_file'] = 'Eliminar archivo';
+$lang['AdminTickets.client.category_open'] = 'A la espera de la respuesta del personal';
+$lang['AdminTickets.client.category_awaiting_reply'] = 'A la espera de la respuesta del cliente';
+$lang['AdminTickets.client.category_in_progress'] = 'En curso';
+$lang['AdminTickets.client.category_on_hold'] = 'En espera';
+$lang['AdminTickets.client.category_closed'] = 'Cerrado';
+$lang['AdminTickets.client.category_trash'] = 'Basura';
+$lang['AdminTickets.client.categorylink_createticket'] = 'Billete abierto';
+$lang['AdminTickets.client.boxtitle_tickets'] = 'Entradas';
+$lang['AdminTickets.client.heading_code'] = 'Número de billete';
+$lang['AdminTickets.client.heading_priority'] = 'Prioridad';
+$lang['AdminTickets.client.heading_department_name'] = 'Departamento';
+$lang['AdminTickets.client.heading_summary'] = 'Resumen';
+$lang['AdminTickets.client.heading_last_reply_date'] = 'Última respuesta';
+$lang['AdminTickets.client.no_results'] = 'Actualmente no hay entradas con este estado.';
+$lang['AdminTickets.search.boxtitle_tickets'] = 'Buscar entradas para "%1$s"';
+$lang['AdminTickets.search.heading_code'] = 'Número de billete';
+$lang['AdminTickets.search.heading_priority'] = 'Prioridad';
+$lang['AdminTickets.search.heading_status'] = 'Estado';
+$lang['AdminTickets.search.heading_department_name'] = 'Departamento';
+$lang['AdminTickets.search.heading_summary'] = 'Resumen';
+$lang['AdminTickets.search.heading_last_reply_date'] = 'Última respuesta';
+$lang['AdminTickets.search.no_results'] = 'No hay entradas que coincidan con los criterios de búsqueda.';
+
