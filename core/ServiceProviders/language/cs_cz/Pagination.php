6b9e1882665207bb1e8337d1d35814b383f565a0
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:05 -0800

    Updated cs_cz language files

---
+++ core/ServiceProviders/language/cs_cz/Pagination.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Pagination
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pagination.first'] = '← První';
+$lang['Pagination.last'] = 'Poslední →';
+$lang['Pagination.next'] = 'Další';
+$lang['Pagination.prev'] = 'Předchozí';
+
