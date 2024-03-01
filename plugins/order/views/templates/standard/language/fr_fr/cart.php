6ca57ff3cc67ccf3ce1511e837cb896199c6d932
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:49 -0800

    Updated fr_fr language files

---
+++ plugins/order/views/templates/standard/language/fr_fr/cart.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cart
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cart.index.header_cart'] = 'Ordonnance de réexamen';
+$lang['Cart.index.table_description'] = 'Description';
+$lang['Cart.index.table_term'] = 'Durée';
+$lang['Cart.index.table_qty'] = 'Quantité';
+$lang['Cart.index.table_price'] = 'Prix';
+$lang['Cart.index.checkout_btn'] = 'Sortie de caisse';
+$lang['Cart.index.order_more_btn'] = 'Commander plus d\'articles';
+$lang['Cart.index.edit_item'] = 'Editer';
+$lang['Cart.index.remove_item'] = 'Retirer';
+$lang['Cart.index.empty_cart'] = 'Votre panier est vide. Commencez par <a href="%1$s">choisir un plan"</a>';
+
