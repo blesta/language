0f4a63a9724191576d1e6c1a4e24dd9a17bfe765
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:41 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/taxes.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Taxes
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Taxes.!error.company_id.exists'] = 'ID d\'entreprise non valide.';
+$lang['Taxes.!error.level.format'] = 'Le niveau d\'imposition doit être un nombre.';
+$lang['Taxes.!error.level.length'] = 'La longueur du niveau d\'imposition ne doit pas dépasser 2 caractères.';
+$lang['Taxes.!error.name.length'] = 'La longueur du nom de la taxe ne doit pas dépasser 64 caractères.';
+$lang['Taxes.!error.amount.format'] = 'Le montant doit être un nombre.';
+$lang['Taxes.!error.amount.positive'] = 'Le montant doit être positif.';
+$lang['Taxes.!error.type.format'] = 'Type de taxe non valide.';
+$lang['Taxes.!error.country.valid'] = 'Le pays n\'est pas un code de pays ISO 3166-1 valide.';
+$lang['Taxes.!error.state.valid'] = 'State n\'est pas un code de subdivision ISO 3166-2 valide.';
+$lang['Taxes.!error.status.format'] = 'Statut non valide.';
+$lang['Taxes.!error.tax_id.exists'] = 'Numéro d\'identification fiscale non valide.';
+$lang['Taxes.getTaxStatus.active'] = 'Actif';
+$lang['Taxes.getTaxStatus.inactive'] = 'Inactif';
+$lang['Taxes.getTaxTypes.inclusive_calculated'] = 'Inclusif';
+$lang['Taxes.getTaxTypes.inclusive'] = 'Inclusif (additif)';
+$lang['Taxes.getTaxTypes.exclusive'] = 'Exclusif';
+
