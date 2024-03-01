6d040bb434169ebde679122bd3aa6cb342890b84
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:10 -0800

    Updated it_it language files

---
+++ language/it_it/module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Module.!error.general'] = 'Si è verificato un errore durante l\'elaborazione della richiesta con il modulo.';
+$lang['Module.!error.unsupported'] = 'Il modulo non supporta questa azione.';
+$lang['Module.!error.invalid'] = 'È stata ricevuta una risposta API non valida o non riconosciuta.';
+
