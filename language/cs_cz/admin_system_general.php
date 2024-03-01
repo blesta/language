c6b3d1edbdf093e077c913c05518a3c1525752d7
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:37 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_system_general.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Admin System General
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemGeneral.edittype.field_typesubmit'] = 'Upravit typ platby';
+$lang['AdminSystemGeneral.edittype.field_is_lang'] = 'Použití Definice jazyka';
+$lang['AdminSystemGeneral.edittype.field_type'] = 'Typ';
+$lang['AdminSystemGeneral.edittype.field_name'] = 'Název';
+$lang['AdminSystemGeneral.edittype.boxtitle_edittype'] = 'Upravit typ platby';
+$lang['AdminSystemGeneral.edittype.page_title'] = 'Nastavení > Systém > Obecné > Upravit typ platby';
+$lang['AdminSystemGeneral.addtype.field_typesubmit'] = 'Vytvořit typ platby';
+$lang['AdminSystemGeneral.addtype.field_is_lang'] = 'Použití Definice jazyka';
+$lang['AdminSystemGeneral.addtype.field_type'] = 'Typ';
+$lang['AdminSystemGeneral.addtype.field_name'] = 'Název';
+$lang['AdminSystemGeneral.addtype.boxtitle_addtype'] = 'Vytvořit typ platby';
+$lang['AdminSystemGeneral.addtype.page_title'] = 'Nastavení > Systém > Obecné > Vytvořit typ platby';
+$lang['AdminSystemGeneral.!paymenttypes.type'] = 'Pokud je nastavena hodnota debetní, jsou transakce využívající tento typ platby považovány za příjmové, zatímco kreditní jsou nepříjmové.';
+$lang['AdminSystemGeneral.!paymenttypes.is_lang'] = 'Toto políčko zaškrtněte pouze v případě, že jste přidali definici jazyka pro tento typ platby do vlastního jazykového souboru.';
+$lang['AdminSystemGeneral.paymenttypes.no_results'] = 'Neexistují žádné typy plateb.';
+$lang['AdminSystemGeneral.paymenttypes.text_no'] = 'Ne';
+$lang['AdminSystemGeneral.paymenttypes.text_yes'] = 'Ano';
+$lang['AdminSystemGeneral.paymenttypes.modal_delete'] = 'Odstranění tohoto typu platby způsobí, že všechny transakce, které tento typ platby používají, budou nastaveny na "ostatní". Opravdu chcete tento typ platby odstranit?';
+$lang['AdminSystemGeneral.paymenttypes.option_delete'] = 'Odstranit';
+$lang['AdminSystemGeneral.paymenttypes.option_edit'] = 'Upravit';
+$lang['AdminSystemGeneral.paymenttypes.heading_options'] = 'Možnosti';
+$lang['AdminSystemGeneral.paymenttypes.heading_is_lang'] = 'Užití Definice jazyka';
+$lang['AdminSystemGeneral.paymenttypes.heading_type'] = 'Typ';
+$lang['AdminSystemGeneral.paymenttypes.heading_name'] = 'Název';
+$lang['AdminSystemGeneral.paymenttypes.boxtitle_types'] = 'Typy plateb';
+$lang['AdminSystemGeneral.paymenttypes.categorylink_addtype'] = 'Vytvořit typ platby';
+$lang['AdminSystemGeneral.paymenttypes.page_title'] = 'Nastavení > Systém > Obecné > Typy plateb';
+$lang['AdminSystemGeneral.license.field.licensesubmit'] = 'Aktualizace nastavení';
+$lang['AdminSystemGeneral.license.field.license_key'] = 'Licenční klíč';
+$lang['AdminSystemGeneral.license.boxtitle_license'] = 'Licenční klíč';
+$lang['AdminSystemGeneral.license.page_title'] = 'Nastavení > Systém > Obecné > Licenční klíč';
+$lang['AdminSystemGeneral.maintenance.field.maintenancesubmit'] = 'Aktualizace nastavení';
+$lang['AdminSystemGeneral.maintenance.field.maintenance_reason'] = 'Důvod údržby';
+$lang['AdminSystemGeneral.maintenance.field.maintenance_mode'] = 'Povolení režimu údržby';
+$lang['AdminSystemGeneral.maintenance.boxtitle_maintenance'] = 'Údržba';
+$lang['AdminSystemGeneral.maintenance.page_title'] = 'Nastavení > Systém > Obecné > Údržba';
+$lang['AdminSystemGeneral.geoip.field_geoipsubmit'] = 'Aktualizace nastavení';
+$lang['AdminSystemGeneral.geoip.field_geoip_enabled'] = 'Povolení služby GeoIP';
+$lang['AdminSystemGeneral.geoip.text_database_not_exists'] = '%1$s neexistuje.';
+$lang['AdminSystemGeneral.geoip.text_database_exists'] = '%1$s existuje.';
+$lang['AdminSystemGeneral.geoip.text_geolite_step_5'] = 'Nahrajte tento soubor do své instalace systému Blesta na níže uvedenou cestu.';
+$lang['AdminSystemGeneral.geoip.text_geolite_step_4'] = 'V části "GeoIP2 / GeoLite2 > Stáhnout soubory" stáhněte binární soubor GeoLite2-City.';
+$lang['AdminSystemGeneral.geoip.text_geolite_step_3'] = 'Přihlaste se ke svému účtu';
+$lang['AdminSystemGeneral.geoip.text_geolite_step_2'] = 'K nastavení hesla použijte uvítací e-mail';
+$lang['AdminSystemGeneral.geoip.text_geolite_step_1'] = 'Zaregistrujte si účet MaxMind';
+$lang['AdminSystemGeneral.geoip.text_geolite'] = 'GeoIP vyžaduje binární databázi GeoLite City, kterou lze stáhnout z vašeho účtu na adrese <a target="_blank" href="%1$s">%1$s</a>. Soubor je třeba rozbalit a nahrát na adresu:';
+$lang['AdminSystemGeneral.geoip.text_setup'] = 'Zde lze povolit funkci GeoIP, čímž Blesta získá funkci lokalizačních služeb GeoIP. Její povolení umožní některým funkcím využívat polohové služby.';
+$lang['AdminSystemGeneral.geoip.boxtitle_geoip'] = 'GeoIP';
+$lang['AdminSystemGeneral.geoip.page_title'] = 'Nastavení > Systém > Obecné > Nastavení GeoIP';
+$lang['AdminSystemGeneral.basic.text_days'] = 'Dny';
+$lang['AdminSystemGeneral.basic.text_day'] = 'Den';
+$lang['AdminSystemGeneral.basic.text_no_log'] = 'Nikdy neotáčejte protokol';
+$lang['AdminSystemGeneral.basic.text_unwritable'] = 'Nelze zapisovat';
+$lang['AdminSystemGeneral.basic.text_writable'] = 'Zapisovatelné';
+$lang['AdminSystemGeneral.basic.text_docroot'] = 'Očekávání "%1$s"';
+$lang['AdminSystemGeneral.basic.field.basicsubmit'] = 'Aktualizace nastavení';
+$lang['AdminSystemGeneral.basic.field.behind_proxy'] = 'Moje instalace je za proxy serverem nebo vyrovnávačem zátěže.';
+$lang['AdminSystemGeneral.basic.field.log_days'] = 'Zásady rotace';
+$lang['AdminSystemGeneral.basic.field.log_dir'] = 'Adresář záznamů';
+$lang['AdminSystemGeneral.basic.field.uploads_dir'] = 'Adresář pro nahrávání';
+$lang['AdminSystemGeneral.basic.field.temp_dir'] = 'Adresář dočasných uživatelů';
+$lang['AdminSystemGeneral.basic.field.root_web_dir'] = 'Kořenový webový adresář';
+$lang['AdminSystemGeneral.basic.boxtitle_basic'] = 'Základní nastavení';
+$lang['AdminSystemGeneral.basic.page_title'] = 'Nastavení > Systém > Obecné > Základní nastavení';
+$lang['AdminSystemGeneral.!tooltip.maintenance_reason'] = 'Tento důvod údržby se zobrazí uživatelům, kteří nejsou zaměstnanci a kteří přistupují k systému, když je povolen režim údržby.';
+$lang['AdminSystemGeneral.!tooltip.maintenance_mode'] = 'V režimu údržby mohou systém používat pouze uživatelé z řad zaměstnanců. Všichni ostatní uživatelé budou přesměrováni na přihlašovací stránku a zobrazí se jim důvod údržby.';
+$lang['AdminSystemGeneral.!tooltip.behind_proxy'] = 'Pokud je tato volba zaškrtnuta, bude Blesta předpokládat, že se nachází za proxy serverem, a bude určovat IP adresy podle hlavičky x-forwarded-for poskytnuté proxy serverem. Toto nastavení byste měli zaškrtnout pouze v případě, že lze hlavičce x-forwarded-for důvěřovat.';
+$lang['AdminSystemGeneral.!tooltip.log_days'] = 'Zásady rotace nastavují dobu uchovávání většiny dat protokolu společnosti. V konfiguračním souboru systému lze nastavit další nastavení uchovávání protokolů.';
+$lang['AdminSystemGeneral.!tooltip.log_dir'] = 'Tato hodnota představuje úplnou cestu k serveru, kam má Blesta zapisovat soubory protokolu. Do tohoto adresáře musí mít možnost zapisovat webový uživatel serveru a uživatel cronu.';
+$lang['AdminSystemGeneral.!tooltip.uploads_dir'] = 'Tato hodnota představuje úplnou cestu k serveru, kam má Blesta zapsat nahrané soubory. Do tohoto adresáře musí mít možnost zapisovat webový uživatel serveru a uživatel cronu.';
+$lang['AdminSystemGeneral.!tooltip.temp_dir'] = 'Tato hodnota představuje úplnou cestu k serveru, kam má Blesta zapisovat dočasné soubory. Do tohoto adresáře musí mít možnost zapisovat webový uživatel serveru a uživatel cronu.';
+$lang['AdminSystemGeneral.!tooltip.root_web_dir'] = 'Tato hodnota představuje úplnou cestu serveru ke kořenovému adresáři dokumentu webového serveru (např. /home/user/public_html/). Nemusí se nutně jednat o cestu k adresáři, pod kterým je nainstalována aplikace Blesta.';
+$lang['AdminSystemGeneral.!tooltip.license_key'] = 'Toto je váš licenční klíč Blesta. Pokud obdržíte nový licenční klíč, zadejte jej zde.';
+$lang['AdminSystemGeneral.!error.geoip_mbstring_required'] = 'Pro tuto funkci je vyžadováno rozšíření mbstring.';
+$lang['AdminSystemGeneral.!success.deletetype_deleted'] = 'Typ platby "%1$s" byl úspěšně odstraněn!';
+$lang['AdminSystemGeneral.!success.edittype_updated'] = 'Typ platby "%1$s" byl úspěšně aktualizován!';
+$lang['AdminSystemGeneral.!success.addtype_created'] = 'Typ platby "%1$s" byl úspěšně vytvořen!';
+$lang['AdminSystemGeneral.!success.license_updated'] = 'Váš licenční klíč byl úspěšně aktualizován!';
+$lang['AdminSystemGeneral.!success.maintenance_updated'] = 'Nastavení údržby bylo úspěšně aktualizováno!';
+$lang['AdminSystemGeneral.!success.geoip_updated'] = 'Nastavení GeoIP bylo úspěšně aktualizováno!';
+$lang['AdminSystemGeneral.!success.basic_updated'] = 'Základní nastavení bylo úspěšně aktualizováno!';
+
