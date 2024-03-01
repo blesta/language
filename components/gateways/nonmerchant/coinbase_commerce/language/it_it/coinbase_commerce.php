a79ea7625f2c462d887d3dbd51ffcaeaff6a0730
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:15 -0800

    Updated it_it language files

---
+++ components/gateways/nonmerchant/coinbase_commerce/language/it_it/coinbase_commerce.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Coinbase Commerce
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CoinbaseCommerce.buildprocess.submit'] = 'Pagare con Coinbase';
+$lang['CoinbaseCommerce.webhook_note'] = 'Prima di iniziare a utilizzare questo gateway, è necessario configurare il seguente url come Webhook nel proprio account Coinbase Commerce e impostare il Webhook Secret.';
+$lang['CoinbaseCommerce.webhook'] = 'Webhook di Coinbase Commerce';
+$lang['CoinbaseCommerce.meta.webhook_secret'] = 'Segreto Webhook';
+$lang['CoinbaseCommerce.meta.api_key'] = 'Chiave API';
+$lang['CoinbaseCommerce.!error.webhook_secret.valid'] = 'Segreto webhook non valido.';
+$lang['CoinbaseCommerce.!error.api_key.valid'] = 'Chiave API non valida.';
+$lang['CoinbaseCommerce.description'] = 'Coinbase Commerce consente di integrare i pagamenti in criptovaluta nella vostra attività e di permettere ai clienti di pagarvi direttamente dal loro computer o dispositivo mobile utilizzando la blockchain.';
+$lang['CoinbaseCommerce.name'] = 'Commercio Coinbase';
+
