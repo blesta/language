d2a22940b3ff52ff620856faaad3998ab11fd667
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:24 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/stripe_gateway/language/es_es/stripe_gateway.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Stripe Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Stripe_gateway.!error.auth'] = 'La puerta de enlace no ha podido autenticarse.';
+$lang['Stripe_gateway.!error.api_key.empty'] = 'Introduzca una clave API.';
+$lang['Stripe_gateway.!error.json_required'] = 'La extensión JSON es necesaria para esta pasarela.';
+$lang['Stripe_gateway.name'] = 'Raya';
+$lang['Stripe_gateway.description'] = 'Esta es la antigua pasarela de Stripe que utiliza Stripe Checkout. Requiere que tu cuenta de Stripe esté actualizada para "Procesar pagos de forma insegura". Le recomendamos que utilice la pasarela de pagos de Stripe en Blesta.';
+$lang['Stripe_gateway.api_key'] = 'Clave secreta API';
+$lang['Stripe_gateway.tooltip_api'] = 'Su clave secreta de la API es específica para el modo activo o de prueba. Asegúrese de que está utilizando la clave correcta.';
+$lang['Stripe_gateway.stored'] = 'Almacenar la información de la tarjeta fuera del sitio';
+$lang['Stripe_gateway.tooltip_stored'] = 'Marque esta casilla para almacenar la información de la tarjeta de la cuenta de pago con Stripe en lugar de dentro de Blesta.';
+$lang['Stripe_gateway.charge_description'] = 'Cargo por %1$s';
+
