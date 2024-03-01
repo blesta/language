aca53563d895006c130302f5c255a4efad0fed10
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:33 -0800

    Updated cs_cz language files

---
+++ plugins/reassign_pricing/language/cs_cz/reassign_pricing_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Reassign Pricing Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReassignPricingPlugin.action_staff_client.index'] = 'Změna přiřazení cen';
+$lang['ReassignPricingPlugin.description'] = 'Umožňuje aktualizovat ceny služeb podle cen z jiných balíčků, které používají stejný modul.';
+$lang['ReassignPricingPlugin.name'] = 'Změna přiřazení cen';
+
