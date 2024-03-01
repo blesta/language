1565a6fc90c5599e4ab93e1113be6eb214da7fcb
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:56 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/deliver_invoices.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Deliver Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.deliver_invoices.method_postalmethods'] = 'PostalMethods';
+$lang['Automation.task.deliver_invoices.method_interfax'] = 'InterFax';
+$lang['Automation.task.deliver_invoices.method_email'] = 'E-Mail';
+$lang['Automation.task.deliver_invoices.none'] = 'Die Zustellung von Rechnungen ist nicht vorgesehen.';
+$lang['Automation.task.deliver_invoices.delivery_success_one'] = 'Erfolgreich 1 Rechnung an Kunde #%1$s über %2$s geliefert.';
+$lang['Automation.task.deliver_invoices.delivery_success'] = 'Erfolgreich %3$s Rechnungen an Kunde #%1$s über %2$s geliefert.';
+$lang['Automation.task.deliver_invoices.delivery_error_one'] = 'Konnte 1 Rechnung an Kunde #%1$s über %2$s nicht zustellen wegen Fehler: %3$s';
+$lang['Automation.task.deliver_invoices.delivery_error'] = 'Konnte %3$s Rechnungen an den Kunden #%1$s über %2$s aufgrund eines Fehlers nicht zustellen: %4$s';
+$lang['Automation.task.deliver_invoices.completed'] = 'Die Aufgabe Rechnungen zustellen wurde abgeschlossen.';
+$lang['Automation.task.deliver_invoices.attempt'] = 'Versuch, Rechnungen zuzustellen, die für die Zustellung vorgesehen sind.';
+
