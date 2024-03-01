c8262e5930a909ad7b00619b3124ed56d6bf8f12
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:37 -0800

    Updated fr_fr language files

---
+++ core/Util/Filters/language/fr_fr/package_option_group_filters.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Package Option Group Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.package_option_group_filters.field_options'] = 'Options';
+$lang['Util.filters.package_option_group_filters.options.hidden'] = 'Afficher les groupes d\'options cachés du paquet';
+
