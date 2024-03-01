e2e30e4f5d4bcd320b6bbd2d2c005ce10de34b9b
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:13 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/order_plugin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Order Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderPlugin.permission.admin_main_widget'] = 'Widget profilu klienta';
+$lang['OrderPlugin.permission.admin_main'] = 'Objednávky';
+$lang['OrderPlugin.permission.admin_payment_methods_delete'] = 'Odstranění způsobu platby';
+$lang['OrderPlugin.permission.admin_payment_methods_edit'] = 'Upravit způsob platby';
+$lang['OrderPlugin.permission.admin_payment_methods_add'] = 'Přidat způsob platby';
+$lang['OrderPlugin.permission.admin_payment_methods'] = 'Způsoby platby';
+$lang['OrderPlugin.permission.admin_payouts_decline'] = 'Odmítnutí výplaty';
+$lang['OrderPlugin.permission.admin_payouts_approve'] = 'Schválení výplaty';
+$lang['OrderPlugin.permission.admin_payouts_edit'] = 'Upravit výplatu';
+$lang['OrderPlugin.permission.admin_payouts'] = 'Výplaty';
+$lang['OrderPlugin.permission.admin_affiliates_settings'] = 'Nastavení partnerů';
+$lang['OrderPlugin.permission.admin_affiliates_deactivate'] = 'Deaktivace pobočky';
+$lang['OrderPlugin.permission.admin_affiliates_activate'] = 'Aktivace partnerského programu';
+$lang['OrderPlugin.permission.admin_affiliates_update'] = 'Aktualizace Affiliate';
+$lang['OrderPlugin.permission.admin_affiliates_add'] = 'Přidat partnerskou společnost';
+$lang['OrderPlugin.permission.admin_affiliates'] = 'Partneři';
+$lang['AdminCompanyPlugins.settings.event.domains.updatepricingafter'] = 'Tato událost se spustí po aktualizaci cen TLD.';
+$lang['AdminCompanyPlugins.settings.event.domains.updatedomainscompanysettingsafter'] = 'Tato událost se spustí po aktualizaci nastavení společnosti Správce domény.';
+$lang['AdminCompanyPlugins.settings.event.domains.disable'] = 'Tato událost se spustí, když je doména nejvyšší úrovně zakázána ve Správci domén.';
+$lang['AdminCompanyPlugins.settings.event.domains.enable'] = 'Tato událost se spustí, když je TLD povolena ze Správce domén.';
+$lang['AdminCompanyPlugins.settings.event.domains.delete'] = 'Tato událost se spustí, když je doména nejvyšší úrovně odstraněna ze Správce domén.';
+$lang['OrderPlugin.card_client.orders'] = 'Objednávky';
+$lang['OrderPlugin.event_getsearchoptions.orders'] = 'Vyhledávání objednávek';
+$lang['OrderPlugin.cron.process_abandoned_orders_desc'] = 'Odesílání upomínkových e-mailů, provádění storen a deaktivace klientů v případě opuštěných objednávek.';
+$lang['OrderPlugin.cron.process_abandoned_orders_name'] = 'Zpracování opuštěných objednávek';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_desc'] = 'Vyhodnocuje, zda čekající partnerská doporučení dosáhla splatnosti, a pokud ano, aktualizuje je a částky výplat.';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_name'] = 'Partnerské objednávky';
+$lang['OrderPlugin.cron.affiliate_monthly_report_desc'] = 'Měsíční e-mailová zpráva o pobočce bude zaslána 1. dne v měsíci za předchozí měsíc.';
+$lang['OrderPlugin.cron.affiliate_monthly_report_name'] = 'Partnerská měsíční zpráva';
+$lang['OrderPlugin.cron.accept_paid_orders_desc'] = 'Automaticky přijímá placené čekající objednávky, pokud to formulář objednávky umožňuje.';
+$lang['OrderPlugin.cron.accept_paid_orders_name'] = 'Přijímání zaplacených nevyřízených objednávek';
+$lang['OrderPlugin.client_orders.name'] = 'Historie objednávek';
+$lang['OrderPlugin.action_staff_client.affiliates'] = 'Správa partnerských společností';
+$lang['OrderPlugin.client_affiliates.name'] = 'Partneři';
+$lang['OrderPlugin.client.name'] = 'Objednávka';
+$lang['OrderPlugin.admin_affiliates.payment_methods'] = 'Affiliate platební metody';
+$lang['OrderPlugin.admin_affiliates.payouts'] = 'Partnerské výplaty';
+$lang['OrderPlugin.admin_affiliates.name'] = 'Partneři';
+$lang['OrderPlugin.admin_forms.name'] = 'Objednávkové formuláře';
+$lang['OrderPlugin.admin_main.name'] = 'Objednávky';
+$lang['OrderPlugin.name'] = 'Systém objednávek';
+
