522fbac29292b2ba0bda52a13f7aff68cef3ab2b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:51 -0800

    Updated fr_fr language files

---
+++ components/modules/generic_domains/language/fr_fr/generic_domains.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Generic Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GenericDomains.name'] = 'Domaines génériques';
+$lang['GenericDomains.description'] = 'Un module générique pour l\'enregistrement des domaines.';
+$lang['GenericDomains.module_row'] = 'Rangée de modules';
+$lang['GenericDomains.module_row_plural'] = 'Rangées de modules';
+$lang['GenericDomains.domain.domain'] = 'Domaine';
+$lang['GenericDomains.transfer.domain'] = 'Domaine';
+$lang['GenericDomains.transfer.transfer_key'] = 'Code EPP';
+$lang['GenericDomains.package_fields.tld_options'] = 'TLDs';
+$lang['GenericDomains.manage.text'] = 'Les membres du personnel peuvent recevoir des copies BCC des courriels de création, de suspension et d\'annulation de service si leur groupe de personnel a défini ces notifications BCC et si le membre du personnel s\'y abonne dans sa section [Mes infos] -> [Notifications].';
+
