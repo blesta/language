67a1245d93beb856d26d1aa64837626181e755c0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:04 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/api_keys.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Api Keys
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ApiKeys.!error.key.generate'] = 'La clé n\'a pas pu être générée.';
+$lang['ApiKeys.!error.company_id.exists'] = 'L\'entreprise indiquée n\'existe pas.';
+$lang['ApiKeys.!error.user.format'] = 'Veuillez saisir un nom d\'utilisateur API entre 3 et 64 caractères.';
+$lang['ApiKeys.!error.user.valid'] = 'L\'utilisateur ou la clé de l\'API semble être invalide.';
+$lang['ApiKeys.!error.user.unique'] = 'L\'utilisateur API existe déjà pour l\'entreprise donnée.';
+
