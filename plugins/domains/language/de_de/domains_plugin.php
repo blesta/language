fa3f2cc9c8bdcc9621707cd257325fc5b73f8a4e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:36 -0800

    Updated de_de language files

---
+++ plugins/domains/language/de_de/domains_plugin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Domains Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsPlugin.upgrade.domains_id_protection_option_group'] = 'Alle Domänennamen müssen gemäß den ICANN-Vorschriften über gültige Kontaktinformationen verfügen. Mit dieser Option werden diese Angaben vor dem öffentlichen Whois verborgen, so dass Ihre Informationen privat bleiben.';
+$lang['DomainsPlugin.upgrade.domains_email_forwarding_option_group'] = 'Erstellen Sie E-Mail-Adressen in Ihrer Domäne, die an eine externe E-Mail-Adresse weitergeleitet werden.';
+$lang['DomainsPlugin.upgrade.domains_dns_management_option_group'] = 'Verwalten Sie die DNS-Zone für Ihren Domainnamen über die von uns bereitgestellten Nameserver. Erstellen und bearbeiten Sie Einträge wie A, CNAME, MX, TXT und mehr für Ihre Domain.';
+$lang['DomainsPlugin.widget_client_home.main'] = 'Domains';
+$lang['DomainsPlugin.widget_staff_home.main'] = 'Domains';
+$lang['DomainsPlugin.enabled'] = 'Aktiviert';
+$lang['DomainsPlugin.id_protection.description'] = 'ID-Schutz';
+$lang['DomainsPlugin.id_protection.name'] = 'ID-Schutz';
+$lang['DomainsPlugin.dns_management.description'] = 'DNS-Verwaltung';
+$lang['DomainsPlugin.dns_management.name'] = 'DNS-Verwaltung';
+$lang['DomainsPlugin.email_forwarding.description'] = 'E-Mail-Weiterleitung';
+$lang['DomainsPlugin.email_forwarding.name'] = 'E-Mail-Weiterleitung';
+$lang['DomainsPlugin.tld_package_group.description'] = 'Eine Paketgruppe zum Verstecken und Verwalten aller TLD-Preispakete';
+$lang['DomainsPlugin.tld_package_group.name'] = 'TLDs Preispakete';
+$lang['DomainsPlugin.permission.admin_domains.configuration'] = 'Konfiguration';
+$lang['DomainsPlugin.permission.admin_domains.whois'] = 'Whois';
+$lang['DomainsPlugin.permission.admin_domains.registrars'] = 'Standesbeamte';
+$lang['DomainsPlugin.permission.admin_domains.tlds'] = 'TLD-Preise';
+$lang['DomainsPlugin.permission.admin_domains.browse'] = 'Domains';
+$lang['DomainsPlugin.permission.admin_domains'] = 'Domains';
+$lang['DomainsPlugin.card_client.getDomainCount'] = 'Domains';
+$lang['DomainsPlugin.nav_client.domains'] = 'Domains';
+$lang['DomainsPlugin.nav_client.services'] = 'Dienstleistungen';
+$lang['DomainsPlugin.nav_secondary_staff.domains'] = 'Domains';
+$lang['DomainsPlugin.nav_secondary_staff.domain_options'] = 'Bereich Optionen';
+$lang['DomainsPlugin.getCronTasks.domain_renewal_reminders_description'] = 'Senden Sie E-Mail-Erinnerungen für Domains, deren Verfallsdatum sich nähert';
+$lang['DomainsPlugin.getCronTasks.domain_renewal_reminders'] = 'Verfallserinnerungen senden';
+$lang['DomainsPlugin.getCronTasks.domain_term_change_description'] = 'Umstellung von Diensten mit einer Laufzeit von mehr als einem Jahr auf eine jährliche Laufzeit';
+$lang['DomainsPlugin.getCronTasks.domain_term_change'] = 'Domänenbezeichnung ändern';
+$lang['DomainsPlugin.getCronTasks.domain_tld_synchronization_description'] = 'Synchronisieren Sie die Pakete mit den TLD-Preisen aus ihrem Registrar-Modul';
+$lang['DomainsPlugin.getCronTasks.domain_tld_synchronization'] = 'Domänen-TLD-Synchronisierung';
+$lang['DomainsPlugin.getCronTasks.domain_synchronization_description'] = 'Synchronisieren Sie die Domain-Dienste mit dem Ablaufdatum ihres Registrar-Moduls';
+$lang['DomainsPlugin.getCronTasks.domain_synchronization'] = 'Domänensynchronisierung';
+$lang['DomainsPlugin.!error.module_id.exists'] = 'Ungültige Modul-ID.';
+$lang['DomainsPlugin.description'] = 'Dient zur Verwaltung verschiedener Aspekte der Domainverwaltung und des Verkaufs. Dieses Plugin befindet sich derzeit in der Beta-Phase.';
+$lang['DomainsPlugin.name'] = 'Bereichsleiter';
+
