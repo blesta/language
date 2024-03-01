b6a1f1d542f9b37ee113f34d2a99733ee40560a6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:07 -0800

    Updated it_it language files

---
+++ core/Automation/Tasks/Task/language/it_it/suspend_services.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Suspend Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.suspendservices.success'] = 'Il servizio #%1$s del cliente %2$s è stato sospeso.';
+$lang['Automation.task.suspendservices.error'] = 'Non è stato possibile sospendere il servizio #%1$s del client %2$s.';
+$lang['Automation.task.suspendservices.suspension_reason'] = 'Mancato pagamento';
+$lang['Automation.task.suspendservices.completed'] = 'L\'attività di sospensione del servizio è stata completata.';
+$lang['Automation.task.suspendservices.attempt'] = 'Tentativo di elaborare le sospensioni del servizio.';
+
