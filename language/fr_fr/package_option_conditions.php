927ca840941409190d533e3d99670a6134e7458e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:32 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/package_option_conditions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Package Option Conditions
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageOptionConditions.!error.id.exists'] = 'L\'ID de la condition de l\'option donnée n\'existe pas';
+$lang['PackageOptionConditions.!error.condition_set_id.exists'] = 'L\'ID de l\'ensemble de conditions de l\'option donnée n\'existe pas';
+$lang['PackageOptionConditions.!error.trigger_option_id.exists'] = 'L\'ID de l\'option de déclenchement donnée n\'existe pas';
+$lang['PackageOptionConditions.!error.operator.valid'] = 'L\'opérateur donné n\'est pas valide';
+$lang['PackageOptionConditions.!error.value.valid'] = 'La valeur donnée n\'est pas valide';
+$lang['PackageOptionConditions.!error.value_id.exists'] = 'La valeur ID donnée n\'existe pas';
+$lang['PackageOptionConditions.getoperators.in'] = 'En';
+$lang['PackageOptionConditions.getoperators.notin'] = 'Pas dans';
+$lang['PackageOptionConditions.getactions.hide'] = 'Cacher';
+$lang['PackageOptionConditions.getactions.show'] = 'Afficher';
+
