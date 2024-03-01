26ffd4375dfb1bc8a1fba4136536a5b77615e3ae
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:39 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/order_affiliates.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Order Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliates.!error.affiliate_id.exists'] = 'ID de afiliado no válido.';
+$lang['OrderAffiliates.!error.client_id.exists'] = 'ID de cliente no válido.';
+$lang['OrderAffiliates.!error.client_id.unique'] = 'El ID de cliente ya está asociado a otro afiliado.';
+$lang['OrderAffiliates.!error.code.empty'] = 'Introduzca un código de afiliado.';
+$lang['OrderAffiliates.!error.code.unique'] = 'Este código ya está ocupado por otro afiliado.';
+$lang['OrderAffiliates.!error.status.valid'] = 'Seleccione un estado de afiliación válido.';
+$lang['OrderAffiliates.!error.date_added.format'] = 'La fecha añadida tiene un formato no válido.';
+$lang['OrderAffiliates.!error.date_updated.format'] = 'La fecha actualizada está en un formato de fecha no válido.';
+$lang['OrderAffiliates.getStatuses.active'] = 'Activo';
+$lang['OrderAffiliates.getStatuses.inactive'] = 'Inactivo';
+
