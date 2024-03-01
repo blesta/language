cff932ba0ff69d8beed429f3d4d6acc46fa583c5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:35 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/payumoney/language/el_gr/payumoney.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Payumoney
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payumoney.!error.invalid_token.response'] = 'Το Payumoney απάντησε με ένα άκυρο κουπόνι.';
+$lang['Payumoney.!error.key.valid'] = 'Μη έγκυρο εμπορικό κλειδί.';
+$lang['Payumoney.!error.auth_header.valid'] = 'Παρακαλούμε εισάγετε μια έγκυρη επικεφαλίδα εξουσιοδότησης.';
+$lang['Payumoney.!error.merchant_salt.valid'] = 'Παρακαλώ εισάγετε ένα έγκυρο Salt.';
+$lang['Payumoney.!error.merchant_key.valid'] = 'Παρακαλούμε εισάγετε ένα έγκυρο εμπορικό κλειδί.';
+$lang['Payumoney.buildprocess.submit'] = 'Πληρώστε μέσω Payumoney';
+$lang['Payumoney.test_mode_note'] = 'Η ενεργοποίηση αυτής της επιλογής θα χρησιμοποιήσει το sandbox της Payumoney. Ενεργοποιήστε αυτήν την επιλογή μόνο εάν κάνετε δοκιμές με λογαριασμό Payumoney sandbox.';
+$lang['Payumoney.test_mode'] = 'Λειτουργία Sandbox';
+$lang['Payumoney.merchant_salt_note'] = 'το άλας του εμπόρου μπορεί να βρεθεί στο ταμπλό του Payumoney στην ενότητα Διαχείριση λογαριασμού -> Ο λογαριασμός μου -> Κλειδί-άλας εμπόρου.';
+$lang['Payumoney.merchant_key_note'] = 'το κλειδί εμπόρου μπορεί να βρεθεί στο ταμπλό του Payumoney στην ενότητα Διαχείριση λογαριασμού -> Ο λογαριασμός μου -> Κλειδί εμπόρου-Αλάτι.';
+$lang['Payumoney.auth_header_note'] = 'Η επικεφαλίδα εξουσιοδότησης βρίσκεται στο ταμπλό του Payumoney στην ενότητα Διαχείριση λογαριασμού -> Ο λογαριασμός μου -> Merchant Key-Salt.';
+$lang['Payumoney.auth_header'] = 'Κεφαλίδα εξουσιοδότησης';
+$lang['Payumoney.merchant_salt'] = 'Εμπορικό αλάτι';
+$lang['Payumoney.merchant_key'] = 'Κλειδί εμπόρου';
+$lang['Payumoney.description'] = 'Στην Ινδία, η PayU εξυπηρετεί περισσότερους από 4.50.000+ εμπόρους με πάνω από 100+ μεθόδους πληρωμής και είναι ο προτιμώμενος συνεργάτης πληρωμών για σχεδόν το 60% των εμπόρων ηλεκτρονικού εμπορίου.';
+$lang['Payumoney.name'] = 'Payumoney';
+$lang['Payumoney.!error.hash.valid'] = 'Μη έγκυρο hash.';
+
