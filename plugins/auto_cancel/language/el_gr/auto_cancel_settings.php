5e17db66a35db98e863b1463e030ee236899265c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:41 -0800

    Updated el_gr language files

---
+++ plugins/auto_cancel/language/el_gr/auto_cancel_settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Auto Cancel Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AutoCancelSettings.!error.cancel_days.greater'] = 'Ακύρωση υπηρεσιών Ημέρες μετά την αναστολή πρέπει να είναι μεγαλύτερες ή ίσες με τις Ημέρες ακύρωσης προγράμματος μετά την αναστολή';
+$lang['AutoCancelSettings.!error.cancel_days.valid'] = 'Ακύρωση υπηρεσιών Ημέρες μετά την αναστολή δεν ισχύει.';
+$lang['AutoCancelSettings.!error.schedule_days.valid'] = 'Το πρόγραμμα Ακύρωση ημερών μετά την αναστολή δεν ισχύει.';
+
