ea69c1f6876a022c68dd42f1cac5b018324a161a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:55 -0800

    Updated de_de language files

---
+++ plugins/mass_mailer/language/de_de/config_export.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Config Export
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Config.export.module_label'] = 'Modulreihe';
+$lang['Config.export.module_name'] = 'Modul';
+$lang['Config.export.service_date_renews'] = 'Datum der Verlängerung';
+$lang['Config.export.service_name'] = 'Dienst';
+$lang['Config.export.package_name'] = 'Paket';
+$lang['Config.export.date_added'] = 'Datum hinzugefügt';
+$lang['Config.export.email'] = 'E-Mail';
+$lang['Config.export.company'] = 'Unternehmen';
+$lang['Config.export.last_name'] = 'Nachname';
+$lang['Config.export.first_name'] = 'Vorname';
+$lang['Config.export.id_code'] = 'Kunden-ID';
+
