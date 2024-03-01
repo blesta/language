b47cb4e0bf9d9d48b477dc27af82f16d2c9eb2a2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:01 -0800

    Updated el_gr language files

---
+++ plugins/support_manager/language/el_gr/support_manager_tickets.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Support Manager Tickets
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerTickets.!error.code.format'] = 'Ο κωδικός εισιτηρίου πρέπει να περιέχει μόνο ψηφία.';
+$lang['SupportManagerTickets.!error.department_id.exists'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο τμήμα.';
+$lang['SupportManagerTickets.!error.staff_id.exists'] = 'Μη έγκυρο αναγνωριστικό προσωπικού.';
+$lang['SupportManagerTickets.!error.contact_id.exists'] = 'Η επαφή που έχει επιλεγεί για την απάντηση σε αυτό το εισιτήριο δεν υπάρχει.';
+$lang['SupportManagerTickets.!error.contact_id.valid'] = 'Η επαφή δεν μπορεί να απαντήσει στο εισιτήριο χωρίς έγκυρο πελάτη.';
+$lang['SupportManagerTickets.!error.by_staff_id.exists'] = 'Το μέλος του προσωπικού που έχει οριστεί να εκτελεί την επεξεργασία δεν υπάρχει.';
+$lang['SupportManagerTickets.!error.service_id.exists'] = 'Η υπηρεσία που έχει επιλεγεί για αυτό το εισιτήριο δεν υπάρχει.';
+$lang['SupportManagerTickets.!error.service_id.belongs'] = 'Η επιλεγμένη υπηρεσία είναι άκυρη.';
+$lang['SupportManagerTickets.!error.client_id.exists'] = 'Μη έγκυρο αναγνωριστικό πελάτη.';
+$lang['SupportManagerTickets.!error.client_id.set'] = 'Το εισιτήριο ανήκει σε άλλον πελάτη και ο εκχωρημένος πελάτης δεν μπορεί να αλλάξει.';
+$lang['SupportManagerTickets.!error.email.format'] = 'Παρακαλούμε εισάγετε μια έγκυρη διεύθυνση ηλεκτρονικού ταχυδρομείου.';
+$lang['SupportManagerTickets.!error.summary.empty'] = 'Παρακαλώ εισάγετε μια περίληψη.';
+$lang['SupportManagerTickets.!error.summary.length'] = 'Το μήκος της περίληψης δεν μπορεί να υπερβαίνει τους 255 χαρακτήρες.';
+$lang['SupportManagerTickets.!error.priority.format'] = 'Παρακαλούμε επιλέξτε μια έγκυρη προτεραιότητα.';
+$lang['SupportManagerTickets.!error.status.format'] = 'Παρακαλούμε επιλέξτε μια έγκυρη κατάσταση.';
+$lang['SupportManagerTickets.!error.status.trash'] = 'Ένα κατεστραμμένο εισιτήριο δεν μπορεί να επεξεργαστεί.';
+$lang['SupportManagerTickets.!error.date_added.format'] = 'Η ημερομηνία προσθήκης του εισιτηρίου έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['SupportManagerTickets.!error.date_updated.format'] = 'Η ημερομηνία ενημέρωσης του εισιτηρίου έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['SupportManagerTickets.!error.date_closed.format'] = 'Η ημερομηνία κλεισίματος του εισιτηρίου έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['SupportManagerTickets.!error.ticket_id.exists'] = 'Μη έγκυρο αναγνωριστικό εισιτηρίου.';
+$lang['SupportManagerTickets.!error.type.format'] = 'Παρακαλούμε επιλέξτε έναν έγκυρο τύπο απάντησης.';
+$lang['SupportManagerTickets.!error.type.new_valid'] = 'Τα νέα εισιτήρια πρέπει να έχουν τύπο απάντησης \'reply\'.';
+$lang['SupportManagerTickets.!error.details.empty'] = 'Παρακαλούμε εισάγετε ορισμένες λεπτομέρειες σχετικά με αυτό το εισιτήριο.';
+$lang['SupportManagerTickets.!error.client_id.attached_to'] = 'Η απάντηση στο εισιτήριο μπορεί να μην προέρχεται από διαφορετικό πελάτη.';
+$lang['SupportManagerTickets.!error.replies.valid'] = 'Τουλάχιστον ένα αναγνωριστικό απάντησης εισιτηρίου είναι άκυρο ή έχουν επιλεγεί όλες οι απαντήσεις. Πρέπει να αφήσετε τουλάχιστον μία απάντηση να παραμείνει.';
+$lang['SupportManagerTickets.!error.replies.notes'] = 'Οι σημειώσεις απάντησης στο εισιτήριο δεν μπορούν να διαχωριστούν σε ξεχωριστό εισιτήριο χωρίς να περιλαμβάνουν επίσης απάντηση στο εισιτήριο.';
+$lang['SupportManagerTickets.!error.tickets.valid'] = 'Τουλάχιστον ένα επιλεγμένο εισιτήριο είναι άκυρο, κλειστό ή δεν ανήκει στον ίδιο πελάτη με το επιλεγμένο εισιτήριο.';
+$lang['SupportManagerTickets.!error.merge_into.itself'] = 'Το εισιτήριο δεν μπορεί να συγχωνευθεί με τον εαυτό του.';
+$lang['SupportManagerTickets.!error.tickets.service_matches'] = 'Τουλάχιστον ένα από τα εισιτήρια δεν μπόρεσε να εκχωρηθεί στη συγκεκριμένη υπηρεσία επειδή δεν ανήκει στον σχετικό πελάτη.';
+$lang['SupportManagerTickets.!error.tickets.department_matches'] = 'Τουλάχιστον ένα από τα εισιτήρια δεν μπόρεσε να αντιστοιχιστεί στο συγκεκριμένο τμήμα επειδή δεν ανήκει στην ίδια εταιρεία.';
+$lang['SupportManagerTickets.!error.ticket_ids[].exists'] = 'Τουλάχιστον ένα αναγνωριστικό εισιτηρίου είναι άκυρο.';
+$lang['SupportManagerTickets.!error.client_id.company'] = 'Τουλάχιστον ένα εισιτήριο δεν ανήκει στην ίδια εταιρεία με τον συγκεκριμένο πελάτη.';
+$lang['SupportManagerTickets.merge.reply'] = 'Αυτό το εισιτήριο συγχωνεύτηκε στο εισιτήριο #%1$s.';
+$lang['SupportManagerTickets.priority.emergency'] = 'Έκτακτης ανάγκης';
+$lang['SupportManagerTickets.priority.critical'] = 'Κρίσιμη';
+$lang['SupportManagerTickets.priority.high'] = 'Υψηλή';
+$lang['SupportManagerTickets.priority.medium'] = 'Μεσαίο';
+$lang['SupportManagerTickets.priority.low'] = 'Χαμηλή';
+$lang['SupportManagerTickets.status.open'] = 'Εν αναμονή απάντησης του προσωπικού';
+$lang['SupportManagerTickets.status.awaiting_reply'] = 'Αναμονή απάντησης πελάτη';
+$lang['SupportManagerTickets.status.in_progress'] = 'Σε εξέλιξη';
+$lang['SupportManagerTickets.status.on_hold'] = 'Σε αναμονή';
+$lang['SupportManagerTickets.status.closed'] = 'Κλειστό';
+$lang['SupportManagerTickets.status.trash'] = 'Σκουπίδια';
+$lang['SupportManagerTickets.type.reply'] = 'Απάντηση';
+$lang['SupportManagerTickets.type.note'] = 'Σημείωση';
+$lang['SupportManagerTickets.type.log'] = 'Ημερολόγιο';
+$lang['SupportManagerTickets.log.department_id'] = 'Το τμήμα έχει αλλάξει σε %1$s.';
+$lang['SupportManagerTickets.log.summary'] = 'Η περίληψη έχει ενημερωθεί.';
+$lang['SupportManagerTickets.log.priority'] = 'Η προτεραιότητα έχει αλλάξει σε %1$s.';
+$lang['SupportManagerTickets.log.status'] = 'Η κατάσταση έχει αλλάξει σε %1$s.';
+$lang['SupportManagerTickets.log.ticket_staff_id'] = 'Ανατέθηκε στο %1$s.';
+$lang['SupportManagerTickets.log.unassigned'] = 'Δεν έχει ανατεθεί';
+$lang['SupportManagerTickets.reassign_note'] = 'Αυτό το εισιτήριο ανατέθηκε εκ νέου στο %1$s. Προηγούμενες απαντήσεις πελατών εκχωρήθηκαν εκ νέου σε αυτόν τον πελάτη και ενδέχεται να έχουν γίνει από άλλο άτομο.';
+
