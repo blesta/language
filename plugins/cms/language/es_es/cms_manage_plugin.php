f31010c9bc18501f8a1af3a319665055ea7a583e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:54 -0800

    Updated es_es language files

---
+++ plugins/cms/language/es_es/cms_manage_plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Cms Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CmsManagePlugin.!success.plugin_updated'] = 'El Portal se ha actualizado correctamente.';
+$lang['CmsManagePlugin.index.page_title'] = 'Portal > Gestionar';
+$lang['CmsManagePlugin.index.boxtitle_portal'] = 'Portal';
+$lang['CmsManagePlugin.index.field_title'] = 'Índice Título de la página';
+$lang['CmsManagePlugin.index.field_tags'] = 'Etiquetas disponibles';
+$lang['CmsManagePlugin.index.field_content'] = 'Contenido de la página de índice';
+$lang['CmsManagePlugin.index.field_submit'] = 'Página de actualización';
+
