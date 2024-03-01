de73445404aae30850e06fbdbefed701b9d818e3
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:03 -0800

    Updated cs_cz language files

---
+++ core/Pricing/Modifier/Type/Description/language/cs_cz/domain.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Domain
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Domain.description.package.item.updated.prorate'] = 'Poměrná změna z %1$s na %2$s (%3$s - %4$s)';
+$lang['Domain.description.package.item.prorate'] = 'Poměrná část %1$s (%2$s - %3$s)';
+$lang['Domain.description.package.cancel'] = '%1$s Storno poplatek';
+$lang['Domain.description.package.setup'] = '%1$s Instalační poplatek';
+$lang['Domain.description.package.item.updated.date'] = 'Změna z %1$s na %2$s (%3$s - %4$s)';
+$lang['Domain.description.package.item.updated'] = 'Změna z %1$s na %2$s';
+$lang['Domain.description.package.item.date'] = '%1$s (%2$s - %3$s)';
+$lang['Domain.description.package.item'] = '%1$s';
+$lang['Domain.description.domain.item.updated.prorate'] = 'Poměrná změna z %1$s na %2$s - %3$s (%4$s - %5$s)';
+$lang['Domain.description.domain.item.prorate'] = 'Poměrná část %1$s - %2$s (%3$s - %4$s)';
+$lang['Domain.description.domain.cancel'] = '%1$s - %2$s Storno poplatek';
+$lang['Domain.description.domain.setup'] = '%1$s - %2$s Zřizovací poplatek';
+$lang['Domain.description.domain.item.updated.date.renewed'] = 'Změna z %1$s na %2$s - %3$s (%4$s - %5$s)';
+$lang['Domain.description.domain.item.updated.renewed'] = 'Změna z %1$s na %2$s - %3$s';
+$lang['Domain.description.domain.item.date.renewed'] = 'Obnovení domény - %2$s (%3$s - %4$s)';
+$lang['Domain.description.domain.item.renewed'] = 'Obnovení domény - %2$s';
+$lang['Domain.description.domain.item.updated.date.transferred'] = 'Změna z %1$s na %2$s - %3$s (%4$s - %5$s)';
+$lang['Domain.description.domain.item.updated.transferred'] = 'Změna z %1$s na %2$s - %3$s';
+$lang['Domain.description.domain.item.date.transferred'] = 'Přenos domény - %2$s (%3$s - %4$s)';
+$lang['Domain.description.domain.item.transferred'] = 'Přenos domény - %2$s';
+$lang['Domain.description.domain.item.updated.date'] = 'Změna z %1$s na %2$s - %3$s (%4$s - %5$s)';
+$lang['Domain.description.domain.item.updated'] = 'Změna z %1$s na %2$s - %3$s';
+$lang['Domain.description.domain.item.date'] = 'Registrace domény - %2$s (%3$s - %4$s)';
+$lang['Domain.description.domain.item'] = 'Registrace domény - %2$s';
+
