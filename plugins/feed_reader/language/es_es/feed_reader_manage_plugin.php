20e1e0cef6f13b15c26466aab8511924fe9c0318
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:13 -0800

    Updated es_es language files

---
+++ plugins/feed_reader/language/es_es/feed_reader_manage_plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Feed Reader Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FeedReaderManagePlugin.!success.feed_added'] = 'La alimentación se ha añadido correctamente.';
+$lang['FeedReaderManagePlugin.!success.feed_refreshed'] = 'El feed se ha actualizado correctamente.';
+$lang['FeedReaderManagePlugin.!success.feed_removed'] = 'La alimentación se retiró con éxito.';
+$lang['FeedReaderManagePlugin.index.page_title'] = 'Lector de feeds > Gestionar';
+$lang['FeedReaderManagePlugin.index.boxtitle_feedreader'] = 'Lector de feeds';
+$lang['FeedReaderManagePlugin.index.add_feed'] = 'Añadir Feed';
+$lang['FeedReaderManagePlugin.index.heading_url'] = 'URL';
+$lang['FeedReaderManagePlugin.index.heading_lastupdated'] = 'Última actualización';
+$lang['FeedReaderManagePlugin.index.heading_options'] = 'Opciones';
+$lang['FeedReaderManagePlugin.index.option_refresh'] = 'Actualizar';
+$lang['FeedReaderManagePlugin.index.option_remove'] = 'Eliminar';
+$lang['FeedReaderManagePlugin.index.no_feeds'] = 'No hay fuentes por defecto.';
+$lang['FeedReaderManagePlugin.index.text_confirm_delete'] = '¿De verdad borrar este feed?';
+$lang['FeedReaderManagePlugin.add.page_title'] = 'Lector de feeds > Añadir un feed';
+$lang['FeedReaderManagePlugin.add.boxtitle_feed'] = 'Añadir un feed';
+$lang['FeedReaderPlugin.add.field_url'] = 'URL';
+$lang['FeedReaderPlugin.add.submit_add'] = 'Añadir Feed';
+$lang['FeedReaderPlugin.add.submit_cancel'] = 'Cancelar';
+
