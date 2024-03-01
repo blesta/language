db6233c67e9f31ca91990ba5f51b74687fbb4db2
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:02 -0800

    Updated cs_cz language files

---
+++ core/Pricing/Modifier/Type/Description/language/cs_cz/discount.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Discount
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Discount.description.coupon.amount'] = 'Kupón %1$s';
+$lang['Discount.description.coupon.percent'] = 'Kupón %1$s (%2$s%%)';
+
