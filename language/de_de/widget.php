31f45de95f377a53613f4737a9a8cf57e9a39053
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:23 -0800

    Updated de_de language files

---
+++ language/de_de/widget.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Widget
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Widget.submit'] = 'Einreichen';
+$lang['Widget.toggle_filters'] = 'Filter umschalten';
+
