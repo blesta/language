cae752ebf112f0a641e2052d3d8df0752a66c516
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:56 -0800

    Updated cs_cz language files

---
+++ components/gateways/merchant/eway/language/cs_cz/eway.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Eway
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Eway.tooltip_test_mode'] = 'V testovacím režimu se použije testovací účet eWAY s testovacím číslem kreditní karty bez ohledu na to, co zadáte. Další informace o testovacím režimu v eWAY naleznete v příručce.';
+$lang['Eway.tooltip_developer_mode'] = 'Povolením této možnosti se transakce odešlou do prostředí sandboxu eWAY. Abyste mohli toto prostředí používat, musíte mít testovací účet sandboxu.';
+$lang['Eway.tooltip_refund_password'] = 'Zadejte heslo pro vrácení peněz, které jste si nastavili ve svém účtu eWAY. Nejedná se o heslo k účtu eWAY. Heslo pro vrácení peněz musí být nastaveno, aby bylo možné zpracovat vrácení peněz prostřednictvím eWAY.';
+$lang['Eway.test_mode'] = 'Testovací režim';
+$lang['Eway.developer_mode'] = 'Vývojářský režim';
+$lang['Eway.refund_password'] = 'Heslo pro vrácení peněz';
+$lang['Eway.customer_id'] = 'ID zákazníka';
+$lang['Eway.description'] = 'Společnost eWAY zaujímá 25 % australského online trhu a obchoduje v 5 zemích. Každoročně zpracovává miliardy dolarů v platbách po celém světě pro desítky tisíc podniků.';
+$lang['Eway.name'] = 'eWAY';
+$lang['Eway.!error.invalid_xml'] = 'Brána odpověděla neplatným XML.';
+$lang['Eway.!error.libxml_required'] = 'Pro tuto bránu jsou vyžadována rozšíření libxml a simplexml.';
+$lang['Eway.!error.refund_password.empty'] = 'Aby bylo možné zpracovávat náhrady prostřednictvím eWAY, je nutné nastavit heslo pro vrácení peněz.';
+$lang['Eway.!error.test_mode.valid'] = 'Testovací režim musí být nastaven na "true", pokud je zadán.';
+$lang['Eway.!error.developer_mode.valid'] = 'Režim pro vývojáře musí být nastaven na hodnotu "true", pokud je zadán.';
+$lang['Eway.!error.customer_id.length'] = 'ID zákazníka nesmí být delší než 8 znaků.';
+$lang['Eway.!error.customer_id.empty'] = 'Zadejte prosím své ID zákazníka eWAY.';
+
