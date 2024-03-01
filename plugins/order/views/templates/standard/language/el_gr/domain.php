3884010a77ccbcdf25077c219bec37d71d26c177
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:40 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/standard/language/el_gr/domain.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Domain
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Domain.lookup.title'] = 'Ελέγξτε τη διαθεσιμότητα του τομέα';
+$lang['Domain.lookup.domain_placeholder'] = 'yourdomain.com';
+$lang['Domain.lookup.show_all'] = 'Εμφάνιση όλων';
+$lang['Domain.lookup.hide_all'] = 'Απόκρυψη όλων';
+$lang['Domain.lookup.loading'] = 'Φόρτωση...';
+$lang['Domain.lookup.transfer_btn'] = 'Μεταφορά';
+$lang['Domain.lookup.lookup_btn'] = 'Ελέγξτε τη διαθεσιμότητα';
+$lang['Domain.lookup.other_btn'] = 'Παραγγελία άλλων αντικειμένων';
+$lang['Domain.lookup.domains_header_available'] = 'Συγχαρητήρια, ο συγκεκριμένος τομέας είναι διαθέσιμος.';
+$lang['Domain.lookup.domains_header_unavailable'] = 'Λυπούμαστε, αυτός ο τομέας δεν είναι διαθέσιμος.';
+$lang['Domain.lookup.domains_header_free'] = 'Ο τομέας μπορεί να είναι δωρεάν όταν συνδυάζεται με ένα πακέτο από οποιαδήποτε από τις ακόλουθες ομάδες πακέτων για τουλάχιστον %1$s: %2$s.';
+$lang['Domain.lookup.domains_domain_name'] = 'Όνομα τομέα';
+$lang['Domain.lookup.domains_status'] = 'Κατάσταση';
+$lang['Domain.lookup.domains_term'] = 'Όρος';
+$lang['Domain.lookup.domain_available'] = 'Διαθέσιμο';
+$lang['Domain.lookup.domain_unavailable'] = 'Δεν είναι διαθέσιμο';
+$lang['Domain.lookup.domain_eligible_free'] = 'Δικαίωμα για %1$s Δωρεάν';
+$lang['Domain.lookup.heading_tld'] = 'TLD';
+$lang['Domain.lookup.heading_price'] = 'Τιμή';
+$lang['Domain.lookup.heading_transfer_price'] = 'Τιμή μεταφοράς';
+$lang['Domain.lookup.heading_renew_price'] = 'Ανανέωση τιμής';
+$lang['Domain.lookup.term_day'] = '%1$s Ημέρα';
+$lang['Domain.lookup.term_days'] = '%1$s Ημέρες';
+$lang['Domain.lookup.term_week'] = '%1$s Εβδομάδα';
+$lang['Domain.lookup.term_weeks'] = '%1$s Εβδομάδες';
+$lang['Domain.lookup.term_month'] = '%1$s Μήνας';
+$lang['Domain.lookup.term_months'] = '%1$s Μήνες';
+$lang['Domain.lookup.term_year'] = '%1$s Έτος';
+$lang['Domain.lookup.term_years'] = '%1$s Έτη';
+$lang['Domain.lookup.order_btn'] = 'Επιλεγμένη παραγγελία';
+$lang['Domain.lookup.term'] = '%1$s %2$s @ %3$s';
+$lang['Domain.lookup.term_recurring'] = '%1$s %2$s @ %3$s (ανανεώνεται @ %4$s)';
+$lang['Domain.lookup.term_onetime'] = '%1$s @ %2$s';
+
