ebf811757fe4e9b174b656ccd6857a2f74e8a46d
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:28 -0800

    Updated cs_cz language files

---
+++ components/modules/multicraft/language/cs_cz/multicraft_module.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Multicraft Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MulticraftModule.!error.ips_in_use.match'] = 'Nastavte, zda je IP adresa aktuálně používána, na hodnotu 1 nebo 0.';
+$lang['MulticraftModule.!error.daemons.match'] = 'Pro každou nastavenou IP adresu nastavte ID démona.';
+$lang['MulticraftModule.!error.ips.match'] = 'Pro každou sadu démonů nastavte IP adresu.';
+$lang['MulticraftModule.!error.log_all.format'] = 'Zda se mají zaznamenávat všechny požadavky API, musí být nastaveno na 1 nebo 0.';
+$lang['MulticraftModule.!error.key.empty'] = 'Zadejte prosím svůj klíč API';
+$lang['MulticraftModule.!error.username.empty'] = 'Zadejte prosím své uživatelské jméno Multicraft';
+$lang['MulticraftModule.!error.panel_api_url.empty'] = 'Zadejte prosím adresu URL rozhraní API panelu Multicraft.';
+$lang['MulticraftModule.!error.panel_url.empty'] = 'Zadejte prosím adresu URL svého panelu Multicraft.';
+$lang['MulticraftModule.!error.server_name.empty'] = 'Zadejte prosím označení tohoto serveru.';
+$lang['MulticraftModule.!error.json.unavailable'] = 'Tento modul vyžaduje rozšíření JSON.';
+
