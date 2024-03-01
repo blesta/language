3956e259d919760e047c37852d47f5cb6e405125
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:18 -0800

    Updated el_gr language files

---
+++ components/gateways/merchant/braintree/language/el_gr/braintree.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Braintree
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Braintree.!error.environment'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο περιβάλλον.';
+$lang['Braintree.!error.private_key'] = 'Εισάγετε το ιδιωτικό σας κλειδί.';
+$lang['Braintree.!error.public_key'] = 'Εισάγετε το δημόσιο κλειδί σας.';
+$lang['Braintree.!error.merchant_id'] = 'Παρακαλούμε εισάγετε το αναγνωριστικό του εμπόρου σας.';
+$lang['Braintree.!error.live_mode.valid'] = 'Η λειτουργία "Live" πρέπει να είναι "true", αν δίνεται.';
+$lang['Braintree.environment.production'] = 'Παραγωγή';
+$lang['Braintree.environment.sandbox'] = 'Sandbox';
+$lang['Braintree.environment'] = 'Περιβάλλον';
+$lang['Braintree.live_mode'] = 'Λειτουργία Live';
+$lang['Braintree.private_key'] = 'Ιδιωτικό κλειδί';
+$lang['Braintree.public_key'] = 'Δημόσιο κλειδί';
+$lang['Braintree.merchant_id'] = 'Αναγνωριστικό εμπόρου';
+$lang['Braintree.description'] = 'Οι έμποροι σε περισσότερες από 45 χώρες/περιοχές παγκοσμίως μπορούν να δέχονται, να διαχωρίζουν και να επιτρέπουν πληρωμές σε περισσότερα από 130 νομίσματα χρησιμοποιώντας το Braintree.';
+$lang['Braintree.name'] = 'Braintree';
+
