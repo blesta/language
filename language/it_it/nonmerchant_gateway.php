24709d9b6a7c57672152f17c938599e5de2264be
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:12 -0800

    Updated it_it language files

---
+++ language/it_it/nonmerchant_gateway.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Nonmerchant Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['NonmerchantGateway.!error.general'] = 'Si è verificato un errore durante l\'elaborazione della richiesta con il gateway.';
+$lang['NonmerchantGateway.!error.unsupported'] = 'Il gateway non supporta questa azione.';
+$lang['NonmerchantGateway.!error.transaction_not_found'] = 'La transazione non è valida o non è più disponibile per l\'elaborazione dell\'azione.';
+$lang['NonmerchantGateway.!error.invalid'] = 'È stata ricevuta una risposta di pagamento non valida o non riconosciuta.';
+
