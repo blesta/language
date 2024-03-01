0dbb927bf7cefee6af380ae5b92fc98b54ea8060
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:39 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/widepay/language/es_es/widepay.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Widepay
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Widepay.name'] = 'Pago amplio';
+$lang['Widepay.wallet_id'] = 'Cartera ID';
+$lang['Widepay.wallet_token'] = 'Ficha de cartera';
+$lang['Widepay.allow_card_payment'] = 'Permitir pagos con tarjeta';
+$lang['Widepay.!error.wallet_id.format'] = 'Por favor, introduzca un ID de cartera';
+$lang['Widepay.!error.wallet_token.format'] = 'Introduzca un token de API de monedero';
+$lang['Widepay.buildprocess.submit_card'] = 'Pagar con tarjeta de crédito Wide Pay';
+$lang['Widepay.buildprocess.submit_ticket'] = 'Pagar con Wide Pay Ticket';
+$lang['Widepay.charge_types.ticket'] = 'Billete';
+$lang['Widepay.charge_types.card'] = 'Tarjeta de crédito';
+
