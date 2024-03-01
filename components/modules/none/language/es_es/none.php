2b4cee8821bbd166637a192241817ba38d742b88
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:56 -0800

    Updated es_es language files

---
+++ components/modules/none/language/es_es/none.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * None
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['None.manage.text'] = 'No hay nada que gestionar para este módulo.';
+$lang['None.name'] = 'Ninguno';
+
