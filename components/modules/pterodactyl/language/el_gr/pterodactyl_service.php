231505502ac7144678159fc6f508933b79679e28
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:00 -0800

    Updated el_gr language files

---
+++ components/modules/pterodactyl/language/el_gr/pterodactyl_service.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Pterodactyl Service
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PterodactylService.service_fields.optional'] = '%1$s (Προαιρετικό)';
+$lang['PterodactylService.service_fields.tooltip.server_id'] = 'Το αναγνωριστικό του διακομιστή στον πίνακα Pterodactyl. Αυτό θα πρέπει να χρησιμοποιείται μόνο όταν συνδέετε αυτή την υπηρεσία με έναν προϋπάρχοντα διακομιστή.';
+$lang['PterodactylService.service_fields.server_id'] = 'Αναγνωριστικό διακομιστή (προαιρετικό)';
+$lang['PterodactylService.service_fields.tooltip.server_description'] = 'Η περιγραφή του διακομιστή.';
+$lang['PterodactylService.service_fields.server_description'] = 'Περιγραφή διακομιστή (προαιρετικά)';
+$lang['PterodactylService.service_fields.tooltip.server_name'] = 'Το όνομα του διακομιστή.';
+$lang['PterodactylService.service_fields.server_name'] = 'Όνομα διακομιστή';
+$lang['PterodactylService.!error.server_name.empty'] = 'Ορίστε ένα όνομα για τον διακομιστή.';
+
