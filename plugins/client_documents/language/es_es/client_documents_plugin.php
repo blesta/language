4e7c77871bcdd279a627c91ef186949114c9003e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:53 -0800

    Updated es_es language files

---
+++ plugins/client_documents/language/es_es/client_documents_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Client Documents Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDocumentsPlugin.name'] = 'Documentos del cliente';
+$lang['ClientDocumentsPlugin.description'] = 'Cargue archivos para que los descarguen determinados clientes.';
+$lang['ClientDocumentsPlugin.nav_primary_client.main'] = 'Documentos';
+$lang['ClientDocumentsPlugin.action_staff_client.index'] = 'Documentos';
+
