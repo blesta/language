ab728c12d60c87ba1785a11b5a759e57acb3f8ca
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:39 -0800

    Updated cs_cz language files

---
+++ components/modules/vesta/language/cs_cz/vesta.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Vesta
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Vesta.!error.actions_password.valid'] = 'Pole Heslo a Potvrzení hesla nemohou být prázdná.';
+$lang['Vesta.!error.password.valid'] = 'Heslo musí mít alespoň 8 znaků.';
+$lang['Vesta.!error.user_name.empty'] = 'Uživatelské jméno nemůže být prázdné.';
+$lang['Vesta.!error.domain.test'] = 'Název domény nemůže začínat na \'test\'.';
+$lang['Vesta.!error.domain.format'] = 'Zadejte platný název domény, např. domain.com.';
+$lang['Vesta.!error.module_row.missing'] = 'Došlo k interní chybě. Řádek modulu není k dispozici.';
+$lang['Vesta.!error.api.internal'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+$lang['Vesta.!error.meta[package_name].empty'] = 'Zadejte název balíčku Vesta.';
+$lang['Vesta.!error.password_valid'] = 'Heslo se zdá být neplatné.';
+$lang['Vesta.!error.port_valid'] = 'Zdá se, že port je neplatný.';
+$lang['Vesta.!error.user_name_valid'] = 'Uživatelské jméno se zdá být neplatné.';
+$lang['Vesta.!error.host_name_valid'] = 'Název hostitele se zdá být neplatný.';
+$lang['Vesta.!error.server_name_valid'] = 'Je třeba zadat označení serveru.';
+$lang['Vesta.tab_stats.info_heading.value'] = 'Hodnota';
+$lang['Vesta.tab_stats.info_heading.field'] = 'Pole';
+$lang['Vesta.tab_client_actions.confirm_password'] = 'Potvrzení hesla';
+$lang['Vesta.tab_client_actions.password'] = 'Heslo';
+$lang['Vesta.tab_client_actions.change_password'] = 'Změna hesla';
+$lang['Vesta.service_info.option_login'] = 'Přihlášení';
+$lang['Vesta.service_info.options'] = 'Možnosti';
+$lang['Vesta.service_info.server'] = 'Server';
+$lang['Vesta.service_info.password'] = 'Heslo';
+$lang['Vesta.service_info.username'] = 'Uživatelské jméno';
+$lang['Vesta.service_info.hostname'] = 'Hostitelské jméno';
+$lang['Vesta.stored_locally_only'] = 'Toto pole se aktualizuje pouze lokálně';
+$lang['Vesta.service_field.password'] = 'Heslo';
+$lang['Vesta.service_field.username'] = 'Uživatelské jméno';
+$lang['Vesta.service_field.domain'] = 'Název domény';
+$lang['Vesta.package_fields.package_name_how_to'] = 'Název balíčku získáte po přihlášení do ovládacího panelu Vesty > Balíčky.';
+$lang['Vesta.package_fields.package_name'] = 'Název balíčku Vesta';
+$lang['Vesta.row_meta.use_ssl'] = 'Při připojování k rozhraní API používejte protokol SSL (doporučeno).';
+$lang['Vesta.row_meta.port'] = 'Přístav';
+$lang['Vesta.row_meta.password'] = 'Heslo';
+$lang['Vesta.row_meta.user_name'] = 'Uživatelské jméno';
+$lang['Vesta.row_meta.host_name'] = 'Hostitelské jméno';
+$lang['Vesta.row_meta.server_name'] = 'Označení serveru';
+$lang['Vesta.edit_row.add_btn'] = 'Upravit server';
+$lang['Vesta.edit_row.remove_name_server'] = 'Odstranění stránky';
+$lang['Vesta.edit_row.basic_title'] = 'Základní nastavení';
+$lang['Vesta.edit_row.box_title'] = 'Upravit server Vesta';
+$lang['Vesta.add_row.add_btn'] = 'Přidat server';
+$lang['Vesta.add_row.remove_name_server'] = 'Odstranění stránky';
+$lang['Vesta.add_row.basic_title'] = 'Základní nastavení';
+$lang['Vesta.add_row.box_title'] = 'Přidání serveru Vesta';
+$lang['Vesta.order_options.first'] = 'První neplnohodnotný server';
+$lang['Vesta.manage.module_groups_no_results'] = 'Neexistují žádné skupiny serverů.';
+$lang['Vesta.manage.module_rows_no_results'] = 'Neexistují žádné servery.';
+$lang['Vesta.manage.module_groups.confirm_delete'] = 'Opravdu chcete tuto skupinu serverů odstranit?';
+$lang['Vesta.manage.module_rows.confirm_delete'] = 'Opravdu chcete tento server odstranit?';
+$lang['Vesta.manage.module_groups.delete'] = 'Odstranit';
+$lang['Vesta.manage.module_rows.delete'] = 'Odstranit';
+$lang['Vesta.manage.module_groups.edit'] = 'Upravit';
+$lang['Vesta.manage.module_rows.edit'] = 'Upravit';
+$lang['Vesta.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Vesta.manage.module_groups_heading.options'] = 'Možnosti';
+$lang['Vesta.manage.module_groups_heading.servers'] = 'Počet serverů';
+$lang['Vesta.manage.module_groups_heading.name'] = 'Název skupiny';
+$lang['Vesta.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['Vesta.manage.module_rows_heading.accounts'] = 'Účty';
+$lang['Vesta.manage.module_rows_heading.port'] = 'Přístav';
+$lang['Vesta.manage.module_rows_heading.hostname'] = 'Hostitelské jméno';
+$lang['Vesta.manage.module_rows_heading.name'] = 'Označení serveru';
+$lang['Vesta.manage.module_groups_title'] = 'Skupiny serverů';
+$lang['Vesta.manage.module_rows_title'] = 'Servery';
+$lang['Vesta.add_module_group'] = 'Přidání skupiny serverů';
+$lang['Vesta.add_module_row'] = 'Přidat server';
+$lang['Vesta.tab_stats.info.BACKUPS'] = 'Zálohování';
+$lang['Vesta.tab_stats.info.BANDWIDTH'] = 'Využití šířky pásma';
+$lang['Vesta.tab_stats.info.DISK_QUOTA'] = 'Využití disku';
+$lang['Vesta.tab_stats.info.CRON_JOBS'] = 'Pracovní nabídky Cron';
+$lang['Vesta.tab_stats.info.DATABASES'] = 'Databáze';
+$lang['Vesta.tab_stats.info.MAIL_ACCOUNTS'] = 'Poštovní účty';
+$lang['Vesta.tab_stats.info.MAIL_DOMAINS'] = 'Poštovní domény';
+$lang['Vesta.tab_stats.info.DNS_RECORDS'] = 'Záznamy DNS';
+$lang['Vesta.tab_stats.info.DNS_DOMAINS'] = 'Domény DNS';
+$lang['Vesta.tab_stats.info.WEB_ALIASES'] = 'Pseudonymy';
+$lang['Vesta.tab_stats.info.WEB_DOMAINS'] = 'Domény';
+$lang['Vesta.submit'] = 'Odeslat';
+$lang['Vesta.tab_client_actions'] = 'Akce';
+$lang['Vesta.tab_stats'] = 'Statistiky';
+$lang['Vesta.module_group'] = 'Skupina serverů';
+$lang['Vesta.module_row_plural'] = 'Servery';
+$lang['Vesta.module_row'] = 'Server';
+$lang['Vesta.description'] = 'Vesta nebo VestaCP je open-source webový ovládací panel, který je považován za skvělou alternativu k cPanelu. Nejenže se kvalitou zabezpečení a častými aktualizacemi vyrovná cPanelu, ale je také zcela zdarma.';
+$lang['Vesta.name'] = 'Vesta';
+
