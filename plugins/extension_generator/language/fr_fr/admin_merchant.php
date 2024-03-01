416975616c1f3e1c99f7bb6378d29bf528b2cb4a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:03 -0800

    Updated fr_fr language files

---
+++ plugins/extension_generator/language/fr_fr/admin_merchant.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Admin Merchant
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMerchant.getsupportedfeatures.cc'] = 'Carte de crédit';
+$lang['AdminMerchant.getsupportedfeatures.cc_offsite'] = 'Carte de crédit hors site';
+$lang['AdminMerchant.getsupportedfeatures.cc_form'] = 'Formulaire de carte de crédit personnalisé';
+$lang['AdminMerchant.getsupportedfeatures.ach'] = 'ACH';
+$lang['AdminMerchant.getsupportedfeatures.ach_offsite'] = 'ACH hors site';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc'] = 'Traitement des cartes de crédit à l\'aide d\'informations d\'identification non stockées ou stockées dans Blesta.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc_offsite'] = 'Traitement des cartes de crédit à l\'aide d\'informations d\'identification stockées hors site par le processeur.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc_form'] = 'Cela permet à une passerelle de fournir un code html personnalisé pour les formulaires de carte de crédit.  Cela permet de prendre en charge les cartes de crédit hors site qui peuvent utiliser des iframes ou nécessiter des champs supplémentaires.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_ach'] = 'Traitement automatisé de la chambre de compensation à l\'aide d\'informations d\'identification non stockées ou stockées dans Blesta.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_ach_offsite'] = 'Traitement automatisé par la chambre de compensation à l\'aide d\'informations d\'identification stockées hors site par le processeur.';
+$lang['AdminMerchant.index.page_title'] = 'Générateur d\'extension - %1$s';
+$lang['AdminMerchant.index.boxtitle_extension_generator'] = 'Générateur d\'extensions - Merchant Gateway';
+$lang['AdminMerchant.basic.heading_basic'] = 'Informations de base';
+$lang['AdminMerchant.basic.heading_authors'] = 'Auteurs';
+$lang['AdminMerchant.basic.description'] = 'Description';
+$lang['AdminMerchant.basic.logo'] = 'Logo';
+$lang['AdminMerchant.basic.signup_url'] = 'URL d\'inscription';
+$lang['AdminMerchant.basic.currencies'] = 'Monnaies';
+$lang['AdminMerchant.basic.author_name'] = 'Nom de l\'auteur';
+$lang['AdminMerchant.basic.author_url'] = 'URL de l\'auteur';
+$lang['AdminMerchant.basic.text_options'] = 'Options';
+$lang['AdminMerchant.basic.author_row_add'] = 'Ajouter un auteur';
+$lang['AdminMerchant.basic.text_remove'] = 'Retirer';
+$lang['AdminMerchant.basic.tooltip_description'] = 'La description affichée dans la liste des plugins';
+$lang['AdminMerchant.basic.tooltip_logo'] = 'Le logo affiché dans le listing du plugin. Les images ne sont pas redimensionnées, les dimensions idéales sont donc 150x70.';
+$lang['AdminMerchant.basic.tooltip_signup_url'] = 'L\'URL de la page d\'inscription de la passerelle';
+$lang['AdminMerchant.basic.tooltip_currencies'] = 'Une liste de devises prises en charge, séparées par des virgules (par exemple USD, JPY, EUR).';
+$lang['AdminMerchant.basic.placeholder_signup_url'] = 'par exemple : https://dashboard.stripe.com/register';
+$lang['AdminMerchant.basic.placeholder_currencies'] = 'par exemple USD, EUR, JPY';
+$lang['AdminMerchant.basic.placeholder_author_name'] = 'par exemple, Blesta';
+$lang['AdminMerchant.basic.placeholder_author_url'] = 'par exemple : https://blesta.com/';
+$lang['AdminMerchant.basic.fields'] = 'Suivant - Champs de configuration';
+$lang['AdminMerchant.basic.features'] = 'Suivant - Fonctionnalités prises en charge';
+$lang['AdminMerchant.fields.heading_fields'] = 'Champs de configuration';
+$lang['AdminMerchant.fields.name'] = 'Nom';
+$lang['AdminMerchant.fields.label'] = 'Étiquette';
+$lang['AdminMerchant.fields.type'] = 'Type';
+$lang['AdminMerchant.fields.tooltip'] = 'Texte de l\'infobulle';
+$lang['AdminMerchant.fields.encryptable'] = 'Cryptable';
+$lang['AdminMerchant.fields.tooltip_name'] = 'Le nom du champ dans la base de code';
+$lang['AdminMerchant.fields.tooltip_label'] = 'Nom d\'affichage du champ';
+$lang['AdminMerchant.fields.tooltip_type'] = 'Le type de champ à créer (case à cocher, texte, etc.)';
+$lang['AdminMerchant.fields.tooltip_tooltip'] = 'Le texte de l\'infobulle à afficher à côté du champ (laisser vide pour ne pas avoir d\'infobulle)';
+$lang['AdminMerchant.fields.tooltip_encryptable'] = 'Cryptage ou non de ce champ dans la base de données';
+$lang['AdminMerchant.fields.placeholder_name'] = 'par exemple, configuration_field';
+$lang['AdminMerchant.fields.placeholder_label'] = 'Par exemple, champ de configuration';
+$lang['AdminMerchant.fields.field_row_add'] = 'Ajouter un champ de configuration';
+$lang['AdminMerchant.fields.text_options'] = 'Options';
+$lang['AdminMerchant.fields.text_remove'] = 'Retirer';
+$lang['AdminMerchant.fields.features'] = 'Suivant - Fonctionnalités prises en charge';
+$lang['AdminMerchant.features.heading_supported_features'] = 'Fonctionnalités prises en charge';
+$lang['AdminMerchant.features.confirm'] = 'Suivant - Confirmation';
+
