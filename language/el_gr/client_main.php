60c70c544252bc4dca9b634ba0a9e28361873919
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:06 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_main.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.getcurrencyamounts.text_total_credits'] = 'Συντελεστές:';
+$lang['Clientmain.invoicemethod.field_methodsubmit'] = 'Ενημέρωση';
+$lang['ClientMain.invoicemethod.text_invoice_method'] = 'Η μέθοδος τιμολόγησης είναι η μέθοδος με την οποία σας παραδίδουμε τα νέα τιμολόγια.';
+$lang['ClientMain.invoicemethod.boxtitle_inv_method'] = 'Μέθοδος τιμολόγησης';
+$lang['ClientMain.invoicemethod.page_title'] = 'Πελάτης #%1$s Μέθοδος τιμολόγησης';
+$lang['ClientMain.edit.field_editsubmit'] = 'Ενημέρωση των πληροφοριών μου';
+$lang['ClientMain.edit.field_otp'] = 'Κωδικός πρόσβασης μίας χρήσης';
+$lang['ClientMain.edit.text_windows'] = 'Windows';
+$lang['ClientMain.edit.text_ios'] = 'iOS';
+$lang['ClientMain.edit.text_android'] = 'Android';
+$lang['ClientMain.edit.two_factor_step3'] = 'Επιβεβαιώστε τον εφάπαξ κωδικό πρόσβασής σας και, στη συνέχεια, κάντε κλικ στην επιλογή "Ενημέρωση των πληροφοριών μου". Θα σας ζητηθεί να εισαγάγετε τον κωδικό πρόσβασης μιας χρήσης την επόμενη φορά που θα συνδεθείτε.';
+$lang['ClientMain.edit.two_factor_step2'] = 'Ανοίξτε την εφαρμογή και σαρώστε τον παρακάτω κωδικό QR ή, εναλλακτικά, πληκτρολογήστε ένα από τα παρακάτω πλήκτρα:<br/>Hex - %1$s<br/>Base32 - %2$s';
+$lang['ClientMain.edit.two_factor_step1'] = 'Κατεβάστε έναν Αυθεντικοποιητή για τη συσκευή σας:';
+$lang['ClientMain.edit.two_factor_instructions'] = 'Για να χρησιμοποιήσετε τον έλεγχο ταυτότητας δύο παραγόντων, χρειάζεστε ένα διακριτικό TOTP. Αν έχετε κινητή συσκευή Android, iOS ή Windows, το τηλέφωνό σας μπορεί να λειτουργήσει ως το κουπόνι σας και να παρέχει τον απαραίτητο κωδικό πρόσβασης μίας χρήσης.';
+$lang['ClientMain.edit.two_factor_desc'] = 'Ο έλεγχος ταυτότητας δύο παραγόντων είναι μια λειτουργία ασφαλείας στην οποία παρέχετε δύο μέσα ταυτοποίησης για να συνδεθείτε στο λογαριασμό σας. Εάν είναι ενεργοποιημένο, θα συνδεθείτε στο λογαριασμό σας με το όνομα χρήστη και τον κωδικό πρόσβασής σας και στη συνέχεια θα σας ζητηθεί να εισαγάγετε έναν κωδικό πρόσβασης μίας χρήσης.';
+$lang['ClientMain.edit.field_two_factor_mode'] = 'Ενεργοποίηση ελέγχου ταυτότητας δύο παραγόντων';
+$lang['ClientMain.edit.field_current_password'] = 'Τρέχων κωδικός πρόσβασης';
+$lang['ClientMain.edit.field_confirm_password'] = 'Επιβεβαίωση κωδικού πρόσβασης';
+$lang['ClientMain.edit.field_new_password'] = 'Νέος κωδικός πρόσβασης';
+$lang['ClientMain.edit.field_username'] = 'Όνομα χρήστη';
+$lang['ClientMain.edit.heading_authentication'] = 'Αυθεντικοποίηση';
+$lang['ClientMain.edit.field_receive_email_marketing'] = 'Θέλω να λαμβάνω μηνύματα ηλεκτρονικού ταχυδρομείου σχετικά με νέα προϊόντα, υπηρεσίες και προσφορές.';
+$lang['ClientMain.edit.field_language'] = 'Γλώσσα';
+$lang['ClientMain.edit.heading_settings'] = 'Πρόσθετες ρυθμίσεις';
+$lang['ClientMain.edit.field_invoiceaddress'] = 'Διεύθυνση τιμολογίων προς';
+$lang['ClientMain.edit.field_default_currency'] = 'Προτιμώμενο νόμισμα';
+$lang['ClientMain.edit.field_taxid'] = 'Φορολογικός αριθμός ταυτότητας/VATIN';
+$lang['ClientMain.edit.heading_billing'] = 'Πληροφορίες χρέωσης';
+$lang['ClientMain.edit.boxtitle_edit'] = 'Επεξεργασία των πληροφοριών μου';
+$lang['ClientMain.edit.page_title'] = 'Πελάτης #%1$s Επεξεργασία των πληροφοριών μου';
+$lang['ClientMain.edit.heading_contact'] = 'Στοιχεία επικοινωνίας';
+$lang['ClientMain.myinfo.link_editclient'] = 'Αλλαγή';
+$lang['ClientMain.myinfo.setting_change'] = 'Να το αλλάξουμε αυτό;';
+$lang['ClientMain.myinfo.field_managed_accounts_search'] = 'Αναζήτηση (εταιρεία, email ή όνομα)';
+$lang['ClientMain.myinfo.managed_accounts'] = 'Διαχειριζόμενοι λογαριασμοί';
+$lang['ClientMain.myinfo.link_addcontact'] = 'Προσθήκη επαφής';
+$lang['ClientMain.myinfo.contacts'] = 'Επαφές';
+$lang['ClientMain.myinfo.number'] = '(%1$s %2$s)';
+$lang['ClientMain.myinfo.setting_autodebit_ach_days'] = 'Θα χρεώσουμε τον λογαριασμό σας %1$s που λήγει σε x%2$s %3$s ημέρες πριν από την πληρωμή.';
+$lang['ClientMain.myinfo.setting_autodebit_cc_days'] = 'Θα χρεώσουμε το %1$s που λήγει σε x%2$s %3$s ημέρες πριν από την πληρωμή.';
+$lang['ClientMain.myinfo.setting_autodebit_ach_one_day'] = 'Θα χρεώσουμε τον λογαριασμό σας %1$s που λήγει σε x%2$s την ημέρα πριν από την πληρωμή.';
+$lang['ClientMain.myinfo.setting_autodebit_cc_one_day'] = 'Θα χρεώσουμε το %1$s που λήγει σε x%2$s την ημέρα πριν από την πληρωμή.';
+$lang['ClientMain.myinfo.setting_autodebit_ach_zero_days'] = 'Θα χρεώσουμε τον λογαριασμό σας %1$s που λήγει σε x%2$s την ημέρα που πρέπει να πληρωθεί.';
+$lang['ClientMain.myinfo.setting_autodebit_cc_zero_days'] = 'Θα χρεώσουμε το %1$s σας που λήγει σε x%2$s την ημέρα πληρωμής.';
+$lang['ClientMain.myinfo.setting_autodebit_enabled'] = 'Ο λογαριασμός σας έχει ρυθμιστεί για αυτόματη πληρωμή.';
+$lang['ClientMain.myinfo.setting_autodebit_disabled'] = 'Ο λογαριασμός σας δεν έχει ρυθμιστεί για αυτόματη πληρωμή.';
+$lang['ClientMain.myinfo.setting_invoices'] = 'Αυτή τη στιγμή λαμβάνετε τιμολόγια από το %1$s.';
+$lang['ClientMain.myinfo.client_id'] = 'Πελάτης #%1$s';
+$lang['ClientMain.myinfo.page_title'] = 'Πελάτης #%1$s';
+$lang['ClientMain.index.page_title'] = 'Πελάτης #%1$s';
+$lang['ClientMain.!tooltip.invoice_method_postalmethods'] = 'Θα εκτυπώσουμε και θα στείλουμε αυτόματα τα τιμολόγια στη διεύθυνση που έχουμε στο αρχείο μας.';
+$lang['ClientMain.!tooltip.invoice_method_interfax'] = 'Θα στείλουμε αυτόματα τα τιμολόγια με φαξ στον αριθμό φαξ που έχουμε στο αρχείο μας. Εάν επιλέξετε αυτή την επιλογή, βεβαιωθείτε ότι έχουμε έναν έγκυρο αριθμό φαξ στο αρχείο μας.';
+$lang['ClientMain.!tooltip.invoice_method_paper'] = 'Θα εκτυπώσουμε και θα αποστείλουμε τα τιμολόγια στη διεύθυνση που έχουμε στο αρχείο μας.';
+$lang['ClientMain.!tooltip.invoice_method_email'] = 'Τα τιμολόγια θα αποστέλλονται με ηλεκτρονικό ταχυδρομείο στη διεύθυνση που έχουμε στο αρχείο μας.';
+$lang['ClientMain.!info.no_managed_accounts'] = 'Δεν υπάρχουν λογαριασμοί που μπορείτε να διαχειριστείτε.';
+$lang['ClientMain.!info.no_contacts'] = 'Δεν υπάρχουν πρόσθετες επαφές.';
+$lang['ClientMain.!info.email_pending_verification_button'] = 'Επαναποστολή email επαλήθευσης';
+$lang['ClientMain.!info.email_pending_verification'] = 'Η διεύθυνση ηλεκτρονικού ταχυδρομείου σας είναι προς το παρόν ανεπιβεβαίωτη. Ένα email επαλήθευσης έχει σταλεί στο %1$s.';
+$lang['ClientMain.!info.additional_services'] = '...και %1$s πρόσθετες υπηρεσίες.';
+$lang['ClientMain.!info.additional_service'] = '...και 1 επιπλέον υπηρεσία.';
+$lang['ClientMain.!info.service_name'] = '%1$s - %2$s';
+$lang['ClientMain.!info.service_in_review'] = 'Οι ακόλουθες υπηρεσίες εκκρεμούν προς εξέταση από το προσωπικό. Μόλις εγκριθούν, θα ενεργοποιηθούν:';
+$lang['ClientMain.!info.invoice_due_other_currencies'] = 'Έχετε επίσης ανεξόφλητο υπόλοιπο σε άλλα νομίσματα.';
+$lang['ClientMain.!info.invoice_due_past_due_text'] = 'Ο λογαριασμός σας έχει ανεξόφλητο υπόλοιπο %1$s, εκ των οποίων %2$s είναι ληξιπρόθεσμο. Παρακαλούμε να προβείτε σε πληρωμή το συντομότερο δυνατό.';
+$lang['ClientMain.!info.invoice_due_text'] = 'Ο λογαριασμός σας έχει ανεξόφλητο υπόλοιπο %1$s. Παρακαλείστε να προβείτε σε πληρωμή το συντομότερο δυνατό.';
+$lang['ClientMain.!info.invoice_past_due_button'] = 'Πληρωμή ληξιπρόθεσμων οφειλών';
+$lang['ClientMain.!info.invoice_due_button'] = 'Πληρωμή';
+$lang['ClientMain.!info.invoice_due_title'] = 'Καλώς ήρθες πίσω, %1$s!';
+$lang['ClientMain.!success.invoice_method_updated'] = 'Η μέθοδος τιμολογίου σας ενημερώθηκε επιτυχώς. Όλα τα μελλοντικά τιμολόγια θα σας παραδίδονται μέσω του %1$s.';
+$lang['ClientMain.!success.client_updated'] = 'Οι πληροφορίες του λογαριασμού σας ενημερώθηκαν επιτυχώς.';
+$lang['ClientMain.edit.field_recovery_email'] = 'Email ανάκτησης (προαιρετικό)';
+
