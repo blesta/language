29988b5623ace2128c2b4567872c5c2ffc1c2a15
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:17 -0800

    Updated es_es language files

---
+++ language/es_es/clients.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Clients
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Clients.!error.client_group_id.exists'] = 'ID de grupo de clientes no válido.';
+$lang['Clients.!error.client_id.exists'] = 'ID de cliente no válido.';
+$lang['Clients.!error.account_id.exists'] = 'ID de cuenta no válida.';
+$lang['Clients.!error.type.exists'] = 'Tipo no válido.';
+$lang['Clients.!error.id_format.empty'] = 'No se ha establecido ningún formato de identificación para los clientes.';
+$lang['Clients.!error.id_format.length'] = 'El formato de identificación de los clientes no puede superar los 64 caracteres.';
+$lang['Clients.!error.id_value.valid'] = 'No se puede determinar el valor de ID de cliente.';
+$lang['Clients.!error.user_id.exists'] = 'ID de usuario no válida.';
+$lang['Clients.!error.user_id.unique'] = 'El ID de usuario del cliente %1$s ya está ocupado.';
+$lang['Clients.!error.status.format'] = 'Estado no válido.';
+$lang['Clients.!error.settings[tax_id].valid'] = 'Especifique un NIF/CIF válido.';
+$lang['Clients.!error.client_id.deleteable'] = 'El cliente no puede ser eliminado mientras existan facturas abiertas, facturas recurrentes o servicios activos/suspendidos. Por favor, anule cualquier factura abierta, elimine cualquier factura periódica y cancele cualquier servicio activo/suspendido antes de continuar.';
+$lang['Clients.!error.autodebit.format'] = 'Valor de débito automático no válido.';
+$lang['Clients.!error.inv_address_to.exists'] = 'Especifique un contacto válido al que dirigir las facturas.';
+$lang['Clients.!error.default_currency.valid'] = 'Moneda no válida.';
+$lang['Clients.!error.default_currency.editable'] = 'Es posible que la moneda no esté actualizada.';
+$lang['Clients.!error.inv_method.valid'] = 'Forma de entrega de la factura no válida.';
+$lang['Clients.!error.inv_method.editable'] = 'El método de entrega de la factura puede no estar actualizado.';
+$lang['Clients.!error.language.valid'] = 'Idioma no válido.';
+$lang['Clients.!error.language.editable'] = 'La lengua puede no estar actualizada.';
+$lang['Clients.!error.receive_email_marketing.valid'] = 'Valor de marketing por correo electrónico no válido.';
+$lang['Clients.!error.name.empty'] = 'Por favor, introduzca un nombre.';
+$lang['Clients.!error.link.valid'] = 'Por favor, introduzca un enlace válido.';
+$lang['Clients.!error.is_lang.format'] = 'is_lang debe ser un número.';
+$lang['Clients.!error.is_lang.length'] = 'is_lang no puede tener más de 1 carácter.';
+$lang['Clients.!error.type.format'] = 'Tipo no válido.';
+$lang['Clients.!error.values.format'] = 'Los valores tienen un formato no válido.';
+$lang['Clients.!error.default.valid'] = 'El valor por defecto no es válido.';
+$lang['Clients.!error.regex.valid'] = 'La expresión regular no es válida.';
+$lang['Clients.!error.show_client.format'] = 'Mostrar cliente debe ser un número.';
+$lang['Clients.!error.show_client.length'] = 'Mostrar cliente no puede superar 1 carácter de longitud.';
+$lang['Clients.!error.client_field_id.exists'] = 'ID de campo personalizado no válido.';
+$lang['Clients.!error.client_field_id.matches'] = 'El campo personalizado no es válido.';
+$lang['Clients.!error.encrypted.format'] = 'Cifrado debe ser un número.';
+$lang['Clients.!error.encrypted.length'] = 'El cifrado no puede superar 1 carácter de longitud.';
+$lang['Clients.!error.read_only.format'] = 'Sólo lectura debe ser un número.';
+$lang['Clients.!error.read_only.length'] = 'Sólo lectura no puede superar 1 carácter de longitud.';
+$lang['Clients.!error.value.required'] = '%1$s tiene un formato no válido.';
+$lang['Clients.!error.value.valid'] = '%1$s tiene un valor no válido.';
+$lang['Clients.!error.staff_id.exists'] = 'ID de personal no válida.';
+$lang['Clients.!error.title.empty'] = 'Por favor, introduzca un título.';
+$lang['Clients.!error.title.length'] = 'La longitud del título no puede superar los 255 caracteres.';
+$lang['Clients.!error.note_id.exists'] = 'ID de nota no válido.';
+$lang['Clients.!error.stickied.format'] = 'Pegajoso debe ser un número.';
+$lang['Clients.!error.stickied.length'] = 'La longitud de la pegatina no puede superar 1 carácter.';
+$lang['Clients.!error.package_ids.exists'] = 'Al menos uno de los paquetes proporcionados no existe.';
+$lang['Clients.getCustomFieldTypes.textbox'] = 'Cuadro de texto';
+$lang['Clients.getCustomFieldTypes.checkbox'] = 'Casilla de verificación';
+$lang['Clients.getCustomFieldTypes.dropdown'] = 'Desplegable';
+$lang['Clients.getCustomFieldTypes.textarea'] = 'Área de texto';
+$lang['Clients.getStatusTypes.active'] = 'Activo';
+$lang['Clients.getStatusTypes.inactive'] = 'Inactivo';
+$lang['Clients.getStatusTypes.fraud'] = 'Fraude';
+$lang['Clients.setDebitAccountFailure.note_title'] = 'Débito automático desactivado para la cuenta de pago.';
+$lang['Clients.setDebitAccountFailure.note_body'] = 'La cuenta de pago %1$s que termina en x%2$s se ha desactivado porque ha superado el número máximo de intentos de rechazo.';
+$lang['Clients.customfield.cf2'] = 'Campo personalizado';
+
