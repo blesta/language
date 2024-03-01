c6a6d6e7ecd3f7c58fd7d9ca3a69c14a414e4480
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:17 -0800

    Updated de_de language files

---
+++ language/de_de/service_invoices.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Service Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ServiceInvoices.!error.date_next_attempt.format'] = 'Datum nächster Versuch muss ein Datum sein.';
+$lang['ServiceInvoices.!error.maximum_attempts.format'] = 'Die maximalen Versuche müssen eine Zahl sein.';
+$lang['ServiceInvoices.!error.failed_attempts.format'] = 'Fehlversuche müssen eine Zahl sein.';
+$lang['ServiceInvoices.!error.invoice_id.exists'] = 'Ungültige Rechnungs-ID.';
+$lang['ServiceInvoices.!error.service_id.exists'] = 'Ungültige Dienst-ID.';
+
