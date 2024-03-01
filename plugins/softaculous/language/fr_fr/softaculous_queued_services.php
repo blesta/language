a1400fcdd7e5f139f659963d1b4cb75680d77f03
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:02 -0800

    Updated fr_fr language files

---
+++ plugins/softaculous/language/fr_fr/softaculous_queued_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Softaculous Queued Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SoftaculousQueuedServices.errors.service_id.exists'] = 'Le service donné n\'existe pas.';
+$lang['SoftaculousQueuedServices.errors.service_id.exists_softaculous'] = 'Le service donné n\'est pas en file d\'attente pour l\'installation.';
+$lang['SoftaculousQueuedServices.errors.company_id.exists'] = 'L\'entreprise indiquée n\'existe pas.';
+$lang['SoftaculousQueuedServices.errors.attempts.valid'] = 'Les tentatives doivent être une valeur numérique positive.';
+
