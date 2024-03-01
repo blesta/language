ef2e8ccd154829c6f321bfb09da8b207440c2d57
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:23 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/suspend_services.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Suspend Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.suspendservices.attempt'] = 'Προσπάθεια διεκπεραίωσης αναστολών υπηρεσιών.';
+$lang['Automation.task.suspendservices.completed'] = 'Η εργασία αναστολής της υπηρεσίας έχει ολοκληρωθεί.';
+$lang['Automation.task.suspendservices.suspension_reason'] = 'Μη πληρωμή';
+$lang['Automation.task.suspendservices.error'] = 'Η υπηρεσία #%1$s από τον πελάτη %2$s δεν μπόρεσε να ανασταλεί.';
+$lang['Automation.task.suspendservices.success'] = 'Η υπηρεσία #%1$s από τον πελάτη %2$s έχει ανασταλεί.';
+
