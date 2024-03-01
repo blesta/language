b5688da974fe917b2f7978297858eb68310614bc
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:54 -0800

    Updated es_es language files

---
+++ components/modules/multicraft/language/es_es/multicraft_service.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Multicraft Service
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MulticraftService.!error.internal'] = 'Se ha producido un error interno o el servidor no ha respondido a la solicitud.';
+$lang['MulticraftService.!error.client'] = 'No se ha encontrado ningún cliente desde el que crear un usuario Multicraft.';
+$lang['MulticraftService.!error.server_id.format'] = 'El ID del servidor debe ser un número entero, o dejarse en blanco.';
+$lang['MulticraftService.!error.user_id.format'] = 'El ID de usuario debe ser un número entero, o dejarse en blanco.';
+$lang['MulticraftService.!error.daemon_id.format'] = 'El ID del demonio debe ser un número entero, o dejarse en blanco.';
+$lang['MulticraftService.!error.ip.format'] = 'Introduzca una dirección IP válida o déjela en blanco.';
+$lang['MulticraftService.!error.port.format'] = 'Introduzca un número de puerto válido o déjelo en blanco.';
+$lang['MulticraftService.service_fields.server_id'] = 'ID de servidor';
+$lang['MulticraftService.service_fields.tooltip.server_id'] = 'Establezca el ID único del servidor Multicraft al que se asignará este servicio. De lo contrario, déjelo en blanco.';
+$lang['MulticraftService.service_fields.user_id'] = 'ID de usuario';
+$lang['MulticraftService.service_fields.tooltip.user_id'] = 'Establezca el ID único del usuario de Multicraft al que se va a asignar este servicio. De lo contrario, déjelo en blanco.';
+$lang['MulticraftService.service_fields.daemon_id'] = 'ID de demonio';
+$lang['MulticraftService.service_fields.tooltip.daemon_id'] = 'Establezca el ID del demonio Multicraft al que se asignará este servicio. De lo contrario, déjelo en blanco.';
+$lang['MulticraftService.service_fields.ip'] = 'Dirección IP';
+$lang['MulticraftService.service_fields.tooltip.ip'] = 'Establezca la dirección IP del servidor Multicraft al que debe asignarse este servicio. De lo contrario, déjelo en blanco.';
+$lang['MulticraftService.service_fields.port'] = 'Puerto';
+$lang['MulticraftService.service_fields.tooltip.port'] = 'Establezca el puerto del servidor Multicraft al que se asignará este servicio. De lo contrario, déjelo en blanco.';
+$lang['MulticraftService.service_fields.yes'] = 'Sí';
+$lang['MulticraftService.service_fields.no'] = 'No';
+$lang['MulticraftService.!error.configoptions[user_players].format'] = 'Si el propietario del servidor puede establecer el número de jugadores debe establecerse en 1 o 0.';
+$lang['MulticraftService.!error.configoptions[players].format'] = 'Por favor, establezca el número de jugadores.';
+$lang['MulticraftService.!error.configoptions[memory].format'] = 'Por favor, establezca una cantidad válida de memoria.';
+$lang['MulticraftService.!error.configoptions[daemon_id].format'] = 'Por favor, establezca un demonio válido.';
+$lang['MulticraftService.!error.configoptions[dedicated_ip].format'] = 'La opción de utilizar una IP dedicada debe establecerse en 1 o 0.';
+
