299a145369d735360d3dc33351116bc3f2349961
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:46 -0800

    Updated fr_fr language files

---
+++ components/modules/centoswebpanel/language/fr_fr/centoswebpanel.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Centoswebpanel
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Centoswebpanel.name'] = 'CentOS WebPanel';
+$lang['Centoswebpanel.description'] = 'Panneau de contrôle gratuit pour l\'hébergement Web conçu pour une gestion rapide et facile des serveurs (dédiés et VPS)';
+$lang['Centoswebpanel.module_row'] = 'Serveur';
+$lang['Centoswebpanel.module_row_plural'] = 'Serveurs';
+$lang['Centoswebpanel.module_group'] = 'Groupe de serveurs';
+$lang['Centoswebpanel.add_module_row'] = 'Ajouter un serveur';
+$lang['Centoswebpanel.add_module_group'] = 'Ajouter un groupe de serveurs';
+$lang['Centoswebpanel.manage.module_rows_title'] = 'Serveurs';
+$lang['Centoswebpanel.manage.module_groups_title'] = 'Groupes de serveurs';
+$lang['Centoswebpanel.manage.module_rows_heading.name'] = 'Label du serveur';
+$lang['Centoswebpanel.manage.module_rows_heading.hostname'] = 'Nom d\'hôte';
+$lang['Centoswebpanel.manage.module_rows_heading.accounts'] = 'Comptes';
+$lang['Centoswebpanel.manage.module_rows_heading.options'] = 'Options';
+$lang['Centoswebpanel.manage.module_groups_heading.name'] = 'Nom du groupe';
+$lang['Centoswebpanel.manage.module_groups_heading.servers'] = 'Nombre de serveurs';
+$lang['Centoswebpanel.manage.module_groups_heading.options'] = 'Options';
+$lang['Centoswebpanel.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Centoswebpanel.manage.module_rows.edit'] = 'Editer';
+$lang['Centoswebpanel.manage.module_groups.edit'] = 'Editer';
+$lang['Centoswebpanel.manage.module_rows.delete'] = 'Supprimer';
+$lang['Centoswebpanel.manage.module_groups.delete'] = 'Supprimer';
+$lang['Centoswebpanel.manage.module_rows.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce serveur ?';
+$lang['Centoswebpanel.manage.module_groups.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce groupe de serveurs ?';
+$lang['Centoswebpanel.manage.module_rows_no_results'] = 'Il n\'y a pas de serveurs.';
+$lang['Centoswebpanel.manage.module_groups_no_results'] = 'Il n\'y a pas de groupes de serveurs.';
+$lang['Centoswebpanel.order_options.first'] = 'Premier serveur non complet';
+$lang['Centoswebpanel.order_options.roundrobin'] = 'Répartir uniformément entre les serveurs';
+$lang['Centoswebpanel.add_row.box_title'] = 'Ajouter le serveur WebPanel CentOS';
+$lang['Centoswebpanel.add_row.basic_title'] = 'Paramètres de base';
+$lang['Centoswebpanel.add_row.name_servers_title'] = 'Serveurs de noms';
+$lang['Centoswebpanel.add_row.notes_title'] = 'Notes';
+$lang['Centoswebpanel.add_row.name_server_btn'] = 'Ajouter un serveur de noms supplémentaire';
+$lang['Centoswebpanel.add_row.name_server_col'] = 'Serveur de noms';
+$lang['Centoswebpanel.add_row.name_server_host_col'] = 'Nom d\'hôte';
+$lang['Centoswebpanel.add_row.name_server'] = 'Serveur de noms %1$s';
+$lang['Centoswebpanel.add_row.remove_name_server'] = 'Retirer';
+$lang['Centoswebpanel.add_row.add_btn'] = 'Ajouter un serveur';
+$lang['Centoswebpanel.edit_row.box_title'] = 'Modifier le serveur WebPanel CentOS';
+$lang['Centoswebpanel.edit_row.basic_title'] = 'Paramètres de base';
+$lang['Centoswebpanel.edit_row.name_servers_title'] = 'Serveurs de noms';
+$lang['Centoswebpanel.edit_row.notes_title'] = 'Notes';
+$lang['Centoswebpanel.edit_row.name_server_btn'] = 'Ajouter un serveur de noms supplémentaire';
+$lang['Centoswebpanel.edit_row.name_server_col'] = 'Serveur de noms';
+$lang['Centoswebpanel.edit_row.name_server_host_col'] = 'Nom d\'hôte';
+$lang['Centoswebpanel.edit_row.name_server'] = 'Serveur de noms %1$s';
+$lang['Centoswebpanel.edit_row.remove_name_server'] = 'Retirer';
+$lang['Centoswebpanel.edit_row.add_btn'] = 'Editer le serveur';
+$lang['Centoswebpanel.row_meta.server_name'] = 'Label du serveur';
+$lang['Centoswebpanel.row_meta.host_name'] = 'Nom d\'hôte';
+$lang['Centoswebpanel.row_meta.login_port'] = 'Port de connexion';
+$lang['Centoswebpanel.row_meta.default_login_port'] = '2031';
+$lang['Centoswebpanel.row_meta.port'] = 'Port API';
+$lang['Centoswebpanel.row_meta.default_port'] = '2304';
+$lang['Centoswebpanel.row_meta.api_key'] = 'Clé API';
+$lang['Centoswebpanel.row_meta.use_ssl'] = 'Utiliser SSL lors de la connexion à l\'API (recommandé)';
+$lang['Centoswebpanel.row_meta.account_limit'] = 'Limite du compte';
+$lang['Centoswebpanel.package_fields.package'] = 'ID du paquet';
+$lang['Centoswebpanel.package_fields.inode'] = 'Nombre maximal d\'inodes';
+$lang['Centoswebpanel.package_fields.nofile'] = 'Maximum de fichiers';
+$lang['Centoswebpanel.package_fields.nproc'] = 'Processus maximal';
+$lang['Centoswebpanel.service_field.domain'] = 'Domaine';
+$lang['Centoswebpanel.service_field.username'] = 'Nom d\'utilisateur';
+$lang['Centoswebpanel.service_field.password'] = 'Mot de passe';
+$lang['Centoswebpanel.service_info.username'] = 'Nom d\'utilisateur';
+$lang['Centoswebpanel.service_info.password'] = 'Mot de passe';
+$lang['Centoswebpanel.service_info.server'] = 'Serveur';
+$lang['Centoswebpanel.service_info.options'] = 'Options';
+$lang['Centoswebpanel.service_info.option_login'] = 'Se connecter';
+$lang['Centoswebpanel.service_field.tooltip.domain_edit'] = 'Cette modification n\'affectera pas CentOS Web Panel, mais changera seulement l\'enregistrement local dans Blesta.';
+$lang['Centoswebpanel.service_field.tooltip.username_edit'] = 'Cette modification n\'affectera pas CentOS Web Panel, mais changera seulement l\'enregistrement local dans Blesta.';
+$lang['Centoswebpanel.service_field.tooltip.username'] = 'Vous pouvez laisser le nom d\'utilisateur vide pour qu\'il soit généré automatiquement.';
+$lang['Centoswebpanel.service_field.tooltip.password'] = 'Vous pouvez laisser le mot de passe en blanc pour qu\'il soit généré automatiquement.';
+$lang['Centoswebpanel.!error.server_name_valid'] = 'Vous devez saisir une étiquette de serveur.';
+$lang['Centoswebpanel.!error.host_name_valid'] = 'Le nom d\'hôte semble être invalide.';
+$lang['Centoswebpanel.!error.user_name_valid'] = 'Le nom d\'utilisateur semble être invalide.';
+$lang['Centoswebpanel.!error.login_port_valid'] = 'Vous devez entrer un port de connexion.';
+$lang['Centoswebpanel.!error.port_valid'] = 'Vous devez saisir un port API.';
+$lang['Centoswebpanel.!error.remote_api_key_valid'] = 'La clé API semble être invalide.';
+$lang['Centoswebpanel.!error.remote_api_key_valid_connection'] = 'La connexion au serveur n\'a pas pu être établie. Veuillez vérifier que le nom d\'hôte et la clé API sont corrects.';
+$lang['Centoswebpanel.!error.account_limit_valid'] = 'Le champ Limite de compte doit être laissé vide (pour les comptes illimités) ou défini sur une valeur entière.';
+$lang['Centoswebpanel.!error.name_servers_valid'] = 'Un ou plusieurs des serveurs de noms saisis ne sont pas valides.';
+$lang['Centoswebpanel.!error.name_servers_count'] = 'Vous devez définir au moins 2 serveurs de noms.';
+$lang['Centoswebpanel.!error.meta[package].empty'] = 'Un paquet WebPanel CentOS est nécessaire.';
+$lang['Centoswebpanel.!error.meta[inode].valid'] = 'Le nombre maximum d\'inodes doit être une valeur numérique.';
+$lang['Centoswebpanel.!error.meta[nofile].valid'] = 'Le nombre maximum de fichiers doit être une valeur numérique.';
+$lang['Centoswebpanel.!error.meta[nproc].valid'] = 'Le processus maximal doit être une valeur numérique.';
+$lang['Centoswebpanel.!error.api.internal'] = 'Une erreur interne s\'est produite ou le serveur n\'a pas répondu à la demande.';
+$lang['Centoswebpanel.!error.module_row.missing'] = 'Une erreur interne s\'est produite. La ligne du module n\'est pas disponible.';
+$lang['Centoswebpanel.!error.centoswebpanel_domain.format'] = 'Veuillez saisir un nom de domaine valide, par exemple domain.com.';
+$lang['Centoswebpanel.!error.centoswebpanel_domain.test'] = 'Le nom de domaine ne peut pas commencer par "test".';
+$lang['Centoswebpanel.!error.centoswebpanel_username.format'] = 'Le nom d\'utilisateur ne peut contenir que des lettres et des chiffres et ne peut pas commencer par un chiffre.';
+$lang['Centoswebpanel.!error.centoswebpanel_username.test'] = 'Le nom d\'utilisateur ne peut pas commencer par "test".';
+$lang['Centoswebpanel.!error.centoswebpanel_username.length'] = 'Le nom d\'utilisateur doit comporter entre 1 et 16 caractères.';
+$lang['Centoswebpanel.!error.centoswebpanel_password.valid'] = 'Le mot de passe doit comporter au moins 8 caractères.';
+$lang['Centoswebpanel.!error.centoswebpanel_password.matches'] = 'Le mot de passe et le mot de passe de confirmation ne correspondent pas.';
+$lang['Centoswebpanel.!error.api'] = 'Une erreur interne s\'est produite ou le serveur n\'a pas répondu à la demande.';
+
