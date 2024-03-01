6990b9a3fa680dcb0b34ed42748a9de1aa06d4e7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:46 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/wizard/language/el_gr/summary.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Summary
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Summary.index.have_coupon'] = 'Χρησιμοποιήστε έναν κωδικό κουπονιού';
+$lang['Summary.index.text_free_domain'] = 'Διαμορφώστε ένα πακέτο %1$s για τουλάχιστον %2$s για να δικαιούστε δωρεάν domain.';
+$lang['Summary.index.coupon'] = 'Κουπόνι';
+$lang['Summary.index.coupon_btn'] = 'Εξαργύρωση';
+$lang['Summary.index.heading_summary'] = 'Περίληψη παραγγελίας';
+$lang['Summary.index.summary_note'] = 'Οποιοσδήποτε ισχύων φόρος θα υπολογιστεί αφού εγγραφείτε ή συνδεθείτε.';
+$lang['Summary.index.table_description'] = 'Περιγραφή';
+$lang['Summary.index.table_price'] = 'Τιμή';
+$lang['Summary.index.empty_cart'] = 'Άδειο καλάθι';
+$lang['Summary.index.heading_paymethods'] = 'Μέθοδος πληρωμής';
+$lang['Summary.index.paymethods_note'] = 'Παρακαλούμε επιλέξτε την επιθυμητή μέθοδο πληρωμής.';
+$lang['Summary.index.term_day'] = '%1$s Ημέρα';
+$lang['Summary.index.term_days'] = '%1$s Ημέρες';
+$lang['Summary.index.term_week'] = '%1$s Εβδομάδα';
+$lang['Summary.index.term_weeks'] = '%1$s Εβδομάδες';
+$lang['Summary.index.term_month'] = '%1$s Μήνας';
+$lang['Summary.index.term_months'] = '%1$s Μήνες';
+$lang['Summary.index.term_year'] = '%1$s Έτος';
+$lang['Summary.index.term_years'] = '%1$s Έτη';
+
