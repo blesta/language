5a45b4860a017bce4b943e90769b3eb91201e6d0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:06 -0800

    Updated it_it language files

---
+++ components/gateways/merchant/blue_pay/language/it_it/blue_pay.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Blue Pay
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Blue_pay.test_mode'] = 'Modalità test';
+$lang['Blue_pay.secret_key'] = 'Chiave segreta';
+$lang['Blue_pay.account_id'] = 'ID conto';
+$lang['Blue_pay.description'] = 'BluePay offre soluzioni di elaborazione dei pagamenti rapide, semplici e sicure, adatte alle esigenze di ogni commerciante. Opera in Nord America';
+$lang['Blue_pay.name'] = 'BluePay';
+$lang['Blue_pay.!error.test_mode.valid'] = 'La modalità di test deve essere impostata su "true", se fornita.';
+$lang['Blue_pay.!error.secret_key.empty'] = 'Inserire la chiave segreta.';
+$lang['Blue_pay.!error.account_id.empty'] = 'Inserire l\'ID dell\'account.';
+
