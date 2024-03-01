edc9dae38f7096bf9167fd1185bc9a09d7dc94e6
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:49 -0800

    Updated cs_cz language files

---
+++ plugins/import_manager/components/migrators/whmcs/8.0/language/cs_cz/whmcs8_0.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Whmcs8
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Whmcs8_0.configuration.no_local_packages'] = 'Před mapováním balíčků je nutné je vytvořit.';
+$lang['Whmcs8_0.configuration.remote_package'] = 'Vzdálený balíček';
+$lang['Whmcs8_0.configuration.local_package'] = 'Místní balíček';
+$lang['Whmcs8_0.configuration.create_packages_false'] = 'Ruční mapování balíčků';
+$lang['Whmcs8_0.configuration.create_packages_true'] = 'Automatické vytvoření všech potřebných balíčků během importu';
+$lang['Whmcs8_0.settings.enable_debug'] = 'Povolení ladění';
+$lang['Whmcs8_0.settings.balance_credit.info'] = 'Pokud je zaškrtnuto, vygeneruje se transakce nebo faktura, aby se zajistilo, že kredit klienta v systému Blesta odpovídá kreditu nastavenému pro klienta v systému WHMCS. To je nutné, protože systém WHMCS nezohledňuje správně klientské kredity.';
+$lang['Whmcs8_0.settings.balance_credit'] = 'Automatické vyrovnávání transakcí podle kreditů klienta v systému WHMCS';
+$lang['Whmcs8_0.settings.key.info'] = 'Jedná se o šifrovací klíč, který systém WHMCS používá k šifrování různých bitů dat. Najdete ho v souboru WHMCS configuration.php.';
+$lang['Whmcs8_0.settings.key'] = 'Šifrování CC Hash';
+$lang['Whmcs8_0.settings.pass'] = 'Heslo databáze';
+$lang['Whmcs8_0.settings.user'] = 'Uživatel databáze';
+$lang['Whmcs8_0.settings.database'] = 'Název databáze';
+$lang['Whmcs8_0.settings.host'] = 'Hostitel databáze';
+$lang['Whmcs8_0.!notice.gmp'] = 'Rozšíření \'gmp\' nebylo nalezeno. Toto rozšíření se doporučuje pro zlepšení výkonu a zkvalitnění procesu importu. Můžete pokračovat bez tohoto rozšíření, ale proces importu může být mnohem pomalejší.';
+$lang['Whmcs8_0.!error.import'] = 'Import byl dokončen, ale vyskytly se následující chyby:';
+$lang['Whmcs8_0.!error.key.invalid'] = 'Je vyžadován platný AESKEY.';
+$lang['Whmcs8_0.!error.pass.invalid'] = 'Je vyžadováno heslo k databázi.';
+$lang['Whmcs8_0.!error.user.invalid'] = 'Uživatel databáze je vyžadován.';
+$lang['Whmcs8_0.!error.database.invalid'] = 'Je vyžadován název databáze.';
+$lang['Whmcs8_0.!error.host.invalid'] = 'Je vyžadován Hostitel databáze.';
+
