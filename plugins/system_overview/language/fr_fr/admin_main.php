81493cc59c528870611b11c4756c030038c3d812
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:13 -0800

    Updated fr_fr language files

---
+++ plugins/system_overview/language/fr_fr/admin_main.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.options_updated'] = 'Les paramètres de la vue d\'ensemble du système ont été mis à jour avec succès !';
+$lang['AdminMain.index.boxtitle_overview'] = 'Vue d\'ensemble du système';
+$lang['AdminMain.overview.statistic.clients_active'] = 'Clients actifs';
+$lang['AdminMain.overview.statistic.services_active'] = 'Services actifs';
+$lang['AdminMain.overview.statistic.services_scheduled_cancellation'] = 'Services dont l\'annulation est prévue';
+$lang['AdminMain.overview.statistic.active_users_today'] = 'Utilisateurs actifs aujourd\'hui';
+$lang['AdminMain.overview.statistic.recurring_invoices'] = 'Factures récurrentes';
+$lang['AdminMain.overview.statistic.pending_orders'] = 'Ordres en suspens';
+$lang['AdminMain.overview.statistic.open_tickets'] = 'Billets ouverts';
+$lang['AdminMain.overview.heading_users'] = 'Utilisateurs récents';
+$lang['AdminMain.overview.tab_all'] = 'Graphiques';
+$lang['AdminMain.overview.tab_clients'] = 'Clients';
+$lang['AdminMain.overview.tab_services'] = 'Services';
+$lang['AdminMain.overview.tooltip_activity'] = 'Dernière activité';
+$lang['AdminMain.overview.tooltip_location'] = 'Localisation';
+$lang['AdminMain.overview.tooltip_last_activity_now'] = 'En ce moment même';
+$lang['AdminMain.overview.tooltip_last_activity_minute'] = 'il y a 1 minute';
+$lang['AdminMain.overview.tooltip_last_activity_minutes'] = 'Il y a %1$s minutes';
+$lang['AdminMain.date_range.days'] = '%1$s Days';
+$lang['AdminMain.settings.heading_statistics'] = 'Statistiques';
+$lang['AdminMain.settings.text_options'] = 'Cochez les options qui apparaîtront dans la vue d\'ensemble du système.';
+$lang['AdminMain.settings.clients_active'] = 'Clients actifs';
+$lang['AdminMain.settings.services_active'] = 'Services actifs';
+$lang['AdminMain.settings.services_scheduled_cancellation'] = 'Services dont l\'annulation est prévue';
+$lang['AdminMain.settings.active_users_today'] = 'Utilisateurs actifs aujourd\'hui';
+$lang['AdminMain.settings.recurring_invoices'] = 'Factures récurrentes';
+$lang['AdminMain.settings.open_tickets'] = 'Billets ouverts';
+$lang['AdminMain.settings.pending_orders'] = 'Ordres en suspens';
+$lang['AdminMain.settings.heading_clients'] = 'Onglet Clients';
+$lang['AdminMain.settings.text_graphs_clients'] = 'Vérifiez quels graphiques doivent apparaître dans l\'onglet Clients.';
+$lang['AdminMain.settings.graph_clients'] = 'Graphique des clients';
+$lang['AdminMain.settings.heading_services'] = 'Onglet Services';
+$lang['AdminMain.settings.text_graphs_services'] = 'Vérifiez quels graphiques doivent apparaître dans l\'onglet Services.';
+$lang['AdminMain.settings.graph_services'] = 'Graphique des services';
+$lang['AdminMain.settings.heading_settings'] = 'Paramètres';
+$lang['AdminMain.settings.show_one_tab'] = 'Afficher un seul onglet';
+$lang['AdminMain.settings.tooltip_show_one_tab'] = 'Ne sépare plus les graphiques dans des onglets, mais les affiche tous dans un seul onglet.';
+$lang['AdminMain.settings.show_legend'] = 'Afficher la légende';
+$lang['AdminMain.settings.date_range'] = 'Plage de dates du graphique';
+$lang['AdminMain.settings.submit_cancel'] = 'Retour à la vue d\'ensemble';
+$lang['AdminMain.settings.submit_settings'] = 'Mise à jour des paramètres';
+$lang['AdminMain.graph_name.graph_services'] = 'Services';
+$lang['AdminMain.graph_name.graph_clients'] = 'Clients';
+$lang['AdminMain.graph_line_name.active'] = 'Actif';
+$lang['AdminMain.graph_line_name.canceled'] = 'Annulé';
+$lang['AdminMain.graph_line_name.suspended'] = 'Suspendu';
+$lang['AdminMain.graph_line_name.new'] = 'Nouveau';
+
