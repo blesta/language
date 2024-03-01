5e3753465585c9a0b8b9d17d9e5f243fc8ced253
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:40 -0800

    Updated fr_fr language files

---
+++ core/Util/Tax/language/fr_fr/europe_tax.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Europe Tax
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.Tax.europe_tax.tax_provider_name'] = 'UE VIES TVA';
+$lang['Util.Tax.europe_tax.tax_id_name'] = 'VATIN';
+$lang['Util.Tax.europe_tax.tax_deduction_note'] = '%1$s 0%% de %2$s';
+$lang['Util.Tax.europe_tax.tax_note_reverse_charge'] = 'Le mécanisme d\'autoliquidation de la TVA est utilisé. L\'obligation fiscale est transférée à l\'acheteur.';
+$lang['Util.Tax.europe_tax.fields.enable_eu_vat'] = 'Activer la validation de la TVA de l\'UE';
+$lang['Util.Tax.europe_tax.fields.note_enable_eu_vat'] = 'Cochez cette option pour activer la validation du numéro de TVA de l\'UE pour cette entreprise. Cette option nécessite l\'extension PHP soap.';
+$lang['Util.Tax.europe_tax.fields.tax_exempt_eu_vat'] = 'Automatiquement défini comme exonéré d\'impôt';
+$lang['Util.Tax.europe_tax.fields.note_tax_exempt_eu_vat'] = 'Définir automatiquement un client comme exonéré de taxe si son numéro de TVA est valide et ne provient pas de votre foyer fiscal.';
+$lang['Util.Tax.europe_tax.fields.tax_home_eu_vat'] = 'Accueil fiscal';
+
