119b8e019ed95a17ddfe174cb17f8bdf830235c4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:36 -0800

    Updated de_de language files

---
+++ language/de_de/admin_system_automation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin System Automation
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemAutomation.index.note_cron_command'] = 'Dies ist ein Beispiel für einen Cron-Befehl, der zur Erstellung eines Cron-Jobs auf Ihrem Server verwendet werden kann. Achten Sie beim Einrichten des Cron-Jobs darauf, den Cron-Befehl zu aktualisieren, damit er auf den Ort verweist, an dem PHP installiert ist, falls er sich von dem in diesem Beispiel gezeigten unterscheidet.';
+$lang['AdminSystemAutomation.index.text_generate_code'] = 'Code generieren';
+$lang['AdminSystemAutomation.index.text_update_key'] = 'Cron-Schlüssel aktualisieren';
+$lang['AdminSystemAutomation.index.text_cron_currently_running'] = 'Der Cron läuft gerade.';
+$lang['AdminSystemAutomation.index.text_cron_never_ran'] = 'Der Cron wurde noch nie ausgeführt.';
+$lang['AdminSystemAutomation.index.text_cron_last_ran'] = 'Der Cron lief zuletzt am %1$s.';
+$lang['AdminSystemAutomation.index.field_cronkey_submit'] = 'Cron-Schlüssel aktualisieren';
+$lang['AdminSystemAutomation.index.field_cron_key'] = 'Cron-Schlüssel';
+$lang['AdminSystemAutomation.index.field_runcron'] = 'Cron manuell ausführen';
+$lang['AdminSystemAutomation.index.field_croncommand'] = 'Beispiel Cron-Befehl';
+$lang['AdminSystemAutomation.index.heading_status'] = 'Cron-Status';
+$lang['AdminSystemAutomation.index.boxtitle_automation'] = 'Automatisierung';
+$lang['AdminSystemAutomation.index.page_title'] = 'Einstellungen > System > Automatisierung';
+$lang['AdminSystemAutomation.!success.cron_key'] = 'Der Cron-Schlüssel wurde erfolgreich aktualisiert!';
+$lang['AdminSystemAutomation.!success.automation_updated'] = 'Die Automatisierungseinstellungen wurden erfolgreich aktualisiert!';
+$lang['AdminSystemAutomation.!error.empty_cron_key'] = 'Bitte setzen Sie einen Cron-Schlüssel.';
+
