9f84004c9dc65eef008f13bc0ad6181d8985a0de
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:59 -0800

    Updated el_gr language files

---
+++ components/modules/pterodactyl/language/el_gr/pterodactyl.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Pterodactyl
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pterodactyl.!actions.restart'] = 'Επανεκκίνηση';
+$lang['Pterodactyl.!actions.stop'] = 'Σταματήστε το';
+$lang['Pterodactyl.!actions.start'] = 'Έναρξη';
+$lang['Pterodactyl.tab_client_actions.status_stopping'] = 'Διακοπή';
+$lang['Pterodactyl.tab_client_actions.status_starting'] = 'Ξεκινώντας';
+$lang['Pterodactyl.tab_client_actions.status_offline'] = 'Offline';
+$lang['Pterodactyl.tab_client_actions.status_running'] = 'Online';
+$lang['Pterodactyl.tab_client_actions.server_status'] = 'Κατάσταση διακομιστή';
+$lang['Pterodactyl.tab_client_actions.heading_actions'] = 'Δράσεις';
+$lang['Pterodactyl.tab_actions.status_stopping'] = 'Διακοπή';
+$lang['Pterodactyl.tab_actions.status_starting'] = 'Ξεκινώντας';
+$lang['Pterodactyl.tab_actions.status_offline'] = 'Offline';
+$lang['Pterodactyl.tab_actions.status_running'] = 'Online';
+$lang['Pterodactyl.tab_actions.server_status'] = 'Κατάσταση διακομιστή';
+$lang['Pterodactyl.tab_actions.heading_actions'] = 'Δράσεις';
+$lang['Pterodactyl.tab_client_actions'] = 'Ενέργειες διακομιστή';
+$lang['Pterodactyl.tab_actions'] = 'Ενέργειες διακομιστή';
+$lang['Pterodactyl.service_info.login'] = 'URL σύνδεσης';
+$lang['Pterodactyl.service_info.password'] = 'Κωδικός πρόσβασης';
+$lang['Pterodactyl.service_info.username'] = 'Όνομα χρήστη';
+$lang['Pterodactyl.manage.module_groups_no_results'] = 'Δεν υπάρχουν ομάδες διακομιστών.';
+$lang['Pterodactyl.manage.module_rows_no_results'] = 'Δεν υπάρχουν διακομιστές.';
+$lang['Pterodactyl.manage.module_groups.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την ομάδα διακομιστών;';
+$lang['Pterodactyl.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον διακομιστή;';
+$lang['Pterodactyl.manage.module_groups.delete'] = 'Διαγραφή';
+$lang['Pterodactyl.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['Pterodactyl.manage.module_groups.edit'] = 'Επεξεργασία';
+$lang['Pterodactyl.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['Pterodactyl.manage.module_groups_heading.options'] = 'Επιλογές';
+$lang['Pterodactyl.manage.module_groups_heading.servers'] = 'Αριθμός διακομιστών';
+$lang['Pterodactyl.manage.module_groups_heading.name'] = 'Όνομα ομάδας';
+$lang['Pterodactyl.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['Pterodactyl.manage.module_rows_heading.host_name'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Pterodactyl.manage.module_rows_heading.name'] = 'Ετικέτα διακομιστή';
+$lang['Pterodactyl.manage.module_groups_title'] = 'Ομάδες διακομιστών';
+$lang['Pterodactyl.manage.module_rows_title'] = 'Servers';
+$lang['Pterodactyl.add_module_group'] = 'Προσθήκη ομάδας διακομιστών';
+$lang['Pterodactyl.add_module_row'] = 'Προσθήκη διακομιστή';
+$lang['Pterodactyl.order_options.first'] = 'Πρώτος μη πλήρης διακομιστής';
+$lang['Pterodactyl.!tooltip.application_api_key'] = 'Εισάγετε το κλειδί API εφαρμογής που δημιουργήθηκε για τον πίνακα Pterodactyl.';
+$lang['Pterodactyl.!tooltip.account_api_key'] = 'Εισάγετε το κλειδί API λογαριασμού που δημιουργήθηκε για τον πίνακα Pterodactyl.';
+$lang['Pterodactyl.!tooltip.host_name'] = 'Εισάγετε το όνομα κεντρικού υπολογιστή ή την IP του πίνακα Pterodactyl. π.χ. panel.domain.com';
+$lang['Pterodactyl.row_meta.application_api_key'] = 'Κλειδί API εφαρμογής';
+$lang['Pterodactyl.row_meta.account_api_key'] = 'Κλειδί API λογαριασμού';
+$lang['Pterodactyl.row_meta.use_ssl'] = 'Χρησιμοποιήστε SSL κατά τη σύνδεση στο API (συνιστάται)';
+$lang['Pterodactyl.row_meta.host_name'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Pterodactyl.row_meta.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Pterodactyl.edit_row.add_btn'] = 'Επεξεργασία διακομιστή';
+$lang['Pterodactyl.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Pterodactyl.edit_row.box_title'] = 'Επεξεργασία διακομιστή Pterodactyl';
+$lang['Pterodactyl.add_row.add_btn'] = 'Προσθήκη διακομιστή';
+$lang['Pterodactyl.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Pterodactyl.add_row.box_title'] = 'Προσθήκη διακομιστή Pterodactyl';
+$lang['Pterodactyl.!success.restart'] = 'Ο διακομιστής κάνει επανεκκίνηση.';
+$lang['Pterodactyl.!success.stop'] = 'Ο διακομιστής κλείνει τώρα.';
+$lang['Pterodactyl.!success.start'] = 'Ο διακομιστής εκκινείται τώρα.';
+$lang['Pterodactyl.!error.module_row.missing'] = 'Εμφανίστηκε ένα εσωτερικό σφάλμα. Η σειρά της μονάδας δεν είναι διαθέσιμη.';
+$lang['Pterodactyl.!error.application_api_key.valid'] = 'Δεν είναι δυνατή η σύνδεση με τη δεδομένη διεύθυνση URL και το κλειδί API εφαρμογής.';
+$lang['Pterodactyl.!error.application_api_key.empty'] = 'Πρέπει να εισαγάγετε ένα κλειδί API εφαρμογής.';
+$lang['Pterodactyl.!error.account_api_key.valid'] = 'Δεν είναι δυνατή η σύνδεση με τη συγκεκριμένη διεύθυνση URL και το κλειδί API του λογαριασμού.';
+$lang['Pterodactyl.!error.account_api_key.empty'] = 'Πρέπει να εισαγάγετε ένα κλειδί API λογαριασμού.';
+$lang['Pterodactyl.!error.host_name.valid'] = 'Το όνομα κεντρικού υπολογιστή φαίνεται να είναι άκυρο.';
+$lang['Pterodactyl.!error.server_name.empty'] = 'Πρέπει να εισαγάγετε ένα όνομα διακομιστή.';
+$lang['Pterodactyl.module_group'] = 'Ομάδα διακομιστών';
+$lang['Pterodactyl.module_row_plural'] = 'Servers';
+$lang['Pterodactyl.module_row'] = 'Διακομιστής';
+$lang['Pterodactyl.description'] = 'Προβλέπει και διαχειρίζεται διακομιστές παιχνιδιών μέσω του πίνακα Pterodactyl.';
+$lang['Pterodactyl.name'] = 'Πτεροδάκτυλος';
+
