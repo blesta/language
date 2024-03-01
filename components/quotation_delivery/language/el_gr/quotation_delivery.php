168875e579e5ee892bf3b9c50235f662f7c993b1
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:07 -0800

    Updated el_gr language files

---
+++ components/quotation_delivery/language/el_gr/quotation_delivery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Quotation Delivery
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['QuotationDelivery.deliverinvoices.postalmethods_description'] = 'Παράθεση #%1$s';
+$lang['QuotationDelivery.deliverinvoices.interfax_subject'] = 'Παράθεση #%1$s';
+$lang['QuotationDelivery.!error.libxml_required'] = 'Οι επεκτάσεις libxml και simplexml απαιτούνται για την παράδοση τιμολογίων.';
+
