fb4b36992f3a5569f4d9779abdb1200cd82962be
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:38 -0800

    Updated cs_cz language files

---
+++ plugins/support_manager/language/cs_cz/admin_knowledgebase.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Admin Knowledgebase
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminKnowledgebase.!tooltip.access_category'] = 'Veřejné kategorie jsou přístupné všem. Soukromé kategorie jsou přístupné pouze přihlášeným uživatelům. Skryté kategorie jsou nepřístupné. Zaměstnanecké kategorie jsou viditelné pouze pro zaměstnance.';
+$lang['AdminKnowledgebase.!tooltip.access_article'] = 'Veřejné články jsou přístupné všem. Soukromé články jsou přístupné pouze přihlášeným uživatelům. Skryté články jsou nepřístupné. Zaměstnanecké články jsou viditelné pouze pro zaměstnance.';
+$lang['AdminKnowledgebase.!text.directory_separator'] = '/';
+$lang['AdminKnowledgebase.!text.add_category'] = 'Přidat kategorii';
+$lang['AdminKnowledgebase.!text.enclosure_separator'] = '/';
+$lang['AdminKnowledgebase.!text.closed_enclosure'] = ')';
+$lang['AdminKnowledgebase.!text.open_enclosure'] = '(';
+$lang['AdminKnowledgebase.!text.root_directory'] = 'Domovský adresář';
+$lang['AdminKnowledgebase.select.none'] = '-- Žádné --';
+$lang['AdminKnowledgebase.article_content.body'] = 'Tělo';
+$lang['AdminKnowledgebase.article_content.content_type'] = 'Typ obsahu';
+$lang['AdminKnowledgebase.article_content.title'] = 'Název';
+$lang['AdminKnowledgebase.edit.submit_cancel'] = 'Zrušit';
+$lang['AdminKnowledgebase.edit.submit_edit'] = 'Aktualizace článku';
+$lang['AdminKnowledgebase.edit.field_categories'] = 'Kategorie';
+$lang['AdminKnowledgebase.edit.field_access'] = 'Přístupnost';
+$lang['AdminKnowledgebase.edit.boxtitle_edit'] = 'Aktualizace článku';
+$lang['AdminKnowledgebase.add.submit_cancel'] = 'Zrušit';
+$lang['AdminKnowledgebase.add.submit_add'] = 'Vytvořit článek';
+$lang['AdminKnowledgebase.add.field_categories'] = 'Kategorie';
+$lang['AdminKnowledgebase.add.field_access'] = 'Přístupnost';
+$lang['AdminKnowledgebase.add.boxtitle_add'] = 'Přidat článek';
+$lang['AdminKnowledgebase.editcategory.submit_cancel'] = 'Zrušit';
+$lang['AdminKnowledgebase.editcategory.submit_edit'] = 'Kategorie aktualizace';
+$lang['AdminKnowledgebase.editcategory.field_access'] = 'Přístupnost';
+$lang['AdminKnowledgebase.editcategory.field_description'] = 'Popis';
+$lang['AdminKnowledgebase.editcategory.field_parent_id'] = 'Nadřazená kategorie';
+$lang['AdminKnowledgebase.editcategory.field_name'] = 'Název';
+$lang['AdminKnowledgebase.editcategory.boxtitle_editcategory'] = 'Kategorie aktualizace';
+$lang['AdminKnowledgebase.addcategory.submit_cancel'] = 'Zrušit';
+$lang['AdminKnowledgebase.addcategory.submit_add'] = 'Vytvořit kategorii';
+$lang['AdminKnowledgebase.addcategory.field_access'] = 'Přístupnost';
+$lang['AdminKnowledgebase.addcategory.field_description'] = 'Popis';
+$lang['AdminKnowledgebase.addcategory.field_parent_id'] = 'Nadřazená kategorie';
+$lang['AdminKnowledgebase.addcategory.field_name'] = 'Název';
+$lang['AdminKnowledgebase.addcategory.boxtitle_addcategory'] = 'Přidat kategorii';
+$lang['AdminKnowledgebase.index.no_results'] = 'V této sekci nejsou žádné kategorie ani články.';
+$lang['AdminKnowledgebase.modal.delete_category'] = 'Opravdu chcete tuto kategorii odstranit? Všechny podkategorie a články v této kategorii budou přesunuty do nadřazené kategorie.';
+$lang['AdminKnowledgebase.modal.delete_article'] = 'Opravdu chcete tento článek smazat?';
+$lang['AdminKnowledgebase.index.delete'] = 'Odstranit';
+$lang['AdminKnowledgebase.index.edit'] = 'Upravit';
+$lang['AdminKnowledgebase.index.go_back'] = 'Přejděte o úroveň výš';
+$lang['AdminKnowledgebase.index.boxtitle_knowledgebase'] = 'Znalostní báze';
+$lang['AdminKnowledgebase.index.add_category'] = 'Přidat kategorii';
+$lang['AdminKnowledgebase.index.add_article'] = 'Přidat článek';
+$lang['AdminKnowledgebase.editcategory.page_title'] = 'Správce podpory > Databáze znalostí > Upravit kategorii';
+$lang['AdminKnowledgebase.addcategory.page_title'] = 'Správce podpory > Databáze znalostí > Přidat kategorii';
+$lang['AdminKnowledgebase.edit.page_title'] = 'Správce podpory > Databáze znalostí > Upravit článek';
+$lang['AdminKnowledgebase.add.page_title'] = 'Správce podpory > Databáze znalostí > Přidat článek';
+$lang['AdminKnowledgebase.index.page_title'] = 'Správce podpory > Databáze znalostí';
+$lang['AdminKnowledgebase.!success.article_deleted'] = 'Článek byl úspěšně smazán.';
+$lang['AdminKnowledgebase.!success.article_updated'] = 'Článek byl úspěšně aktualizován.';
+$lang['AdminKnowledgebase.!success.article_added'] = 'Článek byl úspěšně vytvořen.';
+$lang['AdminKnowledgebase.!success.category_deleted'] = 'Kategorie byla úspěšně odstraněna.';
+$lang['AdminKnowledgebase.!success.category_updated'] = 'Kategorie byla úspěšně aktualizována.';
+$lang['AdminKnowledgebase.!success.category_added'] = 'Kategorie byla úspěšně vytvořena.';
+
