bf9e0b6bc60a03d92394469cdfe53ae657cb937f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:02 -0800

    Updated el_gr language files

---
+++ components/modules/tcadmin/language/el_gr/tcadmin.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Tcadmin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Tcadmin.!error.private_password.valid'] = 'Ο προσωπικός κωδικός πρόσβασης πρέπει να έχει μήκος τουλάχιστον 8 χαρακτήρων.';
+$lang['Tcadmin.!error.rcon_password.valid'] = 'RCON Ο κωδικός πρόσβασης πρέπει να έχει μήκος τουλάχιστον 8 χαρακτήρων.';
+$lang['Tcadmin.!error.user_password.valid'] = 'Ο κωδικός πρόσβασης πρέπει να έχει μήκος τουλάχιστον 8 χαρακτήρων.';
+$lang['Tcadmin.!error.user_name.empty'] = 'Το όνομα χρήστη δεν μπορεί να είναι κενό.';
+$lang['Tcadmin.!error.hostname.test'] = 'Το όνομα τομέα δεν μπορεί να αρχίζει με \'test\'.';
+$lang['Tcadmin.!error.hostname.format'] = 'Παρακαλούμε εισάγετε ένα έγκυρο όνομα τομέα, π.χ. domain.com.';
+$lang['Tcadmin.!error.module_row.missing'] = 'Εμφανίστηκε ένα εσωτερικό σφάλμα. Η σειρά της μονάδας δεν είναι διαθέσιμη.';
+$lang['Tcadmin.!error.api.internal'] = 'Προέκυψε ένα εσωτερικό σφάλμα ή ο διακομιστής δεν απάντησε στην αίτηση.';
+$lang['Tcadmin.!error.meta[startup].valid'] = 'Παρακαλούμε επιλέξτε μια έγκυρη επιλογή εκκίνησης';
+$lang['Tcadmin.!error.meta[priority].valid'] = 'Παρακαλούμε επιλέξτε μια έγκυρη προτεραιότητα';
+$lang['Tcadmin.!error.meta[start].valid'] = 'Παρακαλούμε επιλέξτε μια έγκυρη επιλογή έναρξης';
+$lang['Tcadmin.!error.meta[supported_servers].empty'] = 'Επιλέξτε έναν από τους υποστηριζόμενους διακομιστές Tcadmin.';
+$lang['Tcadmin.!error.meta[server_type].valid'] = 'Παρακαλούμε επιλέξτε έναν έγκυρο τύπο διακομιστή.';
+$lang['Tcadmin.!error.account_limit_valid'] = 'Το Όριο λογαριασμού φαίνεται να είναι άκυρο.';
+$lang['Tcadmin.!error.password_valid'] = 'Ο κωδικός πρόσβασης φαίνεται να είναι άκυρος.';
+$lang['Tcadmin.!error.port_valid'] = 'Η θύρα φαίνεται να είναι άκυρη.';
+$lang['Tcadmin.!error.user_name_valid'] = 'Το όνομα χρήστη φαίνεται να είναι άκυρο.';
+$lang['Tcadmin.!error.host_name_valid'] = 'Το όνομα κεντρικού υπολογιστή φαίνεται να είναι άκυρο.';
+$lang['Tcadmin.!error.server_name_valid'] = 'Πρέπει να εισαγάγετε μια ετικέτα διακομιστή.';
+$lang['Tcadmin.service_info.option_login'] = 'Συνδεθείτε';
+$lang['Tcadmin.service_info.options'] = 'Επιλογές';
+$lang['Tcadmin.service_info.server'] = 'Διακομιστής';
+$lang['Tcadmin.service_info.password'] = 'Κωδικός πρόσβασης';
+$lang['Tcadmin.service_info.username'] = 'Όνομα χρήστη';
+$lang['Tcadmin.service_info.private_password'] = 'Ιδιωτικός κωδικός πρόσβασης';
+$lang['Tcadmin.service_info.rcon_password'] = 'Κωδικός πρόσβασης RCON';
+$lang['Tcadmin.service_info.hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Tcadmin.stored_locally_only'] = 'Αυτό το πεδίο θα ενημερώνεται μόνο τοπικά';
+$lang['Tcadmin.service_field.private_password'] = 'Ιδιωτικός κωδικός πρόσβασης';
+$lang['Tcadmin.service_field.rcon_password'] = 'Κωδικός πρόσβασης RCON';
+$lang['Tcadmin.service_field.user_password'] = 'Κωδικός πρόσβασης Tcadmin';
+$lang['Tcadmin.service_field.user_name'] = 'Όνομα χρήστη Tcadmin';
+$lang['Tcadmin.service_field.hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Tcadmin.package_fields.startup.disabled'] = 'Άτομα με ειδικές ανάγκες';
+$lang['Tcadmin.package_fields.startup.manual'] = 'Χειροκίνητο';
+$lang['Tcadmin.package_fields.startup.automatic'] = 'Αυτόματο';
+$lang['Tcadmin.package_fields.priority.realtime'] = 'Πραγματικός χρόνος';
+$lang['Tcadmin.package_fields.priority.idle'] = 'Αδράνεια';
+$lang['Tcadmin.package_fields.priority.high'] = 'Υψηλή';
+$lang['Tcadmin.package_fields.priority.normal'] = 'Κανονικό';
+$lang['Tcadmin.package_fields.priority.belownormal'] = 'Κάτω από το κανονικό';
+$lang['Tcadmin.package_fields.priority.abovenormal'] = 'Πάνω από το κανονικό';
+$lang['Tcadmin.package_fields.startup'] = 'Έναρξη λειτουργίας';
+$lang['Tcadmin.package_fields.priority'] = 'Προτεραιότητα';
+$lang['Tcadmin.package_fields.start'] = 'Έναρξη μετά τη Δημιουργία';
+$lang['Tcadmin.package_fields.no'] = 'Όχι';
+$lang['Tcadmin.package_fields.yes'] = 'Ναι';
+$lang['Tcadmin.package_fields.supported_servers'] = 'Υποστηριζόμενοι διακομιστές Tcadmin';
+$lang['Tcadmin.package_fields.voice_server'] = 'Διακομιστής φωνής';
+$lang['Tcadmin.package_fields.game_server'] = 'Διακομιστής παιχνιδιών';
+$lang['Tcadmin.package_fields.server_type'] = 'Τύπος διακομιστή';
+$lang['Tcadmin.row_meta.account_limit'] = 'Όριο λογαριασμού';
+$lang['Tcadmin.row_meta.use_ssl'] = 'Χρησιμοποιήστε SSL κατά τη σύνδεση στο API (συνιστάται)';
+$lang['Tcadmin.row_meta.port'] = 'Λιμάνι';
+$lang['Tcadmin.row_meta.password'] = 'Κωδικός πρόσβασης';
+$lang['Tcadmin.row_meta.user_name'] = 'Όνομα χρήστη';
+$lang['Tcadmin.row_meta.host_name'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Tcadmin.row_meta.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Tcadmin.edit_row.add_btn'] = 'Επεξεργασία διακομιστή';
+$lang['Tcadmin.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Tcadmin.edit_row.box_title'] = 'Επεξεργασία διακομιστή Tcadmin';
+$lang['Tcadmin.add_row.add_btn'] = 'Προσθήκη διακομιστή';
+$lang['Tcadmin.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Tcadmin.add_row.box_title'] = 'Προσθήκη διακομιστή Tcadmin';
+$lang['Tcadmin.order_options.first'] = 'Πρώτος μη πλήρης διακομιστής';
+$lang['Tcadmin.manage.module_groups_no_results'] = 'Δεν υπάρχουν ομάδες διακομιστών.';
+$lang['Tcadmin.manage.module_rows_no_results'] = 'Δεν υπάρχουν διακομιστές.';
+$lang['Tcadmin.manage.module_groups.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την ομάδα διακομιστών;';
+$lang['Tcadmin.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον διακομιστή;';
+$lang['Tcadmin.manage.module_groups.delete'] = 'Διαγραφή';
+$lang['Tcadmin.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['Tcadmin.manage.module_groups.edit'] = 'Επεξεργασία';
+$lang['Tcadmin.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['Tcadmin.manage.module_groups_heading.options'] = 'Επιλογές';
+$lang['Tcadmin.manage.module_groups_heading.servers'] = 'Αριθμός διακομιστών';
+$lang['Tcadmin.manage.module_groups_heading.name'] = 'Όνομα ομάδας';
+$lang['Tcadmin.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['Tcadmin.manage.module_rows_heading.port'] = 'Λιμάνι';
+$lang['Tcadmin.manage.module_rows_heading.hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Tcadmin.manage.module_rows_heading.name'] = 'Ετικέτα διακομιστή';
+$lang['Tcadmin.manage.module_groups_title'] = 'Ομάδες διακομιστών';
+$lang['Tcadmin.manage.module_rows_title'] = 'Servers';
+$lang['Tcadmin.add_module_group'] = 'Προσθήκη ομάδας διακομιστών';
+$lang['Tcadmin.add_module_row'] = 'Προσθήκη διακομιστή';
+$lang['Tcadmin.module_group'] = 'Ομάδα διακομιστών';
+$lang['Tcadmin.module_row_plural'] = 'Servers';
+$lang['Tcadmin.module_row'] = 'Διακομιστής';
+$lang['Tcadmin.description'] = 'Το TCAdmin, ο πίνακας ελέγχου φιλοξενίας παιχνιδιών, σχεδιάστηκε από την αρχή για να βοηθήσει τους σημερινούς παρόχους υπηρεσιών παιχνιδιών να εξοικονομήσουν χρόνο και χρήμα, ενώ παράλληλα επιτρέπει στους τελικούς χρήστες να έχουν πλήρη έλεγχο των διακομιστών τους.';
+$lang['Tcadmin.name'] = 'Tcadmin';
+
