af14daf3f5119aa07aeb04c3f0e88cc0bb92025e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:09 -0800

    Updated it_it language files

---
+++ components/gateways/merchant/eway/language/it_it/eway.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Eway
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Eway.tooltip_test_mode'] = 'La modalità di prova utilizzerà un account di prova eWAY con un numero di carta di credito di prova, indipendentemente da ciò che viene inserito. Per ulteriori informazioni sulla modalità di test in eWAY, consultare il manuale.';
+$lang['Eway.tooltip_developer_mode'] = 'Abilitando questa opzione, le transazioni verranno pubblicate nell\'ambiente sandbox di eWAY. Per utilizzare questo ambiente è necessario disporre di un account di prova per la sandbox.';
+$lang['Eway.tooltip_refund_password'] = 'Inserire la password di rimborso impostata nel proprio account eWAY. Questa non è la password dell\'account eWAY. È necessario impostare una password di rimborso per poter elaborare i rimborsi tramite eWAY.';
+$lang['Eway.test_mode'] = 'Modalità test';
+$lang['Eway.developer_mode'] = 'Modalità sviluppatore';
+$lang['Eway.refund_password'] = 'Password di rimborso';
+$lang['Eway.customer_id'] = 'ID cliente';
+$lang['Eway.description'] = 'eWAY rappresenta il 25% del mercato online australiano e opera in 5 paesi. Elabora miliardi di dollari di pagamenti ogni anno in tutto il mondo per decine di migliaia di aziende.';
+$lang['Eway.name'] = 'eWAY';
+$lang['Eway.!error.invalid_xml'] = 'Il gateway ha risposto con un XML non valido.';
+$lang['Eway.!error.libxml_required'] = 'Le estensioni libxml e simplexml sono necessarie per questo gateway.';
+$lang['Eway.!error.refund_password.empty'] = 'Per elaborare i rimborsi tramite eWAY è necessario impostare una password di rimborso.';
+$lang['Eway.!error.test_mode.valid'] = 'La modalità di test deve essere impostata su "true", se fornita.';
+$lang['Eway.!error.developer_mode.valid'] = 'La modalità sviluppatore deve essere impostata su "true", se fornita.';
+$lang['Eway.!error.customer_id.length'] = 'L\'ID cliente non può superare gli 8 caratteri.';
+$lang['Eway.!error.customer_id.empty'] = 'Inserire il proprio ID cliente eWAY.';
+
