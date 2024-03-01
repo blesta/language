831f548ec2200d5d21cd8a32444a0e6cf2ab493d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:38 -0800

    Updated it_it language files

---
+++ language/it_it/admin_reports.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admin Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminReports.index.customize'] = 'Personalizzare';
+$lang['AdminReports.index.reports'] = 'Generare';
+$lang['AdminReports.index.field_submit'] = 'Esportazione';
+$lang['AdminReports.index.field_report_type'] = 'Tipo di rapporto';
+$lang['AdminReports.index.field_format'] = 'Formato';
+$lang['AdminReports.index.box_title'] = 'Rapporti';
+$lang['AdminReports.index.page_title'] = 'Rapporti di fatturazione';
+
