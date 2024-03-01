c30257f207df0580991eb050ce1325ec14628fd4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:35 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/pricings.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Pricings
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pricings.!error.term.format'] = 'Le terme doit être un nombre.';
+$lang['Pricings.!error.term.length'] = 'La longueur du terme ne doit pas dépasser 5 caractères.';
+$lang['Pricings.!error.term.valid'] = 'Le terme doit être supérieur à 0.';
+$lang['Pricings.!error.period.format'] = 'Type de période non valide.';
+$lang['Pricings.!error.price.format'] = 'Le prix doit être un nombre.';
+$lang['Pricings.!error.price_renews.format'] = 'Le prix de renouvellement doit être un nombre.';
+$lang['Pricings.!error.price_renews.valid'] = 'Le prix de renouvellement ne peut pas être fixé pour une période donnée.';
+$lang['Pricings.!error.price_transfer.format'] = 'Le prix de renouvellement doit être un nombre.';
+$lang['Pricings.!error.setup_fee.format'] = 'Les frais de mise en place doivent être un nombre.';
+$lang['Pricings.!error.cancel_fee.format'] = 'Les frais d\'annulation doivent être un nombre.';
+$lang['Pricings.!error.currency.format'] = 'Le code devise doit comporter 3 caractères.';
+$lang['Pricings.!error.term.in_use'] = 'Le terme ne peut pas être mis à jour dans une tarification en cours d\'utilisation.';
+$lang['Pricings.!error.period.in_use'] = 'La période ne peut pas être mise à jour dans une tarification en cours d\'utilisation.';
+$lang['Pricings.!error.currency.in_use'] = 'La devise ne peut pas être mise à jour dans une tarification en cours d\'utilisation.';
+$lang['Pricings.getPeriods.day'] = 'Jour';
+$lang['Pricings.getPeriods.week'] = 'Semaine';
+$lang['Pricings.getPeriods.month'] = 'Mois';
+$lang['Pricings.getPeriods.year'] = 'Année';
+$lang['Pricings.getPeriods.onetime'] = 'Une seule fois';
+$lang['Pricings.getPeriods.day_plural'] = 'Jours';
+$lang['Pricings.getPeriods.week_plural'] = 'Semaines';
+$lang['Pricings.getPeriods.month_plural'] = 'Mois';
+$lang['Pricings.getPeriods.year_plural'] = 'Années';
+$lang['Pricings.getPeriods.onetime_plural'] = 'Une seule fois';
+
