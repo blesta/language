d726fc745d9e8951b338c2bdc9cf3fdb277f7217
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:18 -0800

    Updated cs_cz language files

---
+++ plugins/order/views/templates/ajax/language/cs_cz/cart.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Cart
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cart.index.order_more_note'] = 'Nebo se zaregistrujte/přihlaste a pokračujte v platbě.';
+$lang['Cart.index.order_more_btn'] = 'Klikněte pro objednání dalších položek';
+$lang['Cart.index.coupon'] = 'Kupón';
+$lang['Cart.index.have_coupon'] = 'Použijte kód kupónu';
+$lang['Cart.index.remove_item'] = 'Odstranění stránky';
+$lang['Cart.index.edit_item'] = 'Upravit';
+$lang['Cart.index.table_price'] = 'Cena';
+$lang['Cart.index.table_qty'] = 'Množství';
+$lang['Cart.index.table_term'] = 'Termín';
+$lang['Cart.index.table_description'] = 'Popis';
+$lang['Cart.index.header_note'] = 'Případné daně se vypočítají po registraci nebo přihlášení.';
+$lang['Cart.index.header_cart'] = 'Přehled objednávek';
+
