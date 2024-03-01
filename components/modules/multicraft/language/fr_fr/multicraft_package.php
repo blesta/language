7cc806d2f55edffd16bdb04b54d335fc6a3501f8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:56 -0800

    Updated fr_fr language files

---
+++ components/modules/multicraft/language/fr_fr/multicraft_package.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Multicraft Package
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MulticraftPackage.!error.meta[server_name].format'] = 'Veuillez indiquer un nom pour le serveur.';
+$lang['MulticraftPackage.!error.meta[players].format'] = 'Veuillez indiquer le nombre maximum de places de joueurs à autoriser.';
+$lang['MulticraftPackage.!error.meta[memory].format'] = 'Veuillez définir la quantité maximale de mémoire sous la forme d\'un nombre entier en Mo.';
+$lang['MulticraftPackage.!error.meta[jardir].format'] = 'Veuillez sélectionner un répertoire JAR valide.';
+$lang['MulticraftPackage.!error.meta[user_jar].format'] = 'La valeur 1 ou 0 doit être attribuée à la question de savoir si le propriétaire peut sélectionner le JAR.';
+$lang['MulticraftPackage.!error.meta[user_name].format'] = 'La valeur 1 ou 0 doit être attribuée à la question de savoir si le propriétaire peut définir un nom.';
+$lang['MulticraftPackage.!error.meta[user_schedule].format'] = 'La valeur 1 ou 0 doit être attribuée à la possibilité pour le propriétaire de planifier des tâches.';
+$lang['MulticraftPackage.!error.meta[user_ftp].format'] = 'La valeur 1 ou 0 doit être attribuée à la question de savoir si le propriétaire peut donner un accès FTP à d\'autres personnes.';
+$lang['MulticraftPackage.!error.meta[user_visibility].format'] = 'La valeur 1 ou 0 doit être attribuée à la question de savoir si le propriétaire peut modifier la visibilité du serveur.';
+$lang['MulticraftPackage.!error.meta[default_level].format'] = 'Veuillez sélectionner un rôle par défaut valide.';
+$lang['MulticraftPackage.!error.meta[autostart].format'] = 'La valeur 1 ou 0 doit être attribuée à l\'option de démarrage automatique du serveur.';
+$lang['MulticraftPackage.!error.meta[create_ftp].format'] = 'L\'option de création d\'un compte FTP doit être réglée sur 1 ou 0.';
+$lang['MulticraftPackage.!error.meta[server_visibility].format'] = 'Veuillez sélectionner une visibilité de serveur valide.';
+$lang['MulticraftPackage.package_fields.server_name'] = 'Nom du serveur';
+$lang['MulticraftPackage.package_fields.players'] = 'Machines à sous pour les joueurs';
+$lang['MulticraftPackage.package_fields.memory'] = 'RAM du serveur';
+$lang['MulticraftPackage.package_fields.tooltip.memory'] = 'Définir la mémoire en Mo, ou laisser vide pour utiliser la valeur par défaut.';
+$lang['MulticraftPackage.package_fields.jarfile'] = 'Fichier JAR';
+$lang['MulticraftPackage.package_fields.tooltip.jarfile'] = 'Laisser vide pour utiliser le fichier JAR par défaut.';
+$lang['MulticraftPackage.package_fields.jardir'] = 'Recherchez les JAR dans le répertoire suivant';
+$lang['MulticraftPackage.package_fields.tooltip.jardir'] = 'Si vous n\'utilisez pas le répertoire JAR du démon, Multicraft doit fonctionner en mode multi-utilisateur.';
+$lang['MulticraftPackage.package_fields.jardir_daemon'] = 'Répertoire JAR du démon';
+$lang['MulticraftPackage.package_fields.jardir_server'] = 'Répertoire JAR du serveur';
+$lang['MulticraftPackage.package_fields.jardir_server_base'] = 'Répertoire de base du serveur';
+$lang['MulticraftPackage.package_fields.user_jar'] = 'JAR sélectionnable par le propriétaire';
+$lang['MulticraftPackage.package_fields.user_name'] = 'Le propriétaire peut définir le nom';
+$lang['MulticraftPackage.package_fields.tooltip.user_name'] = 'Indique si le propriétaire peut définir ou modifier le nom du serveur.';
+$lang['MulticraftPackage.package_fields.user_schedule'] = 'Le propriétaire peut planifier des tâches';
+$lang['MulticraftPackage.package_fields.tooltip.user_schedule'] = 'Indique si le propriétaire peut créer des tâches programmées et modifier les paramètres de sauvegarde automatique.';
+$lang['MulticraftPackage.package_fields.user_ftp'] = 'Le propriétaire peut gérer le FTP';
+$lang['MulticraftPackage.package_fields.tooltip.user_ftp'] = 'Définit si le propriétaire peut donner un accès FTP à d\'autres utilisateurs.';
+$lang['MulticraftPackage.package_fields.user_visibility'] = 'Le propriétaire peut définir la visibilité';
+$lang['MulticraftPackage.package_fields.tooltip.user_visibility'] = 'Définit si le propriétaire peut modifier la visibilité du serveur et le rôle par défaut.';
+$lang['MulticraftPackage.package_fields.default_level'] = 'Rôle par défaut';
+$lang['MulticraftPackage.package_fields.tooltip.default_level'] = 'Sélectionnez le rôle qui sera attribué aux joueurs lors de leur première connexion. Utilisez l\'option Pas d\'accès pour la liste blanche.';
+$lang['MulticraftPackage.package_fields.default_level_0'] = 'Pas d\'accès';
+$lang['MulticraftPackage.package_fields.default_level_10'] = 'Invité';
+$lang['MulticraftPackage.package_fields.default_level_20'] = 'Utilisateur';
+$lang['MulticraftPackage.package_fields.default_level_30'] = 'Modérateur';
+$lang['MulticraftPackage.package_fields.autostart'] = 'Démarrer le serveur';
+$lang['MulticraftPackage.package_fields.tooltip.autostart'] = 'Démarre automatiquement le serveur lorsque Multicraft redémarre.';
+$lang['MulticraftPackage.package_fields.create_ftp'] = 'Créer un compte FTP';
+$lang['MulticraftPackage.package_fields.tooltip.create_ftp'] = 'Crée automatiquement un compte FTP.';
+$lang['MulticraftPackage.package_fields.server_visibility'] = 'Visibilité du serveur';
+$lang['MulticraftPackage.package_fields.tooltip.server_visibility'] = 'Définit la visibilité du serveur dans la liste des serveurs Multicraft.';
+$lang['MulticraftPackage.package_fields.server_visibility_0'] = 'Propriétaire uniquement';
+$lang['MulticraftPackage.package_fields.server_visibility_1'] = 'Rôle par défaut';
+$lang['MulticraftPackage.package_fields.server_visibility_2'] = 'Utilisateurs avec rôles uniquement';
+
