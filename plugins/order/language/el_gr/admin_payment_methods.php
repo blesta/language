25b5d5314a601d42fa170fccfeb2aa6223deb66a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:19 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/admin_payment_methods.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Admin Payment Methods
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminPaymentMethods.!success.payment_method_added'] = 'Η μέθοδος πληρωμής προστέθηκε επιτυχώς.';
+$lang['AdminPaymentMethods.!success.payment_method_deleted'] = 'Η μέθοδος πληρωμής διαγράφηκε επιτυχώς.';
+$lang['AdminPaymentMethods.!success.payment_method_updated'] = 'Η μέθοδος πληρωμής ενημερώθηκε επιτυχώς.';
+$lang['AdminPaymentMethods.index.page_title'] = 'Μέθοδοι πληρωμής θυγατρικών';
+$lang['AdminPaymentMethods.index.boxtitle_payment_methods'] = 'Μέθοδοι πληρωμής θυγατρικών';
+$lang['AdminPaymentMethods.index.affiliate_payment_method_add'] = 'Προσθήκη μεθόδου πληρωμής θυγατρικών';
+$lang['AdminPaymentMethods.index.no_results'] = 'Δεν υπάρχουν μέθοδοι πληρωμής αυτή τη στιγμή.';
+$lang['AdminPaymentMethods.index.heading_name'] = 'Όνομα';
+$lang['AdminPaymentMethods.index.heading_options'] = 'Επιλογές';
+$lang['AdminPaymentMethods.index.option_edit'] = 'Επεξεργασία';
+$lang['AdminPaymentMethods.index.option_delete'] = 'Διαγραφή';
+$lang['AdminPaymentMethods.index.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή τη μέθοδο πληρωμής;';
+$lang['AdminPaymentMethods.add.boxtitle_add_payment_method'] = 'Προσθήκη μεθόδου πληρωμής θυγατρικών';
+$lang['AdminPaymentMethods.add.field_name'] = 'Όνομα';
+$lang['AdminPaymentMethods.add.field_payment_methodsubmit'] = 'Προσθήκη μεθόδου πληρωμής';
+$lang['AdminPaymentMethods.edit.boxtitle_edit_payment_method'] = 'Επεξεργασία μεθόδου πληρωμής θυγατρικών';
+$lang['AdminPaymentMethods.edit.field_name'] = 'Όνομα';
+$lang['AdminPaymentMethods.edit.field_payment_methodsubmit'] = 'Ενημέρωση μεθόδου πληρωμής';
+
