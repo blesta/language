d6ba0351efeacb7032563bfa612cc559c3560dfb
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:33 -0800

    Updated fr_fr language files

---
+++ core/Util/DataFeed/Feeds/language/fr_fr/package_feed.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Package Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageFeed.name'] = 'Emballages';
+$lang['PackageFeed.description'] = 'Renvoie le nom, la description ou le prix d\'un paquet.';
+$lang['PackageFeed.getOptionFields.title_row_example_code'] = 'Exemple de code';
+$lang['PackageFeed.getOptionFields.example_code_name'] = 'Afficher le nom d\'un paquet :';
+$lang['PackageFeed.getOptionFields.example_code_description'] = 'Afficher la description du paquet :';
+$lang['PackageFeed.getOptionFields.example_code_pricing'] = 'Affichez le prix, le renouvellement, les frais de configuration, les frais d\'annulation ou la devise de la tarification d\'un paquet :';
+$lang['PackageFeed.!error.invalid_endpoint'] = 'Le point d\'accès demandé n\'est pas valide ou n\'existe pas.';
+
