7f73cce3187f22a195a79b7377e95d3fac26948a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:10 -0800

    Updated cs_cz language files

---
+++ core/Util/Filters/language/cs_cz/package_group_filters.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Package Group Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.package_group_filters.field_options'] = 'Možnosti';
+$lang['Util.filters.package_group_filters.options.hidden'] = 'Zobrazit skryté skupiny schránek';
+
