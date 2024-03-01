c54b97c253cfa7d3db51142c462544b909921270
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:15 -0800

    Updated cs_cz language files

---
+++ core/Util/Tax/language/cs_cz/united_kingdom_tax.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * United Kingdom Tax
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.Tax.united_kingdom_tax.fields.note_tax_intra_eu_uk_vat'] = 'V důsledku brexitu již Spojené království není členem EU a všechny transakce prováděné mimo Spojené království budou považovány za zahraniční transakce. Povolením této možnosti budou všechny transakce provedené v rámci člena EU považovány za operace uvnitř EU a bude uplatněn princip reverse charge.';
+$lang['Util.Tax.united_kingdom_tax.fields.tax_intra_eu_uk_vat'] = 'Transakce považovat za obchod uvnitř EU';
+$lang['Util.Tax.united_kingdom_tax.fields.note_tax_exempt_uk_vat'] = 'Automatické nastavení klienta jako osvobozeného od daně, pokud je jeho DIČ platné.';
+$lang['Util.Tax.united_kingdom_tax.fields.tax_exempt_uk_vat'] = 'Automatické nastavení jako osvobození od daně';
+$lang['Util.Tax.united_kingdom_tax.fields.note_enable_uk_vat'] = 'Zaškrtnutím této možnosti povolíte pro tuto společnost ověřování britského DIČ.';
+$lang['Util.Tax.united_kingdom_tax.fields.enable_uk_vat'] = 'Povolení ověřování DPH ve Spojeném království';
+$lang['Util.Tax.united_kingdom_tax.fields.country_uk'] = 'Spojené království';
+$lang['Util.Tax.united_kingdom_tax.tax_note_domestic_reverse_charge'] = 'Uplatňuje se zákon o přenesení daňové povinnosti DPH z roku 1994, § 55A. Klient musí vyúčtovat HMRC DPH v uvedené sazbě.';
+$lang['Util.Tax.united_kingdom_tax.tax_deduction_note'] = '%1$s Odpočet (%2$s%%)';
+$lang['Util.Tax.united_kingdom_tax.tax_note'] = '%1$s (%2$s%%)';
+$lang['Util.Tax.united_kingdom_tax.tax_id_name'] = 'Číslo DPH';
+$lang['Util.Tax.united_kingdom_tax.tax_name'] = 'DPH';
+$lang['Util.Tax.united_kingdom_tax.tax_provider_name'] = 'HMRC DPH VE SPOJENÉM KRÁLOVSTVÍ';
+
