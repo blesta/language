90e92243b73f0c8a0a12549141067349c84a7d88
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:38 -0800

    Updated cs_cz language files

---
+++ plugins/download_manager/language/cs_cz/download_manager_files.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Download Manager Files
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerFiles.!error.file_id.exists'] = 'Aktualizovaný soubor neexistuje.';
+$lang['DownloadManagerFiles.!error.file_packages.format'] = 'Alespoň jeden vybraný balíček nelze přidat, protože neexistuje.';
+$lang['DownloadManagerFiles.!error.permit_packages.format'] = 'Možnost dostupnosti balíčku je v nesprávném formátu.';
+$lang['DownloadManagerFiles.!error.file_groups.format'] = 'Alespoň jednu vybranou skupinu klientů nelze přidat, protože neexistuje.';
+$lang['DownloadManagerFiles.!error.permit_client_groups.format'] = 'Možnost dostupnosti skupiny klientů je v nesprávném formátu.';
+$lang['DownloadManagerFiles.!error.public.assignment'] = 'Soubor musí být k dispozici veřejnosti, skupinám klientů nebo balíčkům.';
+$lang['DownloadManagerFiles.!error.public.format'] = 'Možnost veřejné dostupnosti je v nesprávném formátu.';
+$lang['DownloadManagerFiles.!error.file_name.format'] = 'Nahrajte prosím soubor nebo zadejte platnou cestu k souboru.';
+$lang['DownloadManagerFiles.!error.name.empty'] = 'Zadejte prosím název tohoto souboru.';
+$lang['DownloadManagerFiles.!error.company_id.exists'] = 'Neplatné ID společnosti.';
+$lang['DownloadManagerFiles.!error.category_id.exists'] = 'Vyberte prosím platnou kategorii.';
+
