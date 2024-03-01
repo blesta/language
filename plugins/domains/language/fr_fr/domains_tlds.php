7848b1d36688217aa75392264667082c9c109873
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:56 -0800

    Updated fr_fr language files

---
+++ plugins/domains/language/fr_fr/domains_tlds.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Domains Tlds
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsTlds.!error.tld.empty'] = 'Veuillez saisir un TLD.';
+$lang['DomainsTlds.!error.tld.exists'] = 'Le TLD existe déjà dans l\'entreprise.';
+$lang['DomainsTlds.!error.tld.not_exists'] = 'Le TLD n\'existe pas dans l\'entreprise.';
+$lang['DomainsTlds.!error.tld.length'] = 'Le TLD doit comporter au moins 3 caractères.';
+$lang['DomainsTlds.!error.tld_id.exists'] = 'ID TLD non valide.';
+$lang['DomainsTlds.!error.package_id.exists'] = 'L\'identifiant du paquet n\'est pas valide.';
+$lang['DomainsTlds.!error.package_group_id.exists'] = 'ID de groupe de paquets non valide.';
+$lang['DomainsTlds.!error.module_id.exists'] = 'ID de module non valide.';
+$lang['DomainsTlds.!error.commpany_id.exists'] = 'ID de l\'entreprise non valide.';
+$lang['DomainsTlds.!error.dns_management.valid'] = 'Valeur non valide pour la gestion des DNS, doit être 0 ou 1.';
+$lang['DomainsTlds.!error.email_forwarding.valid'] = 'Valeur non valide pour Email Forwarding, doit être 0 ou 1.';
+$lang['DomainsTlds.!error.id_protection.valid'] = 'Valeur non valide pour ID Protection, doit être 0 ou 1.';
+$lang['DomainsTlds.!error.epp_code.valid'] = 'Valeur non valide pour le code EPP, doit être 0 ou 1.';
+$lang['DomainsTlds.!error.package_pricing.count'] = 'La tarification ne peut pas être désactivée, le TLD doit avoir au moins une tarification.';
+$lang['DomainsTlds.!error.package_pricing.service'] = 'La tarification n\'a pas pu être désactivée parce qu\'un service l\'utilise actuellement.';
+$lang['DomainsTlds.!error.tld.supported'] = 'Le TLD fourni n\'est pas pris en charge par le module d\'enregistrement sélectionné.';
+$lang['DomainsTlds.!error.feature.unsupported'] = 'La fonctionnalité %1$s n\'est pas prise en charge par le module d\'enregistrement sélectionné.';
+$lang['DomainsTlds.getTldFields.group'] = 'Groupe';
+
