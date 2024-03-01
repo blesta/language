8f4d2e27bcd8b9ef9bb87150e8b51e80703db007
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:05 -0800

    Updated it_it language files

---
+++ core/Automation/Tasks/Task/language/it_it/payment_reminders.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Payment Reminders
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.payment_reminders.autodebit_success'] = 'Consegnato con successo l\'avviso di sollecito dell\'autodebito per la fattura #%4$s al contatto %1$s %2$s del cliente #%3$s.';
+$lang['Automation.task.payment_reminders.autodebit_failed'] = 'Impossibile inviare l\'avviso di sollecito dell\'autoaddebito per la fattura #%4$s al contatto %1$s %2$s del cliente #%3$s.';
+$lang['Automation.task.payment_reminders.success'] = 'Consegnato con successo l\'avviso di sollecito per la fattura #%4$s al contatto %1$s %2$s del cliente #%3$s.';
+$lang['Automation.task.payment_reminders.failed'] = 'Impossibile inviare l\'avviso di sollecito per la fattura #%4$s al contatto %1$s %2$s del cliente #%3$s.';
+$lang['Automation.task.payment_reminders.completed'] = 'L\'attività di sollecito di pagamento è stata completata.';
+$lang['Automation.task.payment_reminders.attempt'] = 'Tentativo di inviare solleciti di pagamento.';
+
