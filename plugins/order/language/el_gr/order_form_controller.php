b98f934a13d5ba6ba50ede12a8754bfb1bbced59
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:27 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/order_form_controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Order Form Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderFormController.!notice.items_removed'] = 'Ορισμένα προϊόντα έχουν αφαιρεθεί από το καλάθι σας λόγω περιορισμών συσκευασίας';
+
