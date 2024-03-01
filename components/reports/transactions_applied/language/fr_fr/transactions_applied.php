e905914470c0b74cec97ffc4e885a3bc79b285a7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:14 -0800

    Updated fr_fr language files

---
+++ components/reports/transactions_applied/language/fr_fr/transactions_applied.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Transactions Applied
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['TransactionsApplied.name'] = 'Transactions appliquées';
+$lang['TransactionsApplied.option.any'] = 'Tous';
+$lang['TransactionsApplied.options.field_start_date'] = 'Date de début';
+$lang['TransactionsApplied.options.field_end_date'] = 'Date de fin';
+$lang['TransactionsApplied.options.field_gateway'] = 'Passerelle';
+$lang['TransactionsApplied.options.field_status'] = 'Statut';
+$lang['TransactionsApplied.options.field_payment_type'] = 'Type de paiement';
+$lang['TransactionsApplied.heading.id'] = 'Transaction #';
+$lang['TransactionsApplied.heading.transaction_id'] = 'ID de la transaction';
+$lang['TransactionsApplied.heading.reference_id'] = 'ID de référence';
+$lang['TransactionsApplied.heading.client_id_code'] = 'Client #';
+$lang['TransactionsApplied.heading.invoice_id_code'] = 'Facture #';
+$lang['TransactionsApplied.heading.gateway_name'] = 'Passerelle de paiement';
+$lang['TransactionsApplied.heading.type_name'] = 'Type de paiement';
+$lang['TransactionsApplied.heading.income_type'] = 'Type de transaction';
+$lang['TransactionsApplied.heading.applied_amount'] = 'Montant appliqué';
+$lang['TransactionsApplied.heading.amount'] = 'Montant du paiement';
+$lang['TransactionsApplied.heading.currency'] = 'Monnaie';
+$lang['TransactionsApplied.heading.status'] = 'Statut';
+$lang['TransactionsApplied.heading.applied_date'] = 'Date d\'application';
+
