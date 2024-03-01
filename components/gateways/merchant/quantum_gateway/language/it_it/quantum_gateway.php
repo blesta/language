43eafacd7d95fd3f4758dc6a7c466f482861d4ce
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:10 -0800

    Updated it_it language files

---
+++ components/gateways/merchant/quantum_gateway/language/it_it/quantum_gateway.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Quantum Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Quantum_gateway.tooltip_maxmind'] = 'Selezionate la casella per abilitare il Controllo frodi di Maxmind. Controllare il pannello di controllo di QuantumGateway per ulteriori impostazioni di Maxmind.';
+$lang['Quantum_gateway.maxmind'] = 'Utilizzare Maxmind';
+$lang['Quantum_gateway.restrict_key'] = 'Chiave di restrizione';
+$lang['Quantum_gateway.gateway_login'] = 'Accesso al gateway';
+$lang['Quantum_gateway.description'] = 'QuantumGateway è stato sviluppato per fornire alle aziende lo strumento di elaborazione dei pagamenti più utile, ricco di funzioni e facile da usare mai creato.';
+$lang['Quantum_gateway.name'] = 'QuantumGateway';
+$lang['Quantum_gateway.!error.maxmind.valid'] = 'Maxmind deve essere impostato su "true", se dato.';
+$lang['Quantum_gateway.!error.restrict_key.empty'] = 'Inserire la chiave di restrizione.';
+$lang['Quantum_gateway.!error.gateway_login.empty'] = 'Inserire il proprio login al Gateway.';
+
