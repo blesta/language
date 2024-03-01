7f00cc5bb457f9b93b726d77b13c32373eed7ef5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:48 -0800

    Updated de_de language files

---
+++ components/reports/package_revenue/language/de_de/package_revenue.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Package Revenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageRevenue.heading.tax'] = 'Steuer';
+$lang['PackageRevenue.heading.package_revenue'] = 'Einnahmen';
+$lang['PackageRevenue.heading.service_count'] = 'Einheiten';
+$lang['PackageRevenue.heading.module_name'] = 'Modul';
+$lang['PackageRevenue.heading.status'] = 'Status';
+$lang['PackageRevenue.heading.package_name'] = 'Paket';
+$lang['PackageRevenue.options.field_end_date'] = 'Enddatum des Rechnungsabschlusses';
+$lang['PackageRevenue.options.field_start_date'] = 'Rechnung abgeschlossen Startdatum';
+$lang['PackageRevenue.options.currency'] = 'Währung';
+$lang['PackageRevenue.option.any'] = 'Jede';
+$lang['PackageRevenue.description'] = 'Berechnet die Einnahmen aus einer Währung für jedes Paket, indem die bezahlten Rechnungspositionen, die mit dem Paket verbunden sind, summiert werden.';
+$lang['PackageRevenue.name'] = 'Paket-Einnahmen';
+
