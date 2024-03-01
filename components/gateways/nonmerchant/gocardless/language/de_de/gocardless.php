b1f4b31bc83e896dae93c1bf23ac0ab9274dc861
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:07 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/gocardless/language/de_de/gocardless.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Gocardless
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Gocardless.!tooltip.webhook_secret'] = 'Das Webhook-Geheimnis muss mindestens 40 Zeichen lang sein.';
+$lang['Gocardless.!error.dev_mode.valid'] = 'Der Entwicklermodus muss auf "true" gesetzt werden, falls vorhanden.';
+$lang['Gocardless.!error.webhook_secret.valid'] = 'Sie müssen ein gültiges Webhook-Geheimnis eingeben, das mindestens 40 Zeichen lang ist.';
+$lang['Gocardless.!error.access_token.valid'] = 'Sie müssen ein gültiges Access Token eingeben.';
+$lang['Gocardless.webhook_note'] = 'Bevor Sie mit der Nutzung dieses Gateways beginnen, müssen Sie die folgende URL als Webhook in Ihrem GoCardless-Konto konfigurieren. Das Webhook Secret muss mindestens 40 Zeichen lang sein.';
+$lang['Gocardless.webhook'] = 'GoCardless Webhook';
+$lang['Gocardless.buildprocess.subscription'] = 'Abonnieren Sie mit GoCardless';
+$lang['Gocardless.buildprocess.submit'] = 'Bezahlen mit GoCardless';
+$lang['Gocardless.dev_mode_note'] = 'Wenn Sie diese Option aktivieren, werden Transaktionen in der GoCardless Sandbox-Umgebung gebucht. Aktivieren Sie diese Option nur, wenn Sie mit einem GoCardless Sandbox-Konto testen.';
+$lang['Gocardless.dev_mode'] = 'Entwickler-Modus';
+$lang['Gocardless.pay_type_both'] = 'Einmal- und Abonnementzahlungen, wenn möglich';
+$lang['Gocardless.pay_type_subscribe'] = 'Nur Abonnementzahlungen';
+$lang['Gocardless.pay_type_onetime'] = 'Nur einmalige Zahlungen';
+$lang['Gocardless.pay_type'] = 'Zahlungsoptionen';
+$lang['Gocardless.webhook_secret'] = 'Webhook Geheimnis';
+$lang['Gocardless.access_token'] = 'Zugangs-Token';
+$lang['Gocardless.description'] = 'Sammeln Sie wiederkehrende Zahlungen aus mehr als 30 Ländern und lassen Sie sich diese auf Ihr bestehendes Bankkonto auszahlen.';
+$lang['Gocardless.name'] = 'GoCardless';
+
