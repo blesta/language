d5e6c516ac67f4f9518e37b96761e9dc769aac0d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:47 -0800

    Updated fr_fr language files

---
+++ plugins/billing_overview/language/fr_fr/billing_overview_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Billing Overview Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BillingOverviewSettings.!error.staff_id.exists'] = 'L\'ID du personnel n\'est pas valide.';
+$lang['BillingOverviewSettings.!error.company_id.exists'] = 'ID d\'entreprise non valide.';
+$lang['BillingOverviewSettings.!error.settings[][key].empty'] = 'Il se peut qu\'aucune touche de réglage ne soit vide.';
+$lang['BillingOverviewSettings.!error.settings[][value].length'] = 'La longueur de chaque valeur de réglage individuelle ne doit pas dépasser 255 caractères.';
+
