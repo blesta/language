a867b4135de7a6f3bf0988f6b4e2d98d109a1336
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:10 -0800

    Updated el_gr language files

---
+++ language/el_gr/clients.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Clients
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Clients.customfield.cf2'] = 'Προσαρμοσμένο πεδίο';
+$lang['Clients.setDebitAccountFailure.note_body'] = 'Ο λογαριασμός πληρωμών %1$s με κατάληξη x%2$s απενεργοποιήθηκε επειδή υπερέβη τον μέγιστο αριθμό προσπαθειών απόρριψης.';
+$lang['Clients.setDebitAccountFailure.note_title'] = 'Αυτόματη χρέωση απενεργοποιημένη για λογαριασμό πληρωμών.';
+$lang['Clients.getStatusTypes.fraud'] = 'Απάτη';
+$lang['Clients.getStatusTypes.inactive'] = 'Ανενεργό';
+$lang['Clients.getStatusTypes.active'] = 'Ενεργό';
+$lang['Clients.getCustomFieldTypes.textarea'] = 'Περιοχή κειμένου';
+$lang['Clients.getCustomFieldTypes.dropdown'] = 'Drop Down';
+$lang['Clients.getCustomFieldTypes.checkbox'] = 'Πλαίσιο ελέγχου';
+$lang['Clients.getCustomFieldTypes.textbox'] = 'Πλαίσιο κειμένου';
+$lang['Clients.!error.package_ids.exists'] = 'Τουλάχιστον ένα από τα παρεχόμενα πακέτα δεν υπάρχει.';
+$lang['Clients.!error.stickied.length'] = 'Το μήκος του αυτοκόλλητου δεν μπορεί να υπερβαίνει τον 1 χαρακτήρα.';
+$lang['Clients.!error.stickied.format'] = 'Το Sticky πρέπει να είναι ένας αριθμός.';
+$lang['Clients.!error.note_id.exists'] = 'Μη έγκυρο αναγνωριστικό σημείωσης.';
+$lang['Clients.!error.title.length'] = 'Το μήκος του τίτλου δεν μπορεί να υπερβαίνει τους 255 χαρακτήρες.';
+$lang['Clients.!error.title.empty'] = 'Παρακαλώ εισάγετε έναν τίτλο.';
+$lang['Clients.!error.staff_id.exists'] = 'Μη έγκυρο αναγνωριστικό προσωπικού.';
+$lang['Clients.!error.value.valid'] = '%1$s έχει οριστεί σε μη έγκυρη τιμή.';
+$lang['Clients.!error.value.required'] = '%1$s είναι σε άκυρη μορφή.';
+$lang['Clients.!error.read_only.length'] = 'Μόνο για ανάγνωση δεν μπορεί να υπερβαίνει τον 1 χαρακτήρα σε μήκος.';
+$lang['Clients.!error.read_only.format'] = 'Μόνο για ανάγνωση πρέπει να είναι αριθμός.';
+$lang['Clients.!error.encrypted.length'] = 'Η κρυπτογράφηση δεν μπορεί να υπερβαίνει τον 1 χαρακτήρα σε μήκος.';
+$lang['Clients.!error.encrypted.format'] = 'Το Encrypted πρέπει να είναι ένας αριθμός.';
+$lang['Clients.!error.client_field_id.matches'] = 'Το προσαρμοσμένο πεδίο είναι άκυρο.';
+$lang['Clients.!error.client_field_id.exists'] = 'Μη έγκυρο αναγνωριστικό προσαρμοσμένου πεδίου.';
+$lang['Clients.!error.show_client.length'] = 'Το μήκος του προγράμματος-πελάτη δεν μπορεί να υπερβαίνει τον 1 χαρακτήρα.';
+$lang['Clients.!error.show_client.format'] = 'Ο πελάτης πρέπει να είναι ένας αριθμός.';
+$lang['Clients.!error.regex.valid'] = 'Η κανονική έκφραση είναι άκυρη.';
+$lang['Clients.!error.default.valid'] = 'Η προεπιλεγμένη τιμή είναι άκυρη.';
+$lang['Clients.!error.values.format'] = 'Οι τιμές έχουν μη έγκυρη μορφή.';
+$lang['Clients.!error.type.format'] = 'Μη έγκυρος τύπος.';
+$lang['Clients.!error.is_lang.length'] = 'Το μήκος του is_lang δεν μπορεί να υπερβαίνει τον 1 χαρακτήρα.';
+$lang['Clients.!error.is_lang.format'] = 'is_lang πρέπει να είναι αριθμός.';
+$lang['Clients.!error.link.valid'] = 'Παρακαλούμε εισάγετε έναν έγκυρο σύνδεσμο.';
+$lang['Clients.!error.name.empty'] = 'Παρακαλώ εισάγετε ένα όνομα.';
+$lang['Clients.!error.receive_email_marketing.valid'] = 'Μη έγκυρη τιμή μάρκετινγκ ηλεκτρονικού ταχυδρομείου.';
+$lang['Clients.!error.language.editable'] = 'Η γλώσσα μπορεί να μην επικαιροποιηθεί.';
+$lang['Clients.!error.language.valid'] = 'Μη έγκυρη γλώσσα.';
+$lang['Clients.!error.inv_method.editable'] = 'Η μέθοδος παράδοσης του τιμολογίου ενδέχεται να μην ενημερωθεί.';
+$lang['Clients.!error.inv_method.valid'] = 'Μη έγκυρη μέθοδος παράδοσης τιμολογίου.';
+$lang['Clients.!error.default_currency.editable'] = 'Το νόμισμα ενδέχεται να μην είναι ενημερωμένο.';
+$lang['Clients.!error.default_currency.valid'] = 'Μη έγκυρο νόμισμα.';
+$lang['Clients.!error.inv_address_to.exists'] = 'Παρακαλείστε να καθορίσετε μια έγκυρη επαφή για να απευθύνετε τα τιμολόγια.';
+$lang['Clients.!error.autodebit.format'] = 'Μη έγκυρη τιμή αυτόματης χρέωσης.';
+$lang['Clients.!error.client_id.deleteable'] = 'Ο πελάτης δεν μπορεί να διαγραφεί όσο υπάρχουν ανοικτά τιμολόγια, επαναλαμβανόμενα τιμολόγια ή ενεργές/ανασταλείσες υπηρεσίες. Παρακαλούμε ακυρώστε τυχόν ανοιχτά τιμολόγια, διαγράψτε τυχόν επαναλαμβανόμενα τιμολόγια και ακυρώστε τυχόν ενεργές/ανασταλείσες υπηρεσίες πριν προχωρήσετε.';
+$lang['Clients.!error.settings[tax_id].valid'] = 'Παρακαλείσθε να προσδιορίσετε έναν έγκυρο φορολογικό κωδικό/αΦΜ.';
+$lang['Clients.!error.status.format'] = 'Μη έγκυρη κατάσταση.';
+$lang['Clients.!error.user_id.unique'] = 'Το αναγνωριστικό χρήστη πελάτη %1$s είναι ήδη κατειλημμένο.';
+$lang['Clients.!error.user_id.exists'] = 'Μη έγκυρο αναγνωριστικό χρήστη.';
+$lang['Clients.!error.id_value.valid'] = 'Αδυναμία προσδιορισμού της τιμής του αναγνωριστικού πελάτη.';
+$lang['Clients.!error.id_format.length'] = 'Η μορφή ID για τους πελάτες δεν μπορεί να υπερβαίνει τους 64 χαρακτήρες.';
+$lang['Clients.!error.id_format.empty'] = 'Δεν έχει οριστεί μορφή ID για τους πελάτες.';
+$lang['Clients.!error.type.exists'] = 'Μη έγκυρος τύπος.';
+$lang['Clients.!error.account_id.exists'] = 'Μη έγκυρο αναγνωριστικό λογαριασμού.';
+$lang['Clients.!error.client_id.exists'] = 'Μη έγκυρο αναγνωριστικό πελάτη.';
+$lang['Clients.!error.client_group_id.exists'] = 'Μη έγκυρο αναγνωριστικό ομάδας πελατών.';
+
