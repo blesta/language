56097b3548b1bd9d1ed8d8cdf29d762cee606986
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:15 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/backups_sftp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Backups Sftp
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.backups_sftp.attempt'] = 'Προσπάθεια δημιουργίας αντιγράφων ασφαλείας της βάσης δεδομένων μέσω SFTP.';
+$lang['Automation.task.backups_sftp.completed'] = 'Η εργασία δημιουργίας αντιγράφων ασφαλείας της βάσης δεδομένων SFTP έχει ολοκληρωθεί.';
+$lang['Automation.task.backups_sftp.success'] = 'Το αντίγραφο ασφαλείας ολοκληρώθηκε με επιτυχία.';
+
