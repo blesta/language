44508055c4d42950f94c4fb4d1cab026e9eeaa4c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:58 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/emails.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Emails.getStatusTypes.inactive'] = 'Ano';
+$lang['Emails.getStatusTypes.active'] = 'Ne';
+$lang['Emails.!error.email.failed_to_send'] = 'E-mail se nepodařilo odeslat kvůli problému s konfigurací.';
+$lang['Emails.!error.attachments.exist'] = 'Alespoň jedna z poskytnutých příloh neexistuje v souborovém systému.';
+$lang['Emails.!error.bcc_addresses.format'] = 'Alespoň jedna z uvedených e-mailových adres BCC není platná e-mailová adresa.';
+$lang['Emails.!error.cc_addresses.format'] = 'Alespoň jedna z uvedených e-mailových adres CC není platná e-mailová adresa.';
+$lang['Emails.!error.to_addresses.format'] = 'Alespoň jedna z uvedených e-mailových adres Není platná e-mailová adresa.';
+$lang['Emails.!error.to_addresses.empty'] = 'Musí být uvedena alespoň jedna adresa To.';
+$lang['Emails.!error.action.exists'] = 'Zadaná akce je neplatná akce skupiny e-mailů.';
+$lang['Emails.!error.company_id.unique'] = 'ID e-mailové skupiny a ID společnosti pro daný jazyk je již obsazeno.';
+$lang['Emails.!error.email_id.exists'] = 'Neplatné e-mailové ID.';
+$lang['Emails.!error.status.format'] = 'Neplatný stav.';
+$lang['Emails.!error.include_attachments'] = 'Zda se mají zahrnout přílohy, musí být nastaveno na 0 nebo 1.';
+$lang['Emails.!error.subject.empty'] = 'Zadejte prosím předmět.';
+$lang['Emails.!error.reply_to.format'] = 'Zadejte prosím platnou adresu pro zaslání odpovědi.';
+$lang['Emails.!error.from_name.empty'] = 'Zadejte prosím název od.';
+$lang['Emails.!error.from.format'] = 'Zadejte prosím platnou adresu od.';
+$lang['Emails.!error.lang.length'] = 'Délka jazyka nesmí překročit 5 znaků.';
+$lang['Emails.!error.lang.empty'] = 'Zadejte prosím jazyk.';
+$lang['Emails.!error.email_group_id.exists'] = 'Neplatné ID e-mailové skupiny.';
+$lang['Emails.!error.email_signature_id.exists'] = 'Neplatné ID e-mailového podpisu.';
+$lang['Emails.!error.text.empty'] = 'Zadejte prosím podpis v prostém textu.';
+$lang['Emails.!error.name.empty'] = 'Zadejte prosím název podpisu.';
+$lang['Emails.!error.email_signature_id.in_use'] = 'Tento e-mailový podpis je v současné době používán a nesmí být odstraněn.';
+$lang['Emails.!error.text.parse'] = 'Chyba parsování šablony: %1$s';
+$lang['Emails.!error.html.parse'] = 'Chyba parsování šablony: %1$s';
+$lang['Emails.!error.company_id.exists'] = 'Neplatné ID společnosti.';
+
