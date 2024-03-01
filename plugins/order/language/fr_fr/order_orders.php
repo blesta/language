17dc2f3a954d4b49f1c742d774c9077f315a6b9b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:39 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/order_orders.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Order Orders
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderOrders.getstatuses.pending'] = 'En revue';
+$lang['OrderOrders.getstatuses.accepted'] = 'Accepté';
+$lang['OrderOrders.getstatuses.fraud'] = 'Fraude';
+$lang['OrderOrders.getstatuses.canceled'] = 'Annulé';
+$lang['OrderOrders.!error.price_transfer_disabled'] = 'Le domaine sélectionné n\'est pas disponible pour les transferts.';
+
