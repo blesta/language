825e0f19359ae942d6c4449d22b699997529f4fc
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:11 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/razorpay/language/cs_cz/razorpay.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Razorpay
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Razorpay.!error.key_secret.valid'] = 'Musíte zadat platný klíč Secret.';
+$lang['Razorpay.!error.key_id.valid'] = 'Musíte zadat platné ID klíče.';
+$lang['Razorpay.buildprocess.submit'] = 'Plaťte pomocí Razorpay';
+$lang['Razorpay.webhook_note'] = 'Než začnete tuto bránu používat, musíte ve svém účtu Razorpay nakonfigurovat následující url jako Webhook.';
+$lang['Razorpay.webhook'] = 'Razorpay Webhook';
+$lang['Razorpay.webhook_secret'] = 'Tajemství webhooku';
+$lang['Razorpay.key_secret'] = 'Klíčové tajemství';
+$lang['Razorpay.key_id'] = 'ID klíče';
+$lang['Razorpay.description'] = 'Razorpay je platební společnost, která poskytuje platební řešení online obchodníkům v Indii.';
+$lang['Razorpay.name'] = 'Razorpay';
+
