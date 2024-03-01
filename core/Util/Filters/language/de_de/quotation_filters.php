eff3236d9c1e665afa09e52083f7a508b7d6e928
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:14 -0800

    Updated de_de language files

---
+++ core/Util/Filters/language/de_de/quotation_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Quotation Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.quotation_filters.field_quotation_line'] = 'Posten Inhalt';
+$lang['Util.filters.quotation_filters.field_currency'] = 'Währung';
+$lang['Util.filters.quotation_filters.field_quotation_number'] = 'Zitat #';
+$lang['Util.filters.quotation_filters.any'] = 'Jede';
+
