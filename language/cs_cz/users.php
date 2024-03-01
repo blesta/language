6b190f4c9e90f537277e0c31f39c26babbc84d32
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:21 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/users.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Users
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Users.!error.clients.exist'] = 'Uživatele nelze odstranit, protože k němu je přiřazen alespoň jeden klient.';
+$lang['Users.!error.username.company'] = 'V tomto místě nejste oprávněni se přihlásit.';
+$lang['Users.!error.username.attempts'] = 'Zjištěno příliš mnoho neúspěšných pokusů o přihlášení.';
+$lang['Users.!error.user_id.exists'] = 'Neplatné ID uživatele.';
+$lang['Users.!error.otp.auth'] = 'Zadané jednorázové heslo je neplatné. Maximální délka je 16 znaků';
+$lang['Users.!error.username.auth'] = 'Pro tuto kombinaci uživatel/heslo nebyla nalezena žádná shoda.';
+$lang['Users.!error.two_factor_key.format'] = 'Neplatný dvoufaktorový klíč.';
+$lang['Users.!error.two_factor_mode.format'] = 'Neplatný dvoufaktorový režim.';
+$lang['Users.!error.new_password.matches'] = 'Hesla se neshodují.';
+$lang['Users.!error.new_password.format'] = 'Zadejte prosím heslo o délce alespoň 6 znaků.';
+$lang['Users.!error.current_password.matches'] = 'Nesprávné heslo.';
+$lang['Users.!error.username.unique'] = 'Toto uživatelské jméno již bylo obsazeno.';
+$lang['Users.!error.username.empty'] = 'Zadejte prosím uživatelské jméno.';
+$lang['Users.!error.recovery_email.format'] = 'Neplatná e-mailová adresa pro obnovení.';
+
