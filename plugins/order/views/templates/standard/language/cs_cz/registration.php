01abd8a8ca55c854f7f738ae6f722bae983cbd8a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:25 -0800

    Updated cs_cz language files

---
+++ plugins/order/views/templates/standard/language/cs_cz/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Registration
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Registration.signup.header_signup'] = 'Zaregistrujte se';
+
