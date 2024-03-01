eb20c92a01720f15967cce0e834b7037304572e6
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:18 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/system_requirements.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * System Requirements
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SystemRequirements.!warning.cache_writable.recommended'] = 'Pro lepší výkonnost zajistěte, aby %1$s bylo možné webovým serverem zapisovat.';
+$lang['SystemRequirements.!warning.soap.recommended'] = 'Rozšíření mýdla je nutné pro automatické zpracování daně z přidané hodnoty.';
+$lang['SystemRequirements.!warning.zlib.recommended'] = 'Pro lepší výkonnost doporučujeme použít rozšíření zlib.';
+$lang['SystemRequirements.!warning.simplexml.recommended'] = 'Rozšíření simplexml se důrazně doporučuje, protože může být vyžadováno k propojení s některými systémy.';
+$lang['SystemRequirements.!warning.mcrypt.recommended'] = 'Pro lepší výkon se doporučuje použít rozšíření mcrypt.';
+$lang['SystemRequirements.!warning.mbstring.recommended'] = 'Rozšíření mbstring je vyžadováno některými volitelnými funkcemi.';
+$lang['SystemRequirements.!warning.iconv.recommended'] = 'Rozšíření iconv je nutné pro kódování a dekódování speciálních znaků.';
+$lang['SystemRequirements.!warning.mailparse.recommended'] = 'Rozšíření mailparse je nutné pro zpracování příchozích e-mailů.';
+$lang['SystemRequirements.!warning.libxml.recommended'] = 'Rozšíření libxml se důrazně doporučuje, protože může být vyžadováno pro rozhraní s některými systémy.';
+$lang['SystemRequirements.!warning.imap.recommended'] = 'Rozšíření imap je nutné k odesílání a přijímání pošty prostřednictvím protokolů SMTP a IMAP.';
+$lang['SystemRequirements.!warning.gmp.recommended'] = 'Pro lepší výkonnost se doporučuje použít rozšíření gmp.';
+$lang['SystemRequirements.!warning.gd.recommended'] = 'Rozšíření gd se doporučuje pro lepší podporu obrázků při generování PDF a interní generování captcha.';
+$lang['SystemRequirements.!info.ext-ioncube-sourceguardian.minimum'] = 'Je vyžadováno rozšíření SourceGuardian nebo ioncube loader. Pokud používáte SourceGuardian, nezapomeňte použít potřebné soubory hotfix přepsáním souborů Blesta soubory z adresáře hotfix-php8. Jinak nebude Blesta po instalaci přístupná.';
+$lang['SystemRequirements.!error.ext-ioncube-sourceguardian.minimum'] = 'Je vyžadováno rozšíření SourceGuardian nebo ioncube loader.';
+$lang['SystemRequirements.!info.ext-ioncube.minimum'] = 'Nezapomeňte použít potřebné soubory hotfix přepsáním souborů Blesta soubory z adresáře %1$s. Jinak nebude Blesta po instalaci přístupná.';
+$lang['SystemRequirements.!error.config_writable.minimum'] = 'Konfigurační soubor (%1$s) a adresář (%2$s) musí být pro webový server zapisovatelné.';
+$lang['SystemRequirements.!error.extension_version.minimum'] = 'Je vyžadováno rozšíření \'%1$s\'. Vaše verze: %2$s.';
+$lang['SystemRequirements.!error.extension.minimum'] = 'Je vyžadováno rozšíření \'%1$s\'.';
+$lang['SystemRequirements.!error.php.minimum'] = 'Je vyžadována verze PHP %1$s nebo vyšší. Vaše verze: %2$s.';
+
