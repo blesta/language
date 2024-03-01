ec54f00eb42ad5ee3d33b5725cf6da4bc55f54c0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:41 -0800

    Updated el_gr language files

---
+++ plugins/auto_cancel/language/el_gr/auto_cancel_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Auto Cancel Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AutoCancelPlugin.getCronTasks.schedule_cancellation_desc'] = 'Προγραμματίζει αυτόματα τις υπηρεσίες που έχουν ανασταλεί για ακύρωση.';
+$lang['AutoCancelPlugin.getCronTasks.schedule_cancellation_name'] = 'Αυτόματη ακύρωση προγράμματος';
+$lang['AutoCancelPlugin.description'] = 'Προγραμματίζει αυτόματα τις υπηρεσίες για ακύρωση.';
+$lang['AutoCancelPlugin.name'] = 'Αυτόματη ακύρωση';
+
