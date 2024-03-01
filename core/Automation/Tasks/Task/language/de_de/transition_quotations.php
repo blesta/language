fc8178c2842828d203a0e9840849f8eaa17b61d5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:02 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/transition_quotations.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Transition Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.transition_quotations.dead_success'] = 'Das abgelaufene Angebot #%1$s von Kunde #%2$s ist jetzt tot.';
+$lang['Automation.task.transition_quotations.expiration_success'] = 'Das ausstehende Angebot #%1$s vom Kunden #%2$s ist nun abgelaufen.';
+$lang['Automation.task.transition_quotations.none'] = 'Es sind keine abgelaufenen Angebote für den Übergang verfügbar.';
+$lang['Automation.task.transition_quotations.completed'] = 'Die Aufgabe zur Überleitung von Zitaten ist abgeschlossen.';
+$lang['Automation.task.transition_quotations.attempt'] = 'Versuch, abgelaufene Zitate zu übertragen.';
+
