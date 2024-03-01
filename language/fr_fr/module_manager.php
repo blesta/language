ac03965d3838ffd68d3b5eee881fd0193ee7e82c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:29 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/module_manager.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Module Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ModuleManager.!error.company_id.valid'] = 'L\'identifiant de l\'entreprise n\'est pas valide.';
+$lang['ModuleManager.!error.class.valid'] = 'La classe n\'est pas valide.';
+$lang['ModuleManager.!error.name.valid'] = 'Le nom n\'est pas valide.';
+$lang['ModuleManager.!error.version.valid'] = 'La version n\'est pas valide.';
+$lang['ModuleManager.!error.type_id.valid'] = 'L\'ID du type n\'est pas valide.';
+$lang['ModuleManager.!error.name.empty'] = 'Vous devez spécifier un nom pour le groupe.';
+$lang['ModuleManager.!error.module_rows[].valid'] = 'Une ou plusieurs des lignes spécifiées n\'appartiennent pas à ce module.';
+$lang['ModuleManager.!error.force_limits.valid'] = 'L\'application de limites strictes doit être fixée à 0 ou 1.';
+$lang['ModuleManager.!error.module_row_id.assigned_service'] = 'La ligne ne peut pas être supprimée car elle appartient à un ou plusieurs services.';
+$lang['ModuleManager.!error.module_row_id.assigned_package'] = 'La ligne ne peut pas être supprimée car elle appartient à un ou plusieurs paquets.';
+$lang['ModuleManager.!error.module_group_id.assigned_package'] = 'Le groupe ne peut pas être supprimé car il appartient à un ou plusieurs paquets.';
+$lang['ModuleManager.!error.module_id.assigned_package'] = 'Le module ne peut pas être désinstallé car il est utilisé par un ou plusieurs paquets actifs.';
+$lang['ModuleManager.!error.module_id.assigned_service'] = 'Le module ne peut pas être désinstallé car il est utilisé par un ou plusieurs services non annulés.';
+
