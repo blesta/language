6a8a5dd0e0571cacf05796c53188cefd9429327b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:11 -0800

    Updated es_es language files

---
+++ plugins/extension_generator/language/es_es/extension_generator_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Extension Generator Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorPlugin.name'] = 'Generador de extensiones';
+$lang['ExtensionGeneratorPlugin.description'] = 'Este es un plugin para generar automáticamente archivos para nuevas extensiones de Blesta';
+$lang['ExtensionGeneratorPlugin.nav_secondary_staff.admin_main'] = 'Generador de extensiones';
+
