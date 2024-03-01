baa3b751521918c81abc1ed987e9debc8a1060d8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:43 -0800

    Updated fr_fr language files

---
+++ components/invoice_templates/default_invoice/language/fr_fr/default_invoice.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Default Invoice
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DefaultInvoice.name'] = 'Défaut';
+$lang['DefaultInvoice.page_of'] = 'Page %1$s de %2$s';
+$lang['DefaultInvoice.terms_heading'] = 'Conditions';
+$lang['DefaultInvoice.type_active'] = 'FACTURE';
+$lang['DefaultInvoice.type_draft'] = 'FACTURE - PROJET';
+$lang['DefaultInvoice.type_void'] = 'INVOICE - VOID';
+$lang['DefaultInvoice.type_proforma'] = 'FACTURE PRO FORMA';
+$lang['DefaultInvoice.type_quotation'] = 'CITATION';
+$lang['DefaultInvoice.type_quotation_draft'] = 'PROJET DE DEVIS';
+$lang['DefaultInvoice.invoice_id_code'] = 'Numéro de facture :';
+$lang['DefaultInvoice.proforma_id_code'] = 'Numéro pro forma :';
+$lang['DefaultInvoice.draft_id_code'] = 'Numéro de projet :';
+$lang['DefaultInvoice.quotation_id_code'] = 'Numéro de devis :';
+$lang['DefaultInvoice.client_id_code'] = 'Numéro de client :';
+$lang['DefaultInvoice.tax_id'] = 'Numéro d\'identification fiscale/VATIN :';
+$lang['DefaultInvoice.client_tax_id'] = 'Numéro d\'identification fiscale du client/VATIN :';
+$lang['DefaultInvoice.date_created'] = 'Date de création :';
+$lang['DefaultInvoice.date_billed'] = 'Date de la facture :';
+$lang['DefaultInvoice.date_expires'] = 'Date d\'expiration :';
+$lang['DefaultInvoice.date_due'] = 'Date d\'échéance :';
+$lang['DefaultInvoice.notes_heading'] = 'Notes';
+$lang['DefaultInvoice.subtotal_heading'] = 'Sous-total';
+$lang['DefaultInvoice.total_heading'] = 'Total';
+$lang['DefaultInvoice.lines_description'] = 'Description';
+$lang['DefaultInvoice.lines_quantity'] = 'Quantité';
+$lang['DefaultInvoice.lines_unit_price'] = 'Prix unitaire';
+$lang['DefaultInvoice.lines_cost'] = 'Coût';
+$lang['DefaultInvoice.watermark_paid'] = 'PAYÉ';
+$lang['DefaultInvoice.payments_heading'] = 'Paiements';
+$lang['DefaultInvoice.payments_applied_date'] = 'Date d\'application';
+$lang['DefaultInvoice.payments_type_name'] = 'Type';
+$lang['DefaultInvoice.payments_transaction_id'] = 'Numéro de transaction';
+$lang['DefaultInvoice.payments_applied_amount'] = 'Montant appliqué';
+$lang['DefaultInvoice.balance_heading'] = 'Équilibre';
+
