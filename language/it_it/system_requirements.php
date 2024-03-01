e069afb4662f907c37a74e3e2322b5686517882e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:23 -0800

    Updated it_it language files

---
+++ language/it_it/system_requirements.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * System Requirements
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SystemRequirements.!warning.cache_writable.recommended'] = 'Per migliorare le prestazioni, assicurarsi che %1$s sia scrivibile dal webserver.';
+$lang['SystemRequirements.!warning.soap.recommended'] = 'L\'estensione del sapone è necessaria per la gestione automatica dell\'IVA.';
+$lang['SystemRequirements.!warning.zlib.recommended'] = 'L\'estensione zlib è altamente raccomandata per migliorare le prestazioni.';
+$lang['SystemRequirements.!warning.simplexml.recommended'] = 'L\'estensione simplexml è altamente raccomandata, poiché potrebbe essere necessaria per interfacciarsi con alcuni sistemi.';
+$lang['SystemRequirements.!warning.mcrypt.recommended'] = 'L\'estensione mcrypt è consigliata per ottenere prestazioni migliori.';
+$lang['SystemRequirements.!warning.mbstring.recommended'] = 'L\'estensione mbstring è richiesta da alcune funzioni opzionali.';
+$lang['SystemRequirements.!warning.iconv.recommended'] = 'L\'estensione iconv è necessaria per la codifica e la decodifica di caratteri speciali.';
+$lang['SystemRequirements.!warning.mailparse.recommended'] = 'L\'estensione mailparse è necessaria per analizzare le e-mail in arrivo.';
+$lang['SystemRequirements.!warning.libxml.recommended'] = 'L\'estensione libxml è altamente raccomandata, poiché potrebbe essere necessaria per interfacciarsi con alcuni sistemi.';
+$lang['SystemRequirements.!warning.imap.recommended'] = 'L\'estensione imap è necessaria per inviare e ricevere posta tramite SMTP e IMAP.';
+$lang['SystemRequirements.!warning.gmp.recommended'] = 'L\'estensione gmp è altamente raccomandata per ottenere prestazioni migliori.';
+$lang['SystemRequirements.!warning.gd.recommended'] = 'L\'estensione gd è consigliata per un migliore supporto delle immagini durante la generazione di PDF e per la generazione interna di captcha.';
+$lang['SystemRequirements.!info.ext-ioncube-sourceguardian.minimum'] = 'È necessaria l\'estensione "SourceGuardian" o "ioncube loader". Se si utilizza SourceGuardian, assicurarsi di applicare i file hotfix necessari sovrascrivendo i file Blesta con quelli della directory hotfix-php8. In caso contrario, Blesta non sarà accessibile dopo l\'installazione.';
+$lang['SystemRequirements.!error.ext-ioncube-sourceguardian.minimum'] = 'È necessaria l\'estensione "SourceGuardian" o "ioncube loader".';
+$lang['SystemRequirements.!info.ext-ioncube.minimum'] = 'Assicurarsi di applicare i file hotfix necessari sovrascrivendo i file Blesta con quelli della directory %1$s. In caso contrario, Blesta non sarà accessibile dopo l\'installazione.';
+$lang['SystemRequirements.!error.config_writable.minimum'] = 'Il file di configurazione (%1$s) e la directory (%2$s) devono essere scrivibili dal webserver.';
+$lang['SystemRequirements.!error.extension_version.minimum'] = 'È richiesta l\'estensione \'%1$s\'. Versione: %2$s.';
+$lang['SystemRequirements.!error.extension.minimum'] = 'È richiesta l\'estensione "%1$s".';
+$lang['SystemRequirements.!error.php.minimum'] = 'È richiesta la versione PHP %1$s o superiore. La vostra versione: %2$s.';
+
