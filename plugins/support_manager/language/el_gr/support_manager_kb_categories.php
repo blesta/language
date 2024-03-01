9dc2ffeb7e15718273d542972f626f08a06ec3ea
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:59 -0800

    Updated el_gr language files

---
+++ plugins/support_manager/language/el_gr/support_manager_kb_categories.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Support Manager Kb Categories
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerKbCategories.!error.parent_id.exists'] = 'Μη έγκυρο αναγνωριστικό γονέα.';
+$lang['SupportManagerKbCategories.!error.parent_id.valid_company'] = 'Η κατηγορία αυτή δεν μπορεί να ανατεθεί σε μητρική εταιρεία από ξεχωριστή εταιρεία.';
+$lang['SupportManagerKbCategories.!error.parent_id.valid_parent'] = 'Αυτή η κατηγορία δεν μπορεί να είναι γονέας του εαυτού της.';
+$lang['SupportManagerKbCategories.!error.company_id.exists'] = 'Μη έγκυρο αναγνωριστικό εταιρείας.';
+$lang['SupportManagerKbCategories.!error.name.empty'] = 'Παρακαλώ εισάγετε ένα όνομα κατηγορίας.';
+$lang['SupportManagerKbCategories.!error.name.length'] = 'Το όνομα της κατηγορίας δεν μπορεί να υπερβαίνει τους 255 χαρακτήρες σε μήκος.';
+$lang['SupportManagerKbCategories.!error.access.type'] = 'Μη έγκυρος τύπος πρόσβασης.';
+$lang['SupportManagerKbCategories.!error.category_id.exists'] = 'Μη έγκυρο αναγνωριστικό κατηγορίας.';
+$lang['SupportManagerKbCategories.!error.articles.parent'] = 'Η κατηγορία δεν μπορεί να διαγραφεί, εκτός εάν όλα τα άρθρα εντός της ανήκουν σε άλλη κατηγορία.';
+$lang['SupportManagerKbCategories.access_types.public'] = 'Δημόσιο';
+$lang['SupportManagerKbCategories.access_types.private'] = 'Ιδιωτικό';
+$lang['SupportManagerKbCategories.access_types.hidden'] = 'Κρυφό';
+$lang['SupportManagerKbCategories.access_types.staff'] = 'Προσωπικό';
+
