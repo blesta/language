987ff9445af226a8e1f72d5f2fa1f29834eec15c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:56 -0800

    Updated el_gr language files

---
+++ plugins/support_manager/language/el_gr/client_tickets.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Client Tickets
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientTickets.!success.ticket_created'] = 'Το εισιτήριο #%1$s δημιουργήθηκε με επιτυχία.';
+$lang['ClientTickets.!success.ticket_updated'] = 'Το εισιτήριο #%1$s ενημερώθηκε επιτυχώς.';
+$lang['ClientTickets.!success.ticket_closed'] = 'Το εισιτήριο #%1$s έκλεισε επιτυχώς.';
+$lang['ClientTickets.!notice.no_departments'] = 'Δεν υπάρχουν διαθέσιμα τμήματα υποστήριξης.';
+$lang['ClientTickets.!error.captcha.invalid'] = 'Το captcha που εισήχθη ήταν άκυρο. Παρακαλώ προσπαθήστε ξανά.';
+$lang['ClientTickets.index.page_title'] = 'Υποστήριξη > Εισιτήρια';
+$lang['ClientTickets.add.page_title'] = 'Υποστήριξη > Άνοιγμα εισιτηρίου';
+$lang['ClientTickets.reply.page_title'] = 'Υποστήριξη > Ενημέρωση εισιτηρίου';
+$lang['ClientTickets.index.category_open'] = 'Ανοίξτε το';
+$lang['ClientTickets.index.category_closed'] = 'Κλειστό';
+$lang['ClientTickets.index.categorylink_createticket'] = 'Ανοιχτό εισιτήριο';
+$lang['ClientTickets.index.boxtitle_tickets'] = 'Εισιτήρια';
+$lang['ClientTickets.index.heading_code'] = 'Αριθμός εισιτηρίου';
+$lang['ClientTickets.index.heading_priority'] = 'Προτεραιότητα';
+$lang['ClientTickets.index.heading_department_name'] = 'Τμήμα';
+$lang['ClientTickets.index.heading_summary'] = 'Περίληψη';
+$lang['ClientTickets.index.heading_last_reply_date'] = 'Τελευταία απάντηση';
+$lang['ClientTickets.index.heading_status'] = 'Κατάσταση';
+$lang['ClientTickets.index.heading_options'] = 'Επιλογές';
+$lang['ClientTickets.index.option_close'] = 'Κλείστε το';
+$lang['ClientTickets.index.option_reply'] = 'Απάντηση';
+$lang['ClientTickets.index.any'] = 'Οποιοδήποτε';
+$lang['ClientTickets.index.minutes'] = '%1$s λεπτά';
+$lang['ClientTickets.index.hour'] = '1 ώρα';
+$lang['ClientTickets.index.hours'] = '%1$s ώρες';
+$lang['ClientTickets.index.field_ticket_number'] = 'Αριθμός εισιτηρίου';
+$lang['ClientTickets.index.field_priority'] = 'Προτεραιότητα';
+$lang['ClientTickets.index.field_summary'] = 'Περίληψη';
+$lang['ClientTickets.index.field_last_reply'] = 'Τελευταία απάντηση';
+$lang['ClientTickets.add.text_add_attachment'] = 'Προσθήκη συνημμένου';
+$lang['ClientTickets.index.no_results'] = 'Προς το παρόν δεν υπάρχουν εισιτήρια με αυτή την κατάσταση.';
+$lang['ClientTickets.index.confirm_close'] = 'Είστε σίγουροι ότι θέλετε να κλείσετε αυτό το εισιτήριο;';
+$lang['ClientTickets.departments.boxtitle_departments'] = 'Τμήματα υποστήριξης';
+$lang['ClientTickets.add.boxtitle_add'] = 'Ανοιχτό εισιτήριο';
+$lang['ClientTickets.add.field_email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['ClientTickets.add.field_summary'] = 'Περίληψη';
+$lang['ClientTickets.add.field_priority'] = 'Προτεραιότητα';
+$lang['ClientTickets.add.field_details'] = 'Λεπτομέρειες';
+$lang['ClientTickets.add.field_attachments'] = 'Συνημμένα';
+$lang['ClientTickets.add.field_addsubmit'] = 'Ανοιχτό εισιτήριο';
+$lang['ClientTickets.add.placeholder_summary'] = 'Εισάγετε περίληψη';
+$lang['ClientTickets.add.placeholder_details'] = 'Εισάγετε λεπτομέρειες';
+$lang['ClientTickets.add.dropzone_drop_files_here'] = 'Αφήστε αρχεία εδώ για να τα ανεβάσετε ή κάντε κλικ για να επιλέξετε αρχεία';
+$lang['ClientTickets.add.dropzone_remove_file'] = 'Αφαίρεση αρχείου';
+$lang['ClientTickets.reply.boxtitle_reply'] = 'Εισιτήριο #%1$s';
+$lang['ClientTickets.reply.heading_summary'] = 'Περίληψη';
+$lang['ClientTickets.reply.heading_department'] = 'Τμήμα';
+$lang['ClientTickets.reply.heading_priority'] = 'Προτεραιότητα';
+$lang['ClientTickets.reply.heading_status'] = 'Κατάσταση';
+$lang['ClientTickets.reply.heading_date_added'] = 'Ημερομηνία ανοίγματος';
+$lang['ClientTickets.reply.heading_reply'] = 'Προσθήκη απάντησης';
+$lang['ClientTickets.reply.placeholder_reply'] = 'Εισάγετε λεπτομέρειες';
+$lang['ClientTickets.reply.field_details'] = 'Λεπτομέρειες';
+$lang['ClientTickets.reply.field_attachments'] = 'Συνημμένα';
+$lang['ClientTickets.reply.field_replysubmit'] = 'Απάντηση';
+$lang['ClientTickets.reply.field_closesubmit'] = 'Κλείστε το';
+$lang['ClientTickets.reply.confirm_close'] = 'Είστε σίγουροι ότι θέλετε να κλείσετε αυτό το εισιτήριο;';
+$lang['ClientTickets.reply.text_add_attachment'] = 'Προσθήκη συνημμένου';
+$lang['ClientTickets.reply.reply_date'] = 'Στις %1$s %2$s %3$s απάντησε';
+$lang['ClientTickets.reply.log_date'] = '%1$s από %2$s %3$s';
+$lang['ClientTickets.reply.system'] = 'Σύστημα';
+$lang['ClientTickets.reply.staff_title'] = 'Προσωπικό υποστήριξης';
+$lang['ClientTickets.reply.dropzone_drop_files_here'] = 'Αφήστε αρχεία εδώ για να τα ανεβάσετε ή κάντε κλικ για να επιλέξετε αρχεία';
+$lang['ClientTickets.reply.dropzone_remove_file'] = 'Αφαίρεση αρχείου';
+$lang['ClientTickets.formatDepartmentCustomFields.encrypted_field'] = 'Τα δεδομένα αυτά είναι κρυπτογραφημένα και δεν μπορούν να εμφανιστούν.';
+$lang['ClientTickets.!error.email.blacklist'] = 'Λυπούμαστε, δεν μπορούμε να δεχτούμε το εισιτήριό σας αυτή τη στιγμή.';
+
