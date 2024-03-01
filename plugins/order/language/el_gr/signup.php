143a572b6e7e71fd0793f15d40f340c95b9a5070
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:31 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/signup.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Signup
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Signup.!error.captcha.invalid'] = 'Το captcha που εισήχθη ήταν άκυρο. Παρακαλώ προσπαθήστε ξανά.';
+$lang['Signup.!error.not_client_owner'] = 'Πρέπει να συνδεθείτε ως ιδιοκτήτης του πελάτη για να συνεχίσετε.';
+$lang['Signup.!notice.force_email_usernames'] = 'Το όνομα χρήστη σας θα είναι το ίδιο με τη διεύθυνση ηλεκτρονικού ταχυδρομείου σας.';
+$lang['Signup.!notice.email_verification'] = 'Έχει σταλεί ένα μήνυμα για την επαλήθευση της διεύθυνσης ηλεκτρονικού ταχυδρομείου σας. Παρακαλούμε ολοκληρώστε την επαλήθευση το συντομότερο δυνατό.';
+$lang['Signup.index.field_taxid'] = 'Φορολογικός αριθμός ταυτότητας/VATIN';
+$lang['Signup.index.field_firstname'] = 'Όνομα';
+$lang['Signup.index.field_lastname'] = 'Επώνυμο';
+$lang['Signup.index.field_company'] = 'Εταιρεία/Οργανισμός.';
+$lang['Signup.index.field_address1'] = 'Διεύθυνση 1';
+$lang['Signup.index.field_address2'] = 'Διεύθυνση 2';
+$lang['Signup.index.field_city'] = 'Πόλη';
+$lang['Signup.index.field_state'] = 'Πολιτεία/Περιφέρεια';
+$lang['Signup.index.field_zip'] = 'Ταχυδρομικός/ταχυδρομικός κώδικας';
+$lang['Signup.index.field_country'] = 'Χώρα';
+$lang['Signup.index.field_title'] = 'Τίτλος';
+$lang['Signup.index.field_phone'] = 'Τηλέφωνο';
+$lang['Signup.index.field_fax'] = 'Φαξ';
+$lang['Signup.index.field_email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['Signup.index.field_username_type_email'] = 'Χρήση email ως όνομα χρήστη';
+$lang['Signup.index.field_username_type_username'] = 'Καθορίστε ένα όνομα χρήστη';
+$lang['Signup.index.field_action_continue'] = 'Συνεχίστε ως %1$s %2$s';
+$lang['Signup.index.field_action_login'] = 'Σύνδεση';
+$lang['Signup.index.field_action_signup'] = 'Δημιουργία λογαριασμού';
+$lang['Signup.index.field_username'] = 'Όνομα χρήστη';
+$lang['Signup.index.field_password'] = 'Κωδικός πρόσβασης';
+$lang['Signup.index.field_newpassword'] = 'Κωδικός πρόσβασης';
+$lang['Signup.index.field_confirmpassword'] = 'Επιβεβαίωση κωδικού πρόσβασης';
+$lang['Signup.index.field_receive_email_marketing'] = 'Θέλω να λαμβάνω μηνύματα ηλεκτρονικού ταχυδρομείου σχετικά με νέα προϊόντα, υπηρεσίες και προσφορές.';
+$lang['Signup.index.login_btn'] = 'Συνδεθείτε';
+$lang['Signup.index.continue_btn'] = 'Συνεχίστε στο ταμείο';
+$lang['Signup.index.signup_btn'] = 'Δημιουργία λογαριασμού';
+$lang['Signup.!error.email.blacklist'] = 'Η παραγγελία σας δεν μπορεί να διεκπεραιωθεί αυτή τη στιγμή.';
+$lang['Signup.index.field_recovery_email'] = 'Email ανάκτησης (προαιρετικό)';
+
