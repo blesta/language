f1ce87599b7a2a262bdb3b821d7ba8fdadc3010e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:05 -0800

    Updated el_gr language files

---
+++ components/modules/vpsdotnet/language/el_gr/vpsdotnet.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Vpsdotnet
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Vpsdotnet.!error.vpsdotnet_template.format'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο πρότυπο.';
+$lang['Vpsdotnet.!error.vpsdotnet_template_group.format'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο λειτουργικό σύστημα.';
+$lang['Vpsdotnet.!error.vpsdotnet_cloud.format'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο σύννεφο.';
+$lang['Vpsdotnet.!error.vpsdotnet_label.format'] = 'Η ετικέτα δεν μπορεί να περιέχει ειδικούς χαρακτήρες.';
+$lang['Vpsdotnet.!error.vpsdotnet_label.empty'] = 'Παρακαλώ εισάγετε μια ετικέτα.';
+$lang['Vpsdotnet.!error.vpsdotnet_hostname.format'] = 'Το όνομα κεντρικού υπολογιστή φαίνεται να είναι άκυρο.';
+$lang['Vpsdotnet.!error.meta[template].format'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο πρότυπο.';
+$lang['Vpsdotnet.!error.meta[set_template].format'] = 'Παρακαλούμε επιλέξτε εάν θα επιτρέψετε σε έναν πελάτη να ορίσει το πρότυπο.';
+$lang['Vpsdotnet.!error.meta[template_group].format'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο λειτουργικό σύστημα.';
+$lang['Vpsdotnet.!error.meta[set_template_group].format'] = 'Παρακαλούμε επιλέξτε εάν θα επιτρέψετε σε έναν πελάτη να ορίσει το λειτουργικό σύστημα.';
+$lang['Vpsdotnet.!error.meta[cloud].format'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο σύννεφο.';
+$lang['Vpsdotnet.!error.meta[set_cloud].format'] = 'Παρακαλούμε επιλέξτε αν θα επιτρέψετε σε έναν πελάτη να ορίσει ένα σύννεφο.';
+$lang['Vpsdotnet.!error.meta[number_of_nodes].format'] = 'Εισάγετε τον αριθμό των κόμβων που πρόκειται να διατεθούν.';
+$lang['Vpsdotnet.!error.confirm.valid'] = 'Για να εκτελέσετε την επανεγκατάσταση του προτύπου, πρέπει να επιβεβαιώσετε ότι κατανοείτε την ενέργεια επανεγκατάστασης.';
+$lang['Vpsdotnet.!error.vpsdotnet_template.valid'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο πρότυπο.';
+$lang['Vpsdotnet.!error.vps.reinstall_failed'] = 'Προέκυψε σφάλμα και το πρότυπο δεν ήταν δυνατό να επανεγκατασταθεί.';
+$lang['Vpsdotnet.!error.vps.cancel_failed'] = 'Προέκυψε σφάλμα και η υπηρεσία δεν μπόρεσε να ακυρωθεί.';
+$lang['Vpsdotnet.!error.vps.action_failed'] = 'Παρουσιάστηκε σφάλμα και η ενέργεια δεν μπόρεσε να εκτελεστεί.';
+$lang['Vpsdotnet.!error.vps.update_failed'] = 'Προέκυψε σφάλμα και η ενημέρωση της Εικονικής Μηχανής δεν ήταν δυνατή.';
+$lang['Vpsdotnet.!error.nodes.add_failed'] = 'Δεν είναι δυνατή η προσθήκη των απαιτούμενων κόμβων.';
+$lang['Vpsdotnet.!error.key.valid_connection'] = 'Δεν κατέστη δυνατή η σύνδεση με τον διακομιστή. Ελέγξτε αν η διεύθυνση ηλεκτρονικού ταχυδρομείου και το κλειδί API είναι σωστά.';
+$lang['Vpsdotnet.!error.key.empty'] = 'Εισάγετε το κλειδί σας API.';
+$lang['Vpsdotnet.!error.email.format'] = 'Παρακαλούμε εισάγετε μια έγκυρη διεύθυνση ηλεκτρονικού ταχυδρομείου.';
+$lang['Vpsdotnet.!error.server_name.empty'] = 'Παρακαλούμε εισάγετε μια ετικέτα για αυτόν τον διακομιστή.';
+$lang['Vpsdotnet.tab_client_console.root_password'] = 'Κωδικός πρόσβασης';
+$lang['Vpsdotnet.tab_client_console.heading_console'] = 'Κονσόλα';
+$lang['Vpsdotnet.tab_console.root_password'] = 'Κωδικός πρόσβασης:';
+$lang['Vpsdotnet.tab_console.heading_console'] = 'Κονσόλα';
+$lang['Vpsdotnet.tab_client_actions.field_password_submit'] = 'Επαναφορά κωδικού πρόσβασης';
+$lang['Vpsdotnet.tab_client_actions.heading_password'] = 'Επαναφορά κωδικού πρόσβασης';
+$lang['Vpsdotnet.tab_client_actions.field_hostname_submit'] = 'Αλλαγή ονόματος κεντρικού υπολογιστή';
+$lang['Vpsdotnet.tab_client_actions.field_vpsdotnet_hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Vpsdotnet.tab_client_actions.heading_hostname'] = 'Αλλαγή ονόματος κεντρικού υπολογιστή';
+$lang['Vpsdotnet.tab_client_actions.field_reinstall_submit'] = 'Επανεγκαταστήστε το';
+$lang['Vpsdotnet.tab_client_actions.field_confirm'] = 'Αντιλαμβάνομαι ότι με την επανεγκατάσταση, όλα τα δεδομένα στον διακομιστή θα διαγραφούν οριστικά και θα εγκατασταθεί το επιλεγμένο λειτουργικό σύστημα και πρότυπο.';
+$lang['Vpsdotnet.tab_client_actions.field_vpsdotnet_template'] = 'Πρότυπο';
+$lang['Vpsdotnet.tab_client_actions.field_vpsdotnet_template_group'] = 'Λειτουργικό σύστημα';
+$lang['Vpsdotnet.tab_client_actions.heading_reinstall'] = 'Επανεγκαταστήστε το';
+$lang['Vpsdotnet.tab_client_actions.heading_actions'] = 'Ενέργειες διακομιστή';
+$lang['Vpsdotnet.tab_client_actions.status_unknown'] = 'Άγνωστος';
+$lang['Vpsdotnet.tab_client_actions.status_pending'] = 'Εκκρεμεί';
+$lang['Vpsdotnet.tab_client_actions.status_offline'] = 'Offline';
+$lang['Vpsdotnet.tab_client_actions.status_online'] = 'Online';
+$lang['Vpsdotnet.tab_client_actions.heading_server_status'] = 'Κατάσταση διακομιστή';
+$lang['Vpsdotnet.tab_actions.field_password_submit'] = 'Επαναφορά κωδικού πρόσβασης';
+$lang['Vpsdotnet.tab_actions.heading_password'] = 'Επαναφορά κωδικού πρόσβασης';
+$lang['Vpsdotnet.tab_actions.field_hostname_submit'] = 'Αλλαγή ονόματος κεντρικού υπολογιστή';
+$lang['Vpsdotnet.tab_actions.field_vpsdotnet_hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Vpsdotnet.tab_actions.heading_hostname'] = 'Αλλαγή ονόματος κεντρικού υπολογιστή';
+$lang['Vpsdotnet.tab_actions.field_reinstall_submit'] = 'Επανεγκαταστήστε το';
+$lang['Vpsdotnet.tab_actions.field_confirm'] = 'Αντιλαμβάνομαι ότι με την επανεγκατάσταση, όλα τα δεδομένα στον διακομιστή θα διαγραφούν οριστικά και θα εγκατασταθεί το επιλεγμένο λειτουργικό σύστημα και πρότυπο.';
+$lang['Vpsdotnet.tab_actions.field_vpsdotnet_template'] = 'Πρότυπο';
+$lang['Vpsdotnet.tab_actions.field_vpsdotnet_template_group'] = 'Λειτουργικό σύστημα';
+$lang['Vpsdotnet.tab_actions.heading_reinstall'] = 'Επανεγκαταστήστε το';
+$lang['Vpsdotnet.tab_actions.server_status'] = 'Κατάσταση διακομιστή';
+$lang['Vpsdotnet.tab_actions.heading_actions'] = 'Δράσεις';
+$lang['Vpsdotnet.tab_actions.status_unknown'] = 'Άγνωστος';
+$lang['Vpsdotnet.tab_actions.status_pending'] = 'Εκκρεμεί';
+$lang['Vpsdotnet.tab_actions.status_offline'] = 'Offline';
+$lang['Vpsdotnet.tab_actions.status_online'] = 'Online';
+$lang['Vpsdotnet.!actions.password'] = 'Επαναφορά κωδικού πρόσβασης';
+$lang['Vpsdotnet.!actions.hostname'] = 'Αλλαγή ονόματος κεντρικού υπολογιστή';
+$lang['Vpsdotnet.!actions.reinstall'] = 'Επανεγκαταστήστε το';
+$lang['Vpsdotnet.!actions.shutdown'] = 'Τερματισμός λειτουργίας';
+$lang['Vpsdotnet.!actions.reboot'] = 'Επανεκκίνηση';
+$lang['Vpsdotnet.!actions.boot'] = 'Μπότα';
+$lang['Vpsdotnet.tab_client_console'] = 'Κονσόλα';
+$lang['Vpsdotnet.tab_client_actions'] = 'Ενέργειες διακομιστή';
+$lang['Vpsdotnet.tab_console'] = 'Κονσόλα';
+$lang['Vpsdotnet.tab_actions'] = 'Ενέργειες διακομιστή';
+$lang['Vpsdotnet.service_field.vpsdotnet_template'] = 'Πρότυπο';
+$lang['Vpsdotnet.service_field.vpsdotnet_template_group'] = 'Λειτουργικό σύστημα';
+$lang['Vpsdotnet.service_field.vpsdotnet_cloud'] = 'Σύννεφο/Ζώνη';
+$lang['Vpsdotnet.service_field.vpsdotnet_label'] = 'Ετικέτα VPS';
+$lang['Vpsdotnet.service_field.vpsdotnet_hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Vpsdotnet.package_fields.template'] = 'Πρότυπο';
+$lang['Vpsdotnet.package_fields.admin_set_template'] = 'Επιλογή προτύπου';
+$lang['Vpsdotnet.package_fields.client_set_template'] = 'Αφήστε τον πελάτη να ορίσει Template';
+$lang['Vpsdotnet.package_fields.template_group'] = 'Λειτουργικό σύστημα';
+$lang['Vpsdotnet.package_fields.admin_set_template_group'] = 'Επιλέξτε λειτουργικό σύστημα';
+$lang['Vpsdotnet.package_fields.client_set_template_group'] = 'Αφήστε τον πελάτη να ορίσει λειτουργικό σύστημα και πρότυπο';
+$lang['Vpsdotnet.package_fields.cloud'] = 'Σύννεφο/Ζώνη';
+$lang['Vpsdotnet.package_fields.admin_set_cloud'] = 'Επιλέξτε ένα σύννεφο/ζώνη';
+$lang['Vpsdotnet.package_fields.client_set_cloud'] = 'Αφήστε τον πελάτη να ορίσει το Cloud/Zone, το λειτουργικό σύστημα και το πρότυπο';
+$lang['Vpsdotnet.package_fields.number_of_nodes'] = 'Αριθμός κόμβων';
+$lang['Vpsdotnet.edit_row.add_btn'] = 'Διακομιστής ενημέρωσης';
+$lang['Vpsdotnet.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Vpsdotnet.edit_row.box_title'] = 'Επεξεργασία διακομιστή VPS.NET';
+$lang['Vpsdotnet.add_row.add_btn'] = 'Προσθήκη διακομιστή';
+$lang['Vpsdotnet.row_meta.key'] = 'Κλειδί API';
+$lang['Vpsdotnet.row_meta.email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['Vpsdotnet.row_meta.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Vpsdotnet.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Vpsdotnet.add_row.box_title'] = 'Προσθήκη διακομιστή VPS.NET';
+$lang['Vpsdotnet.service_info.vpsdotnet_password'] = 'Κωδικός πρόσβασης';
+$lang['Vpsdotnet.service_info.vpsdotnet_primary_ip_address'] = 'Πρωτεύουσα διεύθυνση IP';
+$lang['Vpsdotnet.manage.module_groups_no_results'] = 'Δεν υπάρχουν ομάδες διακομιστών.';
+$lang['Vpsdotnet.manage.module_groups.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την ομάδα διακομιστών;';
+$lang['Vpsdotnet.manage.module_groups.delete'] = 'Διαγραφή';
+$lang['Vpsdotnet.manage.module_groups.edit'] = 'Επεξεργασία';
+$lang['Vpsdotnet.manage.module_groups_heading.options'] = 'Επιλογές';
+$lang['Vpsdotnet.manage.module_groups_heading.servers'] = 'Αριθμός διακομιστών';
+$lang['Vpsdotnet.manage.module_groups_heading.name'] = 'Όνομα ομάδας';
+$lang['Vpsdotnet.manage.module_groups_title'] = 'Ομάδες κύριων διακομιστών VPS.NET';
+$lang['Vpsdotnet.manage.module_rows_no_results'] = 'Δεν υπάρχουν διακομιστές.';
+$lang['Vpsdotnet.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον διακομιστή;';
+$lang['Vpsdotnet.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['Vpsdotnet.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['Vpsdotnet.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['Vpsdotnet.manage.module_rows_heading.email'] = 'Ηλεκτρονικό ταχυδρομείο';
+$lang['Vpsdotnet.manage.module_rows_heading.server_label'] = 'Ετικέτα διακομιστή';
+$lang['Vpsdotnet.manage.module_rows_title'] = 'Κύριοι διακομιστές VPS.NET';
+$lang['Vpsdotnet.add_module_group'] = 'Προσθήκη ομάδας διακομιστών';
+$lang['Vpsdotnet.add_module_row'] = 'Προσθήκη διακομιστή';
+$lang['Vpsdotnet.please_wait'] = 'Παρακαλώ περιμένετε...';
+$lang['Vpsdotnet.order_options.first'] = 'Πρώτος μη πλήρης διακομιστής';
+$lang['Vpsdotnet.module_group'] = 'Κύρια ομάδα VPS.NET';
+$lang['Vpsdotnet.module_row_plural'] = 'Servers';
+$lang['Vpsdotnet.module_row'] = 'Κύριος διακομιστής VPS.NET';
+$lang['Vpsdotnet.description'] = 'Η VPS.NET παρέχει εικονική φιλοξενία διακομιστών.';
+$lang['Vpsdotnet.name'] = 'VPS.NET';
+
