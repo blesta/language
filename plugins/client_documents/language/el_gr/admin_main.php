20f5bb53b65bc7ab92ffb3fecd0e06ce9a0f49d7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:44 -0800

    Updated el_gr language files

---
+++ plugins/client_documents/language/el_gr/admin_main.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.add.field_submit'] = 'Ανέβασμα εγγράφου';
+$lang['AdminMain.add.field_description'] = 'Περιγραφή';
+$lang['AdminMain.add.field_document'] = 'Έγγραφο';
+$lang['AdminMain.add.field_name'] = 'Όνομα';
+$lang['AdminMain.add.boxtitle_adddocument'] = 'Ανέβασμα εγγράφου';
+$lang['AdminMain.modal.delete_document'] = 'Πραγματικά διαγράφετε αυτό το έγγραφο;';
+$lang['AdminMain.index.option_download'] = 'Λήψη';
+$lang['AdminMain.index.option_delete'] = 'Διαγραφή';
+$lang['AdminMain.index.heading_date_options'] = 'Επιλογές';
+$lang['AdminMain.index.heading_date_added'] = 'Ημερομηνία προσθήκης';
+$lang['AdminMain.index.heading_description'] = 'Περιγραφή';
+$lang['AdminMain.index.heading_name'] = 'Όνομα';
+$lang['AdminMain.index.no_results'] = 'Δεν έχουν οριστεί έγγραφα για αυτόν τον πελάτη.';
+$lang['AdminMain.index.boxtitle_documents'] = 'Έγγραφα';
+$lang['AdminMain.index.boxtitle_client'] = 'Πελάτης #%1$s - %2$s %3$s';
+$lang['AdminMain.index.categorylink_adddocument'] = 'Προσθήκη εγγράφου';
+$lang['AdminMain.add.page_title'] = 'Έγγραφα > Προσθήκη';
+$lang['AdminMain.index.page_title'] = 'Έγγραφα';
+$lang['AdminMain.!success.document_deleted'] = 'Το έγγραφο έχει διαγραφεί.';
+$lang['AdminMain.!success.document_uploaded'] = 'Το έγγραφο έχει μεταφορτωθεί.';
+
