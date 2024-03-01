10473c671dbdaef56b7f9a1b33b9cdbaf2314942
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:18 -0800

    Updated it_it language files

---
+++ language/it_it/pricings.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Pricings
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pricings.getPeriods.onetime_plural'] = 'Una sola volta';
+$lang['Pricings.getPeriods.year_plural'] = 'Anni';
+$lang['Pricings.getPeriods.month_plural'] = 'Mesi';
+$lang['Pricings.getPeriods.week_plural'] = 'Settimane';
+$lang['Pricings.getPeriods.day_plural'] = 'Giorni';
+$lang['Pricings.getPeriods.onetime'] = 'Una sola volta';
+$lang['Pricings.getPeriods.year'] = 'Anno';
+$lang['Pricings.getPeriods.month'] = 'Mese';
+$lang['Pricings.getPeriods.week'] = 'Settimana';
+$lang['Pricings.getPeriods.day'] = 'Giorno';
+$lang['Pricings.!error.currency.in_use'] = 'Non è possibile aggiornare la valuta in una tariffazione in uso.';
+$lang['Pricings.!error.period.in_use'] = 'Il periodo non può essere aggiornato in una tariffazione in uso.';
+$lang['Pricings.!error.term.in_use'] = 'Il termine non può essere aggiornato in una tariffazione in uso.';
+$lang['Pricings.!error.currency.format'] = 'Il codice della valuta deve essere di 3 caratteri.';
+$lang['Pricings.!error.cancel_fee.format'] = 'La tassa di annullamento deve essere un numero.';
+$lang['Pricings.!error.setup_fee.format'] = 'Il costo di installazione deve essere un numero.';
+$lang['Pricings.!error.price_transfer.format'] = 'Il prezzo di rinnovo deve essere un numero.';
+$lang['Pricings.!error.price_renews.valid'] = 'Il prezzo di rinnovo non può essere impostato per un unico periodo.';
+$lang['Pricings.!error.price_renews.format'] = 'Il prezzo di rinnovo deve essere un numero.';
+$lang['Pricings.!error.price.format'] = 'Il prezzo deve essere un numero.';
+$lang['Pricings.!error.period.format'] = 'Tipo di periodo non valido.';
+$lang['Pricings.!error.term.valid'] = 'Il termine deve essere maggiore di 0.';
+$lang['Pricings.!error.term.length'] = 'La lunghezza del termine non può superare i 5 caratteri.';
+$lang['Pricings.!error.term.format'] = 'Il termine deve essere un numero.';
+
