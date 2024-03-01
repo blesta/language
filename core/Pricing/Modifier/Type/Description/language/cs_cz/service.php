9a9a1eab664c71e99f90802fce5c78d2d672092c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:04 -0800

    Updated cs_cz language files

---
+++ core/Pricing/Modifier/Type/Description/language/cs_cz/service.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Service
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Service.description.package.item.updated.prorate'] = 'Poměrná změna z %1$s na %2$s (%3$s - %4$s)';
+$lang['Service.description.package.item.prorate'] = 'Poměrná část %1$s (%2$s - %3$s)';
+$lang['Service.description.package.cancel'] = '%1$s Storno poplatek';
+$lang['Service.description.package.setup'] = '%1$s Instalační poplatek';
+$lang['Service.description.package.item.updated.date'] = 'Změna z %1$s na %2$s (%3$s - %4$s)';
+$lang['Service.description.package.item.updated'] = 'Změna z %1$s na %2$s';
+$lang['Service.description.package.item.date'] = '%1$s (%2$s - %3$s)';
+$lang['Service.description.package.item'] = '%1$s';
+$lang['Service.description.service.item.updated.prorate'] = 'Poměrná změna z %1$s na %2$s - %3$s (%4$s - %5$s)';
+$lang['Service.description.service.item.prorate'] = 'Poměrná část %1$s - %2$s (%3$s - %4$s)';
+$lang['Service.description.service.cancel'] = '%1$s - %2$s Storno poplatek';
+$lang['Service.description.service.setup'] = '%1$s - %2$s Zřizovací poplatek';
+$lang['Service.description.service.item.updated.date'] = 'Změna z %1$s na %2$s - %3$s (%4$s - %5$s)';
+$lang['Service.description.service.item.updated'] = 'Změna z %1$s na %2$s - %3$s';
+$lang['Service.description.service.item.date'] = '%1$s - %2$s (%3$s - %4$s)';
+$lang['Service.description.service.item'] = '%1$s - %2$s';
+
