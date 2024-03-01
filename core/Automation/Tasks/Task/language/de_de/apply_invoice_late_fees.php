b32421806062bccaa440de1119ad7caaed998211
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:51 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/apply_invoice_late_fees.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Apply Invoice Late Fees
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.apply_invoice_late_fees.late_fee_success'] = 'Säumniszuschlag erfolgreich auf Rechnung #%1$s für Kunde #%2$s angewendet.';
+$lang['Automation.task.apply_invoice_late_fees.late_fee_failed'] = 'Verspätungsgebühr kann nicht auf Rechnung #%1$s für Kunde #%2$s angewendet werden.';
+$lang['Automation.task.apply_invoice_late_fees.invoice_line'] = 'Säumniszuschlag';
+$lang['Automation.task.apply_invoice_late_fees.completed'] = 'Die Aufgabe "Verspätungsgebühren anwenden" ist abgeschlossen.';
+$lang['Automation.task.apply_invoice_late_fees.attempt'] = 'Versuch der Erhebung von Säumniszuschlägen für überfällige Rechnungen.';
+
