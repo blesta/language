ac061b737d2aeb8174065ba4d45626a4bc531e1f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:14 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/backups_amazons3.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Backups Amazons3
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.backups_amazons3.attempt'] = 'Προσπάθεια δημιουργίας αντιγράφου ασφαλείας της βάσης δεδομένων στο AmazonS3.';
+$lang['Automation.task.backups_amazons3.completed'] = 'Η εργασία δημιουργίας αντιγράφων ασφαλείας της βάσης δεδομένων AmazonS3 έχει ολοκληρωθεί.';
+$lang['Automation.task.backups_amazons3.success'] = 'Το αντίγραφο ασφαλείας ολοκληρώθηκε με επιτυχία.';
+
