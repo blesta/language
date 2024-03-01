3a635a92beac0ad1346fe4a6e543548baab6b812
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:55 -0800

    Updated cs_cz language files

---
+++ plugins/mass_mailer/language/cs_cz/mass_mailer_jobs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Mass Mailer Jobs
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerJobs.status.complete'] = 'Kompletní';
+$lang['MassMailerJobs.status.in_progress'] = 'Probíhá';
+$lang['MassMailerJobs.status.pending'] = 'Čeká se na';
+$lang['MassMailerJobs.!error.job_id.exists'] = 'Neplatné ID úlohy.';
+$lang['MassMailerJobs.!error.date_added.format'] = 'Prosím, nastavte přidané datum.';
+$lang['MassMailerJobs.!error.data.empty'] = 'Uveďte prosím další informace o práci.';
+$lang['MassMailerJobs.!error.task_count.format'] = 'Počet úloh musí být kladné celé číslo.';
+$lang['MassMailerJobs.!error.status.format'] = 'Vyberte prosím platný stav.';
+$lang['MassMailerJobs.!error.company_id.valid'] = 'Nastavte prosím ID společnosti.';
+
