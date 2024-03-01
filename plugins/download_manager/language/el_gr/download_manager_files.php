5c810293c4f0b70cf5e57a44afcbb9c28a994cd2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:54 -0800

    Updated el_gr language files

---
+++ plugins/download_manager/language/el_gr/download_manager_files.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Download Manager Files
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerFiles.!error.category_id.exists'] = 'Παρακαλούμε επιλέξτε μια έγκυρη κατηγορία.';
+$lang['DownloadManagerFiles.!error.company_id.exists'] = 'Μη έγκυρο αναγνωριστικό εταιρείας.';
+$lang['DownloadManagerFiles.!error.name.empty'] = 'Παρακαλώ εισάγετε ένα όνομα για αυτό το αρχείο.';
+$lang['DownloadManagerFiles.!error.file_name.format'] = 'Παρακαλούμε ανεβάστε ένα αρχείο ή εισαγάγετε μια έγκυρη διαδρομή προς το αρχείο.';
+$lang['DownloadManagerFiles.!error.public.format'] = 'Η επιλογή δημόσιας διαθεσιμότητας έχει μη έγκυρη μορφή.';
+$lang['DownloadManagerFiles.!error.public.assignment'] = 'Το αρχείο πρέπει να είναι διαθέσιμο είτε στο κοινό, είτε σε ομάδες πελατών, είτε σε πακέτα.';
+$lang['DownloadManagerFiles.!error.permit_client_groups.format'] = 'Η επιλογή διαθεσιμότητας ομάδας πελατών έχει μη έγκυρη μορφή.';
+$lang['DownloadManagerFiles.!error.file_groups.format'] = 'Τουλάχιστον μία επιλεγμένη ομάδα πελατών δεν μπορεί να προστεθεί επειδή δεν υπάρχει.';
+$lang['DownloadManagerFiles.!error.permit_packages.format'] = 'Η επιλογή διαθεσιμότητας πακέτου έχει μη έγκυρη μορφή.';
+$lang['DownloadManagerFiles.!error.file_packages.format'] = 'Τουλάχιστον ένα επιλεγμένο πακέτο δεν μπορεί να προστεθεί επειδή δεν υπάρχει.';
+$lang['DownloadManagerFiles.!error.file_id.exists'] = 'Το αρχείο που ενημερώνεται δεν υπάρχει.';
+
