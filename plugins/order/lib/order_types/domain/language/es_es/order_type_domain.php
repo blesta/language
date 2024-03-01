7f7caf2293381d26cad4c50f4890a685b241f195
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:45 -0800

    Updated es_es language files

---
+++ plugins/order/lib/order_types/domain/language/es_es/order_type_domain.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Order Type Domain
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderTypeDomain.name'] = 'Dominio y otros';
+$lang['OrderTypeDomain.settings.tooltip_limit_term'] = 'Limite el dominio al menor número de años permitido para el TLD, normalmente 1 ó 2 años.';
+$lang['OrderTypeDomain.!error.domain.invalid'] = 'No se admite ese nombre de dominio.';
+$lang['OrderTypeDomain.!error.domain.empty'] = 'Introduzca un nombre de dominio.';
+$lang['OrderTypeDomain.!error.template.valid'] = 'La plantilla seleccionada requiere al menos un grupo de paquetes.';
+$lang['OrderTypeDomain.!error.meta[domain_group].exists'] = 'Seleccione un grupo de paquetes de dominio válido.';
+$lang['OrderTypeDomain.!error.meta[domain_group].groups'] = 'El grupo de paquetes del dominio no puede ser también un grupo de paquetes asignado.';
+$lang['OrderTypeDomain.settings.domain_group_title'] = 'Grupo de paquetes de dominios';
+$lang['OrderTypeDomain.settings.free_domain_title'] = 'Dominio libre';
+$lang['OrderTypeDomain.settings.field_free_domain'] = 'Incluir un dominio gratis al comprar otra cosa';
+$lang['OrderTypeDomain.settings.field_limit_term'] = 'Limitar el dominio al menor número de años';
+$lang['OrderTypeDomain.settings.field_first_term'] = 'Gratuito sólo el primer trimestre';
+$lang['OrderTypeDomain.settings.tooltip_free_domain'] = 'Incluye un dominio gratuito de una lista de TLD aprobados con la compra de otro paquete.';
+$lang['OrderTypeDomain.settings.text_assigned_tlds'] = 'TLD aprobados';
+$lang['OrderTypeDomain.settings.text_available_tlds'] = 'TLD disponibles';
+$lang['OrderTypeDomain.settings.text_assigned_package_groups'] = 'Grupos de paquetes subvencionables';
+$lang['OrderTypeDomain.settings.text_available_package_groups'] = 'Grupos de paquetes disponibles';
+$lang['OrderTypeDomain.settings.text_term'] = 'Plazo y periodo mínimos';
+$lang['OrderTypeDomain.settings.heading_term'] = 'Plazo';
+$lang['OrderTypeDomain.settings.heading_period'] = 'Periodo';
+$lang['OrderTypeDomain.settings.heading_options'] = 'Opciones';
+$lang['Main.steps.step_1_description'] = 'Elija un dominio';
+$lang['OrderTypeDomain.settings.field_search'] = 'Buscar TLD';
+
