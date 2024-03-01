4f0f4211336b212076ad4f5c151e04a0805a2e2c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:08 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/nonmerchant_gateway.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Nonmerchant Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['NonmerchantGateway.!error.general'] = 'Při zpracování požadavku bránou došlo k chybě.';
+$lang['NonmerchantGateway.!error.unsupported'] = 'Brána tuto akci nepodporuje.';
+$lang['NonmerchantGateway.!error.transaction_not_found'] = 'Transakce je neplatná nebo již není k dispozici pro zpracování dané akce.';
+$lang['NonmerchantGateway.!error.invalid'] = 'Byla přijata neplatná nebo nerozpoznaná platební odpověď.';
+
