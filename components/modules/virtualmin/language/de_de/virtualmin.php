fd8ea7cbbb43e6eaf13f68b2f573c77e7810b8dd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:42 -0800

    Updated de_de language files

---
+++ components/modules/virtualmin/language/de_de/virtualmin.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Virtualmin
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Virtualmin.!error.virtualmin_password.valid'] = 'Das Passwort muss mindestens 8 Zeichen lang sein.';
+$lang['Virtualmin.!error.virtualmin_username.length'] = 'Der Benutzername muss zwischen 1 und 16 Zeichen lang sein.';
+$lang['Virtualmin.!error.virtualmin_username.test'] = 'Der Benutzername darf nicht mit \'test\' beginnen.';
+$lang['Virtualmin.!error.virtualmin_username.format'] = 'Der Benutzername darf nur Buchstaben und Zahlen enthalten und darf nicht mit einer Zahl beginnen.';
+$lang['Virtualmin.!error.virtualmin_domain.test'] = 'Der Domänenname kann nicht mit \'test\' beginnen.';
+$lang['Virtualmin.!error.virtualmin_domain.format'] = 'Bitte geben Sie einen gültigen Domainnamen ein, z. B. domain.com.';
+$lang['Virtualmin.!error.module_row.missing'] = 'Es ist ein interner Fehler aufgetreten. Die Modulzeile ist nicht verfügbar.';
+$lang['Virtualmin.!error.api.internal'] = 'Es ist ein interner Fehler aufgetreten, oder der Server hat nicht auf die Anfrage geantwortet.';
+$lang['Virtualmin.!error.meta[template].empty'] = 'Eine VirtualMin-Vorlage ist erforderlich.';
+$lang['Virtualmin.!error.meta[plan].empty'] = 'Ein VirtualMin Plan ist erforderlich.';
+$lang['Virtualmin.!error.account_limit_valid'] = 'Bitte geben Sie ein gültiges Kontolimit ein.';
+$lang['Virtualmin.!error.password_valid_connection'] = 'Eine Verbindung zum Server konnte nicht hergestellt werden. Bitte überprüfen Sie, ob Hostname, Benutzername, Passwort und Port korrekt sind.';
+$lang['Virtualmin.!error.password_valid'] = 'Das Passwort scheint ungültig zu sein.';
+$lang['Virtualmin.!error.port_valid'] = 'Der Port scheint ungültig zu sein.';
+$lang['Virtualmin.!error.user_name_valid'] = 'Der Benutzername scheint ungültig zu sein.';
+$lang['Virtualmin.!error.host_name_valid'] = 'Der Hostname scheint ungültig zu sein.';
+$lang['Virtualmin.!error.server_name_valid'] = 'Sie müssen ein Server-Label eingeben.';
+$lang['Virtualmin.service_field.tooltip.password'] = 'Sie können das Passwort leer lassen, damit es automatisch generiert wird.';
+$lang['Virtualmin.service_field.tooltip.username'] = 'Sie können den Benutzernamen leer lassen, um ihn automatisch zu generieren.';
+$lang['Virtualmin.service_info.option_login'] = 'Einloggen';
+$lang['Virtualmin.service_info.options'] = 'Optionen';
+$lang['Virtualmin.service_info.server'] = 'Server';
+$lang['Virtualmin.service_info.password'] = 'Passwort';
+$lang['Virtualmin.service_info.username'] = 'Benutzername';
+$lang['Virtualmin.tab_stats.info.maximum_mailboxes'] = 'Maximale Mailboxen';
+$lang['Virtualmin.tab_stats.info.maximum_databases'] = 'Maximale Datenbanken';
+$lang['Virtualmin.tab_stats.info.databases_count'] = 'Datenbank-Zählung';
+$lang['Virtualmin.tab_stats.info.databases_size'] = 'Größe der Datenbank';
+$lang['Virtualmin.tab_stats.info.disk_used'] = 'Verwendete Platte';
+$lang['Virtualmin.tab_stats.info_heading.value'] = 'Wert';
+$lang['Virtualmin.tab_stats.info_heading.field'] = 'Feld';
+$lang['Virtualmin.tab_stats.info_title'] = 'Informationen';
+$lang['Virtualmin.tab_client_actions.field_password_submit'] = 'Passwort aktualisieren';
+$lang['Virtualmin.tab_client_actions.field_virtualmin_confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['Virtualmin.tab_client_actions.field_virtualmin_password'] = 'Passwort';
+$lang['Virtualmin.tab_client_actions.change_password'] = 'Passwort ändern';
+$lang['Virtualmin.service_field.confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['Virtualmin.service_field.password'] = 'Passwort';
+$lang['Virtualmin.service_field.username'] = 'Benutzername';
+$lang['Virtualmin.service_field.domain'] = 'Bereich';
+$lang['Virtualmin.package_fields.template'] = 'VirtualMin-Vorlage';
+$lang['Virtualmin.package_fields.plan'] = 'VirtualMin Plan';
+$lang['Virtualmin.row_meta.account_limit'] = 'Konto-Limit';
+$lang['Virtualmin.row_meta.use_ssl'] = 'Verwenden Sie SSL für die Verbindung zur API (empfohlen)';
+$lang['Virtualmin.row_meta.port'] = 'Hafen';
+$lang['Virtualmin.row_meta.password'] = 'Passwort';
+$lang['Virtualmin.row_meta.user_name'] = 'Name des Benutzers';
+$lang['Virtualmin.row_meta.host_name'] = 'Hostname';
+$lang['Virtualmin.row_meta.server_name'] = 'Server-Etikett';
+$lang['Virtualmin.edit_row.add_btn'] = 'Server bearbeiten';
+$lang['Virtualmin.edit_row.basic_title'] = 'Grundeinstellungen';
+$lang['Virtualmin.edit_row.box_title'] = 'VirtualMin Server bearbeiten';
+$lang['Virtualmin.add_row.add_btn'] = 'Server hinzufügen';
+$lang['Virtualmin.add_row.basic_title'] = 'Grundeinstellungen';
+$lang['Virtualmin.add_row.box_title'] = 'VirtualMin Server hinzufügen';
+$lang['Virtualmin.order_options.first'] = 'Erster nicht vollständiger Server';
+$lang['Virtualmin.manage.module_groups_no_results'] = 'Es gibt keine Servergruppen.';
+$lang['Virtualmin.manage.module_rows_no_results'] = 'Es gibt keine Server.';
+$lang['Virtualmin.manage.module_groups.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Servergruppe löschen wollen?';
+$lang['Virtualmin.manage.module_rows.confirm_delete'] = 'Sind Sie sicher, dass Sie diesen Server löschen wollen?';
+$lang['Virtualmin.manage.module_groups.delete'] = 'Löschen';
+$lang['Virtualmin.manage.module_rows.delete'] = 'Löschen';
+$lang['Virtualmin.manage.module_groups.edit'] = 'bearbeiten';
+$lang['Virtualmin.manage.module_rows.edit'] = 'bearbeiten';
+$lang['Virtualmin.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Virtualmin.manage.module_groups_heading.options'] = 'Optionen';
+$lang['Virtualmin.manage.module_groups_heading.servers'] = 'Anzahl der Server';
+$lang['Virtualmin.manage.module_groups_heading.name'] = 'Name der Gruppe';
+$lang['Virtualmin.manage.module_rows_heading.options'] = 'Optionen';
+$lang['Virtualmin.manage.module_rows_heading.accounts'] = 'Konten';
+$lang['Virtualmin.manage.module_rows_heading.port'] = 'Hafen';
+$lang['Virtualmin.manage.module_rows_heading.hostname'] = 'Hostname';
+$lang['Virtualmin.manage.module_rows_heading.name'] = 'Server-Etikett';
+$lang['Virtualmin.manage.module_groups_title'] = 'Server-Gruppen';
+$lang['Virtualmin.manage.module_rows_title'] = 'Server';
+$lang['Virtualmin.add_module_group'] = 'Server-Gruppe hinzufügen';
+$lang['Virtualmin.add_module_row'] = 'Server hinzufügen';
+$lang['Virtualmin.tab_client_actions'] = 'Aktionen';
+$lang['Virtualmin.tab_stats'] = 'Statistik';
+$lang['Virtualmin.module_group'] = 'Server-Gruppe';
+$lang['Virtualmin.module_row_plural'] = 'Server';
+$lang['Virtualmin.module_row'] = 'Server';
+$lang['Virtualmin.description'] = 'Virtualmin ist ein Domain-Hosting- und Website-Control-Panel, das die Möglichkeit bietet, viele Domains zu erstellen und zu verwalten. Es ist sowohl als Open-Source- als auch als kommerzielle Version erhältlich. Es basiert auf Webmin.';
+$lang['Virtualmin.name'] = 'VirtualMin';
+
