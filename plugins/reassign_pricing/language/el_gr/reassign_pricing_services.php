e1e6571f479bab75fe803c63586c18809152e2bd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:49 -0800

    Updated el_gr language files

---
+++ plugins/reassign_pricing/language/el_gr/reassign_pricing_services.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Reassign Pricing Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReassignPricingServices.!error.pricing_id.exists'] = 'Μη έγκυρο αναγνωριστικό τιμολόγησης.';
+$lang['ReassignPricingServices.!error.pricing_id.company'] = 'Το αναγνωριστικό τιμολόγησης δεν ανήκει στην ίδια εταιρεία με την υπηρεσία.';
+$lang['ReassignPricingServices.!error.package_group_id.exists'] = 'Μη έγκυρο αναγνωριστικό ομάδας πακέτων.';
+$lang['ReassignPricingServices.!error.package_group_id.company'] = 'Το αναγνωριστικό ομάδας πακέτων δεν ανήκει στην ίδια εταιρεία με την υπηρεσία.';
+$lang['ReassignPricingServices.!error.module_row_id.exists'] = 'Μη έγκυρο αναγνωριστικό γραμμής ενότητας από το δεδομένο αναγνωριστικό τιμολόγησης.';
+$lang['ReassignPricingServices.!error.module_row_id.company'] = 'Το αναγνωριστικό γραμμής ενότητας δεν ανήκει στην ίδια εταιρεία με την υπηρεσία.';
+$lang['ReassignPricingServices.!error.service_id.exists'] = 'Μη έγκυρο αναγνωριστικό υπηρεσίας.';
+
