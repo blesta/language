26ec63f1b62d431281c26ddbe66b615d2badc74b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:35 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/ajax/language/el_gr/checkout.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Checkout
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Checkout.index.header_checkout'] = 'Σχεδόν φτάσαμε!';
+$lang['Checkout.index.checkout_note'] = 'Παρακαλούμε κάντε την πληρωμή για να ολοκληρώσετε την παραγγελία σας.';
+$lang['Checkout.index.field_useaccount'] = 'Χρήση λογαριασμού πληρωμών';
+$lang['Checkout.index.field_newdetails'] = 'Νέα στοιχεία πληρωμής';
+$lang['Checkout.index.heading_other_payment_options'] = 'Άλλες επιλογές πληρωμής';
+$lang['Checkout.index.heading_payment_options'] = 'Επιλογές πληρωμής';
+$lang['Checkout.index.pay_btn'] = 'Πληρώστε & ολοκληρώστε την παραγγελία';
+$lang['Checkout.index.no_options'] = 'Δεν υπάρχουν διαθέσιμες επιλογές πληρωμής.';
+$lang['Checkout.contact_info.heading_contact'] = 'Στοιχεία επικοινωνίας';
+$lang['Checkout.contact_info.field_contact_id'] = 'Αντιγραφή στοιχείων επικοινωνίας από';
+$lang['Checkout.contact_info.text_select_contact'] = 'Μπορείτε να επιλέξετε μια υπάρχουσα επαφή για να προσυμπληρώσετε αυτή τη φόρμα.';
+$lang['Checkout.cc_info.heading_cc'] = 'Πληροφορίες πιστωτικής κάρτας';
+$lang['Checkout.ach_info.heading_ach'] = 'Πληροφορίες τραπεζικού λογαριασμού';
+$lang['Checkout.complete.order_more_btn'] = 'Τοποθετήστε μια άλλη παραγγελία';
+$lang['Checkout.complete.invoice_paid'] = 'ΠΛΗΡΩΜΗ';
+
