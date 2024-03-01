b6da9c0cfc206ec802467d09abfa9d13d5ce366c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:40 -0800

    Updated cs_cz language files

---
+++ plugins/extension_generator/language/cs_cz/admin_main.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.confirm.generate'] = 'Generování rozšíření';
+$lang['AdminMain.confirm.custom_path'] = 'Vlastní cesta';
+$lang['AdminMain.confirm.tooltip_location'] = 'Adresář, do kterého se nahrávají vygenerované soubory rozšíření.';
+$lang['AdminMain.confirm.location'] = 'Umístění rozšíření';
+$lang['AdminMain.confirm.text_generation'] = 'Nastavení rozšíření je dokončeno.  Klepnutím na uzly na liště průběhu můžete zkontrolovat libovolnou část.  Kliknutím na tlačítko "Generovat rozšíření" dokončete a automaticky vygenerujte soubory pro své rozšíření.';
+$lang['AdminMain.confirm.heading_confirm'] = 'Potvrzení';
+$lang['AdminMain.general.placeholder_name'] = 'Název rozšíření';
+$lang['AdminMain.general.tooltip_code_examples'] = 'Zkontrolujte, zda obsahuje okomentované řádky ukázkového kódu pro funkce, jako jsou Cron Tasks, seznamy událostí a vazby atd.';
+$lang['AdminMain.general.tooltip_form_type'] = 'Basic použít značně zkrácenou verzi formuláře rozšíření, která obsahuje pouze minimum nezbytné pro vygenerování rozšíření.';
+$lang['AdminMain.general.tooltip_type'] = 'Moduly obsluhují mnoho funkcí služeb včetně poskytování služeb na vzdálených serverech. Zásuvné moduly jsou výkonné integrace schopné v systému Blesta provádět téměř cokoli. Obchodní brány zpracovávají platby a zároveň udržují klienty v rozhraní systému Blesta.  Neobchodní brány posílají klienty na stránky zpracovatele plateb k dokončení platby.';
+$lang['AdminMain.general.tooltip_name'] = 'Zobrazovaný název rozšíření. Používá se také při vytváření různých kódů. Pokud se například jmenuje Můj modul, pak adresář modulu bude my_module a název třídy bude MyModule.  U zásuvných modulů neuvádějte v názvu slovo "plugin", protože to způsobuje určité problémy při generování kódu.';
+$lang['AdminMain.general.basic_info'] = 'Další - Základní informace';
+$lang['AdminMain.general.code_examples'] = 'Zahrnout příklad kódu';
+$lang['AdminMain.general.form_type'] = 'Typ formuláře';
+$lang['AdminMain.general.type'] = 'Typ rozšíření';
+$lang['AdminMain.general.name'] = 'Název';
+$lang['AdminMain.general.heading_general_settings'] = 'Obecná nastavení';
+$lang['AdminMain.index.extensions_no_results'] = 'V tuto chvíli nejsou k dispozici žádná prodloužení.';
+$lang['AdminMain.index.text_confirm_delete'] = 'Opravdu chcete toto rozšíření odstranit?';
+$lang['AdminMain.index.option_delete'] = 'Odstranit';
+$lang['AdminMain.index.option_edit'] = 'Upravit';
+$lang['AdminMain.index.heading_options'] = 'Možnosti';
+$lang['AdminMain.index.heading_date_updated'] = 'Datum aktualizace';
+$lang['AdminMain.index.heading_code_examples'] = 'Příklady kódů';
+$lang['AdminMain.index.heading_form_type'] = 'Typ formuláře';
+$lang['AdminMain.index.heading_type'] = 'Typ';
+$lang['AdminMain.index.heading_name'] = 'Název';
+$lang['AdminMain.index.heading_id'] = 'ID';
+$lang['AdminMain.index.extension_add'] = 'Přidat rozšíření';
+$lang['AdminMain.index.boxtitle_extensions'] = 'Generátor rozšíření - rozšíření';
+$lang['AdminMain.index.boxtitle_extension_generator'] = 'Generátor rozšíření';
+$lang['AdminMain.index.page_title'] = 'Generátor rozšíření';
+$lang['AdminMain.getfilelocations.custom'] = 'Vlastní';
+$lang['AdminMain.getfilelocations.upload'] = 'Blesta Nahrává adresář';
+$lang['AdminMain.getfilelocations.nonmerchant'] = 'Adresář neobchodních bran společnosti Blesta';
+$lang['AdminMain.getfilelocations.merchant'] = 'Adresář obchodních bran Blesta';
+$lang['AdminMain.getfilelocations.plugin'] = 'Adresář pluginů Blesta';
+$lang['AdminMain.getfilelocations.module'] = 'Adresář modulů Blesta';
+$lang['AdminMain.!success.nonmerchant_created'] = 'Nová brána byla úspěšně vygenerována a najdete ji na adrese %1$s.';
+$lang['AdminMain.!success.merchant_created'] = 'Nová brána byla úspěšně vygenerována a najdete ji na adrese %1$s.';
+$lang['AdminMain.!success.plugin_created'] = 'Nový zásuvný modul byl úspěšně vygenerován a naleznete jej na adrese %1$s.';
+$lang['AdminMain.!success.module_created'] = 'Nový modul byl úspěšně vygenerován a naleznete jej na adrese %1$s.';
+$lang['AdminMain.!success.package_deleted'] = 'Rozšíření bylo úspěšně odstraněno';
+$lang['AdminMain.!notice.updating_installed_extension'] = 'Toto rozšíření je aktuálně nainstalováno. Důrazně doporučujeme, abyste jej před regenerací souborů odinstalovali, jinak může dojít k poškození konzistence vaší databáze.';
+$lang['AdminMain.!notice.file_overwrite'] = 'Soubory pro toto rozšíření již existují v souborech Blesta a budou přepsány, pokud použijete adresář Blesta.';
+$lang['AdminMain.!notice.type_warning'] = 'Změna typu rozšíření nenávratně vymaže všechna nastavení uložená pro toto rozšíření.';
+$lang['AdminMain.!error.generation_failed'] = 'Nelze vygenerovat vaše rozšíření.  Vygenerována výjimka: %1$s';
+$lang['AdminMain.!error.name_taken'] = 'Tento název je v rozporu s existujícím adresářem rozšíření.';
+
