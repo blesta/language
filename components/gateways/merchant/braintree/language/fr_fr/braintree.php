c152acf830c2a8d7144664650b9ab3e9a7db8a80
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:23 -0800

    Updated fr_fr language files

---
+++ components/gateways/merchant/braintree/language/fr_fr/braintree.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Braintree
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Braintree.name'] = 'Braintree';
+$lang['Braintree.description'] = 'Les commerçants de plus de 45 pays/régions du monde entier peuvent accepter, fractionner et activer des paiements dans plus de 130 devises à l\'aide de Braintree.';
+$lang['Braintree.merchant_id'] = 'Identifiant du commerçant';
+$lang['Braintree.public_key'] = 'Clé publique';
+$lang['Braintree.private_key'] = 'Clé privée';
+$lang['Braintree.live_mode'] = 'Mode Live';
+$lang['Braintree.environment'] = 'Environnement';
+$lang['Braintree.environment.sandbox'] = 'Bac à sable';
+$lang['Braintree.environment.production'] = 'Production';
+$lang['Braintree.!error.live_mode.valid'] = 'Le mode Live doit être réglé sur "true" s\'il est indiqué.';
+$lang['Braintree.!error.merchant_id'] = 'Veuillez saisir votre identifiant de commerçant.';
+$lang['Braintree.!error.public_key'] = 'Veuillez saisir votre clé publique.';
+$lang['Braintree.!error.private_key'] = 'Veuillez saisir votre clé privée.';
+$lang['Braintree.!error.environment'] = 'Veuillez sélectionner un environnement valide.';
+
