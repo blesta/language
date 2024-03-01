1a1d8faf851cb8f7b65548af8eca551977f525d6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:49 -0800

    Updated it_it language files

---
+++ plugins/support_manager/language/it_it/support_manager_plugin.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Support Manager Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerPlugin.permission.admin_reminders'] = 'Promemoria';
+$lang['SupportManagerPlugin.permission.admin_knowledgebase'] = 'Base di conoscenza';
+$lang['SupportManagerPlugin.permission.admin_staff'] = 'Personale';
+$lang['SupportManagerPlugin.permission.admin_responses'] = 'Risposte';
+$lang['SupportManagerPlugin.permission.admin_departments'] = 'Dipartimenti';
+$lang['SupportManagerPlugin.permission.admin_tickets_client'] = 'Widget del profilo del cliente';
+$lang['SupportManagerPlugin.permission.admin_tickets_delete'] = 'Cancellare definitivamente i biglietti';
+$lang['SupportManagerPlugin.permission.admin_tickets'] = 'Biglietti';
+$lang['SupportManagerPlugin.permission.admin_main'] = 'Supporto';
+$lang['SupportManagerPlugin.event_getsearchoptions.tickets'] = 'Ricerca biglietti';
+$lang['SupportManagerPlugin.card_client.tickets'] = 'Biglietti';
+$lang['SupportManagerPlugin.action_staff_client.add'] = 'Biglietto aperto';
+$lang['SupportManagerPlugin.widget_staff_client.tickets'] = 'Biglietti';
+$lang['SupportManagerPlugin.nav_primary_staff.reminders'] = 'Promemoria';
+$lang['SupportManagerPlugin.nav_primary_staff.knowledgebase'] = 'Base di conoscenza';
+$lang['SupportManagerPlugin.nav_primary_staff.staff'] = 'Personale';
+$lang['SupportManagerPlugin.nav_primary_staff.responses'] = 'Risposte';
+$lang['SupportManagerPlugin.nav_primary_staff.departments'] = 'Dipartimenti';
+$lang['SupportManagerPlugin.nav_primary_staff.tickets'] = 'Biglietti';
+$lang['SupportManagerPlugin.nav_primary_staff.main'] = 'Supporto';
+$lang['SupportManagerPlugin.widget_client_home.main'] = 'Biglietti';
+$lang['SupportManagerPlugin.nav_primary_client.knowledgebase'] = 'Base di conoscenza';
+$lang['SupportManagerPlugin.nav_primary_client.tickets'] = 'Biglietti';
+$lang['SupportManagerPlugin.nav_primary_client.main'] = 'Supporto';
+$lang['SupportManagerPlugin.cron.send_reminders_desc'] = 'Invio automatico di promemoria per i ticket di un determinato stato dopo un certo periodo di tempo.';
+$lang['SupportManagerPlugin.cron.send_reminders_name'] = 'Inviare promemoria';
+$lang['SupportManagerPlugin.cron.delete_tickets_desc'] = 'Eliminazione automatica dei ticket cestinati, come configurato per i reparti nel Gestore dell\'assistenza.';
+$lang['SupportManagerPlugin.cron.delete_tickets_name'] = 'Cancellare i biglietti cestinati';
+$lang['SupportManagerPlugin.cron.close_tickets_desc'] = 'Chiude automaticamente i ticket aperti come configurato per i reparti nel Support Manager.';
+$lang['SupportManagerPlugin.cron.close_tickets_name'] = 'Chiudere i biglietti';
+$lang['SupportManagerPlugin.cron.poll_tickets_desc'] = 'Si collega al server POP3/IMAP per scaricare le e-mail e convertirle in biglietti.';
+$lang['SupportManagerPlugin.cron.poll_tickets_name'] = 'Scarica i biglietti';
+$lang['SupportManagerPlugin.description'] = 'Consente agli utenti e al personale di aprire e gestire i ticket di assistenza.';
+$lang['SupportManagerPlugin.name'] = 'Responsabile dell\'assistenza';
+
