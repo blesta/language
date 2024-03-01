4446928d594b9f33094ffc238339e2de1a483d64
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:33 -0800

    Updated es_es language files

---
+++ language/es_es/module_types.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Module Types
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ModuleTypes.!error.name.valid'] = 'El nombre no es válido.';
+$lang['ModuleTypes.!error.type_id.valid'] = 'El ID de tipo de módulo no es válido.';
+
