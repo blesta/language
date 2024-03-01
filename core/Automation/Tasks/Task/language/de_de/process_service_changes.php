a0e58b2ed3a0d1e9f32e83cc56c7cfa2369ad254
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:00 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/process_service_changes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Process Service Changes
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.process_service_changes.expired'] = 'Serviceänderung #%1$s ist ohne Zahlung abgelaufen und in den Status: %2$s gewechselt';
+$lang['Automation.task.process_service_changes.service_inactive'] = 'Service Change #%1$s wurde nicht verarbeitet, da der Dienst inaktiv ist.';
+$lang['Automation.task.process_service_changes.missing_invoice'] = 'Rechnungs-ID #%1$s nicht gefunden. Serviceänderung #%2$s für Service #%3$s wurde auf Status: %4$s geändert.';
+$lang['Automation.task.process_service_changes.process_result'] = 'Die Bearbeitung der Dienständerung #%1$s führte zu Status: %2$s';
+$lang['Automation.task.process_service_changes.completed'] = 'Die Aufgabe "Prozessdienständerungen" ist abgeschlossen.';
+$lang['Automation.task.process_service_changes.attempt'] = 'Ich versuche, Dienständerungen zu bearbeiten.';
+
