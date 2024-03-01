ff5f8cf360442434d7e3b31ac57bb33faa946c74
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:16 -0800

    Updated es_es language files

---
+++ plugins/import_manager/components/migrators/whmcs/8.0/language/es_es/whmcs8_0.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Whmcs8
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Whmcs8_0.!error.host.invalid'] = 'Se requiere un host de base de datos.';
+$lang['Whmcs8_0.!error.database.invalid'] = 'El nombre de la base de datos es obligatorio.';
+$lang['Whmcs8_0.!error.user.invalid'] = 'Se requiere usuario de base de datos.';
+$lang['Whmcs8_0.!error.pass.invalid'] = 'La contraseña de la base de datos es obligatoria.';
+$lang['Whmcs8_0.!error.key.invalid'] = 'Se requiere una AESKEY válida.';
+$lang['Whmcs8_0.!error.import'] = 'La importación se completó pero se produjeron los siguientes errores:';
+$lang['Whmcs8_0.!notice.gmp'] = 'No se ha encontrado la extensión \'gmp\'. Esta extensión se recomienda para mejorar el rendimiento y mejorará el proceso de importación. Puede continuar sin esta extensión, pero el proceso de importación puede ser mucho más lento.';
+$lang['Whmcs8_0.settings.host'] = 'Host de base de datos';
+$lang['Whmcs8_0.settings.database'] = 'Nombre de la base de datos';
+$lang['Whmcs8_0.settings.user'] = 'Usuario de la base de datos';
+$lang['Whmcs8_0.settings.pass'] = 'Contraseña de la base de datos';
+$lang['Whmcs8_0.settings.key'] = 'CC Cifrado Hash';
+$lang['Whmcs8_0.settings.key.info'] = 'Esta es la clave de encriptación utilizada por WHMCS para encriptar varios bits de datos. Se puede encontrar en el archivo configuration.php de WHMCS.';
+$lang['Whmcs8_0.settings.balance_credit'] = 'Auto balance de transacciones para que coincida con los créditos del cliente en WHMCS';
+$lang['Whmcs8_0.settings.balance_credit.info'] = 'Si está marcada, generará una transacción o una factura para garantizar que el crédito del cliente en Blesta coincide con el establecido para el cliente en WHMCS. Esto es necesario porque WHMCS no contabiliza correctamente los créditos de cliente.';
+$lang['Whmcs8_0.settings.enable_debug'] = 'Activar depuración';
+$lang['Whmcs8_0.configuration.create_packages_true'] = 'Crear automáticamente los paquetes necesarios durante la importación';
+$lang['Whmcs8_0.configuration.create_packages_false'] = 'Asignar paquetes manualmente';
+$lang['Whmcs8_0.configuration.local_package'] = 'Paquete local';
+$lang['Whmcs8_0.configuration.remote_package'] = 'Paquete remoto';
+$lang['Whmcs8_0.configuration.no_local_packages'] = 'Debe crear paquetes antes de poder asignarlos.';
+
