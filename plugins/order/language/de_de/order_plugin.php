1f62d9faa2a0ea7a4ab05c0f0427564a9bb5b1b1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:15 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/order_plugin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Order Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderPlugin.permission.admin_main_widget'] = 'Kundenprofil-Widget';
+$lang['OrderPlugin.permission.admin_main'] = 'Bestellungen';
+$lang['OrderPlugin.permission.admin_payment_methods_delete'] = 'Zahlungsmethode löschen';
+$lang['OrderPlugin.permission.admin_payment_methods_edit'] = 'Zahlungsmethode bearbeiten';
+$lang['OrderPlugin.permission.admin_payment_methods_add'] = 'Zahlungsmethode hinzufügen';
+$lang['OrderPlugin.permission.admin_payment_methods'] = 'Zahlungsarten';
+$lang['OrderPlugin.permission.admin_payouts_decline'] = 'Auszahlung ablehnen';
+$lang['OrderPlugin.permission.admin_payouts_approve'] = 'Auszahlung genehmigen';
+$lang['OrderPlugin.permission.admin_payouts_edit'] = 'Auszahlung bearbeiten';
+$lang['OrderPlugin.permission.admin_payouts'] = 'Auszahlungen';
+$lang['OrderPlugin.permission.admin_affiliates_settings'] = 'Affiliate-Einstellungen';
+$lang['OrderPlugin.permission.admin_affiliates_deactivate'] = 'Affiliate deaktivieren';
+$lang['OrderPlugin.permission.admin_affiliates_activate'] = 'Partner aktivieren';
+$lang['OrderPlugin.permission.admin_affiliates_update'] = 'Update Tochtergesellschaft';
+$lang['OrderPlugin.permission.admin_affiliates_add'] = 'Partner hinzufügen';
+$lang['OrderPlugin.permission.admin_affiliates'] = 'Tochtergesellschaften';
+$lang['AdminCompanyPlugins.settings.event.domains.updatepricingafter'] = 'Dieses Ereignis wird ausgelöst, wenn der Preis für eine TLD aktualisiert wird.';
+$lang['AdminCompanyPlugins.settings.event.domains.updatedomainscompanysettingsafter'] = 'Dieses Ereignis wird ausgelöst, nachdem die Unternehmenseinstellungen im Domain Manager aktualisiert wurden.';
+$lang['AdminCompanyPlugins.settings.event.domains.disable'] = 'Dieses Ereignis wird ausgelöst, wenn eine TLD im Domain Manager deaktiviert wird.';
+$lang['AdminCompanyPlugins.settings.event.domains.enable'] = 'Dieses Ereignis wird ausgelöst, wenn eine TLD vom Domain Manager aus aktiviert wird.';
+$lang['AdminCompanyPlugins.settings.event.domains.delete'] = 'Dieses Ereignis wird ausgelöst, wenn eine TLD aus dem Domain Manager gelöscht wird.';
+$lang['OrderPlugin.card_client.orders'] = 'Bestellungen';
+$lang['OrderPlugin.event_getsearchoptions.orders'] = 'Suche bestellen';
+$lang['OrderPlugin.cron.process_abandoned_orders_desc'] = 'Senden Sie Erinnerungs-E-Mails, führen Sie Stornierungen durch und deaktivieren Sie Kunden für abgebrochene Bestellungen';
+$lang['OrderPlugin.cron.process_abandoned_orders_name'] = 'Abgebrochene Aufträge bearbeiten';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_desc'] = 'Prüft, ob ausstehende Affiliate-Empfehlungen ihre Laufzeit erreicht haben, und aktualisiert sie und die Auszahlungsbeträge, falls dies der Fall ist.';
+$lang['OrderPlugin.cron.mature_affiliate_referrals_name'] = 'Affiliate-Bestellungen';
+$lang['OrderPlugin.cron.affiliate_monthly_report_desc'] = 'Ein monatlicher E-Mail-Bericht wird am 1. des Monats für den vorangegangenen Monat verschickt.';
+$lang['OrderPlugin.cron.affiliate_monthly_report_name'] = 'Monatlicher Bericht der Partner';
+$lang['OrderPlugin.cron.accept_paid_orders_desc'] = 'Akzeptiert automatisch bezahlte ausstehende Bestellungen, wenn das Bestellformular dies zulässt.';
+$lang['OrderPlugin.cron.accept_paid_orders_name'] = 'Bezahlte ausstehende Aufträge akzeptieren';
+$lang['OrderPlugin.client_orders.name'] = 'Historie bestellen';
+$lang['OrderPlugin.action_staff_client.affiliates'] = 'Partner verwalten';
+$lang['OrderPlugin.client_affiliates.name'] = 'Tochtergesellschaften';
+$lang['OrderPlugin.client.name'] = 'Bestellung';
+$lang['OrderPlugin.admin_affiliates.payment_methods'] = 'Partner-Zahlungsmethoden';
+$lang['OrderPlugin.admin_affiliates.payouts'] = 'Affiliate-Auszahlungen';
+$lang['OrderPlugin.admin_affiliates.name'] = 'Tochtergesellschaften';
+$lang['OrderPlugin.admin_forms.name'] = 'Bestellformulare';
+$lang['OrderPlugin.admin_main.name'] = 'Bestellungen';
+$lang['OrderPlugin.name'] = 'Bestellsystem';
+
