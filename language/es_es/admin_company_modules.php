b590831e2a484cacb7619c095e470f324a454eb6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:52 -0800

    Updated es_es language files

---
+++ language/es_es/admin_company_modules.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Admin Company Modules
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyModules.!success.upgraded'] = 'El módulo se ha actualizado correctamente.';
+$lang['AdminCompanyModules.!success.installed'] = 'El módulo se ha instalado correctamente.';
+$lang['AdminCompanyModules.!success.uninstalled'] = 'El módulo se ha desinstalado correctamente.';
+$lang['AdminCompanyModules.!success.module_updated'] = 'El módulo se ha actualizado correctamente.';
+$lang['AdminCompanyModules.!success.group_added'] = 'El grupo se ha añadido correctamente.';
+$lang['AdminCompanyModules.!success.group_updated'] = 'El grupo se ha actualizado correctamente.';
+$lang['AdminCompanyModules.!success.group_deleted'] = 'El grupo se ha eliminado correctamente.';
+$lang['AdminCompanyModules.!success.row_added'] = 'El %1$s se ha añadido correctamente.';
+$lang['AdminCompanyModules.!success.row_updated'] = 'El %1$s se ha actualizado correctamente.';
+$lang['AdminCompanyModules.!success.row_deleted'] = 'El %1$s ha sido borrado con éxito.';
+$lang['AdminCompanyModules.!tab.installed'] = 'Instalado';
+$lang['AdminCompanyModules.!tab.available'] = 'Disponible';
+$lang['AdminCompanyModules.available.page_title'] = 'Configuración > Empresa > Módulos > Disponibles';
+$lang['AdminCompanyModules.available.boxtitle_module'] = 'Módulos';
+$lang['AdminCompanyModules.available.text_author'] = 'Autor:';
+$lang['AdminCompanyModules.available.text_author_url'] = 'Autor URL';
+$lang['AdminCompanyModules.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyModules.available.text_none'] = 'No hay módulos disponibles.';
+$lang['AdminCompanyModules.available.btn_install'] = 'Instale';
+$lang['AdminCompanyModules.installed.page_title'] = 'Configuración > Empresa > Módulos > Instalados';
+$lang['AdminCompanyModules.installed.boxtitle_installed'] = 'Módulos';
+$lang['AdminCompanyModules.installed.text_author'] = 'Autor:';
+$lang['AdminCompanyModules.installed.text_author_url'] = 'Autor URL';
+$lang['AdminCompanyModules.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyModules.installed.text_none'] = 'No hay módulos instalados.';
+$lang['AdminCompanyModules.installed.btn_uninstall'] = 'Desinstalar';
+$lang['AdminCompanyModules.installed.btn_manage'] = 'Gestione';
+$lang['AdminCompanyModules.installed.btn_upgrade'] = 'Actualizar';
+$lang['AdminCompanyModules.installed.confirm_uninstall'] = '¿Está seguro de que desea desinstalar este módulo?';
+$lang['AdminCompanyModules.manage.page_title'] = 'Configuración > Empresa > Módulos > Gestionar %1$s';
+$lang['AdminCompanyModules.manage.boxtitle_manage'] = 'Gestionar %1$s';
+$lang['AdminCompanyModules.addgroup.page_title'] = 'Configuración > Empresa > Módulos > Añadir %1$s %2$s Grupo';
+$lang['AdminCompanyModules.addgroup.boxtitle_add'] = 'Añadir %1$s %2$s Grupo';
+$lang['AdminCompanyModules.addgroup.btn_add'] = 'Añadir grupo';
+$lang['AdminCompanyModules.addgroup.assigned'] = 'Asignado %1$s';
+$lang['AdminCompanyModules.addgroup.available'] = 'Disponible %1$s';
+$lang['AdminCompanyModules.addgroup.force_limits'] = 'Forzar %1$s Límites';
+$lang['AdminCompanyModules.addgroup.tooltip_force_limits'] = 'Aplica límites duros para que el recuento de un %1$s nunca supere su límite.';
+$lang['AdminCompanyModules.addgroup.text_name'] = 'Nombre del grupo';
+$lang['AdminCompanyModules.addgroup.text_order'] = 'Añadir pedido';
+$lang['AdminCompanyModules.editgroup.page_title'] = 'Configuración > Empresa > Módulos > Editar %1$s %2$s Grupo';
+$lang['AdminCompanyModules.editgroup.boxtitle_edit'] = 'Editar %1$s %2$s Grupo';
+$lang['AdminCompanyModules.editgroup.btn_edit'] = 'Grupo de actualización';
+$lang['AdminCompanyModules.editgroup.assigned'] = 'Asignado %1$s';
+$lang['AdminCompanyModules.editgroup.available'] = 'Disponible %1$s';
+$lang['AdminCompanyModules.editgroup.force_limits'] = 'Forzar %1$s Límites';
+$lang['AdminCompanyModules.editgroup.tooltip_force_limits'] = 'Aplica límites duros para que el recuento de un %1$s nunca supere su límite.';
+$lang['AdminCompanyModules.editgroup.text_name'] = 'Nombre del grupo';
+$lang['AdminCompanyModules.editgroup.text_order'] = 'Añadir pedido';
+$lang['AdminCompanyModules.addrow.page_title'] = 'Configuración > Empresa > Módulos > Añadir %1$s %2$s';
+$lang['AdminCompanyModules.editrow.page_title'] = 'Configuración > Empresa > Módulos > Editar %1$s %2$s';
+
