8090c07c805234fc2dc7ae20df721992edbbc715
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:13 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/pricings.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Pricings
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pricings.getPeriods.onetime_plural'] = 'Jednorázově';
+$lang['Pricings.getPeriods.year_plural'] = 'Roky';
+$lang['Pricings.getPeriods.month_plural'] = 'Měsíce';
+$lang['Pricings.getPeriods.week_plural'] = 'Týdny';
+$lang['Pricings.getPeriods.day_plural'] = 'Dny';
+$lang['Pricings.getPeriods.onetime'] = 'Jednorázově';
+$lang['Pricings.getPeriods.year'] = 'Rok';
+$lang['Pricings.getPeriods.month'] = 'Měsíc';
+$lang['Pricings.getPeriods.week'] = 'Týden';
+$lang['Pricings.getPeriods.day'] = 'Den';
+$lang['Pricings.!error.currency.in_use'] = 'Měnu nelze aktualizovat v ceně, která se používá.';
+$lang['Pricings.!error.period.in_use'] = 'Období nelze aktualizovat u ceny, která se používá.';
+$lang['Pricings.!error.term.in_use'] = 'Termín nelze aktualizovat v ceně, která se používá.';
+$lang['Pricings.!error.currency.format'] = 'Kód měny musí mít 3 znaky.';
+$lang['Pricings.!error.cancel_fee.format'] = 'Storno poplatek musí být číselný.';
+$lang['Pricings.!error.setup_fee.format'] = 'Zřizovací poplatek musí mít číselnou podobu.';
+$lang['Pricings.!error.price_transfer.format'] = 'Cena za obnovení musí být číselná.';
+$lang['Pricings.!error.price_renews.valid'] = 'Cenu za obnovení nelze stanovit na jedno období.';
+$lang['Pricings.!error.price_renews.format'] = 'Cena za obnovení musí být číselná.';
+$lang['Pricings.!error.price.format'] = 'Cena musí být číslo.';
+$lang['Pricings.!error.period.format'] = 'Nesprávný typ období.';
+$lang['Pricings.!error.term.valid'] = 'Výraz musí být větší než 0.';
+$lang['Pricings.!error.term.length'] = 'Délka výrazu nesmí překročit 5 znaků.';
+$lang['Pricings.!error.term.format'] = 'Termín musí být číslo.';
+
