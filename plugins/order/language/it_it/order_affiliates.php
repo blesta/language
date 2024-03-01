3eb0dba1759ff5b30bb93da0d1e2b888bc08bd9c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:16 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/order_affiliates.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Order Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliates.getStatuses.inactive'] = 'Inattivo';
+$lang['OrderAffiliates.getStatuses.active'] = 'Attivo';
+$lang['OrderAffiliates.!error.date_updated.format'] = 'La data aggiornata è in un formato non valido.';
+$lang['OrderAffiliates.!error.date_added.format'] = 'La data aggiunta è in un formato non valido.';
+$lang['OrderAffiliates.!error.status.valid'] = 'Selezionare uno stato di affiliazione valido.';
+$lang['OrderAffiliates.!error.code.unique'] = 'Questo codice è già stato preso da un altro affiliato.';
+$lang['OrderAffiliates.!error.code.empty'] = 'Inserire un codice affiliato.';
+$lang['OrderAffiliates.!error.client_id.unique'] = 'L\'ID cliente è già associato a un altro affiliato.';
+$lang['OrderAffiliates.!error.client_id.exists'] = 'ID cliente non valido.';
+$lang['OrderAffiliates.!error.affiliate_id.exists'] = 'ID affiliato non valido.';
+
