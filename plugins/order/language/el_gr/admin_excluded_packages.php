59b704e386ce220570cb805b27c23db8a5d86d67
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:17 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/admin_excluded_packages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Admin Excluded Packages
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminExcludedPackages.!success.packages_updated'] = 'Τα εξαιρούμενα πακέτα ενημερώθηκαν με επιτυχία.';
+$lang['AdminExcludedPackages.index.boxtitle_excluded_packages'] = 'Εξαιρούμενα πακέτα';
+$lang['AdminExcludedPackages.index.tab_available_packages'] = 'Διαθέσιμα πακέτα';
+$lang['AdminExcludedPackages.index.tab_hidden_packages'] = 'Κρυμμένα πακέτα';
+$lang['AdminExcludedPackages.index.heading_available_packages'] = 'Διαθέσιμα πακέτα';
+$lang['AdminExcludedPackages.index.heading_hidden_packages'] = 'Κρυμμένα πακέτα';
+$lang['AdminExcludedPackages.index.field_updatesubmit'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminExcludedPackages.index.text_no_packages'] = 'Δεν υπάρχουν διαθέσιμα ενεργά πακέτα.';
+
