7e2d6484e7a514e3e836c28909f419f004f6c853
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:39 -0800

    Updated cs_cz language files

---
+++ plugins/download_manager/language/cs_cz/download_manager_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Download Manager Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerPlugin.admin_main'] = 'Správce stahování';
+$lang['DownloadManagerPlugin.client_main'] = 'Ke stažení na';
+$lang['DownloadManagerPlugin.description'] = 'Umožňuje nahrávání souborů a jejich zpřístupnění klientům ke stažení.';
+$lang['DownloadManagerPlugin.name'] = 'Správce stahování';
+
