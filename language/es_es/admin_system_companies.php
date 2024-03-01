a5c2bbe5714dc4378605ed4715444c2eefb96c92
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:02 -0800

    Updated es_es language files

---
+++ language/es_es/admin_system_companies.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Admin System Companies
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemCompanies.!success.company_added'] = '¡La empresa %1$s ha sido creada con éxito!';
+$lang['AdminSystemCompanies.!success.company_edited'] = '¡La empresa %1$s ha sido actualizada con éxito!';
+$lang['AdminSystemCompanies.!success.company_deleted'] = '¡La empresa %1$s ha sido eliminada con éxito!';
+$lang['AdminSystemCompanies.index.page_title'] = 'Configuración > Sistema > Empresas';
+$lang['AdminSystemCompanies.index.boxtitle_companies'] = 'Empresas';
+$lang['AdminSystemCompanies.index.categorylink_addcompany'] = 'Añadir empresa';
+$lang['AdminSystemCompanies.index.no_results'] = 'No hay empresas.';
+$lang['AdminSystemCompanies.index.text_name'] = 'Nombre';
+$lang['AdminSystemCompanies.index.text_hostname'] = 'Nombre de host';
+$lang['AdminSystemCompanies.index.text_address'] = 'Dirección';
+$lang['AdminSystemCompanies.index.text_phone'] = 'Teléfono';
+$lang['AdminSystemCompanies.index.text_options'] = 'Opciones';
+$lang['AdminSystemCompanies.index.option_edit'] = 'Editar';
+$lang['AdminSystemCompanies.index.option_delete'] = 'Borrar';
+$lang['AdminSystemCompanies.index.confirm_delete'] = '¿Borrar realmente esta empresa? Todo lo relacionado con esta empresa desaparecerá.';
+$lang['AdminSystemCompanies.add.page_title'] = 'Configuración > Sistema > Empresas > Añadir empresa';
+$lang['AdminSystemCompanies.add.boxtitle_addcompany'] = 'Añadir empresa';
+$lang['AdminSystemCompanies.add.field_company'] = 'Importar configuración de empresa desde';
+$lang['AdminSystemCompanies.add.field_name'] = 'Nombre';
+$lang['AdminSystemCompanies.add.field_hostname'] = 'Nombre de host';
+$lang['AdminSystemCompanies.add.field_address'] = 'Dirección';
+$lang['AdminSystemCompanies.add.field_phone'] = 'Teléfono';
+$lang['AdminSystemCompanies.add.field_fax'] = 'Fax';
+$lang['AddSystemCompanies.add.field_companysubmit'] = 'Crear empresa';
+$lang['AdminSystemCompanies.edit.page_title'] = 'Configuración > Sistema > Empresas > Editar empresa';
+$lang['AdminSystemCompanies.edit.boxtitle_editcompany'] = 'Editar empresa';
+$lang['AdminSystemCompanies.edit.field_name'] = 'Nombre';
+$lang['AdminSystemCompanies.edit.field_hostname'] = 'Nombre de host';
+$lang['AdminSystemCompanies.edit.field_address'] = 'Dirección';
+$lang['AdminSystemCompanies.edit.field_phone'] = 'Teléfono';
+$lang['AdminSystemCompanies.edit.field_fax'] = 'Fax';
+$lang['AddSystemCompanies.edit.field_companysubmit'] = 'Editar empresa';
+$lang['AdminSystemCompanies.!tooltip.hostname'] = 'El nombre de host debe contener sólo el nombre de dominio, el TLD y el número de puerto opcional. Por ejemplo, \'dominio.com\' o \'subdominio.dominio.com:8080\'.';
+
