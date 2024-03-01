1a8e8b4da1bb6cfb48edbaa18ed2e81c08ebfe76
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:48 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/apply_invoice_late_fees.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Apply Invoice Late Fees
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.apply_invoice_late_fees.attempt'] = 'Pokus o uplatnění poplatků z prodlení u faktur po splatnosti.';
+$lang['Automation.task.apply_invoice_late_fees.completed'] = 'Úloha uplatnit poplatky z prodlení byla dokončena.';
+$lang['Automation.task.apply_invoice_late_fees.invoice_line'] = 'Poplatek za zpoždění';
+$lang['Automation.task.apply_invoice_late_fees.late_fee_failed'] = 'Nelze uplatnit poplatek z prodlení na fakturu #%1$s pro klienta #%2$s.';
+$lang['Automation.task.apply_invoice_late_fees.late_fee_success'] = 'Úspěšně uplatněn poplatek z prodlení na fakturu #%1$s pro klienta #%2$s.';
+
