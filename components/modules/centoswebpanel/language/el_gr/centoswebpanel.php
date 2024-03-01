1d093f6fe5eb55234fbd8521110e98a4bb653728
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:42 -0800

    Updated el_gr language files

---
+++ components/modules/centoswebpanel/language/el_gr/centoswebpanel.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Centoswebpanel
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Centoswebpanel.!error.api'] = 'Προέκυψε ένα εσωτερικό σφάλμα ή ο διακομιστής δεν απάντησε στην αίτηση.';
+$lang['Centoswebpanel.!error.centoswebpanel_password.matches'] = 'Ο κωδικός πρόσβασης και ο κωδικός επιβεβαίωσης δεν ταιριάζουν.';
+$lang['Centoswebpanel.!error.centoswebpanel_password.valid'] = 'Ο κωδικός πρόσβασης πρέπει να έχει μήκος τουλάχιστον 8 χαρακτήρων.';
+$lang['Centoswebpanel.!error.centoswebpanel_username.length'] = 'Το όνομα χρήστη πρέπει να έχει μήκος μεταξύ 1 και 16 χαρακτήρων.';
+$lang['Centoswebpanel.!error.centoswebpanel_username.test'] = 'Το όνομα χρήστη δεν πρέπει να αρχίζει με "test".';
+$lang['Centoswebpanel.!error.centoswebpanel_username.format'] = 'Το όνομα χρήστη μπορεί να περιέχει μόνο γράμματα και αριθμούς και δεν πρέπει να αρχίζει με αριθμό.';
+$lang['Centoswebpanel.!error.centoswebpanel_domain.test'] = 'Το όνομα τομέα δεν μπορεί να αρχίζει με \'test\'.';
+$lang['Centoswebpanel.!error.centoswebpanel_domain.format'] = 'Παρακαλούμε εισάγετε ένα έγκυρο όνομα τομέα, π.χ. domain.com.';
+$lang['Centoswebpanel.!error.module_row.missing'] = 'Εμφανίστηκε ένα εσωτερικό σφάλμα. Η σειρά της μονάδας δεν είναι διαθέσιμη.';
+$lang['Centoswebpanel.!error.api.internal'] = 'Προέκυψε ένα εσωτερικό σφάλμα ή ο διακομιστής δεν απάντησε στην αίτηση.';
+$lang['Centoswebpanel.!error.meta[nproc].valid'] = 'Η Μέγιστη διαδικασία πρέπει να είναι αριθμητική τιμή.';
+$lang['Centoswebpanel.!error.meta[nofile].valid'] = 'Το Maximum Files πρέπει να είναι αριθμητική τιμή.';
+$lang['Centoswebpanel.!error.meta[inode].valid'] = 'Το Maximum Inodes πρέπει να είναι μια αριθμητική τιμή.';
+$lang['Centoswebpanel.!error.meta[package].empty'] = 'Απαιτείται ένα πακέτο CentOS WebPanel.';
+$lang['Centoswebpanel.!error.name_servers_count'] = 'Πρέπει να ορίσετε τουλάχιστον 2 διακομιστές ονομάτων.';
+$lang['Centoswebpanel.!error.name_servers_valid'] = 'Ένας ή περισσότεροι από τους καταχωρημένους διακομιστές ονομάτων είναι άκυροι.';
+$lang['Centoswebpanel.!error.account_limit_valid'] = 'Το Όριο λογαριασμού πρέπει να παραμείνει κενό (για απεριόριστους λογαριασμούς) ή να οριστεί σε κάποια ακέραια τιμή.';
+$lang['Centoswebpanel.!error.remote_api_key_valid_connection'] = 'Δεν κατέστη δυνατή η σύνδεση με το διακομιστή. Ελέγξτε αν το όνομα κεντρικού υπολογιστή και το κλειδί API είναι σωστά.';
+$lang['Centoswebpanel.!error.remote_api_key_valid'] = 'Το κλειδί API φαίνεται να είναι άκυρο.';
+$lang['Centoswebpanel.!error.port_valid'] = 'Πρέπει να εισαγάγετε μια θύρα API.';
+$lang['Centoswebpanel.!error.login_port_valid'] = 'Πρέπει να εισαγάγετε μια θύρα σύνδεσης.';
+$lang['Centoswebpanel.!error.user_name_valid'] = 'Το όνομα χρήστη φαίνεται να είναι άκυρο.';
+$lang['Centoswebpanel.!error.host_name_valid'] = 'Το όνομα κεντρικού υπολογιστή φαίνεται να είναι άκυρο.';
+$lang['Centoswebpanel.!error.server_name_valid'] = 'Πρέπει να εισαγάγετε μια ετικέτα διακομιστή.';
+$lang['Centoswebpanel.service_field.tooltip.password'] = 'Μπορείτε να αφήσετε τον κωδικό πρόσβασης κενό για να δημιουργηθεί αυτόματα.';
+$lang['Centoswebpanel.service_field.tooltip.username'] = 'Μπορείτε να αφήσετε το όνομα χρήστη κενό για να δημιουργηθεί αυτόματα.';
+$lang['Centoswebpanel.service_field.tooltip.username_edit'] = 'Αυτή η αλλαγή δεν θα επηρεάσει το CentOS Web Panel, αλλά θα αλλάξει μόνο την τοπική εγγραφή στο Blesta.';
+$lang['Centoswebpanel.service_field.tooltip.domain_edit'] = 'Αυτή η αλλαγή δεν θα επηρεάσει το CentOS Web Panel, αλλά θα αλλάξει μόνο την τοπική εγγραφή στο Blesta.';
+$lang['Centoswebpanel.service_info.option_login'] = 'Συνδεθείτε';
+$lang['Centoswebpanel.service_info.options'] = 'Επιλογές';
+$lang['Centoswebpanel.service_info.server'] = 'Διακομιστής';
+$lang['Centoswebpanel.service_info.password'] = 'Κωδικός πρόσβασης';
+$lang['Centoswebpanel.service_info.username'] = 'Όνομα χρήστη';
+$lang['Centoswebpanel.service_field.password'] = 'Κωδικός πρόσβασης';
+$lang['Centoswebpanel.service_field.username'] = 'Όνομα χρήστη';
+$lang['Centoswebpanel.service_field.domain'] = 'Τομέας';
+$lang['Centoswebpanel.package_fields.nproc'] = 'Μέγιστη διαδικασία';
+$lang['Centoswebpanel.package_fields.nofile'] = 'Μέγιστα αρχεία';
+$lang['Centoswebpanel.package_fields.inode'] = 'Μέγιστο Inodes';
+$lang['Centoswebpanel.package_fields.package'] = 'Αναγνωριστικό πακέτου';
+$lang['Centoswebpanel.row_meta.account_limit'] = 'Όριο λογαριασμού';
+$lang['Centoswebpanel.row_meta.use_ssl'] = 'Χρησιμοποιήστε SSL κατά τη σύνδεση στο API (συνιστάται)';
+$lang['Centoswebpanel.row_meta.api_key'] = 'Κλειδί API';
+$lang['Centoswebpanel.row_meta.default_port'] = '2304';
+$lang['Centoswebpanel.row_meta.port'] = 'Θύρα API';
+$lang['Centoswebpanel.row_meta.default_login_port'] = '2031';
+$lang['Centoswebpanel.row_meta.login_port'] = 'Θύρα σύνδεσης';
+$lang['Centoswebpanel.row_meta.host_name'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Centoswebpanel.row_meta.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Centoswebpanel.edit_row.add_btn'] = 'Επεξεργασία διακομιστή';
+$lang['Centoswebpanel.edit_row.remove_name_server'] = 'Αφαιρέστε το';
+$lang['Centoswebpanel.edit_row.name_server'] = 'Διακομιστής ονομάτων %1$s';
+$lang['Centoswebpanel.edit_row.name_server_host_col'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Centoswebpanel.edit_row.name_server_col'] = 'Διακομιστής ονομάτων';
+$lang['Centoswebpanel.edit_row.name_server_btn'] = 'Προσθήκη πρόσθετου διακομιστή ονομάτων';
+$lang['Centoswebpanel.edit_row.notes_title'] = 'Σημειώσεις';
+$lang['Centoswebpanel.edit_row.name_servers_title'] = 'Εξυπηρετητές ονομάτων';
+$lang['Centoswebpanel.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Centoswebpanel.edit_row.box_title'] = 'Επεξεργασία διακομιστή CentOS WebPanel';
+$lang['Centoswebpanel.add_row.add_btn'] = 'Προσθήκη διακομιστή';
+$lang['Centoswebpanel.add_row.remove_name_server'] = 'Αφαιρέστε το';
+$lang['Centoswebpanel.add_row.name_server'] = 'Διακομιστής ονομάτων %1$s';
+$lang['Centoswebpanel.add_row.name_server_host_col'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Centoswebpanel.add_row.name_server_col'] = 'Διακομιστής ονομάτων';
+$lang['Centoswebpanel.add_row.name_server_btn'] = 'Προσθήκη πρόσθετου διακομιστή ονομάτων';
+$lang['Centoswebpanel.add_row.notes_title'] = 'Σημειώσεις';
+$lang['Centoswebpanel.add_row.name_servers_title'] = 'Εξυπηρετητές ονομάτων';
+$lang['Centoswebpanel.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Centoswebpanel.add_row.box_title'] = 'Προσθήκη διακομιστή CentOS WebPanel';
+$lang['Centoswebpanel.order_options.roundrobin'] = 'Ομοιόμορφη κατανομή μεταξύ των διακομιστών';
+$lang['Centoswebpanel.order_options.first'] = 'Πρώτος μη πλήρης διακομιστής';
+$lang['Centoswebpanel.manage.module_groups_no_results'] = 'Δεν υπάρχουν ομάδες διακομιστών.';
+$lang['Centoswebpanel.manage.module_rows_no_results'] = 'Δεν υπάρχουν διακομιστές.';
+$lang['Centoswebpanel.manage.module_groups.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την ομάδα διακομιστών;';
+$lang['Centoswebpanel.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον διακομιστή;';
+$lang['Centoswebpanel.manage.module_groups.delete'] = 'Διαγραφή';
+$lang['Centoswebpanel.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['Centoswebpanel.manage.module_groups.edit'] = 'Επεξεργασία';
+$lang['Centoswebpanel.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['Centoswebpanel.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Centoswebpanel.manage.module_groups_heading.options'] = 'Επιλογές';
+$lang['Centoswebpanel.manage.module_groups_heading.servers'] = 'Αριθμός διακομιστών';
+$lang['Centoswebpanel.manage.module_groups_heading.name'] = 'Όνομα ομάδας';
+$lang['Centoswebpanel.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['Centoswebpanel.manage.module_rows_heading.accounts'] = 'Λογαριασμοί';
+$lang['Centoswebpanel.manage.module_rows_heading.hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Centoswebpanel.manage.module_rows_heading.name'] = 'Ετικέτα διακομιστή';
+$lang['Centoswebpanel.manage.module_groups_title'] = 'Ομάδες διακομιστών';
+$lang['Centoswebpanel.manage.module_rows_title'] = 'Servers';
+$lang['Centoswebpanel.add_module_group'] = 'Προσθήκη ομάδας διακομιστών';
+$lang['Centoswebpanel.add_module_row'] = 'Προσθήκη διακομιστή';
+$lang['Centoswebpanel.module_group'] = 'Ομάδα διακομιστών';
+$lang['Centoswebpanel.module_row_plural'] = 'Servers';
+$lang['Centoswebpanel.module_row'] = 'Διακομιστής';
+$lang['Centoswebpanel.description'] = 'Δωρεάν πίνακας ελέγχου Web Hosting σχεδιασμένος για γρήγορη και εύκολη διαχείριση των διακομιστών (Dedicated & VPS)';
+$lang['Centoswebpanel.name'] = 'CentOS WebPanel';
+
