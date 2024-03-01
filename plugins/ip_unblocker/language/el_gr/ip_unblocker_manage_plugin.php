9d39968afdda2838902423ce6b452caa4b743eb9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:07 -0800

    Updated el_gr language files

---
+++ plugins/ip_unblocker/language/el_gr/ip_unblocker_manage_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Ip Unblocker Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['IpUnblockerManagePlugin.!success.settings_updated'] = 'Οι ρυθμίσεις ενημερώθηκαν με επιτυχία!';
+$lang['IpUnblockerManagePlugin.index.boxtitle_manage'] = 'Απομπλοκαριστής IP';
+$lang['IpUnblockerManagePlugin.index.client_set_ip'] = 'Επιτρέψτε την παροχή της διεύθυνσης IP από τον πελάτη';
+$lang['IpUnblockerManagePlugin.index.submit_settings'] = 'Αποθήκευση';
+
