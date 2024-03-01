cc48e823d26140c903f9ee010d801afc422b70cf
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:33 -0800

    Updated it_it language files

---
+++ plugins/client_documents/language/it_it/client_main.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.index.option_download'] = 'Scaricare';
+$lang['ClientMain.index.heading_options'] = 'Opzioni';
+$lang['ClientMain.index.heading_date_added'] = 'Data aggiunta';
+$lang['ClientMain.index.heading_description'] = 'Descrizione';
+$lang['ClientMain.index.heading_name'] = 'Nome';
+$lang['ClientMain.index.no_results'] = 'Non ci sono documenti disponibili per il download.';
+$lang['ClientMain.index.boxtitle_documents'] = 'Documenti';
+$lang['ClientMain.index.page_title'] = 'Documenti';
+
