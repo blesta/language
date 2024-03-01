80cda6c4da0d3f69b8c71fcef14eece509ebc53d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:04 -0800

    Updated el_gr language files

---
+++ plugins/thesslstore/language/el_gr/admin_company_emails.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Thesslstore.expiration_reminder_desc'] = 'Ειδοποίηση που αποστέλλεται πριν από 30 ημέρες από την ημερομηνία λήξης του πιστοποιητικού.';
+$lang['AdminCompanyEmails.templates.Thesslstore.expiration_reminder_name'] = 'Υπενθύμιση λήξης πιστοποιητικού SSL';
+
