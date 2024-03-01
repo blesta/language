aed75ed4554dee4d20a49c8da35233a005add769
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:39 -0800

    Updated de_de language files

---
+++ language/de_de/admin_system_staff.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Admin System Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemStaff.edit.field_recovery_email'] = 'Wiederherstellungs-E-Mail (optional)';
+$lang['AdminSystemStaff.add.field_recovery_email'] = 'Wiederherstellungs-E-Mail (optional)';
+$lang['AdminSystemStaff.editgroup.dialog_confirm_edit_assigned'] = 'Sie sind derzeit dieser Personalgruppe zugewiesen. Alle Änderungen, die Sie vornehmen, werden sofort wirksam und Sie können möglicherweise nicht mehr auf diesen Bereich zugreifen. Sind Sie sicher, dass Sie diese Änderungen vornehmen möchten?';
+$lang['AdminSystemStaff.editgroup.heading_subscription_email_notices'] = 'E-Mail-Abonnement-Benachrichtigungen';
+$lang['AdminSystemStaff.editgroup.heading_email_notices'] = 'BCC-Benachrichtigungen per E-Mail';
+$lang['AdminSystemStaff.editgroup.heading_permissions'] = 'Zugriffskontrollliste';
+$lang['AdminSystemStaff.editgroup.heading_general'] = 'Allgemein';
+$lang['AdminSystemStaff.editgroup.field_groupsubmit'] = 'Gruppe bearbeiten';
+$lang['AdminSystemStaff.editgroup.field_session_lock'] = 'Abmelden bei Änderung der IP-Adresse';
+$lang['AdminSystemStaff.editgroup.field_company'] = 'Unternehmen';
+$lang['AdminSystemStaff.editgroup.field_name'] = 'Name';
+$lang['AdminSystemStaff.editgroup.boxtitle_editgroup'] = 'Gruppe bearbeiten';
+$lang['AdminSystemStaff.editgroup.page_title'] = 'Einstellungen > System > Personal > Gruppe bearbeiten';
+$lang['AdminSystemStaff.addgroup.text_uncheck_all'] = 'Alle abwählen';
+$lang['AdminSystemStaff.addgroup.text_check_all'] = 'Alle prüfen';
+$lang['AdminSystemStaff.addgroup.heading_subscription_email_notices'] = 'E-Mail-Abonnement-Benachrichtigungen';
+$lang['AdminSystemStaff.addgroup.heading_email_notices'] = 'BCC-Benachrichtigungen per E-Mail';
+$lang['AdminSystemStaff.addgroup.heading_permissions'] = 'Zugriffskontrollliste';
+$lang['AdminSystemStaff.addgroup.heading_general'] = 'Allgemein';
+$lang['AdminSystemStaff.addgroup.field_groupsubmit'] = 'Gruppe erstellen';
+$lang['AdminSystemStaff.addgroup.field_session_lock'] = 'Abmelden bei Änderung der IP-Adresse';
+$lang['AdminSystemStaff.addgroup.field_company'] = 'Unternehmen';
+$lang['AdminSystemStaff.addgroup.field_name'] = 'Name';
+$lang['AdminSystemStaff.addgroup.boxtitle_addgroup'] = 'Gruppe erstellen';
+$lang['AdminSystemStaff.addgroup.page_title'] = 'Einstellungen > System > Personal > Gruppe erstellen';
+$lang['AdminSystemStaff.!groups.text_group_end'] = ')';
+$lang['AdminSystemStaff.!groups.text_group_start'] = '(';
+$lang['AdminSystemStaff.!groups.text_separator'] = '/';
+$lang['AdminSystemStaff.groups.modal_delete'] = 'Sind Sie sicher, dass Sie diese Personalgruppe löschen wollen?';
+$lang['AdminSystemStaff.groups.option_edit'] = 'bearbeiten';
+$lang['AdminSystemStaff.groups.no_results'] = 'Es gibt keine Personalgruppen.';
+$lang['AdminSystemStaff.groups.heading_options'] = 'Optionen';
+$lang['AdminSystemStaff.groups.heading_staff'] = 'Anzahl der Mitarbeiter';
+$lang['AdminSystemStaff.groups.heading_company_name'] = 'Name des Unternehmens';
+$lang['AdminSystemStaff.groups.heading_name'] = 'Name';
+$lang['AdminSystemStaff.groups.categorylink_addgroup'] = 'Gruppe erstellen';
+$lang['AdminSystemStaff.groups.boxtitle_groups'] = 'Personalgruppen';
+$lang['AdminSystemStaff.groups.page_title'] = 'Einstellungen > System > Personal > Personalgruppen';
+$lang['AdminSystemStaff.edit.field_editsubmit'] = 'Personal bearbeiten';
+$lang['AdminSystemStaff.edit.text_availablegroups'] = 'Verfügbare Gruppen';
+$lang['AdminSystemStaff.edit.text_membergroups'] = 'Mitgliedsgruppen';
+$lang['AdminSystemStaff.edit.field_twofactorpin'] = 'Zwei-Faktor-Pin';
+$lang['AdminSystemStaff.edit.field_twofactorkey'] = 'Zwei-Faktor-Schlüssel';
+$lang['AdminSystemStaff.edit.field_twofactormode'] = 'Zwei-Faktor-Authentifizierung';
+$lang['AdminSystemStaff.edit.field_confirmpass'] = 'Bestätigen Sie Ihr Passwort';
+$lang['AdminSystemStaff.edit.field_password'] = 'Neues Passwort';
+$lang['AdminSystemStaff.edit.field_numbermobile'] = 'Handynummer';
+$lang['AdminSystemStaff.edit.field_email'] = 'E-Mail';
+$lang['AdminSystemStaff.edit.field_username'] = 'Benutzername';
+$lang['AdminSystemStaff.edit.field_lastname'] = 'Nachname';
+$lang['AdminSystemStaff.edit.field_firstname'] = 'Vorname';
+$lang['AdminSystemStaff.edit.heading_groups'] = 'Gruppen';
+$lang['AdminSystemStaff.edit.heading_authentication'] = 'Authentifizierung';
+$lang['AdminSystemStaff.edit.heading_contact'] = 'Kontaktinformationen';
+$lang['AdminSystemStaff.edit.boxtitle_editstaff'] = 'Personal bearbeiten';
+$lang['AdminSystemStaff.edit.page_title'] = 'Einstellungen > System > Personal > Personal bearbeiten';
+$lang['AdminSystemStaff.add.field_addsubmit'] = 'Personal erstellen';
+$lang['AdminSystemStaff.add.text_availablegroups'] = 'Verfügbare Gruppen';
+$lang['AdminSystemStaff.add.text_membergroups'] = 'Mitgliedsgruppen';
+$lang['AdminSystemStaff.add.field_twofactorpin'] = 'Zwei-Faktor-Pin';
+$lang['AdminSystemStaff.add.field_twofactorkey'] = 'Zwei-Faktor-Schlüssel';
+$lang['AdminSystemStaff.add.field_twofactormode'] = 'Zwei-Faktor-Authentifizierung';
+$lang['AdminSystemStaff.add.field_confirmpass'] = 'Bestätigen Sie Ihr Passwort';
+$lang['AdminSystemStaff.add.field_password'] = 'Passwort';
+$lang['AdminSystemStaff.add.field_username_email'] = 'Verwenden Sie die E-Mail-Adresse als Benutzernamen';
+$lang['AdminSystemStaff.add.field_username_username'] = 'Einen Benutzernamen eingeben';
+$lang['AdminSystemStaff.add.field_username'] = 'Benutzername';
+$lang['AdminSystemStaff.add.field_numbermobile'] = 'Handynummer';
+$lang['AdminSystemStaff.add.field_email'] = 'E-Mail';
+$lang['AdminSystemStaff.add.field_lastname'] = 'Nachname';
+$lang['AdminSystemStaff.add.field_firstname'] = 'Vorname';
+$lang['AdminSystemStaff.add.heading_groups'] = 'Gruppen';
+$lang['AdminSystemStaff.add.heading_authentication'] = 'Authentifizierung';
+$lang['AdminSystemStaff.add.heading_contact'] = 'Kontaktinformationen';
+$lang['AdminSystemStaff.add.boxtitle_addstaff'] = 'Personal hinzufügen';
+$lang['AdminSystemStaff.add.page_title'] = 'Einstellungen > System > Personal > Personal hinzufügen';
+$lang['AdminSystemStaff.groups.option_delete'] = 'Löschen';
+$lang['AdminSystemStaff.manage.option_reactivate'] = 'Reaktivieren Sie';
+$lang['AdminSystemStaff.manage.option_deactivate'] = 'Deaktivieren Sie';
+$lang['AdminSystemStaff.manage.option_edit'] = 'bearbeiten';
+$lang['AdminSystemStaff.manage.confirm_reactivate'] = 'Diesen Mitarbeiter wirklich reaktivieren?';
+$lang['AdminSystemStaff.manage.confirm_deactivate'] = 'Dieses Personalmitglied wirklich deaktivieren?';
+$lang['AdminSystemStaff.manage.no_results'] = 'Es gibt keine Mitarbeiter mit diesem Status.';
+$lang['AdminSystemStaff.manage.heading_options'] = 'Optionen';
+$lang['AdminSystemStaff.manage.heading_email'] = 'E-Mail';
+$lang['AdminSystemStaff.manage.heading_name'] = 'Name';
+$lang['AdminSystemStaff.manage.categorylink_addstaff'] = 'Personal hinzufügen';
+$lang['AdminSystemStaff.manage.category_inactive'] = 'Inaktiv';
+$lang['AdminSystemStaff.manage.category_active'] = 'Aktiv';
+$lang['AdminSystemStaff.manage.boxtitle_manage'] = 'Personal verwalten';
+$lang['AdminSystemStaff.manage.page_title'] = 'Einstellungen > System > Personal > Personal verwalten';
+$lang['AdminSystemStaff.!success.group_deleted'] = 'Die Personalgruppe "%1$s" wurde erfolgreich gelöscht!';
+$lang['AdminSystemStaff.!success.group_updated'] = 'Die Personalgruppe "%1$s" wurde erfolgreich aktualisiert!';
+$lang['AdminSystemStaff.!success.group_added'] = 'Die Personalgruppe "%1$s" wurde erfolgreich erstellt!';
+$lang['AdminSystemStaff.!success.staff_updated'] = 'Dieses Personalmitglied wurde erfolgreich aktualisiert!';
+$lang['AdminSystemStaff.!success.staff_added'] = 'Dieser Mitarbeiter wurde erfolgreich hinzugefügt!';
+
