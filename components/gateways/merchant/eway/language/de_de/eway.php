04b832ae97757e30a6083c2ce1503f3d29b7ce24
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:59 -0800

    Updated de_de language files

---
+++ components/gateways/merchant/eway/language/de_de/eway.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Eway
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Eway.tooltip_test_mode'] = 'Im Testmodus wird ein eWAY-Testkonto mit einer Test-Kreditkartennummer verwendet, unabhängig davon, was Sie eingeben. Weitere Informationen zum Testmodus in eWAY finden Sie im Handbuch.';
+$lang['Eway.tooltip_developer_mode'] = 'Wenn Sie diese Option aktivieren, werden Transaktionen in der eWAY-Sandbox-Umgebung gebucht. Sie müssen über ein Sandbox-Testkonto verfügen, um diese Umgebung nutzen zu können.';
+$lang['Eway.tooltip_refund_password'] = 'Geben Sie das Erstattungspasswort ein, das Sie in Ihrem eWAY-Konto festgelegt haben. Dies ist nicht Ihr eWAY-Kontopasswort. Ein Erstattungspasswort muss festgelegt werden, um Erstattungen über eWAY zu bearbeiten.';
+$lang['Eway.test_mode'] = 'Test-Modus';
+$lang['Eway.developer_mode'] = 'Entwickler-Modus';
+$lang['Eway.refund_password'] = 'Erstattungs-Passwort';
+$lang['Eway.customer_id'] = 'Kunden-ID';
+$lang['Eway.description'] = 'eWAY hat einen Anteil von 25 % am australischen Online-Markt und ist in 5 Ländern tätig. Verarbeitet jedes Jahr weltweit Zahlungen in Milliardenhöhe für Zehntausende von Unternehmen';
+$lang['Eway.name'] = 'eWAY';
+$lang['Eway.!error.invalid_xml'] = 'Das Gateway antwortete mit ungültigem XML.';
+$lang['Eway.!error.libxml_required'] = 'Die Erweiterungen libxml und simplexml sind für dieses Gateway erforderlich.';
+$lang['Eway.!error.refund_password.empty'] = 'Für die Bearbeitung von Erstattungen über eWAY muss ein Erstattungspasswort festgelegt werden.';
+$lang['Eway.!error.test_mode.valid'] = 'Der Testmodus muss auf "true" gesetzt werden, falls vorhanden.';
+$lang['Eway.!error.developer_mode.valid'] = 'Der Entwicklermodus muss auf "true" gesetzt werden, falls vorhanden.';
+$lang['Eway.!error.customer_id.length'] = 'Die Länge der Kunden-ID darf 8 Zeichen nicht überschreiten.';
+$lang['Eway.!error.customer_id.empty'] = 'Bitte geben Sie Ihre eWAY-Kunden-ID ein.';
+
