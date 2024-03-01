ece5b4039dc52e75a22252d9055edb3f14b94dd1
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:19 -0800

    Updated it_it language files

---
+++ language/it_it/report_manager.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Report Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReportManager.reportrequiredtypes.custom'] = 'Regex personalizzato';
+$lang['ReportManager.reportrequiredtypes.yes'] = 'Sì';
+$lang['ReportManager.reportrequiredtypes.no'] = 'No';
+$lang['ReportManager.reportfieldtypes.date'] = 'Data';
+$lang['ReportManager.reportfieldtypes.select'] = 'Selezionare';
+$lang['ReportManager.reportfieldtypes.text'] = 'Testo';
+$lang['ReportManager.getformats.json'] = 'JSON';
+$lang['ReportManager.getformats.csv'] = 'CSV';
+$lang['ReportManager.!error.date_created.format'] = 'La data di creazione non è un formato valido.';
+$lang['ReportManager.!error.query.valid'] = 'È consentita una sola query e deve essere una query SELECT.';
+$lang['ReportManager.!error.query.empty'] = 'Il rapporto deve avere una query.';
+$lang['ReportManager.!error.name.valid'] = 'Il rapporto deve avere un nome.';
+$lang['ReportManager.!error.temp_dir.writable'] = 'La directory temp non è scrivibile o non è stato possibile scrivervi il rapporto.';
+$lang['ReportManager.!error.format.valid'] = 'Selezionare un formato di rapporto valido.';
+$lang['ReportManager.!error.type.valid'] = 'Selezionare un tipo di rapporto valido.';
+
