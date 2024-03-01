e47e0142f58fe72d8cd6390f89734ddffcf65165
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:08 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/kassacompleet/language/de_de/kassacompleet.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Kassacompleet
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Kassacompleet.buildprocess.submit'] = 'Bezahlen mit Kassa Compleet';
+$lang['Kassacompleet.tooltip.api_key'] = 'Um den Testmodus zu verwenden, geben Sie einfach einen API-Schlüssel von einem Testkonto ein';
+$lang['Kassacompleet.api_key'] = 'API-Schlüssel';
+$lang['Kassacompleet.description'] = 'Kassa Compleet Integration für die Zahlungsart Kreditkarte.';
+$lang['Kassacompleet.name'] = 'Kredit-/Debitkarte (Kassa Compleet)';
+$lang['Kassacompleet.!error.api_key.valid'] = 'API-Schlüssel ist ungültig';
+$lang['Kassacompleet.!error.api_key.empty'] = 'Bitte geben Sie einen API-Schlüssel ein';
+
