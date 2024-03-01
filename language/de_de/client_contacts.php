0af80a759d481c62996d190cb30c2fc2017dd948
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:44 -0800

    Updated de_de language files

---
+++ language/de_de/client_contacts.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Client Contacts
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientContacts.tooltip.client_quotations'] = 'Zeigen Sie Angebote auf dem Dashboard an, und erlauben Sie, dass sie genehmigt oder abgelehnt werden.';
+$lang['ClientContacts.edit.field_recovery_email'] = 'Wiederherstellungs-E-Mail (optional)';
+$lang['ClientContacts.add.field_recovery_email'] = 'Wiederherstellungs-E-Mail (optional)';
+$lang['ClientContacts.navigation.nav_return'] = 'Zurück zum Dashboard';
+$lang['ClientContacts.navigation.nav_contacts_add'] = 'Kontakt hinzufügen';
+$lang['ClientContacts.navigation.nav_contacts'] = 'Kontakte';
+$lang['ClientContacts.phone_numbers.text_remove'] = 'entfernen';
+$lang['ClientContacts.phone_numbers.field_phoneoptions'] = 'Optionen';
+$lang['ClientContacts.phone_numbers.field_phonenumber'] = 'Nummer';
+$lang['ClientContacts.phone_numbers.field_phonelocation'] = 'Standort';
+$lang['ClientContacts.phone_numbers.field_phonetype'] = 'Typ';
+$lang['ClientContacts.phone_numbers.categorylink_number'] = 'Zusätzliches hinzufügen';
+$lang['ClientContacts.phone_numbers.heading_phone'] = 'Telefonnummern';
+$lang['ClientContacts.contact_info.field_email'] = 'E-Mail';
+$lang['ClientContacts.contact_info.field_zip'] = 'Postleitzahl/Postleitzahl';
+$lang['ClientContacts.contact_info.field_state'] = 'Staat';
+$lang['ClientContacts.contact_info.field_country'] = 'Land';
+$lang['ClientContacts.contact_info.field_city'] = 'Stadt';
+$lang['ClientContacts.contact_info.field_address2'] = 'Adresse 2';
+$lang['ClientContacts.contact_info.field_address1'] = 'Adresse 1';
+$lang['ClientContacts.contact_info.field_title'] = 'Titel';
+$lang['ClientContacts.contact_info.field_company'] = 'Unternehmen';
+$lang['ClientContacts.contact_info.field_last_name'] = 'Nachname';
+$lang['ClientContacts.contact_info.field_first_name'] = 'Vorname';
+$lang['ClientContacts.contact_info.field_contact_id'] = 'Kontaktinformationen kopieren von';
+$lang['ClientContacts.contact_info.text_select_contact'] = 'Sie können einen bestehenden Kontakt auswählen, um dieses Formular vorauszufüllen.';
+$lang['ClientContacts.contact_info.heading_contact'] = 'Kontaktinformationen';
+$lang['ClientContacts.setcontactview.text_none'] = 'Keine';
+$lang['ClientContacts.tooltip._credits'] = 'Erlauben Sie die Einsichtnahme in die Kontoguthaben.';
+$lang['ClientContacts.tooltip._invoice_delivery'] = 'Ermöglicht die Anzeige und Änderung der Versandart der Rechnung.';
+$lang['ClientContacts.tooltip.client_emails'] = 'Zeigt eine Liste des E-Mail-Verlaufs an.';
+$lang['ClientContacts.tooltip.client_accounts'] = 'Erlauben Sie die Verwaltung von Zahlungskonten.';
+$lang['ClientContacts.tooltip.client_contacts'] = 'Erlauben Sie die Verwaltung von Kontakten.';
+$lang['ClientContacts.tooltip.client_transactions'] = 'Transaktionen auf dem Dashboard anzeigen.';
+$lang['ClientContacts.tooltip.client_services'] = 'Dienste auf dem Dashboard anzeigen und ihre Verwaltung ermöglichen.';
+$lang['ClientContacts.tooltip.client_invoices'] = 'Anzeige von Rechnungen auf dem Dashboard sowie von Zahlungserinnerungswarnungen.';
+$lang['ClientContacts.edit.heading_permissions'] = 'Berechtigungen';
+$lang['ClientContacts.edit.field_confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['ClientContacts.edit.field_new_password'] = 'Neues Passwort';
+$lang['ClientContacts.edit.field_username'] = 'Benutzername';
+$lang['ClientContacts.edit.field_enable_login'] = 'Anmeldung aktivieren';
+$lang['ClientContacts.edit.heading_authentication'] = 'Authentifizierung';
+$lang['ClientContacts.edit.field_editsubmit'] = 'Update Kontakt';
+$lang['ClientContacts.edit.field_contact_type'] = 'Kontakt Typ';
+$lang['ClientContacts.edit.heading_settings'] = 'Zusätzliche Einstellungen';
+$lang['ClientContacts.edit.boxtitle_edit'] = 'Kontakt bearbeiten';
+$lang['ClientContacts.edit.page_title'] = 'Kunde #%1$s Kontakt bearbeiten';
+$lang['ClientContacts.edit.heading_contact'] = 'Kontaktinformationen';
+$lang['ClientContacts.add.heading_permissions'] = 'Berechtigungen';
+$lang['ClientContacts.add.field_confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['ClientContacts.add.field_new_password'] = 'Neues Passwort';
+$lang['ClientContacts.add.field_username'] = 'Benutzername';
+$lang['ClientContacts.add.field_enable_login'] = 'Anmeldung aktivieren';
+$lang['ClientContacts.add.heading_authentication'] = 'Authentifizierung';
+$lang['ClientContacts.add.field_addsubmit'] = 'Kontakt erstellen';
+$lang['ClientContacts.add.field_contact_type'] = 'Kontakt Typ';
+$lang['ClientContacts.add.heading_settings'] = 'Zusätzliche Einstellungen';
+$lang['ClientContacts.add.boxtitle_create'] = 'Kontakt hinzufügen';
+$lang['ClientContacts.add.page_title'] = 'Kunde #%1$s Kontakt hinzufügen';
+$lang['ClientContacts.add.heading_contact'] = 'Kontaktinformationen';
+$lang['ClientContacts.index.no_results'] = 'Sie haben keine Kontakte. Um Ihren ersten Kontakt hinzuzufügen, klicken Sie oben auf die Schaltfläche Kontakt hinzufügen.';
+$lang['ClientContacts.index.confirm_delete'] = 'Sind Sie sicher, dass Sie diesen Kontakt löschen möchten?';
+$lang['ClientContacts.index.option_delete'] = 'Löschen';
+$lang['ClientContacts.index.option_edit'] = 'bearbeiten';
+$lang['ClientContacts.index.heading_options'] = 'Optionen';
+$lang['ClientContacts.index.heading_type'] = 'Typ';
+$lang['ClientContacts.index.heading_email'] = 'E-Mail';
+$lang['ClientContacts.index.heading_name'] = 'Name';
+$lang['ClientContacts.index.boxtitle_contacts'] = 'Kontakte';
+$lang['ClientContacts.index.create_contact'] = 'Kontakt hinzufügen';
+$lang['ClientContacts.index.page_title'] = 'Kunde #%1$s Kontakte';
+$lang['ClientContacts.!info.unverified_email_button'] = 'Verifizierungs-E-Mail erneut senden';
+$lang['ClientContacts.!info.unverified_email'] = 'Die mit diesem Kontakt verknüpfte E-Mail-Adresse wurde nicht überprüft. Es wurde eine Verifizierungs-E-Mail an %1$s gesendet.';
+$lang['ClientContacts.!info.no_contacts'] = 'Sie haben noch keine Kontakte, fügen Sie unten Ihren ersten Kontakt hinzu.';
+$lang['ClientContacts.!success.contact_added'] = 'Der Kontakt wurde erfolgreich erstellt!';
+$lang['ClientContacts.!success.contact_updated'] = 'Der Kontakt wurde erfolgreich aktualisiert!';
+$lang['ClientContacts.!success.contact_deleted'] = 'Der Kontakt %1$s %2$s wurde erfolgreich gelöscht!';
+
