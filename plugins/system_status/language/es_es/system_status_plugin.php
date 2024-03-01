4d6d800dd552a0352f099ebe2e18938d7a3eb8b4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:19 -0800

    Updated es_es language files

---
+++ plugins/system_status/language/es_es/system_status_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * System Status Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SystemStatusPlugin.description'] = 'Muestra el estado del sistema en un widget en el panel de control del administrador.';
+$lang['SystemStatusPlugin.name'] = 'Estado del sistema';
+
