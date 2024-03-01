e68102158b10095121ba4c0c83f5df654194ccae
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:19 -0800

    Updated it_it language files

---
+++ components/gateways/nonmerchant/pagseguro/language/it_it/pagseguro.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Pagseguro
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pagseguro.!error.notification.invalid'] = 'Parametri di notifica non validi.';
+$lang['Pagseguro.!error.notification.type'] = 'Tipo di notifica sconosciuto:';
+$lang['Pagseguro.!error.token.empty'] = 'Inserire il token di sicurezza.';
+$lang['Pagseguro.!error.email.format'] = 'Inserire un indirizzo e-mail valido.';
+$lang['Pagseguro.!error.dom_required'] = 'L\'estensione dom è necessaria per questo gateway.';
+$lang['Pagseguro.!error.libxml_required'] = 'Le estensioni libxml e simplexml sono necessarie per questo gateway.';
+$lang['Pagseguro.buildprocess.submit'] = 'Pagare con PagSeguro';
+$lang['Pagseguro.token_note'] = 'Inserite il token di sicurezza creato nel vostro account PagSeguro.';
+$lang['Pagseguro.token'] = 'Gettone';
+$lang['Pagseguro.email_note'] = 'Inserite l\'indirizzo e-mail del vostro account PagSeguro.';
+$lang['Pagseguro.email_id'] = 'Email';
+$lang['Pagseguro.description'] = 'PagSeguro è un servizio di e-commerce brasiliano basato su pagamenti online o mobili per operazioni commerciali. Nel 2015 è stato riconosciuto come "Miglior metodo di pagamento" dal Congresso Afiliados Brasil.';
+$lang['Pagseguro.name'] = 'PagSeguro';
+
