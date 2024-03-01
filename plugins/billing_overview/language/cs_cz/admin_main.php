7bc11a7f0d33561a00d3a54a70a34b193fef9b86
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:24 -0800

    Updated cs_cz language files

---
+++ plugins/billing_overview/language/cs_cz/admin_main.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.graph_line_name.credits'] = 'Kredity';
+$lang['AdminMain.graph_line_name.other'] = 'Další';
+$lang['AdminMain.graph_line_name.ach'] = 'ACH';
+$lang['AdminMain.graph_line_name.credit'] = 'Kreditní karta';
+$lang['AdminMain.graph_line_name.total'] = 'Celkem';
+$lang['AdminMain.graph_name.graph_invoiced'] = 'Fakturovaná částka';
+$lang['AdminMain.graph_name.graph_revenue_year'] = 'Příjmy (od počátku roku)';
+$lang['AdminMain.graph_name.graph_revenue'] = 'Příjmy (rozsah)';
+$lang['AdminMain.date_range.days'] = 'dny';
+$lang['AdminMain.settings.submit_cancel'] = 'Zpět na přehled';
+$lang['AdminMain.settings.submit_settings'] = 'Aktualizace nastavení';
+$lang['AdminMain.settings.date_range'] = 'Rozsah data grafu';
+$lang['AdminMain.settings.show_legend'] = 'Zobrazit legendu na grafech';
+$lang['AdminMain.settings.text_graph_settings'] = 'Správa nastavení grafů, které se zobrazí v Přehledu vyúčtování.';
+$lang['AdminMain.settings.graph_invoiced'] = 'Fakturovaná částka Graf';
+$lang['AdminMain.settings.graph_revenue_year'] = 'Graf příjmů (od začátku roku)';
+$lang['AdminMain.settings.graph_revenue'] = 'Graf příjmů (rozsah)';
+$lang['AdminMain.settings.text_graphs'] = 'Zkontrolujte, které grafy se mají zobrazit v Přehledu vyúčtování.';
+$lang['AdminMain.settings.services_canceled_today'] = 'Dnes zrušené služby';
+$lang['AdminMain.settings.services_added_today'] = 'Dnes přidané služby';
+$lang['AdminMain.settings.services_active'] = 'Aktivní služby';
+$lang['AdminMain.settings.scheduled_cancelation'] = 'Plánovaná zrušení';
+$lang['AdminMain.settings.balance_overdue'] = 'Zůstatek po splatnosti';
+$lang['AdminMain.settings.balance_outstanding'] = 'Nesplacený zůstatek';
+$lang['AdminMain.settings.invoiced_month_proforma'] = 'Fakturováno v tomto měsíci (proforma)';
+$lang['AdminMain.settings.invoiced_today_proforma'] = 'Fakturováno dnes (proforma)';
+$lang['AdminMain.settings.invoiced_month'] = 'Fakturováno v tomto měsíci';
+$lang['AdminMain.settings.invoiced_today'] = 'Fakturováno dnes';
+$lang['AdminMain.settings.credits_year'] = 'Kredity v tomto roce';
+$lang['AdminMain.settings.credits_month'] = 'Kredity v tomto měsíci';
+$lang['AdminMain.settings.credits_today'] = 'Úvěry dnes';
+$lang['AdminMain.settings.revenue_year'] = 'Příjmy v tomto roce';
+$lang['AdminMain.settings.revenue_month'] = 'Příjmy v tomto měsíci';
+$lang['AdminMain.settings.revenue_today'] = 'Příjmy dnes';
+$lang['AdminMain.settings.heading_graph_settings'] = 'Nastavení grafu';
+$lang['AdminMain.settings.heading_graphs'] = 'Grafy';
+$lang['AdminMain.settings.heading_statistics'] = 'Statistiky';
+$lang['AdminMain.settings.text_options'] = 'Zaškrtněte možnosti, které se zobrazí v Přehledu vyúčtování.';
+$lang['AdminMain.index.statistic.services_canceled_today'] = 'Dnes zrušené služby';
+$lang['AdminMain.index.statistic.services_added_today'] = 'Dnes přidané služby';
+$lang['AdminMain.index.statistic.services_active'] = 'Aktivní služby';
+$lang['AdminMain.index.statistic.scheduled_cancelation'] = 'Plánovaná zrušení';
+$lang['AdminMain.index.statistic.balance_overdue'] = 'Zůstatek po splatnosti';
+$lang['AdminMain.index.statistic.balance_outstanding'] = 'Nesplacený zůstatek';
+$lang['AdminMain.index.statistic.invoiced_month_proforma'] = 'Fakturováno v tomto měsíci (proforma)';
+$lang['AdminMain.index.statistic.invoiced_today_proforma'] = 'Fakturováno dnes (proforma)';
+$lang['AdminMain.index.statistic.invoiced_month'] = 'Fakturováno v tomto měsíci';
+$lang['AdminMain.index.statistic.invoiced_today'] = 'Fakturováno dnes';
+$lang['AdminMain.index.statistic.credits_year'] = 'Kredity v tomto roce';
+$lang['AdminMain.index.statistic.credits_month'] = 'Kredity v tomto měsíci';
+$lang['AdminMain.index.statistic.credits_today'] = 'Úvěry dnes';
+$lang['AdminMain.index.statistic.revenue_year'] = 'Příjmy v tomto roce';
+$lang['AdminMain.index.statistic.revenue_month'] = 'Příjmy v tomto měsíci';
+$lang['AdminMain.index.statistic.revenue_today'] = 'Příjmy dnes';
+$lang['AdminMain.index.boxtitle_overview'] = 'Přehled vyúčtování';
+$lang['AdminMain.!success.options_updated'] = 'Možnosti Billing at a Glance byly úspěšně aktualizovány!';
+
