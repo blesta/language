a7fc8b8d7b5f9c485de7984b801eebd12ab95020
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:23 -0800

    Updated de_de language files

---
+++ plugins/order/views/templates/ajax/language/de_de/main.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Main.packages.term_years'] = '%1$s Jahre';
+$lang['Main.packages.term_year'] = '%1$s Jahr';
+$lang['Main.packages.term_months'] = '%1$s Monate';
+$lang['Main.packages.term_month'] = '%1$s Monat';
+$lang['Main.packages.term_weeks'] = '%1$s Wochen';
+$lang['Main.packages.term_week'] = '%1$s Woche';
+$lang['Main.packages.term_days'] = '%1$s Tage';
+$lang['Main.packages.term_day'] = '%1$s Tag';
+$lang['Main.packages.box_sold_out'] = 'Nicht auf Lager';
+$lang['Main.packages.box_client_limit'] = 'Erreichte Grenze';
+$lang['Main.packages.box_selected'] = 'Ausgewählte';
+$lang['Main.packages.box_select'] = 'Wählen Sie';
+$lang['Main.packages.show_cart_btn'] = 'Warenkorb anzeigen';
+$lang['Main.packages.price_start'] = 'Beginnend bei';
+$lang['Main.packages.select_note'] = 'Ziehen Sie, um ein anderes Paket auszuwählen.';
+$lang['Main.packages.text_free_domain'] = 'Wählen Sie ein Paket in dieser Gruppe mit einer Preisoption von %1$s oder mehr für eine kostenlose Domain.';
+$lang['Main.packages.subheading'] = 'Wählen Sie ein Paket aus und konfigurieren Sie es dann weiter unten.';
+$lang['Main.index.field_currency'] = 'Währung:';
+$lang['Main.index.sold_out'] = 'Nicht auf Lager';
+$lang['Main.index.client_limit'] = 'Erreichte Grenze';
+$lang['Main.index.button_order'] = 'Jetzt bestellen';
+$lang['Main.index.field_pricing_id'] = 'Wählen Sie Ihren Abrechnungszyklus.';
+$lang['Main.index.group_heading'] = 'Wählen Sie unten ein Paket aus.';
+$lang['Main.index.domain_bundle_eligble'] = 'Qualifiziert für freie Domäne';
+$lang['Main.index.subheading'] = 'Bitte wählen Sie unten eine Paketgruppe aus, um fortzufahren.';
+$lang['Main.index.heading'] = 'Auswahl des Pakets';
+
