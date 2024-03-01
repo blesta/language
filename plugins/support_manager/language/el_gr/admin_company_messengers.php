a705cc5d0249bdac230999689c0ef1e067a70913
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:52 -0800

    Updated el_gr language files

---
+++ plugins/support_manager/language/el_gr/admin_company_messengers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Admin Company Messengers
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyMessengers.templates.SupportManager.staff_ticket_updated_name'] = 'Ενημερωμένο εισιτήριο προσωπικού';
+$lang['AdminCompanyMessengers.templates.SupportManager.staff_ticket_updated_desc'] = 'Ειδοποίηση που αποστέλλεται σε ένα μέλος του προσωπικού όταν ενημερώνεται ένα εισιτήριο.';
+$lang['AdminCompanyMessengers.templates.SupportManager.staff_ticket_assigned_name'] = 'Εκχωρημένο εισιτήριο προσωπικού';
+$lang['AdminCompanyMessengers.templates.SupportManager.staff_ticket_assigned_desc'] = 'Ειδοποίηση που αποστέλλεται σε ένα μέλος του προσωπικού όταν του έχει ανατεθεί ένα εισιτήριο.';
+
