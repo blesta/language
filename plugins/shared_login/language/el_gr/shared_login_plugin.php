b61a0e6683ee4ad8f27e010f67291e62bcfefd29
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:50 -0800

    Updated el_gr language files

---
+++ plugins/shared_login/language/el_gr/shared_login_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Shared Login Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SharedLoginPlugin.description'] = 'Επιτρέπει τη σύνδεση μέσω ειδικά διαμορφωμένης διεύθυνσης URL.';
+$lang['SharedLoginPlugin.name'] = 'Κοινή σύνδεση';
+
