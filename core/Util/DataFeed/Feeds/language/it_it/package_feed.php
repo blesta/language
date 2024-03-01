2b6276be1f14df640d8861ef2969689d7fef01d7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:15 -0800

    Updated it_it language files

---
+++ core/Util/DataFeed/Feeds/language/it_it/package_feed.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Package Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageFeed.!error.invalid_endpoint'] = 'L\'endpoint richiesto non è valido o non esiste.';
+$lang['PackageFeed.getOptionFields.example_code_pricing'] = 'Mostra il prezzo, il rinnovo, il costo di installazione, il costo di annullamento o la valuta del prezzo di un pacchetto:';
+$lang['PackageFeed.getOptionFields.example_code_description'] = 'Mostra la descrizione del pacchetto:';
+$lang['PackageFeed.getOptionFields.example_code_name'] = 'Mostra il nome di un pacchetto:';
+$lang['PackageFeed.getOptionFields.title_row_example_code'] = 'Codice di esempio';
+$lang['PackageFeed.description'] = 'Restituisce il nome, la descrizione o il prezzo di un pacchetto.';
+$lang['PackageFeed.name'] = 'Pacchetti';
+
