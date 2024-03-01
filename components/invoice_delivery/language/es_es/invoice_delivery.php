96820639ab98c17045eb86db9f88198742366217
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:40 -0800

    Updated es_es language files

---
+++ components/invoice_delivery/language/es_es/invoice_delivery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Invoice Delivery
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['InvoiceDelivery.!error.libxml_required'] = 'Las extensiones libxml y simplexml son necesarias para la entrega de facturas.';
+$lang['InvoiceDelivery.deliverinvoices.interfax_subject'] = 'Factura #%1$s';
+$lang['InvoiceDelivery.deliverinvoices.postalmethods_description'] = 'Factura #%1$s';
+
