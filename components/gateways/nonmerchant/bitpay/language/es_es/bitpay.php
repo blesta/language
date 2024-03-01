1385600a54f0d1f9aec4103abde4930392f64393
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:26 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/bitpay/language/es_es/bitpay.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Bitpay
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Bitpay.name'] = 'Bitpay';
+$lang['Bitpay.description'] = 'Un proveedor de servicios de pago con Bitcoin y criptomonedas';
+$lang['Bitpay.pairing_code'] = 'Código de emparejamiento';
+$lang['Bitpay.transaction_speed'] = 'Velocidad de transacción';
+$lang['Bitpay.transaction.speed.high'] = 'Alta';
+$lang['Bitpay.transaction.speed.medium'] = 'Medio';
+$lang['Bitpay.transaction.speed.low'] = 'Bajo';
+$lang['Bitpay.pairing_code_note'] = 'Vaya al panel de control de BitPay a la sección "Herramientas de pago > Tokens de API" y apruebe el token generado utilizando este código de emparejamiento "%1$s", después guarde la configuración de la pasarela haciendo clic en "Actualizar configuración".';
+$lang['Bitpay.transaction_speed_note'] = 'La velocidad de transacción determina la rapidez con la que una factura (en Bitpay) puede considerarse "confirmada".';
+$lang['Bitpay.test_mode'] = 'Modo de prueba';
+$lang['Bitpay.test_mode_note'] = 'Si está marcada, la pasarela enviará solicitudes de pago a la URL de prueba. Si se cambia, BitPay se desconectará y se generará un nuevo token.';
+$lang['Bitpay.connect.button'] = 'Conectar con BitPay';
+$lang['Bitpay.disconnect.button'] = 'Desconectarse de BitPay';
+$lang['Bitpay.buildprocess.submit'] = 'Pagar con Bitcoin';
+$lang['Bitpay.!error.transaction_speed.valid'] = 'Seleccione una velocidad de transacción válida.';
+$lang['Bitpay.!error.token.valid'] = 'El token no es válido o el código de emparejamiento no ha sido autorizado en BitPay.';
+$lang['Bitpay.!error.failed.response'] = 'No se ha podido procesar la transacción.';
+$lang['Bitpay.!error.payment.invalid'] = 'La transacción no es válida y no ha podido ser procesada.';
+$lang['Bitpay.!error.payment.expired'] = 'La transacción ha caducado y no ha podido ser procesada.';
+
