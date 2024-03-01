273e321907bb1817d01d31b52b66e29c4bdb13c3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:56 -0800

    Updated es_es language files

---
+++ language/es_es/admin_myinfo.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Admin Myinfo
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMyinfo.!success.updated'] = 'La configuración de tu cuenta se ha actualizado correctamente.';
+$lang['AdminMyinfo.!success.notices_updated'] = 'La configuración de tu aviso se ha actualizado correctamente.';
+$lang['AdminMyinfo.gettabnames.text_index'] = 'Cuenta';
+$lang['AdminMyinfo.gettabnames.text_notices'] = 'Avisos';
+$lang['AdminMyinfo.index.page_title'] = 'Mi información';
+$lang['AdminMyinfo.index.boxtitle_myinfo'] = 'Mi información';
+$lang['AdminMyinfo.index.field_username'] = 'Nombre de usuario';
+$lang['AdminMyinfo.index.field_firstname'] = 'Nombre';
+$lang['AdminMyinfo.index.field_lastname'] = 'Apellido';
+$lang['AdminMyinfo.index.field_email'] = 'Correo electrónico';
+$lang['AdminMyinfo.index.field_mobileemail'] = 'Correo electrónico móvil';
+$lang['AdminMyinfo.index.field_mobilenumber'] = 'Número de móvil';
+$lang['AdminMyinfo.index.field_newpass'] = 'Nueva contraseña';
+$lang['AdminMyinfo.index.field_confirmpass'] = 'Confirmar contraseña';
+$lang['AdminMyinfo.index.field_twofactormode'] = 'Autenticación de dos factores';
+$lang['AdminMyinfo.index.field_twofactorkey'] = 'Clave de dos factores';
+$lang['AdminMyinfo.index.field_twofactorpin'] = 'Pin de dos factores';
+$lang['AdminMyinfo.index.field_language'] = 'Idioma';
+$lang['AdminMyinfo.index.field_otp'] = 'Contraseña única';
+$lang['AdminMyinfo.index.note_otp'] = 'Para confirmar que la autenticación de dos factores está configurada correctamente, introduzca la contraseña de un solo uso generada.';
+$lang['AdminMyinfo.index.field_groups'] = 'Grupos de personal';
+$lang['AdminMyinfo.index.field_currentpass'] = 'Contraseña actual';
+$lang['AdminMyinfo.index.field_infosubmit'] = 'Actualizar cuenta';
+$lang['AdminMyinfo.notices.page_title'] = 'Mi información > Avisos';
+$lang['AdminMyinfo.notices.heading_notices'] = 'Notificaciones BCC por correo electrónico';
+$lang['AdminMyinfo.notices.heading_subscription_notices'] = 'Avisos de suscripción por correo electrónico';
+$lang['AdminMyinfo.notices.field_noticesubmit'] = 'Avisos de actualización';
+$lang['AdminMyinfo.notices.no_bcc_results'] = 'No hay notificaciones CCO por correo electrónico disponibles para su grupo de personal.';
+$lang['AdminMyinfo.notices.no_subscription_results'] = 'No hay avisos de suscripción disponibles para su grupo de personal.';
+$lang['AdminMyinfo.index.field_recovery_email'] = 'Correo electrónico de recuperación (opcional)';
+
