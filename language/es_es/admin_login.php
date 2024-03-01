50a18ff6b55a7418104471417c28c0c20401e82a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:55 -0800

    Updated es_es language files

---
+++ language/es_es/admin_login.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Admin Login
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminLogin.index.page_title'] = 'Conectarse';
+$lang['AdminLogin.index.title_adminarea'] = '%1$s | Área de Personal';
+$lang['AdminLogin.index.field_username'] = 'Nombre de usuario';
+$lang['AdminLogin.index.field_password'] = 'Contraseña';
+$lang['AdminLogin.index.field_rememberme'] = 'Recuérdame en este ordenador.';
+$lang['AdminLogin.index.field_loginsubmit'] = 'Iniciar sesión';
+$lang['AdminLogin.index.link_resetpassword'] = 'Restablecer mi contraseña';
+$lang['AdminLogin.otp.page_title'] = 'Inicio de sesión OTP';
+$lang['AdminLogin.otp.title_adminarea'] = '%1$s | Área de Personal';
+$lang['AdminLogin.otp.field_password'] = 'Contraseña única';
+$lang['AdminLogin.otp.field_loginsubmit'] = 'Iniciar sesión';
+$lang['AdminLogin.otp.link_login'] = 'Cancelar, Entrar';
+$lang['AdminLogin.reset.page_title'] = 'Restablecer contraseña';
+$lang['AdminLogin.reset.title_adminarea'] = '%1$s | Restablecer contraseña';
+$lang['AdminLogin.reset.field_username'] = 'Nombre de usuario';
+$lang['AdminLogin.reset.field_resetsubmit'] = 'Restablecer contraseña';
+$lang['AdminLogin.reset.link_login'] = 'Cancelar, Entrar';
+$lang['AdminLogin.confirmreset.page_title'] = 'Confirmar restablecimiento de contraseña';
+$lang['AdminLogin.confirmreset.title_adminarea'] = '%1$s | Confirmar restablecimiento de contraseña';
+$lang['AdminLogin.confirmreset.field_new_password'] = 'Nueva contraseña';
+$lang['AdminLogin.confirmreset.field_confirm_password'] = 'Confirmar nueva contraseña';
+$lang['AdminLogin.confirmreset.field_resetsubmit'] = 'Establecer contraseña';
+$lang['AdminLogin.confirmreset.link_login'] = 'Cancelar, Entrar';
+$lang['AdminLogin.setup.page_title'] = 'Configuración inicial';
+$lang['AdminLogin.setup.title_adminarea'] = 'Configuración inicial';
+$lang['AdminLogin.setup.field_license_key'] = 'Clave de licencia';
+$lang['AdminLogin.setup.trial_newsletter'] = 'Al suscribirse a un periodo de prueba, acepta recibir correos electrónicos nuestros durante dicho periodo. Puede darse de baja en cualquier momento.';
+$lang['AdminLogin.setup.field_newsletter'] = 'Suscríbase a nuestro boletín. Puede darse de baja en cualquier momento.';
+$lang['AdminLogin.setup.heading_create_account'] = 'Cree su cuenta de personal';
+$lang['AdminLogin.setup.field_first_name'] = 'Nombre';
+$lang['AdminLogin.setup.field_last_name'] = 'Apellido';
+$lang['AdminLogin.setup.field_username'] = 'Nombre de usuario';
+$lang['AdminLogin.setup.field_email'] = 'Dirección de correo electrónico';
+$lang['AdminLogin.setup.field_password'] = 'Contraseña';
+$lang['AdminLogin.setup.field_confirm_password'] = 'Confirmar contraseña';
+$lang['AdminLogin.setup.field_enter_key_true'] = 'Tengo que introducir una clave de licencia';
+$lang['AdminLogin.setup.field_enter_key_false'] = 'Quiero iniciar una prueba gratuita de 30 días';
+$lang['AdminLogin.setup.field_submit'] = 'Acabado';
+$lang['AdminLogin.!error.unknown_user'] = 'No se reconoce ese nombre de usuario o no se puede restablecer la contraseña.';
+$lang['AdminLogin.!error.captcha.invalid'] = 'El captcha introducido no es válido. Por favor, inténtelo de nuevo.';
+$lang['AdminLogin.!success.reset_sent'] = 'Se ha enviado un correo electrónico de confirmación a la dirección registrada.';
+$lang['AdminLogin.!info.reset_password'] = 'Introduzca su nombre de usuario a continuación y se le enviará por correo electrónico un enlace para que pueda establecer una nueva contraseña.';
+$lang['AdminLogin.!info.otp'] = 'Se requiere autenticación de dos factores para este usuario. Introduzca su OTP (contraseña de un solo uso) a continuación.';
+
