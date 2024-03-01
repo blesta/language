ac9e84791345807fbdbe3df50bca8fb93750318b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:48 -0800

    Updated el_gr language files

---
+++ plugins/reassign_pricing/language/el_gr/reassign_pricing_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Reassign Pricing Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReassignPricingPlugin.action_staff_client.index'] = 'Επαναπροσδιορισμός τιμολόγησης';
+$lang['ReassignPricingPlugin.description'] = 'Επιτρέπει την ενημέρωση της τιμολόγησης των υπηρεσιών με την τιμολόγηση από άλλα πακέτα που χρησιμοποιούν την ίδια ενότητα.';
+$lang['ReassignPricingPlugin.name'] = 'Επαναπροσδιορισμός τιμολόγησης';
+
