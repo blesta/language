a2f84322621859456f129b3a0b03a24001f92eed
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:27 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/merchant_gateway.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Merchant Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MerchantGateway.!error.card_number_invalid'] = 'Le numéro de la carte n\'est pas valide.';
+$lang['MerchantGateway.!error.card_expired'] = 'La carte a expiré.';
+$lang['MerchantGateway.!error.routing_number_invalid'] = 'Le numéro de routage n\'est pas valide.';
+$lang['MerchantGateway.!error.account_number_invalid'] = 'Le numéro de compte n\'est pas valide.';
+$lang['MerchantGateway.!error.duplicate_transaction'] = 'Une double transaction a été détectée.';
+$lang['MerchantGateway.!error.card_not_accepted'] = 'Les cartes de ce type ne sont pas acceptées.';
+$lang['MerchantGateway.!error.invalid_security_code'] = 'Le code de sécurité semble être invalide.';
+$lang['MerchantGateway.!error.address_verification_failed'] = 'L\'adresse semble être invalide.';
+$lang['MerchantGateway.!error.transaction_not_found'] = 'La transaction n\'est pas valide ou n\'est plus disponible pour le traitement de cette action.';
+$lang['MerchantGateway.!error.unsupported'] = 'La passerelle ne soutient pas cette action.';
+$lang['MerchantGateway.!error.general'] = 'Une erreur s\'est produite lors du traitement de la demande avec la passerelle.';
+
