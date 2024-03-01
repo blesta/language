169e5729e5bb17cc355ca5a6dda268a3c8a0f5e4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:53 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_dialog.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Admin Dialog
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminDialog.confirm.text_yes'] = 'Oui';
+$lang['AdminDialog.confirm.text_no'] = 'Non';
+$lang['AdminDialog.password.field_modal_password'] = 'Mot de passe';
+$lang['AdminDialog.password.field_modal_password_copied'] = 'J\'ai copié ce mot de passe dans un endroit sûr';
+$lang['AdminDialog.password.btn_generate'] = 'Générer';
+$lang['AdminDialog.password.btn_modal_password'] = 'Mot de passe';
+$lang['AdminDialog.password.btn_cancel'] = 'Annuler';
+$lang['AdminDialog.password.btn_use_password'] = 'Utiliser le mot de passe';
+
