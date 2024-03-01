37e782cc5a3c39ee034b476b05c685f9aff65a63
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:50 -0800

    Updated el_gr language files

---
+++ plugins/shared_login/language/el_gr/shared_login_manage_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Shared Login Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SharedLoginManagePlugin.index.page_title'] = 'Κοινή σύνδεση';
+$lang['SharedLoginManagePlugin.index.boxtitle_manage'] = 'Κοινή σύνδεση';
+$lang['SharedLoginManagePlugin.index.field_key'] = 'Κοινόχρηστο κλειδί';
+$lang['SharedLoginManagePlugin.index.submit'] = 'Αποθήκευση';
+
