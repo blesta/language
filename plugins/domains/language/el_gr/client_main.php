064217dbfd6095907ab4a2c31b4432fd0d484d98
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:49 -0800

    Updated el_gr language files

---
+++ plugins/domains/language/el_gr/client_main.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.index.page_title'] = 'Πελάτης #%1$s Domains';
+$lang['ClientMain.index.boxtitle_domains'] = 'Τομείς';
+$lang['ClientMain.index.category_active'] = 'Ενεργό';
+$lang['ClientMain.index.category_pending'] = 'Εκκρεμεί';
+$lang['ClientMain.index.category_suspended'] = 'Αναστολή';
+$lang['ClientMain.index.category_deleted'] = 'Διαγράφηκε';
+$lang['ClientMain.index.heading_addons'] = 'Πρόσθετα';
+$lang['ClientMain.index.heading_status'] = 'Κατάσταση';
+$lang['ClientMain.index.heading_domain'] = 'Τομέας';
+$lang['ClientMain.index.heading_term'] = 'Όρος';
+$lang['ClientMain.index.heading_registration_date'] = 'Ημερομηνία εγγραφής';
+$lang['ClientMain.index.heading_renewal_date'] = 'Ημερομηνία ανανέωσης';
+$lang['ClientMain.index.heading_expiration_date'] = 'Ημερομηνία λήξης';
+$lang['ClientMain.index.heading_suspension_date'] = 'Ημερομηνία αναστολής';
+$lang['ClientMain.index.heading_deletion_date'] = 'Ημερομηνία διαγραφής';
+$lang['ClientMain.index.heading_options'] = 'Επιλογές';
+$lang['ClientMain.index.option_manage'] = 'Διαχείριση';
+$lang['ClientMain.index.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['ClientMain.index.text_never'] = 'Ποτέ';
+$lang['ClientMain.index.no_results'] = 'Δεν έχετε %1$s Domains.';
+$lang['ClientMain.serviceinfo.no_results'] = 'Αυτός ο τομέας δεν έχει λεπτομέρειες.';
+$lang['ClientMain.serviceinfo.parent_service'] = 'Αυτός ο τομέας ανήκει στην <a href="%1$s">%2$s (%3$s)</a>. Εάν ακυρωθεί αυτή η υπηρεσία, θα ακυρωθεί και αυτός ο τομέας.';
+$lang['ClientMain.serviceinfo.cancellation_reason'] = 'Λόγος ακύρωσης: %1$s';
+$lang['ClientMain.getfilters.any'] = 'Οποιοδήποτε';
+$lang['ClientMain.getfilters.field_module_id'] = 'Μονάδες γραμματέα';
+$lang['ClientMain.getfilters.field_package_name'] = 'TLD';
+$lang['ClientMain.getfilters.field_service_meta'] = 'Όνομα τομέα';
+
