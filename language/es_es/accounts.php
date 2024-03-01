e156fb0b97b241e9cc99141edd261f117ff32f6d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:43 -0800

    Updated es_es language files

---
+++ language/es_es/accounts.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Accounts.!error.contact_id.exists'] = 'ID de contacto no válida.';
+$lang['Accounts.!error.first_name.empty'] = 'Por favor, especifique un nombre.';
+$lang['Accounts.!error.last_name.empty'] = 'Por favor, especifique un apellido.';
+$lang['Accounts.!error.state.length'] = 'La longitud del estado no puede superar los 3 caracteres.';
+$lang['Accounts.!error.country.length'] = 'La longitud del país no puede superar los 3 caracteres.';
+$lang['Accounts.!error.state.country_exists'] = 'Seleccione el país que corresponda al estado seleccionado.';
+$lang['Accounts.!error.currency.format'] = 'La moneda no es válida.';
+$lang['Accounts.!error.account.length'] = 'Especifique un número de cuenta.';
+$lang['Accounts.!error.routing.empty'] = 'Especifique un número de ruta.';
+$lang['Accounts.!error.last4.empty_ach'] = 'Introduzca los cuatro últimos dígitos del número de cuenta.';
+$lang['Accounts.!error.type.ach_format'] = 'Tipo no válido.';
+$lang['Accounts.!error.ach_account_id.exists'] = 'ID de cuenta no válida.';
+$lang['Accounts.!error.number.valid'] = 'El número de tarjeta indicado no es válido.';
+$lang['Accounts.!error.expiration.empty'] = 'Introduzca la fecha de caducidad de la tarjeta de crédito.';
+$lang['Accounts.!error.expiration.valid'] = 'La fecha de caducidad ya ha expirado.';
+$lang['Accounts.!error.last4.empty_cc'] = 'Introduzca los cuatro últimos dígitos del número de la tarjeta de crédito.';
+$lang['Accounts.!error.type.cc_format'] = 'El tipo de tarjeta no es válido o no se ha podido determinar.';
+$lang['Accounts.!error.cc_account_id.exists'] = 'ID de cuenta no válida.';
+$lang['Accounts.getTypes.cc'] = 'Tarjeta de crédito';
+$lang['Accounts.getTypes.ach'] = 'Cámara de compensación automatizada';
+$lang['Accounts.getTypes.other'] = 'Otros';
+$lang['Accounts.getAchTypes.checking'] = 'Comprobación de';
+$lang['Accounts.getAchTypes.savings'] = 'Ahorro';
+$lang['Accounts.getCcTypes.amex'] = 'American Express';
+$lang['Accounts.getCcTypes.bc'] = 'Tarjeta bancaria';
+$lang['Accounts.getCcTypes.cup'] = 'China Union Pay';
+$lang['Accounts.getCcTypes.dc-cb'] = 'Diners Club Carta blanca';
+$lang['Accounts.getCcTypes.dc-er'] = 'Diners Club EnRoute';
+$lang['Accounts.getCcTypes.dc-int'] = 'Diners Club Internacional';
+$lang['Accounts.getCcTypes.dc-uc'] = 'Diners Club US y Canadá';
+$lang['Accounts.getCcTypes.disc'] = 'Descubra';
+$lang['Accounts.getCcTypes.ipi'] = 'InstaPayment';
+$lang['Accounts.getCcTypes.jcb'] = 'Oficina de crédito de Japón';
+$lang['Accounts.getCcTypes.lasr'] = 'Láser';
+$lang['Accounts.getCcTypes.maes'] = 'Maestro';
+$lang['Accounts.getCcTypes.mc'] = 'Master Card';
+$lang['Accounts.getCcTypes.solo'] = 'Solo';
+$lang['Accounts.getCcTypes.switch'] = 'Interruptor';
+$lang['Accounts.getCcTypes.visa'] = 'Visa';
+$lang['Accounts.getCcTypes.other'] = 'Otros';
+
