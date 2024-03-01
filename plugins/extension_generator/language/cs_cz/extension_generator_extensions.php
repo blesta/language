8f8d269ce4b572043cf62c89a441803edbc79cc1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:43 -0800

    Updated cs_cz language files

---
+++ plugins/extension_generator/language/cs_cz/extension_generator_extensions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Extension Generator Extensions
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorExtensions.getformtypes.advanced'] = 'Pokročilé';
+$lang['ExtensionGeneratorExtensions.getformtypes.basic'] = 'Základní';
+$lang['ExtensionGeneratorExtensions.gettypes.nonmerchant'] = 'Neobchodní brána';
+$lang['ExtensionGeneratorExtensions.gettypes.merchant'] = 'Obchodní brána';
+$lang['ExtensionGeneratorExtensions.gettypes.plugin'] = 'Zásuvný modul';
+$lang['ExtensionGeneratorExtensions.gettypes.module'] = 'Modul';
+$lang['ExtensionGeneratorExtensions.!error.id.exists'] = 'Zadané ID rozšíření je neplatné.';
+$lang['ExtensionGeneratorExtensions.!error.code_examples.format'] = 'Neplatný formát volby příkladů kódu.';
+$lang['ExtensionGeneratorExtensions.!error.form_type.valid'] = 'Nesprávný typ formuláře.';
+$lang['ExtensionGeneratorExtensions.!error.type.valid'] = 'Nesprávný typ rozšíření.';
+$lang['ExtensionGeneratorExtensions.!error.date_updated.format'] = 'Nesprávný formát aktualizace data.';
+$lang['ExtensionGeneratorExtensions.!error.company_id.exists'] = 'Neplatné ID společnosti.';
+$lang['ExtensionGeneratorExtensions.!error.name.empty'] = 'Zadejte prosím název přípony.';
+
