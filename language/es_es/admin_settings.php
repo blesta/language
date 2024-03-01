e057dbcc2670a61f4ef1e04c582af4179be25f2d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:59 -0800

    Updated es_es language files

---
+++ language/es_es/admin_settings.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Admin Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSettings.company.page_title'] = 'Ajustes';
+$lang['AdminSettings.company.category_company'] = 'Empresa';
+$lang['AdminSettings.company.category_system'] = 'Sistema';
+$lang['AdminSettings.company.boxtitle_settings'] = 'Ajustes';
+$lang['AdminSettings.company.heading_general'] = 'General';
+$lang['AdminSettings.company.text_general'] = 'Configure la localización, las opciones de internacionalización y los tipos de contacto de los clientes.';
+$lang['AdminSettings.company.heading_billing'] = 'Facturación/Pago';
+$lang['AdminSettings.company.text_billing'] = 'Configure las opciones y la personalización de las facturas, los pagos automáticos, los avisos de pago y los cupones.';
+$lang['AdminSettings.company.heading_emails'] = 'Correos electrónicos';
+$lang['AdminSettings.company.text_emails'] = 'Configure los ajustes de correo, edite plantillas de correo electrónico y cree y gestione firmas de correo electrónico.';
+$lang['AdminSettings.company.heading_messengers'] = 'Mensajeros';
+$lang['AdminSettings.company.text_messengers'] = 'Instalar y gestionar mensajeros, que se utilizan para las notificaciones y la comunicación.';
+$lang['AdminSettings.company.heading_currencies'] = 'Divisas';
+$lang['AdminSettings.company.text_currencies'] = 'Actualizar y añadir divisas, establecer la divisa por defecto, configurar multidivisa y configurar los tipos de cambio.';
+$lang['AdminSettings.company.heading_clientgroups'] = 'Grupos de clientes';
+$lang['AdminSettings.company.text_clientgroups'] = 'Todos los clientes pertenecen a un grupo, que determina cuándo se factura, se cobra y mucho más a ese cliente. Crea y gestiona grupos y configuraciones de clientes.';
+$lang['AdminSettings.company.heading_automation'] = 'Automatización';
+$lang['AdminSettings.company.text_automation'] = 'Accede y controla las tareas automatizadas en Blesta.';
+$lang['AdminSettings.company.heading_modules'] = 'Módulos';
+$lang['AdminSettings.company.text_modules'] = 'Instalar y gestionar módulos, que se utilizan para el aprovisionamiento y la gestión de servicios.';
+$lang['AdminSettings.company.heading_gateways'] = 'Pasarelas de pago';
+$lang['AdminSettings.company.text_gateways'] = 'Activar y desactivar procesadores de pago comerciales y no comerciales para la aceptación de pagos.';
+$lang['AdminSettings.company.heading_taxes'] = 'Impuestos';
+$lang['AdminSettings.company.text_taxes'] = 'Active o desactive los impuestos, establezca un NIF/IVATIN y cree reglas fiscales de uno o varios niveles.';
+$lang['AdminSettings.company.heading_clientoptions'] = 'Opciones para clientes';
+$lang['AdminSettings.company.text_clientoptions'] = 'Cree y gestione campos de cliente personalizados y seleccione los campos de cliente obligatorios.';
+$lang['AdminSettings.company.heading_plugins'] = 'Plugins';
+$lang['AdminSettings.company.text_plugins'] = 'Instala, desinstala y gestiona plugins, que amplían y personalizan la funcionalidad principal de Blesta.';
+$lang['AdminSettings.company.heading_lookandfeel'] = 'Aspecto y tacto';
+$lang['AdminSettings.company.text_lookandfeel'] = 'Personaliza el aspecto de Blesta gestionando los temas de color o actualizando la plantilla de la interfaz de cliente.';
+$lang['AdminSettings.company.heading_feeds'] = 'Fuentes de datos';
+$lang['AdminSettings.company.text_feeds'] = 'Las fuentes de datos permiten incluir fácilmente los precios, nombres y descripciones de los paquetes en un sitio web externo.';
+$lang['AdminSettings.system.page_title'] = 'Ajustes';
+$lang['AdminSettings.system.category_company'] = 'Empresa';
+$lang['AdminSettings.system.category_system'] = 'Sistema';
+$lang['AdminSettings.system.boxtitle_settings'] = 'Ajustes';
+$lang['AdminSettings.system.heading_general'] = 'General';
+$lang['AdminSettings.system.text_general'] = 'Configure los ajustes de todo el sistema, incluida la activación del Modo de mantenimiento, la base de datos GeoIP o la actualización de su Clave de licencia.';
+$lang['AdminSettings.system.heading_companies'] = 'Empresas';
+$lang['AdminSettings.system.text_companies'] = 'Añadir o actualizar Empresas.';
+$lang['AdminSettings.system.heading_backup'] = 'Copia de seguridad';
+$lang['AdminSettings.system.text_backup'] = 'Configure copias de seguridad automáticas mediante SFTP o Amazon S3, o descargue una copia de seguridad en su ordenador local.';
+$lang['AdminSettings.system.heading_api'] = 'Acceso API';
+$lang['AdminSettings.system.text_api'] = 'Controla el acceso a la API de esta instalación.';
+$lang['AdminSettings.system.heading_marketplace'] = 'Mercado';
+$lang['AdminSettings.system.text_marketplace'] = 'Navegue y descargue plugins, módulos, pasarelas y mucho más desde el Marketplace de Blesta.';
+$lang['AdminSettings.system.heading_automation'] = 'Automatización';
+$lang['AdminSettings.system.text_automation'] = 'Determine la información de su Cron o tarea programada.';
+$lang['AdminSettings.system.heading_staff'] = 'Personal';
+$lang['AdminSettings.system.text_staff'] = 'Crear o modificar cuentas y permisos de Personal. El personal tiene acceso al área de administración de Blesta.';
+$lang['AdminSettings.system.heading_upgrade'] = 'Opciones de actualización';
+$lang['AdminSettings.system.text_upgrade'] = 'Ver información sobre actualizaciones y descargas.';
+$lang['AdminSettings.system.heading_help'] = 'Ayuda';
+$lang['AdminSettings.system.text_help'] = 'Consulta los recursos de ayuda para tu instalación de Blesta.';
+
