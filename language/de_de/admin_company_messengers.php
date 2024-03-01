68c1cad38856990cc69f22f47a6c2b4fdd0b3818
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:27 -0800

    Updated de_de language files

---
+++ language/de_de/admin_company_messengers.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Admin Company Messengers
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyMessengers.edittemplate.field.edittemplatesubmit'] = 'Vorlage aktualisieren';
+$lang['AdminCompanyMessengers.edittemplate.field.cancel'] = 'Abbrechen';
+$lang['AdminCompanyMessengers.edittemplate.field.status'] = 'Aktiviert';
+$lang['AdminCompanyMessengers.edittemplate.field.tags'] = 'Verfügbare Tags';
+$lang['AdminCompanyMessengers.edittemplate.text_none'] = 'Keine';
+$lang['AdminCompanyMessengers.edittemplate.boxtitle_edittemplate'] = 'Nachrichtenvorlage bearbeiten %1$s';
+$lang['AdminCompanyMessengers.edittemplate.page_title'] = 'Einstellungen > Unternehmen > E-Mails > Nachrichtenvorlage bearbeiten';
+$lang['AdminCompanyMessengers.templates.no_results'] = 'Es gibt keine Vorlagen dieser Art.';
+$lang['AdminCompanyMessengers.templates.option_edit'] = 'bearbeiten';
+$lang['AdminCompanyMessengers.templates.staff_quotation_approved_desc'] = 'Mitteilung, die nach der Genehmigung eines Angebots durch den Kunden versandt wird.';
+$lang['AdminCompanyMessengers.templates.staff_quotation_approved_name'] = 'Zitat Genehmigung';
+$lang['AdminCompanyMessengers.templates.quotation_delivery_desc'] = 'Hinweis mit einer Angebotsnummer und einem Link.';
+$lang['AdminCompanyMessengers.templates.quotation_delivery_name'] = 'Angebot Lieferung';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_paid_desc'] = 'Mitteilung, die den Gesamtbetrag einer bezahlten Rechnung enthält.';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_paid_name'] = 'Lieferung der Rechnung (bezahlt)';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_unpaid_desc'] = 'Mitteilung, die den Gesamtbetrag einer unbezahlten Rechnung enthält.';
+$lang['AdminCompanyMessengers.templates.invoice_delivery_unpaid_name'] = 'Lieferung von Rechnungen (unbezahlt)';
+$lang['AdminCompanyMessengers.templates.service_unsuspension_desc'] = 'Benachrichtigung über die Beendigung des Dienstes, die gesendet wird, wenn ein Dienst automatisch beendet wird.';
+$lang['AdminCompanyMessengers.templates.service_unsuspension_name'] = 'Aussetzung des Dienstes';
+$lang['AdminCompanyMessengers.templates.service_suspension_desc'] = 'Mitteilung über die Aussetzung eines Dienstes, die gesendet wird, wenn ein Dienst automatisch ausgesetzt wird.';
+$lang['AdminCompanyMessengers.templates.service_suspension_name'] = 'Aussetzung des Dienstes';
+$lang['AdminCompanyMessengers.templates.service_scheduled_cancellation_desc'] = 'Benachrichtigung über die geplante Stornierung eines Dienstes, die gesendet wird, wenn ein Dienst zur Stornierung vorgesehen ist.';
+$lang['AdminCompanyMessengers.templates.service_scheduled_cancellation_name'] = 'Service Geplante Stornierung';
+$lang['AdminCompanyMessengers.templates.service_creation_desc'] = 'Mitteilung über die Erstellung eines Dienstes, die gesendet wird, wenn ein Dienst erstellt wurde.';
+$lang['AdminCompanyMessengers.templates.service_creation_name'] = 'Erstellung von Dienstleistungen';
+$lang['AdminCompanyMessengers.templates.invoice_notice_third_desc'] = 'Dritte Rechnungsmitteilung, entweder eine Zahlungserinnerung oder eine Verzugsmitteilung.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_third_name'] = 'Mitteilung über die Rechnung (3.)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_second_desc'] = 'Zweite Rechnungsmitteilung, entweder eine Zahlungserinnerung oder eine Verzugsmitteilung.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_second_name'] = 'Mitteilung über die Rechnung (2.)';
+$lang['AdminCompanyMessengers.templates.invoice_notice_first_desc'] = 'Erste Rechnungsmitteilung, entweder als Zahlungserinnerung oder als Verzugsmitteilung.';
+$lang['AdminCompanyMessengers.templates.invoice_notice_first_name'] = 'Mitteilung über die Rechnung (1.)';
+$lang['AdminCompanyMessengers.templates.auto_debit_pending_desc'] = 'Es wurde eine Mitteilung verschickt, die darauf hinweist, dass demnächst eine automatische Zahlung versucht wird.';
+$lang['AdminCompanyMessengers.templates.auto_debit_pending_name'] = 'Auto-Debit ausstehend';
+$lang['AdminCompanyMessengers.templates.account_welcome_desc'] = 'Willkommensnachricht für neu registrierte Konten gesendet.';
+$lang['AdminCompanyMessengers.templates.account_welcome_name'] = 'Registrierung eines Kontos';
+$lang['AdminCompanyMessengers.templates.text_options'] = 'Optionen';
+$lang['AdminCompanyMessengers.templates.text_description'] = 'Beschreibung';
+$lang['AdminCompanyMessengers.templates.text_plugin'] = 'Plugin';
+$lang['AdminCompanyMessengers.templates.text_name'] = 'Name';
+$lang['AdminCompanyMessengers.templates.heading_plugins'] = 'Plugin-Meldungen';
+$lang['AdminCompanyMessengers.templates.heading_staff'] = 'Nachrichten für das Personal';
+$lang['AdminCompanyMessengers.templates.heading_client'] = 'Client-Nachrichten';
+$lang['AdminCompanyMessengers.templates.boxtitle_message_templates'] = 'Vorlagen für Nachrichten';
+$lang['AdminCompanyMessengers.templates.page_title'] = 'Einstellungen > Unternehmen > Nachrichtenübermittler > Nachrichtenvorlagen';
+$lang['AdminCompanyMessengers.configuration.text_none'] = 'Es sind keine Boten installiert.';
+$lang['AdminCompanyMessengers.configuration.field_update'] = 'Einstellungen aktualisieren';
+$lang['AdminCompanyMessengers.configuration.heading_messenger'] = 'Bote';
+$lang['AdminCompanyMessengers.configuration.heading_message_type'] = 'Nachrichtentyp';
+$lang['AdminCompanyMessengers.configuration.boxtitle_messenger_configuration'] = 'Messenger-Konfiguration';
+$lang['AdminCompanyMessengers.configuration.page_title'] = 'Einstellungen > Unternehmen > Messenger > Messenger-Konfiguration';
+$lang['AdminCompanyMessengers.manage.field_update'] = 'Einstellungen aktualisieren';
+$lang['AdminCompanyMessengers.manage.none_text'] = 'Für diesen Boten gibt es nichts zu verwalten.';
+$lang['AdminCompanyMessengers.manage.boxtitle_manage'] = 'Verwalten %1$s';
+$lang['AdminCompanyMessengers.manage.page_title'] = 'Einstellungen > Unternehmen > Messenger > Verwalten von %1$s';
+$lang['AdminCompanyMessengers.installed.confirm_uninstall'] = 'Sind Sie sicher, dass Sie diesen Messenger deinstallieren möchten?';
+$lang['AdminCompanyMessengers.installed.btn_upgrade'] = 'Upgrade';
+$lang['AdminCompanyMessengers.installed.btn_manage'] = 'Verwalten Sie';
+$lang['AdminCompanyMessengers.installed.btn_uninstall'] = 'Deinstallieren Sie';
+$lang['AdminCompanyMessengers.installed.text_none'] = 'Es sind keine Boten installiert.';
+$lang['AdminCompanyMessengers.installed.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyMessengers.installed.text_author'] = 'Autor:';
+$lang['AdminCompanyMessengers.installed.boxtitle_installed'] = 'Boten';
+$lang['AdminCompanyMessengers.installed.page_title'] = 'Einstellungen > Unternehmen > Messengers > Installiert';
+$lang['AdminCompanyMessengers.available.btn_install'] = 'Installieren Sie';
+$lang['AdminCompanyMessengers.available.text_none'] = 'Es gibt keine verfügbaren Boten.';
+$lang['AdminCompanyMessengers.available.text_version'] = '(ver %1$s)';
+$lang['AdminCompanyMessengers.available.text_author'] = 'Autor:';
+$lang['AdminCompanyMessengers.available.boxtitle_messenger'] = 'Boten';
+$lang['AdminCompanyMessengers.available.page_title'] = 'Einstellungen > Unternehmen > Messenger > Verfügbar';
+$lang['AdminCompanyMessengers.!tab.available'] = 'Verfügbar';
+$lang['AdminCompanyMessengers.!tab.installed'] = 'Installiert';
+$lang['AdminCompanyMessengers.!tooltip.status'] = 'Wenn diese Option nicht aktiviert ist, werden keine Nachrichten mit dieser Vorlage versendet.';
+$lang['AdminCompanyMessengers.!success.messenger_updated'] = 'Der Messenger wurde erfolgreich aktualisiert.';
+$lang['AdminCompanyMessengers.!success.edit_template_updated'] = 'Die Nachrichtenvorlage wurde erfolgreich aktualisiert.';
+$lang['AdminCompanyMessengers.!success.messenger_options_updated'] = 'Die Messenger-Konfiguration wurde erfolgreich aktualisiert!';
+$lang['AdminCompanyMessengers.!success.uninstalled'] = 'Der Messenger wurde erfolgreich deinstalliert.';
+$lang['AdminCompanyMessengers.!success.installed'] = 'Der Messenger wurde erfolgreich installiert.';
+$lang['AdminCompanyMessengers.!success.upgraded'] = 'Der Kurier wurde erfolgreich aktualisiert.';
+
