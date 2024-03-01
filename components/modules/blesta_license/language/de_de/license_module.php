18aa09393e155524b05c144274e95811971399a8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:20 -0800

    Updated de_de language files

---
+++ components/modules/blesta_license/language/de_de/license_module.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * License Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BlestaLicense.tab_reissue.field_submit'] = 'Neuauflage';
+$lang['BlestaLicense.tab_reissue.description'] = 'Durch die Neuausstellung der Lizenz kann die Lizenz installiert oder an einen anderen Ort verschoben werden, wo sie gesperrt wird.';
+$lang['BlestaLicense.tab_reissue.heading_reissue'] = 'Neuauflage';
+$lang['BlestaLicense.tab_reissue'] = 'Lizenz neu ausstellen';
+$lang['Package.package_fields.pricing_id'] = 'Lizenz';
+$lang['Package.getpricings.pricing_id'] = '%1$s (ID %2$s)';
+$lang['BlestaLicense.edit_row.edit_btn'] = 'Update Wiederverkäufer';
+$lang['BlestaLicense.edit_row.basic_title'] = 'Grundeinstellungen';
+$lang['BlestaLicense.edit_row.box_title'] = 'Wiederverkäufer bearbeiten';
+$lang['BlestaLicense.add_row.add_btn'] = 'Wiederverkäufer hinzufügen';
+$lang['BlestaLicense.add_row.basic_title'] = 'Grundeinstellungen';
+$lang['BlestaLicense.add_row.box_title'] = 'Wiederverkäufer hinzufügen';
+$lang['BlestaLicense.row_meta.test_mode'] = 'Test-Modus';
+$lang['BlestaLicense.row_meta.password'] = 'Passwort';
+$lang['BlestaLicense.row_meta.username'] = 'Benutzername';
+$lang['BlestaLicense.!tooltip.password'] = 'Geben Sie das Passwort für Ihr Blesta-Konto ein.';
+$lang['BlestaLicense.!tooltip.username'] = 'Geben Sie den Benutzernamen Ihres Blesta-Kontos ein.';
+$lang['BlestaLicense.manage.module_rows_no_results'] = 'Es gibt keine Wiederverkäufer.';
+$lang['BlestaLicense.manage.module_rows.confirm_delete'] = 'Sind Sie sicher, dass Sie diesen Wiederverkäufer löschen möchten?';
+$lang['BlestaLicense.manage.module_rows.delete'] = 'Löschen';
+$lang['BlestaLicense.manage.module_rows.edit'] = 'bearbeiten';
+$lang['BlestaLicense.manage.module_rows_heading.options'] = 'Optionen';
+$lang['BlestaLicense.manage.module_rows_heading.username'] = 'Benutzername';
+$lang['BlestaLicense.manage.module_rows_title'] = 'Wiederverkäufer';
+$lang['BlestaLicense.add_module_row'] = 'Wiederverkäufer hinzufügen';
+$lang['BlestaLicense.module_rows.name'] = 'Wiederverkäufer';
+$lang['BlestaLicense.module_row.name'] = 'Wiederverkäufer';
+$lang['Package.!error.meta[pricing_id].format'] = 'Bitte wählen Sie eine gültige Lizenz aus.';
+
