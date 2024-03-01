38380a31e0f8cf94c758805cce617ff8e9e70747
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:02 -0800

    Updated el_gr language files

---
+++ plugins/system_overview/language/el_gr/admin_main.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.options_updated'] = 'Οι ρυθμίσεις της Επισκόπησης συστήματος ενημερώθηκαν με επιτυχία!';
+$lang['AdminMain.index.boxtitle_overview'] = 'Επισκόπηση συστήματος';
+$lang['AdminMain.overview.statistic.clients_active'] = 'Ενεργοί πελάτες';
+$lang['AdminMain.overview.statistic.services_active'] = 'Ενεργές υπηρεσίες';
+$lang['AdminMain.overview.statistic.services_scheduled_cancellation'] = 'Υπηρεσίες που έχουν προγραμματιστεί για ακύρωση';
+$lang['AdminMain.overview.statistic.active_users_today'] = 'Χρήστες Ενεργοί σήμερα';
+$lang['AdminMain.overview.statistic.recurring_invoices'] = 'Επαναλαμβανόμενα τιμολόγια';
+$lang['AdminMain.overview.statistic.pending_orders'] = 'Εκκρεμείς παραγγελίες';
+$lang['AdminMain.overview.statistic.open_tickets'] = 'Ανοιχτά εισιτήρια';
+$lang['AdminMain.overview.heading_users'] = 'Πρόσφατοι χρήστες';
+$lang['AdminMain.overview.tab_all'] = 'Γραφήματα';
+$lang['AdminMain.overview.tab_clients'] = 'Πελάτες';
+$lang['AdminMain.overview.tab_services'] = 'Υπηρεσίες';
+$lang['AdminMain.overview.tooltip_activity'] = 'Τελευταία δραστηριότητα';
+$lang['AdminMain.overview.tooltip_location'] = 'Τοποθεσία';
+$lang['AdminMain.overview.tooltip_last_activity_now'] = 'Ακριβώς τώρα';
+$lang['AdminMain.overview.tooltip_last_activity_minute'] = '1 λεπτό πριν';
+$lang['AdminMain.overview.tooltip_last_activity_minutes'] = '%1$s λεπτά πριν';
+$lang['AdminMain.date_range.days'] = '%1$s Ημέρες';
+$lang['AdminMain.settings.heading_statistics'] = 'Στατιστικά στοιχεία';
+$lang['AdminMain.settings.text_options'] = 'Ελέγξτε τις επιλογές που θα εμφανιστούν στην Επισκόπηση συστήματος.';
+$lang['AdminMain.settings.clients_active'] = 'Ενεργοί πελάτες';
+$lang['AdminMain.settings.services_active'] = 'Ενεργές υπηρεσίες';
+$lang['AdminMain.settings.services_scheduled_cancellation'] = 'Υπηρεσίες που έχουν προγραμματιστεί για ακύρωση';
+$lang['AdminMain.settings.active_users_today'] = 'Χρήστες Ενεργοί σήμερα';
+$lang['AdminMain.settings.recurring_invoices'] = 'Επαναλαμβανόμενα τιμολόγια';
+$lang['AdminMain.settings.open_tickets'] = 'Ανοιχτά εισιτήρια';
+$lang['AdminMain.settings.pending_orders'] = 'Εκκρεμείς παραγγελίες';
+$lang['AdminMain.settings.heading_clients'] = 'Καρτέλα Πελάτες';
+$lang['AdminMain.settings.text_graphs_clients'] = 'Ελέγξτε ποια γραφήματα θα πρέπει να εμφανίζονται στην καρτέλα Πελάτες.';
+$lang['AdminMain.settings.graph_clients'] = 'Γράφημα πελατών';
+$lang['AdminMain.settings.heading_services'] = 'Καρτέλα Υπηρεσίες';
+$lang['AdminMain.settings.text_graphs_services'] = 'Ελέγξτε ποια γραφήματα θα πρέπει να εμφανίζονται στην καρτέλα Υπηρεσίες.';
+$lang['AdminMain.settings.graph_services'] = 'Γράφημα υπηρεσιών';
+$lang['AdminMain.settings.heading_settings'] = 'Ρυθμίσεις';
+$lang['AdminMain.settings.show_one_tab'] = 'Εμφάνιση μόνο μίας καρτέλας';
+$lang['AdminMain.settings.tooltip_show_one_tab'] = 'Δεν διαχωρίζει πλέον τα γραφήματα σε καρτέλες, αλλά τα εμφανίζει όλα σε μία καρτέλα.';
+$lang['AdminMain.settings.show_legend'] = 'Εμφάνιση Υπόμνημα';
+$lang['AdminMain.settings.date_range'] = 'Εύρος ημερομηνίας γραφήματος';
+$lang['AdminMain.settings.submit_cancel'] = 'Επιστροφή στην Επισκόπηση';
+$lang['AdminMain.settings.submit_settings'] = 'Ενημέρωση ρυθμίσεων';
+$lang['AdminMain.graph_name.graph_services'] = 'Υπηρεσίες';
+$lang['AdminMain.graph_name.graph_clients'] = 'Πελάτες';
+$lang['AdminMain.graph_line_name.active'] = 'Ενεργό';
+$lang['AdminMain.graph_line_name.canceled'] = 'Ακυρώθηκε';
+$lang['AdminMain.graph_line_name.suspended'] = 'Αναστολή';
+$lang['AdminMain.graph_line_name.new'] = 'Νέα';
+
