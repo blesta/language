b94ebe5e4d1b3d4c0421e79b85e4442195f97f65
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:36 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/main.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Main.!error.package_limits'] = 'Se han alcanzado los límites de paquetes para todos los paquetes del grupo seleccionado.';
+$lang['Main.index.package_price'] = '%1$s @ %2$s';
+$lang['Main.index.package_price_recurring'] = '%1$s @ %2$s (renueva @ %3$s)';
+
