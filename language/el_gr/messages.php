69658f9bd1eed5dc4fc5e0f5766d06ebabdf0750
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:23 -0800

    Updated el_gr language files

---
+++ language/el_gr/messages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Messages
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Messages.getStatusTypes.inactive'] = 'Ανενεργό';
+$lang['Messages.getStatusTypes.active'] = 'Ενεργό';
+$lang['Messages.getTypes.sms'] = 'SMS';
+$lang['Messages.!error.content.format'] = 'Το περιεχόμενο του μηνύματος πρέπει να είναι ένας πίνακας.';
+$lang['Messages.!error.status.valid'] = 'Η κατάσταση του μηνύματος πρέπει να έχει οριστεί σε "ενεργό" ή "ανενεργό".';
+$lang['Messages.!error.type.valid'] = 'Ο τύπος του μηνύματος πρέπει να έχει οριστεί σε "sms".';
+$lang['Messages.!error.company_id.valid'] = 'Μη έγκυρο αναγνωριστικό εταιρείας.';
+$lang['Messages.!error.message_group_id.valid'] = 'Μη έγκυρο αναγνωριστικό ομάδας μηνυμάτων.';
+
