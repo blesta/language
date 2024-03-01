f5c4b7d50ee50e865847c3a3d3ead9630e872f0f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:12 -0800

    Updated es_es language files

---
+++ components/reports/package_revenue/language/es_es/package_revenue.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Package Revenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageRevenue.name'] = 'Ingresos por paquetes';
+$lang['PackageRevenue.description'] = 'Calcula los ingresos de una divisa para cada paquete totalizando las partidas de factura pagadas asociadas al paquete.';
+$lang['PackageRevenue.option.any'] = 'Cualquier';
+$lang['PackageRevenue.options.currency'] = 'Moneda';
+$lang['PackageRevenue.options.field_start_date'] = 'Factura cerrada Fecha de inicio';
+$lang['PackageRevenue.options.field_end_date'] = 'Fecha de cierre de la factura';
+$lang['PackageRevenue.heading.package_name'] = 'Paquete';
+$lang['PackageRevenue.heading.status'] = 'Estado';
+$lang['PackageRevenue.heading.module_name'] = 'Módulo';
+$lang['PackageRevenue.heading.service_count'] = 'Unidades';
+$lang['PackageRevenue.heading.package_revenue'] = 'Ingresos';
+$lang['PackageRevenue.heading.tax'] = 'Impuesto';
+
