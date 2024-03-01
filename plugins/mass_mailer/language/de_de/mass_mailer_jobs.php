f4e9d0a99034ff848a1f2a72c41fe9fa90a3f9ff
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:57 -0800

    Updated de_de language files

---
+++ plugins/mass_mailer/language/de_de/mass_mailer_jobs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Mass Mailer Jobs
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerJobs.status.complete'] = 'Vollständig';
+$lang['MassMailerJobs.status.in_progress'] = 'In Arbeit';
+$lang['MassMailerJobs.status.pending'] = 'Anhängig';
+$lang['MassMailerJobs.!error.job_id.exists'] = 'Ungültige Job-ID.';
+$lang['MassMailerJobs.!error.date_added.format'] = 'Bitte setzen Sie ein Datum hinzu.';
+$lang['MassMailerJobs.!error.data.empty'] = 'Bitte fügen Sie zusätzliche Informationen zur Stelle hinzu.';
+$lang['MassMailerJobs.!error.task_count.format'] = 'Die Anzahl der Aufgaben muss eine positive ganze Zahl sein.';
+$lang['MassMailerJobs.!error.status.format'] = 'Bitte wählen Sie einen gültigen Status.';
+$lang['MassMailerJobs.!error.company_id.valid'] = 'Bitte geben Sie eine Unternehmens-ID ein.';
+
