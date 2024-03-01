c00fd957346d4c050f78124876d29a6c8f607f3d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:10 -0800

    Updated it_it language files

---
+++ core/Pricing/Modifier/Type/Description/language/it_it/domain.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Domain
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Domain.description.package.item.updated.prorate'] = 'Variazione proporzionale da %1$s a %2$s (%3$s - %4$s)';
+$lang['Domain.description.package.item.prorate'] = 'Prorogato %1$s (%2$s - %3$s)';
+$lang['Domain.description.package.cancel'] = 'Tassa di annullamento %1$s';
+$lang['Domain.description.package.setup'] = '%1$s Costo di installazione';
+$lang['Domain.description.package.item.updated.date'] = 'Variazione da %1$s a %2$s (%3$s - %4$s)';
+$lang['Domain.description.package.item.updated'] = 'Cambia da %1$s a %2$s';
+$lang['Domain.description.package.item.date'] = '%1$s (%2$s - %3$s)';
+$lang['Domain.description.package.item'] = '%1$s';
+$lang['Domain.description.domain.item.updated.prorate'] = 'Variazione proporzionale da %1$s a %2$s - %3$s (%4$s - %5$s)';
+$lang['Domain.description.domain.item.prorate'] = 'Prorogato %1$s - %2$s (%3$s - %4$s)';
+$lang['Domain.description.domain.cancel'] = '%1$s - %2$s Tassa di annullamento';
+$lang['Domain.description.domain.setup'] = '%1$s - %2$s Costo di installazione';
+$lang['Domain.description.domain.item.updated.date.renewed'] = 'Variazione da %1$s a %2$s - %3$s (%4$s - %5$s)';
+$lang['Domain.description.domain.item.updated.renewed'] = 'Passaggio da %1$s a %2$s - %3$s';
+$lang['Domain.description.domain.item.date.renewed'] = 'Rinnovo del dominio - %2$s (%3$s - %4$s)';
+$lang['Domain.description.domain.item.renewed'] = 'Rinnovo del dominio - %2$s';
+$lang['Domain.description.domain.item.updated.date.transferred'] = 'Variazione da %1$s a %2$s - %3$s (%4$s - %5$s)';
+$lang['Domain.description.domain.item.updated.transferred'] = 'Passaggio da %1$s a %2$s - %3$s';
+$lang['Domain.description.domain.item.date.transferred'] = 'Trasferimento del dominio - %2$s (%3$s - %4$s)';
+$lang['Domain.description.domain.item.transferred'] = 'Trasferimento del dominio - %2$s';
+$lang['Domain.description.domain.item.updated.date'] = 'Variazione da %1$s a %2$s - %3$s (%4$s - %5$s)';
+$lang['Domain.description.domain.item.updated'] = 'Passaggio da %1$s a %2$s - %3$s';
+$lang['Domain.description.domain.item.date'] = 'Registrazione del dominio - %2$s (%3$s - %4$s)';
+$lang['Domain.description.domain.item'] = 'Registrazione del dominio - %2$s';
+
