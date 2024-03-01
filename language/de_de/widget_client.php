a9b8c1c6902f6266a53318fdbbce1a4fd1df1aae
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:23 -0800

    Updated de_de language files

---
+++ language/de_de/widget_client.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Widget Client
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['WidgetClient.submit'] = 'Einreichen';
+
