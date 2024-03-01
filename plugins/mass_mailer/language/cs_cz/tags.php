78c774336e417003007d8002102968bd14ac174f
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:57 -0800

    Updated cs_cz language files

---
+++ plugins/mass_mailer/language/cs_cz/tags.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Tags
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Tags.module.label'] = 'Označení řádku modulu';
+$lang['Tags.module.name'] = 'Název modulu';
+$lang['Tags.service.status'] = 'Stav';
+$lang['Tags.service.date_canceled'] = 'Datum zrušení';
+$lang['Tags.service.date_suspended'] = 'Datum pozastavení';
+$lang['Tags.service.date_last_renewed'] = 'Datum posledního obnovení';
+$lang['Tags.service.date_renews'] = 'Datum obnovení';
+$lang['Tags.service.date_added'] = 'Datum přidání';
+$lang['Tags.service.name'] = 'Štítek';
+$lang['Tags.package.status'] = 'Stav balíčku';
+$lang['Tags.package.description_html'] = 'Balíček HTML Popis';
+$lang['Tags.package.description'] = 'Text balíčku Popis';
+$lang['Tags.package.name'] = 'Název balíčku';
+$lang['Tags.contact.date_added'] = 'Datum členství';
+$lang['Tags.contact.email'] = 'E-mail';
+$lang['Tags.contact.zip'] = 'Poštovní směrovací číslo';
+$lang['Tags.contact.country'] = 'Země';
+$lang['Tags.contact.state'] = 'Stát';
+$lang['Tags.contact.city'] = 'Město';
+$lang['Tags.contact.address2'] = 'Adresa 2';
+$lang['Tags.contact.address1'] = 'Adresa 1';
+$lang['Tags.contact.company'] = 'Název společnosti';
+$lang['Tags.contact.last_name'] = 'Příjmení';
+$lang['Tags.contact.first_name'] = 'Jméno';
+$lang['Tags.client.status'] = 'Stav';
+$lang['Tags.client.id_code'] = 'ID';
+
