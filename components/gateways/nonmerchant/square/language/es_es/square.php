c9d3f22c79c7fe905621dbc09a0ecdf7072e139c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:39 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/square/language/es_es/square.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Square
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Square.name'] = 'Cuadrado';
+$lang['Square.description'] = 'Square, Inc. es una empresa de servicios financieros, agregadores de servicios comerciales y pagos móviles con sede en San Francisco, California. Tienen una fuerte base de consumidores en pequeñas empresas';
+$lang['Square.application_id'] = 'Identificación de la solicitud';
+$lang['Square.access_token'] = 'Ficha de acceso';
+$lang['Square.location_id'] = 'Ubicación ID';
+$lang['Square.buildprocess.submit'] = 'Pagar con Square';
+$lang['Square.!error.application_id.valid'] = 'Debe introducir un ID de solicitud válido.';
+$lang['Square.!error.access_token.valid'] = 'Debe introducir un código de acceso válido.';
+$lang['Square.!error.location_id.valid'] = 'Debe introducir un ID de ubicación válido.';
+
