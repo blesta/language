37e966504d8b03b50209b8755a8fa440f8bb8544
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:27 -0800

    Updated it_it language files

---
+++ components/messengers/twilio/language/it_it/twilio.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Twilio
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Twilio.configuration_fields.token'] = 'Gettone';
+$lang['Twilio.configuration_fields.sid'] = 'SID del conto';
+$lang['Twilio.configuration_fields.phone_number'] = 'Numero di telefono';
+$lang['Twilio.description'] = 'Inviare notifiche via SMS utilizzando Twilio.';
+$lang['Twilio.name'] = 'Twilio';
+
