b120839a962e46bf5c635a6cbe72aba2027abcf5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:41 -0800

    Updated de_de language files

---
+++ plugins/extension_generator/language/de_de/admin_merchant.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Admin Merchant
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMerchant.features.confirm'] = 'Nächste - Bestätigung';
+$lang['AdminMerchant.features.heading_supported_features'] = 'Unterstützte Funktionen';
+$lang['AdminMerchant.fields.features'] = 'Weiter - Unterstützte Funktionen';
+$lang['AdminMerchant.fields.text_remove'] = 'entfernen';
+$lang['AdminMerchant.fields.text_options'] = 'Optionen';
+$lang['AdminMerchant.fields.field_row_add'] = 'Konfigurationsfeld hinzufügen';
+$lang['AdminMerchant.fields.placeholder_label'] = 'z.B. Konfigurationsfeld';
+$lang['AdminMerchant.fields.placeholder_name'] = 'z.B. Konfiguration_Feld';
+$lang['AdminMerchant.fields.tooltip_encryptable'] = 'ob dieses Feld in der Datenbank verschlüsselt werden soll';
+$lang['AdminMerchant.fields.tooltip_tooltip'] = 'Der Text des Tooltips, der neben dem Feld angezeigt werden soll (leer lassen, um keinen Tooltip zu haben)';
+$lang['AdminMerchant.fields.tooltip_type'] = 'Die Art des zu erstellenden Feldes (Kontrollkästchen, Text, usw.)';
+$lang['AdminMerchant.fields.tooltip_label'] = 'Der Anzeigename des Feldes';
+$lang['AdminMerchant.fields.tooltip_name'] = 'Der Name des Feldes in der Codebasis';
+$lang['AdminMerchant.fields.encryptable'] = 'Verschlüsselbar';
+$lang['AdminMerchant.fields.tooltip'] = 'Tooltip-Text';
+$lang['AdminMerchant.fields.type'] = 'Typ';
+$lang['AdminMerchant.fields.label'] = 'Etikett';
+$lang['AdminMerchant.fields.name'] = 'Name';
+$lang['AdminMerchant.fields.heading_fields'] = 'Konfiguration Felder';
+$lang['AdminMerchant.basic.features'] = 'Weiter - Unterstützte Funktionen';
+$lang['AdminMerchant.basic.fields'] = 'Weiter - Konfigurationsfelder';
+$lang['AdminMerchant.basic.placeholder_author_url'] = 'z.B. https://blesta.com/';
+$lang['AdminMerchant.basic.placeholder_author_name'] = 'z.B. Blesta';
+$lang['AdminMerchant.basic.placeholder_currencies'] = 'z.B. USD,EUR,JPY';
+$lang['AdminMerchant.basic.placeholder_signup_url'] = 'z.B. https://dashboard.stripe.com/register';
+$lang['AdminMerchant.basic.tooltip_currencies'] = 'Eine durch Kommata getrennte Liste der unterstützten Währungen (z. B. USD, JPY, EUR)';
+$lang['AdminMerchant.basic.tooltip_signup_url'] = 'Die URL zur Anmeldeseite des Gateways';
+$lang['AdminMerchant.basic.tooltip_logo'] = 'Das Logo wird in der Plugin-Liste angezeigt. Die Bilder werden nicht in der Größe verändert, daher sind die idealen Abmessungen 150x70';
+$lang['AdminMerchant.basic.tooltip_description'] = 'Die in der Plugin-Liste angezeigte Beschreibung';
+$lang['AdminMerchant.basic.text_remove'] = 'entfernen';
+$lang['AdminMerchant.basic.author_row_add'] = 'Autor hinzufügen';
+$lang['AdminMerchant.basic.text_options'] = 'Optionen';
+$lang['AdminMerchant.basic.author_url'] = 'Autor URL';
+$lang['AdminMerchant.basic.author_name'] = 'Name des Autors';
+$lang['AdminMerchant.basic.currencies'] = 'Währungen';
+$lang['AdminMerchant.basic.signup_url'] = 'Anmelde-URL';
+$lang['AdminMerchant.basic.logo'] = 'Logo';
+$lang['AdminMerchant.basic.description'] = 'Beschreibung';
+$lang['AdminMerchant.basic.heading_authors'] = 'Autoren';
+$lang['AdminMerchant.basic.heading_basic'] = 'Grundlegende Informationen';
+$lang['AdminMerchant.index.boxtitle_extension_generator'] = 'Erweiterung Generator - Merchant Gateway';
+$lang['AdminMerchant.index.page_title'] = 'Erweiterung Generator - %1$s';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_ach_offsite'] = 'Automatisierte Clearing-House-Verarbeitung unter Verwendung von Berechtigungsnachweisen, die vom Verarbeiter außerhalb des Unternehmens gespeichert werden.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_ach'] = 'Automatisiertes Clearing House-Verfahren unter Verwendung von nicht gespeicherten oder in Blesta gespeicherten Anmeldeinformationen.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc_form'] = 'Damit kann ein Gateway benutzerdefiniertes HTML für Kreditkartenformulare bereitstellen.  Dies ist für die Unterstützung von Offsite-Kreditkarten gedacht, die iframes verwenden oder zusätzliche Felder benötigen können.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc_offsite'] = 'Kreditkartenabwicklung unter Verwendung von Anmeldeinformationen, die vom Verarbeiter außerhalb des Unternehmens gespeichert werden.';
+$lang['AdminMerchant.getsupportedfeatures.tooltip_cc'] = 'Kreditkartenabwicklung mit nicht gespeicherten oder in Blesta gespeicherten Zugangsdaten.';
+$lang['AdminMerchant.getsupportedfeatures.ach_offsite'] = 'Offsite ACH';
+$lang['AdminMerchant.getsupportedfeatures.ach'] = 'ACH';
+$lang['AdminMerchant.getsupportedfeatures.cc_form'] = 'Benutzerdefiniertes Kreditkartenformular';
+$lang['AdminMerchant.getsupportedfeatures.cc_offsite'] = 'Offsite-Kreditkarte';
+$lang['AdminMerchant.getsupportedfeatures.cc'] = 'Kreditkarte';
+
