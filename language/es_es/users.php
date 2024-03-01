f3150b5e292b74e70f5196e9a233bd1f6e07f0bd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:47 -0800

    Updated es_es language files

---
+++ language/es_es/users.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Users
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Users.!error.username.empty'] = 'Por favor, introduzca un nombre de usuario.';
+$lang['Users.!error.username.unique'] = 'Ese nombre de usuario ya está cogido.';
+$lang['Users.!error.current_password.matches'] = 'Contraseña inválida.';
+$lang['Users.!error.new_password.format'] = 'Introduzca una contraseña de al menos 6 caracteres.';
+$lang['Users.!error.new_password.matches'] = 'Las contraseñas no coinciden.';
+$lang['Users.!error.two_factor_mode.format'] = 'Modo de dos factores no válido.';
+$lang['Users.!error.two_factor_key.format'] = 'Clave de dos factores no válida.';
+$lang['Users.!error.username.auth'] = 'No se han encontrado coincidencias para esa combinación de usuario/contraseña.';
+$lang['Users.!error.otp.auth'] = 'La contraseña de un solo uso introducida no es válida. La longitud máxima es de 16 caracteres';
+$lang['Users.!error.user_id.exists'] = 'ID de usuario no válida.';
+$lang['Users.!error.username.attempts'] = 'Se han detectado demasiados intentos de inicio de sesión fallidos.';
+$lang['Users.!error.username.company'] = 'No está autorizado a iniciar sesión en esta ubicación.';
+$lang['Users.!error.clients.exist'] = 'El usuario no puede ser eliminado porque hay al menos un cliente asignado al usuario.';
+$lang['Users.!error.recovery_email.format'] = 'Dirección de correo electrónico de recuperación no válida.';
+
