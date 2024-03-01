ddbef7d0a126b3bf508da5357aeb6f52d1dfa367
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:24 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_company_lookandfeel.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Admin Company Lookandfeel
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyLookandfeel.editaction.field_addsubmit'] = 'Upravit akci';
+$lang['AdminCompanyLookandfeel.editaction.placeholder_url'] = 'Stav';
+$lang['AdminCompanyLookandfeel.editaction.placeholder_name'] = 'Název balíčku';
+$lang['AdminCompanyLookandfeel.editaction.field_url'] = 'Adresa URL odkazu';
+$lang['AdminCompanyLookandfeel.editaction.field_name'] = 'Název';
+$lang['AdminCompanyLookandfeel.editaction.field_location'] = 'Umístění';
+$lang['AdminCompanyLookandfeel.editaction.boxtitle_editaction'] = 'Upravit akci';
+$lang['AdminCompanyLookandfeel.editaction.page_title'] = 'Nastavení > Společnost > Vzhled a funkce > Upravit akci';
+$lang['AdminCompanyLookandfeel.addaction.field_addsubmit'] = 'Přidat akci';
+$lang['AdminCompanyLookandfeel.addaction.placeholder_url'] = 'Stav';
+$lang['AdminCompanyLookandfeel.addaction.placeholder_name'] = 'Název balíčku';
+$lang['AdminCompanyLookandfeel.addaction.field_url'] = 'Adresa URL odkazu';
+$lang['AdminCompanyLookandfeel.addaction.field_name'] = 'Název';
+$lang['AdminCompanyLookandfeel.addaction.field_location'] = 'Umístění';
+$lang['AdminCompanyLookandfeel.addaction.boxtitle_addaction'] = 'Přidat akci';
+$lang['AdminCompanyLookandfeel.addaction.page_title'] = 'Nastavení > Společnost > Vzhled a funkce > Přidat akci';
+$lang['AdminCompanyLookandfeel.actions.confirm_delete'] = 'Opravdu chcete tuto akci odstranit?';
+$lang['AdminCompanyLookandfeel.actions.option_delete'] = 'Odstranit';
+$lang['AdminCompanyLookandfeel.actions.option_edit'] = 'Upravit';
+$lang['AdminCompanyLookandfeel.actions.no_results'] = 'Pro tuto lokalitu nejsou k dispozici žádné akce.';
+$lang['AdminCompanyLookandfeel.actions.heading_options'] = 'Možnosti';
+$lang['AdminCompanyLookandfeel.actions.heading_url'] = 'ADRESA URL';
+$lang['AdminCompanyLookandfeel.actions.heading_name'] = 'Název';
+$lang['AdminCompanyLookandfeel.actions.categorylink_addaction'] = 'Přidat akci';
+$lang['AdminCompanyLookandfeel.actions.category_nav_public'] = 'Veřejná navigace';
+$lang['AdminCompanyLookandfeel.actions.category_nav_client'] = 'Navigace klienta';
+$lang['AdminCompanyLookandfeel.actions.category_nav_staff'] = 'Navigace pro zaměstnance';
+$lang['AdminCompanyLookandfeel.actions.boxtitle_actions'] = 'Vlastní akce';
+$lang['AdminCompanyLookandfeel.actions.page_title'] = 'Nastavení > Společnost > Vzhled a funkce > Vlastní akce';
+$lang['AdminCompanyLookandfeel.navigation.text_submit'] = 'Uložit navigaci';
+$lang['AdminCompanyLookandfeel.navigation.no_results_actions'] = 'Pro tuto lokalitu nejsou k dispozici žádné akce.';
+$lang['AdminCompanyLookandfeel.navigation.text_link'] = 'odkaz';
+$lang['AdminCompanyLookandfeel.navigation.field_subitem'] = 'Podpoložka';
+$lang['AdminCompanyLookandfeel.actions.option_add_action'] = 'Přidání vlastní akce';
+$lang['AdminCompanyLookandfeel.actions.option_remove'] = 'Odstranění stránky';
+$lang['AdminCompanyLookandfeel.actions.option_add'] = 'Přidat';
+$lang['AdminCompanyLookandfeel.navigation.heading_options'] = 'Možnosti';
+$lang['AdminCompanyLookandfeel.navigation.heading_action'] = 'Akce';
+$lang['AdminCompanyLookandfeel.navigation.heading_navigation'] = 'Navigační menu';
+$lang['AdminCompanyLookandfeel.navigation.category_nav_public'] = 'Veřejná navigace';
+$lang['AdminCompanyLookandfeel.navigation.category_nav_client'] = 'Navigace klienta';
+$lang['AdminCompanyLookandfeel.navigation.category_nav_staff'] = 'Navigace pro zaměstnance';
+$lang['AdminCompanyLookandfeel.navigation.boxtitle_navigation'] = 'Navigace';
+$lang['AdminCompanyLookandfeel.navigation.page_title'] = 'Nastavení > Společnost > Vzhled a funkce > Navigace';
+$lang['AdminCompanyLookandfeel.customize.text_submit'] = 'Uložit';
+$lang['AdminCompanyLookandfeel.customize.field.client_url'] = 'Logo v záhlaví';
+$lang['AdminCompanyLookandfeel.customize.field.client_logo'] = 'Logo v záhlaví';
+$lang['AdminCompanyLookandfeel.customize.field.client_type_url'] = 'Nastavení adresy URL loga';
+$lang['AdminCompanyLookandfeel.customize.field.client_type_logo'] = 'Nahrát logo';
+$lang['AdminCompanyLookandfeel.customize.field.admin_url'] = 'Logo v záhlaví';
+$lang['AdminCompanyLookandfeel.customize.field.admin_logo'] = 'Logo v záhlaví';
+$lang['AdminCompanyLookandfeel.customize.field.admin_type_url'] = 'Nastavení adresy URL loga';
+$lang['AdminCompanyLookandfeel.customize.field.admin_type_logo'] = 'Nahrát logo';
+$lang['AdminCompanyLookandfeel.customize.heading_custom_logo'] = 'Vlastní logo';
+$lang['AdminCompanyLookandfeel.customize.box_title'] = 'Přizpůsobení';
+$lang['AdminCompanyLookandfeel.customize.page_title'] = 'Nastavení > Společnost > Vzhled a funkce > Přizpůsobit';
+$lang['AdminCompanyLookandfeel.layout.text_submit'] = 'Uložit';
+$lang['AdminCompanyLookandfeel.layout.text_widget_preview_note'] = 'Z bezpečnostních důvodů nebyl vygenerován náhled tohoto widgetu.';
+$lang['AdminCompanyLookandfeel.layout.text_cards_note'] = 'Přetažením karet změníte jejich pořadí, kliknutím na ně změníte barvu a kliknutím na zaškrtávací políčko kartu povolíte nebo zakážete. Barvu karet generovaných pomocí HTML nelze změnit.';
+$lang['AdminCompanyLookandfeel.layout.text_html_generated'] = 'Generování HTML';
+$lang['AdminCompanyLookandfeel.layout.field.background_url'] = 'Základní adresa URL';
+$lang['AdminCompanyLookandfeel.layout.field.text_color'] = 'Barva textu';
+$lang['AdminCompanyLookandfeel.layout.box_title'] = 'Rozložení';
+$lang['AdminCompanyLookandfeel.layout.page_title'] = 'Nastavení > Společnost > Vzhled a funkce > Rozložení';
+$lang['AdminCompanyLookandfeel.template.text_submit'] = 'Uložit';
+$lang['AdminCompanyLookandfeel.template.text_client_view_override'] = 'Umožňuje přepsat šablonu klienta z adresy URL';
+$lang['AdminCompanyLookandfeel.template.text_admin_view_dir'] = 'Šablona pro správce';
+$lang['AdminCompanyLookandfeel.template.text_client_view_dir'] = 'Šablona klienta';
+$lang['AdminCompanyLookandfeel.template.box_title'] = 'Šablona';
+$lang['AdminCompanyLookandfeel.template.page_title'] = 'Nastavení > Společnost > Vzhled a funkce > Šablona';
+$lang['AdminCompanyLookandfeel.!tooltip.client_view_override'] = 'Umožňuje klientovi přepsat šablonu z adresy URL definováním parametru "bltemplate", kde hodnotou je název adresáře šablony, např. "/?bltemplate=bootstrap".';
+$lang['AdminCompanyLookandfeel.!tooltip.disabled_location'] = 'Umístění akce nelze po vytvoření změnit.';
+$lang['AdminCompanyLookandfeel.!tooltip.action_url'] = 'Tato cesta může být relativní (např. clients/add) nebo absolutní (např. %1$sclients/add/ nebo https://blesta.com). Relativní cesty budou vycházet z rozhraní, ve kterém se zobrazují.  Například clients/add bude odkazovat na %1$sclients/add.';
+$lang['AdminCompanyLookandfeel.!tooltip.action_name_language'] = 'Text odkazu';
+$lang['AdminCompanyLookandfeel.!tooltip.action_name'] = 'Chcete-li vytvořit vícejazyčné akce, můžete zadat definici jazyka v jazykovém souboru _global.php. V případě potřeby přejmenujte soubor _global-example.php na _global.php. Tyto soubory najdete v adresáři %1$s a v každém dalším jazykovém adresáři.  Příklad kódu pro definici:';
+$lang['AdminCompanyLookandfeel.!success.logo_updated'] = 'Vlastní logo bylo úspěšně aktualizováno.';
+$lang['AdminCompanyLookandfeel.!success.action_deleted'] = 'Akce byla úspěšně odstraněna.';
+$lang['AdminCompanyLookandfeel.!success.action_updated'] = 'Akce byla úspěšně aktualizována.';
+$lang['AdminCompanyLookandfeel.!success.action_created'] = 'Akce byla úspěšně vytvořena a lze ji přidat do části Vzhled a funkce > Navigace.';
+$lang['AdminCompanyLookandfeel.!success.navigation_updated'] = 'Navigace byla úspěšně aktualizována.';
+$lang['AdminCompanyLookandfeel.!success.layout_updated'] = 'Rozložení bylo úspěšně aktualizováno.';
+$lang['AdminCompanyLookandfeel.!success.template_updated'] = 'Šablony byly úspěšně aktualizovány.';
+
