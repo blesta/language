1d90c3ff8a275ce921ae67f8ba637874bc88ae3c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:55 -0800

    Updated it_it language files

---
+++ plugins/thesslstore/language/it_it/thesslstore_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Thesslstore Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['TheSSLStorePlugin.getCronTasks.tss_expiration_reminder_desc'] = 'Un promemoria verrà inviato 30 giorni prima della data di scadenza del certificato.';
+$lang['TheSSLStorePlugin.getCronTasks.tss_expiration_reminder_name'] = 'Promemoria della scadenza del certificato SSL';
+$lang['TheSSLStorePlugin.name'] = 'Il pluginSSLStore';
+
