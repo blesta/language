baa769f10b4829d6d66b048284428f45eff9e21b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:11 -0800

    Updated fr_fr language files

---
+++ plugins/feed_reader/language/fr_fr/admin_main.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.feed_added'] = 'Le flux a été ajouté avec succès.';
+$lang['AdminMain.!success.feed_refreshed'] = 'Le flux a été actualisé avec succès.';
+$lang['AdminMain.!success.feed_removed'] = 'L\'aliment a été retiré avec succès.';
+$lang['AdminMain.index.boxtitle_feed'] = 'Lecteur de flux';
+$lang['AdminMain.index.no_results'] = 'Il n\'y a pas de résultats d\'alimentation.';
+$lang['AdminMain.index.last_updated'] = 'Dernière mise à jour : %1$s';
+$lang['AdminMain.add.boxtitle_feed'] = 'Ajouter un flux';
+$lang['AdminMain.add.field_url'] = 'URL';
+$lang['AdminMain.add.submit_add'] = 'Ajouter un flux';
+$lang['AdminMain.add.submit_cancel'] = 'Annuler';
+$lang['AdminMain.settings.no_feeds'] = 'Il n\'y a pas de flux.';
+$lang['AdminMain.settings.add_feed'] = 'Ajouter un flux';
+$lang['AdminMain.settings.submit_cancel'] = 'Retour à l\'alimentation';
+$lang['AdminMain.settings.heading_url'] = 'URL';
+$lang['AdminMain.settings.heading_lastupdated'] = 'Dernière mise à jour';
+$lang['AdminMain.settings.heading_options'] = 'Options';
+$lang['AdminMain.settings.option_refresh'] = 'Rafraîchir';
+$lang['AdminMain.settings.option_remove'] = 'Retirer';
+
