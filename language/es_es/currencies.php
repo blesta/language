800635c9e9ea1a0c4b7880da32412ee64706e04c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:21 -0800

    Updated es_es language files

---
+++ language/es_es/currencies.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Currencies
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Currencies.!error.code.length'] = 'El código de divisa debe constar de 3 caracteres alfabéticos, tal como se define en la norma ISO 4217.';
+$lang['Currencies.!error.code.exists'] = 'Una divisa con código %1$s e ID de empresa %2$s ya está ocupada.';
+$lang['Currencies.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['Currencies.!error.format.format'] = 'Formato de moneda no válido.';
+$lang['Currencies.!error.precision.format'] = 'La precisión monetaria debe ser un número comprendido entre 0 y 4, ambos inclusive.';
+$lang['Currencies.!error.prefix.length'] = 'El prefijo de la moneda no puede tener más de 10 caracteres.';
+$lang['Currencies.!error.suffix.length'] = 'El sufijo monetario no puede tener más de 10 caracteres.';
+$lang['Currencies.!error.exchange_rate.format'] = 'El tipo de cambio debe ser un número.';
+$lang['Currencies.!error.exchange_updated.format'] = 'La fecha de actualización del intercambio está en un formato de fecha no válido.';
+$lang['Currencies.!error.currency_code.in_use'] = 'La moneda %1$s está actualmente en uso y no puede ser eliminada.';
+$lang['Currencies.!error.currency_code.is_default'] = 'La moneda %1$s es la moneda por defecto y no puede ser eliminada.';
+$lang['Currencies.!error.processor.invalid'] = 'Procesador de tipos de cambio no válido.';
+$lang['Currencies.!error.processor.empty'] = 'No se ha establecido ningún procesador de tipos de cambio.';
+
