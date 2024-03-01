3e3fca8baf532912f40e85ac0551160dc352b14d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:07 -0800

    Updated de_de language files

---
+++ language/de_de/messenger_manager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Messenger Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MessengerManager.!error.version.valid'] = 'Version ist ungültig.';
+$lang['MessengerManager.!error.name.valid'] = 'Der Name ist ungültig.';
+$lang['MessengerManager.!error.dir.valid'] = 'Das Verzeichnis ist ungültig.';
+$lang['MessengerManager.!error.company_id.valid'] = 'Die Unternehmens-ID ist ungültig.';
+$lang['MessengerManager.!error.messenger.missing'] = 'Für den angegebenen Typ ist kein Bote konfiguriert.';
+
