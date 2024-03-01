7be262460d18148a6ea02a8f0c8246f78b97f90a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:03 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/coingate/language/cs_cz/coingate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Coingate
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Coingate.!error.failed.response'] = 'Transakci nebylo možné zpracovat.';
+$lang['Coingate.!error.payment.expired'] = 'Platnost transakce vypršela a nemohla být zpracována.';
+$lang['Coingate.!error.payment.canceled'] = 'Transakce je zrušena a nemohla být zpracována.';
+$lang['Coingate.!error.payment.invalid'] = 'Transakce je neplatná a nemohla být zpracována.';
+$lang['Coingate.!error.api_secret.empty'] = 'API Secret nemůže být prázdné';
+$lang['Coingate.!error.api_key.empty'] = 'Klíč API nemůže být prázdný';
+$lang['Coingate.!error.app_id.empty'] = 'ID APP nesmí být prázdné.';
+$lang['Coingate.receive_currency.eur'] = 'Euro €';
+$lang['Coingate.receive_currency.btc'] = 'Bitcoin ฿';
+$lang['Coingate.receive_currency.usd'] = 'Americké dolary $';
+$lang['Coingate.buildprocess.submit'] = 'CoinGate.com';
+$lang['Coingate.environment_note'] = 'Povolte "sandbox" pro testování na sandbox.coingate.com. Vezměte prosím na vědomí, že pro režim sandbox musíte vygenerovat samostatné pověření API na sandbox.coingate.com. Pověření API vygenerovaná na webu coingate.com nebudou pro režim sandbox fungovat.';
+$lang['Coingate.receive_currency_note'] = 'Měna, kterou chcete obdržet při výběru u CoinGate.
+										   Vezměte prosím na vědomí, že pokud zvolíte EUR nebo USD, budete požádáni o ověření.
+										   před provedením výběru u CoinGate.';
+$lang['Coingate.receive_currency'] = 'Měna výplaty';
+$lang['Coingate.environment.live'] = 'živě';
+$lang['Coingate.environment.sandbox'] = 'sandbox';
+$lang['Coingate.coingate_environment'] = 'Prostředí CoinGate';
+$lang['Coingate.api_secret'] = 'Tajemství API';
+$lang['Coingate.api_key'] = 'Klíč API';
+$lang['Coingate.app_id'] = 'ID APLIKACE APP';
+$lang['Coingate.description'] = 'Plaťte Bitcoinem nebo Altcoiny přes CoinGate.com';
+$lang['Coingate.name'] = 'CoinGate.com';
+
