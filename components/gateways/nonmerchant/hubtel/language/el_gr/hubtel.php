d18b8b35c6f5390a69bbeffd80188989b6f2d0c3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:29 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/hubtel/language/el_gr/hubtel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Hubtel
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Hubtel.!error.client_secret.valid'] = 'Πρέπει να εισαγάγετε ένα έγκυρο Μυστικό πελάτη.';
+$lang['Hubtel.!error.client_id.valid'] = 'Πρέπει να εισαγάγετε ένα έγκυρο αναγνωριστικό πελάτη.';
+$lang['Hubtel.buildprocess.submit'] = 'Πληρώστε με το Hubtel';
+$lang['Hubtel.client_secret'] = 'Μυστικό πελάτη';
+$lang['Hubtel.client_id'] = 'Αναγνωριστικό πελάτη';
+$lang['Hubtel.description'] = 'Η Hubtel είναι η κορυφαία πλατφόρμα συγκέντρωσης και πληρωμών κινητής προστιθέμενης αξίας της Γκάνας';
+$lang['Hubtel.name'] = 'Hubtel';
+
