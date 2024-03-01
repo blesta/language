8e4b471bc98aadbd4d43858baf3077768c5fa6c1
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:34 -0800

    Updated el_gr language files

---
+++ core/Util/Filters/language/el_gr/quotation_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Quotation Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.quotation_filters.any'] = 'Οποιοδήποτε';
+$lang['Util.filters.quotation_filters.field_quotation_number'] = 'Προσφορά #';
+$lang['Util.filters.quotation_filters.field_currency'] = 'Νόμισμα';
+$lang['Util.filters.quotation_filters.field_quotation_line'] = 'Περιεχόμενο γραμμής';
+
