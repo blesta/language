6ef9c0ab0358c8fcc41caf7d31bc2e197e9c679e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:53 -0800

    Updated el_gr language files

---
+++ plugins/download_manager/language/el_gr/admin_main.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.category_added'] = 'Η κατηγορία έχει δημιουργηθεί με επιτυχία.';
+$lang['AdminMain.!success.category_updated'] = 'Η κατηγορία ενημερώθηκε επιτυχώς.';
+$lang['AdminMain.!success.category_deleted'] = 'Η κατηγορία διαγράφηκε επιτυχώς.';
+$lang['AdminMain.!success.file_added'] = 'Το αρχείο προστέθηκε με επιτυχία.';
+$lang['AdminMain.!success.file_updated'] = 'Το αρχείο ενημερώθηκε με επιτυχία.';
+$lang['AdminMain.!success.file_deleted'] = 'Το αρχείο διαγράφηκε επιτυχώς.';
+$lang['AdminMain.!success.url_deleted'] = 'Η στατική διεύθυνση URL διαγράφηκε επιτυχώς.';
+$lang['AdminMain.!success.url_added'] = 'Η στατική διεύθυνση URL προστέθηκε επιτυχώς.';
+$lang['AdminMain.!success.url_updated'] = 'Η στατική διεύθυνση URL ενημερώθηκε με επιτυχία.';
+$lang['AdminMain.!tooltip.path_to_file'] = 'Εισάγετε την απόλυτη διαδρομή του αρχείου στο σύστημα αρχείων.';
+$lang['AdminMain.!tooltip.category_type'] = 'Η διεύθυνση URL θα παραπέμπει στο τελευταίο αρχείο που μεταφορτώθηκε σε αυτή την κατηγορία.';
+$lang['AdminMain.!text.root_directory'] = 'Αρχικός κατάλογος';
+$lang['AdminMain.!text.open_parenthesis'] = '(';
+$lang['AdminMain.!text.forward_slash'] = '/';
+$lang['AdminMain.!text.closed_parenthesis'] = ')';
+$lang['AdminMain.modal.delete_file'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτό το αρχείο;';
+$lang['AdminMain.modal.delete_category'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την κατηγορία; Όλες οι υποκατηγορίες και τα αρχεία αυτής της κατηγορίας θα μεταφερθούν στη μητρική κατηγορία.';
+$lang['AdminMain.modal.delete_url'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτό το στατικό URL;';
+$lang['AdminMain.files.page_title'] = 'Διαχείριση λήψεων > Διαχείριση';
+$lang['AdminMain.files.boxtitle_downloadmanager'] = 'Διαχείριση λήψης';
+$lang['AdminMain.files.tab_files'] = 'Αρχεία';
+$lang['AdminMain.files.tab_urls'] = 'Στατικές διευθύνσεις URL';
+$lang['AdminMain.files.add_download'] = 'Προσθέστε λήψη εδώ';
+$lang['AdminMain.files.add_category'] = 'Προσθέστε κατηγορία εδώ';
+$lang['AdminMain.files.go_back'] = 'Ανεβείτε ένα επίπεδο';
+$lang['AdminMain.files.edit'] = 'Επεξεργασία';
+$lang['AdminMain.files.delete'] = 'Διαγραφή';
+$lang['AdminMain.files.no_downloads'] = 'Δεν υπάρχουν λήψεις σε αυτό το τμήμα.';
+$lang['AdminMain.urls.page_title'] = 'Διαχείριση λήψεων > Διαχείριση';
+$lang['AdminMain.urls.boxtitle_downloadmanager'] = 'Διαχείριση λήψης';
+$lang['AdminMain.urls.add_url'] = 'Προσθήκη στατικής διεύθυνσης URL';
+$lang['AdminMain.urls.tab_files'] = 'Αρχεία';
+$lang['AdminMain.urls.tab_urls'] = 'Στατικές διευθύνσεις URL';
+$lang['AdminMain.urls.heading_url'] = 'URL';
+$lang['AdminMain.urls.heading_file'] = 'Αρχείο';
+$lang['AdminMain.urls.heading_link'] = 'Σύνδεσμος';
+$lang['AdminMain.urls.heading_options'] = 'Επιλογές';
+$lang['AdminMain.urls.edit'] = 'Επεξεργασία';
+$lang['AdminMain.urls.delete'] = 'Διαγραφή';
+$lang['AdminMain.urls.latest_file'] = 'Τελευταίο αρχείο από <strong>%1$s</strong>';
+$lang['AdminMain.urls.no_urls'] = 'Δεν υπάρχουν στατικές διευθύνσεις URL σε αυτό το τμήμα.';
+$lang['AdminMain.add.page_title'] = 'Διαχείριση λήψης > Προσθήκη λήψης';
+$lang['AdminMain.add.boxtitle_root'] = 'Προσθέστε Λήψη στο %1$s';
+$lang['AdminMain.add.boxtitle_add'] = 'Προσθήκη λήψης στην κατηγορία [%1$s]';
+$lang['AdminMain.add.field_public'] = 'Δημόσια Διαθέσιμο';
+$lang['AdminMain.add.field_logged_in'] = 'Πρέπει να είστε συνδεδεμένοι';
+$lang['AdminMain.add.field_name'] = 'Όνομα';
+$lang['AdminMain.add.field_available_to_client_groups'] = 'Διαθέσιμο σε ομάδες πελατών';
+$lang['AdminMain.add.field_available_to_packages'] = 'Διαθέσιμο σε πακέτα';
+$lang['AdminMain.add.text_clientgroups'] = 'Επιλεγμένες ομάδες πελατών';
+$lang['AdminMain.add.text_packagegroups'] = 'Επιλεγμένα πακέτα';
+$lang['AdminMain.add.text_availableclientgroups'] = 'Διαθέσιμες ομάδες πελατών';
+$lang['AdminMain.add.text_availablepackages'] = 'Διαθέσιμα πακέτα';
+$lang['AdminMain.add.field_upload'] = 'Ανέβασμα αρχείου';
+$lang['AdminMain.add.field_path'] = 'Καθορίστε τη διαδρομή προς το αρχείο';
+$lang['AdminMain.add.field_file'] = 'Αρχείο';
+$lang['AdminMain.add.field_file_name'] = 'Διαδρομή προς το αρχείο';
+$lang['AdminMain.add.submit_add'] = 'Προσθέστε Λήψη';
+$lang['AdminMain.add.submit_cancel'] = 'Ακύρωση';
+$lang['AdminMain.addurl.page_title'] = 'Διαχείριση λήψης > Προσθήκη στατικής διεύθυνσης URL';
+$lang['AdminMain.addurl.boxtitle_add'] = 'Προσθήκη στατικής διεύθυνσης URL';
+$lang['AdminMain.addurl.field_url'] = 'Όνομα URL';
+$lang['AdminMain.addurl.field_file'] = 'Αρχείο';
+$lang['AdminMain.addurl.field_category'] = 'Κατηγορία';
+$lang['AdminMain.addurl.submit_add'] = 'Προσθήκη URL';
+$lang['AdminMain.addurl.submit_cancel'] = 'Ακύρωση';
+$lang['AdminMain.edit.page_title'] = 'Διαχείριση λήψης > Προσθήκη λήψης';
+$lang['AdminMain.edit.boxtitle_edit'] = 'Λήψη ενημέρωσης';
+$lang['AdminMain.edit.field_public'] = 'Δημόσια Διαθέσιμο';
+$lang['AdminMain.edit.field_logged_in'] = 'Πρέπει να είστε συνδεδεμένοι';
+$lang['AdminMain.edit.field_name'] = 'Όνομα';
+$lang['AdminMain.edit.field_available_to_client_groups'] = 'Διαθέσιμο σε ομάδες πελατών';
+$lang['AdminMain.edit.field_available_to_packages'] = 'Διαθέσιμο σε πακέτα';
+$lang['AdminMain.edit.text_clientgroups'] = 'Επιλεγμένες ομάδες πελατών';
+$lang['AdminMain.edit.text_packagegroups'] = 'Επιλεγμένα πακέτα';
+$lang['AdminMain.edit.text_availableclientgroups'] = 'Διαθέσιμες ομάδες πελατών';
+$lang['AdminMain.edit.text_availablepackages'] = 'Διαθέσιμα πακέτα';
+$lang['AdminMain.edit.field_upload'] = 'Ανέβασμα αρχείου';
+$lang['AdminMain.edit.field_path'] = 'Καθορίστε τη διαδρομή προς το αρχείο';
+$lang['AdminMain.edit.field_file'] = 'Αρχείο';
+$lang['AdminMain.edit.field_file_name'] = 'Διαδρομή προς το αρχείο';
+$lang['AdminMain.edit.submit_edit'] = 'Λήψη ενημέρωσης';
+$lang['AdminMain.edit.submit_cancel'] = 'Ακύρωση';
+$lang['AdminMain.editurl.page_title'] = 'Διαχείριση λήψης > Επεξεργασία στατικής διεύθυνσης URL';
+$lang['AdminMain.editurl.boxtitle_add'] = 'Επεξεργασία στατικής διεύθυνσης URL';
+$lang['AdminMain.editurl.field_url'] = 'Όνομα URL';
+$lang['AdminMain.editurl.field_file'] = 'Αρχείο';
+$lang['AdminMain.editurl.field_category'] = 'Κατηγορία';
+$lang['AdminMain.editurl.submit_add'] = 'Επεξεργασία URL';
+$lang['AdminMain.editurl.submit_cancel'] = 'Ακύρωση';
+$lang['AdminMain.addcategory.page_title'] = 'Διαχείριση λήψεων > Προσθήκη κατηγορίας';
+$lang['AdminMain.addcategory.boxtitle_root'] = 'Προσθήκη κατηγορίας στο %1$s';
+$lang['AdminMain.addcategory.boxtitle_addcategory'] = 'Προσθήκη κατηγορίας στην κατηγορία [%1$s]';
+$lang['AdminMain.addcategory.field_name'] = 'Όνομα';
+$lang['AdminMain.addcategory.field_description'] = 'Περιγραφή';
+$lang['AdminMain.addcategory.submit_add'] = 'Δημιουργία κατηγορίας';
+$lang['AdminMain.addcategory.submit_cancel'] = 'Ακύρωση';
+$lang['AdminMain.editcategory.page_title'] = 'Διαχείριση λήψεων > Κατηγορία ενημέρωσης';
+$lang['AdminMain.editcategory.boxtitle_editcategory'] = 'Ενημέρωση κατηγορίας [%1$s]';
+$lang['AdminMain.editcategory.field_name'] = 'Όνομα';
+$lang['AdminMain.editcategory.field_description'] = 'Περιγραφή';
+$lang['AdminMain.editcategory.submit_edit'] = 'Κατηγορία ενημέρωσης';
+$lang['AdminMain.editcategory.submit_cancel'] = 'Ακύρωση';
+$lang['AdminMain.package_name'] = '%1$s (%2$s)';
+
