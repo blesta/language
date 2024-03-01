7ca601f1e35fc4f4f17d3eb2d00eb7172e8f0041
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:00 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/admin_company_emails.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_third_desc'] = 'Třetí a poslední oznámení zaslané klientovi po odstoupení od nezaplacené objednávky.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_third_name'] = 'Dny opuštěných objednávek (3. oznámení)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_second_desc'] = 'Druhá výpověď zaslaná klientovi po odstoupení od nezaplacené objednávky.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_second_name'] = 'Dny opuštěných objednávek (2. oznámení)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_first_desc'] = 'První oznámení zaslané klientovi po odstoupení od nezaplacené objednávky.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_first_name'] = 'Dny opuštěných objednávek (1. oznámení)';
+$lang['AdminCompanyEmails.templates.Order.affiliate_monthly_report_desc'] = 'Zpráva se seznamem registrací a souvisejících údajů za poslední měsíc.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_monthly_report_name'] = 'Affiliate Report (měsíční)';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_received_desc'] = 'Oznámení zaslané klientovi po podání žádosti o výplatu.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_received_name'] = 'Obdržená partnerská výplata';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_desc'] = 'Oznámení zaslané zaměstnancům při podání žádosti o výplatu.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_name'] = 'Žádost o výplatu partnerské platby';
+$lang['AdminCompanyEmails.templates.Order.received_mobile_desc'] = 'Oznámení odeslané při přijetí objednávky určené pro mobilní zařízení.';
+$lang['AdminCompanyEmails.templates.Order.received_mobile_name'] = 'Přijatá objednávka (mobilní telefon)';
+$lang['AdminCompanyEmails.templates.Order.received_desc'] = 'Oznámení odeslané po přijetí objednávky.';
+$lang['AdminCompanyEmails.templates.Order.received_name'] = 'Přijatá objednávka';
+
