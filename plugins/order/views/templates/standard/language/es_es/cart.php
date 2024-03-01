03daa47a476dae9c6a5885fd0d01ed8d16610fb4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:51 -0800

    Updated es_es language files

---
+++ plugins/order/views/templates/standard/language/es_es/cart.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cart
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cart.index.header_cart'] = 'Orden de revisión';
+$lang['Cart.index.table_description'] = 'Descripción';
+$lang['Cart.index.table_term'] = 'Plazo';
+$lang['Cart.index.table_qty'] = 'Cantidad';
+$lang['Cart.index.table_price'] = 'Precio';
+$lang['Cart.index.checkout_btn'] = 'Pedido';
+$lang['Cart.index.order_more_btn'] = 'Pedir más artículos';
+$lang['Cart.index.edit_item'] = 'Editar';
+$lang['Cart.index.remove_item'] = 'Eliminar';
+$lang['Cart.index.empty_cart'] = 'Tu carrito está vacío. Comience por <a href="%1$s">elegir un plan"</a>.';
+
