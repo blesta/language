9828b0dc9f0c6bb653bc931f3b4aac8d158127cc
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:09 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/admin_settings.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Admin Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSettings.settings.submit_cancel'] = 'Annullamento';
+$lang['AdminSettings.settings.submit_settings'] = 'Salva le impostazioni';
+$lang['AdminSettings.settings.text_inheritance'] = 'Le impostazioni dell\'affiliato della società vengono ereditate dai nuovi affiliati, ma in seguito vengono sovrascritte dalle impostazioni dei singoli affiliati.';
+$lang['AdminSettings.settings.zero_amount'] = 'L\'importo del prelievo è attualmente pari a zero, il che significa che gli affiliati non riceveranno fondi dai referral.';
+$lang['AdminSettings.settings.tooltip_signup_content'] = 'Il contenuto HTML che verrà visualizzato dai clienti nella pagina di affiliazione se non sono già registrati.';
+$lang['AdminSettings.settings.field_signup_content'] = 'Contenuto dell\'iscrizione';
+$lang['AdminSettings.settings.field_withdrawal_currency'] = 'Valuta di prelievo';
+$lang['AdminSettings.settings.field_max_withdrawal_amount'] = 'Importo massimo di prelievo';
+$lang['AdminSettings.settings.field_min_withdrawal_amount'] = 'Importo minimo di prelievo';
+$lang['AdminSettings.settings.tooltip_maturity_days'] = 'Il numero di giorni successivi al pagamento in cui un referral matura e diventa disponibile per il pagamento.';
+$lang['AdminSettings.settings.field_maturity_days'] = 'Giorni di maturazione';
+$lang['AdminSettings.settings.tooltip_order_recurring'] = 'Se i referenti saranno pagati per i servizi ordinati che si rinnovano.';
+$lang['AdminSettings.settings.field_order_recurring'] = 'Ordine ricorrente';
+$lang['AdminSettings.settings.tooltip_order_frequency'] = 'First per pagare i referral solo per il primo ordine che un cliente effettua utilizzando un link di riferimento, oppure Any per pagare i referral per ogni ordine che un cliente effettua utilizzando un link di riferimento.';
+$lang['AdminSettings.settings.field_order_frequency'] = 'Frequenza dell\'ordine';
+$lang['AdminSettings.settings.field_commission_amount'] = 'Importo della Commissione';
+$lang['AdminSettings.settings.tooltip_commission_type'] = 'Il metodo di calcolo utilizzato per determinare la commissione di referral.  Un valore fisso per ordine o una percentuale sul totale dell\'ordine.';
+$lang['AdminSettings.settings.field_commission_type'] = 'Tipo di Commissione';
+$lang['AdminSettings.settings.tooltip_cookie_tld'] = 'I giorni in cui un cookie di affiliazione rimane valido dopo che è stato seguito un link di riferimento.';
+$lang['AdminSettings.settings.field_cookie_tld'] = 'Durata del cookie (in giorni)';
+$lang['AdminSettings.settings.boxtitle_settings'] = 'Impostazioni generali';
+$lang['AdminSettings.!success.settings_updated'] = 'Le impostazioni di affiliazione sono state aggiornate.';
+
