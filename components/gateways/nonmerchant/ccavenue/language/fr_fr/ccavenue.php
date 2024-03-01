2055983b075ada2c6bb64bafa55ca363f68d461a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:30 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/ccavenue/language/fr_fr/ccavenue.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Ccavenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Ccavenue.name'] = 'Paiement CCAvenue';
+$lang['Ccavenue.description'] = 'Avec plus de 240 options de paiement reliant plus de 55 banques indiennes en temps réel, CCAvenue est l\'un des plus grands moteurs de débit direct de l\'Inde.';
+$lang['Ccavenue.merchant_id'] = 'Identifiant du commerçant';
+$lang['Ccavenue.merchant_id_note'] = 'Votre identifiant marchand est disponible dans votre compte CCAvenue sous Clés API.';
+$lang['Ccavenue.encryption_key'] = 'Clé de chiffrement';
+$lang['Ccavenue.encryption_key_note'] = 'Celle-ci est générée dans votre compte CCAvenue sous la rubrique Clés API.';
+$lang['Ccavenue.access_code'] = 'Code d\'accès';
+$lang['Ccavenue.access_code_note'] = 'Elle se trouve dans votre compte CCAvenue sous la rubrique Clés API.';
+$lang['Ccavenue.test_mode'] = 'Mode test';
+$lang['Ccavenue.test_mode_note'] = 'Si cette case est cochée, la passerelle enverra les demandes de paiement à l\'URL de test.';
+$lang['Ccavenue.buildprocess.submit'] = 'Payer via CCAvenue';
+$lang['Ccavenue.!error.merchant_id.valid'] = 'Veuillez saisir un identifiant marchand valide.';
+$lang['Ccavenue.!error.access_code.empty'] = 'Veuillez saisir un code d\'accès.';
+$lang['Ccavenue.!error.encryption_key.valid'] = 'Veuillez saisir une clé de cryptage.';
+$lang['Ccavenue.!error.pending.log'] = 'Statut du paiement reçu en attente de la passerelle.';
+$lang['Ccavenue.!error.delay.response'] = 'Nous vous remercions d\'avoir fait vos achats chez nous. Nous vous tiendrons au courant de l\'état de votre commande par e-mail.';
+$lang['Ccavenue.!error.security.response'] = 'Erreur de sécurité. Un accès illégal a été détecté.';
+$lang['Ccavenue.!error.authentication.response'] = 'Merci d\'avoir fait vos achats chez nous. Cependant, la transaction a été refusée. Veuillez refaire la transaction.';
+$lang['Ccavenue.!error.authentication.log'] = 'La transaction a été refusée par la banque.';
+
