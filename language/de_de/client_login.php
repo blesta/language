22dfd0dd85ab814c8f5045b1d3117259dd82e639
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:46 -0800

    Updated de_de language files

---
+++ language/de_de/client_login.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Client Login
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientLogin.forgot.link_login'] = 'Abbrechen, Anmelden';
+$lang['ClientLogin.forgot.field_forgotsubmit'] = 'Benutzername wiederherstellen';
+$lang['ClientLogin.forgot.field_email'] = 'E-Mail Adresse';
+$lang['ClientLogin.forgot.description'] = 'Geben Sie Ihre E-Mail-Adresse ein, um Ihren Benutzernamen anzufordern.';
+$lang['ClientLogin.forgot.reset_heading'] = 'Benutzername vergessen';
+$lang['ClientLogin.forgot.page_title'] = 'Benutzername vergessen';
+$lang['ClientLogin.confirmreset.link_login'] = 'Abbrechen, Anmelden';
+$lang['ClientLogin.confirmreset.field_resetsubmit'] = 'Passwort festlegen';
+$lang['ClientLogin.confirmreset.field_confirm_password'] = 'Neues Passwort bestätigen';
+$lang['ClientLogin.confirmreset.field_new_password'] = 'Neues Passwort';
+$lang['ClientLogin.confirmreset.description'] = 'Bitte geben Sie Ihr neues Passwort ein.';
+$lang['ClientLogin.confirmreset.reset_heading'] = 'Bestätigen Sie das Zurücksetzen des Passworts';
+$lang['ClientLogin.confirmreset.page_title'] = 'Bestätigen Sie das Zurücksetzen des Passworts';
+$lang['ClientLogin.reset.link_login'] = 'Abbrechen, Anmelden';
+$lang['ClientLogin.reset.field_resetsubmit'] = 'Passwort zurücksetzen';
+$lang['ClientLogin.reset.field_username'] = 'Benutzername';
+$lang['ClientLogin.reset.description'] = 'Geben Sie Ihren Benutzernamen ein, um einen Link zum Zurücksetzen des Passworts anzufordern.';
+$lang['ClientLogin.reset.reset_heading'] = 'Passwort zurücksetzen';
+$lang['ClientLogin.reset.page_title'] = 'Passwort zurücksetzen';
+$lang['ClientLogin.otp.field_submit'] = 'Einloggen';
+$lang['ClientLogin.otp.field_otp'] = 'Einmaliges Passwort';
+$lang['ClientLogin.otp.login_heading'] = 'Einloggen';
+$lang['ClientLogin.otp.description'] = 'Bitte geben Sie Ihr One Time Password ein.';
+$lang['ClientLogin.otp.page_title'] = 'Einloggen';
+$lang['ClientLogin.index.link_forgotusername'] = 'Meinen Benutzernamen vergessen';
+$lang['ClientLogin.index.link_resetpassword'] = 'Mein Passwort zurücksetzen';
+$lang['ClientLogin.index.field_loginsubmit'] = 'Einloggen';
+$lang['ClientLogin.index.field_rememberme'] = 'Erinnern Sie sich an mich auf diesem Computer';
+$lang['ClientLogin.index.field_password'] = 'Passwort';
+$lang['ClientLogin.index.field_username'] = 'Benutzername';
+$lang['ClientLogin.index.login_heading'] = 'Einloggen';
+$lang['ClientLogin.index.description'] = 'Bitte melden Sie sich an, um auf den Kundenbereich zuzugreifen.';
+$lang['ClientLogin.index.page_title'] = 'Einloggen';
+$lang['ClientLogin.!success.forgot_sent'] = 'Es wurde eine E-Mail mit Ihrem Benutzernamen an Ihre E-Mail-Adresse gesendet.';
+$lang['ClientLogin.!success.reset_sent'] = 'Eine Bestätigungs-E-Mail wurde an die angegebene Adresse gesandt.';
+$lang['ClientLogin.!error.captcha.invalid'] = 'Das eingegebene Captcha war ungültig. Bitte versuchen Sie es erneut.';
+$lang['ClientLogin.!error.unknown_email'] = 'Diese E-Mail-Adresse wird nicht erkannt oder der Benutzername kann nicht wiederhergestellt werden.';
+$lang['ClientLogin.!error.unknown_user'] = 'Der Benutzername wird nicht erkannt oder das Passwort kann nicht zurückgesetzt werden.';
+
