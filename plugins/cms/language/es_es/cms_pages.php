ea41b51a5b387afc6bfd5db4c2bfbb530f5d5ac0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:55 -0800

    Updated es_es language files

---
+++ plugins/cms/language/es_es/cms_pages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cms Pages
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CmsPages.!error.uri.empty'] = 'Por favor, introduzca la ruta URI a la página.';
+$lang['CmsPages.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['CmsPages.!error.title.empty'] = 'Introduzca el título de la página.';
+$lang['CmsPages.!error.content.valid'] = 'Error de análisis de contenido: %1$s';
+
