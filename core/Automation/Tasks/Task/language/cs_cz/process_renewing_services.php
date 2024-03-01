34cf8073229b139192319ceb681a3aed564076ec
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:59 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/process_renewing_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Process Renewing Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.process_renewing_services.renew_error'] = 'Nelze obnovit službu #%1$s pro klienta %2$s.';
+$lang['Automation.task.process_renewing_services.renew_success'] = 'Obnovení služby #%1$s pro klienta %2$s.';
+$lang['Automation.task.process_renewing_services.completed'] = 'Úloha obnovení služeb byla dokončena.';
+$lang['Automation.task.process_renewing_services.attempt'] = 'Pokus o zpracování obnovení služeb.';
+
