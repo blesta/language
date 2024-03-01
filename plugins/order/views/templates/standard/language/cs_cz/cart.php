fe0b6255efbcd5d49235e1b820beb3349eac358f
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:22 -0800

    Updated cs_cz language files

---
+++ plugins/order/views/templates/standard/language/cs_cz/cart.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cart
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cart.index.empty_cart'] = 'Váš košík je prázdný. Začněte <a href="%1$s">výběrem plánu"</a>';
+$lang['Cart.index.remove_item'] = 'Odstranění stránky';
+$lang['Cart.index.edit_item'] = 'Upravit';
+$lang['Cart.index.order_more_btn'] = 'Objednat další položky';
+$lang['Cart.index.checkout_btn'] = 'Pokladna';
+$lang['Cart.index.table_price'] = 'Cena';
+$lang['Cart.index.table_qty'] = 'Množství';
+$lang['Cart.index.table_term'] = 'Termín';
+$lang['Cart.index.table_description'] = 'Popis';
+$lang['Cart.index.header_cart'] = 'Objednávka recenze';
+
