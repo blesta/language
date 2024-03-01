66ab55c9581cb0f9a72bab869fb29c439bb994d7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:19 -0800

    Updated el_gr language files

---
+++ components/gateways/merchant/cornerstone/language/el_gr/cornerstone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cornerstone
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cornerstone.security_key'] = 'Κλειδί ασφαλείας';
+$lang['Cornerstone.description'] = 'Η Cornerstone είναι ένας από τους κορυφαίους χριστιανικούς ανεξάρτητους οργανισμούς πωλήσεων στον κλάδο της εμπορικής επεξεργασίας στις ΗΠΑ.';
+$lang['Cornerstone.name'] = 'Ακρογωνιαίος λίθος';
+$lang['Cornerstone.!error.security_key.empty'] = 'Εισάγετε το κλειδί ασφαλείας σας.';
+
