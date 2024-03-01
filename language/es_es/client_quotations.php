e03df33b2c60b6d1d354fcbff8e6dfe5f578a6a4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:15 -0800

    Updated es_es language files

---
+++ language/es_es/client_quotations.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Client Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientQuotations.!error.password_mismatch'] = 'La contraseña no coincide con la contraseña de inicio de sesión.';
+$lang['ClientQuotations.!success.approved_quotation'] = 'El presupuesto se ha aprobado correctamente.';
+$lang['ClientQuotations.index.page_title'] = 'Cliente #%1$s Presupuestos';
+$lang['ClientQuotations.index.category_pending'] = 'Pendiente';
+$lang['ClientQuotations.index.category_approved'] = 'Aprobado';
+$lang['ClientQuotations.index.category_expired'] = 'Caducado';
+$lang['ClientQuotations.index.boxtitle_quotations'] = 'Citas';
+$lang['ClientQuotations.index.heading_quotation'] = 'Presupuesto';
+$lang['ClientQuotations.index.heading_title'] = 'Título';
+$lang['ClientQuotations.index.heading_subtotal'] = 'Subtotal';
+$lang['ClientQuotations.index.heading_total'] = 'Importe';
+$lang['ClientQuotations.index.heading_date_created'] = 'Fecha de creación';
+$lang['ClientQuotations.index.heading_date_expires'] = 'Fecha de expiración';
+$lang['ClientQuotations.index.heading_options'] = 'Opciones';
+$lang['ClientQuotations.index.option_view'] = 'Ver';
+$lang['ClientQuotations.index.option_approve'] = 'Aprobar';
+$lang['ClientQuotations.index.no_results'] = 'No tiene %1$s Citas.';
+$lang['ClientQuotations.approve.heading_approve'] = 'Aprobar';
+$lang['ClientQuotations.approve.field_password'] = 'Iniciar sesión Contraseña para aprobar';
+$lang['ClientQuotations.approve.field_cancel'] = 'Cancelar, no aprobar';
+$lang['ClientQuotations.approve.field_submit'] = 'Aprobar';
+$lang['ClientQuotations.invoices.heading_invoice'] = 'Factura #';
+$lang['ClientQuotations.invoices.heading_amount'] = 'Importe';
+$lang['ClientQuotations.invoices.heading_paid'] = 'Pagado';
+$lang['ClientQuotations.invoices.heading_date_billed'] = 'Fecha de facturación';
+$lang['ClientQuotations.invoices.no_results'] = 'Este presupuesto no tiene facturas asociadas.';
+
