1fd132883ea81b25c5abe08d42a896152ae843bc
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:54 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/exchange_rates.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Exchange Rates
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.exchange_rates.failed'] = 'Směnné kurzy nebylo možné aktualizovat.';
+$lang['Automation.task.exchange_rates.success'] = 'Směnné kurzy byly úspěšně aktualizovány.';
+$lang['Automation.task.exchange_rates.completed'] = 'Úkol týkající se směnných kurzů byl dokončen.';
+$lang['Automation.task.exchange_rates.attempt'] = 'Pokus o aktualizaci směnných kurzů.';
+
