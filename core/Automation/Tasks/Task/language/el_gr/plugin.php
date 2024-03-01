c1af40efdad804de352c1cbcc7f4ada8cbff02fc
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:21 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.plugin.attempt'] = 'Προσπάθεια για το plugin cron για %1$s %2$s.';
+$lang['Automation.task.plugin.completed'] = 'Ολοκληρώθηκε το plugin cron για το %1$s %2$s.';
+
