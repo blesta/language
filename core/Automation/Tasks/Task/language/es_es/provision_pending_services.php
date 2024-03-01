2275c63213bd976d39c6b53d8da292d6ef5a638a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:25 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/provision_pending_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Provision Pending Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.provision_pending_services.attempt'] = 'Intento de provisión de servicios pendientes de pago.';
+$lang['Automation.task.provision_pending_services.completed'] = 'La tarea de servicios pendientes de pago ha finalizado.';
+$lang['Automation.task.provision_pending_services.error'] = 'No se ha podido activar el servicio pendiente #%1$s del cliente #%2$s.';
+$lang['Automation.task.provision_pending_services.success'] = 'El servicio pendiente #%1$s del cliente #%2$s está ahora activo.';
+
