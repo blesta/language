0e8b7c5965e5a8b4d538e766bc97df247b4a0ba1
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:37 -0800

    Updated de_de language files

---
+++ plugins/download_manager/language/de_de/client_main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.index.no_downloads'] = 'In diesem Bereich sind keine Downloads verfügbar.';
+$lang['ClientMain.index.login'] = 'Sie müssen sich einloggen, um die Downloads in diesem Bereich zu sehen.';
+$lang['ClientMain.index.boxtitle_downloads'] = 'Herunterladen';
+$lang['ClientMain.index.page_title'] = 'Herunterladen';
+$lang['ClientMain.!text.root_directory'] = 'Startseite';
+
