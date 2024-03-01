7b3e19f6f4a20caa559795904a5f0533997184e1
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:42 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/widepay/language/fr_fr/widepay.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Widepay
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Widepay.name'] = 'Large rémunération';
+$lang['Widepay.wallet_id'] = 'Portefeuille ID';
+$lang['Widepay.wallet_token'] = 'Jeton de portefeuille';
+$lang['Widepay.allow_card_payment'] = 'Autoriser les paiements par carte';
+$lang['Widepay.!error.wallet_id.format'] = 'Veuillez saisir un identifiant de portefeuille';
+$lang['Widepay.!error.wallet_token.format'] = 'Veuillez saisir un jeton API de portefeuille';
+$lang['Widepay.buildprocess.submit_card'] = 'Payer avec la carte de crédit Wide Pay';
+$lang['Widepay.buildprocess.submit_ticket'] = 'Payer avec un ticket large';
+$lang['Widepay.charge_types.ticket'] = 'Billet';
+$lang['Widepay.charge_types.card'] = 'Carte de crédit';
+
