5e0bd420b643cfb383d37fe9adf3bc51731dea01
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:28 -0800

    Updated it_it language files

---
+++ plugins/order/views/templates/standard/language/it_it/checkout.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Checkout
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Checkout.complete.order_more_btn'] = 'Effettuare un altro ordine';
+$lang['Checkout.complete.header_almost_complete'] = 'Ci siamo quasi...';
+$lang['Checkout.complete.header_complete'] = 'Ordine completo';
+$lang['Checkout.ach_info.heading_ach'] = 'Informazioni sul conto bancario';
+$lang['Checkout.cc_info.heading_cc'] = 'Informazioni sulla carta di credito';
+$lang['Checkout.contact_info.text_select_contact'] = 'È possibile selezionare un contatto esistente per pre-compilare questo modulo.';
+$lang['Checkout.contact_info.field_contact_id'] = 'Copiare le informazioni di contatto da';
+$lang['Checkout.contact_info.heading_contact'] = 'Informazioni di contatto';
+$lang['Checkout.index.no_options'] = 'Non sono disponibili opzioni di pagamento.';
+$lang['Checkout.index.continue_btn'] = 'Continua';
+$lang['Checkout.index.pay_credit_btn'] = 'Richiesta di credito e invio dell\'ordine';
+$lang['Checkout.index.pay_btn'] = 'Invia l\'ordine';
+$lang['Checkout.index.apply_credit'] = 'Richiesta di credito (%1$s disponibile)';
+$lang['Checkout.index.heading_payment_options'] = 'Opzioni di pagamento';
+$lang['Checkout.index.heading_other_payment_options'] = 'Altre opzioni di pagamento';
+$lang['Checkout.index.field_newdetails'] = 'Nuovi dettagli di pagamento';
+$lang['Checkout.index.field_useaccount'] = 'Utilizzare il conto di pagamento';
+$lang['Checkout.index.header_paymethod'] = 'Scegliere il metodo di pagamento';
+$lang['Checkout.index.header_checkout'] = 'Cassa';
+
