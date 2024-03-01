3b0255e4d54941f6d1990518e8a60c72df7d4ca7
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:02 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_system_marketplace.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Admin System Marketplace
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemMarketplace.index.page_title'] = 'Réglages > Système > Marché';
+$lang['AdminSystemMarketplace.index.boxtitle_marketplace'] = 'Marché';
+$lang['AdminSystem.marketplace.category_all'] = 'Tous';
+$lang['AdminSystemMarketplace.index.text_unavailable'] = 'La place de marché est actuellement indisponible.';
+$lang['AdminSystemMarketplace.listing.boxtitle_listing'] = 'Extension %1$s';
+
