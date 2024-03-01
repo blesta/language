f743faf61e3faf8881db6d267f770ea8a615ae45
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:40 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/order_forms.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Order Forms
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderForms.!error.name.empty'] = 'Debe introducir un nombre para el formulario.';
+$lang['OrderForms.!error.label.empty'] = 'Debe introducir una etiqueta para el formulario.';
+$lang['OrderForms.!error.label.unique'] = 'Ya existe un formulario de pedido con esa etiqueta.';
+$lang['OrderForms.!error.label.length'] = 'La longitud de la etiqueta no puede superar los 32 caracteres.';
+$lang['OrderForms.!error.label.format'] = 'La etiqueta no se ajusta al formato requerido. Solo se aceptan letras, números, guiones bajos y guiones.';
+$lang['OrderForms.!error.template.supported'] = 'La plantilla seleccionada no admite ese tipo de orden.';
+$lang['OrderForms.!error.client_group_id.valid'] = 'Ese grupo de clientes no existe.';
+$lang['OrderForms.!error.require_tos.valid'] = 'Si requiere la aceptación de las condiciones del servicio, debe proporcionar una URL con las condiciones.';
+$lang['OrderForms.!error.order_form_id.exists'] = 'La orden de pedido no se puede eliminar porque hay pedidos pendientes para esa orden.';
+$lang['OrderForms.!error.date_added.valid'] = 'La fecha añadida debe ser una fecha válida.';
+$lang['OrderForms.!error.visibility.valid'] = 'No se admite la visibilidad indicada.';
+$lang['OrderForms.!error.abandoned_cart_first.valid'] = 'Los días para el primer recordatorio deben ser un valor numérico.';
+$lang['OrderForms.!error.abandoned_cart_second.valid'] = 'Los días para el segundo recordatorio deben ser un valor numérico.';
+$lang['OrderForms.!error.abandoned_cart_third.valid'] = 'Los días del tercer recordatorio deben ser un valor numérico.';
+$lang['OrderForms.!error.field_abandoned_cart_cancellation.valid'] = 'Los días de anulación del pedido abandonado deben ser un valor numérico.';
+$lang['OrderForms.!error.inactive_after_cancellation'] = 'Desactivar clientes de una sola orden debe ser 1 o 0.';
+$lang['OrderForms.getVisibilities.public'] = 'Público';
+$lang['OrderForms.getVisibilities.shared'] = 'Compartido';
+$lang['OrderForms.getVisibilities.client'] = 'Sólo para clientes';
+$lang['OrderOrders.getNoticeIntervals.hour'] = '1 hora';
+$lang['OrderOrders.getNoticeIntervals.hours'] = '%1$s Horas';
+$lang['OrderOrders.getNoticeIntervals.day'] = '1 día';
+$lang['OrderOrders.getNoticeIntervals.days'] = '%1$s Días';
+
