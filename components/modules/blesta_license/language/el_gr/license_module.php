61792697394cd574eb89c5276e782fa48c79b84f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:41 -0800

    Updated el_gr language files

---
+++ components/modules/blesta_license/language/el_gr/license_module.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * License Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BlestaLicense.tab_reissue.field_submit'] = 'Επανέκδοση';
+$lang['BlestaLicense.tab_reissue.description'] = 'Η επανέκδοση της άδειας χρήσης θα επιτρέψει την εγκατάσταση ή τη μεταφορά της άδειας χρήσης σε διαφορετική θέση, όπου θα κλειδώσει.';
+$lang['BlestaLicense.tab_reissue.heading_reissue'] = 'Επανέκδοση';
+$lang['BlestaLicense.tab_reissue'] = 'Επανέκδοση άδειας';
+$lang['Package.package_fields.pricing_id'] = 'Άδεια';
+$lang['Package.getpricings.pricing_id'] = '%1$s (ID %2$s)';
+$lang['BlestaLicense.edit_row.edit_btn'] = 'Ενημέρωση μεταπωλητή';
+$lang['BlestaLicense.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['BlestaLicense.edit_row.box_title'] = 'Επεξεργασία μεταπωλητή';
+$lang['BlestaLicense.add_row.add_btn'] = 'Προσθήκη μεταπωλητή';
+$lang['BlestaLicense.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['BlestaLicense.add_row.box_title'] = 'Προσθήκη μεταπωλητή';
+$lang['BlestaLicense.row_meta.test_mode'] = 'Λειτουργία δοκιμής';
+$lang['BlestaLicense.row_meta.password'] = 'Κωδικός πρόσβασης';
+$lang['BlestaLicense.row_meta.username'] = 'Όνομα χρήστη';
+$lang['BlestaLicense.!tooltip.password'] = 'Εισάγετε τον κωδικό πρόσβασης του λογαριασμού σας Blesta.';
+$lang['BlestaLicense.!tooltip.username'] = 'Εισάγετε το όνομα χρήστη του λογαριασμού σας Blesta.';
+$lang['BlestaLicense.manage.module_rows_no_results'] = 'Δεν υπάρχουν μεταπωλητές.';
+$lang['BlestaLicense.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον μεταπωλητή;';
+$lang['BlestaLicense.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['BlestaLicense.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['BlestaLicense.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['BlestaLicense.manage.module_rows_heading.username'] = 'Όνομα χρήστη';
+$lang['BlestaLicense.manage.module_rows_title'] = 'Μεταπωλητές';
+$lang['BlestaLicense.add_module_row'] = 'Προσθήκη μεταπωλητή';
+$lang['BlestaLicense.module_rows.name'] = 'Μεταπωλητές';
+$lang['BlestaLicense.module_row.name'] = 'Μεταπωλητής';
+$lang['Package.!error.meta[pricing_id].format'] = 'Παρακαλούμε επιλέξτε μια έγκυρη άδεια.';
+
