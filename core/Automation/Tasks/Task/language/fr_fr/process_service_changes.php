cd020f3622dfd77f0d70b241f150d7f5424f01fb
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:25 -0800

    Updated fr_fr language files

---
+++ core/Automation/Tasks/Task/language/fr_fr/process_service_changes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Process Service Changes
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.process_service_changes.attempt'] = 'Tentative de traitement des changements de service.';
+$lang['Automation.task.process_service_changes.completed'] = 'La tâche de modification du service de traitement est terminée.';
+$lang['Automation.task.process_service_changes.process_result'] = 'Le traitement du changement de service #%1$s a donné lieu à un statut : %2$s';
+$lang['Automation.task.process_service_changes.missing_invoice'] = 'L\'ID de la facture #%1$s n\'a pas été trouvé. Le changement de service #%2$s pour le service #%3$s est passé à l\'état : %4$s.';
+$lang['Automation.task.process_service_changes.service_inactive'] = 'Le changement de service #%1$s n\'a pas été traité car son service est inactif.';
+$lang['Automation.task.process_service_changes.expired'] = 'Le changement de service #%1$s a expiré sans paiement et est passé au statut : %2$s';
+
