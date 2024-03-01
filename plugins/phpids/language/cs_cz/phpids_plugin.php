fad28d6b9eaa31f332a5ec5c4c613323a0e8dd22
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:32 -0800

    Updated cs_cz language files

---
+++ plugins/phpids/language/cs_cz/phpids_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Phpids Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PhpidsPlugin.description'] = 'Systém detekce narušení, který pomáhá identifikovat podezřelé aktivity.';
+$lang['PhpidsPlugin.name'] = 'PHPIDS';
+
