8137d6074e2f91be526ed390831a9a9e588b5069
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:27 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/transition_quotations.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Transition Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.transition_quotations.attempt'] = 'Intento de transición de cotizaciones caducadas.';
+$lang['Automation.task.transition_quotations.completed'] = 'La tarea de transición de citas ha finalizado.';
+$lang['Automation.task.transition_quotations.none'] = 'No hay cotizaciones caducadas disponibles para la transición.';
+$lang['Automation.task.transition_quotations.expiration_success'] = 'El presupuesto pendiente #%1$s del cliente #%2$s ha caducado.';
+$lang['Automation.task.transition_quotations.dead_success'] = 'El presupuesto caducado #%1$s del cliente #%2$s ya no existe.';
+
