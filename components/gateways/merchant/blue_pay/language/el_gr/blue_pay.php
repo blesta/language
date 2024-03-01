fc728ed15aaacc582a21dc17f4c905101acde62f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:18 -0800

    Updated el_gr language files

---
+++ components/gateways/merchant/blue_pay/language/el_gr/blue_pay.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Blue Pay
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Blue_pay.test_mode'] = 'Λειτουργία δοκιμής';
+$lang['Blue_pay.secret_key'] = 'Μυστικό κλειδί';
+$lang['Blue_pay.account_id'] = 'Αναγνωριστικό λογαριασμού';
+$lang['Blue_pay.description'] = 'Η BluePay προσφέρει γρήγορες, εύκολες και ασφαλείς λύσεις επεξεργασίας πληρωμών που ταιριάζουν στις ανάγκες κάθε εμπόρου. Λειτουργεί στη Βόρεια Αμερική';
+$lang['Blue_pay.name'] = 'BluePay';
+$lang['Blue_pay.!error.test_mode.valid'] = 'Η λειτουργία δοκιμής πρέπει να οριστεί σε "true", εάν δίνεται.';
+$lang['Blue_pay.!error.secret_key.empty'] = 'Εισάγετε το μυστικό σας κλειδί.';
+$lang['Blue_pay.!error.account_id.empty'] = 'Παρακαλώ εισάγετε το αναγνωριστικό του λογαριασμού σας.';
+
