810a8285542c99ab95048952f59ac6041ae36338
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:36 -0800

    Updated fr_fr language files

---
+++ core/Util/Filters/language/fr_fr/package_group_filters.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Package Group Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.package_group_filters.field_options'] = 'Options';
+$lang['Util.filters.package_group_filters.options.hidden'] = 'Afficher les groupes de paquets cachés';
+
