55b288a339ff654b204f11026c2418dee007e7d2
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:50 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/backups_amazons3.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Backups Amazons3
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.backups_amazons3.attempt'] = 'Pokus o zálohování databáze na AmazonS3.';
+$lang['Automation.task.backups_amazons3.completed'] = 'Úloha zálohování databáze AmazonS3 byla dokončena.';
+$lang['Automation.task.backups_amazons3.success'] = 'Zálohování bylo úspěšně dokončeno.';
+
