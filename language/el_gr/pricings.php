0e8f328ebdc2d954ea3ce171127d6d1c4c7322a5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:31 -0800

    Updated el_gr language files

---
+++ language/el_gr/pricings.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Pricings
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pricings.getPeriods.onetime_plural'] = 'Μία φορά';
+$lang['Pricings.getPeriods.year_plural'] = 'Έτη';
+$lang['Pricings.getPeriods.month_plural'] = 'Μήνες';
+$lang['Pricings.getPeriods.week_plural'] = 'Εβδομάδες';
+$lang['Pricings.getPeriods.day_plural'] = 'Ημέρες';
+$lang['Pricings.getPeriods.onetime'] = 'Μία φορά';
+$lang['Pricings.getPeriods.year'] = 'Έτος';
+$lang['Pricings.getPeriods.month'] = 'Μήνας';
+$lang['Pricings.getPeriods.week'] = 'Εβδομάδα';
+$lang['Pricings.getPeriods.day'] = 'Ημέρα';
+$lang['Pricings.!error.currency.in_use'] = 'Το νόμισμα δεν μπορεί να ενημερωθεί σε μια τιμολόγηση που είναι σε χρήση.';
+$lang['Pricings.!error.period.in_use'] = 'Η περίοδος δεν μπορεί να ενημερωθεί σε μια τιμολόγηση που είναι σε χρήση.';
+$lang['Pricings.!error.term.in_use'] = 'Ο όρος δεν μπορεί να ενημερωθεί σε μια τιμολόγηση που είναι σε χρήση.';
+$lang['Pricings.!error.currency.format'] = 'Ο κωδικός νομίσματος πρέπει να είναι 3 χαρακτήρες.';
+$lang['Pricings.!error.cancel_fee.format'] = 'Το τέλος ακύρωσης πρέπει να είναι ένας αριθμός.';
+$lang['Pricings.!error.setup_fee.format'] = 'Η αμοιβή εγκατάστασης πρέπει να είναι ένας αριθμός.';
+$lang['Pricings.!error.price_transfer.format'] = 'Η τιμή ανανέωσης πρέπει να είναι ένας αριθμός.';
+$lang['Pricings.!error.price_renews.valid'] = 'Η τιμή ανανέωσης δεν μπορεί να οριστεί για μία χρονική περίοδο.';
+$lang['Pricings.!error.price_renews.format'] = 'Η τιμή ανανέωσης πρέπει να είναι ένας αριθμός.';
+$lang['Pricings.!error.price.format'] = 'Η τιμή πρέπει να είναι ένας αριθμός.';
+$lang['Pricings.!error.period.format'] = 'Μη έγκυρος τύπος περιόδου.';
+$lang['Pricings.!error.term.valid'] = 'Ο όρος πρέπει να είναι μεγαλύτερος από 0.';
+$lang['Pricings.!error.term.length'] = 'Το μήκος του όρου δεν μπορεί να υπερβαίνει τους 5 χαρακτήρες.';
+$lang['Pricings.!error.term.format'] = 'Ο όρος πρέπει να είναι αριθμός.';
+
