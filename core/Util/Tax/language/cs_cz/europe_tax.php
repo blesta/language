07f199b4d29eb9fc62f68a6d04f94707a7d32041
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:14 -0800

    Updated cs_cz language files

---
+++ core/Util/Tax/language/cs_cz/europe_tax.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Europe Tax
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.Tax.europe_tax.fields.tax_home_eu_vat'] = 'Daňový systém Home';
+$lang['Util.Tax.europe_tax.fields.note_tax_exempt_eu_vat'] = 'Automatické nastavení klienta jako osvobozeného od daně, pokud je jeho DIČ platné a není z vašeho daňového domova.';
+$lang['Util.Tax.europe_tax.fields.tax_exempt_eu_vat'] = 'Automatické nastavení jako osvobození od daně';
+$lang['Util.Tax.europe_tax.fields.note_enable_eu_vat'] = 'Zaškrtnutím této možnosti povolíte pro tuto společnost ověřování čísla DPH EU. To vyžaduje mýdlové rozšíření PHP.';
+$lang['Util.Tax.europe_tax.fields.enable_eu_vat'] = 'Povolení ověřování DPH v EU';
+$lang['Util.Tax.europe_tax.tax_note_reverse_charge'] = 'Používá se mechanismus přenesení daňové povinnosti u DPH. Daňová povinnost přechází na kupujícího.';
+$lang['Util.Tax.europe_tax.tax_deduction_note'] = '%1$s 0% z %2$s';
+$lang['Util.Tax.europe_tax.tax_id_name'] = 'VATIN';
+$lang['Util.Tax.europe_tax.tax_provider_name'] = 'DPH EU VIES';
+
