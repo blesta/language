34059989525af2ff47f3193cb30c876c7e9674df
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:26 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/managed_accounts.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Managed Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ManagedAccounts.!error.client.exists'] = 'Aucun compte n\'est attribué à l\'adresse électronique fournie.';
+$lang['ManagedAccounts.!error.client_id.exists'] = 'ID de client non valide.';
+$lang['ManagedAccounts.!error.email.invitation'] = 'Une invitation a déjà été envoyée à ce compte.';
+$lang['ManagedAccounts.!error.invitation_id.exists'] = 'ID d\'invitation non valide.';
+$lang['ManagedAccounts.!error.contact_id.exists'] = 'ID de contact non valide.';
+$lang['ManagedAccounts.!error.permissions.manage'] = 'Vous n\'êtes pas autorisé à gérer ce compte.';
+$lang['ManagedAccounts.!error.permissions.empty'] = 'Vous devez attribuer au moins une autorisation au gestionnaire.';
+
