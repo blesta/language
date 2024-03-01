31c30405f97482cd368e3ddd7a4806aaef6ba8dd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:38 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/order_forms.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Order Forms
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderForms.!error.name.empty'] = 'Vous devez saisir un nom pour le formulaire.';
+$lang['OrderForms.!error.label.empty'] = 'Vous devez saisir un libellé pour le formulaire.';
+$lang['OrderForms.!error.label.unique'] = 'Un bon de commande portant ce label existe déjà.';
+$lang['OrderForms.!error.label.length'] = 'La longueur de l\'étiquette ne doit pas dépasser 32 caractères.';
+$lang['OrderForms.!error.label.format'] = 'L\'étiquette ne correspond pas au format requis. Seuls les lettres, les chiffres, les traits de soulignement et les tirets sont acceptés.';
+$lang['OrderForms.!error.template.supported'] = 'Le modèle sélectionné ne prend pas en charge ce type de commande.';
+$lang['OrderForms.!error.client_group_id.valid'] = 'Ce groupe de clients n\'existe pas.';
+$lang['OrderForms.!error.require_tos.valid'] = 'Si vous exigez l\'acceptation des conditions de service, vous devez fournir une URL vers ces conditions.';
+$lang['OrderForms.!error.order_form_id.exists'] = 'Le formulaire de commande ne peut pas être supprimé parce qu\'il y a des commandes en cours pour ce formulaire.';
+$lang['OrderForms.!error.date_added.valid'] = 'La date ajoutée doit être une date valide.';
+$lang['OrderForms.!error.visibility.valid'] = 'La visibilité donnée n\'est pas prise en charge.';
+$lang['OrderForms.!error.abandoned_cart_first.valid'] = 'Les jours du premier rappel doivent être une valeur numérique.';
+$lang['OrderForms.!error.abandoned_cart_second.valid'] = 'Les jours du deuxième rappel doivent être une valeur numérique.';
+$lang['OrderForms.!error.abandoned_cart_third.valid'] = 'Les jours du troisième rappel doivent être une valeur numérique.';
+$lang['OrderForms.!error.field_abandoned_cart_cancellation.valid'] = 'Le nombre de jours pour l\'annulation de la commande abandonnée doit être une valeur numérique.';
+$lang['OrderForms.!error.inactive_after_cancellation'] = 'Désactiver les clients à ordre unique doit être 1 ou 0.';
+$lang['OrderForms.getVisibilities.public'] = 'Public';
+$lang['OrderForms.getVisibilities.shared'] = 'Partagé';
+$lang['OrderForms.getVisibilities.client'] = 'Client uniquement';
+$lang['OrderOrders.getNoticeIntervals.hour'] = '1 heure';
+$lang['OrderOrders.getNoticeIntervals.hours'] = 'Heures %1$s';
+$lang['OrderOrders.getNoticeIntervals.day'] = '1 jour';
+$lang['OrderOrders.getNoticeIntervals.days'] = '%1$s Days';
+
