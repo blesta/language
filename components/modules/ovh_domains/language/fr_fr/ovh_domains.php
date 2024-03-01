8a05061db6b495e1182beb93f57a22b25a7b76bb
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:59 -0800

    Updated fr_fr language files

---
+++ components/modules/ovh_domains/language/fr_fr/ovh_domains.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Ovh Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OvhDomains.name'] = 'Domaines OVH';
+$lang['OvhDomains.description'] = '';
+$lang['OvhDomains.module_row'] = 'Compte';
+$lang['OvhDomains.module_row_plural'] = 'Comptes';
+$lang['OvhDomains.module_group'] = 'Groupe de comptes';
+$lang['OvhDomains.add_module_row'] = 'Ajouter un compte';
+$lang['OvhDomains.add_module_group'] = 'Ajouter un groupe de comptes';
+$lang['OvhDomains.manage.module_rows_title'] = 'Comptes';
+$lang['OvhDomains.manage.module_rows_heading.application_key'] = 'Clé d\'application';
+$lang['OvhDomains.manage.module_rows_heading.secret_key'] = 'Clé secrète';
+$lang['OvhDomains.manage.module_rows_heading.consumer_key'] = 'Clé du consommateur';
+$lang['OvhDomains.manage.module_rows_heading.endpoint'] = 'Point final';
+$lang['OvhDomains.manage.module_rows_heading.options'] = 'Options';
+$lang['OvhDomains.manage.module_rows.edit'] = 'Editer';
+$lang['OvhDomains.manage.module_rows.delete'] = 'Supprimer';
+$lang['OvhDomains.manage.module_rows.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce compte ?';
+$lang['OvhDomains.manage.module_rows_no_results'] = 'Il n\'y a pas de comptes.';
+$lang['OvhDomains.manage.module_groups_title'] = 'Groupes';
+$lang['OvhDomains.manage.module_groups_heading.name'] = 'Nom';
+$lang['OvhDomains.manage.module_groups_heading.module_rows'] = 'Comptes';
+$lang['OvhDomains.manage.module_groups_heading.options'] = 'Options';
+$lang['OvhDomains.manage.module_groups.edit'] = 'Editer';
+$lang['OvhDomains.manage.module_groups.delete'] = 'Supprimer';
+$lang['OvhDomains.manage.module_groups.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce compte ?';
+$lang['OvhDomains.manage.module_groups.no_results'] = 'Il n\'y a pas de groupe de comptes';
+$lang['OvhDomains.add_row.box_title'] = 'OVH Domains - Ajouter un compte';
+$lang['OvhDomains.add_row.add_btn'] = 'Ajouter un compte';
+$lang['OvhDomains.edit_row.box_title'] = 'OVH Domains - Modifier le compte';
+$lang['OvhDomains.edit_row.edit_btn'] = 'Mise à jour du compte';
+$lang['OvhDomains.row_meta.application_key'] = 'Clé d\'application';
+$lang['OvhDomains.row_meta.secret_key'] = 'Clé secrète';
+$lang['OvhDomains.row_meta.consumer_key'] = 'Clé du consommateur';
+$lang['OvhDomains.row_meta.endpoint'] = 'Point final';
+$lang['OvhDomains.!error.application_key.valid'] = 'Clé d\'application invalide';
+$lang['OvhDomains.!error.secret_key.valid'] = 'Clé secrète invalide';
+$lang['OvhDomains.!error.consumer_key.valid'] = 'Clé de consommateur non valide';
+$lang['OvhDomains.!error.endpoint.valid'] = 'Point final non valide';
+$lang['OvhDomains.!error.module_row.missing'] = 'Une erreur interne s\'est produite. La ligne du module n\'est pas disponible.';
+$lang['OvhDomains.!error.domain.valid'] = 'Domaine non valide';
+$lang['OvhDomains.!error.epp_code.valid'] = 'Le code EPP doit être 1 ou 0.';
+$lang['OvhDomains.!error.ns_count'] = 'Au moins deux serveurs de noms sont nécessaires.';
+$lang['OvhDomains.!error.ns_valid'] = 'Un ou plusieurs serveurs de noms ne sont pas valides.';
+$lang['OvhDomains.tab_whois'] = 'WHOIS';
+$lang['OvhDomains.tab_whois.heading'] = 'WHOIS';
+$lang['OvhDomains.tab_whois.field_submit'] = 'Mise à jour du WHOIS';
+$lang['OvhDomains.tab_nameservers'] = 'Serveurs de noms';
+$lang['OvhDomains.tab_nameservers.heading'] = 'Serveurs de noms';
+$lang['OvhDomains.tab_nameservers.field_submit'] = 'Mise à jour des serveurs de noms';
+$lang['OvhDomains.tab_dns'] = 'Enregistrements DNS';
+$lang['OvhDomains.tab_dns.heading'] = 'Enregistrements DNS';
+$lang['OvhDomains.tab_dns.heading_add'] = 'Ajouter un enregistrement';
+$lang['OvhDomains.tab_dns.record_type'] = 'Type d\'enregistrement';
+$lang['OvhDomains.tab_dns.host'] = 'Hôte';
+$lang['OvhDomains.tab_dns.value'] = 'Valeur';
+$lang['OvhDomains.tab_dns.ttl'] = 'TTL';
+$lang['OvhDomains.tab_dns.options'] = 'Options';
+$lang['OvhDomains.tab_dns.field_delete'] = 'Supprimer';
+$lang['OvhDomains.tab_dns.field_add'] = 'Ajouter un enregistrement';
+$lang['OvhDomains.tab_settings'] = 'Paramètres';
+$lang['OvhDomains.tab_settings.heading'] = 'Paramètres';
+$lang['OvhDomains.tab_settings.field_submit'] = 'Sauvegarder les paramètres';
+$lang['OvhDomains.tab_client_whois'] = 'WHOIS';
+$lang['OvhDomains.tab_client_whois.heading'] = 'WHOIS';
+$lang['OvhDomains.tab_client_whois.field_submit'] = 'Mise à jour du WHOIS';
+$lang['OvhDomains.tab_client_nameservers'] = 'Serveurs de noms';
+$lang['OvhDomains.tab_client_nameservers.heading'] = 'Serveurs de noms';
+$lang['OvhDomains.tab_client_nameservers.field_submit'] = 'Mise à jour des serveurs de noms';
+$lang['OvhDomains.tab_client_dns'] = 'Enregistrements DNS';
+$lang['OvhDomains.tab_client_dns.heading'] = 'Enregistrements DNS';
+$lang['OvhDomains.tab_client_dns.record_type'] = 'Type d\'enregistrement';
+$lang['OvhDomains.tab_client_dns.host'] = 'Hôte';
+$lang['OvhDomains.tab_client_dns.value'] = 'Valeur';
+$lang['OvhDomains.tab_client_dns.ttl'] = 'TTL';
+$lang['OvhDomains.tab_client_dns.options'] = 'Options';
+$lang['OvhDomains.tab_client_dns.field_delete'] = 'Supprimer';
+$lang['OvhDomains.tab_client_dns.field_add'] = 'Ajouter un enregistrement';
+$lang['OvhDomains.tab_client_settings'] = 'Paramètres';
+$lang['OvhDomains.tab_client_settings.heading'] = 'Paramètres';
+$lang['OvhDomains.tab_client_settings.heading_auth_code'] = 'Code d\'autorisation';
+$lang['OvhDomains.tab_client_settings.field_registrar_lock'] = 'Serrure du greffier';
+$lang['OvhDomains.tab_client_settings.field_registrar_lock_yes'] = 'Définir le verrou du bureau d\'enregistrement. Recommandé pour empêcher les transferts non autorisés.';
+$lang['OvhDomains.tab_client_settings.field_registrar_lock_no'] = 'Déverrouiller le registrar pour que le domaine puisse être transféré.';
+$lang['OvhDomains.tab_client_settings.text_auth_code'] = 'Utilisez ce code d\'autorisation pour transférer votre domaine vers un autre fournisseur.';
+$lang['OvhDomains.tab_client_settings.field_submit'] = 'Sauvegarder les paramètres';
+$lang['OvhDomains.transfer.domain'] = 'Domaine';
+$lang['OvhDomains.transfer.auth_info'] = 'Code d\'autorisation';
+$lang['OvhDomains.domain.domain'] = 'Domaine';
+$lang['OvhDomains.nameserver.ns1'] = 'Serveur de noms 1';
+$lang['OvhDomains.nameserver.ns2'] = 'Serveur de noms 2';
+$lang['OvhDomains.nameserver.ns3'] = 'Serveur de noms 3';
+$lang['OvhDomains.nameserver.ns4'] = 'Serveur de noms 4';
+$lang['OvhDomains.nameserver.ns5'] = 'Serveur de noms 5';
+$lang['OvhDomains.contact.first_name'] = 'Prénom';
+$lang['OvhDomains.contact.last_name'] = 'Nom de famille';
+$lang['OvhDomains.contact.email'] = 'Adresse électronique';
+$lang['OvhDomains.contact.address1'] = 'Adresse 1';
+$lang['OvhDomains.contact.address2'] = 'Adresse 2';
+$lang['OvhDomains.contact.city'] = 'Ville';
+$lang['OvhDomains.contact.state'] = 'État';
+$lang['OvhDomains.contact.zip'] = 'Code postal';
+$lang['OvhDomains.contact.country'] = 'Pays';
+$lang['OvhDomains.contact.phone'] = 'Téléphone';
+$lang['OvhDomains.service_fields.domain'] = 'Domaine';
+$lang['OvhDomains.package_fields.epp_code'] = 'Code EPP';
+$lang['OvhDomains.package_fields.ns1'] = 'Serveur de noms 1';
+$lang['OvhDomains.package_fields.ns2'] = 'Serveur de noms 2';
+$lang['OvhDomains.package_fields.ns3'] = 'Serveur de noms 3';
+$lang['OvhDomains.package_fields.ns4'] = 'Serveur de noms 4';
+$lang['OvhDomains.package_fields.ns5'] = 'Serveur de noms 5';
+$lang['OvhDomains.package_field.tooltip.epp_code'] = 'Permet ou non aux utilisateurs de demander un code EPP par l\'intermédiaire de l\'interface de service Blesta.';
+$lang['OvhDomains.package_fields.tld_options'] = 'TLDs';
+
