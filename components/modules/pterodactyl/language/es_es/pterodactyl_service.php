dcc081d8f30b7cbf19bdacfaebf415ee7a3b12f9
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:02 -0800

    Updated es_es language files

---
+++ components/modules/pterodactyl/language/es_es/pterodactyl_service.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Pterodactyl Service
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PterodactylService.!error.server_name.empty'] = 'Introduzca un nombre para el servidor.';
+$lang['PterodactylService.service_fields.server_name'] = 'Nombre del servidor';
+$lang['PterodactylService.service_fields.tooltip.server_name'] = 'El nombre del servidor.';
+$lang['PterodactylService.service_fields.server_description'] = 'Descripción del servidor (opcional)';
+$lang['PterodactylService.service_fields.tooltip.server_description'] = 'La descripción del servidor.';
+$lang['PterodactylService.service_fields.server_id'] = 'ID de servidor (opcional)';
+$lang['PterodactylService.service_fields.tooltip.server_id'] = 'El ID del servidor en el panel Pterodáctilo. Esto sólo debe utilizarse cuando se está vinculando este servicio a un servidor preexistente.';
+$lang['PterodactylService.service_fields.optional'] = '%1$s (Opcional)';
+
