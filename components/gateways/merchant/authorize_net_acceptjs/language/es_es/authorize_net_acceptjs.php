44911a194faa802c530f26e1ea7510b415ab5a6c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:19 -0800

    Updated es_es language files

---
+++ components/gateways/merchant/authorize_net_acceptjs/language/es_es/authorize_net_acceptjs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Authorize Net Acceptjs
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AuthorizeNetAcceptjs.!error.auth'] = 'La puerta de enlace no ha podido autenticarse.';
+$lang['AuthorizeNetAcceptjs.!error.transaction_key.empty'] = 'Introduzca una clave de transacción.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.empty'] = 'Introduzca un ID de inicio de sesión.';
+$lang['AuthorizeNetAcceptjs.!error.login_id.valid'] = 'No se ha podido conectar a la API de Authorize.net con el ID de inicio de sesión indicado.';
+$lang['AuthorizeNetAcceptjs.!error.sandbox.valid'] = 'Sandbox se debe establecer en "true" si se da.';
+$lang['AuthorizeNetAcceptjs.name'] = 'Authorize.Net Accept.js';
+$lang['AuthorizeNetAcceptjs.description'] = 'Envíe datos de pago seguros directamente a Authorize.net. Accept.js captura los datos de pago y los envía directamente a Authorize.net.';
+$lang['AuthorizeNetAcceptjs.field_number'] = 'Número';
+$lang['AuthorizeNetAcceptjs.field_security'] = 'Código de seguridad';
+$lang['AuthorizeNetAcceptjs.field_expiration'] = 'Fecha de expiración';
+$lang['AuthorizeNetAcceptjs.login_id'] = 'Identificador';
+$lang['AuthorizeNetAcceptjs.transaction_key'] = 'Clave de transacción';
+$lang['AuthorizeNetAcceptjs.sandbox'] = 'Cajón de arena';
+$lang['AuthorizeNetAcceptjs.sandbox_note'] = 'Al activar esta opción, las transacciones se publicarán en el entorno de Authorize.Net Sandbox. Habilite esta opción únicamente si está realizando pruebas con una cuenta de Authorize.Net Sandbox.';
+$lang['AuthorizeNetAcceptjs.charge_description_default'] = 'Cargo por importe especificado';
+$lang['AuthorizeNetAcceptjs.charge_description'] = 'Cargo por %1$s';
+
