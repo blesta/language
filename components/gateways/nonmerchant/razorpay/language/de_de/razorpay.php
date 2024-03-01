73ef0c0babbfa04b0ae785931200f5fc72486246
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:14 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/razorpay/language/de_de/razorpay.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Razorpay
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Razorpay.!error.key_secret.valid'] = 'Sie müssen ein gültiges Schlüsselgeheimnis eingeben.';
+$lang['Razorpay.!error.key_id.valid'] = 'Sie müssen eine gültige Schlüssel-ID eingeben.';
+$lang['Razorpay.buildprocess.submit'] = 'Bezahlen mit Razorpay';
+$lang['Razorpay.webhook_note'] = 'Bevor Sie mit der Nutzung dieses Gateways beginnen, müssen Sie die folgende URL als Webhook in Ihrem Razorpay-Konto konfigurieren.';
+$lang['Razorpay.webhook'] = 'Razorpay Webhook';
+$lang['Razorpay.webhook_secret'] = 'Webhook Geheimnis';
+$lang['Razorpay.key_secret'] = 'Schlüssel Geheimnis';
+$lang['Razorpay.key_id'] = 'Schlüssel-ID';
+$lang['Razorpay.description'] = 'Razorpay ist ein Zahlungsunternehmen, das Online-Händlern in Indien Zahlungslösungen anbietet.';
+$lang['Razorpay.name'] = 'Razorpay';
+
