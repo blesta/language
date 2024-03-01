adf9745c5bb9ab98ad0f8c1d26e7fe5fee5915ea
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:14 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/order_orders.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Order Orders
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderOrders.!error.price_transfer_disabled'] = 'Die ausgewählte Domäne ist nicht für Übertragungen verfügbar.';
+$lang['OrderOrders.getstatuses.canceled'] = 'Abgesagt';
+$lang['OrderOrders.getstatuses.fraud'] = 'Betrug';
+$lang['OrderOrders.getstatuses.accepted'] = 'Angenommen';
+$lang['OrderOrders.getstatuses.pending'] = 'Im Rückblick';
+
