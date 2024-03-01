ad73fd7dbb5d1b1f0f3c1f2cdb590283f3c49a21
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:59 -0800

    Updated de_de language files

---
+++ language/de_de/email_verifications.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Email Verifications
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['EmailVerifications.!error.contact_id.exists'] = 'Ungültige Kontakt-ID.';
+$lang['EmailVerifications.!error.email.valid'] = 'Bitte geben Sie eine gültige E-Mail Adresse ein.';
+$lang['EmailVerifications.!error.redirect_url.valid'] = 'Bitte geben Sie eine gültige URL ein.';
+$lang['EmailVerifications.!error.token.format'] = 'Bitte geben Sie einen gültigen Token ein.';
+$lang['EmailVerifications.!error.verified.valid'] = 'Das verifizierte Feld muss "1" oder "0" sein.';
+
