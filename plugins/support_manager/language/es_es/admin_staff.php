67e74788f43bc122449a94c597c17c1890a19ebd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:09 -0800

    Updated es_es language files

---
+++ plugins/support_manager/language/es_es/admin_staff.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Admin Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminStaff.!success.staff_added'] = 'El miembro del personal se ha añadido correctamente.';
+$lang['AdminStaff.!success.staff_updated'] = 'El miembro del personal se ha actualizado correctamente.';
+$lang['AdminStaff.!success.staff_deleted'] = 'El miembro del personal se ha eliminado correctamente.';
+$lang['AdminStaff.!tooltip.all_day'] = 'Marque esta casilla para que el miembro del personal esté disponible todo el día.';
+$lang['AdminStaff.!tooltip.ticket_emails'] = 'Marque la casilla de cada prioridad de ticket para la que el personal debe recibir correos electrónicos de ticket.';
+$lang['AdminStaff.!tooltip.mobile_ticket_emails'] = 'Marque la casilla de cada prioridad de ticket para la que el personal deba recibir correos electrónicos de tickets móviles.';
+$lang['AdminStaff.!tooltip.ticket_messenger_notifications'] = 'Marque la casilla de cada prioridad de ticket para la que el personal debe recibir notificaciones de ticket messenger.';
+$lang['AdminStaff.!tooltip.title'] = 'Al introducir un título para este miembro del personal se sobrescribe el título por defecto: %1$s';
+$lang['AdminStaff.!tooltip.signature'] = 'Introduzca una firma para que esté disponible automáticamente al responder a los tickets.';
+$lang['AdminStaff.index.page_title'] = 'Gestor de asistencia > Personal';
+$lang['AdminStaff.add.page_title'] = 'Gestor de asistencia > Añadir personal';
+$lang['AdminStaff.edit.page_title'] = 'Gestor de Soporte > Editar Personal';
+$lang['AdminStaff.index.categorylink_addstaff'] = 'Añadir personal';
+$lang['AdminStaff.index.boxtitle_staff'] = 'Personal';
+$lang['AdminStaff.index.heading_first_name'] = 'Nombre';
+$lang['AdminStaff.index.heading_email'] = 'Correo electrónico';
+$lang['AdminStaff.index.heading_ticket_emails'] = 'Recibir entradas por correo electrónico';
+$lang['AdminStaff.index.heading_mobile_ticket_emails'] = 'Recibir entradas por correo electrónico';
+$lang['AdminStaff.index.heading_ticket_messenger_notifications'] = 'Recibir notificaciones de Ticket Messenger';
+$lang['AdminStaff.index.heading_options'] = 'Opciones';
+$lang['AdminStaff.index.option_edit'] = 'Editar';
+$lang['AdminStaff.index.option_delete'] = 'Borrar';
+$lang['AdminStaff.index.confirm_delete'] = 'Al eliminar a este miembro del personal se anulará la asignación de todos los tickets actualmente asignados a este miembro del personal. ¿Está seguro de que desea eliminar a este miembro del personal?';
+$lang['AdminStaff.index.no_results'] = 'Actualmente no hay personal asignado al sistema de apoyo.';
+$lang['AdminStaff.add.boxtitle_addstaff'] = 'Añadir personal';
+$lang['AdminStaff.add.heading_staff'] = 'Personal';
+$lang['AdminStaff.add.heading_schedule'] = 'Horario';
+$lang['AdminStaff.add.heading_settings'] = 'Ajustes';
+$lang['AdminStaff.add.heading_departments'] = 'Departamentos';
+$lang['AdminStaff.add.field_staff_group_id'] = 'Grupo de Personal';
+$lang['AdminStaff.add.field_staff_id'] = 'Miembro del personal';
+$lang['AdminStaff.add.heading_day'] = 'Día';
+$lang['AdminStaff.add.heading_all_day'] = 'Todo el día';
+$lang['AdminStaff.add.heading_start_time'] = 'Hora de inicio';
+$lang['AdminStaff.add.heading_end_time'] = 'Fin de los tiempos';
+$lang['AdminStaff.add.ticket_emails'] = 'Recibir entradas por correo electrónico';
+$lang['AdminStaff.add.mobile_ticket_emails'] = 'Recibir entradas por correo electrónico';
+$lang['AdminStaff.add.ticket_messenger_notifications'] = 'Recibir notificaciones de Ticket Messenger';
+$lang['AdminStaff.add.title'] = 'Título';
+$lang['AdminStaff.add.signature'] = 'Firma';
+$lang['AdminStaff.add.text_assigneddepartments'] = 'Departamentos asignados';
+$lang['AdminStaff.add.text_availabledepartments'] = 'Departamentos disponibles';
+$lang['AdminStaff.add.field_addsubmit'] = 'Añadir personal';
+$lang['AdminStaff.edit.boxtitle_editstaff'] = 'Editar Personal %1$s %2$s';
+$lang['AdminStaff.edit.heading_schedule'] = 'Horario';
+$lang['AdminStaff.edit.heading_settings'] = 'Ajustes';
+$lang['AdminStaff.edit.heading_departments'] = 'Departamentos';
+$lang['AdminStaff.edit.heading_day'] = 'Día';
+$lang['AdminStaff.edit.heading_all_day'] = 'Todo el día';
+$lang['AdminStaff.edit.heading_start_time'] = 'Hora de inicio';
+$lang['AdminStaff.edit.heading_end_time'] = 'Fin de los tiempos';
+$lang['AdminStaff.edit.ticket_emails'] = 'Recibir entradas por correo electrónico';
+$lang['AdminStaff.edit.mobile_ticket_emails'] = 'Recibir entradas por correo electrónico';
+$lang['AdminStaff.edit.ticket_messenger_notifications'] = 'Recibir notificaciones de Ticket Messenger';
+$lang['AdminStaff.edit.title'] = 'Título';
+$lang['AdminStaff.edit.signature'] = 'Firma';
+$lang['AdminStaff.edit.text_assigneddepartments'] = 'Departamentos asignados';
+$lang['AdminStaff.edit.text_availabledepartments'] = 'Departamentos disponibles';
+$lang['AdminStaff.edit.field_editsubmit'] = 'Editar Personal';
+$lang['AdminStaff.staff.name'] = '%1$s %2$s';
+$lang['AdminStaff.schedule_list.heading_schedule'] = 'Horario';
+$lang['AdminStaff.schedule_list.heading_day'] = 'Día';
+$lang['AdminStaff.schedule_list.heading_start_time'] = 'Hora de inicio';
+$lang['AdminStaff.schedule_list.heading_end_time'] = 'Fin de los tiempos';
+$lang['AdminStaff.schedule_list.no_results'] = 'Este miembro del personal no está programado para ningún día.';
+$lang['AdminStaff.schedule_list.heading_departments'] = 'Departamentos';
+$lang['AdminStaff.schedule_list.heading_department'] = 'Departamento';
+$lang['AdminStaff.schedule_list.heading_description'] = 'Descripción';
+$lang['AdminStaff.schedule_list.no_department_results'] = 'Este funcionario no está asignado a ningún departamento.';
+$lang['AdminStaff.text.no_time'] = '--';
+
