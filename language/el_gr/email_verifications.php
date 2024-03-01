ac907f768490ef41c0b6662747b50e39d336869a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:17 -0800

    Updated el_gr language files

---
+++ language/el_gr/email_verifications.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Email Verifications
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['EmailVerifications.!error.contact_id.exists'] = 'Μη έγκυρο αναγνωριστικό επαφής.';
+$lang['EmailVerifications.!error.email.valid'] = 'Παρακαλούμε εισάγετε μια έγκυρη διεύθυνση ηλεκτρονικού ταχυδρομείου.';
+$lang['EmailVerifications.!error.redirect_url.valid'] = 'Παρακαλούμε εισάγετε ένα έγκυρο URL.';
+$lang['EmailVerifications.!error.token.format'] = 'Παρακαλούμε εισάγετε ένα έγκυρο διακριτικό.';
+$lang['EmailVerifications.!error.verified.valid'] = 'Το επαληθευμένο πεδίο πρέπει να είναι "1" ή "0".';
+
