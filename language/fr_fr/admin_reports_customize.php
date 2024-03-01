6124fa53f88c584e008a1b64bf01254b1f3868ba
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:57 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_reports_customize.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Admin Reports Customize
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminReportsCustomize.!success.added'] = 'Le rapport a été ajouté avec succès.';
+$lang['AdminReportsCustomize.!success.edited'] = 'Le rapport a été mis à jour avec succès.';
+$lang['AdminReportsCustomize.!success.deleted'] = 'Le rapport a été supprimé avec succès.';
+$lang['AdminReportsCustomize.index.box_title'] = 'Rapports';
+$lang['AdminReportsCustomize.index.reports'] = 'Générer';
+$lang['AdminReportsCustomize.index.customize'] = 'Personnaliser';
+$lang['AdminReportsCustomize.index.page_title'] = 'Rapports de facturation';
+$lang['AdminReportsCustomize.index.table_name'] = 'Nom';
+$lang['AdminReportsCustomize.index.table_created'] = 'Date de création';
+$lang['AdminReportsCustomize.index.table_options'] = 'Options';
+$lang['AdminReportsCustomize.index.table_option_edit'] = 'Editer';
+$lang['AdminReportsCustomize.index.table_option_delete'] = 'Supprimer';
+$lang['AdminReportsCustomize.index.table_confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce rapport ?';
+$lang['AdminReportsCustomize.index.btn_add_report'] = 'Ajouter un rapport';
+$lang['AdminReportsCustomize.index.no_custom_reports'] = 'Il n\'y a pas de rapports personnalisés.';
+$lang['AdminReportsCustomize.add.page_title'] = 'Rapports de facturation';
+$lang['AdminReportsCustomize.add.field_name'] = 'Nom';
+$lang['AdminReportsCustomize.add.field_query'] = 'Demande de renseignements';
+$lang['AdminReportsCustomize.add.heading_fields'] = 'Domaines';
+$lang['AdminReportsCustomize.add.field_add_field'] = 'Ajouter un champ';
+$lang['AdminReportsCustomize.add.table_name'] = 'Nom';
+$lang['AdminReportsCustomize.add.table_label'] = 'Étiquette';
+$lang['AdminReportsCustomize.add.table_type'] = 'Type';
+$lang['AdminReportsCustomize.add.table_required'] = 'Exigée';
+$lang['AdminReportsCustomize.add.table_options'] = 'Options';
+$lang['AdminReportsCustomize.add.field_values'] = 'Valeurs (séparées par des virgules)';
+$lang['AdminReportsCustomize.add.field_regex'] = 'Regex personnalisées';
+$lang['AdminReportsCustomize.add.text_remove'] = 'Retirer';
+$lang['AdminReportsCustomize.add.field_submit'] = 'Économiser';
+$lang['AdminReportsCustomize.edit.page_title'] = 'Rapports de facturation';
+$lang['AdminReportsCustomize.edit.field_name'] = 'Nom';
+$lang['AdminReportsCustomize.edit.field_query'] = 'Demande de renseignements';
+$lang['AdminReportsCustomize.edit.heading_fields'] = 'Domaines';
+$lang['AdminReportsCustomize.edit.field_add_field'] = 'Ajouter un champ';
+$lang['AdminReportsCustomize.edit.table_name'] = 'Nom';
+$lang['AdminReportsCustomize.edit.table_label'] = 'Étiquette';
+$lang['AdminReportsCustomize.edit.table_type'] = 'Type';
+$lang['AdminReportsCustomize.edit.table_required'] = 'Exigée';
+$lang['AdminReportsCustomize.edit.table_options'] = 'Options';
+$lang['AdminReportsCustomize.edit.field_values'] = 'Valeurs (séparées par des virgules)';
+$lang['AdminReportsCustomize.edit.field_regex'] = 'Regex personnalisées';
+$lang['AdminReportsCustomize.edit.text_remove'] = 'Retirer';
+$lang['AdminReportsCustomize.edit.field_submit'] = 'Économiser';
+
