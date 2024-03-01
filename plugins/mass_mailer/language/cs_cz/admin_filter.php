65f2bab7fbabec2efe63f78780305cbcb40f43b2
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:52 -0800

    Updated cs_cz language files

---
+++ plugins/mass_mailer/language/cs_cz/admin_filter.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Admin Filter
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminFilter.index.form.export'] = 'Export';
+$lang['AdminFilter.index.form.continue'] = 'Pokračovat';
+$lang['AdminFilter.index.form.include_all_services'] = 'Zahrnout pro všechny služby';
+$lang['AdminFilter.index.form.module_id'] = 'Modul';
+$lang['AdminFilter.index.form.selectable_packages'] = 'Dostupné balíčky';
+$lang['AdminFilter.index.form.selected_packages'] = 'Vybrané balíčky';
+$lang['AdminFilter.index.form.package_group'] = 'Filtr skupiny balíčků';
+$lang['AdminFilter.index.form.service_parent_type_module'] = 'Modul';
+$lang['AdminFilter.index.form.service_parent_type_package'] = 'Balíček';
+$lang['AdminFilter.index.form.service_parent_type'] = 'Balíček/modul';
+$lang['AdminFilter.index.form.service_statuses'] = 'Statusy služby';
+$lang['AdminFilter.index.form.service_renew_date'] = 'Obnovuje podle';
+$lang['AdminFilter.index.heading.service_filters'] = 'Možnosti filtrování služeb';
+$lang['AdminFilter.index.form.filter_services'] = 'Filtrování podle služeb';
+$lang['AdminFilter.index.form.receive_email_marketing'] = 'Omezení na prodejné klienty';
+$lang['AdminFilter.index.form.client_end_date'] = 'Datum ukončení vytvoření klienta';
+$lang['AdminFilter.index.form.client_start_date'] = 'Datum zahájení vytvoření klienta';
+$lang['AdminFilter.index.form.contact_types'] = 'Typy kontaktů';
+$lang['AdminFilter.index.form.languages'] = 'Jazyky';
+$lang['AdminFilter.index.form.client_statuses'] = 'Statuty klientů';
+$lang['AdminFilter.index.form.client_group_ids'] = 'Skupiny klientů';
+$lang['AdminFilter.index.heading.client_filters'] = 'Možnosti filtrování klientů';
+$lang['AdminFilter.index.boxtitle'] = 'Hromadný rozesílač';
+$lang['AdminFilter.!tooltip.receive_email_marketing'] = 'Pokud je tato možnost zaškrtnuta, budou tento e-mail dostávat pouze klienti, kteří se přihlásili k odběru marketingových nabídek.';
+$lang['AdminFilter.!tooltip.include_all_services'] = 'Pokud je tato možnost zaškrtnuta, budou zahrnuty všechny služby. Kontakty případně obdrží více e-mailů; jeden pro každou odpovídající službu. Pokud tato možnost není zaškrtnuta, kontakty obdrží pouze jeden e-mail, který bude obsahovat první odpovídající službu.';
+$lang['AdminFilter.!tooltip.selected_packages'] = 'Služby musí patřit do jednoho z vybraných balíčků.';
+$lang['AdminFilter.!tooltip.service_parent_type'] = 'Služby budou dále filtrovány podle balíčku nebo modulu.';
+$lang['AdminFilter.!tooltip.module_id'] = 'Služby musí patřit do vybraného modulu.';
+$lang['AdminFilter.!tooltip.service_statuses'] = 'Služby musí být v jednom z vybraných stavů.';
+$lang['AdminFilter.!tooltip.service_renew_date'] = 'Služby musí být obnoveny k tomuto datu nebo před ním. Chcete-li toto pole ignorovat, nechte ho prázdné.';
+$lang['AdminFilter.!tooltip.filter_services'] = 'Pokud je tato možnost zaškrtnuta, musí klienti také splňovat kritéria filtrování služeb.';
+$lang['AdminFilter.!tooltip.client_end_date'] = 'Klienti musí být vytvořeni k tomuto datu nebo před ním. Chcete-li toto pole ignorovat, nechte ho prázdné.';
+$lang['AdminFilter.!tooltip.client_start_date'] = 'Klienti musí být vytvořeni k tomuto datu nebo po něm. Chcete-li toto pole ignorovat, nechte ho prázdné.';
+$lang['AdminFilter.!tooltip.contact_types'] = 'Kontakty musí patřit k jednomu z vybraných typů.';
+$lang['AdminFilter.!tooltip.languages'] = 'Klienti musí mít nastaven jeden z vybraných jazyků.';
+$lang['AdminFilter.!tooltip.client_statuses'] = 'Klienti musí patřit do jednoho z vybraných stavů.';
+$lang['AdminFilter.!tooltip.client_group_ids'] = 'Klienti musí patřit do jedné z vybraných skupin.';
+$lang['AdminFilter.text.all'] = 'Všechny';
+$lang['AdminFilter.index.page_title'] = 'Hromadný rozesílač > Filtr';
+
