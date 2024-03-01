541eb26f0b36e9e2a16d6a8e0d3b73306147afca
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:58 -0800

    Updated es_es language files

---
+++ plugins/domains/language/es_es/client_main.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.index.page_title'] = 'Cliente #%1$s Dominios';
+$lang['ClientMain.index.boxtitle_domains'] = 'Dominios';
+$lang['ClientMain.index.category_active'] = 'Activo';
+$lang['ClientMain.index.category_pending'] = 'Pendiente';
+$lang['ClientMain.index.category_suspended'] = 'Suspendido';
+$lang['ClientMain.index.category_deleted'] = 'Suprimido';
+$lang['ClientMain.index.heading_addons'] = 'Complementos';
+$lang['ClientMain.index.heading_status'] = 'Estado';
+$lang['ClientMain.index.heading_domain'] = 'Dominio';
+$lang['ClientMain.index.heading_term'] = 'Plazo';
+$lang['ClientMain.index.heading_registration_date'] = 'Fecha de inscripción';
+$lang['ClientMain.index.heading_renewal_date'] = 'Fecha de renovación';
+$lang['ClientMain.index.heading_expiration_date'] = 'Fecha de expiración';
+$lang['ClientMain.index.heading_suspension_date'] = 'Fecha de suspensión';
+$lang['ClientMain.index.heading_deletion_date'] = 'Fecha de supresión';
+$lang['ClientMain.index.heading_options'] = 'Opciones';
+$lang['ClientMain.index.option_manage'] = 'Gestione';
+$lang['ClientMain.index.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['ClientMain.index.text_never'] = 'Nunca';
+$lang['ClientMain.index.no_results'] = 'No tiene %1$s Dominios.';
+$lang['ClientMain.serviceinfo.no_results'] = 'Este dominio no tiene detalles.';
+$lang['ClientMain.serviceinfo.parent_service'] = 'Este dominio pertenece a <a href="%1$s">%2$s (%3$s)</a>. Si se cancela ese servicio, también se cancelará este dominio.';
+$lang['ClientMain.serviceinfo.cancellation_reason'] = 'Motivo de la cancelación: %1$s';
+$lang['ClientMain.getfilters.any'] = 'Cualquier';
+$lang['ClientMain.getfilters.field_module_id'] = 'Módulos del registrador';
+$lang['ClientMain.getfilters.field_package_name'] = 'TLD';
+$lang['ClientMain.getfilters.field_service_meta'] = 'Nombre de dominio';
+
