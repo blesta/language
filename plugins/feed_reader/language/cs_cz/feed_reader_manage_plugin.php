61cbf873104efb44fc9b34dc6733ff9d7da8f46d
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:46 -0800

    Updated cs_cz language files

---
+++ plugins/feed_reader/language/cs_cz/feed_reader_manage_plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Feed Reader Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FeedReaderPlugin.add.submit_cancel'] = 'Zrušit';
+$lang['FeedReaderPlugin.add.submit_add'] = 'Přidat krmivo';
+$lang['FeedReaderPlugin.add.field_url'] = 'ADRESA URL';
+$lang['FeedReaderManagePlugin.add.boxtitle_feed'] = 'Přidání krmiva';
+$lang['FeedReaderManagePlugin.add.page_title'] = 'Čtečka kanálů > Přidat kanál';
+$lang['FeedReaderManagePlugin.index.text_confirm_delete'] = 'Opravdu smazat tento kanál?';
+$lang['FeedReaderManagePlugin.index.no_feeds'] = 'Neexistují žádné výchozí kanály.';
+$lang['FeedReaderManagePlugin.index.option_remove'] = 'Odstranění stránky';
+$lang['FeedReaderManagePlugin.index.option_refresh'] = 'Obnovit';
+$lang['FeedReaderManagePlugin.index.heading_options'] = 'Možnosti';
+$lang['FeedReaderManagePlugin.index.heading_lastupdated'] = 'Poslední aktualizace';
+$lang['FeedReaderManagePlugin.index.heading_url'] = 'ADRESA URL';
+$lang['FeedReaderManagePlugin.index.add_feed'] = 'Přidat krmivo';
+$lang['FeedReaderManagePlugin.index.boxtitle_feedreader'] = 'Čtečka krmiv';
+$lang['FeedReaderManagePlugin.index.page_title'] = 'Čtečka kanálů > Správa';
+$lang['FeedReaderManagePlugin.!success.feed_removed'] = 'Krmivo bylo úspěšně odstraněno.';
+$lang['FeedReaderManagePlugin.!success.feed_refreshed'] = 'Kanál byl úspěšně obnoven.';
+$lang['FeedReaderManagePlugin.!success.feed_added'] = 'Krmivo bylo úspěšně přidáno.';
+
