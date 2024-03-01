9de55e685883713ffd3f01def8e4f25c54506322
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:15 -0800

    Updated de_de language files

---
+++ core/Util/PackageOptions/language/de_de/logic.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Logic
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.packageoptions.logic.invalid_option'] = 'Es wurden keine Bedingungen erfüllt, um die konfigurierbare Option %1$s zuzulassen. Sie sollte nicht übermittelt werden.';
+
