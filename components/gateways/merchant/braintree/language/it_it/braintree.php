5d2d2ed684dfd4704a0281878722e49acae5e725
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:07 -0800

    Updated it_it language files

---
+++ components/gateways/merchant/braintree/language/it_it/braintree.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Braintree
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Braintree.!error.environment'] = 'Selezionare un ambiente valido.';
+$lang['Braintree.!error.private_key'] = 'Inserire la chiave privata.';
+$lang['Braintree.!error.public_key'] = 'Inserire la propria chiave pubblica.';
+$lang['Braintree.!error.merchant_id'] = 'Inserire il proprio ID commerciante.';
+$lang['Braintree.!error.live_mode.valid'] = 'La modalità live deve essere impostata su "true", se indicata.';
+$lang['Braintree.environment.production'] = 'Produzione';
+$lang['Braintree.environment.sandbox'] = 'Sandbox';
+$lang['Braintree.environment'] = 'Ambiente';
+$lang['Braintree.live_mode'] = 'Modalità live';
+$lang['Braintree.private_key'] = 'Chiave privata';
+$lang['Braintree.public_key'] = 'Chiave pubblica';
+$lang['Braintree.merchant_id'] = 'ID commerciante';
+$lang['Braintree.description'] = 'I commercianti di oltre 45 paesi/regioni in tutto il mondo possono accettare, suddividere e abilitare pagamenti in più di 130 valute utilizzando Braintree.';
+$lang['Braintree.name'] = 'Braintree';
+
