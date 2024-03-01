64162c6ba7abe57d7f39f967d90faba5ad760b7c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:47 -0800

    Updated it_it language files

---
+++ plugins/extension_generator/language/it_it/extension_generator_controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Extension Generator Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorController.gettasktypes.interval'] = 'Intervallo';
+$lang['ExtensionGeneratorController.gettasktypes.time'] = 'Tempo';
+$lang['ExtensionGeneratorController.gettablevels.client'] = 'Cliente';
+$lang['ExtensionGeneratorController.gettablevels.staff'] = 'Personale';
+$lang['ExtensionGeneratorController.getfieldtypes.checkbox'] = 'Casella di controllo';
+$lang['ExtensionGeneratorController.getfieldtypes.textarea'] = 'Area di testo';
+$lang['ExtensionGeneratorController.getfieldtypes.text'] = 'Testo';
+$lang['ExtensionGeneratorController.getnodes.confirm'] = 'Conferma';
+$lang['ExtensionGeneratorController.getnodes.additional_features'] = 'Caratteristiche aggiuntive';
+$lang['ExtensionGeneratorController.getnodes.nonmerchant_fields'] = 'Campi di configurazione';
+$lang['ExtensionGeneratorController.getnodes.merchant_features'] = 'Caratteristiche supportate';
+$lang['ExtensionGeneratorController.getnodes.merchant_fields'] = 'Campi di configurazione';
+$lang['ExtensionGeneratorController.getnodes.module_fields'] = 'Campi del modulo';
+$lang['ExtensionGeneratorController.getnodes.plugin_integrations'] = 'Integrazioni di base';
+$lang['ExtensionGeneratorController.getnodes.plugin_database'] = 'Info sul database';
+$lang['ExtensionGeneratorController.getnodes.basic_info'] = 'Informazioni di base';
+$lang['ExtensionGeneratorController.getnodes.general_settings'] = 'Impostazioni generali';
+
