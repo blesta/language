8a405a73314202926416e4b88e3b3845cad70b73
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:39 -0800

    Updated el_gr language files

---
+++ language/el_gr/actions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Actions
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Actions.getLocationDescriptions.action_staff_client'] = 'Εμφανίζει έναν σύνδεσμο στην ενότητα "Ενέργειες λογαριασμού" της σελίδας προφίλ πελάτη του προσωπικού.';
+$lang['Actions.getLocationDescriptions.widget_staff_billing'] = 'Εμφανίζει ένα widget στη σελίδα χρέωσης προσωπικού.';
+$lang['Actions.getLocationDescriptions.widget_staff_client'] = 'Παρουσιάζει ένα widget στη σελίδα προφίλ πελάτη του προσωπικού.';
+$lang['Actions.getLocationDescriptions.widget_staff_home'] = 'Εμφανίζει ένα widget στο ταμπλό του προσωπικού.';
+$lang['Actions.getLocationDescriptions.widget_client_home'] = '	Παρουσιάζει ένα widget στο ταμπλό του πελάτη.';
+$lang['Actions.getLocationDescriptions.nav_public'] = 'Διαθέτει έναν σύνδεσμο για την πλοήγηση στη δημόσια διεπαφή.';
+$lang['Actions.getLocationDescriptions.nav_client'] = 'Διαθέτει έναν σύνδεσμο στην πλοήγηση της διεπαφής πελάτη.';
+$lang['Actions.getLocationDescriptions.nav_staff'] = 'Διαθέτει έναν σύνδεσμο στην πλοήγηση της διεπαφής διαχείρισης.';
+$lang['Actions.getLocations.action_staff_client'] = 'Σύνδεσμος "Ενέργειες λογαριασμού" πελάτη';
+$lang['Actions.getLocations.widget_staff_billing'] = 'Widget χρέωσης προσωπικού';
+$lang['Actions.getLocations.widget_staff_client'] = 'Widget προφίλ πελάτη προσωπικού';
+$lang['Actions.getLocations.widget_staff_home'] = 'Widget ταμπλό προσωπικού';
+$lang['Actions.getLocations.widget_client_home'] = 'Widget ταμπλό πελάτη';
+$lang['Actions.getLocations.nav_public'] = 'Δημόσιο μενού πλοήγησης';
+$lang['Actions.getLocations.nav_client'] = 'Μενού πλοήγησης πελάτη';
+$lang['Actions.getLocations.nav_staff'] = 'Μενού πλοήγησης προσωπικού';
+$lang['Actions.!error.enabled.valid'] = 'Η επιλογή enabled πρέπει να έχει την τιμή 1 ή 0.';
+$lang['Actions.!error.editable.valid'] = 'Η επιλογή editable (επεξεργάσιμο) πρέπει να έχει την τιμή 1 ή 0.';
+$lang['Actions.!error.company_id.valid'] = 'Μη έγκυρο αναγνωριστικό εταιρείας';
+$lang['Actions.!error.plugin_id.valid'] = 'Παρακαλούμε δώστε ένα έγκυρο αναγνωριστικό πρόσθετου.';
+$lang['Actions.!error.name.action_empty'] = 'Παρακαλούμε εισάγετε ένα όνομα δράσης.';
+$lang['Actions.!error.url.empty'] = 'Παρακαλούμε εισάγετε μια διεύθυνση URL.';
+$lang['Actions.!error.location.unique'] = 'Η τοποθεσία και η διεύθυνση URL πρέπει να είναι μοναδικές για την εταιρεία αυτή.';
+$lang['Actions.!error.location.valid'] = 'Μη έγκυρη έγκυρη τοποθεσία.';
+
