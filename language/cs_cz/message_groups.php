038d19967d9008662327ed55ff3660b0d8afb167
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:04 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/message_groups.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Message Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MessageGroups.getTypes.shared'] = 'Sdílené stránky';
+$lang['MessageGroups.getTypes.client'] = 'Klient';
+$lang['MessageGroups.getTypes.staff'] = 'Zaměstnanci';
+$lang['MessageGroups.!error.tags.empty'] = 'Zadejte prosím značky.';
+$lang['MessageGroups.!error.plugin_dir.empty'] = 'Zadejte adresář zásuvného modulu.';
+$lang['MessageGroups.!error.type.valid'] = 'Typ skupiny zpráv musí být nastaven na "client", "staff" nebo "shared".';
+$lang['MessageGroups.!error.action.empty'] = 'Zadejte prosím akci.';
+
