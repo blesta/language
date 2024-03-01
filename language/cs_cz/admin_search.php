dfe56e16745ce0ba1828afbb21ff1bd648d1eaab
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:34 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_search.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Admin Search
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSearch.services.no_results'] = 'Vyhledávaným kritériím neodpovídají žádné služby.';
+$lang['AdminSearch.services.text_never'] = 'Nikdy';
+$lang['AdminSearch.services.heading_status'] = 'Stav';
+$lang['AdminSearch.services.heading_date_renews'] = 'Datum obnovení';
+$lang['AdminSearch.services.heading_date_added'] = 'Datum vytvoření';
+$lang['AdminSearch.services.heading_term'] = 'Termín';
+$lang['AdminSearch.services.heading_package'] = 'Balíček';
+$lang['AdminSearch.services.heading_client'] = 'Klient';
+$lang['AdminSearch.services.heading_client_id'] = 'ID klienta';
+$lang['AdminSearch.services.heading'] = 'Služby';
+$lang['AdminSearch.packages.no_results'] = 'Neexistují žádné balíčky, které by odpovídaly kritériím vyhledávání.';
+$lang['AdminSearch.packages.heading_status'] = 'Stav';
+$lang['AdminSearch.packages.heading_quantity'] = 'Množství';
+$lang['AdminSearch.packages.heading_module'] = 'Modul';
+$lang['AdminSearch.packages.heading_name'] = 'Název';
+$lang['AdminSearch.packages.heading_package_id'] = 'ID balíčku';
+$lang['AdminSearch.packages.heading'] = 'Balíčky';
+$lang['AdminSearch.transactions.no_results'] = 'Vyhledávacím kritériím neodpovídají žádné transakce.';
+$lang['AdminSearch.transactions.option_edit'] = 'Upravit';
+$lang['AdminSearch.transactions.heading_options'] = 'Možnosti';
+$lang['AdminSearch.transactions.heading_date'] = 'Datum';
+$lang['AdminSearch.transactions.heading_status'] = 'Stav';
+$lang['AdminSearch.transactions.heading_number'] = 'Číslo';
+$lang['AdminSearch.transactions.heading_applied'] = 'Aplikované';
+$lang['AdminSearch.transactions.heading_credited'] = 'Připsáno na';
+$lang['AdminSearch.transactions.heading_amount'] = 'Částka';
+$lang['AdminSearch.transactions.heading_type'] = 'Typ';
+$lang['AdminSearch.transactions.heading_client_id'] = 'ID klienta';
+$lang['AdminSearch.transactions.heading'] = 'Transakce';
+$lang['AdminSearch.invoice.no_results'] = 'Vyhledávacím kritériím neodpovídají žádné faktury.';
+$lang['AdminSearch.invoices.status_unsent'] = 'Neodeslané';
+$lang['AdminSearch.invoices.status_sent'] = 'Odesláno';
+$lang['AdminSearch.invoices.status_draft'] = 'Návrh';
+$lang['AdminSearch.invoices.status_void'] = 'Zrušená platnost';
+$lang['AdminSearch.invoices.status_closed'] = 'Uzavřeno';
+$lang['AdminSearch.invoices.status_open'] = 'Otevřít';
+$lang['AdminSearch.invoice.option_view'] = 'Zobrazit';
+$lang['AdminSearch.invoice.heading_status'] = 'Stav';
+$lang['AdminSearch.invoice.heading_sent'] = 'Odesláno';
+$lang['AdminSearch.invoice.heading_date_due'] = 'Datum splatnosti';
+$lang['AdminSearch.invoice.heading_date_billed'] = 'Datum vyúčtování';
+$lang['AdminSearch.invoice.heading_date_closed'] = 'Datum uzavření';
+$lang['AdminSearch.invoice.heading_due'] = 'Due';
+$lang['AdminSearch.invoice.heading_paid'] = 'Placené';
+$lang['AdminSearch.invoice.heading_amount'] = 'Možnosti';
+$lang['AdminSearch.invoice.heading_client_id'] = 'ID klienta';
+$lang['AdminSearch.invoice.heading_invoice'] = 'Faktura #';
+$lang['AdminSearch.invoices.heading'] = 'Faktury';
+$lang['AdminSearch.clients.no_results'] = 'Neexistují žádní klienti, kteří by odpovídali kritériím vyhledávání.';
+$lang['AdminSearch.clients.heading_email'] = 'E-mail';
+$lang['AdminSearch.clients.heading_address'] = 'Adresa';
+$lang['AdminSearch.clients.heading_company'] = 'Společnost';
+$lang['AdminSearch.clients.heading_name'] = 'Název';
+$lang['AdminSearch.clients.heading_group'] = 'Skupina';
+$lang['AdminSearch.clients.heading_client_id'] = 'ID klienta';
+$lang['AdminSearch.clients.heading'] = 'Klienti';
+$lang['AdminSearch.index.boxtitle_search'] = 'Výsledky hledání pro "%1$s"';
+$lang['AdminSearch.index.page_title'] = 'Výsledky hledání pro "%1$s"';
+$lang['AdminSearch.!notice.results_truncated'] = 'Počet výsledků byl zkrácen. Možná budete chtít vyhledávání upřesnit.';
+
