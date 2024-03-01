732f2e0359bf2726e048af17854ae72e59d238b5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:59 -0800

    Updated it_it language files

---
+++ core/Automation/Tasks/Task/language/it_it/backups_sftp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Backups Sftp
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.backups_sftp.attempt'] = 'Tentativo di backup del database tramite SFTP.';
+$lang['Automation.task.backups_sftp.completed'] = 'L\'attività di backup del database SFTP è stata completata.';
+$lang['Automation.task.backups_sftp.success'] = 'Il backup è stato completato con successo.';
+
