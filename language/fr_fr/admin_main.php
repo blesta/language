2274c92470c210f34cb6fc034a61181e5c7b7f9c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:54 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_main.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!error.delete_event.staff_id'] = 'Seul le créateur de l\'événement peut supprimer cet événement.';
+$lang['AdminMain.!error.event_editable'] = 'Seul le créateur de l\'événement peut supprimer cet événement.';
+$lang['AdminMain.!success.event_added'] = 'L\'événement a été ajouté avec succès !';
+$lang['AdminMain.!success.event_edited'] = 'L\'événement a été mis à jour avec succès !';
+$lang['AdminMain.!success.event_deleted'] = 'L\'événement a été supprimé avec succès !';
+$lang['AdminMain.index.page_title'] = 'Tableau de bord';
+$lang['AdminMain.index.heading_quicklinks'] = 'Liens rapides';
+$lang['AdminMain.index.no_quicklinks'] = 'Aucun lien rapide n\'a été défini.';
+$lang['AdminMain.index.manage_widgets'] = 'Gérer les widgets';
+$lang['AdminMain.index.customize_dashboard'] = 'Personnaliser le tableau de bord';
+$lang['AdminMain.managewidgets.text_widgets'] = 'Faites glisser les widgets de la droite vers la gauche pour les ajouter à votre tableau de bord, ou de la gauche vers la droite pour les supprimer.';
+$lang['AdminMain.managewidgets.text_version'] = 'ver %1$s';
+$lang['AdminMain.managewidgets.text_author'] = 'Auteur :';
+$lang['AdminMain.updatedashboard.text_layout'] = 'Sélectionnez une présentation ci-dessous pour personnaliser votre tableau de bord.';
+$lang['AdminMain.calendar.page_title'] = 'Calendrier';
+$lang['AdminMain.calendar.boxtitle_calendar'] = 'Calendrier';
+$lang['AdminMain.calendar.categorylink_addevent'] = 'Créer un événement';
+$lang['AdminMain.calendar.category_month'] = 'Mois';
+$lang['AdminMain.calendar.category_week'] = 'Semaine';
+$lang['AdminMain.calendar.category_day'] = 'Jour';
+$lang['AdminMain.getEvents.shared_event_title'] = '%2$s %3$s : %1$s';
+$lang['AdminMain.addevent.boxtitle_addevent'] = 'Créer un événement';
+$lang['AdminMain.addevent.field_title'] = 'Titre';
+$lang['AdminMain.addevent.field_start_date'] = 'Date de début';
+$lang['AdminMain.addevent.field_end_date'] = 'Date de fin';
+$lang['AdminMain.addevent.field_shared'] = 'Rendre cet événement visible par d\'autres membres du personnel';
+$lang['AdminMain.addevent.field_all_day'] = 'Toute la journée';
+$lang['AdminMain.addevent.field_addeventsubmit'] = 'Créer un événement';
+$lang['AdminMain.editevent.boxtitle_editevent'] = 'Modifier l\'événement';
+$lang['AdminMain.editevent.field_title'] = 'Titre';
+$lang['AdminMain.editevent.field_start_date'] = 'Date de début';
+$lang['AdminMain.editevent.field_end_date'] = 'Date de fin';
+$lang['AdminMain.editevent.field_shared'] = 'Rendre cet événement visible par d\'autres membres du personnel';
+$lang['AdminMain.editevent.field_all_day'] = 'Toute la journée';
+$lang['AdminMain.editevent.field_editeventsubmit'] = 'Événement de mise à jour';
+$lang['AdminMain.editevent.field_deleteeventsubmit'] = 'Supprimer un événement';
+$lang['AdminMain.editevent.confirm_delete'] = 'Supprimer vraiment cet événement ?';
+
