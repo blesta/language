a428e1851d3a34144d39cb61725e40e1ea9fbb65
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:12 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/order_affiliate_referrals.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Order Affiliate Referrals
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliateReferrals.getStatuses.canceled'] = 'Abgesagt';
+$lang['OrderAffiliateReferrals.getStatuses.mature'] = 'Ausgereift';
+$lang['OrderAffiliateReferrals.getStatuses.pending'] = 'Anhängig';
+$lang['OrderAffiliateReferrals.!error.date_updated.format'] = 'Das Aktualisierungsdatum hat ein ungültiges Datumsformat.';
+$lang['OrderAffiliateReferrals.!error.date_added.format'] = 'Das hinzugefügte Datum hat ein ungültiges Datumsformat.';
+$lang['OrderAffiliateReferrals.!error.commission.format'] = 'Die Vermittlungsprovision muss eine Zahl sein.';
+$lang['OrderAffiliateReferrals.!error.currency.length'] = 'Der Währungscode muss 3 Zeichen lang sein.';
+$lang['OrderAffiliateReferrals.!error.amount.format'] = 'Der Überweisungsbetrag muss eine Zahl sein.';
+$lang['OrderAffiliateReferrals.!error.status.valid'] = 'Bitte wählen Sie einen gültigen Überweisungsstatus.';
+$lang['OrderAffiliateReferrals.!error.name.length'] = 'Der Name des Referrals muss weniger als 255 Zeichen enthalten.';
+$lang['OrderAffiliateReferrals.!error.order_id.exists'] = 'Ungültige Bestell-ID.';
+$lang['OrderAffiliateReferrals.!error.affiliate_id.exists'] = 'Ungültige Partner-ID.';
+$lang['OrderAffiliateReferrals.!error.referral_id.exists'] = 'Ungültige Überweisungs-ID.';
+
