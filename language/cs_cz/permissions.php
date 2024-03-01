64408c9fb8dbb7b301a66ee6f328c495094ed636
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:12 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/permissions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Permissions
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Permissions.!error.plugin_id.group_exists'] = 'Zásuvný modul zadaný pro tuto skupinu oprávnění neexistuje.';
+$lang['Permissions.!error.alias.group_empty'] = 'Skupina oprávnění musí mít alias.';
+$lang['Permissions.!error.level.group_empty'] = 'Úroveň musí být buď "staff", nebo "client".';
+$lang['Permissions.!error.name.group_empty'] = 'Skupina oprávnění musí mít název.';
+$lang['Permissions.!error.plugin_id.exists'] = 'Zásuvný modul zadaný pro toto oprávnění neexistuje.';
+$lang['Permissions.!error.action.empty'] = 'Povolení musí mít akci.';
+$lang['Permissions.!error.alias.empty'] = 'Oprávnění musí mít alias.';
+$lang['Permissions.!error.name.empty'] = 'Povolení musí mít název.';
+$lang['Permissions.!error.group_id.exists'] = 'Zadaná skupina oprávnění neexistuje.';
+
