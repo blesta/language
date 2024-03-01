a5cc2fcd63214753f11f5d86f78cae1e6f854638
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:14 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/client_verify.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Client Verify
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientVerify.!success.email_verified'] = 'Votre adresse électronique a été vérifiée avec succès.';
+$lang['ClientVerify.!success.email_sent'] = 'Un courriel de vérification a été envoyé à %1$s.';
+$lang['ClientVerify.!error.invalid_token'] = 'Le jeton fourni n\'est pas valide ou a expiré.';
+
