ce86253b43e58bdd73a46c6b526b0875c638794a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:16 -0800

    Updated cs_cz language files

---
+++ components/modules/apnscp/language/cs_cz/apnscp.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Apnscp
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Apnscp.!error.apnscp_password.matches'] = 'Heslo a potvrzovací heslo se neshodují.';
+$lang['Apnscp.!error.apnscp_password.valid'] = 'Heslo musí mít alespoň 8 znaků.';
+$lang['Apnscp.!error.apnscp_username.length'] = 'Uživatelské jméno musí mít délku 1 až 16 znaků.';
+$lang['Apnscp.!error.apnscp_username.test'] = 'Uživatelské jméno nesmí začínat slovem "test".';
+$lang['Apnscp.!error.apnscp_username.format'] = 'Uživatelské jméno může obsahovat pouze písmena a číslice a nesmí začínat číslicí.';
+$lang['Apnscp.!error.apnscp_domain.test'] = 'Název domény nemůže začínat na \'test\'.';
+$lang['Apnscp.!error.apnscp_domain.format'] = 'Zadejte platný název domény, např. domain.com.';
+$lang['Apnscp.!error.module_row.missing'] = 'Došlo k interní chybě. Řádek modulu není k dispozici.';
+$lang['Apnscp.!error.api.internal'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+$lang['Apnscp.!error.meta[package].empty'] = 'Je vyžadován balíček ApisCP.';
+$lang['Apnscp.!error.name_servers_count'] = 'Je třeba definovat alespoň 2 jmenné servery.';
+$lang['Apnscp.!error.name_servers_valid'] = 'Jeden nebo více zadaných názvových serverů jsou neplatné.';
+$lang['Apnscp.!error.account_limit_valid'] = 'Limit účtu musí zůstat prázdný (pro neomezený počet účtů) nebo musí být nastaven na nějakou celočíselnou hodnotu.';
+$lang['Apnscp.!error.api_key_valid_connection'] = 'Připojení k serveru se nepodařilo navázat. Zkontrolujte, zda jsou název hostitele, klíč API a port správné.';
+$lang['Apnscp.!error.api_key_valid'] = 'Klíč API se zdá být neplatný.';
+$lang['Apnscp.!error.port_format'] = 'Port musí být číslo.';
+$lang['Apnscp.!error.host_name_valid'] = 'Název hostitele se zdá být neplatný.';
+$lang['Apnscp.!error.server_name_valid'] = 'Je třeba zadat označení serveru.';
+$lang['Apnscp.service_field.tooltip.password'] = 'Heslo můžete nechat prázdné, aby se vygenerovalo automaticky.';
+$lang['Apnscp.service_field.tooltip.username'] = 'Uživatelské jméno můžete nechat prázdné, aby se vygenerovalo automaticky.';
+$lang['Apnscp.service_info.option_login'] = 'Přihlášení';
+$lang['Apnscp.service_info.options'] = 'Možnosti';
+$lang['Apnscp.service_info.server'] = 'Server';
+$lang['Apnscp.service_info.password'] = 'Heslo';
+$lang['Apnscp.service_info.username'] = 'Uživatelské jméno';
+$lang['Apnscp.tab_client_stats.usage_unlimited'] = '(%1$s MB/∞)';
+$lang['Apnscp.tab_client_stats.usage'] = '(%1$s MB/%2$s MB)';
+$lang['Apnscp.tab_client_stats.disk_title'] = 'Využití disku';
+$lang['Apnscp.tab_client_stats.bandwidth_title'] = 'Využití šířky pásma (měsíc k datu)';
+$lang['Apnscp.tab_client_stats.info_title'] = 'Informace';
+$lang['Apnscp.tab_client_actions.field_password_submit'] = 'Aktualizace hesla';
+$lang['Apnscp.tab_client_actions.field_apnscp_password'] = 'Heslo';
+$lang['Apnscp.tab_client_actions.change_password'] = 'Změna hesla';
+$lang['Apnscp.tab_stats.info.bandwidth'] = 'Kvóta šířky pásma';
+$lang['Apnscp.tab_stats.info.disk'] = 'Disková kvóta';
+$lang['Apnscp.tab_stats.info_heading.value'] = 'Hodnota';
+$lang['Apnscp.tab_stats.info_heading.field'] = 'Pole';
+$lang['Apnscp.tab_stats.info_title'] = 'Informace';
+$lang['Apnscp.service_field.password'] = 'Heslo';
+$lang['Apnscp.service_field.username'] = 'Uživatelské jméno';
+$lang['Apnscp.service_field.domain'] = 'Doména';
+$lang['Apnscp.package_fields.package'] = 'Balíček ApisCP';
+$lang['Apnscp.row_meta.account_limit'] = 'Limit účtu';
+$lang['Apnscp.row_meta.use_ssl'] = 'Při připojování k rozhraní API používejte protokol SSL (doporučeno).';
+$lang['Apnscp.row_meta.api_key'] = 'Klíč API';
+$lang['Apnscp.row_meta.default_port'] = '2083';
+$lang['Apnscp.row_meta.port'] = 'Přístav';
+$lang['Apnscp.row_meta.host_name'] = 'Hostitelské jméno';
+$lang['Apnscp.row_meta.server_name'] = 'Označení serveru';
+$lang['Apnscp.edit_row.add_btn'] = 'Upravit server';
+$lang['Apnscp.edit_row.remove_name_server'] = 'Odstranění stránky';
+$lang['Apnscp.edit_row.name_server'] = 'Name server %1$s';
+$lang['Apnscp.edit_row.name_server_host_col'] = 'Hostitelské jméno';
+$lang['Apnscp.edit_row.name_server_col'] = 'Jmenný server';
+$lang['Apnscp.edit_row.name_server_btn'] = 'Přidání dalšího jmenného serveru';
+$lang['Apnscp.edit_row.notes_title'] = 'Poznámky';
+$lang['Apnscp.edit_row.name_servers_title'] = 'Jmenné servery';
+$lang['Apnscp.edit_row.basic_title'] = 'Základní nastavení';
+$lang['Apnscp.edit_row.box_title'] = 'Upravit server ApisCP';
+$lang['Apnscp.add_row.add_btn'] = 'Přidat server';
+$lang['Apnscp.add_row.remove_name_server'] = 'Odstranění stránky';
+$lang['Apnscp.add_row.name_server'] = 'Name server %1$s';
+$lang['Apnscp.add_row.name_server_host_col'] = 'Hostitelské jméno';
+$lang['Apnscp.add_row.name_server_col'] = 'Jmenný server';
+$lang['Apnscp.add_row.name_server_btn'] = 'Přidání dalšího jmenného serveru';
+$lang['Apnscp.add_row.notes_title'] = 'Poznámky';
+$lang['Apnscp.add_row.name_servers_title'] = 'Jmenné servery';
+$lang['Apnscp.add_row.basic_title'] = 'Základní nastavení';
+$lang['Apnscp.add_row.box_title'] = 'Přidání serveru ApisCP';
+$lang['Apnscp.order_options.roundrobin'] = 'Rovnoměrné rozdělení mezi servery';
+$lang['Apnscp.order_options.first'] = 'První nenaplněný server';
+$lang['Apnscp.manage.module_groups_no_results'] = 'Neexistují žádné skupiny serverů.';
+$lang['Apnscp.manage.module_rows_no_results'] = 'Neexistují žádné servery.';
+$lang['Apnscp.manage.module_groups.confirm_delete'] = 'Opravdu chcete tuto skupinu serverů odstranit?';
+$lang['Apnscp.manage.module_rows.confirm_delete'] = 'Opravdu chcete tento server odstranit?';
+$lang['Apnscp.manage.module_groups.delete'] = 'Odstranit';
+$lang['Apnscp.manage.module_rows.delete'] = 'Odstranit';
+$lang['Apnscp.manage.module_groups.edit'] = 'Upravit';
+$lang['Apnscp.manage.module_rows.edit'] = 'Upravit';
+$lang['Apnscp.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Apnscp.manage.module_groups_heading.options'] = 'Možnosti';
+$lang['Apnscp.manage.module_groups_heading.servers'] = 'Počet serverů';
+$lang['Apnscp.manage.module_groups_heading.name'] = 'Název skupiny';
+$lang['Apnscp.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['Apnscp.manage.module_rows_heading.accounts'] = 'Účty';
+$lang['Apnscp.manage.module_rows_heading.hostname'] = 'Hostitelské jméno';
+$lang['Apnscp.manage.module_rows_heading.name'] = 'Označení serveru';
+$lang['Apnscp.manage.module_groups_title'] = 'Skupiny serverů';
+$lang['Apnscp.manage.module_rows_title'] = 'Servery';
+$lang['Apnscp.add_module_group'] = 'Přidání skupiny serverů';
+$lang['Apnscp.add_module_row'] = 'Přidat server';
+$lang['Apnscp.tab_client_actions'] = 'Akce';
+$lang['Apnscp.tab_client_stats'] = 'Statistiky';
+$lang['Apnscp.tab_stats'] = 'Statistiky';
+$lang['Apnscp.module_group.name'] = 'Skupina serverů';
+$lang['Apnscp.module_row_plural.name'] = 'Servery';
+$lang['Apnscp.module_row.name'] = 'Server';
+$lang['Apnscp.description'] = 'Dříve se nazýval APNSCP.';
+$lang['Apnscp.name'] = 'ApisCP';
+
