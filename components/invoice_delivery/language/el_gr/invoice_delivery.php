f6b105b26e02ca8d2954864843a9c7faf7dc361e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:38 -0800

    Updated el_gr language files

---
+++ components/invoice_delivery/language/el_gr/invoice_delivery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Invoice Delivery
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['InvoiceDelivery.!error.libxml_required'] = 'Οι επεκτάσεις libxml και simplexml απαιτούνται για την παράδοση τιμολογίων.';
+$lang['InvoiceDelivery.deliverinvoices.interfax_subject'] = 'Τιμολόγιο #%1$s';
+$lang['InvoiceDelivery.deliverinvoices.postalmethods_description'] = 'Τιμολόγιο #%1$s';
+
