bcecca0d54a6f9889add8ab09dbc3b1ea08b821b
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:36 -0800

    Updated cs_cz language files

---
+++ plugins/support_manager/language/cs_cz/admin_company_emails.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_reminder_desc'] = 'Oznámení odeslané v případě, že na tiket nebyla po určitou dobu doručena odpověď.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_reminder_name'] = 'Připomenutí zaměstnaneckých jízdenek';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_reminder_desc'] = 'Oznámení odeslané v případě, že na tiket nebyla po určitou dobu doručena odpověď.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_reminder_name'] = 'Připomenutí letenek';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_bounce_desc'] = 'Oznámení odeslané v případě, že se tiket nepodařilo otevřít e-mailem.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_bounce_name'] = 'Vstupenka Bounce';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_updated_desc'] = 'Oznámení odeslané při aktualizaci tipu.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_updated_name'] = 'Aktualizovaný lístek';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_received_desc'] = 'Oznámení zaslané uživateli při přijetí tipu.';
+$lang['AdminCompanyEmails.templates.SupportManager.ticket_received_name'] = 'Obdržená letenka';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_assigned_desc'] = 'Oznámení zaslané zaměstnanci, když mu byl přidělen tip.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_assigned_name'] = 'Přidělený zaměstnanecký lístek';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_mobile_desc'] = 'Mobilní upozornění zaslané zaměstnanci při aktualizaci tipu.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_mobile_name'] = 'Zaměstnanecký lístek aktualizován (mobilní telefon)';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_desc'] = 'Oznámení zaslané zaměstnanci při aktualizaci tipu.';
+$lang['AdminCompanyEmails.templates.SupportManager.staff_ticket_updated_name'] = 'Zaměstnanecký lístek aktualizován';
+
