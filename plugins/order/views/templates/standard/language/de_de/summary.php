099ca2af2e335b3378bc92c23d53723188d48bd1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:28 -0800

    Updated de_de language files

---
+++ plugins/order/views/templates/standard/language/de_de/summary.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Summary
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Summary.index.cart_empty'] = 'Es befinden sich keine Artikel in Ihrem Warenkorb.';
+$lang['Summary.index.coupon_btn'] = 'Einlösen';
+$lang['Summary.index.coupon'] = 'Coupon';
+$lang['Summary.index.have_coupon'] = 'Haben Sie einen Coupon?';
+$lang['Summary.index.checkout_btn'] = 'Kasse';
+$lang['Summary.index.login_btn'] = 'Anmeldung';
+$lang['Summary.index.field_password'] = 'Passwort';
+$lang['Summary.index.field_username'] = 'Benutzername';
+$lang['Summary.index.link_resetpassword'] = 'Mein Passwort zurücksetzen';
+$lang['Summary.index.login_link'] = 'Anmeldung';
+$lang['Summary.index.login_customer'] = 'Bestehender Kunde?';
+$lang['Summary.index.box_heading'] = 'Ihr Warenkorb';
+
