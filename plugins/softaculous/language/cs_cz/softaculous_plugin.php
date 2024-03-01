52b81709994b8a4d976b778bf0fa5cfd1378f99a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:35 -0800

    Updated cs_cz language files

---
+++ plugins/softaculous/language/cs_cz/softaculous_plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Softaculous Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SoftaculousPlugin.cron.script_installation_desc'] = 'Instaluje čekající skripty pro služby.';
+$lang['SoftaculousPlugin.cron.script_installation_name'] = 'Poskytování skriptů služeb';
+$lang['SoftaculousPlugin.installation_success'] = 'Skript byl úspěšně nainstalován pro službu s ID %1$s';
+$lang['SoftaculousPlugin.admin_lname'] = 'Správce';
+$lang['SoftaculousPlugin.admin_fname'] = 'Systém';
+$lang['SoftaculousPlugin.script_no_installed'] = 'Skript se nepodařilo nainstalovat: %1$s';
+$lang['SoftaculousPlugin.script_installed'] = 'Skript byl úspěšně nainstalován';
+$lang['SoftaculousPlugin.script_selected_error'] = 'Nepodařilo se určit skript, který má být nainstalován. Ujistěte se, že je název skriptu správný. Název skriptu: %1$s';
+$lang['SoftaculousPlugin.remote_firewall_error'] = 'Nepodařilo se určit umístění softaculous na vzdáleném serveru. Přístupu může bránit brána firewall.';
+$lang['SoftaculousPlugin.remote_curl_error'] = 'Nelze se přihlásit ke vzdálenému serveru. cURL Error: %1$s';
+$lang['SoftaculousPlugin.remote_error_message'] = 'Nelze se přihlásit ke vzdálenému serveru. Chyba: %1$s';
+$lang['SoftaculousPlugin.remote_error'] = 'Nelze se přihlásit ke vzdálenému serveru.';
+$lang['SoftaculousPlugin.library_error'] = 'Nelze načíst knihovnu.';
+$lang['SoftaculousPlugin.name'] = 'Soft Auto Installer';
+
