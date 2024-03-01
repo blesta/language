a7ae4334287d6b2230c01f02616d6dbb1f46ffd8
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:05 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/admin_payouts.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Admin Payouts
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminPayouts.edit.modify_payout'] = 'Auszahlung modifizieren';
+$lang['AdminPayouts.edit.field_status'] = 'Status';
+$lang['AdminPayouts.edit.field_requested_currency'] = 'Gewünschte Währung';
+$lang['AdminPayouts.edit.field_requested_amount'] = 'Angeforderter Betrag';
+$lang['AdminPayouts.edit.field_payment_method_id'] = 'Zahlungsmethode';
+$lang['AdminPayouts.edit.heading_payout_request'] = 'Auszahlungsantrag';
+$lang['AdminPayouts.edit.boxtitle_edit_payout'] = 'Auszahlung bearbeiten';
+$lang['AdminPayouts.index.payment_method_na'] = 'K.A.';
+$lang['AdminPayouts.index.no_results'] = 'Zurzeit liegen keine Auszahlungsanträge vor.';
+$lang['AdminPayouts.index.confirm_decline'] = 'Sind Sie sicher, dass Sie diese Auszahlung ablehnen wollen?';
+$lang['AdminPayouts.index.confirm_approve'] = 'Sind Sie sicher, dass Sie diese Auszahlung genehmigen wollen?';
+$lang['AdminPayouts.index.option_decline'] = 'Rückgang';
+$lang['AdminPayouts.index.option_approve'] = 'Genehmigen Sie';
+$lang['AdminPayouts.index.option_edit'] = 'bearbeiten';
+$lang['AdminPayouts.index.heading_options'] = 'Optionen';
+$lang['AdminPayouts.index.heading_date_requested'] = 'Gewünschtes Datum';
+$lang['AdminPayouts.index.heading_paid_currency'] = 'Bezahlte Währung';
+$lang['AdminPayouts.index.heading_paid_amount'] = 'Bezahlter Betrag';
+$lang['AdminPayouts.index.heading_requested_currency'] = 'Gewünschte Währung';
+$lang['AdminPayouts.index.heading_requested_amount'] = 'Angeforderter Betrag';
+$lang['AdminPayouts.index.heading_payment_method_id'] = 'Zahlungsmethode';
+$lang['AdminPayouts.index.heading_code'] = 'Code';
+$lang['AdminPayouts.index.heading_name'] = 'Kunde';
+$lang['AdminPayouts.index.heading_payout_id'] = 'Auszahlungs-ID';
+$lang['AdminPayouts.index.category_declined'] = 'Abgelehnt';
+$lang['AdminPayouts.index.category_pending'] = 'Anhängig';
+$lang['AdminPayouts.index.category_approved'] = 'Genehmigt';
+$lang['AdminPayouts.index.boxtitle_payouts'] = 'Affiliate-Auszahlungen';
+$lang['AdminPayouts.!error.insufficient_funds_payout_request'] = 'Es sind nicht genügend Mittel vorhanden, um den Auszahlungsantrag zu bearbeiten.';
+$lang['AdminPayouts.!success.payout_declined'] = 'Die Auszahlung wurde erfolgreich abgelehnt.';
+$lang['AdminPayouts.!success.payout_approved'] = 'Die Auszahlung wurde erfolgreich genehmigt.';
+$lang['AdminPayouts.!success.payout_updated'] = 'Die Auszahlung wurde erfolgreich aktualisiert.';
+
