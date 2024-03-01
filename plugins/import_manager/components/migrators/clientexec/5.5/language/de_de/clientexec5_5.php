799c37ef6f96268d6f45e6e782c25d01f6a1f0ce
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:49 -0800

    Updated de_de language files

---
+++ plugins/import_manager/components/migrators/clientexec/5.5/language/de_de/clientexec5_5.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Clientexec5 5
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Clientexec5_5.configuration.no_local_packages'] = 'Sie müssen Pakete erstellen, bevor Sie sie zuordnen können.';
+$lang['Clientexec5_5.configuration.remote_package'] = 'Ferngesteuertes Paket';
+$lang['Clientexec5_5.configuration.local_package'] = 'Lokales Paket';
+$lang['Clientexec5_5.configuration.create_packages_false'] = 'Pakete manuell zuordnen';
+$lang['Clientexec5_5.configuration.create_packages_true'] = 'Automatisches Erstellen aller erforderlichen Pakete während des Imports';
+$lang['Clientexec5_5.settings.enable_debug'] = 'Debugging einschalten';
+$lang['Clientexec5_5.settings.passphrase'] = 'CC-Passphrase';
+$lang['Clientexec5_5.settings.pass'] = 'Datenbank-Passwort';
+$lang['Clientexec5_5.settings.user'] = 'Datenbank-Benutzer';
+$lang['Clientexec5_5.settings.database'] = 'Name der Datenbank';
+$lang['Clientexec5_5.settings.host'] = 'Datenbank-Host';
+$lang['Clientexec5_5.!error.import'] = 'Der Import wurde abgeschlossen, aber es traten die folgenden Fehler auf:';
+$lang['Clientexec5_5.!error.passphrase.invalid'] = 'CC-Passphrase ist erforderlich.';
+$lang['Clientexec5_5.!error.pass.invalid'] = 'Das Datenbank-Passwort ist erforderlich.';
+$lang['Clientexec5_5.!error.user.invalid'] = 'Datenbankbenutzer ist erforderlich.';
+$lang['Clientexec5_5.!error.database.invalid'] = 'Datenbankname ist erforderlich.';
+$lang['Clientexec5_5.!error.host.invalid'] = 'Datenbank-Host ist erforderlich.';
+
