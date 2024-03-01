448b814958016743af3759ff230182c4e1c7e086
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:42 -0800

    Updated de_de language files

---
+++ components/modules/vesta/language/de_de/vesta.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Vesta
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Vesta.!error.actions_password.valid'] = 'Die Felder "Passwort" und "Passwort bestätigen" dürfen nicht leer sein.';
+$lang['Vesta.!error.password.valid'] = 'Das Passwort muss mindestens 8 Zeichen lang sein.';
+$lang['Vesta.!error.user_name.empty'] = 'Der Benutzername darf nicht leer sein.';
+$lang['Vesta.!error.domain.test'] = 'Der Domänenname kann nicht mit \'test\' beginnen.';
+$lang['Vesta.!error.domain.format'] = 'Bitte geben Sie einen gültigen Domainnamen ein, z. B. domain.com.';
+$lang['Vesta.!error.module_row.missing'] = 'Es ist ein interner Fehler aufgetreten. Die Modulzeile ist nicht verfügbar.';
+$lang['Vesta.!error.api.internal'] = 'Es ist ein interner Fehler aufgetreten, oder der Server hat nicht auf die Anfrage geantwortet.';
+$lang['Vesta.!error.meta[package_name].empty'] = 'Bitte geben Sie den Namen des Vesta-Pakets ein.';
+$lang['Vesta.!error.password_valid'] = 'Das Passwort scheint ungültig zu sein.';
+$lang['Vesta.!error.port_valid'] = 'Der Port scheint ungültig zu sein.';
+$lang['Vesta.!error.user_name_valid'] = 'Der Benutzername scheint ungültig zu sein.';
+$lang['Vesta.!error.host_name_valid'] = 'Der Hostname scheint ungültig zu sein.';
+$lang['Vesta.!error.server_name_valid'] = 'Sie müssen ein Server-Label eingeben.';
+$lang['Vesta.tab_stats.info_heading.value'] = 'Wert';
+$lang['Vesta.tab_stats.info_heading.field'] = 'Feld';
+$lang['Vesta.tab_client_actions.confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['Vesta.tab_client_actions.password'] = 'Passwort';
+$lang['Vesta.tab_client_actions.change_password'] = 'Passwort ändern';
+$lang['Vesta.service_info.option_login'] = 'Einloggen';
+$lang['Vesta.service_info.options'] = 'Optionen';
+$lang['Vesta.service_info.server'] = 'Server';
+$lang['Vesta.service_info.password'] = 'Passwort';
+$lang['Vesta.service_info.username'] = 'Benutzername';
+$lang['Vesta.service_info.hostname'] = 'Hostname';
+$lang['Vesta.stored_locally_only'] = 'Dieses Feld wird nur lokal aktualisiert';
+$lang['Vesta.service_field.password'] = 'Passwort';
+$lang['Vesta.service_field.username'] = 'Benutzername';
+$lang['Vesta.service_field.domain'] = 'Domänenname';
+$lang['Vesta.package_fields.package_name_how_to'] = 'Sie können den Paketnamen erhalten, indem Sie sich in Ihrem Vesta Control Panel > Pakete';
+$lang['Vesta.package_fields.package_name'] = 'Vesta Paket Name';
+$lang['Vesta.row_meta.use_ssl'] = 'Verwenden Sie SSL für die Verbindung zur API (empfohlen)';
+$lang['Vesta.row_meta.port'] = 'Hafen';
+$lang['Vesta.row_meta.password'] = 'Passwort';
+$lang['Vesta.row_meta.user_name'] = 'Name des Benutzers';
+$lang['Vesta.row_meta.host_name'] = 'Hostname';
+$lang['Vesta.row_meta.server_name'] = 'Server-Etikett';
+$lang['Vesta.edit_row.add_btn'] = 'Server bearbeiten';
+$lang['Vesta.edit_row.remove_name_server'] = 'entfernen';
+$lang['Vesta.edit_row.basic_title'] = 'Grundeinstellungen';
+$lang['Vesta.edit_row.box_title'] = 'Vesta Server bearbeiten';
+$lang['Vesta.add_row.add_btn'] = 'Server hinzufügen';
+$lang['Vesta.add_row.remove_name_server'] = 'entfernen';
+$lang['Vesta.add_row.basic_title'] = 'Grundeinstellungen';
+$lang['Vesta.add_row.box_title'] = 'Vesta Server hinzufügen';
+$lang['Vesta.order_options.first'] = 'Erster nicht vollständiger Server';
+$lang['Vesta.manage.module_groups_no_results'] = 'Es gibt keine Servergruppen.';
+$lang['Vesta.manage.module_rows_no_results'] = 'Es gibt keine Server.';
+$lang['Vesta.manage.module_groups.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Servergruppe löschen wollen?';
+$lang['Vesta.manage.module_rows.confirm_delete'] = 'Sind Sie sicher, dass Sie diesen Server löschen wollen?';
+$lang['Vesta.manage.module_groups.delete'] = 'Löschen';
+$lang['Vesta.manage.module_rows.delete'] = 'Löschen';
+$lang['Vesta.manage.module_groups.edit'] = 'bearbeiten';
+$lang['Vesta.manage.module_rows.edit'] = 'bearbeiten';
+$lang['Vesta.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Vesta.manage.module_groups_heading.options'] = 'Optionen';
+$lang['Vesta.manage.module_groups_heading.servers'] = 'Anzahl der Server';
+$lang['Vesta.manage.module_groups_heading.name'] = 'Name der Gruppe';
+$lang['Vesta.manage.module_rows_heading.options'] = 'Optionen';
+$lang['Vesta.manage.module_rows_heading.accounts'] = 'Konten';
+$lang['Vesta.manage.module_rows_heading.port'] = 'Hafen';
+$lang['Vesta.manage.module_rows_heading.hostname'] = 'Hostname';
+$lang['Vesta.manage.module_rows_heading.name'] = 'Server-Etikett';
+$lang['Vesta.manage.module_groups_title'] = 'Server-Gruppen';
+$lang['Vesta.manage.module_rows_title'] = 'Server';
+$lang['Vesta.add_module_group'] = 'Server-Gruppe hinzufügen';
+$lang['Vesta.add_module_row'] = 'Server hinzufügen';
+$lang['Vesta.tab_stats.info.BACKUPS'] = 'Backups';
+$lang['Vesta.tab_stats.info.BANDWIDTH'] = 'Bandbreitennutzung';
+$lang['Vesta.tab_stats.info.DISK_QUOTA'] = 'Nutzung der Festplatte';
+$lang['Vesta.tab_stats.info.CRON_JOBS'] = 'Cron-Jobs';
+$lang['Vesta.tab_stats.info.DATABASES'] = 'Datenbanken';
+$lang['Vesta.tab_stats.info.MAIL_ACCOUNTS'] = 'Mail-Konten';
+$lang['Vesta.tab_stats.info.MAIL_DOMAINS'] = 'Mail-Domains';
+$lang['Vesta.tab_stats.info.DNS_RECORDS'] = 'DNS-Einträge';
+$lang['Vesta.tab_stats.info.DNS_DOMAINS'] = 'DNS-Bereiche';
+$lang['Vesta.tab_stats.info.WEB_ALIASES'] = 'Aliasnamen';
+$lang['Vesta.tab_stats.info.WEB_DOMAINS'] = 'Domains';
+$lang['Vesta.submit'] = 'Einreichen';
+$lang['Vesta.tab_client_actions'] = 'Aktionen';
+$lang['Vesta.tab_stats'] = 'Statistik';
+$lang['Vesta.module_group'] = 'Server-Gruppe';
+$lang['Vesta.module_row_plural'] = 'Server';
+$lang['Vesta.module_row'] = 'Server';
+$lang['Vesta.description'] = 'Vesta, oder VestaCP, ist ein Open-Source-Web-Control-Panel, das als hervorragende Alternative zu cPanel gilt. Nicht nur, dass die Sicherheit und die häufigen Updates mit der Qualität von cPanel übereinstimmen, es ist auch völlig kostenlos.';
+$lang['Vesta.name'] = 'Vesta';
+
