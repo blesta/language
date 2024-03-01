5b6d41f0f5a15d90db055a73811f90cd7bae7603
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:09 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/payumoney/language/cs_cz/payumoney.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Payumoney
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payumoney.!error.invalid_token.response'] = 'Společnost Payumoney odpověděla neplatným tokenem.';
+$lang['Payumoney.!error.key.valid'] = 'Neplatný klíč obchodníka.';
+$lang['Payumoney.!error.auth_header.valid'] = 'Zadejte prosím platnou autorizační hlavičku.';
+$lang['Payumoney.!error.merchant_salt.valid'] = 'Zadejte prosím platný Salt.';
+$lang['Payumoney.!error.merchant_key.valid'] = 'Zadejte prosím platný klíč obchodníka.';
+$lang['Payumoney.buildprocess.submit'] = 'Platba přes Payumoney';
+$lang['Payumoney.test_mode_note'] = 'Pokud tuto možnost povolíte, bude se využívat sandbox Payumoney. Tuto možnost povolte pouze v případě, že testujete pomocí účtu Payumoney sandbox.';
+$lang['Payumoney.test_mode'] = 'Režim Sandbox';
+$lang['Payumoney.merchant_salt_note'] = 'sůl obchodníka naleznete na ovládacím panelu Payumoney v části Správa účtu -> Můj účet -> Klíč obchodníka - sůl.';
+$lang['Payumoney.merchant_key_note'] = 'klíč obchodníka naleznete na ovládacím panelu Payumoney v části Správa účtu -> Můj účet -> Klíč obchodníka-Salt.';
+$lang['Payumoney.auth_header_note'] = 'Autorizační hlavičku naleznete na ovládacím panelu Payumoney v části Správa účtu -> Můj účet -> Klíč obchodníka.';
+$lang['Payumoney.auth_header'] = 'Autorizační hlavička';
+$lang['Payumoney.merchant_salt'] = 'Obchodní sůl';
+$lang['Payumoney.merchant_key'] = 'Klíč obchodníka';
+$lang['Payumoney.description'] = 'V Indii společnost PayU obsluhuje více než 4 50 000+ obchodníků s více než 100 platebními metodami a je preferovaným platebním partnerem pro téměř 60 % obchodníků v oblasti elektronického obchodu.';
+$lang['Payumoney.name'] = 'Payumoney';
+$lang['Payumoney.!error.hash.valid'] = 'Neplatný hash.';
+
