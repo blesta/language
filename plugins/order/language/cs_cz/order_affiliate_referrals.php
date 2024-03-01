77f8e1fd8f2b26ec80ab5b03b4db8119473c34b4
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:10 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/order_affiliate_referrals.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Order Affiliate Referrals
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliateReferrals.getStatuses.canceled'] = 'Zrušeno';
+$lang['OrderAffiliateReferrals.getStatuses.mature'] = 'Dospělé';
+$lang['OrderAffiliateReferrals.getStatuses.pending'] = 'Čeká se na';
+$lang['OrderAffiliateReferrals.!error.date_updated.format'] = 'Aktualizované datum je v nesprávném formátu.';
+$lang['OrderAffiliateReferrals.!error.date_added.format'] = 'Přidané datum je v nesprávném formátu.';
+$lang['OrderAffiliateReferrals.!error.commission.format'] = 'Referral commision musí být číslo.';
+$lang['OrderAffiliateReferrals.!error.currency.length'] = 'Kód měny musí mít délku 3 znaky.';
+$lang['OrderAffiliateReferrals.!error.amount.format'] = 'Výše odkazu musí být číselná.';
+$lang['OrderAffiliateReferrals.!error.status.valid'] = 'Vyberte platný stav doporučení.';
+$lang['OrderAffiliateReferrals.!error.name.length'] = 'Název odkazu musí mít méně než 255 znaků.';
+$lang['OrderAffiliateReferrals.!error.order_id.exists'] = 'Neplatné ID objednávky.';
+$lang['OrderAffiliateReferrals.!error.affiliate_id.exists'] = 'Neplatné ID pobočky.';
+$lang['OrderAffiliateReferrals.!error.referral_id.exists'] = 'Neplatné ID odkazu.';
+
