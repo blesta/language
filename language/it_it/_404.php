71dd079434d50ff19809d5e3079c858770c60c91
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:22 -0800

    Updated it_it language files

---
+++ language/it_it/_404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 404
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['404.heading_error'] = '404';
+$lang['404.text_error'] = 'Siamo spiacenti, la pagina a cui si sta cercando di accedere non esiste o potrebbe essere stata spostata.';
+$lang['404.text_go_to_home'] = 'Vai alla pagina iniziale';
+
