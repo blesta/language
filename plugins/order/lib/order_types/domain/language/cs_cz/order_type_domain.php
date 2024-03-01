ded33c362634692fa7baeb3bfe8c0fd46b1594fa
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:17 -0800

    Updated cs_cz language files

---
+++ plugins/order/lib/order_types/domain/language/cs_cz/order_type_domain.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Order Type Domain
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderTypeDomain.settings.field_search'] = 'Vyhledávání TLD';
+$lang['Main.steps.step_1_description'] = 'Výběr domény';
+$lang['OrderTypeDomain.settings.heading_options'] = 'Možnosti';
+$lang['OrderTypeDomain.settings.heading_period'] = 'Období';
+$lang['OrderTypeDomain.settings.heading_term'] = 'Termín';
+$lang['OrderTypeDomain.settings.text_term'] = 'Minimální doba a období';
+$lang['OrderTypeDomain.settings.text_available_package_groups'] = 'Dostupné skupiny balíčků';
+$lang['OrderTypeDomain.settings.text_assigned_package_groups'] = 'Způsobilé skupiny balíčků';
+$lang['OrderTypeDomain.settings.text_available_tlds'] = 'Dostupné domény nejvyšší úrovně';
+$lang['OrderTypeDomain.settings.text_assigned_tlds'] = 'Schválené domény nejvyšší úrovně';
+$lang['OrderTypeDomain.settings.tooltip_free_domain'] = 'Zahrnuje bezplatnou doménu ze seznamu schválených TLD při zakoupení jiného balíčku.';
+$lang['OrderTypeDomain.settings.field_first_term'] = 'Zdarma pouze první semestr';
+$lang['OrderTypeDomain.settings.field_limit_term'] = 'Omezení domény na nejnižší počet let';
+$lang['OrderTypeDomain.settings.field_free_domain'] = 'Zahrnutí bezplatné domény při nákupu něčeho jiného';
+$lang['OrderTypeDomain.settings.free_domain_title'] = 'Volná doména';
+$lang['OrderTypeDomain.settings.domain_group_title'] = 'Skupina doménových balíčků';
+$lang['OrderTypeDomain.!error.meta[domain_group].groups'] = 'Skupina balíčků domény nemůže být zároveň přiřazenou skupinou balíčků.';
+$lang['OrderTypeDomain.!error.meta[domain_group].exists'] = 'Vyberte platnou skupinu doménových balíčků.';
+$lang['OrderTypeDomain.!error.template.valid'] = 'Vybraná šablona vyžaduje alespoň jednu skupinu balíčků.';
+$lang['OrderTypeDomain.!error.domain.empty'] = 'Zadejte název domény.';
+$lang['OrderTypeDomain.!error.domain.invalid'] = 'Tento název domény není podporován.';
+$lang['OrderTypeDomain.settings.tooltip_limit_term'] = 'Omezte doménu na nejkratší dobu povolenou pro danou TLD, obvykle 1 nebo 2 roky.';
+$lang['OrderTypeDomain.name'] = 'Doména a ostatní';
+
