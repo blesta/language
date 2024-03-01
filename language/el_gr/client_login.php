ce090270de70c10c34b194362b4a6071d9cff2eb
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:06 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_login.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Client Login
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientLogin.forgot.link_login'] = 'Ακύρωση, Είσοδος';
+$lang['ClientLogin.forgot.field_forgotsubmit'] = 'Ανάκτηση ονόματος χρήστη';
+$lang['ClientLogin.forgot.field_email'] = 'Διεύθυνση ηλεκτρονικού ταχυδρομείου';
+$lang['ClientLogin.forgot.description'] = 'Εισάγετε τη διεύθυνση ηλεκτρονικού ταχυδρομείου σας για να ζητήσετε το όνομα χρήστη σας.';
+$lang['ClientLogin.forgot.reset_heading'] = 'Ξεχάσατε το όνομα χρήστη';
+$lang['ClientLogin.forgot.page_title'] = 'Ξεχάσατε το όνομα χρήστη';
+$lang['ClientLogin.confirmreset.link_login'] = 'Ακύρωση, Είσοδος';
+$lang['ClientLogin.confirmreset.field_resetsubmit'] = 'Ορισμός κωδικού πρόσβασης';
+$lang['ClientLogin.confirmreset.field_confirm_password'] = 'Επιβεβαίωση νέου κωδικού πρόσβασης';
+$lang['ClientLogin.confirmreset.field_new_password'] = 'Νέος κωδικός πρόσβασης';
+$lang['ClientLogin.confirmreset.description'] = 'Παρακαλώ εισάγετε τον νέο σας κωδικό πρόσβασης.';
+$lang['ClientLogin.confirmreset.reset_heading'] = 'Επιβεβαίωση επαναφοράς κωδικού πρόσβασης';
+$lang['ClientLogin.confirmreset.page_title'] = 'Επιβεβαίωση επαναφοράς κωδικού πρόσβασης';
+$lang['ClientLogin.reset.link_login'] = 'Ακύρωση, Είσοδος';
+$lang['ClientLogin.reset.field_resetsubmit'] = 'Επαναφορά κωδικού πρόσβασης';
+$lang['ClientLogin.reset.field_username'] = 'Όνομα χρήστη';
+$lang['ClientLogin.reset.description'] = 'Εισάγετε το όνομα χρήστη σας για να ζητήσετε έναν σύνδεσμο επαναφοράς κωδικού πρόσβασης.';
+$lang['ClientLogin.reset.reset_heading'] = 'Επαναφορά κωδικού πρόσβασης';
+$lang['ClientLogin.reset.page_title'] = 'Επαναφορά κωδικού πρόσβασης';
+$lang['ClientLogin.otp.field_submit'] = 'Σύνδεση';
+$lang['ClientLogin.otp.field_otp'] = 'Κωδικός πρόσβασης μίας χρήσης';
+$lang['ClientLogin.otp.login_heading'] = 'Σύνδεση';
+$lang['ClientLogin.otp.description'] = 'Παρακαλώ εισάγετε τον κωδικό πρόσβασης μιας χρήσης.';
+$lang['ClientLogin.otp.page_title'] = 'Σύνδεση';
+$lang['ClientLogin.index.link_forgotusername'] = 'Ξέχασα το όνομα χρήστη μου';
+$lang['ClientLogin.index.link_resetpassword'] = 'Επαναφορά του κωδικού μου';
+$lang['ClientLogin.index.field_loginsubmit'] = 'Σύνδεση';
+$lang['ClientLogin.index.field_rememberme'] = 'Θυμηθείτε με σε αυτόν τον υπολογιστή';
+$lang['ClientLogin.index.field_password'] = 'Κωδικός πρόσβασης';
+$lang['ClientLogin.index.field_username'] = 'Όνομα χρήστη';
+$lang['ClientLogin.index.login_heading'] = 'Σύνδεση';
+$lang['ClientLogin.index.description'] = 'Παρακαλούμε συνδεθείτε για να αποκτήσετε πρόσβαση στην περιοχή πελατών.';
+$lang['ClientLogin.index.page_title'] = 'Σύνδεση';
+$lang['ClientLogin.!success.forgot_sent'] = 'Ένα email με το όνομα χρήστη σας έχει σταλεί στη διεύθυνση email σας.';
+$lang['ClientLogin.!success.reset_sent'] = 'Ένα email επιβεβαίωσης έχει σταλεί στη διεύθυνση που έχετε καταγράψει.';
+$lang['ClientLogin.!error.captcha.invalid'] = 'Το captcha που εισήχθη ήταν άκυρο. Παρακαλώ προσπαθήστε ξανά.';
+$lang['ClientLogin.!error.unknown_email'] = 'Αυτή η διεύθυνση ηλεκτρονικού ταχυδρομείου δεν αναγνωρίζεται ή το όνομα χρήστη δεν μπορεί να ανακτηθεί.';
+$lang['ClientLogin.!error.unknown_user'] = 'Αυτό το όνομα χρήστη δεν αναγνωρίζεται ή ο κωδικός πρόσβασης δεν μπορεί να επαναφερθεί.';
+
