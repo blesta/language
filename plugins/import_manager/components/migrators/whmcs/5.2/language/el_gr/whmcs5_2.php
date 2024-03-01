b93fd629c4a2c86f38ddeb1d2acfb1b241c7035b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:05 -0800

    Updated el_gr language files

---
+++ plugins/import_manager/components/migrators/whmcs/5.2/language/el_gr/whmcs5_2.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Whmcs5 2
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Whmcs5_2.!error.host.invalid'] = 'Απαιτείται Database Host.';
+$lang['Whmcs5_2.!error.database.invalid'] = 'Το όνομα της βάσης δεδομένων είναι υποχρεωτικό.';
+$lang['Whmcs5_2.!error.user.invalid'] = 'Απαιτείται χρήστης βάσης δεδομένων.';
+$lang['Whmcs5_2.!error.pass.invalid'] = 'Απαιτείται κωδικός πρόσβασης στη βάση δεδομένων.';
+$lang['Whmcs5_2.!error.key.invalid'] = 'Απαιτείται έγκυρο AESKEY.';
+$lang['Whmcs5_2.!error.import'] = 'Η εισαγωγή ολοκληρώθηκε, αλλά εμφανίστηκαν τα ακόλουθα σφάλματα:';
+$lang['Whmcs5_2.!notice.gmp'] = 'Η επέκταση \'gmp\' δεν βρέθηκε. Αυτή η επέκταση συνιστάται για τη βελτίωση των επιδόσεων και θα βελτιώσει τη διαδικασία εισαγωγής. Μπορείτε να συνεχίσετε χωρίς αυτή την επέκταση, αλλά η διαδικασία εισαγωγής μπορεί να είναι πολύ πιο αργή.';
+$lang['Whmcs5_2.settings.host'] = 'Υποδοχής βάσης δεδομένων';
+$lang['Whmcs5_2.settings.database'] = 'Όνομα βάσης δεδομένων';
+$lang['Whmcs5_2.settings.user'] = 'Χρήστης βάσης δεδομένων';
+$lang['Whmcs5_2.settings.pass'] = 'Κωδικός πρόσβασης βάσης δεδομένων';
+$lang['Whmcs5_2.settings.key'] = 'CC Κρυπτογράφηση Hash';
+$lang['Whmcs5_2.settings.key.info'] = 'Αυτό είναι το κλειδί κρυπτογράφησης που χρησιμοποιείται από το WHMCS για την κρυπτογράφηση διαφόρων bit δεδομένων. Μπορεί να βρεθεί στο αρχείο WHMCS configuration.php.';
+$lang['Whmcs5_2.settings.balance_credit'] = 'Αυτόματη εξισορρόπηση συναλλαγών για την αντιστοίχιση των πιστώσεων πελατών στο WHMCS';
+$lang['Whmcs5_2.settings.balance_credit.info'] = 'Εάν είναι επιλεγμένο, θα δημιουργηθεί μια συναλλαγή ή ένα τιμολόγιο για να διασφαλιστεί ότι η πίστωση του πελάτη στο Blesta ταιριάζει με εκείνη που έχει οριστεί για τον πελάτη στο WHMCS. Αυτό είναι απαραίτητο επειδή το WHMCS δεν υπολογίζει σωστά τις πιστώσεις πελατών.';
+$lang['Whmcs5_2.settings.enable_debug'] = 'Ενεργοποίηση αποσφαλμάτωσης';
+$lang['Whmcs5_2.configuration.create_packages_true'] = 'Αυτόματη δημιουργία τυχόν απαραίτητων πακέτων κατά την εισαγωγή';
+$lang['Whmcs5_2.configuration.create_packages_false'] = 'Χειροκίνητη χαρτογράφηση πακέτων';
+$lang['Whmcs5_2.configuration.local_package'] = 'Τοπικό πακέτο';
+$lang['Whmcs5_2.configuration.remote_package'] = 'Πακέτο τηλεχειριστηρίου';
+$lang['Whmcs5_2.configuration.no_local_packages'] = 'Πρέπει να δημιουργήσετε πακέτα προτού μπορέσετε να τα αντιστοιχίσετε.';
+
