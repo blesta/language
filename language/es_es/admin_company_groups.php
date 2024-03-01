0830daf52afe1f2c761e73410a99d19d0ebfe4aa
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:50 -0800

    Updated es_es language files

---
+++ language/es_es/admin_company_groups.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Admin Company Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyGroups.!success.add_created'] = '¡%1$s ha sido creado con éxito!';
+$lang['AdminCompanyGroups.!success.edit_updated'] = '¡%1$s ha sido editado con éxito!';
+$lang['AdminCompanyGroups.!success.delete_deleted'] = '¡%1$s se ha eliminado correctamente!';
+$lang['AdminCompanyGroups.!error.delete_failed'] = '%1$s es el grupo por defecto y no puede ser eliminado.';
+$lang['AdminCompanyGroups.!tooltip.force_email_usernames'] = 'Elimina la posibilidad de que los clientes definan su propio nombre de usuario.';
+$lang['AdminCompanyGroups.!tooltip.email_verification'] = 'Marque esta opción para enviar un correo electrónico de verificación cuando se cree un nuevo inicio de sesión o un cliente cambie su correo electrónico. Aparecerá un aviso en el perfil del cliente hasta que se verifique.';
+$lang['AdminCompanyGroups.index.page_title'] = 'Configuración > Empresa > Grupos de clientes';
+$lang['AdminCompanyGroups.index.boxtitle_groups'] = 'Grupos de clientes';
+$lang['AdminCompanyGroups.index.categorylink_addgroup'] = 'Crear grupo';
+$lang['AdminCompanyGroups.index.text_name'] = 'Nombre';
+$lang['AdminCompanyGroups.index.text_description'] = 'Descripción';
+$lang['AdminCompanyGroups.index.text_clients'] = 'Número de clientes';
+$lang['AdminCompanyGroups.index.text_options'] = 'Opciones';
+$lang['AdminCompanyGroups.index.option_edit'] = 'Editar';
+$lang['AdminCompanyGroups.index.option_delete'] = 'Borrar';
+$lang['AdminCompanyGroups.index.no_results'] = 'No hay grupos de clientes.';
+$lang['AdminCompanyGroups.index.confirm_delete'] = '¿Está seguro de que desea eliminar este grupo de clientes? Todos los clientes de este grupo se moverán al grupo por defecto.';
+$lang['AdminCompanyGroups.add.page_title'] = 'Configuración > Empresa > Grupos de clientes > Crear grupo';
+$lang['AdminCompanyGroups.add.boxtitle_addgroup'] = 'Crear grupo';
+$lang['AdminCompanyGroups.add.heading_basic'] = 'Opciones básicas';
+$lang['AdminCompanyGroups.add.heading_invoice'] = 'Opciones de facturación y cobro';
+$lang['AdminCompanyGroups.add.heading_late_fees'] = 'Tasas de demora';
+$lang['AdminCompanyGroups.add.heading_delivery'] = 'Entrega de facturas';
+$lang['AdminCompanyGroups.add.heading_payment'] = 'Avisos';
+$lang['AdminCompanyGroups.add.heading_client_settings'] = 'Configuración general del cliente';
+$lang['AdminCompanyGroups.add.heading_client_fields'] = 'Campos obligatorios del cliente';
+$lang['AdminCompanyGroups.add.field_name'] = 'Nombre';
+$lang['AdminCompanyGroups.add.field_color'] = 'Color';
+$lang['AdminCompanyGroups.add.field_description'] = 'Descripción';
+$lang['AdminCompanyGroups.add.field_delivery_methods'] = 'Métodos de entrega de facturas';
+$lang['AdminCompanyGroups.add.field_company_settings'] = 'Utilizar configuración de la empresa (desmarcar para especificar más abajo)';
+$lang['AdminCompanyGroups.add.field_force_email_usernames'] = 'Aplicar direcciones de correo electrónico como nombres de usuario';
+$lang['AdminCompanyGroups.add.field_email_verification'] = 'Activar la verificación del correo electrónico';
+$lang['AdminCompanyGroups.add.text_addsubmit'] = 'Crear grupo';
+$lang['AdminCompanyGroups.edit.page_title'] = 'Configuración > Empresa > Grupos de clientes > Editar grupo';
+$lang['AdminCompanyGroups.edit.boxtitle_editgroup'] = 'Editar Grupo';
+$lang['AdminCompanyGroups.edit.heading_basic'] = 'Opciones básicas';
+$lang['AdminCompanyGroups.edit.heading_invoice'] = 'Opciones de facturación y cobro';
+$lang['AdminCompanyGroups.edit.heading_late_fees'] = 'Tasas de demora';
+$lang['AdminCompanyGroups.edit.heading_delivery'] = 'Entrega de facturas';
+$lang['AdminCompanyGroups.edit.heading_payment'] = 'Avisos';
+$lang['AdminCompanyGroups.edit.heading_client_settings'] = 'Configuración general del cliente';
+$lang['AdminCompanyGroups.edit.heading_client_fields'] = 'Campos obligatorios del cliente';
+$lang['AdminCompanyGroups.edit.field_name'] = 'Nombre';
+$lang['AdminCompanyGroups.edit.field_color'] = 'Color';
+$lang['AdminCompanyGroups.edit.field_description'] = 'Descripción';
+$lang['AdminCompanyGroups.edit.field_delivery_methods'] = 'Métodos de entrega de facturas';
+$lang['AdminCompanyGroups.edit.field_company_settings'] = 'Utilizar configuración de la empresa (desmarcar para especificar más abajo)';
+$lang['AdminCompanyGroups.edit.field_force_email_usernames'] = 'Aplicar direcciones de correo electrónico como nombres de usuario';
+$lang['AdminCompanyGroups.edit.text_editsubmit'] = 'Editar Grupo';
+
