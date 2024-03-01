043b7ad57123683c4bf172887c5fd1ca1dac90e8
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:11 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/order_affiliates.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Order Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliates.getStatuses.inactive'] = 'Neaktivní';
+$lang['OrderAffiliates.getStatuses.active'] = 'Aktivní';
+$lang['OrderAffiliates.!error.date_updated.format'] = 'Aktualizované datum je v nesprávném formátu.';
+$lang['OrderAffiliates.!error.date_added.format'] = 'Přidané datum je v nesprávném formátu.';
+$lang['OrderAffiliates.!error.status.valid'] = 'Vyberte prosím platný status partnera.';
+$lang['OrderAffiliates.!error.code.unique'] = 'Tento kód je již obsazen jiným partnerem.';
+$lang['OrderAffiliates.!error.code.empty'] = 'Zadejte prosím partnerský kód.';
+$lang['OrderAffiliates.!error.client_id.unique'] = 'ID klienta je již přiřazeno k jinému partnerovi.';
+$lang['OrderAffiliates.!error.client_id.exists'] = 'Neplatné ID klienta.';
+$lang['OrderAffiliates.!error.affiliate_id.exists'] = 'Neplatné ID pobočky.';
+
