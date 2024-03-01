b4e7187c7d8a7d2a489c98cbf139c6b731d7a993
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:38 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_system_marketplace.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Admin System Marketplace
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSystemMarketplace.listing.boxtitle_listing'] = 'Rozšíření %1$s';
+$lang['AdminSystemMarketplace.index.text_unavailable'] = 'Tržiště je v současné době nedostupné.';
+$lang['AdminSystem.marketplace.category_all'] = 'Všechny';
+$lang['AdminSystemMarketplace.index.boxtitle_marketplace'] = 'Tržiště';
+$lang['AdminSystemMarketplace.index.page_title'] = 'Nastavení > Systém > Tržiště';
+
