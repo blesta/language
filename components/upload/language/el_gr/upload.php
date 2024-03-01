77c0f67289a0d9ee363d590de7596dcb0228fd2d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:12 -0800

    Updated el_gr language files

---
+++ components/upload/language/el_gr/upload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Upload
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Upload.!error.path_created'] = 'Η διαδρομή μεταφόρτωσης δεν μπόρεσε να δημιουργηθεί.';
+$lang['Upload.!error.write_failed'] = 'Το αρχείο δεν μπόρεσε να εγγραφεί.';
+$lang['Upload.!error.file_exists'] = 'Το αρχείο υπάρχει ήδη.';
+$lang['Upload.!error.mime_type'] = 'Ο τύπος αρχείου %1$s δεν επιτρέπεται.';
+$lang['Upload.!error.max_file_size'] = 'Το αρχείο υπερβαίνει το μέγιστο επιτρεπόμενο μέγεθος αρχείου.';
+
