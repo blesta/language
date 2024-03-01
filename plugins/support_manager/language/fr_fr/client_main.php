163ea25547d86f82787aafd17674fd687166d2ae
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:07 -0800

    Updated fr_fr language files

---
+++ plugins/support_manager/language/fr_fr/client_main.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.index.page_title'] = 'Support > Tickets';
+$lang['ClientMain.index.category_open'] = 'Ouvrir';
+$lang['ClientMain.index.category_closed'] = 'Fermé';
+$lang['ClientMain.index.categorylink_createticket'] = 'Billet ouvert';
+$lang['ClientMain.index.boxtitle_tickets'] = 'Billets';
+$lang['ClientMain.index.heading_code'] = 'Numéro du billet';
+$lang['ClientMain.index.heading_priority'] = 'Priorité';
+$lang['ClientMain.index.heading_department_name'] = 'Département';
+$lang['ClientMain.index.heading_summary'] = 'Résumé';
+$lang['ClientMain.index.heading_last_reply_date'] = 'Dernière réponse';
+$lang['ClientMain.index.heading_status'] = 'Statut';
+$lang['ClientMain.index.heading_options'] = 'Options';
+$lang['ClientMain.index.option_close'] = 'Fermer';
+$lang['ClientMain.index.option_reply'] = 'Répondre';
+$lang['ClientMain.index.any'] = 'Tous';
+$lang['ClientMain.index.minutes'] = '%1$s minutes';
+$lang['ClientMain.index.hour'] = '1 heure';
+$lang['ClientMain.index.hours'] = '%1$s heures';
+$lang['ClientMain.index.field_ticket_number'] = 'Numéro du billet';
+$lang['ClientMain.index.field_priority'] = 'Priorité';
+$lang['ClientMain.index.field_summary'] = 'Résumé';
+$lang['ClientMain.index.field_last_reply'] = 'Dernière réponse';
+$lang['ClientMain.index.no_results'] = 'Il n\'y a actuellement aucun ticket avec ce statut.';
+$lang['ClientMain.index.confirm_close'] = 'Êtes-vous sûr de vouloir fermer ce ticket ?';
+
