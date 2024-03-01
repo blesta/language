10b192777d74eeb70a7f89b365b865f7131340a2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:46 -0800

    Updated it_it language files

---
+++ language/it_it/backup.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Backup
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Backup.frequencies.days'] = '%1$s Giorni';
+$lang['Backup.frequencies.day'] = 'Giorno';
+$lang['Backup.frequencies.hours'] = '%1$s Ore';
+$lang['Backup.frequencies.hour'] = 'Ora';
+$lang['Backup.frequencies.never'] = 'Mai';
+$lang['Backup.!error.amazons3_failed'] = 'Il backup AmazonS3 non è riuscito.';
+$lang['Backup.!error.ftp_failed'] = 'Il backup SFTP non è riuscito.';
+$lang['Backup.!error.temp_dir.writable'] = 'La directory temp non è scrivibile.';
+$lang['Backup.!error.driver.support'] = 'Il driver del database non è supportato.';
+
