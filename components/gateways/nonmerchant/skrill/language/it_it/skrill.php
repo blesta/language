5e4f046a10b908779f5de6602a0c5ae9690400b3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:24 -0800

    Updated it_it language files

---
+++ components/gateways/nonmerchant/skrill/language/it_it/skrill.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Skrill
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Skrill.!error.merchant_id.empty'] = 'Inserire il proprio ID commerciante.';
+$lang['Skrill.!error.mqi.empty'] = 'Inserire la password MQI.';
+$lang['Skrill.!error.secret_word.empty'] = 'Inserire la parola segreta dell\'account.';
+$lang['Skrill.!error.user_email.valid'] = 'Inserire un indirizzo e-mail valido.';
+$lang['Skrill.!error.libxml_required'] = 'Le estensioni libxml e simplexml sono necessarie per questo gateway.';
+$lang['Skrill.invoice.description'] = 'Fatture';
+$lang['Skrill.buildprocess.submit'] = 'Pagare con Skrill';
+$lang['Skrill.mqi_note'] = 'Inserite la password del vostro conto Skrill MQI API.';
+$lang['Skrill.merchant_id_note'] = 'Inserite l\'ID commerciante del vostro conto Skrill.';
+$lang['Skrill.user_email_note'] = 'Inserite l\'indirizzo e-mail del vostro conto Skrill.';
+$lang['Skrill.secret_word_note'] = 'Inserite la parola segreta del vostro conto Skrill.';
+$lang['Skrill.mqi'] = 'Password MQI';
+$lang['Skrill.merchant_id'] = 'ID commerciante';
+$lang['Skrill.secret_word'] = 'Parola segreta';
+$lang['Skrill.user_email'] = 'Email';
+$lang['Skrill.description'] = 'Una piattaforma di e-commerce con sede a Londra che si occupa di trasferimenti internazionali di denaro a basso costo.';
+$lang['Skrill.name'] = 'Skrill';
+
