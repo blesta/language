3977415927eb474d1aa78cab805178f21353da9e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:53 -0800

    Updated es_es language files

---
+++ components/modules/multicraft/language/es_es/multicraft_module.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Multicraft Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MulticraftModule.!error.json.unavailable'] = 'Este módulo requiere la extensión JSON.';
+$lang['MulticraftModule.!error.server_name.empty'] = 'Por favor, introduzca una etiqueta para este servidor.';
+$lang['MulticraftModule.!error.panel_url.empty'] = 'Introduzca la URL de su panel Multicraft.';
+$lang['MulticraftModule.!error.panel_api_url.empty'] = 'Introduzca la URL de la API de su panel Multicraft.';
+$lang['MulticraftModule.!error.username.empty'] = 'Introduzca su nombre de usuario Multicraft';
+$lang['MulticraftModule.!error.key.empty'] = 'Introduzca su clave API';
+$lang['MulticraftModule.!error.log_all.format'] = 'Si desea registrar todas las solicitudes de API debe establecerse en 1 o 0.';
+$lang['MulticraftModule.!error.ips.match'] = 'Establezca una dirección IP para cada conjunto de Daemon.';
+$lang['MulticraftModule.!error.daemons.match'] = 'Por favor, establezca un Daemon ID para cada dirección IP establecida.';
+$lang['MulticraftModule.!error.ips_in_use.match'] = 'Establezca si la dirección IP está actualmente en uso en 1 o 0.';
+
