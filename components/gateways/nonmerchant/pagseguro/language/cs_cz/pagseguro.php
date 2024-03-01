39c161cd39cd72585fbf48192582fbd8bd62e28c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:07 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/pagseguro/language/cs_cz/pagseguro.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Pagseguro
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Pagseguro.!error.notification.invalid'] = 'Neplatné parametry oznámení.';
+$lang['Pagseguro.!error.notification.type'] = 'Neznámý typ oznámení:';
+$lang['Pagseguro.!error.token.empty'] = 'Zadejte svůj bezpečnostní token.';
+$lang['Pagseguro.!error.email.format'] = 'Zadejte prosím platnou e-mailovou adresu.';
+$lang['Pagseguro.!error.dom_required'] = 'Pro tuto bránu je vyžadováno rozšíření dom.';
+$lang['Pagseguro.!error.libxml_required'] = 'Pro tuto bránu jsou vyžadována rozšíření libxml a simplexml.';
+$lang['Pagseguro.buildprocess.submit'] = 'Plaťte pomocí PagSeguro';
+$lang['Pagseguro.token_note'] = 'Zadejte bezpečnostní token vytvořený v účtu PagSeguro.';
+$lang['Pagseguro.token'] = 'Token';
+$lang['Pagseguro.email_note'] = 'Zadejte e-mailovou adresu svého účtu PagSeguro.';
+$lang['Pagseguro.email_id'] = 'E-mail';
+$lang['Pagseguro.description'] = 'PagSeguro je brazilská služba elektronického obchodování založená na online nebo mobilních platbách pro obchodní operace. V roce 2015 ji Congresso Afiliados Brasil ocenil jako "Nejlepší platební metodu".';
+$lang['Pagseguro.name'] = 'PagSeguro';
+
