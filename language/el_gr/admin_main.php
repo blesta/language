f77e3ed2f9041ccc502fdc3d808e7c6deb7e6933
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:50 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_main.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.editevent.confirm_delete'] = 'Πραγματικά διαγράφετε αυτό το γεγονός;';
+$lang['AdminMain.editevent.field_deleteeventsubmit'] = 'Διαγραφή συμβάντος';
+$lang['AdminMain.editevent.field_editeventsubmit'] = 'Εκδήλωση ενημέρωσης';
+$lang['AdminMain.editevent.field_all_day'] = 'Όλη την ημέρα';
+$lang['AdminMain.editevent.field_shared'] = 'Κάντε αυτό το συμβάν ορατό από άλλο προσωπικό';
+$lang['AdminMain.editevent.field_end_date'] = 'Ημερομηνία λήξης';
+$lang['AdminMain.editevent.field_start_date'] = 'Ημερομηνία έναρξης';
+$lang['AdminMain.editevent.field_title'] = 'Τίτλος';
+$lang['AdminMain.editevent.boxtitle_editevent'] = 'Επεξεργασία συμβάντος';
+$lang['AdminMain.addevent.field_addeventsubmit'] = 'Δημιουργία συμβάντος';
+$lang['AdminMain.addevent.field_all_day'] = 'Όλη την ημέρα';
+$lang['AdminMain.addevent.field_shared'] = 'Κάντε αυτό το συμβάν ορατό από άλλο προσωπικό';
+$lang['AdminMain.addevent.field_end_date'] = 'Ημερομηνία λήξης';
+$lang['AdminMain.addevent.field_start_date'] = 'Ημερομηνία έναρξης';
+$lang['AdminMain.addevent.field_title'] = 'Τίτλος';
+$lang['AdminMain.addevent.boxtitle_addevent'] = 'Δημιουργία συμβάντος';
+$lang['AdminMain.getEvents.shared_event_title'] = '%2$s %3$s: %1$s';
+$lang['AdminMain.calendar.category_day'] = 'Ημέρα';
+$lang['AdminMain.calendar.category_week'] = 'Εβδομάδα';
+$lang['AdminMain.calendar.category_month'] = 'Μήνας';
+$lang['AdminMain.calendar.categorylink_addevent'] = 'Δημιουργία συμβάντος';
+$lang['AdminMain.calendar.boxtitle_calendar'] = 'Ημερολόγιο';
+$lang['AdminMain.calendar.page_title'] = 'Ημερολόγιο';
+$lang['AdminMain.updatedashboard.text_layout'] = 'Επιλέξτε μια διάταξη παρακάτω για να προσαρμόσετε το ταμπλό σας.';
+$lang['AdminMain.managewidgets.text_author'] = 'Συγγραφέας:';
+$lang['AdminMain.managewidgets.text_version'] = 'ver %1$s';
+$lang['AdminMain.managewidgets.text_widgets'] = 'Σύρετε τα widgets από τα δεξιά προς τα αριστερά για να τα προσθέσετε στο ταμπλό σας ή από τα αριστερά προς τα δεξιά για να τα αφαιρέσετε.';
+$lang['AdminMain.index.customize_dashboard'] = 'Προσαρμογή ταμπλό';
+$lang['AdminMain.index.manage_widgets'] = 'Διαχείριση Widgets';
+$lang['AdminMain.index.no_quicklinks'] = 'Δεν έχουν οριστεί Γρήγοροι Σύνδεσμοι.';
+$lang['AdminMain.index.heading_quicklinks'] = 'Γρήγοροι σύνδεσμοι';
+$lang['AdminMain.index.page_title'] = 'Ταμπλό';
+$lang['AdminMain.!success.event_deleted'] = 'Το συμβάν διαγράφηκε επιτυχώς!';
+$lang['AdminMain.!success.event_edited'] = 'Η εκδήλωση ενημερώθηκε επιτυχώς!';
+$lang['AdminMain.!success.event_added'] = 'Η εκδήλωση έχει προστεθεί επιτυχώς!';
+$lang['AdminMain.!error.event_editable'] = 'Μόνο ο δημιουργός του συμβάντος μπορεί να διαγράψει το συμβάν.';
+$lang['AdminMain.!error.delete_event.staff_id'] = 'Μόνο ο δημιουργός του συμβάντος μπορεί να διαγράψει αυτό το συμβάν.';
+
