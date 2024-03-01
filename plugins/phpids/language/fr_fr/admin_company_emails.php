848d828eeb50d3fa84310847f93e43c4ec3222ad
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:57 -0800

    Updated fr_fr language files

---
+++ plugins/phpids/language/fr_fr/admin_company_emails.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Phpids.email_alert_desc'] = 'Courriel d\'alerte de détection d\'intrusion PHPIDS, envoyé aux adresses configurées sous le plugin PHPIDS.';
+$lang['AdminCompanyEmails.templates.Phpids.email_alert_name'] = 'Alerte d\'intrusion de PHPIDS';
+
