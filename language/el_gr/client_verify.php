15a05fb20d9926793c4abccea25afd822a73feaf
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:10 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_verify.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Client Verify
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientVerify.!error.invalid_token'] = 'Το παρεχόμενο διακριτικό δεν είναι έγκυρο ή έχει λήξει.';
+$lang['ClientVerify.!success.email_sent'] = 'Ένα email επαλήθευσης εστάλη στο %1$s.';
+$lang['ClientVerify.!success.email_verified'] = 'Η διεύθυνση ηλεκτρονικού ταχυδρομείου σας επαληθεύτηκε επιτυχώς.';
+
