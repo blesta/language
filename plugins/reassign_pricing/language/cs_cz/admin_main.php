998ca9bc9f5b82cb41e04669fa8809cb4e1fa18d
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:32 -0800

    Updated cs_cz language files

---
+++ plugins/reassign_pricing/language/cs_cz/admin_main.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.service.field_submit'] = 'Uložit';
+$lang['AdminMain.service.field_cancel'] = 'Zrušit';
+$lang['AdminMain.service.field_group'] = 'Skupina balíčků';
+$lang['AdminMain.service.field_pricing'] = 'Balíček/termín';
+$lang['AdminMain.service.heading_pricing'] = 'Změna přiřazení cen';
+$lang['AdminMain.service.text_renewal_price'] = 'Cena za obnovení:';
+$lang['AdminMain.service.text_coupon_amount'] = '%1$s (%2$s)';
+$lang['AdminMain.service.text_coupon_percent'] = '%1$s (%2$s%%)';
+$lang['AdminMain.service.text_recurring_coupon'] = 'Opakující se kupón:';
+$lang['AdminMain.service.text_date_renews'] = 'Datum obnovení:';
+$lang['AdminMain.service.text_date_added'] = 'Datum přidání:';
+$lang['AdminMain.service.text_status'] = 'Název balíčku:';
+$lang['AdminMain.service.text_term'] = 'Termín:';
+$lang['AdminMain.service.text_qty'] = 'Množství:';
+$lang['AdminMain.service.text_label'] = 'Štítek:';
+$lang['AdminMain.service.text_package_name'] = 'Název balíčku:';
+$lang['AdminMain.service.heading_information'] = 'Informace o službě';
+$lang['AdminMain.service.boxtitle_pricing'] = 'Změna přiřazení ceny pro službu: %1$s - %2$s';
+$lang['AdminMain.service.term'] = '%1$s %2$s - %3$s';
+$lang['AdminMain.service.term_onetime'] = 'Jednorázově - %3$s';
+$lang['AdminMain.index.option_service'] = 'Změna přiřazení cen';
+$lang['AdminMain.index.no_results'] = 'Nejsou k dispozici žádné služby, ze kterých by bylo možné ceny přeřadit.';
+$lang['AdminMain.index.text_never'] = 'Nikdy';
+$lang['AdminMain.index.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['AdminMain.index.heading_options'] = 'Možnosti';
+$lang['AdminMain.index.heading_date_renews'] = 'Datum obnovení';
+$lang['AdminMain.index.heading_date_created'] = 'Datum vytvoření';
+$lang['AdminMain.index.heading_term'] = 'Termín';
+$lang['AdminMain.index.heading_label'] = 'Štítek';
+$lang['AdminMain.index.heading_package'] = 'Balíček';
+$lang['AdminMain.index.link_client'] = 'Zpět na klienta #%1$s';
+$lang['AdminMain.index.boxtitle_services'] = 'Služby pro klienta #%1$s';
+$lang['AdminMain.index.page_title'] = 'Client #%1$s Reassign Pricing (Znovu přiřadit ceny)';
+$lang['AdminMain.!warning.reassign_pricing'] = 'Změna přiřazení cen změní pouze balíček a ceny spojené s touto službou. Tuto akci neprovádějte pro upgrady nebo downgrady.';
+$lang['AdminMain.!success.service_updated'] = 'Ceny služeb byly úspěšně aktualizovány.';
+
