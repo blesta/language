532d959ac0e171437ef77d3aa1be37fbefb8570e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:50 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/admin_company_lookandfeel.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Admin Company Lookandfeel
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyLookandfeel.!success.template_updated'] = 'Les modèles ont été mis à jour avec succès.';
+$lang['AdminCompanyLookandfeel.!success.layout_updated'] = 'La mise en page a été mise à jour avec succès.';
+$lang['AdminCompanyLookandfeel.!success.navigation_updated'] = 'La navigation a été mise à jour avec succès.';
+$lang['AdminCompanyLookandfeel.!success.action_created'] = 'L\'action a été créée avec succès et peut être ajoutée sous Look and Feel > Navigation.';
+$lang['AdminCompanyLookandfeel.!success.action_updated'] = 'L\'action a été mise à jour avec succès.';
+$lang['AdminCompanyLookandfeel.!success.action_deleted'] = 'L\'action a été supprimée avec succès.';
+$lang['AdminCompanyLookandfeel.!success.logo_updated'] = 'Le logo personnalisé a été mis à jour avec succès.';
+$lang['AdminCompanyLookandfeel.!tooltip.action_name'] = 'Pour créer des actions multilingues, vous pouvez spécifier une définition de la langue dans le fichier de langue _global.php. Renommez _global-example.php en _global.php si nécessaire. Ces fichiers se trouvent dans %1$s et dans chacun des autres répertoires linguistiques.  Exemple de code pour la définition :';
+$lang['AdminCompanyLookandfeel.!tooltip.action_name_language'] = 'Texte du lien';
+$lang['AdminCompanyLookandfeel.!tooltip.action_url'] = 'Ce chemin peut être relatif (par exemple, clients/add) ou absolu (par exemple, %1$sclients/add/ ou https://blesta.com). Les chemins relatifs sont basés sur l\'interface affichée.  Par exemple, clients/add renvoie à %1$sclients/add.';
+$lang['AdminCompanyLookandfeel.!tooltip.disabled_location'] = 'L\'emplacement d\'une action ne peut pas être modifié après la création.';
+$lang['AdminCompanyLookandfeel.!tooltip.client_view_override'] = 'Permettre au client de remplacer le modèle à partir de l\'URL en définissant le paramètre "bltemplate" où la valeur est le nom du répertoire du modèle, par exemple "/?bltemplate=bootstrap".';
+$lang['AdminCompanyLookandfeel.template.page_title'] = 'Paramètres > Entreprise > Présentation > Modèle';
+$lang['AdminCompanyLookandfeel.template.box_title'] = 'Modèle';
+$lang['AdminCompanyLookandfeel.template.text_client_view_dir'] = 'Modèle de client';
+$lang['AdminCompanyLookandfeel.template.text_admin_view_dir'] = 'Modèle d\'administration';
+$lang['AdminCompanyLookandfeel.template.text_client_view_override'] = 'Permet de remplacer le modèle du client à partir de l\'URL';
+$lang['AdminCompanyLookandfeel.template.text_submit'] = 'Économiser';
+$lang['AdminCompanyLookandfeel.layout.page_title'] = 'Paramètres > Entreprise > Présentation > Mise en page';
+$lang['AdminCompanyLookandfeel.layout.box_title'] = 'Mise en page';
+$lang['AdminCompanyLookandfeel.layout.field.text_color'] = 'Couleur du texte';
+$lang['AdminCompanyLookandfeel.layout.field.background_url'] = 'URL d\'arrière-plan';
+$lang['AdminCompanyLookandfeel.layout.text_html_generated'] = 'Généré en HTML';
+$lang['AdminCompanyLookandfeel.layout.text_cards_note'] = 'Faites glisser les cartes pour en modifier l\'ordre, cliquez dessus pour en changer la couleur et cliquez sur la case à cocher pour activer ou désactiver une carte. La couleur des cartes générées par HTML ne peut pas être modifiée.';
+$lang['AdminCompanyLookandfeel.layout.text_widget_preview_note'] = 'Pour des raisons de sécurité, l\'aperçu de ce widget n\'a pas été généré.';
+$lang['AdminCompanyLookandfeel.layout.text_submit'] = 'Économiser';
+$lang['AdminCompanyLookandfeel.customize.page_title'] = 'Paramètres > Entreprise > Présentation > Personnaliser';
+$lang['AdminCompanyLookandfeel.customize.box_title'] = 'Personnaliser';
+$lang['AdminCompanyLookandfeel.customize.heading_custom_logo'] = 'Logo personnalisé';
+$lang['AdminCompanyLookandfeel.customize.field.admin_type_logo'] = 'Télécharger le logo';
+$lang['AdminCompanyLookandfeel.customize.field.admin_type_url'] = 'Définir l\'URL du logo';
+$lang['AdminCompanyLookandfeel.customize.field.admin_logo'] = 'Logo de l\'en-tête';
+$lang['AdminCompanyLookandfeel.customize.field.admin_url'] = 'Logo de l\'en-tête';
+$lang['AdminCompanyLookandfeel.customize.field.client_type_logo'] = 'Télécharger le logo';
+$lang['AdminCompanyLookandfeel.customize.field.client_type_url'] = 'Définir l\'URL du logo';
+$lang['AdminCompanyLookandfeel.customize.field.client_logo'] = 'Logo de l\'en-tête';
+$lang['AdminCompanyLookandfeel.customize.field.client_url'] = 'Logo de l\'en-tête';
+$lang['AdminCompanyLookandfeel.customize.text_submit'] = 'Économiser';
+$lang['AdminCompanyLookandfeel.navigation.page_title'] = 'Réglages > Entreprise > Présentation > Navigation';
+$lang['AdminCompanyLookandfeel.navigation.boxtitle_navigation'] = 'Navigation';
+$lang['AdminCompanyLookandfeel.navigation.category_nav_staff'] = 'Navigation du personnel';
+$lang['AdminCompanyLookandfeel.navigation.category_nav_client'] = 'Navigation du client';
+$lang['AdminCompanyLookandfeel.navigation.category_nav_public'] = 'Navigation publique';
+$lang['AdminCompanyLookandfeel.navigation.heading_navigation'] = 'Menu de navigation';
+$lang['AdminCompanyLookandfeel.navigation.heading_action'] = 'Action';
+$lang['AdminCompanyLookandfeel.navigation.heading_options'] = 'Options';
+$lang['AdminCompanyLookandfeel.actions.option_add'] = 'Ajouter';
+$lang['AdminCompanyLookandfeel.actions.option_remove'] = 'Retirer';
+$lang['AdminCompanyLookandfeel.actions.option_add_action'] = 'Ajouter une action personnalisée';
+$lang['AdminCompanyLookandfeel.navigation.field_subitem'] = 'Sous-poste';
+$lang['AdminCompanyLookandfeel.navigation.text_link'] = 'lien';
+$lang['AdminCompanyLookandfeel.navigation.no_results_actions'] = 'Il n\'y a pas d\'actions pour ce lieu.';
+$lang['AdminCompanyLookandfeel.navigation.text_submit'] = 'Sauvegarder la navigation';
+$lang['AdminCompanyLookandfeel.actions.page_title'] = 'Paramètres > Entreprise > Présentation > Actions personnalisées';
+$lang['AdminCompanyLookandfeel.actions.boxtitle_actions'] = 'Actions personnalisées';
+$lang['AdminCompanyLookandfeel.actions.category_nav_staff'] = 'Navigation du personnel';
+$lang['AdminCompanyLookandfeel.actions.category_nav_client'] = 'Navigation du client';
+$lang['AdminCompanyLookandfeel.actions.category_nav_public'] = 'Navigation publique';
+$lang['AdminCompanyLookandfeel.actions.categorylink_addaction'] = 'Ajouter une action';
+$lang['AdminCompanyLookandfeel.actions.heading_name'] = 'Nom';
+$lang['AdminCompanyLookandfeel.actions.heading_url'] = 'URL';
+$lang['AdminCompanyLookandfeel.actions.heading_options'] = 'Options';
+$lang['AdminCompanyLookandfeel.actions.no_results'] = 'Il n\'y a pas d\'actions pour ce lieu.';
+$lang['AdminCompanyLookandfeel.actions.option_edit'] = 'Editer';
+$lang['AdminCompanyLookandfeel.actions.option_delete'] = 'Supprimer';
+$lang['AdminCompanyLookandfeel.actions.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer cette action ?';
+$lang['AdminCompanyLookandfeel.addaction.page_title'] = 'Réglages > Entreprise > Apparence > Ajouter une action';
+$lang['AdminCompanyLookandfeel.addaction.boxtitle_addaction'] = 'Ajouter une action';
+$lang['AdminCompanyLookandfeel.addaction.field_location'] = 'Localisation';
+$lang['AdminCompanyLookandfeel.addaction.field_name'] = 'Nom';
+$lang['AdminCompanyLookandfeel.addaction.field_url'] = 'Lien URL';
+$lang['AdminCompanyLookandfeel.addaction.placeholder_name'] = 'Nom du paquet';
+$lang['AdminCompanyLookandfeel.addaction.placeholder_url'] = 'Statut';
+$lang['AdminCompanyLookandfeel.addaction.field_addsubmit'] = 'Ajouter une action';
+$lang['AdminCompanyLookandfeel.editaction.page_title'] = 'Réglages > Entreprise > Présentation > Modifier l\'action';
+$lang['AdminCompanyLookandfeel.editaction.boxtitle_editaction'] = 'Modifier l\'action';
+$lang['AdminCompanyLookandfeel.editaction.field_location'] = 'Localisation';
+$lang['AdminCompanyLookandfeel.editaction.field_name'] = 'Nom';
+$lang['AdminCompanyLookandfeel.editaction.field_url'] = 'Lien URL';
+$lang['AdminCompanyLookandfeel.editaction.placeholder_name'] = 'Nom du paquet';
+$lang['AdminCompanyLookandfeel.editaction.placeholder_url'] = 'Statut';
+$lang['AdminCompanyLookandfeel.editaction.field_addsubmit'] = 'Modifier l\'action';
+
