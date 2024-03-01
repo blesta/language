5c16d16bc7556e141f947ce53294b2d236f8247f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:13 -0800

    Updated es_es language files

---
+++ language/es_es/client_managers.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Client Managers
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientManagers.!success.manager_invited'] = 'Se ha enviado correctamente una invitación al usuario registrado.';
+$lang['ClientManagers.!success.manager_revoked'] = '¡El gestor ha sido revocado con éxito!';
+$lang['ClientManagers.!success.manager_updated'] = 'El gestor se ha actualizado correctamente.';
+$lang['ClientManagers.!success.account_gave_up'] = '¡Su acceso a la cuenta %1$s ha sido revocado con éxito!';
+$lang['ClientManagers.!success.invitation_accepted'] = 'Ha aceptado la invitación correctamente.';
+$lang['ClientManagers.!success.invitation_declined'] = 'Ha rechazado la invitación correctamente.';
+$lang['ClientManagers.navigation.nav_managers'] = 'Directores invitados';
+$lang['ClientManagers.navigation.nav_managed_accounts'] = 'Cuentas gestionadas';
+$lang['ClientManagers.navigation.nav_return'] = 'Volver al panel de control';
+$lang['ClientManagers.index.boxtitle_managers'] = 'Directores invitados';
+$lang['ClientManagers.index.add_guest_user'] = 'Añadir usuario invitado';
+$lang['ClientManagers.index.heading_client_id'] = 'Cliente';
+$lang['ClientManagers.index.heading_email'] = 'Correo electrónico';
+$lang['ClientManagers.index.heading_name'] = 'Nombre y apellidos';
+$lang['ClientManagers.index.heading_company'] = 'Empresa';
+$lang['ClientManagers.index.heading_status'] = 'Estado';
+$lang['ClientManagers.index.heading_options'] = 'Opciones';
+$lang['ClientManagers.index.text_status_accepted'] = 'Aceptado';
+$lang['ClientManagers.index.text_status_pending'] = 'Pendiente';
+$lang['ClientManagers.index.option_edit'] = 'Editar';
+$lang['ClientManagers.index.option_revoke'] = 'Revocar';
+$lang['ClientManagers.index.confirm_revoke'] = '¿Está seguro de que desea revocar el acceso a esta cuenta?';
+$lang['ClientManagers.index.no_results'] = 'No has invitado a ningún usuario a gestionar esta cuenta.';
+$lang['ClientManagers.add.boxtitle_add_manager'] = 'Añadir administrador';
+$lang['ClientManagers.add.field_addsubmit'] = 'Añadir administrador';
+$lang['ClientManagers.add.heading_invite_manager'] = 'Gestor de invitaciones';
+$lang['ClientManagers.add.field_email'] = 'Dirección de correo electrónico';
+$lang['ClientManagers.tooltip.field_email'] = 'La dirección de correo electrónico asociada a una cuenta registrada en %1$s.';
+$lang['ClientManagers.add.heading_permissions'] = 'Permisos';
+$lang['ClientManagers.tooltip.client_invoices'] = 'Mostrar las facturas en el panel de control, así como las alertas de recordatorio de pago.';
+$lang['ClientManagers.tooltip.client_services'] = 'Mostrar los servicios en el panel de control y permitir su gestión.';
+$lang['ClientManagers.tooltip.client_transactions'] = 'Mostrar transacciones en el panel de control.';
+$lang['ClientManagers.tooltip.client_contacts'] = 'Permitir la gestión de contactos.';
+$lang['ClientManagers.tooltip.client_accounts'] = 'Permitir la gestión de cuentas de pago.';
+$lang['ClientManagers.tooltip.client_emails'] = 'Mostrar una lista del historial de correo electrónico.';
+$lang['ClientManagers.tooltip._managed'] = 'Permitir la gestión de las cuentas a las que ha sido invitado.';
+$lang['ClientManagers.tooltip._invoice_delivery'] = 'Permitir ver y cambiar el método de entrega de la factura.';
+$lang['ClientManagers.tooltip._credits'] = 'Permitir la visualización de los créditos de la cuenta.';
+$lang['ClientManagers.edit.boxtitle_edit_manager'] = 'Gestor de edición';
+$lang['ClientManagers.edit.field_addsubmit'] = 'Gestor de edición';
+$lang['ClientManagers.edit.heading_permissions'] = 'Permisos';
+$lang['ClientManagers.accounts.boxtitle_managed_accounts'] = 'Cuentas gestionadas';
+$lang['ClientManagers.accounts.heading_client_id'] = 'Cliente';
+$lang['ClientManagers.accounts.heading_email'] = 'Correo electrónico';
+$lang['ClientManagers.accounts.heading_name'] = 'Nombre y apellidos';
+$lang['ClientManagers.accounts.heading_company'] = 'Empresa';
+$lang['ClientManagers.accounts.heading_options'] = 'Opciones';
+$lang['ClientManagers.accounts.option_manage'] = 'Gestione';
+$lang['ClientManagers.accounts.option_give_up_access'] = 'Renunciar al acceso';
+$lang['ClientManagers.accounts.confirm_give_up'] = '¿Estás seguro de que quieres renunciar al acceso a esta cuenta?';
+$lang['ClientManagers.accounts.no_results'] = 'Actualmente no gestionas ninguna cuenta de invitado.';
+$lang['ClientManagers.invite.boxtitle_management_invitation'] = 'Invitación de la Dirección';
+$lang['ClientManagers.invite.text_invitation'] = '%1$s %2$s le ha invitado a gestionar una cuenta. Si aceptas esta invitación podrás acceder y gestionar la cuenta, si has recibido esta invitación por error puedes rechazarla sin problemas.';
+$lang['ClientManagers.invite.field_accept'] = 'Acepte';
+$lang['ClientManagers.invite.field_decline'] = 'Disminución';
+$lang['ClientManagers.index.add_manager'] = 'Añadir administrador';
+$lang['ClientManagers.tooltip.client_quotations'] = 'Mostrar presupuestos en el panel de control y permitir que se aprueben o denieguen.';
+
