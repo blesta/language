dffb57939833696763ef1a2b379e851a27604925
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:30 -0800

    Updated it_it language files

---
+++ plugins/order/views/templates/standard/language/it_it/main.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Main.packages.term_years'] = '%1$s anni';
+$lang['Main.packages.term_year'] = '%1$s anno';
+$lang['Main.packages.term_months'] = '%1$s mesi';
+$lang['Main.packages.term_month'] = '%1$s mese';
+$lang['Main.packages.term_weeks'] = 'settimane %1$s';
+$lang['Main.packages.term_week'] = 'Settimana %1$s';
+$lang['Main.packages.term_days'] = '%1$s giorni';
+$lang['Main.packages.term_day'] = 'giorno %1$s';
+$lang['Main.index.client_limit'] = 'Limite raggiunto';
+$lang['Main.index.sold_out'] = 'Esaurito';
+$lang['Main.index.select_btn'] = 'Aggiungi al carrello';
+$lang['Main.packages.text_free_domain'] = 'Selezionate un pacchetto di questo gruppo con un\'opzione di prezzo pari o superiore a %1$s per un dominio gratuito.';
+$lang['Main.index.field_currency'] = 'Valuta:';
+$lang['Main.index.domain_bundle_eligble'] = 'Qualifica per il dominio gratuito';
+$lang['Main.steps.step_4_description'] = 'Cassa';
+$lang['Main.steps.step_4_name'] = 'Passo 4';
+$lang['Main.steps.step_3_description'] = 'Ordine di revisione';
+$lang['Main.steps.step_3_name'] = 'Passo 3';
+$lang['Main.steps.step_2_description'] = 'Configurazione';
+$lang['Main.steps.step_2_name'] = 'Passo 2';
+$lang['Main.steps.step_1_description'] = 'Scegliete il vostro piano';
+$lang['Main.steps.step_1_name'] = 'Passo 1';
+
