662131ea57888ad594d3cd45b9c55be128037fc4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:43 -0800

    Updated es_es language files

---
+++ language/es_es/actions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Actions
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Actions.!error.location.valid'] = 'Ubicación válida no válida.';
+$lang['Actions.!error.location.unique'] = 'La ubicación y la URL deben ser únicas para esta empresa.';
+$lang['Actions.!error.url.empty'] = 'Por favor, introduzca una URL.';
+$lang['Actions.!error.name.action_empty'] = 'Introduzca un nombre para la acción.';
+$lang['Actions.!error.plugin_id.valid'] = 'Proporcione un ID de plugin válido.';
+$lang['Actions.!error.company_id.valid'] = 'ID de empresa no válida';
+$lang['Actions.!error.editable.valid'] = 'La opción editable debe ser 1 o 0.';
+$lang['Actions.!error.enabled.valid'] = 'La opción activado debe ser 1 o 0.';
+$lang['Actions.getLocations.nav_staff'] = 'Menú de navegación del personal';
+$lang['Actions.getLocations.nav_client'] = 'Menú de navegación del cliente';
+$lang['Actions.getLocations.nav_public'] = 'Menú de navegación pública';
+$lang['Actions.getLocations.widget_client_home'] = 'Widget del panel de control del cliente';
+$lang['Actions.getLocations.widget_staff_home'] = 'Widget del cuadro de mandos del personal';
+$lang['Actions.getLocations.widget_staff_client'] = 'Widget de perfil de cliente';
+$lang['Actions.getLocations.widget_staff_billing'] = 'Widget de facturación del personal';
+$lang['Actions.getLocations.action_staff_client'] = 'Enlace "Acciones de la cuenta" del cliente';
+$lang['Actions.getLocationDescriptions.nav_staff'] = 'Pone un enlace a disposición de la navegación de la interfaz de administración.';
+$lang['Actions.getLocationDescriptions.nav_client'] = 'Pone un enlace a disposición de la navegación de la interfaz cliente.';
+$lang['Actions.getLocationDescriptions.nav_public'] = 'Pone un enlace a disposición de la navegación de la interfaz pública.';
+$lang['Actions.getLocationDescriptions.widget_client_home'] = '	Muestra un widget en el panel de control del cliente.';
+$lang['Actions.getLocationDescriptions.widget_staff_home'] = 'Muestra un widget en el panel de control del personal.';
+$lang['Actions.getLocationDescriptions.widget_staff_client'] = 'Muestra un widget en la página de perfil del cliente.';
+$lang['Actions.getLocationDescriptions.widget_staff_billing'] = 'Muestra un widget en la página de facturación del personal.';
+$lang['Actions.getLocationDescriptions.action_staff_client'] = 'Crea un enlace en la sección "Acciones de la cuenta" de la página de perfil del cliente del personal.';
+
