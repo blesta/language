a174be43dbb017851827ee6dd12f307064f60de2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:58 -0800

    Updated el_gr language files

---
+++ plugins/extension_generator/language/el_gr/admin_nonmerchant.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Admin Nonmerchant
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminNonmerchant.index.page_title'] = 'Γεννήτρια επέκτασης - %1$s';
+$lang['AdminNonmerchant.index.boxtitle_extension_generator'] = 'Γεννήτρια επέκτασης - Non-Merchant Gateway';
+$lang['AdminNonmerchant.basic.heading_basic'] = 'Βασικές πληροφορίες';
+$lang['AdminNonmerchant.basic.heading_authors'] = 'Συγγραφείς';
+$lang['AdminNonmerchant.basic.description'] = 'Περιγραφή';
+$lang['AdminNonmerchant.basic.logo'] = 'Λογότυπο';
+$lang['AdminNonmerchant.basic.signup_url'] = 'URL εγγραφής';
+$lang['AdminNonmerchant.basic.currencies'] = 'Νομίσματα';
+$lang['AdminNonmerchant.basic.author_name'] = 'Όνομα συγγραφέα';
+$lang['AdminNonmerchant.basic.author_url'] = 'Συγγραφέας URL';
+$lang['AdminNonmerchant.basic.text_options'] = 'Επιλογές';
+$lang['AdminNonmerchant.basic.author_row_add'] = 'Προσθήκη συγγραφέα';
+$lang['AdminNonmerchant.basic.text_remove'] = 'Αφαιρέστε το';
+$lang['AdminNonmerchant.basic.tooltip_description'] = 'Η περιγραφή που εμφανίζεται στη λίστα του πρόσθετου';
+$lang['AdminNonmerchant.basic.tooltip_logo'] = 'Το λογότυπο που εμφανίζεται στην καταχώριση του πρόσθετου. Οι εικόνες δεν αλλάζουν μέγεθος, οπότε οι ιδανικές διαστάσεις είναι 150x70';
+$lang['AdminNonmerchant.basic.tooltip_signup_url'] = 'Η διεύθυνση URL για τη σελίδα εγγραφής της πύλης';
+$lang['AdminNonmerchant.basic.tooltip_currencies'] = 'Ένας κατάλογος υποστηριζόμενων νομισμάτων που διαχωρίζεται με κόμμα (π.χ. USD,JPY,EUR)';
+$lang['AdminNonmerchant.basic.placeholder_signup_url'] = 'π.χ. https://dashboard.stripe.com/register';
+$lang['AdminNonmerchant.basic.placeholder_currencies'] = 'π.χ. USD,EUR,JPY';
+$lang['AdminNonmerchant.basic.placeholder_author_name'] = 'π.χ. Blesta';
+$lang['AdminNonmerchant.basic.placeholder_author_url'] = 'π.χ. https://blesta.com/';
+$lang['AdminNonmerchant.basic.fields'] = 'Επόμενο - Πεδία διαμόρφωσης';
+$lang['AdminNonmerchant.basic.confirm'] = 'Επόμενο - Επιβεβαίωση';
+$lang['AdminNonmerchant.fields.heading_fields'] = 'Πεδία διαμόρφωσης';
+$lang['AdminNonmerchant.fields.name'] = 'Όνομα';
+$lang['AdminNonmerchant.fields.label'] = 'Ετικέτα';
+$lang['AdminNonmerchant.fields.type'] = 'Τύπος';
+$lang['AdminNonmerchant.fields.tooltip'] = 'Κείμενο tooltip';
+$lang['AdminNonmerchant.fields.encryptable'] = 'Κρυπτογραφημένο';
+$lang['AdminNonmerchant.fields.tooltip_name'] = 'Το όνομα του πεδίου στη βάση κώδικα';
+$lang['AdminNonmerchant.fields.tooltip_label'] = 'Το όνομα εμφάνισης του πεδίου';
+$lang['AdminNonmerchant.fields.tooltip_type'] = 'Ο τύπος του πεδίου που θα δημιουργηθεί (πλαίσιο ελέγχου, κείμενο κ.λπ.)';
+$lang['AdminNonmerchant.fields.tooltip_tooltip'] = 'Το κείμενο του tooltip που θα εμφανίζεται δίπλα στο πεδίο (αφήστε το κενό για να μην υπάρχει tooltip)';
+$lang['AdminNonmerchant.fields.tooltip_encryptable'] = 'Εάν θα κρυπτογραφηθεί αυτό το πεδίο στη βάση δεδομένων';
+$lang['AdminNonmerchant.fields.placeholder_name'] = 'π.χ. configuration_field';
+$lang['AdminNonmerchant.fields.placeholder_label'] = 'π.χ. Πεδίο διαμόρφωσης';
+$lang['AdminNonmerchant.fields.field_row_add'] = 'Προσθήκη πεδίου διαμόρφωσης';
+$lang['AdminNonmerchant.fields.text_options'] = 'Επιλογές';
+$lang['AdminNonmerchant.fields.text_remove'] = 'Αφαιρέστε το';
+$lang['AdminNonmerchant.fields.features'] = 'Επόμενο - Πρόσθετες λειτουργίες';
+$lang['AdminNonmerchant.features.heading_optional_functions'] = 'Προαιρετικές λειτουργίες';
+$lang['AdminNonmerchant.features.confirm'] = 'Επόμενο - Επιβεβαίωση';
+
