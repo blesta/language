1f056372de745e2f49bb5d56d221619b942446c6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:41 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/standard/language/el_gr/signup.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Signup
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Signup.index.header_login_or_signup'] = 'Εγγραφείτε ή συνδεθείτε';
+$lang['Signup.index.link_resetpassword'] = 'Επαναφορά του κωδικού μου';
+$lang['Signup.index.header_login'] = 'Σύνδεση';
+$lang['Signup.index.heading_contact'] = 'Στοιχεία επικοινωνίας';
+$lang['Signup.index.heading_billing'] = 'Πληροφορίες χρέωσης';
+$lang['Signup.index.heading_authentication'] = 'Αυθεντικοποίηση';
+$lang['Signup.index.heading_settings'] = 'Πρόσθετες ρυθμίσεις';
+
