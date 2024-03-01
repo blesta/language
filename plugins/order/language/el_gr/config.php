581b376e548fd086e033044626b139f5e1a48fca
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:22 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Config
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Config.!error.invalid_pricing_id'] = 'Το πακέτο που δόθηκε δεν βρέθηκε.';
+$lang['Config.!error.invalid_module'] = 'Το πακέτο δεν είναι προς το παρόν διαθέσιμο.';
+$lang['Config.!error.domain_duplicated'] = 'Ο τομέας "%1$s" βρίσκεται ήδη στο καλάθι σας.';
+$lang['Config.!error.invalid_domain_term'] = 'Ο επιλεγμένος όρος για τον τομέα "%1$s" είναι άκυρος.';
+$lang['Config.index.package_price'] = '%1$s @ %2$s';
+$lang['Config.index.package_price_recurring'] = '%1$s @ %2$s (ανανεώνεται @ %3$s)';
+$lang['Config.index.field_module_group_id'] = 'Ομάδα';
+$lang['Config.index.field_default_addon'] = 'Κανένα';
+$lang['Config.index.field_addon_label'] = '%1$s (%2$s @ %3$s)';
+$lang['Config.index.field_addon_label_recurring'] = '%1$s (%2$s @ %3$s, ανανεώνεται @ %4$s)';
+
