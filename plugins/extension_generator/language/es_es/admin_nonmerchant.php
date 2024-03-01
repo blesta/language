af077a47175c90df1755860af9f69c9683696382
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:08 -0800

    Updated es_es language files

---
+++ plugins/extension_generator/language/es_es/admin_nonmerchant.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Admin Nonmerchant
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminNonmerchant.index.page_title'] = 'Generador de extensiones - %1$s';
+$lang['AdminNonmerchant.index.boxtitle_extension_generator'] = 'Generador de extensiones - Pasarela de no comerciantes';
+$lang['AdminNonmerchant.basic.heading_basic'] = 'Información básica';
+$lang['AdminNonmerchant.basic.heading_authors'] = 'Autores';
+$lang['AdminNonmerchant.basic.description'] = 'Descripción';
+$lang['AdminNonmerchant.basic.logo'] = 'Logotipo';
+$lang['AdminNonmerchant.basic.signup_url'] = 'URL de inscripción';
+$lang['AdminNonmerchant.basic.currencies'] = 'Divisas';
+$lang['AdminNonmerchant.basic.author_name'] = 'Nombre del autor';
+$lang['AdminNonmerchant.basic.author_url'] = 'Autor URL';
+$lang['AdminNonmerchant.basic.text_options'] = 'Opciones';
+$lang['AdminNonmerchant.basic.author_row_add'] = 'Añadir autor';
+$lang['AdminNonmerchant.basic.text_remove'] = 'Eliminar';
+$lang['AdminNonmerchant.basic.tooltip_description'] = 'La descripción que aparece en el listado de plugins';
+$lang['AdminNonmerchant.basic.tooltip_logo'] = 'El logotipo que aparece en el listado de plugins. Las imágenes no se redimensionan, por lo que las dimensiones ideales son 150x70';
+$lang['AdminNonmerchant.basic.tooltip_signup_url'] = 'URL de la página de registro de la pasarela';
+$lang['AdminNonmerchant.basic.tooltip_currencies'] = 'Una lista separada por comas de las divisas admitidas (por ejemplo, USD,JPY,EUR)';
+$lang['AdminNonmerchant.basic.placeholder_signup_url'] = 'Por ejemplo: https://dashboard.stripe.com/register';
+$lang['AdminNonmerchant.basic.placeholder_currencies'] = 'p.ej. USD,EUR,JPY';
+$lang['AdminNonmerchant.basic.placeholder_author_name'] = 'por ejemplo, Blesta';
+$lang['AdminNonmerchant.basic.placeholder_author_url'] = 'Por ejemplo: https://blesta.com/';
+$lang['AdminNonmerchant.basic.fields'] = 'Siguiente - Campos de configuración';
+$lang['AdminNonmerchant.basic.confirm'] = 'Siguiente - Confirmación';
+$lang['AdminNonmerchant.fields.heading_fields'] = 'Campos de configuración';
+$lang['AdminNonmerchant.fields.name'] = 'Nombre';
+$lang['AdminNonmerchant.fields.label'] = 'Etiqueta';
+$lang['AdminNonmerchant.fields.type'] = 'Tipo';
+$lang['AdminNonmerchant.fields.tooltip'] = 'Texto de ayuda';
+$lang['AdminNonmerchant.fields.encryptable'] = 'Cifrable';
+$lang['AdminNonmerchant.fields.tooltip_name'] = 'El nombre del campo en la base de código';
+$lang['AdminNonmerchant.fields.tooltip_label'] = 'Nombre de visualización del campo';
+$lang['AdminNonmerchant.fields.tooltip_type'] = 'El tipo de campo a crear (casilla de verificación, texto, etc.)';
+$lang['AdminNonmerchant.fields.tooltip_tooltip'] = 'El texto del tooltip que se mostrará junto al campo (déjelo vacío para no tener tooltip)';
+$lang['AdminNonmerchant.fields.tooltip_encryptable'] = 'Si cifrar este campo en la base de datos';
+$lang['AdminNonmerchant.fields.placeholder_name'] = 'por ejemplo, configuration_field';
+$lang['AdminNonmerchant.fields.placeholder_label'] = 'Por ejemplo, Campo de configuración';
+$lang['AdminNonmerchant.fields.field_row_add'] = 'Añadir campo de configuración';
+$lang['AdminNonmerchant.fields.text_options'] = 'Opciones';
+$lang['AdminNonmerchant.fields.text_remove'] = 'Eliminar';
+$lang['AdminNonmerchant.fields.features'] = 'Siguiente - Características adicionales';
+$lang['AdminNonmerchant.features.heading_optional_functions'] = 'Funciones opcionales';
+$lang['AdminNonmerchant.features.confirm'] = 'Siguiente - Confirmación';
+
