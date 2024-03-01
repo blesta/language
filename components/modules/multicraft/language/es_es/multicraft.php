0278a7a77a92063caeed2d8372a1a2c7736c0338
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:52 -0800

    Updated es_es language files

---
+++ components/modules/multicraft/language/es_es/multicraft.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Multicraft
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Multicraft.!error.module_row.missing'] = 'Se ha producido un error interno. La fila del módulo no está disponible.';
+$lang['Multicraft.name'] = 'Multicraft';
+$lang['Multicraft.module_row.name'] = 'Servidor';
+$lang['Multicraft.module_rows.name'] = 'Servidores';
+$lang['Multicraft.add_module_row'] = 'Añadir servidor';
+$lang['Multicraft.manage.module_rows_title'] = 'Servidores';
+$lang['Multicraft.manage.module_rows_heading.username'] = 'Nombre de usuario';
+$lang['Multicraft.manage.module_rows_heading.server_name'] = 'Etiqueta del servidor';
+$lang['Multicraft.manage.module_rows_heading.panel_api_url'] = 'URL de la API del panel';
+$lang['Multicraft.manage.module_rows_heading.options'] = 'Opciones';
+$lang['Multicraft.manage.module_rows.edit'] = 'Editar';
+$lang['Multicraft.manage.module_rows.delete'] = 'Borrar';
+$lang['Multicraft.manage.module_rows.confirm_delete'] = '¿Está seguro de que desea eliminar este servidor?';
+$lang['Multicraft.manage.module_rows_no_results'] = 'No hay servidores.';
+$lang['Multicraft.manage.module_rows_in_use_0'] = 'No';
+$lang['Multicraft.manage.module_rows_in_use_1'] = 'Sí';
+$lang['Multicraft.service_info.options'] = 'Opciones';
+$lang['Multicraft.service_info.option_login'] = 'Conectarse';
+$lang['Multicraft.tab_client_actions'] = 'Acciones';
+$lang['Multicraft.tab_client_players'] = 'Jugadores';
+$lang['Multicraft.tab_client_console'] = 'Consola';
+$lang['Multicraft.tab_client_actions.heading_server_address'] = 'Dirección del servidor';
+$lang['Multicraft.tab_client_actions.address_ip'] = 'Dirección IP';
+$lang['Multicraft.tab_client_actions.address_port'] = 'Puerto';
+$lang['Multicraft.tab_client_actions.heading_server_status'] = 'Estado del servidor';
+$lang['Multicraft.tab_client_actions.heading_server_actions'] = 'Acciones del servidor';
+$lang['Multicraft.tab_client_actions.action_restart'] = 'Reinicie';
+$lang['Multicraft.tab_client_actions.action_stop'] = 'Stop';
+$lang['Multicraft.tab_client_actions.action_start'] = 'Inicio';
+$lang['Multicraft.tab_client_actions.action_set_daytime'] = 'Ajustar a la hora del día';
+$lang['Multicraft.tab_client_actions.action_set_nighttime'] = 'Configurar en horario nocturno';
+$lang['Multicraft.tab_client_actions.action_server_name'] = 'Establecer nombre de servidor';
+$lang['Multicraft.tab_client_actions.unknown'] = 'Desconocido';
+$lang['Multicraft.tab_client_actions.heading_server_name'] = 'Establecer nombre de servidor';
+$lang['Multicraft.tab_client_console.field_server_name'] = 'Nombre del servidor';
+$lang['Multicraft.tab_client_actions.submit_save'] = 'Guardar';
+$lang['Multicraft.tab_client_actions.status_online'] = 'En línea, %1$s/%2$s jugadores';
+$lang['Multicraft.tab_client_actions.status_offline'] = 'Fuera de línea';
+$lang['Multicraft.tab_client_actions.status_suspended'] = 'Suspendido';
+$lang['Multicraft.tab_client_actions.status_unknown'] = 'Desconocido';
+$lang['Multicraft.tab_client_players.heading_players'] = 'Jugadores conectados';
+$lang['Multicraft.tab_client_players.heading_player_name'] = 'Nombre del jugador';
+$lang['Multicraft.tab_client_players.heading_action'] = 'Acción';
+$lang['Multicraft.tab_client_players.action_kick'] = 'Jugador de patadas';
+$lang['Multicraft.tab_client_players.no_results'] = 'No hay jugadores conectados.';
+$lang['Multicraft.tab_client_players.heading_console'] = 'Consola del servidor';
+$lang['Multicraft.tab_client_console.field_command'] = 'Comando';
+$lang['Multicraft.tab_client_console.submit_send'] = 'Enviar';
+$lang['Multicraft.tab_client_console.chat_entry'] = '[%1$s] %2$s: %3$s';
+$lang['Multicraft.tab_client_console.submit_clear_log'] = 'Borrar registro';
+$lang['Multicraft.tab_client_players.heading_console_tab'] = 'Registros de la consola';
+$lang['Multicraft.tab_client_players.heading_chat_tab'] = 'Registros de chat';
+$lang['Multicraft.add_row.box_title'] = 'Añadir servidor Multicraft';
+$lang['Multicraft.add_row.basic_title'] = 'Configuración básica';
+$lang['Multicraft.add_row.add_btn'] = 'Añadir servidor';
+$lang['Multicraft.add_row.daemons_title'] = 'Direcciones IP dedicadas';
+$lang['Multicraft.add_row.ip_btn'] = 'Añadir dirección IP';
+$lang['Multicraft.add_row.remove_ip'] = 'Eliminar';
+$lang['Multicraft.add_row.daemon_col'] = 'ID de demonio';
+$lang['Multicraft.add_row.ip_col'] = 'Dirección IP';
+$lang['Multicraft.add_row.port_col'] = 'Puerto';
+$lang['Multicraft.add_row.in_use_col'] = 'En uso';
+$lang['Multicraft.edit_row.box_title'] = 'Editar servidor Multicraft';
+$lang['Multicraft.edit_row.basic_title'] = 'Configuración básica';
+$lang['Multicraft.edit_row.add_btn'] = 'Editar servidor';
+$lang['Multicraft.edit_row.daemons_title'] = 'Direcciones IP dedicadas';
+$lang['Multicraft.edit_row.ip_btn'] = 'Añadir dirección IP';
+$lang['Multicraft.edit_row.remove_ip'] = 'Eliminar';
+$lang['Multicraft.edit_row.daemon_col'] = 'ID de demonio';
+$lang['Multicraft.edit_row.ip_col'] = 'Dirección IP';
+$lang['Multicraft.edit_row.port_col'] = 'Puerto';
+$lang['Multicraft.edit_row.in_use_col'] = 'En uso';
+$lang['Multicraft.!tooltip.panel_url'] = 'Introduzca la URL de su panel Multicraft. Por ejemplo: http://multicraft.domain.com/';
+$lang['Multicraft.!tooltip.panel_api_url'] = 'Introduzca la URL de su API Multicraft. Por ejemplo: http://multicraft.domain.com/api.php';
+$lang['Multicraft.!tooltip.key'] = 'Introduzca la clave API generada para su usuario en Multicraft.';
+$lang['Multicraft.!tooltip.log_all'] = 'Marque esta opción para registrar todas las solicitudes de API, o desmarque esta opción para registrar sólo las solicitudes esenciales.';
+$lang['Multicraft.row_meta.server_name'] = 'Etiqueta del servidor';
+$lang['Multicraft.row_meta.panel_url'] = 'URL del panel';
+$lang['Multicraft.row_meta.panel_api_url'] = 'URL de la API del panel';
+$lang['Multicraft.row_meta.username'] = 'Nombre de usuario';
+$lang['Multicraft.row_meta.key'] = 'Clave API';
+$lang['Multicraft.row_meta.log_all'] = 'Registrar todas las solicitudes API';
+
