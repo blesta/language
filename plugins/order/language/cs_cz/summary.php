39f8e135c180cfab0c855e67b81d61f5ea10e052
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:16 -0800

    Updated cs_cz language files

---
+++ plugins/order/language/cs_cz/summary.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Summary
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Summary.index.totals.total_recurring_amount'] = '%1$s %2$s @ %3$s';
+$lang['Summary.index.totals.total_recurring'] = 'Celkem při obnově:';
+$lang['Summary.index.totals.total'] = 'Celkem k úhradě dnes:';
+$lang['Summary.index.totals.tax'] = '%1$s:';
+$lang['Summary.index.totals.subtotal'] = 'Dílčí součet:';
+$lang['Summary.index.totals.discount'] = 'Sleva:';
+$lang['Summary.index.price'] = '@ %1$s';
+
