f79b9cd7b03ddd86cb96dcb67d473111c1a31c4f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:51 -0800

    Updated el_gr language files

---
+++ plugins/softaculous/language/el_gr/softaculous_queued_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Softaculous Queued Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SoftaculousQueuedServices.errors.service_id.exists'] = 'Η συγκεκριμένη υπηρεσία δεν υπάρχει.';
+$lang['SoftaculousQueuedServices.errors.service_id.exists_softaculous'] = 'Η συγκεκριμένη υπηρεσία δεν βρίσκεται στην ουρά για εγκατάσταση.';
+$lang['SoftaculousQueuedServices.errors.company_id.exists'] = 'Η συγκεκριμένη εταιρεία δεν υπάρχει.';
+$lang['SoftaculousQueuedServices.errors.attempts.valid'] = 'Οι προσπάθειες πρέπει να είναι θετική αριθμητική τιμή.';
+
