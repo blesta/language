d8383a28c1be6085cc117ef2110faef0e0e46523
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:10 -0800

    Updated es_es language files

---
+++ plugins/extension_generator/language/es_es/extension_generator_extensions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Extension Generator Extensions
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ExtensionGeneratorExtensions.!error.name.empty'] = 'Introduzca un nombre de extensión.';
+$lang['ExtensionGeneratorExtensions.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['ExtensionGeneratorExtensions.!error.date_updated.format'] = 'Formato de actualización de fecha no válido.';
+$lang['ExtensionGeneratorExtensions.!error.type.valid'] = 'Tipo de extensión no válido.';
+$lang['ExtensionGeneratorExtensions.!error.form_type.valid'] = 'Tipo de formulario no válido.';
+$lang['ExtensionGeneratorExtensions.!error.code_examples.format'] = 'Formato de opción de ejemplos de código no válido.';
+$lang['ExtensionGeneratorExtensions.!error.id.exists'] = 'El ID de extensión indicado no es válido.';
+$lang['ExtensionGeneratorExtensions.gettypes.module'] = 'Módulo';
+$lang['ExtensionGeneratorExtensions.gettypes.plugin'] = 'Plugin';
+$lang['ExtensionGeneratorExtensions.gettypes.merchant'] = 'Portal del comerciante';
+$lang['ExtensionGeneratorExtensions.gettypes.nonmerchant'] = 'Pasarela para no comerciantes';
+$lang['ExtensionGeneratorExtensions.getformtypes.basic'] = 'Básico';
+$lang['ExtensionGeneratorExtensions.getformtypes.advanced'] = 'Avanzado';
+
