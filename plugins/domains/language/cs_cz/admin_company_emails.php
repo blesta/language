93c39904339bc77581afc7992ac9b24bc83284a5
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:31 -0800

    Updated cs_cz language files

---
+++ plugins/domains/language/cs_cz/admin_company_emails.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Domains.domain_expiration_desc'] = 'Oznámení zaslané klientovi krátce po vypršení platnosti jeho domény.';
+$lang['AdminCompanyEmails.templates.Domains.domain_expiration_name'] = 'Oznámení o vypršení platnosti domény';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_2_desc'] = 'Druhé oznámení zaslané klientovi, když jeho doména brzy vyprší.';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_2_name'] = 'Připomenutí vypršení platnosti domény 2';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_1_desc'] = 'První oznámení zaslané klientovi, když jeho doména brzy vyprší.';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_1_name'] = 'Připomenutí vypršení platnosti domény 1';
+
