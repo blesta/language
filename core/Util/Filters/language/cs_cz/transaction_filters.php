c18121cc864e4b330d9500246c350a35219e8db4
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:13 -0800

    Updated cs_cz language files

---
+++ core/Util/Filters/language/cs_cz/transaction_filters.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Transaction Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.transaction_filters.field_maximum_amount'] = 'Maximální částka';
+$lang['Util.filters.transaction_filters.field_minimum_amount'] = 'Minimální částka';
+$lang['Util.filters.transaction_filters.field_amount'] = 'Částka';
+$lang['Util.filters.transaction_filters.field_end_date'] = 'Datum ukončení';
+$lang['Util.filters.transaction_filters.field_start_date'] = 'Datum zahájení';
+$lang['Util.filters.transaction_filters.field_date'] = 'Datum';
+$lang['Util.filters.transaction_filters.field_applied_status'] = 'Použitý stav';
+$lang['Util.filters.transaction_filters.field_reference_id'] = 'Odkaz';
+$lang['Util.filters.transaction_filters.field_transaction_id'] = 'Číslo';
+$lang['Util.filters.transaction_filters.field_payment_type'] = 'Typ platby';
+$lang['Util.filters.transaction_filters.not_applied'] = 'Nebylo použito';
+$lang['Util.filters.transaction_filters.partially_applied'] = 'Částečně použito';
+$lang['Util.filters.transaction_filters.fully_applied'] = 'Plně aplikované';
+$lang['Util.filters.transaction_filters.any'] = 'Jakýkoli';
+
