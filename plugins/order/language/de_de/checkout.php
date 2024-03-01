11638157d71805bc3a95635ae7008162aab5c661
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:07 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/checkout.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Checkout
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Checkout.complete.totals.total_paid'] = 'Insgesamt bezahlt:';
+$lang['Checkout.complete.totals.total'] = 'Insgesamt fällig:';
+$lang['Checkout.complete.totals.tax'] = '%1$s (%2$s%%):';
+$lang['Checkout.complete.totals.subtotal'] = 'Zwischensumme:';
+$lang['Checkout.complete.quantity'] = '%1$s @';
+$lang['Checkout.complete.table_price'] = 'Preis pro Einheit';
+$lang['Checkout.complete.table_qty'] = 'Menge';
+$lang['Checkout.complete.table_description'] = 'Beschreibung';
+$lang['Checkout.complete.almost_complete_description'] = 'Ihre Bestellung ist eingegangen, Ihre Zahlung steht noch aus.';
+$lang['Checkout.complete.complete_description'] = 'Vielen Dank für Ihre Bestellung! Ihre Bestellung ist nun abgeschlossen.';
+$lang['Checkout.complete.order_number'] = 'Bestellung #%1$s';
+$lang['Checkout.ach_info.tooltip_savedetails'] = 'Gespeicherte Zahlungsdaten können für zukünftige Zahlungen verwendet werden, ohne dass die Daten jedes Mal neu eingegeben werden müssen.';
+$lang['Checkout.ach_info.field_savedetails'] = 'Zahlungsdetails speichern';
+$lang['Checkout.ach_info.field_routingnum'] = 'Routing-Nummer';
+$lang['Checkout.ach_info.field_accountnum'] = 'Kontonummer';
+$lang['Checkout.ach_info.field_type'] = 'Typ';
+$lang['Checkout.cc_info.tooltip_code'] = 'Der 3- oder 4-stellige Sicherheitscode, der sich normalerweise auf der Rückseite der Karte befindet.';
+$lang['Checkout.cc_info.tooltip_savedetails'] = 'Gespeicherte Zahlungsdaten können für zukünftige Zahlungen verwendet werden, ohne dass die Daten jedes Mal neu eingegeben werden müssen.';
+$lang['Checkout.cc_info.field_savedetails'] = 'Zahlungsdetails speichern';
+$lang['Checkout.cc_info.field_expiration'] = 'Verfallsdatum';
+$lang['Checkout.cc_info.field_security'] = 'Sicherheitscode';
+$lang['Checkout.cc_info.field_number'] = 'Nummer';
+$lang['Checkout.contact_info.field_email'] = 'E-Mail';
+$lang['Checkout.contact_info.field_zip'] = 'Postleitzahl/Postleitzahl';
+$lang['Checkout.contact_info.field_state'] = 'Staat';
+$lang['Checkout.contact_info.field_country'] = 'Land';
+$lang['Checkout.contact_info.field_city'] = 'Stadt';
+$lang['Checkout.contact_info.field_address2'] = 'Adresse 2';
+$lang['Checkout.contact_info.field_address1'] = 'Adresse 1';
+$lang['Checkout.contact_info.field_last_name'] = 'Nachname';
+$lang['Checkout.contact_info.field_first_name'] = 'Vorname';
+$lang['Checkout.index.totals.total'] = 'Insgesamt fällig:';
+$lang['Checkout.index.totals.paid'] = 'Bezahlt:';
+$lang['Checkout.index.totals.credit'] = 'Kredit:';
+$lang['Checkout.index.totals.tax'] = '%1$s (%2$s%%):';
+$lang['Checkout.index.totals.subtotal'] = 'Zwischensumme:';
+$lang['Checkout.index.totals.discount'] = 'Rabatt:';
+$lang['Checkout.index.totals.setup_fee'] = 'Einrichtungsgebühr:';
+$lang['Checkout.index.field_agree_tos'] = 'Ich habe die <a href="%1$s" target="_blank">Geschäftsbedingungen</a> gelesen und stimme ihnen zu.';
+$lang['Checkout.getpaymentaccounts.account_name'] = '%1$s %2$s - %3$s x%4$s';
+$lang['Checkout.getpaymentaccounts.paymentaccount_ach'] = 'ACH-Konten';
+$lang['Checkout.getpaymentaccounts.paymentaccount_cc'] = 'Kreditkarten-Konten';
+$lang['Checkout.setcontactview.text_none'] = 'Keine';
+$lang['Checkout.index.description_invoice'] = 'Rechnung #%1$s';
+$lang['Checkout.!info.ach_verification'] = 'Sie müssen dieses Konto verifizieren, bevor Sie es für eine Zahlung verwenden können. Dieser Vorgang wird Sie von der aktuellen Seite wegführen.';
+$lang['Checkout.!info.unverified_email_button'] = 'Verifizierungs-E-Mail erneut senden';
+$lang['Checkout.!info.unverified_email'] = 'Es wurde ein Link an die von Ihnen angegebene E-Mail-Adresse gesendet. Bitte klicken Sie auf den Link in der E-Mail, um Ihre E-Mail-Adresse zu verifizieren, bevor Sie mit der Zahlung fortfahren.';
+$lang['Checkout.!error.payment_authorize'] = 'Die Zahlung konnte für das ausgewählte Zahlungskonto nicht autorisiert werden.  Bitte wählen Sie ein anderes Konto oder versuchen Sie es erneut.';
+$lang['Checkout.!error.not_client_owner'] = 'Sie müssen als Client-Eigentümer angemeldet sein, um fortfahren zu können.';
+$lang['Checkout.!error.no_payment_info'] = 'Sie müssen eine Zahlungsmethode auswählen, um fortzufahren.';
+$lang['Checkout.!error.invalid_agree_tos'] = 'Sie müssen Ihr Einverständnis mit den Bedingungen und Konditionen bestätigen.';
+
