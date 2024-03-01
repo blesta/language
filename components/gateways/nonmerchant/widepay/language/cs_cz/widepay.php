53458d2d23837d987d2a06f89d38c617e5fe24b2
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:12 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/widepay/language/cs_cz/widepay.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Widepay
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Widepay.charge_types.card'] = 'Kreditní karta';
+$lang['Widepay.charge_types.ticket'] = 'Vstupenka';
+$lang['Widepay.buildprocess.submit_ticket'] = 'Platba pomocí lístku Wide Pay';
+$lang['Widepay.buildprocess.submit_card'] = 'Platba kreditní kartou Wide Pay';
+$lang['Widepay.!error.wallet_token.format'] = 'Zadejte prosím token API peněženky';
+$lang['Widepay.!error.wallet_id.format'] = 'Zadejte prosím ID peněženky';
+$lang['Widepay.allow_card_payment'] = 'Povolení plateb kartou';
+$lang['Widepay.wallet_token'] = 'Token peněženky';
+$lang['Widepay.wallet_id'] = 'ID peněženky';
+$lang['Widepay.name'] = 'Široká výplata';
+
