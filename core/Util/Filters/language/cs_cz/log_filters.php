1252a2d543e9ad7ced8f91d8600f7f8233ac8f4f
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:09 -0800

    Updated cs_cz language files

---
+++ core/Util/Filters/language/cs_cz/log_filters.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Log Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.log_filters.field_end_date'] = 'Datum ukončení';
+$lang['Util.filters.log_filters.field_start_date'] = 'Datum zahájení';
+$lang['Util.filters.log_filters.field_string'] = 'Obsah';
+
