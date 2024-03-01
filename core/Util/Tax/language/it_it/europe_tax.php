4385e919fe3b9772da227be8d61c40d21cc8f732
pmphillips <paul@phillipsdata.com>
2024-03-01 14:54:21 -0800

    Updated it_it language files

---
+++ core/Util/Tax/language/it_it/europe_tax.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Europe Tax
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.Tax.europe_tax.fields.tax_home_eu_vat'] = 'Casa delle tasse';
+$lang['Util.Tax.europe_tax.fields.note_tax_exempt_eu_vat'] = 'Impostare automaticamente un cliente come esente da imposte se il suo numero di partita IVA è valido e non proviene dal vostro domicilio fiscale.';
+$lang['Util.Tax.europe_tax.fields.tax_exempt_eu_vat'] = 'Imposta automaticamente come esente da imposte';
+$lang['Util.Tax.europe_tax.fields.note_enable_eu_vat'] = 'Selezionare questa opzione per abilitare la convalida del numero di partita IVA dell\'UE per questa azienda. Ciò richiede l\'estensione PHP soap.';
+$lang['Util.Tax.europe_tax.fields.enable_eu_vat'] = 'Abilitare la convalida dell\'IVA UE';
+$lang['Util.Tax.europe_tax.tax_note_reverse_charge'] = 'Viene utilizzato il meccanismo di inversione contabile dell\'IVA. La responsabilità fiscale si trasferisce all\'acquirente.';
+$lang['Util.Tax.europe_tax.tax_deduction_note'] = '%1$s 0%% di %2$s';
+$lang['Util.Tax.europe_tax.tax_id_name'] = 'VATINA';
+$lang['Util.Tax.europe_tax.tax_provider_name'] = 'IVA UE VIES';
+
