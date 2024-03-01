17d054ea46e4e77cd36388ce1215b313469c8115
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:35 -0800

    Updated de_de language files

---
+++ plugins/reassign_pricing/language/de_de/reassign_pricing_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Reassign Pricing Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReassignPricingPlugin.action_staff_client.index'] = 'Preisgestaltung neu zuordnen';
+$lang['ReassignPricingPlugin.description'] = 'Ermöglicht die Aktualisierung von Dienstleistungspreisen mit Preisen anderer Pakete, die dasselbe Modul verwenden.';
+$lang['ReassignPricingPlugin.name'] = 'Preisgestaltung neu zuordnen';
+
