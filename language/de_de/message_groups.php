e8d79447e4cc53db4a33ea0b58f08c384fd86e33
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:06 -0800

    Updated de_de language files

---
+++ language/de_de/message_groups.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Message Groups
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MessageGroups.getTypes.shared'] = 'Gemeinsame';
+$lang['MessageGroups.getTypes.client'] = 'Kunde';
+$lang['MessageGroups.getTypes.staff'] = 'Personal';
+$lang['MessageGroups.!error.tags.empty'] = 'Bitte geben Sie die Tags ein.';
+$lang['MessageGroups.!error.plugin_dir.empty'] = 'Bitte geben Sie das Plugin-Verzeichnis an.';
+$lang['MessageGroups.!error.type.valid'] = 'Der Typ der Nachrichtengruppe muss auf "Kunde", "Mitarbeiter" oder "gemeinsam" eingestellt sein.';
+$lang['MessageGroups.!error.action.empty'] = 'Bitte geben Sie eine Aktion ein.';
+
