a2f1a088dafe5880c41586125a54c82ab181e0cd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:28 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/messenger_manager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Messenger Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MessengerManager.!error.messenger.missing'] = 'Aucun messager n\'est configuré pour le type donné.';
+$lang['MessengerManager.!error.company_id.valid'] = 'L\'identifiant de l\'entreprise n\'est pas valide.';
+$lang['MessengerManager.!error.dir.valid'] = 'Le répertoire n\'est pas valide.';
+$lang['MessengerManager.!error.name.valid'] = 'Le nom n\'est pas valide.';
+$lang['MessengerManager.!error.version.valid'] = 'La version n\'est pas valide.';
+
