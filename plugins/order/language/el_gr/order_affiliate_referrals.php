ded0c3ade1f3c70ff47b6b113ed4b6ba4dd8008c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:26 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/order_affiliate_referrals.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Order Affiliate Referrals
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliateReferrals.!error.referral_id.exists'] = 'Μη έγκυρο αναγνωριστικό παραπομπής.';
+$lang['OrderAffiliateReferrals.!error.affiliate_id.exists'] = 'Μη έγκυρο αναγνωριστικό θυγατρικής.';
+$lang['OrderAffiliateReferrals.!error.order_id.exists'] = 'Μη έγκυρο αναγνωριστικό παραγγελίας.';
+$lang['OrderAffiliateReferrals.!error.name.length'] = 'Το όνομα παραπομπής πρέπει να είναι μικρότερο από 255 χαρακτήρες.';
+$lang['OrderAffiliateReferrals.!error.status.valid'] = 'Παρακαλούμε επιλέξτε μια έγκυρη κατάσταση παραπομπής.';
+$lang['OrderAffiliateReferrals.!error.amount.format'] = 'Το ποσό παραπομπής πρέπει να είναι ένας αριθμός.';
+$lang['OrderAffiliateReferrals.!error.currency.length'] = 'Ο κωδικός νομίσματος πρέπει να έχει μήκος 3 χαρακτήρες.';
+$lang['OrderAffiliateReferrals.!error.commission.format'] = 'Η προμήθεια παραπομπής πρέπει να είναι ένας αριθμός.';
+$lang['OrderAffiliateReferrals.!error.date_added.format'] = 'Η ημερομηνία που προστέθηκε είναι σε μη έγκυρη μορφή ημερομηνίας.';
+$lang['OrderAffiliateReferrals.!error.date_updated.format'] = 'Η ενημερωμένη ημερομηνία έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['OrderAffiliateReferrals.getStatuses.pending'] = 'Εκκρεμεί';
+$lang['OrderAffiliateReferrals.getStatuses.mature'] = 'Ώριμη';
+$lang['OrderAffiliateReferrals.getStatuses.canceled'] = 'Ακυρώθηκε';
+
