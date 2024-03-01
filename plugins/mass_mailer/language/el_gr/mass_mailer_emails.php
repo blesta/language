c29ae7e06c71e38a66902d6fa06352ab00c5eb0f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:11 -0800

    Updated el_gr language files

---
+++ plugins/mass_mailer/language/el_gr/mass_mailer_emails.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Mass Mailer Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerEmails.!error.job_id.valid'] = 'Μη έγκυρο αναγνωριστικό εργασίας.';
+$lang['MassMailerEmails.!error.job_id.unique'] = 'Η εργασία έχει ήδη εκχωρηθεί σε ένα email.';
+$lang['MassMailerEmails.!error.from_name.empty'] = 'Παρακαλώ εισάγετε ένα όνομα από.';
+$lang['MassMailerEmails.!error.from_address.valid'] = 'Παρακαλούμε εισάγετε μια έγκυρη διεύθυνση ηλεκτρονικού ταχυδρομείου.';
+$lang['MassMailerEmails.!error.subject.empty'] = 'Παρακαλώ εισάγετε ένα θέμα.';
+$lang['MassMailerEmails.!error.html.empty'] = 'Παρακαλούμε εισάγετε περιεχόμενο ηλεκτρονικού ταχυδρομείου HTML.';
+$lang['MassMailerEmails.!error.log.valid'] = 'Η επιλογή log πρέπει να είναι 0 ή 1.';
+
