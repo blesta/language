4c2182d2d9f307fcae5c16414c8d5c6514dd033d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:05 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/blacklist.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Blacklist
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Blacklist.!error.rule.exists'] = 'Cette règle existe déjà dans la base de données.';
+$lang['Blacklist.!error.plugin_dir.exists'] = 'Le plugin indiqué n\'existe pas.';
+$lang['Blacklist.!error.type.format'] = 'Le type doit être "ip" ou "email".';
+$lang['Blacklist.!error.rule.format_ip'] = 'La règle fournie n\'est pas une adresse IP valide ou une notation CIDR.';
+$lang['Blacklist.!error.rule.format_email'] = 'La règle fournie n\'est pas une adresse électronique.';
+$lang['Blacklist.type.ip'] = 'Adresse IP / CIDR';
+$lang['Blacklist.type.email'] = 'Adresse électronique';
+
