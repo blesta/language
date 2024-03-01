d1fdb05e51fc2503e64e04bb88dd25f3910f54ce
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:33 -0800

    Updated fr_fr language files

---
+++ components/gateways/nonmerchant/gocardless/language/fr_fr/gocardless.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Gocardless
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Gocardless.name'] = 'GoCardless';
+$lang['Gocardless.description'] = 'Collecter des paiements récurrents dans plus de 30 pays et les verser sur votre compte bancaire existant.';
+$lang['Gocardless.access_token'] = 'Jeton d\'accès';
+$lang['Gocardless.webhook_secret'] = 'Secret du webhook';
+$lang['Gocardless.pay_type'] = 'Options de paiement';
+$lang['Gocardless.pay_type_onetime'] = 'Paiements uniques uniquement';
+$lang['Gocardless.pay_type_subscribe'] = 'Paiements d\'abonnements uniquement';
+$lang['Gocardless.pay_type_both'] = 'Paiements uniques et par abonnement lorsque c\'est possible';
+$lang['Gocardless.dev_mode'] = 'Mode développeur';
+$lang['Gocardless.dev_mode_note'] = 'En activant cette option, les transactions seront enregistrées dans l\'environnement GoCardless Sandbox. N\'activez cette option que si vous testez avec un compte GoCardless Sandbox.';
+$lang['Gocardless.buildprocess.submit'] = 'Payer avec GoCardless';
+$lang['Gocardless.buildprocess.subscription'] = 'S\'abonner avec GoCardless';
+$lang['Gocardless.webhook'] = 'GoCardless Webhook';
+$lang['Gocardless.webhook_note'] = 'Avant de commencer à utiliser cette passerelle, vous devez configurer l\'url suivante en tant que Webhook dans votre compte GoCardless. Le Webhook Secret doit être composé d\'au moins 40 caractères.';
+$lang['Gocardless.!error.access_token.valid'] = 'Vous devez saisir un jeton d\'accès valide.';
+$lang['Gocardless.!error.webhook_secret.valid'] = 'Vous devez saisir un Secret Webhook valide d\'au moins 40 caractères.';
+$lang['Gocardless.!error.dev_mode.valid'] = 'Le mode développeur doit être défini sur "true" s\'il est indiqué.';
+$lang['Gocardless.!tooltip.webhook_secret'] = 'Le Secret Webhook doit être composé d\'au moins 40 caractères.';
+
