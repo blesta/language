d57deab3abbd702b3f96f6195eba076b14ea0d94
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:46 -0800

    Updated es_es language files

---
+++ plugins/order/lib/order_types/registration/language/es_es/order_type_registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Order Type Registration
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderTypeRegistration.name'] = 'Registro de clientes';
+$lang['OrderTypeRegistration.!success.signup'] = 'Gracias por registrar una cuenta con nosotros.';
+$lang['OrderTypeRegistration.!error.template.valid'] = 'El registro de clientes sólo admite la plantilla Estándar.';
+
