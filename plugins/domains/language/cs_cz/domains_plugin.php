8078bcf9c2abd4171e95e055f5699154977702f1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:35 -0800

    Updated cs_cz language files

---
+++ plugins/domains/language/cs_cz/domains_plugin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Domains Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsPlugin.upgrade.domains_id_protection_option_group'] = 'Všechna doménová jména musí mít podle pravidel ICANN platné kontaktní údaje. Tato možnost skryje tyto údaje před veřejným whois a zachová tak vaše soukromí.';
+$lang['DomainsPlugin.upgrade.domains_email_forwarding_option_group'] = 'Vytvořte e-mailové adresy ve své doméně, které budou přesměrovány na externí e-mailovou adresu.';
+$lang['DomainsPlugin.upgrade.domains_dns_management_option_group'] = 'Správa zóny DNS pro název domény prostřednictvím námi poskytovaných jmenných serverů. Vytvářejte a upravujte záznamy, jako jsou A, CNAME, MX, TXT a další pro vaši doménu.';
+$lang['DomainsPlugin.widget_client_home.main'] = 'Domény';
+$lang['DomainsPlugin.widget_staff_home.main'] = 'Domény';
+$lang['DomainsPlugin.enabled'] = 'Povoleno';
+$lang['DomainsPlugin.id_protection.description'] = 'Ochrana ID';
+$lang['DomainsPlugin.id_protection.name'] = 'Ochrana ID';
+$lang['DomainsPlugin.dns_management.description'] = 'Správa DNS';
+$lang['DomainsPlugin.dns_management.name'] = 'Správa DNS';
+$lang['DomainsPlugin.email_forwarding.description'] = 'Přeposílání e-mailů';
+$lang['DomainsPlugin.email_forwarding.name'] = 'Přeposílání e-mailů';
+$lang['DomainsPlugin.tld_package_group.description'] = 'Skupina balíčků pro skrytí a správu všech cenových balíčků TLD';
+$lang['DomainsPlugin.tld_package_group.name'] = 'Cenové balíčky TLD';
+$lang['DomainsPlugin.permission.admin_domains.configuration'] = 'Konfigurace';
+$lang['DomainsPlugin.permission.admin_domains.whois'] = 'Whois';
+$lang['DomainsPlugin.permission.admin_domains.registrars'] = 'Registrátoři';
+$lang['DomainsPlugin.permission.admin_domains.tlds'] = 'Ceny TLD';
+$lang['DomainsPlugin.permission.admin_domains.browse'] = 'Domény';
+$lang['DomainsPlugin.permission.admin_domains'] = 'Domény';
+$lang['DomainsPlugin.card_client.getDomainCount'] = 'Domény';
+$lang['DomainsPlugin.nav_client.domains'] = 'Domény';
+$lang['DomainsPlugin.nav_client.services'] = 'Služby';
+$lang['DomainsPlugin.nav_secondary_staff.domains'] = 'Domény';
+$lang['DomainsPlugin.nav_secondary_staff.domain_options'] = 'Možnosti domény';
+$lang['DomainsPlugin.getCronTasks.domain_renewal_reminders_description'] = 'Odesílání e-mailových upomínek pro domény, jejichž platnost se blíží k datu vypršení.';
+$lang['DomainsPlugin.getCronTasks.domain_renewal_reminders'] = 'Odesílání připomenutí o vypršení platnosti';
+$lang['DomainsPlugin.getCronTasks.domain_term_change_description'] = 'Změna služeb s dobou trvání delší než jeden rok na roční dobu trvání.';
+$lang['DomainsPlugin.getCronTasks.domain_term_change'] = 'Změna termínu domény';
+$lang['DomainsPlugin.getCronTasks.domain_tld_synchronization_description'] = 'Synchronizace balíčků s cenami TLD z modulu registrátora';
+$lang['DomainsPlugin.getCronTasks.domain_tld_synchronization'] = 'Synchronizace domén TLD';
+$lang['DomainsPlugin.getCronTasks.domain_synchronization_description'] = 'Synchronizace služeb domény s datem expirace z modulu jejich registrátora';
+$lang['DomainsPlugin.getCronTasks.domain_synchronization'] = 'Synchronizace domén';
+$lang['DomainsPlugin.!error.module_id.exists'] = 'Neplatné ID modulu.';
+$lang['DomainsPlugin.description'] = 'Slouží k řešení různých aspektů správy a prodeje domén. Tento zásuvný modul je v současné době ve fázi beta.';
+$lang['DomainsPlugin.name'] = 'Správce domény';
+
