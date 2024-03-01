5ca4a2671eed30e84cf2b08b4d83383daf5bc08d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:55 -0800

    Updated fr_fr language files

---
+++ plugins/domains/language/fr_fr/domains_feed.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Domains Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsFeed.name'] = 'Prix du domaine';
+$lang['DomainsFeed.description'] = 'Renvoie un tableau HTML contenant les prix de tous les TLD.';
+$lang['DomainsFeed.getOptionFields.title_row_example_code'] = 'Exemple de code';
+$lang['DomainsFeed.getOptionFields.example_code_table'] = 'Afficher un tableau contenant les prix de tous les TLD dans une devise donnée :';
+$lang['DomainsFeed.getOptionFields.header_name'] = 'Nom';
+$lang['DomainsFeed.getOptionFields.header_description'] = 'Description';
+$lang['DomainsFeed.getOptionFields.params'] = 'Paramètres';
+$lang['DomainsFeed.getOptionFields.param_currency'] = 'monnaie';
+$lang['DomainsFeed.getOptionFields.param_currency_description'] = 'Code à 3 caractères de la devise pour laquelle il faut obtenir un prix.';
+$lang['DomainsFeed.getOptionFields.param_style'] = 'style';
+$lang['DomainsFeed.getOptionFields.param_style_description'] = 'Le style de tableau à récupérer : html ou bootstrap.';
+$lang['DomainsFeed.getOptionFields.param_term'] = 'terme';
+$lang['DomainsFeed.getOptionFields.param_term_description'] = 'Une liste de termes annuels séparés par des virgules à inclure dans le tableau des prix.';
+$lang['DomainsFeed.table.heading_tlds'] = 'TLDs';
+$lang['DomainsFeed.table.heading_year'] = '%1$s Année';
+$lang['DomainsFeed.table.heading_years'] = '%1$s Années';
+$lang['DomainsFeed.table.register'] = 'Registre';
+$lang['DomainsFeed.table.transfer'] = 'Transfert';
+$lang['DomainsFeed.table.renew'] = 'Renouveler';
+$lang['DomainsFeed.table.not_available'] = 'Non disponible';
+$lang['DomainsFeed.!error.invalid_endpoint'] = 'Le point d\'accès demandé n\'est pas valide ou n\'existe pas.';
+$lang['DomainsFeed.!error.invalid_style'] = 'Le style demandé n\'est pas valide.';
+
