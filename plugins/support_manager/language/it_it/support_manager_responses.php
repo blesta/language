f52c9a989a2b61433706f816d94db324a70f78ee
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:50 -0800

    Updated it_it language files

---
+++ plugins/support_manager/language/it_it/support_manager_responses.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Support Manager Responses
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerResponses.!error.category_id.root_responses'] = 'La categoria non può essere eliminata perché contiene risposte predefinite.';
+$lang['SupportManagerResponses.!error.details.empty'] = 'Inserire i dettagli della risposta.';
+$lang['SupportManagerResponses.!error.name.response_length'] = 'Il nome della categoria non può superare i 64 caratteri.';
+$lang['SupportManagerResponses.!error.name.response_empty'] = 'Inserire un nome per questa risposta.';
+$lang['SupportManagerResponses.!error.category_id.exists'] = 'ID categoria non valido.';
+$lang['SupportManagerResponses.!error.name.length'] = 'Il nome della categoria non può superare i 64 caratteri.';
+$lang['SupportManagerResponses.!error.name.empty'] = 'Inserire il nome della categoria.';
+$lang['SupportManagerResponses.!error.parent_id.company'] = 'Questa categoria deve appartenere alla stessa azienda del suo genitore.';
+$lang['SupportManagerResponses.!error.parent_id.exists'] = 'ID categoria genitore non valido.';
+$lang['SupportManagerResponses.!error.company_id.exists'] = 'ID azienda non valido.';
+
