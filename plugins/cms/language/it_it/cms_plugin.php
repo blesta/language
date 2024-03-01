dd37481cfcc4f955b6a645e9387ad531067534d5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:34 -0800

    Updated it_it language files

---
+++ plugins/cms/language/it_it/cms_plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Cms Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CmsPlugin.index.title'] = 'Portale clienti';
+$lang['CmsPlugin.description'] = 'Il portale consente di impostare il contenuto della pagina da visualizzare all\'URL della homepage di Blesta.';
+$lang['CmsPlugin.name'] = 'Portale';
+
