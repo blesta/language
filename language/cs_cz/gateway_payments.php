3c1e0494bf695a2bd957b25412bdab3106dc54fb
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:00 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/gateway_payments.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Gateway Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GatewayPayments.!error.type.invalid'] = 'Typ účtu musí být "ach" nebo "cc".';
+$lang['GatewayPayments.!error.gateway.error'] = 'Platební brána při zpracování požadavku vrátila chybu.';
+$lang['GatewayPayments.!error.gateway.declined'] = 'Platba byla odmítnuta.';
+$lang['GatewayPayments.!error.response_status'] = 'Platební brána vrátila neočekávanou odpověď.';
+$lang['GatewayPayments.!error.reference_id.remove'] = 'Platební brána nemohla odstranit platební účet.';
+$lang['GatewayPayments.!error.reference_id.verify'] = 'Platební brána nemohla ověřit platební účet.';
+$lang['GatewayPayments.!error.reference_id.update'] = 'Platební brána nemohla aktualizovat platební účet.';
+$lang['GatewayPayments.!error.reference_id.store'] = 'Platební brána nemohla uložit platební účet.';
+$lang['GatewayPayments.!error.amount.valid'] = 'Částka musí být větší než nula.';
+$lang['GatewayPayments.!error.type.instance'] = 'Brána tímto způsobem platby nepodporuje.';
+$lang['GatewayPayments.!error.type.valid'] = 'Typ platby není pro tuto bránu podporován.';
+$lang['GatewayPayments.!error.contact_id.exists'] = 'Kontakt na tohoto klienta se nepodařilo najít.';
+$lang['GatewayPayments.!error.account_id.exists'] = 'Platební účet se pro daného klienta nepodařilo najít.';
+$lang['GatewayPayments.!error.transaction_id.exists'] = 'Transakci se pro daného klienta nepodařilo najít.';
+$lang['GatewayPayments.!error.gateway.exists'] = 'Brána neexistuje nebo není povolena.';
+
