0c5f3d64d0c97e01fd9d37237c2c194445635cfa
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:21 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/cart.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Cart
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cart.!success.coupon_applied'] = 'Το κουπόνι εφαρμόστηκε με επιτυχία.';
+$lang['Cart.!success.item_removed'] = 'Το προϊόν αφαιρέθηκε από το καλάθι σας.';
+$lang['Cart.!success.all_items_removed'] = 'Όλα τα προϊόντα έχουν αφαιρεθεί από το καλάθι σας.';
+$lang['Cart.!error.coupon_applied'] = 'Το κουπόνι δεν μπόρεσε να εφαρμοστεί.';
+$lang['Cart.index.quantity'] = '%1$s @';
+$lang['Cart.index.totals.discount'] = 'Έκπτωση:';
+$lang['Cart.index.totals.subtotal'] = 'Υποσύνολο:';
+$lang['Cart.index.totals.tax'] = '%1$s:';
+$lang['Cart.index.totals.total'] = 'Συνολικά οφειλόμενα σήμερα:';
+$lang['Cart.index.totals.total_recurring'] = 'Σύνολο κατά την ανανέωση:';
+$lang['Cart.index.totals.total_recurring_amount'] = '%1$s %2$s @ %3$s';
+
