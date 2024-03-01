fd6697e65420099c4f8e3c205646a48258cf6281
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:08 -0800

    Updated el_gr language files

---
+++ language/el_gr/client_quotations.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Client Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientQuotations.invoices.no_results'] = 'Αυτή η προσφορά δεν έχει συσχετιστεί με τιμολόγια.';
+$lang['ClientQuotations.invoices.heading_date_billed'] = 'Ημερομηνία χρέωσης';
+$lang['ClientQuotations.invoices.heading_paid'] = 'Πληρωμένο';
+$lang['ClientQuotations.invoices.heading_amount'] = 'Ποσό';
+$lang['ClientQuotations.invoices.heading_invoice'] = 'Τιμολόγιο #';
+$lang['ClientQuotations.approve.field_submit'] = 'Έγκριση';
+$lang['ClientQuotations.approve.field_cancel'] = 'Ακύρωση, μη έγκριση';
+$lang['ClientQuotations.approve.field_password'] = 'Συνδεθείτε με κωδικό πρόσβασης για να εγκρίνετε';
+$lang['ClientQuotations.approve.heading_approve'] = 'Έγκριση';
+$lang['ClientQuotations.index.no_results'] = 'Δεν έχετε %1$s Παραθέσεις.';
+$lang['ClientQuotations.index.option_approve'] = 'Έγκριση';
+$lang['ClientQuotations.index.option_view'] = 'Προβολή';
+$lang['ClientQuotations.index.heading_options'] = 'Επιλογές';
+$lang['ClientQuotations.index.heading_date_expires'] = 'Ημερομηνία Λήξης';
+$lang['ClientQuotations.index.heading_date_created'] = 'Ημερομηνία δημιουργίας';
+$lang['ClientQuotations.index.heading_total'] = 'Ποσό';
+$lang['ClientQuotations.index.heading_subtotal'] = 'Υποσύνολο';
+$lang['ClientQuotations.index.heading_title'] = 'Τίτλος';
+$lang['ClientQuotations.index.heading_quotation'] = 'Προσφορά #';
+$lang['ClientQuotations.index.boxtitle_quotations'] = 'Αποσπάσματα';
+$lang['ClientQuotations.index.category_expired'] = 'Λειτούργησε';
+$lang['ClientQuotations.index.category_approved'] = 'Εγκεκριμένο';
+$lang['ClientQuotations.index.category_pending'] = 'Εκκρεμεί';
+$lang['ClientQuotations.index.page_title'] = 'Αποσπάσματα πελατών #%1$s';
+$lang['ClientQuotations.!success.approved_quotation'] = 'Η προσφορά εγκρίθηκε με επιτυχία.';
+$lang['ClientQuotations.!error.password_mismatch'] = 'Ο κωδικός πρόσβασης δεν ταιριάζει με τον κωδικό πρόσβασής σας.';
+
