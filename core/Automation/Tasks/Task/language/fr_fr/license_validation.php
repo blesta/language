ca7d994a07676d75480dc76f513b7b1dd4a57d96
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:22 -0800

    Updated fr_fr language files

---
+++ core/Automation/Tasks/Task/language/fr_fr/license_validation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * License Validation
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.license_validation.attempt'] = 'Tentative de validation de la licence.';
+$lang['Automation.task.license_validation.completed'] = 'La tâche de validation de la licence est terminée.';
+
