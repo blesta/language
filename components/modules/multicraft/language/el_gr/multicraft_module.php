7a58608c301d89a089ab23d842143af41a415a56
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:53 -0800

    Updated el_gr language files

---
+++ components/modules/multicraft/language/el_gr/multicraft_module.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Multicraft Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MulticraftModule.!error.ips_in_use.match'] = 'Ορίστε αν η διεύθυνση IP χρησιμοποιείται αυτή τη στιγμή σε 1 ή 0.';
+$lang['MulticraftModule.!error.daemons.match'] = 'Ορίστε ένα Daemon ID για κάθε διεύθυνση IP που έχει οριστεί.';
+$lang['MulticraftModule.!error.ips.match'] = 'Ορίστε μια διεύθυνση IP για κάθε σύνολο Daemon.';
+$lang['MulticraftModule.!error.log_all.format'] = 'Η επιλογή για την καταγραφή όλων των αιτήσεων API πρέπει να οριστεί σε 1 ή 0.';
+$lang['MulticraftModule.!error.key.empty'] = 'Εισάγετε το κλειδί σας API';
+$lang['MulticraftModule.!error.username.empty'] = 'Παρακαλώ εισάγετε το όνομα χρήστη Multicraft';
+$lang['MulticraftModule.!error.panel_api_url.empty'] = 'Εισάγετε τη διεύθυνση URL για το API του πίνακα Multicraft.';
+$lang['MulticraftModule.!error.panel_url.empty'] = 'Παρακαλούμε εισάγετε τη διεύθυνση URL του πίνακα Multicraft.';
+$lang['MulticraftModule.!error.server_name.empty'] = 'Παρακαλούμε εισάγετε μια ετικέτα για αυτόν τον διακομιστή.';
+$lang['MulticraftModule.!error.json.unavailable'] = 'Η επέκταση JSON απαιτείται από αυτήν την ενότητα.';
+
