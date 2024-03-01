dd0024662ee6fc37bee96a9b14c128c9accd8137
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:02 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/coin_payments/language/cs_cz/coin_payments.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Coin Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CoinPayments.buildprocess.submit'] = 'Plaťte pomocí služby CoinPayments';
+$lang['CoinPayments.ipn_secret'] = 'Tajemství IPN';
+$lang['CoinPayments.merchant_id'] = 'ID obchodníka CoinPayments';
+$lang['CoinPayments.description'] = 'Pokladní systém pro kryptoměny, jako je Bitcoin a Litecoin, s nízkými poplatky.';
+$lang['CoinPayments.name'] = 'CoinPayments.net';
+
