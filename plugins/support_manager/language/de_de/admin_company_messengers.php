54cb5909405a89a7da000af436727218ed790fdc
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:39 -0800

    Updated de_de language files

---
+++ plugins/support_manager/language/de_de/admin_company_messengers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Admin Company Messengers
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyMessengers.templates.SupportManager.staff_ticket_assigned_desc'] = 'Mitteilung an einen Mitarbeiter, wenn ihm ein Ticket zugewiesen wurde.';
+$lang['AdminCompanyMessengers.templates.SupportManager.staff_ticket_assigned_name'] = 'Mitarbeiter Ticket zugewiesen';
+$lang['AdminCompanyMessengers.templates.SupportManager.staff_ticket_updated_desc'] = 'Mitteilung an einen Mitarbeiter, wenn ein Ticket aktualisiert wird.';
+$lang['AdminCompanyMessengers.templates.SupportManager.staff_ticket_updated_name'] = 'Personal-Ticket aktualisiert';
+
