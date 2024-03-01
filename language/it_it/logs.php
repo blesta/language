3a1501f04808b6509eec0b94e90eae43b413040b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:07 -0800

    Updated it_it language files

---
+++ language/it_it/logs.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Logs
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Logs.!error.end_date.format'] = 'Formato della data finale non valido.';
+$lang['Logs.!error.start_date.format'] = 'Formato della data di inizio non valido.';
+$lang['Logs.!error.group.betweenlength'] = 'Il gruppo deve avere una lunghezza compresa tra 1 e 32 caratteri.';
+$lang['Logs.!error.group.unique'] = 'L\'ID del task cron e il gruppo sono già stati presi.';
+$lang['Logs.!error.event.maxlength'] = 'La lunghezza dell\'evento non può superare i 32 caratteri.';
+$lang['Logs.!error.run_id.exists'] = 'ID attività cron non valido.';
+$lang['Logs.!error.last_name.empty'] = 'Il cognome non deve essere vuoto.';
+$lang['Logs.!error.first_name.empty'] = 'Il nome non deve essere vuoto.';
+$lang['Logs.!error.account_id.empty'] = 'ID conto mancante.';
+$lang['Logs.!error.account_type.format'] = 'Tipo di conto non valido.';
+$lang['Logs.!error.type.format'] = 'Tipo non valido.';
+$lang['Logs.!error.staff_id.empty'] = 'ID personale mancante.';
+$lang['Logs.!error.date_changed.valid'] = 'Data modificata non valida.';
+$lang['Logs.!error.by_user_id.exists'] = 'Non valido per ID utente.';
+$lang['Logs.!error.fields.empty'] = 'Mancano i campi registrabili.';
+$lang['Logs.!error.client_id.exists'] = 'ID cliente non valido.';
+$lang['Logs.!error.contact_id.empty'] = 'ID contatto mancante.';
+$lang['Logs.!error.success.format'] = 'Valore di successo non valido.';
+$lang['Logs.!error.messenger_id.exists'] = 'ID messenger non valido.';
+$lang['Logs.!error.to_user_id.exists'] = 'ID utente non valido.';
+$lang['Logs.!error.result.format'] = 'Il risultato del login deve essere "successo" o "fallimento".';
+$lang['Logs.!error.user_log_exists.empty'] = 'L\'utente all\'indirizzo IP indicato non esiste.';
+$lang['Logs.!error.ip_address.length'] = 'L\'indirizzo IP non può superare i 39 caratteri.';
+$lang['Logs.!error.ip_address.empty'] = 'Indirizzo IP mancante.';
+$lang['Logs.!error.user_id.empty'] = 'ID utente mancante.';
+$lang['Logs.!error.group.maxlength'] = 'La lunghezza del gruppo non può superare gli 8 caratteri.';
+$lang['Logs.!error.group.empty'] = 'Gruppo mancante.';
+$lang['Logs.!error.status.format'] = 'Tipo di stato non valido.';
+$lang['Logs.!error.url.empty'] = 'URL mancante.';
+$lang['Logs.!error.direction.format'] = 'Tipo di direzione non valido.';
+$lang['Logs.!error.gateway_id.exists'] = 'ID gateway non valido.';
+$lang['Logs.!error.module_id.exists'] = 'ID modulo non valido.';
+$lang['Logs.!error.staff_id.exists'] = 'ID personale non valido.';
+$lang['Logs.!error.sent.length'] = 'La lunghezza dell\'invio non può superare 1 carattere.';
+$lang['Logs.!error.sent.format'] = 'L\'invio deve essere un numero.';
+$lang['Logs.!error.subject.empty'] = 'Manca un soggetto.';
+$lang['Logs.!error.from_name.empty'] = 'Manca un nome di provenienza.';
+$lang['Logs.!error.from_address.empty'] = 'Manca un indirizzo di posta elettronica.';
+$lang['Logs.!error.to_address.empty'] = 'Manca un indirizzo.';
+$lang['Logs.!error.company_id.exists'] = 'ID azienda non valido.';
+$lang['Logs.!error.deletelog_datetime'] = 'Formato di data non valido.';
+
