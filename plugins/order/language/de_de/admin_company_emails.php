fc5a441391df91dfbdb23b55230f0cbc76daa76b
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:01 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/admin_company_emails.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_third_desc'] = 'Dritte und letzte Mitteilung an einen Kunden, nachdem er einen unbezahlten Auftrag aufgegeben hat.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_third_name'] = 'Abgebrochene Auftragstage (3. Kündigung)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_second_desc'] = 'Zweite Mitteilung an einen Kunden, nachdem er einen unbezahlten Auftrag aufgegeben hat.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_second_name'] = 'Abgebrochene Auftragstage (2. Mitteilung)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_first_desc'] = 'Erste Mitteilung an einen Kunden, nachdem er einen unbezahlten Auftrag aufgegeben hat.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_first_name'] = 'Abgebrochene Auftragstage (1. Kündigung)';
+$lang['AdminCompanyEmails.templates.Order.affiliate_monthly_report_desc'] = 'Bericht mit den Anmeldungen und den zugehörigen Daten für den letzten Monat.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_monthly_report_name'] = 'Partnerbericht (Monatlich)';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_received_desc'] = 'Mitteilung, die an einen Kunden gesendet wird, wenn sein Auszahlungsantrag eingereicht wird.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_received_name'] = 'Erhaltene Affiliate-Auszahlung';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_desc'] = 'Mitteilung an das Personal, wenn ein Auszahlungsantrag eingereicht wird.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_name'] = 'Auszahlungsantrag für Partner';
+$lang['AdminCompanyEmails.templates.Order.received_mobile_desc'] = 'Benachrichtigung bei Eingang einer Bestellung, die für mobile Geräte bestimmt ist.';
+$lang['AdminCompanyEmails.templates.Order.received_mobile_name'] = 'Bestellung erhalten (Mobil)';
+$lang['AdminCompanyEmails.templates.Order.received_desc'] = 'Benachrichtigung bei Eingang einer Bestellung.';
+$lang['AdminCompanyEmails.templates.Order.received_name'] = 'Erhaltene Bestellung';
+
