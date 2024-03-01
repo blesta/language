a754b95a1da106394420bdef61811ab5369455ab
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:13 -0800

    Updated cs_cz language files

---
+++ core/Util/Filters/language/cs_cz/service_filters.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Service Filters
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Util.filters.service_filters.field_service_meta'] = 'Služba Meta';
+$lang['Util.filters.service_filters.field_package_name'] = 'Název balíčku';
+$lang['Util.filters.service_filters.field_module_id'] = 'Modul';
+$lang['Util.filters.service_filters.any'] = 'Jakýkoli';
+
