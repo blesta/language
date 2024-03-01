8471affb18ee5147831309361f44ebced9cfe311
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:26 -0800

    Updated de_de language files

---
+++ components/modules/gogetssl/language/de_de/gogetssl.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Gogetssl
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GoGetSSL.tab_reissue.heading_reissue'] = 'Neuauflage';
+$lang['GoGetSSL.tab_reissue'] = 'Neuauflage';
+$lang['GoGetSSL.service_field.gogetssl_fax'] = 'Faxnummer (internationales Format)';
+$lang['GoGetSSL.service_field.gogetssl_organization_unit'] = 'Organisationseinheit (geben Sie NA ein, falls nicht zutreffend)';
+$lang['GoGetSSL.service_field.gogetssl_organization'] = 'Organisation (geben Sie NA ein, falls nicht zutreffend)';
+$lang['GoGetSSL.service_field.gogetssl_number'] = 'Telefonnummer (internationales Format)';
+$lang['GoGetSSL.service_field.gogetssl_email'] = 'E-Mail';
+$lang['GoGetSSL.service_field.gogetssl_country'] = 'Land';
+$lang['GoGetSSL.service_field.gogetssl_state'] = 'Staat';
+$lang['GoGetSSL.service_field.gogetssl_zip'] = 'ZIP';
+$lang['GoGetSSL.service_field.gogetssl_city'] = 'Stadt';
+$lang['GoGetSSL.service_field.gogetssl_address2'] = 'Anschrift Zeile 2';
+$lang['GoGetSSL.service_field.gogetssl_address1'] = 'Anschrift Zeile 1';
+$lang['GoGetSSL.service_field.gogetssl_lastname'] = 'Nachname';
+$lang['GoGetSSL.service_field.gogetssl_firstname'] = 'Vornamen';
+$lang['GoGetSSL.service_field.gogetssl_title'] = 'Titel';
+$lang['GoGetSSL.service_field.gogetssl_approver_email'] = 'Genehmiger E-Mail';
+$lang['GoGetSSL.service_field.gogetssl_webserver_type'] = 'Webserver-Typ';
+$lang['GoGetSSL.service_field.gogetssl_csr'] = 'CSR-Inhalt';
+$lang['GoGetSSL.service_field.gogetssl_fqdn'] = 'Host/Domäne';
+$lang['GoGetSSL.package_fields.product'] = 'Produkt';
+$lang['GoGetSSL.edit_row.add_btn'] = 'Update Wiederverkäufer';
+$lang['GoGetSSL.edit_row.basic_title'] = 'Grundeinstellungen';
+$lang['GoGetSSL.edit_row.box_title'] = 'GoGetSSL Wiederverkäufer bearbeiten';
+$lang['GoGetSSL.add_row.add_btn'] = 'Wiederverkäufer hinzufügen';
+$lang['GoGetSSL.add_row.basic_title'] = 'Grundeinstellungen';
+$lang['GoGetSSL.add_row.box_title'] = 'GoGetSSL Wiederverkäufer hinzufügen';
+$lang['GoGetSSL.row_meta.sandbox'] = 'Sandkasten';
+$lang['GoGetSSL.row_meta.api_password'] = 'API-Kennwort';
+$lang['GoGetSSL.row_meta.api_username'] = 'API-Benutzername';
+$lang['GoGetSSL.row_meta.gogetssl_name'] = 'Name des Wiederverkäufers';
+$lang['GoGetSSL.manage.module_rows_heading.options'] = 'Optionen';
+$lang['GoGetSSL.manage.module_rows_heading.gogetssl_name'] = 'Name des Wiederverkäufers';
+$lang['GoGetSSL.manage.module_rows_no_results'] = 'Es gibt keine Wiederverkäufer.';
+$lang['GoGetSSL.manage.module_rows.confirm_delete'] = 'Sind Sie sicher, dass Sie diesen Wiederverkäufer löschen möchten?';
+$lang['GoGetSSL.manage.module_rows.delete'] = 'Löschen';
+$lang['GoGetSSL.manage.module_rows.edit'] = 'bearbeiten';
+$lang['GoGetSSL.manage.module_rows_title'] = 'GoGetSSL Wiederverkäufer';
+$lang['GoGetSSL.add_module_row'] = 'Wiederverkäufer hinzufügen';
+$lang['GoGetSSL.module_row_plural'] = 'Wiederverkäufer';
+$lang['GoGetSSL.module_row'] = 'GoGetSSL Wiederverkäufer';
+$lang['GoGetSSL.description'] = 'GoGetSSL bietet seit 2009 SSL-Zertifikate an.';
+$lang['GoGetSSL.name'] = 'GoGetSSL';
+$lang['GoGetSSL.please_select'] = '-- Bitte wählen Sie --';
+$lang['GoGetSSL.!error.api.internal'] = 'Es ist ein interner Fehler aufgetreten, oder der Server hat nicht auf die Anfrage geantwortet.';
+$lang['GoGetSSL.!error.meta[gogetssl_name].empty'] = 'Bitte wählen Sie mindestens einen Wiederverkäufer aus.';
+$lang['GoGetSSL.!error.meta[product].valid'] = 'Bitte wählen Sie ein Produkt aus.';
+$lang['GoGetSSL.!error.gogetssl_webserver_type.format'] = 'Bitte geben Sie einen gültigen Webserver-Typ ein.';
+$lang['GoGetSSL.!error.gogetssl_fqdn.format'] = 'Bitte geben Sie eine gültige Domain ein.';
+$lang['GoGetSSL.!error.gogetssl_csr.format'] = 'Bitte geben Sie einen Wert CSR ein.';
+$lang['GoGetSSL.!error.gogetssl_approver_email.format'] = 'Bitte geben Sie eine gültige Genehmigungs-E-Mail ein.';
+$lang['GoGetSSL.!error.gogetssl_validity.empty'] = 'Bitte geben Sie einen Gültigkeitszeitraum an.';
+$lang['GoGetSSL.!error.gogetssl_name.empty'] = 'Bitte geben Sie einen Händlernamen ein.';
+$lang['GoGetSSL.!error.api_password.empty'] = 'Bitte geben Sie ein API-Passwort ein.';
+$lang['GoGetSSL.!error.api_username.valid'] = 'Eine Verbindung zum Server konnte nicht hergestellt werden. Bitte überprüfen Sie, ob die API-Anmeldedaten korrekt sind.';
+$lang['GoGetSSL.!error.api_username.empty'] = 'Bitte geben Sie einen API-Benutzernamen ein.';
+
