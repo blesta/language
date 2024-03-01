81ddf0b78956db4e6f9a61c5ff8042ca3a0f8f9b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:15 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/admin_affiliates.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Admin Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminAffiliates.!success.affiliate_added'] = 'Ο συνεργάτης προστέθηκε επιτυχώς.';
+$lang['AdminAffiliates.!success.settings_updated'] = 'Οι ρυθμίσεις θυγατρικών έχουν ενημερωθεί.';
+$lang['AdminAffiliates.!success.affiliate_activated'] = 'Η θυγατρική εταιρεία ενεργοποιήθηκε με επιτυχία.';
+$lang['AdminAffiliates.!success.affiliate_deactivated'] = 'Η θυγατρική εταιρεία απενεργοποιήθηκε επιτυχώς.';
+$lang['AdminAffiliates.!success.affilates_enabled'] = 'Το σύστημα θυγατρικών ενεργοποιήθηκε επιτυχώς.';
+$lang['AdminAffiliates.leftnav.nav_affiliates'] = 'Συνεργάτες';
+$lang['AdminAffiliates.leftnav.nav_payouts'] = 'Πληρωμές';
+$lang['AdminAffiliates.leftnav.nav_settings'] = 'Ρυθμίσεις';
+$lang['AdminAffiliates.leftnav.nav_settings_general'] = 'Γενικές ρυθμίσεις';
+$lang['AdminAffiliates.leftnav.nav_settings_payment_methods'] = 'Μέθοδοι πληρωμής';
+$lang['AdminAffiliates.leftnav.nav_settings_excluded_packages'] = 'Εξαιρούμενα πακέτα';
+$lang['AdminAffiliates.index.page_title'] = 'Επισκόπηση θυγατρικών';
+$lang['AdminAffiliates.index.boxtitle_affiliates'] = 'Συνεργάτες';
+$lang['AdminAffiliates.index.category_active'] = 'Ενεργό';
+$lang['AdminAffiliates.index.category_inactive'] = 'Ανενεργό';
+$lang['AdminAffiliates.index.order_affiliate_add'] = 'Προσθήκη θυγατρικών παραγγελίας';
+$lang['AdminAffiliates.index.no_results'] = 'Δεν υπάρχουν θυγατρικές εταιρείες προς το παρόν.';
+$lang['AdminAffiliates.index.disable_affiliates'] = 'Για να απενεργοποιήσετε τις σελίδες θυγατρικών και να αφαιρέσετε τους συνδέσμους πλοήγησης που σχετίζονται με το σύστημα θυγατρικών, χρησιμοποιήστε την καρτέλα δράσης της σελίδας ρυθμίσεων του πρόσθετου Order.';
+$lang['AdminAffiliates.index.heading_name'] = 'Πελάτης';
+$lang['AdminAffiliates.index.heading_code'] = 'Κωδικός';
+$lang['AdminAffiliates.index.heading_visits'] = 'Επισκέψεις';
+$lang['AdminAffiliates.index.heading_sales'] = 'Πωλήσεις';
+$lang['AdminAffiliates.index.heading_date_added'] = 'Ημερομηνία προσθήκης';
+$lang['AdminAffiliates.index.heading_date_updated'] = 'Ημερομηνία ενημέρωσης';
+$lang['AdminAffiliates.index.heading_options'] = 'Επιλογές';
+$lang['AdminAffiliates.index.option_manage'] = 'Διαχείριση';
+$lang['AdminAffiliates.index.option_deactivate'] = 'Απενεργοποίηση';
+$lang['AdminAffiliates.index.option_activate'] = 'Ενεργοποίηση';
+$lang['AdminAffiliates.index.confirm_deactivate'] = 'Είστε σίγουρος ότι θέλετε να απενεργοποιήσετε αυτή τη θυγατρική;';
+$lang['AdminAffiliates.index.confirm_activate'] = 'Είστε σίγουροι ότι θέλετε να ενεργοποιήσετε αυτή τη θυγατρική;';
+$lang['AdminAffiliates.index.text_enable'] = 'Το σύστημα θυγατρικών είναι προς το παρόν απενεργοποιημένο. Κάντε κλικ στο κουμπί ενεργοποίησης για να ενεργοποιήσετε το σύστημα συνεργατών και να επιτρέψετε στους συνεργάτες να αρχίσουν να εγγράφονται και να κάνουν παραπομπές.';
+$lang['AdminAffiliates.index.enable'] = 'Ενεργοποιήστε το σύστημα θυγατρικών';
+$lang['AdminAffiliates.add.boxtitle_order'] = 'Σύστημα παραγγελιών - Προσθήκη θυγατρικών';
+$lang['AdminAffiliates.add.field_client'] = 'Πελάτης';
+$lang['AdminAffiliates.add.client_placeholder'] = 'Αναγνωριστικό ή όνομα πελάτη';
+$lang['AdminAffiliates.add.field_status'] = 'Κατάσταση';
+$lang['AdminAffiliates.add.field_addsubmit'] = 'Προσθήκη θυγατρικών';
+$lang['AdminAffiliates.add.text_no_clients'] = 'Δεν βρέθηκαν πελάτες. Προσπαθήστε να αναζητήσετε ξανά.';
+
