564eed6c12827095d44ed2df1a114c4027c4c100
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:01 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/ccavenue/language/cs_cz/ccavenue.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Ccavenue
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Ccavenue.!error.authentication.log'] = 'Banka transakci odmítla.';
+$lang['Ccavenue.!error.authentication.response'] = 'Děkujeme, že u nás nakupujete. Transakce však byla zamítnuta. Prosíme, proveďte transakci znovu.';
+$lang['Ccavenue.!error.security.response'] = 'Chyba zabezpečení. Zjištěn neoprávněný přístup.';
+$lang['Ccavenue.!error.delay.response'] = 'Děkujeme, že u nás nakupujete. O stavu vaší objednávky vás budeme informovat prostřednictvím e-mailu.';
+$lang['Ccavenue.!error.pending.log'] = 'Stav platby přijatý z brány čeká na vyřízení.';
+$lang['Ccavenue.!error.encryption_key.valid'] = 'Zadejte šifrovací klíč.';
+$lang['Ccavenue.!error.access_code.empty'] = 'Zadejte prosím přístupový kód.';
+$lang['Ccavenue.!error.merchant_id.valid'] = 'Zadejte prosím platné ID obchodníka.';
+$lang['Ccavenue.buildprocess.submit'] = 'Platba přes CCAvenue';
+$lang['Ccavenue.test_mode_note'] = 'Pokud je zaškrtnuto, brána bude odesílat požadavky na platbu na testovací adresu URL.';
+$lang['Ccavenue.test_mode'] = 'Testovací režim';
+$lang['Ccavenue.access_code_note'] = 'Ten najdete ve svém účtu CCAvenue v části Klíče API.';
+$lang['Ccavenue.access_code'] = 'Přístupový kód';
+$lang['Ccavenue.encryption_key_note'] = 'Ten je vygenerován ve vašem účtu CCAvenue v části Klíče API.';
+$lang['Ccavenue.encryption_key'] = 'Šifrovací klíč';
+$lang['Ccavenue.merchant_id_note'] = 'Vaše ID obchodníka je k dispozici ve vašem účtu CCAvenue v části Klíče API.';
+$lang['Ccavenue.merchant_id'] = 'ID obchodníka';
+$lang['Ccavenue.description'] = 'S více než 240 platebními možnostmi, které v reálném čase propojují více než 55 indických bank, je CCAvenue jedním z největších indických inkasních systémů.';
+$lang['Ccavenue.name'] = 'Platba CCAvenue';
+
