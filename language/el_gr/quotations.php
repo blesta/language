6a55611b4f74c3fa390aaa0a323c40dec20d9d65
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:32 -0800

    Updated el_gr language files

---
+++ language/el_gr/quotations.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Quotations
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Quotations.getstatuses.lost'] = 'Χαμένο';
+$lang['Quotations.getstatuses.dead'] = 'Νεκρός';
+$lang['Quotations.getstatuses.invoiced'] = 'Τιμολογημένο';
+$lang['Quotations.getstatuses.expired'] = 'Λειτούργησε';
+$lang['Quotations.getstatuses.pending'] = 'Εκκρεμεί';
+$lang['Quotations.getstatuses.approved'] = 'Εγκεκριμένο';
+$lang['Quotations.getstatuses.draft'] = 'Σχέδιο';
+$lang['Quotations.!error.lines[][tax].format'] = 'Ο φόρος του στοιχείου γραμμής πρέπει να είναι "true" ή "false".';
+$lang['Quotations.!error.lines[][amount].format'] = 'Το μοναδιαίο κόστος πρέπει να είναι ένας αριθμός.';
+$lang['Quotations.!error.lines[][qty].minimum'] = 'Παρακαλούμε εισάγετε ποσότητα 1 ή μεγαλύτερη.';
+$lang['Quotations.!error.lines[][description].empty'] = 'Παρακαλείστε να εισαγάγετε μια περιγραφή του στοιχείου.';
+$lang['Quotations.!error.lines[][id].exists'] = 'Μη έγκυρο αναγνωριστικό στοιχείου γραμμής.';
+$lang['Quotations.!error.percentage_due.valid'] = 'Το οφειλόμενο ποσοστό πρέπει να είναι αριθμός μεγαλύτερος του μηδενός και μικρότερος ή ίσος του 100.';
+$lang['Quotations.!error.percentage_due.format'] = 'Το οφειλόμενο ποσοστό πρέπει να είναι αριθμός.';
+$lang['Quotations.!error.second_due_date.format'] = 'Η δεύτερη ημερομηνία λήξης έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['Quotations.!error.first_due_date.format'] = 'Η πρώτη ημερομηνία λήξης έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['Quotations.!error.status.valid'] = 'Η προσφορά πρέπει είτε να εγκριθεί είτε να εκκρεμεί για να τιμολογηθεί.';
+$lang['Quotations.!error.currency.length'] = 'Ο κωδικός νομίσματος πρέπει να έχει μήκος 3 χαρακτήρες.';
+$lang['Quotations.!error.status.format'] = 'Μη έγκυρη κατάσταση.';
+$lang['Quotations.!error.date_expires.after_created'] = 'Η λήξη πρέπει να είναι κατά ή μετά τη δημιουργία που χρεώνεται.';
+$lang['Quotations.!error.date_expires.format'] = 'Η ημερομηνία λήξης έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['Quotations.!error.date_created.format'] = 'Η ημερομηνία δημιουργίας έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['Quotations.!error.title.length'] = 'Ο τίτλος για τα εισαγωγικά δεν μπορεί να υπερβαίνει τους 255 χαρακτήρες.';
+$lang['Quotations.!error.title.empty'] = 'Παρακαλώ εισάγετε έναν τίτλο.';
+$lang['Quotations.!error.staff_id.exists'] = 'Μη έγκυρο αναγνωριστικό προσωπικού.';
+$lang['Quotations.!error.client_id.exists'] = 'Μη έγκυρο αναγνωριστικό πελάτη.';
+$lang['Quotations.!error.id_value.valid'] = 'Αδυναμία προσδιορισμού της τιμής του αναγνωριστικού αναφοράς.';
+$lang['Quotations.!error.id_format.length'] = 'Η μορφή ID για τα εισαγωγικά δεν μπορεί να υπερβαίνει τους 64 χαρακτήρες.';
+$lang['Quotations.!error.id_format.empty'] = 'Δεν έχει οριστεί μορφή ID για εισαγωγικά.';
+$lang['Quotations.!error.id.quotation_invoiced'] = 'Αυτή η προσφορά έχει τιμολογηθεί και δεν μπορεί να επεξεργαστεί.';
+$lang['Quotations.!error.quotation_add.failed'] = 'Αυτή η αναφορά δεν μπόρεσε να δημιουργηθεί. Προσπαθήστε ξανά.';
+
