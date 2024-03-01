45d636cf8ce3a4e100e1abc6cb39f7714bbd1140
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:30 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/admin_settings.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Admin Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSettings.!success.settings_updated'] = 'Les paramètres d\'affiliation ont été mis à jour.';
+$lang['AdminSettings.settings.boxtitle_settings'] = 'Paramètres généraux';
+$lang['AdminSettings.settings.field_cookie_tld'] = 'Durée du cookie (en jours)';
+$lang['AdminSettings.settings.tooltip_cookie_tld'] = 'Le nombre de jours pendant lesquels un cookie d\'affiliation reste valide après qu\'un lien de référence a été suivi.';
+$lang['AdminSettings.settings.field_commission_type'] = 'Type de Commission';
+$lang['AdminSettings.settings.tooltip_commission_type'] = 'La méthode de calcul utilisée pour déterminer la commission de recommandation.  Soit une valeur fixe par commande, soit un pourcentage du total de la commande.';
+$lang['AdminSettings.settings.field_commission_amount'] = 'Montant de la Commission';
+$lang['AdminSettings.settings.field_order_frequency'] = 'Ordre Fréquence';
+$lang['AdminSettings.settings.tooltip_order_frequency'] = 'Premier pour rémunérer les référents uniquement pour la première commande qu\'un client effectue à l\'aide d\'un lien de référence, ou Tout pour rémunérer les référents pour chaque commande qu\'un client effectue à l\'aide d\'un lien de référence.';
+$lang['AdminSettings.settings.field_order_recurring'] = 'Commande récurrente';
+$lang['AdminSettings.settings.tooltip_order_recurring'] = 'Si les références seront payées pour les services commandés lors du renouvellement.';
+$lang['AdminSettings.settings.field_maturity_days'] = 'Jours d\'échéance';
+$lang['AdminSettings.settings.tooltip_maturity_days'] = 'Le nombre de jours après le paiement d\'une recommandation qui arrive à maturité et devient disponible pour le paiement.';
+$lang['AdminSettings.settings.field_min_withdrawal_amount'] = 'Montant minimum de retrait';
+$lang['AdminSettings.settings.field_max_withdrawal_amount'] = 'Montant maximum de retrait';
+$lang['AdminSettings.settings.field_withdrawal_currency'] = 'Devise de retrait';
+$lang['AdminSettings.settings.field_signup_content'] = 'Contenu de l\'inscription';
+$lang['AdminSettings.settings.tooltip_signup_content'] = 'Le contenu HTML qui sera affiché aux clients sur la page d\'affiliation s\'ils ne sont pas déjà enregistrés.';
+$lang['AdminSettings.settings.zero_amount'] = 'Le montant du retrait est actuellement de zéro, ce qui signifie que les affiliés ne recevront pas de fonds provenant des parrainages.';
+$lang['AdminSettings.settings.text_inheritance'] = 'Les paramètres d\'affiliation de l\'entreprise sont hérités par les affiliés nouvellement créés, mais sont ensuite remplacés par les paramètres individuels de l\'affilié.';
+$lang['AdminSettings.settings.submit_settings'] = 'Sauvegarder les paramètres';
+$lang['AdminSettings.settings.submit_cancel'] = 'Annuler';
+
