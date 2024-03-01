7b34ab333fee38b66286cdf77277a875ee043c7e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:08 -0800

    Updated es_es language files

---
+++ language/es_es/calendar_events.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Calendar Events
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CalendarEvents.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['CalendarEvents.!error.staff_id.exists'] = 'ID de personal no válida.';
+$lang['CalendarEvents.!error.staff_id.valid'] = 'Sólo el creador del evento puede actualizar este evento.';
+$lang['CalendarEvents.!error.shared.format'] = 'El hecho de compartir o no el evento debe ser un número.';
+$lang['CalendarEvents.!error.shared.length'] = 'La longitud compartida no puede superar 1 dígito.';
+$lang['CalendarEvents.!error.title.empty'] = 'Por favor, introduzca un título.';
+$lang['CalendarEvents.!error.start_date.format'] = 'La fecha de inicio tiene un formato no válido.';
+$lang['CalendarEvents.!error.start_date.order'] = 'La fecha de inicio debe ser anterior a la fecha de finalización.';
+$lang['CalendarEvents.!error.end_date.format'] = 'La fecha final tiene un formato no válido.';
+$lang['CalendarEvents.!error.all_day.format'] = 'El hecho de que el acto dure o no todo el día debe ser una cifra.';
+$lang['CalendarEvents.!error.all_day.length'] = 'La longitud de all_day no puede superar 1 dígito.';
+
