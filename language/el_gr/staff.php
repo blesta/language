fb6035f393d810325f8546b473e4d0b7dbc7fde2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:34 -0800

    Updated el_gr language files

---
+++ language/el_gr/staff.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Staff.!error.action[].exists'] = 'Τουλάχιστον μία από τις ενέργειες της ομάδας email δεν υπάρχει.';
+$lang['Staff.!error.action.exists'] = 'Η ενέργεια της ομάδας email %1$s δεν υπάρχει.';
+$lang['Staff.!error.staff_id.exists'] = 'Μη έγκυρο αναγνωριστικό προσωπικού.';
+$lang['Staff.!error.staff_group_id.exists'] = 'Μη έγκυρο αναγνωριστικό ομάδας προσωπικού.';
+$lang['Staff.!error.order.length'] = 'Το μήκος της σειράς ταξινόμησης δεν μπορεί να υπερβαίνει τους 5 χαρακτήρες.';
+$lang['Staff.!error.order.format'] = 'Η σειρά ταξινόμησης πρέπει να είναι ένας αριθμός.';
+$lang['Staff.!error.title.empty'] = 'Παρακαλώ εισάγετε έναν τίτλο.';
+$lang['Staff.!error.uri.empty'] = 'Παρακαλώ εισάγετε ένα URI.';
+$lang['Staff.!error.company_id.format'] = 'Το αναγνωριστικό της εταιρείας πρέπει να είναι αριθμός.';
+$lang['Staff.!error.staff_id.format'] = 'Το αναγνωριστικό προσωπικού πρέπει να είναι αριθμός.';
+$lang['Staff.!error.groups.unique_company'] = 'Μόνο μία ομάδα προσωπικού ανά εταιρεία μπορεί να ανατεθεί σε ένα μέλος του προσωπικού.';
+$lang['Staff.!error.groups[].format'] = 'Υπάρχουν ένα ή περισσότερα άκυρα αναγνωριστικά ομάδας.';
+$lang['Staff.!error.status.format'] = 'Μη έγκυρη κατάσταση.';
+$lang['Staff.!error.email.format'] = 'Παρακαλούμε εισάγετε μια έγκυρη διεύθυνση ηλεκτρονικού ταχυδρομείου.';
+$lang['Staff.!error.last_name.empty'] = 'Παρακαλώ εισάγετε ένα επίθετο.';
+$lang['Staff.!error.first_name.empty'] = 'Παρακαλώ εισάγετε ένα όνομα.';
+$lang['Staff.!error.user_id.unique'] = 'Το αναγνωριστικό χρήστη προσωπικού %1$s είναι ήδη κατειλημμένο.';
+$lang['Staff.!error.user_id.exists'] = 'Μη έγκυρο αναγνωριστικό χρήστη.';
+
