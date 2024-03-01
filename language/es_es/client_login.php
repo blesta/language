971834e4dfd7f62a487395df040c1bf54d563cda
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:11 -0800

    Updated es_es language files

---
+++ language/es_es/client_login.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Client Login
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientLogin.!error.unknown_user'] = 'No se reconoce ese nombre de usuario o no se puede restablecer la contraseña.';
+$lang['ClientLogin.!error.unknown_email'] = 'No se reconoce esa dirección de correo electrónico o no se puede recuperar el nombre de usuario.';
+$lang['ClientLogin.!error.captcha.invalid'] = 'El captcha introducido no es válido. Por favor, inténtelo de nuevo.';
+$lang['ClientLogin.!success.reset_sent'] = 'Se ha enviado un correo electrónico de confirmación a la dirección registrada.';
+$lang['ClientLogin.!success.forgot_sent'] = 'Se ha enviado a su dirección de correo electrónico un mensaje con su nombre de usuario registrado.';
+$lang['ClientLogin.index.page_title'] = 'Iniciar sesión';
+$lang['ClientLogin.index.description'] = 'Inicie sesión para acceder al área de clientes.';
+$lang['ClientLogin.index.login_heading'] = 'Iniciar sesión';
+$lang['ClientLogin.index.field_username'] = 'Nombre de usuario';
+$lang['ClientLogin.index.field_password'] = 'Contraseña';
+$lang['ClientLogin.index.field_rememberme'] = 'Recuérdame en este ordenador';
+$lang['ClientLogin.index.field_loginsubmit'] = 'Iniciar sesión';
+$lang['ClientLogin.index.link_resetpassword'] = 'Restablecer mi contraseña';
+$lang['ClientLogin.index.link_forgotusername'] = 'He olvidado mi nombre de usuario';
+$lang['ClientLogin.otp.page_title'] = 'Iniciar sesión';
+$lang['ClientLogin.otp.description'] = 'Introduzca su contraseña de un solo uso.';
+$lang['ClientLogin.otp.login_heading'] = 'Iniciar sesión';
+$lang['ClientLogin.otp.field_otp'] = 'Contraseña única';
+$lang['ClientLogin.otp.field_submit'] = 'Iniciar sesión';
+$lang['ClientLogin.reset.page_title'] = 'Restablecer contraseña';
+$lang['ClientLogin.reset.reset_heading'] = 'Restablecer contraseña';
+$lang['ClientLogin.reset.description'] = 'Introduzca su nombre de usuario para solicitar un enlace de restablecimiento de contraseña.';
+$lang['ClientLogin.reset.field_username'] = 'Nombre de usuario';
+$lang['ClientLogin.reset.field_resetsubmit'] = 'Restablecer contraseña';
+$lang['ClientLogin.reset.link_login'] = 'Cancelar, Entrar';
+$lang['ClientLogin.confirmreset.page_title'] = 'Confirmar restablecimiento de contraseña';
+$lang['ClientLogin.confirmreset.reset_heading'] = 'Confirmar restablecimiento de contraseña';
+$lang['ClientLogin.confirmreset.description'] = 'Por favor, introduzca su nueva contraseña.';
+$lang['ClientLogin.confirmreset.field_new_password'] = 'Nueva contraseña';
+$lang['ClientLogin.confirmreset.field_confirm_password'] = 'Confirmar nueva contraseña';
+$lang['ClientLogin.confirmreset.field_resetsubmit'] = 'Establecer contraseña';
+$lang['ClientLogin.confirmreset.link_login'] = 'Cancelar, Entrar';
+$lang['ClientLogin.forgot.page_title'] = 'Nombre de usuario olvidado';
+$lang['ClientLogin.forgot.reset_heading'] = 'Nombre de usuario olvidado';
+$lang['ClientLogin.forgot.description'] = 'Introduzca su dirección de correo electrónico para solicitar su nombre de usuario.';
+$lang['ClientLogin.forgot.field_email'] = 'Dirección de correo electrónico';
+$lang['ClientLogin.forgot.field_forgotsubmit'] = 'Recuperar nombre de usuario';
+$lang['ClientLogin.forgot.link_login'] = 'Cancelar, Entrar';
+
