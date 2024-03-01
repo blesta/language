f86ab9c75f98b5965c8aa715cb5bd7d60ea9a6cf
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:33 -0800

    Updated es_es language files

---
+++ core/Util/DataFeed/Feeds/language/es_es/client_feed.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Client Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientFeed.name'] = 'Clientes';
+$lang['ClientFeed.description'] = 'Devuelve el número de clientes de un estado determinado.';
+$lang['ClientFeed.getOptionFields.title_row_example_code'] = 'Código de ejemplo';
+$lang['ClientFeed.getOptionFields.example_code_active'] = 'Muestra el número de clientes activos:';
+$lang['ClientFeed.getOptionFields.example_code_inactive'] = 'Muestra el número de clientes inactivos:';
+$lang['ClientFeed.getOptionFields.example_code_fraud'] = 'Muestra el número de clientes fraudulentos:';
+$lang['ClientFeed.!error.invalid_endpoint'] = 'El endpoint solicitado no es válido o no existe.';
+
