87f5d902d750486d1cd443bc2dffa8e0a680a124
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:05 -0800

    Updated cs_cz language files

---
+++ components/gateways/nonmerchant/kassacompleet/language/cs_cz/kassacompleet.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Kassacompleet
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Kassacompleet.buildprocess.submit'] = 'Plaťte pomocí služby Kassa Compleet';
+$lang['Kassacompleet.tooltip.api_key'] = 'Chcete-li použít testovací režim, jednoduše zadejte klíč API z testovacího účtu.';
+$lang['Kassacompleet.api_key'] = 'Klíč API';
+$lang['Kassacompleet.description'] = 'Integrace Kassa Compleet pro typ platby kreditní kartou.';
+$lang['Kassacompleet.name'] = 'Kreditní/debetní karta (Kassa Compleet)';
+$lang['Kassacompleet.!error.api_key.valid'] = 'Klíč API je neplatný';
+$lang['Kassacompleet.!error.api_key.empty'] = 'Zadejte prosím klíč API';
+
