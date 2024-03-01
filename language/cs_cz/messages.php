355620a6924e3b3342e94fec62de6e3f1a48ee85
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:05 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/messages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Messages
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Messages.getStatusTypes.inactive'] = 'Neaktivní';
+$lang['Messages.getStatusTypes.active'] = 'Aktivní';
+$lang['Messages.getTypes.sms'] = 'SMS';
+$lang['Messages.!error.content.format'] = 'Obsahem zprávy musí být pole.';
+$lang['Messages.!error.status.valid'] = 'Stav zprávy musí být nastaven na "aktivní" nebo "neaktivní".';
+$lang['Messages.!error.type.valid'] = 'Typ zprávy musí být nastaven na "sms".';
+$lang['Messages.!error.company_id.valid'] = 'Neplatné ID společnosti.';
+$lang['Messages.!error.message_group_id.valid'] = 'Neplatné ID skupiny zpráv.';
+
