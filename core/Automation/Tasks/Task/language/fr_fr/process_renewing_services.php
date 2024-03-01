e3502f82b7128719b29a533c8fd5abddaa3a2884
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:24 -0800

    Updated fr_fr language files

---
+++ core/Automation/Tasks/Task/language/fr_fr/process_renewing_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Process Renewing Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.process_renewing_services.attempt'] = 'Tentative de traitement des services de renouvellement.';
+$lang['Automation.task.process_renewing_services.completed'] = 'Le processus de renouvellement des services est terminé.';
+$lang['Automation.task.process_renewing_services.renew_success'] = 'Renouvellement du service #%1$s pour le client %2$s.';
+$lang['Automation.task.process_renewing_services.renew_error'] = 'Impossible de renouveler le service #%1$s pour le client %2$s.';
+
