770fb5d11656aea76e470ee6feb7e0bff94f161d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:50 -0800

    Updated it_it language files

---
+++ plugins/support_manager/language/it_it/support_manager_staff.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Support Manager Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerStaff.days.sat'] = 'Sabato';
+$lang['SupportManagerStaff.days.fri'] = 'Venerdì';
+$lang['SupportManagerStaff.days.thu'] = 'Giovedì';
+$lang['SupportManagerStaff.days.wed'] = 'Mercoledì';
+$lang['SupportManagerStaff.days.tue'] = 'Martedì';
+$lang['SupportManagerStaff.days.mon'] = 'Lunedì';
+$lang['SupportManagerStaff.days.sun'] = 'Domenica';
+$lang['SupportManagerStaff.!error.schedules[][end_time].format'] = 'Selezionare un orario di fine per ogni giorno programmato.';
+$lang['SupportManagerStaff.!error.schedules[][start_time].format'] = 'Selezionare un orario di inizio per ogni giorno programmato.';
+$lang['SupportManagerStaff.!error.schedules[][day].format'] = 'Giorno programmato non valido.';
+$lang['SupportManagerStaff.!error.schedules.unique_days'] = 'Ogni giorno programmato deve essere unico.';
+$lang['SupportManagerStaff.!error.schedules.minimum'] = 'Impostare un orario di inizio e di fine programmato per almeno un giorno.';
+$lang['SupportManagerStaff.!error.departments.exists'] = 'Almeno uno dei reparti impostati non esiste.';
+$lang['SupportManagerStaff.!error.company_id.exists'] = 'L\'ID dell\'azienda non è valido.';
+$lang['SupportManagerStaff.!error.staff_id.duplicate'] = 'Il membro del personale selezionato esiste già nel sistema di supporto.';
+$lang['SupportManagerStaff.!error.staff_id.belongs'] = 'L\'agente non appartiene all\'azienda a cui è stato assegnato.';
+$lang['SupportManagerStaff.!error.staff_id.exists'] = 'Il membro del personale da aggiungere non esiste.';
+
