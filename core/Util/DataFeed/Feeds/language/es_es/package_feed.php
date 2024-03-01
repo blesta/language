f29ebc22617ab61c095aaa9d6bc3e753becb633f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:33 -0800

    Updated es_es language files

---
+++ core/Util/DataFeed/Feeds/language/es_es/package_feed.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Package Feed
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PackageFeed.name'] = 'Paquetes';
+$lang['PackageFeed.description'] = 'Devuelve el nombre, la descripción o el precio de un paquete.';
+$lang['PackageFeed.getOptionFields.title_row_example_code'] = 'Código de ejemplo';
+$lang['PackageFeed.getOptionFields.example_code_name'] = 'Mostrar el nombre de un paquete:';
+$lang['PackageFeed.getOptionFields.example_code_description'] = 'Mostrar la descripción del paquete:';
+$lang['PackageFeed.getOptionFields.example_code_pricing'] = 'Muestra el precio, la renovación, la cuota de alta, la cuota de cancelación o la divisa del precio de un paquete:';
+$lang['PackageFeed.!error.invalid_endpoint'] = 'El endpoint solicitado no es válido o no existe.';
+
