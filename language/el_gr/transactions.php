c8ac54cd62de960b97bb674197e7a8d6eafc6e69
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:38 -0800

    Updated el_gr language files

---
+++ language/el_gr/transactions.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Transactions
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Transactions.debit_types.credit'] = 'Πίστωση';
+$lang['Transactions.debit_types.debit'] = 'Χρέωση';
+$lang['Transactions.status.returned'] = 'Επιστροφή';
+$lang['Transactions.status.refunded'] = 'Refunded';
+$lang['Transactions.status.pending'] = 'Εκκρεμεί';
+$lang['Transactions.status.error'] = 'Σφάλμα';
+$lang['Transactions.status.void'] = 'Void';
+$lang['Transactions.status.declined'] = 'Απορρίφθηκε';
+$lang['Transactions.status.approved'] = 'Εγκεκριμένο';
+$lang['Transactions.types.other'] = 'Άλλα';
+$lang['Transactions.types.ach'] = 'ACH';
+$lang['Transactions.types.cc'] = 'Πιστωτική κάρτα';
+$lang['Transactions.!error.type_id.exists'] = 'Μη έγκυρο αναγνωριστικό τύπου συναλλαγής.';
+$lang['Transactions.!error.is_lang.length'] = 'Το μήκος του is_lang δεν μπορεί να υπερβαίνει τον 1 χαρακτήρα.';
+$lang['Transactions.!error.is_lang.format'] = 'is_lang πρέπει να είναι αριθμός.';
+$lang['Transactions.!error.type.valid'] = 'Παρακαλούμε επιλέξτε έναν έγκυρο τύπο.';
+$lang['Transactions.!error.name.length'] = 'Το μήκος του ονόματος δεν μπορεί να υπερβαίνει τους 32 χαρακτήρες.';
+$lang['Transactions.!error.name.empty'] = 'Παρακαλώ εισάγετε ένα όνομα.';
+$lang['Transactions.!error.currency.mismatch'] = 'Το συγκεκριμένο νόμισμα δεν αντιστοιχεί σε τουλάχιστον ένα από τα σχετικά τιμολόγια.';
+$lang['Transactions.!error.currency.missing'] = 'Παρακαλώ ορίστε ένα νόμισμα.';
+$lang['Transactions.!error.date.format'] = 'Η ημερομηνία που εφαρμόστηκε είναι άκυρη.';
+$lang['Transactions.!error.amounts.positive'] = 'Μία ή περισσότερες τιμές Ποσό προς πληρωμή είναι αρνητικές. Βεβαιωθείτε ότι κάθε τιμή Ποσό προς πληρωμή είναι μηδέν ή μεγαλύτερη.';
+$lang['Transactions.!error.amounts.overage'] = 'Μία ή περισσότερες τιμές Ποσό προς πληρωμή δεν μπόρεσαν να εφαρμοστούν στο συγκεκριμένο τιμολόγιο. Βεβαιωθείτε ότι το Ποσό προς πληρωμή δεν υπερβαίνει το οφειλόμενο ποσό στο τιμολόγιο, ότι το τιμολόγιο είναι ανοιχτό και ότι το άθροισμα των τιμών του Ποσού προς πληρωμή δεν υπερβαίνει το Ποσό πληρωμής ή πίστωσης.';
+$lang['Transactions.!error.invoice_id.exists'] = 'Μη έγκυρο αναγνωριστικό τιμολογίου.';
+$lang['Transactions.!error.transaction_id.currency_matches'] = 'Τα ποσά του τιμολογίου δεν ήταν δυνατόν να εφαρμοστούν επειδή δεν αντιστοιχούν στο νόμισμα της συναλλαγής.';
+$lang['Transactions.!error.message.length'] = 'Το μήκος του μηνύματος δεν μπορεί να υπερβαίνει τους 255 χαρακτήρες.';
+$lang['Transactions.!error.date_added.format'] = 'Η ημερομηνία συναλλαγής δεν μπορεί να είναι μελλοντική ημερομηνία.';
+$lang['Transactions.!error.transaction_id.exists'] = 'Μη έγκυρο αναγνωριστικό συναλλαγής.';
+$lang['Transactions.!error.status.format'] = 'Μη έγκυρη κατάσταση συναλλαγής.';
+$lang['Transactions.!error.reference_id.length'] = 'Το μήκος του αναγνωριστικού αναφοράς δεν μπορεί να υπερβαίνει τους 128 χαρακτήρες.';
+$lang['Transactions.!error.parent_transaction_id.length'] = 'Το μήκος του Parent Transaction ID δεν μπορεί να υπερβαίνει τους 128 χαρακτήρες.';
+$lang['Transactions.!error.transaction_id.length'] = 'Το μήκος του αναγνωριστικού συναλλαγής δεν μπορεί να υπερβαίνει τους 128 χαρακτήρες.';
+$lang['Transactions.!error.gateway_id.exists'] = 'Μη έγκυρο αναγνωριστικό πύλης.';
+$lang['Transactions.!error.transaction_type_id.exists'] = 'Μη έγκυρο αναγνωριστικό τύπου συναλλαγής.';
+$lang['Transactions.!error.type.format'] = 'Μη έγκυρος τύπος συναλλαγής.';
+$lang['Transactions.!error.currency.length'] = 'Το νόμισμα πρέπει να έχει μήκος 3 χαρακτήρες.';
+$lang['Transactions.!error.amount.format'] = 'Το ποσό πρέπει να είναι αριθμός.';
+$lang['Transactions.!error.client_id.exists'] = 'Μη έγκυρο αναγνωριστικό πελάτη.';
+
