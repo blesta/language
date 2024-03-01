9d15b2aebaaa43a11f1f7d8d0deb1696ff1e63c5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:47 -0800

    Updated it_it language files

---
+++ plugins/support_manager/language/it_it/knowledgebase.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Knowledgebase
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Knowledgebase.!text.ellipsis'] = '...';
+$lang['Knowledgebase.!text.root_directory'] = 'Casa';
+$lang['Knowledgebase.view.text_vote_thanks'] = 'Grazie per aver votato questo articolo!';
+$lang['Knowledgebase.view.text_voted'] = 'Avete già votato su questo articolo.';
+$lang['Knowledgebase.view.edit'] = 'Modifica';
+$lang['Knowledgebase.view.text_rate'] = 'Valutate questo articolo per aiutarci a migliorare la nostra Knowledge Base.';
+$lang['Knowledgebase.search.articles_no_results'] = 'Non ci sono articoli che corrispondono ai criteri di ricerca.';
+$lang['Knowledgebase.search.heading_articles'] = 'Articoli corrispondenti a \'%1$s\'';
+$lang['Knowledgebase.index.articles_description'] = 'Articoli per %1$s.';
+$lang['Knowledgebase.index.heading_articles'] = 'Articoli';
+$lang['Knowledgebase.index.popular_description'] = 'Le persone hanno trovato questi articoli i più utili.';
+$lang['Knowledgebase.index.heading_popular'] = 'Articoli popolari';
+$lang['Knowledgebase.index.categories_no_results'] = 'Non ci sono categorie disponibili.';
+$lang['Knowledgebase.index.categories_description'] = 'Sfoglia la nostra Knowledge Base per categoria.';
+$lang['Knowledgebase.index.category_items'] = '(%1$s)';
+$lang['Knowledgebase.index.heading_subcategories'] = 'Sottocategorie';
+$lang['Knowledgebase.index.heading_categories'] = 'Categorie';
+$lang['Knowledgebase.search.form_submit'] = 'Ricerca';
+$lang['Knowledgebase.search.placeholder'] = 'Avete una domanda? Cercate nella nostra Knowledge Base qui.';
+$lang['Knowledgebase.view.page_title'] = '%1$s';
+$lang['Knowledgebase.index.page_title'] = 'Base di conoscenza';
+
