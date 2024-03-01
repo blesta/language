5c557a093face81792b73ce29d8f1fa16634eb2a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:17 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/states.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * States
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['States.!error.name.format'] = 'Zadejte název státu.';
+$lang['States.!error.country_alpha2.in_use'] = 'Kód země alfa2 %1$s a kód subdivize %2$s jsou již obsazeny.';
+$lang['States.!error.country_alpha2.format'] = 'Země Alpha2 musí obsahovat 2 alfanumerické znaky.';
+$lang['States.!error.code.format'] = 'Kód státu musí být alfanumerický podle definice ISO 3166-2.';
+
