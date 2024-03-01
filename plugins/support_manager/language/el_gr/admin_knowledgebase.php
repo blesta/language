4478b17d823e9ca4ddfb954f0d5d85a1dde7704c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:53 -0800

    Updated el_gr language files

---
+++ plugins/support_manager/language/el_gr/admin_knowledgebase.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Admin Knowledgebase
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminKnowledgebase.!success.category_added'] = 'Η κατηγορία έχει δημιουργηθεί με επιτυχία.';
+$lang['AdminKnowledgebase.!success.category_updated'] = 'Η κατηγορία ενημερώθηκε επιτυχώς.';
+$lang['AdminKnowledgebase.!success.category_deleted'] = 'Η κατηγορία διαγράφηκε επιτυχώς.';
+$lang['AdminKnowledgebase.!success.article_added'] = 'Το άρθρο δημιουργήθηκε με επιτυχία.';
+$lang['AdminKnowledgebase.!success.article_updated'] = 'Το άρθρο ενημερώθηκε επιτυχώς.';
+$lang['AdminKnowledgebase.!success.article_deleted'] = 'Το άρθρο διαγράφηκε επιτυχώς.';
+$lang['AdminKnowledgebase.index.page_title'] = 'Διαχειριστής υποστήριξης > Βάση γνώσεων';
+$lang['AdminKnowledgebase.add.page_title'] = 'Διαχείριση υποστήριξης > Βάση γνώσεων > Προσθήκη άρθρου';
+$lang['AdminKnowledgebase.edit.page_title'] = 'Διαχείριση υποστήριξης > Βάση γνώσεων > Επεξεργασία άρθρου';
+$lang['AdminKnowledgebase.addcategory.page_title'] = 'Διαχείριση υποστήριξης > Βάση γνώσεων > Προσθήκη κατηγορίας';
+$lang['AdminKnowledgebase.editcategory.page_title'] = 'Διαχείριση υποστήριξης > Βάση γνώσεων > Επεξεργασία κατηγορίας';
+$lang['AdminKnowledgebase.index.add_article'] = 'Προσθήκη άρθρου';
+$lang['AdminKnowledgebase.index.add_category'] = 'Προσθήκη κατηγορίας';
+$lang['AdminKnowledgebase.index.boxtitle_knowledgebase'] = 'Βάση γνώσεων';
+$lang['AdminKnowledgebase.index.go_back'] = 'Ανεβείτε ένα επίπεδο';
+$lang['AdminKnowledgebase.index.edit'] = 'Επεξεργασία';
+$lang['AdminKnowledgebase.index.delete'] = 'Διαγραφή';
+$lang['AdminKnowledgebase.modal.delete_article'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτό το άρθρο;';
+$lang['AdminKnowledgebase.modal.delete_category'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την κατηγορία; Όλες οι υποκατηγορίες και τα άρθρα αυτής της κατηγορίας θα μεταφερθούν στη μητρική κατηγορία.';
+$lang['AdminKnowledgebase.index.no_results'] = 'Δεν υπάρχουν κατηγορίες ή άρθρα σε αυτό το τμήμα.';
+$lang['AdminKnowledgebase.addcategory.boxtitle_addcategory'] = 'Προσθήκη κατηγορίας';
+$lang['AdminKnowledgebase.addcategory.field_name'] = 'Όνομα';
+$lang['AdminKnowledgebase.addcategory.field_parent_id'] = 'Γονική κατηγορία';
+$lang['AdminKnowledgebase.addcategory.field_description'] = 'Περιγραφή';
+$lang['AdminKnowledgebase.addcategory.field_access'] = 'Προσβασιμότητα';
+$lang['AdminKnowledgebase.addcategory.submit_add'] = 'Δημιουργία κατηγορίας';
+$lang['AdminKnowledgebase.addcategory.submit_cancel'] = 'Ακύρωση';
+$lang['AdminKnowledgebase.editcategory.boxtitle_editcategory'] = 'Κατηγορία ενημέρωσης';
+$lang['AdminKnowledgebase.editcategory.field_name'] = 'Όνομα';
+$lang['AdminKnowledgebase.editcategory.field_parent_id'] = 'Γονική κατηγορία';
+$lang['AdminKnowledgebase.editcategory.field_description'] = 'Περιγραφή';
+$lang['AdminKnowledgebase.editcategory.field_access'] = 'Προσβασιμότητα';
+$lang['AdminKnowledgebase.editcategory.submit_edit'] = 'Κατηγορία ενημέρωσης';
+$lang['AdminKnowledgebase.editcategory.submit_cancel'] = 'Ακύρωση';
+$lang['AdminKnowledgebase.add.boxtitle_add'] = 'Προσθήκη άρθρου';
+$lang['AdminKnowledgebase.add.field_access'] = 'Προσβασιμότητα';
+$lang['AdminKnowledgebase.add.field_categories'] = 'Κατηγορία';
+$lang['AdminKnowledgebase.add.submit_add'] = 'Δημιουργία άρθρου';
+$lang['AdminKnowledgebase.add.submit_cancel'] = 'Ακύρωση';
+$lang['AdminKnowledgebase.edit.boxtitle_edit'] = 'Ενημέρωση άρθρου';
+$lang['AdminKnowledgebase.edit.field_access'] = 'Προσβασιμότητα';
+$lang['AdminKnowledgebase.edit.field_categories'] = 'Κατηγορία';
+$lang['AdminKnowledgebase.edit.submit_edit'] = 'Ενημέρωση άρθρου';
+$lang['AdminKnowledgebase.edit.submit_cancel'] = 'Ακύρωση';
+$lang['AdminKnowledgebase.article_content.title'] = 'Τίτλος';
+$lang['AdminKnowledgebase.article_content.content_type'] = 'Τύπος περιεχομένου';
+$lang['AdminKnowledgebase.article_content.body'] = 'Σώμα';
+$lang['AdminKnowledgebase.select.none'] = '-- Καμία --';
+$lang['AdminKnowledgebase.!text.root_directory'] = 'Αρχικός κατάλογος';
+$lang['AdminKnowledgebase.!text.open_enclosure'] = '(';
+$lang['AdminKnowledgebase.!text.closed_enclosure'] = ')';
+$lang['AdminKnowledgebase.!text.enclosure_separator'] = '/';
+$lang['AdminKnowledgebase.!text.add_category'] = 'Προσθήκη κατηγορίας';
+$lang['AdminKnowledgebase.!text.directory_separator'] = '/';
+$lang['AdminKnowledgebase.!tooltip.access_article'] = 'Τα δημόσια άρθρα είναι προσβάσιμα σε όλους. Τα ιδιωτικά άρθρα είναι προσβάσιμα μόνο σε χρήστες που έχουν συνδεθεί. Τα κρυφά άρθρα δεν είναι προσβάσιμα. Τα άρθρα του προσωπικού είναι ορατά μόνο στα μέλη του προσωπικού.';
+$lang['AdminKnowledgebase.!tooltip.access_category'] = 'Οι δημόσιες κατηγορίες είναι προσβάσιμες σε όλους. Οι ιδιωτικές κατηγορίες είναι προσβάσιμες μόνο σε χρήστες που έχουν συνδεθεί. Οι κρυφές κατηγορίες δεν είναι προσβάσιμες. Οι κατηγορίες προσωπικού είναι ορατές μόνο στα μέλη του προσωπικού.';
+
