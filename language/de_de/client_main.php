dabedd0d4a416beb401637070296b58f90844235
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:47 -0800

    Updated de_de language files

---
+++ language/de_de/client_main.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.edit.field_recovery_email'] = 'Wiederherstellungs-E-Mail (optional)';
+$lang['ClientMain.getcurrencyamounts.text_total_credits'] = 'Credits:';
+$lang['Clientmain.invoicemethod.field_methodsubmit'] = 'Update';
+$lang['ClientMain.invoicemethod.text_invoice_method'] = 'Ihre Rechnungsmethode ist die Methode, mit der wir Ihnen neue Rechnungen zustellen.';
+$lang['ClientMain.invoicemethod.boxtitle_inv_method'] = 'Methode der Rechnungsstellung';
+$lang['ClientMain.invoicemethod.page_title'] = 'Kunde #%1$s Rechnungsmethode';
+$lang['ClientMain.edit.field_editsubmit'] = 'Meine Informationen aktualisieren';
+$lang['ClientMain.edit.field_otp'] = 'Einmaliges Passwort';
+$lang['ClientMain.edit.text_windows'] = 'Windows';
+$lang['ClientMain.edit.text_ios'] = 'iOS';
+$lang['ClientMain.edit.text_android'] = 'Android';
+$lang['ClientMain.edit.two_factor_step3'] = 'Bestätigen Sie Ihr Einmal-Passwort und klicken Sie dann auf "Meine Daten aktualisieren". Wenn Sie sich das nächste Mal anmelden, müssen Sie Ihr Einmal-Passwort eingeben.';
+$lang['ClientMain.edit.two_factor_step2'] = 'Öffnen Sie die App und scannen Sie den unten stehenden QR-Code, oder geben Sie alternativ einen der folgenden Schlüssel ein:<br/>Hex - %1$s<br/>Base32 - %2$s';
+$lang['ClientMain.edit.two_factor_step1'] = 'Laden Sie einen Authenticator für Ihr Gerät herunter:';
+$lang['ClientMain.edit.two_factor_instructions'] = 'Um die Zwei-Faktor-Authentifizierung nutzen zu können, benötigen Sie ein TOTP-Token. Wenn Sie ein Android-, iOS- oder Windows-Mobilgerät besitzen, kann Ihr Telefon als Token fungieren und das erforderliche One-Time Password bereitstellen.';
+$lang['ClientMain.edit.two_factor_desc'] = 'Die Zwei-Faktor-Authentifizierung ist eine Sicherheitsfunktion, bei der Sie zwei Identifikationsmittel angeben, um sich bei Ihrem Konto anzumelden. Wenn diese Funktion aktiviert ist, melden Sie sich mit Ihrem Benutzernamen und Ihrem Kennwort bei Ihrem Konto an und werden dann aufgefordert, ein einmaliges Kennwort einzugeben.';
+$lang['ClientMain.edit.field_two_factor_mode'] = 'Aktivieren der Zwei-Faktor-Authentifizierung';
+$lang['ClientMain.edit.field_current_password'] = 'Aktuelles Passwort';
+$lang['ClientMain.edit.field_confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['ClientMain.edit.field_new_password'] = 'Neues Passwort';
+$lang['ClientMain.edit.field_username'] = 'Benutzername';
+$lang['ClientMain.edit.heading_authentication'] = 'Authentifizierung';
+$lang['ClientMain.edit.field_receive_email_marketing'] = 'Ich möchte per E-Mail über neue Produkte, Dienstleistungen und Angebote informiert werden.';
+$lang['ClientMain.edit.field_language'] = 'Sprache';
+$lang['ClientMain.edit.heading_settings'] = 'Zusätzliche Einstellungen';
+$lang['ClientMain.edit.field_invoiceaddress'] = 'Adresse Rechnungen an';
+$lang['ClientMain.edit.field_default_currency'] = 'Bevorzugte Währung';
+$lang['ClientMain.edit.field_taxid'] = 'Steuer-ID/VATIN';
+$lang['ClientMain.edit.heading_billing'] = 'Informationen zur Rechnungsstellung';
+$lang['ClientMain.edit.boxtitle_edit'] = 'Meine Informationen bearbeiten';
+$lang['ClientMain.edit.page_title'] = 'Kunde #%1$s Meine Informationen bearbeiten';
+$lang['ClientMain.edit.heading_contact'] = 'Kontaktinformationen';
+$lang['ClientMain.myinfo.link_editclient'] = 'Ändern Sie';
+$lang['ClientMain.myinfo.setting_change'] = 'Dies ändern?';
+$lang['ClientMain.myinfo.field_managed_accounts_search'] = 'Suche (Unternehmen, E-Mail oder Name)';
+$lang['ClientMain.myinfo.managed_accounts'] = 'Verwaltete Konten';
+$lang['ClientMain.myinfo.link_addcontact'] = 'Kontakt hinzufügen';
+$lang['ClientMain.myinfo.contacts'] = 'Kontakte';
+$lang['ClientMain.myinfo.number'] = '(%1$s %2$s)';
+$lang['ClientMain.myinfo.setting_autodebit_ach_days'] = 'Wir belasten Ihr %1$s-Konto, das in x%2$s %3$s Tagen endet, bevor die Zahlung fällig ist.';
+$lang['ClientMain.myinfo.setting_autodebit_cc_days'] = 'Wir berechnen Ihre %1$s-Endung in x%2$s %3$s Tagen vor Fälligkeit der Zahlung.';
+$lang['ClientMain.myinfo.setting_autodebit_ach_one_day'] = 'Wir belasten Ihr %1$s-Konto mit der Endung x%2$s einen Tag vor Fälligkeit der Zahlung.';
+$lang['ClientMain.myinfo.setting_autodebit_cc_one_day'] = 'Wir werden Ihre %1$s mit der Endung x%2$s am Tag vor der Fälligkeit der Zahlung abrechnen.';
+$lang['ClientMain.myinfo.setting_autodebit_ach_zero_days'] = 'Wir belasten Ihr %1$s-Konto mit der Endung x%2$s am Tag der Fälligkeit der Zahlung.';
+$lang['ClientMain.myinfo.setting_autodebit_cc_zero_days'] = 'Wir berechnen Ihre %1$s mit der Endung x%2$s an dem Tag, an dem die Zahlung fällig ist.';
+$lang['ClientMain.myinfo.setting_autodebit_enabled'] = 'Ihr Konto ist für automatische Zahlungen eingerichtet.';
+$lang['ClientMain.myinfo.setting_autodebit_disabled'] = 'Ihr Konto ist nicht für automatische Zahlungen eingerichtet.';
+$lang['ClientMain.myinfo.setting_invoices'] = 'Sie erhalten derzeit Rechnungen von %1$s.';
+$lang['ClientMain.myinfo.client_id'] = 'Kunde #%1$s';
+$lang['ClientMain.myinfo.page_title'] = 'Kunde #%1$s';
+$lang['ClientMain.index.page_title'] = 'Kunde #%1$s';
+$lang['ClientMain.!tooltip.invoice_method_postalmethods'] = 'Die Rechnungen werden automatisch gedruckt und an die bei uns gespeicherte Adresse geschickt.';
+$lang['ClientMain.!tooltip.invoice_method_interfax'] = 'Wir faxen die Rechnungen automatisch an die bei uns gespeicherte Faxnummer. Wenn Sie diese Option wählen, vergewissern Sie sich bitte, dass eine gültige Faxnummer bei uns hinterlegt ist.';
+$lang['ClientMain.!tooltip.invoice_method_paper'] = 'Die Rechnungen werden gedruckt und per Post an die bei uns hinterlegte Adresse geschickt.';
+$lang['ClientMain.!tooltip.invoice_method_email'] = 'Die Rechnungen werden per E-Mail an die bei uns gespeicherte Adresse geschickt.';
+$lang['ClientMain.!info.no_managed_accounts'] = 'Es gibt keine Konten, die Sie verwalten können.';
+$lang['ClientMain.!info.no_contacts'] = 'Es gibt keine zusätzlichen Kontakte.';
+$lang['ClientMain.!info.email_pending_verification_button'] = 'Verifizierungs-E-Mail erneut senden';
+$lang['ClientMain.!info.email_pending_verification'] = 'Ihre E-Mail-Adresse ist derzeit nicht verifiziert. Es wurde eine Verifizierungs-E-Mail an %1$s gesendet.';
+$lang['ClientMain.!info.additional_services'] = '...und %1$s zusätzliche Dienstleistungen.';
+$lang['ClientMain.!info.additional_service'] = '...und 1 zusätzlicher Dienst.';
+$lang['ClientMain.!info.service_name'] = '%1$s - %2$s';
+$lang['ClientMain.!info.service_in_review'] = 'Die folgenden Dienste müssen noch vom Personal geprüft werden. Sobald sie genehmigt sind, werden sie aktiviert:';
+$lang['ClientMain.!info.invoice_due_other_currencies'] = 'Sie haben auch einen offenen Saldo in anderen Währungen.';
+$lang['ClientMain.!info.invoice_due_past_due_text'] = 'Ihr Konto weist einen offenen Saldo von %1$s auf, wovon %2$s überfällig ist. Bitte überweisen Sie den Betrag so bald wie möglich.';
+$lang['ClientMain.!info.invoice_due_text'] = 'Ihr Konto hat einen ausstehenden Saldo von %1$s. Bitte überweisen Sie den Betrag so bald wie möglich.';
+$lang['ClientMain.!info.invoice_past_due_button'] = 'Überfällige Zahlungen';
+$lang['ClientMain.!info.invoice_due_button'] = 'Zahlung vornehmen';
+$lang['ClientMain.!info.invoice_due_title'] = 'Willkommen zurück, %1$s!';
+$lang['ClientMain.!success.invoice_method_updated'] = 'Ihre Rechnungsmethode wurde erfolgreich aktualisiert. Alle zukünftigen Rechnungen werden Ihnen über %1$s zugestellt.';
+$lang['ClientMain.!success.client_updated'] = 'Ihre Kontoinformationen wurden erfolgreich aktualisiert.';
+
