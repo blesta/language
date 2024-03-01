f15c21103b56d552523ad9b2c23ac3a5af719488
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:50 -0800

    Updated it_it language files

---
+++ plugins/feed_reader/language/it_it/feed_reader_feeds.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Feed Reader Feeds
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FeedReaderFeeds.!error.date.valid'] = 'I dati specificati non sono validi.';
+$lang['FeedReaderFeeds.!error.staff_id.exists'] = 'Il membro del personale specificato non esiste.';
+$lang['FeedReaderFeeds.!error.feed_id.exists'] = 'L\'alimentazione indicata non esiste.';
+$lang['FeedReaderFeeds.!error.updated.valid'] = 'È necessario specificare una data di aggiornamento valida.';
+$lang['FeedReaderFeeds.!error.company_id.exists'] = 'L\'azienda specificata non esiste.';
+$lang['FeedReaderFeeds.!error.url.valid'] = 'L\'URL indicato non è valido.';
+
