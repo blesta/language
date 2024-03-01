c43484cbeaf3f2c0be1f54820f97bfba035e94c0
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:23 -0800

    Updated de_de language files

---
+++ plugins/order/views/templates/ajax/language/de_de/signup.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Signup
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Signup.index.heading_settings'] = 'Zusätzliche Einstellungen';
+$lang['Signup.index.heading_authentication'] = 'Authentifizierung';
+$lang['Signup.index.heading_billing'] = 'Informationen zur Rechnungsstellung';
+$lang['Signup.index.heading_contact'] = 'Kontaktinformationen';
+$lang['Signup.index.heading_client'] = 'Informationen für Kunden';
+$lang['Signup.index.field_agree_tos'] = 'Ich habe die <a href="%1$s" target="_blank">Geschäftsbedingungen</a> gelesen und stimme ihnen zu.';
+$lang['Signup.index.configure_btn'] = 'Weiter';
+$lang['Signup.index.checkout_btn'] = 'Kasse & Bezahlen';
+$lang['Signup.index.continue_btn'] = 'Weiter einkaufen';
+$lang['Signup.index.logout_btn'] = 'Abmelden';
+$lang['Signup.index.client_note'] = 'Diese Bestellung wird dem folgenden Konto gutgeschrieben.';
+$lang['Signup.index.signup_note'] = 'Sie werden die Möglichkeit haben, Ihre Bestellung zu bestätigen, nachdem Sie ein Konto erstellt haben.';
+$lang['Signup.index.login_note'] = 'Sie haben die Möglichkeit, Ihre Bestellung zu bestätigen, nachdem Sie sich eingeloggt haben.';
+$lang['Signup.index.login_or_signup_note'] = 'Wenn Sie bereits ein Konto bei uns haben, melden Sie sich bitte an, um diese Bestellung zu Ihrem bestehenden Konto hinzuzufügen.';
+$lang['Signup.index.link_resetpassword'] = 'Mein Passwort zurücksetzen';
+$lang['Signup.index.header_login_or_signup'] = 'Registrieren oder Anmelden';
+
