875a680000177c22be8f04dc0cf621434dd181a0
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:34 -0800

    Updated el_gr language files

---
+++ plugins/order/lib/order_types/registration/language/el_gr/order_type_registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Order Type Registration
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderTypeRegistration.name'] = 'Εγγραφή πελάτη';
+$lang['OrderTypeRegistration.!success.signup'] = 'Σας ευχαριστούμε για την εγγραφή ενός λογαριασμού μαζί μας.';
+$lang['OrderTypeRegistration.!error.template.valid'] = 'Η Εγγραφή πελάτη υποστηρίζει μόνο το πρότυπο Standard.';
+
