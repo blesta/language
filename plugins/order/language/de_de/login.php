fd8b6b7ee11ef45190889f7af2df7ee8ac3d841d
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:09 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Login
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Login.!error.captcha.invalid'] = 'Das eingegebene Captcha war ungültig. Bitte versuchen Sie es erneut.';
+
