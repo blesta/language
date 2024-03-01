94b5886658ed07ea291d98a4253a991e595a4d55
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:28 -0800

    Updated it_it language files

---
+++ plugins/auto_cancel/language/it_it/auto_cancel_settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Auto Cancel Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AutoCancelSettings.!error.cancel_days.greater'] = 'I giorni di annullamento dei servizi dopo la sospensione devono essere maggiori o uguali ai giorni di annullamento programmati dopo la sospensione.';
+$lang['AutoCancelSettings.!error.cancel_days.valid'] = 'Annullamento dei servizi giorni dopo la sospensione non è valido.';
+$lang['AutoCancelSettings.!error.schedule_days.valid'] = 'Il programma Giorni di cancellazione dopo la sospensione non è valido.';
+
