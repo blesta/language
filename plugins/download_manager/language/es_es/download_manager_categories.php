aadf59e1eabf452411da6dde209823f5d121c538
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:02 -0800

    Updated es_es language files

---
+++ plugins/download_manager/language/es_es/download_manager_categories.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Download Manager Categories
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerCategories.!error.parent_id.exists'] = 'La categoría superior seleccionada no existe.';
+$lang['DownloadManagerCategories.!error.parent_id.loop'] = 'La categoría padre seleccionada puede no ser la propia categoría.';
+$lang['DownloadManagerCategories.!error.company_id.exists'] = 'ID de empresa no válida.';
+$lang['DownloadManagerCategories.!error.name.empty'] = 'Introduzca un nombre de categoría.';
+$lang['DownloadManagerCategories.!error.description.empty'] = 'Introduzca una descripción de la categoría.';
+$lang['DownloadManagerCategories.!error.category_id.exists'] = 'La categoría que se está actualizando no existe.';
+
