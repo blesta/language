bc08684f39c28c3dba810124e0a6ca10dc1ca966
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:03 -0800

    Updated de_de language files

---
+++ core/Automation/Tasks/Task/language/de_de/unsuspend_services.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Unsuspend Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.unsuspendservices.success'] = 'Der Dienst #%1$s von Client %2$s wurde nicht mehr suspendiert.';
+$lang['Automation.task.unsuspendservices.error'] = 'Der Dienst #%1$s von Client %2$s konnte nicht freigegeben werden.';
+$lang['Automation.task.unsuspendservices.completed'] = 'Die Aufgabe "Dienste aussetzen" ist abgeschlossen.';
+$lang['Automation.task.unsuspendservices.attempt'] = 'Versuch, die Aussetzung bezahlter Dienste rückgängig zu machen.';
+
