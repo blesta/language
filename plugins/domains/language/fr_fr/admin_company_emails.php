417d31bdd8f7d253b943886f3186b18ddae453c8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:52 -0800

    Updated fr_fr language files

---
+++ plugins/domains/language/fr_fr/admin_company_emails.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_1_name'] = 'Rappel d\'expiration de domaine 1';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_1_desc'] = 'Premier avis envoyé à un client lorsque son domaine va bientôt expirer.';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_2_name'] = 'Rappel d\'expiration de domaine 2';
+$lang['AdminCompanyEmails.templates.Domains.domain_renewal_2_desc'] = 'Deuxième avis envoyé à un client lorsque son domaine va bientôt expirer.';
+$lang['AdminCompanyEmails.templates.Domains.domain_expiration_name'] = 'Avis d\'expiration du domaine';
+$lang['AdminCompanyEmails.templates.Domains.domain_expiration_desc'] = 'Avis envoyé à un client peu après l\'expiration de son domaine.';
+
