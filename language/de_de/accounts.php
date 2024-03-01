8394310c4cd2e63df608dc9bd807b61990df6e41
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:18 -0800

    Updated de_de language files

---
+++ language/de_de/accounts.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Accounts
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Accounts.getCcTypes.other'] = 'Andere';
+$lang['Accounts.getCcTypes.visa'] = 'Visum';
+$lang['Accounts.getCcTypes.switch'] = 'Schalter';
+$lang['Accounts.getCcTypes.solo'] = 'Solo';
+$lang['Accounts.getCcTypes.mc'] = 'Master-Karte';
+$lang['Accounts.getCcTypes.maes'] = 'Maestro';
+$lang['Accounts.getCcTypes.lasr'] = 'Laser';
+$lang['Accounts.getCcTypes.jcb'] = 'Japanisches Kreditbüro';
+$lang['Accounts.getCcTypes.ipi'] = 'InstaPayment';
+$lang['Accounts.getCcTypes.disc'] = 'Entdecken Sie';
+$lang['Accounts.getCcTypes.dc-uc'] = 'Diners Club US und Kanada';
+$lang['Accounts.getCcTypes.dc-int'] = 'Diners Club International';
+$lang['Accounts.getCcTypes.dc-er'] = 'Diners Club EnRoute';
+$lang['Accounts.getCcTypes.dc-cb'] = 'Diners Club Carte Blanche';
+$lang['Accounts.getCcTypes.cup'] = 'China Union Pay';
+$lang['Accounts.getCcTypes.bc'] = 'Bankkarte';
+$lang['Accounts.getCcTypes.amex'] = 'American Express';
+$lang['Accounts.getAchTypes.savings'] = 'Ersparnisse';
+$lang['Accounts.getAchTypes.checking'] = 'Überprüfen Sie';
+$lang['Accounts.getTypes.other'] = 'Andere';
+$lang['Accounts.getTypes.ach'] = 'automatisierte Clearingstelle';
+$lang['Accounts.getTypes.cc'] = 'Kreditkarte';
+$lang['Accounts.!error.cc_account_id.exists'] = 'Ungültige Konto-ID.';
+$lang['Accounts.!error.type.cc_format'] = 'Der Kartentyp ist ungültig oder konnte nicht ermittelt werden.';
+$lang['Accounts.!error.last4.empty_cc'] = 'Bitte geben Sie die letzten vier Ziffern der Kreditkartennummer ein.';
+$lang['Accounts.!error.expiration.valid'] = 'Das Verfallsdatum ist bereits abgelaufen.';
+$lang['Accounts.!error.expiration.empty'] = 'Bitte geben Sie das Ablaufdatum der Kreditkarte ein.';
+$lang['Accounts.!error.number.valid'] = 'Die angegebene Kartennummer ist ungültig.';
+$lang['Accounts.!error.ach_account_id.exists'] = 'Ungültige Konto-ID.';
+$lang['Accounts.!error.type.ach_format'] = 'Ungültiger Typ.';
+$lang['Accounts.!error.last4.empty_ach'] = 'Bitte geben Sie die letzten vier Ziffern der Kontonummer ein.';
+$lang['Accounts.!error.routing.empty'] = 'Bitte geben Sie eine Routingnummer an.';
+$lang['Accounts.!error.account.length'] = 'Bitte geben Sie eine Kontonummer an.';
+$lang['Accounts.!error.currency.format'] = 'Die Währung ist ungültig.';
+$lang['Accounts.!error.state.country_exists'] = 'Bitte wählen Sie das Land aus, das dem ausgewählten Staat entspricht.';
+$lang['Accounts.!error.country.length'] = 'Die Länge des Landes darf 3 Zeichen nicht überschreiten.';
+$lang['Accounts.!error.state.length'] = 'Die Länge der Angabe darf 3 Zeichen nicht überschreiten.';
+$lang['Accounts.!error.last_name.empty'] = 'Bitte geben Sie einen Nachnamen an.';
+$lang['Accounts.!error.first_name.empty'] = 'Bitte geben Sie einen Vornamen an.';
+$lang['Accounts.!error.contact_id.exists'] = 'Ungültige Kontakt-ID.';
+
