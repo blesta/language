1266f687f791c77cb12a2f9dd6a0818b052cf094
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:00 -0800

    Updated el_gr language files

---
+++ plugins/support_manager/language/el_gr/support_manager_staff.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Support Manager Staff
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SupportManagerStaff.!error.staff_id.exists'] = 'Το μέλος του προσωπικού που πρόκειται να προστεθεί δεν υπάρχει.';
+$lang['SupportManagerStaff.!error.staff_id.belongs'] = 'Ο υπάλληλος δεν ανήκει στην εταιρεία στην οποία διορίζεται.';
+$lang['SupportManagerStaff.!error.staff_id.duplicate'] = 'Το επιλεγμένο μέλος του προσωπικού υπάρχει ήδη στο σύστημα υποστήριξης.';
+$lang['SupportManagerStaff.!error.company_id.exists'] = 'Το αναγνωριστικό της εταιρείας είναι άκυρο.';
+$lang['SupportManagerStaff.!error.departments.exists'] = 'Τουλάχιστον ένα από τα τμήματα που έχουν οριστεί δεν υπάρχει.';
+$lang['SupportManagerStaff.!error.schedules.minimum'] = 'Ορίστε μια προγραμματισμένη ώρα έναρξης και λήξης για τουλάχιστον μία ημέρα.';
+$lang['SupportManagerStaff.!error.schedules.unique_days'] = 'Κάθε προγραμματισμένη ημέρα πρέπει να είναι μοναδική.';
+$lang['SupportManagerStaff.!error.schedules[][day].format'] = 'Μη έγκυρη προγραμματισμένη ημέρα.';
+$lang['SupportManagerStaff.!error.schedules[][start_time].format'] = 'Επιλέξτε μια ώρα έναρξης για κάθε προγραμματισμένη ημέρα.';
+$lang['SupportManagerStaff.!error.schedules[][end_time].format'] = 'Παρακαλούμε επιλέξτε μια ώρα λήξης για κάθε προγραμματισμένη ημέρα.';
+$lang['SupportManagerStaff.days.sun'] = 'Κυριακή';
+$lang['SupportManagerStaff.days.mon'] = 'Δευτέρα';
+$lang['SupportManagerStaff.days.tue'] = 'Τρίτη';
+$lang['SupportManagerStaff.days.wed'] = 'Τετάρτη';
+$lang['SupportManagerStaff.days.thu'] = 'Πέμπτη';
+$lang['SupportManagerStaff.days.fri'] = 'Παρασκευή';
+$lang['SupportManagerStaff.days.sat'] = 'Σάββατο';
+
