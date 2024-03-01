099ee18e1be845a2df6299e39e06cbba2014e426
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:34 -0800

    Updated el_gr language files

---
+++ components/gateways/nonmerchant/paystack/language/el_gr/paystack.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Paystack
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Paystack.buildprocess.submit'] = 'Πληρώστε με το Paystack';
+$lang['Paystack.tooltip.secret_key'] = 'Για να χρησιμοποιήσετε τη δοκιμαστική λειτουργία, απλώς πληκτρολογήστε ένα δοκιμαστικό πλήκτρο.';
+$lang['Paystack.secret_key'] = 'Μυστικό κλειδί Paystack';
+$lang['Paystack.description'] = 'Το Paystack είναι ο ταχύτερος και απλούστερος τρόπος για να ξεκινήσετε να δέχεστε online πληρωμές στη Νιγηρία.';
+$lang['Paystack.name'] = 'Πρότυπο Paystack';
+$lang['Paystack.!error.secret_key.valid'] = 'Το μυστικό κλειδί πρέπει να έχει μήκος τουλάχιστον 40 χαρακτήρων.';
+
