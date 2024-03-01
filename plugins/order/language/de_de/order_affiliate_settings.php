7719f6ba03256e5e02d48ed8173e9bf3247d5999
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:12 -0800

    Updated de_de language files

---
+++ plugins/order/language/de_de/order_affiliate_settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Order Affiliate Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliateSettings.!error.affiliate_id.exists'] = 'Ungültige Partner-ID.';
+
