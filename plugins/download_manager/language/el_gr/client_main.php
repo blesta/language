36276888a70415077771542ba4fffed3e321a2d2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:51:53 -0800

    Updated el_gr language files

---
+++ plugins/download_manager/language/el_gr/client_main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.!text.root_directory'] = 'Αρχική σελίδα';
+$lang['ClientMain.index.page_title'] = 'Λήψεις';
+$lang['ClientMain.index.boxtitle_downloads'] = 'Λήψεις';
+$lang['ClientMain.index.login'] = 'Πρέπει να συνδεθείτε για να δείτε τις λήψεις σε αυτό το τμήμα.';
+$lang['ClientMain.index.no_downloads'] = 'Δεν υπάρχουν διαθέσιμες λήψεις σε αυτό το τμήμα.';
+
