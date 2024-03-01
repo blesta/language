810c84130edf2c6bbfe14b0bbd5d5f0326f3252e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:04:20 -0800

    Updated cs_cz language files

---
+++ plugins/order/views/templates/ajax/language/cs_cz/config.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Config
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Config.index.term_years'] = '%1$s let';
+$lang['Config.index.term_year'] = '%1$s rok';
+$lang['Config.index.term_months'] = '%1$s měsíců';
+$lang['Config.index.term_month'] = '%1$s měsíc';
+$lang['Config.index.term_weeks'] = '%1$s týdnů';
+$lang['Config.index.term_week'] = '%1$s týden';
+$lang['Config.index.term_days'] = '%1$s dní';
+$lang['Config.index.term_day'] = '%1$s den';
+$lang['Config.index.continue_btn'] = 'Pokračovat';
+$lang['Config.index.addons_notes'] = 'V dalším kroku budou nakonfigurovány všechny vybrané doplňky.';
+$lang['Config.index.header_addons'] = 'Dostupné doplňky';
+$lang['Config.index.header_module'] = 'Konfigurovatelné možnosti';
+$lang['Config.index.header_term'] = 'Termín a cena';
+$lang['Config.index.config_notes'] = 'Vyberte požadované možnosti a pokračujte k pokladně.';
+$lang['Config.index.text_free_domain'] = 'Cenová možnost %1$s nebo vyšší vás v dalším kroku opravňuje k získání domény zdarma.';
+$lang['Config.index.text_free_domain_next_step'] = 'V dalším kroku vyberte kvalifikovanou položku s minimální dobou platnosti %1$s, abyste tuto doménu získali zdarma.';
+$lang['Config.index.text_free_domain_next_step_limited'] = 'V dalším kroku vyberte položku s minimální dobou platnosti %1$s a získejte tuto doménu zdarma na dobu platnosti %2$s.';
+$lang['Config.index.header_configure_service'] = 'Konfigurace %1$s';
+$lang['Config.index.header_configuration'] = 'Konfigurace';
+
