15fccb12c1681b8d27241b9449b63326d56fba40
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:23 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/payment_reminders.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Payment Reminders
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.payment_reminders.attempt'] = 'Intento de envío de recordatorios de pago.';
+$lang['Automation.task.payment_reminders.completed'] = 'La tarea de recordatorio de pago ha finalizado.';
+$lang['Automation.task.payment_reminders.failed'] = 'No se ha podido enviar el aviso de recordatorio de la factura #%4$s al contacto %1$s %2$s del cliente #%3$s.';
+$lang['Automation.task.payment_reminders.success'] = 'Entregado con éxito el aviso de recordatorio de la factura #%4$s al contacto %1$s %2$s del cliente #%3$s.';
+$lang['Automation.task.payment_reminders.autodebit_failed'] = 'No se ha podido enviar el aviso de recordatorio de cargo automático de la factura #%4$s al contacto %1$s %2$s del cliente #%3$s.';
+$lang['Automation.task.payment_reminders.autodebit_success'] = 'Se ha enviado correctamente el aviso de recordatorio de cargo automático de la factura #%4$s al contacto %1$s %2$s del cliente #%3$s.';
+
