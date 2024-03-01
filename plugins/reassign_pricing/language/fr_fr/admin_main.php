04def6d5e926d21c9860de92400e710f54bd2491
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:59 -0800

    Updated fr_fr language files

---
+++ plugins/reassign_pricing/language/fr_fr/admin_main.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.!success.service_updated'] = 'La tarification du service a été mise à jour avec succès.';
+$lang['AdminMain.!warning.reassign_pricing'] = 'La réaffectation de la tarification ne modifie que l\'offre et la tarification associées à ce service. N\'effectuez pas cette opération pour les mises à niveau ou les rétrogradations.';
+$lang['AdminMain.index.page_title'] = 'Client #%1$s Réassigner la tarification';
+$lang['AdminMain.index.boxtitle_services'] = 'Services pour le client #%1$s';
+$lang['AdminMain.index.link_client'] = 'Retour au client #%1$s';
+$lang['AdminMain.index.heading_package'] = 'Paquet';
+$lang['AdminMain.index.heading_label'] = 'Étiquette';
+$lang['AdminMain.index.heading_term'] = 'Durée';
+$lang['AdminMain.index.heading_date_created'] = 'Date de création';
+$lang['AdminMain.index.heading_date_renews'] = 'Date Renouvellement';
+$lang['AdminMain.index.heading_options'] = 'Options';
+$lang['AdminMain.index.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['AdminMain.index.text_never'] = 'Jamais';
+$lang['AdminMain.index.no_results'] = 'Il n\'y a pas de services disponibles à partir desquels il est possible de réaffecter la tarification.';
+$lang['AdminMain.index.option_service'] = 'Réattribution de la tarification';
+$lang['AdminMain.service.term_onetime'] = 'Onetime - %3$s';
+$lang['AdminMain.service.term'] = '%1$s %2$s - %3$s';
+$lang['AdminMain.service.boxtitle_pricing'] = 'Réaffectation de la tarification pour le service : %1$s - %2$s';
+$lang['AdminMain.service.heading_information'] = 'Informations sur les services';
+$lang['AdminMain.service.text_package_name'] = 'Nom de l\'emballage :';
+$lang['AdminMain.service.text_label'] = 'Étiquette :';
+$lang['AdminMain.service.text_qty'] = 'Quantité :';
+$lang['AdminMain.service.text_term'] = 'Durée :';
+$lang['AdminMain.service.text_status'] = 'Nom de l\'emballage :';
+$lang['AdminMain.service.text_date_added'] = 'Date d\'ajout :';
+$lang['AdminMain.service.text_date_renews'] = 'Date de renouvellement :';
+$lang['AdminMain.service.text_recurring_coupon'] = 'Coupon récurrent :';
+$lang['AdminMain.service.text_coupon_percent'] = '%1$s (%2$s%%)';
+$lang['AdminMain.service.text_coupon_amount'] = '%1$s (%2$s)';
+$lang['AdminMain.service.text_renewal_price'] = 'Prix de renouvellement :';
+$lang['AdminMain.service.heading_pricing'] = 'Réattribution de la tarification';
+$lang['AdminMain.service.field_pricing'] = 'Paquet/Terme';
+$lang['AdminMain.service.field_group'] = 'Groupe de paquets';
+$lang['AdminMain.service.field_cancel'] = 'Annuler';
+$lang['AdminMain.service.field_submit'] = 'Économiser';
+
