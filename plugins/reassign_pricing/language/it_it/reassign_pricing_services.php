886a276fa3d1c057824964ac272afd817a92fbea
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:39 -0800

    Updated it_it language files

---
+++ plugins/reassign_pricing/language/it_it/reassign_pricing_services.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Reassign Pricing Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReassignPricingServices.!error.service_id.exists'] = 'ID servizio non valido.';
+$lang['ReassignPricingServices.!error.module_row_id.company'] = 'L\'ID della riga del modulo non appartiene alla stessa azienda del servizio.';
+$lang['ReassignPricingServices.!error.module_row_id.exists'] = 'ID riga modulo non valido rispetto all\'ID prezzo indicato.';
+$lang['ReassignPricingServices.!error.package_group_id.company'] = 'L\'ID del gruppo di pacchetti non appartiene alla stessa azienda del servizio.';
+$lang['ReassignPricingServices.!error.package_group_id.exists'] = 'ID gruppo di pacchetti non valido.';
+$lang['ReassignPricingServices.!error.pricing_id.company'] = 'L\'ID di tariffazione non appartiene alla stessa azienda del servizio.';
+$lang['ReassignPricingServices.!error.pricing_id.exists'] = 'ID prezzo non valido.';
+
