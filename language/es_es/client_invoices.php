92f0f15caed62b7b253648cd6aa8a1a36de0dceb
pmphillips <paul@phillipsdata.com>
2024-03-01 14:43:11 -0800

    Updated es_es language files

---
+++ language/es_es/client_invoices.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Client Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientInvoices.index.page_title'] = 'Cliente #%1$s Facturas';
+$lang['ClientInvoices.index.category_open'] = 'Abrir';
+$lang['ClientInvoices.index.category_closed'] = 'Cerrado';
+$lang['ClientInvoices.index.categorylink_make_payment'] = 'Realizar el pago';
+$lang['ClientInvoices.index.boxtitle_invoices'] = 'Facturas';
+$lang['ClientInvoices.index.heading_invoice'] = 'Factura #';
+$lang['ClientInvoices.index.heading_amount'] = 'Importe';
+$lang['ClientInvoices.index.heading_paid'] = 'Pagado';
+$lang['ClientInvoices.index.heading_due'] = 'Debido';
+$lang['ClientInvoices.index.heading_dateclosed'] = 'Fecha de cierre';
+$lang['ClientInvoices.index.heading_datebilled'] = 'Fecha de facturación';
+$lang['ClientInvoices.index.heading_datedue'] = 'Fecha de vencimiento';
+$lang['ClientInvoices.index.heading_options'] = 'Opciones';
+$lang['ClientInvoices.index.option_view'] = 'Ver';
+$lang['ClientInvoices.index.option_pay'] = 'Pagar';
+$lang['ClientInvoices.index.no_results'] = 'No tiene %1$s Facturas.';
+$lang['ClientInvoices.applied.heading_paymenttype'] = 'Tipo de pago';
+$lang['ClientInvoices.applied.heading_amount'] = 'Importe';
+$lang['ClientInvoices.applied.heading_applied'] = 'Aplicada';
+$lang['ClientInvoices.applied.heading_appliedon'] = 'Aplicado en';
+$lang['ClientInvoices.applied.no_results'] = 'Esta factura no tiene ninguna transacción aplicada.';
+
