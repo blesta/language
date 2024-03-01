b20cf00606410474050ccd618b368a46232f5407
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:31 -0800

    Updated de_de language files

---
+++ language/de_de/admin_myinfo.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Admin Myinfo
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMyinfo.index.field_recovery_email'] = 'Wiederherstellungs-E-Mail (optional)';
+$lang['AdminMyinfo.notices.no_subscription_results'] = 'Für Ihre Mitarbeitergruppe sind keine Abonnement-Benachrichtigungen verfügbar.';
+$lang['AdminMyinfo.notices.no_bcc_results'] = 'Es sind keine BCC-Benachrichtigungen per E-Mail für Ihre Mitarbeitergruppe verfügbar.';
+$lang['AdminMyinfo.notices.field_noticesubmit'] = 'Aktualisierungsmitteilungen';
+$lang['AdminMyinfo.notices.heading_subscription_notices'] = 'E-Mail-Abonnement-Benachrichtigungen';
+$lang['AdminMyinfo.notices.heading_notices'] = 'BCC-Benachrichtigungen per E-Mail';
+$lang['AdminMyinfo.notices.page_title'] = 'Meine Informationen > Notizen';
+$lang['AdminMyinfo.index.field_infosubmit'] = 'Konto aktualisieren';
+$lang['AdminMyinfo.index.field_currentpass'] = 'Aktuelles Passwort';
+$lang['AdminMyinfo.index.field_groups'] = 'Personalgruppen';
+$lang['AdminMyinfo.index.note_otp'] = 'Um zu bestätigen, dass Ihre Zwei-Faktor-Authentifizierung korrekt konfiguriert ist, geben Sie Ihr generiertes Einmalpasswort ein.';
+$lang['AdminMyinfo.index.field_otp'] = 'Einmaliges Passwort';
+$lang['AdminMyinfo.index.field_language'] = 'Sprache';
+$lang['AdminMyinfo.index.field_twofactorpin'] = 'Zwei-Faktor-Pin';
+$lang['AdminMyinfo.index.field_twofactorkey'] = 'Zwei-Faktor-Schlüssel';
+$lang['AdminMyinfo.index.field_twofactormode'] = 'Zwei-Faktor-Authentifizierung';
+$lang['AdminMyinfo.index.field_confirmpass'] = 'Bestätigen Sie Ihr Passwort';
+$lang['AdminMyinfo.index.field_newpass'] = 'Neues Passwort';
+$lang['AdminMyinfo.index.field_mobilenumber'] = 'Handynummer';
+$lang['AdminMyinfo.index.field_mobileemail'] = 'Mobile E-Mail';
+$lang['AdminMyinfo.index.field_email'] = 'E-Mail';
+$lang['AdminMyinfo.index.field_lastname'] = 'Nachname';
+$lang['AdminMyinfo.index.field_firstname'] = 'Vorname';
+$lang['AdminMyinfo.index.field_username'] = 'Benutzername';
+$lang['AdminMyinfo.index.boxtitle_myinfo'] = 'Meine Informationen';
+$lang['AdminMyinfo.index.page_title'] = 'Meine Informationen';
+$lang['AdminMyinfo.gettabnames.text_notices'] = 'Notizen';
+$lang['AdminMyinfo.gettabnames.text_index'] = 'Konto';
+$lang['AdminMyinfo.!success.notices_updated'] = 'Ihre Benachrichtigungseinstellungen wurden erfolgreich aktualisiert.';
+$lang['AdminMyinfo.!success.updated'] = 'Ihre Kontoeinstellungen wurden erfolgreich aktualisiert.';
+
