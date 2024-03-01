448e9e9186712b0bb801bc784c2a458dca2e74aa
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:19 -0800

    Updated es_es language files

---
+++ plugins/mass_mailer/language/es_es/admin_compose.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Admin Compose
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompose.!error.parse_error'] = 'Error de análisis de plantilla: %1$s';
+$lang['AdminCompose.index.page_title'] = 'Correo masivo > Redactar';
+$lang['AdminCompose.!tooltip.log'] = 'Si está marcada, todos los emails que se envíen se registrarán en Blesta.';
+$lang['AdminCompose.index.boxtitle'] = 'Redactar correo';
+$lang['AdminCompose.index.form.from_name'] = 'De Nombre';
+$lang['AdminCompose.index.form.from_address'] = 'Del correo electrónico';
+$lang['AdminCompose.index.form.subject'] = 'Asunto';
+$lang['AdminCompose.index.form.html'] = 'HTML';
+$lang['AdminCompose.index.form.text'] = 'Texto';
+$lang['AdminCompose.index.form.log'] = 'Registro de correos electrónicos';
+$lang['AdminCompose.index.text.note'] = 'Si no se establece ninguna versión de texto, se generará automáticamente a partir de la versión HTML.';
+$lang['AdminCompose.index.heading_client_tags'] = 'Etiquetas de cliente';
+$lang['AdminCompose.index.heading_service_tags'] = 'Etiquetas de servicio';
+$lang['AdminCompose.index.link_back'] = 'Volver';
+$lang['AdminCompose.index.form.preview'] = 'Vista previa';
+$lang['AdminCompose.preview.boxtitle'] = 'Envío previo';
+$lang['AdminCompose.preview.email_total'] = 'Total de contactos a enviar por correo electrónico: %1$s';
+$lang['AdminCompose.preview.heading.email_sample'] = 'Ejemplo de correo electrónico';
+$lang['AdminCompose.preview.text_name'] = 'De: %1$s %2$s';
+$lang['AdminCompose.preview.text_subject'] = 'Asunto: %1$s';
+$lang['AdminCompose.preview.link_back'] = 'Volver';
+$lang['AdminCompose.preview.form.send'] = 'Enviar';
+
