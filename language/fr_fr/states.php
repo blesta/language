f67ca7a241e636297e6ee5bf8da74201a82f2426
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:40 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/states.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * States
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['States.!error.code.format'] = 'Le code de l\'État doit être alphanumérique tel que défini dans la norme ISO 3166-2.';
+$lang['States.!error.country_alpha2.format'] = 'Pays Alpha2 doit être composé de 2 caractères alpha.';
+$lang['States.!error.country_alpha2.in_use'] = 'Le code alpha2 du pays %1$s et le code de subdivision %2$s sont déjà pris.';
+$lang['States.!error.name.format'] = 'Veuillez saisir le nom de l\'État.';
+
