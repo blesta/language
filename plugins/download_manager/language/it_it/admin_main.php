d70be04c5a91b21606e04d26ee4ac9594b4e8ed8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:40 -0800

    Updated it_it language files

---
+++ plugins/download_manager/language/it_it/admin_main.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.package_name'] = '%1$s (%2$s)';
+$lang['AdminMain.editcategory.submit_cancel'] = 'Annullamento';
+$lang['AdminMain.editcategory.submit_edit'] = 'Categoria di aggiornamento';
+$lang['AdminMain.editcategory.field_description'] = 'Descrizione';
+$lang['AdminMain.editcategory.field_name'] = 'Nome';
+$lang['AdminMain.editcategory.boxtitle_editcategory'] = 'Aggiornare la categoria [%1$s]';
+$lang['AdminMain.editcategory.page_title'] = 'Download Manager > Aggiorna categoria';
+$lang['AdminMain.addcategory.submit_cancel'] = 'Annullamento';
+$lang['AdminMain.addcategory.submit_add'] = 'Crea categoria';
+$lang['AdminMain.addcategory.field_description'] = 'Descrizione';
+$lang['AdminMain.addcategory.field_name'] = 'Nome';
+$lang['AdminMain.addcategory.boxtitle_addcategory'] = 'Aggiungi la categoria alla categoria [%1$s].';
+$lang['AdminMain.addcategory.boxtitle_root'] = 'Aggiungere una categoria alla %1$s';
+$lang['AdminMain.addcategory.page_title'] = 'Download Manager > Aggiungi categoria';
+$lang['AdminMain.editurl.submit_cancel'] = 'Annullamento';
+$lang['AdminMain.editurl.submit_add'] = 'Modifica URL';
+$lang['AdminMain.editurl.field_category'] = 'Categoria';
+$lang['AdminMain.editurl.field_file'] = 'File';
+$lang['AdminMain.editurl.field_url'] = 'Nome URL';
+$lang['AdminMain.editurl.boxtitle_add'] = 'Modifica dell\'URL statico';
+$lang['AdminMain.editurl.page_title'] = 'Download Manager > Modifica URL statico';
+$lang['AdminMain.edit.submit_cancel'] = 'Annullamento';
+$lang['AdminMain.edit.submit_edit'] = 'Scarica l\'aggiornamento';
+$lang['AdminMain.edit.field_file_name'] = 'Percorso del file';
+$lang['AdminMain.edit.field_file'] = 'File';
+$lang['AdminMain.edit.field_path'] = 'Specificare il percorso del file';
+$lang['AdminMain.edit.field_upload'] = 'Caricare il file';
+$lang['AdminMain.edit.text_availablepackages'] = 'Pacchetti disponibili';
+$lang['AdminMain.edit.text_availableclientgroups'] = 'Gruppi di clienti disponibili';
+$lang['AdminMain.edit.text_packagegroups'] = 'Pacchetti selezionati';
+$lang['AdminMain.edit.text_clientgroups'] = 'Gruppi di clienti selezionati';
+$lang['AdminMain.edit.field_available_to_packages'] = 'Disponibile per i pacchetti';
+$lang['AdminMain.edit.field_available_to_client_groups'] = 'Disponibile per i gruppi di clienti';
+$lang['AdminMain.edit.field_name'] = 'Nome';
+$lang['AdminMain.edit.field_logged_in'] = 'Deve essere connesso';
+$lang['AdminMain.edit.field_public'] = 'Disponibile al pubblico';
+$lang['AdminMain.edit.boxtitle_edit'] = 'Scarica l\'aggiornamento';
+$lang['AdminMain.edit.page_title'] = 'Download Manager > Aggiungi download';
+$lang['AdminMain.addurl.submit_cancel'] = 'Annullamento';
+$lang['AdminMain.addurl.submit_add'] = 'Aggiungi URL';
+$lang['AdminMain.addurl.field_category'] = 'Categoria';
+$lang['AdminMain.addurl.field_file'] = 'File';
+$lang['AdminMain.addurl.field_url'] = 'Nome URL';
+$lang['AdminMain.addurl.boxtitle_add'] = 'Aggiungi URL statico';
+$lang['AdminMain.addurl.page_title'] = 'Download Manager > Aggiungi URL statico';
+$lang['AdminMain.add.submit_cancel'] = 'Annullamento';
+$lang['AdminMain.add.submit_add'] = 'Aggiungi Download';
+$lang['AdminMain.add.field_file_name'] = 'Percorso del file';
+$lang['AdminMain.add.field_file'] = 'File';
+$lang['AdminMain.add.field_path'] = 'Specificare il percorso del file';
+$lang['AdminMain.add.field_upload'] = 'Caricare il file';
+$lang['AdminMain.add.text_availablepackages'] = 'Pacchetti disponibili';
+$lang['AdminMain.add.text_availableclientgroups'] = 'Gruppi di clienti disponibili';
+$lang['AdminMain.add.text_packagegroups'] = 'Pacchetti selezionati';
+$lang['AdminMain.add.text_clientgroups'] = 'Gruppi di clienti selezionati';
+$lang['AdminMain.add.field_available_to_packages'] = 'Disponibile per i pacchetti';
+$lang['AdminMain.add.field_available_to_client_groups'] = 'Disponibile per i gruppi di clienti';
+$lang['AdminMain.add.field_name'] = 'Nome';
+$lang['AdminMain.add.field_logged_in'] = 'Deve essere connesso';
+$lang['AdminMain.add.field_public'] = 'Disponibile al pubblico';
+$lang['AdminMain.add.boxtitle_add'] = 'Aggiungi Download alla categoria [%1$s]';
+$lang['AdminMain.add.boxtitle_root'] = 'Aggiungere il download al %1$s';
+$lang['AdminMain.add.page_title'] = 'Download Manager > Aggiungi download';
+$lang['AdminMain.urls.no_urls'] = 'In questa sezione non ci sono URL statici.';
+$lang['AdminMain.urls.latest_file'] = 'Ultimo file da <strong>%1$s</strong>';
+$lang['AdminMain.urls.delete'] = 'Cancellare';
+$lang['AdminMain.urls.edit'] = 'Modifica';
+$lang['AdminMain.urls.heading_options'] = 'Opzioni';
+$lang['AdminMain.urls.heading_link'] = 'Collegamento';
+$lang['AdminMain.urls.heading_file'] = 'File';
+$lang['AdminMain.urls.heading_url'] = 'URL';
+$lang['AdminMain.urls.tab_urls'] = 'URL statici';
+$lang['AdminMain.urls.tab_files'] = 'File';
+$lang['AdminMain.urls.add_url'] = 'Aggiungi URL statico';
+$lang['AdminMain.urls.boxtitle_downloadmanager'] = 'Download Manager';
+$lang['AdminMain.urls.page_title'] = 'Download Manager > Gestisci';
+$lang['AdminMain.files.no_downloads'] = 'Non ci sono download in questa sezione.';
+$lang['AdminMain.files.delete'] = 'Cancellare';
+$lang['AdminMain.files.edit'] = 'Modifica';
+$lang['AdminMain.files.go_back'] = 'Salire di livello';
+$lang['AdminMain.files.add_category'] = 'Aggiungi categoria qui';
+$lang['AdminMain.files.add_download'] = 'Aggiungi download qui';
+$lang['AdminMain.files.tab_urls'] = 'URL statici';
+$lang['AdminMain.files.tab_files'] = 'File';
+$lang['AdminMain.files.boxtitle_downloadmanager'] = 'Download Manager';
+$lang['AdminMain.files.page_title'] = 'Download Manager > Gestisci';
+$lang['AdminMain.modal.delete_url'] = 'Siete sicuri di voler eliminare questo URL statico?';
+$lang['AdminMain.modal.delete_category'] = 'Siete sicuri di voler eliminare questa categoria? Tutte le sottocategorie e i file di questa categoria saranno spostati nella categoria madre.';
+$lang['AdminMain.modal.delete_file'] = 'Siete sicuri di voler eliminare questo file?';
+$lang['AdminMain.!text.closed_parenthesis'] = ')';
+$lang['AdminMain.!text.forward_slash'] = '/';
+$lang['AdminMain.!text.open_parenthesis'] = '(';
+$lang['AdminMain.!text.root_directory'] = 'Elenco delle case';
+$lang['AdminMain.!tooltip.category_type'] = 'L\'URL punterà all\'ultimo file caricato in questa categoria.';
+$lang['AdminMain.!tooltip.path_to_file'] = 'Inserire il percorso assoluto del file nel file system.';
+$lang['AdminMain.!success.url_updated'] = 'L\'URL statico è stato aggiornato con successo.';
+$lang['AdminMain.!success.url_added'] = 'L\'URL statico è stato aggiunto con successo.';
+$lang['AdminMain.!success.url_deleted'] = 'L\'URL statico è stato eliminato con successo.';
+$lang['AdminMain.!success.file_deleted'] = 'Il file è stato eliminato con successo.';
+$lang['AdminMain.!success.file_updated'] = 'Il file è stato aggiornato con successo.';
+$lang['AdminMain.!success.file_added'] = 'Il file è stato aggiunto con successo.';
+$lang['AdminMain.!success.category_deleted'] = 'La categoria è stata eliminata con successo.';
+$lang['AdminMain.!success.category_updated'] = 'La categoria è stata aggiornata con successo.';
+$lang['AdminMain.!success.category_added'] = 'La categoria è stata creata con successo.';
+
