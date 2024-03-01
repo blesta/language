dd0be0a7b0250572fa43ad76fb3d79c22ca1718f
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:29 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/order_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Order Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderSettings.!error.embed_code.parse'] = 'Σφάλμα ανάλυσης κώδικα ενσωμάτωσης: %1$s';
+$lang['OrderSettings.getantifraud.fraudlabspro'] = 'FraudLabs Pro';
+$lang['OrderSettings.getantifraud.maxmind'] = 'Κληρονομιά MaxMind';
+$lang['OrderSettings.getantifraud.maxmind_v2'] = 'MaxMind v2';
+
