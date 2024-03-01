ab7a40731651c554bce1a10c28bf91c95db59b49
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:59 -0800

    Updated fr_fr language files

---
+++ plugins/download_manager/language/fr_fr/download_manager_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Download Manager Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerPlugin.name'] = 'Gestionnaire de téléchargement';
+$lang['DownloadManagerPlugin.description'] = 'Permet de télécharger des fichiers et de les mettre à la disposition des clients pour qu\'ils les téléchargent.';
+$lang['DownloadManagerPlugin.client_main'] = 'Téléchargements';
+$lang['DownloadManagerPlugin.admin_main'] = 'Gestionnaire de téléchargement';
+
