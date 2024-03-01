9122cff08e77e1fed42c4be60cc61a8064781467
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:06 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Module.!error.general'] = 'Při zpracování požadavku pomocí modulu došlo k chybě.';
+$lang['Module.!error.invalid'] = 'Byla přijata neplatná nebo nerozpoznaná odpověď API.';
+$lang['Module.!error.unsupported'] = 'Modul tuto akci nepodporuje.';
+
