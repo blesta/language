abd4cefec955f44398cecc35281afd4cfbdc070c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:08 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/admin_payment_methods.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Admin Payment Methods
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminPaymentMethods.edit.field_payment_methodsubmit'] = 'Aggiornare il metodo di pagamento';
+$lang['AdminPaymentMethods.edit.field_name'] = 'Nome';
+$lang['AdminPaymentMethods.edit.boxtitle_edit_payment_method'] = 'Modifica del metodo di pagamento dell\'affiliato';
+$lang['AdminPaymentMethods.add.field_payment_methodsubmit'] = 'Aggiungi metodo di pagamento';
+$lang['AdminPaymentMethods.add.field_name'] = 'Nome';
+$lang['AdminPaymentMethods.add.boxtitle_add_payment_method'] = 'Aggiungere un metodo di pagamento affiliato';
+$lang['AdminPaymentMethods.index.confirm_delete'] = 'Siete sicuri di voler eliminare questo metodo di pagamento?';
+$lang['AdminPaymentMethods.index.option_delete'] = 'Cancellare';
+$lang['AdminPaymentMethods.index.option_edit'] = 'Modifica';
+$lang['AdminPaymentMethods.index.heading_options'] = 'Opzioni';
+$lang['AdminPaymentMethods.index.heading_name'] = 'Nome';
+$lang['AdminPaymentMethods.index.no_results'] = 'Al momento non sono previsti metodi di pagamento.';
+$lang['AdminPaymentMethods.index.affiliate_payment_method_add'] = 'Aggiungere un metodo di pagamento affiliato';
+$lang['AdminPaymentMethods.index.boxtitle_payment_methods'] = 'Metodi di pagamento degli affiliati';
+$lang['AdminPaymentMethods.index.page_title'] = 'Metodi di pagamento degli affiliati';
+$lang['AdminPaymentMethods.!success.payment_method_updated'] = 'Il metodo di pagamento è stato aggiornato con successo.';
+$lang['AdminPaymentMethods.!success.payment_method_deleted'] = 'Il metodo di pagamento è stato cancellato con successo.';
+$lang['AdminPaymentMethods.!success.payment_method_added'] = 'Il metodo di pagamento è stato aggiunto con successo.';
+
