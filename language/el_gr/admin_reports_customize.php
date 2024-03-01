64a9ecb8230868f593048344a334780b0678108b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:53 -0800

    Updated el_gr language files

---
+++ language/el_gr/admin_reports_customize.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Admin Reports Customize
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminReportsCustomize.edit.field_submit'] = 'Αποθήκευση';
+$lang['AdminReportsCustomize.edit.text_remove'] = 'Αφαιρέστε το';
+$lang['AdminReportsCustomize.edit.field_regex'] = 'Προσαρμοσμένη Regex';
+$lang['AdminReportsCustomize.edit.field_values'] = 'Τιμές (διαχωρισμένες με κόμμα)';
+$lang['AdminReportsCustomize.edit.table_options'] = 'Επιλογές';
+$lang['AdminReportsCustomize.edit.table_required'] = 'Απαιτούμενο';
+$lang['AdminReportsCustomize.edit.table_type'] = 'Τύπος';
+$lang['AdminReportsCustomize.edit.table_label'] = 'Ετικέτα';
+$lang['AdminReportsCustomize.edit.table_name'] = 'Όνομα';
+$lang['AdminReportsCustomize.edit.field_add_field'] = 'Προσθήκη πεδίου';
+$lang['AdminReportsCustomize.edit.heading_fields'] = 'Πεδία';
+$lang['AdminReportsCustomize.edit.field_query'] = 'Ερώτηση';
+$lang['AdminReportsCustomize.edit.field_name'] = 'Όνομα';
+$lang['AdminReportsCustomize.edit.page_title'] = 'Αναφορές χρέωσης';
+$lang['AdminReportsCustomize.add.field_submit'] = 'Αποθήκευση';
+$lang['AdminReportsCustomize.add.text_remove'] = 'Αφαιρέστε το';
+$lang['AdminReportsCustomize.add.field_regex'] = 'Προσαρμοσμένη Regex';
+$lang['AdminReportsCustomize.add.field_values'] = 'Τιμές (διαχωρισμένες με κόμμα)';
+$lang['AdminReportsCustomize.add.table_options'] = 'Επιλογές';
+$lang['AdminReportsCustomize.add.table_required'] = 'Απαιτούμενο';
+$lang['AdminReportsCustomize.add.table_type'] = 'Τύπος';
+$lang['AdminReportsCustomize.add.table_label'] = 'Ετικέτα';
+$lang['AdminReportsCustomize.add.table_name'] = 'Όνομα';
+$lang['AdminReportsCustomize.add.field_add_field'] = 'Προσθήκη πεδίου';
+$lang['AdminReportsCustomize.add.heading_fields'] = 'Πεδία';
+$lang['AdminReportsCustomize.add.field_query'] = 'Ερώτηση';
+$lang['AdminReportsCustomize.add.field_name'] = 'Όνομα';
+$lang['AdminReportsCustomize.add.page_title'] = 'Αναφορές χρέωσης';
+$lang['AdminReportsCustomize.index.no_custom_reports'] = 'Δεν υπάρχουν προσαρμοσμένες αναφορές.';
+$lang['AdminReportsCustomize.index.btn_add_report'] = 'Προσθήκη αναφοράς';
+$lang['AdminReportsCustomize.index.table_confirm_delete'] = 'Σίγουρα θέλετε να διαγράψετε αυτή την αναφορά;';
+$lang['AdminReportsCustomize.index.table_option_delete'] = 'Διαγραφή';
+$lang['AdminReportsCustomize.index.table_option_edit'] = 'Επεξεργασία';
+$lang['AdminReportsCustomize.index.table_options'] = 'Επιλογές';
+$lang['AdminReportsCustomize.index.table_created'] = 'Ημερομηνία δημιουργίας';
+$lang['AdminReportsCustomize.index.table_name'] = 'Όνομα';
+$lang['AdminReportsCustomize.index.page_title'] = 'Αναφορές χρέωσης';
+$lang['AdminReportsCustomize.index.customize'] = 'Προσαρμογή';
+$lang['AdminReportsCustomize.index.reports'] = 'Δημιουργία';
+$lang['AdminReportsCustomize.index.box_title'] = 'Αναφορές';
+$lang['AdminReportsCustomize.!success.deleted'] = 'Η αναφορά διαγράφηκε επιτυχώς.';
+$lang['AdminReportsCustomize.!success.edited'] = 'Η έκθεση ενημερώθηκε επιτυχώς.';
+$lang['AdminReportsCustomize.!success.added'] = 'Η έκθεση προστέθηκε επιτυχώς.';
+
