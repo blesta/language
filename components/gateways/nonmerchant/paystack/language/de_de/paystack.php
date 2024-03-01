068b33f4b3a03c52d1d7e4a6004c7fe8ba37e06a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:12 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/paystack/language/de_de/paystack.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Paystack
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Paystack.buildprocess.submit'] = 'Bezahlen mit Paystack';
+$lang['Paystack.tooltip.secret_key'] = 'Um den Testmodus zu nutzen, geben Sie einfach einen Testschlüssel ein.';
+$lang['Paystack.secret_key'] = 'Paystack Geheimschlüssel';
+$lang['Paystack.description'] = 'Paystack ist der schnellste und einfachste Weg, um Online-Zahlungen in Nigeria zu akzeptieren.';
+$lang['Paystack.name'] = 'Paystack Standard';
+$lang['Paystack.!error.secret_key.valid'] = 'Der geheime Schlüssel muss mindestens 40 Zeichen lang sein.';
+
