ba7001bc9fe57df0d574ca6a4587bb2cd9d6415c
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:54 -0800

    Updated cs_cz language files

---
+++ plugins/mass_mailer/language/cs_cz/mass_mailer_emails.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Mass Mailer Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerEmails.!error.log.valid'] = 'Možnost log musí být nastavena na hodnotu 0 nebo 1.';
+$lang['MassMailerEmails.!error.html.empty'] = 'Zadejte obsah e-mailu ve formátu HTML.';
+$lang['MassMailerEmails.!error.subject.empty'] = 'Zadejte prosím předmět.';
+$lang['MassMailerEmails.!error.from_address.valid'] = 'Zadejte prosím platnou e-mailovou adresu.';
+$lang['MassMailerEmails.!error.from_name.empty'] = 'Zadejte prosím název od.';
+$lang['MassMailerEmails.!error.job_id.unique'] = 'K úloze je již přiřazen e-mail.';
+$lang['MassMailerEmails.!error.job_id.valid'] = 'Neplatné ID úlohy.';
+
