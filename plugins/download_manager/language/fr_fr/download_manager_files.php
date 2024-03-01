3a742a3f33947e7aede743dbae0f15b53ef638b4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:58 -0800

    Updated fr_fr language files

---
+++ plugins/download_manager/language/fr_fr/download_manager_files.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Download Manager Files
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerFiles.!error.category_id.exists'] = 'Veuillez sélectionner une catégorie valide.';
+$lang['DownloadManagerFiles.!error.company_id.exists'] = 'ID d\'entreprise non valide.';
+$lang['DownloadManagerFiles.!error.name.empty'] = 'Veuillez saisir un nom pour ce fichier.';
+$lang['DownloadManagerFiles.!error.file_name.format'] = 'Veuillez télécharger un fichier ou entrer un chemin d\'accès valide au fichier.';
+$lang['DownloadManagerFiles.!error.public.format'] = 'Le format de l\'option de disponibilité publique n\'est pas valide.';
+$lang['DownloadManagerFiles.!error.public.assignment'] = 'Le dossier doit être mis à la disposition du public, des groupes de clients ou des paquets.';
+$lang['DownloadManagerFiles.!error.permit_client_groups.format'] = 'Le format de l\'option de disponibilité du groupe de clients n\'est pas valide.';
+$lang['DownloadManagerFiles.!error.file_groups.format'] = 'Au moins un groupe de clients sélectionné ne peut être ajouté car il n\'existe pas.';
+$lang['DownloadManagerFiles.!error.permit_packages.format'] = 'Le format de l\'option de disponibilité du paquet n\'est pas valide.';
+$lang['DownloadManagerFiles.!error.file_packages.format'] = 'Au moins un paquet sélectionné ne peut être ajouté car il n\'existe pas.';
+$lang['DownloadManagerFiles.!error.file_id.exists'] = 'Le fichier en cours de mise à jour n\'existe pas.';
+
