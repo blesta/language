c55001fb6be19a840827ea5f47b0df70243e90e5
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:30 -0800

    Updated de_de language files

---
+++ language/de_de/admin_login.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Admin Login
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminLogin.!info.otp'] = 'Für diesen Benutzer ist eine Zwei-Faktoren-Authentifizierung erforderlich. Bitte geben Sie unten Ihr OTP (One-Time Password) ein.';
+$lang['AdminLogin.!info.reset_password'] = 'Geben Sie unten Ihren Benutzernamen ein. Sie erhalten dann per E-Mail einen Link, mit dem Sie ein neues Passwort festlegen können.';
+$lang['AdminLogin.!success.reset_sent'] = 'Eine Bestätigungs-E-Mail wurde an die angegebene Adresse gesandt.';
+$lang['AdminLogin.!error.captcha.invalid'] = 'Das eingegebene Captcha war ungültig. Bitte versuchen Sie es erneut.';
+$lang['AdminLogin.!error.unknown_user'] = 'Der Benutzername wird nicht erkannt oder das Passwort kann nicht zurückgesetzt werden.';
+$lang['AdminLogin.setup.field_submit'] = 'Oberfläche';
+$lang['AdminLogin.setup.field_enter_key_false'] = 'Ich möchte eine 30-tägige kostenlose Testphase starten';
+$lang['AdminLogin.setup.field_enter_key_true'] = 'Ich muss einen Lizenzschlüssel eingeben';
+$lang['AdminLogin.setup.field_confirm_password'] = 'Bestätigen Sie Ihr Passwort';
+$lang['AdminLogin.setup.field_password'] = 'Passwort';
+$lang['AdminLogin.setup.field_email'] = 'E-Mail Adresse';
+$lang['AdminLogin.setup.field_username'] = 'Benutzername';
+$lang['AdminLogin.setup.field_last_name'] = 'Nachname';
+$lang['AdminLogin.setup.field_first_name'] = 'Vorname';
+$lang['AdminLogin.setup.heading_create_account'] = 'Erstellen Sie Ihr Personalkonto';
+$lang['AdminLogin.setup.field_newsletter'] = 'Melden Sie sich für unseren Newsletter an. Sie können sich jederzeit abmelden.';
+$lang['AdminLogin.setup.trial_newsletter'] = 'Wenn Sie sich für eine Testversion anmelden, erklären Sie sich damit einverstanden, während Ihrer Testphase E-Mails von uns zu erhalten. Sie können sich jederzeit abmelden.';
+$lang['AdminLogin.setup.field_license_key'] = 'Lizenzschlüssel';
+$lang['AdminLogin.setup.title_adminarea'] = 'Erstmalige Einrichtung';
+$lang['AdminLogin.setup.page_title'] = 'Erstmalige Einrichtung';
+$lang['AdminLogin.confirmreset.link_login'] = 'Abbrechen, Anmelden';
+$lang['AdminLogin.confirmreset.field_resetsubmit'] = 'Passwort festlegen';
+$lang['AdminLogin.confirmreset.field_confirm_password'] = 'Neues Passwort bestätigen';
+$lang['AdminLogin.confirmreset.field_new_password'] = 'Neues Passwort';
+$lang['AdminLogin.confirmreset.title_adminarea'] = '%1$s | Passwort zurücksetzen bestätigen';
+$lang['AdminLogin.confirmreset.page_title'] = 'Bestätigen Sie das Zurücksetzen des Passworts';
+$lang['AdminLogin.reset.link_login'] = 'Abbrechen, Anmelden';
+$lang['AdminLogin.reset.field_resetsubmit'] = 'Passwort zurücksetzen';
+$lang['AdminLogin.reset.field_username'] = 'Benutzername';
+$lang['AdminLogin.reset.title_adminarea'] = '%1$s | Passwort zurücksetzen';
+$lang['AdminLogin.reset.page_title'] = 'Passwort zurücksetzen';
+$lang['AdminLogin.otp.link_login'] = 'Abbrechen, Anmelden';
+$lang['AdminLogin.otp.field_loginsubmit'] = 'Einloggen';
+$lang['AdminLogin.otp.field_password'] = 'Einmaliges Passwort';
+$lang['AdminLogin.otp.title_adminarea'] = '%1$s | Personalbereich';
+$lang['AdminLogin.otp.page_title'] = 'OTP-Anmeldung';
+$lang['AdminLogin.index.link_resetpassword'] = 'Mein Passwort zurücksetzen';
+$lang['AdminLogin.index.field_loginsubmit'] = 'Einloggen';
+$lang['AdminLogin.index.field_rememberme'] = 'Erinnern Sie sich an mich auf diesem Computer.';
+$lang['AdminLogin.index.field_password'] = 'Passwort';
+$lang['AdminLogin.index.field_username'] = 'Benutzername';
+$lang['AdminLogin.index.title_adminarea'] = '%1$s | Personalbereich';
+$lang['AdminLogin.index.page_title'] = 'Einloggen';
+
