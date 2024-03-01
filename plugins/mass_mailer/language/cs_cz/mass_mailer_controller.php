55cd9f2c871da0844a0dcfa202b444bee2957c5f
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:54 -0800

    Updated cs_cz language files

---
+++ plugins/mass_mailer/language/cs_cz/mass_mailer_controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Mass Mailer Controller
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerController.!success.export_job_added'] = 'Export byl úspěšně zařazen do fronty. Na stránce s výpisem bude k dispozici, jakmile bude zpracován prostřednictvím cronu.';
+$lang['MassMailerController.!success.mail_job_added'] = 'Poštovní úloha byla úspěšně zařazena do fronty. E-maily budou odesílány prostřednictvím cronu.';
+
