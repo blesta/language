e7125169abbb905fa30686c6b4ad941ba3acad2e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:30 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/payouts.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Payouts
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payouts.!success.payout_requested'] = 'Ζητήθηκε η πληρωμή σας.';
+$lang['Payouts.!error.exceeded_payout_requests'] = 'Έχετε υπερβεί το μέγιστο όριο για εκκρεμή αιτήματα πληρωμής.';
+$lang['Payouts.!error.insufficient_funds_payout_request'] = 'Δεν υπάρχουν επαρκή κεφάλαια για την επεξεργασία του αιτήματος πληρωμής.';
+$lang['Payouts.!error.minimum_payout_not_available'] = 'Τα διαθέσιμα κεφάλαια δεν καλύπτουν το ελάχιστο ποσό για πληρωμές.';
+$lang['Payouts.index.boxtitle_payouts'] = 'Πληρωμές';
+$lang['Payouts.index.category_approved'] = 'Εγκεκριμένο';
+$lang['Payouts.index.category_pending'] = 'Εκκρεμεί';
+$lang['Payouts.index.category_declined'] = 'Απορρίφθηκε';
+$lang['Payouts.index.categorylink_request_payout'] = 'Αίτημα πληρωμής';
+$lang['Payouts.index.no_results'] = 'Προς το παρόν δεν υπάρχουν πληρωμές με αυτό το καθεστώς.';
+$lang['Payouts.index.heading_id'] = 'ID πληρωμής';
+$lang['Payouts.index.heading_requested_amount'] = 'Αιτούμενο ποσό';
+$lang['Payouts.index.heading_requested_currency'] = 'Ζητούμενο νόμισμα';
+$lang['Payouts.index.heading_payment_method'] = 'Μέθοδος πληρωμής';
+$lang['Payouts.index.heading_date_requested'] = 'Ημερομηνία αίτησης';
+$lang['Payouts.add.boxtitle_request_payout'] = 'Αίτημα πληρωμής';
+$lang['Payouts.add.title_payout_request'] = 'Αίτημα πληρωμής';
+$lang['Payouts.add.field_requested_amount'] = 'Ποσό ανάληψης';
+$lang['Payouts.add.field_requested_currency'] = 'Ζητούμενο νόμισμα';
+$lang['Payouts.add.field_payment_method'] = 'Μέθοδος πληρωμής';
+$lang['Payouts.add.min_withdrawal_amount'] = 'Ελάχιστο ποσό πληρωμής';
+$lang['Payouts.add.max_withdrawal_amount'] = 'Μέγιστο ποσό πληρωμής';
+$lang['Payouts.add.available_payout'] = 'Διαθέσιμο για πληρωμή';
+$lang['Payouts.add.request_payout'] = 'Αίτημα πληρωμής';
+
