5adf24c4f6e7f2de850c734e313f89ec9b20839d
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:31 -0800

    Updated it_it language files

---
+++ plugins/client_documents/language/it_it/client_documents_files.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Client Documents Files
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientDocumentsFiles.!error.date_added.valid'] = 'È necessario aggiungere una data valida.';
+$lang['ClientDocumentsFiles.!error.name.valid'] = 'È richiesto il nome del documento.';
+$lang['ClientDocumentsFiles.!error.client_id.exists'] = 'Il client non esiste.';
+
