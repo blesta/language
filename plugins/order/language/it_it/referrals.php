2b33bdda13b40113f444bd1fb113de1688c2f9d3
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:20 -0800

    Updated it_it language files

---
+++ plugins/order/language/it_it/referrals.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Referrals
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Referrals.index.heading_date_added'] = 'Data aggiunta';
+$lang['Referrals.index.heading_commission'] = 'Commissione';
+$lang['Referrals.index.heading_currency'] = 'Valuta';
+$lang['Referrals.index.heading_amount'] = 'Importo';
+$lang['Referrals.index.heading_name'] = 'Nome';
+$lang['Referrals.index.heading_order'] = 'Ordine #';
+$lang['Referrals.index.no_results'] = 'Al momento non ci sono referral con questo status.';
+$lang['Referrals.index.category_canceled'] = 'Annullato';
+$lang['Referrals.index.category_mature'] = 'Maturo';
+$lang['Referrals.index.category_pending'] = 'In attesa';
+$lang['Referrals.index.boxtitle_referrals'] = 'Referral';
+
