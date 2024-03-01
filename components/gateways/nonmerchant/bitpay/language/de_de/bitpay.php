ff035fe35d4f581633fafb2eb782c8908b8c9cdf
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:03 -0800

    Updated de_de language files

---
+++ components/gateways/nonmerchant/bitpay/language/de_de/bitpay.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Bitpay
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Bitpay.!error.payment.expired'] = 'Die Transaktion ist abgelaufen und konnte nicht bearbeitet werden.';
+$lang['Bitpay.!error.payment.invalid'] = 'Die Transaktion ist ungültig und konnte nicht verarbeitet werden.';
+$lang['Bitpay.!error.failed.response'] = 'Die Transaktion konnte nicht verarbeitet werden.';
+$lang['Bitpay.!error.token.valid'] = 'Der Token ist ungültig oder der Pairing-Code wurde nicht in BitPay autorisiert.';
+$lang['Bitpay.!error.transaction_speed.valid'] = 'Bitte wählen Sie eine gültige Transaktionsgeschwindigkeit.';
+$lang['Bitpay.buildprocess.submit'] = 'Mit Bitcoin bezahlen';
+$lang['Bitpay.disconnect.button'] = 'Trennen Sie die Verbindung zu BitPay';
+$lang['Bitpay.connect.button'] = 'Mit BitPay verbinden';
+$lang['Bitpay.test_mode_note'] = 'Wenn diese Option aktiviert ist, sendet das Gateway Zahlungsanforderungen an die Test-URL. Wenn Sie dies ändern, wird die Verbindung zu BitPay unterbrochen und ein neues Token wird generiert.';
+$lang['Bitpay.test_mode'] = 'Test-Modus';
+$lang['Bitpay.transaction_speed_note'] = 'Die Transaktionsgeschwindigkeit bestimmt, wie schnell eine Rechnung (in Bitpay) als "bestätigt" angesehen werden kann.';
+$lang['Bitpay.pairing_code_note'] = 'Gehen Sie im BitPay-Dashboard in den Bereich "Payment Tools > API Tokens" und bestätigen Sie das generierte Token mit diesem Pairing-Code "%1$s", dann speichern Sie die Gateway-Einstellungen durch Klicken auf "Update Settings".';
+$lang['Bitpay.transaction.speed.low'] = 'Niedrig';
+$lang['Bitpay.transaction.speed.medium'] = 'Mittel';
+$lang['Bitpay.transaction.speed.high'] = 'Hoch';
+$lang['Bitpay.transaction_speed'] = 'Transaktionsgeschwindigkeit';
+$lang['Bitpay.pairing_code'] = 'Pairing-Code';
+$lang['Bitpay.description'] = 'Ein Bitcoin- und Kryptowährungs-Zahlungsdienstleister';
+$lang['Bitpay.name'] = 'Bitpay';
+
