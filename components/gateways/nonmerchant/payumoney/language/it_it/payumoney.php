6c6530ab0f2e70b458d9454a1ae657953bdff0ac
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:22 -0800

    Updated it_it language files

---
+++ components/gateways/nonmerchant/payumoney/language/it_it/payumoney.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Payumoney
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payumoney.!error.hash.valid'] = 'Hash non valido.';
+$lang['Payumoney.!error.invalid_token.response'] = 'Payumoney ha risposto con un token non valido.';
+$lang['Payumoney.!error.key.valid'] = 'Chiave commerciale non valida.';
+$lang['Payumoney.!error.auth_header.valid'] = 'Inserire un\'intestazione di autorizzazione valida.';
+$lang['Payumoney.!error.merchant_salt.valid'] = 'Inserire un sale valido.';
+$lang['Payumoney.!error.merchant_key.valid'] = 'Inserire una merchant key valida.';
+$lang['Payumoney.buildprocess.submit'] = 'Pagare con Payumoney';
+$lang['Payumoney.test_mode_note'] = 'Abilitando questa opzione si utilizzerà la sandbox di Payumoney. Attivare questa opzione solo se si sta effettuando un test con un conto Payumoney sandbox.';
+$lang['Payumoney.test_mode'] = 'Modalità Sandbox';
+$lang['Payumoney.merchant_salt_note'] = 'Il Merchant Salt si trova nella dashboard di Payumoney, alla voce Gestisci account -> Il mio account -> Merchant Key-Salt.';
+$lang['Payumoney.merchant_key_note'] = 'La Merchant Key si trova nella dashboard di Payumoney, alla voce Gestione account -> Il mio account -> Merchant Key-Salt.';
+$lang['Payumoney.auth_header_note'] = 'L\'intestazione dell\'autorizzazione si trova nella dashboard di Payumoney alla voce Gestione account -> Il mio account -> Merchant Key-Salt.';
+$lang['Payumoney.auth_header'] = 'Intestazione di autorizzazione';
+$lang['Payumoney.merchant_salt'] = 'Sale mercantile';
+$lang['Payumoney.merchant_key'] = 'Chiave del commerciante';
+$lang['Payumoney.description'] = 'In India, PayU serve più di 4.50.000+ commercianti con oltre 100 metodi di pagamento ed è il partner di pagamento preferito da quasi il 60% dei commercianti di e-commerce.';
+$lang['Payumoney.name'] = 'Payumoney';
+
