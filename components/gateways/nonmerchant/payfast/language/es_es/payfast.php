5cca019f5c07a9dce4f44f26d42bf4f99c13b538
pmphillips <paul@phillipsdata.com>
2024-03-01 14:41:34 -0800

    Updated es_es language files

---
+++ components/gateways/nonmerchant/payfast/language/es_es/payfast.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Payfast
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payfast.name'] = 'PayFast';
+$lang['Payfast.description'] = 'Disfrute de pagos en línea rápidos y seguros en Sudáfrica desde casi cualquier lugar del mundo.';
+$lang['Payfast.!error.merchant_id.valid'] = 'ID de comerciante inválido.';
+$lang['Payfast.!error.merchant_key.valid'] = 'Clave de comerciante inválida.';
+$lang['Payfast.!error.sandbox.valid'] = 'Valor no válido.';
+$lang['Payfast.meta.merchant_id'] = 'ID de comerciante';
+$lang['Payfast.meta.merchant_key'] = 'Clave de comerciante';
+$lang['Payfast.meta.passphrase'] = 'Frase de contraseña';
+$lang['Payfast.meta.sandbox'] = 'Cajón de arena';
+$lang['Payfast.buildprocess.submit'] = 'Pague con PayFast';
+
