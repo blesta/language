325f4602a411803b329fa671d1803e99fb78d8d3
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:19 -0800

    Updated cs_cz language files

---
+++ components/modules/cwatch/language/cs_cz/cwatch.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Cwatch
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CWatch.getsitestatuses.ssl_fail'] = 'Web přidán, instalace SSL selhala';
+$lang['CWatch.getsitestatuses.ssl_completed'] = 'Přidán certifikát SSL';
+$lang['CWatch.getsitestatuses.ssl_retry'] = 'Opakování instalace protokolu SSL';
+$lang['CWatch.getsitestatuses.ssl_inprogress'] = 'Probíhá instalace SSL';
+$lang['CWatch.getsitestatuses.dns_failed'] = 'Stránka přidána, skenování DNS selhalo';
+$lang['CWatch.getsitestatuses.dns_completed'] = 'Kontrola DNS dokončena';
+$lang['CWatch.getsitestatuses.dns_retry'] = 'Opakování skenování DNS';
+$lang['CWatch.getsitestatuses.dns_inprogress'] = 'Probíhající skenování DNS';
+$lang['CWatch.getsitestatuses.site_failed'] = 'Zajištění stránek se nezdařilo';
+$lang['CWatch.getsitestatuses.site_completed'] = 'Přidání stránky';
+$lang['CWatch.getsitestatuses.site_retry'] = 'Opakované poskytování stránek';
+$lang['CWatch.getsitestatuses.site_inprogress'] = 'Probíhající poskytování stránek';
+$lang['CWatch.getsitestatuses.waiting'] = 'Čekání';
+$lang['CWatch.tab_malware.submit'] = 'Odeslat';
+$lang['CWatch.tab_malware.path'] = 'Adresář stránek';
+$lang['CWatch.tab_malware.sftp'] = 'Použití protokolu SFTP';
+$lang['CWatch.tab_malware.port'] = 'Port FTP';
+$lang['CWatch.tab_malware.host'] = 'Název hostitele FTP';
+$lang['CWatch.tab_malware.ftpusername'] = 'Uživatelské jméno FTP';
+$lang['CWatch.tab_malware.ftppassword'] = 'Heslo FTP';
+$lang['CWatch.tab_malware.domainname'] = 'Název domény';
+$lang['CWatch.tab_malware.test_credentials'] = 'Otestujte tato pověření';
+$lang['CWatch.tab_malware.add_scanner'] = 'Přidat skener';
+$lang['CWatch.tab_malware.malware'] = 'Kontrola malwaru';
+$lang['CWatch.tab_licenses.submit'] = 'Odeslat';
+$lang['CWatch.tab_licenses.autoSsl'] = 'AutoSSL';
+$lang['CWatch.tab_licenses.initiateDns'] = 'Počáteční DNS';
+$lang['CWatch.tab_licenses.license_name'] = '%1$s (%2$s)';
+$lang['CWatch.tab_licenses.current_license'] = 'Aktuální licence';
+$lang['CWatch.tab_licenses.cancel'] = 'Zrušit';
+$lang['CWatch.tab_licenses.confirm_deactivate'] = 'Opravdu chcete tuto licenci deaktivovat?';
+$lang['CWatch.tab_licenses.confirm_delete'] = 'Opravdu chcete tuto doménu odebrat?';
+$lang['CWatch.tab_licenses.upgrade_site'] = 'Upgrade webu';
+$lang['CWatch.tab_licenses.add_site'] = 'Přidat stránku';
+$lang['CWatch.tab_licenses.remove_site'] = 'Odstranění webu';
+$lang['CWatch.tab_licenses.deactivate_license'] = 'Deaktivace licence';
+$lang['CWatch.tab_licenses.not_applicable'] = 'NEUPLATŇUJE SE';
+$lang['CWatch.tab_licenses.upgrade_delay'] = 'Aktualizace se může projevit až po určité době, takže se stále může zobrazovat starý typ licence.';
+$lang['CWatch.tab_licenses.no_results'] = 'Pro tuto službu nebyly poskytnuty žádné licence.';
+$lang['CWatch.tab_licenses.no_upgrade_options'] = 'K dispozici nejsou žádné licence pro upgrade.';
+$lang['CWatch.tab_licenses.options'] = 'Možnosti';
+$lang['CWatch.tab_licenses.malware_scanner'] = 'Skener';
+$lang['CWatch.tab_licenses.status'] = 'Stav domény';
+$lang['CWatch.tab_licenses.domain'] = 'Doména';
+$lang['CWatch.tab_licenses.type'] = 'Typ';
+$lang['CWatch.tab_licenses.licenseKey'] = 'Licenční klíč';
+$lang['CWatch.tab_licenses.add'] = 'Přidání nového webu';
+$lang['CWatch.tab_licenses.inactive_licenses'] = 'Neaktivní licence';
+$lang['CWatch.tab_licenses.licenses'] = 'Správa licencí';
+$lang['CWatch.service_info.option_login'] = 'Přihlášení k aplikaci cWatch';
+$lang['CWatch.edit_row.add_btn'] = 'Upravit pověření';
+$lang['CWatch.edit_row.field_sandbox'] = 'Povolení SandBoxu';
+$lang['CWatch.edit_row.field_password'] = 'Heslo';
+$lang['CWatch.edit_row.field_username'] = 'Uživatelské jméno';
+$lang['CWatch.edit_row.basic_title'] = 'Uživatelská nastavení';
+$lang['CWatch.edit_row.box_title'] = 'Upravit účet API';
+$lang['CWatch.add_row.add_btn'] = 'Přidat účet';
+$lang['CWatch.add_row.field_sandbox'] = 'Povolení SandBoxu';
+$lang['CWatch.add_row.field_password'] = 'Heslo';
+$lang['CWatch.add_row.field_username'] = 'Uživatelské jméno';
+$lang['CWatch.add_row.basic_title'] = 'Uživatelská nastavení';
+$lang['CWatch.add_row.box_title'] = 'Přidání účtu API';
+$lang['Cwatch.service_field.country'] = 'Země';
+$lang['Cwatch.service_field.lastname'] = 'Příjmení';
+$lang['Cwatch.service_field.firstname'] = 'Jméno';
+$lang['Cwatch.service_field.email'] = 'E-mail';
+$lang['Cwatch.service_field.domain'] = 'Doména';
+$lang['CWatch.packagetypes.multi_license'] = 'Multilicence';
+$lang['CWatch.packagetypes.single_license'] = 'Jednotlivá licence';
+$lang['CWatch.package_fields.license_premium_free_60d'] = 'Rozšířená zkušební verze Premium';
+$lang['CWatch.package_fields.license_premium_free'] = 'Zkušební verze Premium';
+$lang['CWatch.package_fields.license_premium_paid_with_trial'] = 'Prémium placené se zkušební verzí';
+$lang['CWatch.package_fields.license_premium'] = 'Premium';
+$lang['CWatch.package_fields.license_pro_free_60d'] = 'Rozšířená odborná zkouška';
+$lang['CWatch.package_fields.license_pro_free'] = 'Profesionální zkouška';
+$lang['CWatch.package_fields.license_pro_paid_with_trial'] = 'Profesionální placený se zkušební verzí';
+$lang['CWatch.package_fields.license_pro'] = 'Profesionální';
+$lang['CWatch.package_fields.license_waf_starter'] = 'Startér Waf';
+$lang['CWatch.package_fields.license_starter_paid_with_trial'] = 'Starter placený se zkušební verzí';
+$lang['CWatch.package_fields.license_starter'] = 'Startér';
+$lang['CWatch.package_fields.license_basic_detection'] = 'Základní';
+$lang['CWatch.package_fields.license_type'] = 'Typ licence';
+$lang['CWatch.package_fields.tooltip.package_type'] = 'Balíčky s jednou licencí poskytují v programu cWatch pouze jednu licenci typu, který je zde u balíčku vybrán.  Balíčky více licencí využívají konfigurovatelné možnosti, které zákazníkům umožňují objednat různý počet licencí různých typů.';
+$lang['CWatch.package_fields.available_terms'] = 'Dostupné podmínky';
+$lang['CWatch.package_fields.package_type'] = 'Typ balení';
+$lang['CWatch.manage.heading_options'] = 'Možnosti';
+$lang['CWatch.manage.heading_username'] = 'Uživatelské jméno';
+$lang['CWatch.manage.no_results'] = 'Nebyly nalezeny žádné účty';
+$lang['CWatch.manage.confirm_delete'] = 'Opravdu chcete tohoto uživatele odstranit?';
+$lang['CWatch.manage.delete'] = 'Odstranit';
+$lang['CWatch.manage.option_edit'] = 'Upravit';
+$lang['CWatch.manage.add_module_row'] = 'Přidání účtu API';
+$lang['CWatch.manage.title'] = 'Účty API';
+$lang['CWatch.manage.boxtitle'] = 'Správa aplikace CWatch';
+$lang['CWatch.!error.ftp_test'] = 'Připojení FTP se nezdařilo! Zkontrolujte prosím nastavení a zkuste to znovu.';
+$lang['CWatch.!error.sftp_test'] = 'Připojení SFTP se nezdařilo! Zkontrolujte prosím nastavení a zkuste to znovu.';
+$lang['CWatch.!error.api.internal'] = 'Došlo k interní chybě nebo server na požadavek neodpověděl.';
+$lang['CWatch.!error.limit_exceeded'] = 'Počet licencí aktuálně přiřazených doménám překračuje zadaný limit.  Před snížením limitu uvolněte licence odebráním domén.';
+$lang['CWatch.!error.cwatch_domain.unique'] = 'Doména je již zaregistrována. Vyberte prosím jinou doménu.';
+$lang['CWatch.!error.cwatch_domain.format'] = 'Zadejte platný název domény, např. domain.com.';
+$lang['CWatch.!error.cwatch_country.length'] = 'Neplatný okres.';
+$lang['CWatch.!error.cwatch_lastname.empty'] = 'Zadejte příjmení.';
+$lang['CWatch.!error.cwatch_firstname.empty'] = 'Zadejte křestní jméno.';
+$lang['CWatch.!error.cwatch_email.unique'] = 'Zákazník s daným e-mailem již existuje.';
+$lang['CWatch.!error.cwatch_email.format'] = 'Nesprávný formát e-mailu.';
+$lang['CWatch.!error.password.valid_connection'] = 'Nelze navázat spojení pomocí zadaných pověření.';
+$lang['CWatch.!error.password.valid'] = 'Zadejte prosím heslo.';
+$lang['CWatch.!error.username.valid'] = 'Zadejte prosím uživatelské jméno.';
+$lang['CWatch.!tooltip.autoSsl'] = 'Zda nainstalovat bezplatný certifikát SSL společnosti Comodo.';
+$lang['CWatch.!tooltip.initiateDns'] = 'Zda se má spustit kontrola záznamů DNS.';
+$lang['CWatch.!success.ftp_test'] = 'Připojení FTP proběhlo úspěšně!';
+$lang['CWatch.!success.sftp_test'] = 'Připojení SFTP proběhlo úspěšně!';
+$lang['CWatch.module_row_plural.name'] = 'Účty API';
+$lang['CWatch.module_row.name'] = 'Účet API';
+$lang['CWatch.description'] = 'Nástroj na ochranu vašich webových stránek, webových serverů a webových aplikací před stále sofistikovanějšími hrozbami hackerů.';
+$lang['CWatch.name'] = 'CWatch';
+
