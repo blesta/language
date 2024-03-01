d01d16e189363060c36bf74439da6c1b53af298a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:41 -0800

    Updated el_gr language files

---
+++ components/modules/apnscp/language/el_gr/apnscp.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Apnscp
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Apnscp.!error.apnscp_password.matches'] = 'Ο κωδικός πρόσβασης και ο κωδικός επιβεβαίωσης δεν ταιριάζουν.';
+$lang['Apnscp.!error.apnscp_password.valid'] = 'Ο κωδικός πρόσβασης πρέπει να έχει μήκος τουλάχιστον 8 χαρακτήρων.';
+$lang['Apnscp.!error.apnscp_username.length'] = 'Το όνομα χρήστη πρέπει να έχει μήκος μεταξύ 1 και 16 χαρακτήρων.';
+$lang['Apnscp.!error.apnscp_username.test'] = 'Το όνομα χρήστη δεν πρέπει να αρχίζει με "test".';
+$lang['Apnscp.!error.apnscp_username.format'] = 'Το όνομα χρήστη μπορεί να περιέχει μόνο γράμματα και αριθμούς και δεν πρέπει να αρχίζει με αριθμό.';
+$lang['Apnscp.!error.apnscp_domain.test'] = 'Το όνομα τομέα δεν μπορεί να αρχίζει με \'test\'.';
+$lang['Apnscp.!error.apnscp_domain.format'] = 'Παρακαλούμε εισάγετε ένα έγκυρο όνομα τομέα, π.χ. domain.com.';
+$lang['Apnscp.!error.module_row.missing'] = 'Εμφανίστηκε ένα εσωτερικό σφάλμα. Η σειρά της μονάδας δεν είναι διαθέσιμη.';
+$lang['Apnscp.!error.api.internal'] = 'Προέκυψε ένα εσωτερικό σφάλμα ή ο διακομιστής δεν απάντησε στην αίτηση.';
+$lang['Apnscp.!error.meta[package].empty'] = 'Απαιτείται ένα πακέτο ApisCP.';
+$lang['Apnscp.!error.name_servers_count'] = 'Πρέπει να ορίσετε τουλάχιστον 2 διακομιστές ονομάτων.';
+$lang['Apnscp.!error.name_servers_valid'] = 'Ένας ή περισσότεροι από τους καταχωρημένους διακομιστές ονομάτων είναι άκυροι.';
+$lang['Apnscp.!error.account_limit_valid'] = 'Το Όριο λογαριασμού πρέπει να παραμείνει κενό (για απεριόριστους λογαριασμούς) ή να οριστεί σε κάποια ακέραια τιμή.';
+$lang['Apnscp.!error.api_key_valid_connection'] = 'Δεν κατέστη δυνατή η σύνδεση με το διακομιστή. Ελέγξτε αν το όνομα κεντρικού υπολογιστή, το κλειδί API και η θύρα είναι σωστά.';
+$lang['Apnscp.!error.api_key_valid'] = 'Το κλειδί API φαίνεται να είναι άκυρο.';
+$lang['Apnscp.!error.port_format'] = 'Η θύρα πρέπει να είναι αριθμός.';
+$lang['Apnscp.!error.host_name_valid'] = 'Το όνομα κεντρικού υπολογιστή φαίνεται να είναι άκυρο.';
+$lang['Apnscp.!error.server_name_valid'] = 'Πρέπει να εισαγάγετε μια ετικέτα διακομιστή.';
+$lang['Apnscp.service_field.tooltip.password'] = 'Μπορείτε να αφήσετε τον κωδικό πρόσβασης κενό για να δημιουργηθεί αυτόματα.';
+$lang['Apnscp.service_field.tooltip.username'] = 'Μπορείτε να αφήσετε το όνομα χρήστη κενό για να δημιουργηθεί αυτόματα.';
+$lang['Apnscp.service_info.option_login'] = 'Συνδεθείτε';
+$lang['Apnscp.service_info.options'] = 'Επιλογές';
+$lang['Apnscp.service_info.server'] = 'Διακομιστής';
+$lang['Apnscp.service_info.password'] = 'Κωδικός πρόσβασης';
+$lang['Apnscp.service_info.username'] = 'Όνομα χρήστη';
+$lang['Apnscp.tab_client_stats.usage_unlimited'] = '(%1$s MB/∞)';
+$lang['Apnscp.tab_client_stats.usage'] = '(%1$s MB/%2$s MB)';
+$lang['Apnscp.tab_client_stats.disk_title'] = 'Χρήση δίσκου';
+$lang['Apnscp.tab_client_stats.bandwidth_title'] = 'Χρήση εύρους ζώνης (μήνας μέχρι σήμερα)';
+$lang['Apnscp.tab_client_stats.info_title'] = 'Πληροφορίες';
+$lang['Apnscp.tab_client_actions.field_password_submit'] = 'Ενημέρωση κωδικού πρόσβασης';
+$lang['Apnscp.tab_client_actions.field_apnscp_password'] = 'Κωδικός πρόσβασης';
+$lang['Apnscp.tab_client_actions.change_password'] = 'Αλλαγή κωδικού πρόσβασης';
+$lang['Apnscp.tab_stats.info.bandwidth'] = 'Ποσοστό εύρους ζώνης';
+$lang['Apnscp.tab_stats.info.disk'] = 'Ποσοστό δίσκου';
+$lang['Apnscp.tab_stats.info_heading.value'] = 'Αξία';
+$lang['Apnscp.tab_stats.info_heading.field'] = 'Πεδίο';
+$lang['Apnscp.tab_stats.info_title'] = 'Πληροφορίες';
+$lang['Apnscp.service_field.password'] = 'Κωδικός πρόσβασης';
+$lang['Apnscp.service_field.username'] = 'Όνομα χρήστη';
+$lang['Apnscp.service_field.domain'] = 'Τομέας';
+$lang['Apnscp.package_fields.package'] = 'Πακέτο ApisCP';
+$lang['Apnscp.row_meta.account_limit'] = 'Όριο λογαριασμού';
+$lang['Apnscp.row_meta.use_ssl'] = 'Χρησιμοποιήστε SSL κατά τη σύνδεση στο API (συνιστάται)';
+$lang['Apnscp.row_meta.api_key'] = 'Κλειδί API';
+$lang['Apnscp.row_meta.default_port'] = '2083';
+$lang['Apnscp.row_meta.port'] = 'Λιμάνι';
+$lang['Apnscp.row_meta.host_name'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Apnscp.row_meta.server_name'] = 'Ετικέτα διακομιστή';
+$lang['Apnscp.edit_row.add_btn'] = 'Επεξεργασία διακομιστή';
+$lang['Apnscp.edit_row.remove_name_server'] = 'Αφαιρέστε το';
+$lang['Apnscp.edit_row.name_server'] = 'Διακομιστής ονομάτων %1$s';
+$lang['Apnscp.edit_row.name_server_host_col'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Apnscp.edit_row.name_server_col'] = 'Διακομιστής ονομάτων';
+$lang['Apnscp.edit_row.name_server_btn'] = 'Προσθήκη πρόσθετου διακομιστή ονομάτων';
+$lang['Apnscp.edit_row.notes_title'] = 'Σημειώσεις';
+$lang['Apnscp.edit_row.name_servers_title'] = 'Εξυπηρετητές ονομάτων';
+$lang['Apnscp.edit_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Apnscp.edit_row.box_title'] = 'Επεξεργασία ApisCP Server';
+$lang['Apnscp.add_row.add_btn'] = 'Προσθήκη διακομιστή';
+$lang['Apnscp.add_row.remove_name_server'] = 'Αφαιρέστε το';
+$lang['Apnscp.add_row.name_server'] = 'Διακομιστής ονομάτων %1$s';
+$lang['Apnscp.add_row.name_server_host_col'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Apnscp.add_row.name_server_col'] = 'Διακομιστής ονομάτων';
+$lang['Apnscp.add_row.name_server_btn'] = 'Προσθήκη πρόσθετου διακομιστή ονομάτων';
+$lang['Apnscp.add_row.notes_title'] = 'Σημειώσεις';
+$lang['Apnscp.add_row.name_servers_title'] = 'Εξυπηρετητές ονομάτων';
+$lang['Apnscp.add_row.basic_title'] = 'Βασικές ρυθμίσεις';
+$lang['Apnscp.add_row.box_title'] = 'Προσθήκη διακομιστή ApisCP';
+$lang['Apnscp.order_options.roundrobin'] = 'Ομοιόμορφη κατανομή μεταξύ των διακομιστών';
+$lang['Apnscp.order_options.first'] = 'Πρώτος μη πλήρης διακομιστής';
+$lang['Apnscp.manage.module_groups_no_results'] = 'Δεν υπάρχουν ομάδες διακομιστών.';
+$lang['Apnscp.manage.module_rows_no_results'] = 'Δεν υπάρχουν διακομιστές.';
+$lang['Apnscp.manage.module_groups.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτή την ομάδα διακομιστών;';
+$lang['Apnscp.manage.module_rows.confirm_delete'] = 'Είστε σίγουροι ότι θέλετε να διαγράψετε αυτόν τον διακομιστή;';
+$lang['Apnscp.manage.module_groups.delete'] = 'Διαγραφή';
+$lang['Apnscp.manage.module_rows.delete'] = 'Διαγραφή';
+$lang['Apnscp.manage.module_groups.edit'] = 'Επεξεργασία';
+$lang['Apnscp.manage.module_rows.edit'] = 'Επεξεργασία';
+$lang['Apnscp.manage.module_rows.count'] = '%1$s / %2$s';
+$lang['Apnscp.manage.module_groups_heading.options'] = 'Επιλογές';
+$lang['Apnscp.manage.module_groups_heading.servers'] = 'Αριθμός διακομιστών';
+$lang['Apnscp.manage.module_groups_heading.name'] = 'Όνομα ομάδας';
+$lang['Apnscp.manage.module_rows_heading.options'] = 'Επιλογές';
+$lang['Apnscp.manage.module_rows_heading.accounts'] = 'Λογαριασμοί';
+$lang['Apnscp.manage.module_rows_heading.hostname'] = 'Όνομα κεντρικού υπολογιστή';
+$lang['Apnscp.manage.module_rows_heading.name'] = 'Ετικέτα διακομιστή';
+$lang['Apnscp.manage.module_groups_title'] = 'Ομάδες διακομιστών';
+$lang['Apnscp.manage.module_rows_title'] = 'Servers';
+$lang['Apnscp.add_module_group'] = 'Προσθήκη ομάδας διακομιστών';
+$lang['Apnscp.add_module_row'] = 'Προσθήκη διακομιστή';
+$lang['Apnscp.tab_client_actions'] = 'Δράσεις';
+$lang['Apnscp.tab_client_stats'] = 'Στατιστικά στοιχεία';
+$lang['Apnscp.tab_stats'] = 'Στατιστικά στοιχεία';
+$lang['Apnscp.module_group.name'] = 'Ομάδα διακομιστών';
+$lang['Apnscp.module_row_plural.name'] = 'Servers';
+$lang['Apnscp.module_row.name'] = 'Διακομιστής';
+$lang['Apnscp.description'] = 'Παλαιότερα ονομαζόταν APNSCP.';
+$lang['Apnscp.name'] = 'ApisCP';
+
