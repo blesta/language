dcde3643fac5e9479251a9f071a993b9a55e1a2b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:53:03 -0800

    Updated el_gr language files

---
+++ plugins/system_overview/language/el_gr/system_overview_settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * System Overview Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['SystemOverviewSettings.!error.staff_id.exists'] = 'Μη έγκυρο αναγνωριστικό προσωπικού.';
+$lang['SystemOverviewSettings.!error.company_id.exists'] = 'Μη έγκυρο αναγνωριστικό εταιρείας.';
+$lang['SystemOverviewSettings.!error.settings[][key].empty'] = 'Κανένα πλήκτρο ρύθμισης δεν μπορεί να είναι κενό.';
+$lang['SystemOverviewSettings.!error.settings[][value].length'] = 'Το μήκος κάθε μεμονωμένης τιμής ρύθμισης δεν μπορεί να υπερβαίνει τους 255 χαρακτήρες.';
+
