45de72acc1ffd14ab9a9602564e948a1e92cfdf2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:42:20 -0800

    Updated es_es language files

---
+++ core/Automation/Tasks/Task/language/es_es/deliver_invoices.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Deliver Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.deliver_invoices.attempt'] = 'Intento de entrega de facturas programadas.';
+$lang['Automation.task.deliver_invoices.completed'] = 'La tarea de entrega de facturas ha finalizado.';
+$lang['Automation.task.deliver_invoices.delivery_error'] = 'No se han podido entregar %3$s facturas al cliente #%1$s a través de %2$s debido a un error: %4$s';
+$lang['Automation.task.deliver_invoices.delivery_error_one'] = 'No se ha podido entregar 1 factura al cliente #%1$s a través de %2$s debido a un error: %3$s';
+$lang['Automation.task.deliver_invoices.delivery_success'] = 'Entregadas con éxito %3$s facturas al cliente #%1$s a través de %2$s.';
+$lang['Automation.task.deliver_invoices.delivery_success_one'] = 'Entregada con éxito 1 factura al cliente #%1$s a través de %2$s.';
+$lang['Automation.task.deliver_invoices.none'] = 'No está prevista la entrega de facturas.';
+$lang['Automation.task.deliver_invoices.method_email'] = 'Correo electrónico';
+$lang['Automation.task.deliver_invoices.method_interfax'] = 'InterFax';
+$lang['Automation.task.deliver_invoices.method_postalmethods'] = 'Métodos postales';
+
