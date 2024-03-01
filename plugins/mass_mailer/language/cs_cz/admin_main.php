a501f9be78a0aac72a91f18ebf0e52ce03a1a928
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:53 -0800

    Updated cs_cz language files

---
+++ plugins/mass_mailer/language/cs_cz/admin_main.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Admin Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminMain.index.no_results'] = 'V současné době nejsou k dispozici žádná pracovní místa pro rozesílání.';
+$lang['AdminMain.index.no_export_details'] = 'O vývozu nejsou k dispozici žádné podrobnosti.';
+$lang['AdminMain.index.email_to_recipients'] = '%1$s Příjemce (příjemci)';
+$lang['AdminMain.index.email_subject'] = 'Předmět';
+$lang['AdminMain.index.email_from'] = 'Z';
+$lang['AdminMain.index.email_to'] = 'Na';
+$lang['AdminMain.index.type.export'] = 'Export';
+$lang['AdminMain.index.type.email'] = 'E-mail';
+$lang['AdminMain.index.option_export'] = 'Export';
+$lang['AdminMain.index.heading_options'] = 'Možnosti';
+$lang['AdminMain.index.heading_complete'] = 'Dokončeno';
+$lang['AdminMain.index.heading_status'] = 'Stav';
+$lang['AdminMain.index.heading_type'] = 'Typ';
+$lang['AdminMain.index.heading_date'] = 'Datum přidání';
+$lang['AdminMain.index.job_task_total'] = '%1$s / %2$s';
+$lang['AdminMain.index.categorylink_compose'] = 'Vytvoření nové pošty';
+$lang['AdminMain.index.boxtitle'] = 'Pracovní nabídky pro hromadné rozesílání';
+$lang['AdminMain.index.page_title'] = 'Hromadný rozesílač';
+$lang['AdminMain.!error.export_found'] = 'Export nebyl v souborovém systému nalezen.';
+
