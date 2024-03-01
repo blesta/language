c86459fd0da8c609e55e16bce81d8eb6b71c036b
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:42 -0800

    Updated fr_fr language files

---
+++ language/fr_fr/themes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Themes
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Themes.!error.id.exists'] = 'Le thème que vous avez sélectionné n\'existe pas.';
+$lang['Themes.!error.company_id.exists'] = 'ID d\'entreprise non valide.';
+$lang['Themes.!error.company_id.isset'] = 'Seuls les thèmes propres à l\'entreprise peuvent être supprimés.';
+$lang['Themes.!error.company_id.set'] = 'Seuls les thèmes propres à l\'entreprise peuvent être mis à jour.';
+$lang['Themes.!error.name.empty'] = 'Veuillez saisir un nom de thème.';
+$lang['Themes.!error.name.length'] = 'Le nom du thème ne doit pas dépasser 64 caractères.';
+$lang['Themes.!error.type.format'] = 'Le type de thème donné n\'est pas valide.';
+$lang['Themes.!error.colors.exist'] = 'Veuillez saisir un code hexagonal valide de 6 caractères pour chaque couleur.';
+$lang['Themes.type.admin'] = 'Personnel';
+$lang['Themes.type.client'] = 'Client';
+
