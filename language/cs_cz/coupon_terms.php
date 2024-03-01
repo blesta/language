24b2b56093604920047544e35ddb1e6ec5a5f3d1
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:53 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/coupon_terms.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Coupon Terms
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['CouponTerms.getperiods.onetime'] = 'Jednorázově';
+$lang['CouponTerms.getperiods.year'] = 'Rok';
+$lang['CouponTerms.getperiods.month'] = 'Měsíc';
+$lang['CouponTerms.getperiods.week'] = 'Týden';
+$lang['CouponTerms.getperiods.day'] = 'Den';
+$lang['CouponTerms.!error.term_id.exists'] = 'Daný kuponový výraz neexistuje.';
+$lang['CouponTerms.!error.period.valid'] = 'Zadejte prosím platné období.';
+$lang['CouponTerms.!error.term.format'] = 'Pro každé období zadejte platný termín.';
+$lang['CouponTerms.!error.coupon_id.exists'] = 'Daný kupón neexistuje.';
+
