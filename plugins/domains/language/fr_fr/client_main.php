dac1cb94a0e24b603a6ce90a172f5b0c1465a36c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:47:54 -0800

    Updated fr_fr language files

---
+++ plugins/domains/language/fr_fr/client_main.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Client Main
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientMain.index.page_title'] = 'Client #%1$s Domaines';
+$lang['ClientMain.index.boxtitle_domains'] = 'Domaines';
+$lang['ClientMain.index.category_active'] = 'Actif';
+$lang['ClientMain.index.category_pending'] = 'En attente';
+$lang['ClientMain.index.category_suspended'] = 'Suspendu';
+$lang['ClientMain.index.category_deleted'] = 'Supprimé';
+$lang['ClientMain.index.heading_addons'] = 'Compléments';
+$lang['ClientMain.index.heading_status'] = 'Statut';
+$lang['ClientMain.index.heading_domain'] = 'Domaine';
+$lang['ClientMain.index.heading_term'] = 'Durée';
+$lang['ClientMain.index.heading_registration_date'] = 'Date d\'inscription';
+$lang['ClientMain.index.heading_renewal_date'] = 'Date de renouvellement';
+$lang['ClientMain.index.heading_expiration_date'] = 'Date d\'expiration';
+$lang['ClientMain.index.heading_suspension_date'] = 'Date de suspension';
+$lang['ClientMain.index.heading_deletion_date'] = 'Date de suppression';
+$lang['ClientMain.index.heading_options'] = 'Options';
+$lang['ClientMain.index.option_manage'] = 'Gérer';
+$lang['ClientMain.index.recurring_term'] = '%1$s %2$s @ %3$s';
+$lang['ClientMain.index.text_never'] = 'Jamais';
+$lang['ClientMain.index.no_results'] = 'Vous n\'avez pas de %1$s Domains.';
+$lang['ClientMain.serviceinfo.no_results'] = 'Ce domaine n\'a pas de détails.';
+$lang['ClientMain.serviceinfo.parent_service'] = 'Ce domaine appartient à <a href="%1$s">%2$s (%3$s)</a>. Si ce service est annulé, ce domaine sera également annulé.';
+$lang['ClientMain.serviceinfo.cancellation_reason'] = 'Motif de l\'annulation : %1$s';
+$lang['ClientMain.getfilters.any'] = 'Tous';
+$lang['ClientMain.getfilters.field_module_id'] = 'Modules du greffier';
+$lang['ClientMain.getfilters.field_package_name'] = 'TLD';
+$lang['ClientMain.getfilters.field_service_meta'] = 'Nom de domaine';
+
