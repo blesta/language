fae0f740de2b9f43c9a5815a7fee405f677b9b2f
pmphillips <paul@phillipsdata.com>
2024-03-01 15:01:16 -0800

    Updated cs_cz language files

---
+++ components/modules/blesta_license/language/cs_cz/license_module.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * License Module
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['BlestaLicense.tab_reissue.field_submit'] = 'Reedice';
+$lang['BlestaLicense.tab_reissue.description'] = 'Opětovné vydání licence umožní její instalaci nebo přesun na jiné místo, kde bude uzamčena.';
+$lang['BlestaLicense.tab_reissue.heading_reissue'] = 'Reedice';
+$lang['BlestaLicense.tab_reissue'] = 'Opětovné vydání licence';
+$lang['Package.package_fields.pricing_id'] = 'Licence';
+$lang['Package.getpricings.pricing_id'] = '%1$s (ID %2$s)';
+$lang['BlestaLicense.edit_row.edit_btn'] = 'Aktualizace prodejce';
+$lang['BlestaLicense.edit_row.basic_title'] = 'Základní nastavení';
+$lang['BlestaLicense.edit_row.box_title'] = 'Upravit prodejce';
+$lang['BlestaLicense.add_row.add_btn'] = 'Přidat prodejce';
+$lang['BlestaLicense.add_row.basic_title'] = 'Základní nastavení';
+$lang['BlestaLicense.add_row.box_title'] = 'Přidat prodejce';
+$lang['BlestaLicense.row_meta.test_mode'] = 'Testovací režim';
+$lang['BlestaLicense.row_meta.password'] = 'Heslo';
+$lang['BlestaLicense.row_meta.username'] = 'Uživatelské jméno';
+$lang['BlestaLicense.!tooltip.password'] = 'Zadejte heslo k účtu Blesta.';
+$lang['BlestaLicense.!tooltip.username'] = 'Zadejte své uživatelské jméno účtu Blesta.';
+$lang['BlestaLicense.manage.module_rows_no_results'] = 'Neexistují žádní prodejci.';
+$lang['BlestaLicense.manage.module_rows.confirm_delete'] = 'Opravdu chcete tohoto prodejce odstranit?';
+$lang['BlestaLicense.manage.module_rows.delete'] = 'Odstranit';
+$lang['BlestaLicense.manage.module_rows.edit'] = 'Upravit';
+$lang['BlestaLicense.manage.module_rows_heading.options'] = 'Možnosti';
+$lang['BlestaLicense.manage.module_rows_heading.username'] = 'Uživatelské jméno';
+$lang['BlestaLicense.manage.module_rows_title'] = 'Prodejci';
+$lang['BlestaLicense.add_module_row'] = 'Přidat prodejce';
+$lang['BlestaLicense.module_rows.name'] = 'Prodejci';
+$lang['BlestaLicense.module_row.name'] = 'Prodejce';
+$lang['Package.!error.meta[pricing_id].format'] = 'Vyberte platnou licenci.';
+
