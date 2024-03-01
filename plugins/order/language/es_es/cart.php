b7c75e3cd5d49373dfddd4d2f32cd7ebd3a3bca2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:33 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/cart.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Cart
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cart.!success.coupon_applied'] = 'El cupón se ha aplicado correctamente.';
+$lang['Cart.!success.item_removed'] = 'El artículo ha sido eliminado de su cesta.';
+$lang['Cart.!success.all_items_removed'] = 'Todos los artículos han sido eliminados de su cesta.';
+$lang['Cart.!error.coupon_applied'] = 'No se ha podido aplicar el cupón.';
+$lang['Cart.index.quantity'] = '%1$s @';
+$lang['Cart.index.totals.discount'] = 'Descuento:';
+$lang['Cart.index.totals.subtotal'] = 'Subtotal:';
+$lang['Cart.index.totals.tax'] = '%1$s:';
+$lang['Cart.index.totals.total'] = 'Total a pagar hoy:';
+$lang['Cart.index.totals.total_recurring'] = 'Total al renovar:';
+$lang['Cart.index.totals.total_recurring_amount'] = '%1$s %2$s @ %3$s';
+
