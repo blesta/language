ed7da01e69884b51e68e9e2368de1a1d40a32bfa
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:51 -0800

    Updated cs_cz language files

---
+++ plugins/ip_unblocker/language/cs_cz/ip_unblocker_plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Ip Unblocker Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['IpUnblockerPlugin.!error.remote_curl'] = 'Nepodařilo se připojit ke vzdálenému serveru. cURL Error: %1$s';
+$lang['IpUnblockerPlugin.!success.unblock_ip'] = 'Žádost o odblokování odeslána.';
+$lang['IpUnblockerPlugin.description'] = 'Zásuvný modul pro Blestu, který se integruje s cPanel a DirectAdmin a odblokovává IP adresy na černé listině.';
+$lang['IpUnblockerPlugin.name'] = 'IP Unblocker';
+$lang['IpUnblockerPlugin.unblock_ip'] = 'Odblokování IP';
+
