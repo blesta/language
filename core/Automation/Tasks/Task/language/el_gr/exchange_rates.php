134d6a9978a7c49a599ff7bdca48dc53b91190c6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:50:19 -0800

    Updated el_gr language files

---
+++ core/Automation/Tasks/Task/language/el_gr/exchange_rates.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Exchange Rates
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.exchange_rates.attempt'] = 'Προσπάθεια ενημέρωσης των συναλλαγματικών ισοτιμιών.';
+$lang['Automation.task.exchange_rates.completed'] = 'Η εργασία για τις συναλλαγματικές ισοτιμίες έχει ολοκληρωθεί.';
+$lang['Automation.task.exchange_rates.success'] = 'Οι συναλλαγματικές ισοτιμίες ενημερώθηκαν με επιτυχία.';
+$lang['Automation.task.exchange_rates.failed'] = 'Οι συναλλαγματικές ισοτιμίες δεν ήταν δυνατόν να ενημερωθούν.';
+
