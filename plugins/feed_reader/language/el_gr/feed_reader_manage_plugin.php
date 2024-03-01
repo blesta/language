0ed9588892529d0929204853f5c5e9fdc3e6a06e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:02 -0800

    Updated el_gr language files

---
+++ plugins/feed_reader/language/el_gr/feed_reader_manage_plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Feed Reader Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['FeedReaderManagePlugin.!success.feed_added'] = 'Η τροφή προστέθηκε επιτυχώς.';
+$lang['FeedReaderManagePlugin.!success.feed_refreshed'] = 'Η τροφοδοσία ανανεώθηκε επιτυχώς.';
+$lang['FeedReaderManagePlugin.!success.feed_removed'] = 'Η τροφή αφαιρέθηκε επιτυχώς.';
+$lang['FeedReaderManagePlugin.index.page_title'] = 'Feed Reader > Διαχείριση';
+$lang['FeedReaderManagePlugin.index.boxtitle_feedreader'] = 'Αναγνώστης ζωοτροφών';
+$lang['FeedReaderManagePlugin.index.add_feed'] = 'Προσθήκη τροφής';
+$lang['FeedReaderManagePlugin.index.heading_url'] = 'URL';
+$lang['FeedReaderManagePlugin.index.heading_lastupdated'] = 'Τελευταία ενημέρωση';
+$lang['FeedReaderManagePlugin.index.heading_options'] = 'Επιλογές';
+$lang['FeedReaderManagePlugin.index.option_refresh'] = 'Ανανέωση';
+$lang['FeedReaderManagePlugin.index.option_remove'] = 'Αφαιρέστε το';
+$lang['FeedReaderManagePlugin.index.no_feeds'] = 'Δεν υπάρχουν προεπιλεγμένες τροφοδοσίες.';
+$lang['FeedReaderManagePlugin.index.text_confirm_delete'] = 'Πραγματικά διαγράφετε αυτή την τροφοδοσία;';
+$lang['FeedReaderManagePlugin.add.page_title'] = 'Αναγνώστης τροφών > Προσθήκη τροφής';
+$lang['FeedReaderManagePlugin.add.boxtitle_feed'] = 'Προσθέστε μια τροφοδοσία';
+$lang['FeedReaderPlugin.add.field_url'] = 'URL';
+$lang['FeedReaderPlugin.add.submit_add'] = 'Προσθήκη τροφής';
+$lang['FeedReaderPlugin.add.submit_cancel'] = 'Ακύρωση';
+
