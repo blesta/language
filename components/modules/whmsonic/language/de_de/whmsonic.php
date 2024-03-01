729b0a8f34b3f46338ac9e7fa33ba231f3a26dd6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:57:44 -0800

    Updated de_de language files

---
+++ components/modules/whmsonic/language/de_de/whmsonic.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Whmsonic
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Whmsonic.!error.user_password.valid'] = 'Das Passwort muss mindestens 8 Zeichen lang sein.';
+$lang['Whmsonic.!error.user_name.empty'] = 'Der Benutzername darf nicht leer sein.';
+$lang['Whmsonic.!error.meta[autodj].valid'] = 'Bitte wählen Sie eine gültige AutoDJ Access Option.';
+$lang['Whmsonic.!error.meta[listeners].empty'] = 'Bitte geben Sie das Hörerlimit ein.';
+$lang['Whmsonic.!error.meta[bandwidth].empty'] = 'Bitte geben Sie das Bandbreitenlimit (in MB) ein, z.B.: 1000.';
+$lang['Whmsonic.!error.meta[hspace].empty'] = 'Bitte geben Sie den Speicherplatz (in MB) ein, z.B.: 1000.';
+$lang['Whmsonic.!error.meta[bitrate].valid'] = 'Bitte wählen Sie ein gültiges BitRate Limit.';
+$lang['Whmsonic.!error.meta[client_type].valid'] = 'Bitte wählen Sie einen gültigen Kundentyp aus.';
+$lang['Whmsonic.!error.module_row.missing'] = 'Es ist ein interner Fehler aufgetreten. Die Modulzeile ist nicht verfügbar.';
+$lang['Whmsonic.!error.api.internal'] = 'Es ist ein interner Fehler aufgetreten, oder der Server hat nicht auf die Anfrage geantwortet.';
+$lang['Whmsonic.!error.password_valid'] = 'Die API-Anmeldeinformationen scheinen ungültig zu sein.';
+$lang['Whmsonic.!error.ip_address_valid'] = 'Die IP-Adresse scheint ungültig zu sein.';
+$lang['Whmsonic.!error.server_name_valid'] = 'Sie müssen ein Server-Label eingeben.';
+$lang['Whmsonic.service_info.option_login'] = 'Einloggen';
+$lang['Whmsonic.service_info.options'] = 'Optionen';
+$lang['Whmsonic.service_info.password'] = 'Passwort';
+$lang['Whmsonic.service_info.username'] = 'Benutzername';
+$lang['Whmsonic.service_info.ip_address'] = 'IP-Adresse';
+$lang['Whmsonic.service_field.password.tooltip'] = 'Das Passwort wird nur lokal in Blesta aktualisiert';
+$lang['Whmsonic.service_field.username.tooltip'] = 'Der Nutzername wird nur lokal in Blesta aktualisiert';
+$lang['Whmsonic.service_field.password'] = 'cPanel-Kennwort';
+$lang['Whmsonic.service_field.radio_password'] = 'Radio-Passwort';
+$lang['Whmsonic.service_field.username'] = 'Radio-Benutzername';
+$lang['Whmsonic.service_field.cpanel_username'] = 'cPanel-Benutzername';
+$lang['Whmsonic.package_fields.autodj.no'] = 'Nein';
+$lang['Whmsonic.package_fields.autodj.yes'] = 'Ja';
+$lang['Whmsonic.package_fields.autodj'] = 'AutoDJ Zugang';
+$lang['Whmsonic.package_fields.listeners'] = 'Hörer Limit';
+$lang['Whmsonic.package_fields.bandwidth'] = 'Bandbreitenbegrenzung (in MB)';
+$lang['Whmsonic.package_fields.hspace'] = 'Speicherplatz (in MB)';
+$lang['Whmsonic.package_fields.bitrate'] = 'BitRate-Grenze';
+$lang['Whmsonic.package_fields.client_type.internal'] = 'Intern';
+$lang['Whmsonic.package_fields.client_type.external'] = 'Extern';
+$lang['Whmsonic.package_fields.client_type'] = 'Kunde Typ';
+$lang['Whmsonic.row_meta.use_ssl'] = 'Verwenden Sie SSL für die Verbindung zur API (empfohlen)';
+$lang['Whmsonic.row_meta.password'] = 'Passwort';
+$lang['Whmsonic.row_meta.ip_address'] = 'IP-Adresse';
+$lang['Whmsonic.row_meta.server_name'] = 'Server-Etikett';
+$lang['Whmsonic.edit_row.add_btn'] = 'Server bearbeiten';
+$lang['Whmsonic.edit_row.basic_title'] = 'Grundeinstellungen';
+$lang['Whmsonic.edit_row.box_title'] = 'WHMSonic Server bearbeiten';
+$lang['Whmsonic.add_row.add_btn'] = 'Server hinzufügen';
+$lang['Whmsonic.add_row.basic_title'] = 'Grundeinstellungen';
+$lang['Whmsonic.add_row.box_title'] = 'WHMSonic Server hinzufügen';
+$lang['Whmsonic.order_options.first'] = 'Erster nicht vollständiger Server';
+$lang['Whmsonic.manage.module_groups_no_results'] = 'Es gibt keine Servergruppen.';
+$lang['Whmsonic.manage.module_rows_no_results'] = 'Es gibt keine Server.';
+$lang['Whmsonic.manage.module_groups.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Servergruppe löschen wollen?';
+$lang['Whmsonic.manage.module_rows.confirm_delete'] = 'Sind Sie sicher, dass Sie diesen Server löschen wollen?';
+$lang['Whmsonic.manage.module_groups.delete'] = 'Löschen';
+$lang['Whmsonic.manage.module_rows.delete'] = 'Löschen';
+$lang['Whmsonic.manage.module_groups.edit'] = 'bearbeiten';
+$lang['Whmsonic.manage.module_rows.edit'] = 'bearbeiten';
+$lang['Whmsonic.manage.module_groups_heading.options'] = 'Optionen';
+$lang['Whmsonic.manage.module_groups_heading.servers'] = 'Anzahl der Server';
+$lang['Whmsonic.manage.module_groups_heading.name'] = 'Name der Gruppe';
+$lang['Whmsonic.manage.module_rows_heading.options'] = 'Optionen';
+$lang['Whmsonic.manage.module_rows_heading.ip_address'] = 'IP-Adresse';
+$lang['Whmsonic.manage.module_rows_heading.name'] = 'Server-Etikett';
+$lang['Whmsonic.manage.module_groups_title'] = 'Server-Gruppen';
+$lang['Whmsonic.manage.module_rows_title'] = 'Server';
+$lang['Whmsonic.add_module_group'] = 'Server-Gruppe hinzufügen';
+$lang['Whmsonic.add_module_row'] = 'Server hinzufügen';
+$lang['Whmsonic.tab_client_actions.radio_password'] = 'Radio-Passwort';
+$lang['Whmsonic.tab_client_actions.password'] = 'cPanel-Kennwort';
+$lang['Whmsonic.tab_client_actions.change_password'] = 'Passwörter ändern';
+$lang['Whmsonic.tab_stats.info.whmsonic_radio_password'] = 'Radio-Passwort';
+$lang['Whmsonic.tab_stats.info.whmsonic_radio_ip'] = 'Radio IP';
+$lang['Whmsonic.tab_stats.info.whmsonic_ftp'] = 'FTP-Zugang';
+$lang['Whmsonic.tab_stats.info.password'] = 'Passwort';
+$lang['Whmsonic.tab_stats.info.username'] = 'Benutzername';
+$lang['Whmsonic.tab_stats.info.autodj'] = 'AutoDJ Zugang';
+$lang['Whmsonic.tab_stats.info.listeners'] = 'Hörer Limit';
+$lang['Whmsonic.tab_stats.info.hspace'] = 'Speicherplatz (in MB)';
+$lang['Whmsonic.tab_stats.info.bandwidth'] = 'Bandbreitenbegrenzung (in MB)';
+$lang['Whmsonic.tab_stats.info.bitrate'] = 'BitRate-Grenze';
+$lang['Whmsonic.tab_stats.info.client_type'] = 'Kunde Typ';
+$lang['Whmsonic.tab_stats.info_heading.value'] = 'Wert';
+$lang['Whmsonic.tab_stats.info_heading.field'] = 'Feld';
+$lang['Whmsonic.submit'] = 'Einreichen';
+$lang['Whmsonic.tab_client_actions'] = 'Aktionen';
+$lang['Whmsonic.tab_stats'] = 'Service-Informationen';
+$lang['Whmsonic.module_group'] = 'Server-Gruppe';
+$lang['Whmsonic.module_row_plural'] = 'Server';
+$lang['Whmsonic.module_row'] = 'Server';
+$lang['Whmsonic.description'] = 'Ermöglicht es Ihnen, Shoutcast, Icecast, Streaming Media Hosting, AutoDJ, Radio Reseller von Ihrem Dedicated oder VPS Server anzubieten.';
+$lang['Whmsonic.name'] = 'WHMSonic';
+
