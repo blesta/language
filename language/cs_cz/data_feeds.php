991e1f853c0dea3eeab808c8c5148f8fc5f67bea
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:57 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/data_feeds.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Data Feeds
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DataFeeds.execute.endpoint_not_found'] = 'Požadovaný koncový bod neexistuje.';
+$lang['DataFeeds.!error.enabled.valid'] = 'Možnost povoleno musí být nastavena na hodnotu 1 nebo 0.';
+$lang['DataFeeds.!error.endpoint.valid'] = 'Zadejte prosím koncový bod.';
+$lang['DataFeeds.!error.company_id.exists'] = 'Neplatné ID společnosti.';
+$lang['DataFeeds.!error.class.valid'] = 'Zadaná třída není platnou třídou datového kanálu.';
+$lang['DataFeeds.!error.dir.empty'] = 'Zadejte adresář zásuvného modulu.';
+$lang['DataFeeds.!error.feed.valid'] = 'Zadejte název datového kanálu.';
+
