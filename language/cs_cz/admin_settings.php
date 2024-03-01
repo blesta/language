861534f72286a3c2e698eb222ef0b5ac937fdd38
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:34 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_settings.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Admin Settings
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminSettings.system.text_help'] = 'Zobrazte zdroje nápovědy pro instalaci systému Blesta.';
+$lang['AdminSettings.system.heading_help'] = 'Nápověda';
+$lang['AdminSettings.system.text_upgrade'] = 'Zobrazení informací o aktualizaci a stahování.';
+$lang['AdminSettings.system.heading_upgrade'] = 'Možnosti upgradu';
+$lang['AdminSettings.system.text_staff'] = 'Vytvořit nebo upravit účty a oprávnění zaměstnanců. Zaměstnanci mají přístup do administrátorské oblasti systému Blesta.';
+$lang['AdminSettings.system.heading_staff'] = 'Zaměstnanci';
+$lang['AdminSettings.system.text_automation'] = 'Určete informace o úlohách Cron nebo naplánovaných úlohách.';
+$lang['AdminSettings.system.heading_automation'] = 'Automatizace';
+$lang['AdminSettings.system.text_marketplace'] = 'Procházejte a stahujte pluginy, moduly, brány a další položky z tržiště Blesta Marketplace.';
+$lang['AdminSettings.system.heading_marketplace'] = 'Tržiště';
+$lang['AdminSettings.system.text_api'] = 'Řízení přístupu k rozhraní API této instalace.';
+$lang['AdminSettings.system.heading_api'] = 'Přístup k rozhraní API';
+$lang['AdminSettings.system.text_backup'] = 'Konfigurace automatického zálohování pomocí SFTP nebo Amazon S3 nebo stažení zálohy do místního počítače.';
+$lang['AdminSettings.system.heading_backup'] = 'Záloha';
+$lang['AdminSettings.system.text_companies'] = 'Přidání nebo aktualizace společností.';
+$lang['AdminSettings.system.heading_companies'] = 'Společnosti';
+$lang['AdminSettings.system.text_general'] = 'Konfigurace nastavení celého systému, včetně zapnutí režimu údržby, databáze GeoIP nebo aktualizace licenčního klíče.';
+$lang['AdminSettings.system.heading_general'] = 'Obecné';
+$lang['AdminSettings.system.boxtitle_settings'] = 'Nastavení';
+$lang['AdminSettings.system.category_system'] = 'Systém';
+$lang['AdminSettings.system.category_company'] = 'Společnost';
+$lang['AdminSettings.system.page_title'] = 'Nastavení';
+$lang['AdminSettings.company.text_feeds'] = 'Datové kanály umožňují snadné vložení cen, názvů a popisů balíčků na externí webové stránky.';
+$lang['AdminSettings.company.heading_feeds'] = 'Datové kanály';
+$lang['AdminSettings.company.text_lookandfeel'] = 'Přizpůsobte si vzhled systému Blesta správou barevných motivů nebo aktualizací šablony klientského rozhraní.';
+$lang['AdminSettings.company.heading_lookandfeel'] = 'Vzhled a pocit';
+$lang['AdminSettings.company.text_plugins'] = 'Instalace, odinstalace a správa zásuvných modulů, které rozšiřují a přizpůsobují základní funkce systému Blesta.';
+$lang['AdminSettings.company.heading_plugins'] = 'Zásuvné moduly';
+$lang['AdminSettings.company.text_clientoptions'] = 'Vytváření a správa vlastních klientských polí a výběr požadovaných klientských polí.';
+$lang['AdminSettings.company.heading_clientoptions'] = 'Možnosti klienta';
+$lang['AdminSettings.company.text_taxes'] = 'Povolte nebo zakažte daň, nastavte DIČ/VATIN a vytvořte jednoúrovňová nebo víceúrovňová daňová pravidla.';
+$lang['AdminSettings.company.heading_taxes'] = 'Daně';
+$lang['AdminSettings.company.text_gateways'] = 'Povolení a zakázání obchodních a neobchodních zpracovatelů plateb pro přijímání plateb.';
+$lang['AdminSettings.company.heading_gateways'] = 'Platební brány';
+$lang['AdminSettings.company.text_modules'] = 'Instalace a správa modulů, které slouží k poskytování a správě služeb.';
+$lang['AdminSettings.company.heading_modules'] = 'Moduly';
+$lang['AdminSettings.company.text_automation'] = 'Přístup k automatizovaným úlohám v systému Blesta a jejich řízení.';
+$lang['AdminSettings.company.heading_automation'] = 'Automatizace';
+$lang['AdminSettings.company.text_clientgroups'] = 'Všichni klienti patří do skupiny, která určuje, kdy bude klientovi vystavena faktura, kdy bude účtován poplatek a další. Vytvářejte a spravujte skupiny klientů a jejich nastavení.';
+$lang['AdminSettings.company.heading_clientgroups'] = 'Skupiny klientů';
+$lang['AdminSettings.company.text_currencies'] = 'Aktualizace a přidávání měn, nastavení výchozí měny, konfigurace více měn a konfigurace směnných kurzů.';
+$lang['AdminSettings.company.heading_currencies'] = 'Měny';
+$lang['AdminSettings.company.text_messengers'] = 'Instalace a správa messengerů, které se používají pro oznámení a komunikaci.';
+$lang['AdminSettings.company.heading_messengers'] = 'Poslové';
+$lang['AdminSettings.company.text_emails'] = 'Konfigurace nastavení pošty, úpravy šablon e-mailů a vytváření a správa e-mailových podpisů.';
+$lang['AdminSettings.company.heading_emails'] = 'E-maily';
+$lang['AdminSettings.company.text_billing'] = 'Konfigurace možností a přizpůsobení faktur, automatických plateb, upozornění na platby a kupónů.';
+$lang['AdminSettings.company.heading_billing'] = 'Fakturace/platba';
+$lang['AdminSettings.company.text_general'] = 'Konfigurace lokalizace, možností internacionalizace a typů klientských kontaktů.';
+$lang['AdminSettings.company.heading_general'] = 'Obecné';
+$lang['AdminSettings.company.boxtitle_settings'] = 'Nastavení';
+$lang['AdminSettings.company.category_system'] = 'Systém';
+$lang['AdminSettings.company.category_company'] = 'Společnost';
+$lang['AdminSettings.company.page_title'] = 'Nastavení';
+
