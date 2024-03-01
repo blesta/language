7ed3b3bbf54401a7aad7838915b236e3f264bed4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:38 -0800

    Updated el_gr language files

---
+++ plugins/order/views/templates/standard/language/el_gr/cart.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cart
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Cart.index.header_cart'] = 'Παραγγελία αναθεώρησης';
+$lang['Cart.index.table_description'] = 'Περιγραφή';
+$lang['Cart.index.table_term'] = 'Όρος';
+$lang['Cart.index.table_qty'] = 'Ποσότητα';
+$lang['Cart.index.table_price'] = 'Τιμή';
+$lang['Cart.index.checkout_btn'] = 'Ταμείο';
+$lang['Cart.index.order_more_btn'] = 'Παραγγείλετε περισσότερα αντικείμενα';
+$lang['Cart.index.edit_item'] = 'Επεξεργασία';
+$lang['Cart.index.remove_item'] = 'Αφαιρέστε το';
+$lang['Cart.index.empty_cart'] = 'Το καλάθι σας είναι άδειο. Ξεκινήστε με <a href="%1$s">επιλέγοντας ένα πρόγραμμα"</a>';
+
