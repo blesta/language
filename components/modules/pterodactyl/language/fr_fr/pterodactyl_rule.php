ed673a898b424177f2edf47db11b8cc06d454fd4
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:02 -0800

    Updated fr_fr language files

---
+++ components/modules/pterodactyl/language/fr_fr/pterodactyl_rule.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Pterodactyl Rule
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PterodactylRule.!error.required'] = 'Le champ %1$s est obligatoire.';
+$lang['PterodactylRule.!error.regex'] = 'Le champ %1$s doit correspondre à la regex %2$s.';
+$lang['PterodactylRule.!error.numeric'] = 'Le champ %1$s doit contenir un nombre.';
+$lang['PterodactylRule.!error.integer'] = 'Le champ %1$s doit contenir un nombre entier.';
+$lang['PterodactylRule.!error.string'] = 'Le champ %1$s doit contenir une chaîne de caractères.';
+$lang['PterodactylRule.!error.alphaNum'] = 'Le champ %1$s ne doit contenir que les caractères a-z, A-Z ou 0-9.';
+$lang['PterodactylRule.!error.alphaDash'] = 'Le champ %1$s ne doit contenir que les caractères suivants : a-z, A-Z, 0-9, - ou _.';
+$lang['PterodactylRule.!error.url'] = 'Le champ %1$s doit contenir une URL valide (par exemple www.domain.com).';
+$lang['PterodactylRule.!error.min'] = 'Le champ %1$s doit contenir au minimum %2$s.';
+$lang['PterodactylRule.!error.max'] = 'Le champ %1$s a un maximum de %2$s.';
+$lang['PterodactylRule.!error.between'] = 'Le champ %1$s doit contenir une valeur comprise entre %2$s et %3$s.';
+$lang['PterodactylRule.!error.digitsBetween'] = 'Le champ %1$s doit contenir une valeur comprise entre %2$s et %3$s chiffres.';
+
