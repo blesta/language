129a196aadd95ad5eec5649321ebf76bcb8458ef
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:19 -0800

    Updated es_es language files

---
+++ plugins/ip_unblocker/language/es_es/ip_unblocker_plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Ip Unblocker Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['IpUnblockerPlugin.name'] = 'Desbloqueador de IP';
+$lang['IpUnblockerPlugin.description'] = 'Un plugin para Blesta que se integra con cPanel y DirectAdmin para desbloquear IPs de listas negras.';
+$lang['IpUnblockerPlugin.unblock_ip'] = 'Desbloquear IP';
+$lang['IpUnblockerPlugin.!success.unblock_ip'] = 'Solicitud de desbloqueo enviada.';
+$lang['IpUnblockerPlugin.!error.remote_curl'] = 'No se ha podido conectar con el servidor remoto. cURL Error: %1$s';
+
