2fa1702367bdb405d8d774a304a4f4cb2369584c
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:41 -0800

    Updated fr_fr language files

---
+++ plugins/order/language/fr_fr/payouts.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Payouts
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payouts.!success.payout_requested'] = 'Votre paiement a été demandé.';
+$lang['Payouts.!error.exceeded_payout_requests'] = 'Vous avez dépassé la limite maximale des demandes de paiement en attente.';
+$lang['Payouts.!error.insufficient_funds_payout_request'] = 'Il n\'y a pas assez de fonds pour traiter la demande de paiement.';
+$lang['Payouts.!error.minimum_payout_not_available'] = 'Les fonds disponibles n\'atteignent pas le montant minimum pour les paiements.';
+$lang['Payouts.index.boxtitle_payouts'] = 'Paiements';
+$lang['Payouts.index.category_approved'] = 'Approuvé';
+$lang['Payouts.index.category_pending'] = 'En attente';
+$lang['Payouts.index.category_declined'] = 'Refusé';
+$lang['Payouts.index.categorylink_request_payout'] = 'Demande de paiement';
+$lang['Payouts.index.no_results'] = 'Il n\'y a actuellement aucun paiement avec ce statut.';
+$lang['Payouts.index.heading_id'] = 'ID de paiement';
+$lang['Payouts.index.heading_requested_amount'] = 'Montant demandé';
+$lang['Payouts.index.heading_requested_currency'] = 'Monnaie demandée';
+$lang['Payouts.index.heading_payment_method'] = 'Mode de paiement';
+$lang['Payouts.index.heading_date_requested'] = 'Date de la demande';
+$lang['Payouts.add.boxtitle_request_payout'] = 'Demande de paiement';
+$lang['Payouts.add.title_payout_request'] = 'Demande de paiement';
+$lang['Payouts.add.field_requested_amount'] = 'Montant du retrait';
+$lang['Payouts.add.field_requested_currency'] = 'Monnaie demandée';
+$lang['Payouts.add.field_payment_method'] = 'Mode de paiement';
+$lang['Payouts.add.min_withdrawal_amount'] = 'Montant minimum du paiement';
+$lang['Payouts.add.max_withdrawal_amount'] = 'Montant maximum du paiement';
+$lang['Payouts.add.available_payout'] = 'Disponible pour le paiement';
+$lang['Payouts.add.request_payout'] = 'Demande de paiement';
+
