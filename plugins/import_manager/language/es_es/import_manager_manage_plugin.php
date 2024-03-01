63689badafa46d7d82d87bd224e5fe3c1922c3af
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:17 -0800

    Updated es_es language files

---
+++ plugins/import_manager/language/es_es/import_manager_manage_plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Import Manager Manage Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ImportManagerManagePlugin.!success.imported'] = 'El importador ha sido procesado.';
+$lang['ImportManagerManagePlugin.index.boxtitle_importmanager'] = 'Gestor de importaciones';
+$lang['ImportManagerManagePlugin.index.page_title'] = 'Import Manager > Importar';
+$lang['ImportManagerManagePlugin.index.platform'] = 'Plataforma';
+$lang['ImportManagerManagePlugin.index.versions'] = 'Versiones';
+$lang['ImportManagerManagePlugin.index.no_migrators'] = 'No hay migradores disponibles.';
+$lang['ImportManagerManagePlugin.index.import_via_cli'] = 'Para importar a través de CLI, vaya al directorio raíz de su instalación de Blesta (%1$s) y ejecute el siguiente comando: php index.php admin/plugin/import_manager/admin_manage_plugin/index';
+$lang['ImportManagerManagePlugin.import.boxtitle_importmanager'] = 'Importar de %1$s versión %2$s';
+$lang['ImportManagerManagePlugin.import.page_title'] = 'Import Manager > Importar';
+$lang['ImportManagerManagePlugin.import.continue'] = 'Continuar';
+$lang['ImportManagerManagePlugin.import.submit'] = 'Importar';
+$lang['ImportManagerManagePlugin.import.cancel'] = 'Cancelar, no importar';
+
