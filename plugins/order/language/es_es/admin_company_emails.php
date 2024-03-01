932b4de35eabb3d1090f75df52656e1500f97e4e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:28 -0800

    Updated es_es language files

---
+++ plugins/order/language/es_es/admin_company_emails.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Admin Company Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyEmails.templates.Order.received_name'] = 'Pedido recibido';
+$lang['AdminCompanyEmails.templates.Order.received_desc'] = 'Aviso enviado cuando se recibe un pedido.';
+$lang['AdminCompanyEmails.templates.Order.received_mobile_name'] = 'Pedido recibido (móvil)';
+$lang['AdminCompanyEmails.templates.Order.received_mobile_desc'] = 'Aviso enviado cuando se recibe un pedido destinado a dispositivos móviles.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_name'] = 'Solicitud de pago de afiliación';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_desc'] = 'Aviso enviado al personal cuando se presenta una solicitud de pago.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_received_name'] = 'Pago recibido del afiliado';
+$lang['AdminCompanyEmails.templates.Order.affiliate_payout_request_received_desc'] = 'Aviso enviado a un cliente cuando se envía su solicitud de pago.';
+$lang['AdminCompanyEmails.templates.Order.affiliate_monthly_report_name'] = 'Informe de afiliación (mensual)';
+$lang['AdminCompanyEmails.templates.Order.affiliate_monthly_report_desc'] = 'Informe que enumera las inscripciones y los datos asociados del último mes.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_first_name'] = 'Días de pedido abandonado (1er aviso)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_first_desc'] = 'Primer aviso enviado a un cliente, tras abandonar un pedido impagado.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_second_name'] = 'Días de pedido abandonado (2º aviso)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_second_desc'] = 'Segundo aviso enviado a un cliente, tras abandonar un pedido impagado.';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_third_name'] = 'Días de pedido abandonado (3er aviso)';
+$lang['AdminCompanyEmails.templates.Order.abandoned_cart_third_desc'] = 'Tercer y último aviso enviado a un cliente, tras abandonar un pedido impagado.';
+
