47c07265e9ad6d7314b67123c662f9007fc9b4ca
pmphillips <paul@phillipsdata.com>
2024-03-01 14:58:23 -0800

    Updated de_de language files

---
+++ language/de_de/admin_company_currencies.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Admin Company Currencies
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompanyCurrencies.edit.confirm_edit'] = 'Sind Sie sicher, dass Sie diese Währung aktualisieren möchten? Der Wechselkurs dieser Währung wird überschrieben, wenn die Wechselkurse im System automatisch aktualisiert werden. Sie können die automatische Aktualisierung unter [Einstellungen] > [Unternehmen] > [Währungen] > [Währungseinrichtung] deaktivieren.';
+$lang['AdminCompanyCurrencies.edit.field_editsubmit'] = 'Währung bearbeiten';
+$lang['AdminCompanyCurrencies.edit.field_exchange_rate'] = 'Wechselkurse';
+$lang['AdminCompanyCurrencies.edit.field_suffix'] = 'Suffix-Symbol';
+$lang['AdminCompanyCurrencies.edit.field_prefix'] = 'Präfix Symbol';
+$lang['AdminCompanyCurrencies.edit.field_precision'] = 'Präzision';
+$lang['AdminCompanyCurrencies.edit.field_format'] = 'Format';
+$lang['AdminCompanyCurrencies.edit.field_code'] = 'Währungscode (ISO 4217)';
+$lang['AdminCompanyCurrencies.edit.boxtitle_edit'] = 'Währung bearbeiten';
+$lang['AdminCompanyCurrencies.edit.page_title'] = 'Einstellungen > Unternehmen > Währungen > Währung bearbeiten';
+$lang['AdminCompanyCurrencies.add.confirm_add'] = 'Sind Sie sicher, dass Sie diese Währung anlegen wollen? Der Wechselkurs dieser Währung wird überschrieben, wenn die Wechselkurse im System automatisch aktualisiert werden. Sie können die automatische Aktualisierung unter [Einstellungen] > [Unternehmen] > [Währungen] > [Währungseinrichtung] deaktivieren.';
+$lang['AdminCompanyCurrencies.add.field_addsubmit'] = 'Währung erstellen';
+$lang['AdminCompanyCurrencies.add.field_exchange_rate'] = 'Wechselkurse';
+$lang['AdminCompanyCurrencies.add.field_suffix'] = 'Suffix-Symbol';
+$lang['AdminCompanyCurrencies.add.field_prefix'] = 'Präfix Symbol';
+$lang['AdminCompanyCurrencies.add.field_precision'] = 'Präzision';
+$lang['AdminCompanyCurrencies.add.field_format'] = 'Format';
+$lang['AdminCompanyCurrencies.add.field_code'] = 'Währungscode (ISO 4217)';
+$lang['AdminCompanyCurrencies.add.boxtitle_add'] = 'Währung hinzufügen';
+$lang['AdminCompanyCurrencies.add.page_title'] = 'Einstellungen > Unternehmen > Währungen > Währung hinzufügen';
+$lang['AdminCompanyCurrencies.active.no_exchange_updated'] = 'Niemals';
+$lang['AdminCompanyCurrencies.active.confirm_delete'] = 'Sind Sie sicher, dass Sie diese Währung löschen wollen?';
+$lang['AdminCompanyCurrencies.active.option_delete'] = 'Löschen';
+$lang['AdminCompanyCurrencies.active.option_edit'] = 'bearbeiten';
+$lang['AdminCompanyCurrencies.active.text_options'] = 'Optionen';
+$lang['AdminCompanyCurrencies.active.text_exchange_updated'] = 'Zuletzt aktualisiert';
+$lang['AdminCompanyCurrencies.active.text_exchange_rate'] = 'Wechselkurse';
+$lang['AdminCompanyCurrencies.active.text_precision'] = 'Präzision';
+$lang['AdminCompanyCurrencies.active.text_format'] = 'Format';
+$lang['AdminCompanyCurrencies.active.text_currency_code'] = 'Währungscode (ISO 4217)';
+$lang['AdminCompanyCurrencies.active.no_results'] = 'Es gibt keine aktiven Währungen.';
+$lang['AdminCompanyCurrencies.active.categorylink_addcurrency'] = 'Währung hinzufügen';
+$lang['AdminCompanyCurrencies.active.boxtitle_active'] = 'Aktive Währungen';
+$lang['AdminCompanyCurrencies.active.page_title'] = 'Einstellungen > Unternehmen > Währungen > Aktive Währungen';
+$lang['AdminCompanyCurrencies.setup.closed_parenthesis'] = ')';
+$lang['AdminCompanyCurrencies.setup.open_parenthesis'] = '(';
+$lang['AdminCompanyCurrencies.setup.no_exchange_updated'] = 'Niemals';
+$lang['AdminCompanyCurrencies.setup.field.setupsubmit'] = 'Einstellungen aktualisieren';
+$lang['AdminCompanyCurrencies.setup.field.exchange_rates_padding'] = 'Pad-Wechselkurse';
+$lang['AdminCompanyCurrencies.setup.field.exchange_rates_processor_key'] = 'Wechselkursprozessor API-Schlüssel';
+$lang['AdminCompanyCurrencies.setup.field.exchange_rates_processor'] = 'Wechselkursprozessor';
+$lang['AdminCompanyCurrencies.setup.field.last_updated'] = 'Preise Zuletzt aktualisiert';
+$lang['AdminCompanyCurrencies.setup.field.exchange_rates_auto_update'] = 'Wechselkurse automatisch aktualisieren';
+$lang['AdminCompanyCurrencies.setup.field.multi_currency_pricing'] = 'Paketpreise nur für neue Dienste verwenden';
+$lang['AdminCompanyCurrencies.setup.heading_multicurrency'] = 'Mehrere Währungen';
+$lang['AdminCompanyCurrencies.setup.field.client_set_currency'] = 'Dem Kunden erlauben, die Währung einzustellen';
+$lang['AdminCompanyCurrencies.setup.field.show_currency_code'] = 'Währungscode anzeigen';
+$lang['AdminCompanyCurrencies.setup.field.default_currency'] = 'Standardwährung';
+$lang['AdminCompanyCurrencies.setup.heading_general'] = 'Allgemein';
+$lang['AdminCompanyCurrencies.setup.boxtitle_setup'] = 'Währung einrichten';
+$lang['AdminCompanyCurrencies.setup.tooltip_currency_pricing'] = 'Diese Option setzt voraus, dass die Paketpreise in der angegebenen Währung festgelegt sind, damit sie für die Erstellung neuer Dienstleistungen verfügbar sind. Wenn die Option nicht aktiviert ist und für die Währung keine Paketpreise existieren, wird der Preis auf der Grundlage des Wechselkurses der Standardwährung berechnet.
+
+Die Erneuerung von Dienstleistungen bevorzugt immer Paketpreise in der gewählten Währung, verwendet aber den Wechselkurs der Standardwährung, wenn dieser für das Paket nicht angegeben ist.';
+$lang['AdminCompanyCurrencies.setup.page_title'] = 'Einstellungen > Unternehmen > Währungen > Währungseinstellungen';
+$lang['AdminCompanyCurrencies.!cancel.field_cancel'] = 'Abbrechen';
+$lang['AdminCompanyCurrencies.!tooltip.exchange_rates_padding'] = 'Die von einem Bearbeiter ermittelten Kurswerte werden um den angegebenen Prozentsatz erhöht.';
+$lang['AdminCompanyCurrencies.!tooltip.exchange_rates_processor_key'] = 'Ein API-Schlüssel ist erforderlich, um die Wechselkurse von dem ausgewählten Prozessor abzurufen.';
+$lang['AdminCompanyCurrencies.!tooltip.exchange_rates_processor'] = 'Legt den Prozessor fest, der für die Aktualisierung der Wechselkurse verwendet wird. Die verfügbaren Währungen variieren je nach Prozessor.';
+$lang['AdminCompanyCurrencies.!tooltip.exchange_rates_auto_update'] = 'Aktivieren Sie dieses Kontrollkästchen, wenn Sie möchten, dass die Wechselkurse automatisch mit einem der verfügbaren Wechselkursverarbeitungsprogramme aktualisiert werden.';
+$lang['AdminCompanyCurrencies.!tooltip.multi_currency_pricing'] = 'Aktivieren Sie dieses Kontrollkästchen, um zuzulassen, dass Pakete nur mit dem für dieses Paket definierten Preis bestellt werden können. Wenn Sie diese Option aktivieren, müssen die Preise für jede Währung, die Sie für dieses Paket akzeptieren möchten, definiert werden. Mit anderen Worten, dies verhindert, dass eine Währungsumrechnung stattfindet.';
+$lang['AdminCompanyCurrencies.!tooltip.client_set_currency'] = 'Aktivieren Sie dieses Kontrollkästchen, damit die Kunden ihre Standardwährung aus den im System verfügbaren Währungen auswählen können.';
+$lang['AdminCompanyCurrencies.!tooltip.show_currency_code'] = 'Aktivieren Sie das Kontrollkästchen, um die Anzeige des Währungscodes neben den angezeigten Preisen zu aktivieren.';
+$lang['AdminCompanyCurrencies.!tooltip.default_currency'] = 'Die im System verwendete Standardwährung. Stellen Sie diese auf Ihre bevorzugte Währung ein.';
+$lang['AdminCompanyCurrencies.!tooltip.precision'] = 'Die Genauigkeit ist die Anzahl der signifikanten Ziffern nach dem Komma, die die Währung unterstützt.';
+$lang['AdminCompanyCurrencies.!success.rates_updated'] = 'Die Wechselkurse wurden erfolgreich aktualisiert.';
+$lang['AdminCompanyCurrencies.!success.delete_deleted'] = 'Die Währung %1$s wurde erfolgreich gelöscht!';
+$lang['AdminCompanyCurrencies.!success.edit_updated'] = 'Die Währung %1$s wurde erfolgreich aktualisiert!';
+$lang['AdminCompanyCurrencies.!success.add_created'] = 'Die Währung %1$s wurde erfolgreich erstellt!';
+$lang['AdminCompanyCurrencies.!success.setup_updated'] = 'Die Einstellungen für die Währungseinrichtung wurden erfolgreich aktualisiert!';
+
