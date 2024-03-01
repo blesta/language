34c9934cad1a8f357d183df2e6273a69dc97df0a
pmphillips <paul@phillipsdata.com>
2024-03-01 14:45:51 -0800

    Updated fr_fr language files

---
+++ components/modules/gogetssl/language/fr_fr/gogetssl.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Gogetssl
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['GoGetSSL.!error.api_username.empty'] = 'Veuillez saisir un nom d\'utilisateur API.';
+$lang['GoGetSSL.!error.api_username.valid'] = 'La connexion au serveur n\'a pas pu être établie. Veuillez vérifier que les informations d\'identification de l\'API sont correctes.';
+$lang['GoGetSSL.!error.api_password.empty'] = 'Veuillez saisir un mot de passe API.';
+$lang['GoGetSSL.!error.gogetssl_name.empty'] = 'Veuillez saisir un nom de revendeur.';
+$lang['GoGetSSL.!error.gogetssl_validity.empty'] = 'Veuillez saisir une période de validité.';
+$lang['GoGetSSL.!error.gogetssl_approver_email.format'] = 'Veuillez saisir un courriel valide de l\'approbateur.';
+$lang['GoGetSSL.!error.gogetssl_csr.format'] = 'Veuillez saisir une valeur CSR.';
+$lang['GoGetSSL.!error.gogetssl_fqdn.format'] = 'Veuillez saisir un domaine valide.';
+$lang['GoGetSSL.!error.gogetssl_webserver_type.format'] = 'Veuillez saisir un type de serveur Web valide.';
+$lang['GoGetSSL.!error.meta[product].valid'] = 'Veuillez sélectionner un produit.';
+$lang['GoGetSSL.!error.meta[gogetssl_name].empty'] = 'Veuillez sélectionner au moins un revendeur.';
+$lang['GoGetSSL.!error.api.internal'] = 'Une erreur interne s\'est produite ou le serveur n\'a pas répondu à la demande.';
+$lang['GoGetSSL.please_select'] = '-- Veuillez sélectionner --';
+$lang['GoGetSSL.name'] = 'GoGetSSL';
+$lang['GoGetSSL.description'] = 'GoGetSSL fournit des certificats SSL depuis 2009.';
+$lang['GoGetSSL.module_row'] = 'GoGetSSL Revendeur';
+$lang['GoGetSSL.module_row_plural'] = 'Revendeurs';
+$lang['GoGetSSL.add_module_row'] = 'Ajouter un revendeur';
+$lang['GoGetSSL.manage.module_rows_title'] = 'Revendeurs GoGetSSL';
+$lang['GoGetSSL.manage.module_rows.edit'] = 'Editer';
+$lang['GoGetSSL.manage.module_rows.delete'] = 'Supprimer';
+$lang['GoGetSSL.manage.module_rows.confirm_delete'] = 'Êtes-vous sûr de vouloir supprimer ce revendeur ?';
+$lang['GoGetSSL.manage.module_rows_no_results'] = 'Il n\'y a pas de revendeurs.';
+$lang['GoGetSSL.manage.module_rows_heading.gogetssl_name'] = 'Nom du revendeur';
+$lang['GoGetSSL.manage.module_rows_heading.options'] = 'Options';
+$lang['GoGetSSL.row_meta.gogetssl_name'] = 'Nom du revendeur';
+$lang['GoGetSSL.row_meta.api_username'] = 'Nom d\'utilisateur API';
+$lang['GoGetSSL.row_meta.api_password'] = 'Mot de passe API';
+$lang['GoGetSSL.row_meta.sandbox'] = 'Bac à sable';
+$lang['GoGetSSL.add_row.box_title'] = 'Ajouter un revendeur GoGetSSL';
+$lang['GoGetSSL.add_row.basic_title'] = 'Paramètres de base';
+$lang['GoGetSSL.add_row.add_btn'] = 'Ajouter un revendeur';
+$lang['GoGetSSL.edit_row.box_title'] = 'Modifier GoGetSSL Reseller';
+$lang['GoGetSSL.edit_row.basic_title'] = 'Paramètres de base';
+$lang['GoGetSSL.edit_row.add_btn'] = 'Update Reseller';
+$lang['GoGetSSL.package_fields.product'] = 'Produit';
+$lang['GoGetSSL.service_field.gogetssl_fqdn'] = 'Hôte / Domaine';
+$lang['GoGetSSL.service_field.gogetssl_csr'] = 'Contenu RSE';
+$lang['GoGetSSL.service_field.gogetssl_webserver_type'] = 'Type de serveur web';
+$lang['GoGetSSL.service_field.gogetssl_approver_email'] = 'Courriel de l\'approbateur';
+$lang['GoGetSSL.service_field.gogetssl_title'] = 'Titre';
+$lang['GoGetSSL.service_field.gogetssl_firstname'] = 'Prénom';
+$lang['GoGetSSL.service_field.gogetssl_lastname'] = 'Nom de famille';
+$lang['GoGetSSL.service_field.gogetssl_address1'] = 'Ligne d\'adresse 1';
+$lang['GoGetSSL.service_field.gogetssl_address2'] = 'Ligne d\'adresse 2';
+$lang['GoGetSSL.service_field.gogetssl_city'] = 'Ville';
+$lang['GoGetSSL.service_field.gogetssl_zip'] = 'ZIP';
+$lang['GoGetSSL.service_field.gogetssl_state'] = 'État';
+$lang['GoGetSSL.service_field.gogetssl_country'] = 'Pays';
+$lang['GoGetSSL.service_field.gogetssl_email'] = 'Courrier électronique';
+$lang['GoGetSSL.service_field.gogetssl_number'] = 'Numéro de téléphone (format international)';
+$lang['GoGetSSL.service_field.gogetssl_organization'] = 'Organisation (inscrire NA si non applicable)';
+$lang['GoGetSSL.service_field.gogetssl_organization_unit'] = 'Unité d\'organisation (inscrire NA si non applicable)';
+$lang['GoGetSSL.service_field.gogetssl_fax'] = 'Numéro de fax (format international)';
+$lang['GoGetSSL.tab_reissue'] = 'Réédition';
+$lang['GoGetSSL.tab_reissue.heading_reissue'] = 'Réédition';
+
