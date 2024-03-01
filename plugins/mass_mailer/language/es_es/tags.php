d366b16b12adeca1ceb5225fb538d9c49b2933b6
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:25 -0800

    Updated es_es language files

---
+++ plugins/mass_mailer/language/es_es/tags.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Tags
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Tags.client.id_code'] = 'ID';
+$lang['Tags.client.status'] = 'Estado';
+$lang['Tags.contact.first_name'] = 'Nombre';
+$lang['Tags.contact.last_name'] = 'Apellido';
+$lang['Tags.contact.company'] = 'Nombre de la empresa';
+$lang['Tags.contact.address1'] = 'Dirección 1';
+$lang['Tags.contact.address2'] = 'Dirección 2';
+$lang['Tags.contact.city'] = 'Ciudad';
+$lang['Tags.contact.state'] = 'Estado';
+$lang['Tags.contact.country'] = 'País';
+$lang['Tags.contact.zip'] = 'Código postal';
+$lang['Tags.contact.email'] = 'Correo electrónico';
+$lang['Tags.contact.date_added'] = 'Miembro Fecha';
+$lang['Tags.package.name'] = 'Nombre del paquete';
+$lang['Tags.package.description'] = 'Descripción del texto del paquete';
+$lang['Tags.package.description_html'] = 'Paquete HTML Descripción';
+$lang['Tags.package.status'] = 'Estado del paquete';
+$lang['Tags.service.name'] = 'Etiqueta';
+$lang['Tags.service.date_added'] = 'Fecha de adición';
+$lang['Tags.service.date_renews'] = 'Fecha Renueva';
+$lang['Tags.service.date_last_renewed'] = 'Fecha de la última renovación';
+$lang['Tags.service.date_suspended'] = 'Fecha de suspensión';
+$lang['Tags.service.date_canceled'] = 'Fecha de cancelación';
+$lang['Tags.service.status'] = 'Estado';
+$lang['Tags.module.name'] = 'Nombre del módulo';
+$lang['Tags.module.label'] = 'Módulo Etiqueta de fila';
+
