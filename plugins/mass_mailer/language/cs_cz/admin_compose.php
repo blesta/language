5d19be83f4ebfe1c196385dc9997309eb8556c62
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:52 -0800

    Updated cs_cz language files

---
+++ plugins/mass_mailer/language/cs_cz/admin_compose.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Admin Compose
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminCompose.preview.form.send'] = 'Odeslat';
+$lang['AdminCompose.preview.link_back'] = 'Zpět';
+$lang['AdminCompose.preview.text_subject'] = 'Předmět: %1$s';
+$lang['AdminCompose.preview.text_name'] = 'Od: %1$s %2$s';
+$lang['AdminCompose.preview.heading.email_sample'] = 'Vzorový e-mail';
+$lang['AdminCompose.preview.email_total'] = 'Celkový počet kontaktů, které mají být zaslány e-mailem: %1$s';
+$lang['AdminCompose.preview.boxtitle'] = 'Náhled rozesílání';
+$lang['AdminCompose.index.form.preview'] = 'Náhled';
+$lang['AdminCompose.index.link_back'] = 'Zpět';
+$lang['AdminCompose.index.heading_service_tags'] = 'Štítky služeb';
+$lang['AdminCompose.index.heading_client_tags'] = 'Štítky klientů';
+$lang['AdminCompose.index.text.note'] = 'Pokud není nastavena textová verze, bude automaticky vygenerována z verze HTML.';
+$lang['AdminCompose.index.form.log'] = 'Protokolování e-mailů';
+$lang['AdminCompose.index.form.text'] = 'Text';
+$lang['AdminCompose.index.form.html'] = 'HTML';
+$lang['AdminCompose.index.form.subject'] = 'Předmět';
+$lang['AdminCompose.index.form.from_address'] = 'Z e-mailu';
+$lang['AdminCompose.index.form.from_name'] = 'Od názvu';
+$lang['AdminCompose.index.boxtitle'] = 'Sestavit poštu';
+$lang['AdminCompose.!tooltip.log'] = 'Pokud je zaškrtnuto, budou všechny odeslané e-maily zaznamenány v systému Blesta.';
+$lang['AdminCompose.index.page_title'] = 'Hromadná korespondence > Sestavit';
+$lang['AdminCompose.!error.parse_error'] = 'Chyba parsování šablony: %1$s';
+
