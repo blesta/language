c1799570c4abac3e9536b192396ecfaa9393d00e
pmphillips <paul@phillipsdata.com>
2024-03-01 14:49:08 -0800

    Updated fr_fr language files

---
+++ plugins/support_manager/language/fr_fr/knowledgebase.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Knowledgebase
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Knowledgebase.index.page_title'] = 'Base de connaissances';
+$lang['Knowledgebase.view.page_title'] = '%1$s';
+$lang['Knowledgebase.search.placeholder'] = 'Vous avez une question ? Consultez notre base de connaissances ici.';
+$lang['Knowledgebase.search.form_submit'] = 'Recherche';
+$lang['Knowledgebase.index.heading_categories'] = 'Catégories';
+$lang['Knowledgebase.index.heading_subcategories'] = 'Sous-catégories';
+$lang['Knowledgebase.index.category_items'] = '(%1$s)';
+$lang['Knowledgebase.index.categories_description'] = 'Consultez notre base de connaissances par catégorie.';
+$lang['Knowledgebase.index.categories_no_results'] = 'Il n\'y a pas de catégories disponibles.';
+$lang['Knowledgebase.index.heading_popular'] = 'Articles populaires';
+$lang['Knowledgebase.index.popular_description'] = 'Les personnes ont trouvé ces articles les plus utiles.';
+$lang['Knowledgebase.index.heading_articles'] = 'Articles';
+$lang['Knowledgebase.index.articles_description'] = 'Articles pour %1$s.';
+$lang['Knowledgebase.search.heading_articles'] = 'Articles correspondant à \'%1$s\'';
+$lang['Knowledgebase.search.articles_no_results'] = 'Aucun article ne correspond aux critères de recherche.';
+$lang['Knowledgebase.view.text_rate'] = 'Veuillez évaluer cet article pour nous aider à améliorer notre base de connaissances.';
+$lang['Knowledgebase.view.edit'] = 'Editer';
+$lang['Knowledgebase.view.text_voted'] = 'Vous avez déjà voté sur cet article.';
+$lang['Knowledgebase.view.text_vote_thanks'] = 'Merci d\'avoir voté sur cet article !';
+$lang['Knowledgebase.!text.root_directory'] = 'Accueil';
+$lang['Knowledgebase.!text.ellipsis'] = '...';
+
