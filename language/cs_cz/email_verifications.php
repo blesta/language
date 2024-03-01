11f97c477a9129072ac2ad766fb7f1cd2e126524
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:58 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/email_verifications.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Email Verifications
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['EmailVerifications.!error.contact_id.exists'] = 'Neplatné ID kontaktu.';
+$lang['EmailVerifications.!error.email.valid'] = 'Zadejte prosím platnou e-mailovou adresu.';
+$lang['EmailVerifications.!error.redirect_url.valid'] = 'Zadejte prosím platnou adresu URL.';
+$lang['EmailVerifications.!error.token.format'] = 'Zadejte prosím platný token.';
+$lang['EmailVerifications.!error.verified.valid'] = 'Ověřené pole musí mít hodnotu "1" nebo "0".';
+
