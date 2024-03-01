d0222079012d476e065ce591002c000c6857fa53
pmphillips <paul@phillipsdata.com>
2024-03-01 14:44:03 -0800

    Updated es_es language files

---
+++ plugins/download_manager/language/es_es/download_manager_logs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Download Manager Logs
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DownloadManagerLogs.!error.file_id.exists'] = 'El fichero no existe.';
+$lang['DownloadManagerLogs.!error.client_id.exists'] = 'El cliente no existe.';
+$lang['DownloadManagerLogs.!error.contact_id.exists'] = 'El contacto no existe.';
+
