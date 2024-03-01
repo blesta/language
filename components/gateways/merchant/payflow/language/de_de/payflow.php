7829915a0f1a2cdb35e06c7587d389d01df264ae
pmphillips <paul@phillipsdata.com>
2024-03-01 14:56:59 -0800

    Updated de_de language files

---
+++ components/gateways/merchant/payflow/language/de_de/payflow.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payflow
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Payflow.tooltip_partner'] = 'Dies ist der Händlerpartner, den Sie von einem autorisierten PayPal-Händler erhalten haben, der Sie für das Payflow-SDK registriert hat. Wenn Ihr Konto direkt von PayPal erworben wurde, verwenden Sie stattdessen "PayPal".';
+$lang['Payflow.tooltip_user'] = 'Wenn Sie mehr als einen Benutzer für Ihr Konto eingerichtet haben, geben Sie den Benutzer ein, der berechtigt ist, Transaktionen zu bearbeiten. Andernfalls geben Sie denselben Wert ein wie beim Vendor/Merchant Login.';
+$lang['Payflow.test_mode'] = 'Test-Modus';
+$lang['Payflow.partner'] = 'Partner';
+$lang['Payflow.password'] = 'Passwort';
+$lang['Payflow.user'] = 'Benutzer';
+$lang['Payflow.vendor'] = 'Anbieter/Händler-Login';
+$lang['Payflow.description'] = 'Payflow Pro verarbeitet alle gängigen Kredit- und Debitkarten.';
+$lang['Payflow.name'] = 'PayPal Payflow Pro';
+$lang['Payflow.!error.test_mode.valid'] = 'Der Testmodus muss auf "true" gesetzt werden, falls vorhanden.';
+$lang['Payflow.!error.password.format'] = 'Bitte geben Sie ein Passwort mit einer Länge zwischen 6 und 32 Zeichen ein.';
+$lang['Payflow.!error.partner.empty'] = 'Bitte geben Sie Ihren Handelspartner an.';
+$lang['Payflow.!error.vendor.empty'] = 'Bitte geben Sie Ihr Händlerkonto-Login ein.';
+$lang['Payflow.!error.user.empty'] = 'Bitte geben Sie einen Benutzer ein, der berechtigt ist, Transaktionen zu bearbeiten.';
+
