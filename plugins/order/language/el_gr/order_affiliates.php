39585c96b901cf5631eada35033a6bfbf8d3fde2
pmphillips <paul@phillipsdata.com>
2024-03-01 14:52:27 -0800

    Updated el_gr language files

---
+++ plugins/order/language/el_gr/order_affiliates.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Order Affiliates
+ *
+ * @package blesta
+ * @subpackage blesta.language.el_gr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['OrderAffiliates.!error.affiliate_id.exists'] = 'Μη έγκυρο αναγνωριστικό θυγατρικής.';
+$lang['OrderAffiliates.!error.client_id.exists'] = 'Μη έγκυρο αναγνωριστικό πελάτη.';
+$lang['OrderAffiliates.!error.client_id.unique'] = 'Το αναγνωριστικό πελάτη είναι ήδη συνδεδεμένο με άλλο συνεργάτη.';
+$lang['OrderAffiliates.!error.code.empty'] = 'Παρακαλώ εισάγετε έναν κωδικό θυγατρικών.';
+$lang['OrderAffiliates.!error.code.unique'] = 'Αυτός ο κωδικός έχει ήδη ληφθεί από άλλον συνεργάτη.';
+$lang['OrderAffiliates.!error.status.valid'] = 'Παρακαλούμε επιλέξτε ένα έγκυρο καθεστώς θυγατρικών.';
+$lang['OrderAffiliates.!error.date_added.format'] = 'Η ημερομηνία που προστέθηκε είναι σε μη έγκυρη μορφή ημερομηνίας.';
+$lang['OrderAffiliates.!error.date_updated.format'] = 'Η ενημερωμένη ημερομηνία έχει μη έγκυρη μορφή ημερομηνίας.';
+$lang['OrderAffiliates.getStatuses.active'] = 'Ενεργό';
+$lang['OrderAffiliates.getStatuses.inactive'] = 'Ανενεργό';
+
