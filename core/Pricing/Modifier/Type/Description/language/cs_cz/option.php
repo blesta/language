778daf4b214fe0c3d1cd0cd12a0cceec7c16b8f2
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:03 -0800

    Updated cs_cz language files

---
+++ core/Pricing/Modifier/Type/Description/language/cs_cz/option.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Option
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Option.description.option.item_qty.updated.prorated.date'] = 'Poměrná změna %1$s z %4$s na %3$sx %2$s (%5$s - %6$s)';
+$lang['Option.description.option.item_qty.updated.prorated'] = 'Poměrná změna %1$s z %4$s na %3$sx %2$s';
+$lang['Option.description.option.item.updated.prorated.date.text'] = 'Poměrná změna %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item.updated.prorated.date'] = 'Poměrná změna %1$s z %3$s na %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.updated.prorated.text'] = 'Poměrná změna %1$s';
+$lang['Option.description.option.item.updated.prorated'] = 'Poměrná změna %1$s z %3$s na %2$s';
+$lang['Option.description.option.item_qty.removed.prorated.date'] = 'Poměrné odebrání %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item_qty.removed.prorated'] = 'Poměrné odebrání %1$s - %3$sx %2$s';
+$lang['Option.description.option.item.removed.prorated.date.text'] = 'Poměrné odebrání %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item.removed.prorated.date'] = 'Poměrné odebrání %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.removed.prorated.text'] = 'Poměrné odebrání %1$s';
+$lang['Option.description.option.item.removed.prorated'] = 'Poměrné odebrání %1$s - %2$s';
+$lang['Option.description.option.item_qty.added.prorated.date'] = 'Poměrné sčítání %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item_qty.added.prorated'] = 'Poměrné sčítání %1$s - %3$sx %2$s';
+$lang['Option.description.option.item.added.prorated.date.text'] = 'Poměrné sčítání %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item.added.prorated.date'] = 'Poměrné sčítání %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.added.prorated.text'] = 'Proporcionální sčítání %1$s';
+$lang['Option.description.option.item.added.prorated'] = 'Poměrné sčítání %1$s - %2$s';
+$lang['Option.description.option.item_qty.prorated.date'] = 'Poměrné sčítání %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item_qty.prorated'] = 'Poměrné sčítání %1$s - %3$sx %2$s';
+$lang['Option.description.option.item.prorated.date.text'] = 'Poměrné sčítání %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item.prorated.date'] = 'Poměrné sčítání %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.prorated.text'] = 'Proporcionální sčítání %1$s';
+$lang['Option.description.option.item.prorated'] = 'Poměrné sčítání %1$s - %2$s';
+$lang['Option.description.option.item_qty.updated.date'] = 'Změna %1$s z %4$s na %3$sx %2$s (%5$s - %6$s)';
+$lang['Option.description.option.item_qty.updated'] = 'Změna %1$s z %4$s na %3$sx %2$s';
+$lang['Option.description.option.item.updated.date.text'] = 'Změna %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item.updated.date'] = 'Změna %1$s z %3$s na %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item.updated.text'] = 'Změna %1$s';
+$lang['Option.description.option.item.updated'] = 'Změna %1$s z %3$s na %2$s';
+$lang['Option.description.option.item_qty.removed.date'] = 'Odstranění %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item_qty.removed'] = 'Odstranění %1$s - %3$sx %2$s';
+$lang['Option.description.option.item.removed.date.text'] = 'Odstranění %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item.removed.date'] = 'Odstranění %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.removed.text'] = 'Odstranění %1$s';
+$lang['Option.description.option.item.removed'] = 'Odstranění %1$s - %2$s';
+$lang['Option.description.option.item_qty.added.date'] = 'Sčítání %1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item_qty.added'] = 'Sčítání %1$s - %3$sx %2$s';
+$lang['Option.description.option.item.added.date.text'] = 'Sčítání %1$s (%2$s - %3$s)';
+$lang['Option.description.option.item.added.date'] = 'Sčítání %1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.added.text'] = 'Přidání %1$s';
+$lang['Option.description.option.item.added'] = 'Sčítání %1$s - %2$s';
+$lang['Option.description.option.cancel'] = '%1$s - %2$s Storno poplatek';
+$lang['Option.description.option.setup'] = '%1$s - %2$s Zřizovací poplatek';
+$lang['Option.description.option.item_qty.date'] = '%1$s - %3$sx %2$s (%4$s - %5$s)';
+$lang['Option.description.option.item_qty'] = '%1$s - %3$sx %2$s';
+$lang['Option.description.option.item.date.text'] = '%1$s (%2$s - %3$s)';
+$lang['Option.description.option.item.date'] = '%1$s - %2$s (%3$s - %4$s)';
+$lang['Option.description.option.item.text'] = '%1$s';
+$lang['Option.description.option.item'] = '%1$s - %2$s';
+$lang['Option.description.prefix.nest'] = '↳';
+
