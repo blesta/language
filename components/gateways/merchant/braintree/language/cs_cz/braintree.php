40644ce9abb765d144021c92aeda3b2fc1af14b4
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:54 -0800

    Updated cs_cz language files

---
+++ components/gateways/merchant/braintree/language/cs_cz/braintree.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Braintree
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Braintree.!error.environment'] = 'Vyberte platné prostředí.';
+$lang['Braintree.!error.private_key'] = 'Zadejte prosím svůj soukromý klíč.';
+$lang['Braintree.!error.public_key'] = 'Zadejte svůj veřejný klíč.';
+$lang['Braintree.!error.merchant_id'] = 'Zadejte prosím své ID obchodníka.';
+$lang['Braintree.!error.live_mode.valid'] = 'Režim Live musí být nastaven na hodnotu "true", pokud je zadán.';
+$lang['Braintree.environment.production'] = 'Výroba';
+$lang['Braintree.environment.sandbox'] = 'Sandbox';
+$lang['Braintree.environment'] = 'Životní prostředí';
+$lang['Braintree.live_mode'] = 'Živý režim';
+$lang['Braintree.private_key'] = 'Soukromý klíč';
+$lang['Braintree.public_key'] = 'Veřejný klíč';
+$lang['Braintree.merchant_id'] = 'ID obchodníka';
+$lang['Braintree.description'] = 'Obchodníci ve více než 45 zemích/regionech po celém světě mohou pomocí Braintree přijímat, rozdělovat a umožňovat platby ve více než 130 měnách.';
+$lang['Braintree.name'] = 'Braintree';
+
