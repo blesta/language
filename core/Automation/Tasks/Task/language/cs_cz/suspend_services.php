afa4fd0ab66af7ac25e866ac5e08eee705a28264
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:00 -0800

    Updated cs_cz language files

---
+++ core/Automation/Tasks/Task/language/cs_cz/suspend_services.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Suspend Services
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.suspendservices.success'] = 'Služba #%1$s od klienta %2$s byla pozastavena.';
+$lang['Automation.task.suspendservices.error'] = 'Službu #%1$s od klienta %2$s nebylo možné pozastavit.';
+$lang['Automation.task.suspendservices.suspension_reason'] = 'Neplacení';
+$lang['Automation.task.suspendservices.completed'] = 'Úloha pozastavení služby byla dokončena.';
+$lang['Automation.task.suspendservices.attempt'] = 'Pokus o zpracování pozastavení služby.';
+
