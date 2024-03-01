b958597d0562216cb757ba876956cc55b8932fed
pmphillips <paul@phillipsdata.com>
2024-03-01 14:59:16 -0800

    Updated de_de language files

---
+++ language/de_de/report_manager.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Report Manager
+ *
+ * @package blesta
+ * @subpackage blesta.language.de_de
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ReportManager.reportrequiredtypes.custom'] = 'Benutzerdefinierte Regex';
+$lang['ReportManager.reportrequiredtypes.yes'] = 'Ja';
+$lang['ReportManager.reportrequiredtypes.no'] = 'Nein';
+$lang['ReportManager.reportfieldtypes.date'] = 'Datum';
+$lang['ReportManager.reportfieldtypes.select'] = 'Wählen Sie';
+$lang['ReportManager.reportfieldtypes.text'] = 'Text';
+$lang['ReportManager.getformats.json'] = 'JSON';
+$lang['ReportManager.getformats.csv'] = 'CSV';
+$lang['ReportManager.!error.date_created.format'] = 'Erstellungsdatum ist ein ungültiges Format.';
+$lang['ReportManager.!error.query.valid'] = 'Es ist nur eine Abfrage erlaubt, und es muss eine SELECT-Abfrage sein.';
+$lang['ReportManager.!error.query.empty'] = 'Der Bericht muss eine Abfrage haben.';
+$lang['ReportManager.!error.name.valid'] = 'Der Bericht muss einen Namen haben.';
+$lang['ReportManager.!error.temp_dir.writable'] = 'Das temp-Verzeichnis ist nicht beschreibbar oder der Bericht konnte nicht dorthin geschrieben werden.';
+$lang['ReportManager.!error.format.valid'] = 'Bitte wählen Sie ein gültiges Berichtsformat.';
+$lang['ReportManager.!error.type.valid'] = 'Bitte wählen Sie einen gültigen Berichtstyp aus.';
+
