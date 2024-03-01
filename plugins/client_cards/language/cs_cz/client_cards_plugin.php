b4fcac885db537c4be0403ad9b87c9bd41ae7c7e
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:26 -0800

    Updated cs_cz language files

---
+++ plugins/client_cards/language/cs_cz/client_cards_plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Client Cards Plugin
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientCardsPlugin.card_client.invoices'] = 'Faktury';
+$lang['ClientCardsPlugin.card_client.services'] = 'Služby';
+$lang['ClientCardsPlugin.description'] = 'Zobrazí souhrnné informace o klientovi na kartách v klientském rozhraní.';
+$lang['ClientCardsPlugin.name'] = 'Karty klientů';
+
