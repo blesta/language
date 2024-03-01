56cbc197b1fee72bd5ee37a71f51a9cf29647dd8
pmphillips <paul@phillipsdata.com>
2024-03-01 14:48:20 -0800

    Updated fr_fr language files

---
+++ plugins/mass_mailer/language/fr_fr/mass_mailer_emails.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Mass Mailer Emails
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['MassMailerEmails.!error.job_id.valid'] = 'ID de travail non valide.';
+$lang['MassMailerEmails.!error.job_id.unique'] = 'Un courriel a déjà été attribué à l\'emploi.';
+$lang['MassMailerEmails.!error.from_name.empty'] = 'Veuillez saisir un nom de domaine.';
+$lang['MassMailerEmails.!error.from_address.valid'] = 'Veuillez saisir une adresse électronique valide.';
+$lang['MassMailerEmails.!error.subject.empty'] = 'Veuillez saisir un sujet.';
+$lang['MassMailerEmails.!error.html.empty'] = 'Veuillez saisir le contenu de l\'e-mail en HTML.';
+$lang['MassMailerEmails.!error.log.valid'] = 'L\'option log doit être réglée sur 0 ou 1.';
+
