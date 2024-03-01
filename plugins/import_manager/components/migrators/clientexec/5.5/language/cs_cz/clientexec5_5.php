77b526f4e7031959271cb24512239d0d489dace8
pmphillips <paul@phillipsdata.com>
2024-03-01 15:03:47 -0800

    Updated cs_cz language files

---
+++ plugins/import_manager/components/migrators/clientexec/5.5/language/cs_cz/clientexec5_5.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Clientexec5 5
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Clientexec5_5.configuration.no_local_packages'] = 'Před mapováním balíčků je nutné je vytvořit.';
+$lang['Clientexec5_5.configuration.remote_package'] = 'Vzdálený balíček';
+$lang['Clientexec5_5.configuration.local_package'] = 'Místní balíček';
+$lang['Clientexec5_5.configuration.create_packages_false'] = 'Ruční mapování balíčků';
+$lang['Clientexec5_5.configuration.create_packages_true'] = 'Automatické vytvoření všech potřebných balíčků během importu';
+$lang['Clientexec5_5.settings.enable_debug'] = 'Povolení ladění';
+$lang['Clientexec5_5.settings.passphrase'] = 'Heslo CC';
+$lang['Clientexec5_5.settings.pass'] = 'Heslo databáze';
+$lang['Clientexec5_5.settings.user'] = 'Uživatel databáze';
+$lang['Clientexec5_5.settings.database'] = 'Název databáze';
+$lang['Clientexec5_5.settings.host'] = 'Hostitel databáze';
+$lang['Clientexec5_5.!error.import'] = 'Import byl dokončen, ale vyskytly se následující chyby:';
+$lang['Clientexec5_5.!error.passphrase.invalid'] = 'Je vyžadována přístupová fráze CC.';
+$lang['Clientexec5_5.!error.pass.invalid'] = 'Je vyžadováno heslo k databázi.';
+$lang['Clientexec5_5.!error.user.invalid'] = 'Uživatel databáze je vyžadován.';
+$lang['Clientexec5_5.!error.database.invalid'] = 'Je vyžadován název databáze.';
+$lang['Clientexec5_5.!error.host.invalid'] = 'Je vyžadován Hostitel databáze.';
+
