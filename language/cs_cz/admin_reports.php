9d04557d169f3a0cd23257b22829289aa92cd88a
pmphillips <paul@phillipsdata.com>
2024-03-01 15:02:33 -0800

    Updated cs_cz language files

---
+++ language/cs_cz/admin_reports.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admin Reports
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['AdminReports.index.customize'] = 'Přizpůsobení';
+$lang['AdminReports.index.reports'] = 'Generovat';
+$lang['AdminReports.index.field_submit'] = 'Export';
+$lang['AdminReports.index.field_report_type'] = 'Typ zprávy';
+$lang['AdminReports.index.field_format'] = 'Formát';
+$lang['AdminReports.index.box_title'] = 'Zprávy';
+$lang['AdminReports.index.page_title'] = 'Zprávy o vyúčtování';
+
