811bc5ffcc03e7d6ab48a87db6fb384370541cbd
pmphillips <paul@phillipsdata.com>
2024-03-01 14:46:16 -0800

    Updated fr_fr language files

---
+++ core/Automation/Tasks/Task/language/fr_fr/apply_payments.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Apply Payments
+ *
+ * @package blesta
+ * @subpackage blesta.language.fr_fr
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['Automation.task.apply_payments.attempt'] = 'Tentative d\'application de crédits à des factures ouvertes.';
+$lang['Automation.task.apply_payments.completed'] = 'La tâche d\'application des crédits est terminée.';
+$lang['Automation.task.apply_payments.attempt_group'] = 'Tentative d\'application de crédits pour le groupe de clients %1$s.';
+$lang['Automation.task.apply_payments.completed_group'] = 'Terminé l\'application des crédits pour le groupe de clients %1$s.';
+$lang['Automation.task.apply_payments.apply_failed'] = 'Impossible d\'appliquer les crédits en attente pour le client #%1$s.';
+$lang['Automation.task.apply_payments.apply_success'] = 'Application réussie des crédits en attente de la transaction %1$s pour le client #%2$s à la facture #%3$s pour un montant de %4$s.';
+$lang['Automation.task.apply_payments.apply_none'] = 'Il n\'y a pas de factures sur lesquelles des crédits peuvent être appliqués.';
+
