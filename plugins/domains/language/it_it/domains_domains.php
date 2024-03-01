e2e5377870c0ad1e46637ba0a0c29e19c7eec091
pmphillips <paul@phillipsdata.com>
2024-03-01 14:55:38 -0800

    Updated it_it language files

---
+++ plugins/domains/language/it_it/domains_domains.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Domains Domains
+ *
+ * @package blesta
+ * @subpackage blesta.language.it_it
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['DomainsDomains.!error.invalid_term'] = 'Questo dominio non può essere rinnovato per il numero di anni selezionato.';
+$lang['DomainsDomains.!error.invoices_renew_service'] = 'Il dominio non può essere rinnovato finché non sono state pagate tutte le fatture che lo contengono.';
+
