b3247f0ec2edff97a926d308531d7f51aa41f9af
pmphillips <paul@phillipsdata.com>
2024-03-01 15:00:57 -0800

    Updated cs_cz language files

---
+++ components/gateways/merchant/payjunction/language/cs_cz/payjunction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payjunction
+ *
+ * @package blesta
+ * @subpackage blesta.language.cs_cz
+ * @copyright Copyright (c) 2024, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['PayJunction.!error.test_mode.valid'] = 'Režim pro vývojáře musí být nastaven na hodnotu "true", pokud je zadán.';
+$lang['PayJunction.!error.password.empty'] = 'Zadejte prosím heslo k účtu.';
+$lang['PayJunction.!error.user_name.empty'] = 'Zadejte prosím uživatelské jméno svého účtu.';
+$lang['PayJunction.!error.api_key.empty'] = 'Zadejte prosím platný klíč API.';
+$lang['PayJunction.test_mode_note'] = 'Povolením režimu pro vývojáře se transakce odešlou do ukázkového prostředí PayJunction Labs. Pro použití režimu pro vývojáře musíte použít uživatelské jméno a heslo demo verze a také svůj klíč API Labs.';
+$lang['PayJunction.password_note'] = 'Zadejte heslo k účtu PayJunction.';
+$lang['PayJunction.user_name_note'] = 'Zadejte své uživatelské jméno účtu PayJunction.';
+$lang['PayJunction.api_key_note'] = 'Zadejte klíč API z účtu PayJunction.';
+$lang['PayJunction.test_mode'] = 'Vývojářský režim';
+$lang['PayJunction.password'] = 'Heslo';
+$lang['PayJunction.user_name'] = 'Uživatelské jméno';
+$lang['PayJunction.api_key'] = 'Klíč API';
+$lang['PayJunction.description'] = 'PayJunction ročně zpracuje 4 miliardy dolarů a rychle roste. Její technologie je ideální pro podniky s více pobočkami a více odděleními, které měsíčně zpracovávají kreditní karty v objemu 10 000 USD a více.';
+$lang['PayJunction.name'] = 'PayJunction';
+
